132107b30fe65998237c69c7d93f413fcc1f4c4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:51 +0000

    opponent behind matter tidy takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+print(time.time())
