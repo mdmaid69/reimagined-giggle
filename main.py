dad14f3f8e82fd44f55324f58e3956e2fb881bd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:00:40 +0000

    show pump reduction use sure

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-print(time.time())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
