7828899dffe00a16c529f387bb2908a0a56cfd62
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:53:06 +0000

    support keepout spite Bb load

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-import tensorflow as tf
-print(tf.__version__)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
