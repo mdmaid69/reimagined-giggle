5d0f4d6643a244519d63af84c07e7727981edbcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:27 +0000

    sport nearly reasonable mouth cross

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
