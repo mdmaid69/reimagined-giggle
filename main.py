93c8e1e02200f8cca4d0c0a12c6fa2eb2de112d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:43:32 +0000

    swim holdout add plenty general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
