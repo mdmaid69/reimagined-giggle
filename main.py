544014c5b6040e416d8ee521a43c1a9f4641a62c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:23:02 +0000

    comment approve exchange succeed arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
