f42749dd7e482d9eadb36c468ab195020afba518
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:27 +0000

    sentence profit downstairs sure simple

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_str(array):
+        return str(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
