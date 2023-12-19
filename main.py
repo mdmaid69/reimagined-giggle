5c60a1c07a0d7b2444d0122e0680db8ae441b4bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:44:07 +0000

    fear profit opinion corner exist

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
