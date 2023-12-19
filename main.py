dd39d167cc1c5938b27128c295e3651ab6db3c7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:41 +0000

    chance do tragedy environment refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
