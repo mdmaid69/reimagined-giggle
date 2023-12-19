5147752a6feb8c0378eb56fd1bf92fc0f2beeef0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:54 +0000

    argument typical shape heat by

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
