28a50bf24c92c64bb99c9533180399503d10818d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:22 +0000

    fly after goodbye low present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
