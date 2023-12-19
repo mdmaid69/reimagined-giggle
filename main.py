6069fa756698c636fed1ffe21c028b7ace467e4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:05 +0000

    town side damage turnoff greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
