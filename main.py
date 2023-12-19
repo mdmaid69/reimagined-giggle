7457dddbf95a5a19063276d6a2f742a556e9031e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:26 +0000

    bear understand would free holiday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
