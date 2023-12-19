ae12792b0a89e958625cae6ce7ed070c77ac370f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:12 +0000

    report night past necessary challenge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
