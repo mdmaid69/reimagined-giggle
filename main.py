e3ca17a19f635814bcb05dd65eec3226f364b4fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:33 +0000

    manner advantage street slowly enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
