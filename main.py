dcf90a53a15f4ee191f3ebeb89aa88fb8e336d7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:27:47 +0000

    subject wrap seriously exercise tired

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_list(lst):
-        return sorted(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def cube_number(x):
+        return x**3
