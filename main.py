0f9a996ab0951e770e506889fe8f0b4a35008030
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:51 +0000

    set manage company excuse lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-print(sys.version)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
