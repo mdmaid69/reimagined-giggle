b98fa099d27daf4807fe72f8a79182d3205d81b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:07 +0000

    country excellent prepare equal would

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_pressure(force, area):
+        return force / area
