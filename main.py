a4cfe5f0691a8cd8996281ee7e39d446d258f710
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:47:15 +0000

    strong for change issue store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
