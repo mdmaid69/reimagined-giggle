bd971137ccb69e629a76495a849fdcd1feae3325
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:45 +0000

    suggestion lookaround error coat answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
