db8607ee2fc1ee98937b1cf947fa5bc705d0ce8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:25 +0000

    mistake shake century community stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
