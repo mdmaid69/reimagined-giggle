cbc369f0227826c635ad3a6b933befcd38600a03
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:11:17 +0000

    lower defend grow among stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
