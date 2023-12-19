257fade70987ecd61619363bdee8987c70465a42
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:27 +0000

    Rr tap model scrap adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_exponential(x):
+        return math.exp(x)
