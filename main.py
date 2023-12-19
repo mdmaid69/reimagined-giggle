f121afcba6db7873face8748bfb2940cd504e199
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:00:20 +0000

    scream through comeup sore hour

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
         a.fromstring(string)
         return a
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
