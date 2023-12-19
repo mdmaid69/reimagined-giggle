24b7bfa3e94a618d5e6f322d90662c33d9ad18bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:31:33 +0000

    rain miss plant never step

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
