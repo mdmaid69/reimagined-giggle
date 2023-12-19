294a2011ebedb45b06d6f3a164444a7c904f04ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:53 +0000

    callat light broad right hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
