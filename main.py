ee8f1cbca0d7c8f60656c62fcc451add9a561571
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:25 +0000

    repeat condition mail position Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
+  def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
