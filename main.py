8c3deebc3e70428ee82723c2ce8f3e7120fb4866
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:36 +0000

    off bang thought dance list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
