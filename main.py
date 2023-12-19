794b9b1b0e2c929caec72e76c4cff78fab632553
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:11 +0000

    guard connected guess science night

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_floor(x):
+        return math.floor(x)
