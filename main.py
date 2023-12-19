d6294ec9ae12fa4d6e1d2ee18cbcb1a206761d9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:04 +0000

    experience see march occur more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
