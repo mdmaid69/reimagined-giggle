acc8a6e97914838ee9812678abaa73bcde1d15b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:12:34 +0000

    refuse position middle frame arrange

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
