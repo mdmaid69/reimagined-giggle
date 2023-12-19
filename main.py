2d61da1623d198731a51e9fd2594ca8d4e5ceb90
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:17:11 +0000

    cheque impressed pure deaf then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
