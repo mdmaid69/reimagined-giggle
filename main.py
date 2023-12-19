dacdaf53727aeba593fffff767881d4f2f088d5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:08 +0000

    way trousers just criticize environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_acceleration(speed, time):
+        return speed / time
