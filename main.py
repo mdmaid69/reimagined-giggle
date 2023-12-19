fa74d4a17d64fa24c9a7846aa43ac9023ae4f6d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:13 +0000

    genuine unpleasant Tt pass colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
