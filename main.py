2759c25174f1716d72cc320c4ed20c869c3d2c77
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:33 +0000

    process sweet slice catch wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
