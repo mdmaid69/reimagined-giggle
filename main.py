1820f5b2dd4cee34d910811efb6c604de862d44f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:05 +0000

    situation day funny fill almost

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 def calculate_annuity(payment, rate, time):
         return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
