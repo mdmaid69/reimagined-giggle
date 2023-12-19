ea40f02ca948a11f8035c341fc3ad7c6ed1d1b90
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:57 +0000

    apply catch every similar statement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
