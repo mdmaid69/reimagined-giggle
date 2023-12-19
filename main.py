139a192be66e2dcf177d7926ba3d2d824d00926c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:00:23 +0000

    nervous listen dinner worse swing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_even(n):
-        return n % 2 == 0
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
