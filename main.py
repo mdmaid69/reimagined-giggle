fcc1300a689b1266c73e404dfa1a9da81a06203a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:51 +0000

    listenfor pinch completely survive makeup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
