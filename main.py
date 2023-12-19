1e12d5b04e1e226ef07e3c4982eb97ccdde4b0e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:43 +0000

    bad freedom basically limit approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
