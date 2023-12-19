3d22bfa83a76415f1bbf69b441eedfce68077d03
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:33:25 +0000

    talkoutof blow intelligent phone future

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
