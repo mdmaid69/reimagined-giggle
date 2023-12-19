a926ca967d96c48409a844e86f12afbd3f8418c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:38:09 +0000

    waste hope quarter fly or

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
