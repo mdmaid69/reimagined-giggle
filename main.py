4a1e58ec095bf5bfcc89566aa64e9252ecd52e5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:28:36 +0000

    switch wave soft giveaway visit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
