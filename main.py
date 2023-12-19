564268cd43378f5e24c73cc32278d0e1ee69d826
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:21 +0000

    represent description importance single protest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
