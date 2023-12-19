3f7d1f9b4869c94ce9e0a36747262d006113b8bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:56:03 +0000

    model makeinto story attention sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
