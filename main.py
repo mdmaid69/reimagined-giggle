74a6f8357cb32f970d8e138a3ec1e8f1466d2bd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:31 +0000

    whatever general badly juice Gg

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
