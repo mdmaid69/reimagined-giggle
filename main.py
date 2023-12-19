e5d1541f3a76f58e7334e9ae0a5b372864f94cec
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:01 +0000

    secret dance could talk clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
