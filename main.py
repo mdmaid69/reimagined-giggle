184a39b5740ddc9d183c2b8659b1abacbc5c8d47
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:47 +0000

    noone weak deliberately bus honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
