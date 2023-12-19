2d14ce4fc762a7579d35555340c6e5f4af93e3ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:27 +0000

    escape luck concentrate bringup disagree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
