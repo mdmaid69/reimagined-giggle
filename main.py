55fbcf1397d6178bc7eb3a5491454410f03a84bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:29:00 +0000

    tear blame concerning pile idea

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
