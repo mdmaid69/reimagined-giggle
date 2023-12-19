2efd75527d5259c42b1b3a02a35875c3f29ca033
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:13 +0000

    school sting off clever their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import platform
+def get_os_info():
+        return platform.uname()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
