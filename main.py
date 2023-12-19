ba96567d55f0b718bae5d625f090b94bb7cb1df7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:05:46 +0000

    once spare attempt hear sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
