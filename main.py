635efebad6afa5a61c6611d034aebb374b8a3341
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:56 +0000

    worth impression operation land under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
