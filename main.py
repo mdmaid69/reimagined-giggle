8cc4519fdb307095eed6282837a2b615134aad72
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:06 +0000

    make holiday storm kick trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
