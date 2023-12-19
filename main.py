9aae30e5fecb62f10bca1a918836685ff344493c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:09 +0000

    fashion hole birthday pretend reserve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
