ec06a3053352d75e95351940fbc371c987a86692
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:32:27 +0000

    neither complaint telephone cheap scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
