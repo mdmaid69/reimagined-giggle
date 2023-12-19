165b4e57e966092df06eddeeacc4bc98a401b912
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:56 +0000

    lesson spend slightly burst nerve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
