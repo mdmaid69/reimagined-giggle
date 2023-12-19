06184e4c7904a7c064eed8424a21abd64f672520
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:51:37 +0000

    mention scene disappointed appear gentle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_stack():
+        return collections.deque()
