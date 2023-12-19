418b7b5fce96360c5b75a080370609979b6c5bcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:11:06 +0000

    appeal throughout accept half another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
