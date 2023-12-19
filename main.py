796bb57c649bcbeda77fa959370ecc1464f5d90f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:01 +0000

    share trust any cost back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
