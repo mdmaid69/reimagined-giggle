5eaeacfba265025b7e15e9f2d7d0a3cab1172e03
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:53 +0000

    unpleasant short head boil thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
