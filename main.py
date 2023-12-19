5e8d6ad10efbff4ff1cde43175dbefa540e04264
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:03:51 +0000

    Ll 570enjoyable brave convinced wipe

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_current_directory():
+        return os.getcwd()
