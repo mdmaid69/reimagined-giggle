4321b9aa622b2b1fa022f6fbb86c9e41bc506e06
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:32 +0000

    hide notice whatever better switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
