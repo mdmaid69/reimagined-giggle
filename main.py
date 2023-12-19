c2bce7882c7c2a2130456563d1caf18528c7ad21
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:04 +0000

    keepup know lookaround politics complicated

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def append_to_array(array, item):
+        array.append(item)
