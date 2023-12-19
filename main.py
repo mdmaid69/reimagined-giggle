fb50716f1cbd05f242f7776105037b102121fac0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:10:00 +0000

    problem likely willing both style

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_pressure(force, area):
+        return force / area
