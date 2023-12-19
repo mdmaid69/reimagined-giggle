a6cb84c2e0c396e9d24cc724b2dc9b72f3bc9c8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:40 +0000

    protect bush post closed branch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
