f415634de65cb435235001d9304099105e04ef3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:03 +0000

    terrible open defeat wild care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
