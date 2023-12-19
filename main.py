8a9c15cb8ffac6683f8f34b99c43f87177578854
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:52 +0000

    manner duty basis sight chin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
