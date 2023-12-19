bd2ac7c918193d0aef6a92854dc3f92f3c5acb61
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:10 +0000

    shut play comedown public disappointed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def is_even(n):
-        return n % 2 == 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
