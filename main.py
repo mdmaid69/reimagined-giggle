08fbca68771d22c17223d5896f846a68b3f06e06
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:38:26 +0000

    dangerous refer gradually control before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
