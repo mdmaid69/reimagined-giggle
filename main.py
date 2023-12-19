3a87bccaf12da73d69b014ac9c96cbac774fb834
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:26:06 +0000

    shock possibility broken breakfast brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
