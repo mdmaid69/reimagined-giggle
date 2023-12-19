e932085134f259ff70b46c811c3ca723bf8c7def
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:36 +0000

    result from somehow even pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
