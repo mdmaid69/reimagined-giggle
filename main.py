2cf404d1459434812a23ab0a316fd6530585f4f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:54 +0000

    Ff plate 567enemy mean creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def square_number(x):
-        return x**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
