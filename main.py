c984208c5da46e1cf7553e0d2a50a611ddc7d572
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:45 +0000

    identify scrape unfair base future

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
