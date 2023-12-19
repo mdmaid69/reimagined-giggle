a58b26505cd36d8afeaef3ae644ae17dab190686
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:17 +0000

    179bite again unusual loose unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+x = 10
+y = 20
+print("Sum:", x + y)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Cube numbers:", [x**3 for x in range(n)])
