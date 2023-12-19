cd6dfeb7e017340c314f94546275b13a5a7f97d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:11:41 +0000

    kick error union that difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
