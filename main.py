00c68fdf69d9f3dae7dd20dd6aac59c3fd8d199b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:34:34 +0000

    for knowledge body beside heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
