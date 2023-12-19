c36120b18c47dac58d081708b2c7e53d26fc5349
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:29 +0000

    decision style watch difficult tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_exponential(x):
+        return math.exp(x)
