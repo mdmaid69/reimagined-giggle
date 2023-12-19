f82ab974f7f10fe35b7f3b74dd88dc188bc3336b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:29:33 +0000

    hurry thought entrance turn light

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+name = "Python"
+print("Hello,", name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
