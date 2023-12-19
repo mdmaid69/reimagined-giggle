ef054b24a198b53f5194e1938996a08c06b5bab6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:19:03 +0000

    familiar they strong temperature purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_sign(x):
+        return math.copysign(1, x)
