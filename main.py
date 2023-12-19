0776ad1ed2902ae93a2feb95ceec5a7452cccf2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:06:26 +0000

    recovery especially teach unite swimming

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
