ac845f37654eefa664d8444b7402744aa01a2126
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:51:18 +0000

    unless equipment dare impossible rise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sys
+def print_python_version():
+        return sys.version
