84565708dee6c11e7d5f89abfdbcbf93df76f0fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:38 +0000

    gift responsible imagine interest something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
