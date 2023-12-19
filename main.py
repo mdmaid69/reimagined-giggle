2caffe28f30cb0f3306231e9b8bbe046063daa70
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:20 +0000

    freedom promise fail quantity tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
