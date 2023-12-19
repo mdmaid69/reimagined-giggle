71f4e56c11ffa79f2032afa385453a9005d149c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:11 +0000

    bang tongue thorough kill generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def convert_to_binary(n):
+        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
