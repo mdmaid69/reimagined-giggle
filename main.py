cac47c21f0420ba8f5f03c0d984306bf7aa44f0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:57 +0000

    friendly delight disagree underneath born

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
