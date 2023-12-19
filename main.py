33a0748dd5505dcc48f276de385cd84dbac2f121
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:49 +0000

    like deserve flow profit flight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
