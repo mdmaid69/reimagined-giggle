06e7ca3d72f15cfbaf25ebbfe3d15b9c986aa4dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:41 +0000

    scared subject crazy than near

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
