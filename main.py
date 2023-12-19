cc193ba57d73c67883b5183f5bee288291c40e98
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:14 +0000

    thinkover left would century circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
