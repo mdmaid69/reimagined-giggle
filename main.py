1857f5e1fbb98a675f5eb8275a3ecd12116df7f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:22 +0000

    force present watch 570enjoyable approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
