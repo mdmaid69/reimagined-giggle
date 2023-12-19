d34e1b744ac281e58d7e936619cf43ce0d2dba27
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:59 +0000

    style shape mass lift prove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_as_str(array):
+        return str(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
