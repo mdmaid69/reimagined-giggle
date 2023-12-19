963f851d0588e9544cad8ec7292a0671a809ad99
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:45:26 +0000

    guess no trousers reduction along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
