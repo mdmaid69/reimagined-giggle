ece270b683ec40abafb8f1595a15064dd88ac4bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:07 +0000

    eager I shape poison look

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
