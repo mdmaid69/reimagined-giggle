0a5012209c06cf69cab1d792bca062c738ca243b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:12:15 +0000

    unpleasant advanced disturb earn breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def convert_to_binary(n):
-        return bin(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
