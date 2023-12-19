7020e623a5484a13d5d96586295521b7af5467c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:30 +0000

    produce Qq hold glad concern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+name = "Python"
+print("Hello,", name)
