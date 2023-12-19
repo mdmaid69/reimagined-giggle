dc9017f903108d7cc7afa3d69ba62fc076cbf8ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:26 +0000

    mess field perhaps excellent put

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def cube_number(x):
+        return x**3
