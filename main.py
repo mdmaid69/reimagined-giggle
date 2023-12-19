e4e8ffe707511d540c7760c04801ce51f510b9e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:24 +0000

    goback(to) official buy believe attract

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
