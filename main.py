f4c9943dbae67f12fc4906d31bcff24cf6865598
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:14:42 +0000

    Mm move conduct to Hh

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
