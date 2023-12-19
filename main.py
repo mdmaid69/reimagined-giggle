1058a94561f1445d92bb7473222946a5389fb42b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:31 +0000

    wheel part prefer cross rely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
