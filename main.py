e9c1d83c21a750edfc48d7860dd4cf77b72a77b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:01:52 +0000

    damp challenge butter that them

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def count_characters(sentence):
+        return len(sentence)
