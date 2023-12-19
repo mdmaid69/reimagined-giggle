2e5fc613d4b05eec3e7ba57273c91976a33691d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:16 +0000

    plenty worth might bunch afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
