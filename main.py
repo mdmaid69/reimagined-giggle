9ac524a6fcb95a7ec0dbccff955753898d0aec6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:01 +0000

    mother Uu strange persuade space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
