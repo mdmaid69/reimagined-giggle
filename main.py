b69e8de3d98870824a2bb2868d15a23fd1903154
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:33 +0000

    Ee immediate without suffer make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
