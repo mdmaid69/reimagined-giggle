82e4dbf95a1398881d575756ce1ce8e6a7dab135
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:48:57 +0000

    broken interest may Ii related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
