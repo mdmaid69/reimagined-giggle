02ebe92fc61fa3aab6032f6b187a11bfebb240b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:02 +0000

    fold forgive progress intelligent early

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
