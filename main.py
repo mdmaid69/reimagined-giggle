89e8367cd28597dc6a55beabd5b639f31bbabe89
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:03:47 +0000

    judge miss mark nearly port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
