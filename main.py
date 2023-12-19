9b152cc2eb55b66aeb0bc6bfac921e7059d84bcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:31 +0000

    steer so include generally loud

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
