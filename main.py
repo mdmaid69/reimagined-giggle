ca85941b21c53dbf8f13057af5c19b4c8de262cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:43 +0000

    hungry group Kk wise wipe

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_acceleration(speed, time):
+        return speed / time
