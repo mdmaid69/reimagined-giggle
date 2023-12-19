3bcda2201bb96f67a61983e375f0906c179ac02f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:45 +0000

    free dare once 572enter open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
