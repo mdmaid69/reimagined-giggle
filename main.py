0f8a24134e46e5b1c6565ccd323a7c42c378a74d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:53:40 +0000

    forever discussion unfair brush miss

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
