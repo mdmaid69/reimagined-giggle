bc0c63f9e61b6feed6028f5bb59845ce55a259c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:09:45 +0000

    around form fresh star close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
