20bf585cc8ae98b185dcbdcbd51a15a95529e3ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:43 +0000

    evening ear platform stay would

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
