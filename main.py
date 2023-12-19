98ec7b7f479dca11272668ddf8752cb2cf95a146
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:33 +0000

    imagine into check explanation prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
