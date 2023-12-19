70c5868c4a8d8ff314566a7aa2573b473b7a728d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:06:12 +0000

    key stick relationship price glad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
