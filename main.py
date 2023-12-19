5fd0190e2e5a02aa208c3d2480ed59f4d25a1b2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:06 +0000

    region voice worth popular delight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
