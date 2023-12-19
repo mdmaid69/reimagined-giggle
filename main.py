03e384694c459795fba26056caa723a1c0aa704c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:58 +0000

    walk around picture benefit left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(lst):
-        return max(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_list_from_array(array):
+        return array.tolist()
