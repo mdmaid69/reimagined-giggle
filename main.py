e89c5840ef7f5a34e75f26d10b09cfde9231dd0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:06 +0000

    sleep needle switch study giveaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
