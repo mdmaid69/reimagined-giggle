3b0d04b2a920604d9668e3e7d524a69697a441f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:58:27 +0000

    classroom which hello small report

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import time
-print(time.time())
+def remove_from_array(array, item):
+        array.remove(item)
+  def calculate_area_rectangle(l, w):
+        return l * w
