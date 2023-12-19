08323cfdd3507efa8b2f1ae6c8df03908eb78f12
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:57 +0000

    burn shoulder congratulate behind neat

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
