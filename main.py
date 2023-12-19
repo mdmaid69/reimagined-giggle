f0368c15b0951cb03de48856aab1b9b678d6d39d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:27 +0000

    giveout still disturb realize callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
