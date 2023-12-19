6d1483ee6b56a4bc432d575f7be3d5803bb20aba
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:52:40 +0000

    pause your journey forget join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
