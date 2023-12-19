46cb9583827b0edb9191461eece958f49fb15448
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:52:58 +0000

    order ago ignore loud shape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
