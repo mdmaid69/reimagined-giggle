81f17791247a8559edf5546b3569c76fff3a9076
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:43 +0000

    various connect news solve window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
