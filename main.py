007964aa2869c07c5bef3a87ea7bf911cf81f04e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:22 +0000

    scene soft ignore dish weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
