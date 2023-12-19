a448a5d7b25bcaed1c2687013babda2534766ef1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:00 +0000

    definite lay spray deep rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import sqlite3
   def connect_to_database(db_name):
         return sqlite3.connect(db_name)
