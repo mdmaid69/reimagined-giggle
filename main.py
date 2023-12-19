3241f0c2c69de545fed34ac229ec5855dfb0f5bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:43 +0000

    welcome mess ball protection genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
