0ebd1d684bac62168f6ac4a115decce6725456f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:48 +0000

    characteristic chief treatment condition go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
