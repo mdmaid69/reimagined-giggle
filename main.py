cc76f571906736db866204c151084023f6a89828
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:59:35 +0000

    various block part ask die

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
