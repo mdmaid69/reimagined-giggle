159a2341fe6b2995ff5274b3f012040e7cac854a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:32 +0000

    short window when turnout emphasis

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+for i in range(10): print(i)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
