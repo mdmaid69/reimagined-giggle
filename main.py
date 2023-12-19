e5ebeaff217c3d5c62d4cedb543c5e7650b2205f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:02:03 +0000

    peace dish turnoff address fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
