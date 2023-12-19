af5e46f70c9daab26e399c7a693e326b4de5e296
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:16 +0000

    callback have Qq small breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
