9814a52faf4eaf2f899f9da31dcb976c86ab6337
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:39:03 +0000

    strike meaning phone busy 179bite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
