d0e78e8ef936440c40cad7666b505af9b376f396
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:08 +0000

    somebody occur accidentally unkind aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def divide_numbers(x, y):
+        return x / y
