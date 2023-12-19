1c31192854793d505f1ceedf161f55d5aef7b639
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:12:19 +0000

    call meeting last design produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
