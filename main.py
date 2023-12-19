29ffdc7659b2ba976996a49db758b7169eb4f1ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:30 +0000

    run calmly large profit us

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def count_words(sentence):
+        return len(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
