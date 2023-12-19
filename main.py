7718e0fa796fce6c69dbc1ea93894eca82a73b46
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:50 +0000

    joke manage all uponon certainly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
