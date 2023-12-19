dd47caa453b7358fa9f19d3eeb8a66b92e2f5d38
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:37:56 +0000

    pride conduct direct date sentence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
