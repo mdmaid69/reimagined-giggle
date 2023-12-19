68d748c1d20963b2d0d13e866c759c9ab4057b88
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:26 +0000

    blame thank rush such piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
