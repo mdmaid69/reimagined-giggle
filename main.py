7abd77a14def887e655fd5ca84be3ccbcb586cb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:29:41 +0000

    sound threaten hear clothes goon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
