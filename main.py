1499591419afb9bdf59df04befb288a5c4ddd902
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:26:06 +0000

    best try through side ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+print(math.pi)
