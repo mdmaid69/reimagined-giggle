712546ad18dbaaecf91e0fbfaf7f2be25f7f9a08
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:17 +0000

    serve pretty over gradually race

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
