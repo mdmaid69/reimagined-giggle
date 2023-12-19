7e94a6c127b050b47ade77d80156df09d94972a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:11 +0000

    anywayanyhow art left safe bar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import random
-print(random.randint(0, 100))
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
