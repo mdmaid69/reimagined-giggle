b7b0c080ff226bd023edddf94e7309b8cc7e5be7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:59:00 +0000

    kind sale decision dig slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def reverse_string(s):
+        return s[::-1]
