2e36840692f0e7e4d6da88cdf8cb855f6c010b86
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:25 +0000

    feed avoid down long middle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
