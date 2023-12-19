cc0c016c2e968391a2ccf9d94a99c3c8085cdb7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:04 +0000

    more listenfor table thumb hers

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
