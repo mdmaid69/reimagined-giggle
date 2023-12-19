266aa09f6de413242fd767cfb79198c1a8ac6362
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:09 +0000

    inquire Ff lunch empty fault

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
