a687e5661b6149e26f138e6e0fede860437fc510
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:57 +0000

    usual holdon weigh empty ready

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
