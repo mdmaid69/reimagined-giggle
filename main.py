90d21b94fc597afa24ed5d8ac4f4d5afeccadc16
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:31 +0000

    Uu mine port passage steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
