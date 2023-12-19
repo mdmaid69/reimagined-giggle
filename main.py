24101d9fd840aa4796cfe1c4f03c735ecff596b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:04 +0000

    fasten closely drink throw translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
