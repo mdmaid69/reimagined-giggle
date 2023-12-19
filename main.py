c1622c1a29712185d5a806cc2b1664ae13f838dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:10 +0000

    position opposite clear involve tonight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def find_min(numbers):
-        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
