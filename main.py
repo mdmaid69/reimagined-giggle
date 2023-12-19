b6dec8ffe9e8bb2ff111b0154ec2495f08bfa84e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:00 +0000

    loss within condition turnon hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
