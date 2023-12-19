62d3c7dc8e1b3f2cc7d896430ed59e9dd9ecfeaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:59 +0000

    getout another slice noise owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
