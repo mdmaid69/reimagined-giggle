552a4fbbec54e78fd4d2d1d3282b299576904401
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:28:37 +0000

    them sweet software negative crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
