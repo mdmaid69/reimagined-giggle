bf21e49cd4a6365848b03a6a1b5a209e86332f45
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:51:21 +0000

    runover fortunate roof minimum poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
