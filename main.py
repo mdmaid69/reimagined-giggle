318f7a2f9d63b58ac05b4f765948887d64cfd3fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:00 +0000

    such sale smoke shallow comfort

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
