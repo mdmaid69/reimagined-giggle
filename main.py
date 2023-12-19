a30db26c7c5aa6a7b8c0af75b41e6d08c934e54e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:31 +0000

    unusual dig direction care future

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_power(work, time):
+        return work / time
