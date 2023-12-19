e24ad7ff36df75ac93853b4be273db2dea024e53
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:38 +0000

    telephone immediate moon answer wrong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def remove_directory(path):
+        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
