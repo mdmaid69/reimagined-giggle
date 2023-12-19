2203a3be430ac5e76c945a1b7e9704c99b09d2dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:30 +0000

    protest during nervous statement gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
