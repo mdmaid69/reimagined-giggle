e401f143d659b5bdcb4166fcf3ccaae82324ae4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:04:33 +0000

    Ff proof notice goback(to) company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
