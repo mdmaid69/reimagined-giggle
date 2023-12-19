d8d7fe44e2f5409602d6ffc17ab2096b5ffb309a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:00 +0000

    he plate total road Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_work(force, distance):
+        return force * distance
