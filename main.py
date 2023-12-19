0a9504f45518878bfdf58bb8fddcd9d0bb802921
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:29 +0000

    row detail suggest scared tie

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def calculate_density(mass, volume):
         return mass / volume
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
