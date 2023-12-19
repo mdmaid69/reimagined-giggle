e38bc552ce9aa12b4277b78590019b22900ddad8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:07 +0000

    means getoff crash active unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
