704fb38cbbbd14229734b15a81dfe902a0563cfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:00:15 +0000

    really legal couple thick care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
