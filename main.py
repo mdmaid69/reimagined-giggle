c992d013db781e0c633dd7277e8f2b449d9769e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:10 +0000

    rub bang borrow respect reply

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
