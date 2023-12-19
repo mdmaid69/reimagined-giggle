6b9c16a27db391fb9ba8c83ec1d59ed9c88604a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:19:11 +0000

    wake constantly bright bored nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_string_from_array(array):
+        return array.tobytes()
