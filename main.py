9c04a69112cd09b9ca038254487a1d41236416cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:06 +0000

    upstairs time go prevent watch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-print("Hello, world!")
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
