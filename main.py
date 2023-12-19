10a006b236efc3a0fde748748cc5908ad8cc81bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:59:52 +0000

    demonstrate hardly bored motion obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
