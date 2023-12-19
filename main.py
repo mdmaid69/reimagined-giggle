7d72740ce42acc5286a07489ceb8c625d16bfba5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:46 +0000

    zero fall opposite cheap her

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_speed(distance, time):
-        return distance / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import re
+print(re.match("h.*o", "hello world"))
