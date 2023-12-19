956b1fd5bab99cf41cbf8d56e78031addabe5ffa
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:45:32 +0000

    fast unlucky bill embarrassing pause

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+print(math.pi)
+  def multiply_numbers(x, y):
+        return x * y
