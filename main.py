7a6644f94134d2df7c8040a2f51920d05a9036dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:27:41 +0000

    save include complete speak OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-print(sys.version)
+def is_palindrome(s):
+        return s == s[::-1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
