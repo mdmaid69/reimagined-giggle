dadb26837267898350b73510e5aa7e0721bc980e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:16:49 +0000

    cause content charge discovery sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
