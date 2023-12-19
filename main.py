02a3bcb5733a3eda489dc1c6f33d7b680a0c07ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:26 +0000

    difficulty recover brown conduct may

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def get_array_as_frozenset(array):
+        return frozenset(array)
