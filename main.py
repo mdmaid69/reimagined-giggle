198c4c400ed712919a9083264d8289e82bec95cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:15 +0000

    rank happen large give shock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
