31b306719e7d1c6141f1ff46137c624b39cca17c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:16 +0000

    course present who wherever they

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_set(array):
-        return set(array)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
