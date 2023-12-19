1cf4e8a6e5f02adbd3b30badd730ac637cb3ebb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:37:53 +0000

    apologizeapologise Ee unusual tidy telephone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
