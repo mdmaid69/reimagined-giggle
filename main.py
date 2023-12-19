0c7de7cd12d9c5ba399d3c32be79ebbb256e05af
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:40 +0000

    night send detail hope care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
