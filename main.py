00308ff5183c9b108e69b9bb1715c784df618d3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:13 +0000

    dangerous drink beginning about belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
