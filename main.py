a13f9a7c86ed99c11790f8037730ba6ca8861486
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:01 +0000

    profit there off content run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_octal(n):
+        return oct(n)
