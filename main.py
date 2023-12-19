e2b25d9108c3a6d7e8af58808471576fcd591c96
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:54:04 +0000

    slide brush might only different

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
