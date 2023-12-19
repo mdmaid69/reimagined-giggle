fafb43c57dbce881c81639526ed15f94a30b2d00
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:27 +0000

    grow church situation tidy behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
