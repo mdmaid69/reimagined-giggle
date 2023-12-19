815b7c0b0b3fa962b94b959a0a7bb52d1c6e7644
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:09 +0000

    press steam difficulty set space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
