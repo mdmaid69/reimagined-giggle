5fda8841959005a7689727654df2b6eae4a5e2bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:36 +0000

    time convenient keepaway he undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
