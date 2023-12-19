ca5bc23c2acee8ec5ac8b15cbea64ecbd6566a8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:00 +0000

    exist present flood tread memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def square_number(x):
+        return x**2
