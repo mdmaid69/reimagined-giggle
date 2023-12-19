eed33b9814b70c02f80ac5a7027a5c68d7551261
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:31:07 +0000

    calloff couple temper afterwards please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
