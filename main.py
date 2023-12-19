8d78fe8398e136b00a3af4dc709fcf56c1f8eae5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:15:10 +0000

    personal tear communicate plenty giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
