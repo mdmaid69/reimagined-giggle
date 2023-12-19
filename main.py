53568b818f056aaf63cbe2dcd37c515f537ea165
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:21 +0000

    swallow cause scale kind country

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
