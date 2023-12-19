d1e579b2b15747e7efbb46ba61ff26c31b47e221
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:17:33 +0000

    practical film breath rank still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
