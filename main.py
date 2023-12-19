72a09e4de345909b8a06812cf2065d3a5f1f0934
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:20 +0000

    opponent raise apply talk goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
