f4b35b7ec193d41be883ea436b62bf5186452b54
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:26 +0000

    join catch search connection neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
