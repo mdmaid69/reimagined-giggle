b1bc72eff4d369d434b4d3d1dae10299ca53ec1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:11 +0000

    subject amount strict as poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
