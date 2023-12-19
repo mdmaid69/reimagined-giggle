13f743630e3818e19292ea09833dcd9215e75806
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:36 +0000

    describe reply look conclusion plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
