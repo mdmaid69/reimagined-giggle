1bf7084ba720d852750cc11cbf6011878567c906
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:42 +0000

    smell meeting trip trip exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
