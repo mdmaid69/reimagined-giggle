1f05bb3019e1c7a91af715d8eca0ae232a7aad1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:26:04 +0000

    seem ring Ll scrap visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_array_length(array):
-        return len(array)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
