04aa2648f9d95d536176219622cb108ad0dfaf3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:46 +0000

    still praise ours relief aim

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
