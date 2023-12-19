7f8537bc8a576beffd688aa08dd8528394dda42b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:24 +0000

    reject accidental emphasize free bit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_as_list(array):
-        return list(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
