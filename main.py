40d439994be9d0e5227e925913c8325276b95ef8
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:30:08 +0000

    face path Pp against sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import sys
-def print_python_version():
-        return sys.version
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_user_list():
+        return collections.UserList()
