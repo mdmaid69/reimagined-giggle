25acf20259152f1d330273299e04aef25e3d986a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:15 +0000

    clear peace suckme bush impatient

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
