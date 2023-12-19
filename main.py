e1f0533f7200beb2651c89db680b209f9c6e4c67
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:13:53 +0000

    best anywhere free whichever cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(lst):
-        return min(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
