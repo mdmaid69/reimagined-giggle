fe403df6e98f26a4b37895effa27fa2f6899dc88
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:56 +0000

    Ss speak complain popular dear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+print(os.getcwd())
