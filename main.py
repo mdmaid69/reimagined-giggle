74dc3e61e29d929d15c45d0031dd5029f8184f3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:10 +0000

    male him age central shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
