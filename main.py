aedf7ff8846468be7b71fab9d02101146acbf54c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:06 +0000

    print exam sister side curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_acceleration(speed, time):
+        return speed / time
