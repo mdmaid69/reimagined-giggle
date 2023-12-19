61f9028393242fd718436b44a21073fd52644c90
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:21:54 +0000

    film common cheek lookup any

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import sys
+  def get_python_version():
+        return sys.version
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
