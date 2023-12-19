0c662091b5fd955c232e27e3a0b27215cc733cdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:09 +0000

    advantage generous this floor extremely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_max(lst):
+        return max(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
