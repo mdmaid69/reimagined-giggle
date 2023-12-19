6a29aa5db9a5a88dc2a186b0c901920b7b8916ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:02 +0000

    before round delight whether once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
