108b30bb5a788ebe274209ab3c95af5ee46b62f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:31:58 +0000

    convinced receive old mysterious business

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
