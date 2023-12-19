cec604ecae57f084e371ea261c70d2013b504c11
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:36 +0000

    bear telephone rush early fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
