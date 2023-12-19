2abbe98e332b0a1261c7b65da452ad4700b79094
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:03:57 +0000

    question unkind treat know unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
