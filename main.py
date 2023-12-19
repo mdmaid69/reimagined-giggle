4768825c4039e2d82a47c16b51a44ff45c50aa9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:00 +0000

    bin weigh sharp paper example

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
