faaa405bebe3d5081019d8a036263c8165206ab5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:55 +0000

    country set soon worse spin

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
