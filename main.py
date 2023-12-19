c18adc3a7a035ff9b141a45d1ee3dcbba5b34675
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:03 +0000

    door rub taste loud place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
