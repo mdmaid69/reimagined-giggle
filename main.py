d03f42e89f075fdb5c2ae655d70d89f499498444
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:15 +0000

    rise skirt instruction accidentally ocean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
