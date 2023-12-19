724477b37357ba37d9d37edeb46f1f18d586f438
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:52 +0000

    effort answer platform operate pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+print(sum(range(10)))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
