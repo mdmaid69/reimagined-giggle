948dee72ae8e8ccdb976e2e288fac33c70000f15
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:34:39 +0000

    scale right offer close able

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
