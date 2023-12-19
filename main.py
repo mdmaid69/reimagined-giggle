55317e9dda81704183fc38e34b92d83825027b31
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:43 +0000

    react tonight concerned peace save

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
