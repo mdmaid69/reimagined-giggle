738422d9e536e5b2dbb62a2dcfda44fe62fdf786
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:05 +0000

    intelligent along guard rub runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def split_path(path):
+        return os.path.split(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
