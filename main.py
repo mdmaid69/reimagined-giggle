ea63120e87fb383439e29e970d2acce6f1c43646
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:26 +0000

    centimetre match whether explain choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
