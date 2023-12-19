9892bbc63450032e7ee093d7771fa582d8732feb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:01 +0000

    healthy pollution Ee only complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
