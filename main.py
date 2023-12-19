8cc4c9e61137a900bc30135cb96334bfd5bbc7a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:46 +0000

    base enthusiastic friend all war

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def print_python_version():
+        print(sys.version)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
