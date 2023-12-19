109fe2fd3135ebbd22f29e2cadbfe8e080fe7376
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:54 +0000

    spring impressive choice win admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
