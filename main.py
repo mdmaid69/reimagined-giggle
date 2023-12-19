0861631b5786b79bf1e04717e2e3ec13e8dc69a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:55 +0000

    complain bread miss rather measure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_force(mass, acceleration):
+        return mass * acceleration
