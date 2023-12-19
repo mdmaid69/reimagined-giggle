4eeaa15d7d458cb4669f8b5dae638b0d5c79c8a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:50 +0000

    fire cruel physical well time

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
