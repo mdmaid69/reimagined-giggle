dad9f46cb35b4ab709851585ab148498de728605
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:27 +0000

    wish 570enjoyable bright dig tape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_min(lst):
-        return min(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
