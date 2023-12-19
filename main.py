080a0528de06e2ca9074eed26a440f984a252f08
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:45:46 +0000

    decision trouble reason impressed study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_length(array):
+        return len(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
