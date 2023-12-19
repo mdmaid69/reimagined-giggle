f72ec854277e33960b19d00c2415e9d4b02cb5a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:55:42 +0000

    accept method week model children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
