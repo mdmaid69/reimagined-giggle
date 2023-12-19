67d493ee6ecd1da42857eb6cce6aaebd97dd7614
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:08 +0000

    feed answer serious niggaacademy save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
