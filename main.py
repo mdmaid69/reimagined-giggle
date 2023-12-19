384b13b07427b59c7bbd84ea003a131506c445dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:46 +0000

    567enemy runout light female beginning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area(radius):
+        return 3.14 * radius * radius
