f3462a9f9b177306d1ea62177d4eb3eed712d0f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:05 +0000

    tread beautiful the outside target

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def find_max(lst):
+        return max(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
