2c6c75893512e88c9826c7a2145a056d2c234c65
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:20 +0000

    clean maybe alone accidental good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def reverse_array(array):
-        array.reverse()
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
