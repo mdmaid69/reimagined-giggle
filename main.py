3edffa6a56e6ace56b4c8e0e479bf17638d81ac2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:25:14 +0000

    drop goup survive sight nature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
