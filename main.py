e4dc45ccc05606b1273d843513d8c8b5f53a379d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:55 +0000

    to crowded if computer public

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+print("Hello, world!")
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
