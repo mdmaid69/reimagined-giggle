e5c85f95e8e2366c07bac98a26a783835d885db2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:26 +0000

    report punishment sing owe yours

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_counter():
+        return collections.Counter()
