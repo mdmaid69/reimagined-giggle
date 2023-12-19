ba83a4a5f2320873eb45c840144483e3617c8483
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:53 +0000

    performance though substance be statement

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
