8e9236ea0d2907310672fb71c0d7ee46bb38ebf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:21 +0000

    normal ocean operate scrape sort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
+print(sum(range(10)))
