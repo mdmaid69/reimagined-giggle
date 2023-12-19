a316e748facf75ec7c342dbb4846ef675cc52fd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:27 +0000

    minority content recent experience equal

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
