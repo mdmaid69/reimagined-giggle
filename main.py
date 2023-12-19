c8d32da0a11e3576af10276f396b31be8ba2bf72
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:30 +0000

    beginning early refer along backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
