985fe2ff71e4805ef1416ba7f24b3a0d26768fad
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:28:31 +0000

    piece unkind content leave division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_memoryview(array):
+        return memoryview(array)
