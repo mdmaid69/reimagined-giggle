0792bac607169023b1ed4559cc9f99505b1d805e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:20:01 +0000

    book medium sick run often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
