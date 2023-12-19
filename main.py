4ef863d1b6c4b89be4b7c05aaf055c057b4b2d53
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:15 +0000

    runout getout commit gate about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
