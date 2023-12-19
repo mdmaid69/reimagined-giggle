7f8b9f1e69f9eeef74e878bf4c3d0aa03e05a052
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:10 +0000

    possibility part ever there shoot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
