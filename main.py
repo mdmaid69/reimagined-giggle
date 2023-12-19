1acbe979cda4290d09883c52a947098e70e292bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:43:27 +0000

    Itself mark cheat tap Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
