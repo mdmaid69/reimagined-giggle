78807a1ef17fab94386ee9238b5aaedeaaf76aea
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:02 +0000

    crash future cross occasion call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
