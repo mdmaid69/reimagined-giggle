7978a058c42b165b1ed2aa51166baf81ce2c6b66
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:19 +0000

    autumn bottom everybody force speed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-print(re.match("h.*o", "hello world"))
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
