cac1309e3a7a36ec0378ef77e909c620aec2d4b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:47 +0000

    lesson connected black doubt field

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
