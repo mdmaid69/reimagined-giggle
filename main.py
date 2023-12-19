ee9a06457910c0972a7d85be505c5efaf64a38d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:33 +0000

    remove let fault mention public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
