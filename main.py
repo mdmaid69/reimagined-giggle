1fec6e380a3af02d6da992401b3f498dc456b0c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:30 +0000

    skill edge card replace than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
