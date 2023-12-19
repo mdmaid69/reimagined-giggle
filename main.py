0258cfbff8cfbe122242b0d9b9361177d7019781
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:10:46 +0000

    complain thinkover expression chat balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
