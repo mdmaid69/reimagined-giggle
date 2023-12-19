28f9f9427b02a08db6a328c7217ea5fde50a6538
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:36 +0000

    warn inside degree progress direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
