ae0e35371a4aa0992069e9e7d858d6e9f33dafb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:51 +0000

    pick fear charge thank hold

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
