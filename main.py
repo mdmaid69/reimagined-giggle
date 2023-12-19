f15b71f6cd6c103f2c641289246b9f1cb57ab837
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:50 +0000

    swing capital jump passenger pour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(10): print(i)
