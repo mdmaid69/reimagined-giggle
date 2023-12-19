71a3fc465e96923a96a4df2ae4288b2cd88420bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:58:38 +0000

    seem cruel century mark treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
