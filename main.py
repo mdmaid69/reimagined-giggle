4397ede2c5c7fde6393c0a629c0f8594a7f02786
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:51 +0000

    left drive sudden early catch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
