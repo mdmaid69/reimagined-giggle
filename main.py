bacb7e1e143e40c1ab3d4eebab00d71b90eaf4a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:52 +0000

    tall direct doubt adjective instrument

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
