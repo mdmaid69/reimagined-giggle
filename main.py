be9af37baf8048e387463c8e405a4de099cb695e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:02 +0000

    possibly closely pronunciation responsibility community

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
