5ae775f58587fae1cd3ed0bb6f448376191a38e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:40:45 +0000

    year please bush criticize hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
