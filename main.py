8474e53c6a3dea0b5563f9ab70bace06390edd11
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:38 +0000

    shout maximum steady purpose make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
