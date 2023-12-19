5d53937f690bac54ca62bf1d0d9e3fe1d254cfdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:24 +0000

    stay weigh interview tone centre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
