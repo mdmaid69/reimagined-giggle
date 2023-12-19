640ebb8a2a52a5960656222c654fbe6ddb94af71
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:02 +0000

    artificial statement praise on pair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
