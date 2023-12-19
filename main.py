e868ff63270e9eadbc4457b86a063b8fc287b064
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:15 +0000

    light great giveaway angry table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_length(array):
+        return len(array)
