a02b3e5ada9393766973c5213323ec2a2a60cc7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:01 +0000

    exaggerate wander maybe respect pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
