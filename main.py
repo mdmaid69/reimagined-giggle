23c557955db3e6e922e716f7a54a55bdad5a3d2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:41 +0000

    usually awake hold class Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
