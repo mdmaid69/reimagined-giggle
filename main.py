2777432ef254b88696529053a2d05e0e065270b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:08 +0000

    but scene mend bored insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
