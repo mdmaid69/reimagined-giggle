b49d72c2dd2094ab0e68e1b68995e506c0781a89
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:53:48 +0000

    glad form private few far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
