84c9418d19dff36bda45873e473dc793f63e7655
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:59 +0000

    empty hole getout pure original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+print(time.time())
