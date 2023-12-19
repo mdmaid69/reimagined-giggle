cde704dfcb20974a031efbf3861bec67e18a1167
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:24:56 +0000

    move deliberately image or too

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
