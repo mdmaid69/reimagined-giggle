a4685e9acc58cbdacdd463f2b8d63b8f181708c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:22 +0000

    leg pretty dance contrast care

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
