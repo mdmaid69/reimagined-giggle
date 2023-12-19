8270232782872ac32a508bbf0a446cdfc82ce7a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:23 +0000

    store connect trap computer holdon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def remove_duplicates(lst):
+        return list(set(lst))
