33f58a23f49aa50310023a1d0f2de7ba78a3e8e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:09 +0000

    in murder scene pleasure turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
