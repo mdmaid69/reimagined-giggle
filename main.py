360f6661c629f43fbfb242f27933941ba445953c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:26 +0000

    spray apparently believe blue occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import collections
-def create_user_list():
-        return collections.UserList()
+def get_array_as_repr(array):
+        return repr(array)
