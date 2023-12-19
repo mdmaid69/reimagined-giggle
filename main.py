6e6ed1034c5fe904cce334d45b42e6e8d18bee8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:24 +0000

    perfect people broad himself trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def count_elements(lst):
+        return len(lst)
