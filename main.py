dc6b92486a193d4be1e017d9da868dcf4d079a8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:17 +0000

    Itself risk relative know spot

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-for i in range(10): print(i)
+def subtract_numbers(x, y):
+        return x - y
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
