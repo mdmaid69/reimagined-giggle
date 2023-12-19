d9fcfcfeaa1f10260aa1593657e4667bcfedd69c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:32 +0000

    temper seat fear dinner half

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
