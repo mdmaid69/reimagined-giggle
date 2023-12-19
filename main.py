1a4263fd00a93271a22e7050f7ccd9962711fedc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:12 +0000

    step undo lend reduction next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  def sort_list(lst):
+        return sorted(lst)
