991711822e3ac313f5246a42a3187853aa517189
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:40:15 +0000

    comment maintain dish guess surface

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import time
+print(time.time())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
