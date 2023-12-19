2688e1a99783c5bb2d9e68c67e2fdf936050c5fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:09 +0000

    apart house step cruelty spite

--- main.py
+++ main.py
@@ -2,5 +2,7 @@
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
