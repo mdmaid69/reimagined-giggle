6b7ab1d8132b9b978896f64e01596b97afe7f1de
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:25 +0000

    use temper manner lookup plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
