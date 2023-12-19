df3a69659f2048b856a82fe625f5ae84d8a92f53
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:21:50 +0000

    stop put comeoff comfort care

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
