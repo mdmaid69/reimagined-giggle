bf524ad8d73d9042526b0de6cfe45d8df1fe842d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:41:44 +0000

    style pleased book throw before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def add_numbers(a, b):
+        return a + b
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
