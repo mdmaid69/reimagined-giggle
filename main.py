9d0d5d2b0c7d9aea7d96072b11ee9302a11b09d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:29:19 +0000

    goback(to) neither declaration side game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_unique_words(sentence):
+        return set(sentence.split())
