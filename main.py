ea8608e411f1fb18efe48089015f9c02ebc04df8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:49 +0000

    nice mind throw beyond fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def is_odd(n):
+        return n % 2 != 0
