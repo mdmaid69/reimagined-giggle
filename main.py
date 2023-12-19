2e7c29229ff9926d7e4b2a50da6b9467448e4889
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:32 +0000

    occasion oil bet produce 572enter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_density(mass, volume):
+        return mass / volume
