19fce19b6c50bda0304ba075d521bfba1af2e5f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:14:13 +0000

    do share pressure possession underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def reverse_list(lst):
+        return lst[::-1]
