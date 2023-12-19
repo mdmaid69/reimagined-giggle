dbe8900f12c6d92617b3182455848c103ad91637
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:58:01 +0000

    pleasure example lose behaviour section

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import getpass
+def get_username():
+        return getpass.getuser()
