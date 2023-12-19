30d450205f5e81bdbe0957cf5df40eb142c80cfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:47 +0000

    turndown Uu justice underneath look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
