36a2304d964cb02bea9d31c2e37dbc9fa247cf85
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:53:14 +0000

    neck thirsty cook interest brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
