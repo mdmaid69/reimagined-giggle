d6e47258ffcaa388cf2ea9fda3a39c78f131d80d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:57:26 +0000

    laugh store shallow close unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
