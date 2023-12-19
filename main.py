9604f29c0bd86fcdcd09e4f1baa0f1bbbfc6863f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:30 +0000

    ticket silent remember cum anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
