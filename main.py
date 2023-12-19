b77aec9dbe4ac0d781fa4ea280dbb093c7ec5a7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:17:31 +0000

    trousers least grand fresh discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_floor(x):
+        return math.floor(x)
