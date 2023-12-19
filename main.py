ead973f468bb02debab725a1733ee285b2b1931b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:01:57 +0000

    ignore connected behind OKorokay anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def is_even(n):
+        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
