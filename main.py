c73069a541c24cb713d1318635747601565a6e23
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:40 +0000

    period disappear square opponent medium

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
