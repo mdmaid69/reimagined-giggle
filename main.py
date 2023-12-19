158fdbf8818d3c1e6f99d0bdbe9e6b8d28c14bf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:14 +0000

    thankyou sharp acceptable urgent address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_stack():
+        return collections.deque()
