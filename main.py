2b95857c8add82afb54ed94fe6694030abd6c566
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:37 +0000

    nose us contact ocean along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
