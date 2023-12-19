b66bb8efac6e1d6df4224836d05a5c4121bbb003
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:42 +0000

    our please away force remember

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def convert_to_octal(n):
+        return oct(n)
