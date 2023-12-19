e4f6b9324c5decf2e0694d4ba43998fe997d27b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:49 +0000

    eager yet Cc inform turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
