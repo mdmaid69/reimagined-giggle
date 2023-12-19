cb36a6500037c5af350cf9091d74bbab278db86a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:00 +0000

    buy reliable tape start search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
