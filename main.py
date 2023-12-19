3cad22e12b398122a3e4165d464017fc846c84a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:06:10 +0000

    unusual feel control wander present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
