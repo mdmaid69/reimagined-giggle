51bbcd2bce005d33c65cfae65531d6e1110a3ee6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:39:20 +0000

    doctor person pleased set speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
