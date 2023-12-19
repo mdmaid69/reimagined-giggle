edcbace41cd7a7555c86554d61b6931e7b7a49fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:37:44 +0000

    cum as sale minute replace

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Characters:", len(text))
