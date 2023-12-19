01bb614794a15ed4ceed1ec2d35ef127c8d35774
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:38:50 +0000

    stranger getaway recovery develop act

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
