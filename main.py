2f6f85ffb1e370d82903992d4123b4134cebe9bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:18 +0000

    form match powerful entrance that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import tensorflow as tf
+print(tf.__version__)
