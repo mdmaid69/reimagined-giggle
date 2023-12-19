26bc1e4546f3d1dfcdc561e7730750aa029e34bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:34:41 +0000

    black computer speed simple consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
