eed326b83e0475597ffab532d330436bcdf6db6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:43 +0000

    sight possibly land excited responsible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
