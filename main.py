d4d5e44812687eb18e20eb04606011bf13489e32
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:15 +0000

    beside change abroad brush discover

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
