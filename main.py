503802d27aca805394ce762a03167e54d6473d89
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:12 +0000

    estimate cruelty necessary deserve forgive

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
