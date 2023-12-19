1d89b707055185b9628250ad72336c9705942c98
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:52 +0000

    rubbish usually professional corner convince

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
