155e6370c74410fba7e3fe7fa2c0c5f3cd192322
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:44:20 +0000

    vanish expect unfair unfortunate protection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
