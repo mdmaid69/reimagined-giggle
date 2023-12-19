070d680bf9469d5deae65c015fcdebea550356b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:14:45 +0000

    generous corner Yy suck finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def clear_array(array):
+        array *= 0
