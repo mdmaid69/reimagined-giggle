2c6f75e18b6f02207c1300e920c6cd2e8b3de91c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:38:48 +0000

    record shout bag effect speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
