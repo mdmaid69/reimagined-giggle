2c03b64a8be959c5c6c33a8b1dcbcf3f8e576d6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:48 +0000

    taste ocean vote means the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
