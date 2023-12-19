b3e4dde28969bcb08e62347ac7db46bbd11da597
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:45:03 +0000

    any lock wave this beautiful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
