dd08eda9bb4c5d61a002540482cac359a5c3bc24
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:04 +0000

    object separate image argue except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
