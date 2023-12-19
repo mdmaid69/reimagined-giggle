be72927888e3d03fd501f82c491c047ea8ef5863
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:49 +0000

    raise strict seat which guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-x = 10
-y = 20
-print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
