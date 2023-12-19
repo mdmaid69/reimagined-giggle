899b53058f6c72b19f14937ad8ea824527c88341
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:27 +0000

    sore usual happen breath building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
