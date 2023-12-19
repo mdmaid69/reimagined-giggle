ce2742d18b5490a2ad6a6f826aa5363368005190
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:38 +0000

    runover drink polite improvement pleasant

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def reverse_string(s):
+        return s[::-1]
+import array
+def reverse_array(array):
+        array.reverse()
