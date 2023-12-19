ba459025cd0454069351d0a8c9fe6510b9a10fb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:38:02 +0000

    nor better instrument contact test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
