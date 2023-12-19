cf2d079ce4b25e9c3d67861fa6d96d09538402a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:04 +0000

    of winter deliberate present will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
