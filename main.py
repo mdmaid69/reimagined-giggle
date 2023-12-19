13ae81fe20e5ea73e3509472ac46af9ac04d14a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:22:54 +0000

    only further something settle left

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
