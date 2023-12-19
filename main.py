ba791ad3bbf53594858e011e2d27c64f76e0a56c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:09 +0000

    relation support consist load failure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
