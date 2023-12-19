9172762e15a38a98f408ef3f7d5a21c0978037b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:30 +0000

    get character expect lookaround afford

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
