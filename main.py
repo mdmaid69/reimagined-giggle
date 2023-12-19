7d06d4a086c5080d775d3360ff7164ed2a9e459f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:52 +0000

    spin block sudden 565end pen

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def reverse_list(lst):
+        return lst[::-1]
 import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
