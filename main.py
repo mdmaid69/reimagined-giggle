cbd2bb772aea33b52d2e922ff8d3d5cc9ee977c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:17 +0000

    store sense tour anything attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def get_current_working_directory():
+        return os.getcwd()
