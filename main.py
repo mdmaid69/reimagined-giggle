1e2a0195730344ee73c3ce8ffa5b98f481eae7ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:54 +0000

    doubt continue ear rush by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_area_circle(r):
+        return 3.14 * r**2
