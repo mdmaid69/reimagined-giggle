aeadefe02e44907a74c782b0150549f39e52c5ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:00:32 +0000

    decide on famous black press

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
