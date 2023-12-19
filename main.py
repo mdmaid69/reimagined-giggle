d6bad7a108984db941b8e0d2cdb3dd07a3b8c765
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:25:19 +0000

    prepare idea mistake physical accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
