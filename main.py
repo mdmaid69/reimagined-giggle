9d2f367b505e3799005a6aa804694e21dabf96b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:05 +0000

    middle broad goout wire peaceful

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
