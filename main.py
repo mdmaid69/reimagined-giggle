3428458be9e15969b6206440811fe232a6f5194e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:07 +0000

    spare likely level Ww further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sys
+def print_python_version():
+        print(sys.version)
+import sys
+def print_python_version():
+        print(sys.version)
