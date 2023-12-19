3358eac5904a62e3858d13f4748bc5e4a829b1fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:20 +0000

    strength trust whisper search shine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tensorflow as tf
+print(tf.__version__)
