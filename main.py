7c46336d3d20b87a573a87847121efccfeb032eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:52:49 +0000

    steady shrink avoid show above

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def greet(name):
+        print(f"Hello, {name}!")
