471803414d87682d1739757c4bd4c477fe56c64e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:17 +0000

    bread game board shopping thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def add_numbers(x, y):
-        return x + y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
