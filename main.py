0efc511cc873b72b4bb2d0e968e05adf11bab79c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:30:20 +0000

    action principle fast whistle pool

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
