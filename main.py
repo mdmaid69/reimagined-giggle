d92254123d4faa41e393f75dbb45c7ade364491d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:42:02 +0000

    child memory weekend negative sting

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
