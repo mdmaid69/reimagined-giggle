1bec0a514bf04d13b661f171bf0e63aa8c8f4481
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:51:55 +0000

    hard belief cheek left form

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
