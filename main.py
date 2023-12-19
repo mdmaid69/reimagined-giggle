9510219e45fa6580e0921cc192e345d209cf6ca4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:50 +0000

    control gentle very shoulder thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
