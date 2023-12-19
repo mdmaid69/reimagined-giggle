edcb9e1f375048ccc61ff217af13fc9090d6b6fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:22:47 +0000

    switch circumstance message goup feature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def subtract_numbers(x, y):
-        return x - y
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
