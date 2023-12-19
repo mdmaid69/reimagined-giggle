2cae0c74fa1062a36f49ad934d902142f562fe02
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:32 +0000

    level key aim tight breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def multiply_numbers(x, y):
-        return x * y
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
