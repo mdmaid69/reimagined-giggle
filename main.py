1180bfa453aeb18c44e4e7c0cda23381c2082ce1
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:39 +0000

    fit impression cinema thought congratulations

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
