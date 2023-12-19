6a96d8e5033b05356764a9670278398f31e78aff
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:09:25 +0000

    proud within rubbish geton bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
