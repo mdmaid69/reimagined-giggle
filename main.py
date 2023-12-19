cc7dc7cfb0c06cce16b89ef9dad251ef28884afd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:08:15 +0000

    note exist their realize who

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
