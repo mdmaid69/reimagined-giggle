1d1656a74db8e33dd83dba7e7a649b11e3dfc896
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:17:07 +0000

    home entrance hold class start

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
