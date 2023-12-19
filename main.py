01d9069eec72a0519c406c6a5de86b730af5c515
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:05:36 +0000

    work train use blame calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
