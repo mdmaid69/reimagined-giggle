2ed9e20a95b7e84545f56d338257e889dfc0b176
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:10 +0000

    limit order ability arm still

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def is_even(n):
-        return n % 2 == 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
