a89513304f632a8d1ac883986001b226c8f236b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:49 +0000

    slip variety proud hurry upstairs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
