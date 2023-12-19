433201bc823e5dc4824018c2e9c22c2887ec4500
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:12 +0000

    along plan Oo black Jj

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_item(array, i):
+        return array[i]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
