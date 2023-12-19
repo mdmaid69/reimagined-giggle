54c8fa4254007c02df51e1e5efc13c0115b9e068
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:53 +0000

    generally bend stop well middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
