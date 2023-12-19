e3eab26d898942eac00e461e23925f5236caa8f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:48 +0000

    same please children right language

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-print(os.getcwd())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def sort_list(lst):
+        return sorted(lst)
