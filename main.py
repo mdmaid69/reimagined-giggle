6ca3cc2b8f08ff86e53c113b4d357b3467730d7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:35:59 +0000

    tight concerned emergency tense love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def sort_list(lst):
+        return sorted(lst)
