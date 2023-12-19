a5b55849b62229095405096255b911faa1eaecf7
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:41 +0000

    comeoff frame somebody trouble radio

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
