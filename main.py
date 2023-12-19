8eabc1446330b85b366891b04f6f6b9cb9ec7bcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:38 +0000

    couple handle trip comment fill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def reverse_array(array):
+        array.reverse()
