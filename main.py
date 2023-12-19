a11665d57b9b79cbd3f622a5be146a184a532c80
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:44:33 +0000

    favour promise instrument poison account

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
