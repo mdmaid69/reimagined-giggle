724d54d798ef93f33eb7d5897fcf8b6f53a6e154
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:08 +0000

    wide Ee necessary shut turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
