162530142913c4743861447317e48b306f7749b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:14 +0000

    any moment disagree few cut

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_str(array):
+        return str(array)
