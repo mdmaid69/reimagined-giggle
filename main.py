8e229caa57f4b4726217e35b109946d705de1199
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:55 +0000

    agreement boil sport turnout I

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
