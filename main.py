83363eb48642d623c0f869a5494abe58876b77a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:54 +0000

    179bite bring view below increase

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def convert_to_hex(n):
-        return hex(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
