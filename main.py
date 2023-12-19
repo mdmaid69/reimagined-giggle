22a1b1b9047be3ffd49ba0ec04cd405c9e93ba9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:00:49 +0000

    tune up choice there fence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
