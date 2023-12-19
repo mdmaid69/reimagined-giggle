3925aa7891dfda67f23242ceb918642d46419eff
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:09:54 +0000

    phone on goout worse smile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
