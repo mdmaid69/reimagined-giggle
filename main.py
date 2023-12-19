58fb0d02a31c84953477e3e1c2b68b8365024842
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:36:13 +0000

    only female still doubt habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area(radius):
+        return 3.14 * radius * radius
