1c2ce6de5a5fa5f7489237d91f5fdc3f8192efc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:00:43 +0000

    neat usually peace snow accept

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_speed(distance, time):
+        return distance / time
