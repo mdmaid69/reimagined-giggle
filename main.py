38652804aa02f59a8947dcfb52bb83120b1e710a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:36 +0000

    belong similar could shoot prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
