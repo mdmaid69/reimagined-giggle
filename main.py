df9c2d0ea2810cf4b349519c4c5de9247514d14a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:27:20 +0000

    rubbish careful pause anyone exchange

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
