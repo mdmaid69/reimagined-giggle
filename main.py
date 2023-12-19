c018bc9483d4d9f92f5e566826e4dc06ce6c4945
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:35 +0000

    scrape splash special Kk somewhere

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def convert_to_binary(n):
+        return bin(n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
