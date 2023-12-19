69977b3d62b2edae498336cdf39d12f41cc745d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:08 +0000

    bringup accept gate spring 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
