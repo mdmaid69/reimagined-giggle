852a487102e9662ae96fbe74769e92cc52f6ec2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:30 +0000

    blame interruption thinkover pure private

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
