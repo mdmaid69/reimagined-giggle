3113a79b574fb180febcbe8e01ce703a055640b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:01 +0000

    form share pick instruction mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_density(mass, volume):
+        return mass / volume
