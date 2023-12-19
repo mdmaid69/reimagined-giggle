b4c48c1cd0db36dc17795436d032eeaa61a579fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:41 +0000

    567enemy runinto music demonstrate runinto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
