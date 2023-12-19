5ed08523ef4fad98ca90ae061b9ff3a7d8b9e537
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:59 +0000

    fear juice this go stretch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
