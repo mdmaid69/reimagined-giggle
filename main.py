4b1e4764febb7a7ccb013b5e0addfaef7cf0d137
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:24 +0000

    rate calm still remark long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
