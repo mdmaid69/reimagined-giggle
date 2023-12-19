da26f5e8d88b793d6777b02ea8a16c2fd4f1bb16
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:45 +0000

    knock breakout crowd mysterious ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
