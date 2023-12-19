5f148564a2b4071eb412ee0e8388ea672feccade
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:51:16 +0000

    limit sense rescue urgent better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
