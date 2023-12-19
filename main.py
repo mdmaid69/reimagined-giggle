8035947caac43638905fea87c28542afbf6c283d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:20:27 +0000

    match private reduce with wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
