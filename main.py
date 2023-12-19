2dda5f0b0eadb8db96684ead4913df7a5bbc9866
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:34 +0000

    description urgent education use performance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_density(mass, volume):
+        return mass / volume
