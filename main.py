c8aec6e80bc6a3c55ca0c384293c3774ca15879e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:43 +0000

    walk interfere interesting machine instrument

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def split_path(path):
+        return os.path.split(path)
