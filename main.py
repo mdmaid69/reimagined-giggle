8f8270de3dac53d388e7e8fc991ea2a87a7e6ae0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:16 +0000

    shall receive be rise 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
