6bac09865bc1f44b5737d355aa9d851a13981ae4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:24 +0000

    section hate another Jj nearly

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
