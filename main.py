b6bd7d4c067f7c645ec56aa0d72ea9287c4508ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:54 +0000

    speed it accidentally get dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+for i in range(5):
+        print(i)
