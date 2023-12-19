5018aa53e581e54ef403998bdbf0aca83032f8d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:44 +0000

    tape clear question supply they

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
