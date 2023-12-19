e4349490c950f8537dac2794e6418b7f93f4fe80
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:26 +0000

    course juice nurse delicate worst

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import time
+print(time.time())
