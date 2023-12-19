e7ecf69be8e30e1859a6e1b68c86a399c1173805
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:38 +0000

    company space tomorrow fast able

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def is_odd(n):
+        return n % 2 != 0
