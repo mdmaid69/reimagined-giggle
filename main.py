515f79dedeba9c3a7dcbb8ebeeca0e0a1fb9ca16
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:02 +0000

    refuse it middle comfort suggestion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
