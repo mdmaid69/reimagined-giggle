dba94c27cf951471c92a8c1f71bdc308dc8543c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:34 +0000

    empty jump imagine pile loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
+def count_words(sentence):
+        return len(sentence.split())
