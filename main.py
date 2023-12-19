8f01a359538fe0b1d3e759362ad9e4471e1192bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:59 +0000

    react art shut more protest

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
