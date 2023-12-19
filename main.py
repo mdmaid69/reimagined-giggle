6509245c4bb14a4e6b7bc6c34435430d530b09d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:04 +0000

    comeout 566end accident air star

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
