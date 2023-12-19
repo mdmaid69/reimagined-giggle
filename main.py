58f16b7317a98fb0afa952f952b89fe396a557e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:29 +0000

    fear single very however tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  def count_elements(lst):
-        return len(lst)
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
