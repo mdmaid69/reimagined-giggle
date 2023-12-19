1f59ed6dd1ee8bcfa4e93abeb6cd74a76b8d66c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:56 +0000

    damage 569enjoy offence somewhere arm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
