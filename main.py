8ab6ec72f42aa7338e79d1b612121392246f8313
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:32:42 +0000

    bringforward spot danger peace ocean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_priority_queue():
+        return collections.deque()
