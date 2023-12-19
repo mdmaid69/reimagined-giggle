dbdde289fa01204483291df255c6b12a497e6167
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:57 +0000

    brief impression pretty surprise calmly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def is_palindrome(s):
-        return s == s[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
