9d9cfae27482b55ce6ada8e0e34acc9cd79cea07
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:28 +0000

    plenty factory flow regard wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
