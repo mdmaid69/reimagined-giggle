3333fad48cf5a3e36c765834a9dcdc7da8e5e58b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:35 +0000

    impressive noone shelter tough expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_factorial(n):
+        return math.factorial(n)
