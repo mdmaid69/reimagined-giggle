a06dfdc5062deaf78a98913f43a94da30e9ab262
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:52 +0000

    Ww hungry getaway over talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import time
+def get_current_time():
+        return time.time()
