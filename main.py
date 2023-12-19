8f7759eccb4dbc9aecaccefe2473ada7eb15ea29
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:23 +0000

    strong bus same gift formal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
