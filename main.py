c4f7321cf09a681855e0bcdb6ee08f478fcef1a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:33 +0000

    uponon enthusiastic surprise uncomfortable fun

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def square_number(x):
+        return x**2
+def convert_to_octal(n):
+        return oct(n)
