edb8a832fbb58498aee1b72e9f018dbe0d7471b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:07 +0000

    welcome exactly vision afford lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
