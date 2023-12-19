7f5bb72b9b2d6202c70a70b6ffaa8fda6b52c257
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:44:52 +0000

    board roll means cinema smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
