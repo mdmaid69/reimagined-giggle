6dbadcf35150c512eb04ff8bc897eec5b3053388
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:02 +0000

    ring easy example description contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def reverse_array(array):
+        array.reverse()
