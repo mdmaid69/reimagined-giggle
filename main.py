146339006effacf6ce79e3c905fcc1555d5094fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:39 +0000

    record tonight affect bang describe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def convert_to_hex(n):
+        return hex(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
