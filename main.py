15ad2c67342e6590ea0ae441934ee28765d8fd74
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:23 +0000

    form Ff fast term pronounce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
