56f74d64e64d29ea1a2f58f8192ee2836775ba44
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:31 +0000

    impression dick unkind live dig

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
