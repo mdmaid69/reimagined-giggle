57bac76b370aa609ff844c8cb385150b98fc6c90
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:26 +0000

    edge wild shop centre star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
