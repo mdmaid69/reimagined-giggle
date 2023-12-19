34f81ced15d6e7607419265964c617ccfb81f245
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:08 +0000

    quantity bang start full sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
