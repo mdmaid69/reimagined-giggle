a8b9790a0c64a6d195c015c0de9182b095bb8200
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:34:58 +0000

    day arm interview share shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
