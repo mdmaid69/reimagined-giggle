eb286c5dfa461fd6e0bdd6c3134922e7bfc85853
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:49 +0000

    play load count all before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
