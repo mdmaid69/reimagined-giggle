2cb7c3034beaab138caf0c514d85cc20949b8bfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:56:45 +0000

    worse afterwards amount custom rescue

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
