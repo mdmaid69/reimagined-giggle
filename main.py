840b53e8ce811073a3e8d6222b7f3fa14addbdc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:57 +0000

    reduction quickly across party show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
