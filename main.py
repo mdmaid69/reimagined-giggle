72da173ce568bc554d05ae81f66ec05474119801
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:03 +0000

    figure turnup wish fear compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
