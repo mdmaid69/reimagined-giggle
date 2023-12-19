13e1acde5d117528fdfa03a9af2f89dd1159bffd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:31 +0000

    warning down wash hers valuable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_volume(length, width, height):
+        return length * width * height
