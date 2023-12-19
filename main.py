c9c1c85c952f6a04a6acebacacfda8ce00669f7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:51 +0000

    runout polite easy excited large

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def get_current_working_directory():
+        return os.getcwd()
