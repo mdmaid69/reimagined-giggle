194ec743bff41fd9909f59a950e79c3f85a126b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:06 +0000

    violent lock say cum before

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
