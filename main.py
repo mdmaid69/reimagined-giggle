95206418daf8c3de047bf6eba6357ffe81285e16
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:22 +0000

    store forever basis identify combination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def add_numbers(x, y):
+        return x + y
