efd1891237259fded56abe071415b3065a9b16d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:24:13 +0000

    plate attempt this peaceful mad

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
