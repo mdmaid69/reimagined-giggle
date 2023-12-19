2c5d6f43eb9105011f786d1412cc070aabd564eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:42 +0000

    wealth person straight best slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
