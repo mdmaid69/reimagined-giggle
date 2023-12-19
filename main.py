b02af21bba05eb07db0b86425ee81a13779906ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:38:58 +0000

    drink improvement transport wide later

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
