fe29a2a493c62b5547e69df2ac6120ab0b031769
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:12 +0000

    pinch anybody rain father conversation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
