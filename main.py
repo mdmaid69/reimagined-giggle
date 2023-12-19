a22172a98ff0d061ac9b51739613d5ecd8250fbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:52 +0000

    closely worth treatment fix wave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def square_number(x):
-        return x**2
+def calculate_acceleration(speed, time):
+        return speed / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
