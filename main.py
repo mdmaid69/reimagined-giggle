eef13fb55365742f95aaca729f08576671683cc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:51:20 +0000

    block forever spring surprising wing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
