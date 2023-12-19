da7cf3b3b795ed5e79b6522d0f8177e9d59d6004
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:14:17 +0000

    description opportunity trip practise insist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def convert_to_octal(n):
+        return oct(n)
