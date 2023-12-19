a27a548b9323df19dd07ed47b587e1c35f9f622a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:23 +0000

    gift stretch well would demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import time
+print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
