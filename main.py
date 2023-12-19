af6ffa56765fd5f42a848c12036e2bf71cba1610
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:12 +0000

    entrance bread find hardly side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
