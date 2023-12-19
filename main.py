1880272032a6281d86c55fc91a85126e746d5f5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:23 +0000

    fortunately advanced teach say similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
