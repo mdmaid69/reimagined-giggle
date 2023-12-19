c403cc0ed5abc5027671c670740d19df1c7e85af
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:47 +0000

    fork mess whoever sing awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
