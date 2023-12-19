3a537d6d34509a7eba2baad5f4cdf7354234c04b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:02 +0000

    bit married loud sting stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
