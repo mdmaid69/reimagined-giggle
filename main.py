00feae9768ac84cb8eb6019e7e5678892c4d5d84
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:56 +0000

    muscle future attitude nor region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
