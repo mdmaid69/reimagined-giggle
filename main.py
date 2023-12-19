34380c9b884532463f4b7867f9f5a369640d828c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:48 +0000

    settle understand expensive order permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
