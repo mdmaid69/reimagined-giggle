ee58ee33d4b8b12c99073985c23d428ad36aa2a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:54 +0000

    argue motion involve family 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def set_array_item(array, i, item):
+        array[i] = item
