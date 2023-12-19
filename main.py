7576b0c8cad1f6724172731a96b88bfcc31adc2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:59:12 +0000

    tomorrow join movement month land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
