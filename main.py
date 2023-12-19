b8018ecea417c750028550d4ec2e6f32312cc1b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:27:26 +0000

    worst comfort furniture tongue chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
