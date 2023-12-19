f976026bcee15216dd05d1ded3caf589d0e844bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:52 +0000

    contents wild middle flood Yy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
