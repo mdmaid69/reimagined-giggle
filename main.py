ad2d3d43fa3f42d47c003d3fda25259219ae5484
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:25 +0000

    image patience material lunch creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
