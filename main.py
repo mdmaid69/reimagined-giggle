06c3d1b3ac8d005eb1d249a59d92ea240f9a61c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:03 +0000

    coat frequent drop large could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
