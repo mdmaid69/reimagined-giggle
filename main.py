faf61cc8c68693f937d00984c468f880aa351dac
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:12 +0000

    hear report section power book

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
