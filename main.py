eacca0bca25f392a984546b242315e96d6d01709
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:46:07 +0000

    relationship want respect win limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
