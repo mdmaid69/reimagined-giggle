5fadd8daf76619d15a838ac52b47630243094bcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:00 +0000

    cheat pen wake freedom beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
