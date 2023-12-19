3f51cce7081b188d7544b9dd7e7b79ef0c269b31
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:10 +0000

    extraordinary progress thought everywhere geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
