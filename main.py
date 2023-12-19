3f8e09b8afd9bd4fd9adb44327593d147c65c719
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:18 +0000

    undo well goup steep mistake

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
