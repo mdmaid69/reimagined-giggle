54dab99b46c92969cc6a0836b25892af067bbd96
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:08:16 +0000

    advanced I air bear least

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
