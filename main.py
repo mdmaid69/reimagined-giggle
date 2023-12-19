a8504fc35ca9267ac1279ff8e1e562258c75dfbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:19 +0000

    tonight brain interested comeoff knock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def get_current_working_directory():
+        return os.getcwd()
