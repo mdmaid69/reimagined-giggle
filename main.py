326d725c5b817d098473b914660ae383a3fee7c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:52 +0000

    rough bear accommodation maybe different

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
