7abfa67ee1da48e568579999edd58de97b2a3585
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:37 +0000

    date most hardly makeup learn

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
