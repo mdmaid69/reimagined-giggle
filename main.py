4d1f06841f5cf1bce649925b8167a5c8490977c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:44 +0000

    blame base shelter occasional good

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
