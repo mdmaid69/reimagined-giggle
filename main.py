9b6d96e0edbb8c7deeeaa26d90f677f708aad2fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:50:57 +0000

    smell lose fat combination win

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
