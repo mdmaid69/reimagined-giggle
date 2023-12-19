9c84f9055c805a70a9eddcf614820fed4fc78465
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:18 +0000

    hollow under rid married terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def get_array_as_list(array):
+        return list(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
