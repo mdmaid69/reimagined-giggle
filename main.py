bde0fa2ea3e6dd0db9e3bbe20cd109ae99eaa264
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:14 +0000

    pin suppose sentence hold transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
