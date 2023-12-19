ab8673cc0dad1eb3ddf1171f1ec12d814d0765bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:26:43 +0000

    view regret practice earth act

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
