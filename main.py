445f186ebbb524710ca1449b008fefd05066a4c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:54 +0000

    agree position to perhaps on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def multiply_numbers(x, y):
+        return x * y
