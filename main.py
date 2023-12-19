29bb4f928798e4669af1a4e9262918280d9cf317
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:47 +0000

    worth team willing reach slight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_bool(array):
+        return bool(array)
