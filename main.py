bc71928bebdcb8e85dd68d6d1cb08f8f197bbc89
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:41 +0000

    lesson branch high treatment calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import collections
-def create_queue():
+def create_stack():
         return collections.deque()
