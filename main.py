7495ea98e82a58d63887cd9b19862064886d8903
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:47 +0000

    geton awake effect case standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
