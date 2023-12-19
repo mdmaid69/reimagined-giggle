aae11c466c431195196fbcf92e8ac327ab48ec9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:27:51 +0000

    meet contrast tone another private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def convert_to_octal(n):
+        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
