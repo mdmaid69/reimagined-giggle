85794ab74925b90c637d16de482d489178d14230
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:55 +0000

    improve muscle skill waste final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def sort_list(lst):
-        return sorted(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_length(array):
+        return len(array)
