f776ed9483bbc48aaae06378a8e748f8f5bacf22
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:51 +0000

    kind advantage public invite about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
