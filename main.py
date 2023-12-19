bb83826414d7f4dde51b79eb3fcd142fcdd134aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:10:30 +0000

    trade arm activity command blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
