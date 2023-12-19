4e552e43cf3e5044a95d1b6c1d875c7051cafa8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:00 +0000

    material line touch vote party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
