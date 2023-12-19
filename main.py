1afd33358d7a029ca8fbc9b6cc2cf729c08be073
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:03:11 +0000

    stamp quit term stretch trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
