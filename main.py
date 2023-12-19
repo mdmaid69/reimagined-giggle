67365df63fce7877c47275d3d5fb1edd5e4bfd50
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:03 +0000

    pronunciation keen bath see sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_rectangle(l, w):
+        return l * w
