4a37627fef67f89cfaf03755ffd9dbfd4b58c402
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:06 +0000

    how ready usual its design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
