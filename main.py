b8c8b62ea158ca25f513731e46ce97e2d82b7671
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:08 +0000

    children ocean soil peace last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
