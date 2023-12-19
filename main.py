93b43c865fa6d75bedb11d306b2beee9c93b72ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:23 +0000

    turnout appearance face mad yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
