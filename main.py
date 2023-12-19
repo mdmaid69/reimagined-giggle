4619f067865bdd4816c16facba4b461e9a8971b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:31:50 +0000

    sky strike bar excuse before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
