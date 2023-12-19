9a38511350a13c8b12265002e429e6ff7817e718
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:15 +0000

    check pull progress pronunciation gas

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
