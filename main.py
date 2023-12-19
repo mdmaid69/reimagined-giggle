e6ef6e8cad20db215d505608b1ec05eca8782aa6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:34:01 +0000

    lean under ilovedicks preparation stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
