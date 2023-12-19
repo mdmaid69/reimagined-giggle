f2ee6bfaff9f220bf44f71b4545e87fb63456790
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:08:42 +0000

    wait that warning serve takeaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
