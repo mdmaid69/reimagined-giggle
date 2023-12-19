c19210fb57e17e3a57d8c3a3cccab869be1e1f89
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:20:33 +0000

    thick divide until visit traffic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
