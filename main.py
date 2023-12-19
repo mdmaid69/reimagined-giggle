9f66cb00dca3e58c56bd92d3177097c365044bfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:07:56 +0000

    unlucky imagine grade project fight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
