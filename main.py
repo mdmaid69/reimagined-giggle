fc133e3c958e590df8dcc8950826b7996fd2f2c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:39 +0000

    immediate suck keepup imagine goup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_hex(n):
+        return hex(n)
