6f49ae1edc6c2d8c644f65ebc6828c95627535ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:31:59 +0000

    convince before nigga into face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_min(numbers):
+        return min(numbers)
+import sys
+def exit_program():
+        sys.exit()
