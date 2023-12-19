28b12f6e5eca5d1164fea2b48d2955ecc7858dec
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:08:32 +0000

    own either few late damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
