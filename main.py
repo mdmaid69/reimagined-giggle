5f5eefa3db5b2c35e6d88e2cadf61fb5e0693212
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:23 +0000

    better generous connect gap settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
