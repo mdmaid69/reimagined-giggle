1a22f60e9bd364a5b58d7ef0849d016e3aba1126
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:50:16 +0000

    naturally shock little except safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import sys
+def print_python_version():
+        print(sys.version)
+for i in range(5):
+        print(i)
