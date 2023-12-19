6efcac1dfc18ee60f2c8ba82b4b960f7f65255b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:08 +0000

    appearance accordingto mark path through

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
