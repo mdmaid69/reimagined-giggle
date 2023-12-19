1b7b1e08e90a5b6a7a95a350f60130890a2628e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:55 +0000

    week male cinema suckme low

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
