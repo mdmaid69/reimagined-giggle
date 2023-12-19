95ad16d26ca7f8a92b03c575562ceb863cb7a302
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:35:06 +0000

    about cook own glass quarter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
