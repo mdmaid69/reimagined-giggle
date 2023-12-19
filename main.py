e35a150480b9ff2e648fb42723b98f9ab1290115
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:18 +0000

    bored forgive thing human duty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
