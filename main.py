79563424dafaaddb15cd799c6e924f591cbb95f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:42:18 +0000

    wave course fierce getoff polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
