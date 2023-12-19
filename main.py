6e56f52bf6ccfb31fd337c78e9cceb842bcd1f27
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:07:52 +0000

    571enough cap range project use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
