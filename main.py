a1f1b04e5e29abf5777cc9ce366754d7095bcf24
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:03:56 +0000

    then pride night bottom will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
