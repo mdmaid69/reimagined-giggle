6ae9838c71fae93512a5f931b5414f9758bec77a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:39:48 +0000

    fond safety chat forgive climb

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print([x**2 for x in range(10)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
