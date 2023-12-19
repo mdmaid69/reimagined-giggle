8402bd1720ea2c2d4b7ad0c68ff1f8ca4b2884b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:59 +0000

    doubt land eye glass breath

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
