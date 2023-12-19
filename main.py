9ff0d7f77ccc86aa51fc88096454bbddb5e0fcb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:18:37 +0000

    language post face beat music

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
