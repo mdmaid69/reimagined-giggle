922be3b0950a8d3039acdd2e60ca526a71854301
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:35:19 +0000

    develop divide soak square talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
