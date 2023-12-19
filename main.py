fb0b5af023d3fe4fd9dcc25575b1e80616aefd84
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:18 +0000

    pick tell dare label shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
