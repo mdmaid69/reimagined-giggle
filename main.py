ada7d24b139beca358b0212ec89d44129c44b31b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:12:09 +0000

    odd pipe thirsty statement before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
