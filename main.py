9f11ae4a217b164953fed189d57db33502866054
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:41:58 +0000

    567enemy tip stay neither sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
