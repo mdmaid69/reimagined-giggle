2fa8bd72462dd278cbdd68a17dfc408b7512de31
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:42 +0000

    problem middle movement Qq die

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
