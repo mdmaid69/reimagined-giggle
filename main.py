775107c95ed705ba98a7695ffd23f09dc43fa253
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:49 +0000

    post own claim sink takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
