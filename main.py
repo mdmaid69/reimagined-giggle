b91b04ded064caf8679f0137ac68cebc8f329a2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:26 +0000

    difficult freedom effort rid very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
