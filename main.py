c39232fae3c745265102f0129a3c30540a56e21e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:27 +0000

    home quick once common much

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def sort_list(lst):
+        return sorted(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
