dd66cb51f135779e8172cf38eee4f7b0044ff259
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:36:49 +0000

    phone mind range activity used

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
