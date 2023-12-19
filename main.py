4a82bf344793a6afec80c0f29768c673fa865eee
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:52 +0000

    after occasional state fortunate dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
