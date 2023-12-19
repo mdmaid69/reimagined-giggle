519140cf9030da444d9c34b6f3abedf38bd2e14c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:56 +0000

    favourite pollution regularly appeal fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_int(array):
+        return int(array[0])
