54023907a7a983cc04d6a33d29dd5e01bc08b02c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:47 +0000

    fold experiment connected middle quantity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_even(n):
+        return n % 2 == 0
