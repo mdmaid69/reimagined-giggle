680202833249015a8813974f83718093768ad4d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:03:36 +0000

    dinner square smell centre boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
