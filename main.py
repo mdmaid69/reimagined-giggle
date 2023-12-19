6d53de33944ead1bc16a98ebe8f07670a899b04e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:28 +0000

    before pattern rain divide punish

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+print("Hello, world!")
