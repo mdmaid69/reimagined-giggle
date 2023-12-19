e1504aae97f7db5dab076ce64998df7aa2a4de81
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:30 +0000

    platform cover shape otherwise exactly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import sys
+print(sys.version)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_sine(x):
+        return math.sin(x)
