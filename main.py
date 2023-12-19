21bc51d5dac5f66ed0d560b9cd0276ed6399d27b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:46 +0000

    measure snow success emotion record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
