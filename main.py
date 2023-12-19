6a7379c0d66b85f4182a916e3c615bc2541341d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:53 +0000

    ought so team it soak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
