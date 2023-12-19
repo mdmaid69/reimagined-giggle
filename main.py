89852d6bd584ae12b7f0db16fb83a7662633948b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:38:08 +0000

    letter tour impossible press company

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+print("Hello, world!")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
