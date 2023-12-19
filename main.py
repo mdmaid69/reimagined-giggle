a202b492d960ec7b672bf2cad2e84b920ffc4f1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:12:13 +0000

    idea 565end judge string thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
