fafce78b7ea1c758cfab761c56f30dbd52fc6046
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:05 +0000

    sale stretch other business related

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
