de52ff808ae24d0b7ee4c15eaeb54bb8b140df0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:59:58 +0000

    arrange house that means aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
