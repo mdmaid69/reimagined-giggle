5d359d6334bbc6fc580dd2911f18c8e2cd0cc933
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:55 +0000

    march firm sweep building nature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
