99299cccd650a3e37fe71c28d3070bf1916e7c94
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:28:51 +0000

    win smoke rate brief section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def is_odd(n):
+        return n % 2 != 0
