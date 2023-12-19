b234faa0c714e1b0383d44afed6654ddf317db3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:05 +0000

    juice possibly tear dangerous risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
