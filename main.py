2d754db3cf077f251075c7e39cced37cfb83e162
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:35:38 +0000

    shall test shut turn somehow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def roll_die():
+        return random.randint(1, 6)
