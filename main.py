57f94a055d4e307062326c0929ae1025896fddc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:04 +0000

    hope wash aware closed tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
