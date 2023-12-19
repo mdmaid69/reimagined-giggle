642efe8764c6aa906710a010384df98e2e72a139
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:44:59 +0000

    lock relative deep how bye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def divide_numbers(x, y):
-        return x / y
+def get_array_as_memoryview(array):
+        return memoryview(array)
