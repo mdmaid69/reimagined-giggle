cdf48e460ab7324e0e56ced0d5fcb9f90875db1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:27 +0000

    press tonight loan church cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
