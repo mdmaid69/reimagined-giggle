bbc6557bb09a4e4d09ac0518614f0f5167ca1990
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:19 +0000

    cross space attention contrast one

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
