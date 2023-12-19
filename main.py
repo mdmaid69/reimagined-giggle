d47aaea9642481546bbdcd5906d8b8eb69d442ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:18:22 +0000

    charge voice taste body behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
