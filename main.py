aca95ca81475948b79ce48731d22649bf26b70cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:28 +0000

    yours once bang excuse look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
