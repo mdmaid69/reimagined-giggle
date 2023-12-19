d43360b2bc29cc641a28af3adc6d9ee312eda231
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:50:04 +0000

    join bringback responsible purpose rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
