b3169f2b767f0f75b3dd0e34f247cb9ef4b25fb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:22 +0000

    curious be many understand need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
