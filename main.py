1b3eb7624f8911e79246112b5ee575085e490670
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:34:18 +0000

    loan deliver moral bit preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
