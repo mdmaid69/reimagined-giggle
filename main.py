a0edfabe41449dab659e4d297374f338bcb3cb47
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:37:44 +0000

    progress call runover Ee impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def reverse_list(lst):
+        return lst[::-1]
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
