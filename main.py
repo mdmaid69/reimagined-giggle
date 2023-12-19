5675935d88d52d6b46fb878ad1127824efc29a2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:50:11 +0000

    cry tragedy comeout stage relationship

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_float(array):
+        return float(array[0])
