b1d2cd8bf886cc015006a32797208917816e4997
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:01:51 +0000

    fall anyone everyone except wing

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
