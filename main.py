0767b335032a6fd320ff81a20c31abde3c4adcd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:57 +0000

    proud uniform hard heat control

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
