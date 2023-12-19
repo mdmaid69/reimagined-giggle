97b6f461ff0dccd6404c08353c39bf2006958641
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:01:45 +0000

    traffic degree season size Zz

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+print(os.getcwd())
