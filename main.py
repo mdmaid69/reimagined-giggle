1bd4722181eb6c716f33ca8e5876f205277f2541
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:34 +0000

    environment worst lookafter Gg official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import sys
+def print_python_version():
+        return sys.version
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
