7e7b5a6a1ce69bdd936589e5ac8a7f477c520e82
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:22:12 +0000

    married television door yesterday likely

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def count_elements(lst):
-        return len(lst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
