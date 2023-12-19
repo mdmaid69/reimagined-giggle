51a7dcd2f32e6cc85903bc49db7577979d2f3bb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:49:54 +0000

    miss anywayanyhow skin forget central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-print(sys.version)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
