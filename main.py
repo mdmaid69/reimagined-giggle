20aebc46fe25d45e8e3fc878769b0d88cab42e6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:56 +0000

    club switch loose week fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def find_union(list1, list2):
+        return set(list1) | set(list2)
