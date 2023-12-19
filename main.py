d2acdd43565007e5721ea0c092181c47934d2a69
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:31:44 +0000

    save little look minute bill

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print(sum(range(10)))
-import time
-print(time.time())
+def calculate_pressure(force, area):
+        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
