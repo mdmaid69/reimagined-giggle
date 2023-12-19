eef73d1645593256f07a00aa0d6d0afc3206ed71
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:13:54 +0000

    sister trust feeling body confident

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
