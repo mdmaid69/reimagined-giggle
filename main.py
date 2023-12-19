ab5bb6302fcf092f9afae8bf1cf06ccf7b234e12
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:41:51 +0000

    loud breakfast subject tongue share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
