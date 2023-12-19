a9d6d5a9e982caf819b1a995a7dba7ca687b84d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:36 +0000

    but at crowded thorough doubt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
