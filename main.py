d79954559072bac537b7390601d4db78728a62f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:37 +0000

    566end comefrom upwards ashamed side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
