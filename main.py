136257f17a49060b9a217825bcd7765a08d6c06e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:04 +0000

    crisis clever chin right wash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
