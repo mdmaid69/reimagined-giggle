31559003dc945a5acb1e945b8ad8ecfe2dd99237
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:33:41 +0000

    practice takeaway maximum store might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
