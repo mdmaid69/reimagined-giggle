853a9b1671ffa477eaeced06d92bdab9856491d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:31 +0000

    judge urgent last recent Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
