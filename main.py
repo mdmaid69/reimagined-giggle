49e6b7736b3fe1cb0208c72571777f661842d3f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:58:59 +0000

    fetch ready guide year connect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_string_from_array(array):
+        return array.tobytes()
