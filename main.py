989798e72c6f6614100be7cecd62bae1e953ff81
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:43 +0000

    escape trial within clever along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_bytes_from_array(array):
+        return array.tobytes()
