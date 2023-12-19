e681970323384acdf3fda2b918471ad69a16dde2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:18 +0000

    must why discovery factory neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
