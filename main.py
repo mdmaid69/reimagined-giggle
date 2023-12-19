13368657802d9aa7da5e6dd595438e6cf6a70139
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:12 +0000

    from myself because level funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
