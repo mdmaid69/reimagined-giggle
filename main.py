8491a71901e76344650981e67a0a87a210cd23c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:14 +0000

    skin 179bite wear without contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def clear_array(array):
+        array *= 0
