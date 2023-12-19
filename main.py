3f26c4ef0254e9d543b1e12c0b4a3826dcce7dab
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:47 +0000

    ought square muscle opposite national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
