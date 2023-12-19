c51cfbd1b8b77f42143a824feb1b546caafd4286
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:57 +0000

    address bad joke goup mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
