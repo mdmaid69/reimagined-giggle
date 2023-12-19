502a23b525328144d461444e14f655a0025187f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:04 +0000

    after breakup fold though blind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
