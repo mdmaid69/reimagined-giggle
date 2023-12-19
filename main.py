1bc483f73ca0d1a169b58bdfbc1411d444100c1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:01 +0000

    hurt check damp pain proud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_list(array):
+        return list(array)
