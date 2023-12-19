8570ce092eb3f0165f4a1722388629851cd0b225
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:51 +0000

    generous division division could fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+from collections import Counter
+print(Counter("hello world"))
+import os
+def change_working_directory(path):
+        os.chdir(path)
