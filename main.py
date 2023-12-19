3e5383b7b93addcbd7ae0dec94b9ddc293261b29
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:24 +0000

    game corner eager pause seem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
