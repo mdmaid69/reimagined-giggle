10d306dbe7c55d3367a095c7c551f49b1801e633
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:23 +0000

    order question giveout rise lean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
