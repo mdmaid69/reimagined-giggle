d2bfaf3947494ed4b245e65ee4347094db71b021
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:13 +0000

    variety special bin impressive much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
