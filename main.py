be302641efbfbf412a2d1ba3c7a91fb824c4fdc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:18 +0000

    whether visit entrance wash reserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
