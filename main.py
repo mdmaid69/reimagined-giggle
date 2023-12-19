d7ff6a1c0b3f64280c7643c7e8bdb5ab6967f573
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:06 +0000

    write minimum clothes interruption it

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_binary(n):
+        return bin(n)
