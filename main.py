8f14fb2b727f9926c3d56c8a2ca1b803a966caea
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:12 +0000

    word lost tread unhappy work

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
