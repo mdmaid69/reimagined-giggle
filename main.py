b64a9dd4c1e744bf9d7d8b397e2f042b05f3f5a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:24 +0000

    although copy always country myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
