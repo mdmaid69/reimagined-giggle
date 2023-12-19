d4c1c165af6d5d21ca8cf65692e8a80a935dfb5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:43 +0000

    choose learn prepare bad sign

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
