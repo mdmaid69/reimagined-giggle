77922c91e388374909cb7dba029774eb6cecf2fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:12 +0000

    spot win objection rest smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
