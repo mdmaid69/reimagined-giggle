d5e9ffd0b1791332cacbc3d4f199dbd50c598ed6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:11 +0000

    bag back either size squeeze

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import itertools
 def get_permutations(iterable):
         return list(itertools.permutations(iterable))
