7e8c5b2f4e61e85b1bfaff483c147ec8f92ec9b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:59 +0000

    importance section put favourite shelter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
