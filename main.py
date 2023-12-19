bc1fac2bab75a6e963cc1b67ca940bc0c230216a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:44:49 +0000

    lookup birthday goodbye below stone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
