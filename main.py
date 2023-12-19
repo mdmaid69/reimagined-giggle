e3f62d67e810b7d13cdb5fe30cc4aad87853460c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:47 +0000

    task activity bath polite by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
