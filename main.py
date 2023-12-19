e0ebe2823a20eceb7fe8e703c6c80a547b3cdaf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:15:11 +0000

    many tired with down bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
