d7ea39e8c4fcfa08aef7634dfa90cf08ae9d0e57
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:24:40 +0000

    boil meet active smile declare

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-for i in range(10): print(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def add_numbers(a, b):
+        return a + b
