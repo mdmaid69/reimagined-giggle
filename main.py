8cddc0499d3c44d07cfc944ea4070049631418e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:34 +0000

    achievement popular inform value share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
