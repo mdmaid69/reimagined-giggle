ca08bf49d72fa4128452d5b74e5e1e45d1e09f2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:11 +0000

    inside recovery violent file character

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
