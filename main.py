e5f17ecef943c7ab72b576057dd51ac3a65fe432
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:42 +0000

    pretend change order sister switch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
