7e062a1be9ca6434f00c7900e365c5ac9106f6bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:56:39 +0000

    leg print floor convincing again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
