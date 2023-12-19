a23dd295d98949662b4d581478a5b399b854b536
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:38 +0000

    taste level turnon century though

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
