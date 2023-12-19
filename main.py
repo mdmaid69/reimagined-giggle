32ef4a9cfe268843aee6aa40eae8fbe079aecb4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:14:46 +0000

    value talkoutof treatment generous lose

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import random
+def roll_die():
+        return random.randint(1, 6)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
