eda7998900cab9a629d5d2021981b78e5e2f73e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:19:35 +0000

    cope the comedown surprise buy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
