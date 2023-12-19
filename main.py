6f360a34bd7b9e9d489a83e0444f88e6fa013c35
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:27 +0000

    spin fall importance suggestion model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
