3348862a94ee078939fef98d48a6ed367e260300
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:25 +0000

    smell congratulations lost fashion occasional

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
