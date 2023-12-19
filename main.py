94c56e1143a765bd264be96be8bcf20478a933b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:14:28 +0000

    path care contrast custom collection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
