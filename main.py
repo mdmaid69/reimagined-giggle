1d6a432f2d9ca173c7db0152feb1b2aee244aa8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:16 +0000

    pocket original stage bite away

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_max(numbers):
+        return max(numbers)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
