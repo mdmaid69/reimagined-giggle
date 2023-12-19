973b6583d5fdcb8bcd0910fbec497ca80d81cc01
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:06 +0000

    introduce pay climb number get

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import platform
+def get_python_version():
+        return platform.python_version()
