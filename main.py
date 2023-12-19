7628fdddf52cf182110ae15babe5a05ae904ec8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:12 +0000

    interesting serious activity drink uniform

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+print(sys.version)
