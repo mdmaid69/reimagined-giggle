2204d42ae41dc369a2923f43ee57b5275ca20a27
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:24:32 +0000

    active tear correction admire goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
