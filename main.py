123b8ed47e114ce0125fd91df39a80ea3ba6aa48
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:45:56 +0000

    shut convince then single dance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
