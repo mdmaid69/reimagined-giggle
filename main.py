a3996c0397e941230c8219e1d8c8175de37a9250
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:20 +0000

    arrive road message until such

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
