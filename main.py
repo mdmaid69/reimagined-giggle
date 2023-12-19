6803a52f070a089d0047659af59d3a12dcde8244
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:52 +0000

    illness wing rain perfect compare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
