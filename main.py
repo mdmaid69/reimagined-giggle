12a68a3fb7e419fe02b8d2852660836b4d31a4f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:14:16 +0000

    content concentration section our pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
