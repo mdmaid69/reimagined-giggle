937387337202ec5ed5ae4a355124c4e93774bb36
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:28:58 +0000

    giveaway subject beyond connected boil

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
