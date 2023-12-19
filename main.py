e202d48be12f2532fdcae95cfa926aa46ea9b6f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:09:23 +0000

    announce long squash imagine less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
