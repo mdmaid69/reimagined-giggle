50ab6389d8532b029722b6f5fc412128e17198be
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:31:00 +0000

    separate obtain yes as mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
