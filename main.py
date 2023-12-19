cd1b3c950836ffb521526b371646517f7a5245d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:50 +0000

    joke spring standard past objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
