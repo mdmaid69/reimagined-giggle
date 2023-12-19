62014a7914ca0c653f6e3fa919baf2fffaad7965
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:42 +0000

    trust key simple imagination exchange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
