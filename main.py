355a8e947d538f2436c7dbf89c5bc8e0f44b6f58
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:31 +0000

    seem education play finally top

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
