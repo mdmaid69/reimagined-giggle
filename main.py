2738e7685a148762342b5dfa907fb7747fa64fc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:56 +0000

    negative clean not pair angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
