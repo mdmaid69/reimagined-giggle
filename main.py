58c39a0925a2ffb7982267b8ebeac252ddc0f6fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:37 +0000

    none rain sky hole at

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
