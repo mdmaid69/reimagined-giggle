c3a87657c594156dfbc5fc87e92775ef048f98a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:41 +0000

    low condition recently wind recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
