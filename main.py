d2ff93f90cfd20e14eac5d315d07b3fd50c82374
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:29 +0000

    cut blame goon lesson arm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
