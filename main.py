456ce4d708c3f3f28466e20af5121cc7ec4a060e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:33:16 +0000

    secret period test match often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
