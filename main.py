beb003550bc89105108930d22ec3f628b96fdac4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:35 +0000

    sort distance force Jj lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
