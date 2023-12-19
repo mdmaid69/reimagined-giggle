117ba9a285d7565312e5030424b40df32e157f58
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:42 +0000

    worst manner rush delicate myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_area_rectangle(l, w):
+        return l * w
