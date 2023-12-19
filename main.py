e4dfa3fd04131f7d62ef9f737de9af2674dc734d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:52 +0000

    blame punishment work conduct calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
