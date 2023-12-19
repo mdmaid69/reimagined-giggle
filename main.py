e550628c4ef8046462b1361b44c9b2d2bf13c5e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:03:30 +0000

    set complete Jj round instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import sys
-def exit_program():
-        sys.exit()
+import time
+print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
