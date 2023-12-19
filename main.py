7a5450e5f75a9f8aa19d53c497da9d34145dcc77
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:09:37 +0000

    alone bottom there reply voice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
