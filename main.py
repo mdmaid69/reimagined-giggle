427d414851dfd909e6f9d5b73f2b9cf68b4d5f0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:41:12 +0000

    nose government man music commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
