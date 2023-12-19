1d83e54af2a2eb1518d2737f108a7c073f8096a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:23 +0000

    arrival broken food pen ordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_work(force, distance):
+        return force * distance
