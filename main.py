fd06def3110bef6d50e9434db0e5d9130ef2e54a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:42 +0000

    provide strength past behaviour complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
