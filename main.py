7f1fbbcf04009ae9ef7ea51b121a92132e694aba
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:03 +0000

    law strike fear favour suit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
