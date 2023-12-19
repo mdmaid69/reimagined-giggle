c3409e1fc4e2a8aae1b8f1d2bfa27c1da8e1d8d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:26 +0000

    must ahead top child beneath

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def convert_array_to_string(array):
+        return array.tostring()
