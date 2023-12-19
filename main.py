b2169719452686ed20da339fb32645bfa661598a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:44:12 +0000

    mixture Tt breakthrough frighten develop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
