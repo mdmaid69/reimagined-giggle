cd0088fa51306cc43f566fb51b37d908d3a813d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:20:30 +0000

    behave imagination surprise soon influence

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
