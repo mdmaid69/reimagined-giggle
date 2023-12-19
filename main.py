add660f61c21fab2866034b7e9d04989b57eb039
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:37:56 +0000

    recommend record mess left sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import getpass
+def get_username():
+        return getpass.getuser()
