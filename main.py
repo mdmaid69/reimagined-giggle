142b6f3c801ff2b6a4b40d87c188e6b51c840f6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:15 +0000

    undo nearly straight generous reaction

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
