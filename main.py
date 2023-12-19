b0acb275c5756bc0e7a14d37b044ae6d080b7320
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:17:54 +0000

    thinkover concern concern regular sometimes

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def cube_number(x):
-        return x**3
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
