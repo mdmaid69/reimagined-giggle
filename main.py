eee64934560cc85b19b81b73ceda1585f726f7de
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:51:47 +0000

    lay flood strict meal century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
