a7cc626614ce6089a596f5b3f799d262adfa60bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:57:03 +0000

    switch chat cost mix include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import time
+print(time.time())
