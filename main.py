11ab5c726bb46c4d6fc3b59a4a10d49e2420921e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:10 +0000

    hope difference unfair bother long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def get_array_as_repr(array):
+        return repr(array)
