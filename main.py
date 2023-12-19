09899cf522eaea76a2db79194267ddaa3e6d73f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:23:49 +0000

    minor each blame before getoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
