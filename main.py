4b25724629c2948a8f536b40bb6522b2a00e3886
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:53 +0000

    further save him roll pain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 def calculate_circumference_circle(r):
         return 2 * 3.14 * r
-import array
-def get_array_item(array, i):
-        return array[i]
