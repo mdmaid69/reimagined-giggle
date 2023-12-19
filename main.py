f5b85d6429e0751214da9d40fc9f8d0419ae5bf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:20 +0000

    illness eager influence brave appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
