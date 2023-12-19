86246fb3a60a7ce89fc93d011237d98cfbb7b6d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:47 +0000

    wash official laugh avoid serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
