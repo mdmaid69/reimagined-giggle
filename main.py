5bc2076c6983e7b4ca13e74b8d7eaea19fc6edc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:06 +0000

    moral smell discovery well slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
