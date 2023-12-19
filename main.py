2e02fd09d19c7fbf46e3dbb774c8e934f99d2082
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:46:25 +0000

    decide dream later either cruel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def convert_to_binary(n):
+        return bin(n)
