c328ed8e5a50e31f0086c5da941a3641a32cfbc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:06:41 +0000

    war bother language his serious

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
