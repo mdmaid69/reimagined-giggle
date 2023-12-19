1d089f6c21bfcff6c732362712bf0ea1b97b8963
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:15:40 +0000

    perfect difficulty fresh summer low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
