d42eea8d6cd77c755750bd94b6dacebcd3c006da
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:40 +0000

    hear wipe spend imagine delicate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
