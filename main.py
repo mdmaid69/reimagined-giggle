cb23e68e7a4e7c3e135a0f389729dd85891a6276
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:00 +0000

    advice Rr sticky pause afternoon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def clear_array(array):
+        array *= 0
