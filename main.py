4960500ea7011d3ab4d7a9d17e141999df61ca94
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:42:27 +0000

    demonstrate purpose trouble minimum sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
