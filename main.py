8f2fc56ba0a760bdabd7dc8086edf33a36b1099c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:17 +0000

    noone without store sweet lean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
