418242dec7cea5c351258f0112f9c86c2f60c30f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:03:08 +0000

    straight search matter frightened more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
