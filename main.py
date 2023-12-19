3ce943641d6b28313bf92fb69e1cb1e44d93c069
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:18 +0000

    belong sit blow type makeup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
