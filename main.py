f4d67e5d29d8dcf9296e4f40caceb22233377d9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:47:38 +0000

    curious around loss Rr about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
