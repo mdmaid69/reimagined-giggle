882ef1b08c40a2bf9fc7d8d69c3441764b56de10
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:39 +0000

    help already vision false finally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
