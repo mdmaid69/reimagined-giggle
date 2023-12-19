65163155d83aa02dc7d4d6a08f0fd2c22fd5e67b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:59:39 +0000

    thankyou give impressive whoever let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
