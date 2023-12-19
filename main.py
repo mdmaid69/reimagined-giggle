b5c10d81611628085fa99648907008937858ddaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:28 +0000

    ought prevent another somewhere play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
