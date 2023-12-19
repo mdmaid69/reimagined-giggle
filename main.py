e7a4a262b39c3669bd572220a56746dff2d8ac22
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:46 +0000

    that 570enjoyable equal much certain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
