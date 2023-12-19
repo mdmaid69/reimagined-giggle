c58c4afc2e5c78d84a3ed813de3f9e154f264a2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:59 +0000

    company holiday no fast emergency

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
