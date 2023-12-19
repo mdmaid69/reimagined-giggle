548426d0b32970a07fbde4a936720eab1b7dc0b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:46 +0000

    although dark however variety Mm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
