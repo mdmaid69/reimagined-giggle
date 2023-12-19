48eb26b2f5cd318df7ec1bc6a84a06fc1fefd5d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:32:53 +0000

    chase step male argue person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
