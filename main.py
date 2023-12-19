9c167f6d751548921becd3c7f38cb4d1205174fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:08 +0000

    understand stick exhibition guilty excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def greet(name):
-        print(f"Hello, {name}!")
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
