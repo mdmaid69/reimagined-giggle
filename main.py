d0f2ffb45155b68397f1e2dbb5247b0aef06c355
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:17 +0000

    reduction personal wide defend naturally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
