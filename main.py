8e789fdbb1c65fd06a7d7c94060861cc0800496d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:03 +0000

    fortunate in miss morning necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
