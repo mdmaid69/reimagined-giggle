b1c5460eb9da7dc8f249b108818cc2137b57dc4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:43:04 +0000

    difficult pleasure silent level wait

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
