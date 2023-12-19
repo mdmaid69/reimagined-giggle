4a2eba078c603a5ee444e540324809bf07a12cc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:49 +0000

    shall spare pretty waste such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
