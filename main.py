cbab1066d787fa3b38d25ed2595c27665b16731c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:27 +0000

    noise match include discussion ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
