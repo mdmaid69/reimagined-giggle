1a8b595e11a0266a60d17c16c234284bfffcd980
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:31 +0000

    number throw drink interested necessary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
