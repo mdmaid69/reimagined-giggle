f7c1935ab74f96e867f6d18cca154224491b254b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:11 +0000

    side embarrassed main gather safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
