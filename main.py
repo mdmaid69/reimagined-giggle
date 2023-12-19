37ea76ec4f8ed9512575bc8e8cf42769e438af36
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:30 +0000

    such maintain defend control switch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
