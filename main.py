232d47a27e73ce27a199f8a5ae29ef4d902f0232
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:55 +0000

    payment tall vote holiday glad

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
