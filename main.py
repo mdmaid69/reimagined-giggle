acc1ee526522d94abe222ed0e835ddf33224f123
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:23 +0000

    free holdup nigga grade payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
