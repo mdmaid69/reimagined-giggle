e9e1a4dc2dbe375929965ee10e38174424e95bce
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:58:21 +0000

    perhaps list there surface change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
