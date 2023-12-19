600957095425bd31c32d615e2fcf94f813473e4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:43 +0000

    style hate exaggerate try will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
