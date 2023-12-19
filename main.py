e5175f5d9e47fe42c7b0e20492794ce6c515cf0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:03 +0000

    bank inside appear welcome rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_pressure(force, area):
+        return force / area
