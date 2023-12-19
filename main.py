0ab188ce14c2b11b195ed15fadd11292245332b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:47 +0000

    accident schedule tell finger rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
