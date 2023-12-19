6aff6a71a8e6dbaac6c41a622eda949ca8ea7992
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:52 +0000

    normal mend somehow ball eager

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
