11ebb5730bb28c422256d0bfc60086a3f57ac238
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:50:54 +0000

    exist government exchange hollow wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
