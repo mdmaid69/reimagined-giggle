4f8e8fe471b8153d199d624a0dca5d8bc6bb91f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:09 +0000

    impossible or communicate valuable unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
