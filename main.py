285cd3802054d7859e9df183e5a5e3a9cb5c1d8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:04:02 +0000

    I content congratulations insist accept

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
