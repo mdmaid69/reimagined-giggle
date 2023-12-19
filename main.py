79c3ac7535e7a42c15ea71bb09c745178fe8cf9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:39:25 +0000

    rain compete sensitive opinion violence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
