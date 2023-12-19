ae2e766cf3f2bbc4f1fb1ca84b486cc66cd8029c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:04:53 +0000

    find path square priority thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
