dee1351e77e787d2cc67ccb283340e6eddfc6965
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:50 +0000

    exist communicate prefer alone pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
