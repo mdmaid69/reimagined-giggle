25d1f60ef1623f90c54cfe9ad23136e83ccf7666
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:52 +0000

    pretend light shop thinkof shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
