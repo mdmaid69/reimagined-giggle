68120f10546a6001f7fc7781fcc18538a7f42c99
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:07 +0000

    generally amount cook lookback(on) comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
 def calculate_cylinder_volume(radius, height):
         return math.pi * radius**2 * height
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
