16bd0e07471958314faf12f97376bb30a3882f6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:37 +0000

    pleasant feel correct sing lend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
