c06dd18e23b4748910bb5e332e80bc88f0209837
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:08:42 +0000

    university they tear stir word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
