0f1134f6bb6bec9641c580e26cd2304920927e9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:29 +0000

    lean path war rest motion

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
