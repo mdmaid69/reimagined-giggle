26c969af2d1ac2f7af5bec5957c2baa5925e6b28
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:59:06 +0000

    shut clear block project opponent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
