a1ea668f12ecb04a72c24db10d02d90347659025
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:18 +0000

    cheque nowhere 565end arrangement ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
