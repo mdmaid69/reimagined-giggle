5bee355114edc122d4f7619b17dc8a6d562230d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:17:21 +0000

    often exaggerate hot height secret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
