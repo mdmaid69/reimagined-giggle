3d5c2b6646330e4d5dddce7277760a6c3c8acdcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:40 +0000

    mark when visit rest almost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_speed(distance, time):
-        return distance / time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
