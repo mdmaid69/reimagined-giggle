7cdadfa53bd51f44c12468d6e13681c1fa92dad6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:27 +0000

    belong variety pipe interest accident

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
