67132cb93d9daf66d5294bc40a54dc4918170c60
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:30:10 +0000

    doubt still fire picture ignore

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
