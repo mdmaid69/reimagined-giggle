1465cbb52f2a714511b9bb80749cfacc8cf221de
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:57:39 +0000

    the when calloff sale question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
