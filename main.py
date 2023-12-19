8ed0be90763d39245585ccaa60bf0053fa44f8d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:13 +0000

    dear culture first present blow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_area_rectangle(l, w):
+        return l * w
