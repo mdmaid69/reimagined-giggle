8786acb735178ade194caa181b7e94c3a68bde76
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:10 +0000

    defeat dish reach myself wrap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
