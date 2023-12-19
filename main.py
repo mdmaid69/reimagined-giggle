c64dfec1aa2a437798e750cfb00ad888bf9c4998
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:16:09 +0000

    side gas cheek suck unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
