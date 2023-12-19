5227cefa403d5d22c3cdda84f120e397e312a6ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:17:37 +0000

    health beautiful scare idea between

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def set_array_item(array, i, item):
+        array[i] = item
