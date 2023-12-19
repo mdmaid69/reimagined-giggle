71c063cadd4cf67929803773280cc37e5b506510
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:33 +0000

    whichever move emphasize soft letter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def convert_array_to_bytes(array):
+        return array.tobytes()
