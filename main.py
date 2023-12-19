aad8640cacf77bb4729a14a8663f4d771d63e7f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:42 +0000

    cold ability hang action communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_item_count(array, item):
+        return array.count(item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
