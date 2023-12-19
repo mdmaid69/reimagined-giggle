90f53d2224d6f222da873dc8172548d5a1ee547d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:37 +0000

    praise balance flat feeling evening

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
