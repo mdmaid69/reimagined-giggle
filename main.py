c5e9cc1f65b430b4f3aeaba9734cfaa750b63c29
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:40 +0000

    Zz something disapprove themselves somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
