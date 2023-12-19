c086b3190c005e80553660929ab598e8acf10590
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:13:09 +0000

    separate lookup sure have ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_string_from_array(array):
+        return array.tobytes()
