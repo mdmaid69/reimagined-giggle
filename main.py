baca5ee4175bc90e0873308c2ff22c7745b8b6db
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:50 +0000

    guess stupid peaceful dance report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
