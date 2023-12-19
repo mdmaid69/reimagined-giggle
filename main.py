04d0e01c0cde1e125e600416f14be37e7e5cbfc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:28 +0000

    list anybody face union upstairs

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def greet(name):
+        print(f"Hello, {name}!")
