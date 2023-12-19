8876b6706e9dc2e85fc378f87959dea798f56ee3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:45:10 +0000

    especially sink exactly formal bringback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def reverse_list(lst):
+        return lst[::-1]
