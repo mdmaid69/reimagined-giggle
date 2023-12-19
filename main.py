1f38c725dff29f138208d98bf88217fa948f6dee
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:43 +0000

    holdout getup meeting welcome at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import time
+print(time.time())
