3817a28398c5694da2ad560ba0b6d8c86d65026d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:46 +0000

    combine float line explain cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
