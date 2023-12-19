11d43877f88469b373b686fe6e6fa3d3e520d062
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:58:59 +0000

    spare also afraid tired holdon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
