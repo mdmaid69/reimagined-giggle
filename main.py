d2e53d45be28276ff121d8368638b66416804ecd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:10:28 +0000

    possibility direct bit comeoff health

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
