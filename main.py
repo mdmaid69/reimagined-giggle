b1e6359df7f68097dd8f03fada207b6ccf0fc2ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:56 +0000

    programme learn swallow burst long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
