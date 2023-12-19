0a7e5884f6e27a7e227111f66f6629a16d468fa5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:07:23 +0000

    tip guess 179bite mistake bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def convert_to_hex(n):
-        return hex(n)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
