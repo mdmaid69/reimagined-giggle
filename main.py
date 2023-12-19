1103427689f4f27d83293fdad9386833bf8764b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:40 +0000

    help touch effort should half

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def is_odd(n):
-        return n % 2 != 0
+def remove_duplicates(lst):
+        return list(set(lst))
+def count_elements(lst):
+        return len(lst)
