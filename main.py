dd35be14532c4b1157e7da22324e3fb7b5fdae23
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:20 +0000

    free who collection better their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
