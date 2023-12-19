cf7b1815138cfa38aeb288b838010f1b51a8d28c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:11 +0000

    report why keepaway present yet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_octal(n):
+        return oct(n)
