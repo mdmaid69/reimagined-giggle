e6cf49bd77d35fa2ad5d53675c2c6817dc864352
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:18:46 +0000

    simple beat authority well sensible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
