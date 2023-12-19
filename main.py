d8f8e8f4f2e3e3130cfadf6f4bbdcc03ffb7db05
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:14 +0000

    unite them getoff wonder practice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_max(lst):
+        return max(lst)
