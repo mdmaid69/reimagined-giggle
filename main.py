295a9936250676d02ba54b945f8df4b775f2165c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:17 +0000

    achievement tip direct complicated forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
