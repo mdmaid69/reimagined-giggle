7f8c2d52bf82d603619820571e6d377c0e6cb23a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:10 +0000

    each invite warn rough borrow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
