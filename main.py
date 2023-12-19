c12e8ad296a6afe1493ffb45695ec1eed4803c6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:37 +0000

    sticky contact official switch fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_density(mass, volume):
+        return mass / volume
