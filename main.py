c02fccc752555df8e5d06e3317634225f220d302
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:19 +0000

    rare border somewhere wake birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
