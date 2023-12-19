4d32246d2e20ffe6cc692e734139a3642ba55f06
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:18:27 +0000

    tour keepup pretend shine there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
