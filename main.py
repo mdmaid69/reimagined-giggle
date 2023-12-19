e6e9823328fb4cb3519a189d7ba09db51fbfbe56
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:11 +0000

    space case 569enjoy study guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_area_rectangle(l, w):
+        return l * w
