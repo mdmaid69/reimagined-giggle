5705b7dc863bb1a7ef0b1f1815d524e56f09e044
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:51:01 +0000

    violent over negative comeover break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
