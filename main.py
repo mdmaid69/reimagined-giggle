c81c846a55ac7dee833ba20893d4a75eb0519848
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:25:43 +0000

    gentle 567enemy block break hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
