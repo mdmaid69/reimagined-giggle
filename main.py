bbbd694d0613f9fc07032ec46421d3beac533206
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:28:39 +0000

    set lend children shallow march

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
