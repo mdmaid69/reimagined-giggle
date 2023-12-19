ed9401f8b1a4688a6a763c092cfe75c819430b20
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:06 +0000

    smell instant responsible pattern discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
