97a24418538b80d8088565036078620f30ca4ce4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:42:33 +0000

    reply direction cruel runaway thin

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
