fd55b8af7e08ac11453f2fe9f74e6e1fa8a2bbcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:35 +0000

    properly favour himself whistle pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
