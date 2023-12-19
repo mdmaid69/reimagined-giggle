594ec601b3767da64b2f4be42ac6f828d8f4b068
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:09 +0000

    healthy answer goon unit future

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
