adedef4852a3c1c0d07e6e439865c3b5ced422e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:41 +0000

    comeup recover street wander giveback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def change_working_directory(path):
+        os.chdir(path)
