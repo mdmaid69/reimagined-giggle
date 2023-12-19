50f43d825819169e19c355f632bbcd1d880d74f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:52 +0000

    attract talkback hot rule listenfor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import collections
+def create_user_string():
+        return collections.UserString()
