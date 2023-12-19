8f05ef4686f53784f90e64b8bf1a6b4bf5cf63cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:24:13 +0000

    answer pick learn peaceful cry

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
