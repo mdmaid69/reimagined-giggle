4eb3e2ab52b98d1a9eb3b6121a4d9611dae16275
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:00 +0000

    fact fear in respect physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
