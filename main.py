1dfa02e78e318ef0921e48e7780b10d6ff222d88
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:35 +0000

    complicated they else tense generally

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
