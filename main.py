a109a82c17e31215d7111813320e316562f5183e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:10 +0000

    flight prize valuable lend mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
