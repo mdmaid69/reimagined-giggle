024ed526843d772cf93847c0901a5e0ae256f986
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:14 +0000

    familiar private but 571enough division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
