367598914e978f1f9964050098f7d552330a164c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:39 +0000

    replace turnout forever mistake 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
