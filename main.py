d678065cdf9d9c1a1f1b0ef9715561255a459642
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:05 +0000

    order university along government tool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
