07a4a6b5b9f0f774c3c3c56fbea3d5e8cdc3adad
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:35 +0000

    road live loss closely buy

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
