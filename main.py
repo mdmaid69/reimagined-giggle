619fe0019b78ab2994c887c03801bf7fe0c270a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:37 +0000

    accommodation early fault help meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
