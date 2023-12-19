1ecccf77ba8f66c01b946cd1b0cb7075d0a0f40d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:43:52 +0000

    meet clever study worried spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import sys
-  def get_python_version():
-        return sys.version
+import sys
+print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
