9b24c5be6c503808d27fc75fedaa27a24f417a07
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:42:50 +0000

    change line frequent concerning turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+from collections import Counter
+print(Counter("hello world"))
