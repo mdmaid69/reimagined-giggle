5fbb31f7a06922748c80b1ff186c9bb354d8ac1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:57 +0000

    sore pour splash of ourselves

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def is_even(n):
+        return n % 2 == 0
