c5138448396d1c3bbc5749d0100023a83034f57d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:41 +0000

    better name tall become dream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
