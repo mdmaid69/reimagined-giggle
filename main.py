23e9ad25a1f42e676c44f2dfa7e28c66fa8e1e10
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:14 +0000

    sky tonight about project brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+for i in range(5):
+        print(i)
+import collections
+def create_user_string():
+        return collections.UserString()
