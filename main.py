82a434aa585b9460b56bae16882af5372b6f3aa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:24 +0000

    slip callback confusing terrible sink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
