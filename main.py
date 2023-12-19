2e5caed0fcebbe3527853c03ad5f7b9d6bc6fcbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:31:46 +0000

    zero base memory performance party

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
