bcfded8808e0e4be5696316c5a40ca5695314b3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:59 +0000

    sit bite spare mad hate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
