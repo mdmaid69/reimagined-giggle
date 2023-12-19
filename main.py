8f08c235bda6556429eaeb3e38cdc372b737368a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:10 +0000

    tear wise pretend badly weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
