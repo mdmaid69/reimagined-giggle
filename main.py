5f43a91a0193b4f7fcf9676bdebd6c2c9409081d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:08 +0000

    spray reason cross wake gentle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_numbers(numbers):
-        return sorted(numbers)
+import sys
+def print_python_version():
+        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
