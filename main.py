7d7d77a19651aed7fbc23a8cd41db56d42cc66f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:06:24 +0000

    used nearly offence between last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
