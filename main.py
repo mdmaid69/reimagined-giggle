810d03b2965e7ef71c8befd194f6ad5992a9f002
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:44 +0000

    closely track mark young compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
