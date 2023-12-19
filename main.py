18ce60471281af5a7ac036e12f0bc4ed3558bc79
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:19:13 +0000

    Gg children bush broad error

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
