7d865f38b05711875342f54721800003c3f0e18d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:25 +0000

    appeal steady reduction lead suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
 def calculate_product_of_sequence(start, stop, step):
         return math.prod(range(start, stop, step))
