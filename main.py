af1e061fbd667d284ceaa9ddabd1fded5b5b40d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:22 +0000

    scale behind return cheat everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
