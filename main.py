6cb296a6ed5ae2b7f93df5cca617b37982b62aaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:14 +0000

    high my tear development fault

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
