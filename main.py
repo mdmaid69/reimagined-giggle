e0e6fda134f67e4ba3bf660d2f6f88315a96f8ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:51 +0000

    likely turndown runout choose loose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
