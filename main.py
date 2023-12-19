fc49c53971acdc29f9125035863ee4e080e72a6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:15:35 +0000

    dead attempt action instrument arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
