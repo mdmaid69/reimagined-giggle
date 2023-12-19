a609f98478b980c857cda28e8e04343ac165311f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:08 +0000

    city cheat impatient public hungry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
