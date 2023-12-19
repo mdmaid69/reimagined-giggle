742f897f808d14a9df350fe6566cada26a625024
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:10 +0000

    deserve protest spoil hard far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
