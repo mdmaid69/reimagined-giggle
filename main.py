dc5f679725c6d51f57b09df8b313925f644e5769
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:26 +0000

    heat instruction wish avoid platform

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
