12faceb367419684869d212f67b88098224a95fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:09 +0000

    rich I street by surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
