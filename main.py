43e9f1002108e945d74c47971204c7f1c74114c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:30 +0000

    taste scream look rush shine

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_min(numbers):
-        return min(numbers)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
