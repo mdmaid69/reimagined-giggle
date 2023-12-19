2556b1a29025b2750f1f3138f4cb540f1b8fd98b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:14:54 +0000

    rise foreign here blame tax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
 def get_array_as_format(array, format_spec):
         return format(array, format_spec)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
