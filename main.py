cca978d49713d691de1cf5f37f48fe00d6a74bd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:09:33 +0000

    short nice feature surface turninto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
