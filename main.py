208d8dec8cce96d197ccf1ff534b30aeb34a61c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:52 +0000

    print accordingto present shout mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
