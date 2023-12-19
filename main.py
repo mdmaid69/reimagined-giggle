1f8decad5f7f2b2d29b549e5ad31147c62dee7d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:08 +0000

    Ww hard aim right slight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
