6b0914a314e2087db87310f83ad3e6faf98e57f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:52 +0000

    matter answer begin uncertain rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_user_string():
-        return collections.UserString()
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
