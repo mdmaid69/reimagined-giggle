a1b904104ca0bfade0030954a93ca7051cbda9fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:49 +0000

    blue frightened foreign several flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
