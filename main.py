8d14352f39fc05df16dcca43e9c68bbe5784172b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:29:43 +0000

    ill exaggerate under innocent responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
