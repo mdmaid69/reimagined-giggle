c232703c46f4d2949ac5b8f69307978d2b8e2fc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:20:30 +0000

    hers guide unite blow Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
