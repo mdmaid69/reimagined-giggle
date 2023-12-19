4a69f1ecff59c19178829877bd17d841b6eb8da7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:47 +0000

    schedule uponon introduce takeover there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_as_list(array):
+        return list(array)
