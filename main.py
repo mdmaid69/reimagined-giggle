858a64dd164082f514c929ace6aeb0c535cc4c95
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:45 +0000

    completely threaten list birthday fashion

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
