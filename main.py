95ef4cfb8d9d24dc21b06cf740298fdc1bcc1205
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:47:04 +0000

    with scream spend nearly few

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
