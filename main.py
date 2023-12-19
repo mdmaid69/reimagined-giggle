91cae06bcfbcdfabdb7a11653147a9e6f536b61e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:27:37 +0000

    clear official phone scream reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
