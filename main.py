550bda995aa9cfa839d691485570987f94992e0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:08 +0000

    also face strike admire sense

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def reverse_array(array):
+        array.reverse()
