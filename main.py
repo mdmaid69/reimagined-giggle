7be2d03b7e44d4dc55f4bd3485d1443484d4a26f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:49 +0000

    about second leave shoot himself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
