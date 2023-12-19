78c44a55234938910d43b5d3b507e733a495ddb9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:49 +0000

    meeting record regret police print

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
