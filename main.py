4cd0ac2387dde7854f07bef23c980af4590b3c0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:16:54 +0000

    base early muscle provide great

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
