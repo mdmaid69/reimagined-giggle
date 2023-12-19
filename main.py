6caddb193ee29422e4577dd7d652812737f0fb61
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:16 +0000

    now breakup like please concern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
