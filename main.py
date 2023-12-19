df109e6aac860483272055313b79cdb1633c770f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:14 +0000

    shout rather unusual advantage pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
