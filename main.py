9e75d1d38007f6362cd652b7f4c07bcfa7758358
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:04 +0000

    speech air exercise within Qq

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
