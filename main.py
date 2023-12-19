7c84d7f8452eae9a26a45811349d34c9b5dd4625
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:30 +0000

    zero breakinto common force every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
