2edde4beab7f52253334c2edad0938b4920de2e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:17 +0000

    must boring Ll other protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_user_string():
+        return collections.UserString()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
