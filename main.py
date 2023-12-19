52318e46b44f69f38d6d3ab5429b84f4c874cbf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:21 +0000

    bringup goaway exciting benefit chat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
