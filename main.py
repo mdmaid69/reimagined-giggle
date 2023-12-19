9ffb84d73bb0535e038640bfd6bacae445df7860
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:52 +0000

    school progress jump exist part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
