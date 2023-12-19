684481928e540aa5171b45cb8e5c9359399cc310
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:16 +0000

    side occasional hard fierce happy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
