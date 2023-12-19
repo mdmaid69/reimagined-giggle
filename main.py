adabde97a317e5e78877e40659ef90d7ef359313
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:32 +0000

    comment stranger notice smoke pinch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def sort_list(lst):
+        return sorted(lst)
