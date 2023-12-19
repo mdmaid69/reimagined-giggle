39d4abdd1173ded495d89bd71dfc65ed308f140a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:47 +0000

    every produce step obvious achieve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_speed(distance, time):
-        return distance / time
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
