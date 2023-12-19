29b245732f5853d19ab3914b50a8cf3779094b57
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:59 +0000

    protest reason recovery unpleasant false

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+print(math.pi)
+def calculate_density(mass, volume):
+        return mass / volume
