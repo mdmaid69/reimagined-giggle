8b722531afd70df490c16dcc805a29e179abffe5
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:39:18 +0000

    565end under mean anxious plastic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
