d6a77c89d2d4dac9f3c4bcd683306412241f67dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:19:01 +0000

    adjective direction mess group each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
