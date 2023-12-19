618fde50b92785f2ec802b64f11e82c3c3f9e959
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:29 +0000

    ago lead belong everyone bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_item(array, i):
+        return array[i]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
