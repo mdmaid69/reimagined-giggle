1d1c12ddd967d45e3d4fc8b7e1a5e1a2deed0b06
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:23 +0000

    stupid number disturbing after always

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def exit_program():
+        sys.exit()
