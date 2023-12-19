7f9d1c65828f95d68fd67bbb96ccefe8d40c665f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:36 +0000

    statement happen actual unpleasant respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+print([x**2 for x in range(10)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
