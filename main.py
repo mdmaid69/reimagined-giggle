5772c24a4d3b115184acf97c3dad2344dee0fd20
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:51:57 +0000

    copy inform danger gap bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def cube_number(x):
+        return x**3
