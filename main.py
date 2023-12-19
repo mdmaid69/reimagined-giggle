0ea2f29fd74d0a29b5cdd88bedcbe4e16e54d9eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:30:30 +0000

    married or discovery thinkover lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
