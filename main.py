81f81a49d092dbd4a8c3fb0c115b5cc9a77bfe6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:07 +0000

    mail shape central hit imagination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_max(numbers):
+        return max(numbers)
