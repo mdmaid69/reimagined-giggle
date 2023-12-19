83d33e44d0bcd3e607ac244b8cadd5d91eaa10ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:24 +0000

    risk steam related interview minor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def divide_numbers(x, y):
+        return x / y
