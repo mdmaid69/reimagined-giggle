60d450d41d9c65ee184961d93a775b74f5238247
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:59 +0000

    tool challenge continue underneath unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
