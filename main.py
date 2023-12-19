b3817d3d284bbee25b257e35d5f08513e44ec4d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:23 +0000

    communication pronunciation fat declare afraid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
