dbe2d0f4134136faf9ae63b7dea77948e9dfc4b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:54 +0000

    bridge mail regard object tread

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
