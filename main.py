030dc29651d4db456b93f61a968de53b1c5aa0de
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:10 +0000

    creature into breakup fold thorough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
