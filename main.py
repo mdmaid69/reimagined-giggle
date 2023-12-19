448e128f09583654fd3ccf65ebdfce4b3b978a5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:17 +0000

    near silent wait willing band

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
