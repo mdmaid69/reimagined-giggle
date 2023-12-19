ad67c277be324e18cf6fcda09aab89c0bec46319
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:06 +0000

    unless bringdown expensive grow force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
