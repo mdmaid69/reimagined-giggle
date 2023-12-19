922a03a78a621ca618142e97ef991d0a653e5e05
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:35 +0000

    turninto disagree involve guess receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
