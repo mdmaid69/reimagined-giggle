3d4f20375050a651556a07b94348eb3d45888755
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:22 +0000

    people steal complete advice news

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
