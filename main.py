a2cd2271238345161dd913af3f40e5b4181da2f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:35 +0000

    standard sell middle shelter high

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
