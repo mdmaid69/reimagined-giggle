039174ed79858cb10ccf7169b7ee1729edff691b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:43 +0000

    regret bus project environment bar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def add_numbers(x, y):
+        return x + y
