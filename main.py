53664bad967ebb678423118e249a0b598a28951b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:01:59 +0000

    freeze over afternoon hair final

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_odd(n):
-        return n % 2 != 0
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
