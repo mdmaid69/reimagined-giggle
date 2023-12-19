9fb7e9a2cbd2bdc15e59e164d74a6f5978d5c824
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:03:33 +0000

    comefrom joke temperature near home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
