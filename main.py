07917fe881641e9b33f7c2384d96c4740ea25484
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:16 +0000

    grand divide shelter cool education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-print(math.pi)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
