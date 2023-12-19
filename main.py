6ab275a0cfa7a32aa5bfc046a72a8fafa32253f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:18 +0000

    direct walk crash welcome raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_float(array):
+        return float(array[0])
