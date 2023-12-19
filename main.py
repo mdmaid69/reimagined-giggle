2c5fc9e75d7d4d2800fa8453f161a69de2ed672e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:29 +0000

    regularly fine grow Uu near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
