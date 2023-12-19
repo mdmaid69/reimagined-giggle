970403e53c8ee1df1ed818f202f05f6b1afbcf48
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:05 +0000

    invitation hurry look rush confidence

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
