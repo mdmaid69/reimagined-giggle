eeac53e9fa6d3b0b8714a0f6ba9ef871c2b932f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:43 +0000

    gradually sound lookafter book splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
