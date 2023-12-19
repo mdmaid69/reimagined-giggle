8d3e8bf0b52b8401f285c2d7eb219e1caefd4045
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:24 +0000

    pronunciation urgent rain comeback downstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_area_circle(r):
+        return 3.14 * r**2
