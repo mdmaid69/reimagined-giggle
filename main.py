d57f3820a73966ef72a8773df2c8798228318533
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:55:49 +0000

    quiet imagine side modern so

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
