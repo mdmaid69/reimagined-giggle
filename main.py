72edb49a106439e5af87ea3d773eaede8ce59b64
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:39 +0000

    poison past argument they design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
