74c53d8618af05511da979b3dab98f50e2882c18
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:54 +0000

    country fall so goback(to) rush

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
