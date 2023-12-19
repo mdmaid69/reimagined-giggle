1234a96ae810c4f4997c35bacfafe3a8f0ee9799
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:12 +0000

    press survive lean forgive pull

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
