b54fbe83fdb817f3f42adb25909664b56171dab5
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:59 +0000

    lunch once slow enthusiastic black

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
