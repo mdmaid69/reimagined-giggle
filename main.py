5d2f8f0b37533c56849d02652f59bd7c5852094e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:36 +0000

    disagree quick near belong impressed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
