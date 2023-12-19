5f235ead6b555b375d10ca4f1eb38bab5a75619b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:03 +0000

    negative wherever half water probably

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
