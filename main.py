5557f79efd540400f5aad7864a20c2e5f3ab52d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:47 +0000

    keep appeal free appearance brave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
