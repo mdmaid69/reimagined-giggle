1004c21377611ddbd162298c367bd0d8265e6d10
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:53 +0000

    wherever crazy peace phone copy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
