6ceec8aa82962b9c180f7d109197f1824a73d33d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:57:07 +0000

    more year fit frame tonight

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
