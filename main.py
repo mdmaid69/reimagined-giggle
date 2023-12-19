17e7e469b7f2a1d6b6d6baf987ebf96ce8a285a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:02:38 +0000

    wall set suggestion deal sore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_exponential(x):
+        return math.exp(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
