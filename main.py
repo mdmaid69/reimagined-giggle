87d6a42cf31845799826dfd60841b11c6a74af69
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:02 +0000

    price rid answer light advice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def get_bytes_from_array(array):
+        return array.tobytes()
