2fdcddf5360203084a67c5a39768c098a93de6b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:10 +0000

    exciting arrange report make miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
