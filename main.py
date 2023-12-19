a9e1c6fb78e28bc014d2cbb053dfdad3d0d8c83b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:27:34 +0000

    ordinary search expression neat excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(10): print(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
