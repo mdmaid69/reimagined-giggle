34769f8e653f9bf5c782716fd7c96d12832ec2a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:02:18 +0000

    comein trousers push usually spoil

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
