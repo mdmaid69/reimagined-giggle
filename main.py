6b56d83329f6d6b971b8cd754d2c9bf9e41ed7bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:06 +0000

    waste behaviour takeout nerve over

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
