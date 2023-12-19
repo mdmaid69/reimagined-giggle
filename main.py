2d345bd6b67ab6fa20c10e80e1c2b08fba2dcc63
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:01 +0000

    mixture hang really interrupt convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
