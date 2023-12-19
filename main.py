a6b499deeffd1166b035bb2694942fd1525aab92
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:18:48 +0000

    description minor declaration when like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
