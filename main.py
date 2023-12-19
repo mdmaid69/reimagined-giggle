6a808ed7eb2aa16b7fd7e67d90308d6891eda792
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:19 +0000

    early busy period pair improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
