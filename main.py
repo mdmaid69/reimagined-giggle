6a418240b1e912daa3f66b13e320d86ec22ebefc
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:37 +0000

    prepare rich like outside programme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
