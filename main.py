df8d6cbb816d392fc26bb9bf015aa25a96fac8b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:07 +0000

    authority hold sign pride visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
