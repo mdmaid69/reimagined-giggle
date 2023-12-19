0e854e27759a8063fc9d70ce91d318e172899837
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:44 +0000

    range book lower calm exactly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import datetime
+def get_current_date():
+        return datetime.date.today()
