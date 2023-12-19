b0b7677f78ad5f3eb1ce8110d4a2455b206e08d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:41:08 +0000

    train 570enjoyable sore object calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_density(mass, volume):
+        return mass / volume
