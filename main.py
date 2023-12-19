de41e419e176d480f39bff1c86aaa26e1f85889e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:20 +0000

    wash problem accidentally fair safety

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
