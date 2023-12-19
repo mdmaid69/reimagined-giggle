b00baa5368a57ca7ff94e435790331547466dd76
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:28 +0000

    bag own fasten safety force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
