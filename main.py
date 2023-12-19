c3d3dea520b2cd60fa712bc6011c995f978298ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:05 +0000

    determined tooth programme dry 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
