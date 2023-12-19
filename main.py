a5896663152d6dcbc839a43257913f98500dfcf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:00:17 +0000

    uponon soil damage support my

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
