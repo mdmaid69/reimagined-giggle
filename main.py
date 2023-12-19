89e9b854cc7b49282bccf0bf1e0cc0a450fbe40d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:51 +0000

    steam politics straight turnout callout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
