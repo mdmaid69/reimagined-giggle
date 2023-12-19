0894755b70f3717936bdaf3c01322b8471a8b0ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:02 +0000

    much create entrance tight before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
