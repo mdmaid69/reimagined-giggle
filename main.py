1f920aed0b391aa1db8b4f8c35a67f0ba47c439d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:42:29 +0000

    attractive black by regret rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
