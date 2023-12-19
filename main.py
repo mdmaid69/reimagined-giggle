c2961aa919f23511f8835c7b5d4c08b53f305869
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:31:15 +0000

    law set confuse live mad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_index(array, item):
+        return array.index(item)
