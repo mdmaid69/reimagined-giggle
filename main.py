0d40686841362d6da58c0c3c1adf4205564bdad9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:46:54 +0000

    downstairs anywhere slice appear exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
