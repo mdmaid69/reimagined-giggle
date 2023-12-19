fb8b707b61cf4446a979a24bb3a820e3dee90e2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:50:27 +0000

    Vv pupil sensitive goout mail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
