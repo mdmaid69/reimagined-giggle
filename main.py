b5ce345de2d5b86f1150af395350bcacfb8c06b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:27:27 +0000

    train accept calloff though among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
