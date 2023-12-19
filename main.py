d6dd036980e8b6dda0094e14b9d886296400cf62
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:14:44 +0000

    close discussion date in laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_acceleration(speed, time):
+        return speed / time
