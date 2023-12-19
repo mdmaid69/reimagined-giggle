bb9b92248a4e51ef7d2545b43496f3108337ebfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:19 +0000

    thinkthrough apologizeapologise area clever without

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def cube_number(x):
-        return x**3
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
