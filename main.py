382e0bf1adc06d7536bef55b0db6108c7e8cc262
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:39 +0000

    some taste contrast welcome dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
