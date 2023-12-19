da14381ef55eaeda68d772c7517e3959e095b22f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:32 +0000

    stretch respect workupto fence Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_power(work, time):
-        return work / time
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
