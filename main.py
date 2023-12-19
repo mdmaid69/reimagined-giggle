078cf24ea9303d0122309b52b0cec92ecb353556
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:43 +0000

    impressive money pin rob surprising

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
