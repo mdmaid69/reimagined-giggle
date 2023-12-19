29d9a66cd4bc0f070297561095b82408f429863d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:09:19 +0000

    curious fast rubbish lot approval

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
