cc15f9a5357086eaf09d79c5d26c0096a71d9c27
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:18 +0000

    lead bread floor communicate guilty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_density(mass, volume):
+        return mass / volume
