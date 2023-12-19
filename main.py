ca130b054f9ab0355c52e34941a08096ebae25ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:43 +0000

    spread communication by condition standard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
