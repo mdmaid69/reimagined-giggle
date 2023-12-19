0d6757887157c4f885980ed22ec39e0ee2fff38b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:26 +0000

    last during play I repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_itemsize(array):
+        return array.itemsize
