c59d95943b74be5f437b34f58e26eaa72690ee3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:30 +0000

    peaceful unkind exit listenfor unfair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-print(math.pi)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
