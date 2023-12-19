4ebf356be0f59ca904427b427affcb450477b016
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:36 +0000

    principle practice regional reply completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
