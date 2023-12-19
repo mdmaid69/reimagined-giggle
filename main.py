dabcdc8ad921341cac8018546a873f44d1ed6472
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:10 +0000

    instruction enthusiastic sort she outside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
