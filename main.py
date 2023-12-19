f3dc2a80e74fecf8a102377a6a8977311d4d6874
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:46 +0000

    smile all themselves help whistle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
