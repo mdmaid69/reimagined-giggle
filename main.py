8c4ed724359023d7684ce5b463fb22c91c42d66f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:40:19 +0000

    among build sense unusual trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
