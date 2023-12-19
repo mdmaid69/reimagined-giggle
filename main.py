8cd52cbc1fbfad2a6ebe96754a51d08d31fd0785
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:34:12 +0000

    blow coat various show angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
