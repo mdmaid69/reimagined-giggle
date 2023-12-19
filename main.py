c4fd261e20905f95408e23a00efbe6e8e1c2ad21
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:03 +0000

    attempt tax convincing Mm decide

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
