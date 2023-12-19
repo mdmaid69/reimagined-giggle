eb839d14f216c4088e7f6abef897f5ce50b31fbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:54 +0000

    push hand arrival goaway side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
