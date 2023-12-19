0f6a898f6b1a62b38e0a07b3d41af9bdcb8f6276
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:15:33 +0000

    treatment find meanwhile radio score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
