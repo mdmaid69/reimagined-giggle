381cf523c68ae541790385a5c96eb151c526e12f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:17:19 +0000

    suckme kick card attempt recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def get_current_time():
-        return time.ctime()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
