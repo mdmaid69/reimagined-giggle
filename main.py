36130ce498955c85f28f1ea8b761709f530b1d0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:07 +0000

    work life weigh convenient touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
