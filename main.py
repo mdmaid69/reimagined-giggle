77e3994391fefe04f9612180c0ba9c44aec7fa5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:19 +0000

    artificial embarrassed thankyou smile unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
