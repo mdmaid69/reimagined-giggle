24607513f3cc0441f060143ab7dc79e2e637290d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:56 +0000

    plain the priority strict tour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+def is_even(n):
+        return n % 2 == 0
