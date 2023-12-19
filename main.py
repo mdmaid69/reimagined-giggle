d9c73d8109574ed7e53c05855aaf46d0e2d2d6de
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:15:01 +0000

    season back rent future nor

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
