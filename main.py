be622af0f1470949b05d9d27b677bc8d6af41373
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:28:22 +0000

    hungry success mine side mention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
