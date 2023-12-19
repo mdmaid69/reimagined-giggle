49a1ad1a9b54edcde16e53e2e957aabd06910fa1
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:48:09 +0000

    report popular Oo thank will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_area(radius):
+        return 3.14 * radius * radius
