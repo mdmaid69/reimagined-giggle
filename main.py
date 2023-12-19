2a3a5c94dfb84281fef141572e4c77542275eb01
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:30 +0000

    how follow dick uponon quick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
