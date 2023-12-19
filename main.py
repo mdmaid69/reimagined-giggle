c27ff87188f279c223cb51532a36cd0c09eef81b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:32:15 +0000

    record suck coffee wonder owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
