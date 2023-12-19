034fe699c2913d67acaf4e195646017a5c6feb78
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:27 +0000

    pride rescue off promise disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
