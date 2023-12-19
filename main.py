d633ac2bc62946e3bb010bb973a7471f1e30538e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:23 +0000

    wait fall suddenly slice cover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
