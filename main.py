cdc27a27deef1b48c0ac9ad137ce8589a0e85ff3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:42:31 +0000

    aim kind full protect muscle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
