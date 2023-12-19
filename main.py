f502e06a008c5a6ac49876b44c86216468ae0a28
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:39 +0000

    modern juice star completely broad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def square_number(x):
+        return x**2
