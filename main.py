3c4b3d68ae5770aa2635db6117e8b7da57b330ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:00 +0000

    finger force hit damage concentrate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
