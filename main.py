5b088b60b04fe600f7dca11d6091e84c40d3231d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:58:02 +0000

    job judge violence usual ear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def sort_list(lst):
+        return sorted(lst)
+def find_max(lst):
+        return max(lst)
