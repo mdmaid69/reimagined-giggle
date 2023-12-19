2a257f0602583ebbc81886c7038eba9487010668
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:43 +0000

    include getback ride fast urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
