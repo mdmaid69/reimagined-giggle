8f8bc3f56a1efd85665265efad3508463eca8d94
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:41:29 +0000

    take young environment careful die

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_string_from_array(array):
+        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
