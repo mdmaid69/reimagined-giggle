deb9512582892eb473ab76796f9ce5b45f9dc2ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:38 +0000

    road sick sleep hardly trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
