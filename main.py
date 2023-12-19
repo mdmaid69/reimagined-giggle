333a753c929b0a803d6438745604c0cbdba4718e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:26 +0000

    day shame move anything journey

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
