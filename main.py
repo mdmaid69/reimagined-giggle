bdf8c03b01a9ced5bb4bd4b20e5d88e84c3def1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:07:10 +0000

    replace beyond pleased passenger somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
