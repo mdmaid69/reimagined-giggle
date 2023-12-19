00e940ffb5084ede72cea0a45ab0ce4000827497
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:55:26 +0000

    community music disapprove colour rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
