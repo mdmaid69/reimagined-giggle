8564969d2f456b8416098b0488f8b00411ca36ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:28:36 +0000

    shock both suffer or touch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
