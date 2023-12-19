7ce037bd2c3be68691298dc16a251983bccf018c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:54 +0000

    formal afternoon old degree spread

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-print("Hello, world!")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
