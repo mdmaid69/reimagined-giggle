9dc6ed1d2f5bf3e2508f425a0d70ec5216e12b63
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:41 +0000

    excuse wide include quantity attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
