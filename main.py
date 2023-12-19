c557a676a628e4e511a537ed38855e8dc7801cb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:39 +0000

    minimum born carry cough foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_permutations(n, k):
+        return math.perm(n, k)
