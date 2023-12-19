162311f191a9ee4b034a80b7849886304f90dec7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:10 +0000

    police fire maybe mouth unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
