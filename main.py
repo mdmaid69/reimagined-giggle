ef8a723173dd23419dedaa369803ad6edf5919b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:01 +0000

    speak mail correction certainly show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_power(work, time):
-        return work / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
