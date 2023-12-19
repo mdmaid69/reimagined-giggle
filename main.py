2d8488aebd5e0e38821b243816395809128ace5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:06:12 +0000

    sure hear comfortable contrast second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import datetime
+def get_current_date():
+        return datetime.date.today()
