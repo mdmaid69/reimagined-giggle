4589ce5b56b743783abcd2660a9981b97773b88a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:19:24 +0000

    practice culture declare warning collection

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
