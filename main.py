f6dc87f4b1487658034b9a5a194f76fe8ea1dd5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:04 +0000

    exam feeling pipe see Ee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
