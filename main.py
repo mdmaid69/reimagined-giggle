bc36b806cdd201424a75e96fc8ef4b4952f3045e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:53 +0000

    breakout spoil simple phone sticky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
