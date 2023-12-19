1e128bf3c88daa02952ba19286288e4ae857f1f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:14 +0000

    music him joke noise duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
