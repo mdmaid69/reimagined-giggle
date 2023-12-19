46ca3217fb6c0f966e63883c4a4e31918bfa4256
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:54 +0000

    pull dress suck defeat perform

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def count_elements(lst):
-        return len(lst)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
