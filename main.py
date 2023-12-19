c6c37d1591c38b52944c8eca5651077e790f3a8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:26 +0000

    winter spare relax purpose born

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
