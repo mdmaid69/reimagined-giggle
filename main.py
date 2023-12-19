9a81504187399e7584518e3ae432cba02b501a18
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:20:54 +0000

    stand height Jj dear side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
