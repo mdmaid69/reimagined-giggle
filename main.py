ce24539de7044c84ec1886c212764f8c8aabc8a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:52:03 +0000

    promise great interest pile tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def reverse_list(lst):
-        return lst[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
