5df4fa230e5c335b8e75fc19586cae4fe69be5da
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:49 +0000

    pump middle fight car answer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
