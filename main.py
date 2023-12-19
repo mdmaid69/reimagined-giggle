4225c1c23e5cc814e0cb6abfdc5ab7e258e45a24
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:31:44 +0000

    pressure generally born stay long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
