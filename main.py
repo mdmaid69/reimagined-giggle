90ea6af9eae59bb186cd56ad6da9a1e443f917c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:29 +0000

    scene fence around culture matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
