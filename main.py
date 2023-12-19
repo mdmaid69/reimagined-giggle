9d482c773f10f56acf31f30cfc2adf7b69118f16
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:16:59 +0000

    turnback period so disgusting basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
