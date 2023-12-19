2f1fdad5fcd1364cd2cb3d8e0623be5490e21732
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:37:39 +0000

    town otherwise flow wheel waste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
