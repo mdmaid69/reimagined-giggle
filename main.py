5a9689930e8aa2129097bc3489eeeb6e03aada7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:13 +0000

    challenge oil shadow unhappy forgive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
