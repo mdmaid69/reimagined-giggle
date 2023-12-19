ebe85a59b3ad8e2a74c206d9e4a07e095ded1822
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:49:57 +0000

    touch detail machine family always

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def reverse_string(s):
+        return s[::-1]
