9c0958e7da8e1134f0991940b5fc098d9ee8846f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:24:51 +0000

    probably drop pen actually condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
