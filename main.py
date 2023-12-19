a8c5d189226da25424b4338893ae30ee4e764e2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:10 +0000

    promise leg nothing thin quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
