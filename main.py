3fffc2c0a4de3dea327c66ad46830e6450e3f355
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:26:13 +0000

    slowly mistake Qq positive impressed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
