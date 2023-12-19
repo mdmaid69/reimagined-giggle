869091745786f91662b9987fa0ac2b2aee941a56
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:15 +0000

    sense get emotion cap decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
