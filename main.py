102ad3bae3762a03791bb6ddec54ce3d8dc179be
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:50 +0000

    estimate history woman stretch warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
