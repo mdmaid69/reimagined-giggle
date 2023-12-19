80801521d5f7c0291401765459f4940c30cf3fba
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:05 +0000

    limit patient abroad geton scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def square_number(x):
-        return x**2
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def change_working_directory(path):
+        os.chdir(path)
