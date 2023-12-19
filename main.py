afebe136ccb1c3290c86b766cdaa6a2944c771ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:30:51 +0000

    satisfactory practise gas chat young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import platform
+def get_python_version():
+        return platform.python_version()
