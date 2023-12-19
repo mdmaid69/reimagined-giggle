cf94529ad66b0a8a29bb3ed2e4dfd5bf510b104d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:30 +0000

    571enough satisfied spray shop match

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
