8dd657a0e19b075fd84fbd22a8645c286f7fe7fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:24:55 +0000

    interrupt set distant sale occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
