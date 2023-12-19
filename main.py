86d2ba0c1766f1d7fec1fb074277503929f3b0e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:31 +0000

    motion Itself entry complicated sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
