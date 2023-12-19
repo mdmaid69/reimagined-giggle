a9139e9598f8247470c0c24935adb5c9254bf972
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:32:14 +0000

    thumb wish block dance follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
