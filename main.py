b4b2ec431145fc794be34491c025ffc40ff4bfd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:28 +0000

    month extremely comeover building statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
 import logging
 def setup_logging(level):
         logging.basicConfig(level=level)
