784f841e7fe775a609b3f6a01b69e93d85fdef00
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:46 +0000

    doubt part loose poison Pp

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
