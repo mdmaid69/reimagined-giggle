54cc52c15cf78b328bc89c8b5dbe59bdd237fede
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:56:45 +0000

    criticism report law rely your

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Characters:", len(text))
