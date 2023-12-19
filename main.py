89149f0e4d6a01f98c1f95f6db7c834808a8a6c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:14:43 +0000

    whatever unfair fall dark honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
