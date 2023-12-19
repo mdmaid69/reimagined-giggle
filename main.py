48de0c48503d635ce0d5a07d75835568c57539ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:11 +0000

    relax tongue notice trade improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
