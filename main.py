58af4035e3c714669512c6a759f45ff775fc29ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:40 +0000

    line blame compare look world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def sort_list(lst):
+        return sorted(lst)
+import datetime
+def get_today_date():
+        return datetime.date.today()
