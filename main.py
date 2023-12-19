8f6d0438146c686e25e0085606ff15a8069e05a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:14:13 +0000

    hurry claim cross official neither

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_pressure(force, area):
-        return force / area
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
