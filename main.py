d27fbeaf22caee48cdba0d7ec87ff56d5b50cc9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:22 +0000

    smell nurse dry climb phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
