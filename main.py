407db96708468dbc0f3906509c0b09049bae7dd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:20 +0000

    furniture impression offer reject lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
