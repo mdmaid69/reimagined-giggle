ba68a8d1583a646e2fc780d222d0ddaf70146a4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:41:55 +0000

    rough live desperate swallow duty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
