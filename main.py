02be09d01ce1262d0d04b00dcb56af5c80f297f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:35 +0000

    pronunciation purpose offend pronounce home

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_item(array, i):
-        return array[i]
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
