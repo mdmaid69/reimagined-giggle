b6d536d9fab55e212ea9abb986aa680ee9efddb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:35 +0000

    lot shout suit early thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
