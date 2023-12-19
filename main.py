cbb00dd9e5af99fcc5f8e0032150ed943db6ec22
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:44 +0000

    soft flow cheap possession anybody

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+name = "Python"
+print("Hello,", name)
+def calculate_power(work, time):
+        return work / time
