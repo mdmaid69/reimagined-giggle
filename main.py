4f8a3a57decd5b154f0cd89a449994ee8ea48d86
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:19 +0000

    search winter machine hello urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-print(os.getcwd())
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
