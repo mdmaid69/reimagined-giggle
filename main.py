4d671abdaa317989f42628386403399c1a1c39f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:43 +0000

    message perhaps count Cc 572enter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import sys
-print(sys.version)
+  def convert_to_hex(n):
+        return hex(n)
+import numpy as np
+print(np.array([1, 2, 3]))
