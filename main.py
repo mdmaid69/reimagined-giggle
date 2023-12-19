d17a9b7e88c794f79d17a88d6962171a1bc6776a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:56:39 +0000

    do promise guess week keepup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
