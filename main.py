fbf7dd72f8e7417b75d2252af000c40363d68f11
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:36 +0000

    deliberately snow sweet comeout part

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
