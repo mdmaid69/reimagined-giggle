d65eb47889efa2b3b53b39ef89820c695b2b4d13
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:10:27 +0000

    eat present study through close

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
