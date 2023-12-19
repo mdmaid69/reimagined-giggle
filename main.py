5d5d657d387f740063220dc0aa7a3c6d682e869c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:16 +0000

    their last fail shock reason

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import json
+print(json.dumps({"name": "John", "age": 30}))
