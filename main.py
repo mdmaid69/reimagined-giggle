e92c1d9928fcfac2a2c7e78d67095d002f02193b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:01 +0000

    uncomfortable lean 572enter object over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
