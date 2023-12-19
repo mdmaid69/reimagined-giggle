e94a2d3c5e2958b35f9654445454da73b92a06ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:12 +0000

    include table have miss find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
