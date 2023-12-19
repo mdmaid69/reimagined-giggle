1329c8e4722f53f71659f9768b7a7b0c39a18fc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:44 +0000

    less experiment for continue 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
