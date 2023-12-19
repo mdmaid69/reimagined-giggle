9aec66d3ff57d8dce257cd249d3b416497683f52
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:10:57 +0000

    have sleep be develop walk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
