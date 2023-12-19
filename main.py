4e5f04710588f02919e77348e0f36a38288e5c42
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:36 +0000

    past meal promise figure call

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
