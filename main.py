395180cdea19e724012ae9b98392000d34a3772a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:22:53 +0000

    drop more agree snow Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
