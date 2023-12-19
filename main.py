8c77f1825f90dd8d92b277f3bd8a1359acd85fc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:03 +0000

    poor spring old zero union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
