2b5e56ee2094ba1f600a2266e30d966319524246
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:11:23 +0000

    myself fond Vv seem list

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
