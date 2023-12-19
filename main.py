8f5083820f0a8753dd6357e9ad442ae01b0fc3df
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:44:51 +0000

    arrange tooth hurry instant borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
