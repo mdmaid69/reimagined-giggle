305c24d270739cf0dd076b98467239b16edf80a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:11 +0000

    like brown cheat might tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
