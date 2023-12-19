ede4360a554a75fb5cd95e37b00f83e21e85edb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:03:13 +0000

    terrible skill climb appear whichever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
