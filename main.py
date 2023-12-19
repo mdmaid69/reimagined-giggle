cbc88f124fd290bf21686668f1cad99834dde556
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:20 +0000

    effort tie theirs direction stop

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
