b08293303c64bf4ba5d3dd36c408bd2ee69d62fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:34:07 +0000

    fence keepup stare machine low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import os
+def get_current_working_directory():
+        return os.getcwd()
