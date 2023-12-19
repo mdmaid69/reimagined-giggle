123a913d0e989947d4f93d36499904213995f3a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:58 +0000

    knowledge argument each upstairs temperature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
