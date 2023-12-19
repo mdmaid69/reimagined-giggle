96f59b087464c09c7c0efd81f3fefc2cf3bdd091
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:18 +0000

    blame instead workout interview work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
