50f518cd0e8c6ba438351db7c284c37dd8e086a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:48:21 +0000

    well work out very coat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_density(mass, volume):
+        return mass / volume
