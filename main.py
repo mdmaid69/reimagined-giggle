19b2e35be9bed14275a0dafc5f54c49cb7c83bb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:30 +0000

    fun guess main wind emergency

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
