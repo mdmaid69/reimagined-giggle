99a0bd26b94f461b0c4738999985e0a29ada8889
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:10 +0000

    first young progress raise afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import random
+print(random.randint(0, 100))
