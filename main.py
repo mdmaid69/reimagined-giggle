17b0efa2c0baff34da83f150afb949b0ce13d338
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:06 +0000

    mix service quarter position must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
