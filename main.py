b77fcc3dcd7637aa1ee859de3e9d06c1dd928480
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:34 +0000

    unless spill nose clean only

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_work(force, distance):
+        return force * distance
