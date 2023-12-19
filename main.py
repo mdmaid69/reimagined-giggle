d28a7a586c6f7f5f55b3343ec21df2ac4f9a8847
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:55 +0000

    spill instant along service without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
