75a7f53341db2a87bca9f917e4f030c6362ad7b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:50:05 +0000

    relation wake own dick family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
