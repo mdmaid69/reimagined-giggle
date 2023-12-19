526a7cd3894e57e8214baa57189819fe0c10c665
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:17:49 +0000

    quiet speak bored it just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_min(numbers):
-        return min(numbers)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
