3db163d69bd7ea6ee096caacd1dff80797f4d44a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:51 +0000

    appeal load lunch Itself thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
