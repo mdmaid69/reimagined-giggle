4c5ac389cfd90c21764e5e01f92d8ca738502f19
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:05 +0000

    skirt boat street hurt reliable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
