f5d2948867354a3324abc5c1596292e9676dd2a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:26 +0000

    answer trousers match because warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
