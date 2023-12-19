c8430b041fcc4ad5f6a02b05dfd3e3a90b870364
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:47 +0000

    lean impression lock yet criticism

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
