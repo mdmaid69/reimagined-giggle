2009f33c1ebad1ce4dae79511b0b34e06c0ba0a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:54:11 +0000

    also minute crowded effect easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
