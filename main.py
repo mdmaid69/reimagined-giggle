115f31fe5960c3b1e57b3b875dd64a569999bb7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:34 +0000

    flood evening uncomfortable adjective put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
