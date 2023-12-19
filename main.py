d61c5d2b3e1f80ed57771b151a8b3a4795d384ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:42 +0000

    equal shame discussion along chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
