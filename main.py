2b26697f379ca3a12b8479427bcd761197903b2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:47:02 +0000

    shop after immediate prepare control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
