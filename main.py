f0d40875f5d14ed5b7816dc5175eb5bf83d784a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:01 +0000

    break wave sad agreement live

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
