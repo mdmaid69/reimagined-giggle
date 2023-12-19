dfa4672e575a26faf8758491b775370ef784e1a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:38 +0000

    confuse date spray normal trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import numpy as np
+print(np.array([1, 2, 3]))
