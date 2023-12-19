bc9727f41189805bb172391b77ac2607ff910747
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:51:44 +0000

    likely field passage hardly speed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
