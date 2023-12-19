7d8344ef11792215124661a6e01ce82be4475d18
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:36 +0000

    either will instrument Gg swimming

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+for i in range(10): print(i)
