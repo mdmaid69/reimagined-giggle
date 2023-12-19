ff78b0e89bed802667f3bce85b5853e32bf395f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:49 +0000

    check used temperature pure failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+import sys
+def exit_program():
+        sys.exit()
