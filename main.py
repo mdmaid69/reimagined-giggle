06947d2863b4c6980aa0d2af64ff986fa0db5cc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:29 +0000

    write middle live experiment recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
