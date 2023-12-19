9b3c58cb68c2bef63961fafa5010b64fb8faf907
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:58 +0000

    obvious care watch popular thorough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
