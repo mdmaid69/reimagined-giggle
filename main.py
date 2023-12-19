cc6537cb120092a849754018ac5771d821119449
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:35:44 +0000

    hit Ff best stare stamp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
