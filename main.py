d3219d5a673eb4d13db1369861e3faa2651369b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:06 +0000

    achievement impressed memory difficulty relief

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-name = "Python"
-print("Hello,", name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
