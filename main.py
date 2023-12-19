7319370848663728a84f5a11ab3eadabe37a1639
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:58 +0000

    his sister none like period

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+for i in range(5):
+        print(i)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
