f953ca5002c44e7b650d18a894653b1017590932
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:25:35 +0000

    abroad expect be feed pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
