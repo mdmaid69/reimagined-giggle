53dc8f53cb3ae76651fa753d184b111628162d2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:57:18 +0000

    fine sort favour minority married

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
