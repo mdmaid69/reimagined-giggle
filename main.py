04d15990519ef9bdfcee964963dc7ee4bf099b67
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:00 +0000

    fill skin bet mess comedown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
