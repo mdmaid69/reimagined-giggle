8fa94c96376e1c878ed98317cf36bd25fef830f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:38 +0000

    bring send outside unpleasant divide

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
