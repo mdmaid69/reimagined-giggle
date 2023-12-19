bbbf287ed87ddf3cdbf9d90f869183ffd42cb065
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:51 +0000

    ahead soak though tooth stupid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
