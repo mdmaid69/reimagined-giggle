1d277ac10483fd06d82869c0f8c7912b09040ed9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:36:55 +0000

    wire secret lost stage dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
