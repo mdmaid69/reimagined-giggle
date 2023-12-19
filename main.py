44912026eaa0398302797befbfaa152b21e037b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:28 +0000

    handle partner shine lookaround list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
