d702a0127cc6e5ddf4f812d76c1b771c7a931dfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:41 +0000

    change by basis sing card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
