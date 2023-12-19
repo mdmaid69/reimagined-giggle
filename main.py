f8de7895d0680f83eed8ed1fdf6cefca4bcabd46
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:59:33 +0000

    morning mistake move cause Zz

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
