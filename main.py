46e3e652825292fded19036f641e3168991e0772
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:12 +0000

    event lead confident problem neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
