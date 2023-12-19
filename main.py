582aed5872946ec9390ea756d06981872bd6de73
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:19:30 +0000

    sad exit fit better warning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Characters:", len(text))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
