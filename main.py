6a5bf8c88a446393c84517c0b72200a31e37e1d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:51:14 +0000

    calmly tidy harm degree tonight

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_area_circle(r):
+        return 3.14 * r**2
