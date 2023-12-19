e9b9e61b8e3b08e4145ecf814b9d346caabaabfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:49:08 +0000

    closely city apologizeapologise book language

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
