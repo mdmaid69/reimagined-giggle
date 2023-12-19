00e373c139ed772f25b2101589baea4cf9c7b766
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:36 +0000

    colour central table tragedy beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+  def square_number(x):
+        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
