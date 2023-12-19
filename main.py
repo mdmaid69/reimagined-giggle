d79a1e2fd670e6e26d1709730adbbb6b509c918b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:03 +0000

    both meal peace there their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
