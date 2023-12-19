2b8c3886f01cb3a5ca19633ac11a1392c54bc13a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:34 +0000

    by post anywayanyhow remain course

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def convert_to_binary(n):
-        return bin(n)
-import tensorflow as tf
-print(tf.__version__)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
