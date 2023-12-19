4b2a399cd7470a7b658af4cfc2b350417ce818fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:27 +0000

    say soon accommodation noise breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
