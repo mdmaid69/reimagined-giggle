2e25516d628234c346803746e8cd971a1ce873cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:02:01 +0000

    crash flat average beat start

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
