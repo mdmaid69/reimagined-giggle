6490408ceb64c57a4315b0d57fabb72482cfe08b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:42:48 +0000

    score dick marry forward me

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
