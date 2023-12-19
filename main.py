14e6affc62268ffaf3a0384df1ac1c51504c4e4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:39 +0000

    bet art uniform respect cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
