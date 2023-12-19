1f9185966eecb5c079b62d1e9bba98dfc164e9a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:46 +0000

    muscle unpleasant call wish rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-print(os.getcwd())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
