dad9615f7dc0c2a950d86ecb436446248bd7bb37
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:42 +0000

    behaviour reject major wall clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
