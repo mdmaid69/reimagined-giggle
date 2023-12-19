d98cb0a913fd45862966525eedbdfec71e2f2330
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:49:29 +0000

    form special square getaway only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+print(math.pi)
