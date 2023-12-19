ef95d0818518ec9b5f1d5c1570d7fa0d88e33e4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:16:48 +0000

    convince whether lookafter everywhere soft

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
