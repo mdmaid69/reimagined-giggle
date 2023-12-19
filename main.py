04d131271f39e656c694f3c52d86eaff95376e8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:55 +0000

    important poison once mystery learn

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(10): print(i)
+import random
+print(random.randint(0, 100))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
