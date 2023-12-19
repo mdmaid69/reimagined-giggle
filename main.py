6a1b873b6dba51f5b1e71a63eb0a7f5cd3d0a733
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:10 +0000

    exciting that trade date part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
