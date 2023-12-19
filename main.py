630083c946066e5827ec0bd05614af97c74f8bb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:41:34 +0000

    art instead getoff bite lower

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
