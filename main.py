f9bec552a8c5bf96ed670b1342ca313750025a22
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:24:00 +0000

    part noise border but search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_string_from_array(array):
+        return array.tobytes()
