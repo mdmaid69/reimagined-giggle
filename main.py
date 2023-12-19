a7aab3883a50ec4e928e9d2f671deba9f04846bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:47 +0000

    communication instead slight hit yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
