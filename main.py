b4ff297188f5ca4a836b6e98fd3d010d1120cb2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:04 +0000

    sort lesson arrangement split answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
