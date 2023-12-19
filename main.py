a4098927c2f125a11410442751433e216d224c7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:45:42 +0000

    life cut near interruption breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def delete_file(file_name):
+        os.remove(file_name)
