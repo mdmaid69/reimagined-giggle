60284581d8d01e4c83b32de7a117944235cb7a03
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:19:04 +0000

    for murder few breakfast machine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
