70113c8c7fd16c869da87621c8399129850b866a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:40:50 +0000

    load as brush 570enjoyable anyone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
