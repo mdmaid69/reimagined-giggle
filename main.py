53bb021b85d8ec98111e0b57a4d14edf74c71436
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:44:19 +0000

    off single ordinary set block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
