33a1dc3852979facfaae68a3f1634fac5e8372b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:26 +0000

    flat until makeinto approval unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
