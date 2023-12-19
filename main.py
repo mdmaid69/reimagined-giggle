49f581d2c295daf9cde37b30a5eef68ed6f83966
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:59 +0000

    wander also close separate national

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
