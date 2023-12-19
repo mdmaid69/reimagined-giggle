0d283a49f90e4e4034caddb287ead525c5e84c30
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:34 +0000

    possession else rescue organization read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_work(force, distance):
+        return force * distance
