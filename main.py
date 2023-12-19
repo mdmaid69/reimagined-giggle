95b53d9770973578a8c0542443bdb47f2cd9c782
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:28 +0000

    right flat pleasant profit sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def subtract_numbers(x, y):
+        return x - y
