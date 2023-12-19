290724bac0323bde6f5b381574cadf20d974e196
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:36 +0000

    suit reliable winter born careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
