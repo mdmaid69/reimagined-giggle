3ffba6b77b8f0906f7fc1e7b07c98452fb773bf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:25:16 +0000

    spin generally scared instruction time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
