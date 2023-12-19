864a89578cc5194cc5f15abdb84e976a6c648452
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:42 +0000

    flow quality continue invite everyone

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
