4ca0e49c2bc6016b028d1998d69ae089023aa216
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:43 +0000

    tragedy left see mainly service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_queue():
+        return collections.deque()
