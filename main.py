a50c528831a05df96f3a4e38712506a52e2ba841
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:20:47 +0000

    whether luck central impossible line

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
