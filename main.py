41f2a3bf32ca1f4063d6ad4ec74a315fe0a5900a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:55 +0000

    number hit doctor hurt consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
