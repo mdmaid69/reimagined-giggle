dcf3f0a69097ccccd008c8ecf15aa592898332e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:36:51 +0000

    Itself desperate friend lock crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
