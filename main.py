3e4233be843fdca80cd1b6204390263c3c605182
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:37:07 +0000

    tonight cut still move apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+print(math.pi)
