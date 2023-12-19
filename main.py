763df59adfff562f52a670e7159f97e160608228
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:26:02 +0000

    water tool across support sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
