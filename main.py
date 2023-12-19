2da2ef353a4f146e55ce35e540bd6ae81531b084
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:24 +0000

    deliberate ourselves body love odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def find_min(numbers):
+        return min(numbers)
+import collections
+def create_counter():
+        return collections.Counter()
