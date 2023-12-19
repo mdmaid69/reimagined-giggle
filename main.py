0392dfc277b4e83aff03aebeea5c3df1f0f16f62
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:04:56 +0000

    pleasure shoulder wander attach hospital

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
