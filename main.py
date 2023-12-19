134de634ed9bc94592c18733bf4f27e8fb58ce9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:00:27 +0000

    family aim gradually together worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
