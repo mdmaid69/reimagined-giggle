a52f351e5a5a6c4aab7eb66be7be79bf7f1537b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:46 +0000

    interview total strike single godown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
