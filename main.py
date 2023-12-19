45aba4874948978017aea641e3102de8c97f4854
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:50 +0000

    regret usual boring conclusion unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
