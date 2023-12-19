55ef8bd8c21b662b63859e7642d5808038301f86
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:50:57 +0000

    high grow shoot care character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
