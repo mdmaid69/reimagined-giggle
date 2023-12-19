bbf3240f5800cb83ce761895b5f12ddc879a9740
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:11:41 +0000

    pair generous admire clothes imagination

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
