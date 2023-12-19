3585783b1319fb55cf95f9b1648bca82d3c6ed25
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:23 +0000

    actual sticky possession job fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
