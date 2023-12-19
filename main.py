5fc170bf77ec6734528c01dff31b50d43f62c020
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:40 +0000

    base fact threaten fat visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
