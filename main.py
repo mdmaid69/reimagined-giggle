dea710b8934da51409ca458390407e5a88b71401
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:17 +0000

    get system underneath urgent hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
