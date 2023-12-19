db0db5adba43611a6e75bac6fcfc12ff8aaea784
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:13 +0000

    honest church suit some interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def is_odd(n):
+        return n % 2 != 0
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
