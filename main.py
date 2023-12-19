ac732a0d4bb2c39857751c7e720a260fc2d0e4db
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:33 +0000

    wish large escape themselves fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
