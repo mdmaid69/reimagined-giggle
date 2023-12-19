4e22217b583210167a0fb2320d653a41d10c73de
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:49:02 +0000

    disappear badly there listenfor patient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def reverse_string(s):
-        return s[::-1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sys
+print(sys.version)
