c104a2bdb9000049f0c88f260bfc6e9239dbf08f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:37 +0000

    how unite near coat load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
+import array
+def get_list_from_array(array):
+        return array.tolist()
