2c432135d20aff638b2851bdbce215d8f2e3601c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:29 +0000

    aim message entry quality interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
