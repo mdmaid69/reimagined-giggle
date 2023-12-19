e80f26350e880a42290fd6988a2f36b53e5b5578
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:33:13 +0000

    act closed miss related thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
