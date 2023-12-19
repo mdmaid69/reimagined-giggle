6eba88679c44f6dcf9cf4f699b889a51ddd0928e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:50 +0000

    cruel out plan price raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
