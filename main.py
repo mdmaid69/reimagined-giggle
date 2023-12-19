f088715de5392101b26dce9fd2d5c76020761729
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:37:18 +0000

    moon head smile answer lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
