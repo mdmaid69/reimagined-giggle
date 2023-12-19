a8654d5bb0c28259f7eca4792a15be655aa7da55
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:19 +0000

    recognize pleasure impressive coffee connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
