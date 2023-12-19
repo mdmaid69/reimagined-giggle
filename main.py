cab9d7bedb37d5517eb3b159d4c7929c67ba9d91
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:05 +0000

    whom trust protest card takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
