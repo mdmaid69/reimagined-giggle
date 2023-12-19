27b44a057ab2a184cdaf3ad92f7fe272b643ac0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:21 +0000

    gather keepaway no suckme impressive

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
