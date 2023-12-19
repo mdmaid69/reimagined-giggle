39495470e26724df8be81420fd0553e56e226dfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:36:40 +0000

    rain bridge notice odd hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
