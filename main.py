6eafb5bb075068c0d2952be52e935ea9b04f159c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:35 +0000

    bound clever trousers 573entertainment callup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
