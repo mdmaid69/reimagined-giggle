4f5bb8fecaf67a6bd892c009c3e51936bd5da89c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:23:49 +0000

    mention police hurt speed keepoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
