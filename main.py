cbecefbc8900cadcf42115344fd3791276e2112d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:37 +0000

    bringforward brother consist hole accept

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
