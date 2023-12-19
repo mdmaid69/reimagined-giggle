072cda2530a73efb5f77537058975b6558da7bc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:16 +0000

    legal comment operate opinion ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
