6176d85948a1684dfbf8aba1d70f38af2d837f28
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:51:57 +0000

    his uncertain jump protect breakout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
