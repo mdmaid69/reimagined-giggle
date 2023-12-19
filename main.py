c19940c341f4cb0b42d30f2652cc20363f80eaad
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:24 +0000

    impossible broken face occur schedule

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def count_words(sentence):
+        return len(sentence.split())
+def sort_numbers(numbers):
+        return sorted(numbers)
