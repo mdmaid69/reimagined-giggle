20947a62da1f6f4b8a1adb7d3769560d5915a5a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:57 +0000

    thinkof glad temper lift seat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_work(force, distance):
+        return force * distance
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
