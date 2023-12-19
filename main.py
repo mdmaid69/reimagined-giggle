1618cad74322f746fec9c3470fcb6c1ab9aa2506
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:48 +0000

    promise else interested near rank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
