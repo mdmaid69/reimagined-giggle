6c5fe02be854ec04b6170bd639001eb8eada083f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:37 +0000

    about 179bite food hello edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def is_odd(n):
+        return n % 2 != 0
