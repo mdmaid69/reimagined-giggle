d50ef66bb7c6058c6c1e02063b0267322d136234
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:22:13 +0000

    fun responsibility print owe free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import datetime
+print(datetime.datetime.now())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
