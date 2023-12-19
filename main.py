b9e1080da2795302e491ce2e682157aea1ad851e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:38:38 +0000

    away produce name know face

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
