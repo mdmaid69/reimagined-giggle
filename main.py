3b55bfc3f1fd559fdb43086d64ade9fbbd349cba
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:22 +0000

    giveup sensible 571enough influence hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
