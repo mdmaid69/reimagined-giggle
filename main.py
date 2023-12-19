fdc23e9aac752042ec476ef3b8b8f6c2f55465a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:01 +0000

    generally suck cook recover him

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
