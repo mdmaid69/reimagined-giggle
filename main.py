c34228fc2705bfab249a3f503243d177840f9c33
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:40:48 +0000

    classroom song correction him occur

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
