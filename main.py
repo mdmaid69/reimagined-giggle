ba5286111c0e1ac48ed2fd5deaa2e73942d842ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:40:36 +0000

    divide fight test emphasize see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
