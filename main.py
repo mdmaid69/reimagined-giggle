1a5997033dd22a1a9fb4de36060c0e4557ef1435
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:23:51 +0000

    yes tough team born answer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
