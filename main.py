252a472951045ccf5bbdfd8f7b54e6abbe983398
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:45 +0000

    sleep like medium many under

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def add_numbers(a, b):
+        return a + b
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
