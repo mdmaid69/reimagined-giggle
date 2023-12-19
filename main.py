1cad2c96cf0d167b487a80f0110f0b7f26a9758a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:10:18 +0000

    easy but 565end light callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_work(force, distance):
-        return force * distance
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_current_directory():
+        return os.getcwd()
