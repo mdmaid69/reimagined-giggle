8ba7f9a49d46a1c83995c09ee0078e8ae754a760
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:20:11 +0000

    obvious callat trust immediately difficult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+for i in range(10): print(i)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
