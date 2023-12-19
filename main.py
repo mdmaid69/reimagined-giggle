46baf2c8f277d359657909e3039bd8ace5598ec4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:33 +0000

    broad unless late crowd store

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+  def is_odd(n):
+        return n % 2 != 0
+import array
+def set_array_item(array, i, item):
+        array[i] = item
