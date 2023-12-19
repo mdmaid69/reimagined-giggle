6e9636912174a67ab048c6aa592ee086bda562fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:27:25 +0000

    fairly result contrast deal uncomfortable

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-print([x**2 for x in range(10)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
