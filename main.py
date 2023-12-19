6f07ee6d8ea4093baa47c75399fdae04f4c87e1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:44 +0000

    566end translate standard crowded than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
