d6047025839de8d553acae7853e0d987b0c02cc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:28 +0000

    bin catch border achievement life

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
