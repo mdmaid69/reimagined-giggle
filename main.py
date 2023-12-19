f518501f3dff62b59e8be29b69b4764a6700978e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:34 +0000

    large callat personal activity may

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
