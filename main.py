5eda9b008b69cb51a6929244d4222535b1d1a74c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:45 +0000

    display underneath into space before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
