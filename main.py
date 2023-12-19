34b1c251688b57130016356d644ed05d51da9956
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:32:57 +0000

    progress rent confusing family action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
