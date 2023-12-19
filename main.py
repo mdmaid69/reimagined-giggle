f4cfb88b382533b6b19aebb268dfebcde1ef6cae
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:14 +0000

    track duty dry Vv teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
