9a3eccd98c52c139452e2febc4377ccf689ee686
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:51 +0000

    serve part object manage remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
