b348defc187a99186f8e8afe6641414be9e0078e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:25 +0000

    remain business how mend part

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
