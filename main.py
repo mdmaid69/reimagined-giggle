ffaa84838c24d5bf949586449950187b1dd2c264
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:04 +0000

    accidental proof repeat exaggerate room

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
