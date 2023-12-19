f0f2ae46eecb91bfde5b938de2f120284d5a0d49
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:41 +0000

    neck bag public rush meeting

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+print(math.pi)
+def calculate_area(radius):
+        return 3.14 * radius * radius
