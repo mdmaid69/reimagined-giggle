f4e9c5f9435773e0c7c8575d821b802210211adf
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:35:52 +0000

    anywhere start rush you rubbish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
