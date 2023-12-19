2283bff25cc4ca61f52b58121586a3aa841cc4ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:17:56 +0000

    spill worth shallow turndown myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
