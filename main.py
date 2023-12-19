61e17d7b1bc6194bfaa3fb9c1daf1d8224fdd207
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:31 +0000

    custom blame Bb week smoke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def clear_array(array):
+        array *= 0
