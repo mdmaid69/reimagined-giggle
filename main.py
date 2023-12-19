6c1c64aabc7f9be0d526b97218f9b11f59ccfd69
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:19 +0000

    study firm border 566end cruel

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
