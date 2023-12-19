5bd823ef319a6a1ac05626acaa82d68f152d3a2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:58 +0000

    pretty consider hers job major

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
