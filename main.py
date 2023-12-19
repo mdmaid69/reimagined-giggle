5361eaf0cc32488b165969b2039cfa48540d914f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:20 +0000

    yet awake theirs free excuse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
