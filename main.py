21a4e13e090a1bec7c832b07ed4ca05cb3c02f6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:08:31 +0000

    trousers spring run file takeover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
