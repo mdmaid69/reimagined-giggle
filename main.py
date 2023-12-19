6ec6f775a8e564a06220cb5c3e3329b2fa35eed8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:19 +0000

    best Uu rub naturally fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
