90565ea15863a5f341b162edfbd90bdd41815492
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:48:21 +0000

    slightly spoil comfortable mess plan

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
