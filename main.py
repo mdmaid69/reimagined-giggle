3f1d986edc1013e12ebdfe9dc0c5fcb7d947cc64
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:00:54 +0000

    pretty range concentration without afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
