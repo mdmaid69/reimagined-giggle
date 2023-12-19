61ed78380ebbcc0790531bb0cb8ffedbe6b36728
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:07 +0000

    shall quick record attention regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
