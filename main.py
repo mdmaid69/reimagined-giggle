c8e8b9d2f713a180549981b127e01e236cd02524
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:19 +0000

    classroom instrument talk more or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def subtract_numbers(x, y):
+        return x - y
