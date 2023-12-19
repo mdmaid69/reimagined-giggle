3def19ad005f835bdbf33943a22b2b914f15537c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:19:19 +0000

    empty common smart back fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
