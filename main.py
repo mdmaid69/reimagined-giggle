cfb2e7267d7f3169df60ba09dd990270e0b02b47
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:14 +0000

    present once environment interest forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
