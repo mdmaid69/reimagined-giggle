4585120115f9023c94734f6ade9201c8c14fa69b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:27 +0000

    goaway rely intend consider unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
