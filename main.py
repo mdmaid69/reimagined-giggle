b7d34b5e85d58cfea514cbf644250420bc48f184
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:41 +0000

    round bin lay mad otherwise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def multiply_numbers(x, y):
+        return x * y
