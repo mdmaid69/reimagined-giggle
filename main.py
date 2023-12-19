67327183966a6ceb126cf92887225a58ff62f74c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:54 +0000

    spring stamp stranger late suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
