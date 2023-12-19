7a69ebcfe38349e20dbacac5c8f0c54466574abe
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:11 +0000

    contrast myself post will back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
