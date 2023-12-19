7ba9fd5189f2e2401d6f055e04f1606202833a72
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:48:29 +0000

    original early path concerned disgusting

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
