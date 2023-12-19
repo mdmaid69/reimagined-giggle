908676c2447a5f30573482a17ee4d17171c0b4b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:15 +0000

    search workout justice Ll light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from collections import Counter
-print(Counter("hello world"))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
