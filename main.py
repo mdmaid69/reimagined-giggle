36e2f9961e58b1f885a55e9176c6dd5f68312d49
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:48 +0000

    certain arrival long down wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
