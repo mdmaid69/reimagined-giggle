28ceea36d8c901783866506e83f3f9f16351ddfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:09:10 +0000

    upset battery Qq wash discussion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
