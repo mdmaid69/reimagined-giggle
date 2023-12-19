c5de062f76737fe2a4267881a56a87dc29e529d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:35 +0000

    surprised for sing long argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
