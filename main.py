ad9bd885e8d3f49cd0028a6ab1fc2f08b1a7e44b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:03 +0000

    desperate hello task curious right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-print(math.pi)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_square_root(x):
+        return math.sqrt(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
