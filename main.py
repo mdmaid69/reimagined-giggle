0bffa1403277aca0b6301d4a60846c54a0dbe8f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:41 +0000

    judge inside season obtain pen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def reverse_string(s):
+        return s[::-1]
