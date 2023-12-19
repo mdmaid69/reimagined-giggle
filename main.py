62482778ab9ab7112957f88cf7bf3a4d86ca747a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:49 +0000

    noise turnover during switch doctor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
