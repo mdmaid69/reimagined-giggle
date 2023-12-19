e2f67c935973dd403ebf6e4b2b43ef17c080f158
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:05 +0000

    between piece even even quiet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def square_number(x):
+        return x**2
