7d60ce739f760bdbb70fef10692390eb5f0ba29e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:19:54 +0000

    happen group care shock remember

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def sort_list(lst):
+        return sorted(lst)
