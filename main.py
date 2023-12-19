9134c3c39abdf140c8d189947630adeefc43974f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:24 +0000

    worry type sharp picture goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(numbers):
+        return max(numbers)
