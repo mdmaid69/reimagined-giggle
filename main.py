d3262641615aeb43528fa559cc6cc0f1ccb420bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:55 +0000

    breakinto extra basis worst clever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
