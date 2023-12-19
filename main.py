9b7f398d458bf4fe3728deb0157ad702dca0101a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:38:38 +0000

    wonder have achieve chief explain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
