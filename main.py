f7262b385767c2cf2e328634b921856f810322cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:49 +0000

    listenfor breakthrough hurry law straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
