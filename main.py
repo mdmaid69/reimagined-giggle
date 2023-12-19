101d95151f48b11f49b98ce5d11515567dcde456
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:48 +0000

    crash spring impatient stick man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
