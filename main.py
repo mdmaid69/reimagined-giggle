4fe4658c6b906b7f163224048736a0215ac4c3b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:50 +0000

    mixture whom difficult treat tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
