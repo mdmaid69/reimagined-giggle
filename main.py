289ba6d2effd67021a024bcd0692daa62ddfd4a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:28 +0000

    bar curious describe crash reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
