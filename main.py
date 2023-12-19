c0032f884f8bcf01b5ae6c6fae13866fefc453c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:14:07 +0000

    height date murder pool used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
