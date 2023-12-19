d478beec5e6a4db078421b8d21cf3ed51396bb08
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:54 +0000

    sense motion stand commit brief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
