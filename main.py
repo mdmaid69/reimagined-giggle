66f1e089b0c4c48410fc4fab0ee827021fffd285
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:24:15 +0000

    either hers special side turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
