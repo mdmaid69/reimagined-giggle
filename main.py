8f8e526e062df71a7e9c600bfc4e4373e234bb9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:40 +0000

    few steer soft glass birth

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
