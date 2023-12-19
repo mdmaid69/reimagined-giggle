189be2f13c5da21d8eef6601f3c7a1a45106fbf8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:50 +0000

    keepfrom interview accordingto report plenty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
