325e7967d713dc4e62dfe7cfd4dc29318909dafb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:22:39 +0000

    calloff wrap them hard hers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
