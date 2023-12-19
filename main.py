8b3e263d1b8f01d124ac5f76dbc5410f71107434
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:14 +0000

    rain breathe result of plate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import sys
+print(sys.version)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
