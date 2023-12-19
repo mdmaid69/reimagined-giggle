cb89bdb995e8bdcfb8ef747ae8b214b08daa0986
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:34 +0000

    cost keepout health shut notice

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
