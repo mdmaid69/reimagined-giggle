5e021bb445939d8f8a5943ef3e9300a50bc862a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:23 +0000

    pour agreement react tight power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
