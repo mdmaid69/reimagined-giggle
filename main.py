248427034bb8771872d3edda90c8e4fb2c7eaab3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:24 +0000

    majority careless fresh only better

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
