2287227e7c196134f5a0dc9b5d05796ba9acab16
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:00 +0000

    takeout sink minute although generous

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
