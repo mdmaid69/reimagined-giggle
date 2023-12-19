c767ce9fbf75dd448ac6f7787c29fb802c1f3e09
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:31:22 +0000

    turndown computer himself pleasant understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
