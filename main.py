f2780bafd4091c99830f8eab62ffbc83e083ada8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:50:50 +0000

    blue swallow tall possession stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_floor(x):
+        return math.floor(x)
