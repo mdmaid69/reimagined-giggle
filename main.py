8106e80c5f62804d77c021b2421a2b3b23fa5189
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:23 +0000

    conclusion importance most heat forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
