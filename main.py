fa95d532d3db577b06bcb714d077250670c020e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:54 +0000

    slightly pleased protest national turn

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-print("Hello, world!")
+  def get_current_directory():
+        return os.getcwd()
