ae2b011b65a030200502d5fbb36b9d444b84fc60
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:14 +0000

    rare lie tongue Tt thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def split_path(path):
+        return os.path.split(path)
