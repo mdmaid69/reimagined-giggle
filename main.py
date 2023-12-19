f8a7d0e1fa7af3b5f359233456db819666c78eab
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:17 +0000

    false us none pause fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
