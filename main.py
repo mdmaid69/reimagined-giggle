6c1bd3909f9436e46bedbf8ec648ca3254034946
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:10 +0000

    head not solve tour about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
