5772eda21674e7ed335bb7b52e6cd43ee459223d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:48 +0000

    on dream recent lonely question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
