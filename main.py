73c6f44b65bac264ee5b3bf99b6a6728f473a693
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:57:41 +0000

    thumb straight describe laugh criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import sys
+def print_python_version():
+        print(sys.version)
+import os
+def remove_directory(path):
+        os.rmdir(path)
