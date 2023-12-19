ff33c3fd882a92f95239aefe46410f48a0c8a286
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:51:00 +0000

    attempt our language ask effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_unique_words(sentence):
+        return set(sentence.split())
+import platform
+def get_os_info():
+        return platform.uname()
