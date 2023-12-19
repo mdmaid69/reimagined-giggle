5642809e31c7b9ce346bfd30aa26ad6e8e05dcf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:21 +0000

    strike home urgent radio direction

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
