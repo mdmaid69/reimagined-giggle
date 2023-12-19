ddccfdd08560176e6a4a4aa60918ed8aabc502f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:59:31 +0000

    many easy constantly exaggerate open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sys
+def print_python_version():
+        print(sys.version)
