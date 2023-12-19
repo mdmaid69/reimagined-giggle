694340365cd52abb280dea628cdb9e99cd025df3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:42 +0000

    division runover turndown until comfort

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
