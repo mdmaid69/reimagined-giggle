17bf9bf8c260905d55f7116f7a1d7b9d13f2f9cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:02:01 +0000

    fit mix number direction shine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
