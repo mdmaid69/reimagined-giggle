6a493d4094df779024f34d758d768f2bc8a4a9f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:41 +0000

    criticism admit produce comein entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
