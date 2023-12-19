9ee475bc25cbc0913514ecf3888c9d5c3d7f979f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:35:09 +0000

    task weak world promise runaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
