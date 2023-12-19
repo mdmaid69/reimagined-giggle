e00cc6b308ce1460c65492b3102a5d76347e69d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:22:48 +0000

    admire rush future success surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
