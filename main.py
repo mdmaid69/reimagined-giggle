2416a25a9a5622d8a18b64da08e2f825f84fa5b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:00:17 +0000

    miss cheque embarrassing slow company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import platform
-def get_python_version():
-        return platform.python_version()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
