ef40e0a3356ce24b3cd5f1f371c13f99c5cdbc36
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:05 +0000

    one fortunately perhaps switch happen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
