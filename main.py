edfaaf4bb7a66d0c508b7ce3bd39a8ed13de62a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:34:13 +0000

    goback(to) comfortable calloff answer experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
