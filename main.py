95551050aa5d12ead0b9aeaafbfa6857d68ab353
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:04 +0000

    treat strict defend smart shade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_acceleration(speed, time):
+        return speed / time
