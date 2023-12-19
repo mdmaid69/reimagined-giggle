d767ceac2c8b77e9e8ae376649bba9933c41e117
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:55:31 +0000

    deserve equal possession kill rest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def split_path(path):
+        return os.path.split(path)
