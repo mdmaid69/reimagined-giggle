81c3b7626d1463f287722a865ac2fc058d85be4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:13 +0000

    where bit crime centimetre now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
