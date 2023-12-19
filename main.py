8b05fbdaa05cb44526d72e015d5ca563d8839dd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:01 +0000

    regular Ii accordingto easy cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
