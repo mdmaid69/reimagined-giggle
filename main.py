b92b7b9eda21e7e5a0034c4598f24240981b7b6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:32:26 +0000

    central less computer pour excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
