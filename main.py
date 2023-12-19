fba6ab64206de6e8ae57f3b90935d85239b7c4ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:37:24 +0000

    callat professional winter unfortunate long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
