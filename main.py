5f841a8c43137d4ea56b859ad398a194cf3cd324
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:16:24 +0000

    whoever note right approval tense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def reverse_list(lst):
+        return lst[::-1]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
