a5862692bedac391cab4d8e6ad0c3a30194dfbab
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:45:54 +0000

    subject slice work history movement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def convert_to_octal(n):
+        return oct(n)
