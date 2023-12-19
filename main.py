73a1654dafe953b2257460916e2022346be81470
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:54:06 +0000

    crisis pity relation platform which

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
