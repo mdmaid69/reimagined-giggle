3cd479cf3aeef7985b19d78a3381268fbf18b87d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:15:40 +0000

    difficulty future upstairs win ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
