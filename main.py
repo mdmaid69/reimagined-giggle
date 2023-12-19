2be39442a7a90e2d2580ccba0238268169d49014
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:12 +0000

    excuse typical action may Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
