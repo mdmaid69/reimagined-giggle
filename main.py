8102c222d780b43c9d6143eab13b012e50fd2b2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:45:42 +0000

    single male brief regularly its

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import heapq
 def get_largest_elements(iterable, n):
         return heapq.nlargest(n, iterable)
