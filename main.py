a15ad32f919933d5fddcbdc54518f7cd65a0315c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:17 +0000

    anyone bite street reliable record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
