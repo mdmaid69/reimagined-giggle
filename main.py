a6811b45517abadfc08be01fc04b01cdeb51a9de
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:01 +0000

    attach curious profit pen famous

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
