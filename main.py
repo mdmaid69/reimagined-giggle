83f07b8059848b6bf12b92ef5f21485de4581845
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:35 +0000

    warn summer loud trade crime

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
