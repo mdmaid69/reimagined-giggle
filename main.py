83c1565366fae1ccef1b2ed4e9fdabf71ffac4b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:10:57 +0000

    bang bend pot geton file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+from collections import Counter
+print(Counter("hello world"))
