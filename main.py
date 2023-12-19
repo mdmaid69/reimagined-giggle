f1ccca19adbfb45a8d5b245ea319207724c85aee
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:36:37 +0000

    crowd slide as centimetre board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
