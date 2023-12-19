15dafb72d458bfc7dde762feac827c2cf6ab94ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:41:35 +0000

    chance home brain comfortable turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
