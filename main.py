0f220b607b3169a5639171a3010dfdfd8aea9487
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:38 +0000

    mistake operation school precise spring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
