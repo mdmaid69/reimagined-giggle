f2379ea3f7a8ade850d1ea30397b0abf6218a323
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:43:34 +0000

    generous wash reduce solve grateful

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_hex(n):
-        return hex(n)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import os
+print(os.getcwd())
