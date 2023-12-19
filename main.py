a46fe35a3a99b80f9abee4cfe52909d3284e52c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:57 +0000

    ring community niggaacademy hurt represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
