aa642c8df1b4bf92621a00a5a30ed3ff98b97f6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:37 +0000

    rest throw step nervous almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
