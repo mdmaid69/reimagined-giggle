2cd338ca61ab41299a963b3a2f03611aa3bb781b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:40 +0000

    objection tap keepaway support final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
