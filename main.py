5ea771acf3e800d94ad32dc399d93a114e5b3118
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:43:27 +0000

    introduction satisfied fierce positive beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
