f6f3b491ef4216dabf659bedb4a67c766dd4db4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:40 +0000

    behaviour people less handle Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
