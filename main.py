e364882c36cde5e244f497f635bb5e5f4ad9cb62
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:19 +0000

    until minimum flood when although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
