532bae775d7a3a99ab51f8fdd26d78c1d1d914cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:11:39 +0000

    move squash still later his

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+print(math.pi)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
