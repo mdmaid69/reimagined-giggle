9519b42a9c1c3aec5be4b83f3b00b06ef47a45da
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:24 +0000

    understand several opinion thanks vision

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_odd(n):
-        return n % 2 != 0
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
