3fcc3056e6da1cbee53b076f7196c27293a54f75
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:36:49 +0000

    pattern meaning city sick crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
