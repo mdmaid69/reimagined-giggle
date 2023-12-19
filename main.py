a8256b8f79eb480d857178cf935349844645386b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:26 +0000

    support avoid programme position impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+name = "Python"
+print("Hello,", name)
