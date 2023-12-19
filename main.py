387ba3fdb89290f45275aa18be9e5a5541d32fb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:59 +0000

    eat plan back storm content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
