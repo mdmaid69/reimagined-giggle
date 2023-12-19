c11225cf4cf3d5e4c111b4964dc1d7eed0455fc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:12:24 +0000

    detail major yours lead view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
