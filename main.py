d09845681ea1517cac869711dd274ee4a875105f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:09 +0000

    delight raw consult range fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
