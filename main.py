b2f76325b5202d8fab09e1d28fee7e8de55bc973
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:55 +0000

    coffee land proof anxious to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-print(sys.version)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
