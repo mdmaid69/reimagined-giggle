45ba4f99cbab01faa62e178a1b4a45b2dc1864aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:30:10 +0000

    hard hardly ill waste friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
