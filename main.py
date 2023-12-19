5d8ad87a032e387c7353f695f2d6baeb41525bf1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:20 +0000

    remove hit to leave ordinary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
