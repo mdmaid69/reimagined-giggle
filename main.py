2c2bad345c6ec7c60d90272ea1a6f029343b082d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:12 +0000

    once under play ball convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
