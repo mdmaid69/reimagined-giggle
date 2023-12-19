c62a220ba28a7387b9fa30a6128cb07a70170c40
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:03 +0000

    lookback(on) region style flight duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def square_number(x):
+        return x**2
