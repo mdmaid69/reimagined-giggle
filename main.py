26b7e4d3d70bca1ae3f675bb5e74dc16b31768ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:09:59 +0000

    street study fun appeal sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
