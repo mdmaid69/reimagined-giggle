8ddd4d9c420a6da772a4d485f0c4f391f0ee7056
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:07:06 +0000

    search worse conduct determined programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
