75704fce452935dcec6a59f685a047e3e26b5ac1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:31 +0000

    broken always convenient guess extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_binary(n):
+        return bin(n)
