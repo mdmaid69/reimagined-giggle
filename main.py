59babedd6a22938076cb47e9a2fbeb5ffccbf70e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:04 +0000

    settle name violent responsible best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
