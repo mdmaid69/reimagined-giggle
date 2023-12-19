4474424e19402715d0252b084275b52f0382e4a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:21 +0000

    mouth friend guilty intelligent among

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
