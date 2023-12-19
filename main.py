e5760ac008440ccf90a8923fc84ae97508a2326c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:28 +0000

    tonight touch Hh copy only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
