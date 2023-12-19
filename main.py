44a9ea2ff06afa609aaa24450895edba720924be
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:15 +0000

    original process definite form explain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import json
+def convert_to_json(data):
+        return json.dumps(data)
