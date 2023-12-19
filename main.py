08b9d6ed42edb5a5a59b8d47ddab6fc13eb3284c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:58:44 +0000

    middle discuss distance part sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def reverse_array(array):
-        array.reverse()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def get_list_from_array(array):
+        return array.tolist()
