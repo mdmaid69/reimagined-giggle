4e0ce20dad1048510a1e1d7f783f0a666ceb3a56
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:09 +0000

    shout choose kick ashamed pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def change_working_directory(path):
+        os.chdir(path)
