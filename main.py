5e2671b29ca06807200392ec8d3b8063eb85eebf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:37 +0000

    healthy damage through big wind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def subtract_numbers(x, y):
-        return x - y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
