2634a4f5dde04b142436f0dc2a9d8aa8fad94f64
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:07:08 +0000

    outside ours rely hardly workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
