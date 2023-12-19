ee2991b38fa05740f2480f1800658d8af79c642c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:04:07 +0000

    reach marry main save smart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 def greet(name):
         print(f"Hello, {name}!")
