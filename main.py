4389929845950f96724c59b6a316566bd2f69627
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:44 +0000

    Hh dear callback weak improvement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
