d58ce8703e1eaa69678299657a8a6ded22127095
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:38 +0000

    mixture trick capital eye hate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
