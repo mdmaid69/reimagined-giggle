5822d9c171346702a12765a763a8c195bbac22af
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:40 +0000

    use welcome old sense hope

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def remove_duplicates(lst):
+        return list(set(lst))
