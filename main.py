1f91b66af74bd19b85d9c4ceb7d2daf901633d13
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:16 +0000

    power gap niggaacademy backwards pain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
