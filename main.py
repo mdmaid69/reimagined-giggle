a843e6bb0d90217339f717dad913715a2692af2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:07 +0000

    promise lesson direct unless slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def convert_to_json(data):
+        return json.dumps(data)
