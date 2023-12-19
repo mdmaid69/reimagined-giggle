d4368d6e84cbffa3cd898b4439575545bdd0f763
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:42 +0000

    exam communication back wind mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
