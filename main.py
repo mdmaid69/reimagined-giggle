a51c3038c557bbb6d6420261f8b400e5300f2da7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:37 +0000

    smoke anywayanyhow best combine long

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
