4e2380f8a0f15de5e465303e54f81685ca4175ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:51:48 +0000

    sweep on general reach example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
