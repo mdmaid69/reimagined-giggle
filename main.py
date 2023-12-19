4215c7653c826f5b2f8fa6519e662bdbf58d8637
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:17 +0000

    before similar fond back call

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def convert_to_octal(n):
+        return oct(n)
