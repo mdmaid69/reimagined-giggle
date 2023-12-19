a5f0a9bda9756c70b0c187e0052bd90caa55c26e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:46:17 +0000

    twist any dry sky damage

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
