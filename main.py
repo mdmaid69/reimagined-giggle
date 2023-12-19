d2732ae2c4a697f5509cef0d57b4f548bcd268b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:32:08 +0000

    just getoff feed choose tape

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+for i in range(5):
+        print(i)
