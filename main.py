7c672ff000a2d8887da72ca3e173cce710ae162e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:48 +0000

    otherwise may hardly desperate wave

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
