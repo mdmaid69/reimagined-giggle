2548c02b241fd9043cd833362e24f7112189d4a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:15 +0000

    unable turnaway satisfied expression false

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_length(array):
-        return len(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
