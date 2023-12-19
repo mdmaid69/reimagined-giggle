a19cfcda04c8b501f4ade861b772f289dcfd9a72
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:28:31 +0000

    load long genuine term while

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import collections
+def create_stack():
+        return collections.deque()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
