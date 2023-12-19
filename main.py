68f267e28b4bfc9eb122d465cd2ffeb201cf6ee1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:09 +0000

    environment artificial any it challenge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import platform
-def get_os_info():
-        return platform.uname()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
