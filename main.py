2e873c7c560e727657a035e69759695c856d2038
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:17:01 +0000

    limit beginning perform illness stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def convert_to_octal(n):
-        return oct(n)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
