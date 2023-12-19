943d11db34baeaab660d7df1a40f6f951604c334
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:53:04 +0000

    smell present paper term connected

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_pressure(force, area):
+        return force / area
