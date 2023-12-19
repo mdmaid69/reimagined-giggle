32f46f6eccfde5a3a1e0c3f97d1436dc70b33937
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:27:22 +0000

    negative soak runout worst would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
