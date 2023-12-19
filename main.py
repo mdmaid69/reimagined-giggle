159b248a5406d8aa47af35608c869faca13f2063
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:41:14 +0000

    part uncomfortable serious stupid sore

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
