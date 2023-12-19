b756dcc241ec391e5eb3f3c243fc9890902236a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:35:48 +0000

    afternoon recovery performance drink swallow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
