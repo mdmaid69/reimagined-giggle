f5d04c8b874a6c2af128c44862c67126c6c57ad0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:35 +0000

    past row mix exact moral

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
