41b5fc5aeae1f3b14f355dde05289d70c1d7577d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:51 +0000

    finally commit busy gate compete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
