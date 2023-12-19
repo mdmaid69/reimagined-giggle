e77c9f93a9d11c51a8d5db9b0fb6898e7d18c88a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:01:49 +0000

    punishment minute pair otherwise shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def delete_file(file_name):
+        os.remove(file_name)
