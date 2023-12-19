ca145f915a6bca3785d9cfe2d49ac9e19e1778b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:58 +0000

    private stranger convincing normal brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
