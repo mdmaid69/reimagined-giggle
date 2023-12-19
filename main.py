f25af7e5e6de97cf7a8e3831d5990de984eb3143
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:25 +0000

    Ww undo combine clear stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
