87debda436ff9f7495fbb8a4e2d9cb740f9be77c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:55:41 +0000

    occur single than machine forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import os
 def change_working_directory(path):
         os.chdir(path)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
