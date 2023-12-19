0235facbf2f6ae199f1566ba4c7dbda910c120ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:05 +0000

    consult OKorokay performance should appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+print(datetime.datetime.now())
