8ce68d9114f4b400f9efad36e6cc74044e1cf5ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:12 +0000

    drawing crowded rest work university

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
