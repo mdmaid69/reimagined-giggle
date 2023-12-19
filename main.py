a60ebdafebd7a929f2135cb6943efba29493f6a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:15:12 +0000

    slide check basic history weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_min(numbers):
+        return min(numbers)
