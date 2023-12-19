ba42195da646673b9bd0d8ae2f001ff3e7303998
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:46:57 +0000

    Gg practise you shut burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
