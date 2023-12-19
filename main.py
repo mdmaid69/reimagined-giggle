ec113b18a98748a200639da2e29aeab83076fb37
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:41 +0000

    whichever flavour meanwhile union plant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
