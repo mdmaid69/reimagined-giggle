7e8d73bb29628282f01a7e5e1d9b4bf62a8d771d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:37:11 +0000

    spare crash accidentally powerful connect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
