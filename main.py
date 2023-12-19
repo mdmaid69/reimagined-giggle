4d29c3a70ee0f61816c0584fe9cf901b7abeb258
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:17:25 +0000

    walk politics statement manage will

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_max(numbers):
+        return max(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
