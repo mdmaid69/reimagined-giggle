c7989f58162f7c4a5befccde3b48be929af42b50
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:53 +0000

    whole kill country spring cold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
