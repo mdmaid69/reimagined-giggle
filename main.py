f5df8699811bf38ca64f1f6c47dbce9219514954
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:51 +0000

    marry argue miss entrance beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_tangent(x):
+        return math.tan(x)
