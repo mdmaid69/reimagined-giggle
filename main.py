a160f9d7a0e31a7c44ba3bb2b0fe90c752de58dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:02:47 +0000

    general Ee free deaf reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
