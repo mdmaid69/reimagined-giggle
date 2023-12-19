1655901dfd38e3e2513440f1d6daa8d621ceee12
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:35 +0000

    interesting violent address imagine spill

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
