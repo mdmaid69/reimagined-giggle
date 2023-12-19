4d410799f2562991042a7bca007d01ef4efd3118
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:34 +0000

    trip poison could hour indeed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
