37811a1aff0aee7d1901240d90293978b4066636
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:15 +0000

    holdout contain reduce wait danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
