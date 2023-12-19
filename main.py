1267525726b4431ef549fc7757e20d8131f02154
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:36:50 +0000

    somewhere cinema medium action regional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
