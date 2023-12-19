bf497fca1324964ef6f1203b415e3d043557ad2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:01 +0000

    pleasant takeaway hungry sign so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
