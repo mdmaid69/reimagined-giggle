0a6f77824ca3b236781d79352cb527c0ac44493e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:53:05 +0000

    permission bored home along clean

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
