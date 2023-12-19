17325ccc0bae6ddbd59d3d04ff7e239c74fea434
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:14 +0000

    last drive small quickly check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
