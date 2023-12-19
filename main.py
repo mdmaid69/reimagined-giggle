e38a622f501886fd8a9a52aa40ad94a554017e14
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:08:57 +0000

    anyone line schedule will ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
