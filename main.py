9d4a7956a9fbb689fd680334adfb74800758cd7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:15 +0000

    refer punish course but well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_work(force, distance):
+        return force * distance
