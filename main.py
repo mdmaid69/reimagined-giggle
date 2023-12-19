883eb7656835aafa65574395d1a7b07fc8366c3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:48 +0000

    narrow transport track control callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
