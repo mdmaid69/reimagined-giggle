c7412c28ff8ab5bc4c29a747d59ffaeae5da63d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:44 +0000

    forward traffic hold sight for

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
