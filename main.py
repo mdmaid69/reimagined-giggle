00cc80aa5214533c90ad1135372d4dde8ba75063
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:59 +0000

    respect one guide room task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_length(array):
+        return len(array)
