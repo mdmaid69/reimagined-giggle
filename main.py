ef180ee03eff42588eb85b3b116624ca177b69fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:49 +0000

    successful corner neat criticize unable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
