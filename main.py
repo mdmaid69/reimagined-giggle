853f571ec5b0738fa0f31ae42754d945918b7571
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:05 +0000

    during anywhere classroom tape offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
