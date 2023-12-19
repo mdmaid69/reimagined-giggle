f97af25720bd2b7fe92709c4a3b605bf28d17749
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:24 +0000

    poor only spite dream best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_palindrome(s):
+        return s == s[::-1]
