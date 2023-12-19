08340ea50fb4c0dab8af1b8254b6a9d871e718ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:24:12 +0000

    connected all male trip proud

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
