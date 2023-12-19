951847ee7b118319ffb728d31cc29c84069a377d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:26 +0000

    extraordinary travel crowd progress face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
