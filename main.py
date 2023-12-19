eab84d27a3fe6271aabb0e28a4f10df2e961baad
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:19 +0000

    course fine occasional sort throw

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
