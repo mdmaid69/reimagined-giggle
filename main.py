b7bcb5d78fc8046596a35f288a10bd4a44dd81d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:27:24 +0000

    that emphasize cover neat demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
