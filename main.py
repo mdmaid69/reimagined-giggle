b255f56b03c4ddc440c164626b37369b07d0cb8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:30:34 +0000

    anywhere language hurry arrangement 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
