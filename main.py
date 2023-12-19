d0b702d6b79db1080367b6e2d1713f35cf92c24d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:47 +0000

    fat praise both mend nearly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_volume(length, width, height):
-        return length * width * height
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
