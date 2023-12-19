0c7af287248166b97193bf3fa1f8af4ebaf76e50
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:41 +0000

    knock kill squeeze object pleasure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
