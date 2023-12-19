952bccdceb827cbcc849fa668068171de5102e4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:37:37 +0000

    morning middle holdon process immediately

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
