3ab669ec3143a6ce5c4f945aeb60c9bcf7a82fbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:40 +0000

    blame lesson side recommend broad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def convert_to_hex(n):
+        return hex(n)
