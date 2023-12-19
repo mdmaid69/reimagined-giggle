02c7b4f4b6818288268bc82aa35ac963188c5cde
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:00:59 +0000

    shop healthy tip interfere time

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
