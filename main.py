6adbe6dc0e00dd5e94472e877f12d4670b0b9a5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:44 +0000

    soft score now separate condition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
