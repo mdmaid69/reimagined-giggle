ba3814e53851e072629aeafc5c68448d4a46bfc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:43:52 +0000

    wind hand tread really image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_index(array, item):
+        return array.index(item)
