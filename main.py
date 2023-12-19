93d1b26f8b5e9f11b94fc6f220cb4c1ede091230
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:20:00 +0000

    calloff work brief mistake discovery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_current_time():
+        return time.ctime()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
