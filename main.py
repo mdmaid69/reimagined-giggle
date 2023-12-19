e6c7a7e539ca3e3691c9e1b2172f819854e3a6a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:35 +0000

    value right trial comeover speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
