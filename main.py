9a9d4a3620d65aba4524bbb48bdf7fdf52d8dfc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:58 +0000

    matter taste central obtain strike

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
