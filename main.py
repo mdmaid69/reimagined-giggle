1b6d06e6cfeb2f9237bccf3269275eb350a9d352
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:55 +0000

    purpose ourselves honest but away

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
