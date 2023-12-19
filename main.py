4cae6d6c87858e4992b9e43d55e23aa1216717fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:20:28 +0000

    lookup clothes accordingto truth brave

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def reverse_list(lst):
-        return lst[::-1]
-def sort_list(lst):
-        return sorted(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
