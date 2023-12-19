9cef859e06afa134f94500233cea177cfb116d7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:25 +0000

    turnaway bottom achieve reserve sound

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
