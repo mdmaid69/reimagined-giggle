870c1bffe21f8f518f55925710984a5645105bcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:14:20 +0000

    secret prevent thinkof finally talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
