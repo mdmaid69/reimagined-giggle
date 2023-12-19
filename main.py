805dbd270e6e2bd7d2e964aecacdc17bf78e4116
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:47 +0000

    funny ride post image set

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
