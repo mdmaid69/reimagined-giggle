c592413dbfccdb73eeb58cd7d01a980eca49a793
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:11 +0000

    road 569enjoy keepfrom pay on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
