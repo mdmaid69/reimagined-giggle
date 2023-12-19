345baa4af578054f8d3375da554d5f8765b714ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:29:38 +0000

    last cover whole cost join

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import tensorflow as tf
+print(tf.__version__)
