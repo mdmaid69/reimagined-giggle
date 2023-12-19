3f1ce87022dde1a2b7ac6c37273d04836e1e9ff5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:48 +0000

    both handle number need hit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
