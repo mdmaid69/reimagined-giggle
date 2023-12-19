8699326ef771aeb2f5256a6ae53aa7cece402b39
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:28 +0000

    spend gate warm over disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
