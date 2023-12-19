4e9cafd0222789a62428f4034ba5b25ad4b8eabf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:48:05 +0000

    sorry task record post fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
