41f1b68c65f9c69841fef491853d99e14278ea29
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:57:55 +0000

    stamp anxious agree Oo defeat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
