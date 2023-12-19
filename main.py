73efa0765a2bc7cc3a036e50835a6ef09212737f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:12:24 +0000

    trade set meeting past joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
