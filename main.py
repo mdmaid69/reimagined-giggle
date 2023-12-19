c23984d52d529d93b0c97c59d49978edb9b9fb5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:11 +0000

    for matter turnback slowly pressure

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
