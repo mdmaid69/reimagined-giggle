944ed51914d117c3b96b0101e591e8f51910b334
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:42:19 +0000

    afraid exchange lost comeout connect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
