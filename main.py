cde1dbec1fd28abfacfde9c9c7854dda7bc9d9b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:27 +0000

    close draw yesterday full meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
