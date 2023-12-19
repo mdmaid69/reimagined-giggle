0aa7977c9763be107596fa486f4e8c5adff9635e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:53:33 +0000

    share repeat judge like brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def cube_number(x):
-        return x**3
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
