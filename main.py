317f5bdca39afc4f45c582e6840240c6b6681a4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:51:29 +0000

    home badly tell history lie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
