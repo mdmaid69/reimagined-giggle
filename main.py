6cdef78fb1ee6949acfcd8b56c12521c660c744a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:39 +0000

    combine care commit tour recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_float(array):
+        return float(array[0])
