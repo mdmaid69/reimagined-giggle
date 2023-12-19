7d9fba842037825331adc6848c74ef111f9e6926
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:47:54 +0000

    rare reply record protect slightly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-print(sum(range(10)))
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
