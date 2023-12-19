30a01e577396efa4aaaf543560a054671ad06a50
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:05:19 +0000

    entry telephone float wrap secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
