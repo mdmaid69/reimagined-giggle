57e26dff9bc205b80443285cee686741169d00f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:56 +0000

    central bread entrance pure strange

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
