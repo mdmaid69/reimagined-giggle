e68a04fed302d778fe5bf295bd525fa9d9729266
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:07 +0000

    aim slightly together satisfactory embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
