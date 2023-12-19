7a5d25425f8331bf31a35d257b77cdcfe35f33bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:09 +0000

    already party hardly general so

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
