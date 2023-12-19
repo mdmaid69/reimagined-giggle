2bafe3fdfad5fb36f40dc84b40c5075673e04145
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:23:20 +0000

    conduct shut forget loss keepaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
