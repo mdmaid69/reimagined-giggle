1332ce83d0d6f69f8c3a2b9ec0484ed373ad6477
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:17:25 +0000

    face name more bad rush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
