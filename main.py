a54205746b504cf5dba65218ed08b9d19bafaaf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:34:48 +0000

    plenty level importance copy radio

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def is_odd(n):
+        return n % 2 != 0
