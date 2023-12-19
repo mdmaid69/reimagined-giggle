c66959a32f48376547964951f50c146b8a1b78a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:40 +0000

    project pile description bath begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_queue():
+        return collections.deque()
