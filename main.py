d179a90e67bb7a8838661f7ee7573ebefe4c24dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:21 +0000

    beside average relief object beside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
