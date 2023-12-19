a2b1b25f57d49c190948aead9f23e86796e526fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:47 +0000

    full ours post born guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
