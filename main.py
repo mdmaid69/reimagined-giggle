24e6bb7a0b0fa9e9356283e6d14689945ad7500f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:37 +0000

    art back respect powerful then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
