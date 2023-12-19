4e5f3cbb101aee52a9099d5d9b327f3776ad0cd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:25 +0000

    many it tragedy comeoff nowhere

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def reverse_list(lst):
-        return lst[::-1]
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
