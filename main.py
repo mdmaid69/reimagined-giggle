4b3092abd508cb8e119671513296eabc2e560b95
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:11 +0000

    minute in last my tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
