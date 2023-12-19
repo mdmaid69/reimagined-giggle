377dfa0f8ac3d0417b087a7241d7bbed873ff24d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:48 +0000

    trust instant love flow shoulder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
