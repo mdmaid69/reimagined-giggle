ce6ee4c8398f3316ae5017d79c53692fb711f7fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:38 +0000

    wing divide turnaway thin hungry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
