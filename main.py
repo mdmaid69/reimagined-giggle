dcf5b71927910f9e6bbca867039e98c115ae74aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:23 +0000

    reserve maintain everyone lead collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
