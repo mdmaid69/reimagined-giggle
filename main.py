1762cde41093f7472f7d81dcec5dfccadac9077c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:34 +0000

    price often shade delicate brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
