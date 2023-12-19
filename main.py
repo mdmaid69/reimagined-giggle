82a541460b23e6ad18ef36754a6a2aa7bb4aca2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:12 +0000

    climb fence friendly behaviour pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
