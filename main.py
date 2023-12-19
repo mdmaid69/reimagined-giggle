aa47d1063f4b8650dce1cc9ab1617204dff8d611
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:56 +0000

    risk share turnback black hope

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def is_odd(n):
+        return n % 2 != 0
