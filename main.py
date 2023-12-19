bb3580753a2b4e04743415bbf9b1dbc3dda06f57
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:12 +0000

    act pause noise pure communication

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
