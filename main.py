697c00781a53c5b792facfba548c5060ee92ee99
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:08 +0000

    approve birth previous failure tax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
