8f50efc4992b57dd677c1f667f4202ccc610ee5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:19 +0000

    combine crisis nervous conclusion convince

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
