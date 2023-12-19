78f48b089eb1593d09eca7438ed9cd276023ad6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:13 +0000

    get prize meet work mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
