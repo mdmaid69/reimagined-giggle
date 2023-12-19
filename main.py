17a7f6f970bc1f561e7d8d980fb2d588176e5b05
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:56 +0000

    Hh callout attention excited attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
