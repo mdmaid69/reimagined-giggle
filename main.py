17a698fed3d97038ca586b5e8c6a6dc8c84bb8f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:31 +0000

    crash previous realize mistake mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
