5c2020aab188bd56411c3e24e827524ccc92a5ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:06:17 +0000

    cinema shrink rubbish breakin 571enough

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
