06a54fb50024e0c0736fdb45588cb6752d5b901b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:07:23 +0000

    effort ignore than anywhere seat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import re
+print(re.match("h.*o", "hello world"))
