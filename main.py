95fe0ad6c2a7999a1eed7b24f2d03316add12045
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:18:18 +0000

    brief promise clue lost church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
