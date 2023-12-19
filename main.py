939572cd878edb560eee5d03ff6b395235dfc5b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:26 +0000

    unkind even extra friend note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
