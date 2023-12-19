075fa42cf5f647df0be8ae15d1628036dbd8fb6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:05 +0000

    height surprised condition swim pile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
