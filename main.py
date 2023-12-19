7528fc7e1c6e98ba9d955b82283e8467e8bd1bfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:20 +0000

    demand safe regard fact problem

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
