4358fbd4657a11fa21954ff9cf05dc9563e4ddcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:36 +0000

    means innocent getback control related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
