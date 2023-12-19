c362bc99335832ee75685b661600ea75b9804d42
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:31:09 +0000

    passage gooff right plate sky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
+print("Hello, world!")
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
