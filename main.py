404f71059047a370df497a0631400f855d0991db
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:15 +0000

    her light seem delight criticize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
