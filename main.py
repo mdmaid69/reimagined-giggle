1e5b55d9f57f574ed07345adb9842c7fa0a0bf4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:12:00 +0000

    turninto quickly achieve battery whole

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_area_rectangle(l, w):
-        return l * w
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
