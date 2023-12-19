685816b8d9f8a6c21bbd12f55b42770a6ec7be67
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:11 +0000

    chat moral account substance prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
