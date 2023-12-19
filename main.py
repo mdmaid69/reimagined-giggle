ed1fc235cfa1f8d4c29892b9b831e08b0b8cfbbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:00 +0000

    mystery coat weigh she work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def convert_to_hex(n):
+        return hex(n)
+  def convert_to_octal(n):
+        return oct(n)
