8173cb39f7b8eedf733319be6374e65f80fc0816
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:33 +0000

    various personal final always circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
