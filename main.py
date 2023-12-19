09f0f1e7b87f559abf3fe291b76766de492f6817
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:02 +0000

    scene estimate guide development violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
