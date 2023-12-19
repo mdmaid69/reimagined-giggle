d23883a9c87a7891efb0d0fb3a5386bde5f381e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:59:44 +0000

    giveback wave effect advise game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
