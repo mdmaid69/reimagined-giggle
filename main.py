62b93f1f757a13447be027f5f9a0bbd707eb29fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:36 +0000

    my prefer frightened teach when

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
