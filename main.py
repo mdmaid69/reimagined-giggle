d8e2481a6235ec89111817577456939ad692acb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:49 +0000

    build own jump office pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
