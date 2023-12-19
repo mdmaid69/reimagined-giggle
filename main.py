d906b41f50a511feb60ff9513ecdfa2444e81004
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:08:51 +0000

    tip exercise bar it married

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
