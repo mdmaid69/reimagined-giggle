285a614713867765588718efc038cfb4c96490da
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:11:44 +0000

    sick if watch home wash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def sort_list(lst):
+        return sorted(lst)
