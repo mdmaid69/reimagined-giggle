382f835e95a1f3239d40512a860c3ac2caa31713
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:59 +0000

    degree slip now give tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import platform
+def get_os_info():
+        return platform.uname()
+def find_max(numbers):
+        return max(numbers)
