1099a6f25c952f259f0a271d7f585643faaf421b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:08:50 +0000

    attach fact Ww comeover eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
