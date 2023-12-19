fdea629dd9547bd7919da657f54be5a002983ae4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:12 +0000

    something exactly design why street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-print(os.getcwd())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
