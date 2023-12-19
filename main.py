743c2a2f79e8b1f7dc8f0408fc6ded946a177640
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:54 +0000

    until brush ourselves deal point

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
