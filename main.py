6eb4dbf297bde7db3a784b497ecd365791b4b326
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:52:44 +0000

    count butter law peace hate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
