9f374430d3c06fd30d0b371f0a6dc1ed5f6b43e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:41 +0000

    settle owe around related brain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
