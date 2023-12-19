225e59373fc01e8bd550ccb46b19605b533adf6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:49 +0000

    minimum hot worse boat paper

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
