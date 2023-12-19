7bd4a112af488e4e07fefbeeddca893b62b3523f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:56:46 +0000

    influence runout correct emphasize suggest

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
