55e7318ff7a01efbb58f22d4db572e8eae743dea
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:20 +0000

    table term brave pride zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
