12e21248e33065e45b93dcb03911cf46a774ce38
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:09:02 +0000

    glass dress wind steam forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def square_number(x):
+        return x**2
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
