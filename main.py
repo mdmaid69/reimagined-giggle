770c11b16dec62d32061a8ba96ec04ca994c5523
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:47 +0000

    calm everybody catch round instant

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+from collections import Counter
+print(Counter("hello world"))
