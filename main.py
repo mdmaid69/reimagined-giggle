8fa41e8e1d532c96e91200abd4a463ab1e72d423
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:13:05 +0000

    strict rub explain please welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
