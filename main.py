df0cc2c19a8bfe680d787ebb86df8a7d2b9099ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:10 +0000

    fight desperate give sky birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def square_number(x):
+        return x**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
