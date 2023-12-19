28f2a4a894724d70c52fce1436216385d79a68c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:12 +0000

    Qq eager wealth Mm thin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def multiply_numbers(x, y):
-        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
