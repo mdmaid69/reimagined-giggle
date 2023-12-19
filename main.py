7d10b58ba1edbcdb216b7a3053e2d80583430007
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:36 +0000

    sure public and send burst

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def get_array_as_bool(array):
+        return bool(array)
