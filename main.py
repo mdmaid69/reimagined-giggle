c4ee9d9cd45366501bf462ff1de32cac0695787c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:42 +0000

    pair basis through Kk rule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
