cb470b4ca58edb3422a97e66294b3c9c3d3ec42a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:54:47 +0000

    meet dead news our Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_pressure(force, area):
-        return force / area
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
