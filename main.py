2a2141990541308b71e049645225e3c4d652052f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:14 +0000

    damp weekend convince separate form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
