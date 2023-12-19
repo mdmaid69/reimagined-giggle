0690912e3fcf8304cd85d16378803fd4d5dfb164
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:49 +0000

    correct I comment regard over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
