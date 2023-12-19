82137e07992aba4c1d472a137754b7e355565f91
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:23:39 +0000

    pity peaceful attitude move year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
