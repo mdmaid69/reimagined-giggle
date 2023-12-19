1fe1dce270a713eb15f198751acd023d114e7e58
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:30:59 +0000

    plate disappear before juice declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_set(array):
+        return set(array)
