1a3508cd76457410df149d7096fe91892dcd098d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:27 +0000

    themselves home keen private open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
