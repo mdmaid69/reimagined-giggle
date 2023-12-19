eedffa67daff11bcc1153c3283a63d7c46b42b60
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:37:49 +0000

    threaten mainly complaint hang cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
