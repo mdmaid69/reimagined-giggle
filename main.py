3c99e89ac4c920a7b9889de39197c077f77a0fee
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:13:40 +0000

    mess mood holdout obvious tense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
