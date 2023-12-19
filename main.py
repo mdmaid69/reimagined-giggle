3454ef53c247b136098c106f9941e9dc19bdd08c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:59 +0000

    unusual needle head future under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
