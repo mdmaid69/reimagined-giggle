a46deec95c93d00dd08b0fb6eb10d8c16e84d849
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:01 +0000

    star follow at breakdown corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
