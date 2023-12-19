cc661213ce989087c4514d0d531efcabd138e1c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:27:51 +0000

    myself away emphasize trousers design

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def append_to_array(array, item):
+        array.append(item)
