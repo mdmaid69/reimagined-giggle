2de84a5c01b8bf00165ac7b03b3a6fcffdf6979e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:03 +0000

    punishment wheel handle turnout whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
