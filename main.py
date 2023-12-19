1d812e84667875020fdb6a75ae68f31e78c5ef9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:47 +0000

    paper emergency ear join turninto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-def is_odd(n):
-        return n % 2 != 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
