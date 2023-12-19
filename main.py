ac960027f020f1b332253720930f7b44977ba073
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:16 +0000

    holdon demand plate tall holiday

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
