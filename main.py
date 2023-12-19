f1f18ce7f21f842e16e6451db4631c087351341c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:27:26 +0000

    care father clever swim sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def convert_to_hex(n):
+        return hex(n)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
