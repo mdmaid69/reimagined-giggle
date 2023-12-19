9a4abf9c9fe6d8278c396cdb5d6c8864901d9fe5
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:56:48 +0000

    confident band computer giveup dish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_density(mass, volume):
+        return mass / volume
