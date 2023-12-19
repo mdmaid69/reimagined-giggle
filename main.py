da08be593ae22374a715d670bd4ed37afb2c312b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:00:14 +0000

    old big although lookaround tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import time
+def get_current_time():
+        return time.ctime()
