de71b667142a3b88ca6a3e28262687bf2def5e0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:53:31 +0000

    without confuse but bored comeover

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
