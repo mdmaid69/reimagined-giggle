1c55939fe890bb7d973af690ad43d3a742ec2a3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:55 +0000

    concerned tough usually will nerve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
