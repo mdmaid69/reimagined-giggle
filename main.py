c493137ec1d0983cb2b82487ba80dd341b4e3452
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:41:46 +0000

    thinkof serve equipment another cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
