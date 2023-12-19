965394f77d5e217d64bff3486ae6aad9970734ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:52 +0000

    ask disturbing stretch notice smile

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
