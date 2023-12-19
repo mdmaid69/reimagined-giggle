bcf30671382655b9a7404d1cf5dc316f1a87e211
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:36 +0000

    surface everything work scrap direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
