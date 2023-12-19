715b99ba0366adbbdb9d7d14b3ab636909b5c199
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:47 +0000

    fast alone Vv organization myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_power(work, time):
+        return work / time
