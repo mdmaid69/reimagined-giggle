3a1988143c200821769f9ee0726dd6b9e614b2fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:55 +0000

    mass schedule left call completely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
