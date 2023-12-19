50903505408cd608d4b9b6139b635e0bd7da9b5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:44 +0000

    more embarrassed view but world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
