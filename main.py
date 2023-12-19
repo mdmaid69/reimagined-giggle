abf49ea1802a568b3c88676c320c51296600abb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:52 +0000

    invitation university Ii tread study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+x = 10
+y = 20
+print("Sum:", x + y)
+def is_even(n):
+        return n % 2 == 0
