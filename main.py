d1708ead15bf9b3fd67f8474a65d78dff25074ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:40 +0000

    wheel black rain intend awake

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_unique_words(sentence):
-        return set(sentence.split())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
