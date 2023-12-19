0f64b68d8b928bc469bdbc62f5010b641a1dc51a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:42 +0000

    drop try intelligent trap pleased

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
