81c70e96d81a560d5589564d53ce53491a720ffe
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:30 +0000

    work include appearance stop still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def subtract_numbers(x, y):
-        return x - y
+def get_array_length(array):
+        return len(array)
