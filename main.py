6cce18cb0b851cfe836f3ee82f46b3e0d9f8428b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:48 +0000

    underneath pinch apart move here

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def multiply_numbers(x, y):
+        return x * y
