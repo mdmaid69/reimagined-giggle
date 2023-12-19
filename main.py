5faeacb085e8219c7f2df627782c30831e603517
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:39 +0000

    comeback comeoff nature bury ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
