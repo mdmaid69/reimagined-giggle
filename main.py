2dfaf8d4434882d106e8475128368c4998d66cc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:40 +0000

    cruel muscle earn steam pure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_min(lst):
+        return min(lst)
