95deb587288d2804a57d69ed1e3d272b8c17d9a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:10 +0000

    positive ordinary towards criticize swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
