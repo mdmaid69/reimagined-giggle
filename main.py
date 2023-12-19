5f9be743e627f234cade6bc7d067ed017e690017
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:41:26 +0000

    comeover certain pay interfere fork

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
