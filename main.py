66204959b51d737d927f9ea5aff67e9003d4ea63
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:17:49 +0000

    nice Gg this argue hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
