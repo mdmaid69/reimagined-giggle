31d3755c36a83ea9fdf8a709f865f016acc49426
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:33 +0000

    port ago question male seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def multiply_numbers(x, y):
+        return x * y
