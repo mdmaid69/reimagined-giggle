c7b63ef6ab7139824e4480bb251b41abd332e659
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:12 +0000

    whenever born announce escape us

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
