441edc83b29aa260c332f038ce88d710f572fa4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:31 +0000

    study effect live equal raw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
