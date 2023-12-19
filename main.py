2d352e87f0e913e294e19a0adf25290fdc72bbc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:43 +0000

    still boil ask total account

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
