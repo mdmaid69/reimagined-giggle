928d5facc13cfafe5526f3103bf24084055a8e78
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:41 +0000

    protest complain basic new question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def reverse_string(s):
+        return s[::-1]
