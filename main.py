0f9cd2928f4c7d93f11467617bfc66987e378d9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:43:50 +0000

    intelligent sea violent finally aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
