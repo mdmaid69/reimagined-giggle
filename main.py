a566ab74355b53509849052259ac927ffe940fc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:51:59 +0000

    temperature downstairs declare exactly clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def cube_number(x):
-        return x**3
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
