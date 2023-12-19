c57003bb22c640753fcbc731841bc594459d7ce6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:47:31 +0000

    sound draw worried so about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_even(n):
-        return n % 2 == 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
