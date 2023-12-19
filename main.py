2f32ed96ce615ac477cb48dafd9f0c618c9f72e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:16 +0000

    split shut instant march confused

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
