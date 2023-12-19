d751fd4dea9668a95c98af4c2c2ab2fe44b66663
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:41 +0000

    operate suddenly between man frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
