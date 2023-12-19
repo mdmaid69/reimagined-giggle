d63b96bd54716f26bb0416b064ddc725d6219d0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:08 +0000

    draw contents surprising original reduce

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def sort_list(lst):
+        return sorted(lst)
