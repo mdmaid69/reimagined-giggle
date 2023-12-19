15f19f1d2924ac78fba1ef4d028dafcfbe8089df
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:27 +0000

    sight stage read protest suddenly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def reverse_array(array):
+        array.reverse()
+  def count_elements(lst):
+        return len(lst)
