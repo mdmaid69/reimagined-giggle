91a7be80a858439b8a6c1b6f6982ab8b16c099ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:53:17 +0000

    zero breakup runinto cost bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
