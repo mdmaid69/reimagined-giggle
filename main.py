8ee432bd40533764ebebf70c0f63380ac72bad83
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:18 +0000

    combine car great explain deaf

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def clear_array(array):
+        array *= 0
+  def convert_to_binary(n):
+        return bin(n)
