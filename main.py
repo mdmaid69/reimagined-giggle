cc9665d970a6fd2a10eded7f5a02be9c5da2f3ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:56:30 +0000

    burn spin argue sister instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_sine(x):
+        return math.sin(x)
