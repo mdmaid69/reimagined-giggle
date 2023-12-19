5e105397aee6d640e59122005d55de3a2b677af5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:58:29 +0000

    eager market boat wonder completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_density(mass, volume):
+        return mass / volume
