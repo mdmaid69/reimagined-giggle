3e341b4d6f9c0a0937ad41617ed1cc23d2c037c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:35 +0000

    Cc pipe loose invite especially

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
