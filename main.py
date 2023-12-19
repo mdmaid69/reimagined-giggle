ce89012d1d973e5faa095be2cf784928554105b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:09 +0000

    soak frighten estimate downstairs climb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def reverse_string(s):
+        return s[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
