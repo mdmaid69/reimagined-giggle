b9a950b73d6df9e2f8d4387cc9f1ed05c5f42c59
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:41 +0000

    experience community century unpleasant case

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_hex(n):
-        return hex(n)
+def multiply_numbers(x, y):
+        return x * y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
