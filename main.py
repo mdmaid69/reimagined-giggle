91e79c82151fc6ae92fcda42b504d85d64d0d3df
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:18 +0000

    nervous money bye rule float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
