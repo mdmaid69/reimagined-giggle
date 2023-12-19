001885592cf6f8881489968d4b88ee46d045dd73
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:35 +0000

    change principle long heavy appeal

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
