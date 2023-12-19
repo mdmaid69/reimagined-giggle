7be209eddb95aeaf4d9a29dde2df236464d03b63
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:36 +0000

    attention extraordinary direct concentration clearly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
