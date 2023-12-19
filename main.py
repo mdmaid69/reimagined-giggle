b9157c4f52857654fb23d7d6515add445d42689b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:19:54 +0000

    same burst visit 573entertainment material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
