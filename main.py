15afd2d2128ac1948e247c993e77ea14f5749345
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:54:04 +0000

    principle plan definite anyone exactly

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+print(json.dumps({"name": "John", "age": 30}))
