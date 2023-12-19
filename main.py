3ab806f794eefc7074950993aa05172b26fad50e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:05 +0000

    care delicate house dream bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
