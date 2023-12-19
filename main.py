7bc86d898a327e85642b85108fd2a5f09ab77130
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:42:33 +0000

    surprise sensitive once holiday ring

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
