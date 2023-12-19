b568773c731c01fda98e68deac3afbbc14fd695f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:52:37 +0000

    place thick target nowhere wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def cube_number(x):
-        return x**3
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+print(sum(range(10)))
