0997ac995d1c6696ac0650bfeae60680e60eeef1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:50:53 +0000

    depend closed sense design Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
