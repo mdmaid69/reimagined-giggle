b47a857a107e36edacab04187e8de770e99989bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:42 +0000

    copy regard fasten problem supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
