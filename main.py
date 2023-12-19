6d05b4341b62df68825f194f1c0e7ce81803ab15
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:33 +0000

    best steal never instead clue

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import collections
+def create_stack():
+        return collections.deque()
