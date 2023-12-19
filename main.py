2b1c00565a76b90f3bedc083268cb06f3bbc3272
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:18 +0000

    always collect avoid lose sting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
