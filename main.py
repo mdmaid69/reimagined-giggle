45435872499312cd90d0fd656de4f1b546b2b888
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:16 +0000

    tune mass order only play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def is_even(n):
+        return n % 2 == 0
