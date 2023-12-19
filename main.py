ed9ddd9ee57842f118bc19a47ebf7984212c0f87
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:53 +0000

    realize crash already protest border

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
