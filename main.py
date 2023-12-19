5d69e6879c619e408d1d5f0d711b1ceeb57a7f45
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:05:53 +0000

    shop married upwards children callup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-for i in range(10): print(i)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def subtract_numbers(x, y):
+        return x - y
