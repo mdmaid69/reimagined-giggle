5be033965043e972d22c84d548c20e6e75405e5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:17 +0000

    rent trouble noone funny news

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
