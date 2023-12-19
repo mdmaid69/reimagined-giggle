a30ce57560f79a4cffef2f7c92e29d03cd248179
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:53 +0000

    recommend answer on suggest study

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
