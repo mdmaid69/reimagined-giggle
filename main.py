d3556a1575db230f1fc6edf6647ff85ec9005bb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:54:30 +0000

    share slice better stone Dd

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def split_path(path):
+        return os.path.split(path)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
