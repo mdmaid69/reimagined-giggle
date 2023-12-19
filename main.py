6f0395a7961b72d4a60676b80bf4b1feadd0a49f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:23 +0000

    similar square subject argument argument

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
