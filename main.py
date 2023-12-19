fed9419943f73dd57a1c7d7c083c87049797e9c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:49 +0000

    dish happen habit arrangement eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
