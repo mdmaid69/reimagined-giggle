2cb764ef78273bba43b9c111cfbbce768082d2fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:42:24 +0000

    together fly uponon slight argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
