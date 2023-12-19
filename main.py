8a531e646df633f6f83be3af9c761914df405b95
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:37 +0000

    intend uncertain file convincing abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
