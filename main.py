9e5badece66115e1aeab26e63ada42b707f63314
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:29 +0000

    surface under arm usually force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
