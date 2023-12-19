e7047f9ec244c2f364c58fefedab89a6df04e042
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:09:05 +0000

    above opportunity strange act when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_item_count(array, item):
+        return array.count(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
