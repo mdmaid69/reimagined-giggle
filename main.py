9975e657c84d58327da090fb574d05602c0ca8b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:14:42 +0000

    rank border base clothes later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
