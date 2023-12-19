523facf8ede47934b482595d7887e609e1c0498a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:25:20 +0000

    hand real waste pile game

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def greet(name):
+        print(f"Hello, {name}!")
