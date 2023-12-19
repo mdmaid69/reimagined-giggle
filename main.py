8cdf50f02958637e97601269a7c256e5da542112
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:32 +0000

    hardly hand mass pronunciation wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
