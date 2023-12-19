d1eddc4c130f060027165e8b3c28a16580b9d440
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:07:53 +0000

    exactly want glass combination hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import datetime
+print(datetime.datetime.now())
