7e822bdba151455b3907e09d80c327c3ae273bc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:42:45 +0000

    market cheek mouth centre twist

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
