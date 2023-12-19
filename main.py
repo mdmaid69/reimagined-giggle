f8627213b31b0aa70df28cceb32ae379aacfc62b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:27 +0000

    tread behind she through often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
