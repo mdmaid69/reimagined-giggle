39629a30df9f6acf0c9db2c0308a799c97c297bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:35:08 +0000

    conversation comeround temperature curious service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import random
+print(random.randint(0, 100))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
