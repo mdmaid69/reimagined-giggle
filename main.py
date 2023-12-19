f6f80499e128b8ad391add0e7512b2a375844367
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:55:52 +0000

    formal event speak formal clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
