290cdb9b51e3f949cb31fd58c7755c4f1c480537
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:59 +0000

    permission mark yours hello education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
