3eb9629ab147d1101f8c65d4d2ef5fef3e36f57e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:10:19 +0000

    surface land journey benefit bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_item_count(array, item):
+        return array.count(item)
