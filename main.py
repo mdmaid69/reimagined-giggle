a372ade6a035b943efd7881f31af7f5fe24b11da
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:52 +0000

    consider message prefer health everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
