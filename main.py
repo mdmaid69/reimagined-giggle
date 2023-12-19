546f29c0f5e2376b366d5d4dd1bdb5de82b63511
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:32 +0000

    port loose until inquire commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
