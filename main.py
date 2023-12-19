b7669e4c157f388f9c420cab1271371f19eda16b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:44 +0000

    market no onto mend wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_os_info():
-        return platform.uname()
+def find_max(numbers):
+        return max(numbers)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
