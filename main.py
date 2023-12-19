3b1158cc72752030eac09b94e58060d1cacf1131
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:16 +0000

    different duty generous friendly flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
