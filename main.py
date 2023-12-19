53e3cd4eb32f704cd47ed7346875ea781446db80
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:49 +0000

    load total street embarrassed something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
