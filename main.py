f916e5986010356994dce573a8c8cbe5ac48e04a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:14 +0000

    eager contrast pattern book surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
