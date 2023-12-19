3ab827693efba1ba63dab5f59c56d6824be910d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:23:35 +0000

    572enter illness measure service again

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
