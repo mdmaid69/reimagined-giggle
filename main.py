dc64595d97c72089c80d678e3ebeb6a02744c345
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:09 +0000

    get lock experiment ought born

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
