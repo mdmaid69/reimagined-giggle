f9f07b5cd0ef1e16ef1637a62a9f43ab7bc3551a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:03:51 +0000

    wrap string about flow concentration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
