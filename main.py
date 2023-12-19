aa085e1344ba5940f3f54eb86c1e1f644bfdd3a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:51:23 +0000

    respect survive tour middle schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_density(mass, volume):
-        return mass / volume
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
