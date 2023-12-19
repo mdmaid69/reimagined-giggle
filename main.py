becb3822950bc710cbc1e0fd60a11f0a00cc3aef
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:04 +0000

    peaceful blind closed comeover towards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
