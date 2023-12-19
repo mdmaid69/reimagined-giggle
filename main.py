fc7e0c3139c445e7c03b77382567d7ca0390e463
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:10 +0000

    suggest goon cross which side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
