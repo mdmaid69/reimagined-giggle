0e0249459a34848f4d6b3661be6a6638d4da03af
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:25 +0000

    among news copy supply crime

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
