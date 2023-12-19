77129d2a7e902cd36d1acb381e61c93b4d619d20
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:20 +0000

    power plenty exactly everything pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
