85f3ebe1ad531a16cd83884d5038420ccccd1efd
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:54:44 +0000

    weigh pleasure ring priority shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def remove_duplicates(lst):
+        return list(set(lst))
