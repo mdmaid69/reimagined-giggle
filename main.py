bc5f08e4fa2bf5acea2a8643bed2bbcf2c251922
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:26 +0000

    gradually cry plastic eye better

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
