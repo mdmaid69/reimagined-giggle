aeed9c9d318bee5dc9fec4170894e17f1277b14f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:59 +0000

    calmly over solve path summer

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
