a5aac73e3a61bc89f781c33fb1b3dc4d59b2b2fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:09:25 +0000

    feed charge sweep swallow ago

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
