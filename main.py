7c8f2e48345b1c9998fd6e9d90d967f7684b39e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:31 +0000

    design could touch quit disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
