d4284baa1a16244bd2d1868787e48dfe540e8b0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:03 +0000

    giveout afraid path sea war

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
