380311b6ea5745de5f28356f24e62c59fdf513bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:15 +0000

    crime proud hope shut poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
