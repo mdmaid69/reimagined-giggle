99ea1a921d558d4f802c046581df3d1cd09aadcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:31 +0000

    taste payment relation treatment wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
