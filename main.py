13d7be0d6ecee5e10a4e60845df5857d9ee86052
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:16 +0000

    spring why can watch ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
