67fdc9d6e2ded22ce08844db47da171573a837f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:48 +0000

    gradually sure shop previous reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def find_unique_words(sentence):
+        return set(sentence.split())
