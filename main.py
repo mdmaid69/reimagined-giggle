d0fd54ecb7d44056bed330d3bd462171878ab856
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:32 +0000

    drink here defend standard most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def count_words(sentence):
-        return len(sentence.split())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
