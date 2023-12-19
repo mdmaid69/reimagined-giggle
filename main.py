03bbdea7c3e04fd6be015d2441bb088407e10fd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:28 +0000

    unkind home nothing or even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
