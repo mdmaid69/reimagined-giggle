2ec978745b86e98fe8ae288e110f9dc60e3be0ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:13 +0000

    Dd crowded will exam whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
