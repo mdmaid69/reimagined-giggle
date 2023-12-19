dea1b87bbd979d35d6bcee3aded219a14553fd50
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:43:34 +0000

    however climb brief danger never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
