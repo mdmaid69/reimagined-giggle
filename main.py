e632510424b769231dc25942e2f519c43fa502a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:15:40 +0000

    dinner always press careless strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
