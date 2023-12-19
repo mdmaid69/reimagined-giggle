de645c009b6108448ee195305e488bfcb19bc27b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:36 +0000

    breathe criticize back bar play

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
