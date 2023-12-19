123258b88cc4772b88bf758f273582db61488848
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:06 +0000

    keep professional though suppose use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
