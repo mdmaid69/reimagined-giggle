e85affe181f27d6d2014aac56d092842284a8ad8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:42 +0000

    before perhaps rise talk spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
