c858ed22e91abece15c78d6ef69021c86bad69d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:31 +0000

    bound argue head heavy unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+for i in range(5):
+        print(i)
