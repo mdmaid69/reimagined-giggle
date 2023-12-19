ddbe92400a832eb75fbcaf488ad01e0f27856771
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:22 +0000

    whoever wind demand cost laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
