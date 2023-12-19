85a328927545b1e8ebfa50ca8435e325ce120a1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:43 +0000

    look post theirs love heavy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_rectangle(l, w):
+        return l * w
