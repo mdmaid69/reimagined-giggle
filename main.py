dff4c86bda10398875271a5e0bf2fb605af5e568
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:31 +0000

    climb sore compare port speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
