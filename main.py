282b3611c5c53ac4862a95d1ca20268f029cc58d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:26 +0000

    even speed only turnback stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
