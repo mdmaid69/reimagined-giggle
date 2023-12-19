16441e624e07d4a220413d24336e3cac6e209e71
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:35 +0000

    proof city ask Cc refer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
