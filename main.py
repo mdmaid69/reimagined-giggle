c676ac7a34aebde393e4aa9e5fe0e30762eba75e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:34 +0000

    arrangement interruption idea workupto face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
