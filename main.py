6c4af9dd6947ef1d427b8de79f0fd436948efc84
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:35 +0000

    against similar effort to concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
