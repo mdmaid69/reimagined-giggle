daedd43a5f15f11b3dfc06170350f0dbf610566a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:25 +0000

    giveaway accidentally extremely term case

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import numpy as np
+print(np.array([1, 2, 3]))
