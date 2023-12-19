a3cb23c185bb3ea42fe36ca588db24b06164da94
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:03 +0000

    squeeze cry either remain door

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import numpy as np
-print(np.array([1, 2, 3]))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def greet(name):
+        print(f"Hello, {name}!")
