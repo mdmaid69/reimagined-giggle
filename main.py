0dcd4e82b04e179fefe3084e76d5e14cfd036284
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:00:07 +0000

    most speech introduce far debt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
