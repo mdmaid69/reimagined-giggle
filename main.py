921250717d2643457f74c5ecf99a13390fc690cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:39 +0000

    pretty head point weekend guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import time
 def get_current_time():
         return time.ctime()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
