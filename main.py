4b491985450df89b08f4989b6f11d9f2c0a11928
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:51:56 +0000

    say advise change mean ashamed

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
