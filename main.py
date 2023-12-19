bc1e3798a17ea30b04b12e99cac308270fb45ba4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:33:25 +0000

    male appeal typical listen anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
