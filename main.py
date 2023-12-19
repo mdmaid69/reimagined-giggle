581cfbe9ba9470fc94dca3e02fbfdc539903abfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:52:50 +0000

    wrap tie expression interfere interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
