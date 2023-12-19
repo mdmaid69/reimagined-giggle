3a6be036a24b7e25f2bd48ed7e2250ed0980ae13
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:17 +0000

    stage far correct when shop

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
