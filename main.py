a8fbde6cba635c96cfc8582ee80fec0da9371344
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:01 +0000

    contain dinner turndown 572enter angry

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
+import json
+print(json.dumps({"name": "John", "age": 30}))
