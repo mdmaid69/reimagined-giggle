ef7044baa6747c20310bf6adf35d984963a5d300
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:20:30 +0000

    wear sound rare area illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
