fb83ba056e719302727361478d6d240306bc1554
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:07:20 +0000

    object skill telephone style live

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+print(math.pi)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
