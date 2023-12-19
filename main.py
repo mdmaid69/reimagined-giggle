de70302b00156102f309936c3ac95c0fcaa209de
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:50 +0000

    close present sight deliver bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
