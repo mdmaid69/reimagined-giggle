c91452b69130d7dcb865c3d66dfe68d53ed1a2ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:15 +0000

    Mm takeback person sing murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
