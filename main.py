3cd704d1c5693641203feb8e20ac857c465299f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:29:12 +0000

    tip same slide popular worst

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
