e7e9a4d5f8b373805f6b25302c11dce6b86fa0ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:01:24 +0000

    Jj pretend weather runaway getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
