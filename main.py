2a0feaed484404ebba0edfc0eed6ff00fdc5ebf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:23 +0000

    temper miss scare short war

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def reverse_list(lst):
+        return lst[::-1]
