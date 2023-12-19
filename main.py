fc07938af7a867d48c06bff088b42dc170cbf7cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:58 +0000

    rush strike run injury talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
