60918efeababe6991e222e8a8d1915ad796c681d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:30 +0000

    traffic inquire position home tax

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
