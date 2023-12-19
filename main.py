4bba18d6b4eeedbd02ceb3aa04958e401edc3aa1
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:01 +0000

    length certain front congratulations calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
