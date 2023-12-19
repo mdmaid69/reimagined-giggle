52974fe692e96d719c0e86d09d80a3eef9b6590a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:11 +0000

    glass either criticism laugh certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
