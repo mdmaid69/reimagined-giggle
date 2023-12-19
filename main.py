8230bd9d32ec867a7d950d4ebc1ba140855d2cea
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:59:24 +0000

    Oo necessary free treat favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
