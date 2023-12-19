631a16ff142b1b26068ef1831b9c0bd75a93e261
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:17:07 +0000

    affect agreement night charge suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
