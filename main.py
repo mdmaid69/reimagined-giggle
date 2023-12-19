1333909354ae753075c3c29aa2b921daa445cb69
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:21 +0000

    fix cause definite top lay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
