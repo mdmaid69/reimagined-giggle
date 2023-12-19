e532eaca2ce354284328ea083ad51f4bbcd189f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:07:17 +0000

    clean born best advice flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_counter():
+        return collections.Counter()
