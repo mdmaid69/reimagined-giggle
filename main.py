679fb5bdb7414a04b572e4a2aab999514f344b12
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:51:58 +0000

    private cause begin air shine

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def add_numbers(x, y):
+        return x + y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
