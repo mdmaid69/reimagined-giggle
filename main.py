e577e780eb9e6d8b17cc8ac2ddacf3a54f461419
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:30 +0000

    professional afford cover deal suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
