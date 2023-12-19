33d87c12db94ab89a7dafe56d72c1537172b518c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:46 +0000

    sound sick direct exploration operate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
