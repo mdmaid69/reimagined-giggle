1729f43db9bf7438911f44bc585b77bc2acaac07
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:54:36 +0000

    fit runaway off limit scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
