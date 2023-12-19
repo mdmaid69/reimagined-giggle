d4a3cf8105110ec083f4cacac5dceca876ad6eb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:51 +0000

    warning Ww breakfast beside report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def greet(name):
-        print(f"Hello, {name}!")
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
