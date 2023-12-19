db11bca9a6c38fb78c7cf8833e198aee50caadb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:30 +0000

    object mistake band suddenly other

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import os
-print(os.getcwd())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
