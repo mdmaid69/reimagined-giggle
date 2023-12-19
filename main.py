7cd9bab0fbe2698ded03eb5bf51abd3b51089820
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:54 +0000

    wire glass gate loan bringup

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
