5cd4baf00d255e7464701aa4ffaf7216fa8db15e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:56 +0000

    lookaround mother secret whistle Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def count_elements(lst):
-        return len(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
