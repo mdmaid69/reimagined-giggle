30a5eada860da6ed80231ab03d886f011cde0347
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:54:36 +0000

    once promise recovery even excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
