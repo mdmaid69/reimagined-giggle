2cbb2dd65d336f32287e2245ce4c82a6d7ae1514
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:12 +0000

    be force extra star compete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
