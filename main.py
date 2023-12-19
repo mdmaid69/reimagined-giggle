c7e191cf207e3e8ee2a64b41a82fc24a68bb9066
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:41:15 +0000

    negative she order frighten dress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def square_number(x):
+        return x**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
