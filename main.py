db5d2dd8fb832d57ab5c9a4f0af3a496b43d590a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:45:49 +0000

    machine true radio thanks make

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import time
-print(time.time())
+  def is_odd(n):
+        return n % 2 != 0
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
