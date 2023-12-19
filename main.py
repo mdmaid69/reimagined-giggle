33aac1683e73abebf2cddb38e4c2781cd28f4c13
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:28:41 +0000

    store sort welcome some plant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
