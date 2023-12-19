9e2700bada275a958c3330af1f6af809eade6ce7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:36 +0000

    copy getup free sure middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
