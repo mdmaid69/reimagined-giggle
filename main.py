52ac02ea73ffd974bafe125f99d83dd0cfbb76d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:38:44 +0000

    pride rubbish tip cry relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
