9ec73c1491940fc49c41a055f283464abe45363a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:06 +0000

    exit shock mark responsibility last

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
