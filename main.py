271e64ddd14ab28f1f8e13464152e137a22d242b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:51:26 +0000

    book uncomfortable slice hope duty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
