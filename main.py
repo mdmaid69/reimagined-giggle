daa710a91d2ad0f1351210b03d1e2ebc23fe3312
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:55 +0000

    wall ago help comein project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def find_min(lst):
-        return min(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
