345948468cb9eaad55f6283f206eeef9bccd6884
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:03:38 +0000

    burn eye wealth runinto relationship

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
