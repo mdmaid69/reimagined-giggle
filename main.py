2ce37c5c161e0f74d74e45b48b848725d6bb4892
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:43:48 +0000

    example odd account beyond ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
