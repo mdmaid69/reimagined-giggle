87f0619cf8646306b3795a704dacbc5532206641
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:02:50 +0000

    smoke distance willing swallow fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
