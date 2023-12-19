3032b05e34ed9e660d25f2c337f4faa958a0499c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:50 +0000

    favourite along record public church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-print(os.getcwd())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
