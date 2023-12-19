bdc0b1dffe3b6ab9d67965b79fcee96e8455a5e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:54 +0000

    suggestion time total used contrast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def multiply_numbers(x, y):
+        return x * y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
