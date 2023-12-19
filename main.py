fb4a61d983be58c8191a12ac851814110bc333f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:11 +0000

    apart pretty church sweet skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
