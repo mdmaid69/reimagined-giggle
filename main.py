96986f5a65ebf70dcc7a7678585c6ab3ed733cbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:03:32 +0000

    hello communication because nonsense run

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
