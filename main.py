ac6968ee1f4c50f5351474bcb5b18ccf24b57b96
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:39 +0000

    hospital service passenger copy runaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
