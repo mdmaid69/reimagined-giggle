2828db335abf05c9fea3969888d9711b469df370
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:58 +0000

    wall dead else step shade

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_power(work, time):
+        return work / time
