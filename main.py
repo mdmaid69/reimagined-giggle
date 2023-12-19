62760913ac74c64990b4724bb41f19737419e0e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:53 +0000

    listen bright claim adjective city

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
