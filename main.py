43d29f5f438ce02f46e1002719d11811a8445b53
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:42:48 +0000

    history 568energy swim mistake closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
