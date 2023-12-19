f6b73205acd9934657e8fac3b1ac1ca29c0b9ec3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:36 +0000

    drop tie question become final

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_acceleration(speed, time):
+        return speed / time
