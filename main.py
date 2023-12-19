d4788bc1c5896b0d0cc09fe30b492584aa7d72a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:55 +0000

    estimate study innocent act modern

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
