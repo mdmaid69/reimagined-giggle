6676454fab4cc959c2ee1dc6905bff043c2787a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:34:47 +0000

    importance bottom reasonable jump keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
