1183280ebf4c28d0c0ab1ab73e0863a146bf9f81
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:58:15 +0000

    talkoutof path Nn Pp Ss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def square_number(x):
-        return x**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
