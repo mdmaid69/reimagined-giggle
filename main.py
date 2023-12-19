fe8622aebd2e4a0f27d4b5fc6a195711da884ec9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:12 +0000

    plastic admire convince able rich

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
