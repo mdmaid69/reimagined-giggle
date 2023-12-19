0400605089ac22c6c88db3fb6401a16f9ffd27f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:34:24 +0000

    front sweet situation offend themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_queue():
+        return collections.deque()
+import time
+print(time.time())
