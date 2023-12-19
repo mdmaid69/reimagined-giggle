1c28ab649f0f87a1e471f2241d2b82e63e28d6c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:37 +0000

    base shout protest load matter

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
+def count_characters(sentence):
+        return len(sentence)
