c3758183edc5224de46e9bd1d41e6ec6aa3d10b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:12:28 +0000

    recent blue gooff surprised play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
