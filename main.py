e4467712d57b002174eebb19d388d555734ca966
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:34:18 +0000

    news minimum education rest also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
