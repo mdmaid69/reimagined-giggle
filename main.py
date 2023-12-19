b87300b5ae9eedceec80d2dd310edb6e21df0b9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:53 +0000

    before thought downstairs treat if

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def sort_list(lst):
+        return sorted(lst)
