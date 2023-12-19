14103d7d823cd33170b85fdd11b82da7e40967d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:14:02 +0000

    computer system follow traffic consist

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
