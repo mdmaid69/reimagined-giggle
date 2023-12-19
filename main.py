9e1be804b578ddcf5130bce1ea3468448f3a1a62
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:58:31 +0000

    minimum apparently favourite handle serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
