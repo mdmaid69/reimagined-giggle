566f564508ed74b6f739a8234605aaa3cee6f6c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:39 +0000

    loss attitude workon terrible show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-print(math.pi)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
