141869b720b320e8f261528e2d3b907e0e4dd04a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:11 +0000

    sink deliberately between impossible clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
