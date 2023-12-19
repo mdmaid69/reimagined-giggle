c5950fcb6e773bca79fa9f11016038107d1ec119
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:41 +0000

    face regret clear grade space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
