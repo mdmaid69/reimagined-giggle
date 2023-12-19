2d4e65dc5ca45fc2efece58e6716f775cf43b422
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:53 +0000

    manage choice anywhere fit offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
