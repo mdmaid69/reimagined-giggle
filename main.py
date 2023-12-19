accfb748cddb886c1a133b5839f3aecd7deca601
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:27:47 +0000

    habit sign tend until tongue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def add_numbers(a, b):
+        return a + b
