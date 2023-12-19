75da1c5ee4371ed4abb278faf802a97cc6113cf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:15 +0000

    explanation 566end about though thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
