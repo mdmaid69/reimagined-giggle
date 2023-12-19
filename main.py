e8ad0c7df740e31b5ea93d54aade61403e6561db
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:01:03 +0000

    area taste lookback(on) large emotion

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def convert_to_hex(n):
-        return hex(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def multiply_numbers(x, y):
+        return x * y
