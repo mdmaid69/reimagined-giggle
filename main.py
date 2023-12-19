24017508a097c1fa1d27f70711066ec028d62c14
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:24:14 +0000

    pen basic university gap wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import re
+print(re.match("h.*o", "hello world"))
