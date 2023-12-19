1bc2afb3d687c81fb780a833d65c9eb78a86c123
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:27:29 +0000

    foreign lie persuade factory even

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
