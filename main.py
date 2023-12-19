15e6cabd5a68ae51f11eb5c04da140dc7613d509
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:52 +0000

    appear little ignore shape understand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
