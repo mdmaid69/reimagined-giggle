98572053c1311586cec4dbcb4cc318aa2d86c780
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:42:57 +0000

    guilty skirt thought oil solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
