63366c7020679f87beef4bf886770c26b4f9dce6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:53 +0000

    but drawing cost innocent thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
