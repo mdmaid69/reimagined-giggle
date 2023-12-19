0dcbad1232557b9193643aee4905bbe0798b904a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:58:13 +0000

    trousers present address tax smile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
