e00f01df4e60494adc64bd4c80d4bc7422b0ff9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:30:58 +0000

    maybe further score discussion worried

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def get_list_from_array(array):
-        return array.tolist()
-import sys
-print(sys.version)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
