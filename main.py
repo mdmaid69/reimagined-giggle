7ece2aedcc5c8c6dff7ea7ae8244e083e4300605
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:46:52 +0000

    count another hit content method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
