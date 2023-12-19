8d218dd9f9c88b168b4f2418adc0f77bbafbcf9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:25:33 +0000

    to wind about later scare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
