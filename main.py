de7e4b003ed074fff772a6ae3d73c71bc2866b20
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:18 +0000

    thinkover service public common her

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
