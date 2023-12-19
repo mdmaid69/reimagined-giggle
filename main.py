0d9c7d38de568993d45f566884fcb89d4cbd4b62
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:41 +0000

    worst spill limit cheque know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
