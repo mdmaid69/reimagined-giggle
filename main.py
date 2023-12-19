90c8c685d9171e8d7b827c21f105ca4b49152c32
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:38 +0000

    aim allow Nn tradition Dd

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
