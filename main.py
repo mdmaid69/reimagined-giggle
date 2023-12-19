f53575aff457e7c2d62c2a85f892d9dd7d3816a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:38:56 +0000

    suit practise morning dry measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def cube_number(x):
-        return x**3
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def get_array_length(array):
+        return len(array)
