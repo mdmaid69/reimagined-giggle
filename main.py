14eeefbe0a8663190894bc6193da44628d54a94b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:10 +0000

    also wonder can breakup anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
