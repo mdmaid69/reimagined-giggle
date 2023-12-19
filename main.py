7ccf7e3349d3608bfb7baa9352cc341346ccc126
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:30 +0000

    view everywhere lower about union

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
