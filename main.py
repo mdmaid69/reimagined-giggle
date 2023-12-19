8a668d36b0c59c0e495497b492ef7e5f946f4535
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:36:26 +0000

    risk nervous fire neither frequent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
