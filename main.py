ad54656cabe74e6d02e37f566683d4b087e539ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:31 +0000

    appear scare failure impressed tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
