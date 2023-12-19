c60af1ccd874f779dd207cd1f7615791fe23dbe4
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:39 +0000

    sharp takeback Qq under grade

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def count_elements(lst):
+        return len(lst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
