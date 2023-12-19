5c33d275de115d2a83e1c8a647216affbbbf5a1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:55 +0000

    major who getback design minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
