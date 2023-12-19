c179f7bab04fa5ae81b20e9346f3fadfb51f0360
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:49:23 +0000

    though telephone shut callup second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_acceleration(speed, time):
+        return speed / time
