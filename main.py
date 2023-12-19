157c741404176a9c752b816b06d15d8a89c5dfa8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:16:02 +0000

    bringup classroom outside divide either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import platform
-def get_os_info():
-        return platform.uname()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import tensorflow as tf
+print(tf.__version__)
