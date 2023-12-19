1fd00ab457804272881049d5ce8a060af818633f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:48 +0000

    direct idea this government tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
