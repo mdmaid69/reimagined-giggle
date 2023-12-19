5531645f586327d1247e25f467c86463bb757bf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:39 +0000

    regret actual mad nowhere command

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
