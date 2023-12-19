f56da3685ce1fd83055e7cf4e5b3f83ef4a84a50
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:26 +0000

    opponent Vv final holiday general

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import getpass
+def get_username():
+        return getpass.getuser()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
