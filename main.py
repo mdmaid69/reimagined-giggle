fa64572f08d981e84683f6a4944b3b2004ca0cf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:56:15 +0000

    somewhere answer slight among interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
