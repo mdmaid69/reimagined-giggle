7c0d5bd6281351793913bdcd69e41bef2a54da5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:19:32 +0000

    study takeaway proof pencil sky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
