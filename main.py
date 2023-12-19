9e9b2a8452a601372c78ed9ab34de5a22e226620
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:50 +0000

    that themselves shrink slight wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_int(array):
+        return int(array[0])
