75d68038057f7aaad14c972a4f64ab74c3242f32
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:29 +0000

    Itself underneath skin need judge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def calculate_area_triangle(b, h):
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_area_triangle(b, h):
         return 0.5 * b * h
