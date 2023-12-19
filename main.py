75399bbc75e6b2f6f4b932b89983bbdbcc6333d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:10:34 +0000

    hang increase evening contrast unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+def exit_program():
+        sys.exit()
