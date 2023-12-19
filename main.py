3f1dbd72f667156c5030f13c0973ac41fea5aba3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:12 +0000

    interview reason want person little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
