eff471b2524bced274e0c5e45478ce38d2ac5df8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:48:29 +0000

    other justice beyond look height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
