88b24ca154bc7c8b4f61e6e9a26a737bdeb90cf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:17 +0000

    close history always opposite plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
