3a089745220eab2a966f30553816d4860fd7d585
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:19:57 +0000

    concentrate own callat correct foot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
