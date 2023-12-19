59890a3f36033b978cc40c3fcfb694106f4fd0ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:54 +0000

    cold declare advice pocket when

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
