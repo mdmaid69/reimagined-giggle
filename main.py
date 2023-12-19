8485716633b2dbe213a48f0af3042d8b8ce2720d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:20:03 +0000

    danger ability break sing compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
