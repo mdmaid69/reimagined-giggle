360ebf89027b9f0f1ede4fd533cc2c1cf4397157
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:48 +0000

    then different among near watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+print(math.pi)
