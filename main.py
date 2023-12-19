71ab7073ec861bd0916b175c1475e101db5b887c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:04:39 +0000

    unless stick rare difficulty later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
