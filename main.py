ae64174525b89ea22a48c7378e4eb40d30693120
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:02:54 +0000

    giveaway deal flow base hair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
