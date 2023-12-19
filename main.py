269f814209b79ac709576f6312514efc3b930c57
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:44 +0000

    lonely himself roof bush step

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
