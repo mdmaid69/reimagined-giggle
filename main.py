5c386a713d810b7246c656bea84799bdd4948232
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:00:50 +0000

    include suffer evening suppose shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
