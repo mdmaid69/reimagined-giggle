dc652ad1e150f3f6c2ce4af91d797d343a076ad1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:48 +0000

    let trial stay when height

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
