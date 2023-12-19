eaecfc74163ed5cf8ca320802e3e0b1b02fa16ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:49 +0000

    transparent spot close across spot

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
