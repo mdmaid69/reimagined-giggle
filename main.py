2dbe3e87be3270c0c8a59b8aba078649cf46298c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:11:20 +0000

    movement thirsty sure exploration breakinto

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
