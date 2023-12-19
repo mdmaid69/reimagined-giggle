3bf6d2a290f29d3c4dba896311c28d368e2950d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:19 +0000

    there reason strong immediate any

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
