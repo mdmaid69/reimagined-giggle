a12e17830f5d4fe919431ef43af9e9de187c6e43
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:20 +0000

    plenty them thin edge let

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print(sum(range(10)))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
