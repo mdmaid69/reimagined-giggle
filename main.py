3112b9534771dbc4af029d2044cb1416aab126c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:50:11 +0000

    prove appear threaten unite adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
