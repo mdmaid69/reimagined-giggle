ae30cc3830eb922f523fea6e0b39d74ed82c5313
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:34 +0000

    form quit Itself insist consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
