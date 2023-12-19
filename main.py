a8dd5e135c1e9720b5544f4bda4c6d63b8e881a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:32:52 +0000

    law conclusion soil score run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
