68b27becf172242270beb93416cd8005226c3f74
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:50:45 +0000

    tax ahead maybe previous forward

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
