c646939427f0072d9dfcd6ab209115fa09452e59
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:38:54 +0000

    question search ball corner factory

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-print([x**2 for x in range(10)])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
