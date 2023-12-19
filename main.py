f4dd2dba4ab26e71d8b6b05f10b57128f33f11eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:35:37 +0000

    stiff correct buy waste least

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
