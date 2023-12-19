7dfe8830094fcd7771d501d88a6ecbb9c599ff8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:15:02 +0000

    absolutely active protection close classroom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_speed(distance, time):
+        return distance / time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
