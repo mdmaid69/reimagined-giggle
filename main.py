09c67ec9ab79b7bdb4ddf127b5261dde23661239
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:08 +0000

    serious board tone add treat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
