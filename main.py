26a3f0e5fcf3fe07912821428d892b431dab703d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:57:35 +0000

    far future control cost plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
