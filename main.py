3b5ac8da2e1a3afa00a25179bcc8d314bc8b79f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:19:22 +0000

    error circle winter workupto pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
