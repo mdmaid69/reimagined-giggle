12a740591f864278d26d3b4f061c6ad96f903fec
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:53 +0000

    punishment bound home deserve programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
