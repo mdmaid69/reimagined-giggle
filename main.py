c692e403ca23cb7427a53822b27bafad0488e5d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:47:05 +0000

    benefit bother chat affect shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
