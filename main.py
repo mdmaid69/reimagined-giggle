7e869a743f453719ed3a4698091c2c0469de240a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:52 +0000

    special push fill when difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
