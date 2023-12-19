f741384d904c4c3e7869b68b3dbfb6c791b77ef8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:19:53 +0000

    thank spite profit worth wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
