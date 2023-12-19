a79bbe3201258abf67bf00e29257891098b9f2a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:33 +0000

    beside wise medium on under

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
