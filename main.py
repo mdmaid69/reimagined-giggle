093285f6da9c833b70d9d083d99625871ae8a664
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:44:30 +0000

    lunch jump thank pull sign

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
