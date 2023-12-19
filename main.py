219c3e3b4df312a2e47f553b0bc1c3321b88ca7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:24 +0000

    flow minute name raise view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-x = 10
-y = 20
-print("Sum:", x + y)
+for i in range(10): print(i)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
