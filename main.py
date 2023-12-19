4cb3895e32e182c8cb26aacf75d175e2fa808dfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:05 +0000

    blame callup fierce wide fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
