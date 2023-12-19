e4e6cfe96a484d7b0c161630ee915bc173cb134d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:21:01 +0000

    check joke concerning afterwards flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
