3e50202968d042f363c09f55dd368cb7eeca6222
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:43:25 +0000

    fashion nerve scene satisfactory reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
