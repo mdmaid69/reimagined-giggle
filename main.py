4e5d1333ba17bf72e32085cb9c55fe7ed435d9c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:25 +0000

    near operate intelligent television division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def cube_number(x):
+        return x**3
