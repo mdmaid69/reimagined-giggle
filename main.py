06419b0a128afc791001717461fe8232c2758104
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:13:22 +0000

    goodbye weak butter steam bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def split_path(path):
+        return os.path.split(path)
