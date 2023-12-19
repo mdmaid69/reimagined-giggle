bced08a2ab6ba178d2c2c106f6494afdcdd80a7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:02 +0000

    century clean scare mystery variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
