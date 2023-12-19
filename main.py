05fca733043501de08df42d577d522013c71844c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:44 +0000

    service store study name soil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
