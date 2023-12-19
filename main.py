d35e6a9dc420fd1d501c895f8a5c1bb52284a7a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:00 +0000

    plain used purpose foot food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
