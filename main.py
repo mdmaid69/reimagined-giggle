06af66de2d2a632ca494cdb488c4fca3460778cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:29 +0000

    system number however cost when

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
