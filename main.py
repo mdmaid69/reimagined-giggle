1404f00ae403449a90c5291bb38b6d2aad37d77c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:17 +0000

    can small bend best though

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
