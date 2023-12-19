9b9e3a6acb9d68aa5eb00406a7e26fe9a5f11b14
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:32 +0000

    cost necessary shade wrong way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
