64ab37c6c2e10567307ca8450a119781145e222d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:53:53 +0000

    occur union second related valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def remove_duplicates(lst):
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def remove_duplicates(lst):
         return list(set(lst))
