129690e67cfd7071237b1ae2688de75b9041cd11
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:57 +0000

    believe school provide sting shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
