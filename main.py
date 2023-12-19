425a5662d3a099ebdc9274f6e0377228bf266ffa
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:45 +0000

    action say apologizeapologise direct crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
