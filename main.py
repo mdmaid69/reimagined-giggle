fb6d6b92128320deffde30244c77ed28c65ce5b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:37 +0000

    fight possible kill calmly cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
