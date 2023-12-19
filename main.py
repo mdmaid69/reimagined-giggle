84aa52ed176606b40a8f3b84738799cabf273c31
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:21 +0000

    travel brush scale better answer

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
