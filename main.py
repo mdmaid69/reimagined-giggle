35b29f29cc632a9fcbda15ff77c6b05ead8aab16
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:55 +0000

    almost colour declare holdout connection

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
