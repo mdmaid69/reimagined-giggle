712a417827bee5149b6a31caed4f006d78c0962f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:10:32 +0000

    hospital attract children communication black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_characters(sentence):
-        return len(sentence)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
