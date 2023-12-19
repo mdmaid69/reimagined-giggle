ee2deeebad6de6f0f024b465f2aabdc0238fa11e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:54 +0000

    various coffee protect contents favourite

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
