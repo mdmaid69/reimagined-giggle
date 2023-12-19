71c8f55f2e34b6267ae58d15fdd994e1d0d297f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:13 +0000

    appeal communicate exist wise advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
