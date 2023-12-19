06062f7a32ae25726ed1d50e5ff9e0fa797d97df
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:40 +0000

    simple takeoff cover set disturbing

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-def get_time_since_epoch():
-        return time.time()
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
