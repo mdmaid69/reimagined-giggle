f84fefab199b65d0f292308a012a0e3d368949d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:03 +0000

    quarter season hit please custom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area(radius):
+        return 3.14 * radius * radius
