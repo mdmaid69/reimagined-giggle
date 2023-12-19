1911483afd69c605d890cebd43c0760dc73c2615
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:01:12 +0000

    dinner ago comefrom callat block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
