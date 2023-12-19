1338d6eb752ce6e5eb3b095dd0862e36d268d915
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:32:23 +0000

    influence keepoff hour towards knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
