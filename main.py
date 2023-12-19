da892d3b5ec5b9042cc74ad9683ac129393979a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:44 +0000

    similar steady yesterday behave excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
