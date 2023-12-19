1c4b522f88c28c5522e0924b9d74378da55d6539
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:00 +0000

    equipment fairly sweet against reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
