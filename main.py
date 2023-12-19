68d3cbeb6b43092e21d427fd79cb80eb55150091
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:08 +0000

    second common at outside card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
