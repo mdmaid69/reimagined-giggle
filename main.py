3be95e9f1e5c74c847b5d599c98defa7693125bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:14 +0000

    honest somehow comein opponent any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
