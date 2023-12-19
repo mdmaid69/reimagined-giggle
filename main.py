44a915c28bbb5743534bfbb0416f0ad5569be2b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:03 +0000

    Yy hair plant small small

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_acceleration(speed, time):
-        return speed / time
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
