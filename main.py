d9746461864e3b32afb37354a738574bd7de2b7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:53:40 +0000

    laugh turnaway Oo kick few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
