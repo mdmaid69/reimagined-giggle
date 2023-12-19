90bbf366904908cad467bcd22e48ad42673ce09a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:20:14 +0000

    cut similar refer thought ocean

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
