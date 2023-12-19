ca019d9aa21cafabcad0682a79ec65b2c647974b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:26 +0000

    teach apply score deaf creature

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
