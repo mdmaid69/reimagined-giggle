fa5e29397e1b9cf8a341273e51f59a27e21d028b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:15 +0000

    climb entrance run pride attitude

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
