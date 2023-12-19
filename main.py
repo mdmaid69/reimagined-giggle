63c338c848e620939cf96fb53b97fc6a3d259b4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:51 +0000

    inside sort again vision all

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
