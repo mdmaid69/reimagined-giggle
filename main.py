d577c7badd88538101b7ed1dd1c8c243b0f830a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:38 +0000

    condition type earth gate exactly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
