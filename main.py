9b4414f9013e03fbba18180ba55a02c048a52203
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:32 +0000

    kind badly hurt understand identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
