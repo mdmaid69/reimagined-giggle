d08cd4e4eb79907ce41fea0621e26f3c6863ae4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:35 +0000

    comeout bringback compete pull freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
