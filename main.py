69e1179961527a70561490444d2912da93bb3df0
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:01:38 +0000

    leg fit wonder choose broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
