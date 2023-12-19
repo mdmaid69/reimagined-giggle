7f21527e84244da51b22a224b7e219fe2208a066
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:44:28 +0000

    build steam violent completely sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
