12c3a1ee7c494209d56668ea78924e4eb354b8ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:10 +0000

    operation tax table pour ago

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+print(json.dumps({"name": "John", "age": 30}))
