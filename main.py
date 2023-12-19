84418de6dc43a5737a96da72718b80b233674b92
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:37:39 +0000

    confused middle loose gentle go

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
