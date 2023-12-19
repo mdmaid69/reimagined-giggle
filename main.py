65481d2d04d20c757b76c3ec2fe7b2b323a6357f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:06 +0000

    leg usual strength takeoff keen

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
