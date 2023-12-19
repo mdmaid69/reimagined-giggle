f8d94ec48460a104a43ceb876650008ca68cd75c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:27:21 +0000

    treat road do lunch people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
