7d8d08a4856acbd6c10c50548eb21cad22b213f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:54 +0000

    control handle mail dance history

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
