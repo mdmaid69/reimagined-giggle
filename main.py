ed6ca6946c078fe277130c0c1b9f53b19a3e0c12
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:43 +0000

    compete brown note polite apparently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
