91d3d45d49b6b00c47f213172cbaf13bcf48e5d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:59:02 +0000

    wrap test must breakup both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
