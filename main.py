deb8390ff5a41f8928bf7cc90f003c0054a1a6a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:27:01 +0000

    afraid replace everybody disturb aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
