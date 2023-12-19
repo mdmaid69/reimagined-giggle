8116d5ee4e315fb033de806a8a0ec87bfa521690
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:26:54 +0000

    basic back cheap to open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import datetime
+print(datetime.datetime.now())
