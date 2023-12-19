60ab2b5a2c181dd0a25984f151cb34b25198d5c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:27 +0000

    recovery style duty urgent them

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
