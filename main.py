2f8e1a216658632c10519cec2dc98c895c8a776f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:00 +0000

    hardly I interruption house arm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_item(array, i, item):
+        array[i] = item
