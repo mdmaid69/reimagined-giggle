f39eeb975aa0b3f390a0fe60ca9f55332522867d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:15 +0000

    hope scream jump tend increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
