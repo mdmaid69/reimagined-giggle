71e0f1e1eeafdf0facf627b1deb80c59c03f0543
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:39 +0000

    dead wherever anything half tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
