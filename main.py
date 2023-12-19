c94acd318c1511649c80c56b725fa5b7aa8d019d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:04:15 +0000

    glass spare dream need music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+print([x**2 for x in range(10)])
