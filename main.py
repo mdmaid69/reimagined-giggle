d2550213390d3c98a457e1a5f9d586dc5d991a45
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:59 +0000

    probably dick height to definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
