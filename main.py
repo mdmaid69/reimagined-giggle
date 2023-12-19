8e31f4108eac7301086f36e610b2f095b2743745
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:05 +0000

    spot accident represent means business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
