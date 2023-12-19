f302e5a1f7cbcc4aa35b249172775bc9db5fd5f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:31 +0000

    half line advanced final blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
