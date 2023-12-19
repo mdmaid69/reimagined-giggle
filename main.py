c6993e7c9432068dc36c6f407d1aa3e44deba56f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:58 +0000

    car crowd conclusion become favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
