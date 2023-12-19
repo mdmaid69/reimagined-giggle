fe50b43a286512bffe3359d20eb44712c7c2dddb
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:33:27 +0000

    gather second curious middle dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_list(lst):
+        return sorted(lst)
