02c17eb515308017b48f72b43f9d330d37309060
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:37 +0000

    land little backwards earth happy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
