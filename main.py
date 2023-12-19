7daf5c95fde43aae710163c38436c4c18ddeb548
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:54 +0000

    trip knowledge kill second perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
