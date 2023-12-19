5fb32d098e0d5be8d014f49496ce342627260b6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:40 +0000

    pity soak shut minimum reserve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def reverse_list(lst):
+        return lst[::-1]
