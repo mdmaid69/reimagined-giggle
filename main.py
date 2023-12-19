6fae9fbc01f929a5cd155586535ecc73f922a87c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:25 +0000

    service actual breakinto children clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_item(array, i):
+        return array[i]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
