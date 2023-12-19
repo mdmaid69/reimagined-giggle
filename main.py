ac7ff87c06fa8955d6522c82bb8602e1c4015216
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:02:19 +0000

    can maximum wander themselves different

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
