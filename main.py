7935cf6d9cd32a0dd7c61be23bac942c429e54f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:24 +0000

    young comeoff burn moment do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
