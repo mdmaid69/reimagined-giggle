a402665b42605c9f1e2f48ca96c5f3790be86bae
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:06 +0000

    thanks drop tip safety snow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import time
+print(time.time())
