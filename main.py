7a7777cdae98e326345f85ad96d55902675f2f36
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:16:12 +0000

    moon run whichever free describe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
