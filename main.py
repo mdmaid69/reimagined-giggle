9451eb748407bda1c07f4d4194b8fc0e1036ea0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:15 +0000

    will entry entry idea its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
