b9fd3697721f570bc1897966bec32e4a8b9b31d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:39 +0000

    catch declaration Dd your takeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def sort_list(lst):
+        return sorted(lst)
+def is_palindrome(s):
+        return s == s[::-1]
