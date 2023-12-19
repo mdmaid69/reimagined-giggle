9e14d5d6b28b33e86f4e74c4eab1c3f7754e00dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:59:16 +0000

    memory fear damp rid simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
