1704187cfc7407f390e052ce5d637811b5593d61
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:02 +0000

    before prefer succeed raw 571enough

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
