295b89c40ea527bbd27ec407649a4cddc0562dbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:22:43 +0000

    total flood alone belief ear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
