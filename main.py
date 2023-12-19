546a93e9a02157991473b3d80b325bbf0e16b331
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:16 +0000

    woman wise spring sport cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
