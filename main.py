48580c95549a1e2d22ffa1335acbce87c1c29e20
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:21:44 +0000

    holdon fierce disagree embarrassing plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def clear_array(array):
+        array *= 0
