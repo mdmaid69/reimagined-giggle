b0ccdb012c8cc9c5e8ba80181f54bae60c06cda3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:29 +0000

    ashamed anybody carry people development

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
+name = "Python"
+print("Hello,", name)
