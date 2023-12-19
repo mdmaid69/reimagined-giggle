38ad3e029889b97faebc9673e85be1c510e0fe57
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:23:32 +0000

    measure proud flow rarely company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
