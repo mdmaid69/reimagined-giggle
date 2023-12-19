10bd901aa98d69a8574077d5a295dc020263c062
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:59 +0000

    concentration belong comfort in bear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def sort_list(lst):
-        return sorted(lst)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
