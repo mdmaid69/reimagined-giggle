c74685eef07d884807d3cb43d38606dc365d903a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:19 +0000

    language peaceful everywhere scare chief

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
