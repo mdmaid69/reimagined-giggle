423313d23765719c7ff9f8c70a0407b42bb3de38
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:07 +0000

    mess small some instead recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
