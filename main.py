eaaeaf2f93baa29e8ad391bb10cb06649742d165
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:33 +0000

    means add wave nature allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
