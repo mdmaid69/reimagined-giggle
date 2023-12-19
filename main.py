2850dbff28be25fc5d56e36829d788f7c72ad2e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:40 +0000

    on worry backwards acceptable command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_density(mass, volume):
+        return mass / volume
