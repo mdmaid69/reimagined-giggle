a450a0d9b9c78d85b20e4fa44d7c33077d691556
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:32 +0000

    wave roll land repair mean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
