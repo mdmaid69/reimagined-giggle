37ea463c4921e9ddbefd83846cf7f48fb73b858e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:19:34 +0000

    escape support visit brother unlucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import random
-print(random.randint(0, 100))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
