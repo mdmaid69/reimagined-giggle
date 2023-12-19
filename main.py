b8f489f679e7cb8a8c27023e334f151e9b533a56
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:51 +0000

    wish platform before drop read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
