46184f722c6f236a2ce34363d8dc2620e3ae92cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:19:46 +0000

    false wash objection regard towards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
