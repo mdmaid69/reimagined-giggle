2547af06c5ee691dc598d04e68713481576dbb6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:27 +0000

    give even keepoff nurse exact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
