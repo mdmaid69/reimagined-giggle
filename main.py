4fc0852903fb03684d6474efd7f588a7e1a4246b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:21:05 +0000

    complaint tradition fire hurt active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
