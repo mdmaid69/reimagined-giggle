1ea4e601ccb1e67d1a84d7eeab538d6588bf511a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:22 +0000

    this afford present behind scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
