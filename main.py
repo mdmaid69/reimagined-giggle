709e848200caa8b4081468a1d7abd09888282d84
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:15 +0000

    book interesting any flow platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
