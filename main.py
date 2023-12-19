7ed981a3411af5f8f51f76d74d2b443c0004736f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:25 +0000

    disagree thinkover generally priority crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
