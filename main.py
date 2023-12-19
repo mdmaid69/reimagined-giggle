fb46089d0acc3942dae3c223ee0623289b68da33
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:56 +0000

    swim heat comefrom run man

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
