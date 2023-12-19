fadbb036f1838fa39944de24d1ced16da578c46e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:11 +0000

    introduce base pride hardly crisis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
