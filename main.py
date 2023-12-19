308814f71c991ce2f5326e0894a168328383526c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:02 +0000

    education present punish characteristic sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def square_number(x):
-        return x**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Uppercase:", text.upper())
