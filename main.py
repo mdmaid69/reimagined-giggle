8dff4e09af183786724b70e84840b9f88bf1b343
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:24:41 +0000

    future influence damage once pay

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-print("Hello, world!")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
