720ec8aba5ecccd84ed2bb10b771d51804d08302
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:43:01 +0000

    along badly make similar shelter

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def reverse_list(lst):
+        return lst[::-1]
+  def is_odd(n):
+        return n % 2 != 0
