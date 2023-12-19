9221d76edf81633ec6640ec046a071c7307e3085
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:47 +0000

    understand recommend complaint eat attach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
