f0045435c279a606185dce44b5d0d3d58345f70f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:46:31 +0000

    I still like fetch minority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def reverse_list(lst):
+        return lst[::-1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
