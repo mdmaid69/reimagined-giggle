b09f9d5abea76231413625848843cf605037bafe
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:26:02 +0000

    mess understand power might produce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
