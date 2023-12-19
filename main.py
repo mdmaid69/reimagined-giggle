84909973629284b305c1201e49c54921aa53b5d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:40:05 +0000

    inquire obvious dance yes miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
