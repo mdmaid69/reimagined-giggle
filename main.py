8f15aebc36459b41acb56f3952ac05bc4e0a9d97
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:25 +0000

    choose stand measure almost proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
