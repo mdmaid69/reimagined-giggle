9631d84b61fed51c6c6096a86be40c4641cf671a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:01 +0000

    occasional voice declare separate set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+print(os.getcwd())
