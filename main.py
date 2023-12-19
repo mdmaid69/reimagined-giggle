0446656b69fe066e80f04cb522b62d803cafcf16
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:05 +0000

    thought soil example respect direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
