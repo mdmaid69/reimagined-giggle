0c33d5a025c7f58a4cf5377bce4d635331ead583
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:14 +0000

    satisfactory comefrom fine crash negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+print(time.time())
