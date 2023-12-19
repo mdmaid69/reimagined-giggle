84adc5eeb7aee36d619d1ac5f12e17e4381ce5dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:32:56 +0000

    cheek later break pin wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
