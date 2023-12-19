e2be919661bc9db70282a430a206e3f5cda89656
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:54 +0000

    convenient mix in state by

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def remove_from_array(array, item):
+        array.remove(item)
