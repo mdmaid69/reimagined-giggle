d54fb26cb602b8a0904a935b41d8915cafc52fe9
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:48:46 +0000

    breakthrough hurt Ww contrast thing

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
