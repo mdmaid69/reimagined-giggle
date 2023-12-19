ed9b3a8e88496e42e00fec51ce6b994129ee1ec4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:24 +0000

    night hers call fairly frightened

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
