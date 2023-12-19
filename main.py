faa4665b867fc14d9c8aef546e50b7ae2774441b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:49:54 +0000

    pretty game agreement material choose

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def cube_number(x):
+        return x**3
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
