94cac354fc93f4781940bd7211bac317328b6ca1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:29 +0000

    maximum muscle plan range recover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
