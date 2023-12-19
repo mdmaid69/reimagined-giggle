7f07f1ace9f5368f76692fadd21ca255305884ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:34 +0000

    build dress whether pool doctor

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
