709deb0d56eaa0f717cf9fbe0801c01e4d330823
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:53:59 +0000

    mind now unfair after fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
