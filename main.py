58a72a635ac20c64961ee793312756bd2889f188
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:50:03 +0000

    minimum brief bet score whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+name = "Python"
+print("Hello,", name)
