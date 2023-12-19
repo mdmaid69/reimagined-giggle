081c911315f31443e8cbb230ba87abf012fc6f80
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:17:27 +0000

    winter whichever behaviour scrape chance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
