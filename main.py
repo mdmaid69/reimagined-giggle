1b10fcc3665c59a6f20d48bf11a90f1496509462
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:46:19 +0000

    backwards beat home mystery sort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
