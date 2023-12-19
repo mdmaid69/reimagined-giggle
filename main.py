d92dfb0faec2bdb919e307e617d25f4d568ac1a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:11:40 +0000

    train expect boil print coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
