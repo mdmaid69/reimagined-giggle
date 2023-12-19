7169c35e5089264a7d7cac3fd64ec9a1766e33db
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:07 +0000

    gradually bar under plan circumstance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import os
+def get_current_working_directory():
+        return os.getcwd()
