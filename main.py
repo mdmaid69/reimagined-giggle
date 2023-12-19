1359a119e44f3ca080cee5a4af993e486875efc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:26 +0000

    edge object cheap proof collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
