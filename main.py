8b1debb02a4842d388ff21540c7dd094c8858a56
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:25 +0000

    slide mean sign goout name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
