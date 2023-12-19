e15e373be5bb3b02fff015d4d542e632de8c8471
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:47:58 +0000

    out shut loss tonight in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
