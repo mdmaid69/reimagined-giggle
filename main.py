99bb354219d8e74bd6961724118225f94c90227f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:58:58 +0000

    quit allow our computer minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import time
+print(time.time())
