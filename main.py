bde932ab3fa1984c4cef0c5a17e86e080f1b63cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:01 +0000

    safe heavy by excuse touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_acceleration(speed, time):
+        return speed / time
