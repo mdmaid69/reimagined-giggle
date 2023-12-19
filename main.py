70ba9db23e589b8ecd43638bf6c67de29c67c037
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:27 +0000

    tidy gap light funny fold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
