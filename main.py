75be18919957d9b5a54ec604e7158d5bc3d744cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:29:52 +0000

    result play meal group move

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
