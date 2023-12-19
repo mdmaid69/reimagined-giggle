760faf6e575d225cd01fb3f118e47a39e53a99c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:06 +0000

    him thought and loss fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
