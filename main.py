c219afdb7619360090e19cf4fc848d0daef0766a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:05 +0000

    best love straight guard gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
