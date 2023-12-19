891003265c624a168986341b3d39ca967daf5f13
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:49 +0000

    cruelty ignore breakin walk father

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
