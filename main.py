821d4346d0a5687634721c1c6374f6a7551d1662
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:07 +0000

    point second level complete squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
