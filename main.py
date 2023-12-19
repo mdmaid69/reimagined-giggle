abd8b60fcdf3941dd6bf0f78cd91308983f6491a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:49 +0000

    steep actually continue desperate past

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
