36847ea3db0e60c6592d95d817dde3c3b4e85bec
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:21:37 +0000

    spring 566end against join mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
