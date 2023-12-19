4fb52ded489dc53a8862f255c838b2f68f405704
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:41 +0000

    new famous listen vote container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import getpass
-def get_username():
-        return getpass.getuser()
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_speed(distance, time):
+        return distance / time
