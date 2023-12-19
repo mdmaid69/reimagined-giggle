bd240bc5aa1e520387668cca5a45bf413f8ca5f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:17 +0000

    spill favourite successful entrance nose

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def get_array_as_memoryview(array):
+        return memoryview(array)
