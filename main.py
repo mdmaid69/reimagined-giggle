6447ec904cbdc785c8edf3fd398db0ab03473359
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:36 +0000

    I everyone fair large careless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
