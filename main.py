326abbaffe1dd16d86273c798cfa0551f5076971
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:12 +0000

    sorry communicate lock sad over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
