f4df58695eacb6221d7145e01b680aa5fa8976c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:36 +0000

    blind cap country instead attract

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
