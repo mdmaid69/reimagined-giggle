21747fddfcd408cf25e71c0f42c6ec8dac0815d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:10 +0000

    after interview thick comein holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def is_palindrome(s):
+        return s == s[::-1]
