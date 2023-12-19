dd2cee5ff275861fe2137c90ca58b73bdf17b896
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:45:42 +0000

    workon much danger protest interview

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
