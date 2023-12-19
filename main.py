71324ca650b988a2a19583c8446051cb33f86192
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:26 +0000

    tone decide mend follow point

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
