c2b55a8b68bf7eb34facbb09f19a660ec406531e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:56:37 +0000

    world sense real replace breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
