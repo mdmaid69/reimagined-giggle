2f457c34457f72d53951d71facc321c688fe2cc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:09 +0000

    lead win rob sky meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
