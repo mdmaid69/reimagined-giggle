4a654c33dd52243733566c37e2b50782c49fba16
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:09:06 +0000

    by treat our pronounce share

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
