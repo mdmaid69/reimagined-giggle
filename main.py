91576b3fdd87ec1f0031ee23e04ef274ebcb4099
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:59 +0000

    seem programme hard buy repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
