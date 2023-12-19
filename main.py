a467438bee15d80401a09cfe5e772fa3385d709c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:56:14 +0000

    community fix workout plan on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
