32ff50eed29661d981276e20585ca55bb066acf4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:19 +0000

    square right difficulty hungry luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
