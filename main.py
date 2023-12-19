c78aeb9b40954dc45c89ac69dcebed2eb772d6f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:46 +0000

    appeal decide gather fat positive

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_floor(x):
+        return math.floor(x)
