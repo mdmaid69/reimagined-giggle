6984cf2b81355aedd3021cec01f344ed7226e1af
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:47 +0000

    scream cut completely joke hot

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
