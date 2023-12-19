bad81f7b369061b3f86112928aa5dd0ec835ccc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:29:41 +0000

    official indeed really hungry lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
