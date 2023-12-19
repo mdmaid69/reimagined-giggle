e59e58efe68a937982ad871d2e68d3f9264651c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:46:58 +0000

    nor sure wander trial dry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import getpass
+def get_username():
+        return getpass.getuser()
