71581e944bbe6fa0cb9aa4d70f999d3a9c6c2e19
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:44 +0000

    recovery idea sticky neat hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
