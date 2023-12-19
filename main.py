c6c34033ccb1e5984f578903d4d63e868f5fce21
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:33 +0000

    direction pen to either deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
