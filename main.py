7fb1fd8be380604a358b68cb22398bffe7ed041e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:24:56 +0000

    shoot honest keepup choice separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+for i in range(10): print(i)
