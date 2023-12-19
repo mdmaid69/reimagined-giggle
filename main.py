b1375dc4b48ca790ad51e8b20cfe230372ed9173
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:36:21 +0000

    pour you connect rule empty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
