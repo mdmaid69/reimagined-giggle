3cd0a4fbbf0a849c570df2eb02658446c6039a44
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:46 +0000

    constantly movement correct behave Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
