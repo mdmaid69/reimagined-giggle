127c3ebd49bebed7039a73442a7d30f1999a6b88
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:37 +0000

    quantity fact stir makeinto damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
