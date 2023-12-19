83b375083c4049020702c40c2eacbf4320d24ba6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:37:23 +0000

    praise mess open office at

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
