8daa3fd3291fb063598c8a4adaa3f5809f20af8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:13:44 +0000

    turnon workupto could worth poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
