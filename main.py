90811731b71908aef1f6fcd7ca9d716c9f86107d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:03 +0000

    crazy idea float leg determined

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
