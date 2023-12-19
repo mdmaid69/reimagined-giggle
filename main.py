c687cdd0c04bc24935bd449d9fc661a4bd232977
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:49 +0000

    talk tend doctor file later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
