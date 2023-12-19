0c1353e0b0c7d43f7cc79123ff47bf7ef00a2997
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:02:04 +0000

    chief us amount conclusion through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
