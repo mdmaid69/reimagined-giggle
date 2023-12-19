9841dbbe2d9792abae8c85a0f22710293f95a03f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:31 +0000

    style criticize unite getup still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
