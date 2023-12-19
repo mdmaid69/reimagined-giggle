50ca24bb6b72ece1e26c4dea306e0512bb4b3b92
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:53 +0000

    complaint beginning radio there lower

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
