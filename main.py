b7503606825cf7e132fb9a3e93521f421e46a92a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:25:32 +0000

    within joke central congratulations throw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
