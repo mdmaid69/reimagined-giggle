c4209292d1e03bd8f70ea86f88e4c3d51c20fbe2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:32 +0000

    accident afterwards spare getaway send

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
