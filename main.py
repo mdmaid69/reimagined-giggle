5f43c15c40708d2601506fe1f8d0957209107e07
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:56 +0000

    cost grand equipment each tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
