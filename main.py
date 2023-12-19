25cbebe88cf41274c3c94c5a6765532f7ec9e0d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:28:39 +0000

    quiet cause balance emphasize lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
