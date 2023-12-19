9009e755aa73d1df770a00ec7f255b23d9973c88
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:58 +0000

    talkback term set today matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
