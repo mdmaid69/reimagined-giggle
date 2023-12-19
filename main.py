ad77729a980c69597829eb2b3e23cc3641720092
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:51:42 +0000

    unfortunate learn well tight getback

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
