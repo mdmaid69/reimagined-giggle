555dd2c552d6d7727242a9a2aac63125a109814f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:25:42 +0000

    prefer bury loan total say

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def convert_to_hex(n):
+        return hex(n)
