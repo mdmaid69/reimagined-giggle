bdb3811eebf2d06898ddacde5bb0335fbce480ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:53 +0000

    please little drive very float

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+print(re.match("h.*o", "hello world"))
