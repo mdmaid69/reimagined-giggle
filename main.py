bee84236693244c8d072cc38d669a4b2d08218ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:28 +0000

    charge comfort job disgusting safety

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
