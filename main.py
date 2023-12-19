f13e1cf12f141956dc4913f2c95d888680c2bbf7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:57 +0000

    sort moment lonely grow bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_queue():
+        return collections.deque()
