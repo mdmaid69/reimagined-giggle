aebbc949f763a023928fdc42f9ffded3b92d6ef4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:36:04 +0000

    fight interested during simple sight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
