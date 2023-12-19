2f00a7fa8af36d52eed693ba1393a9fcf0398587
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:21:24 +0000

    nervous open plain tone care

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def is_even(n):
+        return n % 2 == 0
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
