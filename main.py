c9da2bc2794948bee093200a2def89c1150ad65c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:16:32 +0000

    Ll centre will inform blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_current_directory():
+        return os.getcwd()
