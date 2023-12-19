5f3cdc90dc9ac89293db3b725aa7a2e915e99b9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:32 +0000

    pure region day cost long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def is_odd(n):
+        return n % 2 != 0
