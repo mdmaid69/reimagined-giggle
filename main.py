815c2a8375f4ff81daaa1d5746469699ca03e832
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:11:26 +0000

    error above unfair whatever attempt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
