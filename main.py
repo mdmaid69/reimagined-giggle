02e081553c280fbdb81745adc3da7f14ff6d6991
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:44:18 +0000

    scheme bye may thankyou plenty

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def multiply_numbers(x, y):
-        return x * y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
