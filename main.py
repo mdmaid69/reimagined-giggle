db7edca8a3575b96c4ad9a8cecc5057a11642dcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:44:47 +0000

    odd story imagine practise dull

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
