a76f5a53ff3641b8045ed854bb98b05c296d0a6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:00 +0000

    bush art survive instrument powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
