a2679a60b283adb4f44f0eec85731c92b5191fd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:34 +0000

    mother roll ignore other play

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_rectangle(l, w):
+        return l * w
