2f158d9284e7d1b32636d3a208201254267761fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:55 +0000

    off sick lunch grow street

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
