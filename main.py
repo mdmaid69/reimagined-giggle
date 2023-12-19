c070eb02126ed0be9e03794949c604c427147778
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:09 +0000

    healthy through dark miss person

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
