2cf699db4661959669166e01a723b1c0ea532e30
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:06 +0000

    software question Oo regard ground

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
