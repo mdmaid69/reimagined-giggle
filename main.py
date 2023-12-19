8f5b525c3e4e868ef349c5f1999b651de2a0118a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:55 +0000

    wish seriously drawing second scheme

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
