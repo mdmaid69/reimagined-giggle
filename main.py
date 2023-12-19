d77b9c065ce2dc94f7a7352e6528b2f447a77157
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:13 +0000

    clearly fold disagree quit suddenly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def convert_to_binary(n):
+        return bin(n)
