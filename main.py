d9e5753644f8aa42a9a65bdb4d0cd59d7499f853
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:07:47 +0000

    easy slide education sharp share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
+def multiply_numbers(x, y):
+        return x * y
