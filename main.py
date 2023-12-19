4e99fe01df67f9074d10346900d3d968dcba8bc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:34:57 +0000

    close coat like pencil scare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
