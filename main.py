bbe20704dc6033f2e22a317f20029496fecf237c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:20:40 +0000

    value off firm probably thick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
