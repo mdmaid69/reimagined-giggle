88547efa4e10d0f10d91da967992c598fa3e56ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:14 +0000

    tool tidy result independent cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-for i in range(5):
-        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
