a2b3fbb400c70dedabefc7a11e9dffb2a99e76cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:08 +0000

    water sky crowd contrast possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
