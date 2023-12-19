4198f5f5aa43690fa5eb80de5af55a8ade1636f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:51:08 +0000

    yesterday face comedown trap clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
