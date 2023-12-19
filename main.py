61cd51ef9fddc213f7917df663b43520f9d84a5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:30 +0000

    mother father container remain trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_set(array):
+        return set(array)
+def convert_to_binary(n):
+        return bin(n)
