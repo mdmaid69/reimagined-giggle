7940c6c25f4289ddfb550f99401bcad9bd09e2ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:57 +0000

    head busy eager shine standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
