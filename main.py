fa680d110326f8df91b8df311360b2dd48d758fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:04 +0000

    trip force thinkthrough shop suit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
