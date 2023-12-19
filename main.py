bc1d1838641adf25c92bd78643bfa5f22816cfc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:43 +0000

    disappointed post nowhere down young

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
