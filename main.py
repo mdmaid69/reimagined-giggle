fe5be7a30f76b57b6dd85fc7a5979efc4b01576d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:29 +0000

    distance save danger basically thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
