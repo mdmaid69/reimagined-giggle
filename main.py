39abb2852adf19ab62c640ebe2c90064f8eb8bcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:16:15 +0000

    lean explain quick join goaway

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
