c04e52dbb31b3896d0039a1b98ef897b73138347
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:12 +0000

    act holdup tip reasonable claim

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_floor(x):
+        return math.floor(x)
