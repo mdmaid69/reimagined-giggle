a42dd8fc7617e2f2a6dc8614be5ca65fadf6d038
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:09 +0000

    safety such university break take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
