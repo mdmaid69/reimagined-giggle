1ff22f0501c713a678900120a5c82bd004599b79
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:04 +0000

    steep single mistake acceptable here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
