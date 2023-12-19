c50789f05e9c89bf5ad598bd40b67df765cc7762
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:33:23 +0000

    this say defend congratulations late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
