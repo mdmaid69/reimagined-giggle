e5ee31b57261a9b96f7d5b2715277fa264444058
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:42 +0000

    board apart wet 573entertainment keepout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
