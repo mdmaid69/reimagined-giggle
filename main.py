517427c8b5ffd331693b0ab1a925c2eb20678dae
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:16:30 +0000

    flat base cut argument brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
