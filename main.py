f3261f212cac9483b2eb57ff9c3ae33e6943f1a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:12 +0000

    giveout slice check smile exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
