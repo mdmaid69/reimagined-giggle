34ebae548cc5562da54376b45815f07365042a80
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:50 +0000

    responsible interview keepup no bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
