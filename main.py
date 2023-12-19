f50302225d817c94bed30084ad27735568a9fe55
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:31 +0000

    mad mixture geton praise Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
