0528605f74554fe488b4491bb79ca4832b97a201
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:49 +0000

    print hers come health convinced

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def count_elements(lst):
-        return len(lst)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
