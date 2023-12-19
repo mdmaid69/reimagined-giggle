92587c4787144f8baa627c21518ef56277ac2bb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:14 +0000

    search print excuse estimate watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import sys
-print(sys.version)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
