2279ddfd13914bf07a0a9531a8ebfc1e8493cc0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:06 +0000

    mess insist direct compete scrap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
