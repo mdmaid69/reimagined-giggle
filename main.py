8817bfdc7efc68398de8c7752fccdaa05c096bce
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:30:41 +0000

    film check limit mistake little

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
