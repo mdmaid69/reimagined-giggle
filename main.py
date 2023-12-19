d31dda576395265b76dabbf8a12d0027c0491709
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:25 +0000

    herself objection smile reject press

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
