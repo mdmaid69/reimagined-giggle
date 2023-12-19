bb9941ad99fa88857e6cb59fcbdbb49b6e9a8e23
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:22 +0000

    shade material cut disappointed kill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
