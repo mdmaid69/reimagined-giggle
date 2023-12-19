d0844f15b43a036e262706a2f9de1f9ca1b37c7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:48 +0000

    shock clean appearance insist under

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
