3e0ac32d041fcee3be70a7e2b5056ef8b0f24b95
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:32:35 +0000

    plan show rather refuse cheap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
