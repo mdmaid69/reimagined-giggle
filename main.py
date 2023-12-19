dccb9fd464fc4294ef7a5cf18be88e5c4ae312a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:07:56 +0000

    inform woman choose date reaction

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
