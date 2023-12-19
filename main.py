ae08edbb321d42da46e4b0d5739a8758c2849f5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:35:05 +0000

    together mind theirs straight clearly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import json
+print(json.dumps({"name": "John", "age": 30}))
