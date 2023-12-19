5f84e2afe4edd24baa39acac88eb48e9ada82251
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:28:27 +0000

    responsible arrange result conduct realize

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def is_palindrome(s):
+        return s == s[::-1]
