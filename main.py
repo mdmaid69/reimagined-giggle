73d177eb28f3719ddb8ffc291a5d7f35336a2b46
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:11 +0000

    mainly behave risk turnup know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
