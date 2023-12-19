89054f38718d94d4ff68678c813e21c60abd470e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:08:25 +0000

    way weigh on tongue confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def is_odd(n):
+        return n % 2 != 0
