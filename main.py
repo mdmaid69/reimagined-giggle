5f5e3fb1b305318bf60c6dceb9a67392990e9216
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:46 +0000

    emergency undo head minimum war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
