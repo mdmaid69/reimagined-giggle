5a3f6933705d83883e7ef2dc0fef8663f09c0d4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:49 +0000

    sensible born pile goon broad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def find_min(lst):
+        return min(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
