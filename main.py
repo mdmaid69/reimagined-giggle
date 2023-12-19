7d1692ef94832b2c634f123ae074e01b3cd04f44
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:47 +0000

    listen chance rather avoid effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
