e0b59f7d4c272dd87b78918f684737d3008bd6b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:48:04 +0000

    quiet sell complicated willing majority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def cube_number(x):
+        return x**3
