775a0912ee3068c989f2caeb52658833ee43224c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:02 +0000

    start Kk whatever information view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
