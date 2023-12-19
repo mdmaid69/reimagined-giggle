55739b0ef2e971608133788bc187226d3d823188
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:48 +0000

    emphasize doubt personal preparation emphasis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
