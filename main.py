cfe63768c47134c207d914553f941e0da45fc3dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:22 +0000

    dick damage direct hurry expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
