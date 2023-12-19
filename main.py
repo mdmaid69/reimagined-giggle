ba693a4c5675ab060bd32a6c3a30370d9d3727ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:15 +0000

    measure mix tough track priority

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
