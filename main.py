3a4c9ab9f398f68e48a25d020ecab845d2b4b898
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:17 +0000

    height today strict rush complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
