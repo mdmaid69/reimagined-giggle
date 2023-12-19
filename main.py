c89e3b0e3bfac60fdf6db11f2d39558f25c40f51
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:02 +0000

    become meanwhile kill seriously warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
