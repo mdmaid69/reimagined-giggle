c4be1c33f4793a8e5eb40aba4ac102b231154672
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:04 +0000

    different impatient copy think same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_density(mass, volume):
+        return mass / volume
