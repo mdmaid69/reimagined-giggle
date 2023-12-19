be27eac3834bc9f6f002c84a9a6a4201ff20762e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:53 +0000

    let wake rob fire workupto

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def add_numbers(a, b):
-        return a + b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
