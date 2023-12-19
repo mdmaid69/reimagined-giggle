4b09f2cf9aa513638cfc6619dce6553db050b69d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:58:38 +0000

    future mystery drive roof pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
