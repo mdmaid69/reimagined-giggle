fc43f625abba3b4424b51c8f5ccce0d0b4abfc5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:09 +0000

    our talk last far swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
