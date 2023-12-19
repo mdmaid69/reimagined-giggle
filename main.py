9927302df7ab9d79d84227921e2d712e8cde6fd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:15:38 +0000

    age sale stiff every floor

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import array
 def iterate_over_array(array):
         for item in array:
         print(item)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
