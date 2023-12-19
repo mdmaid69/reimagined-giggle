6a8f6ed1e94faea5dc6c4f5ff67b62c450936171
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:42 +0000

    judge region aim formal tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
