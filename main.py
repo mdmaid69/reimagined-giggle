382ccffd389fe28d1b781945afed64b04176bf8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:51 +0000

    head thought smooth mass ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_pressure(force, area):
+        return force / area
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
