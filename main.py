b0b7b5f3b8c6c12d9f6eb4a264639820c77664ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:43 +0000

    weather chin flat heat range

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
