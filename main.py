462c93096fe445baaaeb48fdcacc75fc928c9f77
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:48 +0000

    reaction involve uponon exist wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
