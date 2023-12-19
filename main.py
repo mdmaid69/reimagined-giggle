218c0c322a97c5c653cba7a71720fc42e2491a38
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:47 +0000

    but watch think sound definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
