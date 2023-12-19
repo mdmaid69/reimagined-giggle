17af0d0aa3d87f1bc2e5edb1df06bbfc641b2b4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:44:28 +0000

    by Kk fasten dance hot

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
