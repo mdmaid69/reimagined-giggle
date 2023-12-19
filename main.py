7ddd1ce7dde801aa938fad66ef03dc32fab079c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:13 +0000

    exchange scream information off tend

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
