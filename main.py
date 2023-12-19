911e14d134e4986f8df00f1a1ee640311a22bb1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:25:06 +0000

    official contact promise smile circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
