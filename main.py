86cbef506e829ce1682381eed31c81fedfbef449
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:28 +0000

    deliver result thankyou earth suffer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
