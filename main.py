b80c2c819b9e2bf9745cc52fc691ddb103e7e686
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:58:41 +0000

    nearly time heart no opinion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import platform
+def get_os_info():
+        return platform.uname()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
