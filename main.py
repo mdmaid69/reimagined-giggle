a1081fa5b2a703bff3619440bc51b5ae4d5eaaf1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:32 +0000

    564encourage moon welcome prize doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
