01351650767992f6f654ad3038de244c871f6b18
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:25 +0000

    whom whether classroom emphasis noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
