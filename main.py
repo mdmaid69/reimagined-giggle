fac8bcce0c3ed13e444a81c1a95725c7b8fee0d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:03 +0000

    slide stretch often which cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
