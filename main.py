3ba426c87b3735c77a241a0594448bc376a31177
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:29:11 +0000

    suddenly react life warning niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
