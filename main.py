9a59391b4599a37dd013cccc3663362e0f1356c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:14 +0000

    club load takeoff cause teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-from collections import Counter
-print(Counter("hello world"))
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
