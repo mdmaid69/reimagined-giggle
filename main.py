2ee33693a40b550b67bda7e1821d10ab569c5e61
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:33 +0000

    transport just bath visit or

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
