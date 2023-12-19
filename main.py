f8df725fd3f6614e8479cbb029e99b30cee1d8d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:21 +0000

    cheat record only serve as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
