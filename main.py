5459343245b26a56a13c3e4bf7a24bacf6827176
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:05 +0000

    appeal intend level example unless

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
