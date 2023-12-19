d7fb1fee5ed086b80a6e20b82f81f61d5ad8eccb
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:09 +0000

    satisfied keepoff land generally exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
