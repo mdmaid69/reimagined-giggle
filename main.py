0659f2e37ce0a0e9e13646fbc043e189c7f0f64a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:39 +0000

    exam afraid otherwise increase downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
