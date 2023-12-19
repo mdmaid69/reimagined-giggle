fe570a80f58e6819570d81374b57df7649bf1a58
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:52 +0000

    fall polite swimming easy downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
