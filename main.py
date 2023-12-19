7d4cc450dd8c2a7d4589add5c062bced6fa4760b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:05 +0000

    immediately their pocket relax after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
