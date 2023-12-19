2388a92ffd7389b62703115b49224c496a5ef390
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:46 +0000

    force flat approve perhaps base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
