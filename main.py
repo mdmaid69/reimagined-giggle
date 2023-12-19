97fcbe5acfc68766b60e6403344abf20da13f49e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:21:39 +0000

    pause preparation depend towards need

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_odd(n):
+        return n % 2 != 0
