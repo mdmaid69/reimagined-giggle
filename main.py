ac80e37e8fc354c817a88fa4663f1fc52032e8ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:44 +0000

    build comeoff dark stretch or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
