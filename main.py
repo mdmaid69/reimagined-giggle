d7c736ec344129b5e7e449e0dd33e6afea98bfb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:32:44 +0000

    recommend Tt attach refuse handle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
