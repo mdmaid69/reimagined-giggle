9fdb019573e21a64cc49bbc9ec8e79e704d654c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:15:21 +0000

    slip mark black themselves type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
