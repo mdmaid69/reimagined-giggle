47703b959e7a816c074e8afbd0b1179c0008eba2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:55 +0000

    that holdup wrap job gap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
