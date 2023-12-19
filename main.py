5c083b2bdaba877dfa8338ea4da58ef1d6351ef3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:12:45 +0000

    discuss edge judge threaten hope

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def find_max(numbers):
-        return max(numbers)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
