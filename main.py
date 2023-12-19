c8bdd1c0775da6888acaa32ae71c3a45af9b6e75
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:02:51 +0000

    569enjoy nervous brown fall neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
