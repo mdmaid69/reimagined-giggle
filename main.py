ec3b005640622924573a7f419574f61c9f0ea073
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:08:38 +0000

    future teach drink character direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
