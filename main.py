458d2e720538b3f26a17c39073db9baecfed16c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:59 +0000

    high slide vision comeup funny

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
