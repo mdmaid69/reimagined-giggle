663a5586595993b22a4e3d041cfe3d05a3f1f6dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:56:58 +0000

    drop education until responsibility into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
