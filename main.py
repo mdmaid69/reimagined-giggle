fff8dc183012365460c5c150ac428c97b5fe8f37
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:51 +0000

    raise tidy roof contain ask

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  def square_number(x):
+        return x**2
