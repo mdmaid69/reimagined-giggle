01ea3e62459a53e239295adccf77b8d73b1ca5a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:57 +0000

    suddenly what snow sell way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
