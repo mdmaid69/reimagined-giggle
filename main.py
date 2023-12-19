c9438f806da6a9c49458de44027333a0cff8ea03
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:38:01 +0000

    pool capital snow limit blame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
