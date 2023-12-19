f13383f75d439f7a20fe64b2deba2cad745d13ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:05 +0000

    target again dress already light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
