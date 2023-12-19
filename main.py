7805d6bba7690148effdd7f55817666176a8840c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:25:36 +0000

    result afterwards lend swim square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
