ae09942c3c04caaf8a660dadd34d8486c9c1acaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:28 +0000

    whistle wish suggest partner just

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def calculate_work(force, distance):
         return force * distance
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
