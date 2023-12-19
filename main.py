33760316785059cb418b04e014cacfd7b6a4fc1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:49:54 +0000

    meet lead bye eat lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
