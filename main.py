35475e31937d1359a2db087ef3f0c4c9e4800013
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:36 +0000

    comfortable judge one round edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
