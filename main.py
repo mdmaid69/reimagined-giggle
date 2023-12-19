a3ecc1567b6904fd2cc9f6f8c013822e3c992c4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:39:04 +0000

    getback greeting onto date good

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
