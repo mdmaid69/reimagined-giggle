fef6631f9bc83e4ce2cf57f7c5d802147026272d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:45:34 +0000

    science strike radio sure common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_circle(r):
+        return 3.14 * r**2
