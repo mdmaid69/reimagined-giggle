f23738c809b6cf824bc2c8a7c7d86bf6a6174e44
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:47:03 +0000

    when same slice towards reduce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+import sys
+def print_python_version():
+        return sys.version
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
