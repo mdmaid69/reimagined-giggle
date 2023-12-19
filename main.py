dbaa4b6f306197b0cc393c8cf2cc844899bd9bc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:04 +0000

    everything waste improve pinch familiar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
