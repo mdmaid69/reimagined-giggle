76fe4a46b80e7ff39f7ca716946ba7ea085c4fe6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:58 +0000

    impressive deep pipe yourself brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
