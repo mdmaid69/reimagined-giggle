1bd286286aabb451fe1870fe08d5563932afe549
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:17 +0000

    clear computer face drop cap

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
