cc2e3ff83370c288c538ab3201f43b4826f254d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:53:35 +0000

    small move computer agreement standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
