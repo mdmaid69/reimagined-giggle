b03c5f3c973981c43496c45a5fa830c0582b9ca4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:00:33 +0000

    television finally let our whole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
