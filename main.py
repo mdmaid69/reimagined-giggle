c2ec318490bd86416c4d48bfa328adaf2f6eee5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:31 +0000

    forgive object opportunity genuine programme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
