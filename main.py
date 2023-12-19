88d1250e69696880d98678d083aa18224a2f057d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:47 +0000

    morning feeling collect raise warn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def reverse_string(s):
+        return s[::-1]
