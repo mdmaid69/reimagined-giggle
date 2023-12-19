caa39fba8f42d8161b15fdf1f630011dd36f833a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:34 +0000

    basic deserve demonstrate equal practice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
