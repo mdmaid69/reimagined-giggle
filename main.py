ee17480f5865660ec4e5f4c6c29bbb234e4ab3f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:00:06 +0000

    match to worried himself difficulty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
