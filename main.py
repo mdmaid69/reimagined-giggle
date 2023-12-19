4e9827061a538643185b6066cb88a1fa4e9cc224
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:41 +0000

    prevent unkind grateful illness chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
