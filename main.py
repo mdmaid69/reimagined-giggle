5ab6d02b1573823d5bf3dcff9259ac20418313b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:10:33 +0000

    Ww nervous burn development meet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+def get_today_date():
+        return datetime.date.today()
