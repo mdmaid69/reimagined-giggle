1f60dfdbf815ad529122f0f70de0daa6da6e50ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:00:34 +0000

    train dress unable below false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
