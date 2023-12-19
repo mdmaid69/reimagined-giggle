35fec6a8a215a54b7140361edc5241ab4dd8df69
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:56 +0000

    takeout suit myself tidy scale

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def count_elements(iterable):
+        return collections.Counter(iterable)
