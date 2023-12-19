559dfbad10ef431cf11f1800c4fa00416b222761
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:12 +0000

    float question same glass fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
