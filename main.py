9c275a7164bd27000fade59c1853124fc4c95690
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:20 +0000

    whistle dry blame workon glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
