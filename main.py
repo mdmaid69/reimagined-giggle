89c3e22186530d237e543fd7fdbb652c0316fc87
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:54 +0000

    giveaway clean motion charge familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
