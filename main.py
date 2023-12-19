88db357ef8fd0c40b6df0d60b493b702de37c230
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:04:10 +0000

    sit wind trouble sink programme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_speed(distance, time):
+        return distance / time
