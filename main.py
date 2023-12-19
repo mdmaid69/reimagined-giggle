2d76454c7610cf11cb741adfeb2a5535d6046835
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:13 +0000

    Vv fit noise loss by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
