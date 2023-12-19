f40fd30b380d5dd35cb42c80de36b807cb80da0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:30 +0000

    start unless stamp real classroom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
