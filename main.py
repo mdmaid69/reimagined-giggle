7ebef6e120e522386d8bff34f73b9d7ce4caa374
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:53:52 +0000

    typical recently lonely single drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.time()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
