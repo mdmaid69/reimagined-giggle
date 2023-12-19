1c98eeb140d82d37b19b3722f7c0e81141b35cc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:48:36 +0000

    read approve choice guide contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
