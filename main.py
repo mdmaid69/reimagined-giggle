10f2a68c61b36f0e2779e8e50eddb59105c16d50
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:34 +0000

    slight write direct broad complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
