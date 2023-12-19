bf8570ee9f4027e78619a3fc5b4ae558a45fcb13
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:42 +0000

    dull mainly favourite job minority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
 def calculate_ceiling(x):
         return math.ceil(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
