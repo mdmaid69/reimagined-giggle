1b0d6a6756db08a4da9b4768955ab5d98e87cf73
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:16:28 +0000

    crowd smoke step OKorokay spare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-x = 10
-y = 20
-print("Sum:", x + y)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
