1ba48d7e467a32b55c184872a020a4f4c13286b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:05:09 +0000

    march lean cheek clever promise

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
