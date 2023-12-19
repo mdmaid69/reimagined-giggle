02bd32898f2e29d0bbdcaa52ac38d247d64160d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:52 +0000

    concentration instead exit grateful use

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
