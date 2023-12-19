b4e1f540956b5d7ff5c66a3a95bc8b3080891883
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:46 +0000

    trust hand repeat break boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
