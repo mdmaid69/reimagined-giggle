55574792b386951069bda1506487710c05713aeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:28 +0000

    height desperate and comeout high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
