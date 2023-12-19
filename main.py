b25ee550ec3ac63efe55cf2779b6ec00f4479f27
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:01 +0000

    university arrive pipe near need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def is_even(n):
+        return n % 2 == 0
