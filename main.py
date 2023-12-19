e9092e333ce6e83b3a9d92fb386be7667802b2cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:08 +0000

    wonder name interrupt passage butter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
