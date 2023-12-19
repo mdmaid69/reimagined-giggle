6a11fbbc8044ca4cf9a169d6fb1776e2d6ec2f94
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:20 +0000

    out burst bringforward usually pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
