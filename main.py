7200ee992a2bbc910a560a312d3dac944af09baa
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:40 +0000

    guard grand room detail contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
