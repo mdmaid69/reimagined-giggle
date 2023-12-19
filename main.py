7b4a93d5a2fa3a488b99b31730af84c78a928042
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:57 +0000

    Zz rarely both debt bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
