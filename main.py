3e5514aa33eaf881d5e1398ca5a6e588d4606d75
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:57:57 +0000

    excuse draw but responsible want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
