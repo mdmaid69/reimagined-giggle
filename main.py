b77d8c06e9d0e4ffeb85bc04ccfeab0a37e5d2dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:13:29 +0000

    comein chase runinto really type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
