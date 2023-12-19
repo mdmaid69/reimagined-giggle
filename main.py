c3cb5b54dcadd8c2d351d8625f75b566cf440565
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:32 +0000

    wish provide record effort speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
