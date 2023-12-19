025233acec2d70c1ff8a12e9857a05c25339ebb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:27 +0000

    mother argue transparent calmly welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
