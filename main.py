6a8289af98537ba3bf6af2976c101466449bb31b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:18:54 +0000

    help brush sorry decide connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
