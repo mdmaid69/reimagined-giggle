58c36d6d545a7356b5d41a51b3bb1807b943a513
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:21:06 +0000

    sense border fine story taste

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
