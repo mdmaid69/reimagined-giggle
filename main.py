ae32cd24677f7ca69e222e02eeda791444043df2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:21 +0000

    loan book basically blind change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
