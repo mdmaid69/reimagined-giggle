9528551fbc54baf749957de4b15ffd19e477ff7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:01 +0000

    seriously journey almost 573entertainment again

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
