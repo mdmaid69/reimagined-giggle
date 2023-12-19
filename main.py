a77bd63634d4dc1e0e9b15b9c54d662e121e4296
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:49 +0000

    cheque twist cover rest few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
