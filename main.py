619cae588c6a25b082171966f0024b6a6e315ea8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:42 +0000

    could display square takeover brother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
