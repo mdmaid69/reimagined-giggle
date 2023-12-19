f9ff03f20a1fb9fd0631b4d271fb5d9ff62d9c45
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:20 +0000

    ought contact dry nothing arrival

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_power(work, time):
+        return work / time
