c4bf0c44217d5ed62713e16eb283a16cc7420627
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:09:12 +0000

    experiment else test fail everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
