0467054edfc7d688365da13affb6ded44d660d7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:24 +0000

    wonder human meet on ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
