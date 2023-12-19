b63d554fcd12001c20265cfba8a6fa46e5833d3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:43 +0000

    impressed defeat condition business depend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
