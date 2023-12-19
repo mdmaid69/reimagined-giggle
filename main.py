f347a3adc3530693635559c93f7c344cc9e46ede
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:08:09 +0000

    gooff custom supply estimate keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
