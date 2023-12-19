eb96ba5bebf43147cd29680ed065619cd337ec7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:46:15 +0000

    makeup material listenfor fill better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
