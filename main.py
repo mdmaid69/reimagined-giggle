9b3de9bd1233ee996421d80758e20bd4ae339fa9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:52 +0000

    suck comefrom fun divide takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
