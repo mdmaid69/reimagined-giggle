4c44549010ebbea7bbd4769d60fefcba1a9b1afb
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:21 +0000

    opposite contrast book culture rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
