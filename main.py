6e7a02e4cb1c6285828e97651448f86b2e9ff150
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:54:31 +0000

    pretty stiff pause on write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
