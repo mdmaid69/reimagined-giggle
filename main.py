e242f43dcda3c526b6c0b538789f69f54c7a3cff
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:58 +0000

    top warning bush direct not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_user_string():
+        return collections.UserString()
