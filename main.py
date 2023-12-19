58ac738e0fd69d5aea2b3334e0854370333ac4fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:19:21 +0000

    admit unpleasant afternoon name emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
