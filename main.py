954c987d970160ccc2830dc00309701455d16151
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:33 +0000

    goon lean loose easy ear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
