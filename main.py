6eca7d6afe353fd7c6972cc2135eba100d564de9
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:14 +0000

    quantity bus giveup moral call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_work(force, distance):
+        return force * distance
+import math
+print(math.pi)
