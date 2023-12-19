d943349aab2add582bbc5f599ef38208c9600016
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:32 +0000

    better sting look here loose

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def extend_array(array, iterable):
+        array.extend(iterable)
