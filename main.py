e990dc6393976137c782ee05f7a5b09614aa9521
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:58:15 +0000

    stir march Zz whistle nowhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
