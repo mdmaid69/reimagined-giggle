4d838a0687e36b52c93fd6146877cac674ad6f8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:41:38 +0000

    comeoff since recent modern increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
