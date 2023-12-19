cf3df67771641ad73855af770677863967ef5c71
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:29 +0000

    variety hold comfort quit takeout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_item(array, i):
+        return array[i]
