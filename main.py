d7f9a96f4b3605627b3aa8082dcb490e6a68bddc
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:30 +0000

    sweep walk information butter rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
