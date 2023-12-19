adfa01b95c72c7ec823cb0de226983b48b7e8603
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:00:04 +0000

    himself achieve work film joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def find_min(lst):
+        return min(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
