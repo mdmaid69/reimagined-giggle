d7790dbef2303c6509f19d7da623019e2ad816f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:04:05 +0000

    dress brother comein description refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
