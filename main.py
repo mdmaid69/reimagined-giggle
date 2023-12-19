95fbdac21b0c776d84e61683f7d20695ac1c2c28
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:07:50 +0000

    slight 573entertainment our switch health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+def exit_program():
+        sys.exit()
