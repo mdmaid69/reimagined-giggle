d124c947667e449339eae8545bc3a1a7da564ea8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:43:27 +0000

    escape beneath allow sad pressure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
