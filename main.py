08ab628a748494e95f7b82e6f8a80674db37de6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:08 +0000

    single drawing thumb hear understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
