19d9d10bd3a8e59cde19c9b3e48ec6370fb369b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:02 +0000

    hurt law rise matter positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
