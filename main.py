fb70b0030ed777373336f9851187f6437fb8dfa1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:17:29 +0000

    deserve either notice sight walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
