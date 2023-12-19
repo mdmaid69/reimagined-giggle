8b2178b385b377cb08663c9f4d6e536b7f0a85d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:05 +0000

    sad sudden control desperate confused

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import tensorflow as tf
-print(tf.__version__)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
