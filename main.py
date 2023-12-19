0428a6bee8dbe1169f359d137fd35e74c293b038
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:19:42 +0000

    invite Bb treat whichever healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
