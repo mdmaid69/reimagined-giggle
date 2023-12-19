f7eb46dd37f303b7377950959e4df04c0e5aa0ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:23 +0000

    difficulty lose any event nigga

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
