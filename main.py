12238bd81daccd39008714f73df2d318fa3ad8f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:44:11 +0000

    set side apparently sail sister

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
