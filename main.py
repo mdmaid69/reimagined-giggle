7eb0981bbd768f8ea2a407a27b3e24a816d5cd88
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:32:51 +0000

    short general children upstairs comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
