55a483ccaf377b5a3c126efa54db190b0645ee27
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:51 +0000

    excuse beautiful fail earn wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_item(array, i):
+        return array[i]
+def sort_numbers(numbers):
+        return sorted(numbers)
