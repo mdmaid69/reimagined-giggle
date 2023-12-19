dcb477f7f3d82b6e8bb877712e134144104ea03c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:21:27 +0000

    my window well shall normal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+print(datetime.datetime.now())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
