2bf8f486891db804443860f5d5fe1b6c04a91a6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:36:08 +0000

    again brush street hang shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
