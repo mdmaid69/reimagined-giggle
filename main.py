e9e859a6020e0665dcb574f26c38ba87ecacec72
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:38:52 +0000

    lunch reduction muscle wing minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
