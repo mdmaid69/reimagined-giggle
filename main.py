324f557f9c3c2ee116d837bb354b088bfc253746
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:17:18 +0000

    city muscle fit level post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import platform
+def get_os_info():
+        return platform.uname()
