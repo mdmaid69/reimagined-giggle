daa0d3cc1523b62fcf2d9c66afe75d01760a5d30
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:18 +0000

    sure suck across sweet otherwise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
