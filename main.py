a45211548d6289e31219488bf9bfb1671251811e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:16 +0000

    happen rub month pen mend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
