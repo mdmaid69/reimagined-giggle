1a03f748f8bb59cf41fc11ce1cedb49c7565b5f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:40 +0000

    punish switch warning attitude kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
