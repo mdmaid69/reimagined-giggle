630a71d85f8b6b7e53d0a69fc11a103625ac0ea4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:43 +0000

    measure fond slice all like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
