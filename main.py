14520f06896141317ab6aea3ac03171cb00afe74
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:53 +0000

    answer bad under similar save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print(sum(range(10)))
