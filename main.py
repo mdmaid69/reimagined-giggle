261d018ac3d28de2a5e7cb064414379822a8e08e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:52 +0000

    minimum exit concentration storm generally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
