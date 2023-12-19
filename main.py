abd6070e2584fa9f8c5812ee4993c22f666023da
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:09 +0000

    contain ordinary keepfrom improvement proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
