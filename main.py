bcdcbcc66c3c5ead904c9a7a08afe7566e19ba5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:51:51 +0000

    trick light square let splash

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
