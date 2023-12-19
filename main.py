ad3e87b914a229fe2b5a2e8b52c6ee52e9df4dd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:48:34 +0000

    regularly bottom stop perfect 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
