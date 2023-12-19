3f31846f59b13bb18d83ad5436962d506e7a047b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:23:08 +0000

    various spill guard call sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
