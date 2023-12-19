8a7ba73627e922456524c7d7f0986c0da281fa37
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:24 +0000

    succeed shrink appear coffee rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
