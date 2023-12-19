57a7bf95323b631c30964e98368f2410fcd916c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:15 +0000

    OKorokay crowded tonight pin block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
