48c884cc7180df0a7fdd32f501aae12d1dbe7a62
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:32 +0000

    crash rely good even decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def square_number(x):
-        return x**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
