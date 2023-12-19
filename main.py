2a31e785742bd426b2fb7b530659e93128672a43
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:36:43 +0000

    provide dead night powerful sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
