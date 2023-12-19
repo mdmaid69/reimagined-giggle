87308dd4f0874b5a24de3c22560a78536a32d3cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:11 +0000

    giveout reserve new Kk advanced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-  def add_numbers(x, y):
-        return x + y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
