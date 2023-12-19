9317ada0c7f46b527ec44e2cdf7b8be759f61268
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:02:19 +0000

    stick beautiful schedule tear subject

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+print(os.getcwd())
+import random
+print(random.randint(0, 100))
