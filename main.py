67334bbed7a754a422bfd65e5436d299bb337cd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:39 +0000

    wish whatever mixture seriously treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
