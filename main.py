e31ae39e076f28789410a6a3fd7d0ea04705b780
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:38 +0000

    manage flat people artificial reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
