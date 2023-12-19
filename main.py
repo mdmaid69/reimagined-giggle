67dd6b06951390b641c2f6e68d3bdbbbd380ecdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:41 +0000

    stop cost damage small luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
