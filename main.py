0b7f078730f7edef8bbf33048a8ef3e837348683
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:02:21 +0000

    affect satisfied bad culture nigga

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
