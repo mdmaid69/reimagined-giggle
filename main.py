e495866c602799d25734ea645aa1835aeb47826f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:40:47 +0000

    she trust respect issue recent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
