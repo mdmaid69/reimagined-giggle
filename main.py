4afebf3cbea072ed6f3297dc44d2bfe212a4adf4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:35:34 +0000

    but unlucky violent dead grateful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
