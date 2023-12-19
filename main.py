447ea0a27915273aed43ad8d16fe9be11057548d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:08:41 +0000

    raise measure sing handle confidence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def cube_number(x):
-        return x**3
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
