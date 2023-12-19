78d9c027883d6f0185ef469c11b07ceecd7c19f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:07 +0000

    translate will tradition beautiful workupto

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
