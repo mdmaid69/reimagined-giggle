b0e1f97b09550d36e154808de2b629288527b972
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:30 +0000

    over getout steady interfere herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
