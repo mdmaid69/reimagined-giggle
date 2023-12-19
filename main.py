9ede560c925aa873f39f039910266ee644b48052
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:47 +0000

    now permission their getoff strange

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def is_odd(n):
-        return n % 2 != 0
+import sys
+def print_python_version():
+        return sys.version
+for i in range(5):
+        print(i)
