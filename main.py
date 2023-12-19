3fd9359d04c0d3d7033171b3c5945c968c72def9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:58 +0000

    remove set eager sink mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
