402dd75c0c59a5f13616f0eb160537152f869e52
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:06 +0000

    promise trouble front take opposite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
