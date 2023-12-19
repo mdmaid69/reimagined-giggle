799674f2f8792aeab1fba3e00f67f727689a925d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:39 +0000

    actual consist charge importance time

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
