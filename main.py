7e00b951d43882283d22bfc3d8fd98b1e396a5b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:25:47 +0000

    music exciting label house unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
