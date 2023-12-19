d0570a41f1914692108c477382cdfbf3e7ece91d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:38 +0000

    again stick danger surprised takeoff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+print(random.randint(0, 100))
