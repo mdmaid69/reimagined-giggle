fc7fbcd7675fb1f839da0163208103371bd9da9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:20 +0000

    keepoff taste nowhere consult difference

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
