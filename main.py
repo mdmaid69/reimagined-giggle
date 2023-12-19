61a58026dda9806ed46d20a5112f3a40082072fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:41 +0000

    connection control next play plate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
