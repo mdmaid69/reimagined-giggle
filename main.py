e1d972f2a61d1207bf4025b5dd316b674e281610
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:38:40 +0000

    form treat invitation priority shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
