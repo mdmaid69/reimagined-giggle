9a19c5f67f5a5611e49df3317252ede9f033eeb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:56 +0000

    swing goon bag admire kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
