6710fbca778b53acdca69cf22d7024adb4ebef49
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:55 +0000

    accidental memory light side suckme

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
