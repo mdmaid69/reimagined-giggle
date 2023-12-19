3da27707020d0347721eb7c437eca12035e04b90
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:29 +0000

    to another bother roll sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def change_working_directory(path):
+        os.chdir(path)
