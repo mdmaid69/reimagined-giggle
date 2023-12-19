1c332922eb8cf7a9eb9c3e530208c24bf750af29
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:15:09 +0000

    appeal few content giveup week

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
