6de979a361324defd5c9d42587138f5bdd40318d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:47:48 +0000

    warning trial unfair limit reject

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def cube_number(x):
+        return x**3
