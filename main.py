de981be1384aa99e6b162e02842b8e187006f80c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:06 +0000

    innocent its afterwards taste season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def reverse_string(s):
+        return s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
