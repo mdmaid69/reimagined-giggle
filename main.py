d4c0039126b1bc3dbb5f0eb2550b08789837906f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:11:21 +0000

    emphasis try persuade punish worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
