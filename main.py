230f5d21b093225eb774bc06565d3cfcaf605893
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:19 +0000

    tread impression wave breath impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
