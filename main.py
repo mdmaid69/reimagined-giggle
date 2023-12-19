649c3dee53e7f776f4afcb832fec23db4a3087cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:07:53 +0000

    any OKorokay shoulder same bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
