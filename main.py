898799d716a68af20908d3901ee04f7ad1d86380
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:27:02 +0000

    pencil experiment importance crime opportunity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
