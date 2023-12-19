b2991bfcb6f39fce2abc03864c42c816117b0e75
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:51 +0000

    story protest broken develop class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
