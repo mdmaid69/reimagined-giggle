0df662d0a7302f206cb702c92a75228917f288dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:02:03 +0000

    run combine bush grow vote

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def add_numbers(a, b):
+        return a + b
