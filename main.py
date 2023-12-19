11bc41429f602abf342078b76c8b5f22f261639a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:54:44 +0000

    well slow blame backwards appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def add_numbers(x, y):
-        return x + y
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
