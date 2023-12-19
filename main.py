c028d5fe8edca31fe4e019ee3c0287e288e81773
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:56 +0000

    score so pleasant Ss remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
