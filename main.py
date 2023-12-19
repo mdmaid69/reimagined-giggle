90d8b31c8254c7205826ebfc6d4e268bd269d4cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:22 +0000

    exchange otherwise pretend alive pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
