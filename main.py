d968090b1d8d19e84565f6ca108a39c3eff9fc66
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:19:04 +0000

    die demonstrate scrap zero under

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
