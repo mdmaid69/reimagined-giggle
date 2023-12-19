a067eb9529239bba6ef39fd6ada4eb9215ea9c63
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:13 +0000

    chase broad year anxious actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
