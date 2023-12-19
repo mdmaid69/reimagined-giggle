09f4d5c732b9ff10633335da4a7d47a3f347d497
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:25:41 +0000

    566end actually fetch present drawing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+print(os.getcwd())
