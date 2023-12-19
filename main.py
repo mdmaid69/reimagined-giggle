8d462ff75345b44bffc3e70b77aa338d39750913
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:53 +0000

    broken medium which matter visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import platform
-def get_python_version():
-        return platform.python_version()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
