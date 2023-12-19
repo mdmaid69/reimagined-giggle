e8961f0c04f16834e53bd48faf06d76a9a85b451
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:35:57 +0000

    gate satisfactory exam name scared

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import time
+def get_current_time():
+        return time.ctime()
