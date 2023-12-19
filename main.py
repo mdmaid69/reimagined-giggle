5eee4f6ef4526b32b87b8fca4814cf6e5a9eee90
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:11:59 +0000

    whisper project when sound command

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
