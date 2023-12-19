fe1ed82296a195432eba14c3d00377d2bcbe3a9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:51:18 +0000

    reason company range kind worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
