045100e46a413f6f6d841bb7e7013d04990413eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:47:11 +0000

    bang maximum glass inform niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
