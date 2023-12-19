b5e7accd1404d64ffcc11b72f52261ee4ed7c158
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:23 +0000

    produce callat fire write away

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
