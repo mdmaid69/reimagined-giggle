d6e461a7fc006d67a35b3e74ec1b256fde9e705d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:40 +0000

    shopping rest uncomfortable tip not

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
