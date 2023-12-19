bff782bf87cad824fe8af836f4d12c9728e153ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:04 +0000

    Mm holdout anybody equipment rule

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
