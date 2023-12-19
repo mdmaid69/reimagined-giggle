5956c15025151c6581390f4e8fccf155a9d303cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:23 +0000

    that live win scrap me

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
