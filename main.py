4ebf73be0bbaea8daec4382cc287f39407ac4f8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:48:30 +0000

    lean schedule any probably badly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
