9b0c9729215974da5c4f9671e916eb3e38c7040e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:55 +0000

    class perhaps consist easy disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
