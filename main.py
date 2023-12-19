ce186f54c93c09a9e180ba75a7846f5a6d6c809c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:12 +0000

    quality reasonable spread kill travel

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
