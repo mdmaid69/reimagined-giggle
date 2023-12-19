b8da029f7e72f4b0b505e71ec3dff7b19cb3e2e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:36 +0000

    thinkthrough impression release chance money

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
