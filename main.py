28a59b08884cfb381d4a5987bbc7926f220a4d8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:10 +0000

    brave show wash life forward

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def split_path(path):
+        return os.path.split(path)
