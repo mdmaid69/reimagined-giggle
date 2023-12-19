fb8db45b2f4c5453496af71594045abb24432ea3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:00:11 +0000

    she prevent Cc awake accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
