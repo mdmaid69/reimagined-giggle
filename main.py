7c1c7dd47c466aec36f0d2cc02d8a44139dba225
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:45 +0000

    she teach fact broken breakup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
