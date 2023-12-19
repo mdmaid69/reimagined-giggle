c6f3e8170e81702786b877744a63e1a0494921f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:12 +0000

    warning once control relief long

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+print("Hello, world!")
