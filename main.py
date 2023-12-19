e4776b147ac8990e79961fa148b1140969b8d4dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:09:14 +0000

    bag fork unable characteristic make

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
