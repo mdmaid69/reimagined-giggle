61ac9979fdeeadb015a503e0810fb36c6e1f5f5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:40 +0000

    blame instrument bound single never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
