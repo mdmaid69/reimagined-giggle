a34f7851ac6aa02f997096772beffe93c3bc26b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:33 +0000

    swim stone like hardly shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
