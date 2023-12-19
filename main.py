ac14a23cbdb93745ec28ab363b21424694cafac4
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:05:44 +0000

    perfect present regret peaceful permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
