6f66f18970b75ea8b35de85119d3dfea035b66e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:06 +0000

    fly centre grade flow buy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
