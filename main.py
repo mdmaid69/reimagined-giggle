b9b9546e4024389fd44ba4fc5e833cce7ad605ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:24:07 +0000

    handle criticize among calmly bit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
