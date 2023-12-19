5d790bbfd25da5dadf1f43d5c02e13d2b1523750
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:43:46 +0000

    comein away trip usual remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
