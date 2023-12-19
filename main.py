d6ffb7836e26ea15e76b8845832c8dfdd54f1f30
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:17 +0000

    shadow beginning bang cough persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
