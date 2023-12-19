8f23bc88d02ad0a806efe393e1e6917d70dcccc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:25:45 +0000

    alone offend short run course

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
