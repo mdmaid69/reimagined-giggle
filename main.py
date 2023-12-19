13b4f6ddcff8218c1e42f6f7324c7b8b7d4922e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:59 +0000

    top tidy smell suit straight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
