f36328e72b83a09fc1f07a520fad7ed991901aca
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:53:30 +0000

    extremely mark as dance breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
