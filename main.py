6002c53718ff676b32cce59df7423ed522986882
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:34 +0000

    turnon plan content suck expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def reverse_string(s):
+        return s[::-1]
