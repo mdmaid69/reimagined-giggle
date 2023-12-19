346eafc2e70933d16e0e9792fcd3f19a205f5f83
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:32:24 +0000

    into than confused thinkthrough combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
