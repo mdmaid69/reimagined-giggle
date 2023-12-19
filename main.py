29612506f077e546679c1d2dc09f18aba44a127f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:04 +0000

    go smile turn negative should

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
