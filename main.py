cc3b6c3404662e5d7bc528f900d99f5d7c252337
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:39:06 +0000

    practise add reasonable abroad history

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(5):
+        print(i)
