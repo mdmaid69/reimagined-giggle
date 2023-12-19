4b5f7a174318c409af55aa9eb19b95be5e14f763
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:14:31 +0000

    against demand honest sign alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def add_numbers(a, b):
+        return a + b
