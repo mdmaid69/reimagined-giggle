180aeedbd05c05f95d75c6f85ad67e9a59a5168e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:21:01 +0000

    class lucky sick scene too

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def sort_numbers(numbers):
+        return sorted(numbers)
