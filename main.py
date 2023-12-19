7f21496c58bcb2c7f49bbbbc0ea8e5c0e85cc866
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:50:36 +0000

    as trip hold risk nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
