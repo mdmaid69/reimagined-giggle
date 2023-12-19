0a3efac3e7e8e54473d5ee29373c8748bc582561
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:14 +0000

    Ee cap achieve hour contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
