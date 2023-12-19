a2fa1f6d00178927f538a3b77afd9a51237d0117
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:52 +0000

    well declaration keen estimate escape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
