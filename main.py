f43e3dc93800e51d3695d1c0da31981c57b42529
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:07:47 +0000

    common such hurt until raw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
