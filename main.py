a7070e7b371c980ccda9843df1bf1b3698798543
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:37:35 +0000

    repeat protection ahead thumb fault

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def convert_to_binary(n):
+        return bin(n)
