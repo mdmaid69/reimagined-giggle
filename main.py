e01e643513f2a07d49e7913aca601f0ac5355df3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:34 +0000

    replace themselves tune lay practise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
