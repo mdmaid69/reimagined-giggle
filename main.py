9b5b18e1a42fd768103ed8348e710bb72f91a775
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:50 +0000

    forward theirs his violent contact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
