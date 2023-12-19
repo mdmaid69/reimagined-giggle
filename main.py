ebd7de896d9d27b9ab5c7057c5670c1ea71a2f35
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:48:39 +0000

    strong direction action keepaway recent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import sys
-  def get_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
