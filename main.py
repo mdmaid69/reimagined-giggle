62cdf6cd885f4368994e78f8e48dc5aa67254cc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:24 +0000

    nose pronounce vision workon loan

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
