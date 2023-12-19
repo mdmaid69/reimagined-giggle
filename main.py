75bd1cefc8bd302613d5c5ab5c7ffbc1c2e08d6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:26 +0000

    pipe record some port appeal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
