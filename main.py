02a43f919d5bc5120c4fee56a27e20ca0a8cf2b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:10 +0000

    regard task trouble peace natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+print([x**2 for x in range(10)])
