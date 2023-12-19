c328318fff88787beb7bd8d5c581c61ec1d4fc4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:11:29 +0000

    react somewhere sell giveout run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
