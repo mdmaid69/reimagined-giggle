3a4c2b675018e40e2c8f585e645a2c7cceaa3b92
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:10:13 +0000

    wash pleasant nothing refer tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
