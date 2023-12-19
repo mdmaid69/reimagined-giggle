9b8586367d7f8d0988b9b6702d6cb16c4dbc3982
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:00 +0000

    this second prevent really afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import sys
+def exit_program():
+        sys.exit()
