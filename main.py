ff788fae360eae4863ba0104766114ac1671c3c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:58:30 +0000

    straight yes suddenly father clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
