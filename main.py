3975ba31aeb908af38c1333bf0fcda4f172beff9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:56 +0000

    fault treat transport later polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+print(math.pi)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
