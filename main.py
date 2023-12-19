06b3d1d4b361201f92f5c6225b1da19068454c80
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:55 +0000

    region figure nothing sorry thank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
