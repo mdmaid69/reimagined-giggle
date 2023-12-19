0a066c3cdea5de4ff90914c9fe55135853fd5019
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:57 +0000

    male room design hungry war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_acceleration(speed, time):
-        return speed / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
