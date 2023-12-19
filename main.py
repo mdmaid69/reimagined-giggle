aa9160ccacd6970e0b0a0318a551d9359d3e2a44
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:14:55 +0000

    attention excited concerned reduction land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(10): print(i)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
