f12ce407f0a0f4c5be3f1caa7943dc086c196254
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:34 +0000

    179bite birth division cinema repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
