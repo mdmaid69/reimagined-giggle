e823136a06dce4128fb6b1aef533a6bd2db0b28b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:05:42 +0000

    comment ball holiday whenever clear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
