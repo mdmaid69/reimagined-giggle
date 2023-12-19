8978300c3fba4a0557aee2bf7a947d2383ec1eae
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:09 +0000

    pressure preparation convenient great normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def reverse_list(lst):
+        return lst[::-1]
