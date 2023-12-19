8d8484f0929a3afe222c6667f38e8698d09b400b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:21 +0000

    community memory invitation workupto push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_combinations(n, k):
         return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
