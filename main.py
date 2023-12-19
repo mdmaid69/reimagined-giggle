51c30ee50444166db3bdbc79d1eaf89168badf92
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:48 +0000

    sport generally false where mysterious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print("Square numbers:", [x**2 for x in range(n)])
