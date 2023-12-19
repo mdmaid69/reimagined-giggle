584ddf597dfc2e4fedc653ce16a6ae2e3271a7d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:16:45 +0000

    loose reason this careful Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import collections
+def create_counter():
+        return collections.Counter()
