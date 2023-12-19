7e80b5a33ed7a28e1c5fbd70a7570fe9fb1cda92
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:50:30 +0000

    unhappy yours exit dream section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def multiply_numbers(x, y):
+        return x * y
