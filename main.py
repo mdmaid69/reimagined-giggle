24d0a3a9d037d60348a476c6af5cf56f10a8306e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:05 +0000

    all appear off smile few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
