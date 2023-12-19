dbf3d3bfaf9564fb2439522cfd8cd55fcff35f52
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:20:59 +0000

    solve while pupil hit frighten

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
