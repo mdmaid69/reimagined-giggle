9d0f1b00f3866ef948dbc681e1dfcae4c90abe44
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:04 +0000

    address father spend dinner so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def extend_array(array, iterable):
+        array.extend(iterable)
