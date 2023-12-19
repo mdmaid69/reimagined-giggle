929a24f2c51eb12d0b92cb823f992770ee6e20ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:15 +0000

    story bother correct sweep process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
