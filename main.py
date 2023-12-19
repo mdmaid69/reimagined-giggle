51edf75d25724cbf6380f2ec3f4a630c48695eeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:23:22 +0000

    badly suggest obtain grateful purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
