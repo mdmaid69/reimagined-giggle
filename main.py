3958b495b89b668847827f54919738249d0c09fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:35 +0000

    fair loss boat mess much

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
