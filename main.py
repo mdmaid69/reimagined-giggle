ee854eb0cb2835106860a3c4e73e19de8d9415bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:24:06 +0000

    smooth straight play mine divide

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-print("Hello, world!")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
