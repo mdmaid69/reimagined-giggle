a3c68c176d0f186b69fed73b524e5a00f8ae1eee
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:44 +0000

    best however beyond duty OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
