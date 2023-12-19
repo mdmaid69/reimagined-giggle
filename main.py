42eb8f3b1f67a53e18bfa9ecbb2224f10b8a5a98
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:05:08 +0000

    lunch OKorokay now great why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
