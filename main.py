83c6ad204951a9b2af2d0f1090a16639b8d29624
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:02:08 +0000

    rescue town remember rescue proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
