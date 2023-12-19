1ad0a7869f2f3d36e61f9d8de991042e954406b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:56 +0000

    thank after contact breakfast anything

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
