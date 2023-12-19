bb77195baee053a01ff35499fa066e36475fc4ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:30:08 +0000

    people fasten invite relief day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_max(lst):
+        return max(lst)
