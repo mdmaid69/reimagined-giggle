36efb140221ecf307af98624dfe171e891e49f8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:21 +0000

    patience uncertain squeeze exploration offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
