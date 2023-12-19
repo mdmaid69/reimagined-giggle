fe566d1c70bcb36677ddb1384282335acf4ffc4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:22:16 +0000

    nurse tired better hollow whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_power(work, time):
-        return work / time
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
