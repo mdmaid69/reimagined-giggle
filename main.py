0a3b704e26a3acb28e2a1dc66e29a5ceb4a8ef29
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:41 +0000

    odd accident abroad voice professional

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
