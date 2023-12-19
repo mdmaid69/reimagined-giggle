ab54a64f5ee11f748310a1e51ead0b27ab9ab1bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:23:46 +0000

    attempt about interview advanced allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import platform
+def get_os_info():
+        return platform.uname()
