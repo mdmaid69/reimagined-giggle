e95b3b7c0e68b5795ae85a93f8234b49f22920a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:30 +0000

    dark evidence disapprove bang habit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_float(array):
+        return float(array[0])
