716a456385de5499603e3f63dbb0cc1072670984
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:28 +0000

    strike own hers whether meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
