20c850429a202abae0ccbe4d7fb1c897fd13d2c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:21 +0000

    travel feature badly should change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
