1286a8b173992e4265300a7ab9e74007f1e47f7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:45:23 +0000

    pour long unit workon practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def get_array_as_complex(array):
+        return complex(array[0])
