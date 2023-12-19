eeeda6ad4ef5507b8a5a3af88b0474031cc7b9c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:12 +0000

    either some row careless more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
