3f46a1a09e7723ae263f867b8c0a90386b234f9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:33 +0000

    bang wake coffee wrong shock

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
