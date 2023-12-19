9516473e45f1f8fdab3993c6a33868e891a48f6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:58 +0000

    force agreement flat blind repair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
