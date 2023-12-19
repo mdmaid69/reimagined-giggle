7e9cbe8d2e96b6510bb2f4b5db16ba7c4c74fecd
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:03:05 +0000

    keepup treatment OKorokay bright unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
