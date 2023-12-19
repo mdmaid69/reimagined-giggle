cbc420c409fffbc919439ed9db3325d4875a038e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:12 +0000

    mark behaviour file spread law

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def convert_to_hex(n):
-        return hex(n)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
