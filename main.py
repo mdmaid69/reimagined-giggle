0cb6b2f388dc8073eb8827e2c7830f77f9c2a87b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:58 +0000

    between sudden important excuse help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+print(math.pi)
