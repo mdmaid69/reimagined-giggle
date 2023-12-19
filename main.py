9cc644a85a77812cc78638d17fa60ec0ba8d333d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:34 +0000

    smell half in touch could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
