bf7c863aa35455527cc444e34dee314061e857fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:57 +0000

    chase flow admit bottom friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
