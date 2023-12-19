c6d02fc7c94136721bf30b15ec4ec97e6d5baea5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:23:55 +0000

    hollow pour house partner left

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
