2b9745534e7b89465e9234edcd120b81e1412889
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:27 +0000

    dear giveout man television mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_current_directory():
+        return os.getcwd()
