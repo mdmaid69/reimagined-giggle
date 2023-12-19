7bf018a3fdd3d0107bb7dc6ce30d551069880cb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:15:30 +0000

    present giveback runover lift turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
