70229f8d667e710d211b02dc7c3c3440ae21024d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:32 +0000

    more lose battery Cc each

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
