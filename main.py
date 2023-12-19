75909c41414cd1fedc7915cb47a35331a563b46f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:34 +0000

    happen rich general promise proper

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
