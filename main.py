07abdc6dfe098733b1eccbb4517ffdd88a24f451
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:58 +0000

    slice bored force contrast order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
