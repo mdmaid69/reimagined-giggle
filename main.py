08eec65e81b271793842a05ddc52d4277210e7a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:19 +0000

    profit release someone regional geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
