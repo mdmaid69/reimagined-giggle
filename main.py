ad471f49fdc049aa905f799aa47333e8b306fed4
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:53 +0000

    hollow peaceful building sensitive sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
