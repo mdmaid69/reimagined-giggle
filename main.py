736ebcba12a3da2c8f9a23247299ea76978f8389
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:50:19 +0000

    typical print system try standard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
