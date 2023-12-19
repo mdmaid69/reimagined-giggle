8ac1f313d4757954286472648d0a1caf61a22419
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:43:12 +0000

    clue wall creature build information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
