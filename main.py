cd18f10ddc224b0a354d9524a3f272070f174066
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:17 +0000

    runover block slightly suffer she

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
