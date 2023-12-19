f60cd23ec7e515c45e5604504c6a6261ead32f38
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:19:46 +0000

    and development loud load conduct

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
