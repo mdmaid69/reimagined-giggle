1eb7210a1091b0d63202543a2496c5f63049c978
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:47 +0000

    hit emotion after neither interesting

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def count_words(sentence):
-        return len(sentence.split())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
