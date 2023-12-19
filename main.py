705bfbe3166528cc7572fc3543ab8d9fcc3a7d82
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:20 +0000

    old before occur scrape depend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
