a2b4932e8ce2429c55789b2fd176e60b9533e9d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:14:13 +0000

    deaf office exercise compete hope

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import getpass
-def get_username():
-        return getpass.getuser()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
