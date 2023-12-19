9aa414239272f5b668536c4d1b1b6ec0b63c161a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:31 +0000

    drop interrupt pretend certainly leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
