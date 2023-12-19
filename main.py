06aa6b68dd6ed6b7233377774c32bd090cd18b0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:45 +0000

    smooth cruelty comeback negative drive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def is_even(n):
+        return n % 2 == 0
