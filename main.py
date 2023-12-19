bab0f5a50fcc54d5f73856c771a19312544c9bf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:15 +0000

    callat ring production carry up

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def split_path(path):
+        return os.path.split(path)
