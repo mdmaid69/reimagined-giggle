f686678a3fdcd095fd8c28d58007333034874634
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:37:51 +0000

    free mine him tough treat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
