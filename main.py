100dd33de5d1baea515839837ec535c8007b0b77
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:10:01 +0000

    kill recover climb sale other

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
