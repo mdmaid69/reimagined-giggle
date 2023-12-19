f906174c41a77bed70edae1d8bc260332d4380e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:52 +0000

    downstairs cause head respect fire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def cube_number(x):
+        return x**3
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
