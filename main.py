371d92efca600cd7bfb5ec05bf12aa04ea7f385d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:18 +0000

    unite confusing load transparent various

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
