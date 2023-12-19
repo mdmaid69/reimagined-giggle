95bd53c19df9eb5a44c38e09346e42beed49b713
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:39 +0000

    compare guilty wide loss trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
