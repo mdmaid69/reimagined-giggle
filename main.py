c24e0ba1cce29a015a2c3801ab423de795023b59
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:13 +0000

    day urgent material duty vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
