11023a12484d8ab4dc69fe48e9d9aa7c7602c2de
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:03:26 +0000

    interrupt show naturally thumb immediate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def remove_duplicates(lst):
+        return list(set(lst))
