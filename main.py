cc935e28b7bfe0057170d7a2d2c0690340a9030e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:14 +0000

    deaf confuse join cross smell

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
