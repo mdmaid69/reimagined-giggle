8ea583bf3497d5ecbb852dbd7402c1a45595d0e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:08 +0000

    without gift wash behind cruel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_float(array):
+        return float(array[0])
