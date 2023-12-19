2f4c5bbf316c0e508cbb546e081f12f49b70f48c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:00 +0000

    interested programme scare simple common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
