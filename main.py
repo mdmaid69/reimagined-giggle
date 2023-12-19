dc322fd64371c87ef2e051f701422ea46c0b737b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:28 +0000

    lend sometimes gentle entry peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
