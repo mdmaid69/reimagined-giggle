e3121378c1863fd3f9582964275c34211dd0a440
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:08:30 +0000

    dance shine arrival ashamed match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
