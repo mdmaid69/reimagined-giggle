2972dd28faef06c70c31fb9f09335b32972acea1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:13 +0000

    traffic fight Qq race friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def add_numbers(x, y):
+        return x + y
