5bb2613ff6fab2e3c2e180bb543f75ba2b511b8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:20:22 +0000

    born bill soil young issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+print(os.getcwd())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
