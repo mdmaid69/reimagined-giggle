7e7df9d44662f5062ecb135568284de6189c3abe
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:46:05 +0000

    fence mistake stretch but see

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
