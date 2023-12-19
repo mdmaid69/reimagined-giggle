f63f5fc08beb9230926643673cca554c2f6701f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:50 +0000

    tell bag slide lift sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
