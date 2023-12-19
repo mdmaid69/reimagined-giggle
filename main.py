216c8710388430c5a94637c9bdb1a2f8150feb03
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:59:00 +0000

    fear care condition door emphasize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
