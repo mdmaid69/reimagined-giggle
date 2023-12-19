62c0ba3dc8fc7d830982d788b04a05dae7dd00e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:36:39 +0000

    able failure we night straight

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
