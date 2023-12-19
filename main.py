fa96e123e6e8e5a433bfa00df1065f543624408e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:11 +0000

    surprised main practice occasional away

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+text = "Hello, world!"
+print("Characters:", len(text))
+  def remove_duplicates(lst):
+        return list(set(lst))
