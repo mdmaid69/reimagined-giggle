223bade9819e99e2b4391fbcfee33f3062ca46a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:05 +0000

    whisper comedown cap deaf empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
