edfdb9ff8ec9a82aa73fb78b7655e7636de65d7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:17:50 +0000

    increase uponon stick increase majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def get_time_since_epoch():
+        return time.time()
