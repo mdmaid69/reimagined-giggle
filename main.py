b88ec7e017299e528281efbf4d2c2234d9a38c3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:18 +0000

    wrong beginning but command plenty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
