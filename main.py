1e41dbedb63287f273218aa61f540ffc78a3adfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:57:37 +0000

    disturbing might real belief punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_current_working_directory():
+        return os.getcwd()
