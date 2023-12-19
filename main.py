a1617e2504c9201a9f96cddb8d70aa50b1d3da46
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:11:12 +0000

    pass straight neat doubt plate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
