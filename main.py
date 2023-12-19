eaa672cb67d00de8c77a2d4f579a7bdefe4c1e4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:07 +0000

    eager miss smoke unfortunate pleasant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
