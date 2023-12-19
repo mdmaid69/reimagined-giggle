d488e33ae5f96a533c89b2da7aac33961852f370
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:03 +0000

    single border concerned fairly 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
