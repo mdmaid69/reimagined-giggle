1c6f37858f606bb2c302a59de4801b058451646e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:43:50 +0000

    admire allow beat close note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
