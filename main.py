368c3d4fbee2e41dd51b8e15c755dc53d1854d53
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:59 +0000

    custom accordingto spray stage spoil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
