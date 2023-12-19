707b6d5c83f72afeb1d70616735bcd6a254a5a1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:21:57 +0000

    university contribute apart increase brain

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def reverse_list(lst):
+        return lst[::-1]
+  def sort_list(lst):
+        return sorted(lst)
