af8745824459c400f2aef6293c3f1e3ae7d5fdda
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:37:08 +0000

    wonder stretch see task fierce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+print(math.pi)
+import array
+def get_list_from_array(array):
+        return array.tolist()
