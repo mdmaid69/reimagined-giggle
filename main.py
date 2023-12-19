c172e1223854b0153013ec5f9f70fbfad55b9131
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:23 +0000

    own slide accidentally before power

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tensorflow as tf
-print(tf.__version__)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
