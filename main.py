81fc0aabc797d1820f03f5de47cd3a962ccdd266
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:53:38 +0000

    everywhere look turn takeaway advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_words(sentence):
+        return len(sentence.split())
