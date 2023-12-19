f54a3d9e2447e9ad10acdb23116ccb6708eb923d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:00 +0000

    walk value relax sore on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_tangent(x):
+        return math.tan(x)
