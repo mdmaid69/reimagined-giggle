45d6fbd1ab07894e372bbbac79f2dcbc92accd00
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:06 +0000

    each repair mine jump wish

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
