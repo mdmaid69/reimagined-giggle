9d715f6b58172b4cd5402dc3d2662c850d7a7928
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:51 +0000

    cold runover description practical season

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_list_from_array(array):
+        return array.tolist()
