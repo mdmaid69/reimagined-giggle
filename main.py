f591678c51a14d18b681331e2e5fed72ee8808cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:26:23 +0000

    conduct promise habit often emotion

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
