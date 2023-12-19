1e98656b3e5fac8c54b97eb8774db28fc3ba48c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:55:50 +0000

    reaction real Nn complicated threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
