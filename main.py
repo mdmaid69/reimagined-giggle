e20128490eba5a0e6cc0d67309fb2ca247415fec
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:19:56 +0000

    hand capital description play bin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_area(radius):
+        return 3.14 * radius * radius
