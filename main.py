d1c371ee7adc316d0b2a0ab879aea2b7acbe4b79
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:58:30 +0000

    bored black kill type goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
