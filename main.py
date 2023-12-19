b5d098efc7190f362fbb748d7f24c2f6889be136
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:53 +0000

    trick impression workon unpleasant dull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
