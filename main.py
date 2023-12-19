f25c82f7dc8d4f8c23fd415cef3e1ec78daeda87
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:59 +0000

    myself within turn who wealth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
