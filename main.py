173ee53d577375e617fbfa227e4b5f4a888e9cd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:13 +0000

    roll realize insist coat lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def calculate_median(arr):
+        return np.median(arr)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
