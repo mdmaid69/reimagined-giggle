4c3e78eac6a132896832315e63d63e906b3b03bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:24:37 +0000

    side workout spot solve come

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
