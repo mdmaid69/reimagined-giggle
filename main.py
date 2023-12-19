325caf7deb5507db89533c935e56587081b05715
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:55:51 +0000

    goback(to) typical fresh slide weather

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
