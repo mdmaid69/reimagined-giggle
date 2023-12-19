0bfb6ee59aaebb166499f45642f8af96e2157a69
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:36:08 +0000

    temperature couple development tip before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
