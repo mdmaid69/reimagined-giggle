d073ae99c988efb517e63ef4d9dba4c309b1874d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:08 +0000

    plenty joke effort broad run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def split_path(path):
+        return os.path.split(path)
