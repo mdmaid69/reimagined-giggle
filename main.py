18dda825a577fb0589e0919e4a3d38b37af927ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:04 +0000

    serve depend begin shop straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
