69c2b135990af7948cfe57212916b69a073eee77
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:52:08 +0000

    depend previous occasion ground giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
