32039a7bef81daa7538423f0b446846b197c2a54
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:52:47 +0000

    lift scare thought makeinto closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
