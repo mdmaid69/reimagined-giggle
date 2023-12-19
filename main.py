a95f38586dcf5648f0bf708d52864da659460dc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:45:13 +0000

    pipe flat coat one weekend

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
