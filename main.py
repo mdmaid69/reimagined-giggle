45c73ff8c1d0e5fba44b260ddda0bce8576c7076
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:40 +0000

    entry cope callback go goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
