5e262856170edd9812e9dda7c008b281b568db1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:57 +0000

    anywayanyhow Itself event skirt ability

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
