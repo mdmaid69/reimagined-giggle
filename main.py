486218be0c946aa1928e44adbf5739bd7fb793dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:51:13 +0000

    late sign kind task view

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+print(sum(range(10)))
