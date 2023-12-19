ecd9dff01beaa942495db3475b06c8f602eff0c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:46 +0000

    development big grow search who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
