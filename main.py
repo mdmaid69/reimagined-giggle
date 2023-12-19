588856bfe451bda4ae1b922fe7cc0d75e0f3a702
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:39 +0000

    work formal blame closely ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
