5750dd370bf7a1d7190b9c20271c5754d0c1a7e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:37 +0000

    ground announce 565end beside march

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
