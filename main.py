cfc6fd013e3dd3220b4002b505d94d73f41249e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:40:39 +0000

    until sort own actually separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+print(sum(range(10)))
