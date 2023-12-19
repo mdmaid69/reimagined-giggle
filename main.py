aad6ab46cac11b742b8cbe9073d77f3c1a6b9b48
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:51 +0000

    reserve bush purpose bag somehow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
