4ee2671d73580b359f7ab8e71e45cd96559bd5d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:34:48 +0000

    tape busy male who extremely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
   def get_file_device(file_name):
         return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
