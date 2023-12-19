4df1d6bf518ae47bbc6cf939570a01b133c1f3f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:08:04 +0000

    above calloff easy enthusiastic uncertain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
