3b0764a6a6b844d7bdfd69dceb1def0eacb468a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:48:44 +0000

    fire criticize makeup impressed pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
