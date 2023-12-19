53c3bc3b1b37a8d1c11288bf78283521f3d91c07
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:01:38 +0000

    evening used proud cold guide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def remove_duplicates(lst):
+        return list(set(lst))
