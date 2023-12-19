6690da19cb0e87f74ddcbb5e30388820209c4ece
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:58 +0000

    community pure idea voice rob

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
