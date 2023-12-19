bf54f791c7e0bf038cf24f42f0cf4ea598729e93
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:11 +0000

    song light fun guide famous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
