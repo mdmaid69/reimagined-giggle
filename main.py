14c54b16cc3cd4b78aa26f9b73504f5aa3f606bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:42 +0000

    platform takeaway recovery phone dress

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
