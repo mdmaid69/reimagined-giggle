398f83587cb02164b5357e9489f9c75186121951
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:35 +0000

    in related already interfere rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import random
+print(random.randint(0, 100))
