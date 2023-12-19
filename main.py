12814367b01da671e62aeb84d8634ccbb39880f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:39 +0000

    lead forever need control yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
