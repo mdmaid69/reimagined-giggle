24d13e617063f2b1b0087406e347429518bd3e7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:21 +0000

    further warning mean slide noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
