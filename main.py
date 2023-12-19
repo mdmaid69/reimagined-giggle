766d574b9f414e43d909612ce91a3de105453317
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:01 +0000

    problem programme closed future doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
