385e763c5c2f82f0788af4b9e3fc6e06d6f461b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:55 +0000

    content persuade hard serious choose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def add_numbers(x, y):
+        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
