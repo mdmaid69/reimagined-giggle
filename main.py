b44e1d644ecedfce8fa7d437853520c58922431e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:47:08 +0000

    small zero around nothing print

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
