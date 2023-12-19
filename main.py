79bfb80052d71b64da0b2c639d6a5959debf35d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:46:54 +0000

    couple Uu front greeting search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
