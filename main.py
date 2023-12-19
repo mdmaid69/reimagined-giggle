b64267601436270153e78febcbb2b2498b510b4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:04:37 +0000

    pollution ago suit head turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
