0aacc0487e16279eb91c2f5d5ae18968afd300ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:50 +0000

    justice full rain last fit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
