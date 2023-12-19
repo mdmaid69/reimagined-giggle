319b402ee1f877bb649c8e766dc4b3e6748315a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:51 +0000

    match trick set win direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
