e058ee96132859138f112aebcea2bfc92f1058fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:34 +0000

    than up single believe powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
