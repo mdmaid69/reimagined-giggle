0d5c013371c72f120f9cd12ac529d81172cb6fc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:36:59 +0000

    opponent chase night our brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
