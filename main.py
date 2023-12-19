a6ce8156fe51595632373dde29b3f4488afb2f41
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:23 +0000

    ride passenger instruction settle start

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
