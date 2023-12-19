891a985d6d482b27e7fd130dd2b8aa6de56bc64c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:08 +0000

    pressure unable board cheat influence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
