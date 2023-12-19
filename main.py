c0d627c8f15c9e6f41fb3280970edc455242ca48
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:49:31 +0000

    Qq hour goaway warning inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
