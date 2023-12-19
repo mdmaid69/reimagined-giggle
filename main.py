f85f2d922313b64a02365189cbce40974c8a2aff
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:34:20 +0000

    smart science congratulations Vv close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
