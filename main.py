29e081c7e1f1e507b098722e60e3e4b549d3ff7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:47 +0000

    price national round persuade favourite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+print(re.match("h.*o", "hello world"))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
