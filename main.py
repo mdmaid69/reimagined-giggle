6ed0412231cbc650abd19bdcedeef5c7ac4d39b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:48 +0000

    whole through fun grade 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
