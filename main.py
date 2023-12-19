03e77be0141aabac9b045e7f7a23d508dd44535e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:07 +0000

    recently solve explanation forever after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
+def calculate_power(work, time):
+        return work / time
