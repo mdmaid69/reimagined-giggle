303441a6d47d67a472a53145fc0510f0d3e5c720
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:11 +0000

    motion influence sense wherever learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
