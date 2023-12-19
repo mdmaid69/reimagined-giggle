036ba0b67cc7ffca1ffcd2b85bde1c81d8b172de
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:01 +0000

    increase floor defend support defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
