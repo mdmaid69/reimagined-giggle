68c89be228f395f537ba440cebac15bf4e48e7ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:19 +0000

    direct plenty mystery spite remain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+for i in range(10): print(i)
