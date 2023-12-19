5bbce231ef6c000c8e05ee3b412525604d4cadd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:18 +0000

    schedule false yours line turnout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
