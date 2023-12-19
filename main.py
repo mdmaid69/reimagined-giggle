1b44af63b72d837dd691b392a09e94d6e887f361
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:29 +0000

    better run minimum cough runinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
