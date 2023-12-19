555d9062c1bfa182930cfa5d518c9c1242ab81d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:41:50 +0000

    deliver against dick heat force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+print("Hello, world!")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
