1a69d54243cdcbbdf09ef09959c0fa76b541ada6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:04 +0000

    wide pity image exam firm

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_density(mass, volume):
-        return mass / volume
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
