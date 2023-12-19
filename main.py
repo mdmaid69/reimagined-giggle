9054e813f4a39b9d5f0ab28cf2b3fd7c578437ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:38:30 +0000

    blame story knock science sound

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_octal(n):
+        return oct(n)
