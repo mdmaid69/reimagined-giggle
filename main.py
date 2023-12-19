48198b59b3c7812b1b407c1f933033931223382f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:52:14 +0000

    entrance question second imagine step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
