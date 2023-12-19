e62151b36d463b805fcd11bc42979aa3c4f6a8a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:27:23 +0000

    busy accidental man politics poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
