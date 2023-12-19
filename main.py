2f9775d41883fcc2424ddad86398cfbd93269186
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:44:39 +0000

    friend free centimetre apart towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
