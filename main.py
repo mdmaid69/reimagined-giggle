ebf592877f0a799a377bf60d1eb5b96dd3e6253a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:17:50 +0000

    accidental clever kick ocean across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_directory_name(path):
+        return os.path.dirname(path)
