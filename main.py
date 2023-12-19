975db651aed1472425ea07bc9a186347929cfa7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:29 +0000

    as bill wear hope trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
