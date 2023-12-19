c6be9b2eebea59711fd60d3c9bd7216f034396a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:11 +0000

    570enjoyable speak too stay slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
