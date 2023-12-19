7c4da4c2c9f42059e259bbb71f3bdeba4370949c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:03 +0000

    deep discovery interested instruction confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
