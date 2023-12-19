501c25cbd79500f8c9249c2a3d57594746eb1aa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:30:09 +0000

    aware image label Mm finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_stack():
+        return collections.deque()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
