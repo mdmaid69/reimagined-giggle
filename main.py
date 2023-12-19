b4a76f347f991af3574b12698717bef6bb9c18f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:23 +0000

    speed whichever battery just bath

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_power(work, time):
-        return work / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
