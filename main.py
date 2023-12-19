f87b4aafb4977bd5cce15f5bf79bf8627ee5c18a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:09 +0000

    environment choose blow bottom break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
