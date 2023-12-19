22154d1ab036c24381b186ddb0ee6d4747339803
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:59:52 +0000

    central table extremely Oo snow

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
