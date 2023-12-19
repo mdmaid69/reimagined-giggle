26aabdc2eb25e49ac8fb31069d4a2955b1511d20
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:48:44 +0000

    loss temperature miss stage well

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
