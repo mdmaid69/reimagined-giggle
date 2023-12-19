e029aa679b2ba460bc8b8dc1d66edc7ee604480e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:25 +0000

    burn like content shout aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
