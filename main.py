bb39282178e61e7c4d1c2cfd0774d68393d16686
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:20:03 +0000

    fortunately artificial minimum cost save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def count_elements(lst):
+        return len(lst)
