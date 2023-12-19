b25330d01f9eaf6c42c364a37254576c48e9f428
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:56:02 +0000

    cold influence software flavour switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
