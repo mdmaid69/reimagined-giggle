6be394e1c23f7b723fc6ab83b1c9d3edd520bcfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:00 +0000

    effect married choice on shopping

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
