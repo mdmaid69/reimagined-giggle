75f6a6bd249831b58d527ca9294578ed348f4aeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:11 +0000

    gooff day thirsty arrival mouth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
+def remove_duplicates(lst):
+        return list(set(lst))
