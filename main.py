7a5ea00c0687cb463a50bb9ed1576ac65dc7768d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:26 +0000

    honest occasional room music politics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
