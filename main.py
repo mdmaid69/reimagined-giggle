42c94443f00ad67a97e2de15ba00bbc4db95a8d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:32 +0000

    nervous forever beat summer estimate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+def calculate_work(force, distance):
+        return force * distance
