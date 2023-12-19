6a72490a598ee67bf59e65786e28e9eaf2a4db9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:02:20 +0000

    throughout quiet punishment hold crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
