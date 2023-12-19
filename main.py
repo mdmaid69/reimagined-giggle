2bcb464fb8b3117fefb769c8c55c3fee7d04403a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:57 +0000

    cool disapprove once exact quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
