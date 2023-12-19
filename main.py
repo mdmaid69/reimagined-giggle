6e83e475fde848609e0143322e2158e52d1bdc33
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:37 +0000

    direct tongue suckme there worse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def add_numbers(a, b):
+        return a + b
