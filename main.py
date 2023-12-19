f5ca3f77d5ffcdb6dfeaa8287a7eed84f55e8fdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:16 +0000

    sick school aware rush properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
