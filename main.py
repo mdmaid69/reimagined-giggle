c1e1301fc4fce1443bb7cd5e10898976d9818d6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:00:27 +0000

    defeat about positive scale downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def divide_numbers(x, y):
+        return x / y
