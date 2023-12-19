94b75eea45501876aa64a5e2ab963e3795a53df3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:44 +0000

    beneath speed independent embarrassed accommodation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
