8171619c60c5132a2b4cdf5827447a33d892abb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:13 +0000

    rub programme honest as who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_unique_words(sentence):
+        return set(sentence.split())
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
