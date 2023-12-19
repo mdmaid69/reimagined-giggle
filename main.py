b9d6bf5e848d28dbabb42557c999fd007286f9a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:11 +0000

    also market cross against report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
