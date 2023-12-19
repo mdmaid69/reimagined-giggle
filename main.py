7b1f8d629179d6c4fa0a577838c91a5997655cf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:09 +0000

    knowledge interfere bite takeback time

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-print(math.pi)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
