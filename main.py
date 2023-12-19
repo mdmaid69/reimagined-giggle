d425198c4d87b26e6ee761cd8fdeb8dcf38e72ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:53:27 +0000

    amount study total in reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-text = "Hello, world!"
-print("Characters:", len(text))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
