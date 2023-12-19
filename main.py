76b9a0b7a279e3c494593d35c8d6f68a90e4d5e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:23:21 +0000

    juice meaning buy sweep by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
