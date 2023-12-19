e056f43c4104d944d070095adc4b588a7a830c86
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:16:42 +0000

    brown rather left operate rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
