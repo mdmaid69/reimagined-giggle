a5c52aa72ff649deb06011cd04fa53ed09860919
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:11:42 +0000

    suckme by move floor yes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
