885b6600f32c8eeccdca45f166830c46873749d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:10 +0000

    good mystery container disappear regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
