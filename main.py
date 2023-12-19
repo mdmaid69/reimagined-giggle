9be8269f521cf62eec13938d8cc5bd0abf276f81
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:44 +0000

    generally probably turnback earth roll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
