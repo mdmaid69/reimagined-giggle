44b3a5661843e1171c0ad3026eade5eb8520af04
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:40 +0000

    towards Ll respect bin like

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+name = "Python"
+print("Hello,", name)
