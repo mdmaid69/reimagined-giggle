1d8add7bfb0515bc46055aafcb891e5ca43b312f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:15:32 +0000

    still takeover lie dark but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
