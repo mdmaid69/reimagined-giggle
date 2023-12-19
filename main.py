d7722058a9e0b992a90d999f3969232c290e8b4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:52:52 +0000

    job clever smell damp aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import os
+print(os.getcwd())
