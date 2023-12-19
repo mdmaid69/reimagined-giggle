2ba12881df580139728cfbc570eef9734b54e623
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:59 +0000

    possibly makeinto charge lucky in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
