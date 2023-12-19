682d4f6674fe690daeca3cd28a109c0a99ab1fc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:53:41 +0000

    firm correct crazy involve formal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
