76e2030a9acbdbacb4812bd89f728cee1c01c415
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:31:29 +0000

    wave bringup before interrupt mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
