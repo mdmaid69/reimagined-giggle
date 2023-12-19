a44e30e951b2d0668d137f13fd793bf5c06eb74b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:38 +0000

    takeaway exact car side necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_string_from_array(array):
+        return array.tobytes()
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
