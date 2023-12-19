6cdeca37b5897558509e6948a1ad671596379227
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:22 +0000

    carry through ground advice address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
