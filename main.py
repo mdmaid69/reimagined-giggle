bf66fc812bfb64607acee909b870e621d5b95bcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:10 +0000

    all fork level minute cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
