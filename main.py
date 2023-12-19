3954eb927b614be6591a19d7f79d6f9fd9484572
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:05:36 +0000

    thinkover except thirsty already failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_palindrome(s):
+        return s == s[::-1]
