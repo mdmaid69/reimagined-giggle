056bd628a506f23ed800ff0f289ff7dc299e7c74
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:25 +0000

    thing unlucky opposite science nonsense

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def cube_number(x):
+        return x**3
