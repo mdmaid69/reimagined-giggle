71054055bc51c81e936d83e2dc16cc3882975078
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:44 +0000

    turnoff entry country computer heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def get_current_time():
+        return time.ctime()
