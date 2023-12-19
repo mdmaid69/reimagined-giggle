fcf336c15ff3a3f9beb0948a558641f9a922fe43
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:06 +0000

    obvious used few clothes effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
+import sys
+def print_python_version():
+        return sys.version
