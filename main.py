ddda319419dca9809f53898438d86444bf92fa72
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:48:24 +0000

    face dish poor flavour never

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
