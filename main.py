6b043c867adc55d6d62f1b4d824aef6b37b2db66
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:17 +0000

    situation left abroad bend seem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def count_elements(lst):
+        return len(lst)
+  def count_elements(lst):
+        return len(lst)
