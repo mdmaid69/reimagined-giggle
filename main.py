af669759c808b11c12ef86e51f666deb0eadc205
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:07:07 +0000

    stiff thankyou fit shop undo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
