0ef260c225f99c53e48fee8bd6b55a3f43c970e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:49 +0000

    hospital none fun because have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
