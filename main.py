26d8d29ecdaaacca840df2b1857e707fa50b03ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:43:00 +0000

    base remark hole get shoot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
