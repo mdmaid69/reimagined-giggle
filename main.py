3025c248d2d43f4c2d29d003e64884aa483ce76d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:04 +0000

    within mail when train female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
