7df30bf34f5ffd2abf086584469a0bd6f0ff9d93
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:59:52 +0000

    petrol laugh convenient record borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
