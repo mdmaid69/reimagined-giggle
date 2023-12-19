efcf85430311dd0e0e4290a287311335044514ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:19:11 +0000

    refer state sort seem reaction

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+print(sum(range(10)))
