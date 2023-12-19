04a4f2dda4387f300e0707ca3732fe1c4b4b909b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:21 +0000

    no in aim depend terrible

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
