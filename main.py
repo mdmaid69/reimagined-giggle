0194d8750f58639fc0a47f8f9db6f9dc51b37704
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:29 +0000

    turnover equal here whoever angry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
