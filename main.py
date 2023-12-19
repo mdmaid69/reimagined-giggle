f1f99aa2b39b766e1aeb9fce3fa24c8523f29e66
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:36:26 +0000

    search callout interview no wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def find_max(numbers):
-        return max(numbers)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
