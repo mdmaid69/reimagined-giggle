31a3da58c43d76857f07dd04eff85513d4360e47
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:02 +0000

    recent average offence turnup about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
