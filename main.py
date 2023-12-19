427fbb443499f74ef008a3bc4d30f79607e9881c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:32:43 +0000

    callout train clue pull frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
