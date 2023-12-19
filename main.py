64990608388da956f53fbbc9ead00152d4049015
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:25 +0000

    hollow want government save fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
