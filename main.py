ca9b05d4cbc2acd3ec8e47c058b6d13c1738185e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:12:03 +0000

    design brother impressed bar transport

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
