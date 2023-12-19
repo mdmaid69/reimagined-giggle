ef467a716c9742b7bf6bb638fd2e2737c5f69118
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:57 +0000

    put choice decide intelligent date

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_itemsize(array):
+        return array.itemsize
