dbce7c9ba2a73ff05e68f7c50852d0a1c8eaeb50
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:50 +0000

    wild serious beside guess waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
