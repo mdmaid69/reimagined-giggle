908d636a29a559cfd5cb235c57e1a23947b89e0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:52 +0000

    getup load sale scheme child

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import random
+def roll_die():
+        return random.randint(1, 6)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
