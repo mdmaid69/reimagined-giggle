80de0be470f7666fae9db377db1f4b27c63ea770
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:29 +0000

    spin comment able spill beautiful

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import datetime
-print(datetime.datetime.now())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
