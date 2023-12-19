367d1042c84f436641b60afd9007c98b92ecfc05
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:09 +0000

    long past exhibition whisper right

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
