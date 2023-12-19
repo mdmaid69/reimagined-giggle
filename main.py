534afac28d2ea4105f772b9e391e29889bc619f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:04 +0000

    adjective total team gradually forgive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
