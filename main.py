106b347a14ff00fcb87812fb4067c71d9aeadd48
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:17 +0000

    object common measure gap birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def find_min(lst):
+        return min(lst)
