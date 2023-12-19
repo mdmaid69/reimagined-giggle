04d097fa437479dfdbeffb2708dd10b2384c349b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:39 +0000

    emphasize winter strength crowd mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
