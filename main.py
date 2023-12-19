2d8bc0394c528978d81025b384f4667933384874
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:10:04 +0000

    pretty left fail fairly drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+from collections import Counter
+print(Counter("hello world"))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
