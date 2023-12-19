5e081c6ad7bb8d30ea02f6759f6307961398c0c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:23 +0000

    over breakinto dead month bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
