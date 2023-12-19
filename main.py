c7e2adfa9eafec6ddbd45fc637481dfe1033adf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:20:30 +0000

    thought under ashamed seat unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
