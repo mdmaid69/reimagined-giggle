99f373cd850c7d05ad7f0c1c8a85ab8c16891a27
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:57 +0000

    bank still definite maintain minimum

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
