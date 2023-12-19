3c037ee7b8e88f0473b4b4fca73c998da49c5b2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:23 +0000

    reduction passenger steal present waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
