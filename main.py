f993fa8e6fc9ff29ce87ac24d2efb1923ad1c903
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:50 +0000

    comein hungry sensitive present guide

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print("Hello, world!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
