9d47e2cf58c21ea1df1ba625a469bf70e5f735c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:00 +0000

    there shall action half neck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
