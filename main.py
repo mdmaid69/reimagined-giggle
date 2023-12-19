025dc46bfa3a46fb5cb05a0407ca44f233dc3e5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:53 +0000

    meaning loud public usually possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
