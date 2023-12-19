26a9ce9a5b3ca5a5a849075a9e80c1e3e446bd6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:02:15 +0000

    film tap report meanwhile generous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
