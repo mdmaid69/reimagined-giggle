d3b9b74d9385272b6bcda1bc521d935faa2b0c83
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:19 +0000

    mail callup load various below

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
