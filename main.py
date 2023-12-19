4c171fd6182959580ab7b6ce3cf14234cb51f668
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:05:47 +0000

    emphasis by hope play mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
