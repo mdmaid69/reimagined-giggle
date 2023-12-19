cfab37d123ae114d67b6bffc04c872c7d7316869
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:38 +0000

    my equal swallow reply involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
