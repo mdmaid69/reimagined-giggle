8475d61a260a19b37decfd6d95ecb8999c534516
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:43 +0000

    issue write rub disappear object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_current_working_directory():
+        return os.getcwd()
