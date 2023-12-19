8cac0429888ccbc415679d72a42a9d5e21b316e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:23 +0000

    proud refer old manner personal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def clear_array(array):
+        array *= 0
+print([x**2 for x in range(10)])
