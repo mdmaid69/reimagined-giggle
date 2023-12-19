e395fc1b841ed2e7cef3db8c2ee4ea1b0b5caad8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:30 +0000

    woman tend talk land definite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
