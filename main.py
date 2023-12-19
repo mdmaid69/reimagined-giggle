e6d97fc3222fa5f0e0b731ef240ff02ec2a6add2
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:24:28 +0000

    store Gg ring ought calm

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
