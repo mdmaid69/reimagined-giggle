f56398eb227f963978537216d0c82a450a6e5263
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:07:19 +0000

    passenger operation direct cheat principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
