2417851d82377103d0bc43613d2efc1d5c684b3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:02 +0000

    crime anybody excuse music bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def convert_to_hex(n):
+        return hex(n)
