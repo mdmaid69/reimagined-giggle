4b65b1d704b16ed2d1169da847341b2dc9f0f826
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:50 +0000

    pupil healthy whole first food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
