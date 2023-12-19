6ae9399cf665f761fefc05a373e4f63d39025a0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:50 +0000

    lean exchange class bringup practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def square_number(x):
+        return x**2
