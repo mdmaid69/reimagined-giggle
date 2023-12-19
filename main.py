f600f9fd918134fee03701a037cd3b6568274204
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:55 +0000

    unless process borrow tear statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
