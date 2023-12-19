0fc87af2d7c6be73b8b0b60aa8505b636efb2bb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:04 +0000

    direct glass stop claim if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
