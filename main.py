3ea01714dedb1e0b45db66a50ff12144be1dfd7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:34 +0000

    right both its confident angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import time
+def get_time_since_epoch():
+        return time.time()
