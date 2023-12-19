d164784c6cd4067a2a8a94b1742318713fa8f169
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:49:58 +0000

    project justice trouble simple big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
