db0bbbb6a42b4a32d57a96503a7ea470493803a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:00:16 +0000

    sea off distance intend turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
