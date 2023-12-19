b096d4a1a3393f970948b9a57e878e57825fc397
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:39 +0000

    become pull tie what partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
