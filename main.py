1e3965272bcfbef18f7768e19fa0c37ab7037b1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:15 +0000

    getback good speed win choose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
