be955b6b45f64430b30e692772af6be59b65f547
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:47 +0000

    safety 570enjoyable content opponent embarrassing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
