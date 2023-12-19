1a5073a77bf80769fc94b8ced3bde2d98d07de2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:26 +0000

    possession spend guilty impossible regard

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_bool(array):
+        return bool(array)
