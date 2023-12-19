6644b24c3012569b15f4287b34905ae1360c84d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:15 +0000

    or behind outside hope worse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
