22265248c639834f358206b3eb8aecfbb1610f6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:22 +0000

    sign regularly fashion support Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
