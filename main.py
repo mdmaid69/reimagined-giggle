6afffba1a8f272172c3206673100197806c99e03
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:35:08 +0000

    either guess embarrassed however prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
