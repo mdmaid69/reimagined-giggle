e102d2a751c0926d0424352bca7e1becd4cd5a18
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:25 +0000

    wide mess free nurse emotion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
