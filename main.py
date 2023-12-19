b8d1ddf853ffb45077496bca6613a8c28b91f2f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:05:52 +0000

    smooth police practise fold love

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+import collections
+def create_user_list():
+        return collections.UserList()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
