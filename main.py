8d822d22f2fb5a0549a4db4f7eddc45aae8e9269
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:09 +0000

    subject birthday reply safety sink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
