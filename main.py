abe6b50e07373c23bb69a4bed2cbbd5a47605788
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:16:46 +0000

    no calm test land Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_power(work, time):
+        return work / time
