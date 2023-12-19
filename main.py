187ad44ed6dc5f5d2eba64afb0cb878512c3ef35
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:41 +0000

    criticism trick occasional different nor

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
