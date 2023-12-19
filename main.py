53aae4ef7b58854f33ca1e18b7917ad9378a41cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:14 +0000

    town computer interview something watch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+print([x**2 for x in range(10)])
