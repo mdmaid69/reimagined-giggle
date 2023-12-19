1a84b221eee87c7573389fb8833857af159f4582
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:39 +0000

    single film prevent figure case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def sort_numbers(numbers):
+        return sorted(numbers)
