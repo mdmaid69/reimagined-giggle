bba83f70a634a4c7b36048087b879282908b2bc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:33 +0000

    fond giveback use wait substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
