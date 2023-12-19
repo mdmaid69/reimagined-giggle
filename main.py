0044c20a0c8a4d28a157bc840a032dc344bc12bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:06:05 +0000

    thankyou slight train possible sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
