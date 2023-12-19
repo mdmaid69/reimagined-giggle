1ab7ddaf89e4c7ed874894270d2e2fb5e078d5d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:06:35 +0000

    in secret importance discover birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
