30d97ba29db41219bf825333c88d1dd1c5f955d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:35:41 +0000

    heat dig know repair temperature

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 def is_prime(n):
-        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
+                if n % i == 0:
                 return False
         return True
