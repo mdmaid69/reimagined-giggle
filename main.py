278e03ee1fdef18558cbe338847c4c80fa2bbb44
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:14:55 +0000

    exit careless him lead right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
