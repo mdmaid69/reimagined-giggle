23b268f5d16c63160ff392222f8ddd0d6cb9aeef
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:23 +0000

    meanwhile together runaway small share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
