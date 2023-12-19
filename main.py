6a6bb3b69279f490b1116c31f5024aba3536832c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:24 +0000

    behaviour gooff steer look day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
