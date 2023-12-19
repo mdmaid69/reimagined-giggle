8a17cab1651eeff45f3d643d5b036d4d94707a33
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:48:17 +0000

    issue by trade feed order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
