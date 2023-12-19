cf973c5bed333449203a275189dfd9c78ed11b75
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:50 +0000

    fairly value hospital classroom matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
