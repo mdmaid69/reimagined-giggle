ce5cc6ce55ab44c86cea2959018e9da9fd65ec91
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:24 +0000

    concentration long both sign why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_hex(n):
+        return hex(n)
