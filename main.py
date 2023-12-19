88b215d349c1f922d6140b7561bb2f34436d0aee
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:46:33 +0000

    touch hate on cover able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
