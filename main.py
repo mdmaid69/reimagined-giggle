046a29aa9e59830afe2af4db3a69ede10846febf
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:43:09 +0000

    spot disappointed tell drop swing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
