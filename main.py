2385d7b90a1f024e11a7939bedd87e64e11ea70e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:14 +0000

    react region simple slide free

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
