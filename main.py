322705486f5431f2f36d46e4dc39de61b8c30e28
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:40:50 +0000

    unfortunate purpose worry sister obtain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
+def square_number(x):
+        return x**2
