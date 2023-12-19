a0dbf83f6325277b26ec4155d1531158eb9ec121
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:45 +0000

    ahead down move adjective shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def add_numbers(a, b):
+        return a + b
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
