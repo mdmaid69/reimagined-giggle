48cc679ec735fbafcc7c2ce20f8e4a02d9b3d202
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:24:39 +0000

    bridge even again draw laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
