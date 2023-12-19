8819ca3314f9310563fd773838221942f721f33e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:02 +0000

    substance rest careful second fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
