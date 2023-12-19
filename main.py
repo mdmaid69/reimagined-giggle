b82d7d3f7a81d701acb055b8fe18afdeb3317720
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:28:21 +0000

    cough wind have lookup shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
