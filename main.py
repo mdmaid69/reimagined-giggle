0eaa5aae47bef38525d0c3e8036a56e641e76cc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:53:03 +0000

    law straight agree condition apart

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
