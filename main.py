723daa6fc475dfd57ac65120d62b5033cc2686cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:00:02 +0000

    common joke duty copy cry

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
