c002d45fd7fa4421fa3a8e6bdf6cf38eab48cdd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:43:43 +0000

    priority sport treat towards create

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
