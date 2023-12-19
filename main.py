f16bb9734036d00c0a2f88b5a58b467a328dae6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:31:56 +0000

    interfere gift callout belief cheap

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
