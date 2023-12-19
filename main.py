d3fb17df140d7db45d798f0e177b95970dda5cc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:53 +0000

    cry themselves refuse content basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
