07625587395c8c11bffec777f26874fe9647518a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:47:47 +0000

    with my operation track congratulate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_speed(distance, time):
-        return distance / time
+import time
+def get_current_time():
+        return time.time()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
