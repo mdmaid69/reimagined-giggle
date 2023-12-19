9a5ef2f8e1cefc3cc58a0f2822f9b0a55ce06289
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:15:25 +0000

    succeed regular confident figure juice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import numpy as np
-print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
