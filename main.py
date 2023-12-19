d9ea1de9a95e3a6a4359d10d7a9e989878a433b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:19 +0000

    fence mystery getoff exist dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
