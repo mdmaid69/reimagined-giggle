797c832eadc8b49ad3d02d9768ed462eb0be8efd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:17:42 +0000

    medium warning steam flight to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_length(array):
-        return len(array)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def extend_array(array, iterable):
+        array.extend(iterable)
