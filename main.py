cb81b150ca01d9c9479c57c0cf6d188346a211ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:12:02 +0000

    contact the hollow uponon lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
