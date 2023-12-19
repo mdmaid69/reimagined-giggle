42900e0762a40f83e3b6b630fd49681678deec7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:05 +0000

    trap any hospital round twist

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
