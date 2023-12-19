1dd91b7aeaef95f04930e59a70765f4c846bc1cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:09:46 +0000

    sport print dress kill help

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
