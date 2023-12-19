0d8fece6d58f66d10bf65d7a891f2ca14a611318
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:49 +0000

    comeup miss guess obvious past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
