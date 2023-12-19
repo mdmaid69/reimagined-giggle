add410a1bdfb3a86150998311b1e5154e7525185
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:01 +0000

    curious square organize central age

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
