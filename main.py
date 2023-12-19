918ef55286c428f144bc331a7f2d2b8293b1949c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:39:02 +0000

    appearance lookup bet sore dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
