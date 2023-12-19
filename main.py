ac76829042181499399bd597a798d5261f301c99
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:56 +0000

    crisis memory part sink anything

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
