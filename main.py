cb3cc936ba16328ff30a9572489e5e9b5c324f00
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:06 +0000

    note vote spite better maximum

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_typecode(array):
+        return array.typecode
