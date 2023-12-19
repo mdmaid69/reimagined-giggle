04fb7e850018e0ed1078c8731ad5b6c14e9c49e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:58 +0000

    leg our store government model

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def count_elements(lst):
+        return len(lst)
