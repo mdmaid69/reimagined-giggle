a001a57d7718edd690baf7295150b83fcbef5a86
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:05 +0000

    anything fault science exciting flavour

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
