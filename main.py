6e8767157529ce749b6a7dfd0307ca8762352c87
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:36 +0000

    tip too characteristic notice expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
