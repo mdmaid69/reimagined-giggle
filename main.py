c3bbafaf0760a335f905d8f1b219a2b9327893cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:45 +0000

    over chin approve sport hard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import platform
+def get_os_info():
+        return platform.uname()
