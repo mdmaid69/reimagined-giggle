f83dd8981148f8e13708be590d851c2cbeab1977
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:51:10 +0000

    during service joke remain increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
