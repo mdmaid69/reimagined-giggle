36fc54d7bb85599fdbd76014987dcbbe74671454
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:02 +0000

    dry turninto paper high university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
