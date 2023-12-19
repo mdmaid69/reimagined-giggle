64730af766468a789bc594ecbaf86effa2c145d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:40 +0000

    second 569enjoy black belief fashion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def subtract_numbers(x, y):
-        return x - y
+def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
