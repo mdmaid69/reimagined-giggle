0f1b236cb183b21a868206df1012e64646b35b19
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:47:27 +0000

    exist Pp call next complaint

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
