8e028c5bf5a59d5ec2ecf7972e55421b3abc1c85
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:21 +0000

    measure underneath bring possibility dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
