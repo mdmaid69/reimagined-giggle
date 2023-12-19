57e598f8d4a0fa4ef5bdb3f60ddabb642cea67b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:01 +0000

    flood emphasize laugh laugh worse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+x = 10
+y = 20
+print("Sum:", x + y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
