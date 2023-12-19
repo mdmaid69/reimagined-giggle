1a39192445bf6cd00acafe7864ac936728c06ba3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:58 +0000

    spin convincing Yy always pretty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
