e1704014d932c5d90701249c49b84f7f65f20846
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:20 +0000

    nerve loud by help Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import sys
+def print_python_version():
+        return sys.version
