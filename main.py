7f1deaa54138aa20e470db6e07281654efe0c1bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:10 +0000

    position practical shallow innocent tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
