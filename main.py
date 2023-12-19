367563a7baeff476722523d025d2b2ee42a50164
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:44 +0000

    busy protest full turnout survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
