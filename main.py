8b96429db29e95c17187c96090c76169b196b340
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:07:59 +0000

    sting mainly blame splash politics

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
