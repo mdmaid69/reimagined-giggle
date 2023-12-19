4fe0919885cd0bf4bf2ebfd9909ebaab9d3a6c57
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:15:18 +0000

    use before determined base violence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
