a7f3484197ac8d1ab634704759f33c43b5e63cca
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:07 +0000

    progress short perfect confusing chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
