d6bd62cf639b7decbf3327e46a42a0ad38a47f53
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:22 +0000

    label communicate left effect sudden

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
