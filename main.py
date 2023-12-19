ff74b93efe998b1bb9e3e13f3e38444e996e7529
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:48 +0000

    people exchange keepoff clean downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
