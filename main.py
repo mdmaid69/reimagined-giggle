3f46ef77a96c9a579912e0fc3067e27696f1d189
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:20:46 +0000

    line dig own serious either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
