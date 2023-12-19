4197d0b4636417175309e60eb435f372551ad04f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:48:19 +0000

    neat man match argument surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
