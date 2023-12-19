54bb649534231b494bc94cd0717d3e9777b17d47
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:24:38 +0000

    reduce software agreement marry cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
