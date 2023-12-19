9c808f93a6e0b10a30968a150cd3471062cbeb94
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:44:12 +0000

    standard make hot taste slight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
