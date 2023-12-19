8b648273e16a04cfbb35efeaa411694b837f97f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:05 +0000

    means complain goback(to) abroad opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
