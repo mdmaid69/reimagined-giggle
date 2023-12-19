f86966cc62376ba99cb2ea03b2a974ca012e8476
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:19:56 +0000

    round split choose central anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
