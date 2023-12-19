c0187a3e289481234f7bb5f2576145105cb9f11f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:34:59 +0000

    convinced seriously escape direction object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_acceleration(speed, time):
+        return speed / time
