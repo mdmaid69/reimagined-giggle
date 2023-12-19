62bc66d618f9f11a42601bb7c7cd5ca5b966c5fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:40:48 +0000

    famous payment shop nonsense left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
