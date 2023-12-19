931c6c6887bd55b735c9d60ef0ab69f65cc5d488
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:48 +0000

    correct its snow basic about

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
