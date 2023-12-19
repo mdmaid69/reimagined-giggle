f696d8a5b084f227fe079f51018f39a4932bd6fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:09 +0000

    slice reduce cool popular gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
   def change_current_working_directory(dir_name):
         os.chdir(dir_name)
