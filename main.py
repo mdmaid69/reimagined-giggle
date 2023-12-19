d3df1cb847a8a85447dc08ac93d563304e3b509a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:46 +0000

    want should compare bank under

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def cube_number(x):
+        return x**3
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
