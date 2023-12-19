4eec963858f0cb4e9782bc891520b255e41427fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:38:29 +0000

    hope miss whisper copy goaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
