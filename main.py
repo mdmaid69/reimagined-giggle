1987a2e18c00a8a3304c099146a8b2a546ff777b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:45:28 +0000

    at illness Rr promise open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
