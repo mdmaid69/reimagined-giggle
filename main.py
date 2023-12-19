c5fb538a9f2323dfae6993ce846ec57ff34c80ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:15:10 +0000

    dream coat just love shout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
