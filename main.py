4f0067d7fb870263dcf5e136f8cb56d75bb7fc43
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:27 +0000

    compare perhaps colour dear equipment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
