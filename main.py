b8cdd62991e537ddaf339dab4fbc9715e54574db
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:22 +0000

    special risk swallow pain over

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
