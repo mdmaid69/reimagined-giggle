6491a104f342421eff48ef93400f0e1fd25e6b83
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:43:46 +0000

    fire offence limit go bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
