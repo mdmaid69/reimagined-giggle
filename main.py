f89bfa1ad5da884b47c6756553a82b5842bbea0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:06 +0000

    famous curious person friendly compare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import sys
+def exit_program():
+        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
