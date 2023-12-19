dd4f65228d08d89868ccf168d6472c47efc9f367
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:09 +0000

    may work comeover less invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
