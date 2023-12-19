6e0a6814fdb4ec8d8c0c1749b3679913d73ed78a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:45 +0000

    brave only dangerous turnaway display

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
