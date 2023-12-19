9127b0291d2a93c16c22a8b1d46671427cd42796
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:53 +0000

    same search trap half contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+for i in range(5):
+        print(i)
