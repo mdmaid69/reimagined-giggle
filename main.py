d89ea8dc3dfecd5f93a2aff5146844a133f75347
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:13 +0000

    contact accommodation rest relationship personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
