b495370e50291b39fd25b2a87a4eaea8ff5bf186
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:05:05 +0000

    comment agree city cheap control

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
