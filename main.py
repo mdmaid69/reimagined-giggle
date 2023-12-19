83b2a5e632d2e09a511fb09b50f15fa922ebd46b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:16:23 +0000

    test tap off regard across

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
