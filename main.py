7d773736dc48f7d2d536be73abfcd0164fd4e5d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:46 +0000

    swing vote sharp him interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
