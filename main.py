93c402972f10b04c05446e9d4a2e7b6a64cf41b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:41 +0000

    shout scale cheap remember brain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
