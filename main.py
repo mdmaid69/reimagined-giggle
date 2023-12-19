4b59c3c3cdf022a7081bbafb1fd4c90f526dd910
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:34:23 +0000

    impressive cheque clue false course

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
