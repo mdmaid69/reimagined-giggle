43e8c390344a188560e3182f2a120120de157124
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:22 +0000

    behind down smoke polite before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_length(array):
+        return len(array)
