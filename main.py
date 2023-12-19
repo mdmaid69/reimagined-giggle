cfa5da9bb4d0a9fff59ad07ad9eb718127cc7e3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:43:03 +0000

    light keepup left drive almost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
