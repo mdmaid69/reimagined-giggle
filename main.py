7c3d857a46224a967570befc51de86bd92a9aa91
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:30 +0000

    return sight turninto stare half

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
