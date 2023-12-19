9d37816aad4d454280fff0c2bdf6d64f556fd9ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:06 +0000

    hit sting repeat goback(to) freeze

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+text = "Hello, world!"
+print("Words:", len(text.split()))
