e1626621ac0059fd324493a330af4e540e296f16
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:54 +0000

    opportunity uncertain try hope hang

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def remove_duplicates(lst):
+        return list(set(lst))
