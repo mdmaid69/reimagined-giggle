d42bdd4f909c6a3e5949dcc4e3cd4d4a6c174c02
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:49 +0000

    tie risk congratulations cheque dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
