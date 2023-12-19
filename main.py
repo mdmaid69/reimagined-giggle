e8e684ca177cecdcc2aaa4fc01b736d424308c8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:29 +0000

    guess maximum violence contact attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
