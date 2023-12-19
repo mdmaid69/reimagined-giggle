eea185950cc1df16c3da4882b1c9dc2c6d7df75d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:33 +0000

    free yourself perhaps store seriously

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
