ac67d1c986bef5316818df155ef9dec7672401f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:49:48 +0000

    thirsty tip bringdown emotion hard

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+print(math.pi)
