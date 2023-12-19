aa53ac4954312bd078ea7de924b2569d9a08cfe2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:53:51 +0000

    whether spite sweet class active

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
