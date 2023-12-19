caea3d5b6c918ef4fc0156420261d127d056b619
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:46 +0000

    band tight fierce mix field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
