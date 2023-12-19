4afa0b231701bab494feb6e8b0d63909cbc0554f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:47 +0000

    government bite club platform forgive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
