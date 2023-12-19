5f22d4277404d79ea9b11147857585b6f2cd48a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:32:08 +0000

    machine nothing permission bin badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import sys
+print(sys.version)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
