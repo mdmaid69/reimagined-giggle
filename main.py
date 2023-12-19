b4f02a517f1a83e021446dd6cc4bb36b1a80155f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:18 +0000

    mess impatient married imagine carry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def split_path(path):
+        return os.path.split(path)
