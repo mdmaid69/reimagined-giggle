c5e33a1bfd9c4ca6db4d96007574a149f1e561eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:42 +0000

    born strong believe lower bill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
