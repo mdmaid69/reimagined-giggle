e2cf68de5bd35143e7cf87d8b6e00c1b75acf24d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:09:05 +0000

    cross makeup perform means correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_area(radius):
+        return 3.14 * radius * radius
