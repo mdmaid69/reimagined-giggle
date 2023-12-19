d38a9de818e6c0cc74e36e6acb1af2726a8b1828
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:24 +0000

    separate loud education community wonder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
