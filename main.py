4b3cd9b86c2948cd2aa252683ebf4ca759e57854
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:16 +0000

    hers stay exchange Zz cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
