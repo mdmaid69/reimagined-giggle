f3026914091c4a050936b19a70352950ec7e558d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:42 +0000

    Ww brother keepoff past circle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
