27dceaad4dea0716d04006d52441dc59f27c7660
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:35:29 +0000

    commit noise tear accommodation season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
