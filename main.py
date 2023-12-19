6ce83f7b71bba064e48b0934f584a0c95bc72b20
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:49 +0000

    comfortable towards sensitive case situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-print(sum(range(10)))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
