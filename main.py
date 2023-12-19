239bdfe2226dd8f92bdb4c7a5e14b3ad08827bbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:51:29 +0000

    OKorokay worry play material sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
