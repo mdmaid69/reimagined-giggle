3b77b9917ddff446f22126e05f0dd76153147125
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:36 +0000

    to angry activity buy influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_array(array):
+        array.reverse()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
