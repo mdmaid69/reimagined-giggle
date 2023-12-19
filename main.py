1a45e1b0e5210a6f03aaba4df310c443fac67657
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:25 +0000

    lucky artificial protest relax far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def square_number(x):
+        return x**2
