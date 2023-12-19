c46df77909c6c0c267fb191026552b84292fe86b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:33:50 +0000

    walk name rich yesterday Kk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
