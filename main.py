8ad99857edf8065e8810628a888702b1c80954b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:33 +0000

    string base Ll scare cruelty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def square_number(x):
+        return x**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
