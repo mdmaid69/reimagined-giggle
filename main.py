b8110ba9611591d65e33ec72ace80019d0c645cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:36 +0000

    bad unfortunate there only experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
