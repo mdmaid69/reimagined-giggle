a10b1525abce8e60c110917e5d8b288c417e4291
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:35:14 +0000

    ask curious muscle downstairs I

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
