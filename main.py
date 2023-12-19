9244da5820623df437b6b94f89a2c16ab2b6adef
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:27:13 +0000

    advice post journey circle condition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
