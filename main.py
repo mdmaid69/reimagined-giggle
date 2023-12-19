c2ee1faed68ea38bcd0386af6a1121a988ae7dfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:22:04 +0000

    home makeinto region sticky awake

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import re
-print(re.match("h.*o", "hello world"))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
