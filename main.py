374f338eb7dd0ae6af544490255630afd3c07888
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:44 +0000

    must convincing book agreement promise

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
