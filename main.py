4382db377b9f9d637a20eb348c792c3a9e1ea7d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:20 +0000

    communicate pool school opinion bringup

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
