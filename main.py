14609134635109497b9b89d4a9ed863a7a263ccf
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:01 +0000

    interest last process present between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
