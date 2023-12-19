6fc16e1aeac87796bf86a429aa300ecb35a34353
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:59 +0000

    close as pin help there

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def extend_array(array, iterable):
+        array.extend(iterable)
