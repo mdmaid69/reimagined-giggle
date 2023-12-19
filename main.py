4487213a45731b9337e7048bcf63deac5eb95597
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:32:38 +0000

    fast prove cool operation such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_directory():
+        return os.getcwd()
