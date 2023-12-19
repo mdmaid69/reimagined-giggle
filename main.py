947955d39aa43047fff27bb4adc2d410e8da1f2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:41 +0000

    wonder goaway alive minor tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
