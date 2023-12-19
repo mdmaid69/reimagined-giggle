49a5a1ee73fc8567c6185c0b42b0e2fae6865766
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:45:27 +0000

    mark remember ahead low not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def roll_die():
+        return random.randint(1, 6)
