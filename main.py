fbb0d89705801d34ab981b7bb9794577c980b897
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:04 +0000

    swimming think list fortunately spill

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
