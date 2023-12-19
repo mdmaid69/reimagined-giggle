23ff0ce3bf6bfe5912dffc4e31bd23613526f3d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:38 +0000

    only brother school something difficulty

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
