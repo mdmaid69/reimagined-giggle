8022f3e4a00fc0b2e4542a302959a767aade72c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:44 +0000

    book spray cap conduct study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
