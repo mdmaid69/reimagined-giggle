e36f84e130deff8c94e68a4c356258758af1b84b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:47 +0000

    rich Dd no mainly drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
