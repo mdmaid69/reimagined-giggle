a9021cef14897ab200d93f81b943c20fb5811cec
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:28:37 +0000

    juice break patient charge getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
