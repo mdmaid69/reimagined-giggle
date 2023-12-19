6545cd73a74885d6839c3c69e7f8ce404fb37163
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:10 +0000

    holiday fit flat watch race

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
