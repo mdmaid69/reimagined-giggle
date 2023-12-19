e938a1c2af9ef271177f86184e38e3df36b7b786
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:42 +0000

    drive thought behaviour moral because

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
