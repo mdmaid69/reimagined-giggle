f5a1d8d0e3ec295b94b343092ccac8fe8dd1d163
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:20 +0000

    spoil woman gather consult they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import time
+print(time.time())
