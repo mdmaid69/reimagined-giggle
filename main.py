9bd0bae9edc189d4ceb417eddae08547c5998bdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:01 +0000

    list between blow environment method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
