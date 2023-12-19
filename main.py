8b1a0062000ee1947f7b9ce5093e15a546b586db
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:08:48 +0000

    but reach characteristic find funny

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
