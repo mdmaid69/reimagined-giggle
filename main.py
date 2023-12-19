632ea93ba6a3281d5c155fe55f41060f89f8bdde
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:21 +0000

    change drop mean confidence friendly

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def is_odd(n):
-        return n % 2 != 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
