fcd73a48315c29f752473dd7c0a52441ee1df833
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:52 +0000

    tragedy run noise calm Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
