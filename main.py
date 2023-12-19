e0b6ce928319f48b0a5cf399bda3c9bea111d177
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:13 +0000

    blue pretty when steady test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
