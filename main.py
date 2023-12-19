1d61d272150053f4e3543e1e46320c643abd1916
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:46 +0000

    uponon pupil profit centimetre love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
