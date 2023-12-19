316ccfcd9297932da17c98c3148f5abfcd8fa454
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:42:21 +0000

    copy edge people hear unit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
