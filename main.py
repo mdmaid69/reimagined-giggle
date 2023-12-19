f0cd50f70938ccdea86b4e24c4bb1db494d10165
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:43 +0000

    match blame stamp receive bush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
