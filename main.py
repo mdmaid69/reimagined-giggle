d6d4b1f8a598e222d15c013a3861b172aa73603b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:14 +0000

    old familiar figure whenever string

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def count_elements(lst):
-        return len(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
