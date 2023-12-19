6b0f69bc5ba8f66842c0aa7a4a8b895e41aecdfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:42 +0000

    speed wise name effort flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
