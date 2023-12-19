c247be97e1355d98c51815a1f6f026cb4afaab7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:13 +0000

    treatment bank side disturbing information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
