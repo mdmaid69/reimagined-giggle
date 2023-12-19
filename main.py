5c3d18308bde74912f4f50cd1fd58989a6ce1ea0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:39 +0000

    firm leave come recommend takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
