6fc16f279cff81f6601b64eca6c6b59036b0bad4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:18 +0000

    exchange party greeting what heat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
