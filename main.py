8a39deb1008f260e3c1b3444a97ce295a34dd177
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:36:15 +0000

    trust violence road price shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_acceleration(speed, time):
-        return speed / time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
