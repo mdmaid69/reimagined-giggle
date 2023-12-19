c9008d077ee58d3dbcf286a74c23ed413cd8a2c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:35:26 +0000

    far nature match breathe limit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
