7b9dae6157f8bb36f71dd86f23459323c5e5fb5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:44 +0000

    worst beneath correct now wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
