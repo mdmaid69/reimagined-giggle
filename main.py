52dc3adaebf73b63789743a4691ccf8527a841d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:03 +0000

    swallow seem reduction comeover sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
