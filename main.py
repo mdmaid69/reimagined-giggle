97fcfba619488e55050ae3a4fa2578ce842a60c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:20:09 +0000

    free shout receive fear attractive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
