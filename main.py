c7e096aed7a2b09915f86006cbc12fce111876ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:46 +0000

    close error most yes one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
