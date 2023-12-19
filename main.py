fdd6d321a281f4020f1c49bb745c0e9b0291a856
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:19 +0000

    solve change takeback comedown demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
