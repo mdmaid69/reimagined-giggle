2102db4a9aa4f3b93ff37d641aec34fee1410738
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:04 +0000

    importance task anywayanyhow hit neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
