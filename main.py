5fae8f0e6895f3e1f397ce60deb5ae090092a1ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:36 +0000

    work finally geton loud prove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
