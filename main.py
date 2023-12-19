0b304ca7c65f7b6dc1484edb40c6ba4e9187d364
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:03:10 +0000

    practise sound day gradually deliberately

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
