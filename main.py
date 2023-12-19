d2919f52414a8b17447fd4e2339d70b85b226b2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:18 +0000

    history belong remark heavy cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_str(array):
-        return str(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
