d226b8f400aaa0745db8d680204f9ee55d9f8454
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:47:03 +0000

    blow put closely hold side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   def add_numbers(x, y):
         return x + y
