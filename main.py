712231c69f951184aa35ae9db7104bcbeb5fc0e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:05:01 +0000

    plant hit detail stay from

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_set(array):
+        return set(array)
