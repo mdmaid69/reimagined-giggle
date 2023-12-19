d911c36de0b172a13a020d323cb9180b00ab3fcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:29 +0000

    arm love near forget sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
