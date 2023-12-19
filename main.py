cba3c09665a7c832cc75baba561c63a92db98e52
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:00:05 +0000

    release avoid deliver them taste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
