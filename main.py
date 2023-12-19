37fd641dd6c8411273bb459b753f0adcfa78ae30
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:12 +0000

    display week disappointed trousers runaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_area_rectangle(l, w):
+        return l * w
+import sys
+def exit_program():
+        sys.exit()
