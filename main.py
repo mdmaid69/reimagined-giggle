e046a506997b208b22cb3099460823bab99c1a73
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:23 +0000

    choose likely female tongue shape

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
