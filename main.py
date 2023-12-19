59996b39e35ce206994dba859e45036fe9b6327b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:12 +0000

    emotion shame company another above

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
