f8f0e2b6640b4f4e3d4dcb1f4fef722e7d647f29
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:33 +0000

    work themselves OKorokay talkoutof attractive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def count_elements(lst):
-        return len(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+text = "Hello, world!"
+print("Characters:", len(text))
