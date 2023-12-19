4ac1fd67370b43e70f63be1edf2cf85b0ee560dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:14 +0000

    deserve place support continue sticky

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_set(array):
-        return set(array)
+print(os.getcwd())
