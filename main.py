b2da98b40da48024a3f6922578a3eb1f87d9bb49
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:42 +0000

    arrival benefit instruction fun perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
