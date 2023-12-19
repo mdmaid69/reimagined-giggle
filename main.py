475cf973cc65e30c1e7b3e4cc86d58b4b15f4b42
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:58 +0000

    star lie shopping themselves completely

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_current_directory():
+        return os.getcwd()
