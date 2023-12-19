6f72e3ec46b491c8a73e24cc85dbf6defd01ac7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:05:06 +0000

    in curious record nonsense surprised

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
