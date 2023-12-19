30b7fe84f94814fcf8c2fdbbec20577f6d905058
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:08:53 +0000

    moment exercise wet react need

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import itertools
 print(list(itertools.permutations([1, 2, 3])))
