3108e0fc15d6077dae191293dadfa457a89810f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:57 +0000

    long sticky release least he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
