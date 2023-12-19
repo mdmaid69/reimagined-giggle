a74bf19621eee7e68d4cdbbe0286de42881637da
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:01:23 +0000

    design drive even towards spite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print("Hello, world!")
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
