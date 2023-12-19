ac7e7d09d818b2287d50b7c256f722f0ad6937a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:42 +0000

    patience settle available breathe breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
