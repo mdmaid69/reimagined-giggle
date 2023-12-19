7827073f899a26a8dde2d6fb71e24babfde53b49
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:11 +0000

    damage round show famous conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def add_numbers(x, y):
-        return x + y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
