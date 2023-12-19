ee39d99d7a4f39290e0f87e5724cefa2bb8512bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:18 +0000

    mass worth among rid find

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
