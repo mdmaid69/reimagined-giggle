5602de512c1c8df704114504986ea8054611acf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:49:52 +0000

    base law object tradition train

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
