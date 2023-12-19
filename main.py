b39c9ec3d1b6260ada36635f11151de68f1fbac7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:58 +0000

    belief unhappy inform suit hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
