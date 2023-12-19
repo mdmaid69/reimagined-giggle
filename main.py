8fe0aad6548731d9214fd9bcc998b98c12057206
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:13 +0000

    if left mood turnup innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
