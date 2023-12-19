ded689e004c41f1241914bba30f423f153e56902
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:17 +0000

    difficulty uniform serious worried turnup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
