245e8a5514dd2f9ac196059442622198a0ee5990
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:43 +0000

    first deal lonely strike autumn

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
