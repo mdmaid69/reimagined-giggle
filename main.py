3eac3932c10c6519bd1dd75958d6d16c58b9467d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:33 +0000

    broken temperature section often breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
