16e88697d38b435da304171fd7dcbacfd205cbc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:48:20 +0000

    which best scrape this true

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_directory():
+        return os.getcwd()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
