59895ddff8e84b71805db5c1eb125110f0d57632
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:57 +0000

    understand bus bang check piece

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def convert_to_octal(n):
+        return oct(n)
