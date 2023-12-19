19914d4076d1cee559a43e7fec1437fc76ee77c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:07 +0000

    border table jump tone apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
