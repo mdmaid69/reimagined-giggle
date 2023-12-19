7621262acd2631340d349996e4776b2b71f3c54e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:52 +0000

    storm he during fun I

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
