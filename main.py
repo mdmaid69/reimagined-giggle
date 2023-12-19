d291436a2ff9aa895fada09563278fb99c1a337e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:16:33 +0000

    somebody area wave special blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
