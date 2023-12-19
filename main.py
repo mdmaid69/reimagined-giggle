3295938897b2a323e0c204d73e2190cb185a3c2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:29 +0000

    ground relationship spread listenfor boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def clear_array(array):
+        array *= 0
