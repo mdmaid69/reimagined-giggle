d169175d012c3f7a1cf332837893d21099b48da8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:17 +0000

    impossible winter eager special indeed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
