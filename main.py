d99bc9c3a2a74f03e8d5335013afc5061cf3cf25
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:31 +0000

    temperature doubt clue her set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import re
+print(re.match("h.*o", "hello world"))
