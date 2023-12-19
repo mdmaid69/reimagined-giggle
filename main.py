4ac8c358d1f9261b7414f885dd06f6725c112733
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:28:29 +0000

    friendly argument defend badly contrast

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
