b78eca02de557bf3d95e89a5b08b3f8d43383ba4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:46 +0000

    strange calmly exactly shout reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
