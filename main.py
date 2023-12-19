14fa81cbf646cd7e7342668e3b9e50a74ad8de49
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:47:32 +0000

    rank skin dream star spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
