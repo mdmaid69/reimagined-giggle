176d944607b7c3ad8fd3c68290ee7af53d476d6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:54 +0000

    top might trousers include reason

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def add_numbers(x, y):
+        return x + y
