a739b328022fe4a703c6be249699ff5995d22afc
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:24:28 +0000

    warn compare listen obtain money

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def subtract_numbers(x, y):
+        return x - y
