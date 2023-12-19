1f5790db802f4d8043914d34f2ac8e264cf860ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:27 +0000

    beat main real match count

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def get_array_length(array):
+        return len(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
