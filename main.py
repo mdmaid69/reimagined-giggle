3e6ca261506c427407e42ef829aa6c63ad7ee2ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:59:37 +0000

    Mm common week order just

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
