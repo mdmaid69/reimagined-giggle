b01a5e2cb4882dea999796032b4900a1f0942bf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:34:30 +0000

    control bar concerned clear sister

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
