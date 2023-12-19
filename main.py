d10852c5f9894ee9b9a02ba95905f579f31da8c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:47:07 +0000

    fashion left freedom usually OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import re
+print(re.match("h.*o", "hello world"))
