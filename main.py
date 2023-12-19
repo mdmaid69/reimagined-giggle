604cf2ba387b31ad1aca36759bf978f956a81b14
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:17 +0000

    wipe target sell anywhere cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
