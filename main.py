0eb42942bd30aca77df463b7c46d9cf2ba7f72ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:10 +0000

    length luck bother change best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
