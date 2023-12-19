fece03520ae32407a427021b21b2772387b207c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:24 +0000

    finally married spray mark characteristic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
