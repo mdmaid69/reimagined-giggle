b618572437a53f99b6191512e02f834300596fae
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:21 +0000

    disappear medium race consult process

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
