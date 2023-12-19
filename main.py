144807ba4a397f48840fd1a2733f25296f96aeda
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:30 +0000

    judge thin protest certain game

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
