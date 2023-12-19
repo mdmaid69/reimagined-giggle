9b0af29c826643d7b7029d1fc203810d9fedf3cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:28:10 +0000

    valuable identify stand test tongue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
