c509d5fd289d486609b52805b7134787b50d5598
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:23 +0000

    target will treat comeup corner

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
