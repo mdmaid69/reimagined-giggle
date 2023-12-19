51d3e599d7117ae62f461120684fb49660e8c99f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:27 +0000

    without sign needle easy pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
