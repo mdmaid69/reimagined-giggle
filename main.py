ca29bbc037595d71bae77b3f68457c1b74c863f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:01:51 +0000

    down maintain mine discover below

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
