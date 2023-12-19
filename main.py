3cd4f2c1b7d087a81e2137b043258030b92f72f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:25:16 +0000

    ticket exchange strength hungry term

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_bytearray(array):
+        return bytearray(array)
