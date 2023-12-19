4f4a7f006aee4a5d80451959792d565254e776a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:27:12 +0000

    team abroad heat confusing care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
