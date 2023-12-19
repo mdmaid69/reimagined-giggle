f4d37955dc4a674519b1173d489e92d3b2159f4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:54 +0000

    press service beyond certain side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
