9f14271f0edab3a38474ed86aed3bf16397c1fe0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:57 +0000

    comment cover style lost straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
