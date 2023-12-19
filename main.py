42d6c0beb614e8a60bea8f83aa6dbecb6a017216
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:36:14 +0000

    backwards change opposite snow partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
