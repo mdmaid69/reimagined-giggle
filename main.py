eb26cbe4caf35c731e17f0b04233a0a0e232b2ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:02:20 +0000

    programme exaggerate remember public guess

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
