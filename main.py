e670c6f1b67e53240dcb98802e61bc910b0b75da
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:02:12 +0000

    dull Zz attempt benefit honest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
