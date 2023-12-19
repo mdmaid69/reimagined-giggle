09fcea23f288c486858dc2a31c2e5a0bf48a601b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:13 +0000

    comfortable wrong attract considering ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
