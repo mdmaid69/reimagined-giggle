b07fc77d5dba508f601d73821775620ae8be68b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:13 +0000

    substance direct conversation friend search

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
