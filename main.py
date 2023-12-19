3715eba29aa2d494d347a75eef8dc3ac6314bbeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:09:21 +0000

    classroom describe breakdown vision geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_even(n):
+        return n % 2 == 0
+import sys
+def exit_program():
+        sys.exit()
