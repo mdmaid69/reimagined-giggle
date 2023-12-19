bb3025069c417b34372a7e51e227529147e6f28b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:04 +0000

    back fair 569enjoy television invite

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
