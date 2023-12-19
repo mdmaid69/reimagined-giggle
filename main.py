6c7d87901f159bb9ad129ad39372500e001418b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:20 +0000

    certain father attractive range recently

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
