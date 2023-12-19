d4af6bfde05a2a302e9d6c193f7aef8be869d068
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:56:45 +0000

    in shine second mark listen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
