78e9a698f0e76963625f46d74becc9d6a4281879
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:17:22 +0000

    behave equal copy disappointed answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
