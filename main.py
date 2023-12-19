96ee1757bc6bbcac564d9af093a9ed8e6c313829
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:04 +0000

    level want frighten fast bringback

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+name = "Python"
+print("Hello,", name)
