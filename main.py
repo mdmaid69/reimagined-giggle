ab8e5bc97060c0297ee0b96826e4414d21c809d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:29 +0000

    pipe free half happen petrol

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
