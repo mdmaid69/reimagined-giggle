64bcda1df370af5327881db609eb209e363057f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:25:54 +0000

    pretty immediate unable the mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
