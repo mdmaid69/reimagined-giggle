842074691d58c78bff469d51db27d005ddf5c41c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:23:57 +0000

    exactly complain all around around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def get_current_working_directory():
+        return os.getcwd()
