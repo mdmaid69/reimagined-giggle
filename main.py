133923192407c22fcd0b7c6e8f74e55fff0aea77
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:14 +0000

    record godown generous emphasis great

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
