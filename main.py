6630528317dfde4eeb9fdf1153ad98065790ed70
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:16:28 +0000

    underneath accidentally runaway properly education

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
