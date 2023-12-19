c933afa62124b5600528f8242ce331df48e8040d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:59 +0000

    method until workout foot upset

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def subtract_numbers(x, y):
+        return x - y
