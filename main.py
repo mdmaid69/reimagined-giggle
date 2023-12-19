241f7fdd16f45fa6ee72fca01ba9d909b8406b02
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:40 +0000

    book bring able need result

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import random
-print(random.randint(0, 100))
+import sys
+def print_python_version():
+        return sys.version
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
