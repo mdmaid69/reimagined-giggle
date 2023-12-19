28881c301bf10d995260ea2a6a45f7ac305b5b0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:28:38 +0000

    knock smart excuse hide maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
