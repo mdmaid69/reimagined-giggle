bb5704022d84b3b07a7f4727cf9e807e6a41c5dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:57 +0000

    wear favourite spot destroy without

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
