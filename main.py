370696125be8413c9c9eb3f1361f4a8ca4108582
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:17:49 +0000

    weak once outside comein earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
