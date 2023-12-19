f565c47542a52c52b1b75b258a106514420ca426
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:55 +0000

    not book education university walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import platform
+def get_python_version():
+        return platform.python_version()
