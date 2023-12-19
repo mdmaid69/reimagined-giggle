b80873282018a017f4fdf3ef1b7e80e1f652dbb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:27:03 +0000

    turnback safety onto violence unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
