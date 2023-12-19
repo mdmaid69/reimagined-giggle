3d76b418c13edb6eb4c588497dea58801d75a276
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:28:32 +0000

    right key regional lonely tough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_floor(x):
+        return math.floor(x)
