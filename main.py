40e7ba6385fb06b764496c79181d4311b53f5c29
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:00 +0000

    bringup block come turndown realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import re
+print(re.match("h.*o", "hello world"))
