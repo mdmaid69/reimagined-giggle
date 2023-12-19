7aaf75c00a2c601670c0886d4699f6ad6d0c2f67
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:51:30 +0000

    dry various busy offer closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
