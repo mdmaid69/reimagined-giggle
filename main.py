d38a53f7ad72c261337a638e41ecacb8b4b2453c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:47:51 +0000

    your slow dick doubt shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def clear_array(array):
-        array *= 0
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
