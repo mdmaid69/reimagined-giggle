b5936ad53e1ace715676bbee69c7467253fb1208
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:09 +0000

    slight drawing with actually stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_max(lst):
+        return max(lst)
