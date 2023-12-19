578b3bfb15474e258d0a9cdcc348ffcc572ecb7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:50:43 +0000

    regard pronounce difficulty talkback tomorrow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
