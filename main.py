2951f58108b1bb56e97cbfb3b4fc59029b2aefc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:30 +0000

    help confuse plant skin comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
