86ae47e88c87fc5da1e179650d81e0926629ba8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:04:17 +0000

    convinced tip fall effect show

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-print([x**2 for x in range(10)])
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
