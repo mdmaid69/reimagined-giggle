ddf8cf7541fa9e18c05df1a76258f64a176953c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:05:57 +0000

    voice by up difficult dull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
