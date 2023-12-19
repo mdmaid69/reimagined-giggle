906b23bca62930c3f5c494d4b423a985ea49147f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:59:56 +0000

    declaration properly breakdown benefit where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
