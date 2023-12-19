8610fd30af4424459ee5e8be8b0adec49d756fde
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:57 +0000

    slice pity snow scream right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import sys
+def exit_program():
+        sys.exit()
