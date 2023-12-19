9dfd93b7768ccbe460ea5711702e20afc5decb1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:56 +0000

    fail sore mark shout long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def reverse_string(s):
-        return s[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_unique_words(sentence):
+        return set(sentence.split())
