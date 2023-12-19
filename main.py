0a163c8a2cf5162314a24eeb6469b7bc19d1fad7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:07 +0000

    couple morning hang offence write

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
