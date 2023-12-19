4e954fd83f92567ec90c59b87367bc8a1819985c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:02:03 +0000

    sweet move breakinto trousers hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import sys
+def exit_program():
+        sys.exit()
