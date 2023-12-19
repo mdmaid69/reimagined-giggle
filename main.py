8df6da07fcc2ec5bb346846b4c661ffd3924480c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:15:15 +0000

    next instant runover refuse tomorrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
