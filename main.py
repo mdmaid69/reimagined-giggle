337e7be9a4d3ccaf629560259d4b458683c208c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:48:17 +0000

    home measure change house opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
