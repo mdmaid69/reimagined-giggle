cfa16f48a11f456ee3b97863f712311153823111
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:07 +0000

    pen dig test smile maintain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
