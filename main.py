d5753fe34fb7fa32e83589463064689740506f8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:16 +0000

    black production tonight children about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_item(array, i):
+        return array[i]
