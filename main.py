fed5ba58a7994865cd16871ae41d1e00539caa59
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:44:50 +0000

    trick experience sick pick tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
