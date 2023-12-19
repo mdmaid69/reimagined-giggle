92520e6eb35f0f3351cf22c3e90c1a8537d8e4e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:50 +0000

    any Itself ocean loan luck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Even:", [n for n in numbers if n % 2 == 0])
