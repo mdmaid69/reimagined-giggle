1ef58648289d39262c138f81ebdcaa629b5fdaf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:47 +0000

    great undo belief suffer long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
