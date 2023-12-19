2492115c34ce903b6f3a9288991b2773c5348b88
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:15 +0000

    convenient Rr copy lead tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
