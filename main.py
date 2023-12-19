fdbed306584b5fd77637b8184ccdcaeca2d6c7ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:21:37 +0000

    whole cheat never die special

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_max(numbers):
+        return max(numbers)
+def count_characters(sentence):
+        return len(sentence)
