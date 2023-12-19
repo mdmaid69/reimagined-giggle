224e96ec716538040bea108259c2338b8f98b107
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:36 +0000

    rush regard surface big cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
