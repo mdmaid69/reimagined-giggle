658196e5c7cbe386e8d6c10931e351d1c191a694
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:32 +0000

    corner order record big hello

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
