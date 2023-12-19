997708244bcc1ba16edd5eea9031636042278e7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:56 +0000

    mass paper advice seriously would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
