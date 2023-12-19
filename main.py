5a68807a2bc6012002bda8c7f97616b6d5be48fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:45 +0000

    list apply noone careless whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
