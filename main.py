7862772ea5735d1a5af37c1c6712fc6b7360e252
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:00:52 +0000

    spite bath correction bag for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def count_elements(lst):
+        return len(lst)
