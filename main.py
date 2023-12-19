c0b09d8726b3349bd4408db562dcb1b847ae6a2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:01:05 +0000

    holdon equipment search pity spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
