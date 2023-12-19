eb1780f5cb9c6004d9f93461a68767a1b9591c00
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:46 +0000

    rid flavour imagination not variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import sys
+def print_python_version():
+        print(sys.version)
