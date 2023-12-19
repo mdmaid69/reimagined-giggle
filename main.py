9a9c764b99edd54ae300c8642ba6ed8d06b0ecba
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:34:04 +0000

    Ww television common characteristic trick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import platform
+def get_python_version():
+        return platform.python_version()
