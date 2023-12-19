206967de194443176e45bb05b1235a06578b7fa3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:53 +0000

    do behave lock nothing criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
