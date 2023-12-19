4427bb5a66598fa7854e372d80d14e4c907743ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:39 +0000

    acceptable fond lie shock home

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-for i in range(10): print(i)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
