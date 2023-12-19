4166c201e8c9dceacb32a8c4546b86d17d9548da
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:37 +0000

    season corner regional pretty turnover

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_bytes(array):
+        return bytes(array)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
