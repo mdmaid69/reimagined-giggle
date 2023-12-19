4bd7b516dc7d640ae89197e8da18008be6971739
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:24 +0000

    organization flat jump arrive fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
