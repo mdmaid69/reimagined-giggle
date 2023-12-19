616a1dfc40d1bf3d4358cdd5181c55c01bc86ecb
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:54 +0000

    influence keepfrom display memory care

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_user_list():
+        return collections.UserList()
