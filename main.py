2cc39a5b8719bdd7650c1d5e3e5c7f76318aa96e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:37:57 +0000

    callat science hold pronunciation clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
