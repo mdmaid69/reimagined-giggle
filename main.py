d6b06a000a20aeb4374e18ff657568a6a3fb96a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:21:32 +0000

    side straight sweet successful away

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
