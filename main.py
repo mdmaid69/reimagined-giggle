e9554127219e93c3b2590e1b779a1690a68b1b26
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:32 +0000

    fence mother admit smoke swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
