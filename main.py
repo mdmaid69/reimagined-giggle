0bdbfecc3ece93755eaed0edd0612d5fc11f2deb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:44:16 +0000

    talk appeal plastic be weekend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def reverse_list(lst):
+        return lst[::-1]
+  def multiply_numbers(x, y):
+        return x * y
