7ca6020d9c84a78e469f4cfcb3a2f60e7e117bc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:54 +0000

    neither appeal deserve pretty maintain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+print(re.match("h.*o", "hello world"))
