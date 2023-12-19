b64c72f904af80d9147c60b75ec6552cd9afc62f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:24 +0000

    congratulate somebody line save embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
