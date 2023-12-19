2ee8b40dc2779d60a7591f449f02bb3918ee6f85
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:14:08 +0000

    tie worse sensitive ready suggestion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def subtract_numbers(x, y):
+        return x - y
