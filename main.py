0d6029cacbac33d0c89a2ab067af142ed30bded6
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:10:18 +0000

    basic dance tall star regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
