3039d1c6f807231ee5911cb36dcd0493b9d19208
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:13:19 +0000

    storm principle amount organization bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def reverse_string(s):
+        return s[::-1]
