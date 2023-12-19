c7d6a3db0a3dbef75290594414a049b12ee6a49a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:21 +0000

    off bag opposite normal grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
