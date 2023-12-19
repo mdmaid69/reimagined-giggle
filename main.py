43a8d89002f2a71dad1f57c058981ed0070906de
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:17 +0000

    sometimes warm excuse computer disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
