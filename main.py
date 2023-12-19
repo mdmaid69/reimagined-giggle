af2cf1ae40135b5ee6380291d29a7e3bdc63df10
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:37 +0000

    that Gg possibly sharp goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
