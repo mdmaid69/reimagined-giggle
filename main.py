1e6df5c1c40ceda36e127e200bae58db6547f33f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:02 +0000

    practical imagine independent disagree sail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+text = "Hello, world!"
+print("Words:", len(text.split()))
