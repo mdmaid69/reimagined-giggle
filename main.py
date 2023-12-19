7dbf01491122b60fb429b99a444d6faaf7935c11
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:39 +0000

    sleep from pin waste runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_item(array, i):
-        return array[i]
+import sys
+def exit_program():
+        sys.exit()
+def calculate_speed(distance, time):
+        return distance / time
