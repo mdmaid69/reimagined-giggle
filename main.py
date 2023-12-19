78e8ee5953038ede4fc221237a5349349e137445
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:28 +0000

    charge left print bet opinion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+for i in range(5):
+        print(i)
