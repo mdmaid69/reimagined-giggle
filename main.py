04fbdb4507ded484fd3dc92718dff8f4bc6ef128
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:51 +0000

    spring finally earth damage behind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def convert_to_binary(n):
+        return bin(n)
+  def multiply_numbers(x, y):
+        return x * y
