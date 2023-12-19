505d427bbf64063f0360290651250518611e2039
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:31 +0000

    negative bar last colour any

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+for i in range(5):
+        print(i)
