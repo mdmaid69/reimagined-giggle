b88df0001b0537d4ad4f94a60b1f8c8967c4f98c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:43 +0000

    form share getaway wise schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
