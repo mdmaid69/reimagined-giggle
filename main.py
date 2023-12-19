01d068829069c628c5bbbdd23026f5362091d03e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:02 +0000

    that return move freeze region

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
