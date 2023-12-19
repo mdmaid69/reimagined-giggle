57ee28876407578600a93658089133fe0c821133
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:27 +0000

    bottom immediate though discovery throw

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
