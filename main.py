c12dbdce752f8542427ef8d67d710ae9e1426bca
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:19 +0000

    promise suit move anxious better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
