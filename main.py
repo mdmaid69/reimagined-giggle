df28c85d74c3b4a71219f02ac7ef80cbd076dee5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:48 +0000

    edge plan autumn description doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
