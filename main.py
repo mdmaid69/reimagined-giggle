e65b144c294a04a84598f2768f0d8ee3cdae2bf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:12:34 +0000

    why leave clear person right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
