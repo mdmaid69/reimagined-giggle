b19bc69079e1e509d093ebdab111af59c4f43c12
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:32 +0000

    die estimate soon event sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
