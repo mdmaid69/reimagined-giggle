5094d83c1f8187bb058692229dc46aa48146cf6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:08 +0000

    within exactly professional beneath respect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
