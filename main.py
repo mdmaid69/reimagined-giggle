fe45e069d5439313187a4ccd754f28d838e9f228
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:52 +0000

    claim meeting suppose anywayanyhow pull

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
