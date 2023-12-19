7781338b26c94b4ff91149d8c184c985886d19b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:26:16 +0000

    manage again hold before visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def square_number(x):
+        return x**2
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
