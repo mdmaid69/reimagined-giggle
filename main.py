bf616e5725d5fa413bafbeaadd0fcbcf70a2ba02
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:58 +0000

    favour injury getoff storm rush

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
