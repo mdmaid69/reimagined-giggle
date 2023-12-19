3a1d99ca368436bcdf42701bc3730bb281aee793
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:11 +0000

    region occasion normal successful honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
