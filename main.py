6e138d8b4875e3f274112b0dd3f531f73305c271
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:16:25 +0000

    lookaround patience Bb blow tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_set(array):
+        return set(array)
+import numpy as np
+print(np.array([1, 2, 3]))
