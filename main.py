5c19bf2cade6b0f82aa211a4452c197d534b8eda
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:29:48 +0000

    memory 573entertainment regular occasion copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def get_array_as_int(array):
+        return int(array[0])
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
