96d4df4bc9e338de3155544f480ce90293e99d29
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:42:48 +0000

    operation without ear lie reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import platform
+def get_os_info():
+        return platform.uname()
