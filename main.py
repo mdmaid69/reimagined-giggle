35fc42a8f7398daf34d53c7b27d515beaac1297c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:05 +0000

    turnoff company hit walk delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
