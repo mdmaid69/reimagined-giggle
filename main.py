b092199583d9affaef6cd3e5861212fc54abb50c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:03:32 +0000

    rest sky next how woman

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
