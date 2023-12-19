51f7954f3fcab1909fa3d5d84fbce117e2884c7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:33 +0000

    character include woman turnon nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
