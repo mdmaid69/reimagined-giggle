3a3f735ff7dcafcf2e75cc5c28bca8b70e6b0191
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:27 +0000

    find area community night art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def convert_to_hex(n):
+        return hex(n)
