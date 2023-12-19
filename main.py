8e12396c8d3aeb899b13c517f86cd376e0f91b78
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:57 +0000

    uncomfortable top hardly bunch young

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
