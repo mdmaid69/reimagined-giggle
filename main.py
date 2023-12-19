2953088391ca4df33ac26d7cebf5c600dc9c34ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:07:35 +0000

    send weather damage impatient mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
