7aa8fd31515566d3fd5f2149fb7b63783be908f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:06:30 +0000

    or begin target entrance straight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
