75134ea08817ea6edd6e483411edd352f3ac6790
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:29 +0000

    introduce closely complaint value bank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
