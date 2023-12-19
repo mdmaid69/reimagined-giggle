3a039b1fde7ea9774ca322e25fcea170aa0ae2ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:21:48 +0000

    become air workout top deserve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_user_string():
+        return collections.UserString()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
