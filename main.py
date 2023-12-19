0ab94cb9d5d8ac7152e819e78803757614228736
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:28:07 +0000

    Ee autumn memory brain cover

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
