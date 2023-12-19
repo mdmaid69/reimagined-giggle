26f4e543d261954b80754f23f36fddbe85c03a50
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:49:29 +0000

    medium turnover mind possible tape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
