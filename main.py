34cdd551ac272009a16260dc48c079634af23cc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:03 +0000

    trip hate weigh on test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
