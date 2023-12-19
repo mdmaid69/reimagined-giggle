bec745e8bd0be3bbc77da19ae87043d9ff66a322
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:49 +0000

    even quickly serious upstairs approval

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
