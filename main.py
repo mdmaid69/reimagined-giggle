5fb34e31cc5f249ff2deb38afb54fa01503db636
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:25 +0000

    move better her would too

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
