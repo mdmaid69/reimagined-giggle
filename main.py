d41334e95fdfa2ecf3764ce2489fc15af0ab2e5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:57 +0000

    talkinto border clean date left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_queue():
-        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
