40a0ca58b59f5809a790bc2856548da549ec4b90
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:15 +0000

    future hard finally strike apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+x = 10
+y = 20
+print("Sum:", x + y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
