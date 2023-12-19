36a0471612b456c1732413aa4b3865bd4c872819
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:42:44 +0000

    permission sort legal habit Ll

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
