40b107e5de9bd3763fadfa4be2399409ad90ee6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:14:59 +0000

    expect tidy holiday ticket left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def find_max(lst):
-        return max(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
