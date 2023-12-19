5737f0ac5c3689bafeb3d9c827041e2538777ed4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:46 +0000

    theirs rain autumn acceptable how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
