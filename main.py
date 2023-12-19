ea9583e36b198b837e1b5c26c63a301269b1edea
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:33:20 +0000

    impatient free now practice personal

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
