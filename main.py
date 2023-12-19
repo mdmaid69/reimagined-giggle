52f94ad1451eff62e874e9d5070ace522e63ea3a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:00:37 +0000

    tongue mother foreign another thankyou

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
