890208d2cc41b7cb0d2909c75de4728d8c0615f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:11:08 +0000

    tall whatever touch choose possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
