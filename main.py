fb697e64c1d243970b2332b6b2caaa18c8546d62
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:08:05 +0000

    him purpose throughout punishment attach

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
