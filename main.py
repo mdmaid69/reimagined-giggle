2eabb56f3f9de5038fbd3bba1d44d2243c318bf4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:24 +0000

    beginning pure Zz rent distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_work(force, distance):
+        return force * distance
