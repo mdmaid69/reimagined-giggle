ba2f5191156cf9b1306ff395a5ebfd8e02a18f07
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:10 +0000

    experience town welcome appeal unkind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
