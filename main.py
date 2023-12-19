d337ae1d4dffdaee95c19fd66f2ccc153f797d9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:39:25 +0000

    shrink claim tall Bb curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
