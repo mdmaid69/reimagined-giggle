74209591cb7cef65ca515a529eec7a7f1c354cde
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:37 +0000

    breakthrough willing weak mend rid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
