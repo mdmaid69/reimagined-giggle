0963760b5e1b10f80cb6ff1cc7fe6c653ef00ea9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:30 +0000

    hurry smart excellent even deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
