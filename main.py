442eed971231a4e537cee3907fafbc80874c5796
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:44:35 +0000

    problem slice squeeze tool miss

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
