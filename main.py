08aa999d272f38b771059f8e64f4a406965f386e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:57 +0000

    one 569enjoy class connected workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def roll_die():
+        return random.randint(1, 6)
