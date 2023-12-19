a8963aedcb9b70641e201d20068631ac47bc64de
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:02:29 +0000

    maybe choice complete sore wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
