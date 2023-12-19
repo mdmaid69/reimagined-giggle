fca575ac89ccff29e7064f36bd6316e260ba5477
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:07 +0000

    slight look post long stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
