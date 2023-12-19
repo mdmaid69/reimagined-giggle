ced036c6b8e7617d890f230736efc50cc2bcc70d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:02 +0000

    start write common pollution serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
