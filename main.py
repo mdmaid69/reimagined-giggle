9ff0a68487d47c624fc0c35d1b06920caa0234bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:31:52 +0000

    still hit contact right communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
