a447f9a5f2f53811f4bbd2359fedb98d17541c00
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:59:59 +0000

    dress band bite shout instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
