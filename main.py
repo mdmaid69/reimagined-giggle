dc68ecbc52157c3838fa2b58513942ed7f27cff4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:19:30 +0000

    sky side moon suit defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
