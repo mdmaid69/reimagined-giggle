1562dced8b991155a0300f05253e11df5314289e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:50 +0000

    admit other professional name dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def multiply_numbers(x, y):
+        return x * y
