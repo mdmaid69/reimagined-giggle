6f4d389ce6d5b33f52b7dae0a06b74adb0778c8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:02 +0000

    plate blind escape work length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
