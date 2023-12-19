b15e9fed2d85f0c45f0850791e98237bb1a58174
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:05 +0000

    occur every period skill prove

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
