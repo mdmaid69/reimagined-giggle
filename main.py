01025b4cbddfa0531f6d6dd17f901039d8ba2f3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:09 +0000

    action square tear escape complete

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
