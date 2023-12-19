cda57f3d5645bc1686ceec33e0cf37528e16ef0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:18:27 +0000

    centre trust polite field just

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
