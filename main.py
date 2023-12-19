6cd6047ea7832f3044db10c19b82ee787d236f6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:58 +0000

    label Itself blame sky occasional

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import platform
-def get_python_version():
-        return platform.python_version()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
