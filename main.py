009c22db2b2718e970dffec799ef69d7f6a77c28
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:49 +0000

    freedom over perhaps cheat go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
