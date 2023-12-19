84e3fe8f8411bb861d7aae0f272e6142901f5355
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:43 +0000

    wrap produce 179bite steer start

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
