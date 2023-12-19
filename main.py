281660a09bb5f040bab8b732d2428fc5743a17a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:07 +0000

    talkoutof temper pocket damage switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
+import platform
+def get_os_info():
+        return platform.uname()
