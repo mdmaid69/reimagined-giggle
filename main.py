3e81cd81fda207c95a2e8ea0302789cbbcf4f502
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:37 +0000

    fasten uncomfortable suddenly meet concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
