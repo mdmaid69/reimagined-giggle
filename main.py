f791622a8ea80469394367b645a473c4f26d0f70
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:14:07 +0000

    form equipment protest warm create

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
