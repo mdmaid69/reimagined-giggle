abd0aebb2a572ae47a554b1e71d2512fd1f8528e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:46:21 +0000

    market march way refuse 565end

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import os
-print(os.getcwd())
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_typecode(array):
+        return array.typecode
