783d2c2025d8d4f974343e13b39117c0ed1d4aa9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:53 +0000

    job wild central holiday awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
