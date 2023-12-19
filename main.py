018646dbbc3bbc86faf0b2edd70eedae4679f05f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:12:07 +0000

    thinkof bringback discovery suit together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def sort_list(lst):
+        return sorted(lst)
