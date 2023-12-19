4bf8a1e5bee5b1eec92560922c13ee274aca23f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:09 +0000

    centre threaten cry practical deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
