57b391f205862577dc9836dee162a1ecc401be3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:22 +0000

    convenient mail attract create see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
