1c44692a81cf29fdb859f417b483e9bd28b7ee26
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:59:03 +0000

    sign path music bringforward keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
