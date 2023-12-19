87297440a886823b130bdb885b1020aff03540b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:21 +0000

    length trap favourite previous list

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def greet(name):
+        print(f"Hello, {name}!")
+name = "Python"
+print("Hello,", name)
