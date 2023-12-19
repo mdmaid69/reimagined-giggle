cf8851532e4ee4122ca48432f597ccfafc19e060
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:23:53 +0000

    mind risk quiet need inform

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-  def subtract_numbers(x, y):
-        return x - y
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
