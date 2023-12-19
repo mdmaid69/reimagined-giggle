0871438ef984ab6ada0d6a1c21ed3c246c7796de
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:18:35 +0000

    immediate can family slice fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area(radius):
+        return 3.14 * radius * radius
