17d597b5c915c44b12c4ec21dbcdfaecf0c9fc66
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:18:25 +0000

    comeup Zz Cc awake damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
