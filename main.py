6ba90077923c813f7da7b21b234a357555346bc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:44 +0000

    interview lookafter forever defend mad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
