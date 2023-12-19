b62934d980cf52688159a81d0a050689cda5d813
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:58 +0000

    Dd reliable convince excuse refer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
