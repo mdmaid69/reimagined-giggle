e1121642f8093e0ccaf4f6fafd5b155d136e45c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:40:35 +0000

    pour bag admit upset comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
