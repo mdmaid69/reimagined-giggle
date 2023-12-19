a1f8f9468a2bfab4691a0bee8cfcd869dc5f5542
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:59 +0000

    shock minority need confidence most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
