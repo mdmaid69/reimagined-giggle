81879632761271e9ead39b4108ee5b8b46f697f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:11 +0000

    release space solve hair print

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def add_numbers(x, y):
-        return x + y
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
