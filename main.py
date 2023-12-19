a6ea7e7d64aef684b7ef310487ae75401b671487
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:52:42 +0000

    unpleasant active kick general back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def convert_to_binary(n):
+        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
