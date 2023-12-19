d6abc2224d46a90023f8d3ea448a6881925ec473
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:46 +0000

    bag to late turninto into

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+print(math.pi)
