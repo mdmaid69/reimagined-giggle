04284e01d5d5724bcbb530e022f17e71ac32e25e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:12:13 +0000

    pity refuse private large recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def cube_number(x):
+        return x**3
