8a415274d55eee1c7eb4350dd046b3221946ad1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:59 +0000

    declare lucky fire bet them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
