e1ff0142f4e58242b2e69de5a81d4e8faad80c3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:01:07 +0000

    lookback(on) godown skin late advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
