745a957cc889dae0a266f4ed41ad3711cfd331bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:59 +0000

    bored which step base female

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
