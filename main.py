bc5ef728ac52ab9afd482d884d26f77001e474ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:41 +0000

    project advanced hear town sensitive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
