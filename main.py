d678533faaba37c47fbd103809853b02447be93b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:35 +0000

    surprising responsible force wish report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
