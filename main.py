02c5c03b9bb84bf59550cadcf57a165f869c65cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:04:56 +0000

    pick reduce please tear 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
