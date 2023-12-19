0482db5a341968d0b8ca81bba9f595922151cde5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:28:56 +0000

    contact respect rate precise permission

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
