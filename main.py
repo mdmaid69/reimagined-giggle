af35d0544bc0c818ee59141d54a664750d76f196
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:52:48 +0000

    old mouth payment always have

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
