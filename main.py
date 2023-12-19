902a1b3d692e8186d07cd425021aa0f772d7ced5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:44 +0000

    passage easy we deliver pronunciation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
