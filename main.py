c33f2a6bca92296c1aed1738f77038a23a3df328
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:15 +0000

    ready sorry apart ours need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
