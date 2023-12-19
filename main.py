33586e6afbdac69e7d8e0ebe5908726628f84685
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:49 +0000

    next everyone passage runinto awake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
