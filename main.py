9e89bc93239337585cccb183bbb2921bf71ec298
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:28 +0000

    payment little arm shop tough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-print(math.pi)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_max(lst):
+        return max(lst)
