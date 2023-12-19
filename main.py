bad6777503abca307f7d39f2b297a9cb9dfce40b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:48:17 +0000

    home breath production discover support

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
