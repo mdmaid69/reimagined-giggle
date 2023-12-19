9030adae45084496a8155e5f3894049f559d756e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:24:21 +0000

    pretty tell last lock explain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(lst):
-        return min(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+print([x**2 for x in range(10)])
