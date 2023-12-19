8e64ff45a682c66ed95ada8382641534a5a4febb
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:24 +0000

    scream towards motion Gg operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
