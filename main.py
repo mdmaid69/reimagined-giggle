1e5671c827120b2168bac1e594e76d5c90b5492a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:28:48 +0000

    mystery piece damp suddenly think

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-def calculate_power(work, time):
-        return work / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def reverse_list(lst):
+        return lst[::-1]
