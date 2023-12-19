e426209654590218c71e341b409f5ccd6431bc00
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:11 +0000

    size chase home way buy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
