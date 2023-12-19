9f977b8ad3e92ebc0988e750c767465e989556bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:13 +0000

    interview clean Vv refuse bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_palindrome(s):
+        return s == s[::-1]
