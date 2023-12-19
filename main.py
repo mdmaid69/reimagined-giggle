e0b95e26042ea3422298cb9e52677849f7ef299a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:56:05 +0000

    delight tread catch little command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
