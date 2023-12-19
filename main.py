56c9962d709ba3d35e35dcdeb8920628ff4f3844
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:11:55 +0000

    satisfied condition take your why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import json
   def convert_json_to_dict(json_str):
         return json.loads(json_str)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def reverse_list(lst):
+        return lst[::-1]
