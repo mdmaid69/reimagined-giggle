6591df1bd22b80dcd5ea64618c403065acda3d9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:15:02 +0000

    shut slowly majority freedom stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
