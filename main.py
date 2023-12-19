00646ad27e2716b3a366551c44fed788a9554e92
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:38 +0000

    someone mysterious entry on tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
