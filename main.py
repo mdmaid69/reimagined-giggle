175b5a6ddbcd525cd9990362b5a215fbc321373d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:27:03 +0000

    them develop meaning talkinto scale

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
