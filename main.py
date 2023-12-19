f08173cd67696c562c922795edbfa489e66aba23
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:05 +0000

    plenty class success 569enjoy everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import collections
-def create_user_list():
-        return collections.UserList()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
