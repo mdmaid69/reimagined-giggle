79b4ad8484c16c4cc6abaeec73eb3109f78b2527
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:56 +0000

    exam pretend contact climb lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
