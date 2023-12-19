441176c6eeaeecb768bdf3f2c7f83eb923591130
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:24:44 +0000

    tune holdout 566end under interesting

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_string_from_array(array):
+        return array.tobytes()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
