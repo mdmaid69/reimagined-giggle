56be7c5427fd04a1456b4cd487f283765724c8d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:46 +0000

    talk large too round start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
