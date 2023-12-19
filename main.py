1c468aaa3fe292929332e60d544b894b2e343beb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:20:23 +0000

    gradually price power problem sharp

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import pandas as pd
   def write_to_excel_file(file_name, data):
         df = pd.DataFrame(data)
         df.to_excel(file_name, index=False)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
