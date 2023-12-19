47e393dd6b2cd6bb0672485779c1ed42419b34f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:45 +0000

    raw its feeling search steam

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
