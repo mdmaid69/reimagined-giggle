d1a3efa0e1db22ea59c5c04e472f4355cf442be2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:11 +0000

    old guide hand content steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
