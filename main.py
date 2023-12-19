df2b772d64faf4ecacaa274c98c28eb00e685241
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:08 +0000

    patient serve everybody my beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
