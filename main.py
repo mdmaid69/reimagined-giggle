29931656c2a9e78a70e28b051a8fb74e9b156324
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:42 +0000

    arm hair protect perhaps study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
