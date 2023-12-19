f4696c1dbebab211e06b24360f6647867650a6fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:57 +0000

    short several help sky side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
