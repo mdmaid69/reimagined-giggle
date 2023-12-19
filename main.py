2a051961c57771f249166a9aaae3884136537ac0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:16 +0000

    dress watch common motion probably

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def count_characters(sentence):
+        return len(sentence)
