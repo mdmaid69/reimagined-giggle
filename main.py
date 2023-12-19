29f832250a5c2e7222f350a01d7c97fa4cb66c2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:08:05 +0000

    responsible argument work repeat arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_current_working_directory():
+        return os.getcwd()
