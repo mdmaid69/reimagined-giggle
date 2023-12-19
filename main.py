6a4a734bb99946f8e4d94894102c18ff7ae4d675
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:54 +0000

    programme who separate suggestion achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
