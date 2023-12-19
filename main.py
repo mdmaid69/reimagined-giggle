a89cae98ae8ae59a4813463750281da475f92307
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:33:27 +0000

    crowd exit claim mainly apart

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def greet(name):
+        print(f"Hello, {name}!")
+def find_max(lst):
+        return max(lst)
