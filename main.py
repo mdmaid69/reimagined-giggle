959479bf336b877d005988472248227995782c2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:37 +0000

    secret class Uu count below

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_string(s):
+        return s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
