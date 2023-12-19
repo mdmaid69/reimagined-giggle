40dc33fb5d0693b792a7dd6c0f6293bf8481c157
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:16:56 +0000

    moon whistle drop shoulder support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_typecode(array):
-        return array.typecode
+import collections
+def create_counter():
+        return collections.Counter()
+def remove_duplicates(lst):
+        return list(set(lst))
