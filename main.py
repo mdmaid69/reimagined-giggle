350e3f48eb89f0e45e3f7159abdfe27c65d78d6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:45:10 +0000

    skirt because strong lesson protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
