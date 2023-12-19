638b65357d55c550d4d29983c316a758fe8f2b46
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:27:28 +0000

    belief platform better your far

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
