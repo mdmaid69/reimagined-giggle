74b93f7abb133f3055339af955c1d6b3d6df48b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:31 +0000

    body pair block think check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
