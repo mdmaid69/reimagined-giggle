c00fabddb567aa0c0e29c5373a7c7cb3ca4730b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:42:12 +0000

    suddenly see stone relative aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
