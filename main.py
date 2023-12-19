d0e1c4027db375b631a2dfaf8f5a32617f3c9b68
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:15:34 +0000

    introduce sometimes talkinto maximum away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
