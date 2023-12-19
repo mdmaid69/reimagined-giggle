8405fdace3295367bc7f90911fda99a887533a2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:27 +0000

    she month attach hurry necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
