6eb9cd8df4d9ae2153537e70cbd6185f56e60b90
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:40 +0000

    story unite touch cheek natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
