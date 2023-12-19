904756614d00d072093b4c0c284bf73e0c269a68
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:38 +0000

    fire life when unhappy beyond

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
