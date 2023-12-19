72db27851bbf724f6c1320bdc753dd460df282ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:05 +0000

    clever would knock keep bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def square_number(x):
-        return x**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
