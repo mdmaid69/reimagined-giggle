a230e0f144ab22f93e1ec250aabd2d9bb00a7810
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:46 +0000

    confusing manage noisy pipe Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
