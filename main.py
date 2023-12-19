807a5ca4a1b644aeab457559c1376eb05cfe7fe9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:27 +0000

    bit behaviour before previous surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
