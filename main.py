d78843ce8794e870091075c345a7c938af9d7917
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:48 +0000

    through gas cold recommend leave

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
