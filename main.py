e30b3cdeb1bee7a1da16fc8ecbf38300886fd3ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:35:15 +0000

    intend downstairs each punish permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
