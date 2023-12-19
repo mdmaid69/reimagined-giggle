8c2b65011188a53ba85220f4594613a3c270dfba
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:51:55 +0000

    straight really take pleased to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_even(n):
-        return n % 2 == 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
