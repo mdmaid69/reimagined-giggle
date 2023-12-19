cc93cd07af1b902f2a2de91c2a1a545abfb2af96
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:49 +0000

    trade range visit obtain giveaway

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+print(os.getcwd())
