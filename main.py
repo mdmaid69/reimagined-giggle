34750f2a30479a2650eb97ad948429dd8109bc46
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:47:09 +0000

    recently greeting start splash perfect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
