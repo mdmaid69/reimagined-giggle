ce1046fc8dddf6b5bddf24ecc4b147e1635c3804
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:29 +0000

    somebody runinto goaway eager time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
