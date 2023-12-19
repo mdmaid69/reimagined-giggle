4e59f0bc320f7877e84ad4ea16b0f641ff0a5725
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:26 +0000

    569enjoy juice turnout smell bound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
