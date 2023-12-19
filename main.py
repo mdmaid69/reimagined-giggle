3808356fe5c38ec596feaddd9247ccca0c852597
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:21 +0000

    cook sometimes regret occur confusing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
