ec0191d8227350838e0ecb9fe1594ed5e3aec254
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:39 +0000

    reliable learn workupto dry married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
