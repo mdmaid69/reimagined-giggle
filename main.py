1eb9e8d1583a415785fb737d23136f0ae88a41e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:15 +0000

    so roof season calm rough

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
