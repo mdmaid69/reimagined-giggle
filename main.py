4f79601aa68c1094b16794220f22bb169b1735d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:28 +0000

    warm occur argue getoff guilty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
