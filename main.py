37c19252a6dcfb3a0de652babdab71c6ec696314
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:12 +0000

    short waste strong success lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def find_min(lst):
+        return min(lst)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
