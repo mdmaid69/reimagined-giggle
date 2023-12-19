ceb9eaf896f85cc3038b4a128ff1cbabe572aa5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:22 +0000

    as tap result no whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+print(os.getcwd())
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
