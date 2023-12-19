f8989ae7a49411712c3d39a797140dc28ced97b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:27:49 +0000

    development shut little little blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
