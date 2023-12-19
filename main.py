83db250eb799be952d0bb3b5153aa4c902c0a7ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:03 +0000

    confused possibility whom affect unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
