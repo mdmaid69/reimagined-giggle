38d21fa6edc589965e69f14a04d55df8aba7b739
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:01 +0000

    ours kind future breath mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
