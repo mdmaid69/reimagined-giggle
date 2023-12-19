c09e0bb7ee27b0d031cab180dd695200af8eb297
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:11:20 +0000

    breakinto church switch single himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def convert_to_octal(n):
+        return oct(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
