18684f78c1e56cdab98218397b108845d359dfb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:01 +0000

    no chat advanced word proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
