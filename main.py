08428f4f60ebb6b927152cb52d0c9dd93e0763bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:56 +0000

    church Pp mine aim inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def subtract_numbers(x, y):
+        return x - y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
