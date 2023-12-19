f17e03bfe901292b5578dbfd4f4b6eb5b9d7ca79
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:59 +0000

    piece journey thought dress run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(x, y):
+        return x + y
