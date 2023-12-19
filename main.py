0b002a266d909f57835b364bb87f8f155d40c61f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:34 +0000

    track relief keepfrom little surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
