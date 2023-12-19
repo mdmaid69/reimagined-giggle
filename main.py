2cf4ea37a720d5475dd376d548854ea6b73c02e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:33 +0000

    different bottom minute room crisis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
