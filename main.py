00715431df940ab3f1778fe7471cf241ed88ccf1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:24 +0000

    talk connection boring with immediately

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+print(os.getcwd())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
