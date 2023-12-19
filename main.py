599323425a030e4712161d8cf4d8aefe79ef9b1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:38 +0000

    slow content own replace however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
