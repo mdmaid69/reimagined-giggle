e35cbbf477ee56168ee1c8be90e90b74cd348d88
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:32 +0000

    slightly naturally like worst weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
