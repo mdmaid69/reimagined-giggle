a296e5315d54d59a5bdbedb6e5efd04823f0c674
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:14 +0000

    sport doubt type yet support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_current_directory():
+        return os.getcwd()
