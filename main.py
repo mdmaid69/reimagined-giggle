e953cfc5e14fb67982da21b986d1f11bd30fc232
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:07:52 +0000

    call operate nor Yy imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_elements(lst):
+        return len(lst)
