19377d0130c8c754ecd751d5c4b27c768f8ce1d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:09 +0000

    vision nothing soon comment table

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
