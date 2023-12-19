33b8cc864216b8b8f25f3b6de3d55ef7cad7c90d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:38 +0000

    mind edge importance offer float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_string_from_array(array):
+        return array.tobytes()
