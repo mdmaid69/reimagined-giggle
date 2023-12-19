9fea03e16a02ae8bb9c0435e26bb767675e55ecd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:47 +0000

    branch them music original mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def convert_to_hex(n):
+        return hex(n)
