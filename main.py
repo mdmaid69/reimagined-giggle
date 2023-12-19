ebdcac433900559f94b737a1b6809a2040216773
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:12 +0000

    benefit base taste protection anywhere

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
