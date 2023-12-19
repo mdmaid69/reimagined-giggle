7f33e3a53a6b46229c54903dfd8b77a36f1865ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:56 +0000

    save recognize tall spray clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
