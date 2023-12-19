a8af20a34e6cb46ddb8b976bd3cb8c6f536f3610
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:03:23 +0000

    treat coat now possibility decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def convert_to_binary(n):
+        return bin(n)
