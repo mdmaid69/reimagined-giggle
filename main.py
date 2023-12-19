21d2bbb0f299b410f48324c81d033688421862a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:48 +0000

    but good appearance just square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
