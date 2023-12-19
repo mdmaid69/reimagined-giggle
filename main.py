370a78f3d87d42e747d383d769414305fa07c390
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:59 +0000

    plate interested appear comeout produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
