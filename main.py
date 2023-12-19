f9e3e8949ed15782b0328d1cc532b6a03287e605
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:33 +0000

    exploration tread Cc dinner whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
