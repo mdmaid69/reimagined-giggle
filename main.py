e9533801aa4e00bf06b25f856b9857525fc67edf
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:19 +0000

    warning interrupt whether information keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
