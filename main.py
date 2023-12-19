9e6c2d83d72ebf4baf733f9bff29b23e91e4fa2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:12 +0000

    practical permission behind plan mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def square_number(x):
+        return x**2
