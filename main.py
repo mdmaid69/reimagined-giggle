64d7f1f797c3637fae844fa40aef637a7dd52d48
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:49:06 +0000

    nothing level defeat capital would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
