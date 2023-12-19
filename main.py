2326b6e6caec836e80d437fda4367d0d114df80d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:34 +0000

    to other properly accept human

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+text = "Hello, world!"
+print("Characters:", len(text))
