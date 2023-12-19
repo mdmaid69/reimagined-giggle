77ad28486e89a5538f84142fd9ed2d86e30cd016
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:51 +0000

    back will petrol tell Bb

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
