09e7877a6607f1c28360bbb34386e461feb878ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:16 +0000

    best exciting month carry surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_min(lst):
+        return min(lst)
