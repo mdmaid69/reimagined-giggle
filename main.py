ca5dabbf64c213d1d32e48d85af2caa4286aa82f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:24 +0000

    legal smile gas class shout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
