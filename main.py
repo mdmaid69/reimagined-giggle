26f471dbefb500edb056887968c1ade17b3e3b99
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:33 +0000

    expression success float oil properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
