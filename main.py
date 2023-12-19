9b3539799e52cd0b1b621c942e621901389681e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:04:26 +0000

    complete turn neither after far

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def greet(name):
-        print(f"Hello, {name}!")
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
