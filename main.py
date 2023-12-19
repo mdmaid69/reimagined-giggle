70cc64ac67ead61db380b6be4c87f556e2992d05
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:19:06 +0000

    its introduction fit Uu cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
