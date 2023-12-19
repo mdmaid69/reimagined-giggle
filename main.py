c70056c83fc909d17e6745b6cd112e883d36c3ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:39:02 +0000

    circle hair real stir extra

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
