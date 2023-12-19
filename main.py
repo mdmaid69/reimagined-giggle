ebc15bf71e8fee7718e2621f96ed4bf471c3eab3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:10 +0000

    impatient practice cap brush plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
