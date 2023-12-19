5757caf96dc071d12ccfea688e332398e0a3c86d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:20 +0000

    head thorough space store back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
