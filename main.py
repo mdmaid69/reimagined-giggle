0a423c1844143b87f82a440c9e57262811c14d2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:33:51 +0000

    name lower environment communicate 567enemy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
