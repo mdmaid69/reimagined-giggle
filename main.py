9d5fd3af4bfac7e81566aa8dfe36f17a75a3c149
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:27:24 +0000

    concerning workupto safety care downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
