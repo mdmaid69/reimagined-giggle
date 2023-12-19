4be40c827fa1223fec85d14685595473e8aab63f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:17 +0000

    visit admire wander whichever spare

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
