49c365d61042937c45ce9daf9af596480f2aec92
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:41 +0000

    home bus frequent discuss than

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
