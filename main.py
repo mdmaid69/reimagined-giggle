6a47540f69bc3254fd6caaf4e62f247cbf2fad13
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:46 +0000

    head avoid organization order fit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
