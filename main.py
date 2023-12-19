a33f605f579c0988ee1d95971ee830dc73aa3e85
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:09 +0000

    value if discovery around search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
