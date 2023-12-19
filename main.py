6cdc632a1d9d4626f570b2a638d3e0611bcc828b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:06:50 +0000

    she healthy form post learn

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import sys
-def exit_program():
-        sys.exit()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
