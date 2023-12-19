1d082d4e7d1bf321aa15ea2493b3fd2aa3a49945
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:26 +0000

    ago snow tap another future

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
