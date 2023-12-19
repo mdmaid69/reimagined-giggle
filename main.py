92fc4c379030b3859bd3b79137ad34e21eec7617
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:12 +0000

    something gift opposite charge open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
