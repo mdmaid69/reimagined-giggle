fff2f7ac1a81b5863e377c4a786ffd7ce8d593e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:14:10 +0000

    address plastic supply war shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
