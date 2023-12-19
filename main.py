81600bab22fbda044b2d934381b0dd3194fe0457
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:07:59 +0000

    slowly lookback(on) difficulty mainly cry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pressure(force, area):
-        return force / area
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
