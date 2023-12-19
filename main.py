12aabd88ad470170a517711bed3cf8b5604c29bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:18 +0000

    ordinary hot weekend smell afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
