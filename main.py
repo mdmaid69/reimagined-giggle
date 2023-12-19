74492b5de5d69d5a08395336f62451b71a38751b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:41 +0000

    patient direct properly communication believe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def is_odd(n):
-        return n % 2 != 0
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+text = "Hello, world!"
+print("Characters:", len(text))
