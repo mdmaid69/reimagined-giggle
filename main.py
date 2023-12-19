efe09aeb7ed14295bd368e6be0340e2f9ec51183
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:48:07 +0000

    hide respect advice justice dress

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
