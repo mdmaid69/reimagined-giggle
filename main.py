95e1ba4f633a636704a9a978ee8bf8f29e5eb560
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:41 +0000

    demonstrate lucky importance used intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_sine(x):
+        return math.sin(x)
