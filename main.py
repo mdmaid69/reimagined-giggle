21bbc746b718759ad8c72a42cecaadad0e7b9810
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:29 +0000

    treat comment ago case guilty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
