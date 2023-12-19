6d6eec40da944d167930f069a4a2e46404fa558c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:55 +0000

    cope square air mark market

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
