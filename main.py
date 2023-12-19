11539325049302d54d63c2a24532d7d04311b787
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:59 +0000

    makeup plenty even face building

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
