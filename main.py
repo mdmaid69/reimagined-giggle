f0a2b29f4caa5323d03b2068ae622c16bb2f25bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:52 +0000

    politics sure door check blind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
