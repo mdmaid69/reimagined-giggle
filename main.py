b3c435292a22ed2ce41d23ca5b7ea9035f4779c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:19 +0000

    thing exist group regard bright

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
