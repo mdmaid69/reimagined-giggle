c0fe203c0b8a9499ff399d9c0dfeb88459170404
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:34 +0000

    autumn especially change spring some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
