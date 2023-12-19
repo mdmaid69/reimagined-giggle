ed89e55751337ad7e022216fbd3f86d483e8a40a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:02:42 +0000

    information alive sink jump question

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
