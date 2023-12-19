399484b9ed87457230f3e57b056336de1eab2337
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:00 +0000

    trip deliberately train achieve work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
