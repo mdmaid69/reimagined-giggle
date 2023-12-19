14c48dc32be39cca081f9cac0c7cb75441fdfe6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:29 +0000

    circumstance shout block tread freedom

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
