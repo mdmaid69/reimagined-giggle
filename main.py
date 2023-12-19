debeb88cc83bd155173c790bfdfcb4e68ed9846e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:38:33 +0000

    nature smell suggest neither image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
