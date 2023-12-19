8026983f3dde6b8505bbbf2f3b19bd9db90e47df
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:52:11 +0000

    free education tap quit pleased

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
