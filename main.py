88bd44925f50706957f49e4e02a72be95512921c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:23:05 +0000

    offend finally slide odd unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_as_set(array):
+        return set(array)
+def subtract_numbers(x, y):
+        return x - y
