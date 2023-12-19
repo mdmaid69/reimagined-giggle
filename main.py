7b3b80bd02c4cbeee58fd124bb0147e82ee101df
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:50 +0000

    acceptable change brush guide raise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
