246e2a97701e440381f83b7ac66a6940c5a6706f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:42 +0000

    minute lookaround target feel this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
