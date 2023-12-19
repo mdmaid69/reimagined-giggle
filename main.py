ae61a8b4bb54d26743f06abe0613a3d36f2f607a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:17 +0000

    false strict convince really lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
