ca8fdc651064e30ca96584f66b8049ca5d604a0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:51 +0000

    attach funny wear read coat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
