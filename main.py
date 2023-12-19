56e57a7796a1e360ab3f77871abd59cfc7eb4fce
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:18 +0000

    offer that forgive dead spot

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+text = "Hello, world!"
+print("Words:", len(text.split()))
