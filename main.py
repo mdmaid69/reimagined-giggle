1fe0456730beeb76cd0f3a62ff19f36e6ead864e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:56 +0000

    mainly price use Hh run

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_item(array, i, item):
+        array[i] = item
