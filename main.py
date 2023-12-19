cc217b2a51ef360fb7869351936cbdd7bffabc78
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:52 +0000

    shrink 572enter reaction basis joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
