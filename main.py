5bb8ce99b720e9f09cd1d18a77a28a48db22e202
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:35 +0000

    backwards face fashion wrong position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_min(numbers):
+        return min(numbers)
