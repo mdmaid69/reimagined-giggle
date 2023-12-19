9644c37030a0d6d287128c2ec437f8f246e7ae9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:48 +0000

    wide second sea under shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_acceleration(speed, time):
+        return speed / time
