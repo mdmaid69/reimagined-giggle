90e6e08ff2c4f45524ccc90e76ca739832ced8f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:23:31 +0000

    pocket float doubt gentle sudden

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
