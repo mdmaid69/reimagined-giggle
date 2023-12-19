ff3966ca4fe738fefc63914f4c3300a3a9acdd31
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:38 +0000

    settle blame light I keepup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
