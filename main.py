fd005fcb651c72232cea7ef14d126d7883f9c1be
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:02:46 +0000

    loud board fight soon sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
