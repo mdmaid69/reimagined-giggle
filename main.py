b5940b9d53c92e63d6eb48f57bff2db92fd66ae3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:01 +0000

    closely figure ticket ahead colour

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
