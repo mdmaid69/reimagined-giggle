1f7f1b30aa29d6868e35578bfd2f0dcca55e12a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:11 +0000

    quarter fond step crowded capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_list(array):
+        return list(array)
+def count_characters(sentence):
+        return len(sentence)
