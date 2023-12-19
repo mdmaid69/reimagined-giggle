43e3ac83ee230bb6728055e355dc36e22be4068a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:13 +0000

    bad as turninto like statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
