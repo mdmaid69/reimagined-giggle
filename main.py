ae5d327ae3feebffe1cb50d7b92868835697136c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:20 +0000

    skin sharp trip scale Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_item(array, i):
+        return array[i]
