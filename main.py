1dd025c277ac8a991a8f7ad4c9e59c9dbc429945
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:52 +0000

    proud that school that demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
