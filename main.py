378f8f1ff2114a6c6e4f0f4557df6b9f937f44aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:44:55 +0000

    turnup look concerning shoot ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
