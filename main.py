72e806abd0e158923fe298013e6eeb89a9a83535
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:26 +0000

    high weather beginning hit stand

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
