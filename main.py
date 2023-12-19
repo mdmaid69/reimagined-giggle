dc72ebc618a66d59c6589cb4e2cc983e8e01eff6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:41:17 +0000

    since music godown neat pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import tensorflow as tf
-print(tf.__version__)
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
