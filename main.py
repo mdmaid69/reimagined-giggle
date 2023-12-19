50014fa4d155eb648737ab2926b89a9e9ef2a09d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:47:28 +0000

    equal regret excuse somehow cool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
