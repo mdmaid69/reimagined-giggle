80474a40d6c173218c28f53026d13ba32dc21412
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:24 +0000

    laugh determined Ll recover could

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
