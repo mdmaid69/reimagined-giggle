ee95fb0eacac4d469a1c6b4d49905c039c2273dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:25 +0000

    guess morning interfere improve workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
