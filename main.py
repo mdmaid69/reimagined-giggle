ac05635a9326de2f5084720690263d7d053ffb58
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:33:11 +0000

    anyone water breakinto image guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_sign(x):
+        return math.copysign(1, x)
