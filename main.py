befe8f3a1bc961ccd6d08b90c681e4635e2e41dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:23:01 +0000

    relief ourselves sticky pile discover

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def reverse_string(s):
+        return s[::-1]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
