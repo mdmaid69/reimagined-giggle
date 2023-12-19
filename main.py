00d890ced03387de3d2708b1e17cd83fe59fa7c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:42:40 +0000

    waste final ought Ss vote

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def divide_numbers(x, y):
+        return x / y
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
