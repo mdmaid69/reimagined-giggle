d907d886a8ec01c36e8fb315bd5ba356f3ad8687
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:20 +0000

    suppose grateful expression delight war

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
