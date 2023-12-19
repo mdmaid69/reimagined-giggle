05beb1075d1a54ff854816630a9f614e41f1dd9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:09:33 +0000

    alive steep thankyou getback common

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+print(re.match("h.*o", "hello world"))
