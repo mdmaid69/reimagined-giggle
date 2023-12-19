4d463a3aa55e32e0f483d331ce79a0c5f6fee2d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:22:54 +0000

    pour practise clue themselves mind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
