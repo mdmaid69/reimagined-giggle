e268bfbaaa7e4cbb3b95ca210aae58066b4011b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:33:15 +0000

    through eye think concentration cheat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def convert_to_octal(n):
+        return oct(n)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
