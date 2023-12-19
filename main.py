3c2de716dad27c90576153972d86ab6ca7a706d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:42 +0000

    rarely trick eye attach male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
