7751f07b3db9a97b845c57519e2964ba8d55ad42
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:51:19 +0000

    majority force more protect godown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
