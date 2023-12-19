9fb928f8e6177cd5e1c83e4b7a5785e35ca525c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:26 +0000

    card apply they centimetre himself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import sys
+print(sys.version)
