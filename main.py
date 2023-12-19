1af73b6986fa8b064bffaebb62ac133f4f177868
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:21 +0000

    performance gather giveaway doctor class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
