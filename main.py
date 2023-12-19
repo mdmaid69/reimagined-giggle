ad026c69d499169851aee47b85a01a6f5ec5839c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:44 +0000

    rate bridge careless afterwards passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
