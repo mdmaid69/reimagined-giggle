9bdfb2f56b6965f8d0d5bf4c43a13fb8a1064b08
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:09 +0000

    content quit fire choose build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
