d8a72a7bb84db07acae1e51af8e6c36a214e3996
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:51 +0000

    apart hollow thing disapprove less

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
