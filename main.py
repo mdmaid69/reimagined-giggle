e8f5b6f64682d6a46a57229f14fe9d7b902c4548
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:50 +0000

    smoke interruption stop dress sight

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sys
+def exit_program():
+        sys.exit()
+def calculate_acceleration(speed, time):
+        return speed / time
