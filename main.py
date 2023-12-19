4a4e1d2e9b54c5d6b0415412b29e574dd51cfcde
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:08 +0000

    quantity crowded dance bar mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
