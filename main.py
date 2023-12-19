764aad8a151e17073201b5d97d6f4d6c6268a417
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:38 +0000

    step discuss something week winter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
