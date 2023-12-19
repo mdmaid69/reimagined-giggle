8ed413c48080d12b699d1e85dba02679e065bd98
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:10:24 +0000

    invite sound 564encourage wish afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
