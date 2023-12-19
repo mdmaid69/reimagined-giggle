40c3ed7ff7bef4f31c711063d45b3d5e933fc82e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:05 +0000

    future equal must spare cook

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
