3f228414312224d949e6e620b3d8866f850169d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:00 +0000

    task apply see morning patient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
