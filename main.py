0273f40804317660fbd961bc501659d869359f1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:29:48 +0000

    sort old over quickly suggestion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+def count_words(sentence):
+        return len(sentence.split())
