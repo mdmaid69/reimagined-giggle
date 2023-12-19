ea81f9cbc42740465756cba9bfa67a712c7540ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:29:17 +0000

    receive dinner message live separate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
