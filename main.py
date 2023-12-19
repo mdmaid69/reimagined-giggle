3a70a598ca89fa28759ec6501cc2237159a1a25c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:56:51 +0000

    slice surprising order deliberate quiet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
