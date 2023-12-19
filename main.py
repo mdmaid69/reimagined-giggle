ac8dc5e7a84608a3dec55170fdf1e7e1b0a61459
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:18 +0000

    breath only standard hide square

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
