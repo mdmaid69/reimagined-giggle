b0fc8efdc090317a3d28755e801c39d8b31e20d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:19 +0000

    572enter interested personal clever upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
