f2a10ebe16f32aaa3bbdd0c7483c58defaf1b7be
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:48:04 +0000

    copy plate money picture considering

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_speed(distance, time):
+        return distance / time
