a16b0531edab7234a883dc0db6afb0d6ee17102e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:39 +0000

    cry fairly wipe pronunciation favour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
