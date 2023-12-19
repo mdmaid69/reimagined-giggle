453d39d238dc62df7ca4143ceb837ea5f03dbddb
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:44 +0000

    along environment holiday crash stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
