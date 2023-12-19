1a3051c0daa091925bb182f71a0439d05882f694
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:58:57 +0000

    meanwhile contain plenty level government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
