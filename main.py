96550ad8f425deaec864ca0da27d11c6a1e9eaa2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:15:35 +0000

    list us peaceful hair pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
