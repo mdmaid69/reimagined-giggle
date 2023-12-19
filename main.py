70a38a4bda1f538bd734b56bb1d81f56b2639d26
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:19 +0000

    light rise practical protest lie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
