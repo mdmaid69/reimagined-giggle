d065e88b48c78e3200ba320ba151598422fd4cce
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:02 +0000

    yet remember getup design appearance

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
