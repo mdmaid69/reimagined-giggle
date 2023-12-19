93eb58014172f9ad86e9b20b94108a8ec255d04c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:04:20 +0000

    little occasional ring estimate pupil

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_even(n):
-        return n % 2 == 0
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
