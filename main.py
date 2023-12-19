2501c8f6f7dd99be23d0f0ed61a1fa2dad01883b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:39 +0000

    they quiet fierce solve shoot

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
