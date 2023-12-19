dcdafb23ed1f5c26c58739b621e4bf88eb99bf11
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:18 +0000

    fast refer slow goodbye stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
