e9cfcccef251d542070706f802d96647d8a58e67
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:21 +0000

    score operate debt afraid experiment

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
