e4f3aecfcb3b613727ed41baaef43ed0d308ce04
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:34:28 +0000

    certain office refer come point

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
