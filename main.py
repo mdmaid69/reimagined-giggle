250153e17ae8d825e78939d7713a6391dbe23773
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:34:05 +0000

    offence somebody regularly opportunity care

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def square_number(x):
+        return x**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
