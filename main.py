028de7ee4e23b449ac9c3c411e2d3164526a0fee
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:56:45 +0000

    again fix sticky nervous block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
