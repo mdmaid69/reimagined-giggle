87c8189dc669acbfdf5629d42041141fb784acbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:37 +0000

    lookback(on) work recently interruption pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def sort_list(lst):
+def sort_list(lst):
         return sorted(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
