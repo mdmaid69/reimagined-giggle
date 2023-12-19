65fba4f5199a7d78cf8213fdbedc13837d466114
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:13 +0000

    wave occasion tear develop main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
