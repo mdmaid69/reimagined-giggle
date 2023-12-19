a7b14d96d50b6fd9d31a40dd4fd7bcac982c2d43
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:12 +0000

    dress present progress care survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
