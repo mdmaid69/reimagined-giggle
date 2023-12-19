d72e1bc9a29e139855cf548634989d11d9e38cc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:45 +0000

    remember turnup achieve unless square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
