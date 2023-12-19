4915408ccb3a665c19bef607cd7464eebf79c888
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:00:41 +0000

    shelter impressive light off form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
