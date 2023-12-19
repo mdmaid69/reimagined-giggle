dd4413a256d57e612c3e08165e8613fd2577cc0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:46 +0000

    because open comeout cum relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
