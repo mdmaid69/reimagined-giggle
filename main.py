7f664a191da1504a598530a476d9903af38a2899
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:42:29 +0000

    difference godown talk will joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def check_if_array_contains_item(array, item):
+        return item in array
