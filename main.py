1b3faf644e397a1cc434bc3669c34bb2583d691f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:32:00 +0000

    floor comefrom expensive should water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_index(array, item):
+        return array.index(item)
