4d3d2ac6f90ee561feb8316e15c70d341c72fc98
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:47 +0000

    conduct school soft unusual turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
