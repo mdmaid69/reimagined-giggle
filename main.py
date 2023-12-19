5b45c03b4cbe135fc15fb26fc5670d4544ceb7c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:51 +0000

    bunch move false issue more

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
