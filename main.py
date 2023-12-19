0467c1504e2c71332cf25f8e1ebfb911cb4dd87a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:28:44 +0000

    smooth hope punishment underneath achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
