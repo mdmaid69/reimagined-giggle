d917817cf4cb6cb4761ce1c220e5525667b531c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:40 +0000

    bar better final forget guide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
