bdf7775087df5bce07567b66f40a615657df87b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:46:56 +0000

    improve reaction work hold opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
