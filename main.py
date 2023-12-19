204d401ee3a00863bc8db6ee7ecaa9d758f481c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:50 +0000

    type pocket proof experiment immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
