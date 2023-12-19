7bd4aedeec333a71ea4cab4a915bc2297dd37d92
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:15 +0000

    regular spray threaten year education

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+print(json.dumps({"name": "John", "age": 30}))
