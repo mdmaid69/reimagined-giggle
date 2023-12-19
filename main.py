b9d342e2c2f6c971692cbd2b720409a2aab699b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:19 +0000

    roll through instead use break

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
