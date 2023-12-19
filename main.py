168682b075ec12de94017c466d6e8e20b3beb370
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:04 +0000

    wheel left away talkback estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
