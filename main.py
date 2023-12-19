f5ebf15891461129a6da39f4fc2d85fa856c3b75
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:01 +0000

    autumn his nerve advice less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
