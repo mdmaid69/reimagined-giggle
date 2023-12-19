9d26f248cd2b301f216364d1f3962e52a9600bde
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:16 +0000

    strength over control concentrate ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
