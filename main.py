5c970c68663e07f5fa8d043e470751688f7a91e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:59 +0000

    guard discovery burst real come

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def reverse_list(lst):
-        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_area_rectangle(l, w):
+        return l * w
