cb5fe0fa3c7e473d9da2bd9ba74198b04eb1bb90
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:53:38 +0000

    post takeback goout street sick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import datetime
+print(datetime.datetime.now())
