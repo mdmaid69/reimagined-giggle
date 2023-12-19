9a85d7e424edc3d94bbaa3498043f4727d858b89
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:03 +0000

    sick weigh least extra sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
