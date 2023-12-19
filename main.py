175c569b5bb72ced58f007a50b227fc08ab9ffad
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:20 +0000

    occur sure middle claim skirt

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
