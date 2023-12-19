2b2967b716f322c2a39fccefb1dd7ec84d2afc84
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:53 +0000

    big object children gradually embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
