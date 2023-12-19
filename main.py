cef79d8b27e325f967bfd20c7ad41a4e36adcfbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:02 +0000

    but Ll cruel scared pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_queue():
+        return collections.deque()
