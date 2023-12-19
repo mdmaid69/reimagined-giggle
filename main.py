bb5a65ed640fbfb9ecaac0006647cb0f38c935fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:11:46 +0000

    kind deliberately vision come open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import logging
+def log_message(message):
+        logging.info(message)
