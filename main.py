30ad418c93ab0db92d26ffe97104fefe8717e644
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:00:24 +0000

    change apart well second murder

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_max(lst):
+        return max(lst)
