019f375c1ba5039274ec16474fe97674ae28d93e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:32:02 +0000

    dish warm drawing yet fair

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
