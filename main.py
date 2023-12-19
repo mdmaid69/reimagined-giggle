40df47656027116ac8e57a3093fc34cf3d22a205
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:05:07 +0000

    rough luck attach them tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
