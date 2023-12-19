a6f20bc1d6abf7c778ecd8dbc4f48dd26f91cfe6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:49 +0000

    contact tear excuse ready house

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def convert_to_binary(n):
-        return bin(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
