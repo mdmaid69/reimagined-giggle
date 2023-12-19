bb181f08d0f1afa9af157b06986c637845b5d87b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:00:37 +0000

    anybody off switch test Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
