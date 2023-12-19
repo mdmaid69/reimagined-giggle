9df3a5d40ad92345f30e394f2b58da55986e01aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:49:32 +0000

    splash claim exercise smile solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
