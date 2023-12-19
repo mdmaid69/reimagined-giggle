ed7398ecb1e4af86e8b7e562692e19dc74342427
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:33:53 +0000

    short stick scrap flood conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
