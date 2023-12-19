c1d700661700ee4a09e6d303160f21576bbf4707
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:45 +0000

    least switch inquire nonsense often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
