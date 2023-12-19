00bc9aafab18385e3fa0a11c809599ce2c76e2e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:00:00 +0000

    moment believe makeinto string usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
