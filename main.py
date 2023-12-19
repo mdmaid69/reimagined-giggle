589edfb1109337ba56c17657297b2c8d94cb19c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:35:21 +0000

    comeback sure in summer them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
