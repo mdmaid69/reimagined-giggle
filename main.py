b9a38dfd383a83832ee034f19556a094c8040283
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:37:14 +0000

    sleep slice goup pool spoil

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
