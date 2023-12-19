b0a2d2429d9858a8be7b40c0c3983a0f46e8b1a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:39:34 +0000

    be hour run sound back

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
