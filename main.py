bf21bfc9911bbb7d64d87651068dfd5b78b50767
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:29 +0000

    extremely twist Cc limit measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
