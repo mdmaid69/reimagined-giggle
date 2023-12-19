01d07e68b6e56f15fb802fdb5a3bbb63f345f2cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:01 +0000

    or bringback glass recent rate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
