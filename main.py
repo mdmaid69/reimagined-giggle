194924ed8c9b0d6694739bc08246c0acea6d670d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:35 +0000

    home ready means start slip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_even(n):
+        return n % 2 == 0
