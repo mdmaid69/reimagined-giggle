70b87a982746e688cd93596b632cc27b7f1a9fd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:43:02 +0000

    shake rob study mistake company

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
