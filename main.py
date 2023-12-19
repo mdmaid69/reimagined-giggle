c4c453fc1927f1584c4586ab240a369b26dd8ddd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:21 +0000

    town tough refuse run generally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
