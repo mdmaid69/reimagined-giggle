a47dcef56418d81bc922755f53f9f8f40fc9f1e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:06 +0000

    bath dead smart fight like

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_min(lst):
+        return min(lst)
+def find_max(lst):
+        return max(lst)
