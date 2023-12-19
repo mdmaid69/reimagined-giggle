13dd6e58919f84ed08d2729b26c7a3674dc820e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:48:40 +0000

    scream crash field nigga follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import sys
-def print_python_version():
-        return sys.version
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
