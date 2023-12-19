965031aca9dc578ef220c4576bbb0ff844dbf406
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:36:36 +0000

    handle punishment charge murder lot

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_work(force, distance):
+        return force * distance
