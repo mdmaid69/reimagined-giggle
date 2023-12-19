4e82e951b8d1e8aa0310c259a2263464e459d9b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:15 +0000

    get swimming positive eat let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
