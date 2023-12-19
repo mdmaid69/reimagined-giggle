753321d8546a9b4d656903870416a0deda155c02
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:08 +0000

    accordingto sudden takeout exam long

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
