47e65ff5b3ac5a18f05c7efb2bb090fab9705f8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:35:02 +0000

    accidental clever whenever bread door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
