e2934ad8ec7e4a7cec27849de62820752e21eb36
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:19 +0000

    repeat pattern turnoff mind respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
