2256f2834cc09da711c05964332e9c05fbedaff6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:35 +0000

    Rr half comeround bottom voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
