7a188e7a92c2c8ed9839b24d58604418bf17d017
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:17 +0000

    legal worth modern concerned hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
