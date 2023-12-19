fd8e314563ae43c4b428d374f3618b687116cf90
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:26 +0000

    sense also shoulder workout follow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
