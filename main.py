3555fa454e9b27b892f0aee9474b433af0275b02
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:00 +0000

    shop bring holiday dress influence

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
