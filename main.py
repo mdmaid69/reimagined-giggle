e796720a5301a1cf66a1aae100da8d4c867f6b33
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:20 +0000

    head skill shall spend level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def exit_program():
+        sys.exit()
