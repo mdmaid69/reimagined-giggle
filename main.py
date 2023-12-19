e14fc5ec7abbff9c5768b0f27866b09dc49c0831
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:57 +0000

    ocean able hold need ahead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
