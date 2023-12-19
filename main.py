70dc87e89aa9124ad529a6f16c1a35c878d8d7b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:50 +0000

    strike relationship wherever hollow description

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
