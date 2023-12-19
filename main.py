6bdc846a08b402112aa9736528169c851cec1960
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:14 +0000

    news simple name call bury

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_typecode(array):
+        return array.typecode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
