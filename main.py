7ee73bf9d221cee4f0fad9fdb9786f6c081ba299
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:14:22 +0000

    cost honest getaway movement long

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
