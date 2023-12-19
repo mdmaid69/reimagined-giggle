bd461e9f5ca5c143cdf8e3903c1746c721d60e90
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:46:54 +0000

    apparently basically forever backwards perform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
