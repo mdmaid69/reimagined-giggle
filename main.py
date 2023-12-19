4e8a791e260784a86004280eb6dca4b150489d14
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:37:24 +0000

    whole for convinced spread left

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import datetime
+def get_current_date():
+        return datetime.date.today()
