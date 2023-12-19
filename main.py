2183995b081aa79525895ce391fe50d2b02e006f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:27:55 +0000

    opposite black no he old

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
