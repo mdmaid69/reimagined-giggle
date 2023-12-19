6e7949e1c9e3caf9c263decd0f765bad884dd660
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:54 +0000

    benefit lesson wait difficult limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
