e835f27938f86ba783c42bf1b01913e93f0e7e6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:28 +0000

    count mass idea bring strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_work(force, distance):
+        return force * distance
