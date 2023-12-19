2a4487c56ebf58019b482cdface872add227e036
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:30 +0000

    today team mix knowledge comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
