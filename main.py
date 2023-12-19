cb3ce966cb1a32823c32d50050311a007f0ddf20
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:22:35 +0000

    destroy lend mistake avoid rise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
