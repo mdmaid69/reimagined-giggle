5e0b8f3e44c4d6b9438930ea99678224edc1efc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:44 +0000

    police arrive total deserve visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
