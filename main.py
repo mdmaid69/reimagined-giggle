0a81216b826017f05a5f0e541b1f371670ca9733
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:06:19 +0000

    step whether soak develop store

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
