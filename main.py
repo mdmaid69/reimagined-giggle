835de2e629f71e8fb85617136f9097cddf2ccc2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:28:54 +0000

    style section too else centre

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def multiply_numbers(x, y):
-        return x * y
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
