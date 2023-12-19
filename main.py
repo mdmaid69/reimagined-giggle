8abe6225faf587dfa8fcdf76e6b96f4ebfe7746d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:53:44 +0000

    its lock gather sensible week

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
