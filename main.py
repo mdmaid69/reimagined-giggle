cf405309c8d0201681831289004d36a8dc5933b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:04 +0000

    determined giveback basic present trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
