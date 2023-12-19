91eda4408bf4e717b98be1b821f6a44996bb91f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:40 +0000

    union suit bit dish lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
