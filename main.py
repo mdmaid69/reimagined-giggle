bd39ab72f8dc8d1ed0b7728571654beb7515daa1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:11:51 +0000

    school suckme create die public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
