49407655c24c74459ac9fe9aab19449dd2662fbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:46 +0000

    voice nice lookafter week fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
