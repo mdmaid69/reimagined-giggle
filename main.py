978ebfbe5e67edde8358c0b7800159389c6a7212
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:58:13 +0000

    workout keepoff worst score shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_rectangle(l, w):
+        return l * w
