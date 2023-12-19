667814252cf47fa52703179dc7a0ead5bc5aaa4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:10 +0000

    shrink beyond star pleasure land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def find_min(numbers):
+        return min(numbers)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
