aa3b7cd6e52fa6f4c0e66d60a560c8daad9e2c8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:51:52 +0000

    bury punishment threaten fasten store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
