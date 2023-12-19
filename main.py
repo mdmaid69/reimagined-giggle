c2e8b66b539e9fdb7bcd49523630966d22b74705
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:49 +0000

    operate attitude Ss invitation bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
