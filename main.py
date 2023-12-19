967f4a495401f6ddf5dbc67f825c137d578f28a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:19 +0000

    back culture unkind boring drink

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
