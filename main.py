b2b4d149da4a87f2d10cee40ca9a6f21d0dfb4e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:33 +0000

    hold mind patient angry embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
