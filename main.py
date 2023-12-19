32bc95a051b9fbe5c113df0c4cc91be28e527955
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:27 +0000

    debt round nurse yet team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
