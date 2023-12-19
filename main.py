03a92467e2fc373484bc3efd901e2d1850a089a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:38:23 +0000

    famous idea exit secret land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+print(os.getcwd())
+x = 10
+y = 20
+print("Sum:", x + y)
