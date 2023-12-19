3c2108226016a0f5a5cecd18e799150c9473bcde
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:46:33 +0000

    evidence more machine table this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
