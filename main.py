3686a960048601bbbe19be869d1b3c01ef45e01a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:15 +0000

    open party key even modern

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
