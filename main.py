78a3e8a95955a86dd201a441551e56847df71b42
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:25 +0000

    organize lose goodbye late hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
