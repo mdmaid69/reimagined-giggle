9e75a8ffb57e8adcd75260c1323ce6493df7e968
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:21 +0000

    result keepoff bus spring attention

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_power(work, time):
+        return work / time
