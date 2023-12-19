72fb001774803e83987d7a360e161125f70d9b44
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:33:22 +0000

    angry worth cough independent hard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
