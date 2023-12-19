167fb2e47ef0b7b9e4f5b75b58a10f815780d3b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:38 +0000

    off wealth combine attract miss

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
