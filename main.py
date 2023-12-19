6db9a95b0c02bcd755083bc16629f3a1a8f767d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:51 +0000

    while lunch emergency importance fly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
