27856ea8774cc8be491e7fd57af3abbeee27429e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:41:21 +0000

    frighten work quick charge offend

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import datetime
+def get_current_date():
+        return datetime.date.today()
