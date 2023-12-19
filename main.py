49bdb93bf613a653d50352465b1178f32600813f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:48 +0000

    row emotion switch hang among

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import time
+def get_time_since_epoch():
+        return time.time()
