1aced55c8b1cab9a8789f803e3862bd3737fa63b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:55 +0000

    argue quiet event level imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_index(array, item):
+        return array.index(item)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
