cf29ae533fdeb4cba94a681cec4f40ac4df44853
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:26:20 +0000

    bye organize plan promise copy

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
