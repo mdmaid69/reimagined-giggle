31b60ea0d4bcded465dc607f0d5a48b9219ff2c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:41 +0000

    police inquire else position mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
