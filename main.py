0e28d777eac52561f8635c6b036e774156904c40
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:04 +0000

    side country translate want move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
