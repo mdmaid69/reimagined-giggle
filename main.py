f7e25f43ea2a3d13eecc367af353a0e63b901185
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:02 +0000

    shoulder error lonely city level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
