c0199ae791c829cc45c5c34268757522a7019c34
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:38 +0000

    immediately not connect swim skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
