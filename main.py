652aad3e59e85af21851e907e0b5e000c1c580a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:22:13 +0000

    claim score wave wander runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
