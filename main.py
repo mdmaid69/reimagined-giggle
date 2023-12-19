fa9803e59dff0241e4892813da255cb5ca97ac28
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:43:52 +0000

    big cruelty everyone night accidental

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
