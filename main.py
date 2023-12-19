59116053760442127c30ce6f27d4cd014fd3d447
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:52:47 +0000

    steal breakfast over reject centimetre

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
