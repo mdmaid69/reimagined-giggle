7f7a7af2c2ca40af8858af9c1826605249667601
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:25:51 +0000

    bringdown sound rid bang turn

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_even(n):
+        return n % 2 == 0
