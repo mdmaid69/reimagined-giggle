0d14e85538339a0bf14445e43b04e846eddae6b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:08 +0000

    bag relation contents frightened or

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
