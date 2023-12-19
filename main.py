84c3b5aac750642eac7460762379664e368a7139
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:55 +0000

    memory step tap seat getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
