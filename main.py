239834ddbdb00e3678189927a4705ceacd0fd100
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:42:44 +0000

    freedom bill rarely ball error

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-def convert_to_hex(n):
-        return hex(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
