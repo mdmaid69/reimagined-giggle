00b36f19cfe51c0527a6fb4f46df24780d9a2cdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:57 +0000

    reply style bar pause neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
