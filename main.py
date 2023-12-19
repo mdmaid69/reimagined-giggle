ddfa0c968f982935712efc4f7488cbe40e71e9a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:39:43 +0000

    probably odd basis joke declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
