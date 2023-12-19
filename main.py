216f53ae94476bfb458b3464fd5f29e58bd681bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:13:58 +0000

    record after game whoever awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
