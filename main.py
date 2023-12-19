b906b9eaf73507d6699106eb5f33f86fcd6e77ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:50 +0000

    drive drink refuse excited offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
