6284eebd82fc6aeb5a67d56925bb6f3595056746
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:47 +0000

    open must return model play

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
