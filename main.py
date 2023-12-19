81cd9a6d0d3ad312e52138d2a75ce728146ac5da
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:53 +0000

    possible embarrassing manner freedom well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(lst):
-        return max(lst)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
