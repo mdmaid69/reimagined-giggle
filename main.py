cb75d22df13659bcbafc0f2a49b1e63c7cedbfa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:36 +0000

    squeeze phone single consult less

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
