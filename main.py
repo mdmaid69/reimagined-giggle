a9410bfcd44b4b96ef81b9a30a57938db5a0f372
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:41 +0000

    strike small then silent mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
