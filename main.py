28591f0735a8c21b1abccb7b860cdab454c68e1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:20:08 +0000

    from like frequent book upstairs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import sys
+print(sys.version)
+  def cube_number(x):
+        return x**3
