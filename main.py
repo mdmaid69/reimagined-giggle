308740ace8a8591283b952f6943a4344d4979ac8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:20:52 +0000

    record refer embarrassing adjective need

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
