fa30f7504c29ca7f8a0048daa46124ea7eeb8a80
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:54 +0000

    bother pause large pain side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_set(array):
-        return set(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  def count_elements(lst):
+        return len(lst)
