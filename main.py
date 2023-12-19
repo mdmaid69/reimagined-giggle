e45157ef9843d1e17e7a23909ddd5ea06609071d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:23 +0000

    and flood stop swallow spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
