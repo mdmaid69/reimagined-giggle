6882061e50f18523e96e61ff3b85f72a196bc241
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:08 +0000

    conclusion pretty ticket myself recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
