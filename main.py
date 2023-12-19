76d5b834cd72337230f31f96afc3c2226f726598
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:15:06 +0000

    mistake tooth scream cook compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
