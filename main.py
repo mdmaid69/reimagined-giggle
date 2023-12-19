6fbc685aa7c03f9dc8347dab130d0f4000a335c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:25:28 +0000

    card excuse date present only

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
