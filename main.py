afdfe9dadb6f75c9ec2db114f8d847e0465a73dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:32 +0000

    method broad special attitude pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
