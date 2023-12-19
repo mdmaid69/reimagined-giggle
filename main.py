ae170e8720bebfbec909ab7b114a9902ada86e34
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:28 +0000

    bye thorough relationship field sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_cosine(x):
+        return math.cos(x)
 import array
 def get_array_itemsize(array):
         return array.itemsize
