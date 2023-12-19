0f17547120aa54366a45a0bcdf20bfb1ca516807
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:45 +0000

    owe Yy nor splash back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import collections
+def create_stack():
+        return collections.deque()
+def is_odd(n):
+        return n % 2 != 0
