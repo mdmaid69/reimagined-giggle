71e462133e070c2c5eaf24ad6408262fe7d48d58
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:01 +0000

    of both exaggerate describe people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
