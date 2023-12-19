dd9a54ed3c2d4a66aef5ab33fb683439ff36406d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:43:51 +0000

    motion scream kind try disagree

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
