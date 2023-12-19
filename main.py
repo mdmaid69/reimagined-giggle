270a2f87509ec6ef80d50c72ec8d24ceaab3d064
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:47 +0000

    neat shut sleep intend beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
