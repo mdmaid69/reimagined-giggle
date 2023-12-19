9ef914ca28661b4b47670e1c209f7731ba298534
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:28 +0000

    spin coat fetch emphasis over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
