e1fe6c963434731808d73280a752437e63ad1b5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:44:34 +0000

    compare right hurt explanation mean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
