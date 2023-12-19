f5572e1ec20334c1cb73fd0bc5f3acbf9173e36d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:20:34 +0000

    respect round invite serve passage

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print("Hello, world!")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
