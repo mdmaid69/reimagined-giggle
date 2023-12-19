8a22d8abea994c0df3f68533aec6205142c5e8ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:52 +0000

    priority prefer duty nonsense method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
