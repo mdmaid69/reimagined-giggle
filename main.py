412d58508d207a920ada6a2a66fb4cf517603faa
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:01 +0000

    mother concentrate 566end under dry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
