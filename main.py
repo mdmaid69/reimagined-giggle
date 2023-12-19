374f50386461d8c3e27ad326079fc2b1e16de1ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:54:34 +0000

    phone system Kk delicate although

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
