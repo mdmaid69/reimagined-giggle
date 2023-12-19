dc3c59113f2eb36bc6186b6d066850c2e74da2bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:13 +0000

    task hope boil still war

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
