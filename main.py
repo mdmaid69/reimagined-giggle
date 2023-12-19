f3be9a3142806b43f99b54392840dc74678d4025
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:43:17 +0000

    frame production petrol bored throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
