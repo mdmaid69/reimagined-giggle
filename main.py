dc48d97c72d9663299e82d44cf42d5a37c05999a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:19:14 +0000

    good smooth unfair gather unless

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
