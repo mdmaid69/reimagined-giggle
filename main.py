9c0764c69850a0c93114cd9e025eef8fb61f2aed
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:41 +0000

    mixture crime Cc bringforward excellent

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
