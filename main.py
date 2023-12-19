87020cc12857ce877058f42faeb1c2e9f52e8bbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:51 +0000

    juice of motion bridge offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
