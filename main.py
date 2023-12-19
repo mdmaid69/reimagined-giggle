ef86f8f435ff470741fef085a7c3acc480333e0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:21 +0000

    voice fence ill dig legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
