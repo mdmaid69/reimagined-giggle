552632319533aab9c5da0285e9c8412956325c02
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:27:16 +0000

    clean complain peaceful wild pretty

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
