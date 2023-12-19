edba5c6a9109f8a2b8759137e004ae6d9d6ea721
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:33:10 +0000

    concerned in refuse such various

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
