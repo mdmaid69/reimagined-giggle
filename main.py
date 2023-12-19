60427ff14f22bde1a10ad58c6496e42af4e68322
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:14 +0000

    sweep fill drop crowd central

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
