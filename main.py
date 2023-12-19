b4268154f44767bb76a0733a396758f6fb3661ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:09 +0000

    calmly refuse friend life feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
