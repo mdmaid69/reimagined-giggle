5f8e4009a80a386566ecf645a1277075d5431fcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:19:30 +0000

    sale bread deliver want oil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
