01d4f7d26dc89e210f87be9cd8a7849afde15090
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:41:38 +0000

    crisis get prepare design stranger

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
