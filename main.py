71f5a5f92eff45bff1d2c68cd240ad92990d2b6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:24:33 +0000

    each eat anywayanyhow commit accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_speed(distance, time):
+        return distance / time
