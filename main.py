8e72642278600e58ad203af8e847bccb55c3fbbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:22 +0000

    clear switch break spill surface

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
