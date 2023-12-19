5581df6901bca8c11f5edac790df87e5ccb3677e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:27:55 +0000

    through small least physical can

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def convert_to_hex(n):
+        return hex(n)
