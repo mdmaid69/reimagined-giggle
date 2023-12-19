4e778441b85b6e31e563a5aed49f4b102680017e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:35 +0000

    our sink tear sensitive up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
