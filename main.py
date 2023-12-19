b955eccb813d1d0fcd27a0f85bd6ceff81560638
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:07 +0000

    weekend design lose wander face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
