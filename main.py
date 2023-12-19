0fa408a68a090d2e8779fd6dd3a5731224a67bc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:07:38 +0000

    compare scheme takeover deserve hit

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
