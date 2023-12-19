f6bccac250e027a581001b87f8fd675ad64c49e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:40 +0000

    tell replace beat rare someone

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
