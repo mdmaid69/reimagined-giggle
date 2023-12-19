f6cbed8b3512a0dd1efc1ff292109bf8c9d7dfc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:01:11 +0000

    finger number dry workon bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def exit_program():
+        sys.exit()
