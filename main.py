ce55a24336dc278ac3fc6ac6f097f6a7d9cc642a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:06 +0000

    bang spring progress bang dare

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-text = "Hello, world!"
-print("Characters:", len(text))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
