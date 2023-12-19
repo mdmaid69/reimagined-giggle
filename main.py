5e819c6516c32f49c206c3ad600fb7b0063f68bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:31 +0000

    worried friend confusing turnout whisper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import datetime
+print(datetime.datetime.now())
