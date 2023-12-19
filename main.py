a33b5cdc4b039f8a5985131bb296295155d7be30
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:20 +0000

    dream borrow pure firm attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
