8bb6dfbcf2a53f26317f65a873eb5cc3b10cb472
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:43:28 +0000

    scale so light modern strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import os
+def remove_directory(path):
+        os.rmdir(path)
