084cdbec230783ca3c26ed57733cfcefd94a14df
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:16:54 +0000

    innocent Dd neat prove may

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def square_number(x):
-        return x**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
