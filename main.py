20d65f9bf3476bab379d2dd3ca091876c875b5c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:01:51 +0000

    history other petrol leave task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
