e875c25a079609ee4901e0056c031eb354705f66
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:56 +0000

    part afterwards dance mysterious contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
