f8d179e463a7271ac4a9e69d30d5c5f18cda9449
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:51 +0000

    nerve slow neither of advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_length(array):
+        return len(array)
