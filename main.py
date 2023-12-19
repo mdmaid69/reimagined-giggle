fe2f883a34b0e0170d01399e77c082289ccc9f0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:17:29 +0000

    many sad pipe worry period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
