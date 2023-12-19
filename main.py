35292158f626eb46d13117a1c7efc657070ad491
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:18:41 +0000

    ear dangerous stir nearly settle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
