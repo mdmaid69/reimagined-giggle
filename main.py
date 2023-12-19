9f818c517dacee0e2af5b8055a8e8658985b22a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:42 +0000

    Dd survive question throughout shall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def remove_duplicates(lst):
+        return list(set(lst))
