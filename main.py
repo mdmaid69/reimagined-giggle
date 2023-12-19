0ff05bcd9ebfa3fe7c52b253bdb39aecd194a71a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:25 +0000

    dry ability meanwhile worry relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
