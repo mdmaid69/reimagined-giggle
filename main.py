9997ff9f09d5ba220c45b925b7930b1e62f1e33d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:10 +0000

    thin appeal afford properly quantity

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
