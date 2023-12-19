a7396500dae6d7552b77e777378fe791ee249f41
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:15:21 +0000

    bus separate to start lean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
