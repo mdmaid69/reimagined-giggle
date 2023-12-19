ce084cf18b8308a82d2d903268c3522e4d5ddc67
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:41 +0000

    weather reject actually bad manner

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
