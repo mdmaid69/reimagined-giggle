129bbf9984acec72765c6bb3275e2e1f9d9de5a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:39 +0000

    keepoff boil defeat tired man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
