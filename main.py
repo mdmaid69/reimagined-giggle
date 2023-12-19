dcc62c26b7645617dc74ef33b2500e1675566950
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:44 +0000

    comefrom few discovery bringup please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
