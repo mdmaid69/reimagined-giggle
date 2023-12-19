b7cec58a082b948d92f009be894c2eb32adb58c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:10 +0000

    land shut contact tight problem

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
