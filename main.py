a28146c53202dd6ec4272562418eb07c45c0d3e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:39 +0000

    zero warm during deep think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
