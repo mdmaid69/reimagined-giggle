e8d89ff9a0441ac8e2975bb1afccb2a120665bd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:55 +0000

    guilty clean block miss permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def remove_from_array(array, item):
+        array.remove(item)
