3939284f33e50a885f4e49c533f99a71b190b8ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:39 +0000

    cook fire represent whisper justice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
