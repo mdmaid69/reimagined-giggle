d28cbc27ca43a026a35c1d736f08d4da74f587d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:00 +0000

    ring matter talk match chat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_str(array):
-        return str(array)
+import tensorflow as tf
+print(tf.__version__)
+def divide_numbers(x, y):
+        return x / y
