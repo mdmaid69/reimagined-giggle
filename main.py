434f33a9f0f97958d69a8daf676042bff825c5a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:07 +0000

    slide shrink damage kill circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import datetime
+def get_today_date():
+        return datetime.date.today()
