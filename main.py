c62922c4b8eb23f3e2657f27f92ecce6b595802a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:08 +0000

    pass study minute spot fasten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
