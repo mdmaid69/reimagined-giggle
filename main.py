b05d71626b0e823ae9f9a997f0e011bf7aa8e517
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:46 +0000

    relative else 572enter sure pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
