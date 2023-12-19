928f1a866eeb6416b0b3c3db5627b4215be74876
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:04 +0000

    gooff choice famous refer declare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_hex(n):
+        return hex(n)
