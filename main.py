bc96a444b1a275ed92034081487d15da63a81e68
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:47 +0000

    afraid invite dear complain comeover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
