61770691acf58ab7b0436ba9cc3cec19e7f46969
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:39 +0000

    black takeout turnon school act

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
