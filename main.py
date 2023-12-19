0d23294de8e0afb1ce15d9d4639543dae5364fb9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:48:58 +0000

    study cinema opportunity willing delicate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_length(array):
-        return len(array)
-def find_min(numbers):
-        return min(numbers)
+def extend_array(array, iterable):
+        array.extend(iterable)
