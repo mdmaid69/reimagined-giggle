423792534c3f7fd16822f77721f3e8e3aa50dbf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:45 +0000

    hour little region even willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
