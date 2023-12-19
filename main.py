8636c1cb274605b38495f8f5abcdd7842f0267b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:33 +0000

    major partner calloff breathe turnaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
