0d61184bad80b11f74f53d9bab0c76b7c6bd825e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:27:38 +0000

    build reaction deliberate number pride

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import datetime
-def get_today_date():
-        return datetime.date.today()
+print(datetime.datetime.now())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
