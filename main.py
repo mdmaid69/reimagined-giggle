eae0500c48d6a44ca6bf3bf366465c4a73227d39
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:53 +0000

    back bottom goout emphasize noisy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
