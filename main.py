5db97539f83042315a9941d792fe7ccac0514b84
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:44 +0000

    bunch risk achieve tooth scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
