1686857572767a52e2a6a755996ce04db11ec525
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:07 +0000

    crowd female repair impossible scared

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_even(n):
+        return n % 2 == 0
