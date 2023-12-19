b88e81bcc34781e1bf225a3ac1e85b20184dce78
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:41 +0000

    turnout stare past nerve disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
