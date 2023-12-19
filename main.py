fd80ee66290be3be3a460064f04ebc4238e37a91
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:26 +0000

    crash abroad trap delicate me

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def add_numbers(x, y):
-        return x + y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
