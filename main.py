e355fd031d5924405cc2ff4241b8988c215e27af
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:31:27 +0000

    exhibition hurt wave among turnout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def reverse_list(lst):
+        return lst[::-1]
