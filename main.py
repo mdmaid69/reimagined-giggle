4f87f6904106b1158d8d2158a3bd478e47d216c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:50 +0000

    please watch kind quick off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
