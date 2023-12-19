3c44e68497ed71c8525cc06e0ad32e765b8f25d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:54 +0000

    that satisfactory complete experiment offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
