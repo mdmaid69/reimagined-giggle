500feebc80ab3de35b121446dd05af3a1c10612c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:12 +0000

    reply ground grow rarely nigga

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
