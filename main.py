f1da41d161107499b5a4c8555f592d9a3442855d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:12 +0000

    pair ball dark goon smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
