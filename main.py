2d99e0282c13275b7039f525ada9602b78aa0466
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:41 +0000

    post pure comeout rubbish stand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
