66814a5df09a743396e596a14041ce4bac5150d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:34:44 +0000

    life protection repeat occur finger

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
