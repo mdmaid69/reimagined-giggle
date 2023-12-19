811738d03cf4311f0d88bd17293b6e7b205e9323
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:54:38 +0000

    cum workupto message scare compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
