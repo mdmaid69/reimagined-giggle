b856eceb578fdb753c49dcd153e75ed854832653
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:12 +0000

    otherwise stranger in look discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
