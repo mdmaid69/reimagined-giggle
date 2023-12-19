931882574e8c6e1ca37fc8094e2bca7a7f79d72d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:49 +0000

    us with gap excited most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
