8b53dffe8f3a8d82f244a001098e31b49cc48164
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:56:10 +0000

    hurry waste rush back fresh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
