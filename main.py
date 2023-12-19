859e52e234bf4b2f3376a1e582f33376770d0678
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:01:53 +0000

    disappointed behind kill fly chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
