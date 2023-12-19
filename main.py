01fe09f80ce5c7207b8c8da5f2498f7071b32573
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:05 +0000

    determined interesting true regret large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
