faa5c11850972e8910666b9dd9bc3abf94400ea8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:21 +0000

    recognize always lead sale yourself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
