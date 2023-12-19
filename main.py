5ba0f5c7a696d118665efe2fdfbb8ea099a7f341
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:56:53 +0000

    occur takeback from equal march

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def greet(name):
+        print(f"Hello, {name}!")
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
