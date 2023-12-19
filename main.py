afd5359656db99dd2520207cc3f306c7c3086364
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:36 +0000

    bunch steal sure lookaround concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import platform
+def get_python_version():
+        return platform.python_version()
