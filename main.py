d6b490dcd54ebf18980667c027f9b936bb2286d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:04 +0000

    score afraid television himself loose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(a, b):
+        return a + b
