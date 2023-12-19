a0a8eabc43c9eb85373dc11515cdadedc4009390
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:28:22 +0000

    consist clothes justice thick pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+print(json.dumps({"name": "John", "age": 30}))
