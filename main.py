bf9ac949969143746cd4037bf64f8924c7e767b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:04 +0000

    die pretty between sky available

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
