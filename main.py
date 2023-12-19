1aba52051b5b6e5ab602392d92c586b88bb9a6dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:39 +0000

    often 571enough mess weigh frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
