2f256f282a793d973e0fb1d2860ff20ff56cd678
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:13 +0000

    spare achieve also police much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
