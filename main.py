43f10f3ae0e64ef7e89421da1867dda41add357f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:08 +0000

    comment disturbing quickly frightened supply

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_min(lst):
+        return min(lst)
