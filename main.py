78b660b406140317be8640da3cbeb305b2e21c87
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:29:15 +0000

    emphasize certain either section justice

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
