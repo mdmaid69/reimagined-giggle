13b680834c0e3c1018f2d0c8224ce4e82bced891
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:48:55 +0000

    sister Jj pretty congratulations considering

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-print(re.match("h.*o", "hello world"))
+  def is_odd(n):
+        return n % 2 != 0
+  def convert_to_octal(n):
+        return oct(n)
