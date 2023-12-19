eb13939bc0b63a4368f031b7ca47a0eb4a2751be
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:09 +0000

    height pay confusing shine scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(x, y):
+        return x + y
