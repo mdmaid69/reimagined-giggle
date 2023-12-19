061002b9ea7229d373d47f3e7802833522af9419
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:22:09 +0000

    willing information left fun knowledge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
