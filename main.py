7fd4cafabd0f391f0b50b826c8c1e923f23d7811
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:44:59 +0000

    forget harm instruction only speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
