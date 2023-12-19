be40116395bce7b4df39b7bacc03715011f02e37
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:46 +0000

    stand shame tie born lookup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
+def multiply_numbers(x, y):
+        return x * y
