7917cf32a3ca2cb8de0079ac1f27701c54fa2745
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:18 +0000

    interview pressure demonstrate size obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
