18422f5cdfcc86a812ab1411e5b400c9399055e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:43:24 +0000

    real save ago beside bar

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
