a9ff906e8681a0fa776b422031ee2e2098fbb925
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:04 +0000

    seem bet night tie tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
