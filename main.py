6b1ffdd168f5849628eeb266a4555193524530b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:13 +0000

    conversation push opposite company approval

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_length(array):
+        return len(array)
