4bf4ec2a1d91113e77caf5c30813bc8cc6ed6085
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:24:33 +0000

    scheme move start neither impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
