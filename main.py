e1f01b0b9b7d04ebdc748219e9e1e86a3f4df617
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:05 +0000

    class under yourself ill poison

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_min(numbers):
+        return min(numbers)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
