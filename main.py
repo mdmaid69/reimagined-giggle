128c10108d35d7b05fa4fb75dc80c9104507e9d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:45 +0000

    high holdon laugh single accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
