112395512fdfc158f1b34b7b8c9290a427ba68ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:41:00 +0000

    test apart message benefit true

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
