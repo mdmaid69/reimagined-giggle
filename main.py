59fab3c716811ffcbc3dd9ccd3fc799de3f4951b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:31 +0000

    valuable slice present know weather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
