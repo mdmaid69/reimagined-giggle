67b439f48f2f28b4ac8c8826d574cd48ef50207b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:58:09 +0000

    cook frighten time thin Rr

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
