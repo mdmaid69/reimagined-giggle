e701ecd2683d7cc27cd9c84523f78aaca6e83b38
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:35 +0000

    space might hurry quarter constantly

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
