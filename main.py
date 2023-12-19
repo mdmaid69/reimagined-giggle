53d7ab3242e20824987c6ee08484a4ecebdd71ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:17 +0000

    education black rich regular same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
   def get_file_ctime_ns(file_name):
         return os.stat(file_name).st_ctime_ns
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
