c9ad75c2d610bb81d38a1360a466e6a4351511f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:09 +0000

    replace odd attitude keepaway proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
