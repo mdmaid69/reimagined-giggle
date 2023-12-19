3e84e6a9ed042f06e8179099215893dd84536edf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:41 +0000

    balance anywayanyhow already pretty free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
