646d930856ffc5cf3a3070baaa8a8fc7897a06ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:04 +0000

    slowly belong give spray nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def multiply_numbers(x, y):
+        return x * y
