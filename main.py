3a59be3f6fc51cef7b4ea8bf1225f0c804518704
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:14 +0000

    new something afterwards turnoff get

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
