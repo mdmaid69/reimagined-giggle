d9ce0c3b528b25548205e90a21e944eb9ef69c37
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:59:46 +0000

    manage Ee family immediately off

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+print(math.pi)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
