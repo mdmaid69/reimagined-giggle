3326509d2b351684320e753a66ffe6f234bd6c64
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:36:20 +0000

    quantity hurry its education gather

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-print("Hello, world!")
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+print(math.pi)
