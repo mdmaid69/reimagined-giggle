bb24fcfe836b46366cf0819ad71e49071ea657ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:47 +0000

    mark pocket aim bye 572enter

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
