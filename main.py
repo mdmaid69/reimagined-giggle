a0b040c02d4ed89bad2893eff0bbb6e4f46dd61a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:54:54 +0000

    keepup fire birthday paper police

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import random
+print(random.randint(0, 100))
