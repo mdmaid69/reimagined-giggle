fd4c51a2899d9c42a73aadbdef36d8ef3b73c28a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:18 +0000

    earn succeed blame would path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
