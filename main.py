4db9e6f56ffc76d74d84ed2ae1f823d2714350ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:59:50 +0000

    Uu trousers hollow us distance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
