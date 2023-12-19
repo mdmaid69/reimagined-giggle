dac5bbce4ff3e8beb7f4fb1a75759367cc834ab6
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:32 +0000

    warn order damp big relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
