de650ba8d35df5e87d2adb51f0ccf2cb7f2d7253
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:32 +0000

    weak remain flow pile lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
