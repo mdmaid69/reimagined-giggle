1523da1771f697de9c6cfe8c67b8a68ed91abbe6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:04 +0000

    pool by share practical risk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_palindrome(s):
-        return s == s[::-1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
