fc9e3f78c55a722c19eb530755a8072ae2f465be
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:43:45 +0000

    effect arrive comfortable goodbye scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
