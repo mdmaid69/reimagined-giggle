099b5c0e1bc276fe07e5b58ed2d03787b1e91235
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:18:37 +0000

    coat total now market Ii

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  def multiply_numbers(x, y):
-        return x * y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
