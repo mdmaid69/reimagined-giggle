6abace650426f9d1c81bb984ca8415976cd24476
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:56:05 +0000

    responsible turn have wave bill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
