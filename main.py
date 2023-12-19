6a3dbb2bd63d94e3b61e01190990b1f1b123c76b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:19 +0000

    description wash communicate thick immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
