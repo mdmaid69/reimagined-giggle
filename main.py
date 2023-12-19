54c43a7deacc9ffbbde7f5cb63e178555f527203
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:00:14 +0000

    patience string walk pity season

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-print(sum(range(10)))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
