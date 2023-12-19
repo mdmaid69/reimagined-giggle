7178c0598271b3481083f51de563ecd62298cc63
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:23 +0000

    answer defeat practical power surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_circle(r):
+        return 3.14 * r**2
