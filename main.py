7e4c68584201731fc2b1c85f6c39e4935425e9b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:35 +0000

    scream difference show hate grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
+import array
+def get_array_as_set(array):
+        return set(array)
