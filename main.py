259fe12a38a093b5ed2e67feebc4f3a6a98288c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:20:41 +0000

    569enjoy gift information create action

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
