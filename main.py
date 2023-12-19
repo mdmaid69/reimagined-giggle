5a1dfb33acf546a6d341fbd03dde5e848378628a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:19:46 +0000

    confusing go issue concern treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
