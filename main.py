5deb4d72665a58948bcef60e9cf2a7de3e5b5814
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:07:00 +0000

    hello tip official brain shrink

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_area_circle(r):
+        return 3.14 * r**2
