6245f6e0071df5937ae5750ab62a342bd81bf8e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:16:58 +0000

    debt maximum heavy impressed depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def sort_list(lst):
+        return sorted(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
