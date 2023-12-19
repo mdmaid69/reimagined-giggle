79c485295514aa71096f1040ca53c1a4e202db27
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:41 +0000

    typical thumb takeover not object

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
