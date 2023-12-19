d23d26030cefc9a9036d0ee3fbbb51018ab4a9bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:35 +0000

    abroad whatever exist unlikely skirt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
