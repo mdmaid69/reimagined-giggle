30ae54ead77decca21ff4b9cd87a0768c0f3f993
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:13 +0000

    fold else debt sticky pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
