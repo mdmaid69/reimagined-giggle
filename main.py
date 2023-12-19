5c5df23048a10459951473c9e4f799b91a399996
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:32:58 +0000

    impression later suggest zero moral

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
