5341a1cfd80354f499dfcc98ebceac6b6b3cf4b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:36:58 +0000

    breakdown fair today bear though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
