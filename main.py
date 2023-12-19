c0f9ae8b438ac930fe189334c0b5f30cf924962a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:48:40 +0000

    mark turn scream dear result

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_array_typecode(array):
+        return array.typecode
