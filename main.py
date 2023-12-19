4c527ef60d0f0f6ef084c47d21dbd6ca8152f883
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:14 +0000

    sail music beyond hurt look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
