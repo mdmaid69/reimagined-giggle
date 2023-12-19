b9ee240daee87f1b12470f3684ef5e0ea3cd05bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:32:44 +0000

    year stiff lesson care pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
