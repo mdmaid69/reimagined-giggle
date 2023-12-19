378d0c5630823e9d21b3458f9f83cf8ad9579136
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:43 +0000

    fire die prove pin issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_octal(n):
+        return oct(n)
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def roll_die():
+        return random.randint(1, 6)
