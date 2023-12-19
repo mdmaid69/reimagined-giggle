7fc1a08682206c40c83834485a1c949e70c744e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:21 +0000

    beyond whether allow name modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
