a6089dcbc610a91e2fe7bfbae8da934137650b90
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:43 +0000

    claim apart shop air thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
   def check_if_file_exists(file_name):
         return os.path.isfile(file_name)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
