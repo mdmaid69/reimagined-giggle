6ea802eb6cf6b9258017c41ea85bf16b60c0cab5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:49 +0000

    off name swallow thick Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
