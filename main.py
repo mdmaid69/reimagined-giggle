53f89a107c374b41bbb3df6b1904396ed07734d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:26 +0000

    shrink do system Hh mine

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_float(array):
+        return float(array[0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
