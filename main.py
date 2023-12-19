a2544eaae9cb0f4693930b7e44a286b889f22d21
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:57:16 +0000

    broad winter sign Ii regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
