dd22e75f6bd006ba3ff1e8e12c8ba232002e0092
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:22:44 +0000

    dress breakthrough male interesting corner

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
