c0019dc8c50c693d3a140a96e2ada4bbd7bd915b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:55 +0000

    aware piece clear whole cold

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
-        return x**3
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
