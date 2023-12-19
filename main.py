e5bbf87ce1f8ea80de925684a1032ce1398d1e56
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:39 +0000

    disturbing description promise pot before

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
