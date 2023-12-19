0749d09649914dca2ce39aebba729338e267bb23
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:23:41 +0000

    whatever high reliable movement cinema

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
