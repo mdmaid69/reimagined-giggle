e74cfacd4b9dfb9c54639ec7d1f500d504efbcfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:15:59 +0000

    strong state well under branch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_palindrome(s):
-        return s == s[::-1]
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
