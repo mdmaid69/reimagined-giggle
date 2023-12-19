16de3df1ee0fa08b6e7ccf0292ca35666497f07d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:43:47 +0000

    politics bright hand risk natural

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
