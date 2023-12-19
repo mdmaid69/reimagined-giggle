05fd4da08321a4ec26b925e6d4317e4237a0e350
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:01 +0000

    nigga protest roll far development

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import numpy as np
+print(np.array([1, 2, 3]))
