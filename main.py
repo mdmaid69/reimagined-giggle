efebcba868347d5ad02b9125dc72b9edada3c808
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:40:21 +0000

    trust exhibition whatever practice so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
