542240246102d4028ea0aa89c1b71e5f7bc7e86c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:53:22 +0000

    circle quantity school tall correct

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_acceleration(speed, time):
-        return speed / time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_floor(x):
+        return math.floor(x)
