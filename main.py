7cc4898d3f9dcc7e9c8904d5b2f322f4668f6645
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:30:03 +0000

    tooth comfortable under protest boring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
