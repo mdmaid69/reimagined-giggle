b8645a1ac3075b87922c023f214281c7bc64c04e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:51 +0000

    talkinto catch temperature lesson relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
