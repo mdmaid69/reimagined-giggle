6d1bb0e27ebd5da05ebf3fe3e5fd3f3cd0d4b1cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:09 +0000

    field enthusiastic violence commit survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
