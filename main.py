cead4b84864375bdf767c5cf77d1c866467fb479
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:33 +0000

    furniture interesting outside best bus

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
