e714fa9d3ff87db0383a1f460e2555b3a8e80ab0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:56 +0000

    body shout last trousers hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
