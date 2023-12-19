008a6e3ca612aa1e7b1a3de19e9137f169e15465
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:40:00 +0000

    communication birthday recover rub illness

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def clear_array(array):
+        array *= 0
