9581d24839f2e56d8f928ac2642bd2640901412f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:18:10 +0000

    promise insist godown arrangement shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
