930b4c88c2ecaa52d643204f9504ab568a95d011
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:54 +0000

    far sort heart ill direct

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
