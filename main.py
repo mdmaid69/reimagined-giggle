67fb9320e1ab9f0a8281519167762cf2f4bbb890
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:00 +0000

    excited improvement properly permission promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
