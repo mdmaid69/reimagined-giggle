950fe12faba7de8ba2182dc92761b6b182eab246
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:37 +0000

    impressed noise measure snow fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
