b88b2e9c9d68364b70b87d88cc4cae6b3221d8f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:13 +0000

    on notice Zz power public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
