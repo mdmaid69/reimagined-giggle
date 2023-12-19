42741a9c561d01751be291dcfa5b83c554d652ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:40:32 +0000

    uncomfortable butter artificial through connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
