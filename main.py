21981656fad856a2a5c8d1fdb65143f5bb929b0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:47 +0000

    scrap wind appeal with successful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
