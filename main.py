15acba4abf4ce3753ab1a92bedfa76b71a5a37b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:16:43 +0000

    ready journey present abroad sensible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
