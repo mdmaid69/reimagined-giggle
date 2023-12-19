0f2cc69a8b47adde4fc61fb43c89df76bc1f870b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:33:28 +0000

    before eat damage style owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
