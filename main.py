3fe44187bab5b2f1b0d7f87d027bbd85e4e18c21
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:16:22 +0000

    people getout exactly season length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
