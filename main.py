5caf3e79dc8dd206d484baa1cb199a4a603167fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:42 +0000

    neat early pile afford trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
