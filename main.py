a913766717e37a8a962b0513554aa0e52811b18e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:18 +0000

    design cheek old lie charge

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
