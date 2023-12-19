e42b078a0bfd29fd96431667bf8288438268176c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:10 +0000

    list scrape exercise wet in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
