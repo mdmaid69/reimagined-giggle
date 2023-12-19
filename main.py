ae60da4db4410e7d3380281a881d4c30cc132ad8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:15:45 +0000

    offend tip innocent count spot

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print(sum(range(10)))
