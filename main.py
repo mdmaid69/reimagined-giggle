13ab5f035fafc3bdfdb00c997372eb7ed7f1a5e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:04 +0000

    about my along help sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
