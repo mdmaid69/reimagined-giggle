8d8c99f428acc72684f35a2016255b6552cae337
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:10 +0000

    almost getoff poor getoff between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import datetime
+print(datetime.datetime.now())
