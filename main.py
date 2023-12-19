7db921491b2b049c139f05b367b58cd7625528e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:50:17 +0000

    or scheme exchange stage home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def convert_to_binary(n):
+        return bin(n)
+  def is_odd(n):
+        return n % 2 != 0
