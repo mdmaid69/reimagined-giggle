33ef14858b543a3daebd6fb1f61e9052de39f51e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:45 +0000

    release record vision sticky lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
