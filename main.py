2027fd74a4a8ce489a978a0248e9e7d305dcaf86
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:59:42 +0000

    hold conclusion workon anxious best

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_length(array):
-        return len(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
