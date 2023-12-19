e3cd30a624e7ea379f78cc06f5b680f556e0a84d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:15 +0000

    crazy study fall goaway little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
