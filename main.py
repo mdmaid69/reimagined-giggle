63e6b0653125b50c8f40a89fe70b780b921eac14
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:52 +0000

    who loose valuable wire autumn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+print(sys.version)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_frozenset(array):
+        return frozenset(array)
