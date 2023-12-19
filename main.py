cb3ebc09273f7254632685f343c4662ed27fcbfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:29 +0000

    gas yet through kill float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
