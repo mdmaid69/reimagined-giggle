70e7a0a05db1fd11aab7da6bba7cf36ff32f572d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:50 +0000

    ground reserve least butter fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import sys
-def print_python_version():
-        print(sys.version)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
