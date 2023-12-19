f6e8e0e13efde57c22039947377f9ce1bbe41f19
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:25:26 +0000

    interview grand anywhere gradually visit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+text = "Hello, world!"
+print("Characters:", len(text))
