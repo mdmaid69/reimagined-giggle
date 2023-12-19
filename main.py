915a5af2e8e7b4d9398ca037788cf9f04ab0553c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:23 +0000

    satisfied ignore laugh plate test

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def multiply_numbers(x, y):
-        return x * y
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
