075050bd99b549a72be5af7e413513127405d546
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:24 +0000

    art weather complete bit develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
