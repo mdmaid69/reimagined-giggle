8b5929cfc3bab60e24ca9dd29575699190b8981c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:38:44 +0000

    would last past illness innocent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
