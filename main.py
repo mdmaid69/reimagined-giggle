97c9ce2c1c3fbc5c3d4f52c2aa756c7608c2a81e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:55 +0000

    extra scale trouble return Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def change_working_directory(path):
+        os.chdir(path)
