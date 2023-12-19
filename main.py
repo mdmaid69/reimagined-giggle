9f572277e943a09a157eedc2b3bbadd9f05343b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:45 +0000

    room smoke patient petrol pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
