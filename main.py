e5bfdbd6b1a2889af3fbe780710cf3a979aaef58
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:37 +0000

    top wrong height boring cross

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def count_characters(sentence):
+        return len(sentence)
+def remove_duplicates(lst):
+        return list(set(lst))
