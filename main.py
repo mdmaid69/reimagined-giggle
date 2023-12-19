e1181872a79f1d78f3fb79560062a888049decd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:45:56 +0000

    intelligent convinced play tape trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  def is_even(n):
+        return n % 2 == 0
