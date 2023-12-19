234f5ad998c63ac6b1f11a82a5585979a27c9cdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:57 +0000

    married fact off impressed turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_current_date():
+        return datetime.date.today()
