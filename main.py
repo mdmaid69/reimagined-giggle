063d88e6169b78dda970e9fe71cce32c265e8aa9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:47:45 +0000

    stamp freedom obtain hole 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_rectangle(l, w):
-        return l * w
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_speed(distance, time):
+        return distance / time
