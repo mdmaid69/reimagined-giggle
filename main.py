1db11d7eee75aea047a22306a44d81543fea42cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:20 +0000

    on leave office container brain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def multiply_numbers(x, y):
+        return x * y
