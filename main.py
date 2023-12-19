779c3c9b13ca6bc3022e9d79101d8b71d06746cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:15 +0000

    food environment table fill grade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import datetime
+print(datetime.datetime.now())
