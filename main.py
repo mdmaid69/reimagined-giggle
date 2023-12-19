bb3b75ebb0c6acfde5abfa07e5a0d60737105365
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:33 +0000

    across contain nothing turndown file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def find_unique_words(sentence):
+        return set(sentence.split())
