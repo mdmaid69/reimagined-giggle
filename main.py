6376abc9ca52e3343065db2d7dbb296d66647fb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:28:26 +0000

    age whichever pretty closed pause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
