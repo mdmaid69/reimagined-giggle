27f87b568cf9116a347fa7ec7fd1eb7400602fef
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:59 +0000

    willing question usually cost food

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def find_unique_words(sentence):
-        return set(sentence.split())
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
