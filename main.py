1de8603e53c98bdb8e159cb450a4ef4639d25c3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:29 +0000

    hungry flat century original sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
