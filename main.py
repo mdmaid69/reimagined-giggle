edfe29b1d83c731489e622bcbabf702c71d7b2ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:29 +0000

    just supply butter admit myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_typecode(array):
-        return array.typecode
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
