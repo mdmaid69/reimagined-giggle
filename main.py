54ffc4696f98fea9e30adeeaaa045dd05087acc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:08:03 +0000

    affect worry plenty bringup turnover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
