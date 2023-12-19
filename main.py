f6b97b4dd3947ab0fd91c0ccdc485a06f1ad7a72
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:50:29 +0000

    without it stare complete statement

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
