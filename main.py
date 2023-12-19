1f9a85e8b299c823c7100e74f933a9ee7b90bc5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:01:06 +0000

    repeat lead edge reply now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
