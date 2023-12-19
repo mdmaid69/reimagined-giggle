e09e03aff123595c20776aea66e65d3fc40a8cfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:34 +0000

    brain different closely fire warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
