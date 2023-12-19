1a47f7a1498caf204bb2724ed6f17f4a4bb4d5bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:58 +0000

    length raw ride meeting bottom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def reverse_list(lst):
-        return lst[::-1]
+def cube_number(x):
+        return x**3
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
