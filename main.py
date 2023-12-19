3c0429f7b98bb7a0c344ff0331320734e8ac644c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:40 +0000

    rent dry fill fashion scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
