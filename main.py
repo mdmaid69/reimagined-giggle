ced39854e791ece74de25b7eb1702a9c6861d9ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:54 +0000

    load narrow arm slide introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
