d12343a656f59e22a5d38edece89e740c81b00d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:22:33 +0000

    basically under uncertain Zz soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
