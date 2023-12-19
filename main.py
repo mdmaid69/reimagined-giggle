352244b128650e5b5deb933d5948ee60bb46434b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:15 +0000

    find usually class breakthrough polite

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
