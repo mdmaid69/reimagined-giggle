bb6560b1c9558dfb6522096e8a8acca5db8d0ab9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:15:52 +0000

    exist murder school measure emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
