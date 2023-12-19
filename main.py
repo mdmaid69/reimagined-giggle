ea7cccf7f106f619772f3b98e05ea85a8dd00250
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:32 +0000

    downstairs attract shop wait now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
