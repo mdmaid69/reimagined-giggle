8da6e3bab84f9714ee17c9df56476a87126d8d6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:56 +0000

    round drink perhaps match die

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import platform
+def get_python_version():
+        return platform.python_version()
