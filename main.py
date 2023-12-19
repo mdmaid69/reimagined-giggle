71b218cba1c836423776da6331fd7a69542698e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:19 +0000

    thanks blow sticky imagine kind

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+for i in range(10): print(i)
+  def sort_list(lst):
+        return sorted(lst)
