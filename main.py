8d28cd4379e982bed3103f611756bb91b46e7df3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:14:30 +0000

    hospital passenger mean concentration independent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+from collections import Counter
+print(Counter("hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
