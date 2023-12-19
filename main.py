531534ff3d5c6ba8d0d4893afbf3222cdeb66f65
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:35 +0000

    566end crowded original defeat foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
