45de0cb7b92e17883ea232ae23d75ac30f0a2f38
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:45:01 +0000

    refuse set theirs temper frame

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
