f559ccbc3c2168fcfa0b44ce26de80ff5c2da621
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:31 +0000

    blame anywhere thanks fairly hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+for i in range(5):
+        print(i)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
