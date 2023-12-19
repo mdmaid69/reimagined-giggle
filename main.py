8c35483ef029160d82a0966cb2242ccdc4c40e05
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:45 +0000

    ride age leave central glad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
