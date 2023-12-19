db681a157904bac0b6c01b52ccc1620abd63ec20
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:42 +0000

    ever thin section spare wipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def add_numbers(x, y):
+        return x + y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
