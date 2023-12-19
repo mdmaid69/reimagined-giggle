6bc4ae95ed21feeafb9db4c8a1d6fe77b24c942c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:23:11 +0000

    safe healthy bit upstairs perform

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
