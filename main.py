7babe4d25a13fd53e48e5d7a089a899d62ad2d8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:08 +0000

    else achievement sail to sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def add_numbers(x, y):
+        return x + y
