9d787561507ef95773bc28a302f0f1da284a6634
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:12 +0000

    fairly comedown tie long fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
