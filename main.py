5e0cea3ca0bd2176fd0b30021e7a79705a717f8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:17 +0000

    laugh environment direct lead gate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
