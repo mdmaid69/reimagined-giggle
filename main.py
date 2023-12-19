3c0709eeaba3491ac5644344eab1b2d143cbf28b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:48:27 +0000

    great connection certainly down comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
