bb45a402702fa2084fd08d8d8c90a0c749be2008
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:08 +0000

    train concentration base violence makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
