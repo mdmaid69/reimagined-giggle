98b38a4d9da390dc69792284dd17787957a5a986
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:35 +0000

    thinkthrough workon square stick himself

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
