74991ce1b1b4bf063964f8bb3b3eb5ee323f1040
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:37 +0000

    slice wash nor exchange sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
