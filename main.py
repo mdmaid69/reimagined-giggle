2bae23d9ee1fc146a461543c60202535c5a7046f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:43 +0000

    band team birth bread admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
