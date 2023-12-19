10282324ea84af1ee359821001b6421acee0ddb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:55 +0000

    low keen tone drive walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
