a423cffcbe7b46c0debe01ab8ed65217dff95a0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:32 +0000

    book classroom considering type trust

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
