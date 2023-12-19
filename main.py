86cd6f0ab140928e4a7ac479494e45bc429aabbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:59:58 +0000

    rank reject search soil 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_set(array):
+        return set(array)
