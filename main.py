7423e2c18ddbbbd671fd30193a0902efb6c6af42
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:28 +0000

    over rough file delicate reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
