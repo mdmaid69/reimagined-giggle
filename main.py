e35d0f1152bd20334d0584bd7b1bbfdc76158033
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:28 +0000

    least hit open common right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def count_characters(sentence):
-        return len(sentence)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
