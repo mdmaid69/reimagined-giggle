252e990f0038454c2163cfbcd5c808afc33d3c6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:17 +0000

    decision message human welcome aim

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
