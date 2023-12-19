d69e7a3db4dc38e253e421071eba9cbc457b5a8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:58 +0000

    use prize wave agreement early

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
