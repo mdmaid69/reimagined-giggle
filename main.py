c3c519e0b74cf008290bb9991d2e36617c3adbfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:02:29 +0000

    callback immediate straight whether runover

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import os
-print(os.getcwd())
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
