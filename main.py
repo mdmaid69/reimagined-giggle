32164367b6ff37e54b382ded275429123251f44b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:02 +0000

    poor also interview crash new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
