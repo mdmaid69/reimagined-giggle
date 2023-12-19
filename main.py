99628c7b81710ba0ecf42e614006602a095bfc0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:45:38 +0000

    dear fear hit beyond judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
