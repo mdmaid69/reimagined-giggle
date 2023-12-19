c1a84ffde0873ee721ce102f2bd213cdbd536cba
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:05 +0000

    Jj government sort watch claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
