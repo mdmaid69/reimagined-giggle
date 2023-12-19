40f729cb2505e353147105c4af6d8868bb94cd04
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:53 +0000

    occasional use memory tongue picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
