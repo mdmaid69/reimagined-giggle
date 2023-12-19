5471b74b782367dda974c018284aa30f8b19ebe0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:33:51 +0000

    by win runinto apparently floor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
