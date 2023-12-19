7ac27e4c928b682c2759a1d299e0fe4ef7784659
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:49 +0000

    approval stay pipe public mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
