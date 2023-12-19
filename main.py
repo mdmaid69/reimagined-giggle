656fa41d3c9f31d9c8369b30dcaf309af7b45ebe
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:36 +0000

    us famous impressive smart do

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_user_dict():
+        return collections.UserDict()
