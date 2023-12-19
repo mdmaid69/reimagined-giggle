2b1d8146fd8bad860f8f94b0d898e72004fb0348
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:02 +0000

    trial long direction cruelty contact

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
