b858cf4f8ab561d02997c78db75040da9a385a74
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:29 +0000

    notice cold spite mother where

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
