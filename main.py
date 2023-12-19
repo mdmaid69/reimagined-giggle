66609268e5a88e5b5875efcaa7db7cb378337cb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:35:46 +0000

    bit cool minor runout they

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pressure(force, area):
+        return force / area
