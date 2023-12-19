4335f6c4486017f70df8617864209bbc40aacba0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:51:44 +0000

    tell stone Ee belong 567enemy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
