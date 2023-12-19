ff602f9aaba37f005cf0c7269de9e6b69fe34f0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:09:19 +0000

    label experience calloff characteristic manner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
