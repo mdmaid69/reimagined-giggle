6405d5b949806b8ba0da62e4b9775b71e12b26d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:28:44 +0000

    several telephone broad cruel might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
