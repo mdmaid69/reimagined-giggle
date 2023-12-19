0dc49daa92a3d00552cd3f10fcb320b473a23d95
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:02:52 +0000

    clear dance winter she exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
