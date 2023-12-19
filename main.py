b48e28efb7cae5b5cd921aefd0211c17349c2480
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:12 +0000

    container house reach sudden minority

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
