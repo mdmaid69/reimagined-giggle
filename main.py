54b8b9da51293f4aa9a5b83331a373d0e6c27d1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:51:12 +0000

    and forever declaration strict noisy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
