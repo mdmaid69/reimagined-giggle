dc071f5212fd1605f1550ec7cddbee258c5902b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:09 +0000

    sure bin protest service acceptable

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
