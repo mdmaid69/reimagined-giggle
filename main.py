2730eb5c536b77d5e5f11b2e67e758412972b9e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:26 +0000

    war talk yes zero estimate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_current_working_directory():
+        return os.getcwd()
