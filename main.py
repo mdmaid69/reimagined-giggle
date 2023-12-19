1d76ed1cd0fbc5611836887127b6335d675615c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:47 +0000

    clean day carry question crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
