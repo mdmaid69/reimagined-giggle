4d4712b2752b3171b3e5f2faeb62d9d4746e1bb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:01:15 +0000

    shall line stranger concerned this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
