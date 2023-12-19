264b17fc34877a6ecd606959e6bdaa74335012fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:06 +0000

    reply typical after prove brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
