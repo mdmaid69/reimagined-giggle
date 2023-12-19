ba8b06a0c567a46b088b748d1bb03421b3b9e46e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:21 +0000

    visit suffer realize class uponon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_hex(n):
+        return hex(n)
