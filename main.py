e7f887c003a98a944a3fc610b188b28890ef5495
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:31 +0000

    major punishment direction possession convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
