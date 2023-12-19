bbf2b8db0785543037818d41d1ce3e33ae2671c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:59 +0000

    Pp win clear project Rr

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
