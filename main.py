5e815c585df80dbf0897762e15c43374e81c4935
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:12 +0000

    agree provide lunch than laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
