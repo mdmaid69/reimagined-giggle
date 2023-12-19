3b21067d317e457170d7b72119100f5982fdcef6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:51 +0000

    keepoff thinkover bring border urgent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
