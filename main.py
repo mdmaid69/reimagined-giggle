64458f94dadbab6797ba4c469872cc3f899f2125
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:44:32 +0000

    nice work just turnaway promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_octal(n):
+        return oct(n)
+for i in range(5):
+        print(i)
