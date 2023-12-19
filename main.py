e234ad70e81c78b3362978f42dc505f9151e15e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:59 +0000

    wide record contrast know translate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
