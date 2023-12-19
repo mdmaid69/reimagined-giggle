f1bbabef16851d9486cdb4eacffe836d2cf6ab16
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:15:41 +0000

    minimum along splash joke bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
