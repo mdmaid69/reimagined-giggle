a630aca41316610a104d9008e5fc636cc8ffeec7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:57 +0000

    talk discovery noisy refer shake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def find_max(lst):
-        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def reverse_list(lst):
+        return lst[::-1]
