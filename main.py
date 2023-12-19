47743c612f4f06a01ed67a2fda344f620eb89d08
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:45 +0000

    mean hide border prove mixture

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
