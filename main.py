87438ba2d167a999fe72bc6fb246df32fdf709c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:12:32 +0000

    shout recognize both person apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
