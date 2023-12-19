2e9c60884cf86895995da7bc504141ce6afe487c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:42 +0000

    suffer achieve arrangement influence advanced

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
