579bea28b5dba823c32ca4a73c0ccb1ea09cdd89
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:33:02 +0000

    small subject thinkof new whatever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import getpass
+def get_username():
+        return getpass.getuser()
