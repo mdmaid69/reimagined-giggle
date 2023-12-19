9a994bfb2e5fb64ab4e0138081a86e16d48ebb1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:25 +0000

    naturally fact wave during statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
