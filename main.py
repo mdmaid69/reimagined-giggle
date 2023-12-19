c27b70f7835cfe51e37db0cfbd3ed891aaa32cfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:15 +0000

    block feature building whether shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
