d20ce39020b468abdb6007b8697bbe9a98f48286
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:13:04 +0000

    official bottom silent minority scrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_hex(n):
-        return hex(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
