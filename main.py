411a1138c0ff551860a16acc82cc4349ec2d36b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:04 +0000

    concentration activity table step show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
