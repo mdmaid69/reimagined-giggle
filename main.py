601d992199e0fed1038d8679e6d670bcc62ec42b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:11 +0000

    national pride sink argument beneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
