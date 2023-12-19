efb55b47a09c467b1fbb0ed7981b02b6fd9d9fcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:13 +0000

    job remark save breakin I

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
