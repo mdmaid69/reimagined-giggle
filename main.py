5124e258b69414480f2e75d58ae10d97f2cf5529
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:23 +0000

    joke programme swimming high edge

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def cube_number(x):
-        return x**3
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
