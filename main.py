7ddb15890a3c1f8828bc0120d18a0839343e6033
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:32:45 +0000

    waste steady all comeoff giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
