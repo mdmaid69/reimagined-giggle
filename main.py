563c2b7f8b9ee098149c41a3e9ff145fed45e8e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:51 +0000

    practical table grow present true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
