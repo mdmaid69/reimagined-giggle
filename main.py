aa2677005eb7c7f52522e2c1949bfd4e170c2f39
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:23 +0000

    mention include nigga shoot behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
