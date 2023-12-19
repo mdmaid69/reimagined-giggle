cd4b6a0db13402a88ccdbe6f50f53bd7ff9a2b0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:58 +0000

    keepfrom border lose afraid drop

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
