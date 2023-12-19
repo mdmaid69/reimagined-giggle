1488e227feb7a4f67e1c5506d71951bf17e038a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:27 +0000

    pronounce reaction wind less for

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
