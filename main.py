a914cc3a8041fa27efdcc0b33ae7663fbe7df6f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:00 +0000

    cold success tongue once Rr

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import collections
-def create_counter():
-        return collections.Counter()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
