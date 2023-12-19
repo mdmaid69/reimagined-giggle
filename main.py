a66f7347be7a474d43ed27dc4be1d564fd0338a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:28:00 +0000

    comeover see wave shut set

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import sys
-print(sys.version)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
