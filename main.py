844e5650a4eb4b023b987d2896fbd92a69052c01
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:04 +0000

    detail uniform release voice occasion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def subtract_numbers(x, y):
+        return x - y
