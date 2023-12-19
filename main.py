653248670d563fd74c54fbbdecca4ab58a8dad3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:43 +0000

    name turninto Oo visit medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
