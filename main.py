ccc38d95bd27d6da75355c5e871896ff4f142528
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:07 +0000

    report turnon wish build thorough

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
