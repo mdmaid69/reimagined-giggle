5b388674c1ba41482ac07e6df1d94482f978f96f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:46 +0000

    account Ff getback no meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
