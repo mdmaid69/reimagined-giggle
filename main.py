ceb0e57274b8c5ed5930d422094f4ce601e7e3e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:51 +0000

    content complaint evening new scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_odd(n):
+  def is_odd(n):
         return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
