9c5e193cb1843c6d3bbad70f976199d3a47f1a18
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:58 +0000

    nature exaggerate tragedy scene frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
