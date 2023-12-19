c9f576108743067e6101109cb6cc1a84ae777c2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:02:12 +0000

    loan slowly perform loose minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_odd(n):
+        return n % 2 != 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
