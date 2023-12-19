f0fba7bcde1d3a9878a09abee24a87c6dced1a95
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:27:28 +0000

    expect pay us fashion sticky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_work(force, distance):
+        return force * distance
