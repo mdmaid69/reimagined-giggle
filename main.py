5d6add744d2f21a9474d8ac42dba66b79e0f8a58
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:51 +0000

    fly label opportunity see home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
