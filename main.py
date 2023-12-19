499676156262556e0745be20793a355023eb3363
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:31 +0000

    empty by adjective clothes crash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
