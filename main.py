072fa45830f3474b39f35478afc0e6ec277ffdd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:09:21 +0000

    woman through mine them complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
