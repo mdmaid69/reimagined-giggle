0ee25bf0af3086f8637d0ba3111b682e21e139b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:29 +0000

    demonstrate regional empty deliberate fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
