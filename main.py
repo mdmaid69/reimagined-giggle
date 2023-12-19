ca75c181f07370f58682b476e217ed004f6f31c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:44 +0000

    hurt mail worth show move

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def sort_list(lst):
+        return sorted(lst)
