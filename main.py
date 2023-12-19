cee6167692b72fdd2015ea93ee9e663adaf2e85c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:15 +0000

    piece business group 564encourage block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
