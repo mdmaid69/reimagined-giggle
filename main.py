bf005cb5b5f2f47235e16cc30f775e44a0c144a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:14:48 +0000

    apologizeapologise choose decision scream memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
