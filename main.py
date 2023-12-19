1147fd0e82b998c91b2fa53704d59a0cfc47d155
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:12 +0000

    shop address search thinkover damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
