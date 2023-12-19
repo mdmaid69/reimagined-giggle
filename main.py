dee91279cfd7e066fc7eb184c7f2b0533d31904e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:24 +0000

    point thick speak transport rubbish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
