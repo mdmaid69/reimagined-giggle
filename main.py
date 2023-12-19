eb98985ff65091e375962630c2dc18f17356bfc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:51:46 +0000

    immediate help experiment your like

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
