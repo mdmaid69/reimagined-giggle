4f5b06f3e3f82b0f45a3892e120abd2ecd0fb486
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:44:15 +0000

    clue contact deliver artificial Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def delete_file(file_name):
+        os.remove(file_name)
+import sys
+print(sys.version)
