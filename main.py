997b4da28acd19748e9b6944178405ad491299d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:17:52 +0000

    offer turn tip admit score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
