43732f14c286a23faa56fbdf0b21614f9c3a2029
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:53 +0000

    exist read sort people teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
