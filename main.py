bd36a670f3beb35ad28a109eda973858119344d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:42 +0000

    bother line operation break Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
