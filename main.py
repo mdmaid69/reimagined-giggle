e7dbf054d6b98abe05b59c9850dff52720ca8c73
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:42:39 +0000

    ours silent run repeat squeeze

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
