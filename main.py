df5d44f728fb1627d1c0a338fed2734c4c6dab25
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:07 +0000

    tour or deserve approve tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
