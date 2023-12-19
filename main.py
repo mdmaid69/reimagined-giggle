4f11625a88c3ebb9ce0fc2aa1ec755abcc5d3939
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:49 +0000

    thing upstairs interfere target street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
