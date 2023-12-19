64a0dbea8e1627052231e6e9827b8a883ada5218
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:11:57 +0000

    schedule accept bend eager plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
