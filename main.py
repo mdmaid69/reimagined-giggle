5a4f085f7b34dd39eff95c781983717c9372916e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:22 +0000

    style related raw occasional blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
