325fafa18e5d49c99e6a864fe37743a8a4db11c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:22 +0000

    approve time miss Ll way

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
