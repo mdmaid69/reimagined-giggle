87f372b2e66628c4a83a5040aa2c6d04516deb41
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:51:31 +0000

    trade borrow weigh experiment estimate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-print(math.pi)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
