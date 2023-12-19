8beb793264733533b5c5b59d00038bb4c98ef4c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:51 +0000

    suppose opposite valuable tool late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
