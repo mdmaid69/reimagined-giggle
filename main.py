8dbaa835b95cd6de18a979c486657e9c1cce13da
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:50:08 +0000

    reach adjective basically cap hate

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
