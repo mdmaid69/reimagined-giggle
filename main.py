44abb588780b82afd416c556560d8c4683c1ed8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:45 +0000

    dress basis weak completely hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
