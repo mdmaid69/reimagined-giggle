062607d97825469e4ff25948e17904019229ef08
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:11 +0000

    willing collect offend he calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import platform
-def get_os_info():
-        return platform.uname()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
