826e5dc194e3439617a1c19a0c022ae1fa9a5b63
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:58 +0000

    recognize stop tongue flavour still

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
