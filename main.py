919bc4b28b63df4c077c800b6fd93ca5a4ae748d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:29 +0000

    challenge wall face moral beneath

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
