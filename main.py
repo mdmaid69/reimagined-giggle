3ac5fb7b1ba0e46445fc7cfed46e761006e918e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:41:08 +0000

    search count mine board team

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
