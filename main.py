516f005dd3efc9ad0812475de6148216f1dc9c4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:30 +0000

    strike dead marry friendly mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
