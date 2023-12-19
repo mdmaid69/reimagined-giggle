cb4671840f7820317681da0940570bc976461924
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:36 +0000

    level pool body knock yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
