7cd1e50f623415f2480cffa0c1eb8f15d2ea3dac
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:42 +0000

    learn taste genuine language square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import platform
-def get_os_info():
-        return platform.uname()
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
