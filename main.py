45bf8f235c446947be5f3f47ceea696a0598d9a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:15:42 +0000

    impatient relief main system pinch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
