08cd561c2af053d9384d12d7899b6fd471d9df4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:18 +0000

    false prize fun name taste

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_work(force, distance):
+        return force * distance
