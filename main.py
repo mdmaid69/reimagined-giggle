d014ac58567668aabbfceb4d64ffece2a26e30ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:51:25 +0000

    otherwise radio battery interest tune

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
