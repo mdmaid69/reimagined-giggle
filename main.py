41614d6ff0c6b7f23c26b439b60f820f2c274419
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:50 +0000

    nowhere me rule crazy nice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
