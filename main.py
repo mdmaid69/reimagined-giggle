5a59055b8215f7223bd196bc8d06bb920b216a60
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:29 +0000

    any join actually sensitive Itself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
