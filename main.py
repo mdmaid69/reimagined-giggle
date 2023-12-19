b63338d169a15a77aac3731dc504649954722e32
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:27 +0000

    task window delicate wander feature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def is_palindrome(s):
-        return s == s[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
