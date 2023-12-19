e2ab13aabd2b669c7bb550073e7c4a1ea59bf542
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:10 +0000

    criticize unfair divide getoff minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
