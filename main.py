ec259514794a0e61f399e28396561f6bf89d7329
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:48 +0000

    shallow hot your perform moral

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
