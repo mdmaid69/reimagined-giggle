14ddcb0bd25aaf35141040a739f4be727b3b03f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:06 +0000

    dance further start secret turnover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+print(sys.version)
 import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(("www.python.org", 80))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
