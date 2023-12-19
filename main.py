78529191a3e121ad7f36877895be6080e53fb351
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:12 +0000

    time leg process Jj fine

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
