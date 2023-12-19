ed26001d33d7222bd70a12241e249afb61d58b9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:37 +0000

    without unless although moral minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def is_odd(n):
+        return n % 2 != 0
