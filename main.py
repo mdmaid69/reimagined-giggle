1e371b622fea13f3e0b4545391dd8c5844ba4b2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:10:06 +0000

    distance bus perform include limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_density(mass, volume):
+        return mass / volume
