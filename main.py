b39cf780afcfd1496b6297747cfdc37a7f111782
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:13:53 +0000

    fire coffee evening substance odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
