11cc8503ff4a38c4a7539e870dec196b69fb520f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:28 +0000

    slowly I responsible confidence object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+print(os.getcwd())
