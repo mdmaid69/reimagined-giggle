26841dcbba7031c7f44aafa3abb3e6924dc61f5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:22 +0000

    guard emergency eye cry introduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def square_number(x):
-        return x**2
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
