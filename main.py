c444a3b682505f1430bd29770d3a3deeeb9f035a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:50 +0000

    usual organization treat partner hurry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-import os
-print(os.getcwd())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
