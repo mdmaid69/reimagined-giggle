77b5e71a41ef17286c07e30e042114248fb9c726
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:28 +0000

    give see touch appearance reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
