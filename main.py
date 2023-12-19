3e644b4996faed25a6d56946a88482de03c5b3f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:21:30 +0000

    bury imagination pupil bush clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
