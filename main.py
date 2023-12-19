efa96f44715ceda47b8e6d8cb3919749e6eb714e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:18:03 +0000

    fat test constantly inside detail

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def convert_to_octal(n):
+        return oct(n)
