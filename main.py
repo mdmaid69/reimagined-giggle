6bd300a768bb2a652bef1be69ca5cd75c450f900
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:41:28 +0000

    birth open the equal mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
