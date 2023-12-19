3daec5306bf4215f315b30c4986b738adb846d62
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:22 +0000

    pleasure somehow this impatient day

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import sys
-def exit_program():
-        sys.exit()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
