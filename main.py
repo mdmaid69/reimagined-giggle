b42d3efdb2ed5de83ca9602df41b7013bf0bf567
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:55 +0000

    question which side himself save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
