17e5ea789fae39d6ccc1397f637c6d59562608f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:03:14 +0000

    hello frighten future country discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import sys
+  def get_python_version():
+        return sys.version
