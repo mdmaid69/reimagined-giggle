40649bc30dfb0fcfcf94a62275e9286dc1ce7c54
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:51 +0000

    comeback cum willing choice coat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
