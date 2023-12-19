e086726bfea91ab965d25877d266831993aab46e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:36 +0000

    fail house money considering brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def reverse_string(s):
+        return s[::-1]
