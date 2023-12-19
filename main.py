08debf009afd63744cae6821d3993db695afe0dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:32:21 +0000

    have discover considering rough repair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
