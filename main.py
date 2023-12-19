6bc46c6ac82567e7dff4d26ea28b58aaa17003b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:04:14 +0000

    swallow patience business light capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
