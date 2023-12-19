e008097920a9b93a40205e42a2ad6c083108f1c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:45:42 +0000

    hungry find drive bear laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_floor(x):
+        return math.floor(x)
