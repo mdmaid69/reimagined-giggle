1eed6eca21a8117f9f991b64f3a356676f5b7b3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:02:53 +0000

    blame frequent wave serious your

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def add_numbers(x, y):
-        return x + y
+def add_numbers(a, b):
+        return a + b
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
