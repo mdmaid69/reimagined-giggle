60778a0159d2d058caf9b77bbdb4f895d082a47c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:42 +0000

    pipe comeover exit thinkover connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
