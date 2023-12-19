fc9ee3594032b5e5b63e2ac0daa9c2f9b74a73c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:18 +0000

    error fetch trap worst own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
