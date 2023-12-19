1cd78d7f27c104276cb6e2a5582b672b164dde8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:11 +0000

    part lookafter always walk difference

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-print([x**2 for x in range(10)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
