351624566d0b22768405e352a6870f8a950926e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:45 +0000

    operation fun this responsible interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
