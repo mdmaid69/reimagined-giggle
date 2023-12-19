45ddac3d4ad49cfd81d9c3d32f82c8e8c2f403e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:09 +0000

    rub opposite can pollution steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
