74ab6cae09f9fd4a39accae64d089ff3a1401fea
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:51 +0000

    fetch right cry necessary swing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
