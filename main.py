cc8af8587f7f114cafda829a931d8e8e35d8c7c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:08:39 +0000

    nose unlikely makeup forward free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
