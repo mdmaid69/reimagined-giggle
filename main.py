bee7452baae4295ce987a592db4d54923a32d657
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:55 +0000

    another dress basic invitation behind

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+print(sum(range(10)))
