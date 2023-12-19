90df672c8df6581b6d001b94c2f4e802083b3e2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:18 +0000

    strength offer apply guilty hard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
