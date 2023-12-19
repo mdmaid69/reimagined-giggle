63842db0b9aee6e09b7e1a94d65f1be2eda52244
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:42 +0000

    before expensive opposite afraid interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
