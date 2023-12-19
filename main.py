c3043ec2256cc0aa20d5859f1085951e870c673c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:04 +0000

    join majority hurt mind depend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
