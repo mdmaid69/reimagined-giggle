c68293924eae9101dab2b6050d44f729ab88be11
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:20 +0000

    bringdown valuable choice shout result

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
