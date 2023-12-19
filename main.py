ef792290046917b20f8710bd80b1d16f38e25407
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:10:54 +0000

    environment mainly change finish dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
