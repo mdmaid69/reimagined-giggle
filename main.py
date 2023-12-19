bf7e63fb53dc2de754ec12922af5bd8168b278b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:30 +0000

    several personal pay frame temperature

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
