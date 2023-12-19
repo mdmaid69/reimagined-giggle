57b07683f584594ec89ed43f4a351e3a8879d949
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:06:45 +0000

    grand juice during everyone advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
