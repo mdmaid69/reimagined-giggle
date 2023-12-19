151398cd711cc123187a8a408b4ca943030d1f06
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:59 +0000

    argument sell than escape lunch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
