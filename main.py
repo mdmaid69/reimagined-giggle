a5fefe2223d558844749a31ada1e507fd03c309a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:08 +0000

    our tired please satisfactory exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
