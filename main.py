decbf474f16105b049dc36087aafe3de1eeda14e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:17 +0000

    contents acceptable lookup say freeze

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
