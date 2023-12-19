fa54158e59d71e320135cbd89c5389193323f1d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:21 +0000

    worth bag form punish nonsense

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
