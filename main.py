240f35369a6ab4e851de9c95dc9fb11b416dd18b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:06 +0000

    fall clothes firm sink fly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
