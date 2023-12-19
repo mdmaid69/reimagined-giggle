6c7653c996a5f2302a53677af030c67ace79330d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:30 +0000

    Hh 179bite broad motion or

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
