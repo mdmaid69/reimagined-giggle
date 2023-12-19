6eb1296cd867b421071e1ef08daa40ec43e68da7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:24 +0000

    sound secret turnon 568energy nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
