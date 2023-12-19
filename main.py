bf4735e27115eaf80bcc583370c542e99b851a0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:05 +0000

    smooth wheel notice send put

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
