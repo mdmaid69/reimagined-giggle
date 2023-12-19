7f3fe05081d99b605fa9766da1885af8557c029c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:58:16 +0000

    patience explain shop word doubt

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
