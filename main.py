1bddbecba82fcd3395fa2a4c08ca063334e4dc53
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:08:59 +0000

    arrival check factory minute congratulate

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def calculate_area_rectangle(l, w):
+        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
