5b11bbec046be9d6414838b4932a3e6bf2dfdeb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:53:48 +0000

    breakfast meanwhile doubt lean ability

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
