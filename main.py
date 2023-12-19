042c5f4d0b9a3209c256dc2a1a21b4e501c1b378
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:46 +0000

    small ourselves positive of pay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
