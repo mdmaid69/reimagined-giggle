bf5943727a29c939541216626a5dc07f050634c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:57 +0000

    snow 572enter against decide amount

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+print(sum(range(10)))
