711a3e429b87b22ce5fbf846b8de9f2cba24bee6
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:09 +0000

    save Vv though radio lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import platform
+def get_python_version():
+        return platform.python_version()
