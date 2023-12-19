55693458a813d65c7b2d33c0297805feca2ccf55
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:33:37 +0000

    march fit half service arrival

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-for i in range(5):
-        print(i)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import getpass
+def get_username():
+        return getpass.getuser()
