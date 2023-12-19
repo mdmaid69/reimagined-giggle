3c1ed157e471e75091f946103d6aa6d160e3f2fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:53:23 +0000

    open ground limit straight substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
