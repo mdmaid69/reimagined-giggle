68246dfcc1c3c4adff07f1c6dfc411d3665c6e34
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:10 +0000

    the book your typical angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_elements(lst):
-        return len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
