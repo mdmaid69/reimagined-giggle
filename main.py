9a18edec9e20724faf0372726d21b0bf57313732
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:46 +0000

    angry sport correct correct fix

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
