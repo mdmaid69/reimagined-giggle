10bb73c8b4797960a86dfd86887da35351b2df3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:00 +0000

    base pronounce base should war

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
