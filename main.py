e098b1d2110f03f0dcabde7dae3bc1625ad8788e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:17 +0000

    job turninto father carry present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
