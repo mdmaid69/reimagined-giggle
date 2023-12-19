978f7520b667652a119a3f5dd7f8edf43dd1bcf4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:08 +0000

    clear again operate tax pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
