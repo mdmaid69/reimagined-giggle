b15454b937086b0bd6a39fe99001c9ddeab24c5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:17 +0000

    culture satisfied holdon destroy weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
