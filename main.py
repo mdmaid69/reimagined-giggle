daed70b6d2abd7c4d59873561ff6f62217e501ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:40:43 +0000

    fortunate total usual run result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
