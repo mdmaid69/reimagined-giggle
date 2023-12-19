1bb0a37cc8475f5316ce42c66469e950d1098683
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:05:50 +0000

    slight past reason impatient wish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item(array, i):
+        return array[i]
