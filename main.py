9011eac8b8c6ab0fccb446eb52883a4654142661
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:37:23 +0000

    bet anyone collect spring forward

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
