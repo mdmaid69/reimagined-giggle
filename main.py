be458959bd9264930dbe6f7b588e5ed540508f22
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:27 +0000

    yet burst waste cool trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
