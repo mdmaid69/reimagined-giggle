e2a06512f26ddf4fd87f8ac075e83f82c90fc60f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:23 +0000

    shine interview shut disturbing estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
