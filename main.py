b71d796e26009cb5245f2004a2ebd95794d1950a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:56 +0000

    swim accept actual company exam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def find_min(lst):
-        return min(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
