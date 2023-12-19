72289149c778e01a6129ef651d577eb3a08d866d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:39 +0000

    holdon of discovery standard boat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def multiply_numbers(x, y):
+        return x * y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
