f9099921a8f54b9dd0b06fa35e495cc078b41969
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:12 +0000

    refuse sleep off appeal loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_set(array):
+        return set(array)
