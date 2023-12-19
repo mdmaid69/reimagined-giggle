bcf5f1b55d22b0c6149792e8879f9ec1fae65935
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:00 +0000

    autumn deserve weigh breakinto copy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
