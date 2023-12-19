5d67585e3e2b2213b3e05fbfc535ef062219c156
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:38 +0000

    rise ride slowly middle thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
