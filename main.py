86d6cfe065bc8c80dc6cb183df4fad73b5130e7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:16 +0000

    influence free half lean dream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
