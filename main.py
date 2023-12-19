389887b09c775185f62c4183a50dfd8ff417a650
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:25 +0000

    interview tongue confident now another

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-for i in range(10): print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def subtract_numbers(x, y):
+        return x - y
