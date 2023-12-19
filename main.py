335eceef3efa77cc5049095f63aeb962b2535965
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:15:20 +0000

    surprising best bunch grand deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
