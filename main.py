ba1cc0713b1db4c5e23ac0e929de4a0844e41fc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:59 +0000

    mouth Kk finally measure height

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
