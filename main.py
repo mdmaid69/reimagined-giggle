0d3ef626978581241f256031cc57fcd738dc9cbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:17 +0000

    final prefer speak accidentally copy

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def multiply_numbers(x, y):
+        return x * y
