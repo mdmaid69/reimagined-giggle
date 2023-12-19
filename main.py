5b6bb294f06947403304ec50cb20d53677f4d046
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:11 +0000

    bill majority holdup announce Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
