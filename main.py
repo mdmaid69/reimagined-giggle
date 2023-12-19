ec222afa85fdb94bca62982e421a27abdde8a080
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:52 +0000

    rare let private record block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
