59919d4b2f1a00b082969f06019d999fe0d99cc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:29:40 +0000

    notice store upwards properly mouth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
