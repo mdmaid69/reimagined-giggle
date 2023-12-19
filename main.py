527da84a1f8f382935cfa576983e46f66b535883
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:36 +0000

    meet necessary clear near evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
