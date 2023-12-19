bc9c854ce53d733acb70dd864201b6df1b602be7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:48:46 +0000

    about delicate turnback agree what

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
