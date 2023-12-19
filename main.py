21704ce1a7a13c898fae298ecb6576ded4fd5d9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:49:27 +0000

    touch better appeal talk practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def reverse_string(s):
+        return s[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
