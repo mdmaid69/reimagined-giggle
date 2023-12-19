222e72f76beea4761254fad2f28e2b9a0ff3f518
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:45 +0000

    possibly allow impressed nervous few

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
