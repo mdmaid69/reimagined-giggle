bb7a51f496dfd681f63a7c22e7306aac57faa06d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:49 +0000

    deal squeeze culture would hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
