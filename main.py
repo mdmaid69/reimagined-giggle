011961ce19b04b810d5c105a5ce3eaf0d422e9e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:38 +0000

    aware miss clear squeeze about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
