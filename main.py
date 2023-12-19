80c0b2803594b756486c53debd72af5154a941e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:27:25 +0000

    freeze party turnover plan shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
