8299a3ef7eda9a2e17f946a46dc3031bb3d23241
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:21:28 +0000

    brush male future I accidentally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
