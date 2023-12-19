ceb3087a61416f29dec32f43dea30f4e65a49dac
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:19:28 +0000

    left method command impatient guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
