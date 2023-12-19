9eb9c2ab41023363bd0e654f0ce897161c976036
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:40:29 +0000

    transparent object balance worth late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
