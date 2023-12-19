fbabccf3c45a1c4f59ff8f54ae89d9c3bc363697
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:18 +0000

    sensitive care warm talkback protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
