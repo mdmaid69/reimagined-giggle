290378d64c73184d7b8ffee5d92628582facefaa
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:55:29 +0000

    happen base expect well shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
