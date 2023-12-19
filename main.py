7977dcfb590c130300019b736abf9cf82ea3a9a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:05 +0000

    team takeover slice spare turn

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
