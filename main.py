5c7fbe2b5ea474c142b34435d402d537ac0e1d4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:50 +0000

    must truth embarrassed since convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
