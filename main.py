d046fe0e7a21c66569b6a6fabec203ef234664ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:51 +0000

    into fix over outside float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
