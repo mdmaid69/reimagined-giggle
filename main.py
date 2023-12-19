d533dec52eda4338bd2693d15e2fde925c2244d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:34 +0000

    weigh history pretty punish ring

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
