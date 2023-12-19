2f178ed9e7db49fb717ad3037a9ba9c5b1f81aa6
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:45:35 +0000

    conduct record dry shoot dry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
