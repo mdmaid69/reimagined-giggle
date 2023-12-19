40fb721d9516bf17e56e1d1326299cfe92048c08
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:39 +0000

    lift father relationship surprise world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
