a009e5a4cb0a2d71d548815fb95503e127e1ab26
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:47 +0000

    actual coffee seat rather up

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def add_numbers(x, y):
+        return x + y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
