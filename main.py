0dc968024788ead933e038ceabf598c4af5612d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:38 +0000

    hard prevent tooth off rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
