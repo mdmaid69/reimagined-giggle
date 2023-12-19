14e55d5b21102dc2183e4415c47dc16541ca6f06
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:37 +0000

    reliable left wish kind simple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
