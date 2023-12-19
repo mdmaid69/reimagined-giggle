848556341bdd143fee321651ae0c3b9f8ebc51e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:54 +0000

    basic large manage standard drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
