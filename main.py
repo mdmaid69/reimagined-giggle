5cf5109cdf098672d6881309b3bbe0e13307c89d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:55:39 +0000

    strict period period event branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
