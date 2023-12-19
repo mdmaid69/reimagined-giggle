bad714356b5c9a8d00fb22919db231e985d71c37
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:32 +0000

    show forever sleep bringforward operate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
