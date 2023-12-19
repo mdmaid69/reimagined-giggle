d44c04a0a92004488ae78471c92e4ebe073a32a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:04 +0000

    estimate must cum mistake mistake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def sort_list(lst):
+        return sorted(lst)
