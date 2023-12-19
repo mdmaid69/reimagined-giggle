62da87ee91b460e349dc1cbe07cf0e628ae9ff54
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:04 +0000

    clear machine cinema your fun

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-print("Hello, world!")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
