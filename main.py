f613a085dd0bcacd9b98a8dfb6bae6c96a7883d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:06:27 +0000

    little across holdup be rank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
