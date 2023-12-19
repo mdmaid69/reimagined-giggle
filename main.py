9c35180668693918ae102a7c316bdd2533787d6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:12 +0000

    deliberately error out close grow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
