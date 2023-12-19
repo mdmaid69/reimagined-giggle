f4dcbb4fc9bb5467a9718859a555d23b47cc25c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:44 +0000

    exhibition immediately demand consider almost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
