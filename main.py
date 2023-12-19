7164cc2fbb89c001d3d2cf073074f9161b55ea1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:22:50 +0000

    share along coat inform lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
