91ecce1e767e68466b6944017a7b45e44f95c734
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:33 +0000

    over day direct Dd phone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
