fdacd1f5821ca50377336456831dffe98aaa893e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:06 +0000

    smell bring contrast fast carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
