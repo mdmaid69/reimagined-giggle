b7e6eb3f0d2ce877e7efba36e350f0c0f7c39f20
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:05 +0000

    godown legal basis arrive run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_error_function(x):
+        return math.erf(x)
