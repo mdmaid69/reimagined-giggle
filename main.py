5b7d1e1c09d7e67e7a9e81807cfb49755af76670
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:29:18 +0000

    thumb interfere hair bend away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_directory_name(path):
+        return os.path.dirname(path)
