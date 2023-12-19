192ffdfe255f63a83ef97b00c3902f44ff9e3e2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:17:27 +0000

    pity poison almost music turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
