77319956b1ff7b141c95d63419189a7bb11afecd
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:19 +0000

    drive favour order decision from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
