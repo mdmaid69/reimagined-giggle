6b7c5dea8014c32f657eea485d5cada889dfaf4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:06 +0000

    mess gather anything who lot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
