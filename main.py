9d6e0f6cfd9f0612c6a66cdd369876b4338a283d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:31:52 +0000

    round thanks always travel beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
