a9e62e060097f7859af32c4a7ed8c9b07ae2ae5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:58 +0000

    lunch pile somebody division will

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
