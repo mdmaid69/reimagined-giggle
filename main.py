e48f86db38fa8395290541cbabd1d3dd831442f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:36:52 +0000

    children acceptable course record and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+def add_numbers(x, y):
+        return x + y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
