0c9a21e7ef69e1975d570938562cb0e2f441e43e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:55 +0000

    kill negative common failure radio

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
