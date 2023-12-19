964a39839cec173836b2894b75702c931da9bba7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:19:10 +0000

    secret uncomfortable Zz question world

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import datetime
+def get_today_date():
+        return datetime.date.today()
