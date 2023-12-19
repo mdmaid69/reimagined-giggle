51f613c846da2c31c9f4d184a19ed0c4c452f688
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:11:41 +0000

    refuse everyone future brief difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
