61a277bc9d2c3b76ce1a33c86a17ae9675a19611
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:17 +0000

    anything work mean schedule hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
