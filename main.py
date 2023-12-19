42717de2c8cbab06cd0d33aaa026f59321155bc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:21:12 +0000

    his brush getout concerning land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
