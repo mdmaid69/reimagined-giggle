c95c5096c017ed15c88cbd0420aeef8148ce9f71
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:09 +0000

    day discussion tell interesting influence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
