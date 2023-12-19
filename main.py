15e617f20251d9dd31884d754a505258e3615df9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:30 +0000

    crazy passage respect perfect exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
