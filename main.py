ace97a1cd20a9d87a8848e9bc30d5256910a9b3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:15:23 +0000

    later regional careful uniform lunch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
