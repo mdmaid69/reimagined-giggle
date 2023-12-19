8866b0685304f49a21bcd0ea3f15e00a5e7af7ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:10 +0000

    appeal shop change calm too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
