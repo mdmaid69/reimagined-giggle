83659f3ee2cf3c79f57f0794d78d6d35e4645ce8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:01:39 +0000

    Ss spread ignore complicated mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import re
+print(re.match("h.*o", "hello world"))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
