75fc3651c6dec01367115918955c7ed606c747c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:44 +0000

    various embarrassed clothes track arrive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
