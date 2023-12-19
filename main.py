157734aa7a31c5e7aa382d4bdc5667060c05b92b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:12 +0000

    stage condition dinner game frequent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import collections
+def create_counter():
+        return collections.Counter()
