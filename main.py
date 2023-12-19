df9f6945bef56e0d48864bb451843ade99d23d6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:18 +0000

    male public delight ashamed abroad

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
