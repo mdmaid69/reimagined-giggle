de2bfdff95627167f36e9a2bad2a05444c160b93
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:26 +0000

    callout health near telephone again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
