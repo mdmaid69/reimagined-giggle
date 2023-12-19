5c8c70d8a4bda7be061e31d15d94cd9d91cb16a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:33 +0000

    last answer wish deliberately her

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
