d3e8afe22e80a11c2624eed9c14d4aa2e1948127
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:38:05 +0000

    easy flat constantly will convincing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
