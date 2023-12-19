f9218e24e41042fd0203681b90393aa50f77e76e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:55:27 +0000

    still crash last woman couple

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
