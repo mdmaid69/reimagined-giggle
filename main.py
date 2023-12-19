8b05be68142033736c8e33a7c62b15cb3fdac7bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:33 +0000

    benefit always active birthday could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
