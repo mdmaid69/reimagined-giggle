a550ca14afce751b03fb3b139c4769ea2c6ae9dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:14:34 +0000

    effect worth properly please gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
