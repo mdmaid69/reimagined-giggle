136e40eafdbfa379b574ec5d031b5fe0067093e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:10:49 +0000

    closely steady culture succeed on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
