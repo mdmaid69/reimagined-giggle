d9e3855b1701431a5dd4a0e22bb7b0851d82f8b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:33 +0000

    remove bang start embarrassed ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
