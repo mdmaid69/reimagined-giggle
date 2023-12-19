5ab1bd5f1e0772dbc0cdf721eb3a4eb21fc958f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:42 +0000

    approval record pay side cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
