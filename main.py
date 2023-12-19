ee380660214056540ecb7647d104de5f0820671c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:45 +0000

    joke occasional respect lie extremely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
