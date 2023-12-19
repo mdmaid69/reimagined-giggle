80cc00acf2baf198543ebb520bc4d58808429196
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:30 +0000

    emotion average squash soon point

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_string(s):
+        return s[::-1]
+def calculate_area_circle(r):
+        return 3.14 * r**2
