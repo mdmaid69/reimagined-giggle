87e5b3f403d9e41ba83f500b108ba605d53617ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:14 +0000

    principle order criticism lesson weak

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def convert_to_hex(n):
+        return hex(n)
