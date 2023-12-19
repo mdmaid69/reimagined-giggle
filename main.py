0bd1635631dc034acd3c6e8ab271973e5bd893e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:38:56 +0000

    comeout disappear bottom talk under

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def convert_to_octal(n):
-        return oct(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def count_elements(lst):
+        return len(lst)
