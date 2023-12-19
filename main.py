c87f30b2cdc4c32005c96a1326edb868f90752a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:19 +0000

    use length flat fine limit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
