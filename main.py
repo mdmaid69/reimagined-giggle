76fd1c74d8bda4fdb47d0bbdf007509fb9e80fd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:05 +0000

    answer clear protest combination over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
