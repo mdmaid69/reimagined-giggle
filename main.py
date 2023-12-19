1e85b979ecdda57fa3ce883135fa8ce009af8555
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:06:59 +0000

    pity Cc everything also culture

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
