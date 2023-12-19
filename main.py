cf8888d744a07e60022057ce15eb3cac88d249c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:22 +0000

    bread long meeting in object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
