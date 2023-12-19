e0a159f7c80473ef76aeb5de03f6b849461f31c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:46 +0000

    cover spring gentle upwards crash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
