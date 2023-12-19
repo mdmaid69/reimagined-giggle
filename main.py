53a645c3bf711141bb33de07ca272a2f1d40639a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:30 +0000

    declaration variety amount roof crowd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import random
+def roll_die():
+        return random.randint(1, 6)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
