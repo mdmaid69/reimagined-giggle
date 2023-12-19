73a46578670b831f1c8baca98ca4b2e1060a9064
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:33 +0000

    mean official command wrap company

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
