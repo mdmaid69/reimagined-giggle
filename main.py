73888654f50b511282690b2655fec542b56dddd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:44 +0000

    question bringforward slice goout to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
