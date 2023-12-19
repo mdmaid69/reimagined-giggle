b9cf25573a5006220b5edda381ba2b28a97abf48
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:04 +0000

    practical hand address responsibility everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
