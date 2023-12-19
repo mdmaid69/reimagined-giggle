32984c17d1487ac1a19e66ce51fdb059b9fe221d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:24 +0000

    already flow holdout plate common

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
