4b85195f05cd376f5fee803d44e79243afbe230a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:03:09 +0000

    instead unit after accidental mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+def cube_number(x):
+        return x**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
