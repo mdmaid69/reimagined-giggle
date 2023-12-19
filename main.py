3e0f9d897cd2a4fb72a42f7a41816a3c525a9198
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:42 +0000

    punishment extremely whatever piece behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
