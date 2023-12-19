91f293f95fc4dfab04547d963adc32f85cdc9309
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:57:35 +0000

    favourite include try slightly poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import sys
-  def get_python_version():
-        return sys.version
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
