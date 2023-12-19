049d78957bb2cf946dd76801f2a489e2df25aeef
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:54 +0000

    scared stick limit stick home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
