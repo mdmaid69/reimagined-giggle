28aa62a4691778c168e9f16954db8d734521a65b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:31:44 +0000

    shall sit patient risk then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 def get_current_time():
         return time.time()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
