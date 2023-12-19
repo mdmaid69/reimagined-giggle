f10083c7a02734774380939e522710afb0531fad
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:54 +0000

    example happy perhaps remove hard

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
