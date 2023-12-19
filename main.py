612cfdd5e0a7f401ec892430a6948e4a48fff4fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:57 +0000

    drop circumstance all number 573entertainment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
