2d4f5c261930fdb6b69f1b9c0fca47249278c76b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:56 +0000

    unlikely tie formal uponon attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
