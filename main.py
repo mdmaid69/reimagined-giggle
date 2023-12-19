8ce17f6f0c4d14fd17f911d08a0fa30c012403b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:19 +0000

    afford row step OKorokay ought

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
