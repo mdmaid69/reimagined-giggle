5d90797070ca87789fd894aeb4f77a76c6ca8a72
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:26:08 +0000

    genuine free contact sorry niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
