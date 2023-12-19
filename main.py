89f92876d86fb9217a9efa8d9e9fe49b5f6e1cfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:53 +0000

    every play claim approve war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
