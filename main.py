3eeabb337e2fcc51ab4641daa90a38100aec1f7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:58 +0000

    accidentally prize possible tooth smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
