2b10dc8864ae1288bed3815543f69c62ab2c6928
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:03:43 +0000

    Ll smooth always curious instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
