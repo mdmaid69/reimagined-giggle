31844871339025854d75e47e2f23773cc03e8190
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:21:21 +0000

    determined big anywayanyhow group post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
