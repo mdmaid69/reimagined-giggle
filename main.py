754f513b758de41b0de87cc846611fdefb7648eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:03 +0000

    standard narrow attempt prepare emergency

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
