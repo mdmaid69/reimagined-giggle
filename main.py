e16932696738f9002bae4b5df00aa9f4907b4f98
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:58 +0000

    increase stamp broad Ss later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
