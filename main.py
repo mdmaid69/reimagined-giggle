2e1147b48b13de263e9657a272ccf2c53f574770
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:05:13 +0000

    butter spill height content cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_ceiling(x):
+        return math.ceil(x)
+import tensorflow as tf
+print(tf.__version__)
