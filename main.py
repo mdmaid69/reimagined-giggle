74dd45e827315756cc24333ce39548d02e68fba2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:17 +0000

    fix rank in stretch course

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+from collections import Counter
+print(Counter("hello world"))
