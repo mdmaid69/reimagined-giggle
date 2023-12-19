56bfe2e6d7735c62c8f007666a090319d1e313d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:46:06 +0000

    takeaway shine promise disapprove find

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
