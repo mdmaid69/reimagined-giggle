0e9d9ea7a600f7aae14f521c6e35e81fd4149087
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:34:21 +0000

    advise entry stir card breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
