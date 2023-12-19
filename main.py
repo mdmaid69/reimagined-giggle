fcaabd3b3262a4a3ebd1c542ad04ce7d5dcac9e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:53 +0000

    will congratulate welcome soak attract

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
