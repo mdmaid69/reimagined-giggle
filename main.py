fd6cfdf4ba1293a250a2860cdd39c0bb3c3079e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:24 +0000

    far broad supply welcome pile

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_palindrome(s):
+        return s == s[::-1]
+  def sort_list(lst):
+        return sorted(lst)
