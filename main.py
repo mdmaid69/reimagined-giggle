3ff2421e3ea53f842db20852f387a646ba5cd5d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:41 +0000

    angry wear foot situation regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def change_working_directory(path):
+        os.chdir(path)
