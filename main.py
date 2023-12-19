142b1dc049d3743294817f1a7015ad49b81bf319
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:00:16 +0000

    ready callout Ss accidentally where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
