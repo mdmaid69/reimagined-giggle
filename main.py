1406c2fba9c42f9402a9064ff039425445eeaf5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:46:58 +0000

    press meeting up season drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import collections
+def create_user_list():
+        return collections.UserList()
