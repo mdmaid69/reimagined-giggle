a6050719e58cda23be2659e3d954e54ff247d2d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:12 +0000

    upstairs condition track stiff operate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
