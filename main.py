dd607b16674370a9311bd9abbae732b74268d30d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:59 +0000

    whistle treatment 573entertainment authority pin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def roll_die():
+        return random.randint(1, 6)
