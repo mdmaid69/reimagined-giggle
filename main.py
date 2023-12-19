9f8a25708680110f38457de43eeb06cd41a4ee65
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:42 +0000

    praise he connected temper public

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
