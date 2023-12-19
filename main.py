edb863ec45a8a9fff15399ab747a3d1dfc6ea7ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:43:02 +0000

    female long raise comeback object

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
