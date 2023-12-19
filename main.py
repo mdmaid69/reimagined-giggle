92f635412b7b70dfbc1149d30f9a512f234071c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:45:54 +0000

    kind pretty same plenty remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sys
-def exit_program():
-        sys.exit()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
