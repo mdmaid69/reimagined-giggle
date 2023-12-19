a6d1178af8e759f180ad1bdda3b9afd3dc252d13
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:43 +0000

    square move already ability pain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_density(mass, volume):
-        return mass / volume
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
