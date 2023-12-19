25b1c608c3d489f5aed5e4ec5ea9d0cf6c9727b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:57 +0000

    married artificial music certain land

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def reverse_array(array):
+        array.reverse()
