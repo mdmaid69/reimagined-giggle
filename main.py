637900949ecd073097331bcda245157d4aefff45
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:02 +0000

    least foot another yourself into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def find_union(list1, list2):
+        return set(list1) | set(list2)
