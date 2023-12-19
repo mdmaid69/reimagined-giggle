43cf2b32cc541cf1f4014df038d76b39f1c31446
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:30:50 +0000

    private storm pressure obtain claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+def remove_directory(path):
+        os.rmdir(path)
