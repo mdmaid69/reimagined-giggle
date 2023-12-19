5c240ace93025aba7ec4ee052e67c4e676d2f3e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:50 +0000

    send no dinner dull motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def square_number(x):
+        return x**2
