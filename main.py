34270805855e5e2377dcf5cf74893fa9d7f80754
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:07:05 +0000

    street shoot cold language admire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def square_number(x):
+        return x**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
