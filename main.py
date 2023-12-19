6edf7a4d6545ffde39e5d542995e7f55c169c6e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:23 +0000

    smile desperate switch on lucky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
