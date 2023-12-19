ad444c003d7a821f085bcf145e6636cb9b1e6104
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:22:43 +0000

    split wall Ww today roll

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
