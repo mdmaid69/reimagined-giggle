3b7f14cd148755f82232fe2a8d589239d34dc900
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:49 +0000

    guess dry hold considering keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
