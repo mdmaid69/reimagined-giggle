2314b9d65e907d9b92b9af6e90e569d4d2e45fdd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:52:35 +0000

    normal half become good thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area_rectangle(l, w):
-        return l * w
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def cube_number(x):
+        return x**3
