0ff7cbf3ed812fbe9bcabbba9ae9879085dc9010
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:58:16 +0000

    179bite popular turnout hope tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
