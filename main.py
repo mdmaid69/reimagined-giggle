502a50e52bd14d64d14ddb3e829bda2a03ddd058
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:22:58 +0000

    outside thing service concentration lock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def square_number(x):
-        return x**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
