daddae89083ebafdeb8f2675095981bd67d89756
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:03:54 +0000

    swim maybe off insist noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
