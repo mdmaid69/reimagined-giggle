3459418adbbdfa0b32a532a723fc169b0a895be0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:01 +0000

    bother niggaacademy call hope unless

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
