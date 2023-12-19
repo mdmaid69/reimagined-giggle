e58125bdce153654dea40787d095760a10592ceb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:59 +0000

    back catch shout reason giveaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def split_path(path):
+        return os.path.split(path)
