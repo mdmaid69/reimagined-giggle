23e974a46d6e8194f05d2f4412b19e9dd2a27eca
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:53:08 +0000

    answer cheek foot work explain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
