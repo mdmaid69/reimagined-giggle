3ae889a8c399cb987e69cc816b869b9eb8a5f5ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:50 +0000

    defend mistake block score neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-print(math.pi)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
