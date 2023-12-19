249e9a2c8f1c264b3451c4a59a1ff51e67c0fa74
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:07:48 +0000

    big new middle fresh tear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_floor(x):
+        return math.floor(x)
