c9661e65d6d4239a2710097390e527a3d6b5d202
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:20:38 +0000

    mark mark content crash trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
