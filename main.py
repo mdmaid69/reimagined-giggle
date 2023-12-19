049404dac7d8e8444945794b588727d28335bd42
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:00 +0000

    gooff from persuade sign realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
