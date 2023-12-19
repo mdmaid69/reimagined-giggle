44cb5d1f4415e8d2fcb0254c178249854bc2cbf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:59:54 +0000

    certainly past dick sweet use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_user_string():
+        return collections.UserString()
