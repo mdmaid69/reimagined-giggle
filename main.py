2d1e09a17cc5d2fa31431e7e0edd1162c0922196
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:57 +0000

    own boring owe have radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
