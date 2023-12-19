e56520c9d1fca68909b52f66381880d9e4c63dda
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:46 +0000

    band teach separate lose mixture

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_length(array):
-        return len(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
