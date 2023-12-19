03691b3d78ba057fc984d512527e7fd35ca1c917
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:53:59 +0000

    Mm even sound quantity quarter

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
