0248e15f8fe2a70858106271b650650a061beffa
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:36 +0000

    on cool offer neat last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
