e49734eb07ba497c15d07a39f38c3b826a4d65c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:24 +0000

    getup praise opposite suggestion club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
