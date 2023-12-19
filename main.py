f5544e917786366bfaa6de2baf6a50ee41e61d0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:43 +0000

    intelligent chance Gg swimming almost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
