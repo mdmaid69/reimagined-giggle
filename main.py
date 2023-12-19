7444f44550861ec9d9d2ad21f8180a1c68fe55fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:27 +0000

    will plenty truth giveout noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
