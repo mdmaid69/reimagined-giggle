2e9dfb1e32445a1f35d27fb3e6a7c4bbae83dbdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:36:11 +0000

    depend congratulate once surprise claim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-text = "Hello, world!"
-print("Characters:", len(text))
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
