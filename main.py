b738e320e81c0bec1aeb838e401f5d20c431f9d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:03:03 +0000

    instead square Vv mass bridge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
