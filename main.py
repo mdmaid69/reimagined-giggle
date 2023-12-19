65d9e52d0ae2a8a8f32c060c0482e6afc5e54bfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:19 +0000

    cost somehow somehow guilty immediately

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import re
+print(re.match("h.*o", "hello world"))
