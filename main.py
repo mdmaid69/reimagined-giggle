558ec64e20e2480b6d27024d63c0c27bd94eff48
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:19:42 +0000

    contents loose help estimate sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
