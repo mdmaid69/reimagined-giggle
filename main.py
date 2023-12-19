e0f4e6ad6af02b80c19de1e70500257260e25a1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:43 +0000

    concentrate drive congratulations herself Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 n = 10
 print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
