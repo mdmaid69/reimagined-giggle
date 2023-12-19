988b97a2608d63a756123e81b3fc8b26358ac7b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:53 +0000

    best design offence apart comfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_queue():
+        return collections.deque()
