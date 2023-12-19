0e4fa5ba870e17eb64c5a06a54b19c02ea11e009
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:28 +0000

    turnover repair ground giveback narrow

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def convert_array_to_list(array):
+        return array.tolist()
