885ff124728c6cef8fff8ca32f66393bc1cc75e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:57 +0000

    rub easy exaggerate under only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
