04bb9e6c7f284dd5b21491456753e19cb170fab2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:01 +0000

    wake cause reason holdout stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
