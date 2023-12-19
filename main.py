94e320f675bd35cb18200a00773fd41418ed0732
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:38:42 +0000

    buy do offence calm sharp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+def convert_to_hex(n):
+        return hex(n)
+import re
+print(re.match("h.*o", "hello world"))
