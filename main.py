2090680b6dbb0c3b129267efb700020ddacc2112
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:02 +0000

    565end swim telephone upwards ever

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import sys
+def print_python_version():
+        return sys.version
