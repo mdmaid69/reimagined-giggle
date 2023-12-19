43607c5b1b11fd29d5407394e7e95ce08f63cc37
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:26:17 +0000

    state accept circle this workon

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import array
-def clear_array(array):
-        array *= 0
-for i in range(10): print(i)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
