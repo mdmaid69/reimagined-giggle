b3d2834125cb5668b646fb1bbb80f97033453a58
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:45 +0000

    car suffer fear happy quit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def multiply_numbers(x, y):
+        return x * y
