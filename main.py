6ef9ca962a082bbabf37c85de321e2bbbebddfa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:45:04 +0000

    autumn though imagination autumn ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import sys
+  def get_python_version():
+        return sys.version
