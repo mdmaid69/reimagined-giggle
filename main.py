ca21743d7003cddf1583d4d8df5c4ba2be124afe
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:44 +0000

    bath purpose scream risk church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def reverse_list(lst):
-        return lst[::-1]
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
