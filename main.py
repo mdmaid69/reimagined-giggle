3d1d263c3f6b99348d6a10b8f7aab66f5cc88680
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:50 +0000

    beginning cook rest show shop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
