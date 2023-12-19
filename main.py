ec985ea0c91ab2d021a4a80373430ef36c454947
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:35 +0000

    block sense home giveout tense

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_list_from_array(array):
-        return array.tolist()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
