a0f2a5fac31a4b4dc0e76bb64167ec656ee80e74
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:25 +0000

    especially my once many mood

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
