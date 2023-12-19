998f70ae7b71cce58bed9b34a0c36d5f7c04bb66
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:00 +0000

    apply turninto before exist view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
