ef22cf85f617910c54934ab16d7fad35e50cc033
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:08:04 +0000

    offer word radio joke air

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
