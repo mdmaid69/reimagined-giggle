582b6a5e085ed1f5b437e30a5f50893635d80024
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:07 +0000

    onto this bag plant transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
