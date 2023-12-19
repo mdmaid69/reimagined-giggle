fc97ea534925d20e871355fc9643ad701e10cdda
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:21:41 +0000

    tool memory moment computer evening

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def add_numbers(a, b):
+        return a + b
