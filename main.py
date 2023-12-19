ef064993e953732f30d8bcfaba0a497a79ea47a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:04:19 +0000

    injury exist defeat Oo drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
