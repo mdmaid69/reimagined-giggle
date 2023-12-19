8b0e150aa9a331eff3849c6e7be5c28b0e08b34e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:07:47 +0000

    season punish fairly build vanish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
