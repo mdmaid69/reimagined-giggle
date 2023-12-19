006578cd75c7e255489b2cf25b9faa5547d84739
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:19 +0000

    hurt tax touch thin own

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
