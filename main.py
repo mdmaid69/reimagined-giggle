11cdc1f07e866ce5b9ef07e0abb550968db549ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:23 +0000

    among cry hate same ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
