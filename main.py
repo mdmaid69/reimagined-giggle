c815afe4180bf28306d531e6cf8d0abc9b6cec15
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:53 +0000

    anywhere talk personal careless fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_acceleration(speed, time):
+        return speed / time
