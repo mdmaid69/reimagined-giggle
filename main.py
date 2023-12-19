6acba1b0a1e5258df46c8b22792bc0289b959c04
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:35:01 +0000

    set effort active cruel small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
