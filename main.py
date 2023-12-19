29375301cb2710b9bdad5ee0779f795c3ab65f3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:36 +0000

    himself field mass statement plastic

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
