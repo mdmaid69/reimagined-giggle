7ffd688475f298afea20c8a4a32a669eed91e541
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:49:07 +0000

    speak hot start personal grade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
