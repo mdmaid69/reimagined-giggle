b4a1268ef4fbce46d192038828df4be532ddefaa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:36:15 +0000

    please collect remember content turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
