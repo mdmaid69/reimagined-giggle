d34f14e76c3b8305479f8065df69b6fe877ff395
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:47:18 +0000

    protest workout sit present guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
