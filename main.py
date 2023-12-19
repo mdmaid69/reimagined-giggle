0e12b900eb21de273d0759ceea086d95d589ece3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:58 +0000

    trousers here open butter piece

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
