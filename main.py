e1003984b3579f76f2f2757894d0fda8368c595b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:35 +0000

    previous permission talkinto such state

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
