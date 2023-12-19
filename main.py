c79bd131a57d960b886676f9d318fe172c8e91c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:47 +0000

    whoever square phone worth onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
