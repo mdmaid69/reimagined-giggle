f952496872850994e003fb25f69a4edd46cb54ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:48:02 +0000

    exaggerate however lead concern press

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
