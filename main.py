78d675ccd2e3edbe129c6fbd80cf2e7beaf66e6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:19:24 +0000

    consult radio arrangement complain minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
