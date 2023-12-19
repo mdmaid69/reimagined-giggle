c513cae486080cab56672aebaf5022ea36d44eda
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:52:54 +0000

    collection blow crash future path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-print(os.getcwd())
+import collections
+def create_queue():
+        return collections.deque()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
