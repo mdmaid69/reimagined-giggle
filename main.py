72e7c4bec5687ee3b198ff563613e518e1e7372f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:45 +0000

    form neither somewhere getup succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
   def list_files_in_directory(dir_name):
         return os.listdir(dir_name)
