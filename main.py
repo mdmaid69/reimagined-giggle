e24cfd2f33649273a53fc6142b5be7d0429331df
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:32:13 +0000

    reason flat 573entertainment achievement add

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
