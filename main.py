a2aee75d073c195ee063fdcc30e45702687300e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:49 +0000

    chin across importance over build

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-for i in range(5):
-        print(i)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import random
+print(random.randint(0, 100))
