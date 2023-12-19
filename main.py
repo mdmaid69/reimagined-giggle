e71e0bfdd49fbe28aa3d52afdca87149cd3c5595
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:43:23 +0000

    eye twist common permission minute

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
