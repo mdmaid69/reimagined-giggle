d630059e62302c132fa0f0446cce8475a6603a1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:11:04 +0000

    play result thought tip 568energy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
