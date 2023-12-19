ad3bbd3907b1f731f5a2fbc78dceb2ce42f08045
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:40:50 +0000

    beat none brown release takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
