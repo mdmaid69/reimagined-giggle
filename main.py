5b8b557ab7c71e13e493ba49e1e37368e87b5a69
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:13:33 +0000

    coat shadow swimming such not

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
