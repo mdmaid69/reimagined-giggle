5ea82281b5927120525e37a3b5f960971c6efc7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:47 +0000

    public among translate official collection

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_speed(distance, time):
+        return distance / time
