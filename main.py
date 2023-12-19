9919a17483a1556179dba8a559374f96ba67ddf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:26 +0000

    record introduction sit vote rent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def add_numbers(x, y):
-        return x + y
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_bool(array):
+        return bool(array)
