0ea20221f860343b719722f0cd9673fb6ad55530
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:09 +0000

    also surprised basically change memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
