473fb5814c055349cc2f5bb35c66e6a601cf78a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:50:19 +0000

    on downstairs pile human bring

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+def print_python_version():
+        print(sys.version)
