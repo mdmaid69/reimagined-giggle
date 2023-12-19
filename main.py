66d4a9486d4769de220f9723554858dee8111a34
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:26 +0000

    blame thorough stop petrol change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_max(numbers):
+        return max(numbers)
