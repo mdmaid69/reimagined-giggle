c3cbd024ed5d94408197e5b4155b7949b287d7e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:47:38 +0000

    open Nn children funny person

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
