aa12bc64c726450dfea6d0dd6d6fed70c635ea1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:48:55 +0000

    part only put night course

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_tuple(array):
+        return tuple(array)
