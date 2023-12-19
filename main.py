7011b4890761edea492a049881b9ed5e4a94ea35
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:51 +0000

    bunch emphasize contact book tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
