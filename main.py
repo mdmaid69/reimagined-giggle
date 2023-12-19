751d6039487d23bcb6dc3b35274888c829afa36b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:59 +0000

    unlucky most turnoff experience public

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-print("Hello, world!")
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
