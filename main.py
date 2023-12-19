7fdd77360fc4732a29d4f32f223ff5cc5c4f5235
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:11:36 +0000

    precise stiff force result land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
