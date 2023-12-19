13ee3e13bffaa1ce8ecbf03ca02885494a39c451
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:31 +0000

    mother thankyou obvious contribute recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
