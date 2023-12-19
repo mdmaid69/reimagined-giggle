8062979e409bce737be59207e6258a12825f3ec1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:54 +0000

    stretch sick stamp common trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
