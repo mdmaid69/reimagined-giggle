191aa5749c0144508a54e5e176a91a8013bdaa0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:08:34 +0000

    shrink niggaacademy again hard sad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def square_number(x):
+        return x**2
+def greet(name):
+        print(f"Hello, {name}!")
