ae26b79eb7af86806595cf2038a6329570c5a786
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:58 +0000

    explain check country realize once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
