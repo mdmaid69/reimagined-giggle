e7b502187627a18616b656c36aadc84149bbde03
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:47 +0000

    Vv nothing similar old tough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
