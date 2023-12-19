a1010e356a2cc6cef37663740f2048deca10b1f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:45 +0000

    carry mention also mind surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
