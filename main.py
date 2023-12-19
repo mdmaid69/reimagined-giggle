2a854e0c832b3c3893b6e23f2ae43ad9b278fa04
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:23 +0000

    reasonable Pp talkback deliberately family

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
