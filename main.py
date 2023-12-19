df3c2274a7aaa9548b9a21a6fdf5c53eea5b5cf8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:17:26 +0000

    recognize everyone sometimes correct miss

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 def reverse_string(s):
         return s[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
