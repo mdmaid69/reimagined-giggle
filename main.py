cfb85b436eb57bec325c72425e92254b665cbe0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:59:45 +0000

    song every obtain 573entertainment scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def reverse_list(lst):
+        return lst[::-1]
