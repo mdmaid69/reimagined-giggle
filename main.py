d411734af152c031b8695851c29e5b51fdf6c929
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:12 +0000

    kind team cough surprised listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
