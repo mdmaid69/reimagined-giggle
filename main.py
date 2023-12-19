32a55080ff55cb68c2b7171af259c1250e284962
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:33 +0000

    cost sore off scared flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
