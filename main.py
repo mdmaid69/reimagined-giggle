be0f1192bb2773d1ea0e7f06e7ca089ffb796ea6
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:13:14 +0000

    throughout depend rush government holdout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
