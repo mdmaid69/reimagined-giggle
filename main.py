360379742cb888efe818739c363443c00a0e303b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:18 +0000

    connect reply bury its comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
