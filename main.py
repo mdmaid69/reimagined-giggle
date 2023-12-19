3ac310655e3ad8e442ffb6d74ff7bc8dbac079ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:22:52 +0000

    home reliable violent change build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
