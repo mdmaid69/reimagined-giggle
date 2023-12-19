9b6e2ac53fb616b9d9fd986bedc564fc2724aa9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:45 +0000

    target team round regular year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
