1d9eb5b3b3fd3e4342cd12c4c8def57089321c15
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:07 +0000

    contact probably immediately after closed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
