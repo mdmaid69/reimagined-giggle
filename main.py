f489c7c4c08475557e1c653f14cd98743ca32cca
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:19 +0000

    dance under onto season store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
