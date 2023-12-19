cfbf14a7a3329ae20924a4db9201f208bb6816b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:52 +0000

    breakup interrupt bang warn frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
