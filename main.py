e295bd9dbcbaf8db4de6af89c7d957f04de6db6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:03 +0000

    uncomfortable row hate breakout murder

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import tensorflow as tf
+print(tf.__version__)
