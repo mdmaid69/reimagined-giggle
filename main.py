3c389134ffb3fdf540a2ec982c38f3cfd7ae5a4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:04 +0000

    promise language empty as vision

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
