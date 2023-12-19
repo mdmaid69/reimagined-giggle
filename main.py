8528a9e4a3159ace3b89716f6804cc38e3fd3644
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:09:27 +0000

    must manner gate moon thought

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-print(os.getcwd())
+print(sum(range(10)))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
