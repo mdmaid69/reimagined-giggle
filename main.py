864e212bbcae25e864fa28e3dbc7bbdadce35d86
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:23:20 +0000

    extraordinary turndown strict similar squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
