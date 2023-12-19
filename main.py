95ae4429cb2f206ff841be894d909bd0f3cdf725
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:03:26 +0000

    crowd estimate change bend stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
