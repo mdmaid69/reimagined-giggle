5dd9e99e7f5a2a67e86464665ce16ede93e6e1a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:49:06 +0000

    round meet aim achievement unfair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def clear_array(array):
-        array *= 0
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
