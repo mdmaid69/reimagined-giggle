2f0fb9f9670c9ad0c9c319778fcaf91a5ce06c4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:55:06 +0000

    dinner serve apply Ww base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_current_directory():
+        return os.getcwd()
+def calculate_power(work, time):
+        return work / time
