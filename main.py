9ad46e5ecb714023196f1b4dd1afd30ba357cde1
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:51 +0000

    appearance interfere forever follow record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_item_count(array, item):
+        return array.count(item)
