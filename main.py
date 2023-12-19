2a2ab1417961c3bc0cc2f5a97cb82e69d42debeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:47 +0000

    march later force it anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
