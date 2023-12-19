7a5dc71b6b035b05b7a6eede4a8a492cb9af83a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:14:11 +0000

    fail secret delicate workon win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
