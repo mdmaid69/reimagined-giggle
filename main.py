5c52f419ad7802be526ec017e1843d696787623f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:28:13 +0000

    interesting repair who however around

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
