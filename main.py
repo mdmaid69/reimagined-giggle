91ece43d0d10fb8318a52f843b4ba4f6bf53b0f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:17:23 +0000

    little whistle road roof sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_acceleration(speed, time):
+        return speed / time
