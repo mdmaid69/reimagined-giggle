b6ad136fc3387a25180fe4222fa8061ea24704ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:46 +0000

    discover check comfort favourite leg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
