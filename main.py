f9b6c11853c06f57fa1cc627353226b13c88ad2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:20:12 +0000

    house have definite low stick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_inode(file_name):
         return os.stat(file_name).st_ino
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
