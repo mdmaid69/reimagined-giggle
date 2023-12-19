288e9390aae2547b208a438daa1d8349d12bbe15
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:49:37 +0000

    best rent cost reason mood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
