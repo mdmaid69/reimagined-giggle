2aa097b580d4d38169d9cf0c2479ad0b00b4fe07
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:21 +0000

    communication unless feel female soft

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
