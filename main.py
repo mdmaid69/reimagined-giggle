2f33dc4151dc05d1a382607f648de2cb43323f5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:41:23 +0000

    vanish rely dream mention mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
