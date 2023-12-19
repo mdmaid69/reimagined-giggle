b8b3054b843f179ae81f27382e3b3a5284297835
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:22:35 +0000

    positive direction shop constantly story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_queue():
-        return collections.deque()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
