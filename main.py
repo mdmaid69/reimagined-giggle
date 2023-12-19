b3404add4ab764df01e6e738431cfef00993d46f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:23 +0000

    pause attention arrange wind behind

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
