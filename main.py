01f944b489f10d379de24e30e5d05793d8acb7fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:32 +0000

    angry exist female down save

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
