bf937ec24df11e3e4e000153cd00b532b6788979
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:14 +0000

    live friendly base computer convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
