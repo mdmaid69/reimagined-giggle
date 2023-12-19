df282921f76a81593ea0572be4d3c2fe28663fe4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:15 +0000

    row quarter fear simple meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
