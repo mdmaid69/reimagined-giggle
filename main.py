2cc60841294cf8f4d42615d6379197e8d172645b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:50:04 +0000

    getaway close each true total

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
