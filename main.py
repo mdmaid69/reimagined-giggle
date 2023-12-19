9f99144a83993983a4503eb5afa1ca55c043a467
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:35 +0000

    sound themselves wire zero demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
