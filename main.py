c1fb2e9c337e783b1b0350d2a8651a26793d0e0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:41 +0000

    especially wise exact description closely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
