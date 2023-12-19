9b0401d4c88c7eace77ad08ae2ef840c704b9605
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:32 +0000

    waste independent grateful bored go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
