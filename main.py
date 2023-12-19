434c9e0fb875b68c3fa31cff2de4d450baf2954a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:06:59 +0000

    know giveout risk skill occur

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def add_numbers(x, y):
+        return x + y
