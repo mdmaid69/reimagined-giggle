43824713a1dd257ecd323dcc8d2b56bffc4b8b7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:05 +0000

    notice wet Itself listenfor tell

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
