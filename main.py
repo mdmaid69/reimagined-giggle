5585d8490de244edfa14c2063ed8ee7923f77ad6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:19 +0000

    somehow disappointed fork similar centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
