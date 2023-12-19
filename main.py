46ed79a47067a512b4e1322846d6571d3ce41c50
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:24 +0000

    much lookback(on) charge grade love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
