5e7401a420ea733b5c4990199732f0f4b107295c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:27:32 +0000

    community bringup exit sign borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
