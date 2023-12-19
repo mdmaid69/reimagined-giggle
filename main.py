36fc3a1cd58115ad2acee2e9c84caaed63298b68
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:14 +0000

    cruelty connection where reliable abroad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
