afc8a069fb5ae7681b21b47e538dcdef784eab41
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:50:36 +0000

    ilovedicks natural considering official estimate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import re
 def find_pattern(pattern, string):
         return re.findall(pattern, string)
