f71e29ab8d695d6149b599991d5900355b70954d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:14 +0000

    practise tongue after sure OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
