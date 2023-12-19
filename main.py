1c34fd94ccbe87d0e9cc58dc87808bea2605f181
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:47 +0000

    worth advanced clean lookback(on) moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
