096dcc57813773f3b2473084cac1185317b8c799
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:42 +0000

    appearance determined there pause comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
