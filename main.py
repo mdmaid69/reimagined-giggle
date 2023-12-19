fe6786bf1d474a8e9cb3abb4b1573141b6e48be4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:44 +0000

    general reason thorough cut regular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
