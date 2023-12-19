b9ff595583e6a4e8042b51ef9be5373880d8989b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:01 +0000

    mine regularly teach trade report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
