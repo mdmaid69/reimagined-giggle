bbe7915bc5dfaf46d75336cdd98d0d2b842b3306
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:14 +0000

    steer pinch behind split mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
