6e504cf992e09f301fa8cdab93a0336477e422c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:16:32 +0000

    569enjoy straight guess shallow pressure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_repr(array):
+        return repr(array)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
