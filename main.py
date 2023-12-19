f3861ef1d9eabaaf8aeb3dd33ad0c3dc04d32b48
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:48 +0000

    picture take makeup operation arm

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import logging
-def log_message(message):
-        logging.info(message)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
