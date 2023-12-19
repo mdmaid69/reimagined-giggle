a64fdd7fccdcf24c154d6a1a3b7f3b38cbcfc918
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:24:43 +0000

    field Oo sign date expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import getpass
+def get_username():
+        return getpass.getuser()
