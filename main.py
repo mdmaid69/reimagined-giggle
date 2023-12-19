e340e561dc38008ced9e62b6241a0464744b90bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:06 +0000

    polite form cold at Pp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
