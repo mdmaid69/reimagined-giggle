edc7b33133e786ff3f423de69d24bbbad909c296
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:11 +0000

    squeeze ask control land very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
