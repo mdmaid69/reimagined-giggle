ec555d744bfb5ccd80eb68da19e1c8042aae1afa
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:34 +0000

    bar force whistle professional funny

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def convert_to_binary(n):
-        return bin(n)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
