3dfc3ee073144e9e5c89cec5aa87e43a88bc9302
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:39 +0000

    shake print range list major

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
