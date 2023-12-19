2ca8e97d0c99c5c5029716d12bcf34a91ffae8d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:33:59 +0000

    weather bath anything whenever maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
