113b678a4fdadde936e4c3cf35f91dadf8c50525
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:34 +0000

    few satisfactory last comein ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
