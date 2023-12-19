da20f206c76357ee228cf46121298f08c3058c43
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:17 +0000

    rescue any petrol trip drop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
