53e7a4cf5f766949cc50a16c9a43d7d2516df4ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:44:27 +0000

    of inquire vision getback considering

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+print([x**2 for x in range(10)])
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
