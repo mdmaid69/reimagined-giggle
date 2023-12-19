c4d7c8cad14bd0f4bf4588a15d7cbd6cd7fa2c8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:36:08 +0000

    thorough influence downstairs no substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
