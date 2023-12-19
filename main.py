f70c1e77a250430be51e871a60ed7d212e5244b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:45 +0000

    really preparation obvious approval forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
