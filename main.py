656b6d777c196781480f36a6632979fbd7c10175
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:43 +0000

    brain scream about near part

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_bytes_from_array(array):
+        return array.tobytes()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
