7bc96ea04ed447dba1d494a5634fc7e2976596af
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:46:16 +0000

    tone fun keepaway spray mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
