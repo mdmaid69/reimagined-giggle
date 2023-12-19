98095107c3a0c73072a54370475347c99750e3b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:27 +0000

    married outside reduce record actually

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
