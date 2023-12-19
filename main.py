c2950daa690e2a103f647bf0e26a3ebb21cbe5ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:06 +0000

    measure scale fault callback seat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
