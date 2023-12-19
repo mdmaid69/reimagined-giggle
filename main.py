05fd58f52d812471b1866e0e246122567c1d02b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:56 +0000

    capital scare breakin rough wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
