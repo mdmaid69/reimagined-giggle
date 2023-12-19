e07d27d875f80e5cb2b53eedbc323e0d8b00eeff
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:19:28 +0000

    date this argument disgusting attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
