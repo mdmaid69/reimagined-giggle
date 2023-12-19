f199e59a43d50d165fb9d38c5e1d3002eaf0664f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:17 +0000

    left what glass interesting error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
