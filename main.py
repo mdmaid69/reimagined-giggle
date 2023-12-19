7c6dd2a4b26b118641367371debb758773591971
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:43:06 +0000

    task teach frequent excited substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
