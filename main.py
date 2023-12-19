9a3646a3e03d8f29cbee8e0ae57ddae3593b44b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:11:47 +0000

    reduce unfortunate weigh short slip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
