e4a29118a733665c67b0ae91a18dba48b9b96518
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:58 +0000

    offence light recommend harm towards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
