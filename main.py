9e04ac39dc0fd9f3edc7bd8246196233e1e7dabb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:20:15 +0000

    proud glass cruel off education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
