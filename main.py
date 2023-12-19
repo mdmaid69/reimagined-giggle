0697b86bb43c612c7fbf17f397d75e5f3fefdb45
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:29:50 +0000

    wind it ring scale later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
