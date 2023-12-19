53098c9aec08aec0db262f570e00c6e19abc0e94
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:38:10 +0000

    soak soon dry book circumstance

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import time
-print(time.time())
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def is_odd(n):
+        return n % 2 != 0
