ef063c343218a61cf696ddc81385980982f96b65
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:20 +0000

    rescue certain advise classroom mysterious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+for i in range(10): print(i)
