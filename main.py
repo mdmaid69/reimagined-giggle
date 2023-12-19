d1736fd425b0d35b88adb74105adb13b2d1440f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:44 +0000

    autumn giveaway occur sorry holdup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
