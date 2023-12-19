31c971064dc84567a1c6ed76bcaa324bba1b8dae
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:03:11 +0000

    butter happy store meeting punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def print_python_version():
+        return sys.version
