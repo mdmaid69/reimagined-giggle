9ac6911796f2c3a99e9865f9a3b344d0cd31e788
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:30:25 +0000

    remove continue back Gg watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
