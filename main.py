999365e38284ea9b0e6fcee4b4755d736e5de7c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:37 +0000

    only trouble schedule regret affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
