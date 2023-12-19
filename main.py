31ff274fc0e39cc9984d7d706aaf09e6d0ffd254
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:39 +0000

    fix innocent pain post remember

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
