97683a5a85b9f093d816ce1c026dcc8f9fec1d1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:14 +0000

    warning tomorrow over use feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
