ab7033a78440e06e08dbe76fa3588c2ea18457fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:56:18 +0000

    narrow pain keep temperature object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
