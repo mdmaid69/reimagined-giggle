d56d811ccf073e76eebf6cc4851dfe6bcb163e3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:37 +0000

    arrangement hollow much Hh heat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def count_words(sentence):
+        return len(sentence.split())
