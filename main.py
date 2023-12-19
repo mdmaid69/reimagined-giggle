f39c49c9799220bcd3bc3d8b5203968465f4714d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:16:17 +0000

    meaning tune sit operate rough

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def is_odd(n):
+        return n % 2 != 0
+import numpy as np
+print(np.array([1, 2, 3]))
