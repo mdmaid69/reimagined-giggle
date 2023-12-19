1d41074ac35e7418e18e02a220b0a61640e5d2d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:28 +0000

    interrupt anything look below Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import math
 def calculate_arc_tangent(x):
         return math.atan(x)
-import array
-def get_array_as_list(array):
-        return list(array)
