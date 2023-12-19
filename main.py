baa2b55d123132ade2b054c8cbd2f274f0ae012d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:43 +0000

    exercise want physical bush plan

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
