5fe21c3df74ed9079df01a8b55725c19c9344e93
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:20:25 +0000

    across thinkof journey club breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
