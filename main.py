825af3eaaacbafa74afb39bf4fd0ba427038c816
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:33 +0000

    Bb press react advantage which

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
