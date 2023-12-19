d2a73c56dcfff3f5d8fdec1de8ed2f076aea1c92
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:23 +0000

    away keep difficulty completely especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
