2d09d281e5a6daf39c4ccb3444bd7608cf64eeaa
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:53 +0000

    look nothing woman kind skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def convert_to_hex(n):
-        return hex(n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
