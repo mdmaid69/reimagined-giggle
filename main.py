7de3b16b6b1d1974486906cbfcb438e85e874073
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:42 +0000

    shoulder first gather emphasis summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def convert_to_binary(n):
-        return bin(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def square_number(x):
+        return x**2
