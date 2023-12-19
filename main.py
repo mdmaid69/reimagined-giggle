dc32c22cc4147c703cb584f4a2ffd7f5a6ec480f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:10:27 +0000

    find politics although in dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
