e2b161728514664a720e8c842c52d72dc99b5dd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:25 +0000

    rise busy dangerous profit likely

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
