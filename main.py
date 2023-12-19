2d3f857767388fe16d0dfb428af98e540365291c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:56:37 +0000

    dead worth plate warn start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
