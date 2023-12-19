93f2876205d95c21313bc8d7b85fffa7438f1a75
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:39 +0000

    message hope blame light pencil

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
