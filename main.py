34f09c6b2ebebe37fb4689e01f05f8188e11fbef
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:51:59 +0000

    worry surface teach brain need

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
