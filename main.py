a5451e840aba6754ad29aa2a250058996d64fd14
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:57 +0000

    circumstance term area condition morning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import platform
+def get_python_version():
+        return platform.python_version()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
