4e9d5facbc57b0fe9190ef17a642daf191e5aeb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:49:37 +0000

    event spot solve tough spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
