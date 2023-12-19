6d3a17e7a827d0392b67b41e940ff05a1a9ce390
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:08 +0000

    sight tie 565end then practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import time
+print(time.time())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
