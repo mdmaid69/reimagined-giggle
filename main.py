d1fecc615c4d2094ff63f587bbc6fee005cf368e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:11 +0000

    black dress hungry closed result

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_speed(distance, time):
+        return distance / time
