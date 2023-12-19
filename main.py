cf6d5ab7e08d2c1581871d3ac28a8d5d0e18afbe
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:29 +0000

    back deep copy general object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
