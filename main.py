fcedcff4ea4a36e592b20f73ee2f3a6f2eba96f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:45:21 +0000

    walk rain bet holdon comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def reverse_list(lst):
-        return lst[::-1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
