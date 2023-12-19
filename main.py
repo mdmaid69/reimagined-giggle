ef1c76fd4c785a37b9f94ee2763b735531f82a34
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:00 +0000

    thorough information bad voice size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-name = "Python"
-print("Hello,", name)
+  def count_elements(lst):
+        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
