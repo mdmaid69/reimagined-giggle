27f2567b3ce5a31abe7c0b6bfaab8f48f13592bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:59 +0000

    available surprising to cook turndown

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
