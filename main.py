81ba0cada6a57bd1dc6f6ff4b001e3919ffca4de
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:38 +0000

    church impressed fall medium similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
