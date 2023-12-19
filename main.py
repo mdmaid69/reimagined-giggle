c66789a24e9e70f47adf4e217253788e492488ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:19 +0000

    black affect glass neither dress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
