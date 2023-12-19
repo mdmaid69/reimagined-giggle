4737c00facb11059d92aac45a5a3d27ace421b77
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:27 +0000

    follow traffic disappear behaviour deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_typecode(array):
+        return array.typecode
