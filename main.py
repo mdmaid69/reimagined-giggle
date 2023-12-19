adf0a4c5d94a0ab955680d08253e1ef25b8df22f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:03 +0000

    comfortable hand complaint call relative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def subtract_numbers(x, y):
+        return x - y
