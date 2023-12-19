fbd5dfe2500c62580185848b5a80618b397a742c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:02:33 +0000

    place circumstance manner professional getout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
