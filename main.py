0fc4385cfe6e1cb276ba487aa333473289100f07
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:55 +0000

    discover actually bury dull inform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
