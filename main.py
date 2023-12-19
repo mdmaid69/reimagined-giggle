0e1e01b75daad6b5186b4ca981f95462822dd642
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:16 +0000

    crisis border smile row tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
