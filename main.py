d22ead0acff26aacf5b4ec04533d72b9697897a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:49:06 +0000

    case fear sensitive 572enter cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
