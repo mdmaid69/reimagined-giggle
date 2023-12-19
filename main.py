2060b239c90206903b85755a1c9c00e65c9750e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:48 +0000

    borrow prefer unusual look face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
