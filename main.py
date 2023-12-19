df9f2a9852d22ede7249085695636821d0157ae6
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:09 +0000

    fact party way wide bottom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
