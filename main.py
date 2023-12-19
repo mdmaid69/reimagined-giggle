141085717f55a514b60a642bf419e4884874237a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:18 +0000

    Pp like community suit week

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
