a612e04615cf7801f760aa2b4d134a226b82e18a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:19 +0000

    wear any community pollution sensible

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def reverse_string(s):
-        return s[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
