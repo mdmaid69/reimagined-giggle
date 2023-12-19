107fc4ac29b4282d4f08eb1492f30faf544b67e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:05:09 +0000

    soon success record trust bar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
