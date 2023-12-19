b311e3ea409623e6bff687b3ee1016948b9ff3af
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:14:38 +0000

    bringup night material keepup split

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
