fe1a4806be5e49ab2c25f5d995778c3ed7faf5a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:09:24 +0000

    physical leave cheap introduction translate

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-def square_number(x):
-        return x**2
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
