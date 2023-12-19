a9b732e843ab48ca6298d8add4f21b3c10f82dc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:46 +0000

    use choice everyone lookafter workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
