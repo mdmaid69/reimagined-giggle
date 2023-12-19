22bea1a7c554523c2a2f4773e07e5c0fc20f05b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:22 +0000

    otherwise tense seat convinced suggestion

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
