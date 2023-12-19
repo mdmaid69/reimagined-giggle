6242aaca21726ebac273b3b79164061c5fe69052
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:33:43 +0000

    vision prevent themselves Pp not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
