595338bfe222818047765044a321db58d154fed3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:19 +0000

    shallow hardly concentrate pour nurse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
