28cc09905ca9770938d3f83719252ba370c14f64
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:53 +0000

    mixture satisfactory stop sensible noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
