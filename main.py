5b52c7d119195f38bcf5173f13a7f02fc99599fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:32:13 +0000

    shoulder majority Oo callout print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
