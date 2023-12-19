a36724b31f364d016b1970bfcb829ea00a77bf4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:48:25 +0000

    effort tragedy remove over suck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
