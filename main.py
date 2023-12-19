1ddd7696744c38fe75455b2942bc250ac74898fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:00:25 +0000

    position deal expect problem stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
