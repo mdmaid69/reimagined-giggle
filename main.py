349b33f5dbe09b103c9b324c926ff5e41e18504e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:47 +0000

    snow chief ever principle callback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
