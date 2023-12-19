5ca0e0f0935b8acda1f0e4ee85d3513f0ecb062c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:23:41 +0000

    smile describe almost goout comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_volume(length, width, height):
+        return length * width * height
