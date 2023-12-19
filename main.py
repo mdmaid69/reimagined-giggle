bf2e2bb3f84b01b325c3e063ad3fb101bc3bb66e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:59 +0000

    lead artificial want bored worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  def remove_duplicates(lst):
+        return list(set(lst))
