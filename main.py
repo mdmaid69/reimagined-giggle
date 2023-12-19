daf4e62bff4eeb6cf98b621de57ecfe794bb535f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:15:01 +0000

    sting post fear example length

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print([x**2 for x in range(10)])
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+print(sys.version)
