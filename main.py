287ee9ee2e7672d0e4f8afa3c7b13205adeb827a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:22 +0000

    heart fast spray shallow relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
