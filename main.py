9fe68cae873304d7920e118b86f2adb96439c0ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:19:36 +0000

    happen eat acceptable winter glad

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def add_numbers(a, b):
-        return a + b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import math
+print(math.pi)
