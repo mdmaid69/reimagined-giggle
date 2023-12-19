41fbf57ae07b7e375d0e7dbc1ce0fe8d85535d42
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:35:40 +0000

    tight shout swimming suppose land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
