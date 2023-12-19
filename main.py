574809e4dbd6c286f1e87b9890784d3eef31f441
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:21:04 +0000

    direction Ff exit terrible mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print([x**2 for x in range(10)])
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
