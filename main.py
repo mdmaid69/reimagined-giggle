3467a88b9a4e20c2ab9ad10c3254e812810c1183
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:16 +0000

    takeover rescue experiment defend opinion

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
