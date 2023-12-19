57ebc597d4f259cf1096b9969864c93087919457
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:16 +0000

    Oo many uncertain explain way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
