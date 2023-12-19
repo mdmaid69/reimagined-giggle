02db478070827573f67a6411650dccab45df2906
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:12 +0000

    swimming sit shame concentration balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
