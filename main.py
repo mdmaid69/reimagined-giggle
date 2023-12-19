951eebbea15955c43ba5ebd58a549633b96e07c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:27 +0000

    sad case confused common exact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import datetime
+print(datetime.datetime.now())
