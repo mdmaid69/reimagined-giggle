00ef237dfe5f4c87a23ffb09239b65f9612f6d78
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:51 +0000

    improvement injury equal thick cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-from collections import Counter
-print(Counter("hello world"))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
