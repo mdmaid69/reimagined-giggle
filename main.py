9132b6f6e335d7444ab7251fc8268a9845461b9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:08 +0000

    downstairs edge life machine low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
