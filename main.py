29a3f5ab9843c3d37cd9f48be29c585eec6880f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:26 +0000

    favourite need computer brief round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(numbers):
-        return max(numbers)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
