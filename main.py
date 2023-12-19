5baa7c766d161e000c69957914bc65c457c2182c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:22:17 +0000

    main 569enjoy force none flood

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_hex(n):
-        return hex(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
