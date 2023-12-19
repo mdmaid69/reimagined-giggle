924ef5a762e83d5869db28aa245a288b9995590a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:02 +0000

    part proud clue worth sweep

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-name = "Python"
-print("Hello,", name)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
