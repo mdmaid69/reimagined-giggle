8a42492cc9e452a90a6a69ff67a592fc986f10a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:32 +0000

    moment depend definite Kk general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
