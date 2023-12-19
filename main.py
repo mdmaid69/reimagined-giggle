e1eaf6c3ba71247d6d7fd6abeeac2e491f3b9700
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:25 +0000

    right confident make rush hurt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_speed(distance, time):
-        return distance / time
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
