393030125e678f5bbff3ebeb2bd1da37825571f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:28 +0000

    weak shelter take us most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
