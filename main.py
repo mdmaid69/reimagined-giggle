04df2acd380ee0f1e27fdb26be79d0c53938f734
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:47:32 +0000

    sick glass file match seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
