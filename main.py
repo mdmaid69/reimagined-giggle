649063ae8449745ae99ed91a696b7f19648806dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:54 +0000

    bury friendly knock half pause

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
