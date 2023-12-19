8848615710339958d4bc3d292c0fe7975b7daecd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:17:58 +0000

    bound exercise freeze flow in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
