9747f487b8d3e4dac66349beaf20ef23e3e3cc6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:28 +0000

    pipe combine block kill take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
