29048582598ccbefe877283ca8d76548eebcd433
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:39 +0000

    favourite model off zero sensible

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
