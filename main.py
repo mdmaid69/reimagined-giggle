2130c557a4cddfe3bd30029553c99f5f586ac1a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:31 +0000

    dig home independent convincing uniform

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_pressure(force, area):
+        return force / area
