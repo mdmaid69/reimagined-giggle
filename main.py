217a133868eee490523b042f3711488651f85fd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:44:55 +0000

    slowly use need slow community

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
