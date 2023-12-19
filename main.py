056f1838897b4c1224144d9527e4c509da2fd003
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:31 +0000

    reaction claim bang mad ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
