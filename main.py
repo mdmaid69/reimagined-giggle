7711414107807adf5e1af3c4c4e37d53370bab5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:26 +0000

    I he agreement small read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
