dd82472eef50b1eb915fbd694dea821594f2d58c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:36 +0000

    act fat compete clean arrival

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
