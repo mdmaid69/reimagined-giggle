0614747a2edb1b455129bc04adc3f0b688e261d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:00 +0000

    prepare sink account repeat environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
