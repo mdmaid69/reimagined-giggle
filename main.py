9e4115a68c464ac1c2562ccaedf86ed768b232a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:45 +0000

    cry fresh luck label increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
