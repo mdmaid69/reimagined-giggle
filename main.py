f4af31a7e4b02a546d2cabd56462bed0cbf2f8f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:42 +0000

    while clue sign lead excuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
