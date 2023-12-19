ce74a19ed868b6f70ba6f2aa6297984a8dcf7cb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:06 +0000

    attempt amount area holdon bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
