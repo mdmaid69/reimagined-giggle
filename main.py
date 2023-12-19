05a6f8dcc8eb7741a12533867736ae2f65aacfca
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:16:00 +0000

    squeeze moral wise listen list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def count_elements(lst):
+        return len(lst)
+import platform
+def get_os_info():
+        return platform.uname()
