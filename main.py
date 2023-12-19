f24b253e68ec43b07918c37302e982068136ef23
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:37:45 +0000

    plant ilovedicks mend tour bear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def is_even(n):
+        return n % 2 == 0
