b697e48e5cd05a5020d748d972ce7545dd90ff42
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:35 +0000

    workout hand goon ourselves familiar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
