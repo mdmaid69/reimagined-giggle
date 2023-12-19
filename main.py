45ee4b941726a9700ac2b7f5c7f32f7a67ef75a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:40 +0000

    pause trap depend prefer permission

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def reverse_array(array):
+        array.reverse()
