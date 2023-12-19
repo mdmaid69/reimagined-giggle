4e064ac3b1927b0d00ee551646e460384e1cca08
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:32 +0000

    abroad test clean experience past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
