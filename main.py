a19d3ae16b3024875dad4443d1e36dbad7d4aa72
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:30:32 +0000

    otherwise task anywhere heat visit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def count_characters(sentence):
+        return len(sentence)
