41345100a71532e1b450f6b3d643fa86cfd57942
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:12 +0000

    spend move which mean spin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import numpy as np
+print(np.array([1, 2, 3]))
