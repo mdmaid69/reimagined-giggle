cf695631d9bcf7cac167ece61716902dac6a407c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:58:48 +0000

    post pen part attach shame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
