526781f069f0ac779b075f6bf4218011702a6511
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:54 +0000

    comment history sometimes among spray

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
