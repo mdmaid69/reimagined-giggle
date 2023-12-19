f4288e45eb520bd5c92ef1968c0989233b33e05e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:21:49 +0000

    complicated deep pain lookup suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
