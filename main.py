02be5d7ff3a2d32b7e6f41982f34121f264883d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:11 +0000

    congratulations squeeze spend perhaps typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
