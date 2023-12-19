a2184ab99d6d26e4030495fc3610063257c1b6e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:11:25 +0000

    present dead frequent pain then

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def delete_file(file_name):
+        os.remove(file_name)
