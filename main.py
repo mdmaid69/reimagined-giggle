1d79a05fca1a98ae28f51755994613b66e55468d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:55 +0000

    stare matter plant disappointed violent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
