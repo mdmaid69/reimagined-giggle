4c8f2e9fbbf75e86c215459c46e76e5d6d4e03d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:02:05 +0000

    Cc nowhere sweet game goaway

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
