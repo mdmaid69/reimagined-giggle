8999ce3c81d500f75da6d20d56e2eb7ffb5371ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:14:54 +0000

    often runover fine fire goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
