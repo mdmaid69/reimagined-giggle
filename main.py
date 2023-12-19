38fbeb196e7ac4a5c98d208d24c969c4f400739c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:44 +0000

    cough contents foreign vision love

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
