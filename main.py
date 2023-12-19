efe9beed974a2e846a702553ed28b29b751b5f40
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:06 +0000

    shallow succeed dress calmly why

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
