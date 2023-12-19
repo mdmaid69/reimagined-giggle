ab73f2f5c7fdaf898266bd7bd3dd3544a5bc6c2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:35:22 +0000

    meet lose so think love

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def clear_array(array):
+        array *= 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
