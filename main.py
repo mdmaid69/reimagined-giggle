47e7feb86de04449d8e4e694ed086ec6da4dc7a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:28:20 +0000

    kick terrible estimate want fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
