0b93652b04c6f5482067bded0248091bace1802a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:50:35 +0000

    on race noisy accident 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
