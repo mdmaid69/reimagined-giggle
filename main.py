9fcaac786ce2e8c42fad93ce900ac981f5970e44
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:19 +0000

    cold drop nigga frightened declaration

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
