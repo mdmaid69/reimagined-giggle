857188782f45e7ff50a1f9b6235427ac7545e5a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:59:45 +0000

    pure drink keepup simple land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
