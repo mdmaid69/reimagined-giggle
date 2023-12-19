c8216de9d7b0e8db5a54312ac39d3cbaa35e8f15
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:04 +0000

    kick curious world 569enjoy hello

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
