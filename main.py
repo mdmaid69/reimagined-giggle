0b0bb5a6d11826873af3dc717108439dd0029404
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:07 +0000

    later fast see lean drink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_unique_words(sentence):
+        return set(sentence.split())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
