b7091ff2dcc4f641f02bb5e3f003c2d27fe39b55
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:20 +0000

    meaning breakup fly address name

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def is_prime(n):
         if n % i == 0:
                 return False
         return True
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
