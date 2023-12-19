437b2405c4571fcce98a39116d75e6c9ee1611a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:42 +0000

    break impressed pull cut sticky

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
