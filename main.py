8ebeee76683e4ec0bc8dc3ff623ac1155c87f9a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:53 +0000

    wash match sting fill shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_odd(n):
+        return n % 2 != 0
+def count_words(sentence):
+        return len(sentence.split())
