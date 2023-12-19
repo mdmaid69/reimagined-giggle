9db07b4e05bff1c61c2a555db641f94e8480cc62
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:24 +0000

    remove sudden bread spin sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def find_min(lst):
+        return min(lst)
