965bbd72949a0c511df96aa67898a582542b2bf4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:04 +0000

    fasten advanced dark order urgent

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-print("Hello, world!")
+text = "Hello, world!"
+print("Words:", len(text.split()))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
