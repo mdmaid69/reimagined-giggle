effb27b3ecb5e7128804f05dcf78d9c6d5722817
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:41 +0000

    method sense branch move material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def add_numbers(a, b):
+        return a + b
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
