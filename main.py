2e25e76377bc688a3321e2625a814dfe74ff4574
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:40 +0000

    cover duty risk independent responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
