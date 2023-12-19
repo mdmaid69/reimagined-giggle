78b91684cef537afcfe44837461515f5cf04ded8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:23 +0000

    prefer occur turnup poison contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
