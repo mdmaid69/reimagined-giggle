f3cde204772b3308729a419e2741ea670f4cf760
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:39:48 +0000

    scene importance home art joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def square_number(x):
+        return x**2
