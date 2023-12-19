e0956e1f8f23de1018cd10bf46f33042dae872ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:33:08 +0000

    set runover book turnover deep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
