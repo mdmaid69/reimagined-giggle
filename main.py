19de5987aa9d9db20e4116c3231149bc29a294ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:53 +0000

    wander difficult remember view regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
