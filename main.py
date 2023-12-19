ddba08f5cfbf30ee37ae0521b4752e7c288efc0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:32:11 +0000

    space coat regret ought duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_acceleration(speed, time):
+        return speed / time
