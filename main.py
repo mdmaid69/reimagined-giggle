0a20d5b89d4cf70b2fe6ad30fbcc556ab55e3747
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:29:02 +0000

    throughout friend average path forgive

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def is_even(n):
-        return n % 2 == 0
+def find_min(numbers):
+        return min(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
