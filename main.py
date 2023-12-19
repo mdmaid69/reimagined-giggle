05739ef03b62c8e613a7dc4cd2269b28b4c8d9df
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:50 +0000

    rely uncomfortable fear above crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
