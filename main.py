85ce1812d86cca10cc629b9e8613fda72d108166
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:14 +0000

    week here combination wonder 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
