3e5458f47469ff05d7bb9396ca56a2f3e4b49c98
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:53 +0000

    thinkthrough state brush allow translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
