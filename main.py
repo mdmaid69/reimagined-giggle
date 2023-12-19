8f0995a39700d73cfa2f8b53cfcda0849c39b799
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:42 +0000

    follow wipe interruption kind test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
