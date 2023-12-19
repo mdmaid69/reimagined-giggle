a76a93e74a4d8586b6af545bc18ee17500cc4c85
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:58:14 +0000

    straight eye maybe someone company

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
