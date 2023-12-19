9b776a87d0e95026e7ad64fb7b44c8540308bba3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:15 +0000

    sorry somehow crash smell when

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_typecode(array):
+        return array.typecode
