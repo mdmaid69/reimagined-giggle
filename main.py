843aeac7b7fd5bbf9a1aa01b57165476da20df93
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:21 +0000

    stop message fire handle yours

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
