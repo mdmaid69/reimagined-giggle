dda2cae82c11729bd328711b27ceab9aefa6270e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:37 +0000

    tip less satisfied clearly quit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
