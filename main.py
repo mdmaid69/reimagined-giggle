88ac5332e952a3d96359ff6b8730c441bc8e7792
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:48:09 +0000

    everyone music right own quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
