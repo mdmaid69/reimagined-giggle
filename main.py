b435d0b597525b69bc1dc4e6f1d08ba1f25c6409
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:22 +0000

    will heavy ours unit tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-name = "Python"
-print("Hello,", name)
+def find_max(numbers):
+        return max(numbers)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
