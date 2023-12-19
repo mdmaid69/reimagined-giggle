1ab1ddaff4e5ef775f30fa0852037726e23827b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:26:43 +0000

    thought equipment face cross passenger

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
