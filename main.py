38cf4ab9dbf059386eae953cfaa814057b160b65
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:44 +0000

    cap mail nor custom goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def square_number(x):
+        return x**2
