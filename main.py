ab48b2f535ec15ad225e39086761732f8200acdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:27:56 +0000

    married spoil soft comfort enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
