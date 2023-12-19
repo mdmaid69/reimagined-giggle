1b7060c3a1e93fb74f0f873586a4ef06663db5e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:48 +0000

    challenge crash large fear suddenly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
