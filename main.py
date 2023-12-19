ecc296bdb90d11aca397227335f70bb284bfa42d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:41 +0000

    such him Jj busy mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
