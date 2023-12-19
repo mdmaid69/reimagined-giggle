915d05d8909cfb0760798efcac47bcc2576ed71c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:50 +0000

    crazy finger friend should runout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
