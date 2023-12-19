0047befde5171abf5aa5ec3ce82a257b8e777391
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:52 +0000

    damage sorry willing introduction succeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
