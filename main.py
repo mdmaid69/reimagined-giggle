c818daa9a891fd70fcee4a845db4347c1e4b33df
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:45 +0000

    company cross day stiff complicated

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
