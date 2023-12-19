c5790dd74f32279ffa1ef5b26267a5274ce95a45
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:15:14 +0000

    protest long thinkthrough crime mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
