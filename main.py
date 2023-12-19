50e95ebaf39bee862313b1ceb6ae2d87b600badb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:24:22 +0000

    believe protest cut difference nerve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
