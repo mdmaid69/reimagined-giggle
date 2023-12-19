0011e3f99d83b60955a48286a92cbe7b4f5630d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:17 +0000

    comfortable she date far pass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_as_bool(array):
+        return bool(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
