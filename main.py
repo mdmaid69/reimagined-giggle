1b02b6e96bd608d57ffa9e0d55fea2ceebf61993
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:16:32 +0000

    sting arrangement punish bury snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
