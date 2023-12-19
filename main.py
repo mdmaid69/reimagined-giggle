7d18fe67297e9cb7ca291012afa0ded76b58287c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:26 +0000

    treat book little measure window

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
