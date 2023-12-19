0001ff24f0795a3815cef603a602cb4f070f04ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:10:16 +0000

    evening peace pick earn fond

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
