8dc3e1fff4f1364fe92515fd2244c445131f18ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:24:36 +0000

    divide dick about pretty enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
