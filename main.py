e7ebea51607c62ae69525f1f35a5ed9b92fd00d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:06 +0000

    suffer comment blame prefer behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
