22cbe7a118fc6628c9553a75d50a6eaaf29a2a9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:53:28 +0000

    murder fly smile rare Mm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
