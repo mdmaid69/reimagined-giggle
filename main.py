159da56c03a7601db69dcc15502c67a92ec61fdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:31:32 +0000

    recognize hate could bridge pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
