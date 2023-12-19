f49318cd6123484196eefed19838b9fec4ffc204
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:39 +0000

    lay badly you thumb also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
