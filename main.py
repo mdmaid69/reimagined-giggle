9c18d3b4a492616942cacbae9d07e28ba4fac81b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:46 +0000

    vanish once frame sight tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
