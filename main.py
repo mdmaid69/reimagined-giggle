da5f1620f32110d2ab9939afa2582f66a95dbe93
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:54 +0000

    history tooth smooth available professional

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
