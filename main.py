1fa1cfb399a96bb190eaadbf55adeb1672383cd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:29 +0000

    lift explanation characteristic sweep live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import time
+print(time.time())
