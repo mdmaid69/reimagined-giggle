615dde66721dfc114c8278ac2bc2708f079b6fdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:15:56 +0000

    hour offence average dinner closed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
