ed7fc7b010cfaac01a97d6b7f04364a74812db8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:01 +0000

    anywayanyhow yours behave avoid duty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
