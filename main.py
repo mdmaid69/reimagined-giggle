19da58796dce32bd3fbc7b4fc36357c532513ae3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:23 +0000

    ahead develop Hh tongue print

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area(radius):
+        return 3.14 * radius * radius
