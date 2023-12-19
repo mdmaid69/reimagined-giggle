4ba3c0b366dabeed4a4b32d7653e64c12fa60a5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:10 +0000

    score nature second belong strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
