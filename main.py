2eb40d0fd804498e3fe063d5bc572a2f559b127c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:10 +0000

    runaway Ll honest present giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
