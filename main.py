7a1bcc1375f9ba7952248452ae9d4435c9792a91
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:14:22 +0000

    naturally soak pity actually variety

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
