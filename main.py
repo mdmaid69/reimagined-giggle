1307ac28d991bb3480ee111c066e6dd0c7b5e9e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:44 +0000

    neat wish will instruction turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
