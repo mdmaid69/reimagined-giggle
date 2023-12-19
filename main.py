d2771cf487c652a61547729c541317a06970d28d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:03 +0000

    suppose praise teach tread plan

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import array
+def get_array_length(array):
+        return len(array)
