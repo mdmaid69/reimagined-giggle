3777237ebe92969a60d88c36f118380e432e4adc
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:54:11 +0000

    neat rare violence represent should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
