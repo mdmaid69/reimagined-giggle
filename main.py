3a8d35d5ccfe70e9dbe339a26b7c0675b75ceb7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:28 +0000

    wrong drop no sometimes trade

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
