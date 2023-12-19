9c7b4ca700620a41a7e050adb15ca61af2f16718
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:05 +0000

    lower finally education along bunch

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
