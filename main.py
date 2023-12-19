f132a04c7cdfb31505bf986e868ec609ddfee3cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:30:16 +0000

    flavour tie slow answer keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
