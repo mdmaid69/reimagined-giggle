6c998b5f184d7b7404fd064bf2a8533ef1f0bc7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:18 +0000

    its bringback flavour centimetre change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
