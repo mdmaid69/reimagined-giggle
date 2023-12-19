a0308bd794385a97cd5fbc46924e19c35eb99a22
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:12 +0000

    but kind underneath fix major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
