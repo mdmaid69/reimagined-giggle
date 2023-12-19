8738a3b28e0736dcf8706ad15115addbcb2fad99
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:03:08 +0000

    smoke list song occur kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
