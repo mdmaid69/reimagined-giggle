245c90d94e1d590d1d9ddca0e96e56873dbedc12
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:15:04 +0000

    arrive duty once remark adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
