e8e2d323a44077838f491bde1fb13d9c0c914ba0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:12:55 +0000

    discovery grateful dull consult lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
 n = 10
 print("Cube numbers:", [x**3 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
