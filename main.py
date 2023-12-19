e115161d2b84f9191648d4579fd453855640c30e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:57:05 +0000

    left defeat against exhibition apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
