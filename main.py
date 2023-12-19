208ccfed1dc46b87eff86f78857c41ff7328f6e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:22 +0000

    block communicate wheel regularly teach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
