53ffa693a25a4dbf646a7a70b4e7cc10aa8be309
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:36 +0000

    crazy other pupil himself thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
