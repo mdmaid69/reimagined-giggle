f48ab9ab7fd830c7c86cb721426358e95fb7979a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:46:14 +0000

    squeeze able grade attention stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
