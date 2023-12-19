f3acd8c31ac30186647bb5f29b6ed9a1d2883d81
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:15 +0000

    influence snow swallow Qq child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def reverse_string(s):
+        return s[::-1]
