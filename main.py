11fc44e98d27ed7bf1d83dbea9a1667e68ebd7ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:23:17 +0000

    pile keepout pretty takeoff shelter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
