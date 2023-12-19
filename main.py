f5c761110a55f47b1fe01762d4d19f0a4ec64e81
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:29 +0000

    offer study new help slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
