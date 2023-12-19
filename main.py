36713d47bb3f041ba59f2af3fa4b1a1da04ecd2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:59 +0000

    organize poison minor memory down

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
