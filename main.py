f9c501d0e8014d09eac217147a2b7d30366dc7f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:40 +0000

    turndown minority lead past fly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
