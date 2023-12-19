3bfb89f57164f1cd213d0f8ed799751581468d17
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:06:19 +0000

    wave ordinary unhappy basis and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import math
+print(math.pi)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
