16f6118d4dfbbfc589bdd0224397e196d07f2df1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:13 +0000

    special ocean subject usual relax

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
