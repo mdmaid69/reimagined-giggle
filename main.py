cce30b8ce4f7994c8f760bbd485ffe478675c336
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:16 +0000

    who stare dare violent disappointed

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-def calculate_pressure(force, area):
-        return force / area
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
