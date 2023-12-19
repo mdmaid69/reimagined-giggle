5179b7c40c2fccaedc14f862384b54a5ea7e4147
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:14 +0000

    awake battery team today exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
