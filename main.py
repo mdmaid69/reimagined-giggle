d6a0279aaae9ae1764ccd114bba7cd483ca88610
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:20 +0000

    runinto mark below either influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
