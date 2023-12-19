57b98155449a0a3c18e4742dd5efc65c4b82f008
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:42:36 +0000

    practical through thanks world play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
