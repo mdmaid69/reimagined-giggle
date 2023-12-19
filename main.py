37dadff2c77b49aee88abd8331300a2ba77ab2a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:40 +0000

    telephone miss service view arrive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
