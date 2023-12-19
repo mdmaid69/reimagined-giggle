b059a6bcf4a2cdb0cc2616ebb215a2b88f5c8557
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:21 +0000

    common until dead appeal thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
