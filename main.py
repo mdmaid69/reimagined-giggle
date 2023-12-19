c770bdccc47168c1941fba468637ffc1ca55671e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:02:09 +0000

    back turndown lead string safety

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  def square_number(x):
-        return x**2
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def count_characters(sentence):
+        return len(sentence)
