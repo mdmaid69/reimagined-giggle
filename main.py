9d4386a351df0c65f710f19adfe9b7bc1673abd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:38 +0000

    matter bath fond rarely post

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
