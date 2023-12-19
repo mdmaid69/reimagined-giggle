44dab97cea08cda132a413fe9c4843f65b33b0e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:25 +0000

    little content brother whichever watch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
