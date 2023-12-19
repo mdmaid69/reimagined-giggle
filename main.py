79ae9a166c5f4851bddf39c4461a66801a1f617e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:19 +0000

    worse young occur question disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
