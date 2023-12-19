c1b5aaf20f6a7d3cc74b2b6d3a3ea7cb9ed6fb5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:57:46 +0000

    active his backwards along necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
