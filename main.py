9ea685c693ea51cf7d86bbaf84dd845f783f69c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:38 +0000

    delight moral by runaway consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def convert_to_binary(n):
+        return bin(n)
