61483688b09e7ce91a33daa11c0399db0f20d9bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:20 +0000

    yes choose 573entertainment maximum look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
