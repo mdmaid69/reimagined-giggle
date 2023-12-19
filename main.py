866d21576f62e80e39946957d54866e0abc21216
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:49 +0000

    treat major table hurry dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_max(numbers):
+        return max(numbers)
