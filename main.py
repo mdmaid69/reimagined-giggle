870014520d2337815b1bc4943ed7e0be30c6e955
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:16:55 +0000

    promise trust state cruelty this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_itemsize(array):
+        return array.itemsize
