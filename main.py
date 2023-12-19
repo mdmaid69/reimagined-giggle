1ed2464b809bccd2510f432d126f81a81be72a9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:57:21 +0000

    space polite takeover rich own

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
