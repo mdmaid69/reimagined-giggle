c093e190a47a1162af607b095194abf83098a8dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:31 +0000

    community trousers translate letter light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def is_even(n):
+        return n % 2 == 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
