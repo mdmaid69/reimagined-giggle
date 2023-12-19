a4cf1f13c617ea5df730747dd8c9a995eee40057
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:23 +0000

    top satisfied error probably like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def clear_array(array):
+        array *= 0
