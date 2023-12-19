3d8ba9436acb58d76567a82b25f4dc0131f1e61d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:40:24 +0000

    indeed much lie even set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
