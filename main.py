1f19c909ab0223e5f7a034836d300febaf9fd7fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:07:22 +0000

    him plan earth ring bringback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_factorial(n):
+        return math.factorial(n)
