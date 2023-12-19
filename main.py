6cb8ff9262001e02a911aedf5614f2a4d65d59be
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:46 +0000

    offence naturally false importance pass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def get_array_as_bool(array):
+        return bool(array)
