5f2cd7433ef1b65973c7a6b1963a4422f6cb4ea4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:09 +0000

    substance squash clear fit emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
