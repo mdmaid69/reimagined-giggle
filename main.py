32b00a210a3b6eefefb6954e8f4068f42eff7c83
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:22 +0000

    powerful force estimate tooth get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
