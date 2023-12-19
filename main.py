dea43c538e13b767a2696d11e35ade244a9ab479
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:33 +0000

    sink figure gooff direction obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def extend_array(array, iterable):
+        array.extend(iterable)
