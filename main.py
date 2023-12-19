bcbda2943e81330b37401686e4f20941ecad125b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:10 +0000

    down suit apart speech unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
