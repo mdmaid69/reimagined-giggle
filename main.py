51abad1ba248f5c3fed17e5ebdb8cfc8c940c828
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:50 +0000

    compete body bottom learn connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
