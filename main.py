34e3bc5191b315cae0fa2e3a0cc8c7486ae3f47e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:07:45 +0000

    result gate goup bit tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
