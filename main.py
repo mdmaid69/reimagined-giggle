525bae593f5e22f661c19d61b276488b70ad1be4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:10 +0000

    breakfast obvious tough bad passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
