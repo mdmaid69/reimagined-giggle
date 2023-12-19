603b54c2c2a2f619668786eab5cc3a6bfbbc8105
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:01 +0000

    fact mine Qq inform likely

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_list(lst):
+        return lst[::-1]
