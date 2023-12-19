d2cf426bf12d7caa30ddedabecde3463742c898d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:56 +0000

    away nerve embarrassing comeback loan

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
