4a1ad030a11fbf80bebf92757cd3874780604cd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:26 +0000

    yet expensive poor release trip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
