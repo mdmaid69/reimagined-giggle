bb2b1a9da22efedf7d3e36a31317fc26651d7cf8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:12:04 +0000

    record like advise generally love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-def log_message(message):
-        logging.info(message)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
