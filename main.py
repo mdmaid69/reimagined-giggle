17e0fe76caabd1e86c013192153dbeb5c2565431
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:51:47 +0000

    drop talk me tonight scene

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
