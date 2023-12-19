8ce190e5652d3bf151e94b17892f78c3e99181fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:47 +0000

    ready line comeoff relative talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def cube_number(x):
-        return x**3
+  def square_number(x):
+        return x**2
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
