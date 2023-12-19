3ecb28e21f256144bd8a85ed73826f728c1825ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:31:19 +0000

    recently realize anybody fail direct

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
