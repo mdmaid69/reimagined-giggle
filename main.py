044e333394ee1e042f2481105edb38c4048b4cb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:43:36 +0000

    rather power formal bunch comfort

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
