c914989c4bd09b0f3a1734e8f0b36b013856cba0
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:50 +0000

    meanwhile peace replace print shade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import logging
-def log_message(message):
-        logging.info(message)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
