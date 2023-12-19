a18ade1552dfd481e84c2e39eda527bbd0361a69
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:06 +0000

    single hot split exciting concerned

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import math
-print(math.pi)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
