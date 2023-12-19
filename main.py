69a037c15ca7dac98584ea793a275c100ab81710
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:16:32 +0000

    moment people big water praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(a, b):
-        return a + b
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
