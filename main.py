b011442d69114365bcbfe5ab716e986fc76c2ca2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:37 +0000

    takeoff settle return frightened shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
