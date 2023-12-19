977e6344e02890a05007b7c3d027934ba77209b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:45 +0000

    standard dare emphasize emergency convenient

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
