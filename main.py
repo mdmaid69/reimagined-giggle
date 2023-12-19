49c93ef20b1eb53fe98394ee59225d768decf789
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:45 +0000

    spoil each famous spoil comfort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
