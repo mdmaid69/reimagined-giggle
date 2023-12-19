872bc90f0d7bdbfe36119f3058dfb17ef616c7c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:43 +0000

    him treat other care recommend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
