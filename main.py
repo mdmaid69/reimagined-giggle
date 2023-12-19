4b25b1e74f6584737d163cc275549c3f321fbd0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:30 +0000

    front sweet let production please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import numpy as np
+print(np.array([1, 2, 3]))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
