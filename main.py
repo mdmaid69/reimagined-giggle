49d17a8dca526731204e037832ed7219162fb254
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:02 +0000

    danger well him weekend minimum

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_work(force, distance):
-        return force * distance
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
