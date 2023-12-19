9087805ec2fcb8dbbaca88876060324443228f86
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:28 +0000

    lock move law stranger but

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_user_list():
-        return collections.UserList()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
