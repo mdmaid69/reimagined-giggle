2ce3263ad1e8314dfbaf3011e818c0d36505e7f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:27 +0000

    raise later giveback flat building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_current_directory():
+        return os.getcwd()
