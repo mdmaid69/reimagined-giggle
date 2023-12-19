cc03df094e22e98f8429a22c9052e81000c9bd8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:11 +0000

    only nose lucky person discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
