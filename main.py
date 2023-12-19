22c72bce7d90317d3101960f4e552c75cf562e8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:58 +0000

    time smooth clever brief respect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
