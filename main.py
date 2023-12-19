030d877fe2c60751bc8fcb3e6e34f6a84ec4975d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:43 +0000

    now stretch smell favour bus

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_pressure(force, area):
-        return force / area
+import os
+print(os.getcwd())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
