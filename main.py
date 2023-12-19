0f4fc9376c817b4cdfe24216263c2d93773acd93
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:02 +0000

    responsible force maintain downstairs somewhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import sys
+def exit_program():
+        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
