c271bf0da2c94aa42f3ea6834ec9ec28e5082f98
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:00 +0000

    stretch ahead print family exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
