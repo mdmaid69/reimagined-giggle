19ad2035157c6540d55a2bd492e59231af8cccf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:18 +0000

    Pp responsibility unpleasant wave would

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def reverse_array(array):
-        array.reverse()
+def is_odd(n):
+        return n % 2 != 0
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
