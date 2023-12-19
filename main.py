9a7b7e495a89ac811f4becdbd5879e87710cb58f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:01 +0000

    still getaway half add scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_square_root(x):
+        return math.sqrt(x)
