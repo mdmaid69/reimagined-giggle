1bf2cc99cfd7b4d0c53a2d8cfcf7d1d672c3f3cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:11 +0000

    rubbish stage prefer minute straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
