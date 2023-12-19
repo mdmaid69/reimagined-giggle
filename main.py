764bb06c54f65ea3c2322827a84c00b5b4211eb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:52 +0000

    pattern escape if poor apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
