6054be36b41641de34a7eb8845657375adae52ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:11 +0000

    strength fear spin whisper rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
