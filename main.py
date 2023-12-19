c9ac0557f5073686357093bb2b087a40d6f82d08
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:30 +0000

    satisfied suddenly exploration accidental takeout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
