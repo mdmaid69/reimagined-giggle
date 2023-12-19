d3d6592165f3ceb397238813efee52e8ebb96023
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:55 +0000

    place wind prevent recently step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
