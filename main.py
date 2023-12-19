8330036c3b1c8efd386fa9b5145854557df528b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:35:31 +0000

    unless partner seat tape once

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
