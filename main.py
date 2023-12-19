0a2f07087fe384942c15a0ed89a902fc3ad00580
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:30 +0000

    thought force makeinto top thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
