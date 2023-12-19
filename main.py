283c2cb7e95e1c2bb77bc46bf1ef28ac92e4e1b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:26 +0000

    age peace mine poor convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
