2a6e4518676bbdaecb236dacc506ad50692b2941
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:58:55 +0000

    unfair limit interview gentle fear

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
