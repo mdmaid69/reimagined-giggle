f110117200c40c34c914cfd212436e76c36d82b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:58:40 +0000

    trial Ii your muscle dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
