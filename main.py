bc08cef18320e886b19b853a8d760f5a3c2e2347
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:10 +0000

    old afraid bunch commit basis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
