642db35ca41f994fdcb2eab5a1f2d961c7409d06
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:16 +0000

    otherwise luck inside like recognize

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
