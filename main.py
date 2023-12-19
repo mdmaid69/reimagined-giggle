15b47c2bb77325b6679a21ecfc6b00503c98e9a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:10 +0000

    away marry furniture smell ground

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
