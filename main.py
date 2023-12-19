e34e7c5f9b19ba9aeed70626eed93c73adae7eb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:51:35 +0000

    die hide grow 179bite level

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def remove_duplicates(lst):
+        return list(set(lst))
