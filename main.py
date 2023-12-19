3eb475170c39956846b5b0a41a5cda9247076f00
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:21 +0000

    breakthrough 573entertainment emphasize amount we

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_work(force, distance):
-        return force * distance
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
