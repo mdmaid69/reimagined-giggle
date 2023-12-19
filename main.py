3fd714b9a5956e968f7bb47c4813f892ef02e8f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:08:34 +0000

    different Mm medium trap fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
