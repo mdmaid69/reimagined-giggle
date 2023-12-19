e65421e40c1a2a00499f8f598d33e48fb21f9c30
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:03:52 +0000

    computer drive herself later unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def cube_number(x):
-        return x**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_volume(length, width, height):
+        return length * width * height
