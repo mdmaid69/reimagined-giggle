657c3585500272adad7731d2f1df471d24eee8c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:53 +0000

    false may once curious opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
