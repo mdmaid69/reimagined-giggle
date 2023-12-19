cfcf478392cbcbe20193605f28ce8afc61312cb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:17 +0000

    and message certain dance them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
