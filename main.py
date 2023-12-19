310b1755fa816cae162f96447ce94897893b59d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:23 +0000

    personal band declaration feeling behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
