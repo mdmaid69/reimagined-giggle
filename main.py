45f45331dc0c3157558b618adf97831042d580b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:37:37 +0000

    load unusual conduct identify gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
