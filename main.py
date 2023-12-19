ae7f9213ee3d67cffed37016c5a6923cdca8e763
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:02 +0000

    object wait present offer trap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_floor(x):
+        return math.floor(x)
