3cd3cae8175a3e48dc9fd8c9079dbbc7956a684d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:34 +0000

    measure short she smooth fat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
