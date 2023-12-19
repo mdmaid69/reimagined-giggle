fb0f47bfbffcabb36aedbdc3da23ff0a418cde12
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:16 +0000

    Qq negative if load desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
