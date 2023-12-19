dea42bd60c351ead0e773466fafacaef66af3a0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:56 +0000

    why talkinto from many Uu

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
