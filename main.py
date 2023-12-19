9a35f1bcdfd4c0ad4a9f431781529267e523cb5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:54 +0000

    union unfortunate live correction minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
