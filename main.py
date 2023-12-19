433cbc86a6398f4863a61b39ae5fe0f93cac0b7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:30 +0000

    that communication please perhaps damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def is_palindrome(s):
-        return s == s[::-1]
+def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
