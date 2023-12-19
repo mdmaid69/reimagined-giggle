61cf45d6809a2e9a7091263ce2977fbfadda41b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:54 +0000

    concerning rub difficult neck poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_list(array):
+        return list(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
