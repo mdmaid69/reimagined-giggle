ee0f08b7d0e865bc2a1d786f78fb4856acc348bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:07:20 +0000

    father friendly fold mother swim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
