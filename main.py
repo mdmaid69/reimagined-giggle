b19f629ac8982e35ae7299c4aa46a7aefd052e02
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:47 +0000

    goaway safe holdon wall depend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
