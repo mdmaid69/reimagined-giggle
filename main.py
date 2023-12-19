577a6ce8f47d55521d7f6b80dc814a690dca2d49
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:19 +0000

    pot bridge shallow lie issue

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
