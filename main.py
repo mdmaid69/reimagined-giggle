ac725432c7284311de46a4227e5b15ab1b698992
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:27 +0000

    string responsible beside nowhere dinner

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
