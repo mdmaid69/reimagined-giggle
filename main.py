0cb93a5322dcf035c52602e88a329b4e7876f637
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:16:44 +0000

    takeover medium fair transparent cough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
