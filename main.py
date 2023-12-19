e6fa542897adbabb24e9caf23c2787a3a0e58de9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:01 +0000

    together month price politics compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
