3f1d2d783458f25f67ad6187ef132254c5caf79a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:51 +0000

    minor react job Ww twist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print([x**2 for x in range(10)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
