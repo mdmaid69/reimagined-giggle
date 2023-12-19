ec26a27f3a34b0b8fd0de726d3be28bad5126be9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:48 +0000

    all let cook machine cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
