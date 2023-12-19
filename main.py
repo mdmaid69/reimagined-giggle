92bb1b7aeaaa6dc5ea129784ae0401765b7a8887
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:33:04 +0000

    takeback statement goon describe bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
