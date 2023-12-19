aecae5abcddbcc390bbc5e439d41ba82badfd85c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:49:52 +0000

    describe admit float children gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
