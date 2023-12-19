e7fe516c1c88f01e03f2a66b7b486de6cd02dd57
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:26 +0000

    cross matter subject well autumn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
+def calculate_pressure(force, area):
+        return force / area
