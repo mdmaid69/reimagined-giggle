0270d32ed395e312f8ce13b64f7cc78563ae8a7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:09:34 +0000

    in level dark progress back

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
