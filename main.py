30e5c633ccdcc6ec9685681a7d114c08b6e83193
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:53 +0000

    march century to life easy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+name = "Python"
+print("Hello,", name)
