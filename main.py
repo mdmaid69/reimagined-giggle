2122414e84d7891c31641f3f9c008a332446b00b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:24:13 +0000

    awake legal appearance illness aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
