f0992a779421feb67eeade43acf51b0b1dffe17c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:08:41 +0000

    contact middle recover mainly big

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import sys
+def exit_program():
+        sys.exit()
