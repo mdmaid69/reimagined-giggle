01d136777809fb9be55439607abd07bfbeb2bb34
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:42 +0000

    or uncomfortable front wing whom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import json
+print(json.dumps({"name": "John", "age": 30}))
