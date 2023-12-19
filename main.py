c72d9aa24805ed09cf166199a50fefd7a4ec5f6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:41:27 +0000

    on as remember boring cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def split_path(path):
+        return os.path.split(path)
