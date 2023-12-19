81ebc581ea5cd76f7973fffa74f52d61341a1a39
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:26 +0000

    chat wonder hope rate total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
