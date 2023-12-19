f99fc262a8960f8c857baf619d26ee3fc4a1ff32
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:11:54 +0000

    gradually friendly lot safe wealth

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-print(sys.version)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
