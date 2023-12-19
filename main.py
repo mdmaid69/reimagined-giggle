11b53b9d53eedc049ca5a180dbcc9cc901c450d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:50 +0000

    evening whistle hollow me wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
