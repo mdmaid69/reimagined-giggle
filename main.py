b1034738bc70c768d68d7257d5f10fcf39421a37
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:45 +0000

    be approve prepare serve conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
