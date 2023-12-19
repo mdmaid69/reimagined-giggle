c546aab2f99245c55b71df834d8d89d5dfa4fb31
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:40 +0000

    short empty today takeback cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
