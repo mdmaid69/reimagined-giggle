61c0a6a2f460457cebd449b506f9162cea45a02e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:47 +0000

    lunch basic attempt wish disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
