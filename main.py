6a37c5b7da9fd8d9664a8e156f95ae82c606d3ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:03 +0000

    club remark terrible young read

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
