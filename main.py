debce6039e3e9c612a75522f8daf9d042a7288d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:44 +0000

    guess tidy manage trust takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_area_rectangle(l, w):
+        return l * w
