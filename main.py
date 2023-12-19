a80d2cee4c3b8ef5ef85d532062d8d9935016699
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:03:33 +0000

    moral criticism wheel full view

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
