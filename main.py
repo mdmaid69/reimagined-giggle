0f3282e6a3f24519e60091af9034f621f84cac99
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:09:35 +0000

    connect straight look weekend last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
