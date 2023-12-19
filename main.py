e8d9bfcc4c64d052b1f682b133c1df84e16a0e01
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:47:22 +0000

    in mean payment night freedom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
