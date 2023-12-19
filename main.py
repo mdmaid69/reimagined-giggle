d77731bddde8d5ebed24137cdd65b4d86aabb0b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:29:40 +0000

    draw side real dish thanks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
