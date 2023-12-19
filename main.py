02e327c11b48d09b0be2dbd06d454ad2433da56f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:35:21 +0000

    practice equal calmly respect switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
