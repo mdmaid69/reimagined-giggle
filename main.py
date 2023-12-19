93bf3229e82896624bc5eee6dd2572226469fb8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:48 +0000

    step quick within past common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
