25d3bfc0d8f04987f539e9b6d8d70c3d1a50f591
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:14:34 +0000

    stay art day bored light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_typecode(array):
+        return array.typecode
