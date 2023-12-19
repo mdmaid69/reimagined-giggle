d025e4f94456c8d93f6c47efd49ebb33a751cfd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:02 +0000

    breakin passenger rank turnout aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
