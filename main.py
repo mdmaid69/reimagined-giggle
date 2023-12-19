7486b3d224ef5527623c3c2415f66d46ba1765b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:19 +0000

    nonsense move left estimate plastic

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
