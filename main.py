fab0ddcf27a36cf6013bbe42e0db0a985b67eabc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:22 +0000

    vanish fairly either wish tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  def is_odd(n):
+        return n % 2 != 0
