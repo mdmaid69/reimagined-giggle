409fadac7657dcf440d19bac627180e2993d68c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:11:27 +0000

    separate train rain defend brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
