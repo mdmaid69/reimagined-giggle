2e5ab1f5dfeaa980175cf5763b4719e1ffaa3755
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:24 +0000

    comein range slowly rough trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
