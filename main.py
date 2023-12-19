300256eb91bf5c842fe7d36914cde5356b2ab3d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:36:41 +0000

    build deaf fact wheel rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
