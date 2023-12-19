971ccd2fb937c95e52fcb134346458142b2ff6be
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:24 +0000

    used use number doubt breakdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
