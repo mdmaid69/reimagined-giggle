09ce0b2da30e8c8a0353962c09080a2b61fcc5f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:37 +0000

    bringback rely measure object oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
