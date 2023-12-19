be7bd9476c95d733de9330d9e87fa378b5423b1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:36 +0000

    hole flat service clue strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
+import datetime
+def get_today_date():
+        return datetime.date.today()
