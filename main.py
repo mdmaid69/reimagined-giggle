1331fd89ab6404dc16fde41ea9f57013a7ae4d34
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:43:40 +0000

    there shadow fold shoot sweet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
