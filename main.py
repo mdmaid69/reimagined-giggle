6550f860175ab9e32d819c33a1c50296d6a80f87
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:50 +0000

    command laugh sky pump deep

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
