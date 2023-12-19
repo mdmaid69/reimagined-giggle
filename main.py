503169f01ffa18068c4e4831f004743d4fd9fa68
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:10 +0000

    wish after each family passenger

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
