04ddbf97fe69c9685ac8bca50cb20abc78306017
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:42 +0000

    progress getup dear Nn class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
