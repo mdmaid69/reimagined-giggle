4ac945894cb3c4b851a5b94004ed00f305b0fd34
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:51:22 +0000

    bush treatment thumb who change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def is_even(n):
+        return n % 2 == 0
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
