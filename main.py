168148e0751dbde57537cf257c198b40f65702f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:56 +0000

    represent steam illness feeling mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
