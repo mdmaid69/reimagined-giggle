fc6315009b339158aa4a77e5414514359419519c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:24 +0000

    breakfast eat Ff prove change

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_pressure(force, area):
+        return force / area
