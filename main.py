9e5fc46c27f560b3d62c3202b02ca3563b7fe5a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:47 +0000

    famous partner father valuable backwards

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
