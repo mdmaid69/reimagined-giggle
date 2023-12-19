65afe22f1fad0ac575447a3c10f8c15a77f6d5e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:34:14 +0000

    doubt march include consider the

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import collections
+def create_user_string():
+        return collections.UserString()
