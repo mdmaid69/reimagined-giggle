8864cdc5cd6b1b3b7542c3192f9ead3810526d50
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:58 +0000

    smile opinion result base content

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_pressure(force, area):
+        return force / area
