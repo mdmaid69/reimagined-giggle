0991e90d9c43e08950049a723fed7b180ea670fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:19:29 +0000

    takeaway bin giveback consider trade

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def exit_program():
-        sys.exit()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
