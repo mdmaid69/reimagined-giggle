6d824ef6353cf357312ee438386cb88adcf78c83
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:08 +0000

    rise anything copy office corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
