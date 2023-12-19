de7a8122b5a3d00ceadd064a61465e5135911731
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:49 +0000

    voice display before temper boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_string_from_array(array):
+        return array.tobytes()
