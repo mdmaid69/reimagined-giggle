861b61acfcee2a6c0de9322827508aab53ebd203
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:26 +0000

    steal should else borrow lower

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
