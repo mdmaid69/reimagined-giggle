00ffae582939d68e5601ebef8d7a5add5c44c19a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:31:40 +0000

    well start trouble much disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def append_to_array(array, item):
+        array.append(item)
