63977d9c048b45fcc8574558774a842b2342da84
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:55 +0000

    write defeat thing activity dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
