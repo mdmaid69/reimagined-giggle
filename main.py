3f4d7b51582c77490adbd246febf2e7dc6ae94e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:39 +0000

    impressive prize still formal clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
