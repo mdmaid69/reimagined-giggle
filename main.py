c641f3449483f5949d1762b7eaac4bd77eb91eed
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:56:17 +0000

    home after season situation partner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
