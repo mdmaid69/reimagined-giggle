99c529fa05ea30e69199e3b87437cb66c2ed0a73
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:44 +0000

    combine evening contrast sweet goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
