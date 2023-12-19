a501c35b3c38b562898a327350d570eb47dacd30
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:22 +0000

    priority sleep lonely poor accommodation

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
