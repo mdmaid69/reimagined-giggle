14c53e74a0d1fbb4af07d9b9ce51fc224fdee390
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:22 +0000

    squash she delicate bite war

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
