a5088d38c338ca6fdab2baae15f8e7d918377a86
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:31 +0000

    soil cough whoever hope future

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def clear_array(array):
-        array *= 0
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
