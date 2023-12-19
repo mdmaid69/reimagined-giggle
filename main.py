13ce23bc2c9a2ece291aaeb8016db50e4185c8d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:53 +0000

    pretend life calloff refuse famous

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
