06c7c4cd9c781c3bfb406c468821f624b8d4af67
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:08 +0000

    onto birth force modern open

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
