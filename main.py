4458431ccad4b474a80e86aa5fd40d6b1fe75048
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:50 +0000

    miss within who eye well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def reverse_list(lst):
+        return lst[::-1]
