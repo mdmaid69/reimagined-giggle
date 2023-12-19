564babbbb2aa66868a5a1d48a0a517687f0831d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:03 +0000

    stone letter declare level stop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_power(work, time):
+        return work / time
