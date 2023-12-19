4604cef805f5ef22662327b221e4b3c4079c21a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:00 +0000

    song late content bang welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
