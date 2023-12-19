622814e40b681b216eb6a6570069702326a59a4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:54 +0000

    quickly safety fat far should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
 import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
