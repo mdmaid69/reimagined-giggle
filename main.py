e02599a839dd52974de73c3de94268a9a228b37e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:30 +0000

    gas pity reduction tragedy herself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_speed(distance, time):
+        return distance / time
