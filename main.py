4b968b44d496b0a8636147adf14b1930957ac874
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:25 +0000

    event old season hard drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
