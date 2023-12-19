055238f32c62e8315cece24acf992f2b6345aa9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:15 +0000

    answer neck name late shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
