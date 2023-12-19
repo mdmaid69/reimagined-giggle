242e79030e437df8be358a957d7624bd4efa2913
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:13:14 +0000

    notice deep mail story exchange

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
