02c4de651a8bbabe2ded0378477272c19fbfb008
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:28 +0000

    normal share pump minimum accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
