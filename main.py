f7313df309f16cda09993a998521b4da057ecf80
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:41 +0000

    tragedy mail rescue flood type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
