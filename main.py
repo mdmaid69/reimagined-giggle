8abdcf7d01f280c277a6fa824c0fcd942227a83a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:07 +0000

    all equal constantly be adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
