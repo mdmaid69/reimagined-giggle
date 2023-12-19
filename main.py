03c560032471f321ffb73add4af291bca9cb8b3a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:56 +0000

    equal heart listen exhibition truth

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def sort_list(lst):
-        return sorted(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def sort_numbers(numbers):
+        return sorted(numbers)
