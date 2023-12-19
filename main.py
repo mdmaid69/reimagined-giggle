4e22dccd621d78284ab60a85bc3983b8ae75d4c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:27 +0000

    round picture Rr onto neat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
