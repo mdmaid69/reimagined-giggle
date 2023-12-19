44664461d3b61aa30787c34fd3abf26b4798f582
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:31 +0000

    makeinto hard put face swallow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
