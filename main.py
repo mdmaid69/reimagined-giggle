446851ce8946e9be720bd17c803e3fb12df95020
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:33:12 +0000

    see past circle unfair argument

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_array_itemsize(array):
+        return array.itemsize
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
