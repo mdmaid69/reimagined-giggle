c7475836e7545c1253c1eb2046328a1a843f4a71
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:58:54 +0000

    pot jump laugh shade light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(x, y):
-        return x + y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
