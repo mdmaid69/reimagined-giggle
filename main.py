2a46f549f13b981039744c4d7c8f85b0aac2ea70
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:10 +0000

    cold tell bend person try

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
