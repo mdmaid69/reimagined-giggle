fc5a03e8757e1597b61589cbb954945722dff53f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:10:13 +0000

    stick culture tip pass exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
