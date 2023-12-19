b7230135e7ba92d8aa2595bb24e49a9aba72d1c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:58:11 +0000

    cap careful waste aim stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
