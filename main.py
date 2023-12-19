a2f9f56faa53bb957c61eb43ca01dcec9fb72f0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:04:34 +0000

    contact hot taste rubbish ever

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
