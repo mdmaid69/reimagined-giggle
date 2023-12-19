6149dbf3ae890b88815c2ae17aaa3a7ad375fc0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:14 +0000

    key half answer trouble positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
