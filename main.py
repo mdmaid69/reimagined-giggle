5bd53466eca80fcabf99c81a34f2d7bf07a3ac7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:58 +0000

    up issue wash honest bend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
