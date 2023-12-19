706379d09c6d7711d4defcc338027e815b8b216f
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:42:21 +0000

    complete Zz home dear everywhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
