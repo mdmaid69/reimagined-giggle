6a3206544a64d1f96a56e683f67b694d5b4c64fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:15 +0000

    taste thank medium purpose interesting

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def square_number(x):
-        return x**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
