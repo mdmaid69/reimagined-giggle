36886cdad02453c0fdb14e6b60c034b08c1a1ea6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:23 +0000

    serious other art mistake catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
