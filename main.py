f52620a47dc9b5429638bd78067b6b546a0dd380
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:33 +0000

    will soft sting run score

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
