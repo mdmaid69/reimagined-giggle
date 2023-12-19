3741af840a00bc41ccba5b3cd5cb8efe94e13bb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:30 +0000

    tour draw stop exchange score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
