636e87f018a820c986f2a0eb0b9da1fa374e5bb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:28 +0000

    team turnoff any will breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
