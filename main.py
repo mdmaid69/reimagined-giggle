45fbca2e218bf435f6452ee3076628b2ef735ec4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:04 +0000

    transparent completely education bored machine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
