7fcf9f30182a9759c4fc43f3446105406f229cda
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:58 +0000

    risk between accordingto production score

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
