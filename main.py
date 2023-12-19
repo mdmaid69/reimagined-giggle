88e37f514c63a138f5d4203c2978ae99d6bc98e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:47:54 +0000

    main appear fire secret familiar

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def remove_duplicates(lst):
+        return list(set(lst))
