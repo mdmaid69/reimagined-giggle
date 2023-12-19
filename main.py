e1e06e53978bab18b1bcc505ab706643b6636861
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:46 +0000

    central correction post familiar mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
