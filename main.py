1e9aefe549fac9595a7d8e319908698363075d7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:15:42 +0000

    half ought below truth certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
