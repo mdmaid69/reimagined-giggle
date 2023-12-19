d601a4f75e2af8008612ce34da48e45b919ab4e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:28:24 +0000

    pair flat along interest flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
