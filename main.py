47636624ad218ba4085a2b0affafcccaf25113f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:32:02 +0000

    trouble jump trade anybody clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
