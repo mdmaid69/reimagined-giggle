0eb12bf7e38027c90b828fa6c7fe9e093149b6aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:51:48 +0000

    present stiff 573entertainment hole sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
