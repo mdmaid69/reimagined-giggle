ed29a5b9953400423ff408f238b887d878fa3b24
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:20:39 +0000

    little story tell let nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
