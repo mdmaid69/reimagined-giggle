1feb62f1b7303d62f91671274cccb0b5e89bdc40
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:36:35 +0000

    ordinary wish claim touch thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_contains_item(array, item):
+        return item in array
