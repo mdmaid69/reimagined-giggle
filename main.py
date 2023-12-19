e369c4e87745b2e85e276d2cb6d74359bdd80e4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:24 +0000

    strong pot regard trap combine

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def is_odd(n):
+        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
