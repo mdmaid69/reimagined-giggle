f07747b247ccdba2d314a9d53fb4167b5b230809
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:44 +0000

    kind limit breakup constantly back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
