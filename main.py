1ac27a93df926ae9759aa919584733ce5315e9ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:25:55 +0000

    relationship surface punish block correction

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def is_odd(n):
+        return n % 2 != 0
