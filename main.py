9e875bb22f36c48658f75786b4e0a3f567fd0f4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:48 +0000

    cheque callat stupid confused damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
