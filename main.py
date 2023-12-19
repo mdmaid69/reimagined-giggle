fdd9daaa43dd7fe109f4f7e07665cb75474e2e46
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:13:42 +0000

    interested lock burn connect along

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_force(mass, acceleration):
+        return mass * acceleration
