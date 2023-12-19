0b5bc71146c6671d1121ee994bd196108772c836
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:32:40 +0000

    tooth behave mix book wall

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
