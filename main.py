7896119b9f4f97a455b21df6dcf895a3763a2814
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:30 +0000

    safety split race bringup giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
