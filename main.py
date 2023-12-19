96b756388118bb46d1d1353d155dc46e9855bd93
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:13:13 +0000

    although mystery refuse plenty dish

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
