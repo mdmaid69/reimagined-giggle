22bbb2ad16e6426d7dd370d5f6e0912a3e304ceb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:29 +0000

    bottom learn why interesting drive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def delete_file(file_name):
+        os.remove(file_name)
