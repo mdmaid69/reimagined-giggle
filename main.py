68d8098ca139d42a011a3234698122df97c86bdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:05 +0000

    lost statement question total boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
