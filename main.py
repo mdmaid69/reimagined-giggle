166fa814cb1f539800687ac55571d69c8cf14aab
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:11 +0000

    anywayanyhow breakdown left case fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def add_numbers(a, b):
-        return a + b
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
