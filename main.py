829733d78665672c5bd1fd2eb3b6fb8b56558bc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:40 +0000

    feature single from obtain pen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import tensorflow as tf
+print(tf.__version__)
