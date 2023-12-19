26e72cd82073203f71d1262bdb543e8a1807049b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:54 +0000

    plant practice detail system cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
