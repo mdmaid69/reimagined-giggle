60ae1b53a2f61908a250f36e2d342fc6ce15caf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:09 +0000

    realize country persuade damage persuade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
