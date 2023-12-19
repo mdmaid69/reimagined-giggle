ad8633e3873f88d6f2cc4a7dbdd43c6b38efa3ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:38 +0000

    touch divide reject lock replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
