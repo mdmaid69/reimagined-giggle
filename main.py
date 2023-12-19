f0a7d2cda50bf5121a1e08c8e904bef45d8086b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:40 +0000

    love development basic disagree circumstance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
