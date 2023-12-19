483bd34312c49ec8a3dfa77fa3305c215e25d4fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:55:42 +0000

    turnup zero past confuse Oo

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
