e193eb09d21af8e993d0f217535e5df168897d2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:23 +0000

    control replace arrange view fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_list_from_array(array):
+        return array.tolist()
