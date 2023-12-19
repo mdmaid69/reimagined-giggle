c9645d278382afbf590d3af85cbcf60b7496f5af
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:01 +0000

    violent such dinner where journey

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
