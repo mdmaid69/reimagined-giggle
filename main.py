8cb21ee7acdd573a7ca751112d0d2839f50b8916
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:55 +0000

    out behave means bite news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
