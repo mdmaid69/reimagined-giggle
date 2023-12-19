8bb058cc68482c61a7a23e809fba5e9177e89988
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:16 +0000

    rain move square hair prove

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
