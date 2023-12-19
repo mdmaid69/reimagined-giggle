55ac46cad70a0575cf700ba59af0a8505a65ed8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:27 +0000

    ourselves very form vision progress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
