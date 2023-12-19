3b7c29810ac1b127d485c840ece8bd6305dfd36a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:02 +0000

    afterwards breakthrough relationship success attempt

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
