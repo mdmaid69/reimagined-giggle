3a2dd3aaf0a0c255733176088ab8fd8e36c6695c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:30 +0000

    push keepout wave snow female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
