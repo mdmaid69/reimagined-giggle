ad37ba144aa70c2328ebf8a2a79c9a915273ba29
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:34 +0000

    circle grow laugh cost close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
