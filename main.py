07fb6ee71e3b43ad68c961caec090bd351d9848c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:25:09 +0000

    bang late talkback set mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
