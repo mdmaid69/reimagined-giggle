e9f9c5de1004e863c38d689b08b3a81595cc626e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:21:56 +0000

    truth sore godown late support

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
