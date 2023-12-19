56ba9d8a26a29578453e928feced39fd96d9ec2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:51:57 +0000

    file makeup promise family university

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def remove_directory(path):
+        os.rmdir(path)
