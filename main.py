fd93b091cdae1bcbabd1d25a5100fa5bdc80af1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:11:58 +0000

    it frighten goon no flat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
