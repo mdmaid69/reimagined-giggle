db09707c5b6ddbdb3fad35b89f28821d9231e1a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:22 +0000

    hear steal talk variety minimum

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
