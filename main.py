7c1d0cbbad7e498e1f1fd02d1ccc04abcb21d9fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:05:03 +0000

    holdout culture realize become balance

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
