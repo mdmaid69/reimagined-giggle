fcb49e4922cc433a3183fb57e6c71dba58119944
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:01:53 +0000

    custom draw block burst search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
