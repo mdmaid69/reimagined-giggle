89fa19e595f1d7e7f185f993a68ea17605f44193
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:12 +0000

    fight with nice chat service

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
