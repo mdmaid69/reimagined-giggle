602e1dcc3f9f4be7d2442e886114a86614f49e4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:00:31 +0000

    machine ring turnoff black turn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def cube_number(x):
+        return x**3
