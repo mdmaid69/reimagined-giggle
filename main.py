f2b7ff2d890dd8ff7494e9ffc417e5182161c0c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:00 +0000

    defeat absolutely entrance apply content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_list_from_array(array):
+        return array.tolist()
