bd1fe87ee6b2ddaaf8698ef1542d100d58af8668
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:10:36 +0000

    approve interview pleasure complete disturb

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
