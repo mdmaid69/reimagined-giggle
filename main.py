1149146609d57eb5b8db8101dbed42de322a320f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:58 +0000

    wide one nerve swing drink

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_volume(length, width, height):
+        return length * width * height
