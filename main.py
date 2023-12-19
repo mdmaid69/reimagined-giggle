26324402180dab7dd6739a66f5f13477b110b404
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:50:58 +0000

    gift breathe charge criticize reach

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
