2ddc1a635fca415736f644512b17c1c1383b2a6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:24:44 +0000

    loan bread fortunate weather port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
