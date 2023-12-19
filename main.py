db624bd9221e465c8017be7bd3c497cdf2ed78dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:42 +0000

    dream blue vote argument gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
