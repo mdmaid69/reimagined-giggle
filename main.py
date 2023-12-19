5a5e90607ed44adb041a45f49d64af41ee181164
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:09 +0000

    compare play spend hate arrive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
