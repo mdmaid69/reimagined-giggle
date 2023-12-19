aa21c23b27f4a783b85844d28f929b51d4ddb6c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:22:50 +0000

    still term collection race frequent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
