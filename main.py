bf77734459b386d764d7b969ced0d08df0deb5c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:39 +0000

    most impressed affect thought along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
