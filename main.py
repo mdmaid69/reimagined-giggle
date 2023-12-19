a95d4f84ba4b8fab0c86c1a28ed28821935c8f6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:47:02 +0000

    pretend 565end quiet thinkthrough description

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
