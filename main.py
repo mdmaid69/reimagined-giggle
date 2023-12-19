afacebc20c87e06ac4ea58be3e99dc360ef5d9f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:39 +0000

    getout Gg interruption sing believe

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
