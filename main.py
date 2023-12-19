15ed54f19a3d1b3e6eab31ebe58d80542623837f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:21:52 +0000

    party sticky table main grade

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def cube_number(x):
+        return x**3
