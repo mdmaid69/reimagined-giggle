da4f0802a153aeaff625690f995fb52632352f17
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:55:01 +0000

    in gather grade ordinary long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def reverse_list(lst):
+        return lst[::-1]
