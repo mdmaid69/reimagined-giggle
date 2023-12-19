f28f2cfd14ee43d9cd9619e603f9cf525704871f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:30 +0000

    kind well stranger zero cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
