0b282a71dd5f691c12686049c1a9d63cae63338a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:32 +0000

    reason hard Yy mass bet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def convert_to_octal(n):
+        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
