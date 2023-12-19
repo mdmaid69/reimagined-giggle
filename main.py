8b545578c95737d522918be55728ec6602a9922d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:08 +0000

    total open stretch emergency shout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+text = "Hello, world!"
+print("Reversed:", text[::-1])
