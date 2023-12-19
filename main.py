18c09dee8c549873ca6fcbf8e07f816302b14712
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:40 +0000

    desperate road wild comefrom press

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_acceleration(speed, time):
+        return speed / time
