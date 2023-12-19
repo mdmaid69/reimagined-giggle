4c1524cc196e90c8d970fbcf567154ef3df1f337
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:52 +0000

    someone bound busy past match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
