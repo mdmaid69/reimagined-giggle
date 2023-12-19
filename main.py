4b19a9401e1a2b24be2850b3971a445648fb91e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:03 +0000

    possibly hold unite soon in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
