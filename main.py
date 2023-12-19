3dd4aba7b64f262bfa78bf766c75e2c72c498cad
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:55:29 +0000

    compare blame feed trust please

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import sys
-print(sys.version)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
