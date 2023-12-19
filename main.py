2a5ace5a51f4241daf2f816bb8a73eec8f76d388
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:51 +0000

    show evening suggest television complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
