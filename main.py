6e325b11a0adc6915ae5201c4e12cfff9010e74b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:15 +0000

    greeting defend bear nature talkinto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def add_numbers(x, y):
+        return x + y
