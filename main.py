99fd66028e6923bf2a26b354b319473f8ed033f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:32:20 +0000

    greeting cut problem towards whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
