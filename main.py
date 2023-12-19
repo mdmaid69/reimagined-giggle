bc822257c5e99d182e7fd3299e104f029a2441ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:38 +0000

    bother true generous some flood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
