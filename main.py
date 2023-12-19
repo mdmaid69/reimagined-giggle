ccd953901d86cfaa4efe202e2edd69996075f35e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:39 +0000

    mind correct belief test purpose

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(lst):
-        return max(lst)
+import math
+print(math.pi)
+  def remove_duplicates(lst):
+        return list(set(lst))
