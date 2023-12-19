66b9a6a84c0feabc62dcf9c21dee7ef2ab7c2d22
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:12 +0000

    note independent up but brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
