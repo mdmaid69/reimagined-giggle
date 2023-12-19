11897f1843c6665272d1166eaaf7259decf81471
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:40:53 +0000

    apart invitation contribute narrow sad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
-print(math.pi)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
