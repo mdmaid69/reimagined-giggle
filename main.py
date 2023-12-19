db498d9bf05e7310d089a7d1958670731616fc79
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:22:36 +0000

    determined phone such interest breakinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        return sys.version
