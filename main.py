9e0d52ebae7f399f7003ed545113b3168dbdc07f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:49 +0000

    line human turnaway tear interruption

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
