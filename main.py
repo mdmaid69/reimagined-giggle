e6d5e3a9027fdb59ebb27fbe23e1a6f954aa67bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:12:24 +0000

    good defend emotion usual world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
