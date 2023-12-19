f4799fdd56696d0aba3fdb801169d5ec9bb977ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:09:09 +0000

    square cut report season rub

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def count_characters(sentence):
+        return len(sentence)
+def square_number(x):
+        return x**2
