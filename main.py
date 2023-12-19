1f1847b30aa7a24ca32e64598628c82d191045df
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:53 +0000

    woman ourselves exit hate deal

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def subtract_numbers(x, y):
+        return x - y
