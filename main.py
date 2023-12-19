b230c88ce3e38fca67b1d12a441d20fa3cba4669
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:04 +0000

    excellent back like sure sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_density(mass, volume):
-        return mass / volume
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
