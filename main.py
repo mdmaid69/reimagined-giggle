6f1cd35d42f69804db4bfc44ed75f010f2755af4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:05 +0000

    nowhere agree wake raw charge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
