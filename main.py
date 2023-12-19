a04e563cf992b6f0b231c0f26e272badb8ab47b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:54:45 +0000

    save discovery authority car 571enough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-for i in range(10): print(i)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
