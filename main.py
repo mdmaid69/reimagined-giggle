c296ef71f4fb10eede4bc001358ba6d162a63575
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:35 +0000

    emotion record gentle exploration apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+print(os.getcwd())
