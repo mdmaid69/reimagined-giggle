fa99d63bed424ca10d5097a1c70d8d765468e7aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:07:40 +0000

    surface ignore judge deaf risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
