252f5b355d3c5f79df42b32124f4a66de185dc10
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:40 +0000

    to summer flood shock minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def multiply_numbers(x, y):
+        return x * y
