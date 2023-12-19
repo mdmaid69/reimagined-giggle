399a926e581709ae0c567c1fdd511da658275dbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:43 +0000

    system always unfair smile figure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
