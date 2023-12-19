564ff130e5eb81d8a0d46cb9e0379c66e6447e03
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:08 +0000

    organization do workout sensible takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import time
+print(time.time())
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
