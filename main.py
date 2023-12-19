2942d345a589e7a777a818f3ce32515976d65780
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:24:26 +0000

    narrow butter account wear moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
