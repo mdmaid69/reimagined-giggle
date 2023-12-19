8828d291cc92a08abf5bbe5bfee0d6823f9c54b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:24:24 +0000

    characteristic part anything calm hear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_even(n):
-        return n % 2 == 0
+text = "Hello, world!"
+print("Characters:", len(text))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
