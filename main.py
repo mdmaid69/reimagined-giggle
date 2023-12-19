180957f50b9391d94b136f02c9b9b158c56bec7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:53 +0000

    admit wish gradually prize genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
