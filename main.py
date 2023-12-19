b65ed6f44563e8a637c04b18251ff1fc7ae4db79
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:52 +0000

    sea table Zz zero fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
