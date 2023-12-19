8baf472b322edc32e9cda18d0d993c7ef62778c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:08 +0000

    trial sharp suit woman plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
