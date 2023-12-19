412a86be5d864762980d279b9714c2547b7b6559
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:37 +0000

    destroy pattern plenty card warning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
