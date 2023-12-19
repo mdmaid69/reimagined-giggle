57e4f18b1bf8f8004d5f23f77213cfec73888891
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:33 +0000

    corner uniform no bringdown force

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
