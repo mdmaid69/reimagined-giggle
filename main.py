de7747e0eb07bf180dc5f76b155e568670d56d6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:14 +0000

    fix ready unpleasant appeal busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
