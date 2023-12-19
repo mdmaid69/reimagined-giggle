9526ce16595945fb5617e8328d20202b407127ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:31 +0000

    mass lookaround wealth comment tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import collections
-def create_user_list():
-        return collections.UserList()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
