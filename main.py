435de9d1734111ee106a53db674ddbc8bc34c1a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:59 +0000

    hang turninto careful distance far

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
