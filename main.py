da7b7cbcc9f6bc2a716593cf80d819afd73314bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:06:16 +0000

    police often entrance aim contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
