665551f5d1a02347720242c9143a80379cf9e34b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:15:21 +0000

    everyone strange confident visit way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
 import array
-def get_array_length(array):
-        return len(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def get_array_itemsize(array):
+        return array.itemsize
