ff4dac12248a92d997668861a4523c3597d3cead
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:26:00 +0000

    sit night action earn innocent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
