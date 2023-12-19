c267790e109c9a457f1d446549e93f023b78f2a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:37 +0000

    evidence considering already left brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
