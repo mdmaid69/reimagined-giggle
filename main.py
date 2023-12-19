b35be9ed421754df9358d0f21a575d033213168f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:02:53 +0000

    obvious apply none far coffee

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
