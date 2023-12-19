ebff511f617dda42e66dd15545fdb0b26bd12a51
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:26:17 +0000

    else guess usually sit same

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import tensorflow as tf
-print(tf.__version__)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_current_directory():
+        return os.getcwd()
