f39c5c28f79edd2332a51f68477a875fbb1716e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:49 +0000

    be start comeoff indeed sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+for i in range(5):
+        print(i)
