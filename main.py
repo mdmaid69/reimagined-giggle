d1a2d0a023e194acb149e834334e713ce30a3e90
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:44:12 +0000

    cold difference else fairly rob

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
