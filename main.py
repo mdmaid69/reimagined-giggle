4f5e79deb43c5d58fef03894b2f71e9f1441aa4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:30 +0000

    sister door firm fortunate pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
