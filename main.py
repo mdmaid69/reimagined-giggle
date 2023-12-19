bfd347949bfca90d320a6e7e22addb9268ab099d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:14:56 +0000

    different or visit unpleasant interruption

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
