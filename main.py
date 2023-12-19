6a87aaea071b96a3e91f235313c8983fd3023b5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:45 +0000

    fortunate physical touch attempt value

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
