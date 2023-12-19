27e2f7039148cbddc102d53a04c3de7e39618ea9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:33 +0000

    characteristic change profit advise loan

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def greet(name):
-        print(f"Hello, {name}!")
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
