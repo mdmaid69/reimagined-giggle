715c1dd414d210998ecf35c705e41f00bf260f58
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:50:30 +0000

    beginning perfect difference weekend improvement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
