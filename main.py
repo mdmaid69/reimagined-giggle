83998d5457dd0325af385818ce3b89e5417bb562
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:38 +0000

    completely programme accidental ring later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
