4b5d5e34183ab7591f1c24e6cec10c4336fd3136
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:23 +0000

    rich copy large computer glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
