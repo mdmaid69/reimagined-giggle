90abcc26c13744a301b8c8d84528a3a161741243
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:48 +0000

    unusual point who innocent shame

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
