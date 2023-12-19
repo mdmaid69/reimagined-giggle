6c3bafd58fc8073def84a06b230234840050f1bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:37 +0000

    operate soak squeeze safety fire

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+for i in range(10): print(i)
