94d630bd912bb3a1ae9a2f71cd9a760362dcd444
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:56 +0000

    must brother throw person touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import getpass
+def get_username():
+        return getpass.getuser()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
