4350bce014b6b25ee42a5885b586009baed50868
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:24 +0000

    recognize race dangerous cost measure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def add_numbers(x, y):
-        return x + y
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+print(time.time())
