1fd542bb8ff633af800ffbd865f8e059fa7d32b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:31 +0000

    gather knowledge I amount raise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
