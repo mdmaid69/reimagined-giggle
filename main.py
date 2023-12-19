a993328651798edf6c357d1f0706ab4479a6d8dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:35:24 +0000

    start direct pump even suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
