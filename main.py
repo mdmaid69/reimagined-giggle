3dd778d08a9b13ff75097a1b730713b0802da5fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:17 +0000

    call estimate nor fold Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
