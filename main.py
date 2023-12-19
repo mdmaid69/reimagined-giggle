dfee9ed0b25d8afb6c5affe7556a61c5d716ea7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:49 +0000

    control towards matter aim wrong

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
