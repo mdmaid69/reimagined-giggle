47ba31ef1f96d170537a14ab14f2f314b849696c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:16:58 +0000

    force really pity choose surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
