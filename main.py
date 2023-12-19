090ac5690a7eb0c703e09aaa674f8971d36ecd78
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:03:24 +0000

    branch shout news cold stiff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
