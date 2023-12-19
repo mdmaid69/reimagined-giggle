e3609f538e616b7c42305cd7b52cc1af9df3ebf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:02 +0000

    purpose unit suit present workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_list(array):
+        return list(array)
