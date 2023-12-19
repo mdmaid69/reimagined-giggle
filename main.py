165bf5db3d8b0823ea54f680d986f629e0703b49
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:20:26 +0000

    face rush bound ride within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
