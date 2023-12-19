b3161b85c2c4c1203016832195d16acba8a7773f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:04 +0000

    such suppose insist sensitive takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(5):
+        print(i)
