0979fc00417dc69fa171bccb77f3a836c1833ab0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:19:00 +0000

    like choice Cc trouble support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
