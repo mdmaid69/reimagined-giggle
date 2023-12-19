f848414e04faf221bef13ef3da246c52408beb28
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:11:27 +0000

    climb under 564encourage custom brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_index(array, item):
+        return array.index(item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
