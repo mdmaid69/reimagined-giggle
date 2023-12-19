ce4234f6326dfbc3e3b25058f55c9345ab557d61
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:40:01 +0000

    test task OKorokay either comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
