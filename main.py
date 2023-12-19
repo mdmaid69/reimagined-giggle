333cf9b8461a3d6b3df4d7fb0eb2b66b4dbdf8c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:36 +0000

    deep right angry travel bad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(lst):
+        return max(lst)
