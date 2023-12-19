2a078991f4a2f8d4bb55c8ea8f62d3c22add7241
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:55 +0000

    middle train exercise himself something

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
