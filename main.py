ddcb34d5509363138fc4b704ee7d2001229c2d40
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:11 +0000

    lay confidence heat sticky wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_binary(n):
-        return bin(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import collections
+def create_queue():
+        return collections.deque()
