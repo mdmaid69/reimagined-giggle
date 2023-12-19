80389c43525dbfbe56b414188b75b225914c9e76
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:09 +0000

    mess chin sky call near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
