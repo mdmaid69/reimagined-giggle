9a16d0d6b0505efd412458241cce5a4b9b48a43a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:25 +0000

    connected acceptable gate person justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
