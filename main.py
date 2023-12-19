da89872dcf8379248c843490f3ab01923b0560d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:32 +0000

    unlucky repair passenger against night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
