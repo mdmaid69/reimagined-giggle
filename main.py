ba1dd11765b20e978b48b2fc27b0e2e0f53a0a2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:06 +0000

    neck protect top hard minority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
