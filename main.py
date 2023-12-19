0537a81915fab4b4a4c105b8232accdf109ed3c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:02 +0000

    need move conclusion authority offend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
