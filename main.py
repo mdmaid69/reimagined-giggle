109826ff4c07e3cb0fe4764b957a4252905e7d2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:42:58 +0000

    quickly guide vanish arrival myself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+for i in range(5):
+        print(i)
