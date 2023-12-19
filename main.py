85e8523d521b8234c860abebed21cef80f573802
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:46 +0000

    know along strong bury bush

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-for i in range(10): print(i)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
