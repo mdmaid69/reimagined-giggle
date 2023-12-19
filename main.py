dbb98b1ee9e8d172e1a7bdbb39ac9363dc4f814d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:24:41 +0000

    bored display branch practise best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
