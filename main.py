241e7f70d6ec85ab09074b2f361cc213b41cb2d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:46 +0000

    dress image normal offer uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import sys
+def print_python_version():
+        return sys.version
