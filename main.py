edf5ed8a492d34a85575df44d76696115080e61f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:00 +0000

    will tip ourselves sign cope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
