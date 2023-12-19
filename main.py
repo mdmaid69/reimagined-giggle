d417a2acfaffa79f3371e231ce327030aca9b34a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:48 +0000

    shine appeal sport above getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
