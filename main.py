a5072cb9144314b7a13fcb03a2f88052b7ac3e37
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:22:00 +0000

    available education above attitude blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
