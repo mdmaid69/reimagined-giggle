eebc04d7afbfeec61139335f223f606ffd75c514
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:02 +0000

    nonsense geton feeling before deep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_itemsize(array):
+        return array.itemsize
