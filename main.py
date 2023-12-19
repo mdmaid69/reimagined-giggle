57387208da80e28a6ea08c919e81be4933f6b0ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:55:43 +0000

    past slice Ss bringdown bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
