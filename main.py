11873659d84ff86723f379d597e4b83a16af021e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:53 +0000

    whether snow slowly roll Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
