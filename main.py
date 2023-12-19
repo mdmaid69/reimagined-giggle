a656e32ce02fb14f06badacc04d9c5a456ce394b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:21 +0000

    simple whichever development worth defeat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
