eacbec1c312a21ca237288b5d1e808a2e044088b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:47 +0000

    afford alone explanation shallow serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
