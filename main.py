746f769588bfde15e7f1114d46e053809fd2a0f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:36 +0000

    appearance distance line later paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
