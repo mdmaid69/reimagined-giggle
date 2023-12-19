e7f5374ec6a31a8805f1dafcae39600fe4915cc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:46:38 +0000

    snow slip beside correction Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
