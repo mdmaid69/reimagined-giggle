6eee32beb0f9c3c1477759deebe625a762afa084
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:12 +0000

    stir rest criticism fasten every

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_work(force, distance):
-        return force * distance
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
