e656c3478b58c8f1f4501d125b0bf3c75df87e61
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:09 +0000

    train obvious comedown freeze excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
