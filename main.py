917816736a9b013983ddd579cfc9f39487e4e278
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:46 +0000

    she steady welcome since think

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
