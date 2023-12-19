9d809341f6e3bb6eff96ed9b1e11e04b1f436bda
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:49 +0000

    long doctor guess pretty flood

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
