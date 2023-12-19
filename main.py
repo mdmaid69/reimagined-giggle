e98cf97362bf79b18cba6534fd9a9d9fc9724100
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:36:26 +0000

    discover save for there gas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
