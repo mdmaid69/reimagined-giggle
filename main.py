d35572859123b675a694aa717f98ae09ec754a1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:51 +0000

    peaceful juice emergency earth various

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
