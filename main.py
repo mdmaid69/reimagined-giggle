adb39b3cfda9b9559a9b411c6aae128bfda46a27
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:20 +0000

    sensitive actual straight wild lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
