84e123cf6ae6220e7d9aa4f5e87b6bb57cddf26d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:13:07 +0000

    brother section compare break whether

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
