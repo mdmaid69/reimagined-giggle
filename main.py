7024b11f10f20ab6150f4746f124d896f4e743f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:35 +0000

    dark callat try yet tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
