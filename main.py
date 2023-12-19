3b4c06637ebdfc5fdae7a4a833664f523b1c6f72
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:51:27 +0000

    report practise demonstrate whistle sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
