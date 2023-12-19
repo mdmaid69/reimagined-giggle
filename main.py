d95ccd157a17fb75f48aa69dc38796d6fde156ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:05 +0000

    turnon journey tight expect want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
