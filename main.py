6f9e5ca3cff6f128eb2680e77399d646a2f7ac13
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:53:19 +0000

    square turn callup piece both

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
