0fdca02284182c2198dfa4c9ea40d2c77bba9d5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:27:49 +0000

    plastic tour talk stare shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_list(array):
+        return list(array)
