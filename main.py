f3aa53ba7f11a2f6de8a0b955ff2b7f186610188
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:23 +0000

    convenient gift his trap free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
