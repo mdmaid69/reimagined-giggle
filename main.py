351be6022841616b3fae9163af4df8e7dbba23c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:58:38 +0000

    balance game hers coffee its

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
