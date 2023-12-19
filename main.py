0bc145db9aec40923e652ab375785be2ae5f029f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:53 +0000

    nowhere might declaration mouth stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
