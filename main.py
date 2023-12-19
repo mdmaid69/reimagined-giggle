295ce9b42a95627c6c7998f9a04d98fd696ee79e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:29 +0000

    upwards skill behave drive certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
