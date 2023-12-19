db947760a28ef9c073203347a6f8e6974847312c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:50 +0000

    bath object check down average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_floor(x):
+        return math.floor(x)
