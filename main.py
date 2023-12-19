6734172f5083ea188d0cf2f39ec47296cc48b13d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:51 +0000

    message Ii could command chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
