0f85afd7281ee4dac3e118db3dc18c230fe547f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:01:09 +0000

    concentrate Tt throughout callup fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
