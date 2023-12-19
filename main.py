1f48aa3303f3ee72ba5a7376975a63349bc55069
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:45:55 +0000

    minor chase declaration yours violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import sys
+def exit_program():
+        sys.exit()
