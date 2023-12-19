6f4789ea63b663a726cd4924071e8b86b7dad4de
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:08 +0000

    press band near constantly shame

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
