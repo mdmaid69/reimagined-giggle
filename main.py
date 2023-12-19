3761b76e8940378bf2e57be15ac187fae2621027
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:57 +0000

    border approval badly we along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import time
-print(time.time())
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
