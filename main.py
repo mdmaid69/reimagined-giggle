ba9bccb73eaede4956a466d6d4738ae18b4fc884
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:29:59 +0000

    pipe turn guard best connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
