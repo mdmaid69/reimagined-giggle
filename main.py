093dc33fab803e7d7e83f89b381561955ce8e196
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:21 +0000

    equal speed round reasonable custom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
