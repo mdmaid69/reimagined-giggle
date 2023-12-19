573d61f5276a80ff694b7026b067e9c53e075fdd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:03:19 +0000

    rid frightened official will normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
