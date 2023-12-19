ddaa6a8e98d0bc199ab53297f70421edf1943d2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:03 +0000

    closed rely pencil love otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
