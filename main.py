934831e771b15ffd50e9321d421e6b4126c38ea5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:02 +0000

    floor bus support trip judge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
