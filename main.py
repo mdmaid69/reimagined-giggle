8f2d74e58e6d3a59dea1d4e4ea1431ea33e0de27
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:16 +0000

    handle cum scream least command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
