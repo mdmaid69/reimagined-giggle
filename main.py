974f83b05ce027eabaa66ec3c56f953257f4e894
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:44 +0000

    listen damage mine straight tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
