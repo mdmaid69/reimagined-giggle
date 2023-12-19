4b1e1f0b62bf3cd4d437d4054f1826aafcd0702b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:40:31 +0000

    trade event excuse tax compete

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
