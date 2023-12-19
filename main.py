2d5e9583fabbdba632025a6b2fe1e271eef1cb6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:50:35 +0000

    regularly worse minor stop connected

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
