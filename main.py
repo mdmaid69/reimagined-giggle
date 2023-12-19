9991dc940a09274b0474dcddde3fba3a334e5998
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:11:17 +0000

    escape address connection majority worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
