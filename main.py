38b684ca0f5a46e441819cfbb9e21e85bdfbe94c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:23:32 +0000

    without spin or progress bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
