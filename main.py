7f3ab075837fb5e23ac9a83df2c298edaa7fadca
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:57 +0000

    from Pp boat contain degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
