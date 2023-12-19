94c86cf40b059e5a40782a13567361f2e33674f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:57 +0000

    spray ago twist experiment range

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
