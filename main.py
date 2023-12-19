68ea800b0776add8bab39ffcfd59bdfbfed5a9ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:49:56 +0000

    unable valuable damage comment deep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
