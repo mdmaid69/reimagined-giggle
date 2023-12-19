1553bfc5a90572f9114213aa6a285d51c674239f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:43:32 +0000

    imagine finish news beside classroom

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_unique_words(sentence):
+        return set(sentence.split())
