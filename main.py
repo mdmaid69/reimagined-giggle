fcb89ec41a56c7c909126514b880a38ce74edc02
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:57 +0000

    machine hurry bridge stamp along

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
