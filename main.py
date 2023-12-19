753537a9ea996bdbdf7313de584ab5656cbeb074
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:45:38 +0000

    post bend wide mind punishment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
