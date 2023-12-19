1cdf9fd3bfe66314d087da20b0efdb0878be513b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:41 +0000

    179bite before environment through female

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
