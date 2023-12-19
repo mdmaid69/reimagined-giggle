a0f448b701074adae130c95ec0ce568a620826f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:02:24 +0000

    large well radio comeup pressure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
