33b93464533a46f9cc359146e2293331edf57491
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:08:41 +0000

    stranger wall prefer look accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
