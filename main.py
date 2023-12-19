54e6d94f0b07e88830bc0539f7915284dfaed509
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:08 +0000

    him reason failure hide interview

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def square_number(x):
+        return x**2
+import getpass
+def get_username():
+        return getpass.getuser()
