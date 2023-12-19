4969515d8ef15cd8d2c48d4c4d07cdc664369b11
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:02 +0000

    broken pronunciation tour butter weather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def count_words(sentence):
+        return len(sentence.split())
