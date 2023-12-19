464c6b083c67051d4808ab69611f639670a29119
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:47 +0000

    cause excuse create pressure wet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def count_elements(lst):
-        return len(lst)
+def find_max(numbers):
+        return max(numbers)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
