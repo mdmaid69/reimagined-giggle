2eae2be6b516e7358d659e50e765318cb1c815cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:13 +0000

    wind skin agreement wet bridge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
