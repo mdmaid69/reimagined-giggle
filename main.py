263442a3077d994e57ac4d0b91cbbda794862664
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:35 +0000

    spoil city contact extraordinary rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
