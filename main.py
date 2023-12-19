6e0d8790d743ee9ae319c7344629b6961c14432a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:47:43 +0000

    even kick funny sale wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+print(os.getcwd())
