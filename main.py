90c3fcfd0f7e087ee9682c59bfd72b5739f154f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:57 +0000

    especially home tonight match sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
