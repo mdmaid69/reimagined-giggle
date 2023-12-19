e628491d2206120d36e284fbbdd4551519be9e9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:31 +0000

    general risk repair trip church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
