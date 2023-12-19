4483979d777ac80a10fdae3eac30626165881af8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:26:36 +0000

    about outside extraordinary male play

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def get_current_working_directory():
+        return os.getcwd()
