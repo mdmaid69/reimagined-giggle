59ab2108c0e65b4a5c0b6644cf20bfc6273d516d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:46:11 +0000

    sleep murder ahead avoid room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def is_even(n):
+        return n % 2 == 0
