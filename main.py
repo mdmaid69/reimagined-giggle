0308ca0f81c66d0784204d00e98c5d7b650ebd73
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:51 +0000

    reply busy hardly year chance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
