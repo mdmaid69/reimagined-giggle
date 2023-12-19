11bf0a1acfa7d77863b55a61e0dfd976c81e8d2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:00:52 +0000

    179bite lock brave tread new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
