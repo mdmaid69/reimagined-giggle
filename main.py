be13acdabb51ba500d91386c269d363f995c7a26
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:55:05 +0000

    hang hit pleasant thinkover judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
