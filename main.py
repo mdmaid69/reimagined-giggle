92107eeea5400da88f8a035880c2c2b8830d18ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:56 +0000

    flow custom certain nose tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
