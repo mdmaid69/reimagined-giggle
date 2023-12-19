27e7e500872d74ef1cdd8432dff669e3f2267dcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:19:53 +0000

    feed interest absolutely near careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_even(n):
+        return n % 2 == 0
