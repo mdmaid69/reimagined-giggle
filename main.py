df7772daf692a27b85990d8fd0a28fe646497042
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:58 +0000

    far sensitive discussion just straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
