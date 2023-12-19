d9af00903f1417f91099b0495b37e3b64cb785f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:08 +0000

    meet treatment conversation reduce together

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(lst):
-        return min(lst)
-def find_max(lst):
-        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def convert_to_octal(n):
+        return oct(n)
