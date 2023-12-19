5de1701558e8fe7b5d3289c1040ba62a38824029
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:00:30 +0000

    tall achieve do dangerous pattern

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
