6aea0e188a0ae5daf91528c6199099a9b57da260
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:16 +0000

    floor mind ahead snow feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+print("Max:", max(numbers))
