f4c47f3541b8c9cdf9f30f6c80be515ce7741007
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:32 +0000

    draw fit period again safe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
