7c7aa23ab175a7299597b5676f5716a542d52244
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:16:37 +0000

    tool shrink study blow want

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def count_elements(lst):
+        return len(lst)
