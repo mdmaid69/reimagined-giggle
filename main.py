b3e15818d17ff02f45cc1a8b48dd7c5631e83841
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:33:12 +0000

    set contents kill different quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
