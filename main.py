d48f150f98c4e5043b65741e5e9e9b76d99e8c84
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:05 +0000

    bring lucky confused or bother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
