637312c04dcb2c299f8fec7c225520943ffba8f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:08 +0000

    scared turninto regular talkinto wheel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
