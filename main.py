7e3805b071de7320cdb3284b664283b43297438c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:09:19 +0000

    wide possession cold knock plain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
