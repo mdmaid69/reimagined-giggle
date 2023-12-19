933ff1a727cfcd57b4fc0d1aafe7cb4c4d5ed74e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:27 +0000

    band partner lost hot foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_odd(n):
-        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def reverse_string(s):
+        return s[::-1]
