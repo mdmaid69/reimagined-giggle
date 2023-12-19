dc95d5156a7e8f421f47e3720ebf66d51a3d7cc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:46 +0000

    bin fence aim pause round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_current_working_directory():
+        return os.getcwd()
