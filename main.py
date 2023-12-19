23aae8076ca68746c67f4e827654bc7ae7dc97fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:23:05 +0000

    silent blind opinion plant smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
