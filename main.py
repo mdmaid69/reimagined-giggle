26be74dc3e0a955a18e6e9f5fc6d06c12f03c987
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:51 +0000

    pretend seriously discovery rent climb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
