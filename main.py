18d61148585a4e39496e2b06dc23b88cb5cef946
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:06 +0000

    formal pin satisfactory disturb care

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
