d9e8da2d2a3688eb36687cbc01ea3b3c08018955
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:25 +0000

    picture nearly getup command because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import tensorflow as tf
+print(tf.__version__)
