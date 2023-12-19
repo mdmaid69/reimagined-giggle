d601207ba5c24aa17b9538962dee6571ccc533bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:17:49 +0000

    expression Bb welcome leg makeup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
