a7669e5660b93772c2d9ee80c0cfdc3b7f386502
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:06 +0000

    day public head month bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
