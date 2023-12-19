71de4c30f0655d87d3c74175c67fb6e8b239ec35
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:20 +0000

    later talkback demand 568energy good

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def square_number(x):
+        return x**2
