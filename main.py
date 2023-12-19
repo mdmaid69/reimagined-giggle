59b6c62b75d48e121d1e5a9940f77c4c78459c52
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:56 +0000

    busy gooff shame lonely neither

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def get_python_version():
+        return platform.python_version()
