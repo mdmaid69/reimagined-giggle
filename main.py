b898ff85a48a565f64288572d4c49ca74cefc26a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:48:52 +0000

    recovery really lead great light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
