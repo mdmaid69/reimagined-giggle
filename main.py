82c4b4fc00646caf74ebece988d54028c8a25dfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:56 +0000

    lookaround shine wet until respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
