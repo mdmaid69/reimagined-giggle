31665e13749c16742f78d3efd53b7b32373c0c2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:41 +0000

    apparently defend underneath live giveaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
