c96a59d37ce718a20dfd27e71c992c3e2a33196a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:44:46 +0000

    steal holdon fork arrive shrink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
