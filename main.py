0725591517da85b6cc533193390e53508d5a420b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:18 +0000

    form clear tour stone suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
