701774b73800aefa47199b2aeb66190ed2578311
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:35:18 +0000

    hand weak little ring rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
