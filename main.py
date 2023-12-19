d533e6759d1ca711cd7251129f96d024f536cbaa
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:47 +0000

    tired fairly any brush phone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
