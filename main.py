a9357d12b6016e806217ca60a0a9e82fa467d770
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:43:49 +0000

    entrance comedown treat schedule poor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
