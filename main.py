5a0c7d3e6d9a1ea40fddfb39b9c76cd6d7ecc6e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:05 +0000

    decision glad put young real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
