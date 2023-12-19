3a7fccd319490bab725dcac63e0063fb2e7dc7b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:33 +0000

    expect calmly rise tragedy anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
