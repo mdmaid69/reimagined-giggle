50b51e470eef6f4f108d288ad12ee5280d333af2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:47 +0000

    married bang succeed room passage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
