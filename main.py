ce5a984a0d9db96d7a4b38081ab62984f0dba1fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:12 +0000

    hope after roll swim he

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
