914bb180f3067dae055c6967cf8f399beceb1973
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:30:45 +0000

    reserve someone heavy tense sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
