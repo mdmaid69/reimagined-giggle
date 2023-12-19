008c8ef0f54705012ea531a9a87fd13333366599
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:27 +0000

    pure tragedy produce ring news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
