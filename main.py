55f3f2f40d25cf4ab581b3f331372fdcadb6b0ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:29:25 +0000

    practise OKorokay except body design

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
