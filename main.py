3e168bf74b1f6b54f8a0221b40f88d4bc7f14dff
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:41:44 +0000

    turnout some off tip sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
