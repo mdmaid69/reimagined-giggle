da7245812ec2fdd13e469a7a63b73768aa7e1ad9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:55 +0000

    throughout regret stretch remain claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
