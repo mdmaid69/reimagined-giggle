cfbfdcf8f573862a8055377b3f97dbd2290c08d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:04 +0000

    interrupt about visit where total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
