4f743e789d2fbbdd21f778ab6c8c9256176b966f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:08 +0000

    warm regularly better child measure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+print(math.pi)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
