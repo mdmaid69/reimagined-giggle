1fc967e171d77be29fe7238e64ca37e7224cd113
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:36 +0000

    punishment person fetch tear store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
