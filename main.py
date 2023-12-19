ec889935a66ffb4739bf98eb0276af99ef164687
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:05:44 +0000

    television interview use goon give

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
