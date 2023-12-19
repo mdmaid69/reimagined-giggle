6dc276bd49c650d5514f5e421263e1ca4fd645ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:20 +0000

    trousers dinner heavy low charge

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
