b3b82b6204929f93b71f52d333328df11978c478
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:15 +0000

    bend only serious road performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
