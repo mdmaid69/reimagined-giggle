50cf5eff7fdeeec2ca2a217aa3773c27930721e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:21:39 +0000

    cough touch run speak space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_array_item_count(array, item):
+        return array.count(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
