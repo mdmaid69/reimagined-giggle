21ab16c221702a14b9c0cb46a81e0322c02f65c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:37 +0000

    attach sharp convenient fire now

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
