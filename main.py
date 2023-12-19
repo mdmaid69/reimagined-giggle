281a1a8bfc1e30cc3d1ebb044ec88dda4c4bb66f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:45:18 +0000

    keep cry before suddenly influence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import time
-def get_current_time():
-        return time.time()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
