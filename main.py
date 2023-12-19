da71e81d731a1646adc258154acc0c51fe838c02
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:11:39 +0000

    wake please level towards common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+print([x**2 for x in range(10)])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
