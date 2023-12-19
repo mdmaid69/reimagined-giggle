73266b91db857ec9b9feb65622e5a5cdd0d88193
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:38 +0000

    turn deserve show dark language

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
