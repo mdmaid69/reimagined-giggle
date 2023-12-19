fc302448e652a06f3c2036c5cf7f262a4e43e8f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:21:32 +0000

    heavy equipment much direct fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
