96908a3ae6a0d980cd09daf49e7484f71e61a76b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:09 +0000

    tough protection suit laugh getout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
