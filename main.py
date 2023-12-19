d73e3ffbd919b1c5185186fb0ea21a799a171de2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:08 +0000

    getback necessary let material modern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
