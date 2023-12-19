6b41de36a224ddfa122166c8c8c7182e5eea0bac
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:59 +0000

    tell mysterious Qq foot divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_user_list():
+        return collections.UserList()
