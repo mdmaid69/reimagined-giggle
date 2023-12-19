b365b5e2f93bfafa5b604b56e0dba670b9c4d4ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:17:49 +0000

    guess now distant autumn set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
