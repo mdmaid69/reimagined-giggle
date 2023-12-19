2f7549fc007b718dcf5e7d4523179f802b603520
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:12 +0000

    wise deaf succeed open possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
