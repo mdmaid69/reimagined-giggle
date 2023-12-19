98dca4998ecabcd69eb3dc3556fb0df86137aa8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:29:07 +0000

    any colour transport excellent also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
