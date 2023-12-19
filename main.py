4d7ef962d60e9923da6299fdd0de12f469b542b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:46 +0000

    light traffic back free sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
