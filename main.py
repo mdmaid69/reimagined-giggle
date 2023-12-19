193801f075691bfe11db0f4da6656ad0c778041f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:42 +0000

    regional paper 572enter ignore shut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
