63791e195f991285d75ded039a8170feca0383df
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:21 +0000

    breath move married knock almost

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
