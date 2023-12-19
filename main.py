375ef91493b7d4f5065c52c89ff0a1864093d678
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:37 +0000

    success loud its soft care

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_to_binary(n):
+        return bin(n)
+print("Hello, world!")
