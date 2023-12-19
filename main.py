0b4271e96a43b9ec2ce859421cce697630bdd7d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:36 +0000

    workon stiff mail Ii evidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
