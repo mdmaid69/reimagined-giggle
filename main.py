18578e6cc9adfb698d3b83dd8301cf930852f3e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:31:18 +0000

    fit high abroad explanation giveout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
