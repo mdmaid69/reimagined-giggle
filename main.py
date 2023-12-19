2662c692cf87aefa3cf1652ad978a0fcdee543f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:41 +0000

    shake unhappy bar minority load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
