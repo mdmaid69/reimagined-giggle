569fb3aab1a719001366f63f84e09366f8e16adf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:13:45 +0000

    trip further order tidy dry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-from collections import Counter
-print(Counter("hello world"))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
