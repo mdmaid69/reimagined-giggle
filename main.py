4fbb1f28ddde6212aca166716ab0d719b1fd464f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:53:26 +0000

    lot floor pollution normal succeed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_power(work, time):
+        return work / time
+def calculate_average(lst):
+        return sum(lst) / len(lst)
