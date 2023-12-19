06f56e13b56056afb53fcfdad7386f9ba19d0c40
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:30:07 +0000

    hope calmly wave until hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
