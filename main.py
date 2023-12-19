dcead129211f8e10546bcf4ac81147d0d2f5be7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:32 +0000

    water tone swing Bb party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
