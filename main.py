4126bf970af709058c5b75d52b24489da351bae9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:02 +0000

    play dream second upset crash

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_even(n):
-        return n % 2 == 0
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_pressure(force, area):
+        return force / area
