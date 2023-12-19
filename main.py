e45bbe9f391b34b5b17d953cfc8398429bd061d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:10 +0000

    bunch workon shade instruction no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import math
-print(math.pi)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
