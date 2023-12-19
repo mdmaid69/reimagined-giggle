39bbfa3dc7aa72d88ad5279f25ebb27291a29afc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:02 +0000

    maximum name complaint suggest alive

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-def square_number(x):
-        return x**2
+def is_palindrome(s):
+        return s == s[::-1]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
