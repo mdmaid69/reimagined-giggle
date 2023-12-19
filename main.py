eff5009840103de14635d2c37ad2ed58b705aeaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:20:01 +0000

    spoil sea judge step hollow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
