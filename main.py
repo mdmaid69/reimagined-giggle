8ff22dab19aee473467b704ed40b8991b7ca90c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:06:26 +0000

    ear afraid basic drop fine

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
