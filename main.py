0b9e0d05dbef7dd7212cdc47e76ec151d1e8e2f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:29 +0000

    own strength truth depend pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
