565870c41bf216fd36113852a06c74418891ea12
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:36:44 +0000

    message through probably snow pain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def print_python_version():
+        return sys.version
