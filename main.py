65940466cddc1c570e9f0298d76b49ff1ee74e7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:33:15 +0000

    tell near quiet surprise talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_typecode(array):
+        return array.typecode
