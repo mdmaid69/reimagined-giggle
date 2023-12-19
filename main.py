5b45d2049065bb4e1873774a03b7c09f602b241a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:35:10 +0000

    calloff similar spring convinced anywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
