c6cf9ff547c1b723c7687d440e29dd1218b7e4b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:09:50 +0000

    show leg rush impressive 179bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
