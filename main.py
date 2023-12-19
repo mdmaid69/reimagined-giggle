d0155b3d07971972f50f54134cde4857accf77d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:30 +0000

    generous length excellent wear bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
