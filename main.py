724844bbbbfbdf9d130a43eaee8b1cab13587a39
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:30 +0000

    criticism opinion transport note behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-print(math.pi)
+import random
+print(random.randint(0, 100))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
