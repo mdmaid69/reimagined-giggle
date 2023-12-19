f866cb43c0ea2c260c1ab19e77701191f20ec6b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:57 +0000

    convincing personal speed direct actual

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
