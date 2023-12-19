59f5cb3912d27b7e6359ae5a8bdb76e589c948fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:56 +0000

    purpose comeround goon certainly rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def sort_list(lst):
-        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
