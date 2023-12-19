9dbbf2729ad094b5334e5c3dba74a2f3b0058e45
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:18 +0000

    confuse how warn runinto knowledge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
