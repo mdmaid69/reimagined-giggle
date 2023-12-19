73233b51b93d26b92526740e5f64c4e4d27b3e35
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:03:49 +0000

    godown hole fold mess noone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_palindrome(s):
-        return s == s[::-1]
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
