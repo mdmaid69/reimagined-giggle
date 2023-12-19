e49a62484595bca22077ac181608baa5a52e0f17
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:50:12 +0000

    show down birthday shopping cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
