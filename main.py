2c7b17505b1dabb0bbece19eb25c10ceee8e4630
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:40 +0000

    again meet around protest beside

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import random
-print(random.randint(0, 100))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
