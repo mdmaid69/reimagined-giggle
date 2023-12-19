a598eb2d88e798735cec15ac5d4ad4cd8bebb8ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:51:23 +0000

    firm unhappy opponent rich sudden

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
