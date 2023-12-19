ce80efe99a38280851424615113cae08e06452e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:33 +0000

    look bend exchange joke alive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
