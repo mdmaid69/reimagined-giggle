6928c59ea58e5d7cff6f0939cd874825724c98f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:33 +0000

    lay house night position year

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
