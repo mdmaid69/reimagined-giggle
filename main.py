2209dc558f9cbf298bded4ccfa9e6cbcea4705a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:27 +0000

    slow bet gooff official guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
