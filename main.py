9ca638de314dd4d167da93b430d30bcd1cc3026f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:12 +0000

    rubbish present turn interview rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_volume(length, width, height):
+        return length * width * height
