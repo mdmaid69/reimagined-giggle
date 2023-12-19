f623f161c865d1a0ba169cb8e0772b80ce452d79
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:49 +0000

    path conversation path clearly deliberately

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
