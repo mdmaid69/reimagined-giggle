8227c61da2e2e5489f8e9432f4e5df46881b61c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:42:31 +0000

    relief close contact situation workout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
