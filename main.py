4a3bd5f8549e13073ca746fdfd8b0aa32bf9aa48
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:20:19 +0000

    shall could dark down deep

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
