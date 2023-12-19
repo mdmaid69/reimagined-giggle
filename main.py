a1528ed09652b7ebdc4f264541b96cf51dc24608
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:46 +0000

    threaten open beside ahead shall

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
