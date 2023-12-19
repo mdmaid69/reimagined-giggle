116cf37fbe1a591a6abc46e1bb6f2909a681bd7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:51 +0000

    group save first wherever wealth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
