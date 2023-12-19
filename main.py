611fc28233ccc7750162d07df3312970644f5234
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:39:33 +0000

    estimate move experience trial gap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import random
+def roll_die():
+        return random.randint(1, 6)
