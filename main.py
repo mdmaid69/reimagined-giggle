01ec86a861ec0c03ea2b5f760bd67fc1c27ae4d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:25 +0000

    develop emotion sad rough tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
