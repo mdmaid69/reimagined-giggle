0f7af748313a31568ad0a56e3cc2c009cfc5c01d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:42 +0000

    general exit bringforward weekend influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
