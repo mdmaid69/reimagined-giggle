f4feb3beb99fb9fa74d1a2276a059c4957c06ea0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:59 +0000

    rate sore strength skill giveaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_str(array):
+        return str(array)
