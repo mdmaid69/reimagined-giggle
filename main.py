c909ee4cb9cc1c18e764064824aac7745064c08a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:25 +0000

    cruelty juice runover deal hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
