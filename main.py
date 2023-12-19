052106ebc204f33872c7d4ce12fa3f04d6fee232
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:49:24 +0000

    worry will pretty think string

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
