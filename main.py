eafc0768d41823a60f4f121ed5de176a460f43db
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:50 +0000

    brother complete around disgusting shine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
