63bd37a4eb709f9090ecfbed67c00edbe500f755
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:18:24 +0000

    about drink year badly knock

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
