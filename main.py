44f87ec582ce5130dd2f4eaf9bafa5240395e519
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:28 +0000

    love relationship blame accidentally willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  def add_numbers(x, y):
-        return x + y
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
