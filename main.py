b3866a1eec796432b582d932cc2c8b8516f83b93
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:44 +0000

    quiet estimate outside square drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
