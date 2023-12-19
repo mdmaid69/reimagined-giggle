1fa5b69efdb12fdb881c1ea10f484a85f6cd6a2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:38 +0000

    work reduction sense another considering

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
