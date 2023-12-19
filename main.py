66ad765589ebb464742d7801bc2a7807c49020aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:13:24 +0000

    love slip rid light history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-def multiply_numbers(x, y):
-        return x * y
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
