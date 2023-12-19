75c6f49909a7446ea4aa1f8ddbd2112187ec2fe9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:25 +0000

    healthy judge clearly piece wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
