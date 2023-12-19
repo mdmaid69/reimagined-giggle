bc1dec331bcc2d838a01f32a5abd87abd10d6457
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:24 +0000

    tooth push respect mixture Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
