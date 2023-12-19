4cbf48fbc168dc7157bdf097e7aea0a57a63970d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:55 +0000

    beside Uu project exciting effect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def is_odd(n):
+        return n % 2 != 0
+import time
+print(time.time())
