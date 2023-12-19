bf24c8d18625ffe0d07ca690f5cb59828cd3f4e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:46:36 +0000

    interrupt feel must character make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
