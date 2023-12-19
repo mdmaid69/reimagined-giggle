8c15b289985bcdbdec83bd7b7a1e2ba650b37c18
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:45:05 +0000

    cut yourself maybe impatient statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
