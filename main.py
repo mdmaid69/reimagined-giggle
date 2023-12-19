9477fcf7b2abc781766bd1c60b1ddcf1d082ac1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:31 +0000

    throw wind edge early main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
