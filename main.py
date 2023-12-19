752e42a792d297f34d16b47dd3e086940e738573
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:46 +0000

    discuss avoid rush bad present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
