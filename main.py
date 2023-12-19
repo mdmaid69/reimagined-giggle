e3f81de81d0a7ce4b1ca8ba1b69ef9be9cbdd18d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:14:12 +0000

    chin temper chat flavour bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
