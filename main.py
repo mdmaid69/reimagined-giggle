5b651ff4f0903d2dda6842427eed194d207bbab5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:23 +0000

    result fear late possibly characteristic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
