bffd6bbca815d8489046c345c62e24fcc5fdabff
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:41 +0000

    goout each Ww necessary shine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
