5534d0a04b857d0aecfdc96bb5b69bff37dd711e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:49:21 +0000

    mainly invitation clever 568energy concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
