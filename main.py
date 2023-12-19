c7e230ea170ee2cdcd2ea7f4412bea048ba12907
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:32:39 +0000

    great forget hers last matter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
