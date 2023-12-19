dabbd129eca29c79e6a530072db8d22af96ff6eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:09:12 +0000

    borrow left mother getaway nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def square_number(x):
-        return x**2
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
