e14f0a4158ee4ec39ccf63728173448714277e13
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:52 +0000

    otherwise small style carry frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def subtract_numbers(x, y):
+        return x - y
