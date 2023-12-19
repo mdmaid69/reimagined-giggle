99702e68e3e0ff38c58748d4a63af20d6b067650
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:42 +0000

    old direction and uncertain run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
