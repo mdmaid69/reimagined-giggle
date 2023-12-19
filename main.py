f960c10e6b84345768d0360345b994e28a5a0e9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:26 +0000

    away problem form married would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
