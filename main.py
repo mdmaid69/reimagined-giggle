9b8e12f79e5f471e0eaa1b211c6ea3348f89f75b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:31 +0000

    566end body birth company clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
