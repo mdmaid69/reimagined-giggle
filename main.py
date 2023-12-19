5c031780dbd0ec1eb6b6536e60ac01005b219d78
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:10 +0000

    object holdout active sport neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
