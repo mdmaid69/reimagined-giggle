8b88ee96fffceaaba87473f5ed7a05342920a3ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:48:50 +0000

    mix aware 573entertainment dry weigh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
