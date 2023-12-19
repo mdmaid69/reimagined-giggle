7e5febfcc3702a4f385c7368391fbb01d0801e16
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:38:39 +0000

    market learn invite everyone stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import time
-def get_current_time():
-        return time.time()
+import sys
+print(sys.version)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
