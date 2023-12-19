d45c7f272eae667ec12a923721af1f78176f85e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:44 +0000

    wrap advise friend forever repair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
