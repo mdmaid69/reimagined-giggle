85417a4c7422f24754baf5ad38207c911e7ff47e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:14 +0000

    awake mine man share greeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
