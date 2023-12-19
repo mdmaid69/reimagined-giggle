0f30d28ce63eac610e74b9ebcc2c369f56503fb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:42 +0000

    steer amount lookup keepaway fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
