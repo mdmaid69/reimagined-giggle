088284af856132de33cc0487c7e67ad0dd832ad9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:41 +0000

    burn coat dream well impression

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
