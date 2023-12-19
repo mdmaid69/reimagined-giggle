bd94836f89061d0299f7bb0b9d0be490ab87050d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:34 +0000

    style profit taste runover trouble

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import datetime
+def get_today_date():
+        return datetime.date.today()
