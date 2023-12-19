28d1ca2cb3a7252b421f5b5768e6b60c40a100bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:07 +0000

    pretty follow connected thankyou hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
