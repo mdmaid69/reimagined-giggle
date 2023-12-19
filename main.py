7866db71a311edd0a377241d35b9028cce9cecc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:55:20 +0000

    point exercise sign keepoff interest

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-print(sum(range(10)))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
