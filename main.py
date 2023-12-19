9aed58979b80071647bb54e1e71e7dfeb7e5ff11
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:58:20 +0000

    opposite fall basis us combination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
