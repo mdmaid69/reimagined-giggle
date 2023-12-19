2abf4c3f913040cc6976976c1fc2c4ff276922ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:46 +0000

    coffee flavour goon contrast Nn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
