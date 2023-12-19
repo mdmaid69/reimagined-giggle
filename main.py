568e95b8e0eaedee7eda7aab09c4c33d365968d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:24:30 +0000

    pattern autumn fat perfect just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
