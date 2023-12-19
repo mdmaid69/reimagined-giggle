2742e7cc090dd0a140d104f9aeedf05589a2fc51
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:06 +0000

    our fault freeze idea convinced

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def square_number(x):
+        return x**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
