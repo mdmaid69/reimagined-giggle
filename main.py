88a973483d87be5252c279b02d91f337b189ecf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:35:36 +0000

    wide wrong idea inside reject

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-def count_characters(sentence):
-        return len(sentence)
+import numpy as np
+print(np.array([1, 2, 3]))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
