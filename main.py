f143a3621d5a52d12afd1ae0390d5c189d0a6455
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:26 +0000

    pollution 569enjoy giveout method arrival

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
