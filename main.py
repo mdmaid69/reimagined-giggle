e7f14c90f41934a57f7f53b3cbfea0d829f45928
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:06:12 +0000

    pocket pupil discover tour switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def clear_array(array):
+        array *= 0
