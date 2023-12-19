d9a1ebe0d236ab1056177c43066c7ff2045064ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:09 +0000

    pain public have forgive concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
