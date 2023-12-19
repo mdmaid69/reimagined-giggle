c41eda079645ffb4cbe4d2ca21b015c09d8c16b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:00:16 +0000

    justice shame thick tend track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
