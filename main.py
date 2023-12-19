b987987ced8a3bdc2f82825271b7016a9afbd4f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:20:38 +0000

    centimetre compare ball whoever achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
