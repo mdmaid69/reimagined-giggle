c3e13bd8575647510729853b1f69686148711108
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:43 +0000

    wander yes appeal middle definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
