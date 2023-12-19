ee416631f5d137875d67c5677f8a5e7b36cefca8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:13 +0000

    excuse switch comedown short suit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  def count_elements(lst):
+        return len(lst)
