c544221c60f76a2adab16075723482851a6204a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:35 +0000

    group performance burn search normal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def cube_number(x):
-        return x**3
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
