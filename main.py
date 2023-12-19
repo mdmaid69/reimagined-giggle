594a3df3a4cabd644030a8fb390152554ae4ceb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:34:14 +0000

    event none deal cinema transparent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def is_palindrome(s):
+        return s == s[::-1]
+for i in range(5):
+        print(i)
