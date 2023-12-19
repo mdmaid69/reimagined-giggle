2f4cfb23d6af6e294908f3528596633075811972
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:19 +0000

    search room relief tip right

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+for i in range(10): print(i)
