83e5b73f8a492ef84dc352dad5b68e12fa6a49b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:17:16 +0000

    cold turnover sudden steep objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_list(array):
+        return array.tolist()
