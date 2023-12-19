1255e3bfedb5cca98b3735b98de4fc01b3deb340
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:13:58 +0000

    next support contact total shadow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
