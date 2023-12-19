56e7651b87c297942fb93331bae65a07bef4c378
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:21:46 +0000

    vanish hold both post interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def clear_array(array):
-        array *= 0
+def get_array_typecode(array):
+        return array.typecode
+import numpy as np
+print(np.array([1, 2, 3]))
