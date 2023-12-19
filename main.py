4942eead032eaa8f2b24c2e93ab97b997b913380
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:37 +0000

    considering nor ring ride yourself

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
