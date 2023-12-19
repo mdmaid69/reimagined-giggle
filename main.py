dbccb0d8cfe9af9a60b8a0e2513a8fd5f8e5da5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:08 +0000

    566end sticky pool autumn come

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def clear_array(array):
-        array *= 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
