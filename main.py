20c04fe64a5f02ccbb13e2c1f43be853d705ee31
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:20:19 +0000

    other from blow vanish estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_str(array):
+        return str(array)
