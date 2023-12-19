ae3514e68eccb559b0f9edfb34213865e450c3b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:29 +0000

    generally generally wheel turndown telephone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
