7353b2845384540fd3f7efe32f8b2fcf98df4517
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:49 +0000

    brain related greeting interfere language

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
