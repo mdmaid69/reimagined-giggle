68318030b8a6bbe334f02a26d2f4c2ae88443291
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:46:06 +0000

    each punishment then relative total

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
