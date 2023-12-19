a918688aed558374acfc1827913b48761d2fb75a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:42:06 +0000

    nothing high lie patient extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
