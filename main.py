fe5810607202ea2b68677cb4bf6866707af347d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:31 +0000

    authority match throw safety strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
