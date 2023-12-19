be777f39f09869ca41e4e02165e8ba913451f8df
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:33 +0000

    common confuse band love every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import collections
+def create_user_string():
+        return collections.UserString()
