5112e35636b9d13b15022713e2909653507eb3dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:26 +0000

    word godown cry quality keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
