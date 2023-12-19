e8f00bd86dd2ac8c882b9044d5942cc9024bb3d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:37 +0000

    cry rub breakout its answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
