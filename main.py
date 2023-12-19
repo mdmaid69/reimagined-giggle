1f78a26370a40fe8abd2681fdf23898a0bc4cca3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:46 +0000

    birthday still home father unkind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
