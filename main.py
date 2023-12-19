43fe8a3222ef0cf3281021eb6220fc49fff673eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:33 +0000

    hot back exist round introduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
