ab2495d834be4c4b41d37ea7f43abf61966f239b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:39:41 +0000

    already rest accident grow crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
