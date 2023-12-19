b36af5f029177db111af7d33aa9b3eacc5d70310
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:14 +0000

    divide before height list type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import numpy as np
-print(np.array([1, 2, 3]))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
