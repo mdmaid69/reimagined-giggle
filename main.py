6aa4918f92979d2ca530cee2655bb8013c76a936
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:24:01 +0000

    fill turnback rule next receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
