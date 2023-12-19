4723a36c28a0b3ffb2fb013b3aa500f271ca86c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:26:19 +0000

    blind become constantly job odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
