fa672698198ea7af99146f16f7471c5d24d43b8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:41 +0000

    until prove passage by cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
