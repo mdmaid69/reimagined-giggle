539358ba44b9bb6421c734e304c154b6462cc363
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:04 +0000

    content region please book save

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_combinations(n, k):
+        return math.comb(n, k)
