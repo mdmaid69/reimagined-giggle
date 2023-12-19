1f5718fdd0fe8e12c5bc4b599fe81948739321cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:55 +0000

    finally mention partner give obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
