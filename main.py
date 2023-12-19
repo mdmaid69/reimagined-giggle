14ff563afa108d6299c94927abd0c3a017d9beba
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:32:17 +0000

    busy style watch mention brave

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
