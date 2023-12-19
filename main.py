2fc2dea0778a2550d583a774b1a2e79af961ab95
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:19 +0000

    tear bridge holiday wide everybody

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def convert_to_octal(n):
+        return oct(n)
