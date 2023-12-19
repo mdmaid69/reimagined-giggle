a04f2bbb12999ffc105eb4aed065dd03f7d24578
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:44 +0000

    further wide runover loud keepaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
