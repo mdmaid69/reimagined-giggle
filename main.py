3f81d77b7dacc2d47f8acd00aafbb835b3a50993
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:34:34 +0000

    realize pull come sometimes discuss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
