1597d9bb3e8b35e1f7897505f38199d6e7a46954
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:13 +0000

    successful argument government near on

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def remove_duplicates(lst):
+        return list(set(lst))
