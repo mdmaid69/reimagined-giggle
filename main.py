264c95d7f236c4a5da71792f15a3e2e785ae0d4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:24 +0000

    powerful treat insist answer reason

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
