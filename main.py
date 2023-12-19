9ed7ce4fb566bc3ff3b8646aa3c8e9c85be4a7b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:24 +0000

    correct Bb stamp minority expensive

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
