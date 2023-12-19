0487ba91ef36d73fcbb638e80703a7d1ba974feb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:49 +0000

    Bb heavy miss fight pool

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
