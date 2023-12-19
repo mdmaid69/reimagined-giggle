f02938ce6d394a67144c122f00bf2009ec61fb95
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:57 +0000

    shock sink safe sharp too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
