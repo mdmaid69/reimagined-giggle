03214398a3c7125203d502f61d80dc179e0642a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:18 +0000

    division telephone degree deliberate first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_unique_words(sentence):
+        return set(sentence.split())
