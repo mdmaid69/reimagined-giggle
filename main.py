91d8e329902fe18a94f299ae170d60cb151f02ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:50 +0000

    roll feel somebody snow connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_priority_queue():
+        return collections.deque()
