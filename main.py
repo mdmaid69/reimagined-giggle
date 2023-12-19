69b0b7902c84179bb8f7cc3faf83f42de3ae805c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:50:43 +0000

    well spring opponent interfere please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
