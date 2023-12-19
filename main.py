8b32353b9d156aed223ae4a630f90ac793ae1f8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:04 +0000

    reject care company simple scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def divide_numbers(x, y):
+        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
