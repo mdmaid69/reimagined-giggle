ecb49f95f8b69cd31069cab73fd430de72ac072b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:32 +0000

    breakout match official fight fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_item(array, i):
-        return array[i]
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
