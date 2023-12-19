60755c98f4741535ebfda021ecab76e0c60d4776
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:17 +0000

    can comfort demonstrate worried Ee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
