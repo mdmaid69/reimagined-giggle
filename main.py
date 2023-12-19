632b2253b7eb56edda6ef27e746f5820240cef2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:54:55 +0000

    order Gg slide exercise oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
