e8d5778d2ec91cdd9e8c75ac4ee2fc8459169be7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:04 +0000

    future in lookup neck neck

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
