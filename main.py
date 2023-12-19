7a049be8020fc3c17bb1c0f96c60c411be576c34
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:23 +0000

    sale neither odd negative glad

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
