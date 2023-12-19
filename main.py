f56fd137437f70accb0db9efd0ae574152fae00b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:41 +0000

    against club crime respect thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+for i in range(5):
+        print(i)
