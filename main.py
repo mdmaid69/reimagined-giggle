748f5dc520bfdbfe988aacf0e8d2110b26fe6cef
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:09:13 +0000

    replace inform artificial people line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
