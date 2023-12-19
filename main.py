bda5899c8fb03d1e41ee1c82c19c242766257713
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:21 +0000

    speech active show hurry comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
