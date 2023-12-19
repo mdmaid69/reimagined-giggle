108671b9154ffd282c14d9d588b75f004d3dafa1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:18 +0000

    above usually foreign minimum sit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def square_number(x):
+        return x**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
