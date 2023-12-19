644729ba1a9dc10bcb4af57252547aadb27e30ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:43 +0000

    classroom occasional feeling wish top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def convert_to_binary(n):
+        return bin(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
