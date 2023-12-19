a314aaa5cdc8990270af3f240dc346c8b64fb3e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:29:42 +0000

    anywayanyhow language say pollution upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
