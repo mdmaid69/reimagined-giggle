4d74874a09f597b0cbe8d2482fd8d6efcac97f6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:37 +0000

    easy clean how desperate refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
