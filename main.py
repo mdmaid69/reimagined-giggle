2374f5ed0e3036ecd57f0a3f5f667c71d794eb1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:45 +0000

    read talk convinced store drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
