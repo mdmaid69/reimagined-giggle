2d09f6a1705aee88772ac66ef67c063ffbe8805f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:17 +0000

    may scrap blue advanced bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
