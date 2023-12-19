a398339de38156f4d8ce59d701a4f24c433c657f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:04 +0000

    sensible education suffer cook show

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
