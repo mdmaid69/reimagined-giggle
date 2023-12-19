bf001509978ce4c6922c1aaa851212342b707b34
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:55 +0000

    introduce church division dangerous protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
