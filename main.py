b97e30f1697e12ef550c09c6ff2cefb12ad5d999
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:31 +0000

    my neither rest recognize possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import numpy as np
-print(np.array([1, 2, 3]))
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_list_from_array(array):
+        return array.tolist()
