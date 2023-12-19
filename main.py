b1f19d98e4ada5555321ccf3f93126ea2e34762f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:23 +0000

    nothing within powerful me stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
