b88cbba8ed29129bac6934c67e25d11427b9910c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:21 +0000

    too trick way lay angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
