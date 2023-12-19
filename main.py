d14f4149a89cffe551cb1c34ee33d293c5f06e20
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:59:45 +0000

    shout hair runinto callback offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
