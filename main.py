6767046e45e6c1feb54362b7ca6c2a7b0e068866
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:15 +0000

    noone lost indeed cap out

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
