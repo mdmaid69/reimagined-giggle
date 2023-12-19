eafc27bec7bd58131eaecf834d60beada281adf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:54 +0000

    sport she another need culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
