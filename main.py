d80fab337d719a1e17f6a36436e5a9433f35d06f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:09 +0000

    holdon rescue obvious confusing warning

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def find_min(lst):
-        return min(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print([x**2 for x in range(10)])
