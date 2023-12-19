62d7088840cbd482e2a4192806511d3a4a174e74
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:21 +0000

    soon absolutely stick building fasten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
