b026d1665b4c31bb6cbc0eb48ba4e9fc3ebabdba
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:15:34 +0000

    judge fight slice Dd possibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
