90ab93a4c14f7e2e5316aff301390d7f1d173c7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:14:28 +0000

    brush phone harm unable disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
