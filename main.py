f5929b9becdbb10048e0437969ff934bc7af0b79
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:37 +0000

    shoulder string explain sort man

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import time
+def get_current_time():
+        return time.ctime()
