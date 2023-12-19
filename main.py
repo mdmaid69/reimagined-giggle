0ff9f9a3d4149abb74a1bdffde57f33fd0bf413e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:30 +0000

    Ww cut police takeout exit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
