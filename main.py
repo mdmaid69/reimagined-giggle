eda9077340724d7b4f1697917ad3e02f50416388
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:16:15 +0000

    protest compare will name stamp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
