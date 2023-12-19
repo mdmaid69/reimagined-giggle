7da78b89436e2864f1b294694ee56847f96e9c10
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:39 +0000

    mind stand correct file wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def cube_number(x):
+        return x**3
