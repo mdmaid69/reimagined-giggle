f470c02dcf0177fb5f8cca76664b06caebd92c08
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:23 +0000

    fasten pronounce command tour land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
