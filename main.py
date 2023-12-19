074e1e0f0fd1d6bbd5eb30c9421dda3dec7a208e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:07 +0000

    urgent evening hang inside report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
