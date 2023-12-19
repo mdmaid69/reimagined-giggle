b098c36b7610dd15b651d8870349cef849eece7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:43 +0000

    almost ever pause business drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_list(array):
-        return list(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+x = 10
+y = 20
+print("Sum:", x + y)
