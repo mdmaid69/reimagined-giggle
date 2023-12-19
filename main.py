12eea331fea828446b84b292fa02bd943756405e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:10 +0000

    mean fortunate characteristic really work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
