c8ade2b0ed10b8b48100efd1b0d253b5ad0a81f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:21 +0000

    flight mean second care keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
