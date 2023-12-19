8e4d0baf8e07f314a01a35c5c059e1b8f88a81c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:21 +0000

    spring taste hand right apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
