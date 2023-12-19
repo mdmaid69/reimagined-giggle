36a77cbceafaab423310b3019b1a17b3b079d7f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:19 +0000

    split better steep noisy food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
