6242c1efcf35ebf9a3573ab83b5c5b843dd23cf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:24 +0000

    dress lie apart communicate imagination

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_current_working_directory():
+        return os.getcwd()
