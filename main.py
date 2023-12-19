e4db8dc34b544b531592171706396554a7364455
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:44 +0000

    choose swallow concentrate minority power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_repr(array):
+        return repr(array)
