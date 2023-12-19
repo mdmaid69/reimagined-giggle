5c6ad002e388314c13f922ce85bb0993017b60e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:43:50 +0000

    know exhibition recent will here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
