bf204b1f00a5f069b880d2815b3001d95a951e3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:25 +0000

    congratulate proof process extraordinary invitation

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
-print(random.randint(0, 100))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
