3c08a833a75b2493d9636b1bb5531805b8ae226d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:09 +0000

    then impressed bus this concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
