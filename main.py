39168020398af4c4b135378036ab5636246306ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:25 +0000

    stop put exactly public dry

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
