b55bddbb61a485cb2ea00b799003649c5d742628
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:51:50 +0000

    flat long score town partner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
