c7f82fd1f21f3b6c90a8d84107f6808459ef3b21
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:44 +0000

    impressive quality keepout miss hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_index(array, item):
+        return array.index(item)
