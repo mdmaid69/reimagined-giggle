eaa117e4b9cf1f0b7e1e2fcdca7bb208fbeaffa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:02 +0000

    month tool mad address division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
