6a010c95d129ef544cab3d30109467071fba75ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:06 +0000

    clever as positive chin check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_volume(length, width, height):
+        return length * width * height
+  def remove_duplicates(lst):
+        return list(set(lst))
