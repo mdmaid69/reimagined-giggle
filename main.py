fb5489873e3f3dcd951b118b396fc9ac2d478dfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:00 +0000

    mend tonight reply genuine sentence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
