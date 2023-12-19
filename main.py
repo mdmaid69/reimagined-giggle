9e5fc44b31f264e02fcf464ac6fe3facb5f68e44
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:16 +0000

    still stay used unpleasant think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
