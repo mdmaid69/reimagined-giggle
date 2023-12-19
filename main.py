c59d6dde0807869434f9b48f03f59f9f09844e01
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:43 +0000

    circle uncertain forget address rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
