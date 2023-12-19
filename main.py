e0828d8affa7e9da19648f97076ea35a9519ec5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:00:22 +0000

    strange danger sweet computer emphasis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
