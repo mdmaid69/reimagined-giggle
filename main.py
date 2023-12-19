fb912bb322ed7e26d3fc9d2802c6f4a6fe5d250b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:13:53 +0000

    method dance wrap pump minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
