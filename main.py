30959a9d2f18475808fd67d78d36cbb6313cd149
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:10:35 +0000

    base bringdown must first tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
