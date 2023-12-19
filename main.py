706f626cfe88fa094a16e1048879a2a79e6d0f17
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:57 +0000

    crowded undo land head love

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
