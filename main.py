e9031d2fb2b77201fbd69f555a45d71b3b834d7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:22:11 +0000

    fork dry contact whether force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
