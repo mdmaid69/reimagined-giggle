222c12f43c80119f18d8c2b28247943ec2aefcfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:45 +0000

    comeup refuse rough against amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_speed(distance, time):
+        return distance / time
