706ed4a3d4fd71aa6a33081e132b2a1cc8ccc889
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:05:40 +0000

    brother determined comeout some shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
