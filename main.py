95ca234af1e3ffe1e5d96c2953495f4fcb93e207
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:56:10 +0000

    moon dull search better failure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(numbers):
-        return max(numbers)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
