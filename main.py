826d71cabe2641e717f3fd6b46cef8f611e994c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:35:16 +0000

    stop consist beside unhappy scare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
