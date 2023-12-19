0f6a3cd9c9b6e3b8bb95679a7eccc18d34ade825
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:42 +0000

    mine achievement once Dd round

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_bool(array):
+        return bool(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
