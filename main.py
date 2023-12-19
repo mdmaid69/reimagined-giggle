3edc46dc295f247be2447f82474ee22554d02a33
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:27:02 +0000

    patient mistake fork progress pair

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
