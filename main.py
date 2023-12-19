1ede892422905b00d758faf71239c9f91f3a8caa
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:24:53 +0000

    minority myself sing 571enough bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+def count_characters(sentence):
+        return len(sentence)
