ac86d3a6c7a4681661768ad9805e8b6e9328db77
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:28:45 +0000

    accommodation development all what something

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
+import collections
+def create_stack():
+        return collections.deque()
