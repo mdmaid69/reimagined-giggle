4d9a4af59610f9323cdd0ed69242e894f23dad91
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:07 +0000

    interview national exchange Dd nurse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(5):
-        print(i)
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
