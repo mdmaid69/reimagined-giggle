797c0295dfaa9bc6f35c89c587cbd5fd29b97a57
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:49 +0000

    survive early sensitive me quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
