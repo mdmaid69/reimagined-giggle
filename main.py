c53bdfd7ed34a8fca37c38c1ac31c4d4163f424d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:43 +0000

    glad welcome threaten stamp trip

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
