688f158c58b6296a9f79e5ba3cb8160ba7f82995
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:21 +0000

    favour chase female chin produce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  def add_numbers(x, y):
+        return x + y
