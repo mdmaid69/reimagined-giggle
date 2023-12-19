d8ebe2dd6d2a5452731f59fed2737f8770de3247
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:38 +0000

    hot this dress positive date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
