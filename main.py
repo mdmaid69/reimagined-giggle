55fe30cd435c16646c772771bc72b7eb38e0f94f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:26:19 +0000

    complain section when snow ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
