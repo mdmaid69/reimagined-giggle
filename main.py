fa18c58af3e46ef8940c1fa205a60016b8ae78fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:34 +0000

    proud fire 569enjoy violent ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
