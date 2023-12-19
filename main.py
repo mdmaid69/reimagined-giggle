57fbaf42adc84f3c24eae67774e1775694cfd87a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:25:32 +0000

    tired plant her along space

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
