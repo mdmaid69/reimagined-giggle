576036b135444f022c8f8ca37c5056f8a15857a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:04 +0000

    combination impossible laugh cinema use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
