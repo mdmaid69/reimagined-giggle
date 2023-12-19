6150d673db76050f965aab7734923168416a0e22
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:05:15 +0000

    touch operation hard complete taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def sort_list(lst):
+        return sorted(lst)
