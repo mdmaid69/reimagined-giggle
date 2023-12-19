fe3fe3772f79ad77c3f63045cb6d7f2891e016a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:23:14 +0000

    frightened neither sit usually heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
