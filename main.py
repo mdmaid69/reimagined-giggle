8e3cd2bc65f865ea68f4df55e03650e99dda6d44
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:26:52 +0000

    hide quantity way transport seat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
