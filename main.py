ba34f25271f266456e87f17f002da23821cd3654
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:17 +0000

    note decision getup distance block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
