e10d52fa4c350d51dca5f3e2631f6ac524cdb7f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:22:22 +0000

    forgive weekend deliver shade pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
