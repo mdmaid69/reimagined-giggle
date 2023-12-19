f0ef5cff6ce58efeab18d174724c61162317bbaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:51 +0000

    phone rub confusing one tool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import numpy as np
+print(np.array([1, 2, 3]))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
