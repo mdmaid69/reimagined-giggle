4f4780c514ff75703452dfad249560b5b0d7daee
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:59 +0000

    shallow interview boil hard absolutely

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def reverse_array(array):
+        array.reverse()
