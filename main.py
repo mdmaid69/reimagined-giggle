93a576257a91f5e618501b45597050d354908f5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:51 +0000

    train Mm translate tax deep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_binary(n):
-        return bin(n)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def cube_number(x):
+        return x**3
