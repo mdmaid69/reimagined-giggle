fe678d63f57537e765ba046beb9da1122c3b7dd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:35:48 +0000

    stranger pleased correct once swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
