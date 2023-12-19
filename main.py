ee0eaa6ccc96b274e386c94c078f81f97c8a32a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:53 +0000

    floor market over lock correct

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def cube_number(x):
-        return x**3
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
