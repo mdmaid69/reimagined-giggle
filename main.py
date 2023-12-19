4db61ada51058c768825fdde47209cf4dfe9b852
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:38:31 +0000

    Dd flavour depend along Zz

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_unique_words(sentence):
+        return set(sentence.split())
