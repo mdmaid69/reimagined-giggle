5c9dc99d1875546ee94ced73f630d5cdc0ef6bb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:48 +0000

    fire indeed measure so getoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
