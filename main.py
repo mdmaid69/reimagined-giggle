44bc6f979dc5b748b758dee5bca29eb955c9bbc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:42 +0000

    interview little shelter cheap keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
