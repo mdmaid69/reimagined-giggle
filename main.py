924cf75dfadf8042688f173c1dfc8c07c6411541
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:00:00 +0000

    love neat dinner comeoff brown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
