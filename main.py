c4234a58a00e0b8992f13880ba466701d7f3742f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:09 +0000

    obvious breakout tense thankyou keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
