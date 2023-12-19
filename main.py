1aaec81389b107102a9985ebd2b599e3ae1e364c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:04 +0000

    control discuss education circumstance theirs

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
