e64838f9ebe8b6b97b8e92c888a2aff983578a5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:15:00 +0000

    nigga impressive home disgusting police

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_int(array):
+        return int(array[0])
