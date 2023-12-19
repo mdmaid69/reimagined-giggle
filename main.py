543a3e3848e0c57e6bab1807f4657cec0648dd5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:49 +0000

    clear trade quality twist bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def create_user_string():
-        return collections.UserString()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
