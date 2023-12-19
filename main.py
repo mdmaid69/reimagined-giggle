aff42e55f1f26ea7638b4cacaf1dec0348e765a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:19 +0000

    give fault sticky need summer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import numpy as np
+print(np.array([1, 2, 3]))
