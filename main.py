93ef11cbb287e21daab5ad6a9b935f3736fb81e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:10 +0000

    fire familiar giveaway explanation snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
