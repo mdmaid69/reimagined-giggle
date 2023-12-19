0ba92fac5dfd2c3153d96cf1d450c3ae3dc6bf9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:55 +0000

    smell aim tax run truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
