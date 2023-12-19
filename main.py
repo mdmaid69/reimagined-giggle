73daa4bd5016b772cb43ec14754f5186e4821991
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:30 +0000

    something church standard turnoff list

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
