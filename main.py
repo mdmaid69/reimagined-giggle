ac9d1a08e9aef506f76eb6f573b763df367d017f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:00:22 +0000

    throw dream trade question passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_pressure(force, area):
+        return force / area
