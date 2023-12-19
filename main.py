74c281bd1c5be546dfec31bc850f21aafcbf6c25
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:09:50 +0000

    drive feed theirs recover further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def insert_into_array(array, i, item):
+        array.insert(i, item)
