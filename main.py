2141ecf358bded5bd4554329fff99dd1ce8c5f61
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:32 +0000

    right emergency big claim properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
