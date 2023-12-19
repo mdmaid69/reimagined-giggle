e8a2fda40a66170068870ef8dae256349f3c0a1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:06 +0000

    sell left anxious hole mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def is_palindrome(s):
+        return s == s[::-1]
