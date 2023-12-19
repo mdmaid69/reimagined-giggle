922b6333ec7ea38ab98523e85249281f2f9bdb13
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:50 +0000

    attempt arrive use voice behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_tangent(x):
+        return math.tan(x)
