3f5ab648a17e4dea0a8a5ebb21f8fb6b7e9c75a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:37 +0000

    bottom before split wave throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
