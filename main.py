53520d0fe7152af855b07f5dee1994fbd50c3e99
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:07 +0000

    roof hurt neither day about

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
