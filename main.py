c96fd771a2a3a27ccbc75c21350ca9bf84e12f86
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:56 +0000

    present stir since number my

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
