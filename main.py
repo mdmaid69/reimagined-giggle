dd9474a632a81e464128bdbd7e59485d8b044795
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:50:00 +0000

    uniform characteristic fashion more mother

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
