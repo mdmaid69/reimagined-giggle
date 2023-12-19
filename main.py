94b8f7a4f85a345a740fa84f06a0b3ff1ae899b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:07 +0000

    opinion safety period interrupt solve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
