1a596f1fa1fc0f88d36e30a5aff8ec3233eee65c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:20:44 +0000

    treat embarrassed somehow downstairs eye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
