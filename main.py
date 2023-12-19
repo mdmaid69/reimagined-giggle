22c4f32c1bf92d909704c6b02d963f6704fb883e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:04:46 +0000

    under art history pipe warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
