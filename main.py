f3cb8a8897176c401aa8eb8a49641be84c7b1bcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:29:32 +0000

    mine programme scare die sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
