54a9fc47528368a4c598f1f7a5de043e850ff43c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:34:35 +0000

    conversation good jump game himself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import array
+def get_list_from_array(array):
+        return array.tolist()
