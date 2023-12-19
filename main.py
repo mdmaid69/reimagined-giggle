01df9f9abc3adc587a4fa591c4adc9cba8fbf665
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:03 +0000

    suckme through safe creature call

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
