8533daf9241b214d715d9f48667449fc3c0350fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:21:47 +0000

    use branch nurse container disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import sys
+  def get_python_version():
+        return sys.version
