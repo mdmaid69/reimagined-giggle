3b873494e8f1c588e606f0f1ea4647f7bdb4af6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:08:28 +0000

    callout wrong exit part furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-from collections import Counter
-print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
