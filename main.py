c740145b0b3dbe7343bd3b1725f85e8a57fb5104
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:37:20 +0000

    production hear bang extremely neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_int(array):
+        return int(array[0])
+import tensorflow as tf
+print(tf.__version__)
