f776229d4b7f4d688227903fd06a617b97cc5d88
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:50 +0000

    family spot row Dd lie

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-print(sys.version)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
