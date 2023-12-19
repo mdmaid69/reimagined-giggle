28419ed1884038854b2ff1cf7f247d063e168d46
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:24:05 +0000

    rescue float broad why find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
