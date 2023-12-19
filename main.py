42ddb93831a88caf8d9d607f53877369a2f7befd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:27 +0000

    build lay chin partner tradition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
