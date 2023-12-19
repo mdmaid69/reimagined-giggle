c70baa0f892c80b7e95ceb4354c09fd113a0c2de
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:47 +0000

    behave oil familiar thanks concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
