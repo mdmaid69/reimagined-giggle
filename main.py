9a87db4bd0a395960b3ffc481906a13476422905
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:43:10 +0000

    listenfor side write fault train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
