d23a1e0888cf306b885fb1bfe76717b9316b01f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:14 +0000

    strike frightened consist load connection

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
