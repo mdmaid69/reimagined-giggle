ce3c36064949e955df8c71d9eef818b1e007b890
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:45 +0000

    proud tell speak related narrow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
