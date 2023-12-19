f77c36181b5c9ddf42a734c07fbd614a7218ed0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:48 +0000

    bear minute but open convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
