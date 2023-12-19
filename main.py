d58922b5d910f620fb9745661620b8c0d80dc2c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:01 +0000

    pinch pile Tt service limit

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
