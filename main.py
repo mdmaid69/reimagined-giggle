d436d378287db0cb69ff9cc03e17576747cd5727
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:47 +0000

    tax bringdown help set listenfor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
