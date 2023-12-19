16dd8fe091b71c4dbde5fec02155231ee58103e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:07 +0000

    lower frightened partner kick each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
