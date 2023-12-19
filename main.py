2259f5b204bfa0476c566840f2fbc2f535ac3474
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:07:48 +0000

    drawing twist trade ask country

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
