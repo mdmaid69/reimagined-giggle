dd3d7dbb249d90322ac7cdee9df1daf08b9a0fa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:10:48 +0000

    satisfactory spot common equipment strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
