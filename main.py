49c062b8d3e418a108abf61792932179f2790131
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:24 +0000

    reduction pollution embarrassing freedom bad

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_work(force, distance):
-        return force * distance
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
