84fde3b67f0ad27e5d60aab31bf03089b494c10b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:49:34 +0000

    label delicate undo offer direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
