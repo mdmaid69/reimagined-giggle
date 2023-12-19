4c56a0dfa3b02a452ea491040ccd6541c6b4866b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:53 +0000

    behind way hurt where pronounce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
