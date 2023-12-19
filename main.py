c275f010f4c9489cedaa0840f966e8f715db4d12
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:17 +0000

    convinced unfortunate guess pronounce capital

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
