5a478d5e98194e33f029669708206a63f31d536b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:47:12 +0000

    back used tune quarter again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
