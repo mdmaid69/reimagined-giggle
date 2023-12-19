b80f7010a64e9518d9a8807a6c6aca7fb6f05a62
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:56:55 +0000

    bite write dig almost care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
