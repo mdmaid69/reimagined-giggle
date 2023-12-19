857292b606ec305fadf36b0cb58f2c2ba77ae793
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:51 +0000

    support book ticket giveaway computer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-print(time.time())
+import sys
+def exit_program():
+        sys.exit()
+  def convert_to_hex(n):
+        return hex(n)
