c1749110642fab3e8e84cc8f64613549a2d0c905
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:59 +0000

    acceptable club friend lookafter usual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
