9dda73a283f074086d8be63d699a1089e4d4500a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:33:22 +0000

    yours gradually except exaggerate rest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
