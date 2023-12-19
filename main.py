97953daad031f3c246472679bf86fc00f75548b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:32 +0000

    bored model instrument disgusting thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
