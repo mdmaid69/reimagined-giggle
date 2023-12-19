5fbc3223be5fa90f7aecf7cd2f5a51967d3a8ad9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:29 +0000

    tour secret container tape common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def is_odd(n):
+        return n % 2 != 0
+import os
+def change_working_directory(path):
+        os.chdir(path)
