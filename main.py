a229d07fde75882b7c1c096ec3709483d397835d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:18 +0000

    giveup set right approval case

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
