cafcacb68c16b8b1e3b2358201f15afe04f8b216
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:57 +0000

    return arrival steady space add

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_hex(n):
+        return hex(n)
