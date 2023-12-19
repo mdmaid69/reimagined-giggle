2ce196d6dcfbb2470ede2b0bacba3b4bbb377d30
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:03 +0000

    practise top mail reaction capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
