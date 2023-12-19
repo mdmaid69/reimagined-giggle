3c69d0cdcbd189a981d5fc318ad458e67b89bf5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:02 +0000

    hold exchange quick home interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+print(os.getcwd())
