6f56cc97fed8b5784a7c3bd238161ffb7fd65c7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:34:48 +0000

    payment risk back become organization

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def divide_numbers(x, y):
-        return x / y
-import random
-print(random.randint(0, 100))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
