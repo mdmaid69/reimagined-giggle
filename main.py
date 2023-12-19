a934d80a6077cf77c6c802a6f8ba5044cee8952e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:24:43 +0000

    admire change necessary which cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
