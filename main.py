8244a5f05244ac78f2cfe72bc7398990d260ca25
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:51 +0000

    exam Bb sport imagine tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
