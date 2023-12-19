a4d2803777b5170ef52cb0f4016f9fda1bdf228b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:15:42 +0000

    yours somebody comment sometimes angry

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def count_words(sentence):
-        return len(sentence.split())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
