68ac40d7bfa36225bd06a7e16f98e5506148f193
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:39 +0000

    head food so flow fail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
