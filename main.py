6396a9ab8053ab419738d9f8160931e44da021ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:36 +0000

    along hate prevent ready comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
