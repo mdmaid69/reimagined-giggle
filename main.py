bfd61c63a1e878794cb37dccc8783425d436e497
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:52 +0000

    mean whisper except city high

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
