38529a093e32507d757985933ac0a58099734768
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:57 +0000

    rely store think connected burn

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
+print(sum(range(10)))
