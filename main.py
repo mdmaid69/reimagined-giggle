faadfa348eb1842a235cc3969a064cb0e4600109
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:45 +0000

    hear field complaint price majority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
