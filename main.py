5d35c352a650ed8081b35bc5f3c3a92ce7a87e26
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:43:06 +0000

    such forgive stop tie young

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
