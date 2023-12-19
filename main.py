78b8438a806d7419b9302c0e8e772ebd77ba09a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:45 +0000

    poor order goodbye original instruction

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import time
-def get_current_time():
-        return time.time()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
