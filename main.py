ed50daa698de3514b08c88aaea21391534701c6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:32 +0000

    actually takeout underneath taste heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
