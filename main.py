f5e08eece14bc4b70e58c0c8ea60a5275f9b8b63
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:22:12 +0000

    fit indeed opinion sound old

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-for i in range(10): print(i)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def get_array_typecode(array):
+        return array.typecode
