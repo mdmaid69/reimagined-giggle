31fc989a9e8e4e134cc1d68d4a21596f6c743d78
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:43:08 +0000

    neat runover unless makeinto expensive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+from collections import Counter
+print(Counter("hello world"))
