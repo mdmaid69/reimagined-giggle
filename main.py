e1b77b07aef7ea6eb9e55184cab48b3458d035f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:46:16 +0000

    music quarter produce check lunch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import time
+print(time.time())
