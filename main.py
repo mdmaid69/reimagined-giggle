43f8e8075818024eaa125ef0fb56d618714f7b9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:49 +0000

    matter means equipment slice swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
