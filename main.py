90221e762f4a0cdd75f100f30db1dbd3e7a2bb23
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:15 +0000

    pollution promise temperature unfair of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
