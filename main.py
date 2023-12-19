2d603d993dd2ff3a8bd4ce4e66dae8d915971163
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:37 +0000

    wet broad Ee think age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
