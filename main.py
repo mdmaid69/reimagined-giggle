f95effbed314de387a47d23362d4b080e9a3d6f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:22:43 +0000

    direct admire whenever nature back

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
