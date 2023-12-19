aa10c06e071303e2832862e7b1f4f4b7dad2ffb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:00:54 +0000

    strange pronunciation fine reasonable show

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
