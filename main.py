d4d5d3f2cca33ce4e88d8f2e88dab64a6899cfe3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:36 +0000

    stage complete poor smart react

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def sort_numbers(numbers):
+        return sorted(numbers)
