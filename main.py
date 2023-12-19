6aae928f85285f75bf9bf2ca8d957c52c845cd99
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:34:36 +0000

    cook air opposite allow hardly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
