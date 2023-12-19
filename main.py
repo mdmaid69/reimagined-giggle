1af38c55900144a2f1f2c6f59d98e9de6278347a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:26:54 +0000

    building share secret scale pleased

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
