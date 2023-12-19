607456ecc7a872dea521d79e8dec093a21d34476
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:07:44 +0000

    fun sharp apart community round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import datetime
+def get_current_date():
+        return datetime.date.today()
