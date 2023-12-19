fac248d453c1a9ce65986b7adc958dcde64ad06e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:57:07 +0000

    watch temperature people special stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+from collections import Counter
+print(Counter("hello world"))
