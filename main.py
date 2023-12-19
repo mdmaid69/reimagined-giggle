e3275190ea5e9aefd46399497d1bb72e701d6730
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:22 +0000

    among move nurse path seat

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
