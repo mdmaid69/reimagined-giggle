bb5dbc213a371bd6e47e5177e6e89e50cfee8389
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:27:07 +0000

    summer later sound gift workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def is_even(n):
+        return n % 2 == 0
