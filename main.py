f2965624f062f0a46b245e141d669632cefcc7a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:40 +0000

    brown importance pronunciation sign splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
