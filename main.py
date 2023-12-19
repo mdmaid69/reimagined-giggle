a69facec29ece2ca40b34d73a5527a8cfa70420f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:37:45 +0000

    pass performance false argue date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
