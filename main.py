e7989e90e7e2609e9a755eda63f7a37ab6692d1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:56 +0000

    outside destroy region prepare broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
