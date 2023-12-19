49df5e9e5d85f2d2daf1a99b5884671e6cd19e42
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:51 +0000

    favour acceptable nonsense violence next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_python_version():
+        return platform.python_version()
