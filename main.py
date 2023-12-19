b5a17a1e08469d201cda94fc29996dffca314d0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:01 +0000

    level plenty straight polite calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
