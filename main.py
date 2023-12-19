28705824b2591cd0245ca1ba08463980bc056b06
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:00 +0000

    turnoff usually window change direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import math
-print(math.pi)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
