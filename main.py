bfbbe30b83859faddd2c977746b767d78666491b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:55 +0000

    loan please kick refuse read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
