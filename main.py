429c8e0e887c71350f39469904998071085908ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:46 +0000

    generous protest callup rate impression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
