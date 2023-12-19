3afc71b40d537fe4cb6fe28a11db1d6d78a7f34f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:32 +0000

    fierce thing turnon present urgent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
