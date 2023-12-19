e624e827552db7cffa983ff429642d1e9a40cf40
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:03:13 +0000

    deliver trap century begin impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def is_odd(n):
+        return n % 2 != 0
