34685833d8eb64c5c7bd95a03fa0a6dd39c08c28
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:43:57 +0000

    disappointed we defend pinch clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
