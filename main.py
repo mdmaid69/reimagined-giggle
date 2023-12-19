b6a0af9e72737e81a7cc99a37928b9017431dc1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:04 +0000

    hold neck breakdown test equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
