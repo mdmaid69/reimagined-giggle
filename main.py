f02e5039f668c7af79b69101bf694dbb54e6b31d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:35 +0000

    problem pinch strange store backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
