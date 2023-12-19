2b5f406f1bf30fc4a56eccbbde0aa6131f9403b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:44 +0000

    there treatment inside disturbing pass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
