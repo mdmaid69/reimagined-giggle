7b3bac3317e9953f7437d184556f8da7baa306d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:38 +0000

    likely obvious mix begin yourself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
