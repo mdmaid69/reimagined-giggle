f2897de9b42ee7059a32d075b08b8c9160a73106
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:12:50 +0000

    congratulations outside crime slight organization

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
