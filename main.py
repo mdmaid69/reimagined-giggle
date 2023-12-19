99bad0972dad26e673062b41581779bf18771b1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:07 +0000

    round opponent race trick sensitive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
