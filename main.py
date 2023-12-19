5c8c186bad43e727a5aba435d45ab7e00b73ff56
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:04 +0000

    month manner bang sky breakup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
