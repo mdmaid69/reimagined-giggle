46013b1c286fb9d99298c99b21d3ea734508f00e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:34:01 +0000

    issue smoke Qq harm write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
