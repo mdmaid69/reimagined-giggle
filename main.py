7278d6ef0ebf11b3947184687a9e39ee3ccf21be
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:49:40 +0000

    probably production impressive welcome doubt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
