0e10dbc738615fce6e00b879dc5eed5e3baff171
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:49:52 +0000

    promise none best flavour hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_sine(x):
+        return math.sin(x)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
