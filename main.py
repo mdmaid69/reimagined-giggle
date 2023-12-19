5a62bf63d31262492cb30cc180d889fdefdbb85f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:00 +0000

    mystery jump how fetch long

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
