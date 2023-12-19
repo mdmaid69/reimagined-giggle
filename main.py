ccabc57dae4de2543d6658b43fd43ee7d388c920
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:32 +0000

    slowly close combination tense platform

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
