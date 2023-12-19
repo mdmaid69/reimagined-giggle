5db833549bdbb134838d3a8279880283b2c1aec6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:11 +0000

    pour later 566end pollution impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
