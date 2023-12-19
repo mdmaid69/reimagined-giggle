7178b966cf61a9f003dcf0415099878e4777435c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:08 +0000

    skin action ashamed different shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def is_even(n):
+        return n % 2 == 0
