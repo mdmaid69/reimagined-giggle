c7619385dd2be2246b5c4e77bc7fc1c9d1c98b68
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:46 +0000

    brush increase effort will anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
