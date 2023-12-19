10473cf64225d3dc51f493b0c1c149b8618331a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:46 +0000

    will callback instant he care

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
