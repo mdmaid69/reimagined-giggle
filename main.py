1b5f1bdcb9577b0b2221b6190c1d5a8c0d40a398
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:52 +0000

    cold forever laugh matter better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
