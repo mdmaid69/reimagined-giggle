3a209b9a90976421823061441dd4fc2ab81d3da9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:39 +0000

    custom callup instrument warn up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
