3e32eaa7c6ecb95b7d785be24a2c3f4628dbe8f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:24 +0000

    ourselves hardly want middle about

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-print("Hello, world!")
+def reverse_string(s):
+        return s[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
