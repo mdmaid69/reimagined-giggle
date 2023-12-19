6fa6cf92e23e594ff61d43e88e10bb5a207ec18d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:07 +0000

    expression hit hard recently at

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
