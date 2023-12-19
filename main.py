c2a5daf4587e265e0ac5c1080709143184bf69b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:53:37 +0000

    beat find stage bin Qq

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
