f7fdfb756c6829d39e7ab3ef67cba9e7ce42d3f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:15:30 +0000

    cause entrance confused comeout ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
