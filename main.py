f4288c75d5f4c311306dba2f17b1cbe0adc435e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:46 +0000

    spend thinkthrough prepare share plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def convert_to_hex(n):
+        return hex(n)
+import getpass
+def get_username():
+        return getpass.getuser()
