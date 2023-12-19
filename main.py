aa10383badd49aa2964450eb13105bd0aae4c884
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:14 +0000

    contents pencil leg takeaway friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_bool(array):
+        return bool(array)
