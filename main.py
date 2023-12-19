74c6d06688689ddce6d1a1c0838b1a64dccc1e44
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:48:19 +0000

    rich disturb convince instrument uncertain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
