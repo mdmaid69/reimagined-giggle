bbd49a06556f50eb70d9e503f96da13098904e5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:59 +0000

    glad case create wrap tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
