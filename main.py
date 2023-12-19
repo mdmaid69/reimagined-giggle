faf90f7839ba9a598581b9d3a42b18d3db5bbb10
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:43 +0000

    present deal high able date

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
