4a25b3a1796152c3806127fd3121a19921bf9add
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:47:28 +0000

    bringback cost 571enough grow traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
