4318d665a8b647f26f9b70429fa1d1e1b77453e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:49:31 +0000

    anyone suffer prefer steady earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
