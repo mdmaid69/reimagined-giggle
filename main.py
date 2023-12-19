d1c463f7b61e14be4e2c2aea84a4ef798f4491b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:41 +0000

    fear frame joke needle fine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_as_list(array):
+        return list(array)
