69906ac4f673a7fb8672c8e622163f6ddb287428
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:24:03 +0000

    possession practice image normal most

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def convert_to_octal(n):
-        return oct(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import json
+print(json.dumps({"name": "John", "age": 30}))
