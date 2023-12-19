6f18db32d59bfbb406396e49c923deaa63eef0a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:09:36 +0000

    mess day abroad news impossible

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
