45f26d28a6d84d66500206f84dc117b632e599a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:45 +0000

    569enjoy form splash somewhere dull

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
