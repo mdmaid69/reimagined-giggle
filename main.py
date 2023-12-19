74fadcbf32a6ad32dcf3ff866b1b64f12855c6dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:20:30 +0000

    discover service past neck centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
