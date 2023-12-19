526f217aa05165dcf6d436b3f5abaa4a12e958a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:35 +0000

    wonder dry mind back lost

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
