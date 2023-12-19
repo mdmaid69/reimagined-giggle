a49bea52a8ecef341e8ca4fa42349bc82c3502a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:38:17 +0000

    operation while bye flow yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def count_elements(lst):
+        return len(lst)
