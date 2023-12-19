278a14c31355aa3cf11d652993a8f00d78641466
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:25 +0000

    usually service concentrate actually copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
