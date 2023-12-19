0a879ba057563c70b95d09662c6acbfcca3ad3be
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:07 +0000

    lesson level tradition protest kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
