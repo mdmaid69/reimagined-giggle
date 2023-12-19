b92d4f209b694cad0eb52d604c613a5b07d7809a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:23 +0000

    none will progress similar during

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import os
-print(os.getcwd())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
