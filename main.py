612d9b117403dea8d8e58a95639487ca2f34e07e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:19 +0000

    reduction quarter brief fortunately pleasure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import numpy as np
-print(np.array([1, 2, 3]))
+def get_array_typecode(array):
+        return array.typecode
