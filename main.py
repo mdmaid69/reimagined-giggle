c61c416c96e7d82d967421847bcb1dcc26ec7916
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:20 +0000

    combination lookafter hurry deep cheat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
