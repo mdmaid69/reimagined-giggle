8993455deece5e3328d129b101f81f1f7776c99a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:10 +0000

    fill ball government body godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+  def convert_to_octal(n):
+        return oct(n)
