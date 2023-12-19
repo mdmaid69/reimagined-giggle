045d9d5d26e9dfaed0c5804b55c39f55b2cf3361
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:12 +0000

    on giveaway agreement worried break

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
