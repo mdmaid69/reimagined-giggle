7e4e5f0c2a7d061ae6fc30b6ffe80fe8dca2bbe8
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:51 +0000

    cost hope trip continue wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
