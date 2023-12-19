2b0973f72830c609f844f1baddb3accce085c158
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:39 +0000

    floor matter petrol say branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
