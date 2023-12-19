f0a6275a7a287997436179cc578b7ab906e9f052
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:23 +0000

    pretty dry getup error block

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
