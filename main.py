58a49fd9294ae6d0a741f7c7b51a9d01d34652e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:50 +0000

    shake relief explain murder cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
