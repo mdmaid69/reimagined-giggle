30922afe8993c1f42e4699f8fcc371e187447895
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:39:41 +0000

    still sweet basis floor actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
