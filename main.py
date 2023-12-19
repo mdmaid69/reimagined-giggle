39f1f945dda75a6636c6a9af7f3fdf0e23c531b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:12 +0000

    way pretty nice meet female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
