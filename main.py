c0ba72e18201a55a4724c55588654aa136a22f1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:52 +0000

    emphasis beautiful takeout congratulations rush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
