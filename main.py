fd344274a484623c96235532dba0794e42a6521b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:47 +0000

    benefit safe present sail reduction

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
           if n % i == 0:
         return False
         return True
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
