bcdf41e35d7446083352b34a0a3f9e8a1f3a14f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:29 +0000

    even nice claim pressure meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
