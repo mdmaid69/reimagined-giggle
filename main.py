334e8bde0c00c593b4b25278a368643f66ea8091
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:05:15 +0000

    slide flat boat pause workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
