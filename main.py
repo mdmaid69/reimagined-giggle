dd41e0b9bbed95bc602db48439821a2003ea600b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:15:09 +0000

    imagine such turninto win closely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
