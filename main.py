f45adb3824aaba274c7b662294704ca53bfbac0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:29 +0000

    spill bother giveup serve gift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_list_from_array(array):
+        return array.tolist()
