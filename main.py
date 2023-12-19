c3eccfe59ecc43e77189e3c572edd4c595104c31
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:37 +0000

    store it stamp later joke

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def convert_to_hex(n):
+        return hex(n)
