c1720579549be626ac4474a3851f957ff52d7949
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:35 +0000

    contents oil legal food affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
