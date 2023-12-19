824605e860c42513cfe6ff4dfe713024a1b6cac9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:01 +0000

    rare top hit absolutely variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
