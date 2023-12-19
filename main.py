813ea194345dfab03e0ea1b971783a6c0f68a2b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:42 +0000

    turnout distance star light awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def clear_array(array):
+        array *= 0
