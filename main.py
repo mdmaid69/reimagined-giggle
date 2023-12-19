270f3c836c5a7630e9c25f524bae07ebd7151617
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:48 +0000

    bus embarrassing sound print mark

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
