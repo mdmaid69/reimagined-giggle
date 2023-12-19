3b367043f76641adfc13b9868e0a9d01e1b435dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:19 +0000

    arrive negative damage comeoff bet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
