f3de234ecdd23432481b5df001370e090b7363e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:22 +0000

    increase care receive story in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
