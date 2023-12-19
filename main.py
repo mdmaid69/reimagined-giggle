21583936133744ac15f3500b0279d14a51358449
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:35 +0000

    lend benefit guard wet upset

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
