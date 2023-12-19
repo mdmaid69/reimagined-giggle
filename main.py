e581afc1a35bf5803883ca0490f589ad4626eec9
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:37 +0000

    sleep operate original report drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
