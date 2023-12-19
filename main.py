08b06e2aeb3314eb1e6f4491f6f09e74faa8ae01
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:27:00 +0000

    your murder spill them brave

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def is_even(n):
-        return n % 2 == 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
