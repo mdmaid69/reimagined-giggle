ffb5abc392b21870a7f5628db837053164148970
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:20 +0000

    callat zero then error explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_float(array):
+        return float(array[0])
