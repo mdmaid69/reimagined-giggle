c16c54934dd19aa1c64193ccf3db9fe935626c8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:29:48 +0000

    plastic turnback mine needle wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
