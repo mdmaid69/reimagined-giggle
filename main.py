eb625aeac7d3fdb0e1f81e8c14c17b2242240aac
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:45 +0000

    sale say priority against cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
