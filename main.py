ad3b9e307e1dae9cae9a45da3c1c59a09f9a4016
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:14 +0000

    almost still wind heart I

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
