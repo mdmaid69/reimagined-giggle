f2bc0099c4a03939dfb79bd061cbcbe9c0f7dc2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:48 +0000

    comeover eye trade callout piece

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def change_working_directory(path):
+        os.chdir(path)
