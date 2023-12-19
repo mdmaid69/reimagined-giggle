3d0986bf5c0a9a5f7774b9259b6ac140fc0e4980
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:34 +0000

    plant arrival bread between sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_repr(array):
+        return repr(array)
