d357d8332a368c3041575807108fb9b502012eae
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:15:23 +0000

    lead welcome accidentally consist mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
