d518202e000b7a4012f59c0f5b62fd0c0a17d6fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:23:51 +0000

    drop hope mother empty fall

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
