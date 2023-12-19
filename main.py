cbb756c138272da677859ac3d1948fa9a9a4b733
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:23 +0000

    this whistle before miss window

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
