ada145ef26ca2e93233d98d3bbce58c2887eeb27
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:57 +0000

    trust completely stop birthday aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
