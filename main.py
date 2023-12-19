ed67012f20b8a81f4e476d39b93d948decd44ba4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:20:05 +0000

    like apparently relationship finger pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
