cbbd597e2900555bd9e725b734b4ee23946f3d8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:45:29 +0000

    used oil level keepup bring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
