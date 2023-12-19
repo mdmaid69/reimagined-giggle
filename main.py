f96c82dcf6d5557eb1837f23f66f0df74770166a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:43 +0000

    back settle shout callat happy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def find_max(numbers):
+        return max(numbers)
