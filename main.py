c4d5ef5966b8ebb9d6451fe1efb01e45af88a426
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:16:04 +0000

    comeback thanks unfair move taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
