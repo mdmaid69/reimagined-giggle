e343eab6a868594d7d35e8afdbf6a3b3ac869807
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:55 +0000

    expensive may anyone some month

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
