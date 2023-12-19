dc2c7cd66adc6721d35e4f6303afd37b2f30c133
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:41 +0000

    winter gradually well hope sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
