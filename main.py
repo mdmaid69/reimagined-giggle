12af259534d5f2febf8dcf781f185095c05dbd4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:54:24 +0000

    instant wish lock boil protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
