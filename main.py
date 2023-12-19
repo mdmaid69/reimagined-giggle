40a52138d1dc7d16790f1dd098392d25e22f350b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:25:24 +0000

    raise too opponent country excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def add_numbers(a, b):
+        return a + b
+import array
+def reverse_array(array):
+        array.reverse()
