a51c9c89e497364117d2990abd5a4d003fa8ceaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:42 +0000

    turnover impossible ahead dry congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
