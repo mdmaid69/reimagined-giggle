d4a3966813a5e6760ee17b5783cdc66281a5f7b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:16:50 +0000

    television night total beyond improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
