896558a4edede9072e66588d4602792f5369042b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:41:50 +0000

    available kind round accident direction

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import random
-print(random.randint(0, 100))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def convert_to_octal(n):
+        return oct(n)
