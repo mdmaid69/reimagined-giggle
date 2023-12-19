8d95be4ea4d5b0d6ad07f342d50c733fbeb6cb68
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:54:16 +0000

    closely safety likely cover slice

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_current_time():
+        return time.time()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
