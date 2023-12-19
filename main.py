e6f589dd16ad3fe5c3ff7edc3dbaa2ff25b42607
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:26:38 +0000

    activity lookup 564encourage wide into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
