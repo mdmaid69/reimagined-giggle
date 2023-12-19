fecd2ec5e38094fd066e2c037483cdb34ce46e80
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:44 +0000

    reduction perform them declaration agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
