f9436d94ed978c8bbf72b52b5d1f14096ff07b49
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:38:03 +0000

    trap birthday throw party rise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import random
+def roll_die():
+        return random.randint(1, 6)
