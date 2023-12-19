3fac21da1992750579c4ba8b55cfb77b3ccb855d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:32 +0000

    duty decision little welcome pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def divide_numbers(x, y):
-        return x / y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
