43eb8cfec1a739365649c2e6c202a0be33cafb78
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:51 +0000

    back thorough form carry shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
