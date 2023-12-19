f96e912eb9d7dba8b2c24c0ac07dacf78333c9b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:37 +0000

    study telephone delicate trial smell

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def square_number(x):
+        return x**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
