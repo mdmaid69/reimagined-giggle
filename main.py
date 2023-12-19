4d73ad5aa5c71d481b0e53cc883b356eab3c3a83
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:44 +0000

    after begin minimum left separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
