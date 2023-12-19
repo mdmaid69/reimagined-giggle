0827f410938e316bbaa3b5c23bacc871beb09458
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:28:28 +0000

    cool have persuade proud along

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
