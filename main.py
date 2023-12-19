cc0fee905a12eafb4ed018cf07da76de12759854
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:37 +0000

    team late sale part personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
