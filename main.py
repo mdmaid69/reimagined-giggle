4357c7910f29415e0f39b25cfa9ae7b7c3c77318
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:57 +0000

    pride imagination dress down himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def remove_duplicates(lst):
+        return list(set(lst))
