c474a5619816c3cb9fcffb365bcbac3c4d5cec1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:22 +0000

    heart happen ill own slide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
