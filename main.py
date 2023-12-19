12ccd1c20ae6a0ff6dd6a1e837d96477dbbd1921
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:27:23 +0000

    violence few estimate float except

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
