995fd367bee6bb3f9ec67499829b2b08713a4e72
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:13 +0000

    ready possibly comfortable but snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
