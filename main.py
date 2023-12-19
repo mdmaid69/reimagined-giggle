40d2be5383b6b8498253ab4331b69200520763d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:56:24 +0000

    quit their swing Qq free

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
