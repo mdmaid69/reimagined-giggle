76936aaa6b1c500c4e4a0152a5638e8ddea7ff69
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:57:26 +0000

    information bear divide risk weather

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
