b668d990c16247064442cd9bef554ac659117089
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:41 +0000

    war warm honest go understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
