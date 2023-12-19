e8454056f79c74fe9bc3771c31d0d64d6e7bbaa8
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:40:35 +0000

    Ss action keepup appeal risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
