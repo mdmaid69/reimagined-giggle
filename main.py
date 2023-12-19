970379b5bf398848d64660a181834b955fdeb5c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:53:46 +0000

    reaction sport main ahead argument

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def is_palindrome(s):
+        return s == s[::-1]
+  def reverse_list(lst):
+        return lst[::-1]
