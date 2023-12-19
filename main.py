d1a3aef66ec8256685797dfe471ce37d196ebf2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:34:25 +0000

    angry always several lower dick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
