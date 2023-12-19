fd5a57c4e5f2f01ef41c13db29cac68c1a8a2e99
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:11 +0000

    abroad short unless talkinto suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
