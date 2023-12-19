ef58ec92f02666134ac6a6ca244878adb5ded93b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:37 +0000

    section pleasure fetch familiar dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
