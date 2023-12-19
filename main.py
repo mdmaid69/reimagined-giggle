ef58ba241afcdb6f47b442e4739511dc01ce352d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:25:59 +0000

    vision stand surface print birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import getpass
+def get_username():
+        return getpass.getuser()
