e53cc3c4c5c2489ab6afe273b336a627112019e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:20 +0000

    ourselves miss human takeoff anybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
