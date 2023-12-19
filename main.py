a19b7d8d92f88dce1fc69c6097665ed9f8bcec32
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:12 +0000

    burst earn 565end keepout contact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def cube_number(x):
+        return x**3
