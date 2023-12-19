e2c492c395f1a5e8eab5215c727f897fcee80964
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:21 +0000

    spring while unless risk uponon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
