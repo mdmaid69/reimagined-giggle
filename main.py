3e6d23b083d3d62912391b60db3ddac8a30bdc93
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:35 +0000

    fair use operate software human

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def add_numbers(x, y):
+        return x + y
