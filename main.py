fb36723a7ed975edd99acf63691f09cab6783741
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:16:10 +0000

    gate talk bad tongue more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
