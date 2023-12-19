db49d09439317f8c53dde5cc9ffd781ab5a06284
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:24:25 +0000

    destroy quick aim goon programme

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  def is_even(n):
+        return n % 2 == 0
 def calculate_pressure(force, area):
         return force / area
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
