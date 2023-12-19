70f062ef53bfad1ba287d4a26fd90d5ba39a3421
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:36:52 +0000

    quiet minority we star friendly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
