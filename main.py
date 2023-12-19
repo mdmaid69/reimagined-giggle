ad43a400af39d946e7ab33f3cd806d4c4fad7d4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:22 +0000

    still almost small not forever

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
