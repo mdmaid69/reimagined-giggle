7dff9f713abc1181cdc29e6a3472c6fa22c97a9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:23 +0000

    tip skirt level truth spring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
