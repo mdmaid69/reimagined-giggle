bc6608402da2c73c1bf20ff8805679b9d3bc93bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:03 +0000

    stranger platform good direct advise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
