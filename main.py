117bddfba3ec0053f3be976df0351566b03637f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:41 +0000

    put grand neither quiet reasonable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
