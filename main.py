d9736a7a8388a07d019e929fa103ba339a74c939
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:12 +0000

    offer guard exaggerate leave getback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
