f9e62835f37cf3cde75c32674674d9a03b0ad56a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:59 +0000

    address weak task responsible anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
