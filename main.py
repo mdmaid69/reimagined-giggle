a39bd0f1904e71c802d2811d591408e313df8147
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:02 +0000

    please goodbye aware lend general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
