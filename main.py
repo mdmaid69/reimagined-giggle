f35fa5b7c0c241966a794d79e4eb83d764132079
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:14:26 +0000

    workupto her leg ear once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+for i in range(10): print(i)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
