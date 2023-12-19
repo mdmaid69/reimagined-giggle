7974d3608a88ca517db5f3d4891373c2576620af
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:47 +0000

    open thin pressure joke worst

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
