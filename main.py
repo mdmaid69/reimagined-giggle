4c05f9b3c8a890daa2d0e73c24944fd8356a7bc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:01 +0000

    that fetch waste comment doubt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
