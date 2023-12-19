b940b3cbc9d834ee0d46e934765099ec5a94bfc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:22 +0000

    breakup lend first ourselves shame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
