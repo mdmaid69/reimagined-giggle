7814e951399fe6d54a2bf44da74e6e4dbb18c24e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:56:22 +0000

    present consist autumn young problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_max(lst):
+        return max(lst)
