60d44356485bc630d628b3b3216fa9d7a9c5a62e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:46 +0000

    wide chat now clean off

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
