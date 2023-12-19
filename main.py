659f1d28a72df47bba54310d34fbb9b71216b4ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:03 +0000

    comeout embarrassed sell luck recover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def convert_to_binary(n):
+        return bin(n)
