cf0425799881942b2eab7d2bda01d6310f15efa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:44:44 +0000

    pleasure tip turndown my though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
