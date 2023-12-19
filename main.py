d4b82b6ddde5258c913a6974e08d31b92c212d50
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:49 +0000

    tidy guide hang odd turnup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
