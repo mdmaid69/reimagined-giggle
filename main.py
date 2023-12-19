3adb97c702e1a23455aa6846aa48ada7ea57f8fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:36 +0000

    mind new proper path rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-name = "Python"
-print("Hello,", name)
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
