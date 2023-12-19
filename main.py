00908016cc3d15e1cccc776530a27926a0f2246a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:00:16 +0000

    cruelty pupil earth hole female

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
