a026f61005ac81fb06c34097d219dc9682c21128
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:15:32 +0000

    objection walk choose still point

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import time
+print(time.time())
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
