5bdb2c1bbf4d022455e999ee1b8552aba0c74227
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:59:39 +0000

    private oil now conversation warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def convert_to_binary(n):
+        return bin(n)
