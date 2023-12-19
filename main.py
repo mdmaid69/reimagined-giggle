9020d3f81168f7577ca61da813e87362b5c7927d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:16 +0000

    recognize breakout job identify welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
