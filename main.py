8b1f029138457c3eacef490cf024bb52fa99e878
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:42 +0000

    afford approve beneath achievement clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
