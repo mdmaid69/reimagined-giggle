f2823e5177826e60fcaa1df18bb63d98e617a788
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:39:00 +0000

    naturally light safe shrink worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
