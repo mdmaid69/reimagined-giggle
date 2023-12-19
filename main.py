702b4450c862c5a4fdc56da9b6016168fdf6f379
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:58 +0000

    increase turnoff goback(to) exciting name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
