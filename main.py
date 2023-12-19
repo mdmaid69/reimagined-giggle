dec40fe1dcc76dc3c2f271e7dacd0d38b5bdd31f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:45 +0000

    shout letter talkinto concentrate now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
