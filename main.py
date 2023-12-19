3b296bc3754c305088b9b5f702aec6964bcb840c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:20 +0000

    attractive challenge progress borrow open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
