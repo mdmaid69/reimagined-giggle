ba6e5494c56b1314773a0fbe64a7dc2e09f7ae16
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:54 +0000

    giveup sometimes explain everywhere measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
