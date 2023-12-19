3fed556482832bf0957afd71ca974ac04d929490
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:37:29 +0000

    operation neat not pay fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
