2c54043374f4d1d79c08e6dada4053be9ddc3ba5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:01 +0000

    listenfor minority gap interview aim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
