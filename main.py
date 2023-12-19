a1a15bc680c2e45dc48a772c689fb37aebcace8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:29 +0000

    surface professional dead clothes regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
