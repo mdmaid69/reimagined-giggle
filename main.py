748492996a715304560d5789e4dfe23e9d73e662
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:11 +0000

    steer breakdown compete like habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def count_elements(lst):
+        return len(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
