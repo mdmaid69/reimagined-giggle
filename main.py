1f2f7aac8064f29c58456290af3efde920dc01f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:48:29 +0000

    late appeal beside shoulder comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
