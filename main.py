b3a3b98ca233e9b81b972275aa11550632582f24
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:01:25 +0000

    early where entry married subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
