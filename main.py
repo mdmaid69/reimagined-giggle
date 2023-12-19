718928a98f0571cf1631ff318f2b4eabf6c52d23
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:01 +0000

    flow hers run call wake

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
