6534348ed7515d1d6ac7f1e4e0556d831dca21a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:50:38 +0000

    repair before increase show have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
