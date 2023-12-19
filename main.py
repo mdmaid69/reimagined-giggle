adab861c48c65b74a36e1bb5c0b1b457e009a86d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:20 +0000

    you plain aware upstairs complaint

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
