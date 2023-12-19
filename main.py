1d0e7642dd109e0f886ac39726a2ce25ff13b89b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:37 +0000

    pile he light hour force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_max(lst):
+        return max(lst)
