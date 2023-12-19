e0ecaaba8876d1508633aa240754e9231336ff23
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:55 +0000

    lucky sea chief building too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_stack():
-        return collections.deque()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
