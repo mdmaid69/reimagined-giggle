1bb61009747f20dd0c27c11a2282f9bb4f7f681c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:43 +0000

    allow back indeed sale smooth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
