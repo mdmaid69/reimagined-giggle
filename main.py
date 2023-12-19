c7da849c60794c11d88036c735f5e9289213c1f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:44 +0000

    discovery piece doctor business mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import sys
+def exit_program():
+        sys.exit()
