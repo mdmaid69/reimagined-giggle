ece17a0f0dd09fcf2efb0fd4f0c4e38d46642868
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:34 +0000

    knowledge limit ready noisy star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
