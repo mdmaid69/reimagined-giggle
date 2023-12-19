d1ddffb13bc1925850262cde5dc116671232c5c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:00 +0000

    brush OKorokay need become look

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-for i in range(10): print(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
