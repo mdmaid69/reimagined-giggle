200b9f0394492e1e8d4c9e81cda19b5987264526
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:50 +0000

    hurry false goaway cost spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
