65f42dcb684003398075b41adaabd01c9ff8a31e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:15:21 +0000

    much spite tear shoulder fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
