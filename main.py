331cc4e5ad6db0c57e56bf8f579a31abcaf66230
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:57 +0000

    minute sometimes meet exactly warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  def remove_duplicates(lst):
+        return list(set(lst))
