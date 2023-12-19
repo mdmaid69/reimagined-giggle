3c74741a1dfbe2995eb5c6cd8bead9ea26936be9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:52 +0000

    even disturbing cruel think deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
