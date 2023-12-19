f46ac1d56d7905748e075702811415b5bb32b769
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:27 +0000

    physical gooff delicate once surprise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
