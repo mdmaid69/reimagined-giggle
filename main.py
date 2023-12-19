5d84a2175c9c86261fd53a480616f778f04564f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:29 +0000

    bet makeinto enthusiastic wise popular

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
