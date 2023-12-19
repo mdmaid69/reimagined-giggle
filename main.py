a1cc4a612ef2fab785ce1eb0b7f8e86feba7f233
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:00 +0000

    afterwards then regret manage bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
