aa55a92acbb31efd798f4759bd5768fa778f4c3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:23:04 +0000

    cause properly whether demonstrate free

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
