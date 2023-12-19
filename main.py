2e7315675bd31df8b12cacc3f7201f3bd9aa83dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:45 +0000

    path whatever ocean mark direct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
