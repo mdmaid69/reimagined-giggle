deee539829172d274a10a83cfbe525196411b534
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:10 +0000

    many bunch touch official raise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
