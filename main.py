2fa0b5b4c0958a9c1225bac15a953d5a8486efc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:11 +0000

    group innocent roll talkinto pollution

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
