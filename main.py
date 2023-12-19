2c98a25640a2f2f213160a44dbcb7f41fd4d605e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:19 +0000

    another mind vanish steam profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
