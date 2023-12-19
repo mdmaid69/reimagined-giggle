1c4463b15936291fa491a82e2641ec580d08eb58
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:09 +0000

    left achieve bin care late

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
