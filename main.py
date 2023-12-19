fbc24a11e322d8492477ffdb3cfbb839490a8011
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:35:38 +0000

    sweep godown rather film record

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def cube_number(x):
-        return x**3
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
