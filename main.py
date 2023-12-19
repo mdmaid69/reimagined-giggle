7eaf02909102c83ed5a63dbbde652508eb4b7e11
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:25:38 +0000

    impressive bring length convenient Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_even(n):
+        return n % 2 == 0
