5b9472d08f3f04c9addcf2295fe849fe37190c60
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:03 +0000

    who crowd pity cross long

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
