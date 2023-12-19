301d8ec450ec878969f622f9360d3c08618e963b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:00:49 +0000

    smile achieve thankyou official talk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
