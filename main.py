0667eddcbeeaffc93e08591a5e7c1660968812aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:56 +0000

    suck late type rank subject

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+for i in range(5):
+        print(i)
