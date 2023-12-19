0fd5fcc68e3260f4c72ede5c0267c2721e68ef1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:47 +0000

    tip powerful refuse bother persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
