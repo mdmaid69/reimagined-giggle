4ec96d449af1a862c3345203defad64f4daba814
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:17 +0000

    doubt convinced might cover practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
