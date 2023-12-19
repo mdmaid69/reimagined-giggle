2acbc3ce7d3155f57d2c4b941d905b705dc14b4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:08 +0000

    height swimming unfair perhaps turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import datetime
-print(datetime.datetime.now())
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
