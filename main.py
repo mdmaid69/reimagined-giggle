ac7123f488bde902ca0e4b9bca6bc5cc4c686908
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:02 +0000

    necessary fast job morning cheap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
