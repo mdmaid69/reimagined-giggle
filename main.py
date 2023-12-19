5c873355c813c8ab6ccabe3ad4e33d9b871cba69
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:58:01 +0000

    roof subject climb seat shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import numpy as np
+print(np.array([1, 2, 3]))
