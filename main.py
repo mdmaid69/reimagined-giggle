58ca7da415c2c29f726a8ddaaa4a8d17c88845b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:02:19 +0000

    stick pretend keepaway escape ear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
