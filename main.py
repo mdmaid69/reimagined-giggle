d2889a7149d0049c5b8b20a7e211590eef893963
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:31 +0000

    determined should rise measure loss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
