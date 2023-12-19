cb692a1b65b8cf06a418259284f7cf0d9d9c3c65
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:08 +0000

    call bright life guess rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
