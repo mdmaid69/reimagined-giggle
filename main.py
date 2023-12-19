6b507efa31af0fa953938c651738cc083327e484
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:33 +0000

    trial Ll release wise strange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
