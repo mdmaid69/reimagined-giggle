7ea8f82377e33f59002efc49b539d0440f01b014
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:50 +0000

    centimetre like brain 573entertainment admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
