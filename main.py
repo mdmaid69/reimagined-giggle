a4ad045dfb4aa64044cbfa434c094e5cea3d011d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:16:40 +0000

    what cause learn waste exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import http.server
 def start_http_server(port):
         http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_odd(n):
+        return n % 2 != 0
