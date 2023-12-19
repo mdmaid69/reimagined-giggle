c433a8c8474a295b04b4199d6edfbebf71e97a13
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:30 +0000

    deaf wish attempt after close

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
