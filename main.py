b0e9a97a37bca1977c383d23ffd7029eecfb510a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:33 +0000

    better safe set instant guess

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
-print(os.getcwd())
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
