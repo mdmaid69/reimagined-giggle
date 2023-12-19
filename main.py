37323e776fc78979ba09dcfb9337fe3548b4e4c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:21 +0000

    bringback task method first anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def subtract_numbers(x, y):
-        return x - y
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
