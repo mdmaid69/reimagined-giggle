0b20a2afcd3b06ecdf08da1ab32cb20044af588e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:26 +0000

    spread part crime floor again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
