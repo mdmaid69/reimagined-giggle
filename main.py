f122ae49afbcc9bd99e257c59ac396cdadceefb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:40 +0000

    rough hate matter live expect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_string_from_array(array):
+        return array.tobytes()
