a5eda524e13e46c3765ed953904a7b10637cf86a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:55:40 +0000

    even confuse information complicated at

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 def calculate_force(mass, acceleration):
         return mass * acceleration
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
