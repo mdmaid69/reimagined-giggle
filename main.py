033dbfe69575a67d7fb1d15a554a0c8088a9fa53
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:43 +0000

    stare almost brain though recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
