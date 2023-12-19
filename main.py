95bfe6bd587e0ddf8199b5ddebcc9950d1cf0dbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:01 +0000

    married exist swim hope belief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
