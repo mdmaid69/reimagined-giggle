9400141c7ad23ce36d671724c0396a47bc363e15
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:19 +0000

    naturally ability bend pleased discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
