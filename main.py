dbef23607d432c6ac1b791354620649151f2d609
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:28:01 +0000

    feature spoil factory land design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
