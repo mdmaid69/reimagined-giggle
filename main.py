ac1330bb4b30703fbb2b8b1110addfb1bfdd1726
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:01:08 +0000

    remark seem mark kill hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
