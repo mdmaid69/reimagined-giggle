b99232e5b1b9e5322d29a9f5511c9963df1e1280
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:33 +0000

    touch bite hers behave smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
