b6edb8bdf4f45f63e9d20af9b024b95c29a43ab4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:14 +0000

    difficulty wrap display tidy punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
