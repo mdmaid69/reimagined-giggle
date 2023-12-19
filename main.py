7371e2fb1bce1e6c0a11d25245d72466a2ff9da1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:00 +0000

    closed achieve suddenly clean once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
