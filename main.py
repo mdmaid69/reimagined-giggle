31c919ecb7843efddd8a3ba1933e9333203f6510
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:20:13 +0000

    own meaning wise hurry few

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
