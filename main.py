865a25a2fc015803115846daff5552b1655ff611
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:48 +0000

    block Ss charge succeed immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
 import logging
 def log_message(message):
         logging.info(message)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
