29e6e7bfbe022e8992bc025386fc0a39286e32bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:36:00 +0000

    because jump mess side mainly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
