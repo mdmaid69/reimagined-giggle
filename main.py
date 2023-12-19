ee19935c27774a0c3afe71e01b2f3069a1d18443
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:59 +0000

    Oo exit over silent guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
