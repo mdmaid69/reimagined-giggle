1ba22f8cf23569daef354c15f361e2a4f0723e8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:43 +0000

    belief gentle praise neat value

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
