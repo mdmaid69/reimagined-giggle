69d5b6b6e99093144d4e3d8d959bb6f9621d1ef2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:23 +0000

    translate flavour buy manner big

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
