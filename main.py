1385b22f0ea6a83146d204de1ccc263e5625dfeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:35:00 +0000

    lookback(on) reduce wash work take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
