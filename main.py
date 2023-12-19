fbbf0aac2db219049f6d3a560de73ca96ec844ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:18:25 +0000

    sister level proof substance come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
