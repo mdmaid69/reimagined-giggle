5cf99125c5b91a4e745786ba2a50e3c8624d691d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:27 +0000

    almost moral think study defend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
