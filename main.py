c5b4c4dcc8cd5384c44c8ce9809cf70560583afb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:58 +0000

    connect stone congratulations head impression

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def remove_duplicates(lst):
+        return list(set(lst))
