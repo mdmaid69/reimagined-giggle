4c0b69c5afa9808c6a882f0f497f45eaeb2f4e11
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:00 +0000

    trade government into port valuable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
