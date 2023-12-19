ce5d1150b2e6d1ae12acd0659dcc9118c27e9ff2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:53:58 +0000

    talkback push proof movement impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
