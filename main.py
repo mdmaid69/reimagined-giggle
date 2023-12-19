3176ccf57cfa631cae540dbc3045877085921105
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:37 +0000

    phone their light please long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
