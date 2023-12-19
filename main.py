a820b0f93dac65e5921248eb7ca56eccdeb5b352
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:23:22 +0000

    plain recent opportunity getback shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
