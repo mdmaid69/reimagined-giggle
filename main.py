731eedbe8649f2ccbeb9b5c8e252c4b8f9c16d77
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:07 +0000

    pot discuss holdout sleep press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
