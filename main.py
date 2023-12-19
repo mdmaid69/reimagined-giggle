220ba6c41dc2e16a2ba35f33625504f809647b3a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:08:23 +0000

    slightly division attempt design smile

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def convert_to_json(data):
+        return json.dumps(data)
