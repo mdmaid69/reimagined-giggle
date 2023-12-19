13bde9ef4307fb88487d429d2ef9d681603a4d9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:42 +0000

    bag lookback(on) book good she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_list(array):
+        return list(array)
