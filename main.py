0c3f3eb6e5b0cd6f8831a8cbcef3c536df9c6f55
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:12 +0000

    avoid shock takeoff though mystery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
