f11934ee45faa0317f93691ced8b9a02bb3d39e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:14 +0000

    dry blind post charge unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
