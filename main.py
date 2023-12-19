c22a36195ed47e9ac814aa2478980dce5748c4ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:48:09 +0000

    soil song set bottom choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
