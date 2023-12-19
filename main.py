e95a4e98356b5144c6a2b2947c76686a8a28c189
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:23:54 +0000

    negative education frightened refuse mean

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_repr(array):
+        return repr(array)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
