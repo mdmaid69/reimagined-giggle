9f2e00bebe3772cb76256d4d70bb7c6f49e957ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:24:50 +0000

    forgive storm callback behave thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
