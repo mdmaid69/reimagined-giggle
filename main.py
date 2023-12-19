e08f0c614910b8a8a289c4977b2619091d66d660
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:17 +0000

    danger bear stare crime peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
