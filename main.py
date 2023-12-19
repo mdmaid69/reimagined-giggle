c3444f68803961103d695d1ba23417a26b549fb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:34:02 +0000

    picture convinced breath address lucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
