4309e3d40591c4dcce93469170635aa25f3f5ac2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:26 +0000

    arrangement load supply table this

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+print([x**2 for x in range(10)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
