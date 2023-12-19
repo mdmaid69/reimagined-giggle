e7cba5e9b7711312dfcfd0f2f9244eb6901ea77d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:54 +0000

    week identify contrast worried correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
