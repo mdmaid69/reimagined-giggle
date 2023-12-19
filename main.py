3678451ba28f2dc7690a2902c6ae02cba9e82bb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:07 +0000

    along confusing sink suggestion interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def is_odd(n):
+        return n % 2 != 0
