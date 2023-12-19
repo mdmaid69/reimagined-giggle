a9410349f42142a8d678b3d0c00c0c8f78dff8b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:26 +0000

    theirs frightened aware reserve content

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_pressure(force, area):
+        return force / area
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
