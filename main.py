f6b01b3b5186ef406d5850864d3c4daa877c2578
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:38 +0000

    wrong especially dream pain approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
