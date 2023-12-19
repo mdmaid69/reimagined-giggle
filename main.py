f6d97752a80df914b0c8fc0d35a635a2114631ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:49:10 +0000

    guide on beside never ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  def sort_list(lst):
-        return sorted(lst)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
