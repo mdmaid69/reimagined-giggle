b9a4d88a1d0d76614dba7b8f97eb230628fd1c88
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:27 +0000

    block instant arrival actually all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
