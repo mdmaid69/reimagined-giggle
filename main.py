ec129a2c0823ef5b0a77ada788248bf1b9525cb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:21:01 +0000

    wake Nn preparation borrow dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
