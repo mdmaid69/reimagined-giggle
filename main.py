0b534cff76b75a1cb9d3b3bb432b715a7c1007cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:22:18 +0000

    tongue thankyou concern judge track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
