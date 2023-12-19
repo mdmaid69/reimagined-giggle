abc51a55a777e7ebe75a40532b5d50bd98abb764
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:31 +0000

    brush rescue opportunity few only

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
