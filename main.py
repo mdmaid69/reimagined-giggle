04f2a6bc15d50c97096135122f6323d303242224
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:43 +0000

    young confidence Bb touch general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
