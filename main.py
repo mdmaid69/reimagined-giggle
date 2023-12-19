8e823c76ae027ed0a0141b7a9f9cad0933ed3a4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:51 +0000

    opinion responsibility serious bear want

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def count_elements(lst):
+        return len(lst)
