52e2d78e6c09f9c3731683d79c27cd282c957096
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:51 +0000

    considering level actual nurse murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
