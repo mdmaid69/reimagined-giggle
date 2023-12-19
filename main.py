018ff77892d76f70c8d35a0bb0f50813bfa6c471
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:38 +0000

    culture strike consider dream string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def sort_numbers(numbers):
+        return sorted(numbers)
