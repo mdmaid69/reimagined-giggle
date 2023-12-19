e23ce7f4c235b09dfd5d3aef6b5a9d84baefa372
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:50:14 +0000

    yours next way innocent avoid

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
