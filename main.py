3b6c787001c102ea86608f513cbef351139ad22f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:27 +0000

    else statement telephone shoulder sister

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
