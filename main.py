6210b92d3425f6aa9a63b38dc3d65c1f5d0dd284
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:58 +0000

    television plastic stupid school achieve

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
