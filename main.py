fdfe918bb8179a1b819e04b393ccf427cbcb8091
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:49 +0000

    circle clean positive accidentally confidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
