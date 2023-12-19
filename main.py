898d07e79b078e79319dbdc55618c20d51ea4f83
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:37 +0000

    hide telephone large equal official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
