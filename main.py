7735abffc8fe47137b262fbff654e254bea7f15e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:41 +0000

    fly contact jump friendly operate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
