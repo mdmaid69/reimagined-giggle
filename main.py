d3ed26c9e2ef7169b943957d210d0bb4e86a057b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:53:27 +0000

    workon chat serious pay statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
