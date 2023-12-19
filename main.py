3569ddfce41103fbde0b8f794c5165f1f6390304
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:36 +0000

    creature listen from university receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
