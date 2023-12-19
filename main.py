7309fb4bdbe31d6b15cff05b60d3eda056444c37
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:38 +0000

    impressed theirs blame in sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
