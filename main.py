1818c2731918f4e4656b6bb163ec7860b91770cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:14 +0000

    feature too convincing its match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
