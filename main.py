54a74bdeb286097716f9c64d47707d380b5f8201
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:35 +0000

    medium holiday act spoil judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
