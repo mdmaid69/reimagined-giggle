7ab566d6893da599890c10ac147fdc61c66b9346
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:24 +0000

    but male hurt stiff Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import collections
+def create_user_list():
+        return collections.UserList()
