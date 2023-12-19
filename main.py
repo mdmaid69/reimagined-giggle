5710bb4bdea0f938baf3426107fb3781c1fd7b8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:41 +0000

    answer snow chief burn help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
