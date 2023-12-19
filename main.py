bc821ced613cdeb4f52b62cd10c40bd8225acf01
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:56:01 +0000

    building pin indeed trousers vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
