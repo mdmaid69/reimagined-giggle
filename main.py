75a110553464da92d864bd99ebb0ecc56da92210
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:52 +0000

    profit check degree degree win

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def exit_program():
+        sys.exit()
