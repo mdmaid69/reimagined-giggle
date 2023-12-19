9aa0a4d05bdec03f1b15d55f2f55dc9f7d4ece10
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:26:46 +0000

    part whistle warm rule tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
