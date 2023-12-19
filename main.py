456b43e8271c1fb7426b3c9427a7a12f7be61c40
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:19:00 +0000

    us schedule sleep spray work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
