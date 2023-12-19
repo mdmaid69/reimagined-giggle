c424238bc4674b81c66c89fd863c9edf5c3dd20f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:31 +0000

    warm breakinto hurry call shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
