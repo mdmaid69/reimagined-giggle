178e91a6b9b3b9697487dd514f1e0f2baccdac0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:05:22 +0000

    confidence vote most could except

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_work(force, distance):
+        return force * distance
