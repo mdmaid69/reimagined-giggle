7509e45bb55f61fab439d6a1c884ff8d075d6f55
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:33:43 +0000

    laugh let anxious shake sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import time
+def get_current_time():
+        return time.time()
