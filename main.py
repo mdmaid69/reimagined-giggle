ae43c60be7d40aa4339c52168b209a1892dad13a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:14:56 +0000

    keepaway price shock shrink roof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def reverse_string(s):
+        return s[::-1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
