8303f8a57fc616c7259aab0334d48d5bd695f27f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:23 +0000

    acceptable moment slowly usual allow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
