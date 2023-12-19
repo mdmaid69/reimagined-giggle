767e88568273afbdafe03a8a5efdd22bcf17b8ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:48 +0000

    culture comeoff week cross future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
