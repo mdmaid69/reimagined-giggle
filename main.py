435c56f75fe748dfeab286c76d976fbd2f506d22
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:55 +0000

    comment bit circle thick cut

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def add_numbers(x, y):
-        return x + y
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
