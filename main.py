690d0c625207fb08359039e84f38c06d1fa7e06e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:55 +0000

    already stage mood show study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
