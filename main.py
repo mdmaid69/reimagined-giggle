557e08a42e3ad93b86d1cc0163ec7adbb195302e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:59 +0000

    union software object project imagine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
