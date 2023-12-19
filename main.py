5b7b1c0e20d6c5caaf3c9bd4f6b5277ab445bacf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:27:32 +0000

    disagree check container power morning

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+for i in range(10): print(i)
