285ed153e3181b3c702c1808c501bda1fb5352be
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:47 +0000

    kind sea unfair sharp laugh

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
