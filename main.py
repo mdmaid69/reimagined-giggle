bab1b119eae93c62854c4f68ecc55710296961ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:47 +0000

    morning though unkind complete price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
