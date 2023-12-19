18c83c01faedcc6afd212fb09d29c27de052ed8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:00 +0000

    runinto bridge feel plain enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
