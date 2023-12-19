2e019e5f6091bc2872418e051513485fb993e7e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:52 +0000

    whistle sweep guard dry rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
