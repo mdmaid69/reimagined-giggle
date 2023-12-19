e08f812f8d6bd1517738b66073cfd0f1569d330d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:41 +0000

    exchange crash bring unable pretty

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
