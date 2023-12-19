56bba42bf79eb8be50b5c4eec4416aa83f07c36b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:31 +0000

    announce burn raise giveup idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import logging
-def log_message(message):
-        logging.info(message)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
