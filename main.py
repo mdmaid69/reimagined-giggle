0d4fcf6ee199b05c55d6c4de19b54e13efb73f2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:36:30 +0000

    rush store name flood order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def cube_number(x):
+        return x**3
