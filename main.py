078e37b8155039f46b97c7d103db7fd14276dc66
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:12 +0000

    before drink sink rush permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
