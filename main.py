23a3d615eb6cfd45a5094e8930698e24980c40e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:19 +0000

    Pp Ss life cope space

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
