cbaccc63d7a5397ee327e75706ac1b29231558fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:04 +0000

    steam related show bite escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
