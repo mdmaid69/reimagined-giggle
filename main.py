4fe333715235eaaf32eb3019bbf1625d2fdf3875
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:11 +0000

    need cap start somehow shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
