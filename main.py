30fff43f49937bdb3ac88d9cad1585e8f5e82b00
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:37 +0000

    calm least great hold delicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
