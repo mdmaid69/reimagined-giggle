16542e5042669cfd4e931e4211eee3ca6dcb3d56
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:58:11 +0000

    spot in convinced experiment 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print("Hello, world!")
