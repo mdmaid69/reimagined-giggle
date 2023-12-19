84a36cb8cb314ce82fe97e6587cbe59d5f94802a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:21 +0000

    lookup future own favour receive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_power(work, time):
-        return work / time
+def multiply_numbers(x, y):
+        return x * y
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
