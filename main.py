fc89c18407ad8723f76d9c815a847fb285c6e885
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:28:03 +0000

    relation workon quiet progress first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
