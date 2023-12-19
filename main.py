eb1456bee1e187019102742a02174d0a72106582
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:54 +0000

    comeround fun responsible equipment sensible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
