5af5ff11598ae85186bdd5bf7d4c00fc980a37e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:36:27 +0000

    workout rush past read keen

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
