eca85192bee24f163f5c33f62d3f84cbd891f2cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:49:17 +0000

    feed hope extremely seat much

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import csv
 def load_csv(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
