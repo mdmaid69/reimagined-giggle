4cd72dcfc73c88f7f93870ce475a880224c4c44b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:02 +0000

    calmly giveback error chat wire

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
