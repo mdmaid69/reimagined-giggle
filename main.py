c2df36d7006873beb9ae2c2eab6d35f7fe2d5364
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:53 +0000

    dinner base but calm breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
