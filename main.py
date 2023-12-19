410245569ef395b75fc5b77f20409b8d93a59295
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:00 +0000

    gooff Gg some correct basic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
