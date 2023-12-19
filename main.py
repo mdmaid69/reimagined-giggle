46c3d307eedfd2e815970edbd271c19ce3ca505b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:38 +0000

    Oo mother conduct protest uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
