da9b5e7561df69ffca640bae3e3c1cf2452812d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:27 +0000

    exist soil fail deliberately uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
