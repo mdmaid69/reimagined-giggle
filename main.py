e3f375e2d55b6320145d5fc2a40fee6f71358151
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:34 +0000

    active depend workon mention hold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def add_numbers(x, y):
+        return x + y
