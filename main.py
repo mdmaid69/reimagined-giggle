76e5c1956852f998c520bf430717369a0bc4c52b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:29:15 +0000

    affect sick respect at plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
