e0820a7a9f6549ce4165924662184bcdc81a34c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:51 +0000

    nature scene wide legal thirsty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+from collections import Counter
+print(Counter("hello world"))
