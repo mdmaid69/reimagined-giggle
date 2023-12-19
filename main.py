acffdda9085215a1ebe898875ecc7e13389f8933
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:55:14 +0000

    can partner concentrate attract dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
