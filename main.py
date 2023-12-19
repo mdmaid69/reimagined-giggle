7486ce19fb15162b1ee24a29634c0556e2bbe8f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:59 +0000

    throw suit plan keepup word

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
