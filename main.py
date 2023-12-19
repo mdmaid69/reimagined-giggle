6ea0d87156ee2168a2dfc2422a966239dcfde46f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:33:50 +0000

    post equipment sound learn frightened

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
