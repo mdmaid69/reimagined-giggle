405fc8484f92e7aa226f738d02455b6952b21bbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:33:35 +0000

    keepfrom I lookback(on) shout well

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def multiply_numbers(x, y):
-        return x * y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
