a961bec7b715f14534af6135a3ac76b10da0f23b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:01:11 +0000

    market must soil mine bite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
