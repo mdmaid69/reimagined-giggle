03f7399f20e44c481e313b4193c7bab83f3d036e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:53 +0000

    ground ear fix operation rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+def print_python_version():
+        print(sys.version)
