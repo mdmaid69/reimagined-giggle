7c9db5632428ecce9bb8c5fc944a372beaac256a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:24:50 +0000

    fresh possibility support sink television

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
