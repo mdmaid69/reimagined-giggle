b476c730ea98a60b90b07b96d2ab041606a77908
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:00 +0000

    hole cold keen wish on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
