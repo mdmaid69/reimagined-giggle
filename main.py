ec2236a989f560ff46bb5d1d258f593d6f888fc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:08:27 +0000

    wear land expensive cheek into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
