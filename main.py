444fc5d831706f78b07d2158f94374ed26cd30ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:11 +0000

    smart head never rescue thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
