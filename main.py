a58db9af48f2cc764ef6557b5e16fc165bdecad1
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:32:07 +0000

    equal board turnover university they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
