b1c96c019715ad4ea64b63555b2a58486bb4d069
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:06 +0000

    cruel factory scene violent head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
