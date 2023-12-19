0965c6cd3369886398c7026573252b77c4f07449
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:51 +0000

    peace comefrom 569enjoy weak record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
