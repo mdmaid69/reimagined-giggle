0b1816a1a49839815e668080938f0fce5784f017
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:32 +0000

    battery future new fit expression

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
