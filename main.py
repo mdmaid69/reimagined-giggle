cf8f316861fba13408b117f903f49ecbbc4f1a46
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:36:37 +0000

    muscle far pleasant advice holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
