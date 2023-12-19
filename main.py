a051e6cce3752b736a935df920f6487caf54b509
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:30:45 +0000

    your sky 573entertainment unkind should

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
