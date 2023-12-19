f9c0bf8c3d0a6d1004724ebf2f2acbbdc72160fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:05:15 +0000

    behaviour rubbish exciting answer workout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
