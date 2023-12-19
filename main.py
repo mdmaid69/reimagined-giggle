2b7862816f397e2bb1513f68079f616f50eb83ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:07 +0000

    organize appeal change straight by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
