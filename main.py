a8b440e5bf48d823d35140ad2e0f95dbd429539b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:22 +0000

    scream general style demand start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        return sys.version
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
