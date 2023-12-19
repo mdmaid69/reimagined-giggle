274b90e2e3f1e9336c41a362304ee1c7c89f2ca7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:07 +0000

    urgent mend foot say whom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
