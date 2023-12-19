71f63b46ddc50017ffbec9bc496b5b0bc87dc7ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:57:46 +0000

    compare prefer law arrival party

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def cube_number(x):
-        return x**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
