16e38697e2f20497b5db90faad553756cd3a53b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:34 +0000

    attempt exploration somebody bush Mm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
