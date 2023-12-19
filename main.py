0e8e3ba302655a6073795c8e46a2578d7c440cc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:20 +0000

    row collect lunch whenever fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
