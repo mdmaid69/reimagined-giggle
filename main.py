c17ef46427707dc74fdc4a5b91236e3a669e22c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:49 +0000

    kick gift connection scheme advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
