378fdeb87aea20d63ab230d6d9ef551e4fb81f62
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:07:10 +0000

    holdon answer sale police them

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+for i in range(5):
+        print(i)
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def get_today_date():
+        return datetime.date.today()
