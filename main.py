97a898828f50d8e985064b277028f15635b26e28
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:56 +0000

    compare manage development explain secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
