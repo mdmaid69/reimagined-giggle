a499e34db9eb77132750f57684e1128d7ffd0831
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:48 +0000

    limit satisfactory careless sensitive of

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
