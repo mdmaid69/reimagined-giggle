e9ed8dfc8b09e2e1ee884c5247140f8ea300b05b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:44 +0000

    thin stand friendly fly path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
