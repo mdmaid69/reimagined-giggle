b03aa7646dc5733923cff3a1f5b720a08898a27e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:58:33 +0000

    than direct risk speed march

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
