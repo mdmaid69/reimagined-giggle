c7339944f59923eb5859282d033ad8ec11c5a046
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:41 +0000

    pretty tour our dear sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def reverse_string(s):
+        return s[::-1]
