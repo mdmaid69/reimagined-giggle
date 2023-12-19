f78ef4182934f4fbbbc9a5463379199e3587bdff
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:30:03 +0000

    dark tend undo declaration from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import collections
+def create_user_dict():
+        return collections.UserDict()
