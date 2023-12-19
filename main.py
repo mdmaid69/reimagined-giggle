05827fc2e71165129cb7da6bacbe889305f88ef7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:33 +0000

    style save morning small interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
