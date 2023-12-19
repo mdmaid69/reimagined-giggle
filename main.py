18fb681a1d7431f9d218043a0410eda737db7e66
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:04 +0000

    worth development dick your light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
