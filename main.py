1e54762f7c02796c1dcc6323947ba2e85b29135a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:25:34 +0000

    mixture answer pair expect brown

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
