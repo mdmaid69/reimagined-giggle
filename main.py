43c31f6de4722029c71febc17a457a61375d3708
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:46:50 +0000

    explain Ss book every raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
