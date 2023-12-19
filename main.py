d7c3237aac7d0162ec30dfa7085688acfc500624
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:13:47 +0000

    comeback manage same expensive refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
