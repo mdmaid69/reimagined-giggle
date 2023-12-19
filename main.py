6ae664f5973c44593ca9e802b9ceaa10db186a5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:29 +0000

    justice split will father sure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
