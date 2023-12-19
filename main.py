8102909ec926bd70d0e19635b7aa0da112498bce
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:36 +0000

    Jj friendly whatever brave accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
