abe7aa4c368617409372add276293289d417d2fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:25 +0000

    wash little praise innocent slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
