82ec8bb60e325cffca4f8396a4174f8a9c6cedca
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:51:27 +0000

    emphasize treat breakdown effect worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def clear_array(array):
-        array *= 0
+def find_min(lst):
+        return min(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
