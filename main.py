f0db6e73c0e1174ab49137f92a3c5959f8c0acb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:51 +0000

    design stop prevent common thanks

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
