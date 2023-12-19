d9c68d5b34cb1b16cef2d48d16a15202e9e9e3b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:35 +0000

    spring least smell ability subject

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
