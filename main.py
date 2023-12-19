e85ca113f3faf510c9be03a07a7efe5905a8a7c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:10:09 +0000

    in the prize winter object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
