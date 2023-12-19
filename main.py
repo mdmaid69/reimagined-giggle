4e1abbf273210585e571c614b04b219868ea15be
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:55:21 +0000

    tax butter trade average actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_area_rectangle(l, w):
+        return l * w
