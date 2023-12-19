8f3b998ac68dfe36d3d6daf1f2c6098dc9c8feb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:35 +0000

    steam environment ordinary private calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
+  import re
+  def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
