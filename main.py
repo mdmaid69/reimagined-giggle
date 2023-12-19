bd4866ef80ab74f16d458a45871f665213085253
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:00:09 +0000

    search as leave contact often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
