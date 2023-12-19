5b903ec6ebf9ea04b76278bf56b992e473ed2dc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:09 +0000

    production frighten spite react season

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import time
 def get_formatted_time():
         return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
