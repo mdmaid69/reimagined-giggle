d3da8831c9a0f53e554effeeb8a1dabf2d3742f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:06:33 +0000

    speed scrape noisy obvious runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
