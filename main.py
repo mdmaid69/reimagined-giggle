e55a1557e7917e7ee1017a8baa83ce2677fca1e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:04 +0000

    boring wear confuse shopping type

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
