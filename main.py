8339adffb60ab75c81f07441ba04a37755daa498
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:41 +0000

    opposite trap freedom 573entertainment need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def find_max(lst):
+        return max(lst)
