17f3c8340f23eaeadbdfd14453abcc80fa1582e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:31 +0000

    pot behind frightened usually sudden

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import random
-print(random.randint(0, 100))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import os
+print(os.getcwd())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
