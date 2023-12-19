0c4920ddbde71fbff2eb9dceb3aebf83eddc2f0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:06 +0000

    wash embarrassed instrument turn drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
