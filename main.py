56babc92b217ddebc73ef39c1bde728714409507
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:47 +0000

    goaway eat flood jump negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
