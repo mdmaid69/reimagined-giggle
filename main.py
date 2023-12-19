9d24a83f347a4e92012767ecdabfa4405ad68131
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:43:10 +0000

    free if large business storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
