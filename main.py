4868ae0368bae7b0464c8af7edb95bc85cec3763
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:11:45 +0000

    holdout pipe command trap lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-print(time.time())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
