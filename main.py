31dc1c75d71df3b1057ebe2175652221da59449a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:54:59 +0000

    song turn straight region 567enemy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
