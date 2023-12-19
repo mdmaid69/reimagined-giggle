c290b1f9f17483f326f67b21069a91635eb842d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:17 +0000

    provide touch skirt climb already

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
