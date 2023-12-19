e2397376aec9492610a9d9a34b801205e8ff4791
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:23 +0000

    left read appeal open recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_numbers(numbers):
+        return sorted(numbers)
