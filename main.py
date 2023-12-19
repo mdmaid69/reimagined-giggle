ad7ca3b530bb4f119186576f886f6d602dcbfa98
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:14:45 +0000

    him interview balance takeout winter

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import random
+print(random.randint(0, 100))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
