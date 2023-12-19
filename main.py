54d58a1d7fcd140db39c4ed44f555fcb9caf1d43
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:31 +0000

    by butter welcome could fly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
