0c15543c71842d86918135c817730667b78b1de7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:15:43 +0000

    zero afterwards here dark success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
