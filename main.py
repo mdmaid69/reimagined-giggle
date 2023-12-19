a5550bee5c71a77d229b54cb195050a0e742834d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:36 +0000

    skill again soil consult pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
