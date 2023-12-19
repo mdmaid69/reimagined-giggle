fa2ea2a6183b3b4ca72a6236724535351af61ce7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:32 +0000

    neither keepaway bringback goout record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area(radius):
+        return 3.14 * radius * radius
