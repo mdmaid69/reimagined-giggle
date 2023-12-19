9e99aaa9d4d4f6fed94b09ca8de4baac5132e6c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:08 +0000

    file welcome rent high approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import time
+def get_current_time():
+        return time.ctime()
