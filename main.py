2b81936dd3b9ba5bc29d605e81ae7b931ac033d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:39 +0000

    study disturb thinkthrough bad will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import tensorflow as tf
+print(tf.__version__)
+from collections import Counter
+print(Counter("hello world"))
