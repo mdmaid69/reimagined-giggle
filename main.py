c1c1397d04fc1f0e91e9ed72df104a5d7279ff86
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:13 +0000

    quick absolutely protect least fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import collections
+def create_priority_queue():
+        return collections.deque()
