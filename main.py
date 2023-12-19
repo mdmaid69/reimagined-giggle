c175dbebed7daeda79a0f8c1a15ed89bd9627f0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:52:26 +0000

    really pocket radio instruction lean

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import collections
+def create_priority_queue():
+        return collections.deque()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
