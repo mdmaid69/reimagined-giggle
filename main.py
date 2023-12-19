11e53b54de4827f7841a214c0a5acbf32286db36
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:54:17 +0000

    scared include star fast shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
