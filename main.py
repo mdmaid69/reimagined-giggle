dce9f1bcc64a7fc65da3ee914daad294ac9e1a41
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:37:04 +0000

    sister seriously group replace police

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import numpy as np
+print(np.array([1, 2, 3]))
