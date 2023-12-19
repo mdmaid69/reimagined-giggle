bbb26015839a73e9c7570b94bd7296641c182fbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:33 +0000

    every destroy join large far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_rectangle(l, w):
+        return l * w
