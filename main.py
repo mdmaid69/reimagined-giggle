3bc9fe56464fc4bf000f0db2638643e212289fdd
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:16 +0000

    listenfor secret sail beautiful difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
