087cb61c000464c3571306772c9fdaa388d9004f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:36 +0000

    half sea answer book pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
