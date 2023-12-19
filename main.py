a29ccad2372a39cea910574177562c7467e7907a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:10:00 +0000

    lay reaction communicate spin speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_set(array):
+        return set(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
