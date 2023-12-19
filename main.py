bf577c7c7d3cf49949b5e199773995bb1e99e633
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:19 +0000

    really his steer come march

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def is_even(n):
+        return n % 2 == 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
