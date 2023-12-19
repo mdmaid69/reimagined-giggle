761aa800ae89760a73244754907b051d7dc536a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:50 +0000

    now free late serious hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
