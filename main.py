e06cb76c828938a0d5ed60ecdf975ffebbefc8d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:17:23 +0000

    Ww scrap rent drink wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
