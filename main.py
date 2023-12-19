10115b0fefcdcfba9e2fb9705d0a3f1d2db472b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:14:01 +0000

    control Bb quarter contact few

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
