396e7db666a050cd338502ba769417442f837b16
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:32 +0000

    row damp change closely regular

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
