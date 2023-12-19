bbb565480b86468645a31358d6048d5822cd7f30
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:35:09 +0000

    block class name return brush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
