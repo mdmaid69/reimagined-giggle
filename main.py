9f8cfb1b591af0bceffe8cafaf54aa724c5c6fee
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:41 +0000

    respect less 179bite either really

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def is_odd(n):
+        return n % 2 != 0
