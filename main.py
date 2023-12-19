a3e1906f65ea218dc0e25c0a221ef67a25757938
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:47 +0000

    control key shame keep act

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
