670685d6fd965454685d6c96a619f9a3539ce70e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:47 +0000

    sleep combine experiment forgive person

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
