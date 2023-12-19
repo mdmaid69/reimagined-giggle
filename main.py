d9c15093b895b2362d88db2a060cc2ea14991c23
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:59:17 +0000

    whoever injury 571enough perfect pressure

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
