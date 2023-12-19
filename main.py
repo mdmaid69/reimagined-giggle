9b8a5304890f41ecc18de4ac0ab6faa883309ea0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:20:48 +0000

    sleep government face birthday makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
