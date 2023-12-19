d5d991c3c4a47e551305f6dc14896b4935dadbb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:38:23 +0000

    show birth pen duty sit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
