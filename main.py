e9430082e49754cd1ebbdef9f29edfa7415e89b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:49:30 +0000

    nigga increase mass ride tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
