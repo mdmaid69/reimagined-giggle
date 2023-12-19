fc29cb1955ec959e4f90e59c12044b4766a66398
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:51:53 +0000

    noise nervous love snow tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
