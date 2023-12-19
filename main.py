3e560cb3c9bca74cd3f295072f8feec4cae7c95e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:22:56 +0000

    Ss thanks put cope tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
