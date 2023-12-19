979167f7cef1ae21decd08688c746cd2dd960cfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:01 +0000

    hide chance firm note load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_stack():
+        return collections.deque()
