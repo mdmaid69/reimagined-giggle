0d82a5067d8271d625168ecd44eade1ac973015d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:30:49 +0000

    visit bin care firm beside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
