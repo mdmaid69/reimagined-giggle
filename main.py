e8b69a00f0cdd9cd6a7239e4ff78b9908d2e7454
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:48 +0000

    declaration part low phone obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
