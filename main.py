89e48d03c36c4e9ea49ec9ae20d82cb402eb756b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:47 +0000

    dance now separate use trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
