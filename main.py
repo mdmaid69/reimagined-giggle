75818302de9696b78b2053434b89736b650b50c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:22 +0000

    here instruction niggaacademy workupto himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
