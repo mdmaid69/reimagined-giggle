ec05996beb6cd852b3d24e2ecaee790f58ab32e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:53:14 +0000

    love run either attempt getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
