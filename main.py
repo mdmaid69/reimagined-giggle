208d9c024642f8679e10af3c7a9acd1fd3267f44
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:02 +0000

    learn sentence about warning yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def multiply_numbers(x, y):
+        return x * y
