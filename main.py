b6414090b02890940e6410ef103d681134a9551b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:55 +0000

    public claim turnback join true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
