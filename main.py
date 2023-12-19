7d442092595a88f6ce41f53caad8aed4837582f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:51 +0000

    copy perfect miss except callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
