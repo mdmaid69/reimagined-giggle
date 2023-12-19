dd225b6a44804dc8c5af1b810316f78d35805b6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:50 +0000

    depend range drink charge fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
