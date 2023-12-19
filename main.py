a2e32cf19b04abbe5a56e7a608bee2ed0b53c670
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:11:19 +0000

    question dance gate taste finally

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
