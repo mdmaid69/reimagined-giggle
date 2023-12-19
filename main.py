beae560016dcdf68be23c60312301d5c53fcbb2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:37:55 +0000

    serve month demonstrate disagree high

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
