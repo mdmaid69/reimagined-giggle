5e8241beb04a080bd0d11fb1ec689ce86983920d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:55:31 +0000

    who letter spite fashion load

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
