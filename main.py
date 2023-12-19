efac50b1b4dc6b29319b264953cb16b75ef35753
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:08:59 +0000

    squeeze change take issue uniform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
