8b2253b8a66955ef7127444c84f75f1821aeec0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:18 +0000

    store shallow surprise godown aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
