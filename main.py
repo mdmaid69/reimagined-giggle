38f2a2c49b1c945638165b1ef105c2925ea0dffd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:24:10 +0000

    unusual change dish date kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
