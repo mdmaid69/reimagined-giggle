bf68768cd2a79c84cc258ce8902141ffab709af3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:01 +0000

    giveup bye grateful payment equal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
