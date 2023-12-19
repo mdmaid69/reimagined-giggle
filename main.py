f3cdc237422a6310a403b1b926e1ca1a6a4382e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:06 +0000

    plenty responsibility my formal frequent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
