e4e38cf107ceb1570b4724360debb4b92618e936
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:59:06 +0000

    storm now platform line convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
