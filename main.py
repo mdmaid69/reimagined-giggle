ca041c49b1512b7f1fc67891e1130a8519e1cc32
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:17 +0000

    protest interesting stick dance next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
