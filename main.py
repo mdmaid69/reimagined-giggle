84fb9b0315ad87b2796ffecd9615240f931de978
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:00 +0000

    spite separate Uu sweep far

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+for i in range(10): print(i)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
