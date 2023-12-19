5681ce1717ea8924ab8f99d9b22535058189ba69
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:59:25 +0000

    keepaway meanwhile thankyou platform satisfied

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
