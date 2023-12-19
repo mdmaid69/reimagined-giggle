f7f3cc3b329bae1b7cbf76865aca48de916f3444
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:49:32 +0000

    Nn education valuable tragedy pleasant

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
