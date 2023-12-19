a389cd38bc6d73c40b7324baab451d353092b8bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:00:10 +0000

    murder space it runaway forever

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_volume(length, width, height):
+        return length * width * height
