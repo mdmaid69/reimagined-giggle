8e8c8169cfdc1ca0de684dfaca633a76bca4709d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:51 +0000

    computer tape female lucky sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
