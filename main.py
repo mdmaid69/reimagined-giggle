ad229870f2b86c41f671265f2b7fcc155e031af1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:18 +0000

    weigh tip dream mail affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
