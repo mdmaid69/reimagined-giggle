32025f3f93db54ecf6656ab5daf2932a92b52fd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:03 +0000

    unite hardly sort reasonable forgive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
