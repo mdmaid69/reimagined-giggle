192291d0158cbf0a5963d17a4285b35d17a2c38a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:55 +0000

    work slight climb freedom strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
