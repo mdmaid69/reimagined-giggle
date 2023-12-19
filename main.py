437a7792e7f071b196442b54bd18a4d2a3f3252b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:42 +0000

    steady shoulder us tired terrible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-name = "Python"
-print("Hello,", name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_max(lst):
+        return max(lst)
