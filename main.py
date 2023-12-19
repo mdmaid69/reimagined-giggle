209454289c5a1a1cb5bea37b312650897d3a9e26
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:13:49 +0000

    past second deep busy their

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
