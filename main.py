1ff49515dfa087d70f51f233235b856f6dab2a90
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:33 +0000

    question hair length answer watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
