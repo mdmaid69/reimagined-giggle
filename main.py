af107c6a70afff2f879b813a59f1bb328cb6106a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:03 +0000

    custom another extra rest far

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def remove_duplicates(lst):
+        return list(set(lst))
