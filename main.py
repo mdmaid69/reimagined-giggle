758e5a1afe183462f93da0fbdcce6610793a1488
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:54 +0000

    go convince term everywhere discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
