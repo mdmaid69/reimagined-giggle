e5beb37d4a9644f453a4daf18fd7e34fa6670263
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:55 +0000

    further empty beyond them there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
