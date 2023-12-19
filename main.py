c1eaf1d742782f10c1b821d88d521f59fb959cf8
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:44 +0000

    file place shopping cover live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
