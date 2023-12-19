b4a0a5ca9c426824503079225c42139c713b27f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:33:49 +0000

    sometimes swing spend feature follow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
