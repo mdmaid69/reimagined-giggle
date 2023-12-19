4e2114338210dfa16710b7f5a9ef23f9d6ae907f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:10:03 +0000

    giveaway get risk slowly event

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+name = "Python"
+print("Hello,", name)
