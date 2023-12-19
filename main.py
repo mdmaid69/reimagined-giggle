2220a1eb9a66fa65cecb33e5e77f37c09635dadf
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:55 +0000

    doubt progress affect news waste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
