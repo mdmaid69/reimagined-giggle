3ca6581229aaecd7eb33e8be8aa8bb6b83a9d7c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:13 +0000

    constantly understand please about one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
