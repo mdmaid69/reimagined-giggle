21dd64d2c43c5fe77e15ecad9761265f781d4141
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:07:57 +0000

    finally find uniform Ii commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
