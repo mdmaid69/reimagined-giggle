58cc2a31d30547361dd27640674e51f83300b127
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:29:29 +0000

    intelligent intend geton wrap very

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def add_numbers(x, y):
-        return x + y
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
