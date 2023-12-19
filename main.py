c3b2c06009988774815fb510c26d8dcdd1b5b0fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:08 +0000

    question loan happen walk of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_bool(array):
+        return bool(array)
