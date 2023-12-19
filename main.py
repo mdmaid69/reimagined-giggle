1fa21e076c56fd6fd74f7d2d2f12343b4e589ca0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:34:59 +0000

    song measure attention neither appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
 import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def remove_from_array(array, item):
+        array.remove(item)
