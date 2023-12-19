e692d07c779450f566d57ac85823f2abc8e6f228
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:27 +0000

    section turnup night train unusual

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def divide_numbers(x, y):
+        return x / y
+def is_even(n):
+        return n % 2 == 0
