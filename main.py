02b413b9d54192f242d946959674e6c5058bb37a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:50:19 +0000

    moral improve your most uponon

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import re
-print(re.match("h.*o", "hello world"))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
