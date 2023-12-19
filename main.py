fb3925d59ddbd3b7b6c077f6e4c586101d11b4ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:19 +0000

    season tie moral cover waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
