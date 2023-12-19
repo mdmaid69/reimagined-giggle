b1d97a95ae67a624811158036a4b0b18c34659f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:28:05 +0000

    exam calmly acceptable come abroad

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
