b9c63da60add767eb1cf430c4a7fc36140eb5295
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:21 +0000

    equipment reason number set instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
