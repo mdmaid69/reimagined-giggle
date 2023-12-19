9adc6044013e4865cec096b9ae7a5f96e3f6e045
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:16:01 +0000

    separate deliberately head oil pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
