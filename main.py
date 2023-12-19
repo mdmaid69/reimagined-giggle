e6d6940fd6fbb482404ce8b321dcbecfc5fc4d4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:11 +0000

    world quarter land size secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import numpy as np
+print(np.array([1, 2, 3]))
