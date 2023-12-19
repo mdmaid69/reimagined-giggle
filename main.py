66efdc664471b0db6f5c3b08bf2684786370d32a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:23 +0000

    as naturally recognize wise separate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
