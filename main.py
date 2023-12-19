342c1df4c386b0e934e88c17c9d51b535484b885
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:31:52 +0000

    dream 571enough which complete boil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_min(lst):
-        return min(lst)
+  def square_number(x):
+        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
