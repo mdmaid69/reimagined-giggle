60382241f9effd0a69f75fc19c6e28bdfd636e1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:13 +0000

    real advice priority secret it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
