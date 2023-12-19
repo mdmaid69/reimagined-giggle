72e72913e12ad77dcadefec4706584d26dfc7857
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:22 +0000

    contents fault consider lead power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
