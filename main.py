29ec876b662aa606b8dddaead4f6646064bd0c49
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:42 +0000

    collection eager geton man recognize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-name = "Python"
-print("Hello,", name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
