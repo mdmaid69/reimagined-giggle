8206a4fe9e9e26cb4e4fc078e412187d5bc3b66c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:20:32 +0000

    apparently floor basis final geton

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
