fd2002b4cd97c0ecf4cbd5de7ae4df778fe0de7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:08:35 +0000

    relation juice estimate man confuse

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
