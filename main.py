df0c3217a3fbe0f831f129f51fdbf3cea3d89c77
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:19:41 +0000

    book apart shame decision record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
