b2817c3c2101f604b1511aa7b3d9eede72b52a7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:48:32 +0000

    shut acceptable workout voice special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
