1b370769a810705abb98f091e3d8693390518e62
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:02:16 +0000

    about pair do tour light

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+print([x**2 for x in range(10)])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
