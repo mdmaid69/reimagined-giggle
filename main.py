58cc6c12a6dfbaa04755013da1facbe3ee4bf8dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:33 +0000

    himself slice discussion land because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
