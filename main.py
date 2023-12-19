43946e82b33d03f64d734fa49b25b19aa2d77522
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:33 +0000

    accordingto transparent lower relation dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_power(work, time):
-        return work / time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
