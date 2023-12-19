681d819aeef2de535b4c874c4269fb8a2d08476a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:28 +0000

    base activity urgent ball behave

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_max(numbers):
+        return max(numbers)
