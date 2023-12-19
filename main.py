8a6dcb0bbf593f546f2baf38a68d87a8197f40e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:00 +0000

    thing official show understand strong

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_power(work, time):
+        return work / time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
