87b7c9a92838138cc154bf02cf9c5d60f9141ab0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:43 +0000

    switch negative turnoff heart regard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
