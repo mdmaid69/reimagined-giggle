bc5d2b52c394c64a29a75598cebca0de7256a1cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:56 +0000

    man wash believe rest can

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def reverse_list(lst):
+        return lst[::-1]
