1c5050242b57af971ea1b3bdde96bd8267daa9bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:04 +0000

    disappointed travel rain both fit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_acceleration(speed, time):
+        return speed / time
