8e61c16327a1aeb648c97490d022478c49598af3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:26 +0000

    fence greeting fork rain catch

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def find_max(numbers):
+        return max(numbers)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
