60a571cd4fb3dda481e8804636bab5ea3ca433e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:42 +0000

    past bath shout rely apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
-def get_array_length(array):
-        return len(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def get_array_item(array, i):
+        return array[i]
