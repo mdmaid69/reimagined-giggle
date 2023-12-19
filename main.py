92bbcc676b177d378ae925cff38567da3eb76ebe
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:02:21 +0000

    float careful friend connected touch

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
