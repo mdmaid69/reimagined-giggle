9bada110ba549bc63c570a3d048a64af5a288293
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:15 +0000

    Cc look steer 568energy jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
