412f9141ef354bd857a110efbcab217c70d07888
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:49:57 +0000

    breathe recognize time natural over

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def convert_to_octal(n):
+        return oct(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
