ade48e4368b07fecd59f94706018d28c9ca26ae4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:32:19 +0000

    they lean on sight tour

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
         array.tofile(f)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
