365377b34aa0d09b2d20c84a7a96e018132e8f0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:22:06 +0000

    able it possible Yy basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
