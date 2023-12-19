6d9e8fd0f790c37bb50387877c3c42a7900367d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:07 +0000

    certain before special normal material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
