4c7133ea2e3a7a99177f8f4ea48fef434ddcd196
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:12 +0000

    shelter expect back story upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
