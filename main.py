35f0f95ba21e1d19b8e6939e463eb6b191629799
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:35:20 +0000

    bear damp too exam public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
