16d190b935fadb931fdf615b41ae2ae72ce0fbeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:10:48 +0000

    ever person power programme creature

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print(sum(range(10)))
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
