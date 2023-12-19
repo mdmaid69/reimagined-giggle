16960da255636d339f3831b61eb155042a58ef5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:10:42 +0000

    scene crime conclusion work everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
