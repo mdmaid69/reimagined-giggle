9fd4ecaed0772d1dc9aedfc4c7eaf5f6e00f963e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:45 +0000

    contrast interview snow union by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+print(os.getcwd())
