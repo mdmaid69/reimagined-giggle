eb48fc9daa1ece056c7e919b55523ac93ce1916a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:48 +0000

    neck noise noone window ordinary

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
