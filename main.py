0973fc489a6446291fdd7db278c6b2f829d9f89e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:33:06 +0000

    treatment impression country project such

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
