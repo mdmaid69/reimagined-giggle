7bf0e26460f4088889bca7579ed4f3ca9f61cec3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:54:08 +0000

    dress half Ss peace body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
