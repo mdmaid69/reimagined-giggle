069e375ad697db6bfb7e3b6083eedf122655127d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:11:20 +0000

    file sink hers estimate pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
