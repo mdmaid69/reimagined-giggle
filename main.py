1233b0ac09f528b7b67a303473544b6c051532ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:21 +0000

    second value colour pinch fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
