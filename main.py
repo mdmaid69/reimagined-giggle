7c6a453a1b72db29d6c09cb7a9770577cb60fada
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:02:17 +0000

    platform spring manage modern flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
