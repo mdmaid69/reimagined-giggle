55d7b06157af1354df6d5e29a21ec03c8612a52b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:04 +0000

    score remember load case centimetre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
