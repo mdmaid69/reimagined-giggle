25101ad6438e8cc6b2ae69eec774a6e6a4e168fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:14:24 +0000

    breakfast cook home although grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
