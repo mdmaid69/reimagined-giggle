77cb4f92e5017c600d0db0e774b7f9b05966d9f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:19:50 +0000

    no point trick other clothes

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import time
-def get_current_time():
-        return time.time()
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+print(json.dumps({"name": "John", "age": 30}))
