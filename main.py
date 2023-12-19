2c0a5819e344f6a76263390b8c090dbbabc1a3d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:42:29 +0000

    bright acceptable book threaten pure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
