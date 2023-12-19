ce3516a6f4adc4e2c3789601bcc9f586688cc934
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:15 +0000

    die book shop exciting active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
