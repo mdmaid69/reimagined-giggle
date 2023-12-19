26af19ef0586d465fe0f5062361f982378d74b7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:40:16 +0000

    fierce similar break various getup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
