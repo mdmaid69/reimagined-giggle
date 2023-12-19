7ce1e1bdbd03f684aebf2c3140265b2dfe78019a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:24 +0000

    male radio answer past offence

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def find_min(lst):
+        return min(lst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
