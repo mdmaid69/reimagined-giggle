6109838d2729bafa00cb8b3a6fea6751647cbdf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:12 +0000

    scene defend whoever even impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
