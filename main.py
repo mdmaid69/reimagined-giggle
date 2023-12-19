1f21f26d4f9b144344a0f3981d704be26e7341a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:24 +0000

    deal argue cheek wrong ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
