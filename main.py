b6028190e03d973adb9ff1624b82502c8fa3e2a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:53 +0000

    lead worse connection television order

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
