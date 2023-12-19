133a4a516d84f2809458e886539f190c5c680121
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:59 +0000

    plenty sudden combination original contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
