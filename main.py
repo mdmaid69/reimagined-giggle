063334ffa658dff1baed9be6823688aa3383b7dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:45 +0000

    system extra hungry step understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
