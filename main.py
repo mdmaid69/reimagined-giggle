00fb9e203e293dc58ee31e933e318bdb1d3af250
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:50 +0000

    immediate really apparently regional practise

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
