9051ffa00625ce054acc04eaf532f3523f764bf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:55:06 +0000

    close actual back prefer angry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
