3cfe263058fe5cb0b839a9ca0f28aba38957999b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:22 +0000

    order powerful flat list breakup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
