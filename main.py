469a10b1a47c6670f6c9ca87a5406a5878d5c43d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:24:23 +0000

    book bin shoulder tune anxious

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import sys
+print(sys.version)
