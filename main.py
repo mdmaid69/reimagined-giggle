e207c84566f27ace4fd82d90409f5d5bef4dd87e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:36 +0000

    entrance left compare accordingto connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+print("Hello, world!")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
