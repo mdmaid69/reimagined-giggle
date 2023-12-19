b6041f58757a637cffe837646ec4628d974e7e6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:25:48 +0000

    past ability drawing conduct old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
