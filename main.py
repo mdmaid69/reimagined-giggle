d1faa9c2fccd077499873f1f759659a87d74d429
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:34 +0000

    watch attention at mistake goodbye

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
