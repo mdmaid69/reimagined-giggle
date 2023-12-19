7bfd30cdaeadc5a4ae9ac4c1cd33b43db64541f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:00 +0000

    shop complete build lookup brain

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
