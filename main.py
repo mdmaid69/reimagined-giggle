a48d8e805b1361cbd247d9c30313285bc473b8a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:46:25 +0000

    height obvious fine properly arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def append_to_array(array, item):
+        array.append(item)
