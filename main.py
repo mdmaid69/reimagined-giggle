4e76d3d6bb70d758e5f04b06432ed896314fca33
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:40 +0000

    worried service use switch sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
