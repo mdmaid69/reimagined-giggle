e447055909fd23265aa9cb8f3cf997fecbedecb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:56 +0000

    sort introduce paper song breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
