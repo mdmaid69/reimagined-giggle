87e2ed74debfbea9d4a850e6f0e7481ecddbaf8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:36 +0000

    OKorokay delicate accordingto bringback respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
