3d6d5f3af6f0bc82db93494ebd9553244b55ad70
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:54 +0000

    worth class comedown long draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
