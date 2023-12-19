2893ad9a18eb4f2e4156cd519f36682371b9be79
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:07 +0000

    win pretty finish dry limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
