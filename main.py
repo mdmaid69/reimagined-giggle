1facaf4b5632e59b87d137a053e8d8a553e3893e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:32 +0000

    murder keepup moral estimate shop

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+def add_numbers(a, b):
+        return a + b
