53bb4f38c01041be2ff24128853fb5a84d16831d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:13 +0000

    admit 571enough comment track travel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
