2a0e8ee30678c67a4f029ac4807c568383582488
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:07 +0000

    expensive surprised hit turnon extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
