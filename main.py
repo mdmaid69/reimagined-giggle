0226fd5172cea9aa1b5d2086b0baff00fbaf2c3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:50:01 +0000

    wish mass nowhere prove change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
