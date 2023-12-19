f94c43d92df4fde18b18428a39a642e582b240af
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:24 +0000

    quit previous valuable inside OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
