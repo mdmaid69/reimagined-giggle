622808467c64548870bf91f54a9ea2b6b38a62c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:50:37 +0000

    lead nearly succeed holdon container

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
