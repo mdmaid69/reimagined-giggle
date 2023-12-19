2144dcef43d48904c5060c5ef59f5ec40149d715
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:34 +0000

    nor great lead vanish gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
