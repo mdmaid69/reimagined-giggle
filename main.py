22d13c8d8dd2c0a6ffe4e102574ebe859fee2ccf
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:46 +0000

    remember fortunate undo giveup each

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_cosine(x):
+        return math.cos(x)
