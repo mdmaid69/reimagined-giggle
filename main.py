0946fd05d5f008e2a166c8b4cdfb421150cb1ed0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:23 +0000

    rare dead someone bored father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
