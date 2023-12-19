bcdd1fe7d48b6f0e18168a454c397eb2c45e2af2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:58 +0000

    recommend probably bang boring car

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
