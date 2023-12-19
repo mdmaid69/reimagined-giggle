934129bfb5c5c2d5cb4c09fd5c41ee13f106b843
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:07 +0000

    fail jump principle desperate precise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def append_to_array(array, item):
+        array.append(item)
+for i in range(10): print(i)
