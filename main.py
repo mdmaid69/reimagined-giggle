3490602f4e271606cf87b157a5d1e3f73eb72359
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:06:13 +0000

    side well turn safe common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_current_directory():
+        return os.getcwd()
