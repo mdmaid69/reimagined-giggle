ecf1937509fd4e033cc54a7c2074a022d6674e5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:39:22 +0000

    cut difference unkind eye official

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def convert_to_octal(n):
+        return oct(n)
