68c79f1a98ae8f2af710163d015594d88e9bb68b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:19:32 +0000

    activity fit label free against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
