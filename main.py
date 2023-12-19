322a2fe81f8c05aa9369b7d45bdfc42779648eac
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:57:52 +0000

    worried Zz marry Uu 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
