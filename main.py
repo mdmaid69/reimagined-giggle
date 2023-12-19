51a7e8da9eb274a862091ee2b40789c381b4df1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:20 +0000

    runinto top reject exchange middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
