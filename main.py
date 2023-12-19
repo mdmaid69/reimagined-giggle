0f328d8fecad228d220ca80ef707a7a1c04b5ddc
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:27:41 +0000

    getoff generous closely return dream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
