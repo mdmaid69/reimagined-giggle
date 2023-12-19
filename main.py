75650a2cec497e9488f59d3bf396742f2bac3875
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:00 +0000

    spill argue score flat careful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
