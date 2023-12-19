7cc57152bc428c7f7689a088c9e470df9459fb52
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:13 +0000

    tongue language debt attitude search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def greet(name):
+        print(f"Hello, {name}!")
+import os
+def get_current_working_directory():
+        return os.getcwd()
