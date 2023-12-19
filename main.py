e58ab74d99e27de15e665e4318ab2bc035f002f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:13 +0000

    about proper question Ss hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
