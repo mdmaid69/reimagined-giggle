0acb1e887cb3a4b208a30402529fae148f544789
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:46 +0000

    better expect left attitude judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import collections
+def create_stack():
+        return collections.deque()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
