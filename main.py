d33657cf5d9c2999ebf8df8a77275e202f44d6ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:02 +0000

    circumstance cut fear weather draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
