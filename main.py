dd996bb3e7005512e7102766188d50dd6a7bd497
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:24 +0000

    find gradually or promise lookafter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
