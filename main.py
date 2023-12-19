2c42b0b44b5201348cbeb8d19f0739d2d90305ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:48:57 +0000

    night sure word comment unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
