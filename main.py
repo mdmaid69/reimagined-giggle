8ca0c340de9cc6ecf2a46c2f41ab8ff6ed8b4b4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:16 +0000

    account exaggerate upstairs approval part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
