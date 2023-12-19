3db4ddc35b78146a17366ef8216e28699573a0a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:11 +0000

    formal control damp laugh part

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
