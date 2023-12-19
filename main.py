2bc123d7f0a5ad187fb0c39cfcf70f5aabd2c61e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:41 +0000

    accidentally congratulations fair stand dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
