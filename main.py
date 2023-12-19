71521ab92994c23e6be45ed8503cf800cf8a7adb
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:29 +0000

    embarrassed somehow acceptable pronounce depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def sort_list(lst):
+        return sorted(lst)
