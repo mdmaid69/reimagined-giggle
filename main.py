76a9ced43e04fb48d99bd707de497095276db03a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:44:39 +0000

    stir several scene protection maybe

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
