09e40742b1b98123dacfa236af1243658cd1b8c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:49:59 +0000

    closely thanks deep mark weather

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
 import os
-print(os.getcwd())
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
