4bf5b960792aba800897df75b8126b6d26273c87
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:33:35 +0000

    face tense himself home advanced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
