72bb4eae7d6889e1023385f709588854fedffde9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:20:34 +0000

    list getaway I yours fold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+text = "Hello, world!"
+print("Characters:", len(text))
+name = "Python"
+print("Hello,", name)
