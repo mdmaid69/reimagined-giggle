5b6f3ea2c12e7364fa44a0f08273934b1e95a75c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:57 +0000

    grade without interrupt form sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
