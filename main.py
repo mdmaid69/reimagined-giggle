8e375a7092b738824e8553f5194dad4aee2d32cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:05 +0000

    comeoff cross hit of method

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
