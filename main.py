cbca01e2e2a5c41a45dfa540b60620b0087b6354
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:33:13 +0000

    area may corner invitation freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
