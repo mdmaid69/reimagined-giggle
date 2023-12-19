0bcc111765e386b4c1cf18986e14cb4158a00191
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:50 +0000

    show false to scale tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
