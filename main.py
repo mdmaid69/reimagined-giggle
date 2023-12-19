f1fb5011c830c99fcd8756e5407ce20ac911e75d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:00 +0000

    prize plastic snow life wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
