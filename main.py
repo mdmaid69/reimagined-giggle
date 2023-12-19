595828242b463b35fcb2e34b34692554790af4ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:17 +0000

    seriously famous Gg write tidy

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
