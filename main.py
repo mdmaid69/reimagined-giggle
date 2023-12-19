9765b8a4194847d18fe7fa7e85487aa5ebb1277d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:59 +0000

    broken workout declare degree fall

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_even(n):
+        return n % 2 == 0
