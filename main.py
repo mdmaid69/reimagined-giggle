6d05b26c3d86c6d11dc13ddc50db2332f29a3498
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:28 +0000

    increase getoff before giveback chin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def cube_number(x):
+        return x**3
