5caaeae738c1ec4e4f7878910d4becea5accb49d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:29 +0000

    combination familiar list place visit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
