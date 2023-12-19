36911ad09139a35c7be8bb702aeed53a8e144213
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:30:41 +0000

    plan wake recommend medium fairly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def count_words(sentence):
+        return len(sentence.split())
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
