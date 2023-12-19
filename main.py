85d91fca09b8a7f0b11c4f8f75c232e55d0d2960
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:13 +0000

    street report extremely shelter control

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import platform
+def get_os_info():
+        return platform.uname()
