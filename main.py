a6dbcd147853e8a883bfe166a36ec2628d89f5a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:07:55 +0000

    plenty clean it pity work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
