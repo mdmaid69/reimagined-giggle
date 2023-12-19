3c1b1cb3818c1f95c263b5407eda309a38975e55
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:07:02 +0000

    imagine course priority name wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
