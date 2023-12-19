e27a5e1a3b9c599798544e08470ba2e43f5b5344
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:03 +0000

    promise female deaf serious likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
