f5c79d8e42c905d2763e6332dfc371b3e6f02f7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:53 +0000

    makeinto round call broad post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
