d4382b301322433924bccdb090c0e9e3a5517263
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:37 +0000

    neither nervous afternoon ground comeup

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def count_characters(sentence):
+        return len(sentence)
+import datetime
+def get_today_date():
+        return datetime.date.today()
