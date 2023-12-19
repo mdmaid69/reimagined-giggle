8e1734fcbe0b15a4bf75f51066e4bbe051c0f83b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:55:45 +0000

    pleasant dream machine choose basic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
