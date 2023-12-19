d62c3c3ded5cbdd4f9ab1c0d1ba9036d66c47de4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:11:34 +0000

    soil understand reasonable name attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_queue():
+        return collections.deque()
