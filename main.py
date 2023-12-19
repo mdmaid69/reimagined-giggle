5a2c317cab9a51e1ed723af4009a07792c8be8fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:18 +0000

    comeout range guard thinkof contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
