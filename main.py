6d006c29b90938a17994d0a55a8106a4963b2a08
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:19 +0000

    thanks wish like holdout tragedy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import tensorflow as tf
+print(tf.__version__)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
