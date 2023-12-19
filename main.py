a7f8024438b67a14cd986e37f9eaaa01076a5ddc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:00 +0000

    lookback(on) rescue butter persuade gift

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_sine(x):
+        return math.sin(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
