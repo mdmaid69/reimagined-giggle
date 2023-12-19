ecccf8136e9e8713b013072a738cf3f8be03fd20
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:59:45 +0000

    evening wave mood respect deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
