79aab4dcea8e29e0610c27508db23136c9073a81
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:36 +0000

    murder experiment point interview circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
