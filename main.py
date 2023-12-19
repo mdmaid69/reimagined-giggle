5260ef3369689582011cd1b936f8bbe40b104971
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:51:19 +0000

    beginning foreign meanwhile skirt interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
