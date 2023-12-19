015cb3948fec666bcd16eac23ccb2c2a443b749b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:27 +0000

    environment city formal actual Oo

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
+print([x**2 for x in range(10)])
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bytes(array):
+        return bytes(array)
