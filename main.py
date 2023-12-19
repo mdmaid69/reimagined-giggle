d9fec922a639e102d5e14bebf272fc922fb52ea3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:06:26 +0000

    half beat debt furniture matter

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-from collections import Counter
-print(Counter("hello world"))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
