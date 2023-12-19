ed085171063a1ca47263e260e02612c19662f8a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:28 +0000

    clean swimming exchange unkind whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
