a1bbaee6737e78ef09b53534320451760098eecb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:28 +0000

    anybody affect talkback nowhere me

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_queue():
+        return collections.deque()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
