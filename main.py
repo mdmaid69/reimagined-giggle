eaab774c23697c8d3725750eb79ae206f9692dc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:56 +0000

    mark project calmly bringback decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
