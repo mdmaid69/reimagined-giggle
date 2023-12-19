5f7488caa6cfc2907e011c4dd163d117b3ec511e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:58 +0000

    comfort sensible satisfied phone pay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
