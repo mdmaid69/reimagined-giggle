4f1f4ded871ac96304cb62c833fd30897cc5cb62
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:34:10 +0000

    Ss relationship team injury oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
