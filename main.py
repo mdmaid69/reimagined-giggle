ca81c973da92d5899f056695b98c01ab941cb416
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:49:33 +0000

    importance them indeed instruction earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
