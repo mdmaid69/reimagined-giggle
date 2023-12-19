6e9302d85a27fbff61fca773a2b90e1980f45c85
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:47:03 +0000

    hope loss fit close clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
