ae4d38084d292cc0e5c2acc15af3c761d8883d47
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:21 +0000

    refer break grateful act debt

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_acceleration(speed, time):
+        return speed / time
