f75876b3977fb6da2a462719a7d520013e10c625
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:27 +0000

    long 572enter normal protest hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def find_max(numbers):
+        return max(numbers)
