f3f720d700b370aebcd4a0db82d789ec3c938faa
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:11 +0000

    last lonely challenge lend hospital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
