e72ad3937def01a31bf89a8d2fcf7c31741304cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:23 +0000

    finally bury leave female successful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
