f23d4394650f6a592c13c861184efbe2ce175fb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:19:12 +0000

    Ss doctor underneath slightly last

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-for i in range(10): print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
