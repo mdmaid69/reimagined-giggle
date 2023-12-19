426c245138a37fe508c59a18374b7e4beb42636b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:17 +0000

    hurt tight actual finish serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
