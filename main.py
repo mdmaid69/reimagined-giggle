5daf62902dd67ffea4eeb49b3382e72247bff326
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:12:18 +0000

    second name side then demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_gamma_function(x):
+        return math.gamma(x)
