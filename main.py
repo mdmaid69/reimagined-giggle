1da3bd357886dd337f2ab9327193374c51774a34
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:14:05 +0000

    disappointed complain see hide mail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
