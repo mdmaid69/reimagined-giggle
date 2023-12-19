b8a070662a933a8b771b7783ad20eba97b14c8f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:21 +0000

    roll dry hard room create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
