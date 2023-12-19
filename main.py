c4a28e489ea36725b497874751295695af28b8b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:10:53 +0000

    hollow him week purpose try

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def remove_duplicates(lst):
+        return list(set(lst))
