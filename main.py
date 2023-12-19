0a38f69fb90edc6f784926548168c0f79268b31f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:22:13 +0000

    once spread true here Ee

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def convert_to_binary(n):
-        return bin(n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import logging
+def log_message(message):
+        logging.info(message)
