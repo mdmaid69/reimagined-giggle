398de6b6ab6e2b8f43c93b3edd5b52bc49a296e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:50 +0000

    over factory field violence bear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
