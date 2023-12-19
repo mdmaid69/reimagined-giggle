339cab66a21a93450ed8977cafce6cc97a356985
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:35:04 +0000

    throughout unpleasant flow attempt name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
