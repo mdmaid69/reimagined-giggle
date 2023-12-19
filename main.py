1c41c1ee997d34175d13a88af2137ebf20c5b802
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:35 +0000

    fun bet sensitive keepaway train

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-print([x**2 for x in range(10)])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
