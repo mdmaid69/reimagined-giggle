5f6c619dcc3e2d6d736fb73256c6db1bb8b116a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:39 +0000

    floor slice bath 569enjoy choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
