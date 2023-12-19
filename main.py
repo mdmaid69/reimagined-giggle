5fa0be964bd78e102423470ae7ae0dc36f11f740
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:15:43 +0000

    arrive design truth healthy shallow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
