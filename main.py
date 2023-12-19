134c6f37b4827d48b26b11c9e7103d461241c11c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:45 +0000

    trouble marry goup that towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def count_elements(lst):
-        return len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
