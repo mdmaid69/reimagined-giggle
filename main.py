8654cdb723cf6cdaac5776f07657a2d7e0b3acf4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:39 +0000

    moment display man hollow act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
   def delete_file(file_name):
         os.remove(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
