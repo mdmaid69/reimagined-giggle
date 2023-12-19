3be90d661aff81207e30a33ca68244229c64bbde
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:07 +0000

    demonstrate live gate rough invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
