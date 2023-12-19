52dd6b3cafebb5bd317b0ec77390879f55ac283f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:19 +0000

    sentence admit maintain couple neck

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import sys
-print(sys.version)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
