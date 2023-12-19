3dcac9d5b9ebec4b97565392282242e32f4b9c39
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:07:11 +0000

    fetch surprised reasonable memory flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
