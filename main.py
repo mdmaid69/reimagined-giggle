fa7031e5bac8ac98b0350a1faf8fec6b264cb2be
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:39:46 +0000

    as Ff charge weekend heart

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
