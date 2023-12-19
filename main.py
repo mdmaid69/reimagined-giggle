d6cece6ba5a79f07cc2203b8fc40c513e3178a02
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:14 +0000

    below wild shame learn open

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
