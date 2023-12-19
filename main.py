151bdaec3b9a4a1b920856e7b93d5eca93f2488b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:49 +0000

    formal hole direct load owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
