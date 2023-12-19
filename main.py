c529ec45df7aa322925adacaada8840dc8ff2b8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:25:09 +0000

    excellent cause hospital split doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
