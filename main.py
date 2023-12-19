25cbe8269396bb54425808a4973cabe4cbf78e86
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:35 +0000

    front clearly union pay nature

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
