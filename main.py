e19d880d867f069cadfc6d5b830712d8017ad08e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:57 +0000

    work accidentally circle fail music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
