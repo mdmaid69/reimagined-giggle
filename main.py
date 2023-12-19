3c44afffa6d0db64e17aa0a5e53d4abac01f063f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:51 +0000

    common promise collect ground ball

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pressure(force, area):
+        return force / area
+def convert_to_hex(n):
+        return hex(n)
