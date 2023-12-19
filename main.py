69b4a84377f83ac7cc104355485315b657bcdd46
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:17 +0000

    concern distance collection regularly scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
