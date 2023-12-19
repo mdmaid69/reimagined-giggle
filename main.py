3d1ff1b19ebff0fad321f9355cc38758b297d822
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:24 +0000

    sale holiday mass regular unite

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
