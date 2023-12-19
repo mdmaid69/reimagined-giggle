3a720d84f76f5f36ae2819f7f5ef1622f53dee06
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:55:45 +0000

    meaning art getback connection break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
