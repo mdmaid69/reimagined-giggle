27e18ff031a19ea75e85256b3ec853867f1c8707
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:34 +0000

    occur drive public trick thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
