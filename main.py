a20949cd2a9ac51ceb0a1087727d98b86c01f481
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:38 +0000

    surface object about bother finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def add_numbers(a, b):
+        return a + b
