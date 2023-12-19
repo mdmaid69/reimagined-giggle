e8792d84280f78fa75f01aae8a1911b0dea2f86b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:09 +0000

    nonsense and beat guide sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
