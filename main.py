529cbadb3de555b60b12fab4eec65a339b7b9e00
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:14 +0000

    use Ll opportunity early definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
