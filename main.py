6a48b6e213aa1524504a453546ed6112914fc82f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:16 +0000

    plant ilovedicks rent buy whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
