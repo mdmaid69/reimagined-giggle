db6efba78a0e177befd72f5dc478838fa5d63356
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:27 +0000

    rest later though write flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
