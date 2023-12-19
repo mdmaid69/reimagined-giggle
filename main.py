4dc7b796e0ad6876f3d583e3497c6b9d370d930a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:07 +0000

    pour half fill half trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
