6ab07424dd0862e901129454a05c86904babb58a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:40 +0000

    that lock male discover go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
