f6b5caf85fe2ff9f8dde9bc4e967fdabb58550dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:26:44 +0000

    anywhere however undo benefit law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_current_working_directory():
+        return os.getcwd()
