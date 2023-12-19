2151aea7ec47d30806b0a17d14bf92ff4a2c0d44
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:09 +0000

    combination with just especially new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
