c4a500a355a0207f7a412b1c46ea0738d90e38c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:21 +0000

    interview last violence stay piece

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def clear_array(array):
+        array *= 0
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
