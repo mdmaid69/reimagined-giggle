31fee32d4835c8f16f08889e970ea864b0fe1523
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:20 +0000

    physical belong empty join direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
