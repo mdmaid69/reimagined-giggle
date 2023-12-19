fb71411881a34fa946a49103131ea62bdbe47100
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:06:53 +0000

    statement comment bother weigh club

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
