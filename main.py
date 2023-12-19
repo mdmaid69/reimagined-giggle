fa325e32999abdeb3d4e0e8e216c99c16506a196
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:25:13 +0000

    deep recovery keepout use miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
