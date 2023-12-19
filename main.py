6bc722484887781b8d5cd3ee0063fa25b90efba1
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:22 +0000

    workout discussion sell rubbish meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
