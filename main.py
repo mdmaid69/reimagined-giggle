5e92bf8e007b712b0b4bd612acb96fbd2d1d4705
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:10 +0000

    underneath film miss strong pull

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
