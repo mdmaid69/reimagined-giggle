23451a7e98b91960f26f106af29abe8d01d223ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:34:15 +0000

    clever news something clean scared

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import array
-def reverse_array(array):
-        array.reverse()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
