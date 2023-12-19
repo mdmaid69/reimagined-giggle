ea0041be55e9df9b4387e5401fa869b122d19528
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:44 +0000

    loud neither physical authority control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import platform
+def get_os_info():
+        return platform.uname()
