8be54087fd9925c9a45d6f25f1061ac16cf2fe5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:26 +0000

    copy couple identify seriously sister

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 import collections
 def create_default_dict(default_type):
         return collections.defaultdict(default_type)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
