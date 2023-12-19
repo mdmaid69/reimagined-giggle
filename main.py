974024464f5bdd4b6dfc4640fc0869c71ff7d137
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:32:43 +0000

    bite majority honest equal rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
