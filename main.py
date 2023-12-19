5262d835280b1c270eb4375a9509ff8e7799e95c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:40 +0000

    conclusion wise man tap shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
