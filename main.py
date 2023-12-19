d4291822a26c50bd9f49a2b962761810313c1bfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:54 +0000

    emphasis promise into range except

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
