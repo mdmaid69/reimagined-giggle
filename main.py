995493c7aff5729b532ca42a044569d925a700f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:42 +0000

    raw politics return past involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_volume(length, width, height):
+        return length * width * height
