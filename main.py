1ec2f9b2066cb9eddc76460f83fa295675505014
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:31 +0000

    will case drop affect black

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_binary(n):
+        return bin(n)
