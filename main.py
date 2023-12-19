573649b281b7b08b09a3da2596ed38e34be3bb4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:16 +0000

    weak thorough doctor train completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
