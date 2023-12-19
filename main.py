962ec1ab13e7bec6c1e19dfe560997f51a02dc14
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:48:44 +0000

    meaning worst original year mean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def find_max(lst):
-        return max(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
