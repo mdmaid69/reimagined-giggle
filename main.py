692f85d92ec068d380d27bbe4be1abb7fc09e722
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:33 +0000

    instant prepare afternoon score favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import re
-print(re.match("h.*o", "hello world"))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_area(radius):
+        return 3.14 * radius * radius
