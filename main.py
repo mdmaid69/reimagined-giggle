aa1da6470ec9a16efd3a34b441a8613817a19acf
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:59:55 +0000

    tap set forward record century

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
