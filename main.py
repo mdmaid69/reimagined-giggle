aadd57bbf030903b95c68533c72cfda0c172ab75
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:50 +0000

    half national ourselves cover brown

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
