009c5dc235c02185d6ea0e16cb6fec9ad95a63b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:54 +0000

    contrast destroy sign plant discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
