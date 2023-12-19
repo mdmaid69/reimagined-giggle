1661b2516cc54174a08a5a9818429856b70b399b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:44 +0000

    pressure lie free detail problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
