eef01d746e0035aa5ec09d0e925f83641abdc9b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:20:11 +0000

    connection between change bring deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
