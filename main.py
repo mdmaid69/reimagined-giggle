e96b9470edfd396c38c603b314703aef0fec6c8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:58:56 +0000

    opportunity bus 564encourage proud tradition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
