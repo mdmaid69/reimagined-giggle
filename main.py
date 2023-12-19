9e97835bacfe4478dcd11cb6acddf0957f1f47b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:24 +0000

    568energy long forgive grow much

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
