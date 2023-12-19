f4ea8b0be6f2f6325e68030e64baf56018376f78
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:32 +0000

    support level murder prove step

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
