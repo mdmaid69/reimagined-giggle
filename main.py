945fa3caa687e13ae6c0859d8b405d5944e5169b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:45:04 +0000

    pencil mix trust foot meeting

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def count_words(sentence):
-        return len(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
