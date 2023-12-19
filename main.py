95514c8201e0a60fa51e2f8e580ae4d943587133
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:30:33 +0000

    shape twist excuse wish target

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+print("Hello, world!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
