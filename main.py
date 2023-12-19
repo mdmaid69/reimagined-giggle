ddc2411fbfaafa11f56ca45ffdaf65bb8ef39cd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:25:00 +0000

    science rarely fail petrol everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
