e57ac4df8c7a67d5a4514dd768869d5f61f121fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:36 +0000

    ourselves common talk close frequent

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
