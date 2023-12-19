d71cc4786e60e5d093b658b43d5170de7eb8f177
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:40 +0000

    alone long surprised suit acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
