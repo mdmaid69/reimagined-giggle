a5b0939315526666d66f8131b73f20d36e637d8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:24:36 +0000

    dinner little throw left serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
