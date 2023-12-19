06d19e9bc9d8f0fa4a8bb88081b2be515725b4f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:06 +0000

    special match authority shop hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def clear_array(array):
+        array *= 0
