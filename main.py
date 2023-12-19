eefba33a31b157666f5b3bc1055539e3df20ff58
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:36 +0000

    butter uncomfortable well message no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
