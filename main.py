abefbdf3d34370ef664d0c98670bbcea1c4d57ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:57 +0000

    bored listenfor consist convinced charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
