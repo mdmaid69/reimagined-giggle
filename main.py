d534a62526f1ec447b0ac62aeacd040f9386bb19
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:25 +0000

    used cut me talk car

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
