5398385874b5996645d5d7486ac67a12272264f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:23 +0000

    street black follow keepaway free

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
