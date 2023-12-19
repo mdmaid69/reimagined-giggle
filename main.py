971d4ef8bb61bdab63822333d24772a58b60c93e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:38 +0000

    disappointed open 565end taste trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
