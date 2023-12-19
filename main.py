88455a339aa2b1a30f707e10ded2b620bcec3bb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:36 +0000

    reserve tidy sensible concerning dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_as_list(array):
+        return list(array)
