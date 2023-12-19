4f2835303e4335fa230f19b2e366392ea50592b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:25:28 +0000

    help scale especially bringforward however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
