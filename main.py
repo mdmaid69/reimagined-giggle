8b0d6f95ab48d79870d6892b53b53802ea6056c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:28 +0000

    prevent Itself niggaacademy drop opponent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
