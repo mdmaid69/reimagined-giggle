0f727b8cc19c7e6b6191b93b3d231743725a331b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:36:19 +0000

    desperate weak steep arm invite

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
