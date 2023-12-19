e485b842d36f4f803df090d0bbba91de10c71bbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:25:19 +0000

    dress upwards fork disturbing maintain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
