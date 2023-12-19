3ff90f652df01d95cd64961f7e99ea1187dd40d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:39 +0000

    roll all keepout soft us

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.ctime()
