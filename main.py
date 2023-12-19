47fc58f0450f70072fb4677d7241f8a437fb2a58
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:31:11 +0000

    into impatient awake odd occur

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
