80751931d4853f2814aa3b2b8a87eb38b33c7915
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:43:05 +0000

    for best line unpleasant scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
