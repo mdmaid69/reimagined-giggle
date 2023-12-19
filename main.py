13874cdb56b52c3ae3a6674baabb1653983d866e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:34 +0000

    night stay course ignore steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
