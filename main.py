bd65c139004f95681f531d787d710647e1e18ccf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:04 +0000

    show bringforward sick giveaway lift

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print("Hello, world!")
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
