b0308de2d6ac83355053a4331bcb0b17600fc659
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:41 +0000

    recommend content surprised plan strike

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
