48405ffdfccd729b7e258f016e62f07298068e4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:53 +0000

    complaint border declare group image

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
