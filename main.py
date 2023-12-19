8952ce98df995fe2798bce0ffb6fff98cb66b5f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:16 +0000

    run method change escape image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_power(work, time):
+        return work / time
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
