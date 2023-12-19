30aeecf18ad07a7d89a8b11d7b3c35f8d36c4483
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:37 +0000

    unable vision cruel fold comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
