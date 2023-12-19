5076a026c86daa9f11efe7eac385034b85982655
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:17:39 +0000

    science light properly clean suit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
