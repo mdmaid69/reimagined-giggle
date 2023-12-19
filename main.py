8f4ee4b2efe5b5c92f58da3a8f3fa65e530cb697
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:31 +0000

    gooff short wet benefit owe

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def remove_duplicates(lst):
-        return list(set(lst))
+def square_number(x):
+        return x**2
+def add_numbers(x, y):
+        return x + y
