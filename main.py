c247760ef1260353b4fca25f8263954ade876581
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:36:03 +0000

    late aware slightly poison separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
