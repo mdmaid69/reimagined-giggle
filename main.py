1f9c352e852489e7cfc20f412ab464a279eff384
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:30 +0000

    shoulder angry help loss close

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
