f8114a35f7c5aef708af02979bf1e1d1d2316785
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:48:40 +0000

    guess now pleasure heavy bridge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
