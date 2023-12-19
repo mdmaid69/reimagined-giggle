ea6a7d6fa4a82c7a80979e2476d37d5e378c85b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:27 +0000

    drive which call power trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
