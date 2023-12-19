c0e54124542c6437d71b995a6b356b58ce7f07db
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:46 +0000

    Gg once search count guilty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
