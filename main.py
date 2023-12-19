8622139b8ea106aad1e3f3bafa2a28273050c88e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:48:03 +0000

    win trust scared silent illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
