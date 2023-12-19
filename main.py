579155799e0e41419195feac2530c4eaad16bed3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:16:12 +0000

    put niggaacademy far own keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import time
-print(time.time())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
