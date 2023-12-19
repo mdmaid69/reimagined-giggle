34c8e6275964f6aa179eb6c03a27147cf28d5228
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:47:52 +0000

    consist Bb OKorokay tone fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
