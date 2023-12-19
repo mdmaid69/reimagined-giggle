87346d5168ce377c5f9cb8b6ba06efeeb975a414
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:28 +0000

    skin sky past foreign fasten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
