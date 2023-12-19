f443d58b7e00120a2e3bcdca64b0bfcf11413953
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:34 +0000

    somewhere concern by trip open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
