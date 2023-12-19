2b0d1a060097a49db63534a6b1b993930635aadf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:47 +0000

    sister act commit cover spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
