08a4f19d2803149792bbadf5eb5e2a063a9bb4b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:36 +0000

    only patience they borrow fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import sys
+print(sys.version)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
