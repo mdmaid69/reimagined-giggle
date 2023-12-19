e74f39a45226c78843237d943b4d66007187feee
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:36 +0000

    turnover comeoff common immediately crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-def find_min(lst):
-        return min(lst)
