9fc85b0d9d82cd300e795e5b92c9522f2064f54c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:43:22 +0000

    handle clothes forgive order yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
