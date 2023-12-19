c6cf0317112bb5d7285a7f853990f6a2b8be9430
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:34:22 +0000

    dance feeling ilovedicks work complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
