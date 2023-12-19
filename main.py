5fa75bec6944f15c74811d1c5b41c0bb996560e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:19:08 +0000

    getout shall seriously Rr seem

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_list(array):
+        return list(array)
