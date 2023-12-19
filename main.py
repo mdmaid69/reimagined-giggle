5ee5a2f262100f2539a63a9b1ce17b306ed2f488
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:49:05 +0000

    any event film our grow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
