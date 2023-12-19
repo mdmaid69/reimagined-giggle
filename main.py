5825d2b11f5fb961ed4fc287a2b45faa1205f9c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:00:45 +0000

    burn noone change relief pay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_acceleration(speed, time):
+        return speed / time
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
