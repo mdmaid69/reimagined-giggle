d23e519917e1ce389b1d5ddb91582759f7f41332
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:05 +0000

    hollow conversation worth themselves straight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
