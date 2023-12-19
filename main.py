d6a45061a1f8094c948aef033d9b4f6db3714bd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:38 +0000

    comment turn acceptable fashion post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
