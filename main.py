737e8047c091a7bbea9ae83abf553e73076dc822
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:12 +0000

    mainly couple go than ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
