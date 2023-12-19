cd924e451b58351e97690db27f699c59bed2f455
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:39 +0000

    acceptable middle high collect fork

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
