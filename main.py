7db5c6e6b598f0c6de8171dec29273553ac5a3ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:34 +0000

    transparent choose include unkind convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
