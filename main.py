45ab9d861a471f7f629403aa108de424f2a4ca53
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:38 +0000

    example surprising take ashamed excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
