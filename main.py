b04091c5ad28e4d7d591dc813721a42cdd60da54
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:42 +0000

    false space copy police similar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
