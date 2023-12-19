2249f625f3e09913ac625f849a6f0d2c2c1268d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:48 +0000

    insist Ww train arrange official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
