b7a23ad9cd1efbcb542bbb976ec57f7ba570daa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:53 +0000

    spoil arrangement possible seem duty

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def count_elements(lst):
+        return len(lst)
