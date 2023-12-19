62d3b449df57a7d99472923a5e9c9d115c5b1f82
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:17 +0000

    apparently properly cover pleased but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-def print_python_version():
-        print(sys.version)
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
