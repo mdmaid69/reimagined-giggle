8b623b49e6111e2a6c965cdee133284715bedd08
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:24 +0000

    whoever mystery operate makeup scene

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
