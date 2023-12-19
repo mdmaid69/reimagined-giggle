bc766047ee96a36de6a39063d0b2999e99623772
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:13 +0000

    flight correct loss occur wealth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def square_number(x):
+        return x**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
