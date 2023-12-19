439292636a7b57b26d8d74ae917a057f30c7d70d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:35:42 +0000

    hole run before respect embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def remove_duplicates(lst):
+        return list(set(lst))
