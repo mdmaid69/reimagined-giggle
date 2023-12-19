9ec377b3a4cddd6ab339e06b799c8856ffbf4e79
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:09 +0000

    large imagination judge land bush

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def calculate_area_rectangle(l, w):
-        return l * w
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
