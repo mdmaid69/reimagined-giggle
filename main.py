a6fe0907bdccf2ec2ab22d02424adb3c321ab361
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:46:56 +0000

    estimate public announce mood possible

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
