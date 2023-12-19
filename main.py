465abba535e55a7d0beed80b9313d9c31793c7e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:42:17 +0000

    join press protection Jj bet

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
