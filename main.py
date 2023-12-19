37871a42b9645aab4c7fadf0632f917b6d27edc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:00:44 +0000

    any less science runinto trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
