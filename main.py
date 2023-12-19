ce1f165261e251112340ec4e1fcaae51c2af2b41
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:16 +0000

    study need shall comfort state

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
