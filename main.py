43a4607be57e19dac189af767fcfaed94f2c8f7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:29 +0000

    towards distant someone government plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
