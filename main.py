88b81df4305cd4e17683cfa8f17cf902ebf9e4d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:03 +0000

    argue doctor description fence stretch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
