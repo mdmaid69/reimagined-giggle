bf0f4818b5a06ee351c96ceab60f3de0d96ddb19
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:07 +0000

    wave load situation fierce convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
