1d4f4a4b609fdd534758501b2e0deb2ba00d62b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:31 +0000

    gooff heavy earth shock colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
