e71ef787634bffac188b698d7c4de2ba8dfeff03
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:28 +0000

    expect satisfied forget point split

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def calculate_pressure(force, area):
+        return force / area
