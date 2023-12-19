c0b2b79d41f6ac12da66e49d0802aa99bbe0e966
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:49:12 +0000

    quit borrow advice combination splash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import getpass
+def get_username():
+        return getpass.getuser()
