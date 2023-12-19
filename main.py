ecbc19d95210eeb698c3689151b6a6d139e4991a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:38 +0000

    alone whom production we dare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
