fb670b27b03c8b0c5afb2fe2977b395ffd65ebb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:58:03 +0000

    weigh downstairs mistake nonsense step

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
