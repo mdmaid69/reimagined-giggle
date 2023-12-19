81bae4cfa4410d1812962e6185d32d1dfbd1779d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:13:49 +0000

    slide distant insist basic 566end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
