fe050f52e1f57aab801edd184514a24f95c0a462
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:38 +0000

    seat no afraid label film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print("Hello, world!")
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def count_elements(lst):
+        return len(lst)
