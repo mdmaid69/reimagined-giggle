e1b25444a491e94ba7e574619e96e68e177a6bd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:35 +0000

    account share guess game creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
