26f783ec66dfa9aea06ff8212c168cb3fb1faacf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:54 +0000

    tend scream suppose justice easy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
