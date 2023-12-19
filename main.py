cf6f7656011ecf87c97c8d35f011a8dea2ae9594
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:00 +0000

    fix stop hole cap push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
