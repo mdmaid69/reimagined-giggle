3d8b6ec5b669058eeebe3836f5bd2888f810a194
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:28 +0000

    exact basic heavy cheque target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
