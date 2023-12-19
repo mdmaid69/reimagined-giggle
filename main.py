e5b144874ea4a43334ee674204f6167865faa567
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:46:49 +0000

    afterwards file turnon tax gap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sys
-def print_python_version():
-        print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
