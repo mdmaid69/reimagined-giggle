9e027ff63857ad0b37889381d8e8733ce9d6c42d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:03 +0000

    work nice mix first rain

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
