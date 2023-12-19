72a28b96ed54bde556ab9be9f93d67d6ca71caae
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:59 +0000

    turnback pay tread black contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import numpy as np
+print(np.array([1, 2, 3]))
