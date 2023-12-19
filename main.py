8c8ba988a84b25c11bc179ee2148eeb91a183a0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:44 +0000

    dear trick show 572enter drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def append_to_array(array, item):
-        array.append(item)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def convert_array_to_list(array):
+        return array.tolist()
