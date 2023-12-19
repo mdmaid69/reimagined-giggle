352dd7cb38e7fc49b2045846056bbc039569f620
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:16:11 +0000

    importance damage production responsibility importance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def square_number(x):
-        return x**2
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_index(array, item):
+        return array.index(item)
