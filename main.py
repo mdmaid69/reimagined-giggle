081693dc5a8826445fdfcb9143966f1dbb57a4d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:26 +0000

    journey guess getback least legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_characters(sentence):
+        return len(sentence)
