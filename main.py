5cccf112c48944bce258f28c89dd5a1904f82368
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:19 +0000

    sit light step dish meal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
