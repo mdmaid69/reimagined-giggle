cd79f9adb887324b8993bff037d884f7bc25d733
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:47 +0000

    runinto share yesterday much sometimes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_octal(n):
+        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
