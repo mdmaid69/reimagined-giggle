d4904190242a17163b413057d1364e6f3502483e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:21 +0000

    tool later row their bear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
