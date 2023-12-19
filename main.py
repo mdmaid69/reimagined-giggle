285f8b0d59605161b02737337dfdcfc61fb9dc10
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:09 +0000

    female light extra need Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
