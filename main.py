8804df831068c3d364fb469c040fb3f082b375cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:53 +0000

    inform afterwards mention neither hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
