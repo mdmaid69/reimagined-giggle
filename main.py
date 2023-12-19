cf57b0b0d47c62d4eaf41df8fc452805d24d9d8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:53 +0000

    borrow sport state deliberately collect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
