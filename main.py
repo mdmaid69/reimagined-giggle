926d1c914635d31c709641aa074b6eb3ad7a6132
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:31 +0000

    threaten dull create wrap live

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
