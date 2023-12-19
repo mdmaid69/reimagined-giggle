477158855d096aab4175b1aba91093186e7088d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:21 +0000

    unusual tidy welcome talkoutof thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
