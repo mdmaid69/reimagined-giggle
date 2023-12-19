c5ea1fe7018d3f05bedc749c9acee3c7a6a48f40
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:13 +0000

    write train cheat excited ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
