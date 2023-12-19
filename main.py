fb7ea8bf28808f524159b9be83020564bc59b33e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:43:07 +0000

    change us poison big achieve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
