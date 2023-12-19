5269960ee41eb008bc4938223ef7d16ed2acd986
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:13:28 +0000

    distant great sorry whisper clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_working_directory():
+        return os.getcwd()
