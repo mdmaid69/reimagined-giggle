034386f9a6a768466cc9d1a48d73e0792a0b1cc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:55 +0000

    treat time taste emotion getback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+print(math.pi)
