4271ab059262fa1aa806251ae18672b1f6647094
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:15:13 +0000

    shoot mistake circumstance breakdown exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
