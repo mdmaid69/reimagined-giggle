9c81923a188f23a500784e939440355d9bd1e486
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:19:46 +0000

    university fast 568energy skin more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
