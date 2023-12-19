25db415c7ebd3d73f5c48d2a80a6d45f8f5223bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:12:30 +0000

    lunch create hate between basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
