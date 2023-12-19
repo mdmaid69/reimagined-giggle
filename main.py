714f69b42bbd8385d42c939115f459857f0ae0d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:53 +0000

    sign however card Vv although

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
