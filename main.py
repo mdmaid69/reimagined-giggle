fbb7b9c35347d5aae98e1b37f37dc08170b1e053
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:37 +0000

    eager news hurry attractive occur

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
