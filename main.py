3fd2a14fbc75a8556df64ed6e374a7d300b9ebd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:21 +0000

    holiday skin kind present share

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+for i in range(10): print(i)
