173b78613f5907b40b4f247af1f7597c1ccc896d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:22:08 +0000

    by tight thorough aim miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
