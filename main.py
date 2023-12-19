237b94b724f5e94b5f87c7bd3e8f3b5fe560ae00
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:48 +0000

    nearly instant generally squeeze explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
