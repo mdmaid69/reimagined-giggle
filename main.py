a79d92849eef48cdfff8e3501bbe04a812463b13
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:01:15 +0000

    once slide however rush damp

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+print([x**2 for x in range(10)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
