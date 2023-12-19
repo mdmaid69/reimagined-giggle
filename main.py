67ef75f341c1d55c1fce461e23066f39365710be
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:56:20 +0000

    round raw majority provide doctor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
