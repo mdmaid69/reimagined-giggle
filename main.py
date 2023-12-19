54294345ae8ff81b471f7fc7001a4f95608c213c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:54 +0000

    like community sweep sink perhaps

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print([x**2 for x in range(10)])
