09d38e662ec12d526d8b19a4f7872875ad13c37a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:25 +0000

    in store else reply rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_min(numbers):
+        return min(numbers)
