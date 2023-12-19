7d21c4f1b92327ce6639e3da4516ade077800e1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:52 +0000

    furniture somehow light accident gentle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
