a3ee1eecb233bd4d5561b88f0a04e3b8dcff9b35
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:07 +0000

    sting height bang intend circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_even(n):
-        return n % 2 == 0
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
