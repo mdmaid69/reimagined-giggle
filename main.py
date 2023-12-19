3d4cd94653cbb4f477b303af04a640e9c366844b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:56 +0000

    now flat need mess behind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
