7e6faa1a56538b9e8d7f4bb2ba5f274b2d754798
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:08:14 +0000

    stare approve culture squash separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
