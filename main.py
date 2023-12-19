fd1c84d1065e13b86509aea2ff9b5aea9f40819b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:37:57 +0000

    switch hurry limit thing light

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
