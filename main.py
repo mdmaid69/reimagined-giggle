b41175a2c443343ad8ef1177a692c19cbd0b5957
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:26 +0000

    shop nothing agree feel smile

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def cube_number(x):
+        return x**3
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
