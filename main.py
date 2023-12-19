1b6475680e71d3811392319a6dc96f28f74e75a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:15 +0000

    ring people swallow underneath coat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
