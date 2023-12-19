816e55ff1bfb576fc3e84d8c824a3509666283d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:40:39 +0000

    male closed shrink clean bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
