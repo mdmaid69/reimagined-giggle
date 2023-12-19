551966ee6a515e57ad7a04343abe4ac1a046706b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:21 +0000

    goback(to) great shut spot pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_current_working_directory():
+        return os.getcwd()
+name = "Python"
+print("Hello,", name)
