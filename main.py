72178714c2c79b1aac3a2e88a16e030c971bcd3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:45 +0000

    where afford out replace friend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
