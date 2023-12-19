47fdc9d4c0e875e4f55a5eb4090ea6ff1a2a9cd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:16 +0000

    bad main lend embarrassing lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
