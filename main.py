bb127751455412c8fbf07cca24e15b65e7b4a80a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:07:45 +0000

    left unkind private shade base

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-print(random.randint(0, 100))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
