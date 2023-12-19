ee9c7a7f4613013a3a383b521f0868664642b80f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:45 +0000

    free charge connected comment favourite

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def square_number(x):
+        return x**2
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
