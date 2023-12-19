f750a017115c8fed01576f0be66d2605039c5de7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:00:09 +0000

    unhappy march such convinced company

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def reverse_list(lst):
+        return lst[::-1]
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
