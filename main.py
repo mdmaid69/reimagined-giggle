7d830e48085c39806727f69e7b2fd2ea8f6ac5db
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:40 +0000

    fix valuable unpleasant plenty study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
