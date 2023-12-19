db40d19a601f3fb5ceedaac096a20fb3ab12c5b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:27 +0000

    dry both cheek food for

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
