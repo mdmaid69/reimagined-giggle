3ac22a8a8a8a6b052b208b6e6e5bb3ce217b0278
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:10 +0000

    and transparent like miss side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import os
+print(os.getcwd())
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
