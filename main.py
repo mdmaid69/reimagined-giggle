59d3296bc280828e22b0dc25e85e5cf7d5b78769
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:07 +0000

    smile prize receive likely habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
