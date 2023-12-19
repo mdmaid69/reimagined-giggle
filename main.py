9aad61c8c1446cfc70fb7336c99cb3725b8edbe9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:22 +0000

    healthy separate fine chin shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_user_list():
-        return collections.UserList()
+print("Cube numbers:", [x**3 for x in range(n)])
