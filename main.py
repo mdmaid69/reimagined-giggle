1e14c03497d5cb0a4d80bb0a9028ae03cbdfc51f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:19 +0000

    club realize completely grow arrange

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def find_min(lst):
-        return min(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
