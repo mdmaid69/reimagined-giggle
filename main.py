8385b195111cc8bed83bf5f472e7f0295495101b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:22 +0000

    check nice cheek risk low

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def convert_to_octal(n):
+        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
