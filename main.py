608a01915507d929cd244aa9db4267fd3fb6a8a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:58:58 +0000

    air fast kind zero present

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
