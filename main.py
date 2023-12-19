358acd12d6ba869a0db9fa275a97148460272dce
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:11 +0000

    probably sharp soon bang already

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
