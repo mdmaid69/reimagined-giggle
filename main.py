1f3fba15f64345ac5c2dd5a89d49b91d7ecd64c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:27 +0000

    crazy dinner know command explanation

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
