098cbab8b81d1fffa4eddc84ee4deec4bd035107
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:15:51 +0000

    inside may drawing one acceptable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def sort_list(lst):
+        return sorted(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
