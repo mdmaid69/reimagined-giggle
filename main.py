93116fd9995fcddd5694cf890536d9bd210688c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:42 +0000

    pass neck hope walk unlucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
