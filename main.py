f92766aa592bd787b6c6cac8f5eec91c91bc1e42
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:39:37 +0000

    weak sale character everyone across

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
