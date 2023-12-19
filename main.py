b51e82eabf160ac895e2ce5891de3d804e3142a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:37 +0000

    take smile suck hurry advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
