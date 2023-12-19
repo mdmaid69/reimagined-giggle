2989ce4c733b98c5c99f1718cd62dd5d3c18cd10
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:16 +0000

    half soil regional fork underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
