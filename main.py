2dc2748482e3baa276a1964bd1e044e53db9960c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:58 +0000

    drink thumb notice holdout artificial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
