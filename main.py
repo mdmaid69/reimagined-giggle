ac4fa6473486c4b7818550dfdd919727fdc7b27c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:13 +0000

    surprised shout pair moment priority

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
