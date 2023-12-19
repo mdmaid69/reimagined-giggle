93ea123158840ff506725725fd8e9926d263f0cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:29 +0000

    rid everything strike offence demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
