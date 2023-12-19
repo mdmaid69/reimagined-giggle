492c3a81f80a43dd9f23b92175e826eb39a481fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:27:20 +0000

    area good profit morning another

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def is_odd(n):
+        return n % 2 != 0
