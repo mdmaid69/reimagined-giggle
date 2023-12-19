9bf1d3c8cdae7156f73bf937cec854b2f4dc81e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:49 +0000

    sometimes turnon race suckme summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import getpass
+def get_username():
+        return getpass.getuser()
