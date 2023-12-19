0de0dc449c7444e452717332922e4e71854a810f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:11 +0000

    mail please full charge mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
