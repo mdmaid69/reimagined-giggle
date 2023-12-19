2521bf7436147afb7ddc8db85127c4654e6cc15f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:06:55 +0000

    everywhere base they exploration house

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
