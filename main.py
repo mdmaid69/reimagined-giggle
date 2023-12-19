89e0a3fce4a8e46df245a39e65e112f5a8eed005
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:27 +0000

    introduction visit better warm herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
