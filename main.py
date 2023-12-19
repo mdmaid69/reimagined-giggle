d1bbf412c2b6f13cf0c77f2c844c1b41226e0047
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:32:25 +0000

    strange cry emphasis people likely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
