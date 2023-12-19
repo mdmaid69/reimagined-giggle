ac1779c4512f068a9b0cb2de0ddbcaedb95a43dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:03 +0000

    breakout consider my grow variety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def get_array_itemsize(array):
+        return array.itemsize
