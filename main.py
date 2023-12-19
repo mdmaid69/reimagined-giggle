37663145ecdd82b09b5d7738cad6d0ec7f3f6c8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:29 +0000

    fetch detail its so stretch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def is_even(n):
+        return n % 2 == 0
+def is_even(n):
+        return n % 2 == 0
