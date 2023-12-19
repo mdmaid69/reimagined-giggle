a8c02a71e2325c8824017a10f4477ef899b14b8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:57:16 +0000

    used we mainly never count

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
+  def convert_to_hex(n):
+        return hex(n)
