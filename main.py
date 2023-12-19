41607a2c6d744f0edb832e792d93eef77729ccfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:36:02 +0000

    switch translate reaction glass straight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
