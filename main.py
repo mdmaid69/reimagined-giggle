0c39dbaf4ac85cd8003fc36485d5a00552cb89fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:47 +0000

    argue call watch entrance left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
