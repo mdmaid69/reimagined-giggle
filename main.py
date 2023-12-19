2d6a46b5891416439e430d8afbc29ca151b5d5a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:20 +0000

    friend peaceful bringforward as if

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
