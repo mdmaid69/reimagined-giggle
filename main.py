1267c46fdeb904cb5efeb3cd62aa84b4fea8a9db
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:29:29 +0000

    hope embarrassing guard eager spray

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
