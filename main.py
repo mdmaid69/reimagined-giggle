2f6c7e9057065a65cade83c4807fff85b2e5440a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:27:17 +0000

    system apply thick understand drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def count_elements(lst):
+        return len(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
