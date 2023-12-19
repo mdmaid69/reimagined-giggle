31a380a8a863400a56d69f1a2ec64ef1986b0e83
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:11 +0000

    put nigga whoever connect around

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
