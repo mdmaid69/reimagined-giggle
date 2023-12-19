f0dd1925a18d080465b48001d8c59300f5dd8cb9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:40 +0000

    tune stage sight clothes suffer

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
