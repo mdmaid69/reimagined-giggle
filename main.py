56adfc4d1250a1a0c45faa896bccbae5a9db08b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:49 +0000

    delicate smoke Mm between joke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
