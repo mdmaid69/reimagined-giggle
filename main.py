3f0337b03ef5e1e3e52a9b1fbb91ea8c6badfb1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:34:04 +0000

    food thankyou getup contents upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
