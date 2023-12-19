acc1b8f1ca9f1c4647c72916b833b859cc465edb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:11 +0000

    watch party believe condition squash

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
