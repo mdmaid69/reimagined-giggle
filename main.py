abfc39955ba38858d248b7ecda60ba39a809adce
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:46 +0000

    body yesterday thought dark convince

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-print("Hello, world!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
