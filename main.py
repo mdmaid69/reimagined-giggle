4fe091c920f98813cb61efe357306d85e4379327
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:02 +0000

    anywayanyhow me develop and whisper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_volume(length, width, height):
+        return length * width * height
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
