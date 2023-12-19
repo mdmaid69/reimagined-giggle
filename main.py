128e6194268d15e82eccdb1811d6784963489d60
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:55 +0000

    up to doubt unfortunate precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import datetime
+print(datetime.datetime.now())
