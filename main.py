2f133ce2e4814564b0f7b6c5c7cafad1a9b11159
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:54 +0000

    plate actually wide good 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def find_max(lst):
+        return max(lst)
