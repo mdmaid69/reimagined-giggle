d19e281728275a24e3bd6fea103d3dc5c31216b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:09 +0000

    noone Nn level flat touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
