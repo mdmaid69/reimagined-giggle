e6cc2d6f3a70ee06594a57f6408fefc5b9966848
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:27 +0000

    describe roll previous straight equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
