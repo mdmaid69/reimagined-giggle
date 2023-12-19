e98483630f4424c1ca38f002f69e0376eca8e1d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:03 +0000

    back occasion bottom shall figure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
