a85b450da10d16da0b817be9c56dfef664e82718
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:48 +0000

    row alone future unite science

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
