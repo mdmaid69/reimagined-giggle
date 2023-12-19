9a0ddd59f672e1b0bd24aae28cd6240f875f8ef6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:55 +0000

    understand future defend female turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
