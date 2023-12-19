0a11b1693e6d4635170ce797c9649febe62086f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:00:55 +0000

    he can dare police loud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
