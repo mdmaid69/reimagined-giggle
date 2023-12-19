eaeca928b0829f19baa2d7c07fdb233b26705593
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:57:09 +0000

    hurry idea reaction well give

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
