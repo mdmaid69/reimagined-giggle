c9965a7e01bd06cc392ac4bed688f43715c0e61a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:13 +0000

    beside sense comment escape behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_list(array):
+        return list(array)
