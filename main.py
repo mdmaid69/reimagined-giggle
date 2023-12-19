ab437568085143d8267a38d8d46ca7e481d1568c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:13 +0000

    model what tense pity apparently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
