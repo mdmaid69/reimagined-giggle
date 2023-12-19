1ce21fda2285461bfb82cbbc812a9294564ab142
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:16:53 +0000

    surface on admire move sea

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_min(lst):
+        return min(lst)
