8d38186002f2b203170fddc73e505fa87f30c8a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:42 +0000

    leave well belief visit rather

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
