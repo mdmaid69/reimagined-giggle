deebfe167aa732590d267281c8d292a450cf2656
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:44 +0000

    phone intend considering drop climb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
