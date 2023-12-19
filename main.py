c00ae4a14728fa2e350ee8ef1779cd7b5c194b72
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:02:54 +0000

    interview flight discovery area workon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
