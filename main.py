f35c86544371e20deecfd756b671136fc1b3bb73
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:50 +0000

    clearly order spill handle often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
