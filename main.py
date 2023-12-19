ade359e89518d85f510883435ac5ea5a4d8dc6b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:10:01 +0000

    rough land music of both

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
