5258dc15e1be68e735cd615086a812553d9013ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:40 +0000

    man many imagination laugh frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
