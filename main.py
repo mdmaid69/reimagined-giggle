45266ff78c123c94ca064660bc40386ebf2f0d08
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:50:41 +0000

    kick home badly steam base

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
