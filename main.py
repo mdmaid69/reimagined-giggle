1182432e1a2c5580edc659f3c6dc581ce9a1601f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:30 +0000

    reaction contribute busy damp basis

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
