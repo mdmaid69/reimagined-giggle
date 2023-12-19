dbbd6e62db766abb25118a4a477f59814cfb7f48
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:33:38 +0000

    colour avoid buy which sister

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
