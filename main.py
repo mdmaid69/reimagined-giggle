1b1429d4bc2887c4d7477de9ac59f7f67ac0b8b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:08 +0000

    fence worth cinema moral standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
