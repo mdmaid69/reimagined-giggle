2d8f2c93d1cc55e8cb7e38eba8c7e89395b53953
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:26 +0000

    admire unless night physical illness

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
