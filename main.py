b549e3a5fc4c3b0c92ca0b00b6bbebaf2ffbb64b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:30 +0000

    confusing on crisis lucky present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def find_min(lst):
+        return min(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
