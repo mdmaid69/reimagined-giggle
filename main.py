7bc7b4198776b670fb7b4e2294c346b8f1d08da3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:08:28 +0000

    escape deep takeout teach science

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
