b74eee6d79f73a7a12653228a7879ccf59594b1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:12 +0000

    shopping across good war active

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
