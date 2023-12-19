e1e1db78c1e045e9841a0ec51b4b0367b584b972
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:20 +0000

    to weak nerve equipment unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_typecode(array):
-        return array.typecode
-import collections
-def create_stack():
-        return collections.deque()
+def get_array_as_set(array):
+        return set(array)
