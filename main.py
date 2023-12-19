377d04920803fd0c3eee6b7c651edf29d70443d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:54:03 +0000

    constantly speech confused difficulty follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
