fe84d1967849a959062d2609d384a1df891ea5d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:54:07 +0000

    protect dry cost name stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
