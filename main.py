dc9ad78904bdb94e22c355ca897e3228f51c81ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:11 +0000

    justice dull impossible fear inquire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
