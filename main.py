15c1db007a5fbc396b755652053610ddb639eb96
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:18:16 +0000

    count tell eye unkind add

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_current_time():
+        return time.time()
