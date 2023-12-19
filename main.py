2d793e4ea30ba419664bc55420edb2ee57b7edbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:24:40 +0000

    close sweep pronunciation habit hear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
