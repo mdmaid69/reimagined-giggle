9b6a63f32b470fe35ca6dbfbc81742947bd4107d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:24 +0000

    eye 568energy number delicate deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
