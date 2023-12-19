3205ea07d0e0da95320e4372b0c4dee03e7beab5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:23:09 +0000

    side brush by quality steady

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
