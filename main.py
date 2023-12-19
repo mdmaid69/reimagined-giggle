e4e683c7a29567dafb87fd3b6343be7be18eb23c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:15:00 +0000

    meal best pause afraid objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
