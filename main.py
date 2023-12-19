80b8a78534403ad338af3906d4860842659ca58f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:05 +0000

    hope owe opportunity play food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
