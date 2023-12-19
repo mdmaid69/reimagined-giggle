6e1dc96f065a0e39b430008b2a51f00d84d3c9bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:44:47 +0000

    suffer you badly marry exact

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
