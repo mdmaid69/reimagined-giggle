cf8aabaacd83943399866dbd1d9053979a1f78d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:22 +0000

    quarter how bad town indeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
