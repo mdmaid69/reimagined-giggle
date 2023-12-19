509b60f2d9a7ca745423ec4bfff2f51afb3fd283
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:47 +0000

    sea accidentally similar who dear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+def get_time_since_epoch():
+        return time.time()
