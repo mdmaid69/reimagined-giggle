7b155a51ae6833d70b3b76394f8ca9e51e57561a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:33 +0000

    hot our wonder pause which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
