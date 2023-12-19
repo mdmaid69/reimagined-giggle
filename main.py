e8c8c83166c72e8cb02d4ab08b62c945979f2522
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:49 +0000

    lost place commit whether threaten

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def sort_numbers(numbers):
+        return sorted(numbers)
