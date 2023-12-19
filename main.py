1f8553d452806cb83a523815a6cb25b86f35444f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:23:57 +0000

    afterwards cry chat pour he

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
