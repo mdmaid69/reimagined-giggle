270c54a9f6a5e4d838ddfdbcc10d05bab7477dcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:36 +0000

    through can desperate Kk organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
