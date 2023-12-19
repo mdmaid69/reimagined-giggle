1c060ffb51817af5656078610ea1753d1ca1eee6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:06 +0000

    passenger mood invitation radio principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
