cdc94b708ad17a8eaed8570098cdb946704bc824
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:35:10 +0000

    last go cheek sign object

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-print(random.randint(0, 100))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
