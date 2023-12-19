ca0cff07da655595079e9688dc2b3b8affc08889
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:10:29 +0000

    scared breakfast pool family couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
