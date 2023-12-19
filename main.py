13ee37ab7eaadc9c982b3a574bf56e02f2deae1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:31:24 +0000

    estimate sky only keepup mainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
