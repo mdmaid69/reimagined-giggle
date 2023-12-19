64121559db924a27dbd86ba18bc5526a88e3a7bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:34 +0000

    address straight swim means long

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import numpy as np
-print(np.array([1, 2, 3]))
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
