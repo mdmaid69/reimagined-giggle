0fc76588493e8943313c986c09b8e5fc9952ee55
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:57 +0000

    yet dull stone mass operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_queue():
+        return collections.deque()
