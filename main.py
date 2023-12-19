fb18c789add496207c8f70760f64e4fd5dc84b1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:19 +0000

    since while pretend pin little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
