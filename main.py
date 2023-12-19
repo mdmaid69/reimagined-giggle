cb6820dcc78880932780f89ec1c66b7f09bdba0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:03 +0000

    small long another crash dish

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
