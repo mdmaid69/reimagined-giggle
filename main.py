88878c46987ac8a04ce4c3602e16cfac22ba1e10
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:27:26 +0000

    bury inside Gg platform importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
