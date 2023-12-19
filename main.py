724a0d1a4496777c2f9fe8f8a4065bb347b86d3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:44 +0000

    excuse grand push hurry replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
