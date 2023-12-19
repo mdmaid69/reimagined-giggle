8531d20eaf6e339fd2c51f12d112d665b916b328
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:09:46 +0000

    carry spill switch speak scrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
