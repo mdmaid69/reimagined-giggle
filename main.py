1b4c60cb5f9d8376164b3c807983874552e64840
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:07:01 +0000

    immediate stir character better flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import numpy as np
+print(np.array([1, 2, 3]))
