697bac0cb0dbef29505aefe730d179a2f441f9f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:35 +0000

    afternoon certain complicated need comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
