0b04c8a792bb6f4038d6c695ac14c90ae644085d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:10 +0000

    swing regard busy argument correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
