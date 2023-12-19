e1ffff6d48751d207bac6135fbb4837d415ca414
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:26 +0000

    exchange fast guess share short

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def subtract_numbers(x, y):
-        return x - y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
