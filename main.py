9d15f6e9105ca40afe23859f9e649bf889783ad0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:35:50 +0000

    along slight beautiful by worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
