6a4129451e418a6cf3eb8324186540ddee48de8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:38 +0000

    on style combine wise impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
