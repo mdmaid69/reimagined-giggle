1510772cafe7a89199c933356a50e9e8c02c0bfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:06:41 +0000

    drop spend neither after funny

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
