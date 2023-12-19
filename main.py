6fae895cc98d3844d9b8775597f5cd58131b700e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:36 +0000

    study bath considering tear unite

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
