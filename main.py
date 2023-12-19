7dbf48ae04bad804cba29082abdb62b129daab30
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:38 +0000

    on oil attempt swim distance

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
