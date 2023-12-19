73afc8609a9a94d407d3df24c50f7d8282b32642
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:14 +0000

    contain old hear sure coat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
