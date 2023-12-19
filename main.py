27119d2165241fc4a1c51b257816e075b1ba8cc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:50:09 +0000

    set walk evidence Yy creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def remove_duplicates(lst):
+        return list(set(lst))
