17e9c0b1c735bc8cdad550ce585388f82f96baa2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:20 +0000

    plan uncomfortable scare joke period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
