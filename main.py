9b6edb460e16167fd9c73540b3726e04ba01721a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:54:50 +0000

    keepfrom getout push gradually crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
