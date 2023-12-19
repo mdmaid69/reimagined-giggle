041567eed75ccbb3bc836487c66a461b15805701
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:27 +0000

    word unusual smoke ago during

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
