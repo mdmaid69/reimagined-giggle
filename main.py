8cc9a17f2236198cab32be8651c73c073d8bceee
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:02:18 +0000

    should swim possibly after movement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
