4317cd03bbed43b3bea2592c31bd3769976736bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:02:17 +0000

    bored computer sign load copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_area_triangle(b, h):
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def calculate_area_triangle(b, h):
         return 0.5 * b * h
