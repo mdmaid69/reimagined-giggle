e3a36ecc00e51a78892fdc4d35415a03dbb63722
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:31 +0000

    steer entry punishment contact joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
