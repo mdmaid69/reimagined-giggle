1b8c1a45262299eba7dd109ba52a900a3f035fee
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:50 +0000

    sensitive trip fierce strike advice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
