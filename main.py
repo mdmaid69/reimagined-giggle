cfa95aad5d487216d2ec9862a6d28cc9340e7909
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:01 +0000

    government pity opponent partner advantage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_even(n):
+        return n % 2 == 0
