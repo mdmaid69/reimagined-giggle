8d4513ead172cd8396044861194cec0956bbad86
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:53:09 +0000

    complain niggaacademy further bag although

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print(sum(range(10)))
