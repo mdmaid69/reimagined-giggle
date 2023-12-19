53dd001683312f4594764257630ac0beb57d7dfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:35:55 +0000

    smile boat recovery mystery market

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def cube_number(x):
+        return x**3
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
