cbc890014defff5b6d724692f737cc3aac2d27b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:50 +0000

    through plate offer once line

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import random
+print(random.randint(0, 100))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
