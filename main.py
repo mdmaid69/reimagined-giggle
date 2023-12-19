b42430868f4c3c906c60be54e74f6ae19927769c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:48 +0000

    below event interview tradition trap

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
