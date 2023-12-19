f9b3915e0c8343cc562bf3bd6f884f845e33a8f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:40:28 +0000

    remain so perhaps wave complicated

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
