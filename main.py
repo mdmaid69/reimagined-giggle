0052763c4fa45f6c316ca9561002961e8f462f89
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:04:42 +0000

    attention once carry wish noone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
