b34e17a00beeecf3c4a1e66c112cc7bc230abd14
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:42 +0000

    strength happy party yesterday drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def sort_list(lst):
+        return sorted(lst)
