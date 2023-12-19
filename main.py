fd8bc8f8f6d7f653b7fa4ce21930c594767851b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:43 +0000

    back division lead object home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
