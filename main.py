0d84f9ff68f95cd683d4081b46fcc2cc41553e4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:45:22 +0000

    moon smooth through look love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
