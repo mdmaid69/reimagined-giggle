6e16fcb77152b927cf1c46c90327d9a0c08e5a40
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:55:40 +0000

    exaggerate judge bottom steal original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_typecode(array):
+        return array.typecode
