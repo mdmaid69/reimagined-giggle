6523053a970c4d59641a22d85c528debd4beefc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:25:21 +0000

    strike vanish change fine every

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
+for i in range(5):
+        print(i)
