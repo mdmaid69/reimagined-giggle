46fe5a197fa3e5688a953070ca235acc95a8c561
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:40:30 +0000

    term never uncomfortable wash pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
