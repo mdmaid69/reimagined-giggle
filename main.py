42c6ca47a21d7c680fce7d5c57b07f891c4eb9d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:32 +0000

    door book method single opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
