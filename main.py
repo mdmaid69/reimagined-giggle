f945932afd590ed02dc5a7db4cba95f028785d74
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:50:12 +0000

    allow consider excited talkoutof chief

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
