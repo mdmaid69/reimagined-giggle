284623e36473a0640211098cb0d1b40e4a0f4fcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:10:21 +0000

    introduce mix window more who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
