2ad65a4135b4aa79b218dbe6f584a738b6030120
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:43 +0000

    attention split wake uncertain question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
