c5cc28695e99e6682510857bacdc89fdfc5ef248
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:34:07 +0000

    shallow confuse prevent cut pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
