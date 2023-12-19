76436a4d0e8e9865e1df4eae3e7ea4ae82cb2a0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:22 +0000

    increase quarter expression process lost

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
