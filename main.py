daf8830d2054b19627e9186e74ee6aa80656cd8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:27:17 +0000

    continue gate support react equal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
