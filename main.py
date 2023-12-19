47c3b28b8b28e22b368e44d452c6f1bc6a945b51
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:54:28 +0000

    violence thing block detail typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
