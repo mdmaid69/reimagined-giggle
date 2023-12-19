b83f93c4cd5bff19196d56c3fd32088853c87a44
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:54 +0000

    fast carry accommodation accidental distant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
