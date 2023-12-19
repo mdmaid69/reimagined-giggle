c13ea8e49145788e5bf9d5f853d2bd50a1fe8333
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:31 +0000

    threaten smooth objection polite slip

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
