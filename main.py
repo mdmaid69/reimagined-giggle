6158e9dc78708d93a9ebb7b910cb624d3aec5b7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:25 +0000

    receive taste still temperature medium

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
