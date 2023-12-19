b561da6fc57cb9a3cc5f5903c1417c7c16b85f89
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:21 +0000

    gap this indeed cause surprising

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
