1c924265a0e8eec9880cf89965dae3185f4a85f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:21:06 +0000

    excuse light spin lonely valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_item(array, i):
+        return array[i]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
