6a56d1a2791233227e82bd04c40c37773dd4cc74
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:47:34 +0000

    coat fight ourselves letter like

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
