848f4b5bfb22d293a474cd13fa1b61c35280dc8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:11:58 +0000

    respect visit tread push crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
