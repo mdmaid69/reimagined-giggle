62f040a3e5b33d3056ec46b252ab44e0a608d841
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:42 +0000

    port possession increase question after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
