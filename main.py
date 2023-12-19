906e33e3a995097f63dc9e6ee46e79d6c47eda4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:17:54 +0000

    contact lie desperate drink visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_min(numbers):
-        return min(numbers)
+for i in range(5):
+        print(i)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
