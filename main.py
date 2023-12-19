fa681b27a72ec34590e27185219726feb00cfd1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:02 +0000

    could field transparent accidentally necessary

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
