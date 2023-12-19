70dd18119a305ab15902c066160868ea7aaa91ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:48:25 +0000

    ever nigga stiff first wish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
