1299b4d16cd162fecf66f64284a6e0fc15b851ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:44:23 +0000

    hit throw without rain so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(x, y):
-        return x + y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
