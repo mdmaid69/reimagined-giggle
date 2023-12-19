31f7993991433c243dd9fbf3aeba3f0346dda373
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:09:13 +0000

    shoulder occasional pay concerned behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
