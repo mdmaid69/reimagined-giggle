34fda3f388aa514513acf97d626cac60bd698cf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:58:20 +0000

    thin occur tool on talk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
