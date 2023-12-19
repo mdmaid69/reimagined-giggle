0733befb79ab7f28d8696f837cf129447de6f443
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:47 +0000

    battery feeling supply he refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_min(lst):
-        return min(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+text = "Hello, world!"
+print("Reversed:", text[::-1])
