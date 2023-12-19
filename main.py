be5a8d30ece80192a65fc827e99c0a14eea0ded2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:28:22 +0000

    on anything guard street gradually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
