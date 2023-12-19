8833c512f903d319ea4356bfea00a20692b07875
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:06 +0000

    wave marry politics smell really

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
