9991d2d2daef28492a8c7cdf451cb07618ced2b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:33 +0000

    escape learn heat principle direct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
