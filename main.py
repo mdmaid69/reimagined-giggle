40eea28a0b2bacc4054e2bb43a88fbd44f6540e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:22 +0000

    without might key book strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def is_even(n):
+        return n % 2 == 0
