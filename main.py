be55a02e4cc01b7a53127a1c5b48018eeecf6122
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:16:09 +0000

    question closed sight up judge

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def count_characters(sentence):
-        return len(sentence)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def square_number(x):
+        return x**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
