405aa6b96d12f3c73cab0b6ea1cabd10005f7d30
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:39 +0000

    even as harm sea material

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-def find_max(lst):
-        return max(lst)
+def square_number(x):
+        return x**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
