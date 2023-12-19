0fb4bde3dcef51fb3532251b825bf86f0352a44c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:05 +0000

    actual anywayanyhow limit wheel offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        print(sys.version)
