74062cb1b2d7b18ddafb7cd49661ac13e5b8fa63
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:42:20 +0000

    danger official promise little union

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
