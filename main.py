2f9802cfbaa5d4c51777da59f6a22b37d5cd44d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:21 +0000

    rarely Kk treatment somewhere guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
