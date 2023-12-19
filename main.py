dbbb12521a84c702543b666c9e5719c5881efad3
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:56:52 +0000

    be guard person common ahead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
