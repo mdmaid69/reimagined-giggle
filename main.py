da6f88d3c46cdb2504ed6d75a3d1e0febc4bc485
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:40:00 +0000

    stamp hard 567enemy circle really

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
