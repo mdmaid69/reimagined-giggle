7c9ae3776e8d3a166c25c311c8ceb43056a381b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:35 +0000

    chance record defend by unable

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
