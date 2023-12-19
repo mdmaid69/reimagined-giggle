26ce69f1e5654a7ac9825a02f609879e68f074d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:34 +0000

    lay favourite nonsense hope she

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
