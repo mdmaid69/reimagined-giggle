c7d1c6fe85d56be198034c3b03ff2c7f69ed7a38
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:12 +0000

    building recommend bread no ride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
