6b2eff1c96800d825038495c68f0e5018d714a16
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:17:14 +0000

    square connection tooth marry preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
