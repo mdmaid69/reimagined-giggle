cda2f314a97bd774aefe870ccb6661463726f004
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:31 +0000

    cum connect unite everything workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
