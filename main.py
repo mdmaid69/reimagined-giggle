134604581e8050cdd46da585404748b2603853f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:29 +0000

    undo final dull peaceful joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
