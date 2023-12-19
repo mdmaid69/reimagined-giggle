416d04bb39574103c38e12070966a6dc94e2288a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:20:01 +0000

    bath from month hard still

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
