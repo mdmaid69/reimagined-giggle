017ebe5fded9edb6c72b2d9cbc7336435d52bc78
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:41:18 +0000

    provide lay tend ought thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
