99d82123719dcb8b0566cd5b902b398e9a747b63
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:13 +0000

    convince might glad violence neither

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def add_numbers(x, y):
+        return x + y
 import math
-print(math.pi)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
