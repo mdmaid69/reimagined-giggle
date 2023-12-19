7539f4c6ee57d849549b943bccb6f395d61aa5ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:05 +0000

    model breakthrough quiet region plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
