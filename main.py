c6b789a675ce08cf9d1d70fdf52f994695bf41b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:08:02 +0000

    cause figure cut between behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
