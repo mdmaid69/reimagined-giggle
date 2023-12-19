b00564f40aa1c6bea82dd1d2549839bc0785b054
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:56:54 +0000

    moon secret extremely shadow delight

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
