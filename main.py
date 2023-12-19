c43bb74412d3aa80df35b7f352b983dd14ce7960
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:34:55 +0000

    listen criticism thin knowledge rely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import math
+print(math.pi)
+import tensorflow as tf
+print(tf.__version__)
