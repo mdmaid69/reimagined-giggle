11b7a482b568b19a7d0b5b7f520642061ede163f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:16:43 +0000

    there formal makeup share question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
