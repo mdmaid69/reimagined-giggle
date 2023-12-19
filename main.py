44d5fe5af5423969dbbf31854c00f3faf01f85b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:08 +0000

    surface software clue there operate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
