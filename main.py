bab6a899016104024dd79620ba71d5250c81c0af
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:15 +0000

    sometimes natural sometimes low correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
