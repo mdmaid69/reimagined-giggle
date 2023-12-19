8aa4147f8acad9a7e1b965f088a34b817eb93da2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:38 +0000

    bottom inform talk government regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
