e48759afe59f3f257b65ab2b63ae29181d18c6fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:22:34 +0000

    proof much know home please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
