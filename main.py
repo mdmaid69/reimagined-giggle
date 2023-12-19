87e7e3e20bd784a8f70d968fc4df39acdd1bd080
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:51 +0000

    possible separate Vv nose lead

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def add_numbers(x, y):
-        return x + y
+import os
+print(os.getcwd())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
