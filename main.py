71feb2cd289937c8b5f8cb5ae872e1addec69080
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:10 +0000

    agree name violent miss bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
