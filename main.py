5b5e60de73f818c81c0c7c75e8e90c4facfba911
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:02 +0000

    appeal direct difference ilovedicks abroad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
