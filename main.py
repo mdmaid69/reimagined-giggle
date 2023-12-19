3c705e9adeb4a667232a0c94d4a30a22b98558cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:56:45 +0000

    name talk about disturbing still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
