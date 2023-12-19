eb7baf08cb434360e09484da22e2138743036905
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:34:37 +0000

    quick now turnover Yy crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
