6c4ab321f6e2fa8eb120278b42226fe774f9f970
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:32 +0000

    present follow smile year mind

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def convert_to_octal(n):
+        return oct(n)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
