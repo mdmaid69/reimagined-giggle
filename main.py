9a3293177c0c6c8bd7f8a8276aadcd5a2b61b5b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:03 +0000

    through unable 570enjoyable sleep Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sys
+print(sys.version)
