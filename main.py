8e8831f5b4cd0601a6e99640b1342042d276fb3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:58:13 +0000

    comeover life boring comment operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
