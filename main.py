b15f51e357eac8ed12b9f289daadcdd517323d9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:05 +0000

    event difficulty proof front shape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
