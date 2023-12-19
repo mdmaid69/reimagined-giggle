12cec15e75ad7540113b0669215cfd3a4ef58d21
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:26 +0000

    moment weak nor reduction wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
