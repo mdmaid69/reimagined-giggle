0876cfa2b93c622a01219d5a1b8aa3b8ba876664
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:22:25 +0000

    home interfere gooff class below

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_density(mass, volume):
+        return mass / volume
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
