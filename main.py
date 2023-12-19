528ad7536676b9908e15f845d1af5dfb6c2219c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:48 +0000

    poison last hot freeze brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
