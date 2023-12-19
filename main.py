949f5032b7e1a3a6671174df70583353d38af6ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:34:33 +0000

    blow shout often angry raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
