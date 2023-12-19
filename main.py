eceb1a8eb03650c3a3d5fb534ef87c6dbbba613b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:11 +0000

    persuade Vv gradually comeout thought

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import time
-def get_current_time():
-        return time.ctime()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
