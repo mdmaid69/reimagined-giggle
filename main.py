bbdb0cb818379c2c4c6a8cf840d8e495a3b38cca
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:50 +0000

    his brief advantage spring official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
