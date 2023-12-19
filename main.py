6bb08f2df101eb24ce37bb4dd978703d5fd3a1bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:29:57 +0000

    better argument convince belief offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
