82c46c1a8fcb35d2370e0ea90d46abf1e13d1921
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:13:24 +0000

    warm date scream meet round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
