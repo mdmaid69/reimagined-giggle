a1e9eeb8559d70e7b2b80ff02c407d186046ebac
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:26 +0000

    shall lookback(on) describe perhaps she

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
