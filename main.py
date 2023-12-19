b00ad00a908a8f118ecfe8b4ada17e1b79898511
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:15 +0000

    single star protect reasonable collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
