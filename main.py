353918f2bdc2b7568eb49ff0c168c83654ffa86f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:51:22 +0000

    unusual sad big watch begin

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
