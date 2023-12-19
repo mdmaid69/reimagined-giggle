5f0a2f4a927ffa345b1ba6623e7e264e61f7f44e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:59 +0000

    depend illness leg oil building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def remove_duplicates(lst):
+        return list(set(lst))
