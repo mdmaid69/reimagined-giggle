aba6443a0fd359994b33f8cb09da68b4074e5327
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:51 +0000

    watch forget damage drive mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
