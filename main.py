118d243cf1a7ea005c9ad9659fb94ff757f2a581
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:27 +0000

    mother train then holdup notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_ceiling(x):
+        return math.ceil(x)
