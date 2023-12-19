955c6cd9525aff96a7572fb13a6a624633e2f567
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:28 +0000

    right climb offend hope mainly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
