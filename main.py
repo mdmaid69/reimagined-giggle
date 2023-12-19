7450e864e8f91d4f81c40b021ccabeb9227963d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:34:34 +0000

    interest imagine Kk everything rare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def exit_program():
-        sys.exit()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_volume(length, width, height):
+        return length * width * height
