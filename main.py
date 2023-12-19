6df09378734efb399bf4e8e44ae1f3c78e705d88
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:51 +0000

    right whether education distance capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
