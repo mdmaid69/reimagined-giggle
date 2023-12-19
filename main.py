9079278a563a3e1901994556d9fd141c057e9318
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:31 +0000

    movement glad brother turnout wish

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
