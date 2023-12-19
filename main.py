001c1610c20cfcb74f57bbe817e7622f0e79f3d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:45 +0000

    sing offend persuade happen say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
