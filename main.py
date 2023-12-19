6753bb2cfd51f424f069ab0423f990fecd1cc3e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:04 +0000

    under along display load female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
