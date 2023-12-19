7189e1331f0d717cd21678b11c55afd86e4484a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:10 +0000

    rest completely wipe store sudden

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_tuple(array):
+        return tuple(array)
