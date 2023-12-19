5a1cb02a8197a1fbb8eee321587ae26f45e435fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:01 +0000

    trouble finally want bag outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import os
+print(os.getcwd())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
