9fd253f5d083920e1ca801926303c054c3fb0330
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:19 +0000

    rush clothes lookup thinkof hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
