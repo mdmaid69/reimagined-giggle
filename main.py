988c93a75db1e1cb21cb84dc12fbc7824b4fd45a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:49 +0000

    whoever off mysterious Ii spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-print("Hello, world!")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
