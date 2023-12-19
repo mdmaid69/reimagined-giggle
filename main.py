e700d946f193349790d5015e06ed3fe0ea575962
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:13:52 +0000

    lower attempt send easy sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
