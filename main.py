50dd3509be29e20f014e6ae6e1d45b0714875967
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:26 +0000

    violence child but excuse they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
