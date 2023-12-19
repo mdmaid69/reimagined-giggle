238e920890d9609fa6338cf5ca5bc8e6f6650277
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:27:39 +0000

    cap responsible evening at street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sys
+  def get_python_version():
+        return sys.version
