ac58ecea9720836786df1613b713b531742ac849
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:47 +0000

    sharp corner force cost project

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
