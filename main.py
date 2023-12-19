7f945e486f5f6bcd46baf85d4dfff61b05cc2bbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:40:22 +0000

    guess computer press share whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
