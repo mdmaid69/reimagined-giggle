71da4bcc61590dc9b75d42d2da0f9eabf7cfacb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:14:18 +0000

    Nn protest control contribute pretend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
