2aad872526ce1ad4a4ea720e6d858d37537e4bfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:27:50 +0000

    while description would insist protest

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
