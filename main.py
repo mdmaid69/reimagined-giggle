c300ac9fdce9d97deebe883bceeb09d6102e1aab
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:19:52 +0000

    crowd exploration lesson town expression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
