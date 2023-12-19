e0c27ba810073180162ecc55e572f4d80cede0ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:30:22 +0000

    workupto off may much process

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
