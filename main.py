2cbc4c40c00217f605c39de0dc4cf7fef9f9305e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:20 +0000

    thank mention design complete badly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
