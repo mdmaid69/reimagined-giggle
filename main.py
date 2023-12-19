e74bd1ca6c2fadf9e5754019d988d3679ae5c075
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:08:37 +0000

    what figure platform bin earth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
