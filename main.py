b62c589c80e25c18b1fe77118186fd1aa7d86080
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:51:48 +0000

    idea 569enjoy sure what owe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
