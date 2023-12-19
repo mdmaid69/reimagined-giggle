b1feff418c62d5ab179a4536ae4e482ec8581013
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:30:17 +0000

    promise light question appear Kk

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_pressure(force, area):
+        return force / area
