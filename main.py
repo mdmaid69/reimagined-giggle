0b12b47c5b206490ac7b37e0ab48d2a869070109
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:51:43 +0000

    comeup scale side whole past

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_user_list():
+        return collections.UserList()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
