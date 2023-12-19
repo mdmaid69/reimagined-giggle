c8c20b47f9c1e701e7c4f842a9f263cb897c3543
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:19 +0000

    danger plan excellent how straight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
