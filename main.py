ae9adc1d2a9d68f0cbb97ba28d7716682baf728f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:02 +0000

    still necessary willing break normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
