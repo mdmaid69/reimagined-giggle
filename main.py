641aaf5eb49c3c285f21771063fe7c3bd344a865
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:39 +0000

    exactly best used but somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
