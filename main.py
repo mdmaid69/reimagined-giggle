86eab84d9f2537fbd1a9ff0c792dc6220fa77264
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:24 +0000

    authority university behind open scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
