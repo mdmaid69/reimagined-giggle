d3a0edbe1b0d7c83643c99c68053e1907032092f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:09 +0000

    simple gas confidence serious play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
