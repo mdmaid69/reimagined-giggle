be715bdc8008b658f133dbb81bfbb75616479583
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:45 +0000

    opportunity mystery thing swallow write

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
