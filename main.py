8dd3cb370c599eba2fe8b4e022d53fc54d78eaa6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:50 +0000

    simple lay human uncomfortable same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
