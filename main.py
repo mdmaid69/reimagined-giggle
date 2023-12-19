991264a7dc4554cbfdc714568543d1a9e361eb87
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:06 +0000

    worried show conclusion hold class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
