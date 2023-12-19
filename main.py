bba0185cf3a5c364be608b06b4fde83f6b10b812
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:06 +0000

    match call nothing food bring

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+print(sum(range(10)))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
