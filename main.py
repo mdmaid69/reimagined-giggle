64b25ce2a04c3a2428432439634129cefbf72eac
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:23 +0000

    impressed size flow unusual boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
