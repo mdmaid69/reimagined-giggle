195e4ff8ffbb4fdf11cc8e24d99983176f2cd011
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:17:51 +0000

    process yesterday lookback(on) getoff effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
