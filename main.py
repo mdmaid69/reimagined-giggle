051a906fe8b40dc07e0bd0c81a01945eb6c1fc22
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:49:45 +0000

    apply start mainly legal love

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_power(work, time):
-        return work / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
