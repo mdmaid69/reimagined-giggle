9419e92d3fcf629ff62087019838f03acdaa8559
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:03 +0000

    so standard everything hurt 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
