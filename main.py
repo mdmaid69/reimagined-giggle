a5257d02cfb0f3d98a35c31a30bef41c51917154
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:36 +0000

    available slice stare unfortunate measure

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
