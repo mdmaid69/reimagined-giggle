6f1876c669c53bc6171cb51d681dc656d2e698b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:56 +0000

    cruel raise occur ilovedicks bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+print(datetime.datetime.now())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
