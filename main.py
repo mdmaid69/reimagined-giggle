a975d6930de990eb753672bf7c6dae5fbebe712c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:00:40 +0000

    side flat nor key drive

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
