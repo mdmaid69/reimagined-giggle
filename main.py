44cd8d2f31879a49b14046b9bb164b071e6a17c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:00 +0000

    regularly may back lose chat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
