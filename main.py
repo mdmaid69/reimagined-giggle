0f728e2c4a412a906a2e11bc79a6df7579f454a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:54:40 +0000

    crash period shoot communicate judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
