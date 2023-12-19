b357dbbd82f13e465fb7fad8e69e7ec0c8f01f2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:31 +0000

    petrol considering plan suddenly burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_str(array):
+        return str(array)
