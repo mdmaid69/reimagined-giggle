1a6c385aeb08eb46b41557ebe89bf61462384a5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:05 +0000

    ready mine hate or freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def cube_number(x):
-        return x**3
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
