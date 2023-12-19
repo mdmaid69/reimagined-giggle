1d920df1cf37402e75806b95fcb1b6af65f48755
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:02:19 +0000

    careful strong telephone door stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Reversed:", text[::-1])
