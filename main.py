5bf8d775bf844e24e00145ef70f3d1d3f98d2f02
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:14 +0000

    little slowly description cut entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
