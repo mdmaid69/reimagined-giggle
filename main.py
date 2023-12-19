a744ee5ed0e15bed6fc46d21f5bd82d2f9f9cf25
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:52:21 +0000

    load actually accidental stiff see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
