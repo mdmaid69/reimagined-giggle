99293fd02f9eec4b8ad07db7d3a5f040a040bebc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:25 +0000

    related declaration aim poison and

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def square_number(x):
-        return x**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
