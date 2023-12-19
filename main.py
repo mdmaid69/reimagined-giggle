04ec890a6c23ac67ee378ded535c982acae2e371
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:24:54 +0000

    hour where medium shop chat

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
