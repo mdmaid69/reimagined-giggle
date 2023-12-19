6250547c55cf9b289ab6f87fb5a0aea47924a38a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:30 +0000

    excuse shout stiff somehow gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
