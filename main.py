b8ae6c01a9b84403503f89d66baf25916d12d292
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:24 +0000

    interview cost excellent character find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import json
+def convert_to_json(data):
+        return json.dumps(data)
