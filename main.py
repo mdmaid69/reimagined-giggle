aca5790c9ce581de2390a7fa6235591e6cf00c95
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:53 +0000

    whom holdout sweep trade difficult

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
