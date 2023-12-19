f5b91ed3a4a150be159d93df15d7fe353e216d62
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:39 +0000

    Mm odd regret block somebody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
