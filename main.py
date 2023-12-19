bfb2657ba1f2ffe5204886175958ebad0189cd22
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:42 +0000

    succeed callat father friend so

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
