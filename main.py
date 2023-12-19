b8f9641b3e1d7ea88493adbd691bf3427f9144ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:51 +0000

    whenever supply sorry platform injury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_item(array, i):
+        return array[i]
