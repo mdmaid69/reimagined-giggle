a9805f270ba47d16e6fa0928eaa2201773c95816
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:46 +0000

    impatient uncomfortable benefit trick bang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+print(random.randint(0, 100))
