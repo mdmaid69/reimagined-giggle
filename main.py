0648f1fa0c55b4a49bc04dfba6c33eaec03c6095
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:10:11 +0000

    necessary suggestion contact patient behaviour

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
