2c50e71790b3a2cfe685a731ccc1d7927abb771d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:03 +0000

    keepaway petrol air frame us

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
