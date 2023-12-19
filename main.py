5cf52a21e523bb1dc88235eb3c5176003aed8264
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:12 +0000

    giveout trade hold best forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
