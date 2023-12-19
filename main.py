7cbbcf4e6188f289bf96b54946e696821f98416d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:24:49 +0000

    exercise fetch leave believe thinkof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def find_max(numbers):
+        return max(numbers)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
