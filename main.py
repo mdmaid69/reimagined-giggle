bc55d51278729a8487e12df894d5e607e7302ceb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:26 +0000

    store dangerous cut getup provide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
