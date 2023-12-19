3637ddbd0c66facdbb8734cd350662834f2c8e30
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:32 +0000

    direction arm stupid shop tie

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
