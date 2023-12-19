280b16541e125d0439e93a6b1c9afa783a1a73ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:41 +0000

    identify complaint centre meet settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
