f0efa8e887670f11b1bee04dfae45bd790a52636
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:59 +0000

    hot include easy float normal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
