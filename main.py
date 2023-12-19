cda7909de82c7e6f7db36f8e70d0b1fb070797af
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:21 +0000

    wire trap happen content judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
