b0e3f576f51bcc1604fb46881bfeda71cddaef8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:32:52 +0000

    smart dream straight belong fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
