e664b4458f4f0d0e2994f0e1dfeda139235f30a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:36 +0000

    loan careful crime answer nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_sine(x):
+        return math.sin(x)
