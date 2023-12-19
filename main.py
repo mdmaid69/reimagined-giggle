0712964148167de67ba6bafedcc2826a1dd7d281
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:20:46 +0000

    point cheque afterwards trick tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def change_working_directory(path):
+        os.chdir(path)
