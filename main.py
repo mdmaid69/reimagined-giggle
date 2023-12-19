e056ab672fc0348ee0d369c18ebc9c84f53ef9fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:00:26 +0000

    remember loose ring identify suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
