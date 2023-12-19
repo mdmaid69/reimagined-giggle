aa387ca4af37c860032a4575a87b241eef99a64c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:21 +0000

    blind weak wash we wait

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
