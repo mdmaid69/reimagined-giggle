df1d3a5e8bdb5d1721d17d64a6eb66034ba6ad12
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:08 +0000

    boil physical tend report part

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
