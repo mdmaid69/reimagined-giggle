1118c4b88da9caa23de0f0d5e2103cdfb8717c9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:35 +0000

    frighten pull against model inquire

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+for i in range(10): print(i)
