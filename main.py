87f67ab24a368888cd66f4cdbc8a5a53feb63ee9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:05:24 +0000

    official workout open succeed better

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def cube_number(x):
+        return x**3
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
