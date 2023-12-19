be5ab30914869a2422ca4f023d5e3e7944b6e641
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:54:09 +0000

    because chief play direct but

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import platform
+def get_python_version():
+        return platform.python_version()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
