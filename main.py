0aeb2a20400f50e76ef4761bf7b5b4147091b710
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:38:52 +0000

    half none negative process cook

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
