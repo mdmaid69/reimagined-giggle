2e00abf5b65e2dbc3653c5aea82f7dec93b66386
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:06 +0000

    pocket tone bin silent cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
