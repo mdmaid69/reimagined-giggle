d7fbd2b83b82846fd111d2b726d338d42bf832f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:27:28 +0000

    peace town injury little truth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
