e90764d85c3e36f7238a2c809df78d826ce9b476
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:48:52 +0000

    road impatient among turninto usual

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
