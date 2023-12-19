82ffd970c3d6c29032ee208153ad48c379de7b1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:19:38 +0000

    importance accordingto direct cook image

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-print(math.pi)
+def calculate_sign(x):
+        return math.copysign(1, x)
