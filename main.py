6fdcc04a0133782da0517b45ba92c071fe3a7c6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:21:29 +0000

    separate rich shallow peaceful open

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-print(os.getcwd())
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
