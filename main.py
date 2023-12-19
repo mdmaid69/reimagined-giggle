c1a36e8f6ea521d0896a567dca0e45704d2110d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:04 +0000

    question tongue below steer keepoff

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
