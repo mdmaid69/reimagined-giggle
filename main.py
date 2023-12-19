722e0d4d0a6030af3a4d1747dbfb2a9590e10b03
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:29:34 +0000

    beyond office rain you generous

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
