6498806a61654f5646eb0e74f77ed923cbce20b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:57 +0000

    567enemy cruelty fall purpose generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
