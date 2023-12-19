ccd0fe6e52247ac5430dfe37759be1c93c5e60f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:08 +0000

    challenge bright turninto attempt when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_work(force, distance):
+        return force * distance
