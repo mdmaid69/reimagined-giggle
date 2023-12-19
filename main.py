be785512a6e4fe93a75d3848429d61fd2b14fdb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:30 +0000

    money Oo about protest call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
