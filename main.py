883bd76c3a32e93197346f691693505f68f66097
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:53:36 +0000

    sound spring yes single rest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
