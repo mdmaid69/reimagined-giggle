559b1f4189a4370bc0a2469d59e7483cfbe12db9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:58 +0000

    question small concern tidy satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
