8773829217eaf16aad98f77662c7509214ce6b19
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:07:07 +0000

    practical drop concern family would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
