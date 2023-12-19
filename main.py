1688e6b5316cbe66f608443808f352d85526899f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:54 +0000

    separate step decision tight brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def convert_to_binary(n):
-        return bin(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
