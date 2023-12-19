6730de6a6f84562f950efe6cdf7c49183136fae5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:59 +0000

    more share say flat everybody

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
