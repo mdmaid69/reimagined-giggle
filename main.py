1c7ec975734c418905b2618e38709099748dcbf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:34:02 +0000

    pen shoulder bottom responsible world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-print(os.getcwd())
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
