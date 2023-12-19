f996d5db9ef53c9833f0a75045c158b9d62f74d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:43:20 +0000

    discussion area trust Mm him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def is_odd(n):
+        return n % 2 != 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
