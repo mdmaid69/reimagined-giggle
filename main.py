c1ea467c5153b23fa5d116c511c1d790333e0022
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:58:56 +0000

    takeover about outside laugh late

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        print(sys.version)
