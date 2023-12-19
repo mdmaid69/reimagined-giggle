74f253f0b6d204c7ce781840971d945924349bea
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:29:26 +0000

    manage recognize until between state

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
