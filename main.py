1cf3f6fa677cd28fd0878b5f2fa6e413f00bc3b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:54 +0000

    regular send satisfied sort capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
