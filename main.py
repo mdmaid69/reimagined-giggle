7ced46b97c9f1285dc034e53b20cb761eac50ae5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:17:09 +0000

    forward ought home spite fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
