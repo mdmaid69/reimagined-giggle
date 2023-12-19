7a91938412731a70cfa7dbf5cdb5d7849574c597
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:04:52 +0000

    ourselves middle save attract goup

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
