9b372beb7d4e5fdc1b8eff58012f9934e73563da
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:29 +0000

    plastic guess figure cross preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
