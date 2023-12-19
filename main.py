7261c55bb5e02acc7cef87719db3a293859dee34
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:06 +0000

    failure heat mess you contact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Square numbers:", [x**2 for x in range(n)])
+import logging
+def log_message(message):
+        logging.info(message)
