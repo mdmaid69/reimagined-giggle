86a6a1a41d0bc93afc702659310fd34f7216b789
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:35 +0000

    sister wealth lookback(on) mainly serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
