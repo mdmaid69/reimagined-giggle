da51b2d46504e6409ec0ab5843e26e0b73b2d167
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:36 +0000

    combine demand instruction best edge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+print(sum(range(10)))
