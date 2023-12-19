0a8102049cd208ab233389839b75932f5f7b91a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:05 +0000

    pay funny hurt your certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-print(os.getcwd())
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
