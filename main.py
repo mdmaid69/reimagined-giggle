b4236c745ff4de78e362ad501bfe2d9db2fa1a4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:41 +0000

    intelligent pain tomorrow uncomfortable neat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
