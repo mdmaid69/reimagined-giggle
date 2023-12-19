132410097aa2ccbd82f3fb50382c6a2b9b6263d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:05 +0000

    find remember yet judge limit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
