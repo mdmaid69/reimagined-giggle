e3903becba3f29d91179aa1e4db5e828b9a63521
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:27 +0000

    temper transparent couple rush tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
