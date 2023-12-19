c8c0753339af9354fc346b1d1cc0a81d2bfee9e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:23 +0000

    tax tape future bunch comein

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
