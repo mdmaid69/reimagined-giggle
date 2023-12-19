7183d37afd041b60d0bf67837b472af21de51ec8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:53 +0000

    workupto protect valuable government sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
