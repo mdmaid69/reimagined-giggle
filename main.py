7d98ad30a938757c7f639a1473a58d90a1a41642
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:49 +0000

    bus tie cost calmly risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
