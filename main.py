a1811f4e95943fd616b88278f90e7f9908e6dc52
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:19 +0000

    cheat charge uniform cum rarely

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
