e3a01cad98511a907606bb4cd6738488362e96e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:01 +0000

    concern scheme smart wide rescue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
