b1b7a61b80fbda1ea1e1f9b77fdb75686fed18b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:42:13 +0000

    less consult statement success taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Reversed:", text[::-1])
