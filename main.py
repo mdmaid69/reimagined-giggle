432f8c0a872710dadbe318f872124df88efb4547
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:54:13 +0000

    eye equal somewhere ocean just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
