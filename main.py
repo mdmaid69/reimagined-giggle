bfe4f2466db45d8c5fab71738324de5cc79e350c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:38 +0000

    hollow account car breakin smell

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
