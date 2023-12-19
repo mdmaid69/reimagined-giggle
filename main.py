8006cd1b2b15aaebe10554c8eea369d9a0f4a0d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:07 +0000

    cry flat last activity giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
