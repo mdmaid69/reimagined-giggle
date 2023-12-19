521a219846a03822b94050dc6d8fbc8e69d54c31
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:40:56 +0000

    comein print suffer smell comeout

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
