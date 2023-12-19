9226e1046ad17275dd22e071bcdfd77cd65041a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:49 +0000

    pity schedule foreign however turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def convert_to_binary(n):
+        return bin(n)
+def calculate_speed(distance, time):
+        return distance / time
