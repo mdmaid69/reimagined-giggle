d77f374c26554eab4102a448b0a1142c6bf442a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:05:17 +0000

    free path pool imagine advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sys
+def print_python_version():
+        print(sys.version)
