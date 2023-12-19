575966bc7ead5519dcefba9d9c449625e8177dee
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:57 +0000

    can keepup ring create mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
