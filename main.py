6dd1a87b0c0122198a3c560d5849c2d5c3514077
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:47:56 +0000

    number contents sign mood hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def remove_duplicates(lst):
-        return list(set(lst))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
