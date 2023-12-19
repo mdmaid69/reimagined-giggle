5554f10ca2dfa0009f8ae77d4456e76b51b075e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:16 +0000

    gentle relation more bringup violence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
