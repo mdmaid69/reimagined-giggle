fa1fb9a635650430fed66aec368ce66d88b9409f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:33 +0000

    false suckme mad view guard

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-print("Hello, world!")
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
