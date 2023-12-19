b45b6ad12e6d05841d3d41dc7a1fc67d42ea315e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:21 +0000

    operate pocket drive wait odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
