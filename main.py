5126005c9781a752a031a8596ac45add1ec32138
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:25 +0000

    lower equipment bend boat afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
