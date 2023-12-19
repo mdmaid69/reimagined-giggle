afac8303ab594fca05bc3f27bf1939d600fc9e31
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:40 +0000

    vision name be book offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
