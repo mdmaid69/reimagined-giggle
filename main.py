23562e45e8f101d72a431d996f34c899a967f618
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:25:34 +0000

    home course life software excited

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
