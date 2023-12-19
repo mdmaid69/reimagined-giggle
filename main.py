0e2fb89a9ffd0494a8d2913d05dc03dcc0d30de8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:11:03 +0000

    middle tradition story smart prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
