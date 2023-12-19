ed7a15c2e6ff145a284b95f360ece9213f4b39c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:35 +0000

    meeting problem connection to proof

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_acceleration(speed, time):
-        return speed / time
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
