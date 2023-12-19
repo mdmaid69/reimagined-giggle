e556ab7435e6c214dfa6fc1b47bb83ae83bc78ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:07:30 +0000

    doubt book lookaround situation sometimes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
