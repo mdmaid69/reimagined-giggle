0b195f2ea9ccf166e4c05fc9b4bdeb8141038338
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:56:32 +0000

    567enemy thirsty generous order centimetre

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
