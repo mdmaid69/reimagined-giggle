b19653ac0876041a8a386bdd20dea3d8f89a0fcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:30:01 +0000

    talkback Zz unit shape law

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_current_working_directory():
+        return os.getcwd()
