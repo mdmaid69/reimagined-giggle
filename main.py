1ec50050db999395cf62bb295d7e37525d367337
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:11 +0000

    cross surprising tend practice compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
