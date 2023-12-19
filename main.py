b84793753ea7adeeb30e2b0ee0389549d6525bc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:27:55 +0000

    quickly neither quality warn hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
