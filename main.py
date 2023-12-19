05dd7b07ef76e647ba252f98d36aeb7151fb4c86
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:47:50 +0000

    centimetre lay once share impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
