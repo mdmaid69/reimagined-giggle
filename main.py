f53315a5721435264e785a9cd7616a9c8a3680ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:45 +0000

    gas brain shopping similar dull

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def count_elements(lst):
-        return len(lst)
+  def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
