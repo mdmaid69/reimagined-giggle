4f9a3a4e58e37a5f6d3a19be5e59c0028ef1c7a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:35 +0000

    either bang bottom central person

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
