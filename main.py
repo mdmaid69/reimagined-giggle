7fbb4c7e0494edab82faeef632633022bffb0fe5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:26 +0000

    name giveup address neither tread

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_pressure(force, area):
-        return force / area
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
