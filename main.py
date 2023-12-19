1704bd21bd08966f11e55ee2a624009a840618b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:29 +0000

    regard birth quiet least back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+i = 0
+while i < 5:
+        print(i)
+        i += 1
