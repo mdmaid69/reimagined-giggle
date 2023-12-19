1af92215d4e4e03ea0101a15c0941d23071020b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:45:30 +0000

    mention explain store Yy shop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
