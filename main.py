c595b9a2a4a42ea39e9a556b2e9fc884cde81cc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:55:13 +0000

    finger image house train juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def sort_list(lst):
+        return sorted(lst)
