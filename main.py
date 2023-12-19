41e57f8ccb6b0a99b961c89f900e032d32ac6f34
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:44 +0000

    tidy left none block talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def reverse_list(lst):
+        return lst[::-1]
