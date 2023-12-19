6be9f9f29f2ae0e7b810927b4d27aef7a0b80a9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:37:11 +0000

    sensible complain school various pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def clear_array(array):
+        array *= 0
