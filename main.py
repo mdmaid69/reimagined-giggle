120e7726a817430172fc5906d129c2b1d13f1854
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:19:03 +0000

    acceptable print friendly Mm collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-print(os.getcwd())
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
