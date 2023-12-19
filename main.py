5b66cc88e8322f25846e36939a25319a4b2d0c5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:04 +0000

    calmly wrap impossible limit watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
