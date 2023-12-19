fe55f18bfac33e576b6426d1cca65f55ffa5161c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:34:24 +0000

    talk rate style thinkof past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
