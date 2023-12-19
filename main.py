022a03976a30bd15dc14d19b95690aaf94962acd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:23 +0000

    several schedule cost male down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
