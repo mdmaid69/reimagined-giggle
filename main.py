faf96352783095753e10635cf06ac92429609df3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:40:34 +0000

    rise treat pipe keepup flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
