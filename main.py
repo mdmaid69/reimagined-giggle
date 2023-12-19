d526d7b04df566cb391e260c1065bb34e349e28c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:09 +0000

    basically up drive about miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_str(array):
+        return str(array)
