b0e419e1dbb0bb1dae69403abe924aa48bc55809
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:39 +0000

    guess sore over dance brush

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def multiply_numbers(x, y):
-        return x * y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
