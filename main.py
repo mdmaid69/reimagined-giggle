df70806604a6bed592a625826eb6bcf80ccb01c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:50:14 +0000

    pity her undo piece ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
