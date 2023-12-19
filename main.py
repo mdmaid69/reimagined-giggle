ff7db1d7636eed244aec031f500f395cd8062abc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:49 +0000

    progress challenge workupto art trust

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bytes(array):
+        return bytes(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
