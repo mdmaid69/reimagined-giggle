1f05e8a20c1ac51cec5a3b54719f61ae77d611f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:56 +0000

    satisfied wonder size terrible public

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
