e454b270396fc49e9a42d36da102440ee26c0a3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:10 +0000

    attempt hello damage complete pick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
