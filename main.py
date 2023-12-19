ed5f3e366820b93becf4c7c50edbc2931af50fd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:02 +0000

    through obvious score careful extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
