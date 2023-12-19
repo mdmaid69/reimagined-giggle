af1de54617faebe9be492c411399116281e8160d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:05 +0000

    more run flood people present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
