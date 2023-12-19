1241957401fa689e3973fa0f1eec1c37a2bd7bd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:08 +0000

    broken unite lock bringdown back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
