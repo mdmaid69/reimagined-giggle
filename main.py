1c92d4a1abf0ffa21148d4f4d70cefa5f59162cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:18:50 +0000

    inside taste office paper reply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import os
+def remove_directory(path):
+        os.rmdir(path)
