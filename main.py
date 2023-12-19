74c6031c73f2104764ad464eb8067b46168ee511
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:49:58 +0000

    school refer away range steal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_queue():
+        return collections.deque()
