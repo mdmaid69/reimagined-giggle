3da72db1c573e89a6038f6824f07be814a0edcfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:35:33 +0000

    rank travel sometimes common married

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
