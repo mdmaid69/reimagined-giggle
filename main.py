3cf4b610f64288c67221b81626d0d1edf1b9d08e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:25 +0000

    thinkover quickly allow learn half

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
