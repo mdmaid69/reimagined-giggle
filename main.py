dbc7be20d6844f9d8b1a0a6b4e1dba527855196d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:31 +0000

    sound print trade black trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
