540d626497dc96b39e28342ceb6e509ff6242ee3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:39:32 +0000

    tear invite below variety through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
