23142509f6fc33913d6a12f96b8c4c4204f75552
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:23 +0000

    start in range impossible agreement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
