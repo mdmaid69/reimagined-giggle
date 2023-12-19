e05272cc700d2a4bbbf11a6a2d0153b62a3a5556
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:14 +0000

    matter press Vv organize drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
