8f635412a748ef333b2fb35196b5a698fd993b6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:18 +0000

    cold perhaps equal through will

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
