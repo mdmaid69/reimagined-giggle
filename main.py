b7a4eb8b954b80d20cecbd2c77e0d96a9483cad4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:18 +0000

    odd air runinto possible fact

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
