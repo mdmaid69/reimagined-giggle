90f11fd600b85dfd40a0ed89a8cc597b0954d14a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:33:50 +0000

    would heart view workon 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
