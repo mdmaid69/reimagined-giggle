de81b3d050d9b49ae52992127ffcb11db3c811d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:06 +0000

    keepoff doctor Zz run difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
