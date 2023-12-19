432374d3cd52592e68f9cb810b715798db3f0728
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:43:47 +0000

    pile spring later comeout joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
