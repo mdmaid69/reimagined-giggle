5702045324252a3510ba7131f22451ddf4a87ec2
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:29 +0000

    feeling against bang charge thorough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
