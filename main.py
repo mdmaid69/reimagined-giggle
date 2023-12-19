e6afeeecf70ee33570c498ab04cd58030fc96f69
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:43 +0000

    wish attention round your rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
