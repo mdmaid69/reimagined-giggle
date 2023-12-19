7e548b308c6113a2c9bd6af94c4e5a1fc59348b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:48 +0000

    consider deliberate disagree OKorokay chief

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
