bf84d2ed6b028df7d32213c7b47df6dee7fdf121
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:42:23 +0000

    worth bill form congratulate gift

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
