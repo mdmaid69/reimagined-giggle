179ef5062e60550c9d69c623e8722d358201ace1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:05 +0000

    wonder worst hollow 573entertainment Kk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
