85345682ab5db1fc1c282b6f539979239f2faaeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:17 +0000

    run which then sea now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_tangent(x):
+        return math.tan(x)
