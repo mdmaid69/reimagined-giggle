11838213b8540c9abee763343474494d8845bf42
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:14 +0000

    duty holdon underneath when hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_characters(sentence):
+        return len(sentence)
