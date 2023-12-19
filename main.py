676af3c0008f28c0102be3afbee986aaf770de9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:29 +0000

    truth ignore by exactly shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def convert_to_binary(n):
+        return bin(n)
