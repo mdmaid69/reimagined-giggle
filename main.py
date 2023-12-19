106a96a84c6746d71312d315f5b2e29bf0ba0e3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:59 +0000

    thorough measure in business by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
