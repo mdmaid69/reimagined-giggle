7725d09edd212723e60ba4fbc1b46f33bf5c21d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:32 +0000

    explanation breakout otherwise common improve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def cube_number(x):
+        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
