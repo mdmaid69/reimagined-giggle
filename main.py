5ffbbd603eff8ae3f348a71b3da64a35eb74cd99
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:12 +0000

    wing empty Yy increase emphasis

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_typecode(array):
+        return array.typecode
