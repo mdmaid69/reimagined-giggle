b65fe0c68e8536819e796e59851eb696119705ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:48 +0000

    they lot shop usual priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
