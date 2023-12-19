9a9c6d72517776569681fd05dd3d0c796610a5bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:19:53 +0000

    only ilovedicks bringforward moon short

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
