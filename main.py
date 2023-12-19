8ba0023bfa81b09c419db3fa248efa5c2d580f9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:38 +0000

    soak confuse travel properly memory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-def exit_program():
-        sys.exit()
+def is_even(n):
+        return n % 2 == 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
