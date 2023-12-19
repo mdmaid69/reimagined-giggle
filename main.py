f22d5ec2523814e653591a8132cf6b34502628cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:23:06 +0000

    watch will goout breakdown fold

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
