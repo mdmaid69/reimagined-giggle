f9cde695c9bc07883c857cba5402f96c83283ae9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:42:06 +0000

    eye thank regularly honest ready

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def append_to_array(array, item):
-        array.append(item)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import json
+print(json.dumps({"name": "John", "age": 30}))
