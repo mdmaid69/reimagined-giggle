31c7eeee7d28eefa0e77428d23017fd07590c623
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:39 +0000

    shape very ring half goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
