166fa80ba5f78ad12df106ae7d80205a324bc501
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:58 +0000

    telephone sensitive burn when talkback

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def count_characters(sentence):
+        return len(sentence)
