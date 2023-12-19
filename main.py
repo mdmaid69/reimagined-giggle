d98a6af852e92f79736ed68e8b3e3216d72eca85
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:48 +0000

    point woman fill uniform damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
