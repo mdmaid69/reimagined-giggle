7948429db4fbb197e45632316a66d6271e566239
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:28 +0000

    long difficult circle prepare narrow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_set(array):
+        return set(array)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
