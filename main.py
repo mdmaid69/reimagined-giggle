b2050953178819bb3cd3ffaf87633ad80ca096ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:43 +0000

    priority nigga away nearly party

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
