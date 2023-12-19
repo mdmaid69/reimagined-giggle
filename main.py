27c5aeb7e58b45e2bf261dc0b4e05985c060dd51
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:49 +0000

    official used tell ahead ring

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
