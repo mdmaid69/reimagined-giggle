dd028c63585187ff1cf84a40de57fcbbf00fb3f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:30 +0000

    accordingto until comeback result nose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
