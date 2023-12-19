cb20e523a206aedf089efdc36b2fef9ccc99d476
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:48 +0000

    rely square neat children port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
