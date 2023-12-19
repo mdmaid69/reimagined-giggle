9068fe99d7b893e5223316065e88b46627198ebe
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:37 +0000

    appear workout your strike similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def set_array_item(array, i, item):
+        array[i] = item
