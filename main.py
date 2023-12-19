867a5d414522a4773536e0e4e73005e6fb097811
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:06 +0000

    perform limit next flat 569enjoy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
