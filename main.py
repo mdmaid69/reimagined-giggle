8a12f700d529c2e53c050c1fdf6d0fc4025dddce
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:43 +0000

    earn shopping spoil brush person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+for i in range(5):
+        print(i)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
