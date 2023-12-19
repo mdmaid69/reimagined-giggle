f0cbef6f88ccde3f51e6cf330217067743ebbf65
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:56:39 +0000

    mad active then busy callat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
