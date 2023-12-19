9095de5a9ea98a9b355337ca3714b6811393fdb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:53 +0000

    married increase group succeed only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
