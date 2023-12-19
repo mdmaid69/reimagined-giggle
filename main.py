8cad07cae695cf42110c7ae5d708bb7e2daf8196
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:04 +0000

    reserve mean hide apart need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
