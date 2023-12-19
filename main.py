7ae3f51cccf8e66e668daf3b7ebd4a12d2492616
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:39 +0000

    tear lose human guilty soft

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
