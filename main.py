96443389fcdb02ae591523457b90cb6a3429779f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:28 +0000

    press ilovedicks noisy drop community

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
