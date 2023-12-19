9da71fd6451e6c4f4d5a97774f20956e1cd964f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:14 +0000

    everyone trust pleasant flat fold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
