26cee907356f9d2325178599908241d776fce8c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:57 +0000

    young mad after guide children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
