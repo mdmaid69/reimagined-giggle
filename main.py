bb3382931fb4111853dc0064d3da088f9f06714c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:09 +0000

    touch neat equal unhappy boat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
