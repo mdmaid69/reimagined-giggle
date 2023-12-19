c09ad563dc79cf766b3e1e3ca8436baa95d65a94
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:11:31 +0000

    dinner lock although today once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
