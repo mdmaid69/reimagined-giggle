9e7f2572592963d614a3f6435a78c4f211c38606
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:02 +0000

    attract first confidence thinkover noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
