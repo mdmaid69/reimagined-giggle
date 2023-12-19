74383dd1e252c02390152dc699eee10d745282de
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:35:14 +0000

    season reason claim basis service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
