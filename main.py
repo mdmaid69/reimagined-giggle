6df652c7ac407d75a329e70303176d0accc865a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:11 +0000

    takeout dear start tomorrow tall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
