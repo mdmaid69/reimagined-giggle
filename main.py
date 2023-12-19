f0c53ac35ae4e9775b35024dde16e180e96b3df8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:22 +0000

    party bar club cook approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
