cdcda3a73c603dc850b92425eedfd992929a9eff
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:45 +0000

    worry length protest ear false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
