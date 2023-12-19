03f26292311802241d113744a51f05b4dcd58277
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:18 +0000

    type free respect pay plain

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
