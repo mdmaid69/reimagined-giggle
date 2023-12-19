013beb01a1f191573ad7bff2c12ea18efb01f676
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:05 +0000

    partner empty show everybody spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
