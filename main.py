fcc96c5604e801e34c242d9a1985374ae852b294
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:18 +0000

    means here heart mixture I

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def append_to_array(array, item):
-        array.append(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
