32d5b38338b77e0c6543c82355f57e5dd3a1c9c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:23:35 +0000

    instrument welcome mad possible minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
