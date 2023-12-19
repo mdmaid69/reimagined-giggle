7975d57823f0c78a1d8b3ede255f2aa7c147f79e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:39 +0000

    object Ww workon point agreement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
