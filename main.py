7d7bd060e1ad4d539281355bc5ef3608809f5d17
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:52 +0000

    mother field scrap murder post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
