7ebc60b34d72fea76eabcebc0ff6c6e68b763dda
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:34 +0000

    frighten calm properly send hair

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+text = "Hello, world!"
+print("Characters:", len(text))
+  def reverse_list(lst):
+        return lst[::-1]
