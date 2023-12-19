0a710f45d42640d89b3a7df6fbf1487fba5bcbe6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:16 +0000

    before move walk complaint cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+for i in range(5):
+        print(i)
