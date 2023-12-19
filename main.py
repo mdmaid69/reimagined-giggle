f300c59e6fedb57e1c005176247bb1de3f69728b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:02:44 +0000

    hurt dig final themselves sail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
