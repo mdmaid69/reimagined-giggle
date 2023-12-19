f0496a3a4e83149e8b522883a0c9f20076d37109
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:33:53 +0000

    borrow muscle Ee various turn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
