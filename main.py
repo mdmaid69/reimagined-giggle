e0825ab825eba09b0fb3752e415bf803db407808
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:07:09 +0000

    chin original colour all generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
