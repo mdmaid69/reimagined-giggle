5656a153ca40095a9ac1e009883455375a01c4f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:15 +0000

    might punish material low Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
