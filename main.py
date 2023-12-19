524ab8a69c28f989c8f77c528d67f313e284914a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:32 +0000

    forever roll embarrassing replace tight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
