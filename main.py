404009360d9a7d0efe6082b748abbe6d85e41fe4
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:26:49 +0000

    drop often theirs undo holdon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_user_string():
+        return collections.UserString()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
