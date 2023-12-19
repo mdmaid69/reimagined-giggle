b36175ad12b2586f18e8f34fb0050209733df29f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:37:27 +0000

    however aware positive excited relief

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
