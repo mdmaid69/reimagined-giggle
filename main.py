f2ec95139cbfe37040eef486739bc03a10cd4c6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:27:31 +0000

    571enough light wipe amount discuss

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
