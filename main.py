ec290c456023d15fdadc20f131f2c396dce0ef8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:54:41 +0000

    beside where money pocket city

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
