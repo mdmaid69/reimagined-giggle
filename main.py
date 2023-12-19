21f915932bbf63f6bdb895d775772e75478a7154
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:42:39 +0000

    rush pocket unhappy busy sign

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
