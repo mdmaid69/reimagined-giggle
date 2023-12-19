c05c4ab4a6085734be3a6c3a3de4a0eebb231e95
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:36 +0000

    matter cruel sport hour touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
