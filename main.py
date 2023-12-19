a650b2f7b5da5cd69329f4be1fb86cde6c3eca42
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:10:47 +0000

    fence teach quantity ticket preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
