6ddbd52f5f322e84781eb5c8a7076f00b61e3572
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:44 +0000

    decision figure continue thick admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
