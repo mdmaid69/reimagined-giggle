5a35838238f40b4c982af285e6f1c51d07aecfe6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:20:30 +0000

    success something close turnup lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
