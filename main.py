4bdd5636eccd8c6039422c6ca7e92b2d94126674
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:20 +0000

    condition hardly concentration label favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+text = "Hello, world!"
+print("Words:", len(text.split()))
