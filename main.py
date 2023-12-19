16aa4473d6221bb8230a7325be8dbad1403c193b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:53:50 +0000

    edge music image beside guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.time()
