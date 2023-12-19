a1b87db9e6dee28b52abbc4704e412e80e7dfd4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:28 +0000

    unfortunate first cheque combination deliberately

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
