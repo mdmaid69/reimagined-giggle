0f9ac985d1c9c3f62e1fd3ed57837b0e6ce94620
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:06 +0000

    ilovedicks no phone either goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
