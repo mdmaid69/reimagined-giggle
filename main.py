a7d2476f4ae40e591b5d12b25bddb6cfce80e862
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:39:35 +0000

    far Nn soft pipe 564encourage

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import random
-def roll_die():
-        return random.randint(1, 6)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
