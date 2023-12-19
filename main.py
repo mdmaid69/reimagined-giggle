d3ff30f144f27e046e51c52bd50659f3031cc869
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:18 +0000

    even father bang car approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
