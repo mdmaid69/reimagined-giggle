4ca97e3aca9a4036917c19fead8736bc8cfa7a69
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:49:59 +0000

    loose soak walk communicate thin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
