366a903d545a0cc7763ca3e1478ceb6156a3b91d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:56:55 +0000

    error edge case deliberate birthday

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
