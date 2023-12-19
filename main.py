866785cac285227f7f2acac734855cc3f4798550
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:54:51 +0000

    read delicate quarter away almost

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
