83ad8c7770a3be035d8ba9ccc959771764c9babc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:51 +0000

    keepfrom search little talk fine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
