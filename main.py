93824778e4a27cfd22d61600c62258ff76bb068a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:11:51 +0000

    her load label maybe steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 def calculate_work(force, distance):
         return force * distance
