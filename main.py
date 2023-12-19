615469e7b1d23fd63bdbcc0d82774dcc14a4722f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:30:18 +0000

    to mystery stone occasional set

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def multiply_numbers(x, y):
+        return x * y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
