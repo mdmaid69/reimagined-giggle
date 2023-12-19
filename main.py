a962284d15b616bc319d5ad299b422babd0a5d16
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:38:43 +0000

    paper theirs wipe keepoff transparent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
