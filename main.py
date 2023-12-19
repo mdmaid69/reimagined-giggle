6180e63c2e9bc240b2aa36b15eea3c986aea870d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:02:50 +0000

    open that home Ee position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
