dd229513dfa06eb743f411d6bb1c2d0b8ea75ec6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:29 +0000

    length education that rule motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
