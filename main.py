4d500351580bdc7501aa6678ad8342c393b9c858
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:27:47 +0000

    drink away bad shut properly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
