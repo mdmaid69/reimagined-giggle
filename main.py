f2fa307f563eeb495a77079d0d6ebba54e664b00
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:58 +0000

    area tell hair crash loud

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
