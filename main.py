23922708eb89eb9fe482a21d83baf786c3bf02a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:21:27 +0000

    unpleasant house closely test bread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
