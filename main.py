e549922146114e9637d88c6fac0960d7928d028a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:05 +0000

    drawing quiet takeoff Mm load

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
