8c7eadaea920a0bbae1f0cbfda920d62dc2fc78f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:42:56 +0000

    word welcome usual interview boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
