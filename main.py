4c1305c75808e7c78b5ba4eb681f28e16b69890f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:33:34 +0000

    mail grand with matter second

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
