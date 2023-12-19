3f83325e61a373d51ceba68e6bf5978b8e6587e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:01 +0000

    old busy correction muscle back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
