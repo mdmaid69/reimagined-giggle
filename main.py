9488931acfe7b78000426250378f53524be3fcd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:57 +0000

    wind dress gentle great breakinto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
