eaa5f941da1b2f426548af59325f71dff56ecb52
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:20:37 +0000

    which travel some concerning really

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
