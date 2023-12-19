cb41b734eb8d2d56cdfc2b836ade0150747c5d20
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:11 +0000

    impatient wild sticky paper use

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def reverse_array(array):
-        array.reverse()
+import os
+print(os.getcwd())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
