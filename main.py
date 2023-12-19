8eaee1e7c8fdf78ab54ffd89e7e93c13d5172d9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:34 +0000

    suddenly wise drawing no may

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def cube_number(x):
+        return x**3
