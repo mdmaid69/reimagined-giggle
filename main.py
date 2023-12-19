ef58459b19c8fbe060b79ddb918130f0febd5e7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:14:49 +0000

    sky size giveback judge where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
