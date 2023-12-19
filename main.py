e081714dfec058feace05c4aad71b4c688c8c1eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:47 +0000

    like sure land cruel regular

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
