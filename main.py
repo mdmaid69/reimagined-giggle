7362319d7d9f97ef0a995f254f100fa6ccffeb3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:22:45 +0000

    tight word ever continue transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
