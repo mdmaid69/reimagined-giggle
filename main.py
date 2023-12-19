9f4fab9c515038fa5986906180905b98f5041f12
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:35:18 +0000

    team relax stranger crime attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
