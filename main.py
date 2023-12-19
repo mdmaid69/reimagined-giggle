729cd1f9be0c00bc94a4fee73eb4768af1a4206f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:31 +0000

    shelter plenty design careless herself

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
