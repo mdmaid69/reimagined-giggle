4975dfa737a87a92c56d28ce1b829a228095d76a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:23 +0000

    nose inside read 567enemy under

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
