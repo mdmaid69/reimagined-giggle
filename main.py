f613f3137738e0009732edd9485c64fdf79b9dad
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:11 +0000

    centre tired tight thinkthrough least

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
