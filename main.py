e728a742303e95bb9d6f61c571e6fa6576bf5975
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:24 +0000

    accommodation poor respect breathe best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+import math
+print(math.pi)
