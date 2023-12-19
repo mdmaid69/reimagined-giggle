56e620be91e1a4a396401824d7920db5d0e7cc1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:06 +0000

    scared improve separate their port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
