03152c41c3cad4b93aacc527725230943c35e269
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:46 +0000

    limit half remain free reduction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
+print(sum(range(10)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
