7b49ceca25d4c6fe14be19cf300211f4ad6bf640
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:48 +0000

    past tend instant field undo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
