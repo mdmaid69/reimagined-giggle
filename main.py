1373731febb664a640bfed28119b8fc5e3048cd0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:54:21 +0000

    how dream healthy agree occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
