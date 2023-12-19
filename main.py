25ab5a96b36d3c42a3ea2c714791fe5189ec36dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:41:39 +0000

    cum clear count accordingto safety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
