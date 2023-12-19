fdc64996708b36453a872d30f02cfcda8e2a0195
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:54 +0000

    minimum feeling mail benefit under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
