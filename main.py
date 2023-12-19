24eb4a5b86f82e837d4267c5c38246ce1d274144
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:03:07 +0000

    demonstrate bet luck history repeat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_power(work, time):
-        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
