19e8582786fc77a07f878995e52ad6098d096793
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:15 +0000

    permission control its thing file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+print(datetime.datetime.now())
