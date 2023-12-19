7fe44d539ebcc62a80fc2e31ec19f47ba84c1459
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:14 +0000

    line through like second string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_work(force, distance):
+        return force * distance
