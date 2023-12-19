4c2a7631f586767a6ffd6fff9d42a8639971afd0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:03:51 +0000

    add meeting imagine knowledge unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
