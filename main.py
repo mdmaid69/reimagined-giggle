407540fa4eefcc675dacc616cc0adc97af9f05e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:37 +0000

    bush term load opportunity air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def greet(name):
+        print(f"Hello, {name}!")
