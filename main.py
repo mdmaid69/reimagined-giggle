beb5dc010b00c0386b29bfa0d3a3671a27a405ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:52 +0000

    heat disturb reduction excited sense

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def square_number(x):
-        return x**2
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
