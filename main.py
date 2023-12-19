54385b4323f8a9178992fb54c61da4a56af91a16
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:50:55 +0000

    my connection arrival permission wash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
