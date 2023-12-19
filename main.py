9256ff84e0682e94bd952e46d3df07b63353a657
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:23:31 +0000

    actually work nose mean exercise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
