d36367c663cfd593b2a14de1b752857720f603b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:17 +0000

    quality classroom become transparent quarter

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
