c713b38867ed98d7344379a5d1f25e008b66d7f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:23:39 +0000

    sell slow birth lost trade

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def square_number(x):
-        return x**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
