403fb88da31b19159e2e447ecef3990c8f5b5185
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:51 +0000

    pause approval gentle responsibility check

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Squared:", [n**2 for n in numbers])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
