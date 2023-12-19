a7b118628717b0ebfc0801ee4e7b33f028420fe0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:10:47 +0000

    fact pronounce variety suit receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
