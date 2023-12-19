c553fdfbb7d82cccee59114b3f7167e96c4c0617
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:59 +0000

    deliberate modern ours genuine dangerous

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
