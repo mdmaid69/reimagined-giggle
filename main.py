b579c06f0b4bbf5085d6da0231c61d4de233be77
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:45 +0000

    talk ask pleased suit lonely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
