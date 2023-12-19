ecf31b9c3449775f675d6d954ec859ebbcaa581f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:26:45 +0000

    tongue entry steam present main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
