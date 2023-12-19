f12c79bc7be341fbf698fdcd32874c18e7544f83
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:40 +0000

    everywhere search makeinto if special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
