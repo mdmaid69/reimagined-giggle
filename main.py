6ae73f4d0141094b62cf2e602a7bc60ee2e72875
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:00 +0000

    cost exist side swim sale

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
