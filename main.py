fed58207397ce65286c84310a7b0afff6b66fed9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:33 +0000

    surface interesting age warning fix

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
