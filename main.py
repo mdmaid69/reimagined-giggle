9ea592acdf2e01bce1d32328fd33dcd2d2f817b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:37:02 +0000

    cover hope agreement description top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def add_numbers(x, y):
+        return x + y
