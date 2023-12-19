e1e423c26e008e14fd8f3532bb19a3f5d4809a54
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:31 +0000

    describe string commit Ss lock

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
