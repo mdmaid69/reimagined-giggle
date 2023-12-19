0e18ab250b1b624200cea106f9651c59aeedc297
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:22 +0000

    steep genuine group pool whom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
