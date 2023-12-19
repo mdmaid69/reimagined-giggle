b0a817936661b8a9d556acdecca00973f4a9b456
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:23:05 +0000

    married workon space declare name

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(10): print(i)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
