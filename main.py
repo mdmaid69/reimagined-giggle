f36a61c25fcec5099ccb4ff817321511e6304db8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:41:46 +0000

    continue chat duty history will

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
