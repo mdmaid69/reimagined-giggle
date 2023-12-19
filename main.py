478e41025ff0b7fa2a63b965686f355766dccd35
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:04 +0000

    recommend remark wish talkinto stare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
