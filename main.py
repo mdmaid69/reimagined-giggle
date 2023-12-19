53de0d318165dea4220798a2e9afa21ed3f4ab02
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:09:03 +0000

    OKorokay laugh sensitive feeling another

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_frozenset(array):
+        return frozenset(array)
