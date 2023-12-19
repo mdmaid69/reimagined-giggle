0b793b97bea994c2fbd69cefccec166a04810b91
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:45 +0000

    reply when regard confusing dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
