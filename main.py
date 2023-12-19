d8401dca244b5a32505a18d59c925cf6232f4622
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:40 +0000

    choice wise otherwise normal win

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def multiply_numbers(x, y):
+        return x * y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
