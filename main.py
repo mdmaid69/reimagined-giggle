5ff15d58ff2c302ab31241289a7b0a367168e303
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:29 +0000

    contribute summer freeze callup lucky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
