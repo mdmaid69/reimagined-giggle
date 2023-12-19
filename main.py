c0555cb8e42a68a478744a0adbbe0f643dd6c947
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:45 +0000

    base throw absolutely disagree polite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
