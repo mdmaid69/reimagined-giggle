4ca58c0bd445217ba23a8e8cc2dedf9840ebc049
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:05 +0000

    giveup instead second shut child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
