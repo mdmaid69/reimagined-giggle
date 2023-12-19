e6420cb9e43e44a76de545a2063069a8a221e42a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:26 +0000

    pinch makeup remain nigga workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
