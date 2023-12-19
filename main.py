e1ca6c8211197423e82af61b87b2a06dbc0995c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:55 +0000

    neither travel rubbish boat university

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
