0a978cf619e04bcc795bc44af95ec51c56e9828e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:05 +0000

    crazy difficult achievement violence indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
