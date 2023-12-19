bf291e28381cc6b4e7b914d53a5f2f745a4f0188
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:16 +0000

    design smoke soon grand Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.time()
+  def get_base_name(path):
+        return os.path.basename(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
