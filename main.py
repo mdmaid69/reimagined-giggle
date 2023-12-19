1dbc71ca8e620960c626e492f2c052a6b5f34e52
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:43 +0000

    typical building separate base guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
