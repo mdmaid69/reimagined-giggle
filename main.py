0e1d95e6f35c15c9283fe751dd28f079a97666d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:13 +0000

    narrow attract programme word area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
