e8aeef237378f050cac77e46bffe32ea7a030f62
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:09 +0000

    oil complicated flow ocean roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
