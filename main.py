f0c50b07e9deed59b7cd9593868a5f151b09e698
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:31 +0000

    favourite walk down callup borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
