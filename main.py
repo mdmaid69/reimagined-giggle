fe166b777027470941dbddf956fd879c5814764d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:22 +0000

    dress nerve approve false poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+print(math.pi)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
