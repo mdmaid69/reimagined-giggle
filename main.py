aa08eaea8ddcf392357093ca65cd76edb6ecb5af
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:56 +0000

    head runinto gate lift fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
