09c759b03e526ea8ad899c869887da680c2a37a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:07:10 +0000

    relation expensive meaning sign purpose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
