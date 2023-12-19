79e7be94d4c58c3dff57eea4ea4f2be6858f20c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:17:34 +0000

    cum innocent you else ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
