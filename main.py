86f00dca961e387de2dc68cb0666390afd56f155
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:46 +0000

    shame on brown complaint interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_item(array, i):
+        return array[i]
