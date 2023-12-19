d067ece28767dcc4b2e22d7b972b1e15789eff4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:19:07 +0000

    black entrance accident Ll characteristic

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_density(mass, volume):
+        return mass / volume
