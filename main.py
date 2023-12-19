35ad55153ddd9a241ca824da3f48a7dae1ea091f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:15 +0000

    phone admit like possibility wave

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_max(lst):
-        return max(lst)
-print("Hello, world!")
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
