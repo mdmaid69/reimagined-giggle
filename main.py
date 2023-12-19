35a0807eacf500610cb79e4c827a5f28ea7e2993
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:29:56 +0000

    impressive print under unable rough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
