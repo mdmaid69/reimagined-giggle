7311c434bc8a86759af0c1792cb7159eb6b7b9c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:27 +0000

    injury beat somewhere later battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
