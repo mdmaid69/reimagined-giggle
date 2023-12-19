f6b7cc4c2a012af63c5f4bdba9faa231f126a5d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:15 +0000

    trust turninto build tight neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
