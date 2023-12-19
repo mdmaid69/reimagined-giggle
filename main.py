927c108a748c4b6d81f186aa642f8802ab70ade3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:27 +0000

    clear education lay pretty agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
