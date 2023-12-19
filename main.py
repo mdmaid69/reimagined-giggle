683b8d0ac4456c02a075f1db2cddfabf77d21eac
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:31:42 +0000

    platform nerve slice roof physical

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_tangent(x):
+        return math.tan(x)
