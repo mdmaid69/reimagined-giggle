d545fd4f4c5dd3a6bcb8cbf4e3fe75672bb624ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:12 +0000

    regard side problem spend night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
