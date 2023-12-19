3118f75ddb8fbacc5c414f68d84b2f1c840dde73
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:51 +0000

    must official count claim possible

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def is_even(n):
+        return n % 2 == 0
