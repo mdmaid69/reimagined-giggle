4eb98f761bfb6d40a27b7c88138d05cfe6642baf
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:02 +0000

    constantly earth instead nor relief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
