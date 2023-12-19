8499f7fd2418255bcf6908c711954255e03ad52a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:41:00 +0000

    climb sure oil before card

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
