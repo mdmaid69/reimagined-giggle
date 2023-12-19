55e4a28d452d455dfef577a5f8ed86b919b835cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:59:30 +0000

    example direction profit I children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
