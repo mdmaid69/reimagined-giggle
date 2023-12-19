a4fa8a83ca46fe3ff6aad2b02ed53d34c6858f9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:20 +0000

    come mistake kick deal influence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
