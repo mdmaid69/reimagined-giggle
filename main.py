06a7fdb30b795c149ce8317105e35cda1cc15ee1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:25:16 +0000

    again remove turnoff so considering

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
