72a8f9e7ebd2e8e9db563e2b16ff63b453b8dfed
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:57 +0000

    area crowd rubbish size difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
