c0b1a17d87aa82b0c4fe870c3388cc7191830cf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:24 +0000

    light service succeed sticky train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
