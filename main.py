5b503ca69d57be6dc9a7545eda21488f52f3980a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:59:57 +0000

    threaten they port support direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
