5d84fbb48341ac6a9db17477c0512beabbe95e2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:24 +0000

    length give soon beautiful desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
