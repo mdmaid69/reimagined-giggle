880aa22e9f9433e3fe7329725b749919db35dfb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:35 +0000

    matter talkoutof acceptable disagree culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
