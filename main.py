5aac74ca557c1881d96faee6b2c69a18576f0fb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:42 +0000

    team determined pleasure land call

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
