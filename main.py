da5407d8daa9e91387a29fd5c3e10b9513f958f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:27:44 +0000

    suggestion message bin far pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
