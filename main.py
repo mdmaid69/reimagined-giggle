0d05d11300c306fb933949587f5ffce0df6097ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:59:14 +0000

    fun free disturb situation cause

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
