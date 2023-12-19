9c06fabccca246b1744b81ec7b8776e7e6145f87
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:09 +0000

    excited say mess sensible variety

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
