7376ea003fd61b40fea37d63b712c2d56090864e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:58:07 +0000

    circle female slide true congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
