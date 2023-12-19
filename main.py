2e644eef971b0791c52d7bac5e5256abe412d81f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:40 +0000

    century accordingto chase offer careless

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
