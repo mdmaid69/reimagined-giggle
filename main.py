40c5508a6cdfddadceddcdefd8ae4d5933cc0007
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:59:14 +0000

    careful pour rely positive turnback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
