95914891558b541dc43a871fbea8fa854812cb9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:33 +0000

    interfere neither however better nothing

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
