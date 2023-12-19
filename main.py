71396bb61fe4f7f5e3742e855c9d6fb8840903cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:48 +0000

    politics idea black left hit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def is_odd(n):
+        return n % 2 != 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
