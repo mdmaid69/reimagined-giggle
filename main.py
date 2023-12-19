950dd25bc40e484bedaff9a3c485fb550340ac39
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:52 +0000

    contribute private attempt common surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
