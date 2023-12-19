12ed76c88bd14199799a3603fbe8a1d619d13546
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:41:42 +0000

    gap activity border principle kind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_acceleration(speed, time):
+        return speed / time
