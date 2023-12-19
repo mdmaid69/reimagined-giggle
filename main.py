674eb9aecb65f6b9c016f80bf6d14fbb11031254
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:04 +0000

    reply important professional else trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
