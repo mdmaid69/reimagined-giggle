05c1a58d5f543dca9defd7e3c2047e881544a851
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:40:49 +0000

    land become shall unlikely bother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_work(force, distance):
-        return force * distance
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
