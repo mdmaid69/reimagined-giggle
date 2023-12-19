4497105d77b6a21c69381f93a3dfc848ba0881cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:13:20 +0000

    middle rich since frame programme

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
