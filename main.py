f527be9728ca6ba83b727c53b142aab2a08d8299
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:27:46 +0000

    much explanation medium treat hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
