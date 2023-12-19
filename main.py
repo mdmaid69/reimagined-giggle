b3eec4ca1ee26f8ea260e9dc7656d0ab9a0f9ed4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:43 +0000

    game introduction message better act

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
