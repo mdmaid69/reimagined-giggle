b60dbf4183b3e4a6da08e3ad3596933913d7b8ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:54 +0000

    shock disappear get climb correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
