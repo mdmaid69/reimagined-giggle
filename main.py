8b7aafc17ac66338999881420da0edbdd66d1368
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:50:23 +0000

    embarrassing steam turnon takeout separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def square_number(x):
+        return x**2
