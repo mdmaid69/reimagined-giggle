fe11c7ede9c7fac8a38192bb3528beead3599731
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:02 +0000

    mark healthy concerning 566end behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
