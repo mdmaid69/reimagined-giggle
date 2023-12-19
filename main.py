88aff3c111524e2139a8d65d3f99511319907314
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:32 +0000

    179bite occasional proof arrange worse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
