966e9d7a26583767dac7be2a8e6083f0eb1ba3e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:16 +0000

    pour vanish maximum mind everything

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
