556d8303fc596e70e3d1dbf9d72c0206084c63b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:25:02 +0000

    maintain allow anywhere month modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
