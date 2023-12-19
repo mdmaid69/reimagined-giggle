a07fcc22dd482dbfba8758509bbe33f61c3d49a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:03:06 +0000

    small out material answer crime

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_to_hex(n):
+        return hex(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
