fcfb99a88b025eb31c82cd738d937ca294a5a930
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:41:17 +0000

    concerned shut begin only catch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
