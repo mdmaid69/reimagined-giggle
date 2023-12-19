9160c6a1950a517f310f773af5f14453c220ed8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:58:48 +0000

    feel influence desperate calm important

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
