710454a844d2409f20e62c2d5ef018824e8215b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:48 +0000

    base through concerned Dd loud

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
