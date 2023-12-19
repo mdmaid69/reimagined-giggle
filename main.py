793c23be599d7b2514fb604dbad791d2e7875c3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:39:50 +0000

    crime cross grade violent defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
