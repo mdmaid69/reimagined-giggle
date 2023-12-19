a4676201fc0b90f6f2d613c3b4210ce519f015c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:24:32 +0000

    beyond bus last division capital

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
