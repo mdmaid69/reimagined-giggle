336fe5272e3756148baa72bf105101f49ef40f7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:53 +0000

    further congratulate heat pronunciation original

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def extend_array(array, iterable):
+        array.extend(iterable)
