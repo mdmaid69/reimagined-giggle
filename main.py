86e79f428a5ea8722685130c32b7bbb1c4446cef
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:53:51 +0000

    sorry value lean shop scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
