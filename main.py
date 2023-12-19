fdc40091da7a32d754aa1c7ae4f8be138a0c5468
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:14 +0000

    skill tight bringup tend tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
