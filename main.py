8477160974a4e358a437781f7fb018b5e8db9e91
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:57 +0000

    settle complaint ourselves achievement motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
