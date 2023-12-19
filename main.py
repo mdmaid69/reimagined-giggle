5e1f65234634fa5ad701723029fd20a5449027aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:21 +0000

    sale remove black bright defeat

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-def get_current_working_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def calculate_area_rectangle(l, w):
+        return l * w
