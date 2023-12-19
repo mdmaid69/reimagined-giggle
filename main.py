62074441516a7975d929aade789d087587f16977
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:18 +0000

    illness event unfortunate plan score

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
