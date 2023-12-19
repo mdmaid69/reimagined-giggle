7f30bc4ca2868d77fb8fd2043721dd9323ede990
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:14:42 +0000

    for dish communication sort invitation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import random
+def roll_die():
+        return random.randint(1, 6)
