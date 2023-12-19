6e2af8cb1c42315d5005eefbab7e670d18a9b703
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:01 +0000

    represent hold there then spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
