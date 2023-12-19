612db90f8a46a90d3779cb47ef171a5af0d09d16
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:47 +0000

    succeed about excuse strength shout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
