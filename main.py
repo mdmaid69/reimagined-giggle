051676d2af85438eefa7c0a1254371f375307e95
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:05:32 +0000

    exit rubbish turn middle poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
