8dc321918ba876dae384b2ab4a1930a3a37ae5dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:12 +0000

    tour along argument Rr whatever

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-x = 10
-y = 20
-print("Sum:", x + y)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
