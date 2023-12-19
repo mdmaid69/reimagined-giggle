759927310640994804c8d8fc29e8c105538043ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:57:28 +0000

    promise original file music along

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
