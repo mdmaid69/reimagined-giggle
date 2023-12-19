60f4c2afdf39921e173f11033198631b4c03c9f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:32 +0000

    government dead many open mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
