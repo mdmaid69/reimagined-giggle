146d8f7177d09ce188f98fb44a4d998701cdae32
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:00:34 +0000

    belief large measure switch acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
