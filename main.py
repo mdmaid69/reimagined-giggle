3f092a63ffc66da2bc89ac66196f3d0d88321ff4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:11 +0000

    herself rain contact safe warn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+for i in range(5):
+        print(i)
+  def reverse_list(lst):
+        return lst[::-1]
