3ed4a22831b97808b393c35d4cd1406df3916340
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:52:38 +0000

    turnaway instruction avoid advantage square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
