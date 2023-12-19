6ad9ad164c5d7f0da58c3954a480326f6d4af52d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:54 +0000

    dress achievement bet middle over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
