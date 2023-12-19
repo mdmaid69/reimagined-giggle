0c5f4fba8aa487910025cffd0374276496bd24b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:49 +0000

    finish fork smoke recent difficult

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import sys
 def add_to_python_path(path):
         sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
