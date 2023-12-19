245129f832217e8f0f29c84e82e11e00eeff3e81
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:30 +0000

    exhibition strong Ee soft fast

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
