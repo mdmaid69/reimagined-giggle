98ab1ec4d77bb7dcc35f2e4659af825febf76ff1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:28:16 +0000

    late safe bang arrange union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
