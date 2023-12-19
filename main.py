59938fe7a1e1ace41d1039a97409b6c614838621
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:24:31 +0000

    taste feeling figure shame model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
