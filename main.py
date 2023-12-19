0019ebc4dbec13bd661852b780ad0b98fb1b5c03
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:36:26 +0000

    even uponon complete age wherever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+print([x**2 for x in range(10)])
