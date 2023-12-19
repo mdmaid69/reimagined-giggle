1924ccee6fe66853acbcaf2c3bc7779101e7244d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:05 +0000

    false neck so lower short

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def cube_number(x):
+        return x**3
