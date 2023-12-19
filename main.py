76ef49895354ed2eb2d20784755bf8c3a0bb9b8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:28:25 +0000

    your wall event chat lesson

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-print(time.time())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
