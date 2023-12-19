d5bd98ba2b1a9ef4cb4ed80d3e9ff83c7a598274
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:27 +0000

    scheme row truth please shop

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
