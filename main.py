ac2cc8c776a546bf44bb402a3a60a9228e7dc5a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:34:45 +0000

    area touch across bank complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_odd(n):
+        return n % 2 != 0
