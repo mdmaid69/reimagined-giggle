5c4f2fed6422a49a10db707f93fa78907dce419e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:21:54 +0000

    light recognize better communication gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
