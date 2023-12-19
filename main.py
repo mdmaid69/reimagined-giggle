aec0e334cf4d2bac14e5ad103b823da374614031
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:34 +0000

    ever row dig convinced mark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import collections
+def create_stack():
+        return collections.deque()
