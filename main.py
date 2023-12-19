6555f0152a9cf478d61b90ebfea30831c866c284
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:17:13 +0000

    experience purpose reduction below satisfactory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
