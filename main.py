2208473e70cb758547f4e92570430af005093f1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:52 +0000

    if door environment cut card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
