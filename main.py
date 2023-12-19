eccd600c4988fc6f22292ca433ae8589b4c82b6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:50 +0000

    improve mood few crash deep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
