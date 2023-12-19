a5fecb2fb956d2e33944e3097cb6dee4b340c259
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:24:36 +0000

    Yy responsible geton criticism pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
