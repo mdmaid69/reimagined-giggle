6ba5938904eb5a7cd561f247e7061c311ca9ed70
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:22 +0000

    such fine safe tap rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
   def get_environment_variable(var_name):
         return os.getenv(var_name)
