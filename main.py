e9618ce3325a0fa18abb493441418a0e1e4ce306
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:53 +0000

    polite eye search lookback(on) situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
