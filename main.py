a2245b34611f57536fb6824e203505550703c865
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:49 +0000

    mine breakdown bye actual goup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
