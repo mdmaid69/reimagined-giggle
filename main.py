e356b488f62aa7818dca9816f54e5c2761106c3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:09 +0000

    family several runout in area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import os
+print(os.getcwd())
