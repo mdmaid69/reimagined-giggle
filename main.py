0ead8979b4a649eed8773af38bd29ece0d68902b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:21:32 +0000

    hold bush start plant equipment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
