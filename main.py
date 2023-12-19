33f51dd640474b990f929372644f7e5b0a6c9efe
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:34:52 +0000

    solve account quick complain circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_volume(length, width, height):
+        return length * width * height
