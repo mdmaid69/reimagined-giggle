ea2ffba4edb3349b61ee96cd176e6a473fd70b17
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:49:56 +0000

    ask guess grateful delicate wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
