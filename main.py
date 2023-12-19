2abc7380599816bbd80a13c26dfbe694e39cfb42
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:10 +0000

    blame task remember public protect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def reverse_string(s):
+        return s[::-1]
