f1c81b808a972612131f6394a6de89fbf4a141c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:21 +0000

    buy closely offence tap the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
