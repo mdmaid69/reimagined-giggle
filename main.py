0e6338a234b49c083071906beaa210ff3404f63e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:19 +0000

    neat delicate suckme Kk game

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_item(array, i):
+        return array[i]
