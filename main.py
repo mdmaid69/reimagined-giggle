c3eb9b55a4b909f3527c060c8e5fe3b9bdabad76
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:52 +0000

    wrap pleased complete keepoff soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
