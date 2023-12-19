c8ff291fe84cff0b25d064fff6d0f6ba09a1be64
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:34:07 +0000

    sort cost suit hope fact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
