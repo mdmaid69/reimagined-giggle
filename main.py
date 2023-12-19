960621da2932be0788bd851fd56003543ec5d197
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:13 +0000

    nurse stir powerful needle point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
