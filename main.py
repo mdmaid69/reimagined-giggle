3dec03d09d77adaa040334020bbd1c034be3a27e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:47 +0000

    exam anybody agree flow our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
