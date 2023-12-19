4c04802b6748bd080518501e7ba025b651dac80e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:15:48 +0000

    frighten forgive stamp fear score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
