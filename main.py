fca826f7765cac4d204ec4619cd1dc2e70353a57
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:01:29 +0000

    doubt absolutely one watch consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
