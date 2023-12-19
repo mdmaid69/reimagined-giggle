1bf3d6f609c8fec3c5cd38270a4ce06c78614f3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:18:54 +0000

    mad crowd study like house

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
