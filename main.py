6a9cf34f79527b99eb1fa50e1a3d3169ab8f4ddb
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:31 +0000

    division immediately narrow concerning post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
