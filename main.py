6845aa591bd7aa5f556db22dc78f0502b40000bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:37 +0000

    typical butter offer choice shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
