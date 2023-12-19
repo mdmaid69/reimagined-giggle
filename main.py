3b1351a4a4f196d1bdcdaf0c76307f132fca56fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:12 +0000

    hang dull enthusiastic region callout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
