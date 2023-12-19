70d36237cfed5b5524d5c5b2029b339902fa605b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:52 +0000

    thorough whenever before firm rest

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
