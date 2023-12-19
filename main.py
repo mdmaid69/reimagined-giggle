d726b0087892ce5c8e0da1e1dff2748a4988b9b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:10 +0000

    any project fit matter hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
