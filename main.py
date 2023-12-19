b0a7df5588ceb6ed8c387cd462ff72ec74c7c8f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:23 +0000

    tie coat recommend again dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
