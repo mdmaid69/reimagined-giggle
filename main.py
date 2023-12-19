2fce439bcca9a10375c7412c193122920346ea69
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:04 +0000

    colour tough appeal passenger chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
