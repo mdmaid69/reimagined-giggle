ba66c2bed97c1c36e100e821296ee6d4ec96350c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:46 +0000

    spill joke steer direct welcome

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
