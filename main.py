ad28f0683717f71d5429c0621edba1882a6f2731
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:34:24 +0000

    under light object conduct emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
