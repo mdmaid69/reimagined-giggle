8851f1855f5b0578ba72a5bc8ee6abf498445db5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:33 +0000

    comeout him pen Pp convenient

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
