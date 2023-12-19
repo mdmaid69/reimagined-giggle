cc311208f50a15b013c6fd4438e5df7c6c600b4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:22:31 +0000

    sing hurt foot surprised table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def square_number(x):
+        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
