78765fa66ed28d7072c3fc8a8ad4f958e2966a04
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:27 +0000

    mean century please evening rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
