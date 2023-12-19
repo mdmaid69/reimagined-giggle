c007f2ed45cf6f3f37a767c7c36a5f03ab6ec2ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:34 +0000

    father over strike ordinary fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
