4cf8183d5e6303ebb6b1ff13a422f53085949f16
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:14 +0000

    outside amount key clever course

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
