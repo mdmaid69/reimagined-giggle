20d98e1509633a2e694c7a6ad6490002168dc85e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:50 +0000

    appeal pay depend influence snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def sort_list(lst):
+        return sorted(lst)
