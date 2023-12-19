f006daaab1fd42f6a8595d03b9d5eea3b8faab0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:11:28 +0000

    year use fail young draw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
