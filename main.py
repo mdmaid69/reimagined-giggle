48b048a79937afe3ab8e3605f4a7acef159229cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:04:56 +0000

    influence somebody strike wire flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
