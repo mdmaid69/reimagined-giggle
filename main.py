1e96ee7e8209f3b8895187a4d4c1b028c535e5f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:34:41 +0000

    back gift family school compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
