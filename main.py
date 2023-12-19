05a6f1094b79ef58ad862006f5caa79cf577a52e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:15 +0000

    relation belief score takeover shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
