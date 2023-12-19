7ebecd73c4bf3017522259ac19cead8b870f7bf5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:56:43 +0000

    shrink plant plenty true project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
