e9f01ecb93addff63f0ad71cda55ee56e9f69b0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:03:33 +0000

    above rise and able long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
