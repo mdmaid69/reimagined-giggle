e3f64c23fef6db7762dc8dc2c7d395533c7ea500
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:19 +0000

    free takeout breakdown tip keen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
