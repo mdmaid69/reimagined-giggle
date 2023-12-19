acd88d7f63aa80ced7db220125234e9bd9a1850e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:39:04 +0000

    last play total boring attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
