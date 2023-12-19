dba46f3473dc4e39c534b8cb4ee8952ebad33223
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:23:08 +0000

    fact picture sky progress satisfactory

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def square_number(x):
+        return x**2
