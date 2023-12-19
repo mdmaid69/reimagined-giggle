efb42b4430db688d235d74148070f15688011a54
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:21 +0000

    join direct flat fold information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
