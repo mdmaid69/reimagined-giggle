583a14cd1b6f08fb4f5ab8dd8d43750f293f6e8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:40:47 +0000

    sometimes Uu suckme shall afternoon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_directory():
+        return os.getcwd()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
