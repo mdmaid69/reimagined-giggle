3a5275c1ad4af4899a30ed44295dc5cded6e4ca3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:38 +0000

    share pronunciation cope priority whenever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
