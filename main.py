6ffcf1dbf43e465e516a983af9491c71df9ed4f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:19 +0000

    sure afternoon best willing kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
