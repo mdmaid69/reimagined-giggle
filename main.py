dc0b54c8be1ebad1865f656c78c5fb0708cca808
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:39 +0000

    dream too them foot solve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
