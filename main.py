338f7c91d577c69d5905594d6377174b95b25fcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:52 +0000

    commit address pair throughout clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
+def find_max(numbers):
+        return max(numbers)
