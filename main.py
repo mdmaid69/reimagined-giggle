67471b5d0273c8a6337f4f337f7da121792f57c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:37:37 +0000

    meal excellent turnover suddenly anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
