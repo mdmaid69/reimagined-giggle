bbf973b3bbb3e7022854a5b508975d84860460b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:52 +0000

    wise nothing win listen final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_circle(r):
+        return 3.14 * r**2
