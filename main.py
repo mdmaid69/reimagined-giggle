3f82642168d3a664fb8c3bb27ce04805aa70821f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:55 +0000

    ring gather our wave bill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
