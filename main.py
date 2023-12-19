cc6bcdb27ee22191791a041e3741934a609ac855
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:14:31 +0000

    thirsty fierce custom tragedy shallow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import time
+print(time.time())
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
