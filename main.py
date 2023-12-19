1e8d0b6c079b17e078f3fb30a453b1b79cf0bfb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:48 +0000

    left possibly border file damage

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
