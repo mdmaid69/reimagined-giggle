099759237bf03719a54c69e08557139838cb6f78
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:32:34 +0000

    start sound hers deliberate quickly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
