2522cb9ae7befb7b260de7a1e1149838c5301ba2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:52 +0000

    laugh godown year hold bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
