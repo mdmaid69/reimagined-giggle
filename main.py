63a88367b3c03baac7efe12bf05cc0c78830425f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:29 +0000

    sure mail tend watch risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
