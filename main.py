824cd7dd77b63592f4efe8a21d7e93fc5fd7a8d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:25:32 +0000

    handle control by badly evening

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def sort_list(lst):
+        return sorted(lst)
