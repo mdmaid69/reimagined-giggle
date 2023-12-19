246d3d7686d438d24839a24d365660266c0c661a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:52 +0000

    pupil ahead herself thin mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_length(array):
+        return len(array)
