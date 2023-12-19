2f7aeaf8155ee6d9eac312c026ef2bbac71388a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:26:37 +0000

    development different zero own quiet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
