e89bfd209f2f8a7950035d374c3a11b5edc76790
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:21 +0000

    speed wave account picture stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
