99278f2926c2423eee9a0078d1075702e3c80621
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:01 +0000

    bill beside go comfortable communication

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
