75b34ee632e035b5c9d41d4587ca0ee07d0e72f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:35 +0000

    sad search accordingto country prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
