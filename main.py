56a71356e8c249fec82b08a955263b61de43c51e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:50:56 +0000

    contribute good somewhere rise shock

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
+def convert_to_hex(n):
+        return hex(n)
