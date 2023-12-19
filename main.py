48ad7d9f1404deb00cd88eaa1309df9c3e158ec3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:41 +0000

    free 573entertainment separate good heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
