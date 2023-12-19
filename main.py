09ec606d3a21253fee80ae54ea6bfad8e90b84a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:40 +0000

    undo fit minimum Ss limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
