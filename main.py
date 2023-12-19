3c355030ed38200bd390a21d42d6b898b0ff86f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:31 +0000

    rise year interested result female

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
