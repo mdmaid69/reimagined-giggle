42e4902f8e5767f77dcfe0009b20d37a61f0795c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:31:43 +0000

    crash rescue question dance in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def sort_list(lst):
+        return sorted(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
