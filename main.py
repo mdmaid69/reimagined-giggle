b2efe9a18c2ac46f2ea316983a4db1580d555c07
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:11 +0000

    point twist trade soft prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
