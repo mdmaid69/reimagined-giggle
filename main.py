c0ab63f6e0aa90e5d632fb45b1d6ea0495631d9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:21 +0000

    when satisfied cause further turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
