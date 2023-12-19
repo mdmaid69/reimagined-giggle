c17493afddf2a8e12349187b6961d2597042dd3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:51 +0000

    564encourage influence shout payment harm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_list(array):
+        return list(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
