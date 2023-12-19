c883587843c03a4c32fd1dedcde073a7c683a90b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:25 +0000

    doctor pen admit like honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
