a656d9c3f59669ac1f2d9d27a7b2219b7ef13188
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:26:50 +0000

    passenger keepoff interview Nn centre

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
