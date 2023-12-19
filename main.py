a928ce80cbc6318489594126d8f075cdc34a2e64
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:01:52 +0000

    rely comedown surprised support male

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+print(sys.version)
