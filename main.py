6ef691fb2d8bd0aa669e6f0d79ba50b2f7ada7e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:33:34 +0000

    sting loose solve underneath discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
