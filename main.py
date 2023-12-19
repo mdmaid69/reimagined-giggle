4e842b0b81d06ac3cfbb27590ba67f2bc2eaa39a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:32:09 +0000

    stage unfortunate odd vision slight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
