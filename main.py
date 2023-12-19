1c45ac584da5cb015e4d737e225ac6fc37792700
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:46 +0000

    wire squeeze bus lend bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def find_min(numbers):
+        return min(numbers)
