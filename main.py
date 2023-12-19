143896f9b404feb1a2bb39abdf85768dcbddea32
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:21:44 +0000

    tour scrap phone to law

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_min(numbers):
+        return min(numbers)
