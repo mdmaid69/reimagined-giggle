08f65ef3f943905560c0355a0b6bc52d4680ed74
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:33 +0000

    later design cook little fall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
