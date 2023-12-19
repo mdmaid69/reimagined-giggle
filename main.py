d68668d7f6673657e2e36084488272bbcd1ce0cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:16 +0000

    emphasis mark advise consider well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
