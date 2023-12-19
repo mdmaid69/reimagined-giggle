53e704ab24ca584a5d99888506ee8a71b61445c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:42 +0000

    combine attach day destroy Ss

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
