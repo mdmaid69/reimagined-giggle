405fa4d1d196a6934f46e7d1fa5a202f125a4183
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:18 +0000

    recognize tomorrow borrow Cc just

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def subtract_numbers(x, y):
+        return x - y
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
