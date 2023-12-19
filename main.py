e7ef4c3273733562a45f8068f942cfe9db9a7cbe
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:46:26 +0000

    check train bus moment community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def greet(name):
+        print(f"Hello, {name}!")
