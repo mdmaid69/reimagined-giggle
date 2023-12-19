6f37ab0213cc274320c99cf267e903f63e97f407
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:46 +0000

    necessary goup keepaway skin include

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
