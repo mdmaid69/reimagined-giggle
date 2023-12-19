d29694065efc33c86acb5986b4ed898d9153e4f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:23 +0000

    mess your female different mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_priority_queue():
-        return collections.deque()
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
