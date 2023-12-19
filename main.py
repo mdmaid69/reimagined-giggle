31b6ea9ceecd7c00ab98e975ccdfbc637c349e3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:03 +0000

    official big perhaps clean firm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
