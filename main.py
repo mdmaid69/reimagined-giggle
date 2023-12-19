4e2e4a0e6e5bb7bdd42f1458e9d9b01237f61a3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:31 +0000

    surprise pretend sister taste check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
