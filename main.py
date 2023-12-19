dba0914610edb63a7788f43d92fa8ea4208b1d66
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:58 +0000

    absolutely 569enjoy supply value front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
