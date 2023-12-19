2a269e201fa3678e2c47c93167ddb2fb5bc1c802
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:07:52 +0000

    responsible dream noone shake man

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
