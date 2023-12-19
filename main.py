898805025d01e6d7db78319261512bc5d0251a4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:25 +0000

    contents rough bringforward ours recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
