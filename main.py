ab34760c4ae7e747ae371d9b2bb3f51cb75ed0b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:40 +0000

    radio apart else Uu work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
+import sys
+print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
