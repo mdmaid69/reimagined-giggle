321683f7db61095bde7fd100faee98886c3c4b28
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:10:20 +0000

    on turnup trick deep warning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def square_number(x):
-        return x**2
+import array
+def get_array_typecode(array):
+        return array.typecode
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
