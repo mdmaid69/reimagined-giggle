1b83e4c82452b09445abcb6205f886be17f14d5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:48 +0000

    first that past accordingto one

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
