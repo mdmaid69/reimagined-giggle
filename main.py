06b4d539d570e8366cf3e820e63c813d6d59668b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:35:51 +0000

    ring sad pretend woman success

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def count_elements(lst):
+        return len(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
