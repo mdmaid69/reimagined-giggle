81fbc05c4eec1e19fa5c2307fa0622e9f915a4a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:03:54 +0000

    pronounce interruption near that build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import math
 def calculate_least_common_multiple(a, b):
         return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
