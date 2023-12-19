f33b86d960e1da4fd3264d7c975f6f1ce94aa9b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:05 +0000

    set through above depend rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
