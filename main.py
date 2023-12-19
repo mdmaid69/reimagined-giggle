0aa7169a322c1341db00266c073f415f89a5b596
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:53 +0000

    vanish perform maintain Gg wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
