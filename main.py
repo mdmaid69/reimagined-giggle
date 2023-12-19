829cf95dd21c8abb5390467dca4a6f20899d44aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:38:00 +0000

    wipe think emphasis ordinary switch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
