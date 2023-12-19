be9d6fb5fb18628671a6e8985601d481cc4a80ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:38 +0000

    everybody drink rain moment natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
