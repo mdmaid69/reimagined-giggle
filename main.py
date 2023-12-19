3d486bd1fd794a465b9880f33b1faaee87dd5841
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:56 +0000

    satisfactory secret loss private guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
