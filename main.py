00249e88e63298fff6e635319b86ecf7db0c0bdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:48 +0000

    beside ticket start make shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
