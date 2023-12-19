0940ae62efc9a2f8a14f4c7da775cca62faf4227
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:11:53 +0000

    inquire could image date value

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_power(work, time):
+        return work / time
+def convert_to_octal(n):
+        return oct(n)
