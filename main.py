2df2f26fb1cd89e954b10516ed1d00eb09defafc
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:25:10 +0000

    present praise legal law pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
