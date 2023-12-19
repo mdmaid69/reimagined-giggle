b091b43058b2b70a4a810b81eb06fda8b44c5f91
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:55:14 +0000

    include prepare replace careless tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
