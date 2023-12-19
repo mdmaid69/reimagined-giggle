0a938ad823f84378b8c6c1e201c1a4194ea50c25
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:45 +0000

    up empty goon answer container

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def check_if_array_contains_item(array, item):
+        return item in array
