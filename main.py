c168ff52785d30fb81c52dd9d89aa30b16d0f958
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:24 +0000

    fasten stage gentle again centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
