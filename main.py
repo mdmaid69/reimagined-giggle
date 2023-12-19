018c1e532308ae7fd3abc86d3abaf674538cb303
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:25 +0000

    figure television storm eat murder

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
