1c4ccff92e7d20e938c1261befc9331a98e5051d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:11:28 +0000

    compete path produce embarrassing sale

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_queue():
+        return collections.deque()
