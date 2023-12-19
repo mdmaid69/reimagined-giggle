02d2ee3a61937d7238c6a0e46d3ab4a927cfd079
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:11 +0000

    wash professional cut different sort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
