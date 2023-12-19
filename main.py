47b3ab15121121fa630a9ec10f3061f15a9aa9c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:33 +0000

    slowly listenfor know scream attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
