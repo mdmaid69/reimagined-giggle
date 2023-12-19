6838b5f028f7e75e6985ccea4ad1b41e7d9e6a6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:53 +0000

    bound receive taste force plan

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
