ef85a429b3d4f8f8d03b0e718a92971f4afee8ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:34:20 +0000

    long trap as peace first

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
