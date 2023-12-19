09e795be1b05c68d823ad21e5192891c22bb2753
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:32 +0000

    favour runover necessary difficulty inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
