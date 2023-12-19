be4cc5bad67992b4d3f22126c8ab177fb7e04614
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:04 +0000

    snow apparently different forgive consider

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
