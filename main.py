d557b901cd6cf3357eaf617309d324f48dc5b54d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:06 +0000

    dream tap complaint stick raw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
