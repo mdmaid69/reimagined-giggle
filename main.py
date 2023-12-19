7ee7a8126d8175fb3932fb10a26f31528a6a70bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:44 +0000

    plate pay mysterious union workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
