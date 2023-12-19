acf055dd058221d8b9e132d5fa25120ac639cd48
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:03 +0000

    similar really short work development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
