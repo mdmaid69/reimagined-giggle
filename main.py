b45488e52768b52aac4ae1038135982b7da8f4a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:24 +0000

    voice negative bunch shame bottom

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
