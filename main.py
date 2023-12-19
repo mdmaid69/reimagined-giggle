0bc56275ae9e3549ac64c4a956a1ec6f73dfc2dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:22 +0000

    pay explain comment underneath hello

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
