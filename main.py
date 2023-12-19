4ad2cd7b2f8eb05432f7237b9c5c8286f108494c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:04:37 +0000

    string one region majority respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
