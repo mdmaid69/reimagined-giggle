23d5e08bd5e94c2b2ca4d4dfac2d2257ce9c6530
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:54 +0000

    steal level die big delicate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
