65ddf223069f24264b3ced065e53ba62b93fe312
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:41 +0000

    early suppose whichever persuade frame

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
