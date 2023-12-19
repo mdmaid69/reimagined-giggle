3899784f86e21e4eae78aa674a5c35e8b2b146fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:11 +0000

    duty talkback around worry laugh

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import csv
   def read_csv_file(file_name):
         with open(file_name, "r") as file:
           return list(csv.reader(file))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
