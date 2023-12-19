1ad76f8d056a6ad5cde5c8bc274f019e5f7492ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:16 +0000

    pinch reason they city lock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import sys
+print(sys.version)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
