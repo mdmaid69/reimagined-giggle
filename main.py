f952404d878f8ccc09e9ac098e297893e510e0ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:14 +0000

    mainly muscle square oil spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
