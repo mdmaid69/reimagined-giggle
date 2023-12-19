1092aff97859ab3635dafab55a21e4e175e6524a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:01 +0000

    along light pure offend birth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
