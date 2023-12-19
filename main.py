2ad7976a0183499198dbd878b91bf50456b0ce66
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:42:45 +0000

    thirsty operation herself instant square

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def find_min(lst):
-        return min(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_sine(x):
+        return math.sin(x)
