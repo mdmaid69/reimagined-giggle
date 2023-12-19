1d2e638634e033c719daf191a209b68bb555e195
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:08:18 +0000

    proof display sudden mystery alone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
