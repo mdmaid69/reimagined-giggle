bd695be46947e4225c187cdfa9361f2569ac1ac1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:57 +0000

    value takeout finally busy somebody

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
