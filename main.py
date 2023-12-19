d351f4c7a232f404a3735f882f55f8a2aa021986
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:37 +0000

    level general accept sting collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
