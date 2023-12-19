d5efdc64c21f2321d1d4ca07ba9a3f7cf28124a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:43 +0000

    people to shall idea rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
