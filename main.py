37d3feb3c397cb07d98b7508108bba43a1703a73
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:30:37 +0000

    sight eager healthy over slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
