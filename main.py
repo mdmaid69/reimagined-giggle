a2c61124f470d144f08aea8b2da88ca22e6b7a9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:31 +0000

    leave admire sea by mean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
