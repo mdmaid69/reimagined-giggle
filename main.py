7b2142c644c228b8999d308d33f5b44d8b2464f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:07 +0000

    season up correction 564encourage remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
