62fa23bf9a7a36908a6b54dfdb5f73aa0700f044
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:25:04 +0000

    drive tired getup building because

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
