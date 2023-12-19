12f93391baac17b0ff1c74ecaca6fcc322aabade
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:13 +0000

    least lookaround platform fat corner

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
