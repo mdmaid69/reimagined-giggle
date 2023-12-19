52e6bf04f135b99ad13a8e63126998e8264b96f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:21:37 +0000

    succeed concerning hardly only tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
