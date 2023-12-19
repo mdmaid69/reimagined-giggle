be5882fd416596d7635e87d35cc2748f21288620
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:06 +0000

    gentle reduction prepare correct thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+def get_current_time():
+        return time.ctime()
