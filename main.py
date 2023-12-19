65ff337d6ef0747d3a2ab0ec9b20b1e80f92b5df
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:19:01 +0000

    gradually company common private around

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_speed(distance, time):
+        return distance / time
