68182fc8168f452a1703e820fcf14766f63cfa93
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:50:18 +0000

    spot family failure collect or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
