0dc0b2b94603750a4234b427c64bb5e6eba2989f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:46:35 +0000

    onto high original mine eat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
