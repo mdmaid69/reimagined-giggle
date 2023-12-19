8306dff7cffdc57b51ed6dcfdc382697ab64c53b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:07 +0000

    lookafter nowhere minimum underneath squeeze

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
