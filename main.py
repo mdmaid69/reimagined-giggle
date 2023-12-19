54ca09765b9c924dae462ff7148ad81a6ea209a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:58 +0000

    good top like press tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import numpy as np
+print(np.array([1, 2, 3]))
