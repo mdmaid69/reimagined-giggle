5688472f0502387f56df96cfe595ed5a604fc201
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:21:26 +0000

    nature accident separate embarrassed inside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+print(sum(range(10)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
