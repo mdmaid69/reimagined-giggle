ef8fdb8858fffeb04ad0f418828be9baa5365207
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:14 +0000

    level imagine however possible advice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
