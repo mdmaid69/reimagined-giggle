ca71fc9e6b0fe539c1c1ddf31ef5a5c6087c0944
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:12 +0000

    fashion union set creature write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
