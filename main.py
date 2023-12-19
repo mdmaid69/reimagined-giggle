d5afd71eb951826f3ae3e527157d408e0c4a1b70
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:47 +0000

    comedown your pair near myself

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-for i in range(10): print(i)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
