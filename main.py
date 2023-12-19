aaeb430b6a3e043d1347027526b7fbaf6ae7ad43
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:00:50 +0000

    treatment load prevent clean Dd

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
