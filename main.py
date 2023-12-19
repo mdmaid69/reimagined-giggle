84d7d71c7dfb8e68302f3c3671ec4c40ccbc7b27
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:33 +0000

    matter approval spare 569enjoy takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
