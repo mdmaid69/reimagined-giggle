3cd5d57c3db73528a6778b60254a05137507116c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:33:38 +0000

    please move sister wet fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
