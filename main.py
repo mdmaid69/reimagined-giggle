e4d6b63aaed3153e56459a16e95a6c7b5e31070e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:51 +0000

    concentrate plan transport hate sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
