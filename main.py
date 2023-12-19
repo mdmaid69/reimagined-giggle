27e5de573c657b8da8a29990742b227a64c7b7f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:31:46 +0000

    information within at tip Tt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.ctime()
