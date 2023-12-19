fa297afac9dfba6186ea3e0a6b7bc3410ea43985
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:40:28 +0000

    familiar damp achieve fault beginning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+print(math.pi)
