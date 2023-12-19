6edc0383819435781f4f5fe34d8937b985508e32
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:08:01 +0000

    unit characteristic Jj 569enjoy stick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import sys
+def exit_program():
+        sys.exit()
