7eab57dd07911f603edb460165cf6a4d5d4b72c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:52 +0000

    temperature poison tomorrow give neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def convert_to_octal(n):
-        return oct(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
