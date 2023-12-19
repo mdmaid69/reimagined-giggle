fe52743a2f0562c586c19cd939039ebef46e1af7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:19 +0000

    basic wipe popular late various

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
