76d37230fd940ced6daa588a1ad377732bc95b7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:41 +0000

    join chat glad count visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
