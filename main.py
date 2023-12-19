1d1dbcbe331d8b27f4a3b33fe0932ed2b62cad46
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:14:09 +0000

    guilty rid left man look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
