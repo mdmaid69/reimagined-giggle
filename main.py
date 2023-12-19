fbc865c9cf5af493f00ecacd877c41f80d7d50b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:37 +0000

    exchange tooth market risk geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  def sort_list(lst):
+        return sorted(lst)
