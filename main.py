75b01925a254fa32278f9b7571a9c1aa812a2c6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:53 +0000

    temperature sign weigh bored regularly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
