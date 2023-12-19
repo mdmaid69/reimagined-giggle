e8a7e4dd2be8be0fc13afb8157787b512212a612
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:52:21 +0000

    sit admit everywhere belong various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
