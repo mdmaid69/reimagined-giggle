5b4c0705cfe2ed26fc2efe9dc6231ccd5c8dcdd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:03 +0000

    as pencil can friendly hold

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
