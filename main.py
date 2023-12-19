ba32a1936d647b3bfe99eabdff80e2353606de32
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:19:32 +0000

    visit use confusing cover relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
