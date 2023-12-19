4c7e4ac9d075ea18bd173c17454d0b9c5894c4f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:34 +0000

    family smell all along attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
