04c271b50a45a1052329ea90503762e5c2fa18ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:18:34 +0000

    passenger shake control extraordinary number

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def count_elements(lst):
+        return len(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
