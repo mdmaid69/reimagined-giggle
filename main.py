0c3592ffea12e83d1ba49d5bcffcfe5a10431593
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:39 +0000

    deliberate over level shout tragedy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
