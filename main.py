37a04f6790b46fa0100ace568a8ae1bb76124eb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:46:37 +0000

    plan advise summer wander confident

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+print(time.time())
