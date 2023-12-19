60c4cb3644d698286101a8a356cb7898aa85595d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:02 +0000

    solve smell and whistle brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
