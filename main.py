2de502c4a013fecfba46eadffbce4fb2095674e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:42:04 +0000

    kind hollow willing change Ff

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def append_to_array(array, item):
+        array.append(item)
