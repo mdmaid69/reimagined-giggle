16f8dae088057e6063de6aea1f4365fd7acc9e81
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:56:59 +0000

    common expensive pretty plenty hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
