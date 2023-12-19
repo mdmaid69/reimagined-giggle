3e08f315591b563640a211162728069e22105772
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:13:31 +0000

    film coffee phone gather miss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
