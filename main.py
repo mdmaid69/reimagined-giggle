6c10d5377fe43aeef6673e00666061c516fea5d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:08:57 +0000

    near point yesterday wipe bottom

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
