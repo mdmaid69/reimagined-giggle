4df761ffa804cefd7e78b01e58bc8842437627f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:07 +0000

    couple clear congratulate coffee contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import sys
-  def get_python_version():
-        return sys.version
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
