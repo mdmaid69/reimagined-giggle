6511c91517b3e9aa98cb05e05e2caad31779f9bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:19:17 +0000

    live partner passenger embarrassed know

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import platform
+def get_python_version():
+        return platform.python_version()
