fda54ecf4b62cf5f8f6eb93741f8ccd1ac802672
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:06 +0000

    relative scared here side appearance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_min(lst):
+        return min(lst)
