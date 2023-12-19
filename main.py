4188b62111fbb8160494c03daabec2268d50f90d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:17 +0000

    keepup closed water sound blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import datetime
-print(datetime.datetime.now())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
