98df3cf347100b27bf1fba3e1d695b3f68d3a48d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:59 +0000

    pile explain gradually danger seriously

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-print(math.pi)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
