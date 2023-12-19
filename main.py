8a8e750aadad3028cb938d0a6065c3f672d3b20e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:56 +0000

    connection colour violence quantity form

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
