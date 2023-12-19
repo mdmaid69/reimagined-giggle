52828170b2f75ae04bfd9f93711f374c10ffa4bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:14 +0000

    exaggerate once common impatient appearance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
