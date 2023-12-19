595bd198a6e07afadb7c5d8ff7d96a754095cca0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:07:22 +0000

    regional unfair nerve boil lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
