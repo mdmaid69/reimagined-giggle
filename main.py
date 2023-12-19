917040fd95d5a094469fff306588d4681913ccfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:30 +0000

    immediately repeat practise entry combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area(radius):
+        return 3.14 * radius * radius
