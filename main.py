265bca498a28a849f4f51f5492bb6cd27e12940f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:59 +0000

    goodbye region tell emphasize move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
