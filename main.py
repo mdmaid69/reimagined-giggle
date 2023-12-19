dcac2f1c2e23b1242a3fc33d52985b1e8996796a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:00 +0000

    brave guilty top name juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_typecode(array):
+        return array.typecode
