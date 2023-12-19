8337591f3687a2bd2f913a7fe1de96c000ec03af
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:56 +0000

    grade healthy love warn roll

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def multiply_numbers(x, y):
+        return x * y
