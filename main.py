65e048a418c5edfef14c1a099a7eb46152c2d0c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:26 +0000

    style holdup provide silent path

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
