3dd86d9b6c9a207c769f8fff4f9cc5f2dd411001
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:03 +0000

    describe instant production risk anything

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
