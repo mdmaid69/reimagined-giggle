1b0b6c41c2d6035af54073a4bf4ce26006581285
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:28:59 +0000

    become obvious recently square perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
