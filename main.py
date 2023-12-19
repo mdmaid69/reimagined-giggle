cf5e8b584a1e34fa6718d3b39e19190e86d24550
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:04 +0000

    he nor union underneath history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
