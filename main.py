14ab56c91bdecd33df36c43efcb848552ea1418d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:03 +0000

    morning stretch yourself pinch otherwise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
