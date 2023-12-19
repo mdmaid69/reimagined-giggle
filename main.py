23b40490e6104780a731bbbb1ed4792f352d9841
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:09 +0000

    valuable down confused crowded reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_acceleration(speed, time):
+        return speed / time
