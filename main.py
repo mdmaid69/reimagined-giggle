a900ed65847304b8da3831f039beea94a69add1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:13 +0000

    doctor spite call produce aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
