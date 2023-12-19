7c4e5b8e513a2045ebdb7e47e9f2417a099aa71d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:24 +0000

    doctor drive wave fail wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def square_number(x):
+        return x**2
