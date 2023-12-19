804e832de555cb3929def4a97a1801a4c2e47811
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:50:47 +0000

    base understand completely advanced field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
