bafbb1503f04f72df1c03e02b5b0832a9edab4ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:21 +0000

    total custom count centre motion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
