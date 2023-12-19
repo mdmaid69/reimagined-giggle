177992540faae7ca459b5570439444698fbbe493
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:56:10 +0000

    coat part dinner contrast tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
