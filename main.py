f98c0ff63c19dc3aed6018543168e8a1d6eba960
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:12:17 +0000

    protest job fetch matter ball

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
