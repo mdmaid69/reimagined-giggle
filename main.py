cfaf41f98f29e860cd4abbcf1b9d357653c32267
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:19 +0000

    with combination habit getout face

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
