0435ac2fc951dfc411e08f39a3e583782b851778
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:31 +0000

    keepoff exam dress nothing entry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def square_number(x):
+        return x**2
