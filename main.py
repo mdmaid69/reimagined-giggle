e0e80e22761c1e18cd818644e3b631f8b76799b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:21 +0000

    cost worst sing goodbye put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
