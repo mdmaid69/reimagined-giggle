3e2409b304987fb6913f4ae7c633a3211c85238b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:36 +0000

    about winter include height spare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
