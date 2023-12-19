e6ee0c7f67516fc5447f9ee83c22f009dbd1d485
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:51 +0000

    broken shape provide night without

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def convert_to_octal(n):
+        return oct(n)
