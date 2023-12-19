9e68589248621b051c86cbdc949a0f7db655999c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:43:32 +0000

    kind card runaway union used

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
