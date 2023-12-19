171758b97859b960ef3fab19308edfcbb8a575a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:12:36 +0000

    other operation mysterious neither rise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
