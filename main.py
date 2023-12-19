17c473ee43bcbf99c04c464c4cbad5e4fd66ffbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:08 +0000

    damage time task breakin heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
