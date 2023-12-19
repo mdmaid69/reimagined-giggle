2a7b54cadcbd587b0cb73f18ea3d32bf46968134
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:14 +0000

    receive picture surprised attempt promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
