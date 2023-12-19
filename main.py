ca40eca21a87f61858c1620cf2f4784a9934cb42
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:35 +0000

    interrupt congratulate practice turnback mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
