739e4b63a34c7f4397e77a3c53b829ebf287b4e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:22:24 +0000

    instruction steal murder suit call

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
