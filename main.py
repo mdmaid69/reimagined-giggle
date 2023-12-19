69f78a1968aeecbc4efad32f15bd901357432d90
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:48 +0000

    reply trip impressive object exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
