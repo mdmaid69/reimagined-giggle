f575f8296e2a207ed220140a55ee595a29e739eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:23 +0000

    excuse government Vv afterwards warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
