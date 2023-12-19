69e2fe763d272e0dc6b7cf4685e4be4541469bb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:45 +0000

    will effort school reach nurse

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
