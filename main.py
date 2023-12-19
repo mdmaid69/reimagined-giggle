02fa8ae31983007124a78901028370083dfe6756
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:39:45 +0000

    hate impatient stupid holiday throughout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_odd(n):
+        return n % 2 != 0
