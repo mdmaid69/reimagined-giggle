b6a5618994bcd01143291829520a7a454aa97c27
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:54:12 +0000

    question slightly face bit rise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
