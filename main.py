4ae234e42e5504aaa283a0f793fd2284751f2ed4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:55 +0000

    ahead declaration silent number the

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
