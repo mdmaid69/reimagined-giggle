36198a24d0124d50f987837971515737e69a16a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:37:18 +0000

    substance coat image should stranger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
