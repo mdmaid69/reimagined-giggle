2174725570f434937fb7c21e21b1ec91dd3677cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:25:24 +0000

    open holdon complaint blame cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
