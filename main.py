bf39255d48aa7e9d67bbaaf349fc334da6e14b05
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:49:47 +0000

    succeed swing market until tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_area(radius):
+        return 3.14 * radius * radius
