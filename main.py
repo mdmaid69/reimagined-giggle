0d7261fb0e0e831db74f3a1c1ca314efa1e1fa44
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:08 +0000

    crisis success consist moral limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
