5725b8abe245694eeae43691d5bbd085d758c13c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:34 +0000

    practice method moment exercise get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
