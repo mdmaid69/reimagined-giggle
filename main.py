855d6dd9313cec3ef15590d5d7d29593aa51cd06
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:27 +0000

    keep evidence upstairs dry use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
