3cb082d8ae40dc682a011a593eeff0e4289c6abc
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:56 +0000

    gas thorough name include certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+text = "Hello, world!"
+print("Characters:", len(text))
