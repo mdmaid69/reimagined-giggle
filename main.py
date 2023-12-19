06f670d91e7b37ab34af6d11af8bdf551ff755bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:57:32 +0000

    talkinto print strength bringforward on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
