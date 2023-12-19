d156fd104938db77f8e1fd11cc7e5bc4544eeb8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:24 +0000

    use comeup thinkthrough 568energy whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_density(mass, volume):
+        return mass / volume
