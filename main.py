1a0fbba4e8f76da7a6e9c61362fa0a11d0d44d82
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:30:24 +0000

    strict skirt finish eager clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
