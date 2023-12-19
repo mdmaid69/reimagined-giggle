78db5fc103392a47ef6c59c61c43440d9af7f289
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:03 +0000

    mind goup stay tread thanks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
