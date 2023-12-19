4e41889935a8b03ac9dbaf7a59a042ab1709263d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:47 +0000

    keepup no rest fond rent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
