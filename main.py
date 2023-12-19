f19ee67d74f47bdb0021f8367b1ef94ab695536e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:04:12 +0000

    small opinion connect priority anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import random
+print(random.randint(0, 100))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
