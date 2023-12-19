b042c69a95ef3a9091b56d4355d7ce452f62cad1
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:20 +0000

    length scream risk if unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
