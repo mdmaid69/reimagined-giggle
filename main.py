ae6545dc04d4b158f85c6518e5e2247b1928f417
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:32 +0000

    reduce someone embarrassed congratulate board

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def convert_to_binary(n):
+        return bin(n)
+import tensorflow as tf
+print(tf.__version__)
