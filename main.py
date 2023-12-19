d415fba3b9e2ca0c22c06208a2176065c8d9bbe3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:01:01 +0000

    start proud poison attempt air

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
