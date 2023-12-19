8ac8f9b889ce9cf8c46dc3e9501a2e16b1c12e1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:25:30 +0000

    watch ours cold control calloff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
