d59079c0ed3fd440028f52713df105f9284203b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:50:19 +0000

    used break development blue kill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
