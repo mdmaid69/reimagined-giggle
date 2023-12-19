21b2d67d7544e5c0f594d97fb62d199ceea719b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:20 +0000

    bank count best slow return

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
