e1d3f9140482bfc889f98a4b7f36d8c530ab2f0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:30 +0000

    left class platform fortunately main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-print(sys.version)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
