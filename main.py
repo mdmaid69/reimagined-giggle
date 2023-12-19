afcbb375bb75d927bf74147939414c592601d4b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:43 +0000

    smoke border present about appear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def sort_list(lst):
+        return sorted(lst)
