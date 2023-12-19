92f4f9667b95535b9b561daa2b587711c85987a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:50 +0000

    stare rank suckme less teach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
