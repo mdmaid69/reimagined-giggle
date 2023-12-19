ae8bcbe881d1e6533a037ecc4b92104c3502172b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:15 +0000

    hope ball bar comeoff free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
