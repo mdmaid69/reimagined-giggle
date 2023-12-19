c48f41be87890d82a5e239c3fd003724aa475a74
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:10 +0000

    class along whoever fall grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
