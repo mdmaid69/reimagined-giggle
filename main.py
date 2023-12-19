52932f3d336b2436528019b4c438741120951021
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:34 +0000

    tend preparation regional purpose opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
