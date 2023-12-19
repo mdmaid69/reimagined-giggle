d771c60fc58dca7d4563991e8b7310d61c00db9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:22 +0000

    sign square basically deep violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
