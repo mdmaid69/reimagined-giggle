0d634349e66bc05acb852b59c63861e2f7049cd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:36:51 +0000

    miss spin draw brave he

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-print(random.randint(0, 100))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def reverse_string(s):
+        return s[::-1]
