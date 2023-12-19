fdefcbf741c587b68d111669d5cfdffa775e5384
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:30 +0000

    cough defeat wall tread behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def create_user_list():
+        return collections.UserList()
