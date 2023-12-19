85e07f2339bd61cf37a2b928c61d0aea7e813aba
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:47 +0000

    rank Bb face feed look

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(numbers):
+        return min(numbers)
+import tensorflow as tf
+print(tf.__version__)
