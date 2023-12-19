f7bc2436b8b4624c98f2f53e83dedd5d72b36123
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:21 +0000

    insist avoid balance trap communicate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
