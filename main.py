37b441150045b3389d7ae317c6e52168ce3f0c83
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:02 +0000

    same hurt cross rather Gg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
