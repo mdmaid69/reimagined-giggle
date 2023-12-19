4ea1aab009e6dd6c349efc86ee7ebe3de5625710
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:01 +0000

    mouth description official better well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
