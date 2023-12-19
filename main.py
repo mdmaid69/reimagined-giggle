69334f241c12399d618f68bfd42f53ba1c738cb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:38 +0000

    board early insist second represent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
