7bae5a65ae4932b6aa7018bfba5f7e449e023dd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:51 +0000

    proud touch room permission strange

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
