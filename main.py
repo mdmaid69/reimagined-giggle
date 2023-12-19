35e924025dc439aa107c7a1a3d8153db49377ab7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:17 +0000

    bottom soil neither information silent

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def find_max(lst):
-        return max(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
