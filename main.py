bf43669f078bfebbd542c995a5b6fb1b673eb16c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:43 +0000

    half seriously equal finish relationship

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
