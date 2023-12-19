c81a07516736892649aba2642490e7d18b4c42ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:30:42 +0000

    cruel dance respect thin fair

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
