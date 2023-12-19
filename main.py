e44dcf5666795f579b0a5136f6bf20d420fc74ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:00 +0000

    prevent bread test kind colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
