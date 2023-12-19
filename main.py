44baec71c0e7eaa2605f1830a0c857100ca37d37
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:15:20 +0000

    television me rather view fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
