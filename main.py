5e0d7a0b34509a3acdd98cd0c8bba739f1903737
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:16:20 +0000

    neither need Yy afternoon sleep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
