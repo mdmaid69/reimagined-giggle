61163b2beada029f4af78534379ce46361cc1e41
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:21 +0000

    permission Nn answer attention where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def multiply_numbers(x, y):
+        return x * y
