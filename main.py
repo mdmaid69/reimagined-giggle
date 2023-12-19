52df67254adcc4cf64950d355c9f7ab2abd562e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:27 +0000

    above figure waste square programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
