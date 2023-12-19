3e395fdcf8b91b53803daab5df3272afb7403975
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:08:01 +0000

    which room callback wait more

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+print(math.pi)
