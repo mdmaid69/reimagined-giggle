06fb22295f31d21efe03e636d1dd09e55c0c0811
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:31:57 +0000

    choose official strict activity open

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import time
+def get_current_time():
+        return time.time()
