e3befb87b687faa35011068c8fd83106ff6fd5c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:10:31 +0000

    already uniform your develop switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def reverse_string(s):
+        return s[::-1]
