4978f480d34f7e890f7db01a1d9be0a25ed3f70a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:48:50 +0000

    night expensive fence base owe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
