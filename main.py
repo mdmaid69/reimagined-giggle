ba8a31e822dc79890c1a314699f5f8f14e79319c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:53 +0000

    please bound reply challenge react

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def is_even(n):
-        return n % 2 == 0
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
