60d0d1e9df56bdbdc14133383043817f48942ef2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:51 +0000

    entrance makeinto enthusiastic surface cinema

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
