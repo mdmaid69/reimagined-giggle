c4c077b1c83f39a972271c8d9bd8e50cc81eef48
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:43 +0000

    fence pinch gap among obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
