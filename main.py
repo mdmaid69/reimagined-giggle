4335a0e68c199ad39015e002106b0328b069e64c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:41 +0000

    pleasure skin comeup exciting female

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
