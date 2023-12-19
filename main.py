261b80a6081e012883cc29647e3a2a198fd5eb0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:54:19 +0000

    moment computer determined walk everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
