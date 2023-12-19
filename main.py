2b9aa31e7745fb27387ffba2c4d1dc16c655bb3a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:45:05 +0000

    love true tap just sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
