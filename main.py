161de0cc7f6acd085df66181ccf6f84fc80e3feb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:59:02 +0000

    personal concerned tool story progress

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def reverse_string(s):
-        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
