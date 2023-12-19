dfbc04d7a494fa83b92501c9a9e190e4190cc407
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:55 +0000

    relationship somewhere pen drop produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
