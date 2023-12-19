f08728f9d7217824a07f8045ccf7d9da151dbd41
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:36:03 +0000

    tap arrange information tape difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
