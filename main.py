d999d01a4c33a18e72d84b314a18a291137b523f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:10 +0000

    illness transparent many pride likely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
