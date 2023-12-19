d61f5343a1fbc004e23b3029b83ed9b78ea7dd41
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:20 +0000

    eye chin go nature contain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
