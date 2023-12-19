6811a352c2a2ccce3d24b85b5cc5d8a5809476d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:58:58 +0000

    blow now maximum your total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def remove_duplicates(lst):
+        return list(set(lst))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
