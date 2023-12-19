d80ee07911467877065b939c9e52a915398404a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:32 +0000

    official compare thorough duty run

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
