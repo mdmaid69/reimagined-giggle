66e5f6bcb8dd1fd2eb898a14f13a386321030131
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:46:02 +0000

    corner rest limit agree odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
