e8fa8ef56a1ed6cace99308db978f3c9a9d218ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:26 +0000

    workon deliberately quickly hospital enthusiastic

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
