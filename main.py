088c7e50cd581b49ed39abbd7feb1120d3ed2747
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:10 +0000

    workupto hang protection believe entrance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def divide_numbers(x, y):
+        return x / y
