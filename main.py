df59c580f787131c0b0ae3d00aaea8040a1f1bba
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:07:52 +0000

    suggest careless realize collection great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
