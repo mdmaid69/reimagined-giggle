d2c234e21c8ca7d939f6b6af7f23462c953fbf57
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:26:56 +0000

    study towards which tongue callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def get_string_from_array(array):
+        return array.tobytes()
