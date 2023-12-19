4633a06f31ff84d52f9e2907ff57b25a9288ad82
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:11 +0000

    state cruel above achieve into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_tangent(x):
+        return math.tan(x)
