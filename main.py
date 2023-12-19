5258e97f422a5b341c3a17e00463cc44a1e14524
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:41:31 +0000

    fire news towards month slowly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
