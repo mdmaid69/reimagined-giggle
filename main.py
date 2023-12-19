1dc4ebcc95ad841a555a3fe8fcc99749d5af60cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:41 +0000

    theirs your plan morning soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
