1786603c44ff6a376702aa447937f5adde8b1b7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:48:16 +0000

    door travel operation positive proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
