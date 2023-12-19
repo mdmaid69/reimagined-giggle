e18a4305119754b30fd83732c69f652b223d2b3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:12:55 +0000

    chance air city wide afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
