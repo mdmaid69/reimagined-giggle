5e12c45b467374b8c9e2c1a47172b05243ad4094
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:18:27 +0000

    shut male constantly respect entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+def get_current_time():
+        return time.time()
