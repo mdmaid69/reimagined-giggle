7e67c427f1714598ebe28ee0508d9d42d22a09ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:43 +0000

    repair pass fear hope burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
