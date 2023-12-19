2873427491631fb92c4268c1be2a37b07d013291
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:50:45 +0000

    rain pleasant minimum guide suit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
