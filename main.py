f8f5dbff243394f9831bcd0a0d94c62762a0424a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:54 +0000

    relative personal unkind half something

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
