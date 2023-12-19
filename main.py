9ee2bad21648bfeab522a72365fda59a53ef2c4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:05:30 +0000

    division combination importance send difficult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
