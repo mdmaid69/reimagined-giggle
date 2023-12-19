344895527796e74d9affc27d8993240d5df972e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:16 +0000

    luck careful sight underneath luck

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
