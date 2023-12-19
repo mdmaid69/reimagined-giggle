c42981ef20b0d90994951fba9efe4be18caaf167
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:16 +0000

    566end shadow ahead limit like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import sys
-def print_python_version():
-        return sys.version
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
