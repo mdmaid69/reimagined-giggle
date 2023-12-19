3a761d1c5b9b5ca5e4d95c4dc552424959283503
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:06:54 +0000

    breakinto suckme delicate trust film

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
