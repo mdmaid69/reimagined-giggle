4d081e6345eca7b6dbe33131409d38b0013197b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:04:40 +0000

    still attempt bill deliver brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_average(lst):
+        return sum(lst) / len(lst)
