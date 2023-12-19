dba19ee052df325f65a54d87702721dea9920427
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:07:36 +0000

    note direct computer well naturally

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def find_max(lst):
-        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
