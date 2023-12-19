f886f27aa57a03189581dde0a6afac80dfd0bce2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:30 +0000

    depend control wet match place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
