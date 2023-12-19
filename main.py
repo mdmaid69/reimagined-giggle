a9f06a1bf1a920313cad5c77871e8c39bdd21033
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:31 +0000

    interest introduce vote bridge soon

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
