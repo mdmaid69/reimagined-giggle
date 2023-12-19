0e7345b7ff13be422fa0de1272831a6b71afd375
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:10:41 +0000

    match skirt second other home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
