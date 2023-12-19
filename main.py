025f471bdaa9000437847e4d7b4b5c14878123f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:49:15 +0000

    say develop giveout rob extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 def calculate_force(mass, acceleration):
         return mass * acceleration
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
