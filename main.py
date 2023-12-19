a3711040fe54fd42834b0dcd9714fa4224e5e228
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:38 +0000

    squash set after typical yourself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
