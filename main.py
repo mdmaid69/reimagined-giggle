39b93fd8232f6521674a5baf23bccbab8bd5e135
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:26 +0000

    platform fast gate lock shut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
