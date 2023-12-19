a6a807dce99718a166f30dbae5481326152e0550
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:38 +0000

    remain claim charge prize system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
