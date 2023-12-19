47a437c64d4fc7410820da0a8d7ba72b258763c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:02 +0000

    sure smell rubbish swim pull

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def convert_to_hex(n):
+        return hex(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
