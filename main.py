07403c771e03846950ea62260ecc9f15cf05aacf
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:41:06 +0000

    shade nowhere straight little brother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
