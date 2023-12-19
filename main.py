1b970edf0ffcb66ce1a02c56832b06927fb75a4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:10 +0000

    secret different fetch basis within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_int(array):
+        return int(array[0])
