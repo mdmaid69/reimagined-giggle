32bf778f9f032522f35e462a13c0e28c4daf5866
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:54 +0000

    thin roof many marry lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
