1305a850dfcd140fc54c695cd1caffd652417585
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:03 +0000

    show rich correct emphasize where

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
