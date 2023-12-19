5dc8b654a0c42a6b6b721d6c10be091f7ab18c7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:39 +0000

    call block floor honest rob

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
