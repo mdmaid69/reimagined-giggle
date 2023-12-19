fee42cca99879f107551fb7da99c48b75dd606c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:55:34 +0000

    switch lost turnon gather someone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
