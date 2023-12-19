3118a66b72f5c10f2c36cbcea177493cf2f4f26d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:52 +0000

    lookaround disagree form any direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
