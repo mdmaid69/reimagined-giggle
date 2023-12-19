3fdb02dab422c51d87215ab576417748bbb8685a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:08 +0000

    sting big yes comment lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
