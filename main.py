9c86413106e90fdcf08cb3e748b43c0f49d48d4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:12 +0000

    count sound clean spot not

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
