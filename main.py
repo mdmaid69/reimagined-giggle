3b2cca5e874e4b3752db09cb98614e3de5495aab
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:48:04 +0000

    study violence hard yet indeed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def sort_list(lst):
+        return sorted(lst)
