2e302f175b3f2a22ce9500997c3250e7990ed08b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:56:02 +0000

    wide burst lookup collect before

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
