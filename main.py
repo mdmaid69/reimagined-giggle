8a8ba91a645ad14a093345b3200f5a0c4b849ce3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:46:10 +0000

    clue prepare meal fall steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_item(array, i):
+        return array[i]
+def calculate_average(lst):
+        return sum(lst) / len(lst)
