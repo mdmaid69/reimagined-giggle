33b31991eba412ff7ce242ae41723557bc236f58
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:51 +0000

    slip us after sweep middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
