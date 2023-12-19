182db9078ae401c71c3efd93f6417918d4e9776a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:22 +0000

    fail favourite anywhere completely thank

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
