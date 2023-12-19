942354a76e8d7ecb674c6aac5528f6a40e4194b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:08:18 +0000

    rule pull love disappointed minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
