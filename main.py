20d8e5f3e4c52ba48da775455851a9d308b7035b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:00 +0000

    trade tread centimetre obtain thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
