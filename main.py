9df1fbebf994e5b4df1f932fd63cb2bbed501baf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:53 +0000

    relative last copy rain pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
