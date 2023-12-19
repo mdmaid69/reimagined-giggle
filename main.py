47af6119bd8912f979fe11d95cf961b9d69764b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:06:27 +0000

    stick doubt entry regional contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def divide_numbers(x, y):
-        return x / y
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
