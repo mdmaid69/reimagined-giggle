eb79c2b42b39000c7401a0dee73839602eacbe03
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:47 +0000

    standard variety famous unless along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
