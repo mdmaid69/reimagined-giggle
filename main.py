717e71e1188dd12c8eef05f805eb86632c70bf96
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:33 +0000

    race nowhere less whether about

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
