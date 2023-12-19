b12fd07f36c491259889959e9172bc69bba95e1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:03 +0000

    rubbish pause dinner slight occasional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
