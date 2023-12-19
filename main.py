7c98d812cce974dff1e76f7bc0335152f39ea74f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:45 +0000

    consider slow equipment connected bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import getpass
-def get_username():
-        return getpass.getuser()
+def get_array_slice(array, i, j):
+        return array[i:j]
