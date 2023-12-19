972ebbacebab2ec507e23c26989a1920b9ef77a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:27:49 +0000

    no Kk Jj nurse though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import sys
+def exit_program():
+        sys.exit()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
