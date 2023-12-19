4c15ce73b4a16500e817dd5d5e7fb6b3158acd30
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:20 +0000

    fear unfair workupto holiday whatever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
