31ee41e09d068e31b08d7f34312dd287c760d692
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:33:19 +0000

    year protection second future moment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
