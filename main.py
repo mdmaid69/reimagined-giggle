1af7742a0c00192069071a2e40b378fc27ca5c15
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:38 +0000

    great loan break afternoon touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
