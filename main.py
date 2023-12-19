55a8cf8adcb19179d102a866ae334b3e5e000c91
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:58 +0000

    balance mess hate back whistle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
