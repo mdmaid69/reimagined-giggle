76306070e57f5071cf82c8694576882add7d3032
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:24 +0000

    getaway heavy getaway tool ring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
