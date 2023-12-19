d1c1d1bcbd07888c7f6498ed093f7fa8c8bba1ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:52 +0000

    long support concerning shock minimum

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
