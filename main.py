9ac51541177672d77cd4a8bdfd02080859a539e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:11 +0000

    advanced mine interfere strength demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def remove_from_array(array, item):
+        array.remove(item)
