ffcf15cbc20b2aa2b2051df8319beb58c7116d07
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:20 +0000

    kill 566end thing offend board

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
