71326d594868cabdd3861b3f54a605fc2f0ce6fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:56 +0000

    more skin wear the thankyou

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
