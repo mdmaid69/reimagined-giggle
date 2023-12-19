bcceea66e2dc7f84e504021de5319cb59a6262fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:13 +0000

    force national 567enemy clothes heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
