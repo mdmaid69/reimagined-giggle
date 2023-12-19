14612fb667cce29a1cf64e9df9b375484d2a69a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:16 +0000

    warm little mix fill but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
