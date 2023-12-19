1f44eb329f198fed6a8f53fb1c6fb635205cdafa
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:39 +0000

    meal theirs health gate cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+x = 10
+y = 20
+print("Sum:", x + y)
