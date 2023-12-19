edd0cce0182a852cd07f568aa7cb4cf6820b03fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:04 +0000

    boil commit chat earn charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
