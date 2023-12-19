83566c0a067c818966c2369ec7ce082c6fd5d9ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:02 +0000

    level sink power possible indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def count_elements(lst):
+        return len(lst)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
