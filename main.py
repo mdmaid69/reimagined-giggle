337911e9e791fd3c9a50aa1e8b86b436152afb2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:52 +0000

    shrink none office 566end round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
