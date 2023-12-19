7bd3f2634de131a536b2a05339742d0f1551f3c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:26:30 +0000

    task imagination fill top consist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
