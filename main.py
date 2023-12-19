2e3946454529b9340cd48e4749cc6b878643b6a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:46 +0000

    swim waste plan track previous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
