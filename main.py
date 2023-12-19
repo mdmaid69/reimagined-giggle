9d80071337159c8faa115e6531d953a44ba097b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:11 +0000

    warn row sea below exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_list_from_array(array):
+        return array.tolist()
