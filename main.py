5bcb75a6ba495cb19d851bb74b163694084ae71e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:55:10 +0000

    contribute talkback increase themselves manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_item(array, i):
+        return array[i]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
