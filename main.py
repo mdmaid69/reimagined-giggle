d20e66ad018503934bcb55ed1d0db515e8f52c86
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:47 +0000

    shine control story home Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def count_words(sentence):
+        return len(sentence.split())
