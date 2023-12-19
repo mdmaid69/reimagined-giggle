2df2464205865f484f2db001f043b5b6db6abc24
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:32 +0000

    anyone talk talkinto comeback choice

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
