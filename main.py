a472b51fe1555f59589022007404556f9dc5c30e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:11 +0000

    proper crowded neither along support

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_elements(lst):
+        return len(lst)
