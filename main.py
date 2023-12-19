4e50c5bf25575208d27f944d74164ff8d6b2f76d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:18 +0000

    furniture ride practise produce transparent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import sys
+def exit_program():
+        sys.exit()
