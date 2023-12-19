f86767cb1c2341420e86520f3c434195119a24ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:02:49 +0000

    active wet command Ff well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
