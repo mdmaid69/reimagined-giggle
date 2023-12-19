c0fa62782818e8229c49e6f6c21df1046358910b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:56:26 +0000

    wing herself please clean basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
