5c3314eab3a621c9e38eb236e56a63cdb57a28c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:06:35 +0000

    classroom change takeaway deep dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
