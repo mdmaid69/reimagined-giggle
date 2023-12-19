a6e3f60783be25d0e2c1dfde3ac889c5adaad13f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:48 +0000

    claim disapprove load everybody creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
