30c92607d2c979a83817e856cc34885f3baa0be7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:15 +0000

    pronounce rich fine half journey

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
