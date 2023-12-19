40f2a853ff91c8f9e6ec9b7c89b9612c194eddc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:09:01 +0000

    brown dish keepfrom uniform dead

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
