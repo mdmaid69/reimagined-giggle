f4b3fd8d793ba938f44f3cb2fc4ba97216b74c31
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:36 +0000

    bottom treat sick willing form

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
