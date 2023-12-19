27d075aff6970e05e0abaa551ee71c36f7c4634a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:24:26 +0000

    tradition list light wander dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
