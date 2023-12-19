7d565648cf54d5e4da7e3fd900cf933e34ca282e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:44:04 +0000

    clearly cheat whistle walk taste

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
