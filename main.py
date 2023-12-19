6fd120958151ef18fab43256bb1d250a7eb52402
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:00 +0000

    yes arm strong visit sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
