699008ba31e87df38ad71e6de3f259e4fdf16cfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:08 +0000

    somebody opinion no wheel unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
