543735291d2e79b55adfde51180725960deaaece
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:30 +0000

    total willing handle aware telephone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
