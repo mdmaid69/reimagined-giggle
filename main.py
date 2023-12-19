971fc57515e9bc1f0aeb5851161af0afbd7a47d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:25 +0000

    colour track brother vision trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
