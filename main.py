fa396d4e769047ce153cfe81ce1f4a274d913580
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:14:54 +0000

    violence brave comfort punish avoid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
