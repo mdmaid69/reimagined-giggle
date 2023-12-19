54a0e28f89f9c3fdc271a9604c5cfebe1adf3d84
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:08 +0000

    about disagree your original everybody

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
