84f98a14aef85f06223825db150683324f9ab8e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:15 +0000

    correct anywhere main might target

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
