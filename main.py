5072343061dbcf6adc017fe395388e6508bde3fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:19 +0000

    truth square container watch argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
