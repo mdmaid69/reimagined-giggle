d2e50fdaa16344cff6ce282c7907b443c18fc415
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:52 +0000

    low apparently low tense storm

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def remove_from_array(array, item):
+        array.remove(item)
