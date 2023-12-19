f1b5dac90749ce492c392505898f4131f3e78daa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:51:34 +0000

    thing square point recover word

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
