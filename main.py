9d490c6e9a3f9a1628ba19a0678ca402698e8772
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:07 +0000

    top this fence black block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
