53a9f10f732f8bf6e58ec78a129d646ee7738402
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:17 +0000

    accept worried describe pin suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
