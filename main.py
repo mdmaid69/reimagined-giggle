4fc2e64c0ab69a37655b617ab42bdf9a832cd2e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:29 +0000

    force over extremely command pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
