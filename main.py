d3952ea460dbc8ec7201d0a0ad17a04ebb64fbff
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:16 +0000

    thorough contribute skin search much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
