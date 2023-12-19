63a18c52690856bafd78ef8e90205ddf14913d77
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:51 +0000

    comfort large address snow pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
