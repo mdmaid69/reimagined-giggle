fb2b21a2bc924806c48b3bdfed1939feb2fecfbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:46 +0000

    mouth recently lookup field check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
