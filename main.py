c0c153aad5dc8e24c36e605ed78ca45762b2564a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:26:05 +0000

    herself keep slip ocean square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
