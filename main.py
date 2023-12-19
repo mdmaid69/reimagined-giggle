2f75013fb463b6f5015f01f39e243374ea9a90a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:21 +0000

    practise centimetre fashion ilovedicks pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
