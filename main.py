0d83f774ea5f4b94d73df5c00084f3ac57804b4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:44 +0000

    high total be near proper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
