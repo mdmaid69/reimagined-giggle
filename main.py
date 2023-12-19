afacedba73900cd2d302667a7616f279a75ebeac
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:00:19 +0000

    size possibly onto post crazy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def sort_numbers(numbers):
+        return sorted(numbers)
