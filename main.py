e41dc99f105715e0511b02e1124a557c9ca9ad0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:14 +0000

    art heavy distant contrast past

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
