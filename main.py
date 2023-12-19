d0b75036d207c8c8bcb900f767598d15db1c8fed
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:49 +0000

    majority story long getout care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
