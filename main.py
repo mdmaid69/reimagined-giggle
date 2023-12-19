889e400d70d79468ef342d8707ea8195a6d1f8ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:28:15 +0000

    priority call pot direction imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
