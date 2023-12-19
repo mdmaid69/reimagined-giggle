3359eec2c1c3578a04a9fa6a7301490b224daacb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:52 +0000

    fire regional lower gas argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
