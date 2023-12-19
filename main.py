70053db1dd2c1fc9e7e5777b152c3082221a9b52
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:18:37 +0000

    land entry danger breakin sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+for i in range(5):
+        print(i)
