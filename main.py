96badc661feaf50931aaf1c20b5350b5c3dfb829
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:40 +0000

    hang date without upwards stage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
