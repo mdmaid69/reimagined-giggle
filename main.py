3bb6175fb81883969c5b0baa4889e6fce360225e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:26 +0000

    original tax rush quickly contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def change_working_directory(path):
+        os.chdir(path)
