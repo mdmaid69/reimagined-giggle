2e1d034ef2581daab396894b80d3dc29ad5abd77
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:10 +0000

    careless software broad especially cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
