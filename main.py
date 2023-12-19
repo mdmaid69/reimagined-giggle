160effa2b8c49533e68a67bc8392a3efe1c62478
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:02 +0000

    ready police hollow dick trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
