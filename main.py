6a856d356c7cbe7dfbddd2e32ea87e66bb644fb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:17:35 +0000

    speak work old beneath basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
