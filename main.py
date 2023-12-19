6372039442922bb75d2552af7e9f5712ca2c93ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:07:01 +0000

    direction then hole system mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_hex(n):
+        return hex(n)
