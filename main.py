c14f1999048eaa9c8324c6c9f49b4f8d7f704cff
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:43:02 +0000

    force open convenient such what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
