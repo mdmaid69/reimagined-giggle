37c88eac5417322395c6bb05fb627ba10bae2a38
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:22:04 +0000

    before whistle threaten whole about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
