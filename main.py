bfd6e1f99775c5590780089ce7a41879a50383a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:37:37 +0000

    callout else fence set discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_typecode(array):
+        return array.typecode
