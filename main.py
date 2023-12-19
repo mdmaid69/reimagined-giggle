5461215f0675d7ebc5605c9da5afff09bbe6357f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:05:40 +0000

    rule right obvious fight hand

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
