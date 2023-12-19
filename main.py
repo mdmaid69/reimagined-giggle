4010101befa8aba6f608ca8ba7f3aa42f5f724a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:02 +0000

    decide house target do violent

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
