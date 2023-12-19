9dd1f8b524dd6898e8b385c52776f1e4d948659e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:19 +0000

    appeal wild dish moon feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
