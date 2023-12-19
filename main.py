87bcd8175d2cbdf33f75b094e42d5ce15a7cf47d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:11:16 +0000

    knowledge watch appear eager close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def subtract_numbers(x, y):
+        return x - y
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
