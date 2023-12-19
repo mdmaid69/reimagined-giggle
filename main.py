c0a60944afba914010d05426cd539c7bf360683e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:00:20 +0000

    trip keepup pool tongue work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_error_function(x):
+        return math.erf(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
