3463d079711f3b2f48a2a6f1392a70abfd8e48bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:07:41 +0000

    rob record split turndown needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+def cube_number(x):
+        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
