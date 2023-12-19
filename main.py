9c3bab0bf18b860a73e7cc230b427c24fa78e7d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:17 +0000

    slight authority under detail buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
