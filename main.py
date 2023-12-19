e7b03885fd8bdd943f57323e8565d67487973793
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:41 +0000

    material wall favourite lunch base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
