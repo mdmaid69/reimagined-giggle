2c64958ab30381d506c3f4440cf4c8baaf74c81b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:19:50 +0000

    occur equipment manage strike demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
