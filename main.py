5b1fe7a394e433c5ed8bf3f9458358812fb1eea8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:38 +0000

    standard begin completely forever lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
