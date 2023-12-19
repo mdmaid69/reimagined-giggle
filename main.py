7731ce9f17f7090a9a2f8984c27830c214881a45
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:24:56 +0000

    responsible move comeoff treat release

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
