7e2453a18b3d6db75ff3158bdefc38b04ed62e20
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:34:37 +0000

    proud operation moment stick before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
