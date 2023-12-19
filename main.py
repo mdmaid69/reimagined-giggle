d8c360db2900d11e70d4e91709bf65246f178978
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:03 +0000

    pinch game dead occasional well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
