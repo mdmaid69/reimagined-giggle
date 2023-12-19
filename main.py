7909402cae1d5c69cee82b675cda973c389c41f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:22:50 +0000

    talkinto transport show probably comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
