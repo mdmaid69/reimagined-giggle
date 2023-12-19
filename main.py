5db3cce089106b39bc214726720d526fd9a2925a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:55 +0000

    company winter workupto Dd come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
