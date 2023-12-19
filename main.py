c12fd794b89de11f00288ed6a3e069d87626cc4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:23 +0000

    provide period happen small hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def square_number(x):
+        return x**2
