55f209c79af8a64e1e98d669af09efed924bd16a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:32:14 +0000

    successful square education deserve tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_set(array):
-        return set(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
