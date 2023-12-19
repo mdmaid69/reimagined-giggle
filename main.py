0d19b4ce57edadd655de069afa789aaca1eafec1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:10 +0000

    eye fire makeinto cheat string

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
