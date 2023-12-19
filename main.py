8b37aff87fddd2de1205245ff925bdb357578e97
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:24 +0000

    voice 179bite way himself exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def convert_to_octal(n):
+        return oct(n)
