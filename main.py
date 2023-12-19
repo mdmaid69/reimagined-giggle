d00ae99f2e4383120d17f5cdb03d6a52874cf43f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:48 +0000

    achieve warn getup risk who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
