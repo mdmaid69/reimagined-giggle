7325502fb31a6a68eae692b42308394cbfed787f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:50:10 +0000

    meal walk treatment power shelter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
