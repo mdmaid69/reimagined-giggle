3c1c758d23646df9df81e5662a6d089fbb44c75d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:51:20 +0000

    defeat dead represent admit emphasize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
