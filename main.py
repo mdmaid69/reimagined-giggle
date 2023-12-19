cd04fa9319faa3087e1187220d9c600704a806aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:08 +0000

    feeling confidence doctor simple advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
