ad89fca00a1d5c0922ae7c04d81f07870f2ff1c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:03 +0000

    anywayanyhow bet attempt trust habit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
