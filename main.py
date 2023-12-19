8046eff43e193a69cc0dc7a07a31f8dfa3865582
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:07:30 +0000

    recover wheel then cinema soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
