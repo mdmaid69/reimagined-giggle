906633e0f09d08a7a59e8f774d9545025b61a7d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:16 +0000

    oil expression lookafter paper climb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-def remove_duplicates(lst):
-        return list(set(lst))
+for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
