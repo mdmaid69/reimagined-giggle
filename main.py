f5e273026dbf158e9e152bfc1e703730889b7825
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:33 +0000

    patience getaway flavour legal splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import os
-print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
