03c8f18172d2a91166b441d55779f2b1eb3a80a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:21:59 +0000

    major coffee previous behave miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
