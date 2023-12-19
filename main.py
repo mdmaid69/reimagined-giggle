afbe35a1f32d6ae71b3336bb234eab890c4bdd7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:56 +0000

    dream satisfied crash mistake remember

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
