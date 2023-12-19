a74ac17c7d579ae0cc49f0a52408cd382c4ffb9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:21 +0000

    spread Itself job right defend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
