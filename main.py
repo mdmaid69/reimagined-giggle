b2c1b31460e24c9192de8cc676cbe6ea4169e2d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:23 +0000

    violence room into 565end urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
