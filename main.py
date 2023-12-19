bcccad6dfaff5418877093f8ed8ec9a7951aed53
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:22 +0000

    tone clearly offer Ll OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def subtract_numbers(x, y):
-        return x - y
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
