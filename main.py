83fa17042abfc555e2e509290a10384b09e9d42c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:17 +0000

    joke sight runaway stay capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def count_elements(lst):
+        return len(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
