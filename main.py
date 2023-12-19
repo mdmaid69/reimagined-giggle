4f6412a72a2aedb6f114d1f80e1868bc75a420d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:00 +0000

    ignore shopping day this tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
