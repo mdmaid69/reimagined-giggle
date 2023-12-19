f2562872e8bba85f4ce2b84356f4d6380aedb27f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:04 +0000

    laugh regional fix spoil convincing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
