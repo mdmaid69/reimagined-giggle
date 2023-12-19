910f544af07f1d7155192e249a4faa21738c5086
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:43:38 +0000

    lot brown any clue coat

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
