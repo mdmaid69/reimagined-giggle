43315e74b4cd94c7869ed71997294861029147dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:32 +0000

    destroy put comefrom fat sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_unique_words(sentence):
+        return set(sentence.split())
