ba9423626ab2a0df8ffc6dca35378f2666641dfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:31 +0000

    steep reduction acceptable wrap take

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+  def reverse_list(lst):
+        return lst[::-1]
