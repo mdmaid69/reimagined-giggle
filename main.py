db51a472fe4b0bf146daaf17993c2af15e56754d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:16 +0000

    someone start remove proud while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
