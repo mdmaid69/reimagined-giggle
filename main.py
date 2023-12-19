4973353cf84a226afa9341c97a5e0d1cb2049bd0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:54 +0000

    imagine float will mood class

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
