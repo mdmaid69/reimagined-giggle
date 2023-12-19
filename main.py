8bb5ce207455416ff33e5de1cc769b2958d8c4dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:26:28 +0000

    loose underneath Cc bang importance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item_count(array, item):
+        return array.count(item)
