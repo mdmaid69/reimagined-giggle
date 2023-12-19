bca7abf195658ce77dfea45584917d11b336f8cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:15:32 +0000

    around whisper port acceptable to

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
