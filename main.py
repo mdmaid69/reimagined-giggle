37866d3ab2a7d761d2c5d140a9c579c482c4e4fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:55 +0000

    569enjoy declaration recent benefit about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
