a437cb393968264293f962a2fafaa27f0fe947a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:54:57 +0000

    dangerous imagine Ff Mm method

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
