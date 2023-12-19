f14d36c160e6526c602292bd66907a2b7baa7ea9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:29 +0000

    crash fairly flow me rubbish

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
