3c42a27919b337619daa8fc549920f60ab64b0b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:45 +0000

    develop manner warn food interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
