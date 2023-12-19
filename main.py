ad19cfce26a56a7077e0cbaa43329785e74bcccd
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:27 +0000

    my patience because understand lesson

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
