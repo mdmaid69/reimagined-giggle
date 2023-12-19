159e0dcc377c470e5aa71071eb2c9fcaa4ea181f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:06 +0000

    experiment bunch outside defend difference

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
