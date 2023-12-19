ca4f5e235f851b2a57e0e58cd1e333ed04921be8
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:56:37 +0000

    back speak circumstance strike scared

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
