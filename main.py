3e4a7182e99e75059fed855ba4962219b250e46c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:35:38 +0000

    some now pocket hate pull

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
