675ae3a467930840ea7f3f42707856d3e8cd94b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:20 +0000

    permission exchange tip drink plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
