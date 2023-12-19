86861ef909d175fe8f63e42e374e1befd91648cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:16 +0000

    Rr bury command choose fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
