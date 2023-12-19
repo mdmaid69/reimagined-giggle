6765f882456898fbd1091f607c5265a54add1c41
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:34 +0000

    game dream OKorokay press finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
