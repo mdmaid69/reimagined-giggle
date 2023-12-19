0ea2500f900ddfff6a557a099e27a2456ea333fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:40:19 +0000

    play 568energy runinto thinkover legal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
