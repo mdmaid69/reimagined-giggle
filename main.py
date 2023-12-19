e0db7e326a1c97d1335c0a9663d255399fcb8b96
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:30 +0000

    Ww rely phone colour direction

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
