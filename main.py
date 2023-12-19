32aaf174f2ebf4002e96db0154785f340d2a3b8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:57:36 +0000

    read top gas exercise anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
