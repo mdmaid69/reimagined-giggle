2915ffec56ec9dc0ef2da0fa39aba286e6ab85c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:27 +0000

    breakfast material show tap limit

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
