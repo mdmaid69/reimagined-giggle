6a193a8d139190338dd31b81afdaaa634ea8b0fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:54:32 +0000

    convenient value surprising whichever second

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
