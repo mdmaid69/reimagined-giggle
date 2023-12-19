ebc441b5c7e7894db5d3279f02bc0a5f1b46d096
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:07 +0000

    ticket gooff forever official first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
