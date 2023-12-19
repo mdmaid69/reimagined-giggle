45890b0f10c290c64a8bdac173deb1d1c8a05c20
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:06 +0000

    basis become failure price flavour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_memoryview(array):
+        return memoryview(array)
