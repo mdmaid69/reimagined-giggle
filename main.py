ff672d12f96d688171fc71a37e59ff2dc90637a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:49:30 +0000

    carry blow clearly mine further

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
