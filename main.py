d6d0ee9abe14636663d0ebd08d70701511dd43b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:52 +0000

    busy storm boat bet squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_str(array):
+        return str(array)
