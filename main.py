b01f6eb0f8c063aebfaf509bdd8ee321404905ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:49 +0000

    violence speed motion book sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
