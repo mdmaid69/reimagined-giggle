107b1d6d3f16e141e91a495d20560927596f4bdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:58:57 +0000

    crisis how low anybody towards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
