a4623c5197d325b30e929da44a63a7ad44a051f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:54:10 +0000

    direction need compete wing story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
