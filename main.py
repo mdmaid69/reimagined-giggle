3d3bd1794d9e20d74ec2a2aac507b6261e0a202c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:10:06 +0000

    protect fold freeze reserve accordingto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import tensorflow as tf
-print(tf.__version__)
+import time
+print(time.time())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
