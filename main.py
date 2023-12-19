99509a2455dfc7881de2ff7790d42bdeb9b85bba
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:13 +0000

    instead afford trick clear catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+for i in range(10): print(i)
