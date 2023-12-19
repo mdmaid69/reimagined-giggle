55e33f1586aefaf865239d69f4be7cfcb31d0b6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:41:12 +0000

    watch correct pity truth shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
