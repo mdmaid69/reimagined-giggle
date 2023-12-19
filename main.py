c83b14022362aa34f2c07fa46e4b4d0b60696bf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:11 +0000

    bound hard public scream paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
