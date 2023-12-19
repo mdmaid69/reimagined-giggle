914147f842cb8dc35d39ff138a9ee1b43c98f3cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:12 +0000

    about last politics support whoever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
+  def convert_to_octal(n):
+        return oct(n)
