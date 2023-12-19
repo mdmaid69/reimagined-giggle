906b5c927b1ef1e8b0defb7f07eb6402ca9704b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:16 +0000

    bar then visit model onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
