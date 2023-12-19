c8ce22bd6402ddcb3f8a390f135d986ec946756d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:37 +0000

    variety praise duty class want

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
