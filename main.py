0143b684c29b14dcbbe97889781ae2f441e45ec3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:24 +0000

    culture throw agree spite final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
