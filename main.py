f00d949c435d852dbef51e3e4b0aaeef1322674f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:29:56 +0000

    walk disappointed story tap square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
