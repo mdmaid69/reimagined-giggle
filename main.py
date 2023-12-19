0d9243990ca12ea9d7082e643f3c3603034ebea6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:26 +0000

    mess appeal apologizeapologise before whoever

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-name = "Python"
-print("Hello,", name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def cube_number(x):
+        return x**3
