1d8b2699d0db9719ffb4e2b0f042884022cec04d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:40 +0000

    slide want apply fine breakout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
