d82736f9f0140c3d648413c079940149b5bfd3a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:42 +0000

    supply keepup guide recently lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
