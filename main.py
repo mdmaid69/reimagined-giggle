7d5422279379709de524fcb475a20fcfe00d1ce8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:11 +0000

    butter search pity several summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
