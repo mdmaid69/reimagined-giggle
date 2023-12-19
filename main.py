25480dc7eab5e50eb265baabcfe79197e714b963
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:03:30 +0000

    peaceful occur measure exploration thankyou

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
