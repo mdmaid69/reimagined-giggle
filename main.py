ce1961569ad91f77e72d2fd52b3a2ad5ca5ef3cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:12 +0000

    strike love must past workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
