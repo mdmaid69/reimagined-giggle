485c58a87cc023d9f202540e6f78b20d7c2727cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:25 +0000

    she takeaway getup release scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
