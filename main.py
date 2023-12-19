f0705eed5dfa680809a37d46e4e3936c3b872d5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:20 +0000

    determined increase live keepout comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
