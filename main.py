aad33695ae913c20434890c5c29da3b9d408468a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:26 +0000

    think spill paper heat right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
