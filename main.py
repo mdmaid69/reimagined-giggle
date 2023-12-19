59642a4535dc38d0a45b6ecbdbec021c51ce24da
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:17 +0000

    entry attitude only continue do

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def append_to_array(array, item):
+        array.append(item)
+  def is_odd(n):
+        return n % 2 != 0
