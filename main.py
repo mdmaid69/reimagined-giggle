39054361fd4393d62c3188d780ae8bc92546229c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:51 +0000

    tomorrow excellent worry freedom collect

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
