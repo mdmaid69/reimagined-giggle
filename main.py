8e6b8c6188dc3df0bb62ec83f9b2b9e67ca8260d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:54:55 +0000

    may hold hardly suppose surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
