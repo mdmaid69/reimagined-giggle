eb58c1b0458c6e0b4e1e99b74a342c8d325451e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:17 +0000

    closely change thorough happen afford

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import re
+print(re.match("h.*o", "hello world"))
+print([x**2 for x in range(10)])
