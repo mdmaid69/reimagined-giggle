66911869f1bbcd16c6af71960c05f30cf654cea4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:36 +0000

    soon story remember to than

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def count_words(sentence):
+        return len(sentence.split())
