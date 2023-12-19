f26dd4913acf873c2e4865149f818f4a97adca62
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:01 +0000

    keepaway getoff branch exciting train

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
