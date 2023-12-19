c2fb9c6d439b356b5b9ec15491727b3e76685282
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:09:21 +0000

    ill hate card school sometimes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
