60126efe38bc3157bac74adabe235ca6d67ee9d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:53:00 +0000

    attract closely corner object trouble

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
