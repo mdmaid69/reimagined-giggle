55c89307c67caa726e87fed925b04691c55bb752
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:15:28 +0000

    world blame event aim ring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_time_since_epoch():
+        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
