5d26920db7be78f3245ae51f6a6553bdec95a88c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:16:42 +0000

    wrap imagine by relief sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_volume(length, width, height):
+        return length * width * height
