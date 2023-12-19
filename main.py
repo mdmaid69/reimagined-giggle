a7c76742ec864ad15606ed11016193da7a4534a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:26 +0000

    forward squeeze cause possible false

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-print(math.pi)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
