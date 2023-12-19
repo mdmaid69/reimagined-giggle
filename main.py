acea7a4108aa06a46a9b3509a31e566ed021621a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:36:37 +0000

    want time loose getup job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
