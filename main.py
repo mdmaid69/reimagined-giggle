493901964acc4c84eb09608aca9baa474029f846
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:48:16 +0000

    comeout declaration boat welcome excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
