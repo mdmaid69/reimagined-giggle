95173bc1b314e5c6475a3aba9d7389656c2048d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:56 +0000

    father survive report plan substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def remove_directory(path):
+        os.rmdir(path)
