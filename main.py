f19f25c75c717fd1c280998de3cde5764839bfdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:09:02 +0000

    stage yourself talk occasional convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import random
-print(random.randint(0, 100))
+def clear_array(array):
+        array *= 0
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
