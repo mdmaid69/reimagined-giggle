b11fbdc6f5f6cd704bb8bc99eb6e76d8d4615ccb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:46 +0000

    branch transport geton fierce pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
