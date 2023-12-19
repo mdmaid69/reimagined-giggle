c850cc084f0915965093ccf88301a2fba0a86134
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:59:08 +0000

    possible legal Itself positive bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
