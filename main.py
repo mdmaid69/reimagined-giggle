37c4d3ed7272a17f8455d16c74ddaef7027b77d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:26 +0000

    exciting Ii low matter beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
