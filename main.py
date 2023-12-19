39de80d1a043a86e0c70ad223007cb6c389a1e76
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:04 +0000

    quality city exchange message back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
