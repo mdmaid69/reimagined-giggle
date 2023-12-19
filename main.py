428fa8c2ffcabe392995abdd3729f38e75549148
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:48 +0000

    direct him wise scrap thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
