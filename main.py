4e1277feffae409e0e0806638da64d8ab3e1d8bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:08 +0000

    after duty wander win grow

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def convert_to_hex(n):
+        return hex(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
