624b6e78e76552485e3fad7963a250d07845e496
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:41:50 +0000

    centimetre bringback nerve thought cough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
