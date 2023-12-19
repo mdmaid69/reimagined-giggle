15a81bf9e56fa1b9c214bb4707d1ce4b7bdccc5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:27 +0000

    effect shock free development separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
