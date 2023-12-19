ebfd9c510bcc2ae11ac42dc251b4ba80bbc54924
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:40:30 +0000

    fire respect space 570enjoyable plain

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
