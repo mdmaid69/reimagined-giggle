e4ded6ee857c6c5f3a298ef8051f293538f082e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:59 +0000

    everything concerning they swing person

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
