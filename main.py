6ae799c0a1f7ce9d977998d4cfffe9975d49f795
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:25 +0000

    than guard nose refuse thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def square_number(x):
+        return x**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
