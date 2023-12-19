24bf84f9dd5434e9b4febaf72b3f8ef971e8bab8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:36 +0000

    insist punishment mine thick concerned

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_power(work, time):
+        return work / time
