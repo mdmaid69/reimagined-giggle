756fdf3aef4a9c8193d547bdc1681e6aa23a4abf
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:41:15 +0000

    game every loan punishment moon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
