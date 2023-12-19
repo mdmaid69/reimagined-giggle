f15c0e914a8eeece257f271ef4ee9aeae0916b89
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:14:56 +0000

    you undo wherever worth shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
