0a60070d4ee14935853c3a839fec15af1655a684
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:39:36 +0000

    several turndown general tomorrow invitation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
