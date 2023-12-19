11bbcf374ac33857842d41ef8ac31a9af3a95725
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:24:47 +0000

    interview thinkof table famous quit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
