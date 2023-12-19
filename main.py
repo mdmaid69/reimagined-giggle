fa7bee71460476135ace420ec9aa7488665e24a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:19:58 +0000

    gather near another except giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
