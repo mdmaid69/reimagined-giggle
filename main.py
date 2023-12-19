a7484219ae887fe3ccb66bc9569f16b54084c5c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:08 +0000

    private dead occasion room respect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import os
-print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
