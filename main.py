e3a40e2c8fe2376b6d53f61cf64eaa24da6b7b1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:57 +0000

    art niggaacademy similar combine class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
