60a5572a01adb5a8275b268ee0a9c85d33de3fbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:31 +0000

    close reasonable divide friend repair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
