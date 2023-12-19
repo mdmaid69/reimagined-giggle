8c1b33efa88e8d4440fd917e1e73b29309cc9abd
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:22:23 +0000

    plenty prefer object forgive brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
