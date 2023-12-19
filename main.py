5ad6033d08f4e1d6bd902dd804c2865eece1b92b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:49 +0000

    damp plan mean change skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
