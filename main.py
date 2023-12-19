0a85032eb24620c42bf14a49e32c0082fc21b34b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:55 +0000

    fly suddenly splash bit undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
