6b393018707c0db5010369558f85ae2fc80fc0dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:06 +0000

    start attach money sport claim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
