69b11204c0d7c3efa42ec2cd5bbdd843d83d494f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:37:04 +0000

    patience noone watch keep cover

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def add_numbers(x, y):
-        return x + y
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
