29e65fd9c04e60de3c5017079f71e7a06cf354c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:26:02 +0000

    task build wind jump his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
