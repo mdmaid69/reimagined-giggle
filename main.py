754bfd34a485a65dd71e618b417de38c9d65d65c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:19 +0000

    572enter broad stop popular blame

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
