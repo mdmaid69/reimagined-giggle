22d2f5f0e7e5ff22659841ef360a91af731440e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:41 +0000

    collect yet wind children dead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
