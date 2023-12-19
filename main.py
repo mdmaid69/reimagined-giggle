4c02ac6b34eb7ba38d4f748a63be6f01f944b9f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:48 +0000

    joke terrible old favourite rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
