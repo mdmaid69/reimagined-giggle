a51c2c420f3701130d1d673fd6cf26c0c7d40cd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:00 +0000

    finger target top sink keen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
