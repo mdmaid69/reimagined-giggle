9b7aa4bf4356bace65bb3d37570302f3e1a7f156
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:31 +0000

    future skirt score tip free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
