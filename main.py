3beb06d4e710a8e611ecde4e48cb3fdcb811e124
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:50:30 +0000

    truth track turnout decision admire

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
