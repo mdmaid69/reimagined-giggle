1156dd97390a1307f17a12cd47f1bb95ceae4c2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:21 +0000

    school basic cross swim actual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
