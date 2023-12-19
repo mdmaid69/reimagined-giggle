322a6be519d9058077a86c7d6d3bba6ac57cce7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:52:39 +0000

    bury goout truth board favourite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def reverse_list(lst):
+        return lst[::-1]
