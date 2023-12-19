8fd060c495bdf7e7ddf9ade2fc4af22d3d871972
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:13 +0000

    little progress they kind certainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
