95aa2c5942f142296c315263f24e6979cc3f11c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:08 +0000

    show than tomorrow impressed work

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
