d106820af84c62a26fb5de143da3bbfd3bbb8b53
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:25:24 +0000

    wide their behaviour deep young

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
