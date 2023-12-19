25493bc31524fc1323117732f4e7166a672fc8f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:04 +0000

    only cheap snow sad violent

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
