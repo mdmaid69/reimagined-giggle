00b997c7b8d5eaf3e141ece1caffbed6091f18d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:15 +0000

    represent succeed insist rare early

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
