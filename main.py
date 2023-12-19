a1ad3a26825e7a12e8d37fd6527a70ad6e9677e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:33:31 +0000

    suckme effect grateful trust name

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
