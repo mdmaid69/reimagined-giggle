756007e4c4f4687c3af41840893123e508a0abe5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:58 +0000

    surprise small row rank foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
