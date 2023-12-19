e0a8826d664c8823d1f01dcc8884f9e0f91034c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:21 +0000

    thinkof takeback left tight worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
