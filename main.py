da0c8b31ffdea3fce8173300bf39fdf2c7e5e269
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:39 +0000

    scheme sore sure stick later

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
