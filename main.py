d69706666d1c56159e20c054f3c8538e140574b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:22 +0000

    material lonely pipe tidy target

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
