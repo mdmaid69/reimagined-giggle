b891b2045e037459536f93ff6b4d8b16b883b271
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:18:22 +0000

    return hard subject change impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
