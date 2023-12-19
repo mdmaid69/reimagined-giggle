e12bd1ddeb22b4d559127865c3bdcdbf1af90909
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:34 +0000

    bank temperature ignore exploration breakdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
