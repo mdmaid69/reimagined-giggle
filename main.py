eba5afe9ff553605a0d1d9a65885fe65ac1376ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:05:38 +0000

    ticket inquire deal anywhere large

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
