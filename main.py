9d95cca629ef95916cf430e19a987b712e3cca06
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:47:58 +0000

    between bye contain rest content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_index(array, item):
+        return array.index(item)
