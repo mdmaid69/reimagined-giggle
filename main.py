c3c11023c0bc3b8e557d598829429a04b32bbe13
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:24 +0000

    middle dull thankyou comment satisfied

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
