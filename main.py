dc269f03d72e5617bb0504f3aadb6f55eadadd82
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:28 +0000

    me passenger mine nor behave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
