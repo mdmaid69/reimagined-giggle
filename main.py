0d4f902249b8a87db5755da425b99133250b801e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:15:47 +0000

    soon make break bag genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
