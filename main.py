14a6d2ab22e58d2f71302a08d1c9a59e2dcbfdde
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:25:12 +0000

    able intelligent university question prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
