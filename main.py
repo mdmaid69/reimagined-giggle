6a162146b92cf91841467682a715d8dbe2d5c4fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:22:44 +0000

    pipe cover platform allow mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
