560f0e64ab340f0ba93dd5fb4b8f3b31e1e9fc8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:46 +0000

    everyone loose slice fight spend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def get_array_as_list(array):
+        return list(array)
