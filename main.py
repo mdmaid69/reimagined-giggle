55070bb08f443bd834c8c3dbc8d7bd53b39701a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:48:12 +0000

    correct sky appeal arrive method

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
