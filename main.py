06d2052203c03c3263ee2b862ed08423a9ebd4c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:14:00 +0000

    complicated mistake pay water ask

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def square_number(x):
-        return x**2
+import os
+print(os.getcwd())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
