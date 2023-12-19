a465ca3730df87a6fe1a7d959510ef497eb795ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:59:10 +0000

    attitude somehow lonely love affect

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
