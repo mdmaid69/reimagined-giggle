cd02e763872235faa6a5abcd095a57f88a9f713e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:31:15 +0000

    shoulder cough practice precise scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
