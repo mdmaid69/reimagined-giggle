e74045df61cb3c3de489ffd35045dfc0d7dc0d93
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:19 +0000

    brave store interview gift promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+print(time.time())
+  def reverse_list(lst):
+        return lst[::-1]
