4a5fe862b9a0c30a9fc974be9671fb726ce5c4f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:59 +0000

    far spot sort hungry world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
