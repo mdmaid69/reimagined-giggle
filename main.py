467d877d2fcd4d18dcb62056109e575704c40208
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:51 +0000

    hour method into free ordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
