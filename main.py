f385b02762f123f0893dcfc6e45f67d0ea72267e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:14 +0000

    relation unhappy slow beyond maintain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import os
+def change_working_directory(path):
+        os.chdir(path)
 print("Hello, world!")
