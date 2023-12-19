ff0a1c391ee59404162d8a28d518979188c9c30c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:35:57 +0000

    sell between foot deep sign

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
