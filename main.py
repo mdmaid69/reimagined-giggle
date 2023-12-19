0e20bfe812c4d9a624e59b3936f03975fb408dc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:14:56 +0000

    immediate Qq hang pity step

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def add_numbers(a, b):
-        return a + b
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def split_path(path):
+        return os.path.split(path)
