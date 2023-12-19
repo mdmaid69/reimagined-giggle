872b17617fc34215f56156367c3d839802e92f77
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:08:47 +0000

    peace cough sink past during

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
