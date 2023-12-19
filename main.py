9155d683653055dd3e31a72138a187e994af56ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:32 +0000

    mend accident type available best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
