fd1aa8eeb5ac5c47680c26e4c825a43d61cf97dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:31 +0000

    tooth mark cheat that frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-print(random.randint(0, 100))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
