3c4f1710fc5bb99d54a9828fc80d3802bd4effc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:04 +0000

    challenge interview strict raw child

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
