ec1ca09a027d0d0a210173d1d071dceb25d4701c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:33:02 +0000

    understand steep pause become unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
