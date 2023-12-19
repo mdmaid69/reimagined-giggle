4b45540ae9fa6fbfb4109dd1ecfaa4d49d9d37c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:39:37 +0000

    what talkback nor foreign sister

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_min(numbers):
+        return min(numbers)
