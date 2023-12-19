b0c294657ad2ce3e7a25ccc3eaf3e35bf16f6502
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:36 +0000

    rain goup subject join deserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-print("Hello, world!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
