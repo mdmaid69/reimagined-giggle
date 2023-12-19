a3607b36214ee17d9378c9741848e0d24ed78489
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:56:01 +0000

    rush tooth able pretty punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
