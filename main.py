33441bb0b82f558a2caf145ddf4c44d249924461
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:45:37 +0000

    always watch Yy disappointed watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_priority_queue():
+        return collections.deque()
