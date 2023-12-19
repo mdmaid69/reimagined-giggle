35cf5358d1d4775ccae3ed54b3f38b81b5f97dbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:05 +0000

    shop system bear I and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
