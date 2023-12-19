42a7bb872af2a3f7c46c494d0666f9c1f655beee
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:05:48 +0000

    sleep patience total fight lookup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
