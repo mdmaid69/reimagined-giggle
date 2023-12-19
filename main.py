ceb49198df5d26e5867c6fab5ec888cb083d8e23
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:39 +0000

    scrape protest support further sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
