7d1385e9c49df0b60089c38ad91f2277d49af101
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:21 +0000

    news punishment strong cost discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
