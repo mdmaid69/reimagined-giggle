8ea001ebb5ced82be0a5993ada2f7e7365f4830e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:41 +0000

    welcome quick none tend service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
