56b8b83f7a8e53f626a73fa6f6f545366e9c790e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:03:37 +0000

    move manner fierce getout distance

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
