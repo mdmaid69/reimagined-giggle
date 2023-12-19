e8f2d3c4b7cd38a2703b4a4c526b381b1b3288aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:50 +0000

    slow middle needle over callback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
