4eca4279ecc4eb437967bdb1913f2858f8e7acd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:48 +0000

    drop violence result suffer run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
