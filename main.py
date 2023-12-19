2d247b2116403d0faaf2f0e929ab6ced157a4481
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:37 +0000

    want compete hungry both glass

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
