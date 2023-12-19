6c2c049d8e6a2398e313afcd82485989e6ace16e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:53 +0000

    forward plan think separate mood

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
