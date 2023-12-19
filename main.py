9d3fccf6623328984958166f215898aaf7bf4d8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:58 +0000

    force Kk necessary explanation warn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
