9ba52c29db7b2ffec1a08b5ad669d22289dd939d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:12 +0000

    secret happen touch exaggerate guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+print(math.pi)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
