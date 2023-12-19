16f1e4584c26f01b9897632362ce8e1f9ffb720f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:43 +0000

    practical listenfor instant any over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
