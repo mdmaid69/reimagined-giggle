f8fe10a08c840762e076455d76563e46bc78faec
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:49 +0000

    swallow prevent benefit exercise alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_as_list(array):
+        return list(array)
