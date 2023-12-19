8db244eafe6e4cb1ded2a2de06e6a11ecba8bf11
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:45 +0000

    on settle be flight pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
