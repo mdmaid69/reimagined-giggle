2b034c8014625f11d4b329946b542f71a037292d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:25 +0000

    trap catch boat admire invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
