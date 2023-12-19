6248fc4845523d48f7ede5712e13027d6fddc5e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:49:55 +0000

    hungry fairly neither room develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
