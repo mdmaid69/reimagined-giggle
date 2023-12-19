b1199ba9c5ad3d9f45731efb65eb226fc28ebfb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:21 +0000

    communicate central free old slowly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
