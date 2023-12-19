0df8b1896df3de70a3f246f7ae9eb385b4f7f5e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:13:42 +0000

    memory away responsibility basically dare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
