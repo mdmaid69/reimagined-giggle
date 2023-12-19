462e5a9108e79a116abe50ada7b96010c6760522
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:59 +0000

    today slightly their embarrassing mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
