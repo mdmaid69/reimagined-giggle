a1a6d87865457b9ad5356cdb62ef6cf73f87b1f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:00 +0000

    always fold other mass bridge

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import array
-def convert_string_to_array(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
