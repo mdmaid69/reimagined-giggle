b8188fd3c4db7a2aa9bec966d08efe8b4e17dedc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:01 +0000

    ground name temper improve congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
