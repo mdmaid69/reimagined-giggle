10d4da61ae9cde42b4f495f88ccc1edfff63d04c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:21:33 +0000

    region car passage awake interesting

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
