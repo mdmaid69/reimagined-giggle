6005b2c268e284f3b27fd00872ab189503349d32
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:42:39 +0000

    pen race form rely word

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
