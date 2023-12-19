af25b8b894a2e8229efa8dd24651ec56f0c73bd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:12 +0000

    danger explanation clever short father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
