8804b1fcd9d433301e6db0ab163f19986ad89513
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:58 +0000

    comein yet government accidentally new

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
