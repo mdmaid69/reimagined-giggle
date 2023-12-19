01562b128d74f73385eb79cbbf5b191bfa6bd0e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:45:07 +0000

    pure succeed tap good complete

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
