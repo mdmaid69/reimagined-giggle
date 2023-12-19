7257942218521ef7287b11adeffd37db9a70e792
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:21:07 +0000

    same them crime piece book

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def find_max(lst):
-        return max(lst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
