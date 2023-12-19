54521843805bd20a3710cbafb1dfa2b9ef285653
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:50 +0000

    pencil angry gradually head yesterday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
