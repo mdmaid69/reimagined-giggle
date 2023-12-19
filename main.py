186f83486ed6de21299d86828b81ccf533d9091b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:34:41 +0000

    close soon intend move patient

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
