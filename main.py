442901b26b382b53ab84afb21b4bf4b958848e6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:40 +0000

    confused treat match form name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
