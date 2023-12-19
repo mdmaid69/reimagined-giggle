183d00c92458c6befe1da5b93d44f5b8e8505790
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:03:25 +0000

    stop holiday help space prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
