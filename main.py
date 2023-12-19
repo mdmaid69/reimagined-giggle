b07691e117579c5727c1b3d2c88829b32d3196fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:13 +0000

    control convinced burst float depend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import tensorflow as tf
+print(tf.__version__)
