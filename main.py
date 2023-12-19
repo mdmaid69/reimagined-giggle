2b972b10ffe7dfe460ca943bb827ff36a92415f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:34:32 +0000

    bet comeback whenever fresh exam

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_work(force, distance):
+        return force * distance
