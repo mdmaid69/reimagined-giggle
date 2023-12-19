4932dc49378b2bdce225a8f50f0e920f7f23cf7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:03:11 +0000

    well lead congratulate used concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
