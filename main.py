77daa746ce66b4ad42114bf8c58811c4fa670c41
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:21 +0000

    apply music go demonstrate concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
