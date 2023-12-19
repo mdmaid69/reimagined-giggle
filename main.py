a8742127ae84d1374b99c1ac6a21f6fb80160374
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:15:05 +0000

    available empty song sea left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def square_number(x):
-        return x**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
