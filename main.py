0657098f106d3196cd8b0fef86635d8d365c7586
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:19 +0000

    flavour long neat relation front

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_power(work, time):
+        return work / time
