f5c9473bb1da7e1633b6eac996c3fce24225e15d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:12 +0000

    town classroom figure produce fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
