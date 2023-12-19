4aa1a90f44b1671faca23468eab3211884fab281
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:10 +0000

    against disappointed chance Bb decide

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
