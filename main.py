1e5ea04c11167cee1c82581125f2153a230652d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:56:10 +0000

    last supply forgive unite turnaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import time
-def get_current_time():
-        return time.ctime()
+def check_if_array_contains_item(array, item):
+        return item in array
