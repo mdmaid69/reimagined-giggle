3899f94669c37fa1d23b9d54ae1f80772c13b5de
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:49 +0000

    arrive write feed drawing region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
