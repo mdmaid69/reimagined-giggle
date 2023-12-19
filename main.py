42cf2b49b7459dffa1d211b6205bb7f51bbf8ed2
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:01:51 +0000

    sail bound pity offence inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+def change_working_directory(path):
+        os.chdir(path)
