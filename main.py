fe8e255301c65fde159e01a1d9884ab8f8f66aff
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:41 +0000

    job cum business will speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_float(array):
+        return float(array[0])
