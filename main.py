6079d63f30b9843d9de24afdf1a66be27ca69ca5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:19:27 +0000

    flavour flow trade noisy slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
