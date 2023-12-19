7ad730efe758f902a9446a2bfe757eea27ab75b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:25:05 +0000

    sport comeout want platform keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
