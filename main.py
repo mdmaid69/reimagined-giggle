5452f49cece3e15eff54ca0cb9696928e415928e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:04:55 +0000

    divide tip another less decide

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
