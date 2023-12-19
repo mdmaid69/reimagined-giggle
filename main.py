f7ef6be48da817bbe420589917b3494ce377e3d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:02 +0000

    belief custom mass will prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
