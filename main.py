a17a91cdac3a17ce038068d2fc468cbfd13cec48
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:22:01 +0000

    in bank negative emotion doctor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def reverse_string(s):
+        return s[::-1]
