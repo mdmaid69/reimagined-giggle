1451a79943a954e53172b03a94e1c6a0626c4c62
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:06 +0000

    low goon pattern when unlikely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
