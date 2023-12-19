2f5f02dbd67295023fb00ff6671da5ca033c1cbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:06:24 +0000

    on term shape likely scheme

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
