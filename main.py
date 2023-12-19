9a8ac50e40cf8ba238b4f2ba41fa41e11164f7c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:21 +0000

    feeling game disappear female remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
