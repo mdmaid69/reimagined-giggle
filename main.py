48235aee66744b1ee18bead30f23841a8caeb262
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:43 +0000

    forward move many waste star

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
