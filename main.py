25ee3e612c91096ea4043d8125b4bb8cb9b9f044
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:45 +0000

    excited whisper confuse home pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
