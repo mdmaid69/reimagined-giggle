7732ccfcf51ca931947e0c68c586d8b6bfc7e54d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:57 +0000

    complete everyone business half wave

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def extend_array(array, iterable):
+        array.extend(iterable)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
