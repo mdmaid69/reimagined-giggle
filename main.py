26df2392761ca898e80b3a7b0abdfebce9a61a1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:19:36 +0000

    strict expression typical before 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
