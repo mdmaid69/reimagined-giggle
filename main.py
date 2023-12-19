2867c27201059a1e16fb0659b4ad7d811e8ca597
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:36:12 +0000

    Pp favourite remember keepaway board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import sys
+def exit_program():
+        sys.exit()
