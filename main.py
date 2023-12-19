d255616d6eb95a6ec46977bfc37fdb49170d8eea
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:06:57 +0000

    sick wealth spread feeling worth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
