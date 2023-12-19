1379ec3f339bf25fde0312010c16fe9dd6636b10
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:39 +0000

    ocean save grade shallow set

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
