53debd3843294770d4f425939ac66cfcfa42826a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:57 +0000

    lunch oil turnaway answer inform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
