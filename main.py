547aa1694cf062d18a9caa5fcc5dfa0aaa54ef85
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:34:11 +0000

    may event someone protest come

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
