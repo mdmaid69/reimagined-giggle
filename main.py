d19670b856fcabb135c07ceac7a2b80fe67b5f44
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:31 +0000

    storm earth drop obtain turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-for i in range(10): print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def remove_directory(path):
+        os.rmdir(path)
