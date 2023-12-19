9c27bd1a3c99f93227bcaa2796eb5ffd79af5ed9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:19 +0000

    lunch upset major star brave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
