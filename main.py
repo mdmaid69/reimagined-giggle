7a86ffce356c58521b4bc1c4e869377662a8f9fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:15:00 +0000

    begin basis another twist beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
