a75158677cdd6485f64b5187fb9bab89e69aceb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:32 +0000

    Ee bread lie square face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
