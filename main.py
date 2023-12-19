bd67bf22a9dc68e7ef46ed76513c76c237d96532
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:11:45 +0000

    breakinto calm square probably job

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
