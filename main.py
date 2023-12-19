afc087976434fdfbfcf4ee616bb815b5ab4729da
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:20 +0000

    they listenfor murder expect imagine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  def convert_to_binary(n):
-        return bin(n)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
