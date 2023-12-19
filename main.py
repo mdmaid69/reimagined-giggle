5e344c87fcb952b2aeff92cbacc83e79997be7ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:12:01 +0000

    ball why male pool support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
