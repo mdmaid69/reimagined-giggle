77dfe71d2c21064de215a659f9eb7adb238858f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:26 +0000

    fail position increase Mm prove

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import getpass
+def get_username():
+        return getpass.getuser()
