69c91c24d3cdfe1e03e4021109fb5f36ea9f6261
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:31 +0000

    environment side may nonsense themselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
