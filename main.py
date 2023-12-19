f45a9959058b2bc75cc6fe6cf9f7859b817bf163
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:14:43 +0000

    guess doubt date answer straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
