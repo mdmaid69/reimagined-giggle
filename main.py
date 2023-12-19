1a1c8ddfe59716c26403dbab66ae1ee7f918656d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:09:16 +0000

    tall active turnoff instrument rise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def reverse_string(s):
+        return s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
