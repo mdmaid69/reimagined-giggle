fc1e4c4815d0839887784eba32caa07ae127f633
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:14 +0000

    mention strong bring nearly whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
