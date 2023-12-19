9eed84081556e923250e2264be77b1a19045965a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:46 +0000

    put innocent heart collect use

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def square_number(x):
-        return x**2
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
