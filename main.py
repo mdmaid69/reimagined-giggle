09154bc505f850528776a4e6ea36f0a69088f71e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:45 +0000

    makeinto listenfor pretend important dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
