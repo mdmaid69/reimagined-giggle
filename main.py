b810f16581fe3ddbbeecdef0d794e782e27158c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:07 +0000

    sick good rare beginning like

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
