090faaa79429253d19035dc36b1e181364b4f344
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:43 +0000

    season design shall apart takeout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+i = 0
+while i < 5:
+        print(i)
+        i += 1
