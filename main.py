c8a0b411ffe6023281e2d46fe40b9f32aa8c79e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:08:49 +0000

    getaway front trick danger birthday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def convert_to_json(data):
+        return json.dumps(data)
