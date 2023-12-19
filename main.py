4e9e583269f43c5763e4d961381ba786b770cba5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:35 +0000

    not usually power getoff choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
