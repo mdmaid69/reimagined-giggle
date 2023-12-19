a26891cab3e9867d2ab5b6ab2731ef33665069c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:44 +0000

    leg bush further calm nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
