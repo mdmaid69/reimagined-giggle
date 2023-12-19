95bf4443eb36f9222d93b06b3ba577a7012fe699
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:25 +0000

    many pay like pipe piece

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import threading
 
-{body}")
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
