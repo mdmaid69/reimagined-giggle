2c378b6cb46cf93546e82bb5a0a6c97ddb0fd435
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:54 +0000

    sight reply pencil apparently backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
