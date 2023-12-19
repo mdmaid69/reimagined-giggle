acede9a2dd62f37718cd4d62d534bded54e7d03b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:46:39 +0000

    walk slip quiet basis ask

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
