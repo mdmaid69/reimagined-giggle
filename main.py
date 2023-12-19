f9c9908e0e89055d3c489ea4b421f0856fd958b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:24:52 +0000

    substance rate sink foreign popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pressure(force, area):
+        return force / area
