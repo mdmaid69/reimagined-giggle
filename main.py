bc503d51a8584266c36be1be5aeeef026cca03da
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:24:18 +0000

    suckme build float total minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
