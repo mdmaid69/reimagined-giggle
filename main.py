4038a4fbec3c865c78ff2425189f8d93442792be
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:33:52 +0000

    now murder complete experience land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def sort_list(lst):
-        return sorted(lst)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
