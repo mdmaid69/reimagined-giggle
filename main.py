57bd2660b01b45accf0833b6f246e62bdd2c6efd
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:23 +0000

    tip rub respect information happy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
