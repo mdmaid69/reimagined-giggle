a967dc553bb33879ec515ff30a018faa6303bb8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:46:59 +0000

    oil length window Ll fine

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
