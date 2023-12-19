36a3896039cd3c5598d38c50d523d346fb9716ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:37 +0000

    fill recently worse honest holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
