54120db7511834fb35b3eecc4eba6697d40a1b8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:10 +0000

    check work plant so plenty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
