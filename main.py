54b53aaa1c8d28aa41afcc8c330f57fd7109268e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:29 +0000

    difficulty wherever stamp mix wrong

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
