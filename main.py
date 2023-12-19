4a7a8348aa96b9e4e303811196ae8e214726e722
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:46:08 +0000

    nurse noise trip help race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
