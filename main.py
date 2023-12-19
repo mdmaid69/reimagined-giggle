fb0e2e13532acc88b385eb7eecec7631a795f92f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:47 +0000

    argument inquire produce seem breakthrough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
