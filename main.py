5e21dbfebdef58310aaef810d0a7a8b3c566d8c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:44:22 +0000

    565end fair group surprising must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
