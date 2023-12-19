39a17d7495c02c112d02cda7cf26f2311deec020
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:13 +0000

    delicate convenient star conclusion woman

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
