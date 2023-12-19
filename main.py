3315d450b24b6f4fb1ff469536b4d332088ac9cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:28 +0000

    quiet suck interview mood lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
