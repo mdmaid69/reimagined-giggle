bf46cc28d66747a33e23cccafd900a5854a41d49
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:15:55 +0000

    scream thanks gap beginning variety

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
