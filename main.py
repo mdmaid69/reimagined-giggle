fb117f32bc28b5aa587fb73fdf9a10ee2fd194fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:04 +0000

    block along history bag seem

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def reverse_list(lst):
+        return lst[::-1]
