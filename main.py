1a17e6eace41a01b6d43aa4e48f3e31745afa352
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:50 +0000

    proof raise equal brush sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import sys
+def exit_program():
+        sys.exit()
