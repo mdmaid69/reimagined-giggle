0bcd0b76997d296cca1b4459d8b2f81d53e7c7ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:27:55 +0000

    compete come culture happy weekend

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
