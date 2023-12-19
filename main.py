e42af7f567cab2099aa1592df355c7e4bb61971d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:12 +0000

    ocean view standard better empty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
