f2c0e2d8c12195b493762e219dd645bcaffffc83
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:44:51 +0000

    intend score temperature feel trousers

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
