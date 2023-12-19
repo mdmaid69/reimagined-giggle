fc284f9268897a0c5492bc11c8609fd6042bd666
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:45 +0000

    intend protest successful platform watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
