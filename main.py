38fe710449b25edf9a03a3486810f2b57ce78f98
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:28:26 +0000

    noise nice eager announce near

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
