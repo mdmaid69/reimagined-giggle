4b25240e1fb64548e41c67d750ed1c9c1237f13f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:18 +0000

    attempt case tape finally sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
