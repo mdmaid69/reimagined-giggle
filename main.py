2e71615239cf2d09e6d8d8bf8bd4b5d173f8332f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:42 +0000

    back still estimate charge hollow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
