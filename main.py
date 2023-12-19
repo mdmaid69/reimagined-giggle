a3b5543cf2fc5ec066eafe03017bc1b51a9cc8cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:20 +0000

    tradition pronounce bright what bet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def convert_to_binary(n):
+        return bin(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
