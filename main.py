e83ca6f82e8bb2d3e53ae7c74e44c7ad8fac313c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:39 +0000

    underneath market keen 565end joke

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+for i in range(10): print(i)
