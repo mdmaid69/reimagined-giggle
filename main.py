a7305071e291c51f93421850f3565b951e825eaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:16 +0000

    car spend promise march could

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
