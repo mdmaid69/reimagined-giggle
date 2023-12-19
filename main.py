c3c5f9b6bb9fbac0f6893c6630ea8654d495bbd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:35 +0000

    plan only quick hungry study

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def subtract_numbers(x, y):
+        return x - y
