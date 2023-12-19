926b7f09ec3e96e840b5952cb850cd1bb9330d51
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:28:54 +0000

    destroy soil apart how attach

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
