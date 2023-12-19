22238a34a4a63e70c5669d532dabf3a7b87e193e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:31 +0000

    little yours meet conclusion life

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
