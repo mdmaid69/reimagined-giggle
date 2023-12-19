9e99dcef08790b4df1b3ca868c00afa8a4945c02
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:55:59 +0000

    myself mood wash shut slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
