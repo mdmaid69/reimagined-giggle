4ee7766efd8928275aa7c0ccfa01961c5022cba0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:36 +0000

    again achieve concentration further mystery

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
