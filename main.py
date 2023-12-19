bcdff8a1708af48e93da837c1d894a9d0c83471a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:48:39 +0000

    responsible past hurry sharp Ll

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
