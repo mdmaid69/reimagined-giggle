279fc64c7561ffe726dc80412ae5780cac22da5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:40 +0000

    considering report computer whistle 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
