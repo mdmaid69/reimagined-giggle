1a4f4d504a9d65a313c17e3b7bce554d9f74cb8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:32 +0000

    listenfor really ground want hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
