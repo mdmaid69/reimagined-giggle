8c236645f7ff98df843ee53b1ce0dc5f60c29a6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:26 +0000

    normal himself hardly under abroad

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
