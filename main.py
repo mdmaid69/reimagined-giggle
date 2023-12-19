86bdca2b6a432b004cc4c7b907e3968be1fa3a19
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:54 +0000

    tap minute common talk dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
