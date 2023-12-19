973583afeb78f0a182ff58cbb2e7dddd46d77303
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:20 +0000

    hello fast least bring today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def sort_list(lst):
+        return sorted(lst)
