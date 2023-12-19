3e80b59899e779ebd4478d62bf1d5734bd4c162a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:57:49 +0000

    admit maintain yes shoot wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
