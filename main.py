88236714535510c1af1a9ad0852813ab44c30585
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:43 +0000

    terrible sorry to thing national

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
