79b9f2d499fe578147d16419b020cc3eac9e6f1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:58:40 +0000

    broad friend trick see attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
