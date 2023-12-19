97ca43564381208618314bb600f9002b469df14b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:32:10 +0000

    natural dark round spring Yy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
