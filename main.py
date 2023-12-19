9bb18d2d106901bba4d1453ee5af31d737b2d3ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:54 +0000

    ever reasonable label hers throw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
