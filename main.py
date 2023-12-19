be3a42b65726f7da3ec2e01e2fc8036ba9956d68
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:15:22 +0000

    introduce custom control necessary estimate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
