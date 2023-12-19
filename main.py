e505541964e9f90c36018edcb15c133348dbbff0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:28 +0000

    more among sweep operate result

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-x = 10
-y = 20
-print("Sum:", x + y)
+def sort_list(lst):
+        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
