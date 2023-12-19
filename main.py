12c8fbb72fb079627ca189433f69ede3c7bfbe13
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:31 +0000

    long offence disagree scale dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
