09ac44bc863a55aa434cd0c730084c6dfc2aa161
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:34:58 +0000

    collect loan trust call range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
