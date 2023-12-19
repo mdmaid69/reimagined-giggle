bb0d190c04296b751649fe5ca440ad94c527761f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:56:27 +0000

    undo talk kind band off

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
