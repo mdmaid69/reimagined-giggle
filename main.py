0a886fc40bec5b088d602cc1f25a98351a334fc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:14 +0000

    admit pattern woman storm tip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+print(datetime.datetime.now())
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
