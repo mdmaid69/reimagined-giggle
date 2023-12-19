c21b31c85b6e37c3cb89bd9b8aa7e2c41f98bf21
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:46 +0000

    issue feed broad connected run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+for i in range(5):
+        print(i)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
