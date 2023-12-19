ec1949cf8aac8d8e8f9578f0058706e5b42562bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:21:55 +0000

    flat breath dry hollow arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
