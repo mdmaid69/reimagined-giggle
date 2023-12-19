dc970cd7a844a9101b2ebc5bb116e40df7caa43a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:21:10 +0000

    especially along learn constantly you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def find_max(numbers):
-        return max(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
