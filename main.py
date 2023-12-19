b510ce604ed1fe47347b9f197f8edfb70c5cb76c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:04:04 +0000

    ask sink think cover traffic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
