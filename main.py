fc9820411e12a0ac4ba016634ebebe0a9f67521a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:12 +0000

    today space but favour exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
