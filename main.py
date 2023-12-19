6644ce167c389bf452eb5f2856b4bd59945f6813
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:11 +0000

    target total her Ww well

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
