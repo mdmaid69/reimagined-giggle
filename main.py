28ba7ff7b2de99236b886b2b382d11b84b8765de
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:14:53 +0000

    run shall delight after return

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+print("Hello, world!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
