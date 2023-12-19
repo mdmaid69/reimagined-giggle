d5ed16a283babcbedfcc7cc3fe1ec65a05c0fbbe
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:45 +0000

    lose dare once more undo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
