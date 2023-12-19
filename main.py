04a28a2e7b53688d59a84367b79ec3819fcb239d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:07 +0000

    answer black arm less theirs

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
