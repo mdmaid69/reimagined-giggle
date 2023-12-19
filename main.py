7e2fd63226568eadb1445fe8e05ca816827a717f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:55:33 +0000

    low fast very pretend radio

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+for i in range(5):
+        print(i)
