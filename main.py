500d475dbee76dbd1a9f4b90f770978edb781131
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:06 +0000

    container firm tread peace rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
