6cf29b3d1538f31974c4810ab8af21a387bdca49
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:29 +0000

    community pencil yesterday takeoff Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
