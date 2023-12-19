16ed37b3e7ebc59ddbf285a467a61d4079f3dcc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:40 +0000

    behave event lookup ago already

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
