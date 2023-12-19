fd4fc232ce4a80d7b1115fd667e242f041e60a3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:37:02 +0000

    begin government workout depend suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_index(array, item):
+        return array.index(item)
