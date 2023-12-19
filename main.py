bf0a81d9f874da72b1c2e7165d629e4f202ffa77
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:20:50 +0000

    flavour trade feeling complain nonsense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-print(os.getcwd())
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
