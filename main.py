89781d561c09e3807f90012cb0b647658eabef04
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:53 +0000

    hope over group discuss face

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
