1b15ba51f17b05b1f2211bc5d810bc3368ff11e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:44 +0000

    gate water mine practical fall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
