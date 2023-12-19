89909e7eb31833862b8ee4d561e6b25120098e8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:47:19 +0000

    matter walk minimum danger disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
