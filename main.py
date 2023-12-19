6ec2b89c53c308fa1828f3e3b00f05b20145bf0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:34:17 +0000

    track class than gas trick

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
