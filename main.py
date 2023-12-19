a3c582fe6fb688b8137a64e639deceb9f802f991
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:47:32 +0000

    head wander once process see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
