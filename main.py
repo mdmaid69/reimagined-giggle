8eef87d13e4c116e2f35ee4fcd50fbdbf9fac557
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:59:32 +0000

    luck art organization paper yet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
