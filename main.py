f9e1d8703a42c0f625b0300ecd7c7f2cb81ec016
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:42 +0000

    distant modern fierce frequent too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def square_number(x):
+        return x**2
