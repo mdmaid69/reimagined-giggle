51013d5899a2feb89cd51289965121795b44c966
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:11:09 +0000

    month chin up storm thorough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
