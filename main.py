274e37c6f6d48609df5af7c0bc39bef9d84a58fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:17 +0000

    town admire like neat answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
