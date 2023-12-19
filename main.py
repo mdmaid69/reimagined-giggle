692f29bb73b955d66eeaa8d1bcfe29ecfe29cc24
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:50:04 +0000

    guide valuable art together damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
