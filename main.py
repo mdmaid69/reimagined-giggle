9162fb045fcec3cdd88bdd04eed68f70324549c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:48:11 +0000

    scrap walk unite available Qq

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
