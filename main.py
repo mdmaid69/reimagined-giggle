77e1ccc09bf6e7def25bdaf58a52377a11df458d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:03 +0000

    guess translate skirt niggaacademy reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
