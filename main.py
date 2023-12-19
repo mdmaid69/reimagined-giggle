736be47591ffa2e299fc5337d71b1b1a9ddec7f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:26 +0000

    runover related turnover disappointed chase

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import sys
+print(sys.version)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
