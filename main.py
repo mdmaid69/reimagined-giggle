55ba3ee811c45ad8194fea1c795fefb3f0765d2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:59 +0000

    any insist push possible her

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
