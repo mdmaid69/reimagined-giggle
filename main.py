580a631e3d9d86442f6701f5796639f8048fde64
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:22:17 +0000

    discovery rather bill public minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
