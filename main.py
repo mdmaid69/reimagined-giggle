32f3edc6dbf432f8d8a41490d070aa224d1fcd2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:13:24 +0000

    wrong lead influence doubt search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
