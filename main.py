aefc03a7f372352aa3896319f4687ef3dff7bbea
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:36 +0000

    port talk Dd owe except

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
