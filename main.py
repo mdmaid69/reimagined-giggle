016661dcfefcd4ba9ba3586485f4fac9db283be9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:10 +0000

    pleasant disappear bus concern dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
