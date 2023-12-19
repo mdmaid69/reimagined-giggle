05714d89fd768c459e84c9d1f2da0cb30c0faf85
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:44:58 +0000

    no like powerful zero mystery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
