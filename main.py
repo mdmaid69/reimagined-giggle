12cac799025348a2665ac38be1b33c48d79d8122
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:29 +0000

    contrast performance excellent now instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
