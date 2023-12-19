a18a6c528dbf9254497cf1c83ff7c34741454c23
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:14 +0000

    properly clear meal imagine level

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
