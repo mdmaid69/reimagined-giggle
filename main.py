fa75ffa6bd2b87a69c2ce31d1bc4d9bc6d3b36c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:35 +0000

    sure consider pinch spring debt

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
