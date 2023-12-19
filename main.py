8f01f2444e8501b6c885974ff3f954e763af5002
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:13:09 +0000

    callat tax please earth you

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
