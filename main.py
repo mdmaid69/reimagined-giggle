3d3aabacecdcb942fa89668ae4f00f5effff9561
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:43 +0000

    success bunch basic light best

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def roll_die():
+        return random.randint(1, 6)
