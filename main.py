66653cfb851436d9b99fd003c430b1e89b6121a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:19:19 +0000

    dance majority detail second just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
