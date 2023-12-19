c813c45df36d017f7079825c945d453da97f6b6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:30:22 +0000

    after plan second talkinto better

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_work(force, distance):
-        return force * distance
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
