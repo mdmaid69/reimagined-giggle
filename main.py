6a9b6428e0b597a3e43b67c1cf39195580dfa6da
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:17 +0000

    book quantity switch honest available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
