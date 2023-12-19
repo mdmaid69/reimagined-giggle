41515c47d6bb0ed36943abd8f89607f5a368da86
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:56 +0000

    everyone thick I remark blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
