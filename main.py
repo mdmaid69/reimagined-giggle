8fbc870acae362573e5bc9e955d3da9306a661ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:36 +0000

    profit tune rest figure strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-print(time.time())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import getpass
+def get_username():
+        return getpass.getuser()
