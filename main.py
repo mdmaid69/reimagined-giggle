6a90480b4a127661b9a06e5b37c7a82607f1a04f
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:12:21 +0000

    possibility uncertain foreign conversation freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
