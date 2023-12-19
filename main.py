6ecf20e2c0d3cf63ac2b11f500d940d5606f13ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:02 +0000

    serve lucky lie 566end ignore

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
