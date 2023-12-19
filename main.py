7fec309e4f99ea17b66bfc2e6859a8f888f8abc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:01:37 +0000

    this preparation another attention sad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
