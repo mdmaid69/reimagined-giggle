0667fec10af94643b3c14aa955628d5781daeb39
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:52:46 +0000

    recent often copy region stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
