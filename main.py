53b086c287b22c57200c62f95aa40d19cf8650cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:32:57 +0000

    stand at coat limit look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
