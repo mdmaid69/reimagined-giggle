9cb202f31c1cfe5a9e936091cdae0551360f19dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:18:00 +0000

    intend want mine positive shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
