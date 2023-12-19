f9f588a28d5b7f9a4a6e2096e322c4fc165f07fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:30 +0000

    line comeoff centre turn catch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
