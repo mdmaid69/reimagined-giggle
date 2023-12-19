57077ca219a2bd1ca8aab506a066001e490ebe59
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:39 +0000

    congratulate occasional shout properly reject

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
