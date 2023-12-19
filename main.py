cb2939d861a0c7528477a4fa7670ec9d0e61dafe
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:32 +0000

    exploration this phone guess public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import datetime
+def get_today_date():
+        return datetime.date.today()
