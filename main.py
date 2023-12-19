3e4f60ccf5062452686da17c565b4bb3f61c0b5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:49 +0000

    like declaration eye right priority

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
