88e5d2dcb88a5d90a0b98b42fb2fa0a009af6e5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:20 +0000

    waste body argument card hurt

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
