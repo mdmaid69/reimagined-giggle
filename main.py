47f795735301474c7e8b4234b30cb929da35f06c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:05:26 +0000

    target thing person slightly hot

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
