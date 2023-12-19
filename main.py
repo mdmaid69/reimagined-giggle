d719c72bbe1727c635891692985b80c0ed0b127b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:18:23 +0000

    protest obvious forward odd OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
