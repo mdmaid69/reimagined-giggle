70852906bc651f416f50bac8f5732c77f78a67ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:52 +0000

    genuine accident quarter furniture board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
