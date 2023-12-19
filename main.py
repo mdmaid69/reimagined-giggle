ded38ccf58cd251b63c1f2024c98ade7f6bfc7e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:37:46 +0000

    Cc empty thank brave listen

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+print(math.pi)
