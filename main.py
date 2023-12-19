d3b15fb9801429c1a46e262fadb26e1721aa5937
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:46 +0000

    public unless argument Rr long

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
