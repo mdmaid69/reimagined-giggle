7246097af0496c07288f9271331a45cff4229d6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:33 +0000

    sea protest break idea distant

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
