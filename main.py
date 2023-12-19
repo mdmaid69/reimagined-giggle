4ccf7ce6ad7336c87824518b6866e4a362215468
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:10 +0000

    happy copy direct street cold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
