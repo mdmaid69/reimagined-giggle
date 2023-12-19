e1945a695b4e23e391e82c064b95271f1abb98e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:22:23 +0000

    side holdup tune compare law

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
