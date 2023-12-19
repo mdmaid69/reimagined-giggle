660acf3e63710be74cee6d8d85e48b00ece8bacf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:40 +0000

    rush half friendly unlucky sing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
