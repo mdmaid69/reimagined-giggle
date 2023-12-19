81c39204f946ecd64233d4e8b17bd430bccb62aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:54:10 +0000

    second react bush over marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
