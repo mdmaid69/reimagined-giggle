f874b1d69ddff86ea5a996626dbd7293dcb88ce0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:35 +0000

    flight raise terrible angry discuss

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
+def subtract_numbers(x, y):
+        return x - y
