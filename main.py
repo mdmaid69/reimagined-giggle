b016bb03e37129ef232b49f40de0c351e7c05743
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:55 +0000

    fight interrupt read loan possibility

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
