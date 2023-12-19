61ccae7385107bd8417e68d08bd6783dcec47e01
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:07:59 +0000

    board pride against flavour rough

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def remove_duplicates(lst):
-        return list(set(lst))
+for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
