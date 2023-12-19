f5dc34c5c2b719533e88804eaebcb0ee18e0fd59
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:17 +0000

    character touch thanks bye position

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_list(array):
+        return array.tolist()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
