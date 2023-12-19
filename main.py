90ca82a88d405edb88197a4033b47da047ffb3a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:00 +0000

    ordinary relief business touch block

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
