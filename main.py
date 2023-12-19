347130485a38c45658e200babdb9abf3d67fcabe
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:57 +0000

    partner where say workout snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
