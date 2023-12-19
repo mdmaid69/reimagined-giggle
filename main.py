418aec42e857f150c2167327b09c4e0e1f5475da
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:45:02 +0000

    study ignore refer smile pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
