e63fed044b32ad6e933ab30ff247c12b231d7c6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:23 +0000

    smart scale pity hollow sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_odd(n):
-        return n % 2 != 0
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
