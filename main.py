b4e658e8c68e55a5711a78b966d5e50be94d67b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:10 +0000

    direction comment quick weekend Tt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def add_numbers(x, y):
-        return x + y
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
