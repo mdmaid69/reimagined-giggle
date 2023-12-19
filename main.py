76bc0d4c71c87de686955c8f328371f170ee15ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:27 +0000

    record attract treat question rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
