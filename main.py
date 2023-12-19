a39b5d9db54f3feb6ea997d80d0c7ffe6195637e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:21:50 +0000

    mine change suggest impatient quit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def print_python_version():
+        return sys.version
