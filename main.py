7cc9b863bfd8872e1e62d9abeb3615c9f4a6317d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:30:18 +0000

    majority fortunate should tip long

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+print(os.getcwd())
+def subtract_numbers(x, y):
+        return x - y
