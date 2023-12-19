4fbb715b610074224c20d60db95c872e6d8add79
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:13 +0000

    hear suckme thanks save disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import tensorflow as tf
+print(tf.__version__)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
