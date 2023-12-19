d2da789b7a82f1451becdf4a1f11fa82a7a41d3a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:24:54 +0000

    concentration foreign practise mess float

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
