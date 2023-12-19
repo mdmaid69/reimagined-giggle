35eb2cb2ede14e901743e967b47a86cdac83bad1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:05:19 +0000

    afternoon warn demonstrate feature learn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
