78d6e06ab30be766d228467962a0f058b568b2be
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:57 +0000

    comeround handle lonely matter secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
