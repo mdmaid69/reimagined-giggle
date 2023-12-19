c364ef5096117a8925eb36bbcc665d1a92fe5657
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:45 +0000

    fear zero whichever minute moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
