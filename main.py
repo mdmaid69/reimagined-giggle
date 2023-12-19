c66d89cb26abaea677ee4dd573d4f31b37094eda
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:39 +0000

    furniture sensitive road opinion drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
