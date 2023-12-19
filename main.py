dd62c5fb981825965f3dbe4d4767df937def2e6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:04:36 +0000

    concentrate content concern position hello

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
