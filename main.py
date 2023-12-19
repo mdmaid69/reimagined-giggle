bfece4b42e208204a7b3d738f6537e2c04cb5369
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:54 +0000

    crowd mainly pure distant weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
