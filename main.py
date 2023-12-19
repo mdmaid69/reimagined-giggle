e4524ee94600e5cfa4ea64286f2f10a1920b8bcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:28:44 +0000

    become identify circle that fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
