6bf0feed635a467ac4cfceeb432c046ffa725bdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:18:58 +0000

    people bush fear away whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def remove_from_array(array, item):
+        array.remove(item)
