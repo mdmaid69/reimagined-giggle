142dfaa4cacad606b3b696276f7c0b07699e5e8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:07 +0000

    yours yes sharp rescue ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
