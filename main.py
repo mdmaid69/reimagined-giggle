1e5868edf7aaf14356289dbf51600d1ba9388e70
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:51:40 +0000

    about however unkind badly by

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
