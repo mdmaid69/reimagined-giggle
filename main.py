f4a056cf2b1d6f507cb92bd9945f0e490ba554d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:34:50 +0000

    subject sell intend correct bringforward

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
