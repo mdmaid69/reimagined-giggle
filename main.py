3ce5ff62fb546bf594bae1ab800f407700ab0dab
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:56 +0000

    choice comfortable show prepare fact

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
