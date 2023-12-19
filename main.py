5afabf55fe71190988c57c72e56bd6747d9e10cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:49 +0000

    change joke shall knock with

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def divide_numbers(x, y):
+        return x / y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
