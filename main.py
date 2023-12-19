cd9c4a143a4fda53813d3a0b5ff344ac98928f5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:34:35 +0000

    voice underneath runaway knock geton

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
