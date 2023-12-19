8e134fa8ea3f38eee3d2d50d06789a9155485899
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:40 +0000

    address description rest partner otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
