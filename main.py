19985432c10271e66c70f8d1f6d04764990b5ddc
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:38 +0000

    give where content square great

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
