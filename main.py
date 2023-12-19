bef5b97e388e5dc5fb4f8562501c12d270f8e935
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:55 +0000

    sweep politics purpose relation apart

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_hex(n):
+        return hex(n)
