f75cc7b02ef5eb81a6e20bcc2d534ad4dfa2ba9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:16 +0000

    combine funny successful point practise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
