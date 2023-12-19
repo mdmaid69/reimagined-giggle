6cca33fdba2ad93f37ca97bae223cb9c7a5502a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:37 +0000

    minimum naturally goback(to) wonder drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
