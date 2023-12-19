e262153e509fc8591c970c9a22443606eaeea47d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:39 +0000

    birthday in bored priority relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
