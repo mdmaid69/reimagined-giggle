2c9515c1ce3a4365b6dd4f3daee066d617aaee0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:35:01 +0000

    wait card interest still various

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_itemsize(array):
+        return array.itemsize
