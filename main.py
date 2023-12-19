ba9405b67d5fc745ae86d7ed242e63d0dbf789a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:23:47 +0000

    behind necessary repeat reply smart

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
