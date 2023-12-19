cae8fd1c2db73c3ae4062b17533f1af2b9d83846
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:27 +0000

    treatment scrape it already fierce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
