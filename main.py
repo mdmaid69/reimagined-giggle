b6713b477fe5ebf0e8be9f10a101217df56f1d83
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:27 +0000

    fast getback conclusion check keepout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
