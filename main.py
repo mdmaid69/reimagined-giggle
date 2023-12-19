f189163083b3a859942ef87bdba64c583feecddd
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:06:10 +0000

    risk damage whom autumn instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
