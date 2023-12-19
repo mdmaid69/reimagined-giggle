5d9dee9050d27968a96e1388057bf7835fc9bec1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:17 +0000

    someone smile frame disgusting politics

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def find_union(list1, list2):
         return set(list1) | set(list2)
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
