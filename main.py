2e3a0e7ac7276a225ef56961cf70ef80a6b39784
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:58:49 +0000

    seem too friendly base aim

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello, world!")
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_min(numbers):
+        return min(numbers)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
