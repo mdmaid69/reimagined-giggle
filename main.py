1fbcf9b8886cdff61ac3ae7c5ffadef28ce9d25d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:59 +0000

    solve private young square contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
