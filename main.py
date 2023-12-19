9817ae32c55b4da9bdfdf19303bbb48eb5f11267
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:13 +0000

    record weekend callat slowly order

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
