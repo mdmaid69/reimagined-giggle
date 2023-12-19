d9d5cb50322032fbf5f2c4e76152c9f0f8f91f93
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:56 +0000

    in instrument willing present dangerous

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
