9ff4aa9c9dc6c8785c06d979e5e3adc7069a1254
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:13 +0000

    recover why figure record refuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def square_number(x):
+        return x**2
