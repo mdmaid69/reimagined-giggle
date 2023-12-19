d08309d9316ba134bd5ef55039b09164b8a1789a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:11:12 +0000

    band sensible there start fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
