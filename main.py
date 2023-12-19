59b9999095749c3acb7a40dfc1ee6ba8df762f8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:14 +0000

    dinner fast matter quit determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
