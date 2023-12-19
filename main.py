f59af79ffa65d17683025e3924c72616bb3351ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:21 +0000

    smart paper because responsible benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
