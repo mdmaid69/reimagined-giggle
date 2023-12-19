3440895776a3ce76841bd9511908b31468af3ecb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:26:58 +0000

    tonight vision stare dance do

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+from collections import Counter
+print(Counter("hello world"))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
