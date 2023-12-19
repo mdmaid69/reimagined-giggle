60dc715582988e5c874b5b9b75f11efed1976233
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:32:12 +0000

    blame float challenge shop responsibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_max(numbers):
+        return max(numbers)
