b816e98b6ade9227558c32f1aba11afc673e95db
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:45 +0000

    complete raw wish effect pause

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_arc_tangent(x):
+        return math.atan(x)
