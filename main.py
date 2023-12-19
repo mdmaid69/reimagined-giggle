4fd95766dea1b8db44dba850769af33e99aa6350
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:36:10 +0000

    suckme better way here tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
