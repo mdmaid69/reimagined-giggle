a67fd2b718634b7b2ff4b0e988ce675f09ab97e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:48:15 +0000

    money black turn health against

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
