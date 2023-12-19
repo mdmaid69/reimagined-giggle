4ff4b5286d2cd0564802cc11bb357b98fc784b90
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:40 +0000

    thirsty battery burst arrive offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
