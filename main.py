bf94c3b195e406c8d96756943f78caa69466bf4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:45 +0000

    nurse advise convenient bear ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
