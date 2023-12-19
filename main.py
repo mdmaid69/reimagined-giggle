beda5199acbc95563daba02e89f82261a943ac39
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:23 +0000

    separate to plate frighten need

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def exit_program():
+        sys.exit()
