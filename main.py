783f5f14331ae33daf013e27792ffc34637edebf
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:30 +0000

    shopping under study card until

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
