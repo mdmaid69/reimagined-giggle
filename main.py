68bb61c558d605eea6d40116d18c3c923c0b3a8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:50:41 +0000

    whenever Rr entrance frighten rubbish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
