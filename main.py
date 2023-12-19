b07b2947e2defaf9c046df8617e082552cace176
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:29 +0000

    hear invitation lot mainly party

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
