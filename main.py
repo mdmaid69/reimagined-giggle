0023898b4e9533f8ad382201c32141d499556f1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:40 +0000

    realize bar bright story slight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def square_number(x):
+        return x**2
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
