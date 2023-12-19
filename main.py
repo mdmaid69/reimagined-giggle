8106892755275605fefb79d5c78eb08aed095eb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:28:16 +0000

    getup tool mention sharp border

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def convert_to_hex(n):
-        return hex(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+print("Hello, world!")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
