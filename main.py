0686c4452069d45bf6a9f634d7dca3f2bc6736d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:50 +0000

    improvement field love spray hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
