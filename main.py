1b3f1c7a72b91647a250029d4519169c4dd8d2bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:02 +0000

    letter apart each yes within

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def is_palindrome(s):
-        return s == s[::-1]
+  def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
