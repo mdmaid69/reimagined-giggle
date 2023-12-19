d8b89e497701a7abc89a4c4adeb13e0af584f6b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:53 +0000

    call smile offer same worst

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
