7f8ef8bc00217811bb8c267cceb6c7393b48957b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:51 +0000

    yours funny expect develop gather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+x = 10
+y = 20
+print("Sum:", x + y)
