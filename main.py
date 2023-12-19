c013234bc20cfa6083a63cfb452ceca0131d9427
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:43:17 +0000

    simple lesson sometimes win constantly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def subtract_numbers(x, y):
-        return x - y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import sys
+print(sys.version)
