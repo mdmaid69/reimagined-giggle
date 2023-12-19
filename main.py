4ec3e84d8faeb93b000db76c019713dead1398cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:03:19 +0000

    branch none official manner winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def create_counter():
+        return collections.Counter()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
