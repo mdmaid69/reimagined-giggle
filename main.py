531f4502b14f4381748e444af2e2ac38895fb0ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:50 +0000

    left taste usual or interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
