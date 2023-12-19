e7e077cbc1ff45fb4329947e8a37702885861d4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:19:59 +0000

    ordinary goon quit rate belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
