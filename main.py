c5b63dda5f57c8f3850c7a2481fb8d59697a26ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:06:11 +0000

    quarter back suck will comfort

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
