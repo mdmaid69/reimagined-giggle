0de41064cc42d3bf76e0051afef86fb7935e08a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:26:17 +0000

    then 567enemy phone bunch table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_python_version():
-        return platform.python_version()
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_as_bool(array):
+        return bool(array)
