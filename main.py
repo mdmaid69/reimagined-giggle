984f47de9a3e0dff6739a3cbbbd218b22b33c1f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:54 +0000

    kill schedule proof tell sister

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
