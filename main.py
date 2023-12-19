99b3239c26c2415613248137bdc1979e7bb1836f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:46:29 +0000

    number rarely weather material loss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_absolute_value(x):
+        return math.fabs(x)
