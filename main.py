c30c4cbf08c6b390009d70a4661ecbd2be43e31c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:26:09 +0000

    that stiff operation ashamed slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def convert_to_binary(n):
+        return bin(n)
