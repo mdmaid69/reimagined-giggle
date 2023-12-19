689c2ae3bffaddcab9d31a1155c362b8a75d9118
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:48 +0000

    cook clothes stand heavy communicate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
