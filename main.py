b2f11d747e433187d08d858ff0e029b5267d570b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:19:27 +0000

    impossible trip Oo attitude exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_power(work, time):
+        return work / time
+import sys
+def exit_program():
+        sys.exit()
