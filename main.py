7656b88f1f1c4dc06d69abc5f19d27a68d3e95b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:21 +0000

    special ground motion loose tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
