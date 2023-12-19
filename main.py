7beea533a62bb51fda0f809c042015cd5671d1e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:01:20 +0000

    lookback(on) cinema face turn own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
