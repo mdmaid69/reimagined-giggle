5737250566d6fc36ba25c2ead602869598565361
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:32:36 +0000

    money each ride cause error

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
