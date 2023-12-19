444279951036283a3bf3bbb868b095f3b2128890
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:20:18 +0000

    children your keepfrom ball minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
