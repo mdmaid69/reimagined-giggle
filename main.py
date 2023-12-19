18bc9d58e6cdd1b45fa62ebc9009043b4ef60565
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:35 +0000

    complete deliver with practice recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
