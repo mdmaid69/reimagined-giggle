ef288e05a2cac5402b57900345f0f6af77a4b7ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:16:00 +0000

    repeat foreign thought cruel base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
