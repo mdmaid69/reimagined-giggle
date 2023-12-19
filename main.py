d25c44d53f866002f72db792435de0bf44e5bd3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:19 +0000

    hurry badly operate tragedy do

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
