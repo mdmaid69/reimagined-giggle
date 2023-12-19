6a0abe9a6813fbb73547c9c6af34679338b72dc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:00 +0000

    your stage myself check damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
