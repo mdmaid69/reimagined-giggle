3484d2b073daf48c65e3d6d569580615ad0c325e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:55 +0000

    tough colour burn story language

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+print(sum(range(10)))
+import json
+print(json.dumps({"name": "John", "age": 30}))
