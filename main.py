68c09edf0cb19e0aafa402e017a97da69fb7abab
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:54 +0000

    twist whatever possibly hide deal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
