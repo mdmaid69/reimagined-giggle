d46030efe246d5b3cb921f681dcf25ade276ae49
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:39 +0000

    steady talk themselves dry official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
