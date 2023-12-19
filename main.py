f8bac9fcc9a90cf244fd45d362dd9487716d5b72
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:42 +0000

    welcome giveout exist unfair eye

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
