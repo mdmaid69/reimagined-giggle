d71bda39ca6a6dc5e22c9ffa7e9b23a7960bb17d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:27:18 +0000

    whatever clever difficulty danger announce

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def get_array_item(array, i):
+        return array[i]
+def remove_duplicates(lst):
+        return list(set(lst))
