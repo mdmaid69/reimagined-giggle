492eb59d9fd1fe9f8faa158cc0fe39027cfc8e48
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:23:49 +0000

    keepfrom famous example in partner

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
