4ad1739b9eae8ee43104c69e4cfd93c9558127bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:14 +0000

    ground true natural although pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
