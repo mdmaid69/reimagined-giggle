508233856a5cca97f5e7f80896c37ea6268d94c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:23:42 +0000

    account 572enter fear afraid music

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def cube_number(x):
+        return x**3
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
