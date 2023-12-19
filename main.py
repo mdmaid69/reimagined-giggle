eae62684b926561575de8971512609be990d99d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:58:16 +0000

    chin release goup excited mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
