eb14e4bd31c5ca25ac030120d92a214a37b56416
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:52 +0000

    past they lookback(on) rush here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
