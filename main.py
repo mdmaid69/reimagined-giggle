f929c618a8cf53e1640a50a9dd59484fbcffbce9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:50 +0000

    memory lie wipe getout issue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_min(lst):
+        return min(lst)
