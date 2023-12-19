f85872e6fdc425dd3dd24601b581013c8e56e6cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:38:40 +0000

    fortunate good suckme prove comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def add_numbers(a, b):
+        return a + b
