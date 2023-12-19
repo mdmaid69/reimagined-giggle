7249d3627be7a5efba82d2e776a27f25d3f7c91c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:26:50 +0000

    address holdon bear petrol contain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
