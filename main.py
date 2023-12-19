f73cb6ac94fd13418b92e9febcad5a472297449f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:24:50 +0000

    disapprove hot comment way mother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
