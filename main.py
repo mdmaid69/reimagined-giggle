b9016d1e340f4943362299b979c8b884bbfc9239
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:15 +0000

    air guess takeaway date today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_floor(x):
+        return math.floor(x)
