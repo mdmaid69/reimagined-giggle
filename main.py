e82073ebb85f6e3cf4f602cf37673525dd60d259
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:05:04 +0000

    matter shrink storm first organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
