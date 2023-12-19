2cbf4a540b72f16b87e3d67db275b5eb775cea29
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:29:45 +0000

    achieve bad dangerous sleep confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
