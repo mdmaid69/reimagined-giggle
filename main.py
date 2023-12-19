128ceb856583eab097ce090e9ba082fc4a334f10
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:36:10 +0000

    recommend awake trust shop create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
