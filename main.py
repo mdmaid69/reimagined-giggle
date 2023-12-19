af21fa0c06695a26bc7478757db3b3ba1d2e219a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:09 +0000

    good reply butter traffic criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
