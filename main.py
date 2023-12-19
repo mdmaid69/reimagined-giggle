58eb5c10495f07d5249d3ab424f51fae529e8e3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:30 +0000

    pity like for region study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
