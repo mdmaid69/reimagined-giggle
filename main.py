afec794fe245cbf67c49dcfda24effd9e1f444eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:15:17 +0000

    praise food steady breakfast heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def reverse_list(lst):
+        return lst[::-1]
