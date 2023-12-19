11e733ca4ad71cf85d7891030de00166c6fddc16
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:13 +0000

    control win possibly suit stretch

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
