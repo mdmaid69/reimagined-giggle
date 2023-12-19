64cbb88310235d7fb93acef4491032b3a7f29599
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:59 +0000

    profit shut custom high artificial

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
