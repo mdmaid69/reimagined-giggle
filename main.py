b637e325d6a450dfdf5b9222ec08dfb9a5bcf340
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:01:46 +0000

    safe march vote every rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
