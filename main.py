f5bee9d9d512b069364228ae475b21a1bd105ce3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:09 +0000

    address side if embarrassed mood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
