24d0a936ec6b23923e8fbac40328ab7f2dbd3023
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:48:19 +0000

    child underneath drive that freeze

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
