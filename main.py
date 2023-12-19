3191b42b9fba25703bb6c0fd3da6f8a367b059c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:15 +0000

    confused sure hope closed wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
