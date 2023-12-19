0017f8757a62cbe4be0d82c4ac6ca96c0d849107
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:26:50 +0000

    trust from comeout willing pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
