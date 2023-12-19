115f4221ffd6b91fd6b0910d90ac818597635bdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:29 +0000

    divide petrol communication blind phone

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+x = 10
+y = 20
+print("Sum:", x + y)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
