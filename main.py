5e5eea9e4838f55837a28c277834195eee69cdc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:24 +0000

    talkinto Ff someone cry crowd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
