b14906880cf2daafa2099f9f5a895d953787595b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:07 +0000

    point load rubbish study makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_typecode(array):
-        return array.typecode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
