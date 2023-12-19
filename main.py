4cf7e651e01a70ddff116aa4e4bbd0bcb38f0e1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:24 +0000

    cruel turnup perfect muscle warn

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def append_to_array(array, item):
+        array.append(item)
