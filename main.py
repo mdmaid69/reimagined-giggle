dd343f7d40f15c22eee99a8c1700db056dea2263
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:46:46 +0000

    completely cheque cheap short roof

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
