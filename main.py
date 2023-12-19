4ded002dae13ebb84ccce48ec2ce84ebe01e6dd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:38 +0000

    touch borrow private way grade

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
