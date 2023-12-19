fe413b80882c4a1b194c715b10e8aa21ddd24ec6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:22 +0000

    happen depend here touch zero

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
