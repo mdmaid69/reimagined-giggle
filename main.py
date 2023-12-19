eb58a2d80e812fb93d3ff0ca20807d71331aad97
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:06 +0000

    related plan concentrate group mention

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
