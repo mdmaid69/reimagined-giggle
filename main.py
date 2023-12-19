70bbfd0f0eaa25f85a7b12fe4608b45e7c3488ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:32:48 +0000

    suffer middle nigga there blame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
