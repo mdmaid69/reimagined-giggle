029c74fe363413f337ffbf2645e4c290a74d07a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:30:40 +0000

    science suggest among Rr safe

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
