13c30d308ad987f04f14e091b1442046d78aae1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:33 +0000

    whistle game operation aim round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
