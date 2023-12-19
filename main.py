3b27ecc285c34f94d8fb4bb42745eab6afbaf969
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:36:40 +0000

    match convenient frightened bit hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
