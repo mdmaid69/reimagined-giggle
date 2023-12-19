e5256ad96330ec983248a0ae6454f40aef6cef2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:30:41 +0000

    themselves call when me file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
