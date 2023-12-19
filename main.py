35cfb7238d2f65d83d3360ba62fbdae83331522d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:23 +0000

    impatient suppose activity culture Yy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-print(sum(range(10)))
+def reverse_string(s):
+        return s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
