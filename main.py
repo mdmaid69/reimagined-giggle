93a46d78e664a250e6889fbe9e792c070653af09
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:34:07 +0000

    ball exactly foot doubt experience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
