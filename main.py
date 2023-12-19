b590dd800fb47e56e3bd232f248950cf4a30e5c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:43 +0000

    back round embarrassed shall bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_length(array):
+        return len(array)
