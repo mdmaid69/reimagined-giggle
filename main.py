a699a385ad4fb185258355f36cf4ee0a01ffe3ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:15 +0000

    react train arrive swing weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
