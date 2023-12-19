eb6cfb23e62244e5f7e1ae70b561ecfec15ea540
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:04:06 +0000

    tone hurry form stand deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
