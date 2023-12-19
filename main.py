e4a966e67c8a07e10ec92b7b98e79cb7685b8f03
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:14:40 +0000

    usual opportunity fortunately accordingto actually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_speed(distance, time):
+        return distance / time
