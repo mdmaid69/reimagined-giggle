5e5a59cbfe4dc21758f3402f1f61d80765d629a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:38 +0000

    rob true have thick breakup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
