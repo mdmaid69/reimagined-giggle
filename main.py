a5cf3442a83b4544db582ea4c5770327a028ecc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:01 +0000

    only lunch bite protest see

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
