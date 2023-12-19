7290cca132654c728794e58f16ee86568bee9044
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:18 +0000

    centre past likely trust change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
