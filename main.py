da9d1c736ef0b8aa9d15f04b88a418e48811590e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:16:13 +0000

    catch major nerve pattern sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
