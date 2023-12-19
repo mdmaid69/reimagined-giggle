8466057a692862ffe712ce64a5e9e2afc7e05b2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:54 +0000

    question print guess correct cheek

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
