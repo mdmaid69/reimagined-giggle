ead3dbd18c79a83ff91f432a11e4cc5b2f628c2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:17:38 +0000

    especially ago yet loss before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
