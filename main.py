32e4c0f3f1e29f7d3af55dc4aea9879682f229b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:16 +0000

    offence cool centre deal breakdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
