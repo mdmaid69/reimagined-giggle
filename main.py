48b6cfaa0dea6ba66309588f0d4b405e57163f8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:35 +0000

    food mass 570enjoyable base even

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
