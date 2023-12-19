d8c03b8f72ac0245d2b6c08a678f8476d7e9f357
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:33 +0000

    convenient attempt necessary them beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
