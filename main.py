cd2cab22aed4801a40d6a8af9aa99b521a98a082
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:56 +0000

    praise whenever problem absolutely might

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
