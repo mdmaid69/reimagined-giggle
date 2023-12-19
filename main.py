e7f671bc186ff5a4ed09e47caa827e5bef327158
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:59 +0000

    small wind about famous round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
