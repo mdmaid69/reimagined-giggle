6855969694bd203ca95e3ee6c7e5ec3c7e165f52
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:34 +0000

    fly success brief bread laugh

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
