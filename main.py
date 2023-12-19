5309741a6da60a2f53ca4d917608688c4353fe1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:42:46 +0000

    home now town calmly equipment

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
 import collections
 def create_user_dict():
         return collections.UserDict()
-print([x**2 for x in range(10)])
