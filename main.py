cf5a562bd3fd396e67bf7e9017f22b2c189cb6b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:26 +0000

    ball guilty geton to square

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_area_circle(r):
+        return 3.14 * r**2
