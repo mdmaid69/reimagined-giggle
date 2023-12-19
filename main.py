e56f1b37f03cb4711ddeb0abbbb6500d55ee4768
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:42 +0000

    kill same woman hit Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
