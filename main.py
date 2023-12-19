48e0788986a00dc8b69be9e946caba1284999c09
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:25:36 +0000

    stage thorough score benefit food

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
