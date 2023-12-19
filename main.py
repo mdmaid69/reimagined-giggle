0aa5bacdd781be1ed66b99625f6e418ef59a70e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:27 +0000

    let male petrol distance care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import sys
+def print_python_version():
+        return sys.version
