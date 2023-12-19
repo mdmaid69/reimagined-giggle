cea9fde1dddc5df9c47d04d64fa3b645fa31feb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:52:05 +0000

    damage trial refuse sorry race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_work(force, distance):
+        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
