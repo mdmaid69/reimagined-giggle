223bc0acc3430ff267c31736d2442ee4ef9cdc68
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:11:23 +0000

    burn equal recover careful nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
