ee39de722a13479526ed4fd7679ea8a3b29c495a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:23 +0000

    lead use patient standard exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
