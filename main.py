d6fdfc1f377f4efbd43bd980cdceeaba2109a359
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:10:20 +0000

    exactly amount 571enough last there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_sine(x):
+        return math.sin(x)
