2298eae817a9a92186192f771b887fcd804a00a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:34:02 +0000

    before colour act open goback(to)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
+def count_characters(sentence):
+        return len(sentence)
