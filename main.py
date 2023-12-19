0ebfc871c76cfd05bd3adeefb56edf3dca6d2a8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:39:19 +0000

    trial minimum work inform shrink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
