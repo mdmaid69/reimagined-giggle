c8f9566146debbf096a11c4759bd61561811f705
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:09 +0000

    shock wind nothing steam house

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+name = "Python"
+print("Hello,", name)
