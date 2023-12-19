9a4bac0ac313e4db15b3300d63309c4ae84ddd9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:22 +0000

    name burst main gift communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
