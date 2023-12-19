0d98105ac4db629a9a038afc7a98e0275a1206da
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:58:14 +0000

    think press mean boat tradition

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def square_number(x):
+        return x**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
