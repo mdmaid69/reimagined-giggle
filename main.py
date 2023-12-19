da13d63be0a4b4f1f0d0271a47692450adfd666b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:52 +0000

    even anywayanyhow discovery arrival fold

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
