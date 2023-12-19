e520bee1813eb1864344fc303b9951627d677803
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:19 +0000

    operation politics married meanwhile possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
