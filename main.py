3b6188e9ef8ef0bcdfb283dece7ea8d5cea9630f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:41:54 +0000

    on complaint sit around confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
