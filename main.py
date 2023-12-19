1fd549bb37763cba2833e269f2b281319984d0e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:37 +0000

    about generally neither process Rr

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
