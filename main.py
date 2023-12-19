9761cd77443db43a64226b595cd021e4d75e03e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:16:08 +0000

    during trade dig possibly food

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import platform
-def get_os_info():
-        return platform.uname()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
