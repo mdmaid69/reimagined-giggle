a509c37569d4cfdceade10470d7a0ce3e215a924
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:36 +0000

    shame share mass connected carry

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
