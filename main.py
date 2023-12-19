b4b71d3c0403b54ab6d99bc22eaa1de1edad5a36
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:06 +0000

    opposite mysterious correction level discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
