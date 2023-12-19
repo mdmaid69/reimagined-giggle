af6d9830b704bd2da56b9e4fe3e992bea154fee2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:34 +0000

    autumn bringup remain matter thank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def square_number(x):
-        return x**2
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
