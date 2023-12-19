a27e32039f974439bc325f385d6b61064a38de63
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:10 +0000

    length trick they tread ask

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(numbers):
-        return min(numbers)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
