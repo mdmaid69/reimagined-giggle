9c2c6aefb1b942198b784b90935f11a613fb5c56
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:19 +0000

    room below honest theirs worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_stack():
+        return collections.deque()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
