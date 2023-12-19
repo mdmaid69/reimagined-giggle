549be9a1b64912d8099e136bfe49d6aee3a4b59f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:43 +0000

    lower train him happen anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_binary(n):
-        return bin(n)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
