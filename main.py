7ca585d0146a5acf7590eb9fc1eae8f5bfe219b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:38 +0000

    proper exact mass wide outside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
