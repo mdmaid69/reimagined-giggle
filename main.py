82bbac8214b245190175f258cc0306b16dd58ac2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:58 +0000

    join half tend pipe again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def count_characters(sentence):
-        return len(sentence)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
