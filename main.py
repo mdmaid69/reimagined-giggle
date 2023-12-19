1b6a54f47ac1ae2996eef9a05ba81738b5264375
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:40 +0000

    demand task dear definite price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
