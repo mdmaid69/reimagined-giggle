1ed52605033e1c0d382be9024d9529aa97feb0dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:27 +0000

    Bb direct ride sore boring

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
