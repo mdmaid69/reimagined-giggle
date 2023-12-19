2929f21d6ebda351fee7615bef5944a431cfb659
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:17 +0000

    wash treat absolutely lock plenty

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
