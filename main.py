fea9c6ae8428b877e8475980488b9d8b60d89636
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:47:01 +0000

    trap improvement sport something subject

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(10): print(i)
