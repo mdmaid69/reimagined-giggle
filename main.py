cafc648bb6e2b351db5868bf6238f7e31c75926b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:32 +0000

    any run Ll wait round

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
