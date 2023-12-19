a980b3471b935578095514605bee36c55acaf19e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:40 +0000

    although sleep home welcome progress

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
