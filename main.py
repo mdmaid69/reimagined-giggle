8f881e5c27fc4c0092b2068242918388367e0fca
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:22:04 +0000

    extremely yesterday owe provide spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
