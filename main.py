1710fc7fde6b1008a00d8a82dbf54ad2ceb2a9e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:05 +0000

    broad rise usually nowhere little

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
