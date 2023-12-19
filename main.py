1fd84ec84ec1a0040e159116b9440d099406fe41
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:43:12 +0000

    different point expensive refuse trip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
