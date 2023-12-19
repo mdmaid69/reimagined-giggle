df7ff43c8a7285cd4d88df237cb75f89e76d7f06
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:11:32 +0000

    unfortunate vision suppose embarrassed list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
