7f7a1361b6ffbf37705e2aa6c27fe4fb9c073300
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:38 +0000

    cheek generous slowly hard take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
 import array
 def extend_array(array, iterable):
         array.extend(iterable)
+import array
+def get_array_as_set(array):
+        return set(array)
