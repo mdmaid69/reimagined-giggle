2446a14a982404c2f0a6dea513ca31777997726c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:10 +0000

    chance object content fairly another

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
