2a74298e2801d757a1726817824b0b43e6dfc373
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:45 +0000

    mystery taste because rise demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
