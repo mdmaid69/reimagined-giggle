1fe056b79612768c9e078d7bf51c9619214bbc61
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:26 +0000

    young start movement song similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
