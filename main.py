7258fd1df7f2cfc48cf24bb8459fd950d764361f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:39:23 +0000

    prove politics rest everything effort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
