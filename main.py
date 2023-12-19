ae9722be983e99c7591aec8b944c35719ea66235
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:26:11 +0000

    watch brave treatment everything along

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
