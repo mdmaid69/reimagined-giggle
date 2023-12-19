2274e304584c76b3be13ef9ef9f94a2f9313281c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:30 +0000

    switch short permission comeoff star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
