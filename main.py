98fc705972aa3134b7b6cad4abedef35cc94a2e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:07 +0000

    approve cover market ahead post

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
