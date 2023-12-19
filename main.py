845091ebf4e980bd9b71960e01e9973ff27087a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:32:26 +0000

    different float contrast underneath entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
