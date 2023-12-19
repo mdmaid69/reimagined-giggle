2551e900ca0830f86b0b65cfc83938389844579e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:08 +0000

    say need lookafter otherwise comeout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
