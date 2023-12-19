58f61d633808ef602af4cf1299d8793df3df0dce
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:58:28 +0000

    lot who stand total combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def insert_into_array(array, i, item):
+        array.insert(i, item)
