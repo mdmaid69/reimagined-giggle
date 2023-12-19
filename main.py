b225db1bb4ce2854190edea0b6f87201f7645e8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:11 +0000

    close difficult freedom shape obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
