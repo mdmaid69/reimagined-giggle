770550c489257789e02597aca147e7067e789001
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:49:22 +0000

    married pen interrupt provide work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def is_odd(n):
+        return n % 2 != 0
