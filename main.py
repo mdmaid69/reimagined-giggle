8bc7972e296b052aed503cd9958225c929ef2b40
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:37 +0000

    add usually cough bush hold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
