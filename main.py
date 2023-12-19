7133785e58de718f6bc859eec13f972996dccfa3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:03:48 +0000

    ear flat front study fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
