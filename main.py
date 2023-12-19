2e765c1c1844f7bec6d3caab959b430d585dcbfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:35 +0000

    but soil far fact makeup

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def count_characters(sentence):
-        return len(sentence)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
