240e13d070b4e608d735c75cffc7a48b7d1c191e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:50 +0000

    call tap idea many pass

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
