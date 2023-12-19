e141b8e0941f00d9e099865430bb03b7f6032f6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:56:29 +0000

    limit against hand word goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
