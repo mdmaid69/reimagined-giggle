a5b62acb29807527ef33a75189560a5486168871
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:17:01 +0000

    roof deserve lookup appearance crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import numpy as np
+print(np.array([1, 2, 3]))
