d209f86acb71e0b2b3ec79ef4a467bbd91a326db
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:00 +0000

    comeoff just dream move aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
