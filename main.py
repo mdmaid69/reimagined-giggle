49b6ff6ae1a553d310450fd9137d564762e020c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:58:53 +0000

    among comment bridge clear lookup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
