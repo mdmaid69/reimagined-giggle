3efa60cb7ce152747a5a56f2e0300bf582d0dada
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:39:35 +0000

    fit somewhere work maybe sound

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
