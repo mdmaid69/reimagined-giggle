af222b78c64356a80f3b63ed922e86592a4d8466
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:53:39 +0000

    basic trap persuade definite relief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
