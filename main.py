624c595ad5892dcaed0cf1578d5055aabd396d38
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:55 +0000

    general hardly instant wash tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
