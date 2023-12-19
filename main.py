c723d3d2e2ed967dac2aaca1ee8f3287c5269e88
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:36:52 +0000

    mixture pretty extraordinary worse comeback

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
