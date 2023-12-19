1eff500191586053f97d5f8d678979eb81fb7096
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:34 +0000

    well justice somehow occasional equal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+print("Hello, world!")
