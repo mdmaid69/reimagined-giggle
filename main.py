4e6119a7895435ae439ea6f634470cddd3157ee8
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:00 +0000

    reasonable shame whenever married feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
