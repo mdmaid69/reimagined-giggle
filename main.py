cc7949fd5a9dabe4f55af358ca61cc4259aae3ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:46:30 +0000

    listen reject design trial attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
