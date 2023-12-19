22f3bb8fe2766c13b485aee0ce7ac883e9bdd777
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:12 +0000

    field course person although desperate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
