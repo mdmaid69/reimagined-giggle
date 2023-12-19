bad2262442e01b25ac298b5f33c89617f6c60256
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:50:39 +0000

    ground class character attention convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
