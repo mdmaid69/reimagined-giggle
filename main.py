80eacd589e494ced7614a55a6e95a82fbd48dda2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:11 +0000

    thanks attempt underneath completely cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_acceleration(speed, time):
-        return speed / time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
