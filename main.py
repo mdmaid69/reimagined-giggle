32b4344a4384bab7391f9d2dfad4dd5661baa02c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:05 +0000

    study whoever load emotion some

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
