d8c5f56c59323ae7d6117c0db9716c8487163b53
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:51 +0000

    big afterwards guard separate name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
