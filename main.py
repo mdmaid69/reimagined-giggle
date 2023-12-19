934c38bebbeb8e0d0efa14e1bbc3052fb0bc9d21
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:12:55 +0000

    hide town harm undo reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
