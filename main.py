90eefeb707d0627dfb0713fbf5bf60c6a7c87734
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:22:33 +0000

    during previous open under callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
