6de92ca64d6962c8f91bb854ca9ec3180cacd69e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:26 +0000

    bright human protection murder dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
