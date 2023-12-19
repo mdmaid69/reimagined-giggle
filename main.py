cf4184e29626206e71071254919b2f337804adfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:04:53 +0000

    why neat forget exam empty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
