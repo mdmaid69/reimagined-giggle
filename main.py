46fb20c1fe540c0686b68cd2d59d12660a646dcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:40 +0000

    feel getaway little on valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def convert_to_hex(n):
-        return hex(n)
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
