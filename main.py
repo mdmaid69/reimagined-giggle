5d8de2c7eaaed16238c4b0d28494aeb11230ac4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:26 +0000

    564encourage emphasis your same phone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
