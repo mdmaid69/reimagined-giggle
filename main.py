82daf7c809af9c53c5013efc37149fbfeb294444
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:03 +0000

    neat rest praise guilty sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
