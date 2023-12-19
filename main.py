102a9e9898c2d3ec50978d8b09e9ec33e879e2f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:55:52 +0000

    refer tense popular punishment deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
