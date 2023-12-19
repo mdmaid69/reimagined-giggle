6924342fc54745bd627071b7595bd1d3ef74500f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:45 +0000

    power land street wing imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
