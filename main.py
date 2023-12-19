c4ecbeba0ee7c83e04a61819a4613e16d9b54752
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:44 +0000

    drink young course danger noisy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
