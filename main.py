1bc9d64b5b5c0ed0798c222687c1a100112177ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:53:47 +0000

    whoever telephone glass fold world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
