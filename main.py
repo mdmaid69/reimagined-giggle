068179bccce2b4998702f724697f5ab396d38350
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:32 +0000

    take advice pride part belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
