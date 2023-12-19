7d6fcc09a6e74f8a6e215910caed56206e0eb9db
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:15 +0000

    hold Rr new first gate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_user_dict():
+        return collections.UserDict()
