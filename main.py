53919868a24eaedac277386512d4b97c8e28369c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:47 +0000

    throughout shine giveup face direct

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
