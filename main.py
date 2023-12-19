3fc2fdc445d86aed6902ff9838ab388682136c13
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:52:52 +0000

    cheat visit hurry confusing grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def multiply_numbers(x, y):
-        return x * y
+  def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_error_function(x):
+        return math.erf(x)
