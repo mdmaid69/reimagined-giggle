5b0332ae3861ef9b750a96fd2ef31cb167b814a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:14 +0000

    bet oil rule reason favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def greet(name):
-        print(f"Hello, {name}!")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
