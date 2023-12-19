54ee850b25fb20ece0708ddad9ca74d4f7660bef
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:21:43 +0000

    true run precise image familiar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+print(datetime.datetime.now())
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_error_function(x):
+        return math.erf(x)
