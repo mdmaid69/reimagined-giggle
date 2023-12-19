eb454964d08750479a55542bebaca02c828ff3f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:48 +0000

    straight success shoulder condition reasonable

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
-def find_min(numbers):
-        return min(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
