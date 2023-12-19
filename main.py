847d53bc4cff5980609ae8026c773e80a6ae4904
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:43:34 +0000

    instrument Ll turnover trust flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
