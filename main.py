72395c784d6f3cc0588bc68580fef507f0a63e0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:31:19 +0000

    system rubbish tie allow tradition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_user_list():
+        return collections.UserList()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
