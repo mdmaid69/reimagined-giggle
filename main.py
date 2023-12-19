9e2ad32d9ce6f30e29f52e0af85335d606e81faf
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:49:17 +0000

    watch write without full discussion

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
