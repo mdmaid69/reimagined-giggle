be91ec9b0ffea5c9c5059a2807419e9ee0e40c18
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:11 +0000

    private light pretend design noone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
