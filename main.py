2681ff6973d451191ea703a21df195903eeab6f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:11 +0000

    enthusiastic cum bang fear anything

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
