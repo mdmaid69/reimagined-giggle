b2ef4434732c3d52226b345010b7def7d2698eb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:42 +0000

    feeling eager boring behind please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
