872e47eaf79bc2caf8c94b3e7a23079ca7165139
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:21 +0000

    summer frighten underneath opponent quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
