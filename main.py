beb282d5744751cb39de5465e7ac79b4854923b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:49 +0000

    music 569enjoy regular strike imagine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
