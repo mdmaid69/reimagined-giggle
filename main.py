01247b5d9ac22779fadd94e43473152dc893c670
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:32 +0000

    complain defeat swallow whether play

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
