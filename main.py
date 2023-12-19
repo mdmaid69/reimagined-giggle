17c33df2fea985cf01239fc5df60f419cb0b9439
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:11 +0000

    block bright square ignore occasional

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
