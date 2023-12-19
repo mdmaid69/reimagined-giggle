3cfc48978dd07bc473948ad4cc136257ca7506a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:55:10 +0000

    write complete temperature ashamed wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
