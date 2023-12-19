6c8290cf5ddee3c76128e97428d481d5caa45c38
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:08:13 +0000

    contact criticize away gift curious

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_hex(n):
-        return hex(n)
+import os
+print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
