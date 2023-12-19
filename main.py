eaecd1fccc78ffbbdbb0ab8c631a46245928344a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:06 +0000

    takeout beside probably gift comeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
