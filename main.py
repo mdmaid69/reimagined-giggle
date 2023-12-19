ea23adc8399216aeaf96b705a24700e379935949
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:50 +0000

    pronounce pen old bring correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
