fd789adb3d0aa6ad5f4a7991216ec91592c56033
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:43 +0000

    demand at late further in

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def multiply_numbers(x, y):
-        return x * y
+import re
+print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
