6dd8cca4ba64fe8107b7e589d7ae5e9a1983b656
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:08 +0000

    variety emotion yourself extraordinary night

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
