5edcfeb4f01178a6b3648407b800f0e628e5009e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:10 +0000

    confidence determined goon child work

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-print(os.getcwd())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
