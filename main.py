384f413a2cc4ff2a67b1e335b1d0edefe200d99b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:15 +0000

    hospital produce owe whichever mind

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def find_unique_words(sentence):
+        return set(sentence.split())
