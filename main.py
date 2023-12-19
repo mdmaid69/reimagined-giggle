c27a487087a4f67a47c67c0876b8d21a3fe68708
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:12 +0000

    every bath possession unhappy wonder

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
