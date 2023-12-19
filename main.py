5cb62c51037c7bb68065a43a84f74c94f8e9df5c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:40:04 +0000

    between collection about instead regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print([x**2 for x in range(10)])
