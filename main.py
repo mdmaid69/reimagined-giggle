f6c11fdd047bbe3dfe524ca2da5b7e44682e235d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:27 +0000

    calloff contribute experiment announce fork

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_str(array):
+        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
