892b68c11de129698492ac78498ceff97e7d4ef9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:15:54 +0000

    sleep spring minimum common jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item(array, i):
+        return array[i]
