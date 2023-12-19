bc427ac5f7ca4e2369fe2f29f0db8fd6c49a1dea
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:56 +0000

    string properly cum will cold

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def convert_to_octal(n):
+        return oct(n)
+print(sum(range(10)))
