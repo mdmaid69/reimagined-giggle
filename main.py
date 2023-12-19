f277f2d22a7e40348dda395b1085171d70c42806
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:58 +0000

    constantly boat thank impressed whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
