2723d3106e3db8d3ff5e6a45d3cc4fb9a309f9f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:55:13 +0000

    weather bright smooth child hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
