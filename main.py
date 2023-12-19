ec8beafdc4539a05840ca950f292e7c6f33e6d5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:56:36 +0000

    cheat birthday getout coffee pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
