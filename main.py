e8a0b15a5ec9ce7a7d2e1c18a12c14c11a3cdc12
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:12 +0000

    near comfort rub quick them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
