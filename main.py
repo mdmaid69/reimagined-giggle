9021ae4c6cfab4b330a6929d1b7472a0d7068df1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:53:10 +0000

    build state plan small before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def find_max(numbers):
+        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
