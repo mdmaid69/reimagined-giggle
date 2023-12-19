dacb71443555bf04b7ead42609312a8e3c27b7a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:12 +0000

    turnup come remain afternoon challenge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
