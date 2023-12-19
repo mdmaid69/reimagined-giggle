388efdd2cbee6249c436578229f91a7f90e82f91
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:17 +0000

    opposite worst brief report touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+print(os.getcwd())
