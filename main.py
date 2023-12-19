1e938ff638cf3d1c1d3e6c5771d81e1ae9d72a6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:43 +0000

    seem call collection moon number

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
