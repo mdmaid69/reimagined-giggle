0b1a85437491b254a000679807e5d60a657d81a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:27 +0000

    burn know slip size sweep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
