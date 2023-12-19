e449b5167ad7d1b199c99eb01b12a4f1783899fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:37:03 +0000

    careless heat holdon we takeback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
