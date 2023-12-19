b749ea6aa5cd11fc63ff726883e0f53c8ab70131
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:31 +0000

    wealth congratulate clear callout quiet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
