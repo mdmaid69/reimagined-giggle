f1f2a5e749cf5b21e9d639981d042c6cdf0e57f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:14 +0000

    weekend laugh mine tooth rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def convert_to_octal(n):
+        return oct(n)
