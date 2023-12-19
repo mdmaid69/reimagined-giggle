2e5340e56168b67b9cf04093b997865bc1887e48
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:11:12 +0000

    period drawing lower share mix

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_even(n):
+        return n % 2 == 0
