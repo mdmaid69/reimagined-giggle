4a3b3455e03e0b4df6cc1463751587cd62c67e3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:18 +0000

    consist holiday plan keepoff suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
