720c89490d6e297d9125406e37a9e1aaedd5efc2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:03 +0000

    fault workupto getout tight tax

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
