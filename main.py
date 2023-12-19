03da34cdf8d68998d7fb8d17d327bb71073b3266
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:25 +0000

    your temper weekend independent port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
