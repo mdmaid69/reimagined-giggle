813b7d23249f9f3501cac22ca862b6029f577214
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:41 +0000

    excuse high live shape or

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
