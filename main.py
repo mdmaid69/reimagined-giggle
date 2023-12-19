145c633b34e517e23cac9fca3158739fe53b73d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:20 +0000

    always now natural unfortunate rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def cube_number(x):
-        return x**3
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
