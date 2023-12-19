6c3901c7be4a94464594fba442a04f966814eda4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:04 +0000

    relief wash thumb rest thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
