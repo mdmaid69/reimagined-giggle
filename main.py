51b04f73c6781977485f5b27787e723f0406cecd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:38 +0000

    physical rent again piece pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
