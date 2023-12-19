ff6a189075ec8edbebe77e28d547ede9878636a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:18:51 +0000

    usual mistake modern aware anything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
