1039fc1baeb8e326022bd992c105f62a4e128fa9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:03 +0000

    rough estimate row choice home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
