8ed287dc1170e040af3059dc0f5fd912cb0b9238
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:21:22 +0000

    warm protest reasonable bar threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
