838ebcd94968bc4b5d5ecf2abf322e04e6277760
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:13:30 +0000

    complete special concentration drink Ii

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-for i in range(10): print(i)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
