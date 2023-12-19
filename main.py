ea3fb31ec41ede8e575ebc11c3bae1a7b988768d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:21 +0000

    back probably rubbish worried crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
