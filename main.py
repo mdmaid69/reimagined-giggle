0a8bc04a60fbb4308a0499bc72d219caf5cb2efc
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:23 +0000

    once equal show trust offer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
