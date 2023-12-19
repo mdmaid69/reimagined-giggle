440293f70d0086df67967ed97a6dcefa8ed9cf45
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:57 +0000

    doubt passenger sort beat nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
