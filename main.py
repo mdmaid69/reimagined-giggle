3fe35a21034eb77fa2f5829af50eaaabb663f40a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:32:55 +0000

    matter opinion check accident clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
