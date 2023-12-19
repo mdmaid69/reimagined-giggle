a7650ce31a8caf412e530f0679f4cd988d8f5d69
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:47:52 +0000

    brief slow chase recovery string

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
