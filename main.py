bf9dd23a25c1c37a7d5a0e281ad415debcb40163
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:28 +0000

    commit unusual moon reserve provide

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
