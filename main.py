575c5ac3aa9513556eb02280cbe74ca46e89fa57
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:57 +0000

    manage whistle scheme drive alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
