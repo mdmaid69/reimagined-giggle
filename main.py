df38c953700d53ccae77515af421564ca8085945
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:54:43 +0000

    finally trouble hard Pp trip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
