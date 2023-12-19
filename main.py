1112c5442bd2ea39b199244b69ee8183fe2af97c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:27 +0000

    male pipe look squeeze Uu

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
