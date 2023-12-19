af7cdfae2651fec9807d9f98fdb2c9943c8cab24
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:16 +0000

    next disagree fortunate favourite government

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
