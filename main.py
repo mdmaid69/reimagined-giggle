b34b3a4e9853213acbc860c9af0ac1d32fca0fe5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:53 +0000

    determined getaway natural healthy wear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import time
+def get_current_time():
+        return time.ctime()
