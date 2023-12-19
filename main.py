09a7156545545ea154900a5a780491fcaa6d8075
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:21 +0000

    book pupil would total reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_sine(x):
+        return math.sin(x)
