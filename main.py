0638e2f4dd4092b6069a4e63ff9683c3e5b20d2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:20:21 +0000

    mainly claim near reject find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def subtract_numbers(x, y):
-        return x - y
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
