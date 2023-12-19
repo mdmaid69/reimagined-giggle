b2b3a977f351eadfa9d8fa41574b918811227b57
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:34 +0000

    achieve short environment weather strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
