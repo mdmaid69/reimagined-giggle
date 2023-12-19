d3be6af50ff4011985d67358cb96f3a6f6fc9af7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:03 +0000

    between achievement design target limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_current_time():
+        return time.ctime()
