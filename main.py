30cc28ef1dc4569f13914a5919a834edb28d9d96
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:09 +0000

    wait breakthrough image material read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_work(force, distance):
+        return force * distance
