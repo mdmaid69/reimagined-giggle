685ee59f5da548edb2ec7fb4eeac563f9997fbaa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:35 +0000

    priority opportunity dry though such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
