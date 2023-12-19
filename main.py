bcc9bbafc77f135edc8c426d168b003b81514cf7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:14:17 +0000

    know sentence wash my precise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
