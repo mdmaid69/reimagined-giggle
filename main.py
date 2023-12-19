706e9c82ecf24cbbe5c9629aecfb92e039466b89
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:05 +0000

    wander year keepfrom listen slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
