607559fa3c4ecdc45fe207309d8a282782b3b83a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:01:59 +0000

    across theirs connect search music

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
