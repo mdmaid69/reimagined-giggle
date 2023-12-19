aff741fe050d0b9e04e19de5bae1f494e484b459
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:16:28 +0000

    flat recommend basic unit mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
