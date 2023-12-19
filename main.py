dc4f29aa675a9a475b340b4fe926cf6de530b848
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:10:02 +0000

    make visit clear active opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
