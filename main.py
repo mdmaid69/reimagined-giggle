0c3c3efcb7e9dd72f2181c1a6553a0540fb75859
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:39:00 +0000

    report and loose deaf unite

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-print([x**2 for x in range(10)])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
