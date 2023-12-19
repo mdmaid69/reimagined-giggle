4e74083af41badac898786d8a763ba0d17fc453b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:14:15 +0000

    organize shelter form exaggerate joke

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
