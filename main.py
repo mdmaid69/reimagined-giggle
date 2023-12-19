96deeecfbabbf3d5bd587abf0017e0e25623a904
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:22:32 +0000

    pleasure hole rough appearance importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
