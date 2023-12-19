2595f68248dbc25e7531587941351080f0e8de75
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:33 +0000

    bury hello runaway dare sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
