7a143130469720ba9f95244a0854ca0d4e157c31
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:54:55 +0000

    proof worth anywayanyhow often original

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
