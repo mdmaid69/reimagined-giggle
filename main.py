76153c61ab2cb73df1f3a93474a515bde824181f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:25:08 +0000

    funny moment godown breakin earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
