9bde04aec6fedc9d6bfd17ea9f411165ca1afd3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:08:45 +0000

    blame succeed skin runaway tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
