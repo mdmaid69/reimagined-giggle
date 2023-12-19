29ebf70b6df2879325ade89915bef3f46f062a5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:06 +0000

    smile comedown convinced hope same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
