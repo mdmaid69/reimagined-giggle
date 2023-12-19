42caae3343ef4db5b8046ad5eca646c6f2287c97
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:53:06 +0000

    television afterwards it offer near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
