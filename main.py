b1221c942d67aa1776e4b0113b4b1b42f73845f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:41 +0000

    easy seat cool unlikely protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
