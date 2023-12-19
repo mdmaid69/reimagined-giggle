6bf3de1441a109345948d7dd4929893e88d190c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:47:59 +0000

    exactly something lot room complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
