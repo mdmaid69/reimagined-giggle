3733e9a073c0527dfc3cb602304cf15753071eb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:50 +0000

    getoff brain worth excited often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
