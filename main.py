f17e79b2b7e4e0f13dc6c709e85cfe2a480295e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:34:22 +0000

    separate row steal explain slightly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
