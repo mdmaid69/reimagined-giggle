971520556d1ceb7e8d734057a64ede756bc504e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:44 +0000

    create collect ought autumn earth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
