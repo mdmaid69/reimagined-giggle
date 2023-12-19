ff38739ee3550d3fe325f2784a3cef2d90dfabe1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:24:34 +0000

    either opinion find start basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
