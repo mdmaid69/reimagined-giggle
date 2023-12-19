3d886c7c1aeeb395fee6dfd881b845fa3f7291b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:13:46 +0000

    mistake prize order sensitive true

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def square_number(x):
-        return x**2
-import time
-def get_time_since_epoch():
-        return time.time()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
