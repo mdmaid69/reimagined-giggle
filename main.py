350bac07f0fae56df77591c7fc814b33bc23f362
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:38:58 +0000

    add rest heat combination once

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+print("Hello, world!")
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
