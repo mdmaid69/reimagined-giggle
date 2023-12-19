b31f0a7f594882b8fe7d60cd280d8ea9283162c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:59:54 +0000

    coffee deserve lucky form tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
