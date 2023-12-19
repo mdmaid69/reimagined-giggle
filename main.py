8cb27fa8143636342fc0e2e6bcf2a2a599aaf272
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:46 +0000

    climb steep inside change by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
