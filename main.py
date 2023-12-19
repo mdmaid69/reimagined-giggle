7f031f50af52d1c4d3e4b8d5549382939604bf03
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:43 +0000

    hurry continue several sensible prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
