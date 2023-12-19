6bf7bc16271f8d99b48cda6a1fec2cb830fdac40
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:02 +0000

    dress visit round immediately ago

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
