2855674e9624f138fc3e3add3909dd533e7ed24a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:25 +0000

    any wave tax comfort somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
