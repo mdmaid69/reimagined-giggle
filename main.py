481d59c798a298f86d76ae728bc8232c39a1996a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:23:44 +0000

    exam language stamp skill between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import platform
-def get_python_version():
-        return platform.python_version()
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
