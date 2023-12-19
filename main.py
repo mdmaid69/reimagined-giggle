37b3275c1b9a7fa64f222060a0463168417a1162
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:04:54 +0000

    visit classroom difference in brown

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
