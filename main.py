348c06e69809cea4e2f8b4ccc2793a2b243d36de
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:38 +0000

    dangerous luck cut reduce brief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_str(array):
+        return str(array)
