9c9b1aa489af8a3e93fce444e094e92fc62ad582
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:33 +0000

    warn crash yet sure connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_list(array):
-        return list(array)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
