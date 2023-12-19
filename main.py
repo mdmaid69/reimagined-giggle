da0f949c224a4ec006f007ef4917cbfd2e0504b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:30:08 +0000

    leave record slowly arrangement thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
