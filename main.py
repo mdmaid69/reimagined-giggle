c76f2dc85c11a019fd3b82f0a0a0020d6ef3fec4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:10 +0000

    whoever bear strange goon juice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
