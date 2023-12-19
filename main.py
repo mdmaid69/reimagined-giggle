dacda6b82447fc1652bdf2cfb8591c8b53a21a5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:39 +0000

    so store takeaway recognize top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
