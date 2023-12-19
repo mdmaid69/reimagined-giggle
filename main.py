2bf19be9783f49c71a842108f573060cce1cc42e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:56 +0000

    unusual dangerous thanks repair loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
