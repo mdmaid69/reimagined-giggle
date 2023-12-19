32fb496547e039a3861097ef588374927f1ce3a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:32 +0000

    tip must choose event embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def append_to_array(array, item):
-        array.append(item)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
