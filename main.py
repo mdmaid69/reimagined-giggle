c266e4fec5d6bc61e29e7548c0e075e1c103f1af
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:42 +0000

    hospital copy show since tonight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def convert_to_octal(n):
+        return oct(n)
