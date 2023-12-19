7af35e40aecace969ac3ad2f36b22a8e4efb6a58
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:27 +0000

    delight emphasis remember none everywhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
