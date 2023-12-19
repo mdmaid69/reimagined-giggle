6eb6cf53d833f9d91ceba9189e3552b8f2bcfbc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:52:39 +0000

    Zz means month profit exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
