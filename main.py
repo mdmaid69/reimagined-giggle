c5b09905b121845a7cde247157f5ac77bcbf1a5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:15 +0000

    satisfactory sweet breakthrough clothes smile

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def reverse_list(lst):
+        return lst[::-1]
