10aadaaee92704192f1a63f1836a2e5ca8671f75
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:45:09 +0000

    quick by everybody deep ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
