aeac69f59863eab51131cbabd2e8d9bcb980b063
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:20 +0000

    narrow bright voice task equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
