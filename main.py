09d6feabc6048d79a0241ab151a4ecadbd1ee259
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:38 +0000

    mother sort example exciting file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def is_odd(n):
+        return n % 2 != 0
