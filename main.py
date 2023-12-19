0db9fd0b3e4639d2560bf2f29186757728e4c6fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:06 +0000

    lunch shake light after sign

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
