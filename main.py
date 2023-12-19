b3d216fdd6be077e882208184111afb0ba319e8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:42:25 +0000

    deal fresh moon statement row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+for i in range(5):
+        print(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
