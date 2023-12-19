a6a5372f6150a46522673e75c8804c8477a8e99d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:55 +0000

    preparation order deserve persuade search

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
