7e2fdaa4d61b1b0c1415cf31d460a5196dc6eb31
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:21 +0000

    post class tell rid sensitive

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
