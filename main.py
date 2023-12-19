1d6a5ed48d16faef7f67d271e4d29cbed714e5bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:55:12 +0000

    exam suit situation correct turninto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+print(sys.version)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
