eca06a84346eea6686fb8b8b1e09815094ad1068
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:47 +0000

    mention church mistake duty whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
