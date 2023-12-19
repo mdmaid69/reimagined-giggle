7a84079ecec70b4f2e5ab03b94e3b462a248def1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:46 +0000

    please test warm music polite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_area_rectangle(l, w):
-        return l * w
+import tensorflow as tf
+print(tf.__version__)
+import os
+def change_working_directory(path):
+        os.chdir(path)
