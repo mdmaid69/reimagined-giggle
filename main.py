186253df99344492fa9f6bddff39317d5213b39e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:37:47 +0000

    quarter lay right order post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_even(n):
-        return n % 2 == 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_density(mass, volume):
+        return mass / volume
