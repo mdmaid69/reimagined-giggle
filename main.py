56fc0999db1495ba40ab76a84d326d4c3b0c99ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:23 +0000

    centimetre port recovery share search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
