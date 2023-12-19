94daff22f696150ff43ad593c50cfb21f47fa449
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:10 +0000

    pressure pleased replace gooff attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
