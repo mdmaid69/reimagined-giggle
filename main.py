56b5791f7dd8c63f729373e4e27e0748c8c7d983
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:21 +0000

    rise satisfactory up force related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
