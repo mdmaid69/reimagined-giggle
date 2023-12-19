d46aa7bf3c10665a53c5014e7fbc91af97013cf1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:11:11 +0000

    OKorokay scared sense advanced weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def square_number(x):
+        return x**2
