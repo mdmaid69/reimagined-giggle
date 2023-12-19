638ba8234f7f6c067964f120b36ac973f89e5665
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:09:54 +0000

    separate hear real whisper none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
