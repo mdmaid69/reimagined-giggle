8cb36318a5f2bd1700bd118a9b3c0f3b8e062525
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:06:36 +0000

    beautiful getback thirsty bring feel

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def sort_list(lst):
+        return sorted(lst)
