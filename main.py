6e0e8d29cb90b0966f6ce6e4a58abf5cbb828efc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:28 +0000

    turn platform money deliberate properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        return sys.version
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
