78906008451381f6b17588d7334325c2ebfe9422
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:31:11 +0000

    proper hot clearly sea load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
