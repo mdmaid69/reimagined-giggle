c9cd7ee8bf24f2bea3989abef92657332b994c15
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:54:48 +0000

    split empty short offer comedown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
