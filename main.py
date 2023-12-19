4774711960ce7d5eada6e8f46f906925f3d24988
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:16:01 +0000

    front cope although stop about

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def add_numbers(a, b):
+        return a + b
