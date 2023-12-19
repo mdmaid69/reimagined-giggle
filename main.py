a624b1eb2c9960afca5ed7e642997bc9fbea3f68
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:13 +0000

    eye job keep sport provide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_list(lst):
+        return sorted(lst)
