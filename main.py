5a439c98016eb785bf53dacaf488e0920f97fceb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:57:50 +0000

    teach gather move basis bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
