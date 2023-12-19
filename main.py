ac19023f9606727fdb4dddfaac652970ffaf631b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:37 +0000

    takeaway 568energy that sensitive unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
