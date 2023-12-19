27909d26782543e67114fa8f0d2eadbfe1fb2348
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:53:39 +0000

    tomorrow dry happen later lose

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
