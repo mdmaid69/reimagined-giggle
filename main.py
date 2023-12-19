87cf396c085ca532ac30e784ede3efe3d20ed936
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:06 +0000

    worse exhibition doubt rough complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+import array
+def convert_array_to_string(array):
+        return array.tostring()
