74c333f45a6006592a42c4b843f4a538b010db0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:11 +0000

    pile regional getup level invitation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def multiply_numbers(x, y):
-        return x * y
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
