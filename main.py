05e90cc358711423952e9c73714a372d7a8e5250
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:21 +0000

    connect present none wet design

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import datetime
-print(datetime.datetime.now())
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
