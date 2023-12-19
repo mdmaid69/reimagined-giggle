f9b799fdb1425db99caf6e3324695f59dc0ea6e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:39 +0000

    change mention music anxious about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
