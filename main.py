df741587f0a82a136f225047367c7429b5ff54b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:37 +0000

    wonder hospital amount connect record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
