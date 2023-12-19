b6616f54ae218c744823122f43626a662693abaa
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:20 +0000

    fork must bother that chief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-for i in range(5):
-        print(i)
+def get_array_typecode(array):
+        return array.typecode
