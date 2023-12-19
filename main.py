14ad3bb43fec9ead106d93b083e579d95e405bf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:31 +0000

    obtain fly dress poor bath

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_typecode(array):
+        return array.typecode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
