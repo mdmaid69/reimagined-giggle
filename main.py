6ca307e905387c6fae6356fe8c50f62c9ad63b4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:51 +0000

    example sweep on load comeoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
