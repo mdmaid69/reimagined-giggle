37da98a7b21e996a0755460c32ef4cd19c388844
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:55:04 +0000

    score physical fast perhaps thin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
