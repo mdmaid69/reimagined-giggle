8749f4cb6fe174bea028ad3c1c6bfc51babc8ae2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:07:05 +0000

    brush mood flavour pressure deliver

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
