e756a9ac73abd884f283359513740c62c7734048
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:29 +0000

    smell mess fold please hear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
