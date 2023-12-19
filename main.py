a6c141e77a170d40b17b50c01029a5c73e580da0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:44 +0000

    poison shrink interview most alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
