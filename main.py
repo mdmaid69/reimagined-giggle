d2185507ecbc706ca2e61930e09bbffe614c1cd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:16:34 +0000

    nervous listenfor emphasis family defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
