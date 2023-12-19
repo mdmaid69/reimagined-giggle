dfe2ad508748f1504e14c583bf50722cd79f7846
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:12 +0000

    back strong unable exit consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-print([x**2 for x in range(10)])
+def reverse_string(s):
+        return s[::-1]
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
