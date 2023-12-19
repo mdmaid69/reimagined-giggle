a8b31c494495768eb391336867c19f7526603238
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:21 +0000

    miss poor sister sort notice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+for i in range(5):
+        print(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
