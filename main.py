ca3d37ac3d7011c9eb47aeb64dd96e13fc9df528
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:20 +0000

    case taste more plastic crash

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
