d80ffe4f667c0b0fb572ab29d8e1c0921766f84a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:19:50 +0000

    strike under blame Rr 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
