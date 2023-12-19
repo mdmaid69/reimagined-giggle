bf94d57ecae8bd8ac7fc7c6ddb98e90b5431b5a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:47 +0000

    stare next broad fashion heart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
