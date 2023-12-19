8dd03211a2f9035ae5f2f505a2e349b084824163
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:38:51 +0000

    trick smart much bridge prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
