581c5741f42568cc980dfe973148ab1d495a1f62
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:22 +0000

    around government close discover train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_directory_name(path):
+        return os.path.dirname(path)
