5a4f28e9dc8417e06e9173031dc543c438d3f7ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:25 +0000

    translate replace really cost safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
