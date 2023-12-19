b74d7ea455590d996d0e60b7bb0349a4634e542a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:01:32 +0000

    keepup drop least perhaps discovery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+from collections import Counter
+print(Counter("hello world"))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
