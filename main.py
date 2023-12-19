3f76b8cc24f70505ccfad9a11b624ed6f7b6f2c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:12:50 +0000

    empty this survive modern pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_counter():
+        return collections.Counter()
+def count_words(sentence):
+        return len(sentence.split())
