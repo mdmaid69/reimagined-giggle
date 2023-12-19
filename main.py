a1d2bf0bcf67178442ccd04c7752e1b16c167b7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:49 +0000

    sign range custom for steer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import tensorflow as tf
+print(tf.__version__)
+import time
+def get_current_time():
+        return time.time()
