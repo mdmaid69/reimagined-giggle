b93dfb0451b533a8acf5f9e71ae88b96ea2d2382
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:25 +0000

    uncomfortable oil waste occasional her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_speed(distance, time):
+        return distance / time
