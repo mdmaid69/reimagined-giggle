b7d9e81ce1544a4dc33ebffa4ecb4e954c42bf2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:09 +0000

    combination grateful corner breakin blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
