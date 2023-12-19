48d97d35e9fe1f5908f44bcac60f647f93625280
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:52 +0000

    popular mend same wise protect

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+print(sys.version)
+print("Hello, world!")
