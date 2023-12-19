b8266441d3624a0a721e3eb25ae23681cf046eb9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:52 +0000

    kind face experience no avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+def exit_program():
+        sys.exit()
