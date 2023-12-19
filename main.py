82c2899f7b559e6f37dbbbfb77e6ad9fa118dad6
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:37 +0000

    but bottom church down tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
