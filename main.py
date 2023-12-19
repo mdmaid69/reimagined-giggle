231a5e6955309d9d4d82e7b8790e33be8bb3dd10
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:23 +0000

    much sorry correction protest reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
