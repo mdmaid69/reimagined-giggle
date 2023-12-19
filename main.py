bdd2fb776b36a2f0e13a700ab12d5857882b3f57
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:14:44 +0000

    action Qq room check capital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
