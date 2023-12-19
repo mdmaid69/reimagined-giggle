2cc7ce2d28f5851cb7e8f775e04a3e68af48851a
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:31:23 +0000

    authority happen spread threaten avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
