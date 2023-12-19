06965e8ec8938a7af2b870cede9941aacc2a1343
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:44 +0000

    loud law nowhere prevent gather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def remove_duplicates(lst):
+        return list(set(lst))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
