78a76a4ab3ab32b6793acecfa7a566f179cbd902
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:49 +0000

    consist regularly impatient money lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
