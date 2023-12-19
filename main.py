faed298a800c60ea4f9afa08ff62046732ee6d28
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:14:06 +0000

    confusing class reliable crash confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
