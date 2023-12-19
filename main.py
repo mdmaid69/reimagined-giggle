cf00f1e125d00733b00581cc2c6ea638201a1004
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:53 +0000

    meal disagree tight confident switch

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import sys
+print(sys.version)
