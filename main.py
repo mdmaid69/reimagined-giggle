5fecdef520287d1c020ad51da0334ddc02311cc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:46:29 +0000

    load effort bill past memory

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
