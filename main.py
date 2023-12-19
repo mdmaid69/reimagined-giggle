e11a0854a225ab002b6d46756f99da387a137f13
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:41:19 +0000

    turnout explanation stamp water remark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_to_octal(n):
+        return oct(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
