9cbd5d3e2676044cd3d9630ae9651c7623fa5b04
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:28:04 +0000

    pair train hollow none breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
