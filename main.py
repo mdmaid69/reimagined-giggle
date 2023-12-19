00ce76e1f7107d2570a59d6a2ad8862e4819c477
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:31:29 +0000

    study exercise story actual score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
 def set_array_item(array, i, item):
         array[i] = item
