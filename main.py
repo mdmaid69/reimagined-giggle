ad81d2449d76ef6ad39fc69e94ca16ba91229bf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:50 +0000

    choose equal such or test

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
