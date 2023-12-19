f993a7e074e4c76a18ce5bc1ac689b2f38eb4111
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:20:32 +0000

    explanation trial dear passage test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
