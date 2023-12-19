9e6add90d722d040cf3c0af1131a550133995117
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:26 +0000

    waste adjective weak healthy mysterious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_user_list():
+        return collections.UserList()
