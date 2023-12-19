35c191b2bd60135efd9dd7fd2d3bea32f24228fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:17 +0000

    talkback cope trip wide spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
