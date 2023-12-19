7fc196908a5a9921d46e29a9caf628e9adbbd899
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:51 +0000

    whoever wave block roll pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
 def is_palindrome(s):
         return s == s[::-1]
