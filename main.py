aca0edf8f1886121675f36ef25e54a3bc6105535
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:33 +0000

    turnaway secret land figure make

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
