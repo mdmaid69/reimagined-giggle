4c1e6dbc65a6addb954490886d91d4cba834d27c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:44 +0000

    fortunate abroad avoid disturb bunch

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
