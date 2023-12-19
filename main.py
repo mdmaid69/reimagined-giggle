59e9ef7c41529c963d23d0be5c1092a68c6d049d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:47 +0000

    Qq guilty great Hh copy

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
