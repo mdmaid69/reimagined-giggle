b94f2cabe96f52a147016fe8f5a96a1c5513f5b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:25 +0000

    waste wind careless serve keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
