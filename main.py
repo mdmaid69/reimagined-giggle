397b45ac3c7cf745d6d3961f87ea1d863e608dc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:50 +0000

    until lose live friend within

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_power(work, time):
-        return work / time
+import os
+print(os.getcwd())
+def is_palindrome(s):
+        return s == s[::-1]
