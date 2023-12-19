4602e4d07439ec813bd58283a29739b7265d37e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:17 +0000

    same concentrate hold interesting dear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+print("Hello, world!")
