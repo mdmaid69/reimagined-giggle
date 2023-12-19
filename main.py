18b3ec36143a5c6eba58277458260820232930f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:44 +0000

    fire week usually unlikely plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
