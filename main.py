cf6f8c0d0ba752dd9b7ff4b73f95e20adbd6e832
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:23:37 +0000

    hungry bang thumb comein sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
