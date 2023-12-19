25bb9937d5aecd8a64ad020044739aa9d969557f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:50 +0000

    comedown female entrance memory natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
