45b92361d26549ef41ca984646dcb03a8b4930c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:11:49 +0000

    defeat closely get mean makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
