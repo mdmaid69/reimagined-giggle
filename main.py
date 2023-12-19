0573e42ba1dbb836ae43f7f4dd3a53fde88ec524
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:56 +0000

    flat heavy nature complain vanish

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
