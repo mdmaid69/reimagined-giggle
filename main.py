46b52389bb832f868514a2f5268fc1ea0eb79c8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:42:51 +0000

    here shout event since back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_power(work, time):
-        return work / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def convert_to_hex(n):
+        return hex(n)
