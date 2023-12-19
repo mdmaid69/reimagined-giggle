e8189ed9e126e079c7aaff254a0daac903bdede5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:53 +0000

    eat create piece bring alive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
+import unittest
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def is_odd(n):
+        return n % 2 != 0
