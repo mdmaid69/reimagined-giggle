e63ef9fec9624123ec000242ea1ec7c155962be5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:50:07 +0000

    wish future nonsense front sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
