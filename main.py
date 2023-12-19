f64ee32707c9098a7ba39db5c57b75f958670740
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:06:16 +0000

    wake fire travel male chief

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
