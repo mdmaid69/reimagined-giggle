7c65c6acc3e0cdd96d096e359dccd865b4f695fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:06 +0000

    voice unlikely avoid calmly tired

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pressure(force, area):
+        return force / area
