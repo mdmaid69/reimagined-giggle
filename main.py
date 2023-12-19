7c5001e2278d0794cc6adc0b772b0f4f364a0ed9
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:34 +0000

    573entertainment friend please thinkof suppose

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
