ac3c34d3f15322573768f5f878910fde5bcd0481
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:29 +0000

    exist seat juice nor please

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_bool(array):
+        return bool(array)
