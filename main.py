09cada094252659b6666cf5d3f3dc559f64af62c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:22 +0000

    contents absolutely build here supply

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
