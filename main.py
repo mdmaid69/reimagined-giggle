e71713b4231adf1baedda6c8b79dc23a766b9acb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:02:16 +0000

    share kind aware almost mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
