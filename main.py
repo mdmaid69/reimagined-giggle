51c852e9c6da4a9307794593ecd17d2bcbe808e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:47:25 +0000

    children dick rubbish call shopping

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
