3ff8083e7e9424c71a849ffd53f598a10c31dacd
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:29 +0000

    relative after film walk disapprove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
