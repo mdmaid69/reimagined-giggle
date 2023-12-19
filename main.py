b618b0f9efda603cc5d5a5bbc37df3b923208b67
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:00 +0000

    uponon right basic mention valuable

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import sys
-print(sys.version)
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
