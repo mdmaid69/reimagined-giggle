cea588f0934028064fd0180475ed09ad8bd98a17
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:39 +0000

    aim shade tidy strange Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
