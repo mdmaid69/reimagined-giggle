2f97618a4d9c3e8adc32f93d7dd0680fb71c0633
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:55 +0000

    fear lookup date address lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
