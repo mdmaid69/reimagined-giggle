f48fc0306e8102cef85cde99509db1923712615f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:30 +0000

    pleased even concerned music comefrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import logging
+def log_message(message):
+        logging.info(message)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
