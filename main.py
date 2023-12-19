1af38ac962e90d4675f0e756e2e3be42ba288a64
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:54 +0000

    vanish destroy clue fun ahead

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print(sum(range(10)))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
