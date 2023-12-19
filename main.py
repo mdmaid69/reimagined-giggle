1989515263f4c228a51509fca9bd166862a08bb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:48 +0000

    callback come snow complain strong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_acceleration(speed, time):
+        return speed / time
