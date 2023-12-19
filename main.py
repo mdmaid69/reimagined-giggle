9138f07be5341a1a830d77cd39058a79228241e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:57:22 +0000

    summer gap large change behave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_list(array):
+        return list(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
