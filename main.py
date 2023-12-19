51c1728e701bc590ce07578d1a632c5cc2957f69
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:38 +0000

    flight bar note practical sharp

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_work(force, distance):
+        return force * distance
