749f4d4f63e67bf3189c79e2f076f35794c97232
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:04 +0000

    task discover getup deliver free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
