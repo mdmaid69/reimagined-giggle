48e3f037782eae5ac9f8d82f2729e48830de801e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:43 +0000

    through window way where minute

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
