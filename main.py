b84c0f638add6cbc2d4406d45ceb45239aeb1b28
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:27:23 +0000

    radio measure mainly then card

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
