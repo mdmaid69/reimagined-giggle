fde03d6ce1e003a324beccb02c22239014088476
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:59 +0000

    edge owe discussion stamp congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
