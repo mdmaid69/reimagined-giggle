80c22a7f7d45ec6200ffa78616a99ace993ae5e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:20:40 +0000

    forgive Ll vanish message relationship

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
