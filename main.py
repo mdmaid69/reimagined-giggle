873789b0d6dbef1a3d92716a3e1c2bd63e0e7da2
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:07:33 +0000

    hospital their majority region thing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_as_list(array):
+        return list(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
