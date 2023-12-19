b2d3ba2326eb65f6d4d8a300dd001c3b8652d718
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:57 +0000

    turnoff tidy direct excellent strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
