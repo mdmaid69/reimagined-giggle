747162a2a9cb449692992e27cc57029575b8a485
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:40 +0000

    reduction Ff impossible improvement lost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
