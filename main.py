b457c3b464176d16c67e5b559b44b8aef781cc58
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:20 +0000

    summer Ee work drive arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
