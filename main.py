e08e7f78443e8a165c5e3e65f362d227f2f24d3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:33 +0000

    turninto clear path makeinto cool

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-def subtract_numbers(x, y):
-        return x - y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area(radius):
+        return 3.14 * radius * radius
