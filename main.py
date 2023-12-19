8391cad8d7b8af32e137a83b5039b0f070b7440e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:56 +0000

    tape reaction consist straight consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
