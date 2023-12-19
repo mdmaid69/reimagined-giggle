41aeee9be6d6cce769e70be95229cdfbdb1993f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:18 +0000

    dream match hide bring lay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
