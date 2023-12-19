f9b2681ba27d02c04a3eb9db64d036260f027017
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:07:28 +0000

    contrast block exaggerate 569enjoy light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def is_odd(n):
+        return n % 2 != 0
