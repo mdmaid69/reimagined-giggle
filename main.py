63a0e2c69c6bd11bfd74dabe4a38b7c384da38de
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:25 +0000

    shopping shallow first raw speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import collections
+def create_priority_queue():
+        return collections.deque()
+def find_max(lst):
+        return max(lst)
