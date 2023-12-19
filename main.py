202dcc3691573d1d5bb4fcc8db748171de4d6218
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:32 +0000

    spoil between turninto behind criticize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
