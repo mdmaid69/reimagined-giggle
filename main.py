20f5e52bd5b6398a833b9a1ab1892dfd7030485b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:22:55 +0000

    main listen container to admire

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
