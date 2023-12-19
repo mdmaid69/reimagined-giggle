9a08eb4e286e1017699862183f87418eee72586a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:29 +0000

    increase whoever another rough entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
