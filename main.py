b18a919b74ace925c1565d1ea065ec55faa40d4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:50:39 +0000

    brother wonder mystery memory pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
