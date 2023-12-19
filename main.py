2c07dde8444c59d37c4755c1c7490452e953b4ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:29:20 +0000

    mind objection move debt best

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
