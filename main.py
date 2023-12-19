5a7e4d4fea30ed6e09fcfcf2658e4b42bd09916f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:50 +0000

    table teach generally mysterious trouble

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def find_unique_words(sentence):
-        return set(sentence.split())
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
