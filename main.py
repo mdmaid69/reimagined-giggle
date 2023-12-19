50ed0d58130d9210fb0abb94026d726baf6a9e99
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:16:05 +0000

    collection influence wrong exit out

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(numbers):
+        return min(numbers)
