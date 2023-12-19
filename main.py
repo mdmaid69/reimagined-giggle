7abe80d2522f384dcd64b6f61672636fd3a46714
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:42 +0000

    your noone love bunch close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
