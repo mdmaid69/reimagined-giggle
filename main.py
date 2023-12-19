f377c50c166eb3cd2ef68ed8fc85fb0333c30e26
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:44:03 +0000

    hand everywhere emergency estimate welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
