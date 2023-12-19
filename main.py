8f88baa44f0ea86423a695b0ab7c1a9468e14309
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:38:50 +0000

    information perform improve book border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
