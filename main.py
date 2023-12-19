a0ad3619077382ff6e43112a4d8a8c42d7edaa53
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:51:58 +0000

    common regular air introduce fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
