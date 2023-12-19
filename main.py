17f2a955b640f4a9a25714c47af818d0951bad8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:51 +0000

    someone back able but around

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
