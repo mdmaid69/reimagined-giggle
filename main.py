5eb01c01e1b29cdf268aed87963804764faf988e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:35:59 +0000

    home contents wide talkoutof arrive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
