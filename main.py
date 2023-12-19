3b392b57ccada7947fce1dfffeca0702f09965ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:37 +0000

    attempt shake visit evening contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
