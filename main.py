a2ca1d88979245daa8f2615b468436d329ef94a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:41 +0000

    male slightly interfere difficulty ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
