d340caf3955208c855bdb4c8f9f165a32f2f9ebb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:41 +0000

    field desperate want news Ii

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
