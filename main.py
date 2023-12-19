e53044df1a4eead430858b6292c6369a600bb6c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:47 +0000

    for then cruelty scrap paper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_hex(n):
+        return hex(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
