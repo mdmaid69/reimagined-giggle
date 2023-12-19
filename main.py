6bbf8bf7f07ff870504041dbf5151a957132b914
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:30:37 +0000

    mother education brave previous trap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
