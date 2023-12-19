3972465f3646610c873780504192b0a8ee0edba2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:56 +0000

    custom something single fork ago

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
