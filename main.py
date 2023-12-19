ffbcf05b3c6fed75e9eafd9dc81a781e0a4498be
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:18 +0000

    join earn region frighten goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
