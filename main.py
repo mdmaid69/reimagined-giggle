d9517a4dd4800085d245e1d23c2013fa73a058de
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:49:55 +0000

    pump wise hollow advice activity

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
