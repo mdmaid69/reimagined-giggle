983eb7bdd417b9145b117a39ad805cef7da3f046
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:36:06 +0000

    bye warn window let takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-name = "Python"
-print("Hello,", name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
