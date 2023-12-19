473ade0e8a11ccb92a58f1cfd7c879c5cab3fbd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:46 +0000

    objection show point goaway skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
