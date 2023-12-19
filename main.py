380f6558ddf93ac3233b39e6844541bb00d6000b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:43:54 +0000

    Qq spot same interrupt power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
