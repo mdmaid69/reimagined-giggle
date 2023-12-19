8d458d26740312e63258353d505c9038c20c4b2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:52:40 +0000

    temperature disagree tense bringforward black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
