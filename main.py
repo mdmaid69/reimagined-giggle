c089178690f18a44c37e3560300f0806f63516ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:46:12 +0000

    believe in convinced nervous float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
