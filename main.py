1aa0ba3cd8124746919b964a7f557be4ad40d701
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:11:26 +0000

    rain join then emphasis store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
