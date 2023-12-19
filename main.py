3095f7064253f3767889bd582d062f9f6406425e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:00:54 +0000

    unpleasant like care type nearly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
