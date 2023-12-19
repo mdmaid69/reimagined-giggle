854a11efba7666f1693095712092eb891286c7c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:10 +0000

    split official comeup Rr shall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
