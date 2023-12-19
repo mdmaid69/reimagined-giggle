192ed7e4a513a466916084d551ac5bf555cb9e8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:32:50 +0000

    voice compete getback attempt then

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
