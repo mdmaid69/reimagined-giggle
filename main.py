464d1085ee4c5d9531d61ccf4c4d359da346d86e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:16 +0000

    task born quality floor each

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
