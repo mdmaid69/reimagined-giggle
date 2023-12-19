134272527d9bf1ea597917142befc5555d109341
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:13 +0000

    treatment connection dull call story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
