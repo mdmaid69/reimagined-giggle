4a145ab77d858c7e1b9d8958a9cbab59956615bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:11:19 +0000

    settle voice experiment winter while

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
