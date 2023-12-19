0b8f4d78d67f78959629935ca347c4dff04d4d7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:12:15 +0000

    type stage mess only load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_power(work, time):
+        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
