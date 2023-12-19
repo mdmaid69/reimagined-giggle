89a880fc15b3b87c20a9bc7fbd02a24698737792
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:59 +0000

    clean noone limit perform scrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_queue():
-        return collections.deque()
+def count_words(sentence):
+        return len(sentence.split())
+def find_max(numbers):
+        return max(numbers)
