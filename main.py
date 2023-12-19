6ef2a456f55c528dc0132e131a43708ecdc3b4a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:07:52 +0000

    number home near building strike

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import sys
-def exit_program():
-        sys.exit()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
