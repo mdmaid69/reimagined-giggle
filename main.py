bc6310c9f2e0df62e1a6e17f285b480af942be10
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:03:39 +0000

    schedule probably rent trust catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
