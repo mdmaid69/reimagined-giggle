760f85e2c4ec0291ee055fb6841d5792889cd5d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:05:52 +0000

    near demonstrate fasten file building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
