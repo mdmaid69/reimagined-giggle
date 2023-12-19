2c447c62e6bf1284326cd2912547e7f8036262e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:48 +0000

    sorry could true desperate comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import platform
-def get_os_info():
-        return platform.uname()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
