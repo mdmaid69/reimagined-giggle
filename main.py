1071dff63ada87e1d02671aec118ff6f4679e671
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:19 +0000

    your clothes spring build pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
