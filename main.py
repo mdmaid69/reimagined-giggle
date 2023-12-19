bf9df419397de661b47ef03a809b3e4e69751722
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:14:53 +0000

    piece force creature home tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
