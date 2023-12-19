e9e0d8c5691c0ed240a17120325583141a0e68f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:28:33 +0000

    practice leave marry area square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_directory():
+        return os.getcwd()
