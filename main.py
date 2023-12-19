d2dc2d9feea988dddb67321222d1ce11135f7690
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:44 +0000

    modern avoid extremely choose warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
