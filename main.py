058618dbab51fc664c737ea541c5a1d39f21eea9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:12 +0000

    already shopping possibility false eye

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
