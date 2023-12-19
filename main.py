3b3ddab333470ce2a10f2386e8936fccddec2870
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:25:18 +0000

    vision punish takeout provide create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
