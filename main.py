6c978df87969c387f25773a1ee1496a2a52acd12
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:00:20 +0000

    card apologizeapologise many group come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
