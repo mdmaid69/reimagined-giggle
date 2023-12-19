785df07bfb5162e34d3f571b893420fb782fc781
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:13:29 +0000

    foreign worth convincing which proof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
