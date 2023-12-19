0a2c9e2f57f13af6cd7652714f98d1e259f1d860
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:00:16 +0000

    sore dinner coffee practise hungry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
