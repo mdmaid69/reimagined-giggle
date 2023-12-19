ca0d9890d89d927eef4d670882af76ead886fd0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:10:22 +0000

    period thin undo first blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
