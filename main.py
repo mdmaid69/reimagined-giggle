88cb3781fc2c30a739e0042de69417d575c34e17
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:37:41 +0000

    class exercise move pair entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import time
-def get_time_since_epoch():
-        return time.time()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
