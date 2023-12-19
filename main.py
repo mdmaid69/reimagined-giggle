8fc7ed21a71667993eef70b5a26e801540296eae
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:16:16 +0000

    damage Uu hide bringup fact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def reverse_list(lst):
+        return lst[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
