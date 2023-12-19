4d45f61ad73645865b710f0153899f4ba89970bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:17 +0000

    attempt smell today comfort practice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
