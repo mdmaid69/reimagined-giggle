3696dad461ee8322a80c08aab8b324ba741b5f79
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:47 +0000

    safe store today boil dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
