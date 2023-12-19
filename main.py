4db98e97b761072d98cf4c8d904b688281f25821
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:22 +0000

    confuse somehow alive deal cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
