39e59c2519b36c342d4d10439faee370432a4173
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:47:19 +0000

    wide lay country where goup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def add_numbers(a, b):
-        return a + b
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
