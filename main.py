1cfcf3b9067dbbbb9892a4e88fa2a24a852a7a31
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:27 +0000

    doctor temper mess second choose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
