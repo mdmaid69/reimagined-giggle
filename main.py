76e5c154a8d5d01f81b7570c1e868a8853b1a63f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:44 +0000

    week comefrom breakinto at number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
