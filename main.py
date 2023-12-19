f5dacc784b009ba5bb203866f0b85c8caed3f040
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:28 +0000

    niggaacademy treat practise repair mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import sys
+  def get_python_version():
+        return sys.version
