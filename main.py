a428fac1b436ac3d06760783c13edd20de997aef
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:32:56 +0000

    mass middle twist centimetre after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
