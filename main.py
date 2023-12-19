abd81826496e11d68da45f6eb163f9137311b6ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:37:05 +0000

    admire birth hear tip nice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
