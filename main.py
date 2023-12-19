83b4cf60cfcfa00c41adcb8e72596fef933a0ba1
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:25:17 +0000

    all respect stretch 569enjoy add

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+def sort_numbers(numbers):
+        return sorted(numbers)
+def find_max(numbers):
+        return max(numbers)
