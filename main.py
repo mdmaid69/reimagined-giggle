09b7c67d64bac80b2354df0816c097aa34a4a3df
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:01 +0000

    hold summer software innocent undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+print(sum(range(10)))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
