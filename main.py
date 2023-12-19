c1d4b71d938383f7754f03fce6f51923c007ff79
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:16:07 +0000

    himself still to cruel quarter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def multiply_numbers(x, y):
-        return x * y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
