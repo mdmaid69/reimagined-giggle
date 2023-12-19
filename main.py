c1efdebe76d162839ba99d4e5f9a928c42b2b8d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:48 +0000

    large rather pressure clean pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
