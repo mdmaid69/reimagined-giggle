6b19084c14a06112983dba90c3053f0c2f1ad3d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:49:33 +0000

    birth flight job exchange break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
