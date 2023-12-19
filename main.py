f04fc1d54de3c1c637292110402a9f8fed8bb546
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:30 +0000

    advanced instead hold big often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
