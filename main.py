c80d5c6de370d609d881d9cd2ea158ea50964ed2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:25:52 +0000

    tool government quiet butter willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
