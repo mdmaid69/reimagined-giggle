643c5486009b0c40a8e147c91efb6787fe0e794e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:37:54 +0000

    add far there impatient guess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
