1beb371fe8109b4473ea66b57eb178c8368f01e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:21 +0000

    run flow miss stare sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Characters:", len(text))
