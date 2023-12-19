d16f573a255182329855ee7c8062f015c7339507
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:31 +0000

    load thorough gentle slide goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_area_circle(r):
+        return 3.14 * r**2
