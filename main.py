f74138d44e049dd07d00f4831ac3fc6b3e1f6a3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:37:43 +0000

    boil exactly mad muscle arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
