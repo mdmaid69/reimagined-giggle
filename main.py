fdebe8764a76461192c40ebc25687ff385594b94
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:00 +0000

    on production extra reasonable exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_typecode(array):
+        return array.typecode
