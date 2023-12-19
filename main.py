7f725c2d1ed900ff8102888b012e15cf65812789
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:33:52 +0000

    release new quality able normal

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
