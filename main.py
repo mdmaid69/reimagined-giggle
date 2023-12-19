be20faa573d80e42218a86f0780d2c24d3f80560
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:34 +0000

    too lesson Pp mark ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def add_numbers(x, y):
-        return x + y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
