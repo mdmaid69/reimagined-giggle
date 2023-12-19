4cf7cb18a534bce90c97172dbafba21764018fde
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:05 +0000

    pause slow wash backwards call

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
