db1928d6f320ec0b2c1e506162fd4dee57395f5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:12 +0000

    interesting around wing opponent stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import collections
+def create_user_list():
+        return collections.UserList()
