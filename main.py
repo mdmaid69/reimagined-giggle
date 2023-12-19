fcd2302341045e9e4cb858f824330db7e51589c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:24:37 +0000

    collection unite sharp trick upset

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
