dc200e2a43d270cffba61be7f388dcf723477a7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:57 +0000

    branch fail this before indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
