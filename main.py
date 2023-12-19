a11a51c49d7b83e82dad1b08b41dc88a68037930
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:12:49 +0000

    punish minor else Ff smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def subtract_numbers(x, y):
+        return x - y
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
