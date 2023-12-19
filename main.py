656966f7cfad29600250e1fe5423286143920eb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:27 +0000

    about plenty whom lookup appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import numpy as np
+print(np.array([1, 2, 3]))
