bdb90709412475445b2c372a82621cac3f8e6fa2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:36:44 +0000

    especially protection increase finally finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
