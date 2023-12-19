bcc8b7054231b409ff4ae51907489c698a7ae6eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:09 +0000

    goback(to) but match interfere project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
