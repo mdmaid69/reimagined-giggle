ec48f2018a47388154c2a8e34132ecf0b67dd7b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:06:24 +0000

    skin talkoutof influence nothing dry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
