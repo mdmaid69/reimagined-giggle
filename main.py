0bd217fea4d7401739f714f2e0976b369024f979
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:24 +0000

    path light geton through shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
