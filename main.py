06dcef3b66fbf0b0e99ae05863d3f69989745d8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:00:04 +0000

    situation teach nothing date accidentally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
 def calculate_factorial(n):
-        return math.factorial(n)
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
