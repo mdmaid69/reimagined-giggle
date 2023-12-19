831d2db2c37b368a801580efffc9da39cc12acef
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:22:43 +0000

    stupid feed quality left country

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
