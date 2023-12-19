41ca4c2ea7c9585063d6c7aca9ec3b92114dd521
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:55 +0000

    laugh worst break common fairly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
