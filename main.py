6f90d93e5683e2cb3aab66241e7f0243bddc9259
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:50 +0000

    language notice conclusion pronunciation funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
