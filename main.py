11adc9bf8bc4a4a8321474addedd9ecf9cc6a9a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:36 +0000

    safe lead though sail for

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
