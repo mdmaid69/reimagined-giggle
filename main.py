5c4af03062a726f91c482e54a794466e2bd6c308
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:11 +0000

    possibly Mm ready development flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
