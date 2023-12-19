6344e1c33729a40be3cf1afd71d8405e36db8de0
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:03:11 +0000

    sharp none nurse gate home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
