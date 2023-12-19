c2f9b540844dc45f7262ee4543bcc519fea7545d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:09 +0000

    favour second allow famous action

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
