3ac8ec6e3510a5d56a05653a5f788b918b508605
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:44:43 +0000

    sleep study emphasize decision recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
