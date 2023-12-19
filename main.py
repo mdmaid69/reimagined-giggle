8079d9ae4950ed4b3e663631a53cf0d43ee0d96b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:58 +0000

    powerful building connect experience wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-name = "Python"
-print("Hello,", name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
