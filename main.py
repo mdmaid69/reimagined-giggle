f28d11440437f76b8af599cfff646843bc9bba23
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:44:03 +0000

    add contents example not spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
