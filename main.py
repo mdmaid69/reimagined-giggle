f798ec90a7157f390f7296b675c6c6544aee2b56
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:33 +0000

    show past instead swallow meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
