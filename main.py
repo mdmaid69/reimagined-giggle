bf06a710030829405d8cfff0c9226e1292f2fbfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:30 +0000

    bottom tidy more failure start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
