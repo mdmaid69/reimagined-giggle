ea8bf8099d77715a7e73b90ed2b4e4138a1b5c9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:25 +0000

    healthy level shut in fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
