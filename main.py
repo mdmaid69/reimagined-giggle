43756c8473d77fbd9554fd742cb982056eadb632
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:47 +0000

    expensive strike desperate young lookafter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def add_numbers(x, y):
-        return x + y
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_int(array):
+        return int(array[0])
