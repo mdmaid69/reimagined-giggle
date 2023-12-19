a9ce8d98e8d85ab789a62346e01b4b3667985ebe
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:21 +0000

    tight Cc shout brown sensible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
