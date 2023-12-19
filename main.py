08a0f8a42f3b557e04dadbae2e5c6bd9a117c9dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:39 +0000

    information connection embarrassing similar will

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(a, b):
+        return a + b
