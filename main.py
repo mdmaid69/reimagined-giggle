5386f0ad405f6518c60e85c6e0962d8cabc9938d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:13 +0000

    obvious valuable exchange trousers give

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
