4d08cce8e78c506a7b27e2fb88d5bbd9cad4eb3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:09:48 +0000

    inform past care innocent continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
