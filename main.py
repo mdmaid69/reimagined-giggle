07324902228aac648487611039bc3f82a9eb8102
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:34 +0000

    respect history trap narrow entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
