8e49e45a5921b9ceb548e0e29a26974d82bc486d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:43:14 +0000

    herself air consult lookback(on) trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
