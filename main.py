b86c11949521ded33be09f94f1bfebd1e0b7cb2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:45:51 +0000

    herself shelter workout shadow too

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
