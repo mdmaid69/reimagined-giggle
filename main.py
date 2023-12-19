959b45fe4c84ad67a23a0cf0bc40d98e3d740c4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:00 +0000

    average form lock runaway funny

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
