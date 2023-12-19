39d8d63286ccfd3587d3d6dd362833645b3757cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:47:40 +0000

    better slight survive dear picture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def add_numbers(x, y):
+        return x + y
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
