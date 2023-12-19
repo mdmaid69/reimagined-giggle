bffd73727fe1958bb1d2300c87627ace9baf7dc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:46:36 +0000

    sweep healthy sound target spray

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_pressure(force, area):
-        return force / area
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
