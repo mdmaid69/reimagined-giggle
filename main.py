79ed13fd70f7202edb2a4aa37410dc60f1afc1f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:50:02 +0000

    imagine board plenty cruel food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
