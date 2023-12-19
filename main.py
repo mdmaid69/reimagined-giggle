be7bf839377a567be8017c77f62a599108fe2897
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:51 +0000

    swimming rank sting tone spend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
