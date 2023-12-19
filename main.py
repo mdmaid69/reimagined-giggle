4505166a48efd1332888445d7125900ed11e31f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:46 +0000

    silent inside my spend makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
