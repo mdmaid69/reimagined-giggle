71555c87ea76fae5ed1064b6d8e9df57479282ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:37 +0000

    handle unhappy giveaway pull undo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
