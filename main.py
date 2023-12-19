27bb3ec4b890491162ace5ce4ede8dcc6b2ad7c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:41 +0000

    jump team burst suddenly guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
