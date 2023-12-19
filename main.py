7a99855fb5b5639431c4587f3d8a78ff0a8064ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:36:40 +0000

    after doubt answer difference impression

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
