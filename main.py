f93af0f7512057d1072f0f1249e610243b258354
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:06:38 +0000

    morning content impressive talk satisfied

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
