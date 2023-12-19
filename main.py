1772b11adca7e596f607d20cb1116cea2890b764
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:09:46 +0000

    dare 567enemy ilovedicks excuse Nn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
