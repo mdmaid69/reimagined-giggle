31a8b989e3e641b9e70b62a521dcdacde4d5ec21
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:01 +0000

    mind type software regard head

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_power(work, time):
+        return work / time
