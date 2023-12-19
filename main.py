b55aff0add76e761aede4d344c55578122ddf2f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:59:09 +0000

    warning deep furniture book man

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
