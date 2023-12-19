374d04d64356e8c4446226af3d621f1ebb7e655c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:55 +0000

    argue second winter rid slice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
