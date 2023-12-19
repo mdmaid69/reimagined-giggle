1b4e203d38d4c4b7cf0df8e7228d33b9cc8f5781
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:42:55 +0000

    team change getaway voice ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
