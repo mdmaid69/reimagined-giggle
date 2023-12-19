1469d90bedc3ca09bd411a86737989928856aa12
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:36 +0000

    illness turnover raise mean pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
