a9348ffc2851e54ad946cda087f4f1e19b7f6c85
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:42:06 +0000

    swimming begin reserve guide please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
