d618e43e2298d589fc89644fbde826db43233010
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:23 +0000

    large motion as bite argument

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import json
+print(json.dumps({"name": "John", "age": 30}))
