109709657d2690da0588ea928f04833c25bf4cc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:28 +0000

    interest public shopping human damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
