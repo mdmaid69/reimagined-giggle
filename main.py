98eb6fe9a5930f213ecfcefe82b652eeae44de77
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:19 +0000

    cross turnoff suddenly some view

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
