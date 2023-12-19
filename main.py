f208ec3dbc703618184f476d15f3609703e46a9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:49 +0000

    backwards aim hurry flat makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
