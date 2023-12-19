50912faa922654aee25f0e3cf077562258792b0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:34 +0000

    welcome hit container unhappy idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
