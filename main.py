ab03d57a17de6ed4ab120a4abd4439d4d5b0994a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:01 +0000

    busy check weigh deaf reserve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
