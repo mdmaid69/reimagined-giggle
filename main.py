60b3ef3f4eb73a773d7f8b1abe1ecd1172a365e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:46 +0000

    real shopping clue married contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
