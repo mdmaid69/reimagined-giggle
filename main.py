82c5e93f1c76e8c07e67a4e82864e281287fbd11
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:11:48 +0000

    worse reduction poor closed night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_itemsize(array):
+        return array.itemsize
