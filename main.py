f15eb2d9e36a3ed5b65ea61d47e94bee739560dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:00:40 +0000

    voice accidentally unlikely card official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def sort_list(lst):
-        return sorted(lst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
