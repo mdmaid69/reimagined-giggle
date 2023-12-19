2b12868ea74ecec5d7a0078d8e0f925e44210347
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:54:31 +0000

    dress ask on escape variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
