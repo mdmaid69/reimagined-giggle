e0ce295506227a771f7bc5484f5acf2d08f11e16
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:16 +0000

    pure shout question comeoff shadow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
