6ea62d20a17465aefef87483dd06c690e3bdba44
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:40 +0000

    detail she bury present lower

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
