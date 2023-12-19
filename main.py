ed7276b5ee5c008d5cd8034bdfc84308ffa7e9e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:59 +0000

    serve plain whisper although clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
