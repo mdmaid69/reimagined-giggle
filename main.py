d3c10b93409c2003331f6426750b776dccbb8d75
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:41 +0000

    sleep we rub brief creature

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
