1efe88bc2bc291bf0dd492aed38f517ac00c7b87
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:10 +0000

    fresh through oil pain minute

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
