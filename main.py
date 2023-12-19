4ed71bd0c6c53ce738ab5dd3324af088504ea633
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:11 +0000

    them through wash sharp Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
