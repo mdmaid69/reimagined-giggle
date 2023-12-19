a041adfbed212b57b8a03f9f4467664797bda2b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:41:36 +0000

    actually pool evidence mess present

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
