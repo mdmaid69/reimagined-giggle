c339431d7dddaa4fc92ee9188e61d3400fdcce76
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:16 +0000

    spray motion actually degree modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
