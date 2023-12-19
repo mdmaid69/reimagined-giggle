6a61b26c5c65d8316cc82427b082075d1889387c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:16:00 +0000

    brief announce sign interview exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
