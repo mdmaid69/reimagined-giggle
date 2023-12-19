51eaa1b649806336b4610e651da8b87033deb1ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:56 +0000

    turn apparently during tread 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
