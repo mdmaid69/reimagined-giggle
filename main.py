f03e2c54a02f85faea2143d50f1af637fbc2d934
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:33 +0000

    telephone discuss art polite very

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
