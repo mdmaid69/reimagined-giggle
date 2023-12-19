a3670c912863fde05ded03e49dd340a40c50433c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:34:50 +0000

    argument complaint ball terrible ashamed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-def convert_to_binary(n):
-        return bin(n)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
