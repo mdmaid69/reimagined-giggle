5ee0a2034754446b66d74e738e784a8d7c688a5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:20:05 +0000

    everything instrument ability else kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
