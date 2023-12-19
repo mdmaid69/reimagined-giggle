47d4cf38f313f912cd71ef42f465fa04f355dbf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:27 +0000

    arrange angry convenient abroad although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+print(json.dumps({"name": "John", "age": 30}))
