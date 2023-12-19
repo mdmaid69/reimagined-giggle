ed4ef2f426534f3abaffb15eb1707ac817636e92
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:10 +0000

    interested boil sensible whisper beneath

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
