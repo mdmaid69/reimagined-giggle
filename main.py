b2c96073bec07b2154b27b02be571b15fcf6ec7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:52 +0000

    modern bored harm squeeze sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
