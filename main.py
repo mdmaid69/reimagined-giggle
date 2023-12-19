1fb4ca099d803626a2f249227d2e81ea0ba66172
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:56:56 +0000

    match introduce say keep avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
