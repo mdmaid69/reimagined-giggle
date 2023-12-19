549445a7ae589f3050a247cf5a8fd4b5726515d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:02 +0000

    spare anybody climb talk 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_octal(n):
+        return oct(n)
