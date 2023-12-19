1dfba2d39b90d1bc6fd0b4c0d26465e3a8ddde43
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:27:38 +0000

    me original good task thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
