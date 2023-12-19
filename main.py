6c768740bd56afe54c93862e5b965c60af21691a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:55 +0000

    hole prevent appear which drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_string_from_array(array):
+        return array.tobytes()
