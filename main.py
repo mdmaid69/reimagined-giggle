e67102a26f00eb90460be22f70210850cd8a4bd7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:47 +0000

    any tool tread display foot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def count_elements(lst):
-        return len(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
