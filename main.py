d72ca2c4b650df14c58c76b1fc94c921607f9819
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:57 +0000

    immediate murder display home match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
