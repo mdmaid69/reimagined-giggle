ccf6e6fadc26fd7a0345809d43b08e18fa7000fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:44 +0000

    phone bar exaggerate thin over

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
