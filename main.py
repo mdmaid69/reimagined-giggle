6dfd7fb8e50bfcb15e7442b9c4343a9a2e10d8bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:34 +0000

    treat divide room woman chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
