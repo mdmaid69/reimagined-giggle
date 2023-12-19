e629adeb9fe723e5891cfe002a4feb011e251c43
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:18:56 +0000

    change wide wherever language achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
