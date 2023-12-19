5b21172a353d0ca9551e8eb7e391fb1acb20fb66
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:51 +0000

    both pupil statement dance convenient

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
