e0a9f4c278543bede711f8d82e1f16a5ce74a32e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:09:01 +0000

    bet getback ours stand quit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def get_array_item(array, i):
+        return array[i]
