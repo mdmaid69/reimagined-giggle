c965a8143d7fc22ea2c0d36ebef4731d78a46dc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:52 +0000

    pair count shelter comeout cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
