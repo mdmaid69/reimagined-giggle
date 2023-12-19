6de115fb20e9b831f181ea0ed7c336fbb2734656
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:49 +0000

    expect goodbye supply road free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
