500d03e7f74dd529aea8c6a052759eaed83c9db9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:54 +0000

    room be tape reduce at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
