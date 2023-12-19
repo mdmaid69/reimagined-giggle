a00786c85c56468140e3c9bfd2b6575f650438a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:07:12 +0000

    regular excuse poor single mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import time
-print(time.time())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
