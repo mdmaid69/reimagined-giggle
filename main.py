8184b87f0d351d8576a5bdb5be78f60ab32f9661
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:48 +0000

    surprised mine admit invite effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
