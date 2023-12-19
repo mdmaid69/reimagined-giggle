ef58be1855b4648570f6f4a76a5bf62de09be5f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:06:23 +0000

    break account more shop call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
