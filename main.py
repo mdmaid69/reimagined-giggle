09ac42f883f454f0e640e63c2fcff0570991eba8
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:17:12 +0000

    makeinto sleep fact forever invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
