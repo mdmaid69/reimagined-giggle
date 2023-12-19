bb4626ab8f1065ccaaa43c38bd566d02ca5ed110
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:51:59 +0000

    exhibition unlucky dark Uu drive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
