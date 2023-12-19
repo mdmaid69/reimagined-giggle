e0c8febe0cba5b3bb72e0ab78249fc45290f4b8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:29:54 +0000

    talkoutof communicate level sort sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
