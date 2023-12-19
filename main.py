e20bc945b8f604c7c075ecb280e36968497b5d45
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:56:49 +0000

    usually hole worried bill distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
