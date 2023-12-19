2622e597ced3a85759fcd442b25c011fe40d95f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:41:46 +0000

    matter suffer press never hollow

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import platform
 def get_os_info():
         return platform.uname()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
