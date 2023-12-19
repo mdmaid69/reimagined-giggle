005ec9d110c1b7519760bd03d18796f44879e2dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:34:06 +0000

    above compare paper fire holdout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_memoryview(array):
+        return memoryview(array)
