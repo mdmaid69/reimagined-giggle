e80febaaa7e5dea2cfc03f5cca3499669d223dc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:10 +0000

    colour inquire office regional dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
