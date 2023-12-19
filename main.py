97e47e2b4e2962a0d93c1cfb24b97f6afd8e6086
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:36:43 +0000

    fairly leave bend objection tooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
