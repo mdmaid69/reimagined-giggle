ea399a7be8b9008ec8a2a22e824ea4d52247d909
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:46:18 +0000

    wall create range content no

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
