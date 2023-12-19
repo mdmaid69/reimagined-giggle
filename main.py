a862de697e49626741c77cf4f16ef447abee5484
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:20 +0000

    hard suck nothing sharp know

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_acceleration(speed, time):
+        return speed / time
