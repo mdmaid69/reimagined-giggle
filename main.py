e8ddc693aa5f96c1f5b0927b4fd9248ec8f827e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:39 +0000

    I admit equipment night passage

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+print(sum(range(10)))
