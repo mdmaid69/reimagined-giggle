2041547a7430bac85f1ef727e87c4016bfa3ad9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:22 +0000

    natural Zz size exactly evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
