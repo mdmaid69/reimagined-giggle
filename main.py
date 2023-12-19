3c141d45a4fda394af095d048d9c79f9ff10383f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:25 +0000

    threaten problem hello bad silent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
