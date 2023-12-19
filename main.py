a3841ebb6cfb9b8447d58ccb7b0cfa39d6d682d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:38 +0000

    nothing attractive blame wire cheap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_bytes(array):
+        return array.tobytes()
