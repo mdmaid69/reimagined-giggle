3b8ebc94df21a568a6b9dc1b6b68f5c5ae50a188
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:24 +0000

    connected tradition develop business record

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
