37013b935763d51c17413e03374df397e3ff8dfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:02 +0000

    impossible great possibly connect giveaway

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
