2ab77c3d55ea7f773594806f2ca05c5daa3524de
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:11 +0000

    runinto wrap score possibility desperate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
