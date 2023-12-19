900ea5125b8fbe10c02d97d44a723090e2944d17
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:40 +0000

    wish bottom nurse basically emphasize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
