52dc2f52a57b7c72cc0d1e508cd15040a703c824
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:07:37 +0000

    present over tip food consider

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
