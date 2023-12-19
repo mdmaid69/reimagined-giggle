531228fa94e59a3991083365714ba96fc8810ee3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:13 +0000

    temperature middle little thorough value

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
