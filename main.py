1eb01ebdbf156b101520da62b0244f7a2bcc3330
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:01:20 +0000

    however object quit drawing hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
