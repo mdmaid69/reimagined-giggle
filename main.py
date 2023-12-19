b38edc63422c792671883546a8b246527bd758a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:12:30 +0000

    possible butter spoil crash surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-def exit_program():
-        sys.exit()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
