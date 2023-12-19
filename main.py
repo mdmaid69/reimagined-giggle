654ba9ce4bf5b327e6db2c090f6ce0b517da1ca4
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:58:00 +0000

    proper for disgusting worried ride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_acceleration(speed, time):
+        return speed / time
