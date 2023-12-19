8d7d9b42e770ee199e8c633aa181849af6788d52
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:34 +0000

    communication lay turnon congratulate job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
