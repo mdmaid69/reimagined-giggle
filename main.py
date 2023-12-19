fc1ebb9c0ae73d886021929c62e2de7644cd7285
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:27 +0000

    hurry both corner strong push

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
