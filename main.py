cbee5b17447b95c4803e9c0025ed45e4ee3a2616
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:40 +0000

    division scrape pick upwards pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
