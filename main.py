728f85430423b69031f893ed7ed875b12bbddb07
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:57 +0000

    turninto band damp free shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
