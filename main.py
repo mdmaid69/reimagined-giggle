cb27f0af16db3d2cdb62598f7cdace30421fe39d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:24 +0000

    watch breakup agree yet tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
