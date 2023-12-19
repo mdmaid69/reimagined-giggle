4871cbc397ccb080cfb1c8073cef53185b8a1ccc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:54 +0000

    away early unlikely tip yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import getpass
-def get_username():
-        return getpass.getuser()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
