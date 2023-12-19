f8ad81a51a4e0c12a664ada46e4eee8033178ec4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:25:47 +0000

    glad woman instead nowhere cough

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+print(os.getcwd())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
