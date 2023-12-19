ec13b9fd74066cf66e8ba0e98ef795fb5f67c343
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:25:18 +0000

    innocent connection argument importance neither

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
