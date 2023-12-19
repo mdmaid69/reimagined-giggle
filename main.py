277706e7c252b5d0fd9fd30c247b1dd37378c180
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:10:20 +0000

    his march off major ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
