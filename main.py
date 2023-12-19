140377dec3546d8d3d05fdf93d6c13ea8f7f1155
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:42 +0000

    571enough attract usually keen welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
