dec70c9e9b92674da5a9869de1c3a5f52ab56378
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:12:56 +0000

    inquire set education copy serious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
