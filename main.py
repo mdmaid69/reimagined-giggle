fdda99eebaa55efc54b4f54c0e6641b02f3194cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:25:09 +0000

    believe chin takeaway high behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
