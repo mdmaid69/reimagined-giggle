da3d5d6bba434cfe3aed7378566b537c1f1883f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:43:21 +0000

    medium stare translate understand swallow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-name = "Python"
-print("Hello,", name)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
