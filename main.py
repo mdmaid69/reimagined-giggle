1574694e75bb90d5310ef4a0352192d7c59b8584
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:13 +0000

    deliver run experience admire equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
