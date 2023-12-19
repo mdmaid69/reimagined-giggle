8b63cec0b8bfe4942b99beeb3f9fa8fc448a13eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:23 +0000

    bet face comeout funny ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def reverse_list(lst):
+        return lst[::-1]
