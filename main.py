8b19cee0698f0e0d3e5a0a1815bf7e05036bcf69
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:01 +0000

    form community nerve whole talkback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
