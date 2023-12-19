20244ff6d5ec6987da49616f462534847ffc97c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:36:16 +0000

    shrink disturb wild cry cruelty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_unique_words(sentence):
-        return set(sentence.split())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def square_number(x):
+        return x**2
