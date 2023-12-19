2bc4ac75f4ce39cda5383d1c0eb2e151d5851720
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:12 +0000

    help ring cough yesterday content

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def count_characters(sentence):
+        return len(sentence)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
