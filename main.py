c73db0e2862b898db1eaeebb5beffc57bdfba970
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:38:44 +0000

    head final moment cook base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import time
+print(time.time())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
