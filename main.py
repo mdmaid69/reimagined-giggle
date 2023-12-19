69461fb3c942c138b4dbad75594d6809cfb338ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:13:22 +0000

    worth activity takeover next back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
