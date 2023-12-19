e559b7b60bb53803b4d3492a927d8114195a1540
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:42 +0000

    really office wipe fierce Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
