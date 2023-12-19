776aafc4f87b8a76428f050a022f6c55f563e8e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:49:15 +0000

    position closely rid of sale

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
