7be6643ae7e6d13f741fe5a4f4f004e8f0e0784b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:41 +0000

    include business performance reply 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
