783438980d7fcdbc0bb37a0fbe33d113091f3831
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:32 +0000

    turndown week bottom next further

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
