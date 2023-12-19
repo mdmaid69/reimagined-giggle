e842ceae83c060f9f9d266529b5e2e6cd2241752
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:06 +0000

    report loud below effort exact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
