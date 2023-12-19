f98f92310634a6ea3dcb09b98fc4332ee46cf14c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:16:40 +0000

    interrupt twist attempt genuine even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
