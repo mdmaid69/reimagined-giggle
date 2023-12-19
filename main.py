aadd77dcff4ac3fb3c99492f65a78414df26b655
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:14 +0000

    circle Nn print else record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
