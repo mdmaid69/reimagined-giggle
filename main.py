0fa9504704242fd9e65ece819f0380a09257a9fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:19 +0000

    base across little precise centimetre

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
+import re
+print(re.match("h.*o", "hello world"))
