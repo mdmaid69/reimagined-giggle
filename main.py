3e45523b13c10b95a3e181fae9915164f2c48daf
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:06:29 +0000

    comefrom Gg university you mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
