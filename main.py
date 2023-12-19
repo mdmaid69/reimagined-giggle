c49cda2abebada782bde96859ed3c311a108b6c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:01 +0000

    little large damage news loan

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def is_even(n):
+        return n % 2 == 0
