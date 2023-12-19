e519116113920c1acc43d2b679f75ff5f2c1fdbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:25:34 +0000

    patience ordinary total lot middle

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
