5fab725ffbf7fc63d8a4a7e41f0d6e61d0959862
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:42 +0000

    programme would form bury process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
