89470ef8b25fe93fdbdca95462a7d21383904383
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:05 +0000

    turnover agreement figure hit middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
