7078a96d6c94b4ea840c2d4ddda3dedec847469a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:12 +0000

    wet spend load comfort notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
