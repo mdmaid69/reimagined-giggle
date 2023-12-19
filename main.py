afcc6cb8b0b8ada0769973c952d1e1ac46b8529e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:55:17 +0000

    supply better sight drawing whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
