c33d3e94bf47edbeae0e9f91bb5d59c51dcd8fda
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:19 +0000

    fear country simple guess giveback

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
