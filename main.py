dbbf044ef9504438ceee37fe74e770c01e0982e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:51 +0000

    married price defend attention dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
