64ebbb94708ad841abf2c8b432be848bd2e5a58d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:50:00 +0000

    motion well ago wander fetch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import random
+print(random.randint(0, 100))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
