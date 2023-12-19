e2e6a29b9639dd66594df1d769b92056c10b6412
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:43:37 +0000

    its back goout nigga guess

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def convert_to_octal(n):
-        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
