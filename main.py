165d42356fa5d9bcc67a25fc9b9009c98f642754
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:45 +0000

    class wash acceptable artificial touch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def clear_array(array):
-        array *= 0
+def square_number(x):
+        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
