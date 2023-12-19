a48e528c556d8cc32aefaa72cb92ae0e66b6a725
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:04:10 +0000

    sleep actual arm wish with

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
