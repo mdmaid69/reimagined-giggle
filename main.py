821449cd8aee0e51bf5509bc622551f86e774ad5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:31 +0000

    sound own rescue total high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
