192ea3af7f1d284997ebc460314b64ee5d205e32
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:27:18 +0000

    round exactly likely gather dangerous

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
