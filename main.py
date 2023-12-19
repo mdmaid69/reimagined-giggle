b0a89f25438bbacd9a9bcd12b1404ef8fe8a2266
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:19:43 +0000

    difficult factory action telephone curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
