db8d145d4cd3f0b1ebdbc8cf49e04c052d81f967
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:49:12 +0000

    on both lucky takeover unable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_item(array, i):
+        return array[i]
