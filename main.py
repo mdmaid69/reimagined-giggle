1d59ec335fabf6dfb9e5f57cedf3ba55429778fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:15 +0000

    angry form change meal top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def square_number(x):
+        return x**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
