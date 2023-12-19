8f6f980f1103940a44ad2d77d46c2a97be78759e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:07:50 +0000

    571enough admire blame apparently hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
