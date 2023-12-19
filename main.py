744910585fb30e835ba53ba9e4d74a3b99e82ffd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:36 +0000

    suggestion sort last sweet forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
