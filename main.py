4c62bc6a5d206b2c5dfe36b563b3ecf01cb99230
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:51 +0000

    free set shelter broken beautiful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
