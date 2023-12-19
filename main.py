7c534574084e46838d596abf5998763131bb0b2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:07 +0000

    takeback excuse bath vanish final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
