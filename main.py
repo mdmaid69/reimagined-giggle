589f0ad424ae6af4dcf6f503ad1d15d181c9eff1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:28 +0000

    demand emphasis immediate depend month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def get_array_as_list(array):
+        return list(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
