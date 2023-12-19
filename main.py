baf16e94abb3a9f570a1635c4f2669c133f301d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:05 +0000

    happen suppose strange tell dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
