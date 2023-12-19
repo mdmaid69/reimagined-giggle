7ad0f9f16440e569bcd29373fda930e8b56ac3d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:15:53 +0000

    neck separate defeat getout shout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
