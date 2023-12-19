11ff291a89571c43953ca5fca9cab98c7baf7c10
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:54 +0000

    repeat steal thick beat weak

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
