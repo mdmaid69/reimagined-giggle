2266f5758130bc7249cc761809f0fdb4c3394b0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:45 +0000

    572enter bet anywayanyhow tip crowd

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
