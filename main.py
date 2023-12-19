4d03cc6ba7a797c77a0ad0ae4f1866502da35608
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:41 +0000

    aware recover offence spot possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
