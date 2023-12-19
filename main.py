398b59ae34ed92bff0dd39d8b9ffba74126b4d40
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:39:25 +0000

    comment rain beside visit key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
