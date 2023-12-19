383bd79eabb13ceea43781cc3b96d6e56c1ac3d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:33 +0000

    as really possibility before wipe

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def multiply_numbers(x, y):
-        return x * y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
