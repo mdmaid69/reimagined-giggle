eb309e2f331d2bbf2bad91e86dee3017d7ececc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:31 +0000

    release winter another recommend actually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+print(os.getcwd())
