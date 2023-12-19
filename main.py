00e3c34c25584db82d4426a78bbff028951d919a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:07 +0000

    honest care more else stare

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_power(work, time):
+        return work / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
