04806c724dc146b83a9ba064c54eec13d0e612eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:29:53 +0000

    hear skirt breakin talkoutof team

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def find_min(lst):
+        return min(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
