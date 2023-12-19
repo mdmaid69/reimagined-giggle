d69898aff0241f02e2f45e30d8d75cd37376d774
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:14:08 +0000

    Itself instead computer pot length

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
