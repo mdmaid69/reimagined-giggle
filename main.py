527583570cebc84a7cee488acb538da3fb9b6409
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:07 +0000

    stranger from bringforward spray aim

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-for i in range(10): print(i)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
