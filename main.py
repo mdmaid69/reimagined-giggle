0db1cacaa8ccf7f8ecd09ce9d6970cc3a0f99160
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:07:30 +0000

    deliberate and act arrive basically

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
