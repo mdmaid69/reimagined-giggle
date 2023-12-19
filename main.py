f46ca2f70bee8ec0eb7a055edd38f8f5ec474974
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:24 +0000

    maybe national close light around

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
