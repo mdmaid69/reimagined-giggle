e6bf60fcd9deb5a2d2196f90249d7cc06c3056a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:03:00 +0000

    almost nowhere imagination print run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
