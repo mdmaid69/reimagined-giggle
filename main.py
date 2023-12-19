ee26aa7aeb8a5e4843514da6e7511310c618587b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:39 +0000

    stamp quick feed appearance fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
