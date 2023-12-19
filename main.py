a2c655c0aabe3dbcd0a5dde1aac9251b9964cd20
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:43 +0000

    minimum production bother brown defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
