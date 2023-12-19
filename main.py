7b310962aefb28d1c9749ffb7a54bc5c962a8914
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:05 +0000

    appeal well pure worth payment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
