aca6438a8ddb0688db0d2d624010ac45e4238bd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:51:34 +0000

    season something body Pp roof

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
