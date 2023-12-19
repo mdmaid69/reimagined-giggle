686ade1a1c57bf8ea87850ac8b3b43578dd9ce70
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:23 +0000

    lookback(on) share where influence practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
