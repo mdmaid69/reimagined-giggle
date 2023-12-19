a0afb6b63f62f24315a1894dba007e14acbc6eef
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:28 +0000

    glad luck total difficult born

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
