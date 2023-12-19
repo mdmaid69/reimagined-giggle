051b0b597a2c1f887dc52c50bbb87678f30fe372
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:33:31 +0000

    laugh protest situation instead spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
