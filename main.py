0ec56980e400a473881b94e7e35f3064ed70d829
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:33 +0000

    environment remain gooff production hide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
