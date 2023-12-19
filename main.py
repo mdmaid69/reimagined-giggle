c6d6ffef592d13888d2a4c70b977d26b9bc4618a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:41 +0000

    cold pour amount Oo visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
