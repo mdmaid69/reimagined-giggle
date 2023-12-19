d93d54ff125acb33c0b64a8c4c465db33e8b5c33
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:09 +0000

    on Rr problem spring report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def sort_list(lst):
+        return sorted(lst)
