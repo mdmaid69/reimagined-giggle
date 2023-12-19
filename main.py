ebbb6e0fd74b00053cb03e35efb01319d7447a17
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:27:32 +0000

    attempt university dead stand under

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-  def square_number(x):
-        return x**2
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
