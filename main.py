cba2ef00a9ac952186f713e4b2ea09cd3c3a699a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:22:57 +0000

    turn better type undo bet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
