8ec950c83981b96543a9d9ed785b626b9f84beb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:51 +0000

    over home quarter personal well

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-for i in range(5):
-        print(i)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
