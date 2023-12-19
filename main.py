c9703fedfbb9c542fa9a4e729b5fb7cb2395c578
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:54 +0000

    vision hard suckme drive acceptable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
