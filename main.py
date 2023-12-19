04ee3c50a28e0a0155785e873faa2553694e9f2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:28 +0000

    sure several family nor precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
