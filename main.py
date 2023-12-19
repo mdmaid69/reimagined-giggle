5b3dcce1d88b7b9f1408f31ef96695cff642c58d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:25 +0000

    condition report important spray form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_even(n):
+        return n % 2 == 0
