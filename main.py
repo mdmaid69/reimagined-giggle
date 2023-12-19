5e2e1ee12a97e79eea19c74a27d823d9b8a22ace
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:59:45 +0000

    escape stretch worth butter city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
