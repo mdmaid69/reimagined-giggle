8db043a9602c0fe58ea6d92a2d332dc0f7d4e859
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:46:24 +0000

    confuse pattern content large win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
