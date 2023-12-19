b3b193cd4f3233e6a115123849ffd4e2b3129095
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:45 +0000

    choose chase arrange before rough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def count_elements(lst):
+        return len(lst)
