cccb65be998872d89012be7c2b9f963e743d2900
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:01:40 +0000

    rich maximum challenge path introduce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
