2567407c6f39987f39fdfc315236f4af3ffd9747
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:42 +0000

    blame pump hurry form spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 import array
 def get_array_as_dict(array):
         return {i: item for i, item in enumerate(array)}
-def calculate_acceleration(speed, time):
-        return speed / time
