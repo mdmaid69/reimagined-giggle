6d98a86b7ff92968dc970ebbd84bb421c858b6b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:59 +0000

    letter decision somehow different thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
