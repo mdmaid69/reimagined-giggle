20872cbf05240269567f26ba8fda94faf3d94d31
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:28 +0000

    moment show move black obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
