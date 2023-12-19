4a4f98459090ccffb40f4af5ecef560d1afe87d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:40 +0000

    block respect brave along special

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
