3a06df2ec1d9039b4b2c35a38d61dd1386f9fba6
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:45 +0000

    convenient love building mistake ready

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import platform
+def get_python_version():
+        return platform.python_version()
