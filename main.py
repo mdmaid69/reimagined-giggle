caf0f6faca08721f58f4bf36dd49e060a3ea9fd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:06 +0000

    though sense how reliable now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
