96eadeff08756da0fcb9f97cd12361b4e5143a10
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:48:46 +0000

    murder modern roll surprising finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
