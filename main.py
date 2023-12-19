898feac91f242a29808147737bde373a90808003
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:51 +0000

    worry tight lunch narrow spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def extend_array(array, iterable):
+        array.extend(iterable)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
