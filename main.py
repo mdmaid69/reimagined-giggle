195980110c81c6043b5cbc0f8e7d9ef089f58973
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:52:54 +0000

    towards runout increase breakin embarrassed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
