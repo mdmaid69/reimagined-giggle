bce0cbf4cc95292947fd6f34f3cf37ffd56dafbe
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:10 +0000

    from its Pp congratulate hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
