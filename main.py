1e48db4120e595ff070bc086c200975bc9383539
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:53 +0000

    thing team whole affect suppose

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
