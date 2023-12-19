d2606a088f0125fb02188328ea54454c8bc72b5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:15 +0000

    pretend defend workout method telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
