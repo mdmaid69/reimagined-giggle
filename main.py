5af68212c40076e3f917e4cf97556b785914b7c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:51 +0000

    write sort peaceful offer track

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+print("Hello, world!")
