eaf06a855c08a3e604eb7fa6fc83bd65078897fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:40 +0000

    off confuse contents bet often

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def sort_numbers(numbers):
+        return sorted(numbers)
