a083ca4218b27a19aca5bceb0d7376c565df159c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:29 +0000

    concerning result wing sudden low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import os
+print(os.getcwd())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
