104ab77c366a126f9980492174181d1de3360677
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:47 +0000

    breakup rush priority cost angry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
