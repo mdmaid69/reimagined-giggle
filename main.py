7135140f291865fa389c25cc36cd60be1c7eb939
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:03 +0000

    split they towards regional underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
