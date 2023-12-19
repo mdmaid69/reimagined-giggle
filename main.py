0c28b67574a5ca31a2a516eabeb9e8ba9f880ae1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:09:45 +0000

    fond lookup rain present test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
