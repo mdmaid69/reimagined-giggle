8e09569d776f559186a67fc3f7e533127b0cfa18
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:26 +0000

    slip where bear clearly fashion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-print(math.pi)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
