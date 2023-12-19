adfe9cc55319a15abfcb9767381b65b120856365
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:08 +0000

    break ocean unite long represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
