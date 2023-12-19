5e773001e0559b717e8c824dba2bb02eb6a7c291
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:48:23 +0000

    brief share wave pretend thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
