1a4e2aadd037d3e956a2596c9f8cb23b3755f3a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:44:55 +0000

    have wherever read reasonable carry

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
