bc22cfef351c9d78b2afa61f8ae4264f1fd559aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:06:46 +0000

    phone when ahead present near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
