35b3e883ffdec04cc2986a3f261116919b069d07
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:38:27 +0000

    possible skill smooth dare abroad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
