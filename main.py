be2d202af220d0e59d3df872156297c34ec4aff1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:33 +0000

    fire wear become other may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
