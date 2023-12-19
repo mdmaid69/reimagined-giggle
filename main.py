063966e0d17f83b2a4dc8a9a62f744679da599d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:53 +0000

    door task lonely naturally spare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_min(lst):
+        return min(lst)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
