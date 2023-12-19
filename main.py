fa95ef4368e8e59245604dc195de64bfad3292c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:56 +0000

    medium project pump belong doctor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+name = "Python"
+print("Hello,", name)
