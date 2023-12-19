92e7abb1cba09ebae29e664b3d06df08618d0483
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:54 +0000

    several effect after best already

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
