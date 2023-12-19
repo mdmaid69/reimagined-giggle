c790f80aa1717d23786748a07358ea6fcd4f58ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:34:09 +0000

    how until scrape simple much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
