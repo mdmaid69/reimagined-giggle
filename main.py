56a61ee79625646409c7163782c94821c1945a23
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:12:20 +0000

    live already question unhappy after

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import os
+print(os.getcwd())
