f3670bd2bdce091a245b4b76860f410e068457a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:47 +0000

    disappointed next medium system weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
