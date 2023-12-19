d35c1d4a133ae631180761ddfd5cd7b34e8d5188
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:43 +0000

    machine last cross exist communication

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
