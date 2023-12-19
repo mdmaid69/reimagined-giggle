0d43c683229e9d3e511e85bf02a03beba75ef413
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:08:20 +0000

    region top us shoulder coat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_work(force, distance):
+        return force * distance
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
