fec55120b739109db07a6244e095d5c62bed9f17
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:42:17 +0000

    succeed begin both jump skill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
