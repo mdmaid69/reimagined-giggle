f39c9b34531790cade01fb2bbefcf92ae5d3ab7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:29 +0000

    ear OKorokay event content approve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
