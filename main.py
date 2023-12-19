7f65b953bdc6fb46c17b0df6579abac49f803957
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:37:01 +0000

    pattern fight operate Mm right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
