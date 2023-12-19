789aea5831ef8a8742aff9096a918adacf3fc093
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:22 +0000

    unpleasant fly reject moral load

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_str(array):
+        return str(array)
