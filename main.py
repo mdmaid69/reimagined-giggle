d7e526369a986927ed23de4385fa7e4218e19a1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:53:51 +0000

    rest roll notice excuse study

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import tensorflow as tf
+print(tf.__version__)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
