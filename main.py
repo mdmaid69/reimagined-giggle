322163852cc35199d4c710cd88335f722f83f121
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:08:07 +0000

    weak tip quiet arrangement almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_palindrome(s):
+        return s == s[::-1]
