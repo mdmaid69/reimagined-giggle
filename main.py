99ca24506f53f24eb0392bbdc6e6ae537e1bff84
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:24:02 +0000

    threaten really complicated maybe juice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
