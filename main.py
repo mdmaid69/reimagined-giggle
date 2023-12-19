068a53ccb9614afa4f0ae500e5250484b38308d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:48:19 +0000

    never anyone that goodbye fat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
