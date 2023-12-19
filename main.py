1d66a5bb87579e5e3831b8c0662f1e6f9df58bce
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:38 +0000

    advanced regional safe comeout instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
