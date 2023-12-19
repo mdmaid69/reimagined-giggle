a12340b5d334a11668e13ffb3daf3d03d0f6ce91
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:30:28 +0000

    inquire fence real clean depend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
