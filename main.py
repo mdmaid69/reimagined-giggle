09bbfc7490cf652e0af99821c5c6ff543e10e9ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:20 +0000

    narrow temper available holdup demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
