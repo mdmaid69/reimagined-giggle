a78cf1ba60f788536ab03e021ff3dd5ca0efebd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:04:58 +0000

    platform distant us refuse taste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import datetime
+print(datetime.datetime.now())
