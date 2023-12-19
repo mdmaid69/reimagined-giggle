3a7295af58dadbd3c1aff9895ba3c5087846ce28
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:22:14 +0000

    basis destroy window lend create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def sort_list(lst):
+        return sorted(lst)
