009599e6c21cd62826e2f3fa49eea978def0dda3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:06:15 +0000

    keepup interfere answer set pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
