d081ea7454eeaee4a1f2491ba5ed97a6d66d6602
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:33:42 +0000

    active its arrive early tip

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
