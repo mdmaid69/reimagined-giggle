3c493e0789e97845b6757c0df6641c03320fd44d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:07:51 +0000

    whistle town estimate sit sing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_list(array):
+        return list(array)
