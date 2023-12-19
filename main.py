318bce8b16584866f8b39ac472bf7b78f0fad147
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:24:45 +0000

    even rub quiet full destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import getpass
-def get_username():
-        return getpass.getuser()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
