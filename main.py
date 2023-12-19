8a08db89aad68323da8f4319b8488d57d6e35e04
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:51:24 +0000

    street health safe last bringforward

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_list(lst):
+        return lst[::-1]
