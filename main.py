37a989eb1e2c04249162d03a3444b9abe7e0bb36
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:35:28 +0000

    frequent mainly bit nature attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
