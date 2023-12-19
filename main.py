0bb92352ebb6d0155b694f943e878795df9f4ab3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:52 +0000

    certain past impression getoff majority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
