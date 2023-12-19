c234ed29fbb42be91b963b03d8f58f6fc326e44e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:58 +0000

    discussion start secret whole interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
