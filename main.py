30d19c690df8aaaac6e2b892242473c41375e127
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:11:16 +0000

    original drink prefer confidence wash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
