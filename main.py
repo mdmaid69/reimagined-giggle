f5a1efa2e06bcb886d0037b835de8bd944b57638
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:13 +0000

    use life after stand open

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
