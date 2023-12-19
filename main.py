f9fa769fd0d8ef9ba649af61a6a0c45d712b99b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:50 +0000

    main figure turnback complaint software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
