2bc96deb2866036b4d53390422c022b947d6bb98
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:09 +0000

    accidental turnover lookback(on) tip dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
