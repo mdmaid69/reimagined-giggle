dd7c7fc3aab8780ff7b65af03023917ba92f3379
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:23 +0000

    quarter cut platform wind creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
