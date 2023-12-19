a0577e87394d1a1b5a11b3ac4df7a935afb20166
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:35 +0000

    near usual service lock bored

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
