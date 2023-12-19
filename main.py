89c3af28780e0cddd4fe92e1b1a0a1ded6cd47ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:21 +0000

    nurse listen thumb other shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
