d59551385699d5df5241755777666880f6f743c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:50:57 +0000

    shake bad cause flight plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_factorial(n):
+        return math.factorial(n)
