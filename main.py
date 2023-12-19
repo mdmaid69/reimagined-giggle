ea7f6a987c19726c9192cbe8fa34a15d4b1a8c09
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:26 +0000

    noone glad protect sweep demonstrate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
