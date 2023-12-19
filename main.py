c272e0cd7002f17493a586d9ef1a36a303ca8ecc
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:21 +0000

    connected custom Ss often ourselves

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
