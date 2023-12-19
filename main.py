5de6111fdfc94a630e646b0ea885670bed51f742
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:59:48 +0000

    train well threaten for turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def convert_to_hex(n):
+        return hex(n)
