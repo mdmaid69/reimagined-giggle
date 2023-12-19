0a3b8b979b445c9d1b1fa03e6acadb02b7894dab
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:18:21 +0000

    breathe recommend giveout hot put

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
-def log_message(message):
-        logging.info(message)
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
