e6f6a3f12b1e310e249daeacde1ff34f4dc9b7d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:05 +0000

    increase Ii spill contrast blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import sys
-print(sys.version)
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
