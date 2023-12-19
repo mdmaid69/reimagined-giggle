31ff8a50d4fa9ccad1042688d9e4fdd888948d4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:25:07 +0000

    appeal appeal cheque tooth lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-print(time.time())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
