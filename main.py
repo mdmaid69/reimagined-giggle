9c6cd5b542c9b366a20e2b3749102382b9ba2e77
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:53 +0000

    count excuse steep wish turnon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
