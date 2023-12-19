7cdad3b0eacbd76c2cd03b815ca5dc739058e3e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:18 +0000

    understand century careful crowd afternoon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
