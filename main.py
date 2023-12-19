46d04fddc76764e0d45034e7f021b4f7237dae72
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:39 +0000

    callat hollow news minimum realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def sort_numbers(numbers):
-        return sorted(numbers)
+def get_array_as_frozenset(array):
+        return frozenset(array)
