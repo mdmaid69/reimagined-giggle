70190bcbcfb06e058dbf3f89ad719fbfd8a056b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:19:22 +0000

    development story although nurse standard

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
