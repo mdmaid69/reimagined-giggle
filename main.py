5785f17468d282c31c17902477d51ac56b82f445
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:18:51 +0000

    maximum file begin let embarrassed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def remove_from_array(array, item):
+        array.remove(item)
