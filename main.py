68bb78b05f1b79cd2e9aedab9a92f45dd6edfb12
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:19 +0000

    harm tip persuade rely very

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
