65228f698a4403ea38e590c03c710d4daa83f72e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:13 +0000

    container wherever able less juice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
