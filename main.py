a834376bd94b5163557d719bec38b9ad3d94eafc
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:38 +0000

    create fierce professional equal opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
