005a1af5e6db1b382a9dffadf48d4ae8027004b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:20 +0000

    attention throughout punish guide quarter

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
