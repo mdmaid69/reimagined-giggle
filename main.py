de416436e0ed5fc58944086e4daecfaac3756898
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:07:08 +0000

    excuse fight will accommodation play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
