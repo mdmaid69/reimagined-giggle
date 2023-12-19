ad2a6a5813a0da301c50a311e4229ff156ea14e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:16:41 +0000

    ourselves past lay little middle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
