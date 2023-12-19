f2a112ff1eb3bc05271ed16287ff01ee169cfdb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:00 +0000

    Zz string limit scrape callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
