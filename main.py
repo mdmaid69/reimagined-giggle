f47e951c86354773964d12216d99bdfa662b90f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:53 +0000

    capital high worry interest ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def square_number(x):
-        return x**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
