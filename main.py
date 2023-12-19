e88ecd3dfb7b6c8a3b0eb0c499fd391aeade056d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:54 +0000

    clear accept track young actually

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
