2555d0a4d017e8fdb0171fd21be65eed5c8c1228
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:42 +0000

    unit afford nice side copy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
