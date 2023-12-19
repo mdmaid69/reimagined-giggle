16e8e660633e4c5796c1e4d46bfaabf6af95421a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:33 +0000

    know account load development object

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
