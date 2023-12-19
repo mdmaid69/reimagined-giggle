63e4e968eda310f11033210239be30840eb693e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:33 +0000

    kind fence Jj increase thick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
