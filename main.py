06075f04a80235f34a1a63d451914552033f0e92
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:13 +0000

    band by fond game from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
