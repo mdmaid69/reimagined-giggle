eb314ca5f0735d2c68386e1efd21408547cd931f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:07 +0000

    independent stupid fine character exhibition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def is_odd(n):
+        return n % 2 != 0
+def reverse_list(lst):
+        return lst[::-1]
