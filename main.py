00375b5ba73b5a3598705f5f9f95ac2051154f40
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:47:13 +0000

    innocent fashion nerve home workupto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-def calculate_work(force, distance):
-        return force * distance
