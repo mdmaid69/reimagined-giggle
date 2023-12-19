f6a1c8914ef1f55fa0ef7ead66a3380822d701f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:11:49 +0000

    thing contribute bag out small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
