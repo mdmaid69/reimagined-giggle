b39b9b96431a1f33c93e17325a30280fe8cf936c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:07:02 +0000

    task when waste better hot

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
