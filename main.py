5e452562efbb6da59bb5cdde7f11fadd7b9776d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:39:09 +0000

    raw sticky OKorokay weak production

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def add_numbers(a, b):
+        return a + b
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
