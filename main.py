0713023a3dd50502d3d03aea88154e83a05f3b56
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:07 +0000

    recover image lunch damp repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
