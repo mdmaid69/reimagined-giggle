6eefb03ddc7f0cd24428ca27c084bdf5c81e4a6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:02 +0000

    eager reliable lead in damp

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
