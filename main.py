bcb89d9abaafe684750ce6dee6f2c6d18fc73fe8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:00:15 +0000

    bar minority glad again regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
