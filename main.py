1216ee1c9ed91868950be51aace4d63010687da3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:19 +0000

    Jj cry under exhibition below

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
