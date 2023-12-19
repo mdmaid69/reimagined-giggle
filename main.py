fb443cb1a58c321f9044744d38194313c4532e6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:39 +0000

    custom material law drop though

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def sort_list(lst):
-        return sorted(lst)
+import sys
+def print_python_version():
+        return sys.version
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
