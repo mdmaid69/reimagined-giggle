716f65459782e710fa9d512b56359575ff75e469
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:38:59 +0000

    across on cope dance left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
