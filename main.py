b0487c03abb23737ee1fab19b5b240f9821e2ea6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:57 +0000

    same gate holdup goback(to) education

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
