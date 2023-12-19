389d4e9336690906fae82e495f1c756c79a4b7b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:20:40 +0000

    demand view chance sleep connection

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
