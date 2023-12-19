b2e014f853a28e162055ee3cf1c0d22cea057413
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:15 +0000

    rent branch basically dry rob

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
