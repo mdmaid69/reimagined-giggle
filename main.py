807b7ca822fa08625712ab8678cc1402ed38b57c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:15 +0000

    shoulder store dance cost description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
