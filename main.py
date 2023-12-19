d8e4b6dea48248406bb914f8eca59c28d328cac2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:34 +0000

    bringforward safe priority plenty closely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
 def get_array_typecode(array):
         return array.typecode
