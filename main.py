17345e9d1c7a759c3664d9eb7a3b8104913a9884
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:00:54 +0000

    not fine pause back simple

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-print(math.pi)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
