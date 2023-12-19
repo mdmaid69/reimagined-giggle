edf2b51022d82b34c980a9aa40a90c305f937bd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:53 +0000

    clothes bottom impossible guide ill

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def append_to_array(array, item):
+        array.append(item)
