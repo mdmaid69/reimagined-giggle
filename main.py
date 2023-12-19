9f4bd0a0c6a75f7acc552ac489b1c22d68eaff91
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:32:29 +0000

    both always across pay Cc

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
