9838f50641fb6dda03260d6155e2a54abf552fd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:15:23 +0000

    this married someone future sing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
