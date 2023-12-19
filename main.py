312fe9d72e3903a44d4a33db55ad97819c15a74c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:46 +0000

    569enjoy must board deliberate race

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  def is_odd(n):
+        return n % 2 != 0
