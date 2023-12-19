e4f6a874ce9c620e10f7bedca626d78caaf78e98
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:46:02 +0000

    order tour responsible earn declaration

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def reverse_string(s):
+        return s[::-1]
