38316c3872d3a2d7279119815592523052a03264
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:13 +0000

    exactly pronunciation sail rule second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def calculate_area(radius):
+        return 3.14 * radius * radius
