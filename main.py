1d83be3c36a6699d224b61b80e84e2d949fa65a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:39 +0000

    you mistake she slight design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def multiply_numbers(x, y):
-        return x * y
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
