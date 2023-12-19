8720fb36a7806c8f09fc74e91ff1320836917457
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:21 +0000

    plastic fair conclusion fix sweet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
