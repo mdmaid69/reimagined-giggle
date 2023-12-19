4e9ca18e79adfd0a72abc21753fa2f2e42c54c13
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:48:23 +0000

    transparent could kill instead wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
