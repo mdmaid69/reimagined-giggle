70001879681eff374ff7119e7abb1746332f3530
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:34 +0000

    study major fast generally closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
