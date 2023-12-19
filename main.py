6476d32d01349324b25a6a0ef2b81b70c35cfa8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:22:50 +0000

    suddenly spring house appear obtain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
