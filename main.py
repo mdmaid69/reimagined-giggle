d598b01f995f0522dc0cedd662942391c9ea86f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:22 +0000

    party any dress lookaround usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
