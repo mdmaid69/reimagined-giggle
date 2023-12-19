8f70305ed743be0882db4ce18107fe162017da0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:36:07 +0000

    listenfor turnoff deaf copy confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
