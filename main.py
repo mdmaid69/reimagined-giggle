dec3d6c4438777a92caf1d3103e4c5b821076576
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:04 +0000

    another exam let possibly spread

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
