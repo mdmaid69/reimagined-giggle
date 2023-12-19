74b399ba2362bd01645e510d8e5f2714ef900a1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:19 +0000

    church fault fashion advanced far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def convert_to_binary(n):
+        return bin(n)
