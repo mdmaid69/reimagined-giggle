593b818f84771cf8dab570682141272505f1910d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:17:33 +0000

    blind war swim short realize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
