9e522e5671a7dccde08fb988b031d423a47dc3fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:59 +0000

    tidy less distance sale runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
