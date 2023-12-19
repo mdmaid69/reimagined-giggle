da098752f7c4d96eb17b8edad71a88548dc24cdf
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:13:47 +0000

    offend plan wonder push reduce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
