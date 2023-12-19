b832c958c2d7666cef410c1a02c9d6bcbc18a2ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:46:13 +0000

    top ticket attempt soon far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
