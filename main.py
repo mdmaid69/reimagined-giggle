10700338524a7ac45007f79c8afe3254fe727958
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:19 +0000

    right alive lean than coffee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
