b5a3e1db6af6dcfc023377386940b861c137286e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:31:09 +0000

    shallow honest angry thinkof OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
