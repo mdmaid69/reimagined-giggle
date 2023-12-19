69e34bc68427ff6042c13c11449262fbc6aed4a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:07:35 +0000

    complete hurt opponent eye fork

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_odd(n):
+        return n % 2 != 0
