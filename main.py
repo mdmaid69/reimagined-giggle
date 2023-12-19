8ccf04ef5d4acc6af15b515347ab51011b2bec79
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:02:37 +0000

    favourite pin small disappointed seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
