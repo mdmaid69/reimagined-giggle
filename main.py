67d45e429854345a8021affb37d681602225ea7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:27 +0000

    himself false satisfied pity tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
