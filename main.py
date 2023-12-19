fd5a400c3b4771a9c466b47ed27fcaa38936e59d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:13 +0000

    intelligent strange female talkoutof between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
