5b3d34cfc23915f23b75a85c5f8511037d68d092
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:56 +0000

    consist visit communication later news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
