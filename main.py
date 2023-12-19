52fca4e7a7ff9905c7647ec5d676e1954545f01d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:15:59 +0000

    blind exam mine wonder do

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
