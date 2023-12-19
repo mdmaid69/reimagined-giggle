edce054fc95e4f4b8305fd79aa2a6224cd3b0d4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:47 +0000

    storm turnover punish empty alive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
