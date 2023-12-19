f59b445938f20233e03b7295d6647f59d1403a97
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:53 +0000

    equal pinch company period chin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
