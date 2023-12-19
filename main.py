f538db81572066bafb0daba8bf0fa66002cf65e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:49 +0000

    control watch lock second couple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
