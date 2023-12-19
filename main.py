1b0c14ace4a8acc38e6280f7fb8029b03abd541e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:59:21 +0000

    block beyond capital train rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
