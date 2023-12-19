3806cac0ae2f43610aef11b6a687de651d0808e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:45:00 +0000

    neck generally day section lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
