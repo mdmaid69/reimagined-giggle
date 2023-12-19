1a5437a85af8da9a914a5c2aafdb251225e0a527
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:11:51 +0000

    thought weather back neither petrol

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
