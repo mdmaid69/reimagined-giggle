88bce264daf6329036d093fa92af346334e95e95
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:27 +0000

    above moral all mine right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
