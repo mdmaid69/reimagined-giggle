0992e30a23a9349902dee925501fe85ccf9ee177
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:01:36 +0000

    division adjective lookafter open float

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
