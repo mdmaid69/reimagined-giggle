10941680aa9694bc3ef8ceef1e2d7444f7b5a5e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:31 +0000

    bet office list culture squash

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
