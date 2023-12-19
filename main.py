f7bf918e147b2851bb9c69ce84ca7b67d4ca8dcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:04:16 +0000

    fortunate reasonable quick cruel turninto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
