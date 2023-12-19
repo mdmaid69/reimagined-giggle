c56a0c501a07c6649dec0f01d9b9a1a6048cc90b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:04 +0000

    piece grand lookup translate plant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
