e3e7ed35caab10006a581108c51eb9db6c6039bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:57 +0000

    traffic get escape sleep dance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
