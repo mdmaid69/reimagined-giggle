3c50a6b67ce9c742f7781a70a71ffe0d3d549ab4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:41 +0000

    pity hollow 571enough except favourite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_length(array):
+        return len(array)
