f2df63b829110c11a5bc431cb81c2ec14449573e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:26 +0000

    mind regard giveback get love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
