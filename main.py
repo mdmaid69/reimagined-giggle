6875b70b8d637e4158f9c830b40f6341f0d6e39c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:55 +0000

    remain burst programme fence square

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
