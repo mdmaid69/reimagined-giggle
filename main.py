37d08932b66990c3d1cc073eab33aaa276bb2047
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:10 +0000

    callout present squeeze improve instrument

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_speed(distance, time):
-        return distance / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
