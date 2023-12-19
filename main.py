f5b7e53ae7081aba2b53838076a0e6fbac7177fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:46:44 +0000

    maybe interruption wet young shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
