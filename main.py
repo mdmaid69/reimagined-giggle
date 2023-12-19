f81320e0c65652cf7219b15fd08d3a7e296cd5e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:25 +0000

    thank 565end reserve understand mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def is_palindrome(s):
+        return s == s[::-1]
