b31ee0bab7d8fe5d05941b0e6b68baeb2607c942
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:56 +0000

    round regional exercise guess sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
