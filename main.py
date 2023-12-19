ba0e135a2384181c4a4263eb7344d642eabd39b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:24:33 +0000

    minimum community apply second slice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
