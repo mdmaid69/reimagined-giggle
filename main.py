0c17f0e622e621e685a04e6acf6260cba324e709
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:30 +0000

    performance heavy sign gap getup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import json
+print(json.dumps({"name": "John", "age": 30}))
