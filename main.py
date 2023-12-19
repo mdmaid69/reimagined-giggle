b7fce14a86f61db7ad30a0b6682eb4764f2c1673
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:16:31 +0000

    finish scrap alive occur matter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
