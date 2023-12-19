37020e75e6c0f888c4e5736f269d6bd769302eda
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:33:45 +0000

    region scream watch earn yesterday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
