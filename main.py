7cb3cf3f3d8318ac6566e35388e340f79d10f9d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:51:20 +0000

    swim thick turn see substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
