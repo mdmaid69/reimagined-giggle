2a7f737e8eb5fa5d8e544ee5d1cc88a16282d1b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:55 +0000

    reaction getout through young bit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
