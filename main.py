b290ca331a404ac242cc8d5a8642503136574c8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:44 +0000

    sad squash appearance regret keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
