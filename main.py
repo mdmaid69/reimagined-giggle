93f53030da848f5f24b5c0f2ecb952601a15311b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:12:50 +0000

    sound what something contribute amount

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
