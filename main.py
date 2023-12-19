eb106f88d0d346ee29cee717c5d3b294a05e82f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:25 +0000

    shut nowhere climb goout majority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
