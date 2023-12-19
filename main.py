15349941d1c1cacff71d7e7edd1b597cf425a51a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:18 +0000

    anyone shelter surprise handle anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
