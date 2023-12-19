b1bcabaf91060bf889bf55585355ad7b71a5f5a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:04:51 +0000

    holiday load straight what draw

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
