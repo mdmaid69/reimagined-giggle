3015e41cbb1dacb860a91750a97c1f30fbb2bbbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:09:05 +0000

    various post shallow apply hurry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
