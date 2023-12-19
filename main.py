24adb6d95ce4adc5bb13e0db7a4eb652a3461f36
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:08:44 +0000

    abroad something Oo hard below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
