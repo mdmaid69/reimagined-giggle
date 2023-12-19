2501335bf8679c5daa4a103760af091249fea640
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:09 +0000

    sister comedown century trip warn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
