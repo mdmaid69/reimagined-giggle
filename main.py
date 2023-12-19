2501aca5ddb0984a289654f68919db2250d26d91
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:54 +0000

    agreement under experiment while fat

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
