91e25785cdbc941ed98efbe5b122f4fcc152b8ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:36 +0000

    live turn negative print bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def square_number(x):
-        return x**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+text = "Hello, world!"
+print("Characters:", len(text))
