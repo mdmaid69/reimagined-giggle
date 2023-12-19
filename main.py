965f6dacccd7fd01562270ab6a221d98ac35505a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:47:23 +0000

    noone influence comment sure criticize

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import json
+print(json.dumps({"name": "John", "age": 30}))
