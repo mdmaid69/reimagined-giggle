43b31df86f0defea88d4ae2108211fadc91cf89c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:33 +0000

    affect furniture beat no its

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_speed(distance, time):
+        return distance / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
