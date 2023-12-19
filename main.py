06eab2d06ca02fa51b063850e2290e556edf008a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:43 +0000

    confusing comeback stamp escape talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
