766737725caae37007ac04de2a81f176fc9d30fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:21 +0000

    excellent little affect upwards holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
