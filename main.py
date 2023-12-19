439ac4a4294218bb797d5807be0f169f3f386d26
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:44 +0000

    least fierce share pen professional

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
