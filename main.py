9fc1a65a0f0f95b0f463b1c80c67e409eaa4346c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:37:17 +0000

    force record average error clothes

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
