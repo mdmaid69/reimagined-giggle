00c9859c7c8040717143eedd0c303ac53fdc91c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:56:25 +0000

    blue successful exam plenty wipe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
