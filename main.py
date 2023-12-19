9b78880eeefde7edddffd406a111eeaa9e1c94e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:40 +0000

    scheme wish block boat gate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
