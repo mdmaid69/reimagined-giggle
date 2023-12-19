26935da704acab8bd28a435a910e0da8ce3cff96
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:22 +0000

    wind here please purpose gap

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
