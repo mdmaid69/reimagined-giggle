9167cc45d544902b30fb439194253938fe034b0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:50:29 +0000

    share easy prefer harm harm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def subtract_numbers(x, y):
+        return x - y
