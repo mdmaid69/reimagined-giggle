dcd0312b17378b74b070ddfc766b7d9b961c506c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:20:38 +0000

    cover temper offence forward funny

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
