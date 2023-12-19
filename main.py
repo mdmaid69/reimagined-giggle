e84a037d0a9fcd15b87cd65e62385226085747f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:30 +0000

    guess contact opportunity everywhere gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
