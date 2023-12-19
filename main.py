1c17e7c3ba8831da5750c12e446d98172a16838e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:10 +0000

    dick belief bush separate design

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def calculate_density(mass, volume):
-        return mass / volume
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
