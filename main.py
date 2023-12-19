71735f3bc674408a1a438a148a24b01a77fbc574
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:34:43 +0000

    speech damage Zz firm work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_combinations(n, k):
+        return math.comb(n, k)
