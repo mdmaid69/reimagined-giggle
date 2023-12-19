18f2f48aef5a69356fd02a47a721c4b4f67c177c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:56 +0000

    and temperature ours interrupt interrupt

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
