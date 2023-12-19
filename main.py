d528723691cac0bc0ce5a87b768bf569c627e677
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:17 +0000

    standard afterwards record they exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
