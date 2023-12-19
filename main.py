128d4763ea13184cb77c4858ef5082e8ab3d88dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:17 +0000

    discuss cut which to suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
