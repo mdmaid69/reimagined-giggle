027748540b39fb83c396cf8e899a225088648d77
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:54 +0000

    second let moment used hurt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
