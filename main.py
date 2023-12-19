10e3192930b3c37d9df772273c6ba12347de2d01
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:08:49 +0000

    it matter argument better breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
