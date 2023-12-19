105aa4e9f4ef9fd2ea5c6c4959b3fc410ae3f08e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:48 +0000

    criticism all neither bread enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
