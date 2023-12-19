11ef2bd03ac96e10fa3b6275db44a08c092444f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:36:10 +0000

    off corner sometimes giveout window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_speed(distance, time):
+        return distance / time
