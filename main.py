11c4b2e0e0c9713d130887a6b90dd96c16ee9d66
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:14:44 +0000

    urgent sort bath relationship welcome

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
