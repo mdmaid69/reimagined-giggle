65a3c6f614212c122d2862b12f5674c4b5d45e0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:25 +0000

    hurry thirsty every imagination until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
