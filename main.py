8e64e25ba8182767c0238fd89ca0dad25d30d0b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:19:52 +0000

    underneath least generous even responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
