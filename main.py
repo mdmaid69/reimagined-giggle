e34e50ffad63391bb6f6eb86f545fc5710eba61d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:36:52 +0000

    should object respect report arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def delete_file(file_name):
+        os.remove(file_name)
