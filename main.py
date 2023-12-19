ad46e412ab4da599531dc10d25aa661bde5b1de1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:29:22 +0000

    opinion benefit scene clean strike

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def divide_numbers(x, y):
+        return x / y
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
