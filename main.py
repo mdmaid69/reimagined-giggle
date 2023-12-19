baf715e1531874e38d446aa4e227bf0755cead75
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:37:21 +0000

    defend than some getoff scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def multiply_numbers(x, y):
+        return x * y
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
