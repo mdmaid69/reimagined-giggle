0a888564302ca23228f7688484399a0d56dd5d68
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:59 +0000

    furniture bag neck knock change

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
