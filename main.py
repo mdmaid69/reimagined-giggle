ae2ef244ab732070c25c9262f0150dc35f7bcb8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:00 +0000

    far traffic damp report goup

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+print(sum(range(10)))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
