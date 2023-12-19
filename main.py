cc3e2df1515a82cef9770710f8f2a121133e0dfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:02:20 +0000

    sister Bb ear cum challenge

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def reverse_array(array):
+        array.reverse()
