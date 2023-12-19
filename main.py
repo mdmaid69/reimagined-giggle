447b771ca466127f238dd424d9542b7bf3cec3df
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:25:47 +0000

    chin operation temper purpose fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import array
+def get_array_as_set(array):
+        return set(array)
