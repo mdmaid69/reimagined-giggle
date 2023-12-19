1745c6f80ff44c39d8db1073273da96be50511b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:00:14 +0000

    sorry completely drive weather scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
