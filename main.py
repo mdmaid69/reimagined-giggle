dede2b6e3f1391176384d55137c5b562fc729c47
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:23:04 +0000

    sick repair among which so

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+name = "Python"
+print("Hello,", name)
