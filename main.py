f55975a31e11591acb76e92c120013d2eb64ecc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:40 +0000

    comefrom tend experience equipment centre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import collections
+def create_queue():
+        return collections.deque()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
