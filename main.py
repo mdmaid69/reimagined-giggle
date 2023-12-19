7b9b098eadf06cc8b0221d3b77385557440d8865
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:24 +0000

    cause word scrape national be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
