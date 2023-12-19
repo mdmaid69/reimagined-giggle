fad6883b5f35f88da1253e36cff79af7120893d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:36 +0000

    film wind somewhere doubt variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def reverse_array(array):
+        array.reverse()
