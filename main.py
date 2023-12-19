abe81fb342d8d95fea346970ebf753559f710b94
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:52 +0000

    gooff judge charge fit minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def insert_into_array(array, i, item):
+        array.insert(i, item)
