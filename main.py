887c45be2505f5a637324ac3d83c60b38a2d2e69
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:47 +0000

    hospital impressive soak add concern

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
