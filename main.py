bac3b44bb4ba8fbf5e0f12ed4c8f3cfcf13a25ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:10 +0000

    block so heavy grow threaten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
