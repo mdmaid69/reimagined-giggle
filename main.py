660d20ac52e936cef2c2cced8dbab91184c3847a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:29 +0000

    turnup expression open country accommodation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
