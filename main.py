d58e04e8a4649c7d7690e2403c78eaa5a9112d79
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:32 +0000

    rest goout quality tie justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def convert_to_binary(n):
+        return bin(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
