2107e48b2f4bf87dcb88dd2a32f723f31b1bdd92
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:41 +0000

    satisfied spoil occur behind movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
