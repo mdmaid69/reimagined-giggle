a22adb414c9d67a2cffff76aa31f46b3729d3e8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:32 +0000

    opposite transparent develop relation cough

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import time
+print(time.time())
