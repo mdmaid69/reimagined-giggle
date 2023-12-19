bbd971c59f12b06adcda2cdcdb9ce05471d81d71
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:59 +0000

    see congratulate pronounce cook forgive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
