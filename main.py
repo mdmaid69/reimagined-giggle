9556b4cc56ac7dae0494a55443699a632d20be37
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:39 +0000

    form oil protection description change

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def sort_list(lst):
+        return sorted(lst)
+def convert_to_hex(n):
+        return hex(n)
