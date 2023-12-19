ae90bade2a2f46f42a4e4eb569748b68a98ea770
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:15 +0000

    once furniture beneath interruption display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def sort_list(lst):
-        return sorted(lst)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
