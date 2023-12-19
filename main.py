cd21191fefd04351cd9e336b0bcdbb83e3f63dd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:23 +0000

    name interested reach news nothing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_palindrome(s):
+        return s == s[::-1]
