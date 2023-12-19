c469ceab5fca0c41c22a44371b327b405fa306e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:08:11 +0000

    meet tidy pleasure opponent goon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_speed(distance, time):
+        return distance / time
