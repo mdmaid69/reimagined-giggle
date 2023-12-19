69fccbb9465030cb407a2ab7b5ca3a9e7a1b0f62
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:31:52 +0000

    storm way occur train popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
