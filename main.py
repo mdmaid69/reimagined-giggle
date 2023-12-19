683e451b3b842ee69eda4f7f76b9bc379a8aca40
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:19:17 +0000

    earth spot talkinto feeling prove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
