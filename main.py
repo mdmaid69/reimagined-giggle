4b439914ea99d0361f9bb0a47d56f3743b5c8358
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:00 +0000

    rain goon consider cool typical

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
