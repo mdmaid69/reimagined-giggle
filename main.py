47fce786428eef7189eae53f8d77b39f790c7759
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:58:38 +0000

    sister stone interfere deal wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
