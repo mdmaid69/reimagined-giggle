851766831853bdd79abd2776ea5cc253f26c6e35
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:06 +0000

    field stay many to crash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
