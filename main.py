20ef571a06ac47d18cdb5f8a595030a567198983
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:32:23 +0000

    correction organization damp callat go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
