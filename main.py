4aca262785618cc27616a7b161a1e46807d2754b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:03 +0000

    leg learn excited bet power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
