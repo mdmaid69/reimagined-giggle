97e73120c83cdfa0a251007e83d281198b97ea51
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:38 +0000

    circle effect conversation above on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
