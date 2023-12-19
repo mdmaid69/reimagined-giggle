993205a2a22c3593cacbad751c8c1ca67ce24ccb
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:31:54 +0000

    replace afford comfortable left careful

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def count_elements(lst):
+        return len(lst)
