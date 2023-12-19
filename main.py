1c077256eed2479547267ca22ceaa9ad58e44dfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:52 +0000

    claim perhaps holiday air lookup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
