dfd3ff2a6ac826bb6c20fcf734aaf8dd8f7fd108
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:48:55 +0000

    minimum absolutely meeting actually schedule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def add_numbers(x, y):
-        return x + y
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
