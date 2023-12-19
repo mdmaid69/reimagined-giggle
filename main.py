99680146ab88d6b3128c339bf19cc9a74400c989
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:47 +0000

    nothing skill lesson dare hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import sys
+def print_python_version():
+        print(sys.version)
