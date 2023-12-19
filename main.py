f64f2ed0fa366ff258418e2826a18640e5362819
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:55:46 +0000

    kick holdout shake spring notice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def set_array_item(array, i, item):
+        array[i] = item
