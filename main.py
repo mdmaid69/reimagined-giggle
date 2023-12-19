d0e38ae445fe5bb5e89f68dea575df3005eee65c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:21 +0000

    turnon raw scene oil pile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
