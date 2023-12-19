62b251db7bab88989ce525e3a411bfc5303fd8b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:51 +0000

    interesting system affect tell suggest

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-  def is_even(n):
-        return n % 2 == 0
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
