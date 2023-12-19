67d58c885f6fd8ad39a1950825fbfd3dc050cc7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:37 +0000

    takeaway increase stir cook Nn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
