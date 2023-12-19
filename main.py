cc0c5109ae9250aa6518d8c5cc45f5f690e35028
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:46 +0000

    hit absolutely now chase fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
