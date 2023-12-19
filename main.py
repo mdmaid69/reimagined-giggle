1f2c5152e3712c4d70019cf984d7d1c7eea2ec9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:29:06 +0000

    afraid please blow track count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
