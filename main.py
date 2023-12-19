8a2a33f0f9bfb736a9de6f823f52d04731cc8ff7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:21 +0000

    include deep once create switch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-import re
-print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
