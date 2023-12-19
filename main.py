2b3b677e2beda52e747eac005a6aa3e78c47a79b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:02 +0000

    frequent emphasize flow low fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
