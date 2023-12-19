e7b69575061b607b3f75330f4d0a951e65e5337b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:55 +0000

    distance sound fat number well

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-for i in range(10): print(i)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
