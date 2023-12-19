ea20a00960aace4bd6c044b5e82db8a1010d568e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:33 +0000

    set switch attract thing tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
