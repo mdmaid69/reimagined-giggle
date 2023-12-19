caad44eae25d33afcf9be7d6b47f33588a479bd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:31:05 +0000

    pipe offence wash obvious tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
