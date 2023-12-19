b8c384115e6ad750e842eb4e4b088a43f048d1fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:16:23 +0000

    traffic spill tell sign outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
