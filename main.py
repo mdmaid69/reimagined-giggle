55f1e45e978bdc42738c8ce80ea164f4b2ef678a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:22 +0000

    front instruction careful modern ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_min(lst):
+        return min(lst)
