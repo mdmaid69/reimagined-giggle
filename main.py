5b2902b52b1d100079ecbf85730949aad5f97c4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:49 +0000

    interesting grade face stage loose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
