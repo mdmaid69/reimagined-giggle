11a8b3eb9adfa1f85738924c0d76071bbedb5899
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:48 +0000

    before complete clue interfere similar

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
