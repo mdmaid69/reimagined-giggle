ae5766e3604eaaeebd8a47c97881d6a6ddb4c45a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:24 +0000

    move world long how next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
