f3bf04e3bbbea5a568aef9ec1ee42af97f1252d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:54 +0000

    office decide pure change least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
