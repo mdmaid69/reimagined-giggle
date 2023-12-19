2a3385bc5bdeb6f81108edf15d0550b9c849b64c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:34 +0000

    person giveback find region safe

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
