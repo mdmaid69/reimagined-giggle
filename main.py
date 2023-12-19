75c57b157a247bf8e2440e22c6b6f8c898774c7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:02:17 +0000

    tongue announce straight wild offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
