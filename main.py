b90072af3cf2239d72fe221371632b160b3b30eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:03 +0000

    reaction sensitive board born know

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
