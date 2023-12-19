7baa4b8f21f9e052bc6765d024cd3190d5c87591
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:07:29 +0000

    fire week Ee blow debt

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
