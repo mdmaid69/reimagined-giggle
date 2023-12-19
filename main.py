4c127740131ed224174e4c6dda908dbbed03d929
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:17:20 +0000

    thorough Ss original figure secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def square_number(x):
-        return x**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
