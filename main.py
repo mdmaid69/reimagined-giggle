13a3622dfaa1c9db4c6edd674b0bf0cd53745a4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:52 +0000

    point workout land wall substance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
