5408cd0fe5b795543c7c5a54b91f58f0a3133d2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:27 +0000

    dear old wide respect correction

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
