a598e50b361ddf16eddba4f620df99f656ef397a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:32:13 +0000

    peace operation picture skin wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
