d2999d8aa13b8a7ec3d55becd1359688360a66c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:35 +0000

    across worst concerned wild gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
