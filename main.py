f778e293065c9f7491a634160a7af1be7583fa81
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:51:17 +0000

    why careless runinto case sign

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
