ebed378d92477d6765d0ea8be3858bb98bf60e78
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:55:33 +0000

    view cost present important Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
