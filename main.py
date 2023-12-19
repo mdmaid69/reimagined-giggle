622a4e613d94cd665a2eaa9bb3b62fe75f4701f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:44:04 +0000

    old communicate luck judge copy

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def is_odd(n):
-        return n % 2 != 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
