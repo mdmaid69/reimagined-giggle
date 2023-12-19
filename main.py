c364344986e59848199edf5b4a7ad0dae3a403ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:44:33 +0000

    prepare practise strict promise drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-print([x**2 for x in range(10)])
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
