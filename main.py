f7dd87c80497ef3c25677b526c3ca686eaa2bb08
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:38 +0000

    573entertainment noise light underneath ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
