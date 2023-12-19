c7d57d2acc1a627a24187e9b9d2d5b2d60a80f37
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:45:45 +0000

    interfere tooth fetch extra female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
