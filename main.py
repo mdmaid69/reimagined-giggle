14f0e0e314995867ada8bb8c81da28c95dbefb11
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:18 +0000

    add favour boil dance aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
