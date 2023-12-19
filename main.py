7ece818b4c603fd209ab690a38c81b1b655b8a71
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:28 +0000

    yourself time choice every but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+print(os.getcwd())
