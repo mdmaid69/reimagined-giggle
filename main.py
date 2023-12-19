348f195c76ffe1ab06a5fcc1336de438fe1633f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:32 +0000

    march shade unfair marry country

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
