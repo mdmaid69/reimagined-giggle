27f1dfbe496fdfa79a51e0c56767b82a05712c1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:34 +0000

    yesterday most pipe feeling match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def count_elements(lst):
-        return len(lst)
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
