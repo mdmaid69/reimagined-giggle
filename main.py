caf9f3a5dd98d73d4a3a0fcd5ac9b61200b99685
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:44 +0000

    properly travel importance weak comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
