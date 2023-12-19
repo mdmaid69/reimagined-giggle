03045318ec9d8ff46d232c945255144f4b176feb
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:46:57 +0000

    plate male label star interfere

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
