5add821f1506a97131e5603c4312cb1f2e2b50af
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:58 +0000

    drop course circle shape luck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
