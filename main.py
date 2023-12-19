52ea2f4a1d3cbad84e02d438cc28b1155980a9f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:36 +0000

    middle extremely downstairs remark split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
