d2aeb44fbf3d4142673f93f61c9b961c7f692572
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:36 +0000

    must nervous education copy clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sys
+def print_python_version():
+        print(sys.version)
