8eb190c4152c4fecb334b5cc8e4f978b6ff573f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:46:52 +0000

    tap now male worst away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
