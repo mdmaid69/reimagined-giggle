1af7d2ae28678e25798afadac7557ce4a9cf55ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:21 +0000

    room lift special hit agree

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
