2b3ad8da1625baf8e97efccfd613f0929ee2951d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:46 +0000

    know means impressive emergency live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
