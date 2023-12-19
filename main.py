e1d8e3338ace72123327a35f3e2339911a5f4d92
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:39 +0000

    wash pencil succeed importance group

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
