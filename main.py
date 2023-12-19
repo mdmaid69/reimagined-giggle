68c49f846504b9acea334eeb30115987d9db4adf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:08 +0000

    vanish mean about hold way

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+print([x**2 for x in range(10)])
