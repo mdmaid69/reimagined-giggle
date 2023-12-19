550e4622f6f4be869e432d4e52bfd8b465e07a46
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:56:43 +0000

    refer polite stick intend invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
