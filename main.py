1cba649f83fb376de57c764632d74ecf2a5f0023
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:57:28 +0000

    comfort influence next together ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
