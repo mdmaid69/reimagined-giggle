4e6dbd1cbc92daa2138d73b3707a09e5c57aa7e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:31 +0000

    full match frightened the unit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
