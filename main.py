abf6f0a51b70dcd16a744c29bc1882e22606d804
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:36 +0000

    opposite skirt waste cheap show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
