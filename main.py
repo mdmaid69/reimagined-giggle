1364f604b822c968d33d4769b294220f3055d0fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:31 +0000

    union shine freeze lock team

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_tangent(x):
+        return math.tan(x)
