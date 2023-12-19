42ef7154e986d24be97d711e9aea3514b10b2ccf
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:06 +0000

    seem hope phone year dish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
