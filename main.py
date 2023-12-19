7d2fca29bdb27414b2c390a93b582c8e86bf02be
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:20:48 +0000

    Ii its murder step same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
