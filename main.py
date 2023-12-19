5fd3122389bea5d8f8f71fc93305306bbd52b00a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:23:14 +0000

    bother worry comment 573entertainment Tt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
