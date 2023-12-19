ce84bbb5ca2b3d357c0cef36d4334d6189f4e30d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:19:50 +0000

    greeting action bottom burn turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_even(n):
+        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
