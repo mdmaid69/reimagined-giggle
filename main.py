4158a6e887cd0d39cb0eff77709d3069cefdac50
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:39 +0000

    lay like worst themselves amount

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def get_array_as_set(array):
+        return set(array)
