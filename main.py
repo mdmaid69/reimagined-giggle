96bf3bee9073a05d3074a1ea82f6550b7bd41fdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:44 +0000

    comfort but nonsense lookback(on) effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
