4caa9a163dbd00d6ee5499a7a403317536c41f28
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:57 +0000

    charge next hit damage point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
