c64c1d2367bed53fd0f0654c03ce5acf11bfe473
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:39 +0000

    everywhere keepfrom minimum persuade yours

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_even(n):
+        return n % 2 == 0
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
