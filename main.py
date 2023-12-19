b5fbbad463c123d8289781ea24d482067e056aa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:49 +0000

    force possibility substance worth comedown

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
