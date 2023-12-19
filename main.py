a57d9593a1743010aac83f8721eeb63cd2c6aa09
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:51 +0000

    relation level flat total sticky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
