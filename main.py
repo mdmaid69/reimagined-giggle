178bb9cf4b94dfc5beb8be2e4fc56c73a893433b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:27 +0000

    squeeze copy interview damage science

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
