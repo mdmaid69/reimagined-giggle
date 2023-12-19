59c0939cd5105d263bff3f6982a17faca08f343c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:29 +0000

    warn hang question talk do

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_force(mass, acceleration):
+        return mass * acceleration
