9d9524c99cc242bbe0e634f7eec5124837d4d72c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:35:31 +0000

    this equal wonder condition material

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
