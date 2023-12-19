6a5b6819659c50067d727923d1ddfaf7072479c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:26 +0000

    science separate prize meet voice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
