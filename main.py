c1efa69f360bb93dd53bdd42f1e6a1ad95609713
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:11 +0000

    mail sensitive example copy lay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_work(force, distance):
-        return force * distance
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
