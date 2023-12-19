929d21205a49dcd5ee9e6ec5c005879bf3d80c77
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:09:37 +0000

    independent public likely push Vv

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_even(n):
+        return n % 2 == 0
+def is_odd(n):
+        return n % 2 != 0
