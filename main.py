408891a656637e82c9210ea96882d10ae737345f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:05 +0000

    name avoid community shall through

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
