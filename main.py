0704a3e1cd0b903fc87321bdc76b47541543f68a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:39:39 +0000

    pollution afterwards grand perform understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import random
+print(random.randint(0, 100))
