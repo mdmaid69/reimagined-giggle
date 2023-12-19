d08e7bcb5f0d6534fb1f40e45d3cc8ccf48bb925
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:39:27 +0000

    gradually step central few runover

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import os
+print(os.getcwd())
