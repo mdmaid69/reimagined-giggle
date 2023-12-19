313a09ed655b88400292cd526b89cb07d4ce9649
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:32 +0000

    small muscle disapprove in everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_volume(length, width, height):
+        return length * width * height
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
