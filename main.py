b0d86ba7ef39e8e014b12974258a00bbc83c596c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:38 +0000

    opposite laugh long hope deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
