4462dacb4d3dafd71d3ebcb4f8337124d232b7d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:11:41 +0000

    call special strong method damage

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
