b913624f050ffc29ba83e0c008e6013d623a82fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:09:21 +0000

    petrol fit thankyou hospital neither

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
