a8a18a651533d1309a4cabfb057e1a2d133075b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:36 +0000

    contribute achieve purpose tired loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
