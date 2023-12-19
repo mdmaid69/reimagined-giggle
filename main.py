2b667227d1a5eaf872857f068bcb340fcd88dcd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:33 +0000

    amount gooff alive turnover window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
