03ccdcb7edacb9bf0b66872774bd9b053940af75
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:44:45 +0000

    blind block about sale train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
