87f93edc70dc86253447a3b2f2b11efc3cbb5f18
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:40 +0000

    common name start excited Yy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
