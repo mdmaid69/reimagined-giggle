a97a8e788bf12233d94d3da85299fdad6d362bfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:53 +0000

    attitude possibly person kick still

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
