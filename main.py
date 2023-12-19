6151d3ae0699d16e0d9245d58e8438a3438c72e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:33 +0000

    serious bus pour discuss eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def get_array_itemsize(array):
+        return array.itemsize
