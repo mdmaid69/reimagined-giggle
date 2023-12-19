969ebd84263b1a898352ca5a0e1d1dfb97d2e85f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:12:47 +0000

    place present stand thankyou slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def append_to_array(array, item):
+        array.append(item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
