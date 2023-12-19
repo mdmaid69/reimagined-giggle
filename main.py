2328c279eacb3c1a705fc3dba7a978d28aa63cb7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:16:37 +0000

    replace priority spite cheque patient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
