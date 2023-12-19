27db840d01c9183beb98c57db5fd16fdf359e8d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:15 +0000

    style because company sign shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def cube_number(x):
+        return x**3
