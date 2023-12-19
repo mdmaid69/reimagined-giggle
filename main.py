0af85a9e2fbf622d8fe04ac5e1a079fd33eda054
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:11:39 +0000

    duty car spot seat mind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
