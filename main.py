62aa19a6d2dced7d0398af45f406ddb37bb5b456
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:33 +0000

    urgent press light mail regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
