b4ed4079ac146095ae7bc80154c76a682a6d53ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:06:40 +0000

    measure describe hit nowhere eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
