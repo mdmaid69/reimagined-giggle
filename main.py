7adf820ace6c7b65a367c660edab3547647081a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:58 +0000

    check first getaway raw help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
