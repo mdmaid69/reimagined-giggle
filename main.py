204a85060c2a03c381d401dc7c539dc84990ee45
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:47:40 +0000

    challenge sound needle storm minor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
