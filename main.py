1611552a17d1ad2860e3cbca75cd18d1801472d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:22 +0000

    mark possible chance information ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
