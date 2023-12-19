d3850c418df156785ba18f6414a859f0ee5647c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:42 +0000

    almost eager market difficulty comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
