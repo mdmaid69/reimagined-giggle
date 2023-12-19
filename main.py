3f06ecf0e20801d887c47e7153d372d0171f6457
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:12 +0000

    ocean pretty emphasize may hour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
