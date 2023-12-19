5789ce9e6a1bea15d9aeeb7fcedc1277b343434d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:14:20 +0000

    different room unpleasant guess lookaround

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
