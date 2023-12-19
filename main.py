e7ec10f3d1e70cc16545da63703497769e7bf353
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:49:59 +0000

    meeting choice decision crash leg

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
