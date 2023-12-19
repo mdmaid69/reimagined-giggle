c76150ff48403099a0e30d6f9093321950ca97ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:24:58 +0000

    month Jj check operate very

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
