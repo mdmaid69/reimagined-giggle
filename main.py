47d6598315dd87e4463556aab2f74b252dfd6d28
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:19 +0000

    man 570enjoyable belong quickly different

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def remove_duplicates(lst):
+        return list(set(lst))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
