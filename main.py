c5f38fd2773bb80ab4dd3a13604b84ce43e1b8b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:09 +0000

    lookup window speech fresh normal

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def count_words(sentence):
+        return len(sentence.split())
