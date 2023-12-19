a91d4524c8b08a42235f93c53fe5ea5676ae6d08
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:23 +0000

    contrast wrong cruelty nerve firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
