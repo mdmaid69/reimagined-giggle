2d2bbb00c6add6d2bbeab02561274278ef60ec43
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:52:21 +0000

    problem remember Jj callup temperature

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
