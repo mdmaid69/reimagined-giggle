eeb59dfd7e9b87646ddb70581d3aecb1bc111794
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:13 +0000

    often obtain considering result plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
