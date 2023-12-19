e04f9c5103f24745b088d4a7f95a043e81170c08
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:17 +0000

    sink game arrangement accidentally apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
