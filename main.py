757282dd4e54514084c4543f129dd43bd1a6ad10
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:24 +0000

    miss extraordinary welcome certainly destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
