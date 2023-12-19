f535cbb6320bc38c666d6a0149a1eab45ab898ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:33 +0000

    sale loose against over disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
