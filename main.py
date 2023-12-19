a0c3c8dfb2dd04bebcdecaa5472e4cc678c6f826
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:38 +0000

    wash bright height pot gift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
