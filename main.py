76cea3ac448e1bc31040703b01a996d1a5b8757d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:52:45 +0000

    left undo large pull car

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
