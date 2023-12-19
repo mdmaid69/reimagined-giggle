628327e6059951019d7a1640483818aa5b11d8ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:22 +0000

    reduction film his improve talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
