6810e8d1e6261ce7579198ffdd058e2380533f5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:13:31 +0000

    hide direct onto relationship dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
