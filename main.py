772a3d86a6cf449467c6c2a6b52d647acad61c0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:39 +0000

    worried music ring violent feeling

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
