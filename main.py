a175ab7dd1b0ba73db75411f4f8d58d24ddb9e5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:24:00 +0000

    unlikely charge calm turn live

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
