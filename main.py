a02f194d3751894431bc5f98b5fa46596f04a8f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:43 +0000

    weak edge frequent crisis swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
