f4b0d0d47858acdc1a089ed30868b1965493f974
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:37:59 +0000

    control week repeat store contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
