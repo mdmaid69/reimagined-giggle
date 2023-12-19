af1cbed2d4fcb939b27aee044a540100976e8c77
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:14 +0000

    well unpleasant kill wise drive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
