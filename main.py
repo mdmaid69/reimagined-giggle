fe481d171cb8f94994c0395a8dafa29c09d595b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:13:33 +0000

    size geton deliberately change Cc

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
