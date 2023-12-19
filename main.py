689e7e4f4ab1f4eddc6865a16a0c34014f05e679
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:34:18 +0000

    harm takeaway borrow copy possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def clear_array(array):
+        array *= 0
