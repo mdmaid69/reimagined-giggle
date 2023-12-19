8edd423a3060d6de3e1c299d80a7deb6b9ed84ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:52:49 +0000

    cope crowd shade callat party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
