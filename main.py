b8564c7ef3c73d27ed3f52b02714aaf020e71149
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:35 +0000

    spoil nurse smell chance shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
