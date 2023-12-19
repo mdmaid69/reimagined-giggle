9bb3a5395c3aca4269d05e626800769b01a197c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:54 +0000

    various distant inform description left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
