1b96055d8f166a709392a1fdffb19d167cc8e68d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:57 +0000

    freedom rub clue yet paper

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_power(work, time):
-        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
