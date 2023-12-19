f05a02854da505db983be68b806628a05bf82558
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:58 +0000

    ourselves would breath wing jump

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import os
-print(os.getcwd())
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
