4e19e5bf0fd3fc881d10d0532718ca66cab84ff8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:31 +0000

    sign equal constantly moral cost

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
