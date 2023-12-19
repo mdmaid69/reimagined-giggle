84856a63b4f172861e54568ed931972f56afe663
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:21 +0000

    pleasure short lunch taste accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
