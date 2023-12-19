342a31b77d928c61a43677bc7a5be0b2c67cc802
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:42 +0000

    available order easy surprised eager

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
