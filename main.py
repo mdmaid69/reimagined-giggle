f3f62e9d96938eda59aa11b7ee5f033c31acdca0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:51:59 +0000

    concentration tell callback light imagination

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
