46b2ea303fbf26e66b19450d38e7e54a99f22c1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:48 +0000

    emergency news machine beyond rob

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
