fa15ee463965d90414d8dee98ed1ea0ccd2b4707
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:53 +0000

    which popular comeout land unpleasant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
