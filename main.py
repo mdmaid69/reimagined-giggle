0b1e0dbb55becdef8843512987fea44a80a493e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:14:02 +0000

    deliver rush characteristic deserve suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
