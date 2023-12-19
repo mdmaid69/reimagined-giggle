cf9f3209aca3656f201bd5d38f9a6ae9d38b3ba2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:03:47 +0000

    turnon organize value length combination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import sys
-print(sys.version)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def is_palindrome(s):
+        return s == s[::-1]
