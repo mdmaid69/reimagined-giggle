44f687784e537662deff8c5b930c7a4682f9b60c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:40 +0000

    ahead study along police plastic

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
