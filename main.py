d13e241ac210eb56be63c6289f0b775673eb007e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:39:51 +0000

    ready undo calmly exchange paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_density(mass, volume):
+        return mass / volume
