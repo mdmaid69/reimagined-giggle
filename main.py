f94d45b8b1c314089e3926533083675f316bab27
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:30 +0000

    lonely front careless identify there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sys
-  def get_python_version():
-        return sys.version
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
