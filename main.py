098057ce845f68419b554cb95b79c7538ab966f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:41 +0000

    after emphasize possession unlikely invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
