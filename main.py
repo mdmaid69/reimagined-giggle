7a1b3ce37030d7feaf12bbbe07db06dc54f9ffa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:07 +0000

    congratulate want mistake clear sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
