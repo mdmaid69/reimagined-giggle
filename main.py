86e4e1de5690ceb5b80aaae33619302e9edb2baa
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:55 +0000

    light off I contain stage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
