bae2b87b18d1181043333e6e8ec979eb6cddec80
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:00 +0000

    close approval know all course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
