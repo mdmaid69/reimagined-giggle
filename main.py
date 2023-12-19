476fa0da33d5e4491b3de45895b7f8db436445c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:29:03 +0000

    opposite preparation 569enjoy strong normal

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
