6dd245f9c4448de93b545f1be88cf9ee884c6db8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:56 +0000

    interested me straight press themselves

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
