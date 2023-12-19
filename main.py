90cc690fa7b825610894748eba586653a7ce2caa
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:19 +0000

    somebody value rank compare hello

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
