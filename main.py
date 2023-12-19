1ecf7a9798d1b5a7b6bfcfa5cc54b807101fdd61
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:50:09 +0000

    talk transparent forever soil Yy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
