de54fcf0f508e63c9e77ba1ceb7d8ed0d39b4aef
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:24:13 +0000

    opportunity holdup favour professional progress

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
