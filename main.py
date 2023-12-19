64e90887997cbc4ba7e12f3cde855ef17c25e8bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:13 +0000

    calloff wherever shallow smell wish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_speed(distance, time):
-        return distance / time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
