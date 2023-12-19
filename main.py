d627377745a55741e0966cf2b7f07c89412412af
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:16:13 +0000

    interview permission sign equal accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
