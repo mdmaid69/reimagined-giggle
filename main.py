e1b23fa64767d83164c1c653fabcfab3b705bcb9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:08:37 +0000

    describe seat quickly describe neither

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
