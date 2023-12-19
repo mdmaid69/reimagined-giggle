5f9e7e4ed3f5f41cbdb2283cfba433ad61632ecf
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:58 +0000

    equal takeaway us attractive blind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
