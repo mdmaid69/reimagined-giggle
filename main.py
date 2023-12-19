9c4786921b068313066b0a7f5ac3b91058291188
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:15:14 +0000

    authority happy exit stay speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
