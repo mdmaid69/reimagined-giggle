e5b15a34c4a979625167c2b3db15a7f70644aecb
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:00:22 +0000

    cheat quality set fault change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
