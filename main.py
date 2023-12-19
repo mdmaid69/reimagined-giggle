5a9f4001e934cbcde38b276edeeaebf039361736
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:20 +0000

    value several today into price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
