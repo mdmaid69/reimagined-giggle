b3d56b218f18966e2f152bf33980273a02855a86
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:15 +0000

    refuse makeinto scrape limit brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
