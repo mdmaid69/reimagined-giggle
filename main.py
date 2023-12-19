d0e1387412d1884c4d9d03d3c148c2cb03f4cd2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:22 +0000

    awake this tap Ww drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
