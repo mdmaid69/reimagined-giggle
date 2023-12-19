229acb8c8be0541e781ac2a180b11cc29d639a01
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:53 +0000

    prepare trousers unfortunate factory sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
