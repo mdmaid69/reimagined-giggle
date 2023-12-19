46141fb30a16f66f259a56442baa273595b0c00a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:16 +0000

    condition definite town way legal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
