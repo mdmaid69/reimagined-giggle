2305dc9181355df2a1ebc22be5f9f330263fa6f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:43:54 +0000

    regular clothes mysterious scream view

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
