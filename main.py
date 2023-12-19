1779826e2fef1cffa25e7de4841908d6fea566f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:44 +0000

    who forget taste through machine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def find_min(lst):
+        return min(lst)
