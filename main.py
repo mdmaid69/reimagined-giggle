a8580720e432baa116e52be089e6dcfdd70428a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:59:04 +0000

    on official meaning peaceful experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
