850fe818f20b1a56b6aaa365bab86e9724a221d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:25:43 +0000

    old speech father probably science

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+print(time.time())
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_list(array):
+        return list(array)
