2b5dd8bea22632bd15d38a0653fbdbb45cb064db
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:32:32 +0000

    sweep probably offence finish far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
