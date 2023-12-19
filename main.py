b3e8a970d64962852cd30805aaaa7552f1808198
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:03 +0000

    form slow stop during backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import datetime
-print(datetime.datetime.now())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import numpy as np
+print(np.array([1, 2, 3]))
