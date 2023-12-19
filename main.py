60c103115f003851ea77103a7c71e3d3e8e31a64
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:14 +0000

    whoever thin really black hot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
