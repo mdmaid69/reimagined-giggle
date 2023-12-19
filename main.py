009ebcace50d5f0f38aee0e088c181fade32d2a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:16:28 +0000

    direct like means achieve 179bite

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
