03ea481a36a5db99a47db180419d39cdd0a3ec27
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:25 +0000

    comefrom treat shallow slip turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
