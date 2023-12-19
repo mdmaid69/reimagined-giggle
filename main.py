ef4fd8d4f46512a1f75788d44d6a87f0ddaf7c98
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:36 +0000

    air which form tune Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
