877501cbd837d31a6b0bd7f15d309bd2ebfefeae
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:54:26 +0000

    gas lost beneath kick proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
