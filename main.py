4ede31b2a35b99d81c5902a6ae23710750a9cfac
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:52 +0000

    grow piece answer temper passage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import sys
-print(sys.version)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def convert_to_binary(n):
+        return bin(n)
