2c6aca096204f83a95f808a8e44ffe73f09710a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:46 +0000

    scrape suck contents cross hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
