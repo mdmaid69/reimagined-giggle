5a8f90f64525737519ec86564354e60dffdbae0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:13:11 +0000

    sink splash dinner jump excellent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
