efc6e6438a467e5f5683338d79d5411bfa879928
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:57 +0000

    part bit accident hide space

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+name = "Python"
+print("Hello,", name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
