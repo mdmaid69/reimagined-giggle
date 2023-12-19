3da1c1b7862721ce2175defc983d158720d01652
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:50:43 +0000

    think unable turnout quit them

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
