a83ecd6e438361faa396aba2cd56cb5e63153839
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:32:04 +0000

    weigh winter lie name deaf

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def multiply_numbers(x, y):
+        return x * y
