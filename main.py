356a1d572533be6bcd6d057a36435cb5c962cffd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:47 +0000

    properly identify within trap final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
