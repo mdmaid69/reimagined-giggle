d2e50e449a515ff2383f568eef1180fbbd174e33
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:09 +0000

    office turninto angry yours above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def get_array_item_count(array, item):
+        return array.count(item)
