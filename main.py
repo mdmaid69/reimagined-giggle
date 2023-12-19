309981cfe35df679e73002de6535d155c72f3150
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:46:24 +0000

    hope shall relax moon freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_int(array):
+        return int(array[0])
