26ebb4c68c1400ac30648182796524f33c1e1ead
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:28 +0000

    direct muscle friend themselves favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
