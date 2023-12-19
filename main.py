2a510d2124aaac22c9ad6d6e729bc90e688617a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:29 +0000

    use Uu improve return material

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
