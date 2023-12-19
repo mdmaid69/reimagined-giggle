9ae2882408a6aa109a6c8cace35e841b85ece185
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:26 +0000

    different wish follow by concentration

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
-print(sys.version)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
