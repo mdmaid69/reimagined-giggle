37102e30ac73acb118d4d11eb512f0b57eef23e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:53:16 +0000

    mouth turn fit upstairs whichever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def clear_array(array):
+        array *= 0
