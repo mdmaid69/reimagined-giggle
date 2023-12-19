cb878f9bd9e75bea3d0362ff320f928c9dfcd6d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:59 +0000

    ilovedicks genuine object unit air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
