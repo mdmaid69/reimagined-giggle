5a825d886428ff157757335d21aa698550f4a072
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:26 +0000

    violent battery first used bringback

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
