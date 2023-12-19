ab72c765f78eb146eaf81edff5103f510b1a0200
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:31:19 +0000

    catch store side occasion sensitive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_area_circle(r):
+        return 3.14 * r**2
