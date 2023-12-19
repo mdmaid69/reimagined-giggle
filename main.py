8ca4a3e6f327b53d4e01574ea948149b9d2024b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:30:11 +0000

    fly match zero rain again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_array_slice(array, i, j):
+        return array[i:j]
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
