32075c25b6f96b483bba91ba92734fea0ab0ccfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:18 +0000

    name classroom block cut drop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
