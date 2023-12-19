2b550176268cc5fe90742646f29b51dbd0c3d0f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:55 +0000

    trial lunch stamp hurt visit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
