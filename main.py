186ad746652155d9138b53bc1eaaf92e46fea7db
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:07:17 +0000

    slowly most rid promise through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
