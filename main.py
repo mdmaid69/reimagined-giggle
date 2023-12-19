e5d0d6ee4299ef037240d77a8757c526dd6d417e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:49:24 +0000

    spray protest destroy past rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_array_to_string(array):
+        return array.tostring()
