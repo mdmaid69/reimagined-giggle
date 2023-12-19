3e770443543fa3fd2a29da5db66389d6e1b3b42b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:59 +0000

    abroad getaway everyone nonsense every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
