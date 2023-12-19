5087f197408e772b45cb446c1819daeb6e8b721b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:37 +0000

    great year takeaway letter lesson

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
