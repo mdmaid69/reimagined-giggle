e2920c231acf4730e004eac4410ea1b7d2f1419e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:10 +0000

    thankyou responsibility work voice free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-text = "Hello, world!"
-print("Characters:", len(text))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
