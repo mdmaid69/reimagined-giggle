a507c5d7be8211d4bd72e3d46279697bdc23270a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:11 +0000

    possibly deep hurt bite pick

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
