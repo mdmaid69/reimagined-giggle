0ea098093f3936c73dfb7aac8ccb547bd957164e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:16:02 +0000

    floor computer tune already offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
