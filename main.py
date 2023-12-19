803443ba8f39932eaec755185a1547f2d05118e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:00 +0000

    change ignore charge bread fence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  import sys
+  def get_python_version():
+        return sys.version
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
