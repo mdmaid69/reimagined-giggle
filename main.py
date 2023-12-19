461fe20fb2008da7955a3c3eb3449d90602e91f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:29:58 +0000

    getout combination method laugh direction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_pressure(force, area):
+        return force / area
