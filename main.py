241703c8d5fda0d5bdfed6d411d3aaebdb072ca9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:57 +0000

    condition argue face finger recently

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+print(math.pi)
+  def is_odd(n):
+        return n % 2 != 0
