7fa79aed5f27a218a1fe3b9ae15e2f6649318a37
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:43 +0000

    suppose calmly excuse skill tomorrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
