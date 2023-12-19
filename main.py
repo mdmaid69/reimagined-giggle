d42f14c172ccdc918a2b93140b621d2b886fcec0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:43 +0000

    not regular murder advantage contents

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def is_even(n):
+        return n % 2 == 0
+  def convert_to_binary(n):
+        return bin(n)
