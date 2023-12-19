f7ca9eafd0b82b40d32d2c7e8c15e8781b20ecd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:24 +0000

    Oo I content study glad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
