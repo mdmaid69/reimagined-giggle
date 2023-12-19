e097e5f54dd043575b5cf2dc3ef2225b3f6f6897
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:19:28 +0000

    measure swallow act our rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
