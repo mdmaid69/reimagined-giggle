37d43d2f947d38614342c6246f6df25f592d9fbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:04 +0000

    feeling hard Pp satisfied later

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
