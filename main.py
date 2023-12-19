cfaa5cf4a3cd935d43a0f6504a43104839166669
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:03:35 +0000

    bottom breakinto supply memory equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
