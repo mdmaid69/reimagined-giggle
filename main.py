92fe99207d71de73a3f2178aedb32635ee78c859
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:33 +0000

    study tongue underneath grow commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
