9316db7d5730493caee1581c80a74d776662e9d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:51 +0000

    breakdown butter sharp exhibition bottom

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
