8047d273ee39c8e4a9768b95f18cf8115baf2057
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:26 +0000

    lookaround exchange wash company cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
