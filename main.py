6dca89edabf59ee11d9c018c8d23dd0b3a8f06e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:14 +0000

    female trick apply above crash

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_density(mass, volume):
+        return mass / volume
