4904e43259f5167c12bdf8d98627eafe9ae83cd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:30 +0000

    571enough curious acceptable Nn sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
