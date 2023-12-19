461397b83340f763e447c4491ffec99eb9db9453
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:55 +0000

    present partner goaway release hide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
