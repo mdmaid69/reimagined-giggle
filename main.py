a46e6535d0e3469fd03451eba4fcb09d8a41bbbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:39 +0000

    want mix rob business rain

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
