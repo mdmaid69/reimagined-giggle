8bb70e78caffad69e428cd4ab9b13bdc3982d62c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:20 +0000

    rely surface no storm computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
