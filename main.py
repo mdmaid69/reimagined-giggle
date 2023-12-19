fad6a63fbe36b8946775f7324dbf701d98be55b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:29:04 +0000

    artificial line turnoff respect any

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-for i in range(10): print(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
