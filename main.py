443cf484d5ca259b611c89eb2fd2c528190fcefd
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:34 +0000

    break comment grand determined 571enough

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sys
+def exit_program():
+        sys.exit()
