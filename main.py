4c0dae08d7f93030861cc2d4731a31a833e892cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:29 +0000

    offer some music correction notice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_counter():
+        return collections.Counter()
