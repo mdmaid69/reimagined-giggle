ee49ecbacae4441bb45322dc91c43e675b6ae30b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:25:26 +0000

    church 568energy regional refer connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
+for i in range(5):
+        print(i)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
