a27598c53d390ff9513d55f31300a362dcd01371
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:36:39 +0000

    butter quarter themselves spot concentrate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
