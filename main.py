1748e532387216135452cc709a4bdf93dec720a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:01 +0000

    back dinner total tooth possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
