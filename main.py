70902ddfe0dfdb0a456f24d6ac9e2d07973ddb94
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:10 +0000

    rule fold us quiet flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
