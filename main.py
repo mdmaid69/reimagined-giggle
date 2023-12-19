cbd22c0ca21ae4aebe127a1b53d55070ba8c056a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:36 +0000

    brown correct crazy dish recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def add_numbers(a, b):
-        return a + b
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
