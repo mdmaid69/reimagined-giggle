a1b21ad41c2cc9502c557c3e472d6ec4c8a122a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:54:34 +0000

    every mean proof string steady

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
