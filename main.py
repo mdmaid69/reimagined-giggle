92d79179fe2a61bcfaaf96c28b881d8fc8043b60
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:57 +0000

    takeoff account whether fasten slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
