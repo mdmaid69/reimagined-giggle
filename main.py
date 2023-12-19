528ab8b3f76419a68868a33f235406e336ab8fc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:55:31 +0000

    pity fast each call form

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
