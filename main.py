2671b82d8e211581073ef698bdabb2097ff606d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:52:05 +0000

    certain future key slowly hurt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
