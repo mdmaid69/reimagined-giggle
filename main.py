e8893bdc0e66cac6e4715ec52dc7d26de0bdb08d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:59:13 +0000

    choose offer consult before but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
