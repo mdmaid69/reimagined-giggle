ccfbff0c41e0c459afa9c7ee9636f2b60caf26bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:47:42 +0000

    young doubt Ww immediate play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
