308b910fb08fdece5a5211f3392aec205be5ad71
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:36 +0000

    single passage explain scrap trip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
