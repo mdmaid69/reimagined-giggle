5994bc77d7f10a24e5069d49776e92d37ed1262a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:25:20 +0000

    dick sick getback near sleep

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_circle(r):
+        return 3.14 * r**2
