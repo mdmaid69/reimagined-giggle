060b33b3968e1d36fbf6fcb6531f5ad9e013c3e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:49:02 +0000

    personal straight keep underneath combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def square_number(x):
+        return x**2
