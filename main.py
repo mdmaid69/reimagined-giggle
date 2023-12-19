7893030ac563913adbaaf2fd0d7dc4ec61897299
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:47 +0000

    inform evening experience tongue coat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_even(n):
-        return n % 2 == 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+print([x**2 for x in range(10)])
