7b7b72d5b2dd99e6a3410917e5c92d38ae360588
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:37 +0000

    want transparent direction rarely rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def is_even(n):
+        return n % 2 == 0
