9537b1d8b225c5a24b26fe80b7cd5ede4bd20ed5
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:00 +0000

    smell risk final comment below

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
