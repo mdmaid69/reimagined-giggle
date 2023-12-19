55d43a1b75fb75d28fd77ca5afb085e0bf976b85
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:32 +0000

    weekend come waste rescue doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
