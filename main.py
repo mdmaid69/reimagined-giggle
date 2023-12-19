e3abb565008d97dea68262273de252f67b24d762
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:25 +0000

    regularly evidence pocket what its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
