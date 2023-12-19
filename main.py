823ad53903377c92642d44d761d2eb634053c662
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:44 +0000

    part smell cruelty minor board

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def get_array_as_set(array):
+        return set(array)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
