dd2742ae896d60c648ec02c332f5b2623f4ad723
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:41 +0000

    while expensive Nn difference from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
