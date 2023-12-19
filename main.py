e6fe9ea72313bbe5b59c5fe134adc6179f1aec0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:05 +0000

    strike fix forgive little actually

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
