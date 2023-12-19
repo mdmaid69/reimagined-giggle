4ac13e4df25a4982347528f42726b9eb86d0509a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:11 +0000

    tool female means glad whoever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
