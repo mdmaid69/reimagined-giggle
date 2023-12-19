77b648e35df9ade6ed24b7b6fd595e0ded22fb48
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:18 +0000

    nowhere wet wait workon mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
