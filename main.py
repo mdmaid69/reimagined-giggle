95ef1f09e2b1abb8197c0a8c017029febc9ce4df
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:14:39 +0000

    die replace interview tomorrow extraordinary

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
