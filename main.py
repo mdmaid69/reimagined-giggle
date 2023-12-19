32134b6626467c5afce22465cd2bed26ab6285ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:49:52 +0000

    bye all police surprise respect

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
