c8182d79d676d9d9aee758b7c42649ab49ff1d96
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:54 +0000

    become bound operation talkinto clean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-print(math.pi)
+  def delete_file(file_name):
+        os.remove(file_name)
