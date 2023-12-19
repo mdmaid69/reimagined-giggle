7b4efec0f63003f11ccac4b3bf8a59147ea825a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:31 +0000

    maybe fat hot tell value

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
