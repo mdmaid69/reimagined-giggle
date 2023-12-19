9f672a83a25d5e7b1ca99fa65492d7ed4a6c4918
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:00 +0000

    evidence spring actually extra recovery

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-print(sys.version)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
