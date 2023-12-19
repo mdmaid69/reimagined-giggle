dd1579d8b1bfb5248e45fc4e27c83aa0332e2520
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:34 +0000

    comment persuade before recovery recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
