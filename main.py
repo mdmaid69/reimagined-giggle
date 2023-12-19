f8aeb0f4bc7fbd7aa89f202bd6287cef09c9611a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:56 +0000

    comeup insist tune music into

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
