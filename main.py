17b1cda5eab238bf9e1f41c4e19a0ce7c07fbe18
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:09:45 +0000

    broken see possession again generous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
