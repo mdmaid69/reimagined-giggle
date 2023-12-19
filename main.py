7eb0e1b1c05a8bb69b66656218760acfdce5ebe9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:33 +0000

    Ee that underneath back worth

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def count_words(sentence):
+        return len(sentence.split())
+def convert_to_hex(n):
+        return hex(n)
