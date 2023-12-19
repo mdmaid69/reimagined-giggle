b34c0d97798fec3b76620786b822ac15097a35fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:09 +0000

    would cheat steal sticky if

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
