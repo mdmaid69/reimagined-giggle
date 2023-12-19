74ec6051f554140402ad85391ae517079e098ee2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:36:19 +0000

    fence equal suckme but anyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
