b29ae2f318b4ed7359e4cbd4907e1e67b902b362
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:36 +0000

    forever art answer wherever slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import datetime
+print(datetime.datetime.now())
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
