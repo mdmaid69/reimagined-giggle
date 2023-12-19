a6b4f176747f1bbc15d442124f90060bad0c44b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:25 +0000

    sting music cover mass stretch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def divide_numbers(x, y):
-        return x / y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
