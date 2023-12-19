30b84ca0f75c2cf85c599ec86c0c4c3a6907cbe5
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:17:00 +0000

    giveback journey ring attach emphasize

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import sqlite3
 conn = sqlite3.connect(":memory:")
 c = conn.cursor()
 c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
