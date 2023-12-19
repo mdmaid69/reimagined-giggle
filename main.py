15ccf3b8ba86efd684aeccb38e3a00a791a7b52a
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:16 +0000

    news behaviour yours family substance

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
