a52bc030f87a7e4575885924a0d5e12919dee037
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:18 +0000

    quickly ride moral lead exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
