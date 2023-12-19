95e06c1963daab6d2256bbc013d095d15c3f2f73
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:13:12 +0000

    speech give all wrong sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
