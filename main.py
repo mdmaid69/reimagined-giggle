15fead85ab6e8ae66db00f7523c6d0804b5c04bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:12 +0000

    human little satisfied conversation favour

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
 n = 10
 print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
