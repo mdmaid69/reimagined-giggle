8517c13e32760a36e850f09e02f7a3c28a1875e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:54:14 +0000

    correct rise destroy thumb after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def remove_duplicates(lst):
+        return list(set(lst))
