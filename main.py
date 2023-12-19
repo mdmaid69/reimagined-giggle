b74443c522bd2cd257dd86c1f2de41b88e107125
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:33 +0000

    split takeback many statement under

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def add_numbers(x, y):
+        return x + y
+import os
+print(os.getcwd())
