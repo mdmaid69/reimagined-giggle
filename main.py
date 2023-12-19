7c9add42dd169d02fdd6b48d00d257bed2cb6802
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:28 +0000

    runinto build fly mean exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
