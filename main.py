db5f0e3bf40c0a53a3ad0e6b16298d7dcc4213f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:13:25 +0000

    break thinkover surface century snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
