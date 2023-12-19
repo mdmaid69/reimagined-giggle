7e2d7b0b20f4c19cdfd5877d904da1116fdca500
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:44 +0000

    shoulder exaggerate clue hand path

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def find_min(lst):
+        return min(lst)
