426d564a260dbbb3464c71c82e6a122bcbba43f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:57 +0000

    tie insist beautiful argument mistake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
