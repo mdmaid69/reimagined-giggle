9f21b53e5e937924e583c1cc0d11339467f6b224
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:44 +0000

    basic holdout lookaround nerve rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
