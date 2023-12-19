e145baf2d1ec1b559de7c59c9dac2cdcc8b7e49e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:31 +0000

    criticize absolutely tread concentrate near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
