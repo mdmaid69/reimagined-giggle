40fc025841d415bf67ad9e8c3e53a9ce2fa42457
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:34:52 +0000

    cost steer gate home dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-print(math.pi)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
