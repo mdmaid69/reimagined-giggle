1f431dfb77ea9d7b872b3d1001323d98b9525ba4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:36 +0000

    condition clean may affect already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import time
+def get_current_time():
+        return time.time()
