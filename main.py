3da54e04b767f0c376e6de84a07deba6533a8e7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:55:39 +0000

    alive runout lend interfere name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
