71becb2df6a60708b1296dd969934cfd224b107f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:36:32 +0000

    gather discover formal identify unkind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
