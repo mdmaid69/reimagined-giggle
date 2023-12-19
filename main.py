6afd657c2b9362611f090b0d1ac0ae6a173f9d8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:04 +0000

    whatever line feed spite rain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
