54551281b71bca9a66e0173d3dfcaed3f3fd8cd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:00:03 +0000

    scheme day group original snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
