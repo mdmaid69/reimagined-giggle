33b57e18c76a79925aff6c3f397a1fbc05a604e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:02:23 +0000

    underneath lesson degree ring mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+print("Hello, world!")
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
