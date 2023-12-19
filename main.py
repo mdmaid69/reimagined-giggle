b26cd14e0702872ab8ab9118f6dc523f58070ea3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:55 +0000

    fine callat table love man

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
