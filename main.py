1fff4c6657339c225bdb4204a8060c41ad79cd91
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:51:12 +0000

    failure failure crazy pencil underneath

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
