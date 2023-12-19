430a59465fcaf946c0486e40225fb1445b9b6ad1
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:20 +0000

    wise Ee level continue manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
