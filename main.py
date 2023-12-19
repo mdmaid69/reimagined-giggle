20cc1da272c408b59a76faa03e4da4957596bfcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:07:27 +0000

    test flat Ss disapprove embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
