df3f87f03bd050960b1f69e94851051689c0a90c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:58:33 +0000

    complicated necessary properly eager sound

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def is_palindrome(s):
+        return s == s[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
