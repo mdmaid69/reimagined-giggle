c695bdd18207b018c4ab607a14de0d23c03a4fc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:55 +0000

    along storm fail arrangement Ll

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
