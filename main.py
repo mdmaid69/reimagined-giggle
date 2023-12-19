c8ffcd5cabaf29e276d3fd22de06430c4d56200a
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:01:35 +0000

    lookback(on) centimetre live interrupt happen

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
