85ef4335acb21b7e221739c4a56d701c0acbad2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:10 +0000

    frightened Yy fall aim press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
