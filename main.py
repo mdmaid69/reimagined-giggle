38c36af347513cdb14d37928cb985b47b05dc7cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:38 +0000

    tip save design spread interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Characters:", len(text))
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def convert_to_binary(n):
+        return bin(n)
