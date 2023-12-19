7138c90668fc9f81cc01cd61d135c7c731e278b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:59:53 +0000

    whenever write mouth healthy free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_as_list(array):
+        return list(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
