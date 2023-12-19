77ad6efd19a2515fd37e532600614b96019e1c12
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:22:05 +0000

    disturbing load mean file slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+print("Hello, world!")
