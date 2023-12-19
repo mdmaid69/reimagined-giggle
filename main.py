1d44b6c60419e0f1a0ac94b9aa467c7aa8194fab
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:21:37 +0000

    doubt answer careless cope oil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
