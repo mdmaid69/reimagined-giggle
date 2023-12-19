6c26abb500a9fc3657cdd4c9cc9e84e2360f7756
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:09:46 +0000

    female calm finally present violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
