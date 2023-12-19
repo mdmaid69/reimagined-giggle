78dcba9ed298241cd95f7eaf6a15f3f3f93f1319
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:49 +0000

    strict sale journey your into

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
