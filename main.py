c8fdff493863c652d1b302cc6dc79e63bcbe660a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:42:52 +0000

    where tread runout port cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_even(n):
-        return n % 2 == 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
