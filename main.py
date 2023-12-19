97e95ba734c420cc068917e88e92f5cc3bbd2b99
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:42 +0000

    address march shame keen used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
