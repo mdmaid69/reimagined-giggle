469ee87980f555dbed885bafbd809dd43bfbf689
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:15:37 +0000

    right crisis secret shake disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
