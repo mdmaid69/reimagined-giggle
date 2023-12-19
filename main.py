d36cc1a322cc7f201b089d959936544999ee7f3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:48 +0000

    claim keep situation back ever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+name = "Python"
+print("Hello,", name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
