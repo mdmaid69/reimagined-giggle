d270df32e7c14ee266709db40d36bd07f7aabec3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:15 +0000

    office behave snow truth much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
