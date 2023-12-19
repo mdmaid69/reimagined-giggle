4a15beb61d47761ab3ea3105aa1831a6a8471dd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:04 +0000

    operation stamp would collection sure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def find_min(numbers):
+        return min(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
