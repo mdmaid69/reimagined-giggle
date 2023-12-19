5d00356aa02ba05c4593e959018bbbc56b3bcfae
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:56:14 +0000

    protection picture switch terrible organization

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
