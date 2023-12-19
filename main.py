c025f1dd0b4678668263b69b8ffa160d9c45dcfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:05:17 +0000

    but storm always second name

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_typecode(array):
+        return array.typecode
