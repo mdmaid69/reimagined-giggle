574c5a01e659c69a79a527821142a43784010588
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:35 +0000

    eye attract blind attractive task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
