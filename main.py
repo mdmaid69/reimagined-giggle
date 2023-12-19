59108520c3e57355cb5417d6469ce84e2cf92d4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:32:06 +0000

    error total job brief turnup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
