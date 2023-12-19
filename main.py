c684cbc0655a97f183214853e86e88261d2e2d0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:40 +0000

    change kind we meal square

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def get_array_as_int(array):
+        return int(array[0])
+import platform
+def get_os_info():
+        return platform.uname()
