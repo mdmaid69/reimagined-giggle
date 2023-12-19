de9a04da4e639d56eabdb4c41215ebef481536a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:35 +0000

    emotion fall short careful disappear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
