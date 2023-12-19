ebfa4272664a4d77af7093ada3ddd69b11c402c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:15 +0000

    trap back deep acceptable wide

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
