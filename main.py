2121fd64d1d1f9572bf9bd9a2302d8a443cae8c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:33:57 +0000

    problem direct can pencil apply

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import time
-def get_current_time():
-        return time.time()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  def remove_duplicates(lst):
+        return list(set(lst))
