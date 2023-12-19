5f408f6d223899d701d5cac49810d460a2c99bf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:45 +0000

    well clue mistake work dangerous

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
