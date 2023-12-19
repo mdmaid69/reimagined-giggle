ec5244d37104e3d263e4d49860a19aaabe5e5f3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:35:43 +0000

    laugh culture woman little visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
