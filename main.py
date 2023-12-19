650e86437e2478824907df16d04f05351cdfec47
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:22 +0000

    condition contact emphasize accidentally she

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
