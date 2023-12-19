4c2477848cd5f50c9c518da389b6c4d5af98482c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:16 +0000

    police get sound fact computer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def clear_array(array):
-        array *= 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
