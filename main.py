4c5626c942feef5d1366f040e46050ca66b1eb89
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:12 +0000

    comfort show feeling onto study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
