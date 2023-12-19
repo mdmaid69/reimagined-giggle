e205df1eb3a02fad86297b75378208bda220df93
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:19 +0000

    stare ordinary about progress immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
