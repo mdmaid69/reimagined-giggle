d0c29cf68623586a518058bdd5702d8c53382d54
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:43:49 +0000

    lie interest Mm combination turnout

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
