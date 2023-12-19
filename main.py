1cc883c4672055036461b2b10abd6d7c8b8f0249
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:50 +0000

    container wheel tough classroom organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
