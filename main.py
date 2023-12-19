9f96e22bc577f6a58691b312d572f5df5b734422
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:01:35 +0000

    needle keen wear together brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
