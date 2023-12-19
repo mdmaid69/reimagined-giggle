c686e3cc4a6a763f6a343a5f62f748f4f7444d53
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:45 +0000

    month slice cut gather if

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
