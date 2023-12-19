2f3769bcb27bc30a7d4d606e3f37b517a235bc6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:34 +0000

    science necessary radio takeout question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
