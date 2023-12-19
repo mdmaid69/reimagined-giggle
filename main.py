fda44bdf82c44c474666b59fef81cd649737674a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:54:20 +0000

    sorry search answer rent send

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
