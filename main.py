6f89c8e95ffc93045ae15e9083d0166ba9b28d23
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:50 +0000

    copy decision him height real

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
