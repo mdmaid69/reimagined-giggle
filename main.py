989e06295d42ae13d079ea7a424a8db453e3bca7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:42:54 +0000

    rich top light they after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
