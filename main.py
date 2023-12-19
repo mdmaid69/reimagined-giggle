9a138caf20a4d8bbe0eafc174b90514c83e20103
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:03 +0000

    even attention nervous as dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
