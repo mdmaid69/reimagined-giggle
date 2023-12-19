c9dbba51cab28d158e268a81cfcc4d41fb19fdf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:44 +0000

    estimate violent each power unhappy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
