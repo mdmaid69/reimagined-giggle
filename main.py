73022fe0e73bc8e7def22b166a9da6f415280c41
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:27:11 +0000

    impressive Nn get violence scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
