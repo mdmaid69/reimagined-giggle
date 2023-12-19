a6f146522a451d1b70c5e8ba6f144de66bf99cb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:07 +0000

    uponon worse because take emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_index(array, item):
+        return array.index(item)
