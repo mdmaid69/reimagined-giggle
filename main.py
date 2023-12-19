5142926cb77a4ad0091c2df10776053ec82849d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:48:36 +0000

    Nn impression wander now deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
