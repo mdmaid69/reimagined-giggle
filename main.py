37b8670f6dfe7848b6e866da2563b2ba051e414c
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:09:31 +0000

    entrance whoever bottom right children

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_str(array):
+        return str(array)
