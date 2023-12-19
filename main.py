267343d3521f0486d8f188c28f235c9db0254c2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:35 +0000

    back within unusual Ff step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
