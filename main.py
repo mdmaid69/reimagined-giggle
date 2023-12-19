65dbc9bdf439590a093ec8ddcb764820e7ac6db7
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:06:56 +0000

    pronounce bet remove edge affect

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
