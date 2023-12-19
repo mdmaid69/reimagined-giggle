b4f85555ea99c671c9488f3928295e7ee0f759f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:00 +0000

    fail software himself anywhere pupil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
