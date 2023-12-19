98821a5c1e378c76d09eb4e82ccd3e7d9259c171
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:07 +0000

    towards identify steep physical dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
