5fe4afde744ce0ae0b3eaea23d747b11f3a69acd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:27 +0000

    ever sing over heavy make

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
