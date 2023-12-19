6dc010d97ac45835c213d4301cb3dcfb39411b33
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:59 +0000

    tall sister separate pure uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def count_words(sentence):
+        return len(sentence.split())
