dac08baeb221abb5069be8858d8f2f03fe8dbda8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:17:09 +0000

    reach advantage knowledge principle owe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
