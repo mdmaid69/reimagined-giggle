aec89204316de8893cc6f0a6a962246b3075b96b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:32:34 +0000

    trick only church unfair young

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
