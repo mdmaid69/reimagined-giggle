8d1e222b79c3ae4f4b2fced6fc4484cc86c5833a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:25 +0000

    his press dull hide exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
