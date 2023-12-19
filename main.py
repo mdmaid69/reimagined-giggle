2377ab250184591b607fb997bf81b5361fa7eda3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:15:35 +0000

    impression dangerous wide popular succeed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(numbers):
-        return min(numbers)
+import sys
+print(sys.version)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
