02199e95559cbc12b327f741fa5e4fc1df18f45f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:52 +0000

    pollution generally reject brain inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
