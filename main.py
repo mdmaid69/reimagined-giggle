9748ade31b2c14cfe59fae4bc5f9678e70849254
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:56:43 +0000

    table pour across church follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def split_path(path):
+        return os.path.split(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
