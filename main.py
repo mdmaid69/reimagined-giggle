500676656590e97f11b8efa674f180221983f692
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:48 +0000

    lesson travel finally tight seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
