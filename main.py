5e9c4e885cf05c5698e1582f0615ceb725f5c6d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:24 +0000

    runover fork exam list ticket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
