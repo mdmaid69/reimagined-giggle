09ae03d01b5d0b37b87f3b5cc95feb5e6f60fe7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:53:35 +0000

    shade comeover anywayanyhow money not

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
