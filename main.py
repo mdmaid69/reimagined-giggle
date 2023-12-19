3df14e17503d4e3cbf17a2fc553f371ac4f339bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:25:32 +0000

    lay badly certainly regional force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
