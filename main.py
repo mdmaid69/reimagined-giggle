bc0fd04432001ceab4fc673f1f24dcc74ea34479
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:39:04 +0000

    round surprising not trip arrive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
