9ab086585b328ee171da070e979f479ca86d9bcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:11 +0000

    head cause but equal path

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
