adc2e714a1a5b236d232c27797ca215eb7da62cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:21:27 +0000

    break proof confused noisy kill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def reverse_list(lst):
+        return lst[::-1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
