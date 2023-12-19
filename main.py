784f96839544d25441051695d5b4488b7fa27462
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:10:51 +0000

    address even identify contribute spare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
