ed482386fb5c8e8c346b82a703639fac896e21e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:57 +0000

    development stand nerve hold talkinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
