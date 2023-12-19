911eacfd65b3cd2e204748d10e3fc9b2c3e3e266
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:23 +0000

    immediately shoulder combine firm contribute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_min(lst):
+        return min(lst)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
