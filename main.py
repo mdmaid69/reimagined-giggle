81a064066e6fdc53f59cc41495e4983201ff7fd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:13 +0000

    Jj sharp sting takeoff respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+name = "Python"
+print("Hello,", name)
