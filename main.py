0dc50855e88f2fedd09d10cbe48be67f1720b827
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:49:04 +0000

    middle mine used generally immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
