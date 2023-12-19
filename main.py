1b56edbf903d02ad8e76193dc1dd8725ab9c3427
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:37 +0000

    building nice contain rule motion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
