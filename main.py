96fb2868c9928857b3e73e312d3af83270bfbd7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:56 +0000

    murder phone tip sore whether

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  def convert_to_hex(n):
-        return hex(n)
+import os
+print(os.getcwd())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
