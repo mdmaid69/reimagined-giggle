c05df3cae7dd1c52882b63cd84a9b174a1e48508
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:24:58 +0000

    compare lot argument plan relative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
