1ed953905e5f98711f64c9e2cc7fd1d203dfec4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:41 +0000

    instead no breakinto usually several

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
