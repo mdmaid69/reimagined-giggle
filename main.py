711d9ed6e1297932d048d03645ed3138044f0ba1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:59 +0000

    reasonable feel relief shock should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+def get_time_since_epoch():
+        return time.time()
