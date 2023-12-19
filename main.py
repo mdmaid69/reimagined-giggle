10d677ec2bd0e0d9b718045a57b27f9a1aeb973a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:24 +0000

    tonight everyone suit notice tune

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
