a167d2b930cc10388c0a0938d2ffe189c03a4ecb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:52 +0000

    succeed glass bury notice settle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def delete_file(file_name):
+        os.remove(file_name)
