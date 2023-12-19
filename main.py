6a309b075d97c8b8af7e057a011b397b3df2e0ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:51 +0000

    basic reduction calm lose bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
