5d431cde427ee02b0051cf9b57c9535a99720f89
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:05 +0000

    remark react anywayanyhow offence moon

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
