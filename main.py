ad14b27ddbbe7ad5b4e74c3414950a5c3218aa81
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:59:40 +0000

    doubt draw thing mistake in

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
