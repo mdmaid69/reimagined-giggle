b47e87e37e9b54f7e9b060cb6bb3b794c7fe7adc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:09 +0000

    dress male treat mouth pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
