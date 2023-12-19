02fd33f39f8600d8dd854fd9280fb6a53ebf2196
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:19:16 +0000

    best unable theirs guess preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def square_number(x):
+        return x**2
