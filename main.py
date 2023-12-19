f65da918e47f7778baa1a16ab21ee6cdc8a4dab8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:50 +0000

    one understand tough true law

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import collections
-def create_user_list():
-        return collections.UserList()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
