0d912632b36599c998d07c543f7522fd18f5e677
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:43 +0000

    steam careless agree single waste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
