7c52d63c1826e9e909623d3339f7a0dcd343883f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:36:17 +0000

    ready favour sorry detail scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
