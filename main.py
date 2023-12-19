4435b532e076d4264d9d67feb2b7a6520d2de908
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:05:29 +0000

    remain usual major scrap wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
