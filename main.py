0f6838c713d24f3893593d2aafdb4515562904ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:55 +0000

    smooth expression fall comfort news

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def subtract_numbers(x, y):
+        return x - y
