a9450ed55804ee4515a91ee825ebec3be23297bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:53:58 +0000

    neck bet dick history bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import sys
+print(sys.version)
