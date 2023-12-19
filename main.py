c7983e84b5b5efcc878ba7a6b5b633914f52a38c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:41 +0000

    post achieve operate comeback concerned

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
