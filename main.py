ee59b69ee815a8a4f0f861470d7935c830d6fa88
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:52:10 +0000

    hour collection crazy group pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_area(radius):
+        return 3.14 * radius * radius
