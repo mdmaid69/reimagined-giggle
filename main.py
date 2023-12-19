052224d55172051cb2923516f9ba55759ff34553
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:55 +0000

    occasion practise across place surprising

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
