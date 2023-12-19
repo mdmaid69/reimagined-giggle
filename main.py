825af5074e725b01499ff1fc0054e955fab93e58
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:00:21 +0000

    herself time heavy fail bang

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
