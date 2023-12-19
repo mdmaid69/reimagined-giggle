3a82ca8b6743bf3da1f0baaf41ea563caadb08a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:09 +0000

    marry payment generally whole scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
