198e8d076c79790b64b00170343c10b69f23682d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:58 +0000

    substance holdon comein spoil concerning

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
