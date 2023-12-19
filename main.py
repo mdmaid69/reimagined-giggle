ac15336edba188ef4a4f6dd55c005d3a9790e245
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:45 +0000

    public tidy live feel underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
