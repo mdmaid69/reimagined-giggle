0dac25ebbcefc1c01e40bbe2001a82bead9197ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:18 +0000

    scare why speech load secret

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
