954150a653586646ccbdd683d03673f03ec952b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:46 +0000

    quickly takeaway point like blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
