37e708b924939398cb07e5aff281328f5d3fa828
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:36:32 +0000

    every practical mine music duty

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
