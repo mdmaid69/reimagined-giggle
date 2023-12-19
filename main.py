630830ff844e62522905777e9f397308d9b4008f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:52 +0000

    respect offence arrangement steer part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
