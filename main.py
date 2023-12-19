3b2f1f63239dfeeb6cda64f3965518992b0324a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:46:51 +0000

    room case provide sad but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
