bdb63734f4213fd41a3eff967b2b8cefbe15032e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:00 +0000

    teach already public fall try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
