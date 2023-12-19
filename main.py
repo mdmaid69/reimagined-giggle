06d3bfd96ec6d544241f1825d507b8cb64dfe843
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:16:29 +0000

    afraid carry slip show base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
