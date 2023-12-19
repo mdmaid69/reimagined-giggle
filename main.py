40deac80d4242d5ccc8ae9b12c053954697493ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:37:47 +0000

    best minor recognize feed none

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
