81188e5468a54e6ab8c0024790dfa44a692af638
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:43 +0000

    now your consider depend roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
