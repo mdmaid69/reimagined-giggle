bf81244977136b05c4b1b4c66ae83d310b4bd333
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:42 +0000

    successful bringup activity 566end memory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import platform
+def get_python_version():
+        return platform.python_version()
