bea30f11eb46d5953243e4eb66bf0e20465488d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:17 +0000

    face comedown check off run

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
