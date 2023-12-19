350189e3cfd9c468ba7c2ecabba2754d5a89d0ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:13 +0000

    press kill file afterwards gate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_item(array, i):
+        return array[i]
