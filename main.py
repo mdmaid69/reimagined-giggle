583eb6e82c2a5f54d3fef93723d76a4219b78fee
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:38 +0000

    lesson image scale smell country

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
