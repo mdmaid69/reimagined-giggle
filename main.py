66794fc669eb1246f3cfdcbbcb6ace65c3511a88
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:19 +0000

    somehow blow course fault clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
