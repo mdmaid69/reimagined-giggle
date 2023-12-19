b368b9a6b06620eee42441255265b9cfc8fa62ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:29:46 +0000

    through bright run honest any

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
