7eadbfd23bb4118b83432f380594248667fd7c0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:57 +0000

    when they cook confused watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
