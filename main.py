0960ca5608193bbd9029ca922b40b06d0f92d399
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:34 +0000

    cold turnout mistake surprising look

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
