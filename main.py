5526a8ab3622c7f8dbadae25ce08715b4f6274d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:35 +0000

    formal march interesting office respect

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
