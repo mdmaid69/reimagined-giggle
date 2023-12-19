0afb14242f16e25f3ff651408f9ae6f1540c5041
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:53:02 +0000

    note whistle please violence sight

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def subtract_numbers(x, y):
+        return x - y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
