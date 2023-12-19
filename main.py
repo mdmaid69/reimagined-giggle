1e61e7cd7b3bf463889fa82e8dfa37b28cf9c35a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:42 +0000

    cap model over trust trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
