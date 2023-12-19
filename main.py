bb1d7fc6878120e8b79b742bd779fcef10d2fa2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:13 +0000

    terrible foot figure public who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
