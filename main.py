9f927b283e97e755b602f718dee76f6206ed2086
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:33:08 +0000

    about hot nurse but square

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def count_elements(lst):
-        return len(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
