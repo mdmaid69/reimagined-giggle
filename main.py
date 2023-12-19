f3c01fdcb67f886f675479b94868010c8a2683cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:49:19 +0000

    manner every contact frequent mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
