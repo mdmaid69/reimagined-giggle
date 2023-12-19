aa25b7688eb594c335eff9e527be998d733333ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:40 +0000

    leave turninto style recover whistle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
+def calculate_acceleration(speed, time):
+        return speed / time
