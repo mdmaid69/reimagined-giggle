fcc1af0eaf594b19a64cb8533e0c001f1889692c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:55:24 +0000

    reserve near odd near effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
