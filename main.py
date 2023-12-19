7387e1dd2f69ac8f10bb6e0574c2d3f672036584
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:51 +0000

    trouble workout sink prevent partner

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_array_as_str(array):
+        return str(array)
+def calculate_density(mass, volume):
+        return mass / volume
