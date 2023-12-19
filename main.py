dbe92cb934257350db926920545f0d5d183cf287
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:48:21 +0000

    keepfrom praise cost maximum trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
