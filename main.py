b5a7af4b8014ebb8479d1e71a8936a87b236bbee
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:04 +0000

    show near contain benefit pump

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def remove_from_array(array, item):
+        array.remove(item)
