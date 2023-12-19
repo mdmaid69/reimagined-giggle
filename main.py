d2d521b4755188633820e428e1ac752e84373095
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:12:36 +0000

    Gg trade clever goout be

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
