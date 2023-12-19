755e23047fc9b8fb9e386d507b789b2cff4f548e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:33 +0000

    seat wing shock force giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
