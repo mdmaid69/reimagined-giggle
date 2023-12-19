3e208541289d67b12f4a9d78d5af7a071b07795e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:20 +0000

    meeting earn lead tidy secret

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
