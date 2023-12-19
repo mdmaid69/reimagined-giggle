7d2bff87d2123df17d759baaba2cd9996ac9157e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:46 +0000

    brief keepout embarrassed dark afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
