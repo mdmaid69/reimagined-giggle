a41c5bcf904def591a04cc2b709cf6e2bd34ffd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:25 +0000

    occasional slice later long leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
