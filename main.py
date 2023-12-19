352cc04140fc870ee66c5d235ef4912538fd79ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:57:57 +0000

    catch dear area handle none

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
