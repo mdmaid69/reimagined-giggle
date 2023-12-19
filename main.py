9b3f7452aaa72a2f2b614a29f41a01147b149d2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:56 +0000

    gap genuine getaway march direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
