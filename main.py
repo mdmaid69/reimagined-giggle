d28e35f1b2f8e9c46823fb77d204c45b6738205f
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:51:29 +0000

    mean prize lift important limit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def is_palindrome(s):
+        return s == s[::-1]
