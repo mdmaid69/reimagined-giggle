8587297bc269eaa9c846e8efcc5a7b7f1f406eee
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:37:37 +0000

    vote sensible lend remove about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import time
+print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
