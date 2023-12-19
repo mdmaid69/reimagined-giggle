80565996eeb8803ca953fc2d48e8eb8d0cb102c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:10:08 +0000

    afford achieve country under popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
