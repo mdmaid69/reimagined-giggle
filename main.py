2ebd523b303d3f173f3d188bad7a33af06833857
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:35 +0000

    weekend apply attitude skin live

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
