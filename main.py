b3c41e22cd947e5cb2913eeb542a1fb62a019f31
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:34 +0000

    record blue perfect stare personal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def get_array_as_bool(array):
+        return bool(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
