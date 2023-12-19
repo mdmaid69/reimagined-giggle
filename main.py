2125c84307cbacdb1f7c4e7831c068d044c4344b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:42:31 +0000

    regular next trap nose clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+print(re.match("h.*o", "hello world"))
