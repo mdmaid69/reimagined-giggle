42d0faf8bcb0e96c8c0a57a9784e15bfa6302292
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:00:57 +0000

    concern side holdon band book

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
