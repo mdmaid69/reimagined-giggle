87181b5028ac8d079b92165c750c85e10c76aec9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:17 +0000

    contribute suit replace normal exam

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
