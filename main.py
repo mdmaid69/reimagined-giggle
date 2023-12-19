f44d99dc059c82f67446b7dc58c0627e322732f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:28 +0000

    glad battery permission task discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
