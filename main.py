977000431cf286dfd27ab1d19b0ff7fcb30490ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:28 +0000

    exhibition grow it first boil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
