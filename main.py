09dd223776bdb17f221502764eaa153100d89dec
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:27 +0000

    ourselves generous both flat advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
