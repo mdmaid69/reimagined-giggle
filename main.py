0c7458081b3ac6f0ed3a5663e22efddbfb5438a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:31:28 +0000

    excellent closely mouth flat theirs

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_int(array):
+        return int(array[0])
