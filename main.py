682cecef5b7e179640a2f2500c63f4b9673f0c71
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:16 +0000

    566end group comedown success sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
