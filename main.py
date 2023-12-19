aefc649dfa276aedac910e6548704837c1a934f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:05 +0000

    destroy back string callout before

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+x = 10
+y = 20
+print("Sum:", x + y)
+import random
+print(random.randint(0, 100))
