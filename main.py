f14c49294342105ca281cf15ac352361e9296b14
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:13 +0000

    lock not interview conversation safe

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_str(array):
+        return str(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
