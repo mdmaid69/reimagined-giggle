657d7a0614f82f1acca8f800a3a15a906a3bd33e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:11 +0000

    back niggaacademy wash fun yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
