800c9462f9c8f4099deb3f3797e6f1c7c8d8698b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:06 +0000

    survive duty key drink nurse

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
