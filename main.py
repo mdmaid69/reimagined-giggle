e4b74cc470a42dd5b20978c9cecd994c6136f3f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:32 +0000

    responsible breakthrough independent branch bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
