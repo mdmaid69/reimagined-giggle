b8cb42d5240e032a495579210a86b18f8d414616
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:54:46 +0000

    different careful anxious bin stamp

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def remove_duplicates(lst):
+        return list(set(lst))
