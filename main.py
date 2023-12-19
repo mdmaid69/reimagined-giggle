7c762c77109aaa329df58ee206166a0d0fec4aed
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:22:44 +0000

    embarrassed bridge man noone sky

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def count_characters(sentence):
+        return len(sentence)
