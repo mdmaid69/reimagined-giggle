b68f55fb431fbc227247482d0225ed659badbcf8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:24:57 +0000

    side discussion exam scream injury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-for i in range(10): print(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
