3e74c3690d9e39dcdd9b7c69a87babd0c0dd887c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:51 +0000

    should hand formal street available

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-for i in range(5):
-        print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
