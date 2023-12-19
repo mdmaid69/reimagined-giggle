e7b11d9121709e0b8f18c5c6d633cee9bf2c64e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:16 +0000

    responsibility separate container clean beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
