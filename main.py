ba39fe7435d005df486a2ad2c420ff3090c9ce9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:14:14 +0000

    through cheek opinion clearly speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
