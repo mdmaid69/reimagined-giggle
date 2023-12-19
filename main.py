6187f63eb41edb8f56cb498d19b3a4a0523b1d83
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:00 +0000

    favourite underneath appeal throughout light

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
