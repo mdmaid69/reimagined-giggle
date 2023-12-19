55ded7b590259698bfe936dc7427b9d5cfe37a4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:29:46 +0000

    increase someone outside whisper experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
