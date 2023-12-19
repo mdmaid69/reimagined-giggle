c7945aa66c358b45441ebb9b7623134ed7de310b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:07 +0000

    179bite OKorokay related change precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
