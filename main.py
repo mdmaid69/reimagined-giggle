eaf5783927d95a81bf593fb70c93687c4ba17913
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:51:54 +0000

    embarrassed ask comedown still manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import collections
-def create_user_list():
-        return collections.UserList()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+print(sys.version)
