2d7fb935092a4300546299060292bd0a39f8462b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:25:43 +0000

    concerning mail rush plan equal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
