ac3c2fd1509773d88ffe9a2d698c4abc5c9cd6ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:02 +0000

    describe book area tend refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
