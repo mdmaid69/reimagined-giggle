6383b6259040f36f556beb41bca78bbf3635fdac
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:24:54 +0000

    scheme out activity mix preparation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def square_number(x):
+        return x**2
