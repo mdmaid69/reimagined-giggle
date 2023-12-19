3f4c557034b5030a13b39efd27a1ebf163e26dde
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:28:39 +0000

    reasonable passenger bringback drop air

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import collections
+def create_priority_queue():
+        return collections.deque()
