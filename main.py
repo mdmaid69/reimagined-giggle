61e12836a5f4d02e8975d4a0db73ee9121a2fd0c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:45 +0000

    among sit list object communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def reverse_string(s):
+        return s[::-1]
