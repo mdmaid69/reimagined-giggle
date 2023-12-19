3add68a8767fd71df21e072c867cace3cab16cc1
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:44:54 +0000

    explanation explain honest dance surprising

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
