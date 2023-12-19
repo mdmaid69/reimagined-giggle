2e325a9f407198eb63eb1e9e9ba4f9291247a84e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:47:23 +0000

    willing serious bringdown kind breakinto

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import datetime
-print(datetime.datetime.now())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
