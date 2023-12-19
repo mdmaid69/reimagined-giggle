ade6e3bb5d54b94dd3e2a5bdf6b4c6d2c92d91fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:37 +0000

    brush recent draw remember protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
