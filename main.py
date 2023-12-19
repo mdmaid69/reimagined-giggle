e8e88749756e2b159bb3f5e31c7d13b845c8c2a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:33 +0000

    in arrival Kk shoulder runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
