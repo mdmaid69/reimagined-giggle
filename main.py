014a7450bc82e4a6954a5a92e8bed477d6f6c1d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:58:17 +0000

    thankyou unable switch house transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
