bee56b4f1b79fc6d29ba93e9e72d3e9e835a728b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:10 +0000

    bye term computer chief appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
