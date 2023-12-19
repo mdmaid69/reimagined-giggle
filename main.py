79ac633a25cfbbe095f5517b898c84e66c50692e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:43 +0000

    hope sight maybe proof Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def cube_number(x):
+        return x**3
