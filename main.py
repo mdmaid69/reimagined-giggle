1a8eb13c44440e0d4521dfed1af2179df6b944f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:12:17 +0000

    declaration mystery rule welcome swim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
