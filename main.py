44ab0c3a36897f5b3e47362d68975b1c4fee8dce
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:12:35 +0000

    inform address mess begin dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def multiply_numbers(x, y):
-        return x * y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
