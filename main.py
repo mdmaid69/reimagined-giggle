59036d0c82f142066d70ca276cfb57fcd259664d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:16 +0000

    slide welcome brown foreign copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
