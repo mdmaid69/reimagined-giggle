8cf1d0a96791b1d48187ecd200a6aa23600047cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:34:39 +0000

    contact before who gap aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
