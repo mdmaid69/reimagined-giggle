8936eb3b00d3f3851f5cc026ad26855f935cc1a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:52:17 +0000

    anyone surprise tie pain include

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def cube_number(x):
-        return x**3
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
