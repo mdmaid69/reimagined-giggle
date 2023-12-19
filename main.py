a5bb262da731ecd802dc87359ba89280fe81cf60
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:55 +0000

    its not may deliver test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
