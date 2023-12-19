8e03a423967c1dbb0608bf7c7c8d3b60ffa9a8af
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:25 +0000

    burn equal weekend amount ask

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
