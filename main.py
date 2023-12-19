46c2479f443a989ee553e486965ef1f06bb5909c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:58 +0000

    town thought patient small mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
