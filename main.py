33c290c936c924d812ae10638ce449ebc9094d38
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:11:08 +0000

    attention beginning lookback(on) eat surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_index(array, item):
+        return array.index(item)
