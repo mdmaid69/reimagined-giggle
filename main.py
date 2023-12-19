a924a25ad2d62ced98a35d849f8f4e6b394785fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:58 +0000

    order joke scale survive wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def remove_duplicates(lst):
+        return list(set(lst))
