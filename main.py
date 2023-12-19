886b2a12d45a6c785fe53fb48a5e43ce0f94a50a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:47:20 +0000

    act put arrangement possibility well

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_binary(n):
+        return bin(n)
