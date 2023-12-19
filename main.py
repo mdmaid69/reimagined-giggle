f0b4d2c4febbf9d3a8bb4f8b6779473c9a60aeb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:23 +0000

    fetch after crash Ww turndown

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
