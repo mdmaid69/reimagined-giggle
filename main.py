f09a5587dbb2211f35fe45d8d574cd69bcd4aed4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:00 +0000

    section fresh free muscle holiday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
