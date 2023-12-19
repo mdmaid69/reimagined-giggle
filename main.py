abdcbccf20d4ec9f158d51e85f3dd90824711fbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:22:36 +0000

    giveup manner concern sound game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import random
-def roll_die():
-        return random.randint(1, 6)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
