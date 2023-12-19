d9c031ce12fc75f37aa8ffc0a121878624e3efdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:18:54 +0000

    save away phone child material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
