a3028b06a71298b908157f95e3cad79f6414d0b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:22:37 +0000

    train like strike breakdown detail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
