633613369903cecbce62c9a19ed004762b60969f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:06 +0000

    climb little curious yours soon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+print(math.pi)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
