c3e8a1d816a73dffe8815bb037df334169569fbb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:35:39 +0000

    job radio fit steam no

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def divide_numbers(x, y):
-        return x / y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
