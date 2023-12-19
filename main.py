fc83997f8fd40d6ebe076b857f79b2b3f602fcdd
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:16 +0000

    home important cruel deaf cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_length(array):
+        return len(array)
