9ae1023e6f7d5fd94c202a4cd5104854f5f0ec43
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:52:09 +0000

    proof turn opponent confident face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
