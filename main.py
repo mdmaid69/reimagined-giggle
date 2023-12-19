a0d84d642400bceafc0bc640869ef35f0f789c4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:57 +0000

    develop hold none cut spread

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import random
-print(random.randint(0, 100))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
