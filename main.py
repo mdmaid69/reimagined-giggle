952a2ad2a5701d0175c984b70e17c8ccc189916a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:10 +0000

    mark them square brown opponent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
