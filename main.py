fe36f89bbf38276918210056d766b3fc1325a963
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:06:53 +0000

    charge report start block break

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
