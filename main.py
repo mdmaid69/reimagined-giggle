1ce7d3df24d87a501b5886ca6628061c4c579b3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:10 +0000

    area nonsense breakthrough visit except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
