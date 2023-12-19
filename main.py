9b40b6873b1f2c38a79dd77161ea12a26ee0749c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:23 +0000

    guess recommend run lock think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
