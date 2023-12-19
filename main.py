932ae950af2fdc6ce7b55bc2d88c8e5a2fe2d71d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:37 +0000

    treat accidentally now correct downstairs

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
