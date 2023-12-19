dc90e004af3c0c80dc948cc294139e6f1c489fd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:26 +0000

    plain scene increase take you

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-x = 10
-y = 20
-print("Sum:", x + y)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
