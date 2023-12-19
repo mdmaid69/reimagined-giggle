14e6cccdd8d5e3ed76d30c350b57185faf5f214b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:16 +0000

    equal time moment neither wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Powers of 2:", [2**x for x in range(n)])
