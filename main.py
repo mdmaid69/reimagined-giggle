c7e35f2a907c0d417cd093cff9ffda2d27770aca
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:43:25 +0000

    content climb your holdout noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
