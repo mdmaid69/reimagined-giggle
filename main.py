1ef2389fc9ce95bddf357b2aeb0cdb0441998119
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:58 +0000

    willing advise autumn sort fetch

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
