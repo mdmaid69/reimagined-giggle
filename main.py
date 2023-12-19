19dffbd483ad2d4534e83fb89f66a46091e98509
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:17:51 +0000

    can contribute effect rare wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
