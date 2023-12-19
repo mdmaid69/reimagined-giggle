6c9d27ba309d7770245b4345bd0762e6ee355a7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:39 +0000

    copy reliable expect trust brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_min(lst):
+        return min(lst)
+def find_unique_words(sentence):
+        return set(sentence.split())
