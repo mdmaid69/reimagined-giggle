98287ad24dbfa79d2fec8e4d717c3ef2017c6a3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:35 +0000

    window joke culture float angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
