95081f686408ec10e8961e29e4bb0cdb1717a17a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:19:48 +0000

    keepaway winter responsibility definite swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
