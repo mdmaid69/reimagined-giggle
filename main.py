e0e2bd3009f739b53a90885179af698236f0061b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:08:04 +0000

    early each fire punishment process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
