adc76d0a429ea06ea93432e4a8a7716a4cd5f6ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:05 +0000

    deserve stop cum straight lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
