eadde81cb44df215b3163be27b718f9ffbc8683d
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:33:29 +0000

    prefer talk lookup stick develop

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
