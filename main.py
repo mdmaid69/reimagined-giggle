3b7b2a1812244d059b717a222044a4140cb5a531
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:25:57 +0000

    wherever speak Bb advanced wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
