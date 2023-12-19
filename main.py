ed2080a0fd20d9e4efc65297f4aae8ec0e3f86b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:18 +0000

    genuine majority range close mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
