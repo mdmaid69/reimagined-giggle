ea4b911999f35eb425c3680cfeae034a23bd7439
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:56:30 +0000

    innocent charge fork waste music

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import sys
+  def get_python_version():
+        return sys.version
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
