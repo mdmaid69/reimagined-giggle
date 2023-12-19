7261dc2c759d39187c23788d49340664b7b125db
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:04:54 +0000

    dry wonder you deliver bus

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import sys
-def print_python_version():
-        return sys.version
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
