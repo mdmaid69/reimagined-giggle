d6086a10084e79a38ee439f657d4a48c66f52c8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:27:04 +0000

    hold below past area perfect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_os_info():
+        return platform.uname()
