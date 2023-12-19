f0eaa9409d705dfa4289cbc3360754a4b90e30ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:42 +0000

    write use dream cheek calm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
