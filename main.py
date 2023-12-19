41b597b4898df9f6b6fbf8e59d45e288cae823da
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:38 +0000

    charge situation important visit schedule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-print([x**2 for x in range(10)])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import datetime
+def get_current_date():
+        return datetime.date.today()
