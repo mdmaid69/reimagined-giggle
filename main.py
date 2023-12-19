ed76f6d7f0886ca2e84a783967539c96913a2b0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:23:55 +0000

    sleep in you run trial

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
