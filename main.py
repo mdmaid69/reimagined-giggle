a9db87fd53aa6f83376050a797e5330f8979c33c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:35:45 +0000

    earth sign turnon paper wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
