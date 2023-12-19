662bbb78a05b140afcdb64b840cc69f8206b8013
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:17:06 +0000

    style especially behind laugh water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_unique_words(sentence):
+        return set(sentence.split())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
