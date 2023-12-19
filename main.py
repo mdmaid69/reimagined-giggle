831706fefcde62212234472c90d96010f336864c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:48 +0000

    brave person carry may care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
