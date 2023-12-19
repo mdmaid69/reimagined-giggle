aeee1517726ec2c1ea970396fba63b928e11e53e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:27 +0000

    downstairs immediately relax there talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
