b7f3b3b212f69ebf058f87ad1f6267d86635adeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:34:17 +0000

    tune write variety bath relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
