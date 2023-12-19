5298ba779a32a9bdfb7e2c549a749a78052161a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:58 +0000

    count skirt onto alone pity

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
