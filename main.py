61d30bcc217cf33b96fa4aad176d2f99d253701f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:42 +0000

    mend ill dare workon conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+print(os.getcwd())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
