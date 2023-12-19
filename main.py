ab65333fd47d5ec4f9498664b83c5639740b8282
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:01 +0000

    shake upwards wherever shallow stay

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
