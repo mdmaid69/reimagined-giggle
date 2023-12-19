f7deb473fc92bb1242078e62f1dab2081bdcef00
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:32 +0000

    child often sick wonder chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
