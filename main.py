a5d3528300e9572cea8b9056a510280998004fcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:23 +0000

    heart previous drive murder please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
