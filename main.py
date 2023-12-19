e3da3a82092a89348ff9c610f624ae66e307eefb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:21:23 +0000

    careless regularly way pretend in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_sine(x):
-        return math.sin(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
