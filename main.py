c6cc0b5d633838b9b316215d254701ab2d50de0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:54:28 +0000

    meal cum takeoff load stupid

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def is_odd(n):
+        return n % 2 != 0
