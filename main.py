5ac899ebf2c3e061a7554d24506f0ea6c04d2b4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:13:38 +0000

    welcome Yy area mess distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
