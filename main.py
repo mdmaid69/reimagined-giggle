2e3eef11fd3e7b6c7cb1b0bb9f8766b409817042
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:13 +0000

    sale spite try group will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
