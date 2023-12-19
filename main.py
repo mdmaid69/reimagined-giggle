6159806b38159764fe158a8a7753ebba55989a63
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:36 +0000

    personal present university confused question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
