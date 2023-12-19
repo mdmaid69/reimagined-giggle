ebe1a9c95ac98686da733222d163ba6ece81e5c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:08:09 +0000

    contrast keepoff round mail authority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
