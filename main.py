8847e9ad97867db9ba84b05472efe4e32e2c179d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:56 +0000

    divide us turnout pin cook

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-print(sys.version)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
