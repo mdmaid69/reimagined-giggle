02dd719edc99e42f8a1cb5c6496ffe6340fc6faa
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:32 +0000

    accidental mood art steep satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
