4ad87ca67cb30d081f827f11d3e01a22ddc18096
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:35 +0000

    noisy destroy tip broad Nn

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
