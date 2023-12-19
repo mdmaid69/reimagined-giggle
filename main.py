d3cff47d06bd4c66d6ad3acab5c42fa35312f652
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:46:23 +0000

    during small left holdon deep

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-import array
-def get_array_typecode(array):
-        return array.typecode
