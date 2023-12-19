2790adb1248b8cb462fe7e551a5956b3e6cc556b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:09 +0000

    nothing innocent charge defeat love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
