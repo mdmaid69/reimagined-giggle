874147649659c798b196ce5bb1481cd079298ea2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:12 +0000

    something conversation cruelty broken train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
