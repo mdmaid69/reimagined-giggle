ef6c1be925a8d8d60ea8f1b3b3cf1f002d0f7c43
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:55 +0000

    all communicate dick second post

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
