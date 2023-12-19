b4102e908dc7a17fb88fd3587cf2a9c4cf8f532d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:45 +0000

    noise bring join meet interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
