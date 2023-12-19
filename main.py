1de44e653f9b1528660ea3e91579c1324887a2c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:24 +0000

    improvement under breath twist love

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
