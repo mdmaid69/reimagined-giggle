5a7390028c4d46e96267224084e89a4b8a6d9256
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:24 +0000

    anything immediate physical value pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
