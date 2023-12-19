662c9396cd53c09ae0ac8345b830e1eeb016416d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:05:56 +0000

    relief owe sound person long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
