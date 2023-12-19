0363b196e29cee88ed453104cac944aed0b7543c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:35:58 +0000

    type travel attract close avoid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
