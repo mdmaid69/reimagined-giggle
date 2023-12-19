2c6c094b0237ddab8a03929bd51a90a0f09d0ecf
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:56 +0000

    comein own offend mine copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item(array, i):
+        return array[i]
