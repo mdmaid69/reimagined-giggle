7c96be861acdc3f07961c5604245bda8fe92666f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:17:04 +0000

    immediate change edge enthusiastic illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
