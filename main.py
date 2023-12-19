642256b8476ed14591c49d2a92e702709a2f21b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:15:19 +0000

    tradition dark somehow out split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
