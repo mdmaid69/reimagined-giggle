b72cdefdfbd0c6276be01b0ab88525ef6a156ca7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:57 +0000

    class sink simple sick hate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
