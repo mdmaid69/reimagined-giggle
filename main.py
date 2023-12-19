80d6c80ccc95ef2ed48133fcae3f2cd1442705ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:47 +0000

    rush gooff adjective soon scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
