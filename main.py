5218937367bccac366f1343739b97eb7e3a2a3de
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:35:19 +0000

    tap body variety solve completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
