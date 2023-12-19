596780e70e80d52c8a9962dfa0787afaaed02216
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:41 +0000

    repeat listen draw my get

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
