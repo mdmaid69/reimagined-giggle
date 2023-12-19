51edab9485f3952c9ab3db9aee11b0188d78d3e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:55 +0000

    rubbish wind process Mm habit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
