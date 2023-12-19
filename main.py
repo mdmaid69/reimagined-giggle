388a9535e0f6fdfbdd0d743f5110a8872f8c6588
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:52 +0000

    shut gooff party failure permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
