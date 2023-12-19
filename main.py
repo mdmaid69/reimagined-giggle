0c566b1b0df600184a5f4a419703d16e4f087c83
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:39:36 +0000

    where goback(to) up adjective dear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
