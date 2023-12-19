07a678b552bab35fd3b654475597250298b3dbde
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:23:50 +0000

    fresh substance eat change together

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
