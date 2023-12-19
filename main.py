717d0fec9f7896207208636cc526ebc9f49532d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:07 +0000

    frame lead legal majority none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
