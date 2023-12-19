81aae89d2706dedc05e33ced25d76333896e86b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:21 +0000

    definite mistake giveout party programme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def reverse_list(lst):
+        return lst[::-1]
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
