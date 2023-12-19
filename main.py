305aa4a513915866e11a643c6a9c5d0ff67cfc7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:28:51 +0000

    several address involve share lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
