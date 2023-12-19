a990e7d773bed58f702b6aeccdb7d02b320f90d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:11 +0000

    part provide outside hello burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
