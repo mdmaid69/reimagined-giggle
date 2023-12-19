68fe37d72ef143daf868c0075f6d7728dea782b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:00 +0000

    release shock object damage thought

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
