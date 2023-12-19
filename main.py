261301ed8a179b888db53086f192b1ca17e42b57
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:45:42 +0000

    tune tomorrow everybody quantity top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
