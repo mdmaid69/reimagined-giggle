063a9413315743029e8d00ea23c35f8ebeee07d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:40 +0000

    unit serve move hold shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_binary(n):
+        return bin(n)
