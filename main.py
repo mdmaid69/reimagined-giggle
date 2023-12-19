08cfb74ef62e3580c6e272d28aee6db5303cf8f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:55:49 +0000

    freedom sure very cinema bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
