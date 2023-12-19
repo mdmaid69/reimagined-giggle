f682e69f4b8fc247055b569290cdf925e2f2f05f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:11:31 +0000

    central pause later when slightly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
