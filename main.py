ef5e471c2a216e2252cc575279076a9464519347
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:54 +0000

    across key treat mess little

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def convert_to_octal(n):
+        return oct(n)
