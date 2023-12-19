eb538bf789466837373537457f9e49a82b8775b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:29 +0000

    sale sell guilty pay pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
