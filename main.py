3fc83b73f8164828ea38a2cc88cb437d42846301
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:01:47 +0000

    translate uniform drop attention ever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
