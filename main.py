0a65391c08096eadf6474e4e6af6e2fcf2495a7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:34:10 +0000

    design polite close clean plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
