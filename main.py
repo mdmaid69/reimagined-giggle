804cf093ecdaa008d5eb8de9019c1252a86fde5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:11 +0000

    create suddenly improvement amount godown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
