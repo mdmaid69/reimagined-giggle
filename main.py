59dbb67f46800c7223fe3468c74578c5b4406104
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:41 +0000

    continue old matter change everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
