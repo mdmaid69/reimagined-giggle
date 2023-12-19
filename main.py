f39f7dec1c6e9ceee918092a21bf45cf52c253ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:34:01 +0000

    paper shrink Gg left set

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
