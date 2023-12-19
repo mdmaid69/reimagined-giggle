0a4f84cbc3458fab7a63a85cd41f792a37cfd14a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:30 +0000

    tip command once probably takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
