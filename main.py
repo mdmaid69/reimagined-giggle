d3cfa30c31eb6e21770024db8104ad3179e4ccf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:15:59 +0000

    friend judge effect only recently

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
