a6ea15ad04788440409c9d2c9967e053438c4681
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:16:59 +0000

    turn suit buy lookafter majority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import random
+print(random.randint(0, 100))
