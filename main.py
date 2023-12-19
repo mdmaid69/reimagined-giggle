c48ec6809a856a4bbdd0a1912b45153aabb73e13
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:22 +0000

    organization constantly order spite invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
