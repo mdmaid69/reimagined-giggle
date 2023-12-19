4354db09fe675a3dc2bb1c9ed96ecab54cad60e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:27 +0000

    play comfort division beneath search

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
