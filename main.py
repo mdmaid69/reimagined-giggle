4be27699c398d90eaca2751869d19ccb34330c58
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:29 +0000

    goback(to) back stop Oo behave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def convert_to_octal(n):
-        return oct(n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
