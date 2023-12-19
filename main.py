e7cd7295c694583ee27a34f8be562a7d4bca94d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:20 +0000

    smile especially duty lunch combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
+import collections
+def create_user_list():
+        return collections.UserList()
