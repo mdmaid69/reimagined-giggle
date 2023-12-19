0d6dc8c22dde29b3f2f0deff9b3f56b9953c3d69
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:10:55 +0000

    separate keep happy familiar another

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
