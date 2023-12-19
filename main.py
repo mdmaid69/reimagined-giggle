bb44dd64f938409f4a228130932f488ee67a148a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:45:13 +0000

    slight gather go worried holiday

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
