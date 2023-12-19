45bca0bcb25b9486eb9d88ce1eb6446d04f3c17f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:27:59 +0000

    beyond coffee easy different thin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Characters:", len(text))
