e045a34fc3c67454b98052337f18faf28d57988c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:29 +0000

    thank bringback thick anxious but

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
