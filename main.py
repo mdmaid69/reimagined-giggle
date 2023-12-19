4b42eda788847feb4700831a55eaa370d7436d82
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:24:43 +0000

    usually explanation laugh modern problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
