5f86105fac5fbb7ee6fb9b9efe74e2f88d6aa8a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:28:28 +0000

    chat sore immediately somewhere comedown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
