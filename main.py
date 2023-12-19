1dfe770263a5d6ab523b024a8101c4fca9d363fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:33 +0000

    drop breath ilovedicks luck question

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_min(numbers):
+        return min(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
