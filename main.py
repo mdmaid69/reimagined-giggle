5e5639bfcbe262639e631c7e46d50580c378de27
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:07:59 +0000

    responsible low obtain steal feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
