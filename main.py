db10864c1b4531feb29ab3ec4c5d95457337966c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:57 +0000

    sort pause control offend doctor

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
