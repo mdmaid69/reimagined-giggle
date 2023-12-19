422cfa6cc572bfb90e0d19cc1a4e804d6ad9c4cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:44 +0000

    right least scream take write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_volume(length, width, height):
+        return length * width * height
