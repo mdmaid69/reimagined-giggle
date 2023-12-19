0cbdf2e409340b9cd120ae8ef2207e6fa130ea4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:46:29 +0000

    work feed odd share personal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import datetime
+def get_current_date():
+        return datetime.date.today()
