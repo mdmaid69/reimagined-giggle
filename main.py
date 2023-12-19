4be91fa86a51dfe8cb91d5037a80f4a3542c474a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:46:38 +0000

    ours contact height herself protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
