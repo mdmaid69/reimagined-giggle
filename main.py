bc1b7b9dcde810aa7ee866c518babcc61426eb87
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:42 +0000

    sink comment listenfor separate press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
