66e18c0ebd817dd0c64589f34c4e461976e05e1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:36 +0000

    traffic fetch goback(to) extra situation

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
