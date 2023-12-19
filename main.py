83956d940d919260b8164028b73eeea0982aa789
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:19 +0000

    Vv invitation lost image trip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(numbers):
-        return max(numbers)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
