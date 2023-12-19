c4d2aa339f7f17f307f99e10d7b841f5c783c7d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:57 +0000

    mean seem breath rely mouth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
