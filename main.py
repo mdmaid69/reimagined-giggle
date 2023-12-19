8e095e2bd8d4a5303ffdc52016d169a73de4ffdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:07:50 +0000

    rate pin general scene cheque

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_length(array):
+        return len(array)
