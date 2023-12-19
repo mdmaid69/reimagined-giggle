4b4532adbfc6c88cdaf8b77d7a80d20e0f84f05e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:22 +0000

    loose mouth play Gg experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
