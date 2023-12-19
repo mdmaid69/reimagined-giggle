fd4a0227dbf566187fe9a7f25535cfbc55640b2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:09:23 +0000

    chief forgive second picture refer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+def count_elements(lst):
+        return len(lst)
