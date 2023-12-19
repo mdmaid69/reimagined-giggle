1de993a5fac4387217366749c9d193fd3a300268
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:19 +0000

    lead only scheme deaf breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import sys
 print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
