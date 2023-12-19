635e86e2af4c8ea6a26c55f49ec8e270387ff3b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:17 +0000

    near deaf custom getup serious

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
