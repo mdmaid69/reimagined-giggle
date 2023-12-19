eb80b80bbbe6ccc34dda8aed29d97b5c176e9953
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:32:13 +0000

    border otherwise thanks bus myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
