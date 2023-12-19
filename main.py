94e1d73e768bccc72c09c34b1ab88d60dced6eea
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:37:27 +0000

    speak leg giveback later gooff

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
