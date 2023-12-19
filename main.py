382db9feabe7e10301c27030a919a56ec52612e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:03 +0000

    command attempt hot exist few

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
