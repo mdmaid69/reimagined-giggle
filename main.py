11e3ce6dcfea50630737e3dc9cd504bf8f311a0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:58 +0000

    hope month story else system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
