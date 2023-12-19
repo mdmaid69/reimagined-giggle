80d807cdd1939befdbd4b580f57e55a4abfbd637
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:48 +0000

    powerful authority loud support climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
