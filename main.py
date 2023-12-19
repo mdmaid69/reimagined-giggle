6782947bffd51995c3840d08c272ee0600a1f827
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:17 +0000

    afraid after act skill passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
