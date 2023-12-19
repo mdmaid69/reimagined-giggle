c96bdad8196e05b16efc7a7d0ea7f1e8ba8d08b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:35:08 +0000

    actually increase turnoff slightly aim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
