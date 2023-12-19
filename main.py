1981cad9b57380203a1beb7ba0be02780e0b6d10
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:28:55 +0000

    majority show realize sell Pp

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def count_words(sentence):
-        return len(sentence.split())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
