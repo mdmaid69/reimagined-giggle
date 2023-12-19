6afb0a49aa3f0c9473fbbcbb14b4781a93055671
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:05 +0000

    for tidy public fast communication

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
