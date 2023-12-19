67b83ca754b303cf64f8b7d33ed8ee9ae2d39374
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:41 +0000

    view example greeting near and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
