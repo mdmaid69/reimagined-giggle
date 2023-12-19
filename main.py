ee03cb62c86809435e367c21675f6adf1bf247f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:53:58 +0000

    whoever damage pool relative human

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_set(array):
+        return set(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
