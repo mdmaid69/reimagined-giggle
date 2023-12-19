004f87e84449c3aca99c4366bbca3afd6c5d1655
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:33:16 +0000

    curious confidence realize up dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
