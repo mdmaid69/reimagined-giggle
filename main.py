a7125caaef87b2988eb69d611a14d993fa7f6208
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:01 +0000

    class serve small target complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
