8dee4b0ada77e7b83d853e45d33ce806c596ddb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:19:48 +0000

    never firm shake invite different

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
