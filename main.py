092b23063b1ba99e1e3a06e6192aaf19012ff6dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:54:26 +0000

    and burst take official middle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import sys
+def print_python_version():
+        print(sys.version)
