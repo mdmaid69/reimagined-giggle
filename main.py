b42cb4ab69dc9c53a5659f994a473eb9985cfced
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:33 +0000

    moment heat interesting smile agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
