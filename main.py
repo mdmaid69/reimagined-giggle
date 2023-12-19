39250d4c6f5b44c322084cd73b4a16262b811721
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:05 +0000

    entry use branch leg tool

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import random
+print(random.randint(0, 100))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
