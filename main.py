fba7f2958e7de4027fa8d79145a96f0c56e4a1ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:29 +0000

    practise tip game error less

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+name = "Python"
+print("Hello,", name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
