e2851c92724212727aae38d72d76bf18a07f2b73
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:05:28 +0000

    depend workon announce thankyou I

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(5):
-        print(i)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
