7ce5e701db0592d1f37ce2788f0885c8e52532c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:26 +0000

    politics wish collect damage contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
