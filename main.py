dbd9f7635f0857a4ccc656395d524deec2d9f964
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:26 +0000

    problem collect nervous edge show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import platform
+def get_python_version():
+        return platform.python_version()
