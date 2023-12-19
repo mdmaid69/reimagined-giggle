af011d75f0f223f3374dbfeb4ee56dc82c4c3d31
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:34 +0000

    every game total then survive

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
