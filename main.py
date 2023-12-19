536e76d3ea93559551d7615205ee2ef2db3ef657
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:28:01 +0000

    practical bringdown imagination law frighten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
