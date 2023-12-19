6e3918c83612f86b547f1490777e7d6790235f04
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:47 +0000

    frequent tired sentence face problem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
