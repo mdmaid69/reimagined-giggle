dbf090dd4459faed5cf9711bb23fff80eddc83cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:07 +0000

    start happen regular single imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
