d87afe2f6c2015e833bc019109ff090c46e2718a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:36:00 +0000

    language expression she comeround film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
