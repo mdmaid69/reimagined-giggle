3969a696b7af8df7f11d98888f6abffba09559d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:26 +0000

    gooff cap deal fire float

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
