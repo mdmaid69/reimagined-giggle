ef7cfec248661d3e6cff3da08df2fe80c68d30db
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:31:32 +0000

    watch point cut lend waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_length(array):
-        return len(array)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
