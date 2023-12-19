3c90efe83c1fa0b4d5bc91225140da7d220f4bcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:28:59 +0000

    rush tour capital manage room

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
