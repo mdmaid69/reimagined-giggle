867828a5cd358739ca75cf70caeaa354f7328360
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:37 +0000

    nervous limit correct innocent edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import math
+print(math.pi)
