b7377246964be7683fa43216b6fe910518d27816
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:01 +0000

    question break welcome change material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_working_directory():
+        return os.getcwd()
