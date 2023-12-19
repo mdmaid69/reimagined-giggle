5060e94dd0b86d0c9085f013ff01623ffce88cc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:05:59 +0000

    wrap pleasant gate chase father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
