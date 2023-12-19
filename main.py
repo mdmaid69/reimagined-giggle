f8b96f1714c3aca7895089ad657c38bc4eca214b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:53 +0000

    break rush early disappointed advantage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+for i in range(5):
+        print(i)
+  def convert_to_binary(n):
+        return bin(n)
