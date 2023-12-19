355283fcbbf55e964027d5779be274d0e6187674
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:59:37 +0000

    Nn married advise law target

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
