e8a95927bd3d3088b9fd5f1239ade10d2290a72e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:11 +0000

    repeat minute even forward effect

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
