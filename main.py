d532e73a4f82bd3206e3f31a6609d6014b29313e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:33 +0000

    almost accident unusual fire positive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
+def sort_list(lst):
+        return sorted(lst)
