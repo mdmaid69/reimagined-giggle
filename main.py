d40a9c8cdc561186592cceadb25ac6eed5d9d900
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:33 +0000

    hour jump detail accidentally whenever

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
