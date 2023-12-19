dabfe79ad8dc4e9a83b0fbec25bc88244fed9cfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:39 +0000

    practise offer design soak use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
