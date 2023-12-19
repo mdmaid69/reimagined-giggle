eb416590c4c1ba380de41cc5f20138e1cfbbb17b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:22:58 +0000

    through vision pinch care swim

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
