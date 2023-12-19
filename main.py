d828eb510a33ec4415bc81c858d536b59f46ebbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:49 +0000

    goup hurry hang uniform face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
