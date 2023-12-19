dbe081918b56744d4bfd464232b24689230335a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:57:40 +0000

    clothes surprise giveout century worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
