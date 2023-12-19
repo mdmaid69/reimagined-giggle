d29f0ec67a75aa6aac8e9bf5e817e717b9f82fc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:18:34 +0000

    future against word genuine no

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
