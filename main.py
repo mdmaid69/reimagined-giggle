12c23d2f23893b628122106fe67593709a53e073
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:23:18 +0000

    care ask custom week wrong

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
