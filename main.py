81b01c85fbffa1713294018d95338e9f0fe7fb83
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:08 +0000

    Dd none defeat argument play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
