779fec9bc78e2bbcae5bd7cfea15126a7ac0896d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:49:05 +0000

    early simple style draw the

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
