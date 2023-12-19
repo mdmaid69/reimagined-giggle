a7dd594375212557fd98450a2944aa124a961176
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:36 +0000

    that modern pocket Itself dry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def get_array_as_list(array):
+        return list(array)
