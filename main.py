dabaef09843de40efb1469d1d35b3fcfe09f1742
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:05 +0000

    destroy giveaway also burst design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
