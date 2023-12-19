05fb2b587af6bd74d5c5cde80606907541cb9b09
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:24 +0000

    sink plate unhappy cheque limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def convert_to_binary(n):
+        return bin(n)
