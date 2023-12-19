8f11a4046f2cbe96191aa3e9d52a8d3541be6863
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:27 +0000

    protection difficulty lot unlikely slowly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
