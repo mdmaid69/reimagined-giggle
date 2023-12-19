2c9e683e20222605d670016615da9da1be225933
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:29 +0000

    late or story welcome practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
