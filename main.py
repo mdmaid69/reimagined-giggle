3a5e08b095e105c5d29c1a271b408b25cd4697d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:59:31 +0000

    evidence block face scene mainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_density(mass, volume):
-        return mass / volume
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
