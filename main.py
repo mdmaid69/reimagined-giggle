03f90b774dfa207b425ef9e024b37fa6beb86258
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:12 +0000

    doubt load dead himself software

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import sys
-print(sys.version)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
