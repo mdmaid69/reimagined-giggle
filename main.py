e3273cdc98f559a0da31295b09f1b3a4da39d885
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:11:45 +0000

    fact low season communicate get

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
