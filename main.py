9d385330392eb278cd845cb160da8f16d4cd07ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:11:37 +0000

    safe charge supply out friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
