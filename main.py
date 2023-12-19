017b3e7fc5f5cae04eb934a0d7fe77e185236f83
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:22 +0000

    Nn spill prefer full Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
