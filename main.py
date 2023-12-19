ed01c8fc5be3ca8faed940ad5bcb6d67c864e73e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:44 +0000

    chase declaration fear claim shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
