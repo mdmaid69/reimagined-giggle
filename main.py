c345b5b6f5c8deba428be82678121139d8c9e277
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:35 +0000

    stop chat smoke last keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
