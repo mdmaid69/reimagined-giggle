6f3c717986adea7aa609f6d9cf091d6616aeaf82
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:45:48 +0000

    Oo autumn probably deserve body

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
