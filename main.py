260545001f9d950eaee1b85ab96c2aa3e9e1a59d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:55:44 +0000

    regular fly safe office strike

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_as_repr(array):
+        return repr(array)
