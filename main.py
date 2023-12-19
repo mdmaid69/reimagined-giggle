159a836866266b5a43ba5889a5cf53f12e1c39b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:04 +0000

    here boat card proper who

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+print(sum(range(10)))
