35c34ad736dc45fbe3238c6eb882248233bfdbfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:43 +0000

    polite light Dd stage getaway

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
