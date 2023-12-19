d207e079eeb47b6d21e51fa11631971d1a7abaf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:30 +0000

    comeround protection thankyou strike throughout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
