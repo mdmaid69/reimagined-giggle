9cd1b3db26430ff79a253d393179c254ac538435
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:59 +0000

    step Ll hurry runaway lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
