1779e6655a8a59f633a09f052cac064b1eda8d7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:03:34 +0000

    really variety she generous minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_force(mass, acceleration):
+        return mass * acceleration
