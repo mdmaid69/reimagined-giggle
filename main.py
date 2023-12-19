850a5bfbf23402dc4ce7c07198155723f382baa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:33:03 +0000

    opponent remove print trip sick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
