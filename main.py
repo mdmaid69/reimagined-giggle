3f177fd9b7b0d2b6d06690a97bd4e00abf6f6c4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:42 +0000

    estimate react buy always kind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
