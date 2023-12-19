6d73b881e909e6302237457a8248cc22988df67d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:05 +0000

    quiet firm persuade Gg kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
