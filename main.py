1706ad1d3d8161cd07b3800ed4ea7e0016f52400
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:53 +0000

    delight yesterday image refer callup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
