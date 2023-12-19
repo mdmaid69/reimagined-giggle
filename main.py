63be029c8902b338d001c3618ae3e993004ae1af
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:29 +0000

    shape only trick clever tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
