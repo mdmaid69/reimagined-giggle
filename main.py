39466a1fad88fcdd5fcc8b957c26434a13241f98
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:49 +0000

    once good idea few sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_sine(x):
+        return math.sin(x)
