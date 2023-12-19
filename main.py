89a904e192d495b7f3bafd4db330f8fafb94c76c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:08:37 +0000

    married scared succeed mean everything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def greet(name):
-        print(f"Hello, {name}!")
+import sys
+def print_python_version():
+        return sys.version
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
