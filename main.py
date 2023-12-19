7f5eef06d2a9c1dabf7cd14e77e88c8f1c4e60dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:27 +0000

    stamp correction remove suckme nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
