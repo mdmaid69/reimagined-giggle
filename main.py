b6bbe0fee93b899c35d2fd4af4b785f2d563e743
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:28:59 +0000

    talk clearly callback involve dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
