455dd027e1c03a5ee734efb42a67b8b29f903dad
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:30:58 +0000

    think game angry thought personal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
