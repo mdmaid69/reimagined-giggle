c8c60dff8f3d92fedcac4accb1122f9c29ad933e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:21 +0000

    share careless interruption afterwards pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
