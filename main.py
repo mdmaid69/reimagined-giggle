ff3aa3990a6fc182ea79795d70ff47843de1153c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:18 +0000

    fly underneath material waste tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
