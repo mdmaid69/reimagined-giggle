1c50f483eb0ce664bd3bfc8570055aa287c3c9ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:22 +0000

    pen occur trap whistle protection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_power(work, time):
-        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
