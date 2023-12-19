c39f3ddc6b95a653e720220b33c190c3d70c81cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:14 +0000

    in 572enter stage soft thick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import sys
-print(sys.version)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
