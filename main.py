c26e42caf32902e8d75b059af5bb5d1a66986bd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:30:35 +0000

    measure debt scrap firm its

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
