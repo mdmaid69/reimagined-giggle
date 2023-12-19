a8d31dd2c8f60eca4b5e174ebba0978142a749a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:41:41 +0000

    land team well believe praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
