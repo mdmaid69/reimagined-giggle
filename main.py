cb846ba3b88f618aa318de0b7e67d3c1ca9e13bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:46:23 +0000

    float what until section appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
