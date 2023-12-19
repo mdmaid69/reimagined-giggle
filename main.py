72286830cb2e98c6c6af40d94221757a71a38748
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:56 +0000

    definite home confusing precise question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+name = "Python"
+print("Hello,", name)
