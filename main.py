880eee2c1855b3b5118beedd083dc369aa156043
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:43 +0000

    worth laugh shallow indeed sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
