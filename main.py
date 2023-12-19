7bdb8245f85ff97183f8386fd07461b6b30f1e14
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:40 +0000

    release right reasonable winter round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
