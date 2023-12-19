200570bb0ca97c94cc02f03d30861c2d1ad09b6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:45 +0000

    performance machine prevent battery minute

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
