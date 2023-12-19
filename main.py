c7974ede11495d1f9caae2d8b62d3e7c6f10e9b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:08 +0000

    quality duty already match to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
