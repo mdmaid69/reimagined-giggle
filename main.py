91f9457147058d199200819c0374c8a7b226988d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:19 +0000

    pretty regard jump promise after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
