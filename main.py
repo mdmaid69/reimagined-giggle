60d6c28fb8260783d992694a80ba33fcab3f40ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:56:57 +0000

    continue dress off exactly avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_speed(distance, time):
+        return distance / time
