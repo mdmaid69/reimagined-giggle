08e3f7cba204ba47eb00640591be541faf0aa11b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:43 +0000

    rent supply story blind march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
