be1488acf041ac4bc6552e5b068a62bf4532c680
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:08:39 +0000

    behind relative method convinced pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
