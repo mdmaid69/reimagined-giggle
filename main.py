12a7a482d4ce2688a28379d28e4389669d3389c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:18 +0000

    afraid tired ask float debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
+def multiply_numbers(x, y):
+        return x * y
