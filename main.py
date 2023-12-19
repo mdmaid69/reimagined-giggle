4c11a8935ff7784acd62d58df30c33c417df6b81
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:50 +0000

    goon everyone make exactly save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import json
 def pretty_print_json(data):
         return json.dumps(data, indent=4)
