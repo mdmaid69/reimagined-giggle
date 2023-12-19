ec41b3c2cf0f176a72e4bb9d1d9d67dc4f5bb53b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:08 +0000

    interfere hurt should sign authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
