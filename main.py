603f13c3843b6b2681113000724dd79affb31b41
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:25:10 +0000

    rain measure dress along next

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
