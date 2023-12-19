4998c969785766a39f541cc7d73af0e952159c51
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:20:21 +0000

    entrance mess touch present responsible

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_item(array, i):
+        return array[i]
