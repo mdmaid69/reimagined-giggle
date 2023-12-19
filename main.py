e1311e5c01de9822a5422f4d0501a209848a7b73
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:21 +0000

    fortunate extraordinary wide one bang

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_typecode(array):
+        return array.typecode
