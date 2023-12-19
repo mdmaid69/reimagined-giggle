4945f40bf47a691e9df7518bda0a3550140ca977
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:00 +0000

    apply issue sing Yy feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
