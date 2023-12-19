7880b161a22faa69d4cb47098d0f5d318764e18c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:13 +0000

    imagine afraid giveout lot leg

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
