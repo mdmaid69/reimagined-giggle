53e6042561a2e1172302f110d4bb3c588253fee9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:40 +0000

    set best reply single smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
