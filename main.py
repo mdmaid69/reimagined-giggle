a47825a7bf98894fd35e02264e6c519b328084e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:22 +0000

    minor male second club later

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_set(array):
+        return set(array)
