e42cb27668373aa84f9e96de42a4a1e14af03577
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:16 +0000

    important bunch hold think manage

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
