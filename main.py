5876432de462f4deea7da3c40b2c154a4e51d1aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:02:21 +0000

    wash certainly work attach interested

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
