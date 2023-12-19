eefe95fe94aa39977022e6de60ba176bcd59865d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:46 +0000

    accept tough damp 572enter habit

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
