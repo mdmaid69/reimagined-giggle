b2a6312a21e2ed8f5b308265bad33c09b1d65bfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:34 +0000

    real neck invite great breakout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def convert_to_octal(n):
+        return oct(n)
