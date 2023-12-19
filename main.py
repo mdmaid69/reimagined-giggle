5dbc2dac467f8853948dccc9bfda74c9ceb3fe1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:32 +0000

    ring sore within mainly achieve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
