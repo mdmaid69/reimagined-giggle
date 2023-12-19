0588b283a9b5a45f017171e036158cbcf44e806a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:11:09 +0000

    also bound reason allow rescue

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
