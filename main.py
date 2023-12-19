b582ca80fb006ccc36a3446132156852fd10aaf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:43 +0000

    thin after harm spill place

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
