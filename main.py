5a99286147dec8b66ef47bade4e3078a60d712d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:45 +0000

    century declare by distant but

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-print(sys.version)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
