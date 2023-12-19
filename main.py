ef68c025587d4a674e52434ff42f19c010ac5bfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:53 +0000

    form situation brother will underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
