6ce1d330706d97063cae5fe588f766e94344f74c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:29 +0000

    Zz environment thumb pattern kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
