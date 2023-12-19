c926bebb92da1c3f147e3bb18f3dd1baa7bb9078
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:30 +0000

    single below thick major passenger

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+print("Hello, world!")
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
