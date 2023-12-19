0370a7fd6a640397b329ec208a935d7efc428126
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:28 +0000

    community hold dance process minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-import collections
-def create_priority_queue():
-        return collections.deque()
