99c2609885c55d70089085dd92af8c2ffb1590ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:33 +0000

    hurt uncomfortable like exactly fond

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
