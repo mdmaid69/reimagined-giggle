abeb0ecf20becf10a795103a0ffd2a470e16fb8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:17:38 +0000

    sister closed mysterious poison dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
