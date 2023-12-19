d497c7c7b0bc723a0b40af8c03e15eda64a751ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:14:46 +0000

    exist fork fail burst respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_directory():
+        return os.getcwd()
