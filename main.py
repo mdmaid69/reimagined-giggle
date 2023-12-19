6268fefb2671423db4323a5f7c3e269809ecba65
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:46:53 +0000

    worth dangerous emphasis shop he

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
