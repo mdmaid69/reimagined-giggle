c1800001241e4413301f54ea4fc4f6b830ec91ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:28:46 +0000

    so boil straight waste heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
