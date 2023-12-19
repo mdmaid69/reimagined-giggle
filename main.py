f723bc49a1f383c2ed15706470a12aa04986e683
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:37 +0000

    region giveback abroad comefrom friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+print(math.pi)
