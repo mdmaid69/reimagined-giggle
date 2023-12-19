581e206b3f4455e82e49b7f16572f2023b4e4de0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:03:20 +0000

    reply almost turnover nice promise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
