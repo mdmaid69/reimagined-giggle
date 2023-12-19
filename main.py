f4f982f86aa1a1f10f07f0858b4db6fbab2afb2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:05:45 +0000

    test mystery smoke shine culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
