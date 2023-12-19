c1b6eab20c31b8d0cc380d528aed36c7141aa66c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:09 +0000

    length today bright clearly doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
