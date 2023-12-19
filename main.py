e33488db33193e14ee258b123bc4b5650473709b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:47 +0000

    interesting equal school knowledge fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
