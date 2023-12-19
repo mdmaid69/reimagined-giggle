210ff72f89e085b3fd76d749e3c93e83443da3fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:28 +0000

    label distant shadow this polite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
