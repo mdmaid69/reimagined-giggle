cddd49f353ee70d491fc2bf319dc803557de9ab8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:16 +0000

    extra thank basis do light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
