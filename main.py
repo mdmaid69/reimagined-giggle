2d3caff07743513ab99fc02d8bddf541e8e1d422
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:34:08 +0000

    paper turnoff winter along develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
