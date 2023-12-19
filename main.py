dc6279f92651d0aa22082d359a4351014482b921
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:56 +0000

    away some promise pretend time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_octal(n):
-        return oct(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
