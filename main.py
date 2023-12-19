daca5538fd84ba15ddf949e701ca01dfe1432118
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:57:16 +0000

    avoid someone some contact memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
