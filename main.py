1129fd4ae32302fb5885488e38d82ae271154d8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:50 +0000

    through interest area by abroad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def get_array_as_list(array):
+        return list(array)
