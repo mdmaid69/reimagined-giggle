cd59ddf26862f34ab7df3426378a5905aa4a3ef8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:14 +0000

    getback black conduct sudden in

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
