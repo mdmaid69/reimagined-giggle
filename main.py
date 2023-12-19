eef814219ecfc24e27a54a4b6c72eab326fe4959
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:44 +0000

    travel object on separate thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
