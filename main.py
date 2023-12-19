c3fa1a6749961ce8b6b4d1cb25b72b6a4162ecdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:02:07 +0000

    contact dish voice continue side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def append_to_array(array, item):
+        array.append(item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
