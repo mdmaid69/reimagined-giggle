050828de3b7a0545fc59ae8060825d9a5ed10289
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:58:44 +0000

    crazy call concentration proof direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
