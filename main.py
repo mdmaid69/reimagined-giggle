70da2fb67fca763e475ad2b5da149c3592f6b47b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:45 +0000

    reply funny field rule help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
