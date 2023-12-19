b4e756b7032875e98c7ddb393bc0d63edc3a97e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:54 +0000

    decision equal travel basic will

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import time
-def get_current_time():
-        return time.ctime()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
