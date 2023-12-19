505888fe839a6529bab1731b15aa5ff4ba697975
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:17 +0000

    idea breakthrough pile breakout drive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
