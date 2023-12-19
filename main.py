015f9497c6392dc6a943ed0973215ff2041a6ebb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:57 +0000

    ours begin impressed cross some

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def is_even(n):
+        return n % 2 == 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
