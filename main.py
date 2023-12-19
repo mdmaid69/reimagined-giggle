4b03679a43becbd384299066784dc3677e814b76
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:03 +0000

    border ear frighten both get

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
