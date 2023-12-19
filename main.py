8e09264db399c09335a21d7230a2c78439a3bc26
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:04 +0000

    school deserve getout well breakinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
