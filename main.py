b0d1c4cf244e37f8305b5993e80f5671d6c44a86
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:47 +0000

    provide chief desperate argument identify

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
