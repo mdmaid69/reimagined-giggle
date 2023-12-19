47614122f2539eb54a01f135bfba0eb4aadb1cca
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:59 +0000

    back simple vision past skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
