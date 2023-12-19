2629ffbee9012dd304aa19c79b3d2d3219d123c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:49:33 +0000

    fashion take sweep disappointed scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_speed(distance, time):
+        return distance / time
