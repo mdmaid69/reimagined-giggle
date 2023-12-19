c80ec30eb34c3fec640ad5358081ee30dadfb38c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:03:35 +0000

    wrap lookafter hurt warning male

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
+def calculate_speed(distance, time):
+        return distance / time
