2df068c7867c8e702c0b20d6fd9925f059608054
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:35 +0000

    chance rubbish Rr pay sit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
