c4036b7eff2182502490245df3a0880695f7a543
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:01 +0000

    instant grow tongue interview whistle

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
