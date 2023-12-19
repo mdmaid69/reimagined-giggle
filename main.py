613e498797737a763e3388209ddd14c3f255f580
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:44:43 +0000

    although disgusting slight break common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
