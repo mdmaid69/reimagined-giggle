26390a8ae241ea0e9a52be2aee72854c237a5728
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:47:39 +0000

    no little act fall relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
