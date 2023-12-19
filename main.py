cb7c743dbe9baf04bd244b0312827dca628ac02d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:05:43 +0000

    offend community recognize entry instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
