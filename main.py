2e9d871e73afb98aec53d35c023edd4cf368bd86
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:17 +0000

    store evening reply maintain average

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-print(sum(range(10)))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
