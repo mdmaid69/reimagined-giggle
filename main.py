e5f9f10fc5234032d433a0f356c69b79490c1ac4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:20:36 +0000

    least ourselves habit complicated love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def find_min(numbers):
+        return min(numbers)
