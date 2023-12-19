f8e3f90863519f6ad7d597699551e9ba2c9d5ca1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:20:32 +0000

    watch like pressure criticize bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
