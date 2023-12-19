2cfbcffa0618377d69de10177da459c8fb235d8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:39:10 +0000

    design sting steep test mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
