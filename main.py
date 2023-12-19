f27cc956a3963cf870b313c28cc9079e9c341905
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:51:32 +0000

    snow child similar might bridge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
