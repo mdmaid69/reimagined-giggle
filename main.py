2ffb6a01d19facdb415f6b5bb6252a0a2da9eb4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:31 +0000

    OKorokay gift partner unfair stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
