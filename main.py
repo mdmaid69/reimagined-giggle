a76455ccbb8f222b5cf1b301c90d40f24bc57c84
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:09:11 +0000

    swim work common judge organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
