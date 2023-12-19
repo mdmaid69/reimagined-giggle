b52281e9c9a859437686ba4a4dd1910e006f532c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:44 +0000

    duty frighten order race duty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
+print("Hello, world!")
