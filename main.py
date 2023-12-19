4e5ed9fd7ae1f4bd36b140e4061f151a3b965268
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:36 +0000

    gas lonely carry bringback custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
