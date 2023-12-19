0096b1d0bbd3d7c89a69f09fc572242704991d25
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:41 +0000

    letter emergency blue crash bill

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_current_working_directory():
-        return os.getcwd()
+for i in range(10): print(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
