c91a422c781accabe4a85c7c793245b91bd61abe
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:27 +0000

    ordinary design base hit motion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
