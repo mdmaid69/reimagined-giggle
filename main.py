b09ae22ee3c28701ad64bfb11d47c8ac82c520a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:22 +0000

    decide recently wash question realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def find_min(lst):
+        return min(lst)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
