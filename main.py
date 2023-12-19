9ca20d1c0896c97909e7611e2ca413d00af072ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:57 +0000

    edge passage loud history punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def clear_array(array):
-        array *= 0
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
