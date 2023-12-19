40bfb66fdba3c4ad4e8baa50258a135af4f3bfa6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:39:49 +0000

    blame refuse bath frighten supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
