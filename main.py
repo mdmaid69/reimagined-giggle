cc9afbb0a1bd3fc4eea237dfab3b2caa227255b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:14:33 +0000

    giveback damp when copy fill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
