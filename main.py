3a64eb3129ec9c63103f291b9a60f8b17bdb61b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:58 +0000

    will warm shallow past sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import time
+print(time.time())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
