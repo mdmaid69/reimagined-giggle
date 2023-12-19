5b06ee35bc761cfc17284df3dd6a65428187c113
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:04:10 +0000

    thank unfair occasion move laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
