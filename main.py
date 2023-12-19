5c9951de57ec3ae443059ced78ca470bd23a58c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:35:55 +0000

    put law everybody experiment illness

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
