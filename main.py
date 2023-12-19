0635cb283788af8f0ae824d2bb1f14916e4b3c72
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:25:22 +0000

    Kk modern means beneath fast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
