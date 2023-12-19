4c060d0af9cd9ee97b37551b6bd4e6b6cfcc1fba
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:58 +0000

    bath art your best best

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
