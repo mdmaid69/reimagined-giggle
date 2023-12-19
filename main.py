8a268c23e1eca49ec6e08aa7d38f3821c4b7e2d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:23:42 +0000

    themselves shut callback comeoff roof

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sys
-print(sys.version)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
