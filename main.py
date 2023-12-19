37b6757b2437b2b9c25cf9d47bd9d51067c6b3d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:48:33 +0000

    write drop Kk female circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
