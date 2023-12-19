b9eee37d4b345c19c7e5f898a2fbe3fc4108be17
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:40 +0000

    backwards damage cut mention book

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
