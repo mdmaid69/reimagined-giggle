7751f718015c44ed6dda5baa52cc23d30407267b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:54 +0000

    thanks declaration reply beneath dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
