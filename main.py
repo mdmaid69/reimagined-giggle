154c3edc2a99352544d4337b1493cc74efadc6a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:24 +0000

    search develop divide ashamed future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
