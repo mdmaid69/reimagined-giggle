e88afe89eb5c6ead3fc98baf2e32da31432d74a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:23 +0000

    hand favourite passage spring turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
