cbc5becf79f2f3818023c8920b1c0ee35416d814
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:35:59 +0000

    arrangement easy apart bother project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
