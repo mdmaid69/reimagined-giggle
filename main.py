d9d7576208b8a3609fd3b0184a2200eaf9e6e244
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:17 +0000

    pocket whistle swim Ww born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
