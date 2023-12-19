40ed92e3150a0af801862a79898c4b1f767e1724
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:52:17 +0000

    loan getback workupto fast upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
