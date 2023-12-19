b5388e872bede527134c097b50b051dee918e8ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:02:42 +0000

    appeal sensible depend school never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
