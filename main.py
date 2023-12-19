81a59bef6cda405aab2d5d461774eb36e6bb2030
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:51 +0000

    act Ff old debt nothing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
