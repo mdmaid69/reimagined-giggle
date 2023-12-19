f25ac95f6d2206b8f14218020bae1906ee87a110
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:25 +0000

    fill separate communicate especially afford

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
