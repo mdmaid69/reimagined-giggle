f4f8f601c3dc3dc83a0730ed74bd79dcbbda0953
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:50 +0000

    believe weather beside reason entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
