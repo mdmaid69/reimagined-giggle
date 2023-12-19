5c469e5b51756d0a29bdb36cce8ee03a272207c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:38 +0000

    slice board mean birth willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
