a9b2bd310e8143200bd88f14d75fdb9afe8e2394
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:43:48 +0000

    impressed himself laugh artificial take

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print("Squared:", [n**2 for n in numbers])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
