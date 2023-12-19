76c82ffdda43a67bbfe3963ba09255de59c28835
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:39 +0000

    wing funny disturb trust side

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
