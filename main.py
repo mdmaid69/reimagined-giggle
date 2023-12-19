91e9441191700ca321cab758454d14184d5733a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:03:07 +0000

    turnaway small might cover bringdown

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
