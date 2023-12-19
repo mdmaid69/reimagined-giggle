4319c1df3f97e4247828dc2907dc7a877a36a772
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:47:42 +0000

    factory journey miss gate argue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
