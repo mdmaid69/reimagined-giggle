e0a363149e162ec3c11330db8344128ede24a648
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:13 +0000

    band keepfrom without pleasant surface

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def subtract_numbers(x, y):
+        return x - y
