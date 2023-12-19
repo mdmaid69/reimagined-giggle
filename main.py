96837cbc43a6a8dd66e146d2c70228bbc9141641
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:02:38 +0000

    takeoff Tt score gate hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
