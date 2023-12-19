63709020128d4b34ef54d6f8487af3f3aa41fc35
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:02 +0000

    nor pinch feel nigga silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
