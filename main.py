89afe8c394b71fb8b50858ccf3c43121089dc897
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:37:33 +0000

    crazy squash our squeeze get

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def square_number(x):
+        return x**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
