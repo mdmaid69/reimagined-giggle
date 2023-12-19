05b54b2798e7517d340279bf0fb596c70dbc4bea
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:43:52 +0000

    any contribute urgent explanation accommodation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
