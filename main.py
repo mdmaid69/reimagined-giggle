d50c52b9e396438df9f85df2283023937ee6cdd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:32 +0000

    warn crash purpose drop whisper

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
