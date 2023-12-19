518cd180859fdaceeaed186250c53615980cca45
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:34 +0000

    fold surprise city fast phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
