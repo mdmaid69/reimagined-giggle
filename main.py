3584075b681c9131696d04f95286f5de9a1590d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:57:24 +0000

    tell guess grow straight note

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
