97134b556c7fa56ec9f3b9e75701683f7dcad94e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:17:18 +0000

    who pin wander repair never

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def square_number(x):
+        return x**2
