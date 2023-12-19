8895f2a3faf32618847df898c58118cd8b0e0666
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:54:01 +0000

    remember giveaway weak contact fork

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
