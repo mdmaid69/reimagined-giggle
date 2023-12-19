69bccdce961606c9395abaf85e933814c3e612a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:35 +0000

    able among false persuade home

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_rectangle(l, w):
+        return l * w
