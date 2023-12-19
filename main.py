5200a1c814d331312b2947a7bf6d748260723d7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:00:57 +0000

    tense bush film confused brush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_pressure(force, area):
+        return force / area
