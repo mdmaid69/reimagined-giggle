99145834b07a5f653de2a5b028251b9db183befb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:23 +0000

    water ear difficult bottom collect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
