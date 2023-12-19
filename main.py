13f89731bdb4bea915acbc8e8153deb55a44535e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:58 +0000

    responsibility afraid bread break slice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
