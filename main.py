dd7ffb11531f4df4872e4d33c15ccf2cab3b52f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:19 +0000

    wire exactly thick degree clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_density(mass, volume):
+        return mass / volume
