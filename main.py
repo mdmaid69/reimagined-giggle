0a1465cde63df3a12281281664ce615993ac0b14
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:38 +0000

    call general estimate passenger public

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
