95b3a4e7c9e21494b063409bcdd1977a2d29e2a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:06 +0000

    container steady spot furniture behind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def count_elements(lst):
-        return len(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def subtract_numbers(x, y):
+        return x - y
