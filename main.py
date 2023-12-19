cf09886848bc179cfe285ebbabad407a37756531
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:10:09 +0000

    discussion adjective long motion appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
