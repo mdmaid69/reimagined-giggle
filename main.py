18a0617aebf68628b106d00bfa56122a27c03bf4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:34 +0000

    interrupt wise so criticize burst

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
