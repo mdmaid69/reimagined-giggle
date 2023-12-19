be8602a094519fab072cf0fe318ae1416f13f3b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:15:14 +0000

    drawing it hurt cut turndown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-for i in range(10): print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
