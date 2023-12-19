1cf1445f14361cc2baa50a7802a4fd4a66ee680b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:08 +0000

    commit target right sight fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
