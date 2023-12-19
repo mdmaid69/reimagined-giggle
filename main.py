ad59002cf7cf985a13724a9f5576abbb856052e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:33:50 +0000

    authority breakthrough uponon money generous

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
