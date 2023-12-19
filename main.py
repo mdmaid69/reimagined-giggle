51966e55f0778a1c557ce2eb2b0d8243a32abfc4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:22 +0000

    minimum bite comeout familiar gift

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_typecode(array):
+        return array.typecode
