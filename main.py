59acd6997166cd70c67bbd758f9c7130ad3436d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:20:52 +0000

    belief programme with offer finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
