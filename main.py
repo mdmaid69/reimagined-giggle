7f605249ff6d1d0ee8e4bafbbbc00d592c11d0f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:23 +0000

    sleep computer correct 567enemy cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
