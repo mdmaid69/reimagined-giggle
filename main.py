acf25f8a1dcafcc909fbaa1017ceb05d13133bfa
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:16:30 +0000

    square be surprise smart production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def add_numbers(a, b):
+        return a + b
