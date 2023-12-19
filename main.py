3aea184d85e106e72e67e467d2d7105a838786e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:27:53 +0000

    busy breath takeback never unhappy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
