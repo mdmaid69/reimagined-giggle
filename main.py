a40e9d0dedddb97260b1e729420b291186acf030
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:05:27 +0000

    heart admit day swing remark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
