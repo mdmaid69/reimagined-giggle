7bf0dfaa979699a4903efd2c14bb58d886ee4220
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:04 +0000

    undo than calloff divide bring

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
