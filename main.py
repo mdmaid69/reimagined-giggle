7b9d72ecec981c623868393a107a6077ef0dc561
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:18 +0000

    off calloff rub separate harm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
