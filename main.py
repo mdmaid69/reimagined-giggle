7c99bb2db60989ec0486f2bfc74938ad61f97d3d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:11 +0000

    shade height comeoff opinion complicated

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
