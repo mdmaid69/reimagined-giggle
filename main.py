5fd200bc99274d418e210e0f01e10d95ea730921
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:50:51 +0000

    worth project wander goaway describe

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+print([x**2 for x in range(10)])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
