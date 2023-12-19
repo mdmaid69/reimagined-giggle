b9702d24ec33583af0fdcb98d62cbde487297b65
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:45 +0000

    worth right makeup physical bottom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
