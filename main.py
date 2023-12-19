b52ecf7baa10d5873a13c0ec93dfac341ba14261
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:01:19 +0000

    dress first welcome catch forever

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def reverse_array(array):
+        array.reverse()
