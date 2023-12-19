15df02ef1a5547cedf2134078659d2ee07e8c635
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:44 +0000

    light sting cook think description

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
