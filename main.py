2db1500064ddafc6cb7b7ac3ce82a75e7a2a1c16
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:30 +0000

    method mail reply young quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
