5d73eff081bd9b0623952c564f4bd8b73dfc5cd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:32 +0000

    off decision ashamed delight responsibility

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
