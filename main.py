b868e7e4efff6474cf32a113814c116cffba799b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:56:26 +0000

    fortunate them foreign perfect Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
