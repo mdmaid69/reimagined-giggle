ced9bf8f05388781d8bc9bde7f1e36cececfff3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:49 +0000

    fortunately holdup clothes worth tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
