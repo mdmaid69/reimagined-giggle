3b4cc34f67c6d2e929b6a84c52dd4a976213a7a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:38:34 +0000

    sky alive class afterwards train

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def clear_array(array):
+        array *= 0
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
