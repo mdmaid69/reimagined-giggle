02aa2be3ee1fc0e6901b4716edb35a47f3a990c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:49 +0000

    basically decide content commit far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
