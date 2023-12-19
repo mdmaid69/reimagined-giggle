9bac068013f18a36b2a6fbb9fb8ece3d92e1bf40
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:04 +0000

    well wake he care after

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
