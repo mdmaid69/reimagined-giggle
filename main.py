41ede5fe2399322e73904f62b3f25634d47d1972
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:06:41 +0000

    table cheap storm amount old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
