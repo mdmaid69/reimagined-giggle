fac8e2b42187ec0cbcc79762e6a9fa46226e36d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:29 +0000

    supply convince somewhere lookup remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
