6774ad360547958a2ea14ed37fcf6f370006605d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:21 +0000

    beat proof sensible brief straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
