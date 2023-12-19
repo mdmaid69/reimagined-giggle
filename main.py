93d50e032a8473bdd3aff408e6d85ddb59c77fb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:08 +0000

    entrance guess turnback passage once

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
