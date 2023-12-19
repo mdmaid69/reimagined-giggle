b2ea8bdfae322aefe544ffe2661b333932615a73
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:15:37 +0000

    light threaten get square mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
