6b6d97a6c0b604cd89ed885075c8f3f66f67adab
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:53:52 +0000

    beginning bag plain famous difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
