d0c6b86737a47454adf4ba122cbcd401e02235f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:02:45 +0000

    behind body clothes funny side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Average:", sum(numbers) / len(numbers))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
