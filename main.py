97ff551f11b866005833824c8f0126d4669397b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:10:04 +0000

    guide partner party concerned cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+name = "Python"
+print("Hello,", name)
