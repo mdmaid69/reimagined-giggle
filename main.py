ab7768111f53dec427ed1e5ab9b5766618d9c30b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:56:03 +0000

    nowhere reach flow underneath minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
