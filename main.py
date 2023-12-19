44d4ec0ecc917c1818e874d88f141277cb21b53b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:38:03 +0000

    careless somebody basic share still

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import sys
+  def get_python_version():
+        return sys.version
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
