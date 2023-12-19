ea75017d534f832211bc94d3c22807e4c113553a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:06:03 +0000

    crowd ring project exit do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
