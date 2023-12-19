209ae9a31202b6ccd46e09ef64f07ef86d9a9279
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:18:59 +0000

    attach ours lead copy tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
