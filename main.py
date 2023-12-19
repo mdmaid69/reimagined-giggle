b8772b7760556807599a61f649e2cae94772232a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:28:07 +0000

    section last available steam chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def cube_number(x):
-        return x**3
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
