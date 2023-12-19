12968f63def76927607d39dabd1bead60b4ea4b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:08 +0000

    takeback connect closely advanced arrival

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
