a9b5aa02b43b6bd9ff9743cbe9259fa1bf595a3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:22:25 +0000

    bill his male comment clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
