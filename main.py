bf3ff7415e88fbb3946d386e34eb878c68367570
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:22:47 +0000

    strict poor bring before profit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
