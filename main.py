9ee397c3980d9151e6b8cfa808b970100e4b2e21
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:57:04 +0000

    block message Ww back recover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
