e9f6950e75819a05620f18bb00a0cfc876c7d993
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:56:55 +0000

    hit market Cc hot stamp

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
