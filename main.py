b8c13eaceb8e076d95bce0e2cf5f360a428b36be
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:32 +0000

    measure cry damage object ask

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
