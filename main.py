d76504b2857489c973875c599d833d5a85036ca0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:37 +0000

    poor whistle own giveup bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def square_number(x):
-        return x**2
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
