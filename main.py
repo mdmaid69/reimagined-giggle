c95d2e4d771946e24ce1bfd45e4984fb02421dd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:56 +0000

    mix breath fear swallow roof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def square_number(x):
+        return x**2
