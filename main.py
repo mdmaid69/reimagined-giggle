a7f909c61493f285ceae0b80d9c23397c947390d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:33 +0000

    blame need sudden copy weak

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_pressure(force, area):
-        return force / area
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
