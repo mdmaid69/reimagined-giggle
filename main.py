65bc9112ca904646f39975026e3b488baf1d10ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:18 +0000

    furniture Oo process calm sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
