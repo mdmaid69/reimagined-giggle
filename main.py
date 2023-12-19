1104269eb41915c4fd4ae88879bf69ca98162206
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:21:34 +0000

    bring heavy base pronunciation service

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
