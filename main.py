45979c07e0ec5bb9e8d1a59eb7dcfdd3315baabf
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:04 +0000

    minimum exchange reply run reaction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
