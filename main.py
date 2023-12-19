b9d9b624e2ab718607896d6df224e11eed23e155
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:42:12 +0000

    blind wave fresh mass war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
