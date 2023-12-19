220187987f3eddeae580803726905cf219c7bf7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:18 +0000

    satisfied brother cap discuss in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import random
+print(random.randint(0, 100))
