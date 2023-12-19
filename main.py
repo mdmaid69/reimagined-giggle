96b19f34a9618ac904dc1f90e298ccc775280da9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:06 +0000

    secret complaint wild high home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+print(os.getcwd())
