5e723cbc652587be26db9eba2bafc84a32065f47
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:59:11 +0000

    rather collection morning past spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
