9c56291fc9f7dafd74579011a62230f7322089cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:28:58 +0000

    tidy sure contrast file count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
