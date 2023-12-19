aaf6f504ac18cc9c52168326c8c904f8d0406825
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:23 +0000

    slowly trade whistle city advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
