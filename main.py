79dfea8e612f4f2b8f73bfc1a65e1fce60391056
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:24 +0000

    strike business failure claim keepout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_list_from_array(array):
+        return array.tolist()
