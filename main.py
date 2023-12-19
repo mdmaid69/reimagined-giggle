8e53422c8f3fbd0a8a47b65fcd9a1ea8ab363ecd
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:07 +0000

    where bend whoever tongue wear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
