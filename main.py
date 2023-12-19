10261cf509a1afb04e265c6a81e72a9eb79a688f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:43 +0000

    large figure lookaround length practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
