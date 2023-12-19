41e50933adddab5badc53ddf6d0dc826ad8ad341
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:02 +0000

    food communication bored completely drink

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def is_palindrome(s):
+        return s == s[::-1]
