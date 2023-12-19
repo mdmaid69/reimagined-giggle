d053a31a53b1f9be213531c3de8fdd7597106ae3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:46 +0000

    address themselves cruel shame she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def add_numbers(x, y):
+        return x + y
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
