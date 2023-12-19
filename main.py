e9300964e80a76b99c8bf2c585dcbc4e7b5c0656
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:20:07 +0000

    common traffic post comeup shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
