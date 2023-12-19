d299a9b2c435f5274cb00367d2aaeccc871b4197
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:18:29 +0000

    month flood fear ask complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import os
-print(os.getcwd())
+def get_current_working_directory():
+        return os.getcwd()
