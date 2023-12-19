24f652a1ac77744c86d84ca48cc9391b6c7e80fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:02 +0000

    start unless 566end safe goaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import collections
-def create_user_string():
-        return collections.UserString()
