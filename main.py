65bc9f8fc0f8bfbd61b7cc446ecd7e4faa57d622
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:12 +0000

    sound geton light statement correction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
