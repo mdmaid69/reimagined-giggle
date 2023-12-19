1e2c3999ba5a03f894ff35f6fb44d06a043ba7ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:32 +0000

    serious protect approval uponon nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
