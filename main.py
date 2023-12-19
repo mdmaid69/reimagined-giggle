bc0fc4019765e477428a65666c045c14e58ffaa6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:20:46 +0000

    please my deliberately cheat spring

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def is_odd(n):
-        return n % 2 != 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
