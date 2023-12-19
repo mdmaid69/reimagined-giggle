5677bf76cdaeb62e2bed6f041d849c8479caf223
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:15 +0000

    walk film earn thinkthrough work

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
