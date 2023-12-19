a486bbbee957ff2a97c54165e297f923a15a04c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:53 +0000

    seat challenge unlucky knowledge seat

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
