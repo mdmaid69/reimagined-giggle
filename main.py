f1ad06422c575aba9ed1595299313a7d182048de
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:47:29 +0000

    whether refer maybe conduct steal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
