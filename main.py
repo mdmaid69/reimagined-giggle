bc6fa5b7cdf3add083e60b38f0a6a6f67d2cf744
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:05 +0000

    look needle vote total interview

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
