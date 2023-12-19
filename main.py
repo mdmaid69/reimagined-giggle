750cc605e66f1169413afbcc08274cb0bafc5473
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:48:37 +0000

    because spring minor count unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
