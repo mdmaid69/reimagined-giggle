c65e9ac9fed7f009415ca632b57f681bf3e7c231
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:46:52 +0000

    than instead figure date fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_min(lst):
+        return min(lst)
