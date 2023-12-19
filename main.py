9ce2c9f5c03a2a08cba5444f91faa596d6ab1b74
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:46 +0000

    dark organize bright admit large

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
