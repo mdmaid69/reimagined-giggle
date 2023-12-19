4c0ace5e31962a14bc083c78a2acf804968b8116
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:54 +0000

    dress duty pool regional mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
