ca5952ab5854ca0ced3e9f4487d0aa5b0cf28bf7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:18 +0000

    anywayanyhow modern 565end sort drink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+for i in range(5):
+        print(i)
