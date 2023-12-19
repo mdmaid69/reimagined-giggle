85d2e31c65442befe42acd9e74a16b6705cf1615
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:16:46 +0000

    quit past regular movement immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
