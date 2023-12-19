7be5f014195ac3f9a4af60996db8cbc28b57d082
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:44 +0000

    independent worry proper port out

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
