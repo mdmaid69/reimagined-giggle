42cae0cecbf1f47c3a58a4eb6fb0382995dfbcf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:58:34 +0000

    unlucky turnon goup business scale

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
