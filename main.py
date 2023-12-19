7864ebcd9f56626be0713aba137c77b28058832a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:15 +0000

    blind inquire Ii read even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import datetime
+def get_current_date():
+        return datetime.date.today()
