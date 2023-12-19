5a83a7815b9f8676e54ae310d706e387f664a739
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:27 +0000

    unhappy woman woman Bb repeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
