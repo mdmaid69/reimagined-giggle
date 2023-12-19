2ca6aee3a0a1aa4c0f422ff2ac80be5d96571975
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:16 +0000

    disgusting production hospital event side

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
