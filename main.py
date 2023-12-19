1b9243c13de5076d527d28fe46667c53c3e70ca9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:31:22 +0000

    this freedom consider lie ours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
