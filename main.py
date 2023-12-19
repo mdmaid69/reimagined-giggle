e954a6637832ede4d791d05f7bb5e22e2d4e9e93
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:23 +0000

    begin present size expect middle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
