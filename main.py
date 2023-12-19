c67ad6c37c536a6e33449bdde6362ba226457073
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:17:59 +0000

    569enjoy store fortunately backwards freedom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
