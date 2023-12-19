b5fae28c0039fb92ba302e54a24427aa301c8a8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:12 +0000

    appeal next bringforward hello callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
