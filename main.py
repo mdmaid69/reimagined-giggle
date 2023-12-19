b471d94f89968793a2c4af371883906d86e59f90
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:40 +0000

    away branch turnoff form number

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def count_characters(sentence):
+        return len(sentence)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
