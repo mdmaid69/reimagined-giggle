e7a8d69c0df0f075b740304989013eab149161bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:28 +0000

    shine tidy down 565end picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+print(time.time())
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def remove_directory(path):
+        os.rmdir(path)
