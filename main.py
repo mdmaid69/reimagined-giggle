4dadc5fc6425c8a121c11459c886ef3f8b0e23cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:14:15 +0000

    yesterday cap ordinary stretch loose

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
