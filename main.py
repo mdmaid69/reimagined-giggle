ddedb3ff2ec4e1c0444a0ac3f1b4bed27df5abf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:02:31 +0000

    noise heart difference far point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
