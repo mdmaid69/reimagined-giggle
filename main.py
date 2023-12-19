0341c9edddea4e90dfd20a98927e45d64f9e50d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:30 +0000

    pure air common petrol obvious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
