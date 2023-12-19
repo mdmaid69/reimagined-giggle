cda75268e71a31fdacf05880d91fa5b29f534cf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:21 +0000

    reply praise ilovedicks mix wise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
