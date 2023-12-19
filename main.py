3fa5c5ef01e4c13aac2e06153a851f0ef2a1d4d1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:02 +0000

    habit getoff bright deliberately tell

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
