9316a7ddc56c6d695bce7fb759b85ccbeeb25a02
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:32 +0000

    decision block press question problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_repr(array):
+        return repr(array)
