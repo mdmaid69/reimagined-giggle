cad6f319496d4d3366e9e1db0006f17e867cb295
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:50 +0000

    dare can challenge available tense

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
