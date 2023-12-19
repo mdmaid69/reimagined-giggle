2194a563d00f8dfd6a6fc825d90d73a0b6f24df2
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:28 +0000

    scream arrival danger visit bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
