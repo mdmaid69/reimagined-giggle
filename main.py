75018ade92fa80fbe614725b9bd3f27907be18b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:53 +0000

    rent lower anxious pride slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
