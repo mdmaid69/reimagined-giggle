7abafd8d4f3a94fb2a62327bc777d312b79bf5ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:38:36 +0000

    right before dry day previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
