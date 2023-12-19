58b1a5160374939db349731c9ca6b1c4348a8cf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:51:34 +0000

    pause talkinto afternoon sharp check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
