fb2c4da076ab4527325ec32118f426dd5f7acd25
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:31:22 +0000

    by by sea partner wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
