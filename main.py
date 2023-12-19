b6ebea39e2863ecc4b8001ef4314938e0df528c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:49 +0000

    autumn move prize wild measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
