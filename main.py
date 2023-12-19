8d7c5e52ace09fceb2e6a3b7acde6d2b2fb6113e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:05 +0000

    special sea again workupto win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def add_numbers(x, y):
-        return x + y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
