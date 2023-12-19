42247293cad842105df58b4e6ee3e07f229e414c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:23 +0000

    combine gradually string cap rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
