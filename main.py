2510abc564d7f3ee198d0647760d5ecfbea9169c
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:59:39 +0000

    failure beyond national rain schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
