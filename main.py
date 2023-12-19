3f7c6839a97d219831dc8e55e86b3ef4eda0410a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:43:41 +0000

    downstairs impressed choose invitation true

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
