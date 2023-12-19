da4f76543ced7bd666b58eff88bb6098c72b2741
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:56 +0000

    regret joke beginning trip person

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def count_elements(lst):
+        return len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
