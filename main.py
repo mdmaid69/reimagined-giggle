dcc885b2adeb3c97003aad2ef2b45c34e309d7f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:28 +0000

    scale if mention unpleasant this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def count_elements(lst):
-        return len(lst)
+def get_array_as_list(array):
+        return list(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
