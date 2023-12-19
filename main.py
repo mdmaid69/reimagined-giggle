48f0213a86a43170c10bb4a37cf2a83faf9f5c87
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:45:56 +0000

    trick star space angry foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
 def set_array_slice(array, i, j, iterable):
         array[i:j] = iterable
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
