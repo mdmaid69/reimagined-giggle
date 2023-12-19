dc90ca906e7d1075a849488f0d409853ae65074d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:33:57 +0000

    error feeling neither project secret

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
