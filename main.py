29683fe5c50176baff6bc1b6d9637f2b9a4b2dd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:24 +0000

    runover punish size world open

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def get_array_length(array):
+        return len(array)
