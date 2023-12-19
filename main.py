1bb72ea7258f385f8931482eb85ebd632e50f24b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:03:27 +0000

    shoot alone act convenient save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
