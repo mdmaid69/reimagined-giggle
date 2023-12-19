abdd9d4b031f861016f2ad45215f78241a16453f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:18:20 +0000

    polite to proud innocent would

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
