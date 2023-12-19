f10a0752d822f1fdf8a84c2000e2cd145698482f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:09 +0000

    window jump plenty uncertain base

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+def calculate_volume(length, width, height):
+        return length * width * height
+import os
+print(os.getcwd())
