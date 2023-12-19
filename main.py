d9f00c22b8803d7e5762525465360afc328d2f30
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:19 +0000

    post do star wait bringup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
+def square_number(x):
+        return x**2
