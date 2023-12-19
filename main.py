867851a17c2e7877f28008d7004ff540be68efbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:35:50 +0000

    wake perform butter promise born

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_speed(distance, time):
+        return distance / time
