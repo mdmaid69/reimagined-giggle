6f48337af099e0d893996173dbbf2e0e19ff1d6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:01 +0000

    myself order basically strict cheat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def divide_numbers(x, y):
-        return x / y
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
