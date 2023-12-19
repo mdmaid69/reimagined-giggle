da2d26802f1e652eeacc1cfb82b0c6d8cc5ff160
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:05:55 +0000

    chase answer unlucky wild object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
