a0ac17c5f8ea0da9ba89c04e5cafdc9f0ca9b94c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:42:41 +0000

    become finish even coat curious

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+print(math.pi)
