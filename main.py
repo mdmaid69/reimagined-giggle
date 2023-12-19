0dfeff070e1f70153668973a987cb2526dcba27c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:15 +0000

    tap inform persuade involve related

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
