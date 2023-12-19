c6df62a627bbe67249b5c9402aa8ac83b2068ae5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:54 +0000

    rid whisper confusing very which

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
