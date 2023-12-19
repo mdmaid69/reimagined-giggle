013f96ca0a9435f4486153c5a315edb92de2de3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:34 +0000

    necessary accident earn dull Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
