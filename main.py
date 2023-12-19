3ca7f0e6041c51016ac69563731076db566e0d1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:20:57 +0000

    machine more hope danger top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
