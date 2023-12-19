718612bf214d6ebd5706aff09cae048d6b8e8026
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:33:47 +0000

    off dark ready around original

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
