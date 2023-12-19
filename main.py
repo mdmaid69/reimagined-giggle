aee7144d41c190a32759d39802e92d39fa679918
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:06 +0000

    offer refuse note fetch choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def reverse_list(lst):
-        return lst[::-1]
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
