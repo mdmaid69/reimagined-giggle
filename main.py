73f73eff89002c5dc277122dbd41b4fb6d1c5db3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:37 +0000

    sing pocket meet notice contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
