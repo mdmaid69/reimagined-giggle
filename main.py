0893bb2921b9fe639799da171c9655d06c3c67af
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:09 +0000

    steal admit pocket uncertain raise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
