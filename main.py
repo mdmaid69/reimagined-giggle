d4da3942b35a7e82943c655490fa4bc326d306ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:14:58 +0000

    day previous fine active callback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
