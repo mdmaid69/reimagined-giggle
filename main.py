2a649d54586d87d2780dd8b628847d117f10fafb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:26:52 +0000

    bar equal certainly frame damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
