b8ea669e32dbf0ad46ed933b0aa31b1f62320781
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:44 +0000

    door finger sign cheat constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
