664e35c7b5c3676837b65eb79be2140b42b93d13
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:54:36 +0000

    bright prove debt motion fresh

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def square_number(x):
-        return x**2
-def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
