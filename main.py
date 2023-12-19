eed7f4477e62913b4e7eb5ecb54f5a131e81c8a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:02 +0000

    deserve doctor preparation spend play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
