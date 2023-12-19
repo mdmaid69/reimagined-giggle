64aaf889ec697bb51467c1c64819725bec0dabed
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:05:42 +0000

    power school big through fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
