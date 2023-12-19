74aecd641edd69ec21d8213eecc25fc788faf68f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:58:41 +0000

    sensible slow comfortable think check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+name = "Python"
+print("Hello,", name)
