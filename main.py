c6b51131f0b7a1239eb4d56d7c46037a0c1f6704
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:12:41 +0000

    across machine eye home difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
