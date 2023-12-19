3b41d395e81de6bea32db8ab6182b63bc160e20c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:38 +0000

    window conversation holdout concerned picture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
