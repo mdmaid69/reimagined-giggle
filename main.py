cd9d00fb901899f31803bb328fbc987669fa04af
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:55 +0000

    strike necessary and defend spare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
