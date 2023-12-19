702a73b7f90341d625993e5255baae10790fe8fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:57 +0000

    community wish entrance below well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def count_words(sentence):
+        return len(sentence.split())
