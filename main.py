67ff475ff49e99dd5fe5bb2dbd9778d2c8ae3c23
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:50:46 +0000

    bite afterwards nose accident needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
