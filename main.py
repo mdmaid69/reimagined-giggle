1c225d05ab586b49d5ac448e089d127bcd8571f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:11 +0000

    breakdown face flood basic circumstance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
