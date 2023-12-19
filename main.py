94210c9f2ce1a375278718164b0fcddf5b8bdcec
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:32 +0000

    getout only explanation protest sleep

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  def subtract_numbers(x, y):
-        return x - y
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_set(array):
+        return set(array)
