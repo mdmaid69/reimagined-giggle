51453a58c78c25b1c76c16a821b8bd5d094bfee9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:31 +0000

    slip summer rest desperate persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
