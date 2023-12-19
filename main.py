cc39856a561e1651a14af25e2cbe3d29a14622b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:01:41 +0000

    clothes mark cheque ourselves entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
