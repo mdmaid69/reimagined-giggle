492176b0b910e78cb8b02bffc6afeaea916facc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:27 +0000

    future afraid chat suit promise

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
