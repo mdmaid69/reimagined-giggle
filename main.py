2117810bf90e426a0674c0819d9caa4b6faab235
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:03 +0000

    in trade thumb low stick

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def square_number(x):
-        return x**2
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_power(work, time):
+        return work / time
