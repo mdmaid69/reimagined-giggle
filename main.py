7d2b5c18f22aad3444efb783c8a6f067e7be6731
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:32:25 +0000

    thick more any tell design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+text = "Hello, world!"
+print("Characters:", len(text))
