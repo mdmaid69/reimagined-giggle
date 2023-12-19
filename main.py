e6bdadaa4ab547deca4171d17bfcfe9b5b80afc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:37:21 +0000

    right callback impressive fight takeaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area_rectangle(l, w):
+        return l * w
