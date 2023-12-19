c2d554b1b8b12772f72269ae7e5884956ec8b1b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:55 +0000

    right welcome politics store answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
