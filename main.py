cb1d102ed1905d37a6b5e142ead9f95b8c022786
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:04 +0000

    government manner clothes stamp maintain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
