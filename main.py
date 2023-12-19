1515dcfb6942c14dc3097bcbbc8da8e1e0580b88
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:41 +0000

    attempt check neck sticky trouble

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-print(os.getcwd())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
