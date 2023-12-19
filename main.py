98fa6e557019f0658905ee46fc0333d729d13700
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:57 +0000

    appear Ww concentrate subject violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def is_odd(n):
-        return n % 2 != 0
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+print(random.randint(0, 100))
