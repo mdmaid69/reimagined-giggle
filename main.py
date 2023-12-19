3d2371aed81399006b23f1fe44b2f4c271039e9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:25 +0000

    embarrassing bound anybody noone walk

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+from collections import Counter
+print(Counter("hello world"))
