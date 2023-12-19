8ee2fa491cd9481d47e7151c8965d29e80bdaf1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:17 +0000

    improve like standard comeup solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
