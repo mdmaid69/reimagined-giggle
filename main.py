a3e5f132bc5480e83f59f6e6a2b1d57dfaf3100c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:42 +0000

    waste them step lunch stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import collections
-def create_queue():
-        return collections.deque()
+def create_user_dict():
+        return collections.UserDict()
