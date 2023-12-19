4b9baa4cbf65997b08acb63e0f279987ec246694
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:57:39 +0000

    connect wheel follow laugh exercise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
