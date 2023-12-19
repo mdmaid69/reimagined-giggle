7234acc171de3a8a14aacd3d5d69774a0a36faeb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:41:07 +0000

    group sentence 569enjoy serve reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
