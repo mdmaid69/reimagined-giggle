2c48d6f3f9942e0e99c289d600d043e5c171d829
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:09 +0000

    bear flow advise top colour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
