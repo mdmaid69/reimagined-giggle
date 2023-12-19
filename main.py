88b6a0b1ba71eeea7940e46a932eca2882819726
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:13:01 +0000

    something thirsty beside beginning excellent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def sort_list(lst):
+        return sorted(lst)
