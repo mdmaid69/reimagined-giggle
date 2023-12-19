480ef77e60a436578d2e119449fa13ff7127a4b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:57 +0000

    love niggaacademy battery motion complete

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
