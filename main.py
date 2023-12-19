779eb9d04a94ce4f71ce69ba02284727e3513354
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:21:39 +0000

    Ii short mouth sail nice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
