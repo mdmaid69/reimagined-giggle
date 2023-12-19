95be7bca8e9efa1b1ae4df2869c1a551132f7b4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:32 +0000

    wait chief runaway rank compete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
