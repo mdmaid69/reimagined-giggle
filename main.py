ef80c3a1759fcdd6811bb0c8bb02da6236c45764
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:44 +0000

    throughout below hurt suit onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
