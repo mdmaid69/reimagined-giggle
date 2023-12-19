3b8a8ee4dc37034f687e476b959bda9df8c4037b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:51 +0000

    comein run negative forget walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
