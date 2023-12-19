47a5ab7bca0cd6226affcda72f2334f33a0c1d05
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:47 +0000

    sale consult afternoon central scene

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def count_characters(sentence):
+        return len(sentence)
