9ceae2a2dee3b4d892c0a141a371b76a779b88ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:30 +0000

    swimming violent hold scream right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_list(array):
-        return list(array)
+def append_to_array(array, item):
+        array.append(item)
