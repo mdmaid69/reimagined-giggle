1ccb3f4e7c40931c08cd16159985d2e449198f61
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:35:57 +0000

    road attempt whole grade letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
