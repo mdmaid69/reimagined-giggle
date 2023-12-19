16242e64235800f60aa56d0dc029a39bcd692ad0
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:30:16 +0000

    offence to begin run please

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import time
-print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
