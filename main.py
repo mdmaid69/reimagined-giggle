6aac1312a03f71b32ef259ebd3d96cbdbd7a7e36
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:14 +0000

    die willing flood burn rule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
