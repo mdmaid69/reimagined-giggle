52772643ef79bc53c789349c44f748f2f5d9b4f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:38 +0000

    behave finally force save information

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
