21707eb43003bb78203785d047507d5b83a9f417
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:59:27 +0000

    view common factory ocean reduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
