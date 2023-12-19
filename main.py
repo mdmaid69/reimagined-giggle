f6061848d14e2d3c69d16ba26da40f046b30db3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:15:28 +0000

    glad bring rule speed any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
