c16fae01f2b77449ccff7403e80ff3b3ae8bc324
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:01:50 +0000

    content practise worried getoff patience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
