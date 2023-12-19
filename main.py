f9f3f1db26127eb7436cceccbe437d22342da08b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:40:15 +0000

    meet convinced with dream middle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
