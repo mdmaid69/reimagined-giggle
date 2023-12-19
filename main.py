0c38088c3736f60821e4608fe62b06470a488fa1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:17:16 +0000

    development sing of poor process

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
