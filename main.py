5d42ebddb299d450d822dc3b1dec44ecc23b9f3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:59 +0000

    badly game flat cruelty embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
