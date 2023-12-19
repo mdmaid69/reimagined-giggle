f67b7d7e6ee5c83a3cf7b5e7582aea4562aa4515
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:14 +0000

    our myself drink all organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def convert_to_hex(n):
-        return hex(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
