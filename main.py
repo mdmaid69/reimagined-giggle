5915e782520030f74807693108bc1e90c06d970e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:35:07 +0000

    capital how people sell certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-print(math.pi)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
