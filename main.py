602c2461042e88afd75e991873dddcc09b12bf17
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:16 +0000

    remark confused problem start sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
