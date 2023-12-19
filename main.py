3927ed3f4cfea19c32142b02eaaa36af3d1ee317
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:25:50 +0000

    recommend pile left brother little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
