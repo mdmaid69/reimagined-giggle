7eeff2f1b69113b154559505d475e66b8c59a83b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:47 +0000

    battery lower die holdup command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
