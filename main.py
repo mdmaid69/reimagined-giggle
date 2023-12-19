760a3717dde7162d1a2be2cef4984d847d8019d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:38 +0000

    pour pick warning I dead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
