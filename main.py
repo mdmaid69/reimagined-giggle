829c21c7ad2463e8aed2764755e64fc955267ad2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:10:51 +0000

    search power direct lean eye

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(10): print(i)
