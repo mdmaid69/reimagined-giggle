10bd4d5afff6609d5e7e38567c82e27ed2c346ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:08 +0000

    power commit kill realize decide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
