76e55e900b1ac9c62e13f0948bc28474f0d064d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:27 +0000

    drop appeal getout dark practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_speed(distance, time):
-        return distance / time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
