b78a53ff668cd040de50de39d82410b5e38100e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:01 +0000

    giveback ring scare shallow pleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  def reverse_list(lst):
-        return lst[::-1]
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
