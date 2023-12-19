59e1678511ef5d4315eced281932a712498fd8ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:27 +0000

    everything tax extra hungry together

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-print(sys.version)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
