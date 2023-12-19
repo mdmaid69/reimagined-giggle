24500c2d70c5df6af5452afe127559a104b6fec1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:54:46 +0000

    generally this drawing cheat forget

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
