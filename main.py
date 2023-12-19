ba0d783c793f94f945ab35d75f606c6cc31aa5c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:35 +0000

    excellent below wish callup yes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
