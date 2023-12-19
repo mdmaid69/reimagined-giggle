cf92fe8eaf767d1fd61aebbb44a822a94a0b9534
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:23:12 +0000

    argument yesterday science apply pipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
