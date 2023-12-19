9870d9b290b4e789a1a0fa4f2bb0991c33924913
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:01:42 +0000

    nose effect ill draw because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
