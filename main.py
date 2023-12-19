ed817805d1b1b175505b7a104e33800a512f94fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:42 +0000

    callat read talkback tomorrow advise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
