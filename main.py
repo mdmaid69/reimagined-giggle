bb86975e5df4c23a2a45d09c8d483f14082dc740
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:59 +0000

    winter blue wealth search disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
