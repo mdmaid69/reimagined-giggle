795e5cef709ca9c47f8593734aff5a58e796ee01
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:11:38 +0000

    unless miss the meet road

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
