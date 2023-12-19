2f91caf2712e870d64d908d642829b81f2bb3f8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:22 +0000

    murder send shop official idea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
