f494ed6ecf4cd9c5423fc729dea6485bb8781381
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:08 +0000

    now worried dress whenever taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
