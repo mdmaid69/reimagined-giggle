ced546cef13ec12321bbf4828aab8c011f5f6fc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:48 +0000

    usually cry wake report even

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
