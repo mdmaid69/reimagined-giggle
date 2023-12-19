aac75a8412c12196035711b78c5d12ae60c9f822
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:25:09 +0000

    fact target suckme unkind shine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import os
-print(os.getcwd())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
