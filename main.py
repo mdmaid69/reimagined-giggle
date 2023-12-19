06156a32c7b49f129e18fcd6180749532cfb646a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:40 +0000

    mess date bush major bite

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
