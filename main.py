2e3ef187789d0a7d1fd21bc9891a4d031d1ab9e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:31:13 +0000

    male store geton flow hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
