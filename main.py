f2dbd5555405d0b17b486f5aa11a57e6fa5e3d3c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:33 +0000

    slice fly divide approve workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
