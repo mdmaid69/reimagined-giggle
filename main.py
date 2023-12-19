3b90a9e85290343884032fec8949d513ad462c35
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:16 +0000

    offer accept book talkinto tour

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def append_to_array(array, item):
+        array.append(item)
