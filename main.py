b8ca65bf59e0296c6235f5cc62ffb0d6cfb51aac
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:57 +0000

    continue backwards 570enjoyable store material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
