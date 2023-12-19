a5dfb2b89fadaf124818181f9eaf174925ec0e15
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:56 +0000

    meet sail brush team throughout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
