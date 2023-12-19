1eb621a0ddee35ad750885113d2e46c33eab7ca0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:48 +0000

    minimum wipe wash continue quantity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_speed(distance, time):
+        return distance / time
