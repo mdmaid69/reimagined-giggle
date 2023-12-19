be182dc7b60c4c0f42acf598e1e3208411726e35
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:37 +0000

    come remove avoid that stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+for i in range(10): print(i)
