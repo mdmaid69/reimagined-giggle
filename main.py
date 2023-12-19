ad332f66d32910bac43efb1ef157092058b66b11
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:27 +0000

    accident mind event split black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
