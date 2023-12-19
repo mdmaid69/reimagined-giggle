144e826f5c9d6bf6fb85a21e1e65d1c884032f84
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:46 +0000

    until what experience confusing oil

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import sys
+def print_python_version():
+        print(sys.version)
