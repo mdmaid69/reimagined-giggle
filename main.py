ebc373607c471b21aac342a4f2c15710d06132c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:06 +0000

    sink call Ww language care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
