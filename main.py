8d4cb71be86f6e23beaf3c57ef050546ce7201e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:43:52 +0000

    smile gift tight attempt container

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def convert_to_hex(n):
+        return hex(n)
