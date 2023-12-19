5d581744497ea0421b12fb8255a8743ffb4679b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:38 +0000

    shop issue performance giveout dish

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
