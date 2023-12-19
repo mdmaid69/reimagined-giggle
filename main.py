d5760c96e5fea6f7037856d690388ef8eee86373
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:26 +0000

    bound give that bad love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
