4bace18e43c11a165e8cbcadaac351b063a36a9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:26 +0000

    snow join listen left block

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+from collections import Counter
+print(Counter("hello world"))
+def count_elements(lst):
+        return len(lst)
