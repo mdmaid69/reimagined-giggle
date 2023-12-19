460f60424da0cd7426a38216cf7e037d729106a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:11 +0000

    bang score turnover face where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
