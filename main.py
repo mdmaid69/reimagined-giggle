3cde1037d108a5b30c1238a774323d8c5ae5b11e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:47:25 +0000

    spend heat regular rare loan

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
