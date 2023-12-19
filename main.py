192d72c5ad40df7cfb2e1a5101079505b2ac5dd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:39 +0000

    twist to smile favour concerned

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
