f7a2cdf8610fda61cab2492bf3ce65c98428ea5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:38 +0000

    ill plenty mail know properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def convert_to_binary(n):
+        return bin(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
