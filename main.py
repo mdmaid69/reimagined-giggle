cfaa451090fa253646875b5751a53491d5a93579
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:02 +0000

    Cc recovery law flow direction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
