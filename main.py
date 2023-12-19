1bc6d967abcb62f35d2812538c390f5efe834aff
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:19 +0000

    reaction announce now excuse road

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import time
-print(time.time())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
