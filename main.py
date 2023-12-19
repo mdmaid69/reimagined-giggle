6ec5b8c77d9f896a377a3b5ca4ac9e87a2aac00b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:05 +0000

    influence translate healthy wind string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
