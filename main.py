ddffb86a799d113063ef4a575955fe28ead10239
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:46 +0000

    share term runaway trial broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import array
 def get_array_as_set(array):
         return set(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
