445a12353ead8cddb513e738b172f8097b2c027b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:14:18 +0000

    there ocean suggest build welcome

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
