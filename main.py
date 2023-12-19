27d87e17791d30271b31d33af668aaf4c2dad6a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:52 +0000

    repeat free decision brother pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_itemsize(array):
+        return array.itemsize
