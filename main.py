dc90d1a94689a27ac41aac0222a83c308824b7f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:20:36 +0000

    win illness unusual obvious weekend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def is_odd(n):
+        return n % 2 != 0
+  def convert_to_octal(n):
+        return oct(n)
