07bb7d72f55393785ac99d31207d2461677dbd74
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:38 +0000

    regional support control store circle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-print(sum(range(10)))
+import sys
+def exit_program():
+        sys.exit()
+  def remove_duplicates(lst):
+        return list(set(lst))
