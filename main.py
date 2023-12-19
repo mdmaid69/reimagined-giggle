422c1ccc8915958173bc934472f58a3d6eeb42aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:14:02 +0000

    total workout software involve hurt

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_priority_queue():
+        return collections.deque()
