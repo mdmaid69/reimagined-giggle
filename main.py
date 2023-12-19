9836c4e789a9e1c9e401ef8869413c03de2548ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:06 +0000

    stop 571enough sore now again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
