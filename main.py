8b943223196ee7cdddf695579c7411aa26823488
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:59 +0000

    spring communication practise block hurt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  def is_odd(n):
+        return n % 2 != 0
