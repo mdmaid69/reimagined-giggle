ecf85cfdb06c32c84005c3ddbdd2dd096c0a8524
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:19 +0000

    project unless lend minimum evidence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
