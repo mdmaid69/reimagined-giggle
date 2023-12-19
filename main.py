26b5ab0b42d544e884f9ff0c7cfded4814bd1e6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:29:30 +0000

    rare smile lot dry power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_dict():
+        return collections.UserDict()
