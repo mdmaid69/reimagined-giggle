c53199281c4b8e131d317d59d6a6caf08a556344
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:04 +0000

    sink late Itself no exit

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
