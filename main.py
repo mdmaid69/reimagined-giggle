90c757629b89d7c2094542bf5bbfef98519a2e98
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:51 +0000

    lesson copy fine always Ii

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
