1f9813694a4e41853dace2cbfe4eaaca9c725127
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:20:24 +0000

    her fond draw see comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
