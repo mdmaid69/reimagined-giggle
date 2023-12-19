e3ccaa5246e02a96dc1c1a097aace85a155163d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:15 +0000

    switch cheat date union generally

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
