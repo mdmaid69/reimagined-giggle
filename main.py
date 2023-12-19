e758569677815f9f732256564edfeb898e0af6d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:27 +0000

    shelter against equal apparently run

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def get_array_as_bytes(array):
+        return bytes(array)
