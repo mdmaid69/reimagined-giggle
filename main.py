f0d00bc03cfa82f01cfdd706911ebb5ef4633c82
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:55:37 +0000

    fire follow hit tough unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def find_union(list1, list2):
+        return set(list1) | set(list2)
