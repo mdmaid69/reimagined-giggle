c0afed204672983e0df18ee7871e210ffe23e011
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:01 +0000

    runaway recognize whisper watch so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def find_max(numbers):
-        return max(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
