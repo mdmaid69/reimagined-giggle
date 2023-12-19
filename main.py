d8bd944d33ad41a114d51c0b85f2dc9f42017203
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:10:46 +0000

    smoke thankyou swallow previous callback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def change_working_directory(path):
+        os.chdir(path)
