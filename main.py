d7575cd9310388ecafd4b03872fa7dac94254c30
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:07 +0000

    direct measure message dick window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+print(math.pi)
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
