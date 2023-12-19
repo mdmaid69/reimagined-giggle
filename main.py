b2e48b24a81dde20c353e0040189cc456306a3ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:50:15 +0000

    open apply circle way water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
