491f299494f1c7dc3cc885fbecd879966eae22f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:07 +0000

    shelter active on track bottom

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def cube_number(x):
+        return x**3
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
