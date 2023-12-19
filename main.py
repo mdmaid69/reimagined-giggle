cc4a0a8b4f7f7fabb3793c56e88ba67a9ab857ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:58 +0000

    variety takeback forget belief imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
