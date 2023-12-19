4dcccb667afdc32963ea9941c10daa5c742dc67e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:39:41 +0000

    otherwise turnup pin frightened secret

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-print(random.randint(0, 100))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
