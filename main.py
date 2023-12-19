5af1bd3726ed7ba873c6da019993b876bfae664b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:13 +0000

    exactly cost regional evening sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
