f64d0789c8ab8f3f9b3ed675422395f403a5d773
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:56 +0000

    region related chase workon at

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
