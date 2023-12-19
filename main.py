f8e4bf8d3b3841487b0db6bd056bac01d1fd5620
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:34 +0000

    description who now worry sometimes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_palindrome(s):
+        return s == s[::-1]
