dab1dac6c9555c8e35a258ef196f9f07cda0c379
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:33:27 +0000

    dear 569enjoy healthy welcome when

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
