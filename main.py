78230bd51e9115b003662f240aea776988ed68f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:54 +0000

    cheque brief bringdown right protest

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
