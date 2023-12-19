e906dd18734aa59e9dc97a9e3ace632960c71be9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:46:15 +0000

    tie bet step recent press

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
