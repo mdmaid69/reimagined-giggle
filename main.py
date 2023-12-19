12d4840565fda01f16c064c93a632adf1fee04ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:21:29 +0000

    criticize noise entry Rr report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
