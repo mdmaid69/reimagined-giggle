af812b366805043354ca83415c10cfcaa996885b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:33 +0000

    against accommodation instant basically everywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def count_characters(sentence):
+        return len(sentence)
