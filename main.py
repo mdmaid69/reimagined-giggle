02e5587843d052c34326c35f50df176278d5292f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:03 +0000

    rub deep refer treatment strength

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
