212cfdf5cc039abaa734c4970c5c8ad19f443f0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:40 +0000

    play before pile normal wise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def convert_array_to_unicode(array):
+        return array.tounicode()
