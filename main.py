ecb3f3ba03d1a3593df5de05d6a3d7950a502de8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:31 +0000

    which passage present store quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+x = 10
+y = 20
+print("Sum:", x + y)
