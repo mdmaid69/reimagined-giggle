f6adb34cffc23a59e64854aa6890c5d0a6691a67
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:54:47 +0000

    seem excuse set move wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
