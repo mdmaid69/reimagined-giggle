139646cf75bb5c036ae9def7fd1971d32fb73c99
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:46:33 +0000

    bored her charge base lesson

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_current_directory():
+        return os.getcwd()
