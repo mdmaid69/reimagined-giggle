78f3fb84754ddec1c366fa7a9810bf38ebdd2966
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:30 +0000

    I at plain print possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_working_directory():
+        return os.getcwd()
+def find_max(numbers):
+        return max(numbers)
