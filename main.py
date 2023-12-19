da386cf4b4b17f16f124ef22f4392fd7c8f99d46
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:36 +0000

    bottom think want word reliable

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
