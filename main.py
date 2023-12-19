088317d34a89399757b3da7f10d9bcb29174525e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:03 +0000

    inform lucky regularly by under

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def count_words(sentence):
+        return len(sentence.split())
