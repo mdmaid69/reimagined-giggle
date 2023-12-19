4e95e5fc32a490b27692b320ef5979550aaec12b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:03:38 +0000

    pipe uponon sensible talkoutof advanced

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
