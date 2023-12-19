b0e8ad6f054740c683dccf8f2b219ae4e30f31eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:23 +0000

    judge hard plant welcome passenger

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
