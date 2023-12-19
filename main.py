ad7d65384d3131da854986d8f4114abb22c9da48
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:23 +0000

    turnup sleep abroad pair nerve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
