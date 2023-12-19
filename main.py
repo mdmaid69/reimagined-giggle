ad3e247554a2b2ff451fe58bf9bb66b2d428a050
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:25:50 +0000

    learn want person connection breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
 import array
 def get_array_from_list(list, typecode):
         return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
