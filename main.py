d1d51f948438f3a1c5016347409af39aaadf08c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:33:36 +0000

    impressed produce show powerful cruelty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
