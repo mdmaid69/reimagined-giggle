c60f7ee1829e4e58608108e801878a2d44dd3b2c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:28 +0000

    opportunity rank abroad expect realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
