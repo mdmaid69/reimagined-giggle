e2cfcfbf04332854be1391c81a839dfb85bed652
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:01 +0000

    flavour round include possible light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
