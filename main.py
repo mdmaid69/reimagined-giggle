470ee114828aa984b23d8dea269e09cbb92a269c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:43:15 +0000

    perhaps far drop work complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
