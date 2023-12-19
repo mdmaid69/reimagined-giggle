9bbf5402446d5c8dac95343071d5e34973f42092
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:37:35 +0000

    path independent our regard law

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
