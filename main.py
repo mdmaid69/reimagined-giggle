1067e299ee1fd38a0b868e0f38d6940755ff0903
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:24 +0000

    turn education border promise beyond

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
