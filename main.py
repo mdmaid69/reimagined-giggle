62820187b6202c0def0ce92b37ac7d36d15f21f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:21:16 +0000

    public duty strike pure return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def sort_list(lst):
+        return sorted(lst)
