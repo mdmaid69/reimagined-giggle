95019f75c27404a058433c18cf308c5b581c803f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:07 +0000

    move simple bother tear ground

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
