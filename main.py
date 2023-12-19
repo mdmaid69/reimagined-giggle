f3928e97ae299d17910b32793f920a4b4fffeac8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:28 +0000

    scene excuse recommend tongue march

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def count_characters(sentence):
-        return len(sentence)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def reverse_array(array):
+        array.reverse()
