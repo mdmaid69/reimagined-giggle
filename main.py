98a62c1f81935d62b8aca711a736f0dcb12c8539
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:18:17 +0000

    tidy comeup keepfrom freeze recover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
