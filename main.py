64c159247fdc24757dc310016a831ec07b77e748
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:49 +0000

    bringforward listen near secret turnon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
