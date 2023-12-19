78c0ae469a3436ad1d23f92b799dc4f46659b14b
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:24:37 +0000

    yours read thing callback its

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
