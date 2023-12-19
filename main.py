3db852e14b184bfb5be3cc6762a9ec0a1cce8516
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:15 +0000

    mean statement belief importance classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
