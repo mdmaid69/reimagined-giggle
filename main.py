8751cf9e1366ebb2a7b23d9e0d772407f9730c82
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:20:23 +0000

    right capital accidentally valuable sky

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-print("Hello, world!")
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
