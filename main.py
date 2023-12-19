c4002e9f89105069df27bc6f913bfeef55efd7d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:45:46 +0000

    month chat Uu thinkover cough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
