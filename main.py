740aa7a31f3b6c90d7a247c7ec5414953cb1aee5
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:18:56 +0000

    manner turninto suppose bridge exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_user_list():
-        return collections.UserList()
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_int(array):
+        return int(array[0])
