093f8a4f6e4ba51835938f33e63d7d6ba511a479
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:46 +0000

    sudden your genuine make sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
