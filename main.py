58559adaddedc9300245b5bd67386b49cf7e6689
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:55 +0000

    pay nature any sweep narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
