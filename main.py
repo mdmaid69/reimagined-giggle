72112010a1fc352ea818939d97439c4aa8e84a8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:56:21 +0000

    570enjoyable few power yes protect

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
