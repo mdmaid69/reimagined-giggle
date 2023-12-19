101455470f4f78b974ae560d78a306a6d546e207
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:09:22 +0000

    bend impressive light friend settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
