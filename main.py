4df389c8b16f18ab8866533862495903e6e2af92
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:26 +0000

    government scene active practise border

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import datetime
+print(datetime.datetime.now())
+def calculate_work(force, distance):
+        return force * distance
