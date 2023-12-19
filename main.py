6eb8429ce0bd7d5da0999d33f9b8d7ebed1b4723
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:34 +0000

    beautiful introduce another large conduct

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
