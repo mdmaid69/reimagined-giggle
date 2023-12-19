85a9aeb51f3b132154f1edc038a6f549e095371d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:23:15 +0000

    load sight space than behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
