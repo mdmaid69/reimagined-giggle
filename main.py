cc70f217b8fc0e3ea3e58e564a74304a1e875325
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:36:18 +0000

    arrange scale approval hair major

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_pressure(force, area):
+        return force / area
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
