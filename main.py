e11224ac52452b3916612365c8b3a043b5f51275
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:12 +0000

    thin complicated callat shock plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
