ed2f40e3902f9c1f4e8c87688725ef530207f13e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:20 +0000

    count bring persuade usually official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
