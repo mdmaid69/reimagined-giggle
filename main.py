b90672ae3ab4d204acc25cbfad08fabec879be25
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:38 +0000

    information few priority fire suffer

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_volume(length, width, height):
+        return length * width * height
