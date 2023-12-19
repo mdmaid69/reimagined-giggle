fa70c8f726f105e690fc12683394e20a48bf8acf
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:19:08 +0000

    formal lead rate score drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
