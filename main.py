67a8935c2f6aaf1dce2b94a2961f72356da6ebc7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:22 +0000

    genuine offence simple beautiful comeoff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 numbers = [1, 2, 3, 4, 5]
 print("Average:", sum(numbers) / len(numbers))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
