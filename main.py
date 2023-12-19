687d1b8ab11c059057f2991727f252dc60f3f241
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:40:27 +0000

    repeat combine pattern last future

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
