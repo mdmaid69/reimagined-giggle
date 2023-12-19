62cf7b1b4b96bb2bc0a2d321429404033249a5bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:55:26 +0000

    watch half feel argue object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
