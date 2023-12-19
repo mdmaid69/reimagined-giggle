178d0c34154dcccb8cf7d8f4613aa715f6026fad
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:54 +0000

    closed upwards comfort great neither

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
