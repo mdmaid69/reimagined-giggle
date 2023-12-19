c5d18f17b241b966d21d12dbd8feadd8a1e22e36
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:35:50 +0000

    stare unite reduction trip music

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
