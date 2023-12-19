5331d6583688b70bd7f9921b6a8181b240d64331
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:10 +0000

    combine politics better scream visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
