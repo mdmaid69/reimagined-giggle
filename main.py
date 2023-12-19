48524b656ac3935446848247057ab605e3fa3f82
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:36:56 +0000

    amount unit reach refer boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
