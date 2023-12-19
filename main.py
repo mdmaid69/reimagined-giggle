efb27a40c6e3f4d6ba1726f185a94c15b97e065d
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:58:02 +0000

    about satisfactory drink look wing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
