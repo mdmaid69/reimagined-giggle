481f8ed4bb9d6408616d59b1f8e008fdf29c0b1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:59 +0000

    might child serve though needle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def subtract_numbers(x, y):
+        return x - y
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
