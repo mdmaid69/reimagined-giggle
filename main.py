2ab1443416c45c6dc0e20f70f251059fafb43e26
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:11:06 +0000

    why bit fashion sweet embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+text = "Hello, world!"
+print("Characters:", len(text))
+import array
+def get_list_from_array(array):
+        return array.tolist()
