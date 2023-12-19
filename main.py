358427ea5f6fbe87dcf7ed9a13a83e686dba067e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:05:51 +0000

    its worse stone bottom right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
