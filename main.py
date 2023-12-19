6d6baf08319219275f7c98438ec248ce93c4e700
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:34 +0000

    division home arrival wing section

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
