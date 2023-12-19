34c94a5b34c0e2475b7c89119931844f9f7502d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:37:54 +0000

    circumstance freeze confuse claim game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
