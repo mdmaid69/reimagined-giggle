b489007dd7563df542788e11a1c6575ed233c5cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:50 +0000

    least stop workupto home strong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def reverse_list(lst):
+        return lst[::-1]
+  def add_numbers(x, y):
+        return x + y
