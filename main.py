da967cf8597579ac0c63874163f722b5863e7dd6
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:39:21 +0000

    open himself party him scared

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
