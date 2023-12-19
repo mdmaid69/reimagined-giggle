feb7dadbd447a4ecc3dbab2a49fd632d95765e88
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:38:40 +0000

    definite group however talkoutof himself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
