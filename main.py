758949b2484a5d8d8ed20d942032eb1fc3b2be11
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:13:26 +0000

    turnaway warm pollution payment workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
