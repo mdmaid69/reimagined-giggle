0fca9d6abaeb309dec094bc6d8af0959033a4e31
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:30 +0000

    closed public heavy surface always

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
