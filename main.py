9f206f10706c4e326c72030e1843d6f426f9b5cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:02:26 +0000

    much Tt curious friend with

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def count_characters(sentence):
+        return len(sentence)
+def calculate_area_rectangle(l, w):
+        return l * w
