21d91fac4067467eaaa2b3eb1fcc3fc4cc912145
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:12:47 +0000

    heart search standard goodbye pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
