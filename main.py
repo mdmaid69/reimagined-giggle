56ffb01c017a00c870f1131599c96703e015a710
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:00:54 +0000

    sharp turnoff such regular fear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
