98ada195d85310130a4fa5a1b1893f8f003d9415
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:29:26 +0000

    divide occur peace wrong expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
