93dffa8908c66cefd14a46d92ffc34288f65e1b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:04:14 +0000

    from my pause search language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
