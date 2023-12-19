3edae57bee8813e6a95d62d3ebe4ea3597767336
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:57:52 +0000

    hang child fetch major next

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def add_numbers(x, y):
+        return x + y
