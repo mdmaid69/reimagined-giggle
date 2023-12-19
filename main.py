f0fc6dc4e00526114c6be5a52f21a9aef91314f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:16 +0000

    failure tip love thing accidental

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+x = 10
+y = 20
+print("Sum:", x + y)
