bba263ee3c524e9c8875f93de14b84926d2d868b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:51:01 +0000

    lower shoot force excited Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
