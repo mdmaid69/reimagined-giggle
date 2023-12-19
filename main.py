d60e112eb254138059dc632b8653c32abc7f7b40
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:54:12 +0000

    hit bush stone impressed speak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
