d8fd2588aab07a11ed397cde20707ad46291ec06
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:23 +0000

    visit shock communication limit issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
