1b8aec5eca14e8515f003f7dc8f00a13f63e2f85
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:36:26 +0000

    concentrate flat hurt clever cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
