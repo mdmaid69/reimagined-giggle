0c827103b3f2884c5673dd7f1023af5a46515739
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:55 +0000

    official fierce material low wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
