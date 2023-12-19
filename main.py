89738dd8bdfc3151e734b1c9195c3d08cd5b733a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:16:23 +0000

    better rule top awake yet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import sys
-def print_python_version():
-        return sys.version
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
