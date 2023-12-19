dce0d203d996f148ff54e82358c9cc058625fb97
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:26:01 +0000

    dead square fine swim cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
