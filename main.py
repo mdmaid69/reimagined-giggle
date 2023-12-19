70d2119e7850723463317c9be9560de95d56469b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:14:03 +0000

    evidence put blame sport platform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
