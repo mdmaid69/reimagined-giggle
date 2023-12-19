4b0e0a4d4a12845f2e3779ccf36a161b6b13a827
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:17 +0000

    sting belong slow report release

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
