cdcbfd6c58bd1f1afbfc3f58885f47e6b12f2492
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:19:01 +0000

    side receive complain willing get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
