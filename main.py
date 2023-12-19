834a129cb7e70dbba1cf46b8fa00e2b69b097ef7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:53 +0000

    bet guilty unfair reject angry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
