c68f5e7e7bc63e4f7ea8e5aec8d230d4832f117f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:34:37 +0000

    sensible defeat giveback claim lonely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+print(time.time())
