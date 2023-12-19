3dc9d9bdf8cd706bae928207f3d1be7094d56989
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:03:53 +0000

    upwards refuse general honest mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
