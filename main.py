e88edfda4f333f2ea7902237089c6c6f354544cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:14:59 +0000

    same themselves arrangement change empty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
