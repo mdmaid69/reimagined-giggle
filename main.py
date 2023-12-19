d67609053f93e2e460e30de2e78819eff0ee47b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:13 +0000

    cruel most exchange big expect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
