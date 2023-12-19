867e22ac255cfeae428aad439cb140c7a63bc41d
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:58:02 +0000

    pronounce accept public shout neat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
