94733f52df47faaa07dd3652d47956f1dc1663d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:49:32 +0000

    typical getback care suggest accept

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
