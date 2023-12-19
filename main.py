6179a51aceb033de09dd91b0b41bcb1279713133
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:26 +0000

    serve taste occasional shade fortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_work(force, distance):
-        return force * distance
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
