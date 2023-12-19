d96e83bddc220c53a3fac9e6997f4743366822a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:28 +0000

    paper comfort leave tired total

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
