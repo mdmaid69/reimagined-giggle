cef60aae7cee148baba79944373cc41fa1f55ab5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:36:05 +0000

    gate similar single cause love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
