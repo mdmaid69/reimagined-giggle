7f0a5d0aeb462975fd02254455558a28f30de56e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:12:05 +0000

    window thinkover centimetre pronounce pay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
