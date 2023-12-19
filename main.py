eec9dd6bba95fcfd0b40e1f9e53ea4d1a675b17c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:02 +0000

    branch bar nearly deliver original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
