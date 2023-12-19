e9e0d6814800f0fb79186da940c68664a503a7d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:48 +0000

    service book often common whenever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
