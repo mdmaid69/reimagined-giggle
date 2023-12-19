3dedf09f0caf2cbe8cadce2f2a01c4551134972b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:13:35 +0000

    turnon rid say computer interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def add_numbers(x, y):
-        return x + y
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
