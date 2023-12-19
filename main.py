34f83cf01d5ce1624c4b09c7d7b6b3a6d9748ee2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:32 +0000

    close suffer bus system learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_int(array):
+        return int(array[0])
