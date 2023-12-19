16ac531ed322389ca32bc26af1c71d06a514f33e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:20 +0000

    detail wrap usually behind rob

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
