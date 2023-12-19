8db726a63bc6dd3d478717f90a8bfe703b63e4b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:43 +0000

    keepoff work sensitive tip fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
