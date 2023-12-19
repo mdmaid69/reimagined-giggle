fb8f81e9f5912922573cfe904063104f365cb634
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:17:51 +0000

    instead help please safe rare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
