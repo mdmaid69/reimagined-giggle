826ea2142db576f12b4ad4c9b895cee61b7d54d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:22:05 +0000

    repair project along disappear broad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def reverse_array(array):
+        array.reverse()
+for i in range(5):
+        print(i)
