832e794049fe335f232e08253cb2aec9213a6bad
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:05 +0000

    funny trade welcome rise holdup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
