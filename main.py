41b2b1f4e36887b8c55e22fe60b5b555285ef944
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:22 +0000

    position conversation unhappy official whatever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
