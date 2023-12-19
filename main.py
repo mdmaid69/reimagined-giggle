101325a1765b0044c64c0af83975e482bc9653b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:55:44 +0000

    transport explanation occasion high generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import sys
-def print_python_version():
-        return sys.version
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
