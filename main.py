950ee3ef0b3b750b8ad373de6a6fd4ec5295fddf
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:29 +0000

    opposite always to danger main

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def count_characters(sentence):
+        return len(sentence)
