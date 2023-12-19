1641f4f52ec149d5bf6e7a158b5b71b3b11af61a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:38 +0000

    pronunciation steep trick spread handle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def add_numbers(a, b):
-        return a + b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_even(n):
+        return n % 2 == 0
