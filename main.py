bc76a1323c5e6b00ee4664b8524f94952cf66438
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:17 +0000

    fun everything generous sign official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   def convert_to_binary(n):
         return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
