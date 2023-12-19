92179783780c6b1172dd49d326fa811f8bed087f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:31 +0000

    impression ill empty along whisper

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
