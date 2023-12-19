0a695eeef8dbd81b9207993b79a4db71d212bbe0
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:08 +0000

    another solve loss turn original

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def add_numbers(x, y):
+        return x + y
+def is_odd(n):
+        return n % 2 != 0
