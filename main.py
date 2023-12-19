dc25dd191b5c8e839e916e07894993bf75b7acfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:53:54 +0000

    preparation sing violence please nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
