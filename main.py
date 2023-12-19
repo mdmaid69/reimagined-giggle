2cb73f56e3d9c49088f30e260b5d53632f296512
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:32 +0000

    cause mess write pattern loan

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
