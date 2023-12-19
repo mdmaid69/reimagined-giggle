0a586b0caf986f3ab049cda0976452ea9cc3aaef
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:03:59 +0000

    cheque feature content bill switch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
