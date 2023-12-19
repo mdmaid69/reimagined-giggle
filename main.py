854e6c5c0536419bc1fe03c00115db20ed32cc91
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:31:13 +0000

    material spray luck drink legal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
