ec712e17b2a3b0e1aeecdbfef17ab8056d312d26
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:53:51 +0000

    willing pleasant degree identify table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
