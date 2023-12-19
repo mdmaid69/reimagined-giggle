9ec4203791950bd0babb61ea3c2e373690621b06
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:10:08 +0000

    stamp test shallow wheel wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
