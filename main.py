4beab73e32841b78fb69cd4223cd4b617007e3dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:22:35 +0000

    peace reliable ourselves teach food

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
