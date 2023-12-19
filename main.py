c722f2758d4dac89d60917759a52550fce390c5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:20 +0000

    party everything cum ill splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
