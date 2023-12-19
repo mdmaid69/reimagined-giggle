92383ae09e85cd6348671a454960aff8bb794e2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:16 +0000

    back harm valuable rub Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
