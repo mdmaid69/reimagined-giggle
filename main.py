ccb1c4cade0c5d4b7e2df06b7a93d6dcba4e6f36
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:57:55 +0000

    crash choice commit contrast born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
