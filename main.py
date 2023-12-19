eca5acd3087dcdfd4339de6c90a916a420c9a8ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:15 +0000

    combine passenger here sentence pull

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
