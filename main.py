011327a58215e49d89217c8e66131065408e6a1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:02 +0000

    smile child attention wave thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
