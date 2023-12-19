2fa2dadac99f63efb81ea2c7ffc734262d52e362
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:06:17 +0000

    read sweet afraid some intend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
