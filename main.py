918b1ac1570c6327814dcf79f38abe34c998f47f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:08:41 +0000

    narrow stop strong second matter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-for i in range(10): print(i)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
