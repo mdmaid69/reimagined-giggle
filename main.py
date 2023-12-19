91929133dba11fa8f5f2d0d36c45a7bb5329f9f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:58:02 +0000

    represent pick interview evidence healthy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
