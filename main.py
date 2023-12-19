eb7ad0df68ef8df34f7ba4161b7df6f6ca74f90b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:55:12 +0000

    use note example distant refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
