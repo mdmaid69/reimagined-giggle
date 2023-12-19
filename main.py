d3dc1d399f6b04bd0b6ffb3c8da09bf195fa1993
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:28 +0000

    shock breath turndown fortunately below

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def reverse_string(s):
-        return s[::-1]
+import queue
+
+q = queue.Queue()
+
 for i in range(5):
-        print(i)
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
