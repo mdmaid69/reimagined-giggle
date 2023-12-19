ce01cf09d6434386f6825fc47effa9b83981dfc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:55 +0000

    feature invite far winter hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
