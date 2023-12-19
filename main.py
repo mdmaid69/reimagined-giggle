be8d732892b975e9f7b555a751c45e05f41d9fc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:07:06 +0000

    cost talk talkback rough fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
