bbf469300dbed9f7bcf1af2e3000fe72b9af7b67
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:07:22 +0000

    by near rent calloff load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
