19c570e1048fa8a5b0f6796dfbcb54142457b63b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:30 +0000

    skirt meanwhile charge brush pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_as_int(array):
+        return int(array[0])
