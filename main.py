8fdc72d9af34b3089b04e715ec4b5c7db1e3d07e
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:22:50 +0000

    soil stamp attempt lookaround smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
