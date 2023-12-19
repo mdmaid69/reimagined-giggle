0c7e96f6726bfb464d9dc5cca9391e5951dab39f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:08:37 +0000

    price runinto spite experiment message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
