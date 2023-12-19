ab7c696146ccac4497331db3bda0ceb65f84d448
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:00:36 +0000

    off yes plant callup town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
