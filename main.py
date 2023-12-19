df222466b0f2f79b70ebaddebcd3682207833bbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:59 +0000

    vision accordingto plant influence suffer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_typecode(array):
+        return array.typecode
+import random
+print(random.randint(0, 100))
