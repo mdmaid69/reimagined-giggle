ac716a858231e5ad7cef6dbfa68d20e9beeb79c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:44 +0000

    talk tense translate fat callat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
