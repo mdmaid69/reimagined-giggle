7fa47ec77921b2d5fb26dbbf9d0677469589fec1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:38:13 +0000

    obtain lend attention content keepup

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
