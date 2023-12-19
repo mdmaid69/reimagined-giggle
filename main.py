bcf803772da78c06de98e284094811160280fe82
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:19:35 +0000

    system vote reduction getout true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
