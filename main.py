ba91552bc666fef2a020d36a39dfa8113eeceaa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:26 +0000

    average spring better through lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
