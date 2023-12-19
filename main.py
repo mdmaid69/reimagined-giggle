5e0db8aebf8eb34e1aa554ef657ad09b4bcc4d70
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:22 +0000

    rid future tidy post tune

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-for i in range(10): print(i)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
