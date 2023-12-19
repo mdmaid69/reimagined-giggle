93b627f846e7d6e9f200c65ecd1d74735d897c25
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:17 +0000

    steal cool wash apart anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
