e801e4aa21b0a7eea64f134828482b73672055a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:40 +0000

    game frighten debt married between

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def multiply_numbers(x, y):
+        return x * y
