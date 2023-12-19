a126db2a08713ade88d03b2838dcf56bf0901ce1
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:03 +0000

    inquire join pause will sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_even(n):
-        return n % 2 == 0
+def greet(name):
+        print(f"Hello, {name}!")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
