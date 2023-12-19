c575b46d74d5e9482e50c158f97d97199edcc872
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:49 +0000

    spite power empty damp tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def greet(name):
+        print(f"Hello, {name}!")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
