0c0b6cf089306b84c57d8dcb1d7f7c567af6276f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:20:40 +0000

    how certain large concentration plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def convert_to_hex(n):
+        return hex(n)
