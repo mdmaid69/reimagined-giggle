bc20e15ae4a6bd2011545b417e866850b5d77dcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:35:56 +0000

    goon glad avoid moon top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
