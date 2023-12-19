05b041b71e1f639fdcc947ab17937cc48cba7d86
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:38:43 +0000

    fast hide pile transport development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
