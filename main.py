770ec8c9671c59bd4695585383e6fc4f1dd9434e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:48:20 +0000

    appearance score risk drop show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
