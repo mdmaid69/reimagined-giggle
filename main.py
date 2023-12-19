d5b9bd2a6270b0810eed9c152365380115a791f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:55:23 +0000

    deep speed check break way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
