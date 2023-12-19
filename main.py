650bf952f906626ecb035d3591837245bfdc4f8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:57:01 +0000

    neat 570enjoyable but rely minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def count_words(sentence):
-        return len(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def clear_array(array):
+        array *= 0
