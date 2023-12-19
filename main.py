c9ded8ad2cebe7a035ea402fef5640f2d52c56fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:53:21 +0000

    match ourselves field official paper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
