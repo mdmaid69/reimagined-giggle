78589fa94960b4153fe6feee83defda3c3963124
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:31 +0000

    rank below least remain Kk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def get_array_index(array, item):
+        return array.index(item)
