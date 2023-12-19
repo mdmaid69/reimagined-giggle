2af6c8be9e90498ae9ed7200c487c5edd4824daf
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:23 +0000

    exchange luck bush blind watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
