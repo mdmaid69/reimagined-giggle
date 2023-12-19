87211fda03761103ca31f563346f16ac8023f4de
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:38:12 +0000

    man previous Ff however 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
