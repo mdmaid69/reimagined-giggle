8b497b9780e87597aaa3ce6b7b30ea96257da465
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:09 +0000

    rarely Jj bright sister most

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
