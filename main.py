182a65332d31b49cebe192fc0dfecba930b19c88
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:12 +0000

    capital during think possible boil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
