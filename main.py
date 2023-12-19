2ea9074868ad2989325bfc874a98ad4908899c7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:51 +0000

    underneath murder string responsible meaning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
