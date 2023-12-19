28986c878b5cebc63510822c448d371fd2b254ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:52 +0000

    port factory fault bring task

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
