5e201af3b406f7fc65b6914dd172d3312c209d99
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:02 +0000

    stare impressive turnoff hungry over

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
