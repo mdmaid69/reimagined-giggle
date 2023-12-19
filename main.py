69d255b0730c8d522a3fd6dec187311a23ca1807
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:15 +0000

    job some blue their somehow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
