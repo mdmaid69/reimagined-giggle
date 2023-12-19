f066718329c41421162b9f9458b4018f0205aeda
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:06:22 +0000

    within doctor today imagine wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_int(array):
+        return int(array[0])
