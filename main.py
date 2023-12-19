850e34e262dec6499463c1684d9cd483ebacf0aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:52 +0000

    least quarter only mend fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
