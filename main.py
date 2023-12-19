fa5686d56f020cbf02bd79f93b85ca6016726348
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:47 +0000

    ordinary react completely grade progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_repr(array):
+        return repr(array)
