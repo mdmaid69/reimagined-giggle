16eb40b03d9c59effd79facbb58aa79ac6d6402e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:46 +0000

    steam takeoff slight evening walk

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
