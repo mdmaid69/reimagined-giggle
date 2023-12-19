6dd9a24fa4411dbd7f167c416c0a4ed2097a4b5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:34:18 +0000

    pause maintain runinto practical point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
