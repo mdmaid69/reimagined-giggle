0af9248a09467c8be9d1edf885f97e957d1def7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:51 +0000

    harm mark everybody any top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
