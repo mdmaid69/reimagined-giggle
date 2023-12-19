006e3b3e01ce691ff1e717a1381837d76f54508e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:11:49 +0000

    practical key proud dear since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
