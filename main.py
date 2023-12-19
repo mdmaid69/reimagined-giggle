9af5c13380cfb7480e528bf0e4f493c05c58bce2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:10:35 +0000

    definite smell typical geton exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
