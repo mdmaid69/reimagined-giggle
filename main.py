ec78f32ab9dc771f5f582670b5c9900f3a8ee570
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:49:23 +0000

    attempt language protect advice service

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
