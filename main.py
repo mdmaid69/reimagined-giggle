3eaea34eda7d25429bab072e19a3345f29107b98
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:20:15 +0000

    discussion need connection plenty story

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import sys
+  def get_python_version():
+        return sys.version
