97815ad8450c27b60a956f8e7d9063d4a17b8c53
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:54:36 +0000

    dead blind alive sort board

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-for i in range(10): print(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
