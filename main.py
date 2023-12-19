dc6aebb59a4bc52d6f7e16a17088f81fd0d1a21a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:27 +0000

    translate separate always pour circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def reverse_array(array):
+        array.reverse()
