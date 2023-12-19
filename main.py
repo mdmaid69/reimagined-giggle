39cfa9440f7c4b39284ac1b30d03beac129ad341
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:06 +0000

    bar comeout book organize imagine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+x = 10
+y = 20
+print("Sum:", x + y)
+import collections
+def create_queue():
+        return collections.deque()
