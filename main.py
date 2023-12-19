9961500be0f877a6b2704bbcf0d52551034ab120
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:40:11 +0000

    truth close start try collect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
