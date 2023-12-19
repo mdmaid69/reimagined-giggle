e5d30d2ed9ea3303ef9370b18036853efeb985cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:47 +0000

    ready anxious 179bite turn bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
