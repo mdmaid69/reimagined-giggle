6f6883c96f624b1aa9495b2b2a3955a2c261311f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:35:26 +0000

    treatment sell home crash goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
