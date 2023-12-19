8db577b99ed07a64864a69410e33bcf8bf8035cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:49:22 +0000

    urgent computer tough fortunate tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
