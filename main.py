29bf7be6bdc0e1ab46936fe9619c7a466b1681cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:46 +0000

    become pain window belong snow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
