8c0dbc336632e0347858f689a87aa85664485a30
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:52 +0000

    high advanced raw gas mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
