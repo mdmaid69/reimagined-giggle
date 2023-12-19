8e3f62e4ea5c635a934955ea93969b8a254f858b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:19 +0000

    survive lock system bored door

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
