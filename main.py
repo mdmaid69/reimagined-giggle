eded5da5f6efbdecf6b5569dcfb5314bfb2b0a3a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:24 +0000

    runinto cheque walk add independent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
