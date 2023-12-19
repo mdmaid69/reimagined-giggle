48adfedf685f0a90c4b6e4e5dd140fb7f433e364
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:16 +0000

    game owe stay doubt otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_density(mass, volume):
+        return mass / volume
