8e8bbd66bb97dddadbbfcdd6f0135c0aaff325a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:14:51 +0000

    depend getup continue comein plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
