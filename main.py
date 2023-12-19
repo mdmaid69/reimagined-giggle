05db6178f5cfddae701b12dc129e57e8cc0fe0bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:32:36 +0000

    turnon completely opposite mean warm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
