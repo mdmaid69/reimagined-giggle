9afc1df0cc92a883ca69860c726a834cab7a1e91
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:45:26 +0000

    hungry anywayanyhow interview month getback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
