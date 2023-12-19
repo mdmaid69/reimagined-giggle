698ccf41e1a92705e6a3e9875a70888300dd7e02
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:23 +0000

    second press skirt order listen

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
