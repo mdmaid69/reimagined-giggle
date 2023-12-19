57f451daf1878eb10b8ae0a293b30ce8ce874e8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:13 +0000

    treat complaint platform under responsibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
