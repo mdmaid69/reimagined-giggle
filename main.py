a557d96e7d43f39009ee0dc5619afc1e15f13bf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:54 +0000

    plastic somewhere hide concerning also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
