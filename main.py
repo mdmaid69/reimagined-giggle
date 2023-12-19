bce106a180936b502285e2339c9a2758be4844ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:35 +0000

    cut along air way awake

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
