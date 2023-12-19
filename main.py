4adca9ba3c8c516c0221fafcac963aab1e76a0db
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:10 +0000

    fresh offer petrol OKorokay previous

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def get_array_typecode(array):
+        return array.typecode
