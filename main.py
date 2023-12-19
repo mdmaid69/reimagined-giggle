6681e87c0db075ee8a9528b0442be3321741ee66
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:08 +0000

    exist divide fire perform trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area(radius):
+        return 3.14 * radius * radius
