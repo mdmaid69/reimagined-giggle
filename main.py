8681db5b4b9ac0b08bdc6854ec7724d1eab81f4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:30:24 +0000

    rush never wave which spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
