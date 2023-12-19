12460b806cb0a81df5d0e5a9bb3b76d3a387ad78
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:30 +0000

    exploration truth compete produce feature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
