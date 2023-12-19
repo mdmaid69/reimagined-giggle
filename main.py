ed03cbb19b92494e6ae85ec53f2d13c19f3291f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:50 +0000

    basically speak extra simple disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
