cf4cc8ab069660b8cffb10e65b57cce5b1c8c9d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:07:00 +0000

    takeback frame correct beside suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
