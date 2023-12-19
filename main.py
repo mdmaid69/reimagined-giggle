c917d053ff57589e20f4c4b1743038444f49723d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:07:54 +0000

    keepfrom store carry invite phone

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
