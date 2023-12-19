623ddcb4f39760a173b08d6f8f3b1b9c6d317a99
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:33:17 +0000

    admire really 572enter lesson stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
