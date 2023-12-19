97e78102fa745b097ad3dd4dabc1f3806e02bcb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:00 +0000

    cross absolutely prevent cruelty grateful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
