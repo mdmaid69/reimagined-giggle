4d953f7c9564625eceb1ddc1a019d7bd20c8a4d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:03 +0000

    guide present brush callat board

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def remove_duplicates(lst):
+        return list(set(lst))
