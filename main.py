6c4e6bdb3ad38a50c38c2bffb6dbfc1d2567c727
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:05:31 +0000

    attractive shop thought say light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
