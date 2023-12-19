d36e43f6ca5355ff3b7e427ec241982a4f9ba0d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:39:34 +0000

    continue turnon right bringback speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
