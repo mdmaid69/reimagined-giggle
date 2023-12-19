c51424fc20dd796a52e45ddac898392463b165a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:22:27 +0000

    divide capital cry but straight

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import math
+print(math.pi)
