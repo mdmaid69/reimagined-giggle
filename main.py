48ade6a55a1fda1999fe1e0196c8a4d6d036ce62
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:01 +0000

    recent warn fault time mend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
