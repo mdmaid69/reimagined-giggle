23185596620b41511779fdfc97cbe9b668f78d85
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:13 +0000

    quarter callup extremely switch fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
