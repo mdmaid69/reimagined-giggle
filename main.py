f5240c15e07808ca4a5dbce004ac781ea56187b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:32:21 +0000

    challenge flood post notice admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def sort_numbers(numbers):
+        return sorted(numbers)
