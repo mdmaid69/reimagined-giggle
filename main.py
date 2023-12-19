44d2fa8649c116ec3c277da1b8a7c83edea4f93b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:31 +0000

    approval total control street handle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
