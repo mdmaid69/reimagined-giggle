3cc5155ebecb6d7936d38666f3aa9efe1c76a865
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:11 +0000

    shape Pp mixture left impressive

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
