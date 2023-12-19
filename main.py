e004181954e768d5cff2fd8d6fae3eaa9f145ece
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:27:11 +0000

    like needle and fortunately back

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
