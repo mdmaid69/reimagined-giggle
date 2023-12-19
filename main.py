fd9c31c1c0d11978ae9adb4962055436070f0c85
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:17 +0000

    about short design back trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_tangent(x):
+        return math.tan(x)
