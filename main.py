5592a91ccc416e8d15df20cec6f0f595c35178f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:01:32 +0000

    side most week move evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
