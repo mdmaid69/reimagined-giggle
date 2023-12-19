dac1a5989829d27d2963531ee2b49c3b998745a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:53 +0000

    lead form march protect eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
