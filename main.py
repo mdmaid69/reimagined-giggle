9393ec7d51c0dfdff150c14530a3880b7a7ba1f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:45 +0000

    possibly claim active takeout recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
