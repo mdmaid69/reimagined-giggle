ab716c9fa3e0a4531fc043bdb0a8ce53f112510b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:07:23 +0000

    perform soak floor disagree somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
