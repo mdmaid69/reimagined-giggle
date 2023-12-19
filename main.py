5681bc23bb701a61ea4a3618c23689eb4bc4a781
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:40:49 +0000

    question true ahead natural pain

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
