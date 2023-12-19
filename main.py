0b0f8688f77338496f36d380d0c52a8529e67380
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:49:05 +0000

    why exchange soil almost computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
