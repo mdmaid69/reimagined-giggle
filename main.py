8d9c892b557cc1fd05658609ce94a89f1ce4ad33
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:32:11 +0000

    little eager than across worried

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
