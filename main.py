f9ee6c7669ee7c201892cb69bef94b6d8a35423b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:15 +0000

    statement complicated lead funny by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_string():
+        return collections.UserString()
