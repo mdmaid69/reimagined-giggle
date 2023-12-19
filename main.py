1ea4ff70edb2560d7358e6a93344e6f5377a79c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:05:14 +0000

    environment disappear flavour breathe distance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
+print([x**2 for x in range(10)])
