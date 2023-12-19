e74938ec3b0d0abc8e5a25f0cdc8c24a9e91cee1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:48 +0000

    duty neck follow takeout performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+  import sys
+  def get_python_version():
+        return sys.version
