31e78000a56f827929e70ffd7632c2fb8f79f978
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:25 +0000

    arrive police turnback table sea

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+text = "Hello, world!"
+print("Words:", len(text.split()))
