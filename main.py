1d518e4cd4de9dc0b26f888490faa6f2badcd75d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:44 +0000

    later disappear yes know welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_volume(length, width, height):
-        return length * width * height
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
