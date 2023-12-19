64c6efbe50e94bb044ae8c2f7e87674e21b6be5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:12:27 +0000

    build start country loud bush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
