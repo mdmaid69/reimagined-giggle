3447a2571ef0dd6e77b888edd4c9e33c529f8d64
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:56:33 +0000

    rain attract runout rush house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
