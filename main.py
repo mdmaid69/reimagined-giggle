3525fc259f97df1ef7a4c2d9486aa8e3bbbebcd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:49:16 +0000

    dry mouth later among temper

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_power(work, time):
+        return work / time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
