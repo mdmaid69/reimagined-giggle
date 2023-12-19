55d4550dca047e7eb2ab24d624a1d2d333a14b7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:26 +0000

    at pin official kick attractive

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def reverse_array(array):
+        array.reverse()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
