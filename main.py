27b2478a68cb0a0d899dc6b66f59f8891ac3b9a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:57:00 +0000

    lesson estimate breakinto dead upwards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
