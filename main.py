8b447289d1b970d7d0079eb12b42ea97be8fa820
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:52 +0000

    treat us dream except question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
