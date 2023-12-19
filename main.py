26b9678847eefe31aac11497ea2ba326c7fea541
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:56:40 +0000

    guess lost object hide shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
