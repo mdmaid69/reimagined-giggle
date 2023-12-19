dc9682a5583908a9e3f468932eaf16a0faa7f227
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:36 +0000

    back round that order seem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
