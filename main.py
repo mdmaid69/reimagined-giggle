5d7cef0c50513d1607d4d98672e280583b3943ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:42:06 +0000

    morning smile lookafter mood better

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
