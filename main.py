6abbb190d1d6414fb9d36a6e9f997c1c1ebed6a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:58 +0000

    dull half what exam sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
