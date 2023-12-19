91636b7ed356abd119915e5f5aacccef8590ad70
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:04:23 +0000

    shopping personal bus concentration cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
