4794fc9c385c66c803f51b901a6870ca9d864b23
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:59:36 +0000

    glad argument reject Cc remark

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area(radius):
-        return 3.14 * radius * radius
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
