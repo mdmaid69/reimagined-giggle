5aa60aead4db075b5d1b11568d28505636f2af5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:45 +0000

    have heavy head clear off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
