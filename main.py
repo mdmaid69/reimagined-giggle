8cdc8bda66a3b0fc7e7d4f6bfc4e21b9a4526fa6
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:48 +0000

    workout disapprove turnout hard bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
