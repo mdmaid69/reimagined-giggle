e9a66d0642516bded863aecef284ce3f7b22c4ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:26:28 +0000

    entrance subject but interesting bury

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def add_numbers(a, b):
+        return a + b
+import sys
+def print_python_version():
+        return sys.version
