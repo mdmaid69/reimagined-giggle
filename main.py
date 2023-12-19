3b4f9bf5a94ee3dfcb57248c81cfa420e5386dce
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:06:17 +0000

    ball hold sentence first impressed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
