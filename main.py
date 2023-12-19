25db8ef09d5684ad76afaef1d379f2503ca715c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:58:50 +0000

    stop name ready smooth rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def convert_to_octal(n):
-        return oct(n)
+def is_odd(n):
+        return n % 2 != 0
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
