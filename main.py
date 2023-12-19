fa33d62a1fc3e1b774a89b293467802ce9208303
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:48 +0000

    height turninto performance unit soon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
