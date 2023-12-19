86603e3be6fcf6fc1c92ae7a846e7a71a4efd27a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:00 +0000

    week name uncomfortable trade evidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+text = "Hello, world!"
+print("Uppercase:", text.upper())
