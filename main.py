cd1b0c46771f9204a6e394223a310a91d703a392
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:25 +0000

    man scream so produce basically

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
