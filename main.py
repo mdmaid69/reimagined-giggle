bdac52c3c4eef4b35b11140a90fd39c72938bbb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:13 +0000

    pronunciation remember bridge hurt university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
