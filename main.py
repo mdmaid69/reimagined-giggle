2ea228b4e20fdbadb2963f50df5c0dfa9556ba54
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:56 +0000

    ever improve invitation believe hurry

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import random
-def roll_die():
-        return random.randint(1, 6)
+def get_bytes_from_array(array):
+        return array.tobytes()
