bf8af31b4ccd545b54026a237c5ccee52bbf3333
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:30 +0000

    pile variety holiday themselves roof

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
