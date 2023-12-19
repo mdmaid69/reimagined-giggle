c6584a3ceddcefeb1ff1df69918fe04d98ef733a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:27 +0000

    fence tax temper constantly guilty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def convert_to_octal(n):
+        return oct(n)
+  def convert_to_octal(n):
+        return oct(n)
