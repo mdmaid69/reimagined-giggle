fff6f767618cf7b87af385a5d46620c9116e3712
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:35 +0000

    press own Gg rank crisis

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def cube_number(x):
-        return x**3
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
