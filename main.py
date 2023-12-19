4766ee361e380f62504999dcc12e1c7c06389082
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:23:38 +0000

    discuss Bb smile true dear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def count_words(sentence):
+        return len(sentence.split())
