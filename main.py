8d5d2fc0dcc7713d523ebbdd111f2509da50c559
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:03:01 +0000

    pair convinced punishment male exit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
