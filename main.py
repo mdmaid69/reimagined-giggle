29d01084544643e49300ef33ad80109c4db1b35f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:24:39 +0000

    report goon compete office recover

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def reverse_string(s):
-        return s[::-1]
+def reverse_list(lst):
+        return lst[::-1]
+print("Hello, world!")
