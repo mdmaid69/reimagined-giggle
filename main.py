c77afeea880358778916bf1d57ceae9bc15689e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:51:31 +0000

    apparently spite perhaps direct close

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
