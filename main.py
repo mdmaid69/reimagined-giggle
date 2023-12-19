910683d35af7d62a2e259a6b0da3fb5e8b77a4ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:28 +0000

    bye respect stay uncomfortable drive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
