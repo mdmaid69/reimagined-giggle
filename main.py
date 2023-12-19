4ab636a27a9eca53f23494a8d72754e7acbdd7e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:15 +0000

    necessary uncertain regret constantly evidence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
