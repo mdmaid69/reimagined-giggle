9cc034bdcf3a4399a90dba82b3d622b1d3a3ad7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:15:16 +0000

    remain otherwise create free act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
