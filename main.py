ca2b885680dd6080d75383b24acd71bdfe175eb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:29 +0000

    luck copy able basis sound

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
