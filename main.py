13c5c422506cdab664905b90f584d91f800e9139
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:14 +0000

    proof callout translate accidentally please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def reverse_list(lst):
+        return lst[::-1]
