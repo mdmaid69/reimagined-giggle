de2cbc97dbfdbcdfb32f8521f66dc502ca14090d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:28:59 +0000

    refer split bet theirs computer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
