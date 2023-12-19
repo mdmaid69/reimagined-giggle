6a08f214b320eb60eca9eb6311b40f8de1eaa4f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:09 +0000

    until build tell match watch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_max(numbers):
-        return max(numbers)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
