145c4dc9afed01ab77d845a465b2e81399265214
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:18 +0000

    colour can direction impossible ocean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_bool(array):
+        return bool(array)
