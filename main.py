4f0d11123d3f74f99203f86fae9f107fc64fed17
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:01:54 +0000

    pair nature score other key

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def check_if_array_contains_item(array, item):
+        return item in array
