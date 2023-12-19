8475980b701d91db1a3145355ddf7fd7026a6231
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:56:20 +0000

    secret start pretend strike important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
