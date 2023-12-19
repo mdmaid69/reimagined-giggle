87ffb916ccfed6c39d1cd01814ed892ceafe8d1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:53 +0000

    getaway scale or ill tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
