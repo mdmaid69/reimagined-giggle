5c4cf33ff0c1c1da76ce8917dc708e9240e149f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:50 +0000

    fall forever afterwards size makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
