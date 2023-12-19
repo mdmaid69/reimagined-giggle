bbaf0b1b81f14e33bc0cadf80143788288530d30
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:49:47 +0000

    after price small previous water

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
