10d6167363b98cae2c56e6541cb4a5ca70dd552a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:52 +0000

    mine clue pick embarrassing shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def clear_array(array):
-        array *= 0
+def get_array_typecode(array):
+        return array.typecode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
