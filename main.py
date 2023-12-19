c3b581d6d52a884c98f4e74351ec39622838ec58
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:34:11 +0000

    still clearly favour worse protest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def multiply_numbers(x, y):
-        return x * y
+for i in range(5):
+        print(i)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
