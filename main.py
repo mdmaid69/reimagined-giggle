e1e057125fb688ee9cbc9f8dd87d1040c73058a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:35 +0000

    song large value information form

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
