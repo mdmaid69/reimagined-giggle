2d60ae537295cc3d78ba73aa095c6935b93248b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:14 +0000

    against attempt block tool trust

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_pressure(force, area):
+        return force / area
