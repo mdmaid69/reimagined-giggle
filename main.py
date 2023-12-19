1788225c7ade106ab2a5bb84a75bef10c31a490c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:09 +0000

    change radio extraordinary noise somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
