ad83809bebfad6d02af7f1bfe9cb24bd671b422a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:47:49 +0000

    lean carry food reply win

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
