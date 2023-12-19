a85963df6afe1dc9aebb891131c87ee77ca7ae47
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:04 +0000

    thank smoke willing spray period

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
