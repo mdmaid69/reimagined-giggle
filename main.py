943eb87ff80bcb77c2f11bffad753ca438a90585
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:07 +0000

    standard ball pretend swim underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
