b50f0dd10ca36dc143dd70534c976017e9712259
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:14 +0000

    any stop air field mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
