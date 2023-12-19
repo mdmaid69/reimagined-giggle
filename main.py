ff24e3534420c6b747c113f672f4dc593ba94ef4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:37 +0000

    joke most whatever entry attract

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
