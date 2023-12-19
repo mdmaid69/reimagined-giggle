a9159a172a566b31836f83f42e9740764e74e55e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:37:18 +0000

    crisis exact plenty bright old

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
