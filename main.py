e095373919a72efa97efb9eff5472d290330eb00
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:57:38 +0000

    perfect will always cook sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
