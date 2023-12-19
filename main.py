2eb82ebec69f3739cabcd780a2e7a11ce4bf48e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:47:15 +0000

    hurry tall black trial help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
