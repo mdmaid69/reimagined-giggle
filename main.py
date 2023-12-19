fc0972d687a4e15fbbf4d686aa4f9b85067c7cb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:25:26 +0000

    contain crazy callout heat callup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
