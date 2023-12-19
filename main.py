741d150a40f5b19df418e7abe3d02cd5a63f181e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:17 +0000

    entry conclusion star blue rubbish

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
