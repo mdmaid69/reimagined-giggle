e204a3519b4daa00a402e88379c77d70843b9809
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:16:48 +0000

    before name suddenly fit different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
