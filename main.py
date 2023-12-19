db6903022a743cbd65a7f66260fe7a1b8d3e6cbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:30:41 +0000

    car serve absolutely run circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
