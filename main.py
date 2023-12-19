7df8182e1290784178c68646c5e86d526fc3dc65
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:58:19 +0000

    punishment news married touch strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_queue():
+        return collections.deque()
