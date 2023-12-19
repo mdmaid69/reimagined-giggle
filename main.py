ea22fdaad403a4ad9ca8e2f3304b02a7057a812c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:28 +0000

    boat different bath trap grateful

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sys
+  def get_python_version():
+        return sys.version
