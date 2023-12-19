4fcdf76c15ebc3ffd1973cde26b16e6facc16d50
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:27:42 +0000

    correct top plain once famous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
