66f9957ac34e976f93453659adf7a8ec71c57887
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:41:18 +0000

    taste hit frightened train protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+x = 10
+y = 20
+print("Sum:", x + y)
