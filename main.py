a0349e7d64e92c4f846c346eec778850f4834219
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:37 +0000

    keep tense wander away join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
