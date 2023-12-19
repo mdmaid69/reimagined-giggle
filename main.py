41ad7212bf1b1b43983bce876480dba4a66e3f94
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:12:54 +0000

    command protection punishment instant throw

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
