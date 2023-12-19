cb0c024ebd91513513d988e8446213212c6b4a6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:45:21 +0000

    wide satisfactory spite distance wave

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
