881666bf9263f75181634bbc638ccd0d718721d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:43:21 +0000

    my burn factory taste some

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
