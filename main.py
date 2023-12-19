72ee6c0a80cd7bf88689cce6783e87e31e797604
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:26 +0000

    supply bar square neither pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def check_if_array_contains_item(array, item):
+        return item in array
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
