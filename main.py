edd84517f815a330f4b7bd994273c372bbb98cba
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:07:31 +0000

    modern recommend wonder at ignore

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
