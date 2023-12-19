18a663ee4038e998e27a52d730be19aa354838db
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:35:12 +0000

    scream wrong dull sad this

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sys
+print(sys.version)
