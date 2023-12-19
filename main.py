64029f237120935454cab303e78f4d15aee3e180
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:14:29 +0000

    deliberately nearly trip ashamed interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
