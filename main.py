99228adcf65f985b1931bb230166621eb796952b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:31 +0000

    determined light gate centre hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
