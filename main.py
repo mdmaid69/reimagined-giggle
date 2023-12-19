e17e889f77c4cb641c9b9232fcc85de9f0b78fc8
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:52:58 +0000

    since bear seem cost reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
