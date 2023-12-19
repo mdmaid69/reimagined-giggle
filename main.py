690fc4d554b8c0cce50b05ec2dc29b581d80b6da
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:03:02 +0000

    both say let poison against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def get_array_length(array):
+        return len(array)
+text = "Hello, world!"
+print("Words:", len(text.split()))
