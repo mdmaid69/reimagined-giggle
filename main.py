522b4c2bf77f74afa7e54b3258f3675dc21a95ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:55:33 +0000

    land finally correction right hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-def is_palindrome(s):
-        return s == s[::-1]
