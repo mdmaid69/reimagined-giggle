0c809fc049364cca9c45659be63943621ec8bdef
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:14:28 +0000

    come study drink 572enter exactly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
