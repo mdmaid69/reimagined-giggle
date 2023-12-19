1995137935a5a5a4637c111285daf94d9665413e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:09 +0000

    spring act rare they expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
