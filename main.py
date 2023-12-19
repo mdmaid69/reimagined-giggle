1932f8ecd848f270324ef5fc4eace3684677791e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:43:40 +0000

    present custom accommodation pupil stage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
