d8665f18320cc653fc32a49ae757cdb6ca28ae30
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:54:59 +0000

    but guess watch meal live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
