541b26eb19b27921ce4295aabe4b5e1c14ed36ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:51:21 +0000

    object comefrom central attach concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
