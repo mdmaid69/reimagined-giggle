9c7af034c363f4b4f1e0cfa8364811bd74826b31
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:14 +0000

    impression nonsense able rare unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_str(array):
+        return str(array)
