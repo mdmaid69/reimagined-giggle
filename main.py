5cf4ae7bd14e56cad70d8c6dcab9ee7031d73928
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:57:31 +0000

    collect talk gas form wide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+from collections import Counter
+print(Counter("hello world"))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
