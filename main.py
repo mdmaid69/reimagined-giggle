b30046b7fe841741e20e5e5eaffb70eba920fc60
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:54:01 +0000

    remove way comment black any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
