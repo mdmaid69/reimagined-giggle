06220c0eb3614556a81924d2e898b000ab8a3d38
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:04 +0000

    callat purpose wire today colour

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+print(math.pi)
