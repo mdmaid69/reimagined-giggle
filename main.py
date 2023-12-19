a6daa30f5195763b5a85abbeebffcfe2e92c137c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:37 +0000

    572enter official undo crash floor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_speed(distance, time):
+        return distance / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
