8f5b8655b9ee0b4af6e756c4f291c4037cb30829
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:30:56 +0000

    Hh tomorrow door usual judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
