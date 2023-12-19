7ac0473b21cdf96ab745023ddf2fb0438e4bfc10
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:03:26 +0000

    aim before formal sense just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
