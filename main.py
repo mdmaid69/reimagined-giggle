71afcd50207ec3341dda9fa470a3f1ac5ea86ee9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:17 +0000

    ride blue injury everyone clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
