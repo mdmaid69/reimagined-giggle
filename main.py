5f477aa16746bd6fc856d06ce074fae8e7160a74
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:56 +0000

    spread tall Pp minute free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
