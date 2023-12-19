96bd50256cb72f386b83f44b784348a769943968
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:53 +0000

    behind offer feed professional ordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def count_elements(lst):
+        return len(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
