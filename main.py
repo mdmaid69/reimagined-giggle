d0b082f43c2a675f2f804555c29ce5d1904237ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:53 +0000

    sleep law honest path upwards

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
