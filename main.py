ff497bc3b7f6f778532ec8054975f9b45d278671
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:03:51 +0000

    game culture may definite after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_work(force, distance):
+        return force * distance
