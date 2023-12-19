2952f833ff39c08654497b42edf29863771cfd38
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:22:37 +0000

    Uu might rent matter capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+print(math.pi)
