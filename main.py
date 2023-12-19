510f2382ff925f5273ee1fac4b0993a7eb13593e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:16 +0000

    borrow for negative lend quarter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-print(os.getcwd())
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
