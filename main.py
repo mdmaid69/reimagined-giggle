db6ba8ce1414375b1a2a0e6466b61334993ed6fa
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:18 +0000

    pretend later unlucky occasion Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
