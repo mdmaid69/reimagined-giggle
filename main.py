749ab9e740ae3024afd1f64f7f5d99061e41419e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:04:38 +0000

    spot during slowly getback school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import tensorflow as tf
-print(tf.__version__)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
