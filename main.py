66e151aac6edc278a3d6bb6d3e7e0d0e1410b49d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:51 +0000

    inquire dress wash could pay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
