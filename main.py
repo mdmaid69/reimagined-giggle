e1a31cdfe21ccdb984b407501f2f62555353f237
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:59:48 +0000

    firm please somehow cry home

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_string(s):
-        return s[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
