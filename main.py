c86c6fdc915547b33a40609e5a20d20fe9a4d0a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:30 +0000

    strong convince near realize piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
