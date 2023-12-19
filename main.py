ea5f8b5a6647590162feb2e287048389eafa22cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:29 +0000

    song hit available translate regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
