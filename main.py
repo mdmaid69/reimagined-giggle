75e796ea084f7568fcc6c099f7aee631d582cbb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:17:06 +0000

    figure die with instrument relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
+  def add_numbers(x, y):
+        return x + y
