59fe555f9a96a5cfe18f6d2b154d6c828ee4ae31
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:37 +0000

    stranger advise bye exam university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
