37a2fda4db1a9f3e9cf7f06a7173ea1bae7fd931
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:50:30 +0000

    chance goodbye coat opposite minimum

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
