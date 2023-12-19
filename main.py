aa9a1b9bd2284c2beec04dc609a09ac3fcf8b521
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:49 +0000

    work even equipment along Yy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def change_working_directory(path):
+        os.chdir(path)
