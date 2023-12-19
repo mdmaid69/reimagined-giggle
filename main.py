b2965da077ab51abf0eb6409e4e163b89bfa0a1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:21:48 +0000

    573entertainment tidy type complain average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
