6ab2af4fa3289e20c7617e6c6cf54f260c3c165b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:40 +0000

    crash history value test dish

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
