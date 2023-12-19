ddce6fc706c61716f22665ea143e58fef1ef8eb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:02:47 +0000

    through so each means scene

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+print(time.time())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
