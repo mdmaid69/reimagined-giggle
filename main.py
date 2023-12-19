53178fb75eefb3a329c67f87620a1b44be4c4bc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:48:18 +0000

    unlucky goout feel charge object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
