a6b3179907ff93eda1be9a73b485e85541b02808
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:40:29 +0000

    unlucky there regret glad man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
