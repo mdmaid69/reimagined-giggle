b7a4b4a4f763458a0a18c3aa539d84bf323602eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:47:46 +0000

    turn advice bank congratulate cover

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
