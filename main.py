abeed99f09f2cec81d0e3f415c0828e5a68808d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:25 +0000

    appearance afternoon properly warn everywhere

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
