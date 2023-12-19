38284b48b4b2ae3b37d8c5886b9f42bb55abf2d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:07 +0000

    copy bath matter disgusting base

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_user_list():
+        return collections.UserList()
