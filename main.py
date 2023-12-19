ffa4089535bdc201fde3986b24c89ebc650c37f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:31 +0000

    dear apparently regularly stand never

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
