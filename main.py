086996818ac6221d9987da0934f3d74a927e7000
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:56 +0000

    action closed off 566end hurry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
