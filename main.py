1dd1ab43555f2eaa3a9ca5d6c70690871648e407
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:49 +0000

    law dull smell whistle only

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
