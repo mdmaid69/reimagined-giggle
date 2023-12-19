6e483c4b3748dbb952ba4279e662cac459ddc17a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:51:09 +0000

    switch circle violent become field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
