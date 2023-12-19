eec90d1768e80ab38996013ff003692261325e4f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:40:18 +0000

    Vv careless willing mend keep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
