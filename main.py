9f498076626805defa5ef47e46aac3cbf562cfd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:44 +0000

    achieve common press form subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def find_unique_words(sentence):
-        return set(sentence.split())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
