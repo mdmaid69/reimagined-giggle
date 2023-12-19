a33a85a968768f95dcf363f3bd15f8d9ea6c9c76
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:32:01 +0000

    seem relation name century everything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area(radius):
+        return 3.14 * radius * radius
