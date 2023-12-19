1e2832f141092e2cba6452e1e468e35fb8d3145e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:48:07 +0000

    attitude surprise base consist subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_counter():
-        return collections.Counter()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
