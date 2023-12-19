7a95165e0c187a09123dcd16fb03b9c985988bd3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:55 +0000

    the right somehow natural practise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
