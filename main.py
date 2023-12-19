dd5606cb29915531bf7f773f71f730431306e4cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:19:44 +0000

    small shop ilovedicks loss should

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
