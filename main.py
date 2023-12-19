2cb0ef22ccda455f129e5cf20fa08388fa5a6a2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:48 +0000

    here do movement like bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  def remove_duplicates(lst):
+        return list(set(lst))
