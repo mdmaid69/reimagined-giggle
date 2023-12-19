28ecfa65de4b24822120687f21c87f7ddd832bf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:35 +0000

    finger upset recommend run turnover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
