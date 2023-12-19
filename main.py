c0baa5e62a683bebe95e97d4756aa353f18f2ec9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:28 +0000

    bite stupid strike spite harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
