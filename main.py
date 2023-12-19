e0f8f70989e985b07fe6ed0905bef44d6a04560e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:19:31 +0000

    lost offence goon issue OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
