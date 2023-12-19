c7bf3500a216fdccd12f0fa3510106393b2432de
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:32 +0000

    string storm recently anywhere everything

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(10): print(i)
+import re
+print(re.match("h.*o", "hello world"))
+import numpy as np
+print(np.array([1, 2, 3]))
