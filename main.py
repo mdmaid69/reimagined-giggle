5af72eeb4ddeb345f5199083729f5fcc1e587bb5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:24 +0000

    pretty fear cope why callout

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_even(n):
-        return n % 2 == 0
+print(sum(range(10)))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
