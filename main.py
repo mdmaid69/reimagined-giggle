7368cecfe7bbe373d53c8de3e3d2e29a16b697fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:31:25 +0000

    mystery operate imagine remember smoke

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+i = 0
+while i < 5:
+        print(i)
+        i += 1
