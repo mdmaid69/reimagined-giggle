915ffcd829309fd994bcebd376a75e734d272a1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:55:50 +0000

    hang mine measure official intelligent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
