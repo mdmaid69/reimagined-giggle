3940d8fc17d5d94dc43b4df0a71f420759655597
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:15:30 +0000

    piece post undo hollow clean

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
