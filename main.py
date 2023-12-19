eee4c54fb07788adb34575811ebf823f00966dcb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:59:53 +0000

    however responsibility must along tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
