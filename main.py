d02eaf5672265409502de5229de02e3c0e670f49
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:42 +0000

    get sail difference closely impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
