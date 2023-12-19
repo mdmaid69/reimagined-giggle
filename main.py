9612f8d261adb4fabc6ee629010930b86ae977bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:00:57 +0000

    adjective minor confused speed pupil

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def is_palindrome(s):
+        return s == s[::-1]
+for i in range(10): print(i)
