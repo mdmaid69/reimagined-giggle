2b6ae5e5966c92346a58c5a21590fa4e14b1ea9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:25:19 +0000

    commit tradition keepfrom closely damage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
