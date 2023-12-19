d405ba5fa82fb23d6dab87b0efae669b33f70348
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:54:16 +0000

    callat deserve music comment nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
