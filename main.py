3211c8dda68112845f554822e49a2459a9d54984
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:50:09 +0000

    shoot thick well butter will

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
