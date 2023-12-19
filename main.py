1d316c682e3ec54174b56ee0e778b54d693c43da
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:00 +0000

    repeat trick correct permission push

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
