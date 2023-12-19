8abc48bc6bfdf17f757d838568cf7497c0365130
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:59:56 +0000

    detail prove activity where pure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
