1d9d10561bbaf5e71b8b76c42b757be5763765ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:47:26 +0000

    famous tap their care distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
