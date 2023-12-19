2bb28703377c6f52a17b4bca8babf406899e8e9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:23:34 +0000

    morning that right OKorokay upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
