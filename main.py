b09d854631d3d6f95eba7c7a0d380f45356fe71a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:20:53 +0000

    her scream means abroad cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
+x = 10
+y = 20
+print("Sum:", x + y)
