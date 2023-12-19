84e56631b2f9445b09bb0747fe9d03e53249b654
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:01 +0000

    fine whenever change notice far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
