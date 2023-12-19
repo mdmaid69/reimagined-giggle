308b1ad52fba87172c74474ab1426d4f0c95a795
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:02 +0000

    height fortunately decision relationship smoke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
 import math
-def calculate_sine(x):
-        return math.sin(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
