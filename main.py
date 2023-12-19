6eb3f499c4f8e5cd55ee3b04cd8ede762b0c04af
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:47 +0000

    variety order argue heat goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
