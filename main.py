64e92ccf75c4e61a2de9cb340e513876e607835e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:34:11 +0000

    difference do ticket dangerous improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
