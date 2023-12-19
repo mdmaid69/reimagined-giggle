5ddbd23a5ed365bc104e5f02c43280c954cf1745
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:53:47 +0000

    vanish first cool interfere anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
