181c4a13ccb2888090a43b72915f7a2582bf44e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:34 +0000

    suppose Ee empty drive apparently

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
