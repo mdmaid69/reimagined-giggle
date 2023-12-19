55c37c86f1f4155c20f6ee829a0d2ecda1a7e434
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:43 +0000

    over chin wrap heat design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
