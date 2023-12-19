04e8d4e71a09a2b9acc726f3d9a36e089145c390
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:23:12 +0000

    foreign wash beginning side ask

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
