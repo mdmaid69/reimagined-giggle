0982a59f31fe8cd447d705d02de02bf4c53fb18a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:27:15 +0000

    mean pleased embarrassed wealth correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_int(array):
+        return int(array[0])
