f3b51dffa9c733c72bc5060aa427f0460be9dbfe
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:03 +0000

    dull holdout luck sign test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
