46edfef17374a54ea46ca956d11c764988f6118d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:39:34 +0000

    hard problem better if exist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_volume(length, width, height):
-        return length * width * height
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_force(mass, acceleration):
+        return mass * acceleration
