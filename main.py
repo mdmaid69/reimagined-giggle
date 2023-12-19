a93dba072d14f626b1b76ebaee46e6051ee078fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:13 +0000

    unable one possible nose practical

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.ctime()
+def find_max(lst):
+        return max(lst)
+print("Hello, world!")
