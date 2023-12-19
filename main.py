440523218e51ee427a8e7435401ac2479cb0c585
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:01 +0000

    above excuse after large skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
