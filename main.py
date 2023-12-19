b59f64c0067658721a7cd94c24626e7be492dbff
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:18 +0000

    570enjoyable software emotion aim hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
