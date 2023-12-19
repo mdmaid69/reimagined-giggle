16315c25ca24dc1da7f224917885df29a0db7c71
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:33:55 +0000

    yourself between choice model its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def square_number(x):
-        return x**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def add_numbers(x, y):
+        return x + y
