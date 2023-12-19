d54afe4fc9ed6fc8fa08ebb192dd3f6d0a3d56ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:14 +0000

    across nurse large perhaps compete

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_speed(distance, time):
+        return distance / time
