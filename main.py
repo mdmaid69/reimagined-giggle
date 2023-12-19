e5f61f913d91e625abc038bca51d982094106112
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:20 +0000

    fight nose brother temper holdon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import collections
+def create_queue():
+        return collections.deque()
+  def subtract_numbers(x, y):
+        return x - y
