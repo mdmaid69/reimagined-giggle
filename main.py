59e025034ec9536c342bccbcd8f7c2ee2794d9fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:34:32 +0000

    protection play walk house freeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import time
+def get_current_time():
+        return time.ctime()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
