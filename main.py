c8b43808f5b5d6539bca9a518583dc0f64a6732f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:42 +0000

    contrast fork dead rather shout

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def count_characters(sentence):
-        return len(sentence)
+def is_palindrome(s):
+        return s == s[::-1]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
