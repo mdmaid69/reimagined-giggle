bed8dd5844f5db32f0ac341c3b6d5fe136210438
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:13:22 +0000

    comfortable anything force wise straight

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
