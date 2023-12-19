b43ed47aad38c3f30d85f0fdad243fc55e81824b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:27:29 +0000

    wave declaration ready completely tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
