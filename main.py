0990eca6f6e5e9c63d1dafef084dc3dbdfd9b210
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:56:17 +0000

    reduction polite outside light benefit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
