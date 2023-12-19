5f196742de1f80a9d331f884c290c2f620479448
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:18 +0000

    on last rob house compete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
