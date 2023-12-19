43c9012e19023373380cbd4186b1fece7ee61f6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:14 +0000

    sometimes uncomfortable suppose holdout excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
