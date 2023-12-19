83e2112368561d81d3961246f1ddee10ed572930
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:15 +0000

    slow suit maximum give suggestion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
