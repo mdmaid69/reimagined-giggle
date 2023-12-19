7dc4b6199ed89a2cbf709260ec3fcbd789a1b574
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:41:42 +0000

    instant drop relative smile brother

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
