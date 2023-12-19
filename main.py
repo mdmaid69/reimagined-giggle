815191f8f1112b226b9753a277e8931f32bef1f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:33 +0000

    unless join song cry storm

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_length(array):
-        return len(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
