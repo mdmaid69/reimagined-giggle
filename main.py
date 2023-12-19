764e0f09c9b4e9ee41355118c76cb604adf00f32
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:21 +0000

    emphasis announce note this keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
