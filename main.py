f8e68d9abc59b7e483836d7b6355b93a4e28a962
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:51:04 +0000

    die before spread uponon interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
