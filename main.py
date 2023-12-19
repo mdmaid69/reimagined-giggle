7b47e4c4b475246f1c43bd92565410f4157ac6d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:24 +0000

    bright yes switch cope confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
