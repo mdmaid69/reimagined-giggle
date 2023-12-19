16feeea9fee18c522f2ad969c438f9633f57ad0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:25:13 +0000

    rare expression second prove settle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
