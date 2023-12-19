e1a78defdedb192e1dac674a96f0569ee132c6ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:10:25 +0000

    care fall unable familiar worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
