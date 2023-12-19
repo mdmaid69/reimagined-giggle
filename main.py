6d1cb2fdc9c91c029eec7c957e49efc60ebd7bd0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:36 +0000

    can convincing healthy aim century

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
