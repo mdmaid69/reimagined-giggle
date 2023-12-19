f5ac903a80edf5cde2b75cf650002b0be2ef1139
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:57:57 +0000

    disturb maintain approval advantage born

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+print(sum(range(10)))
