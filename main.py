c7f93bde3ebfcc2d0732f6ebba73189464dd44da
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:15 +0000

    suddenly hardly second during vision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
