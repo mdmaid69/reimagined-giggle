ea50322241fe8e61e68758c162323b25ea96db70
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:08:01 +0000

    basis otherwise method responsibility light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
