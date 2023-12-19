b6c61afc058af56d57bc4853c3776832bd4f286f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:01 +0000

    aim him undo giveup next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
