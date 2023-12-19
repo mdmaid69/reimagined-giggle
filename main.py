2a5d8b27fe9f76f8875725fe29e1c035102105f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:40 +0000

    trust cope also innocent flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
