bd3c50b1c3fa3bab0aaf013d4190fcb6d74f1adf
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:59:11 +0000

    complicated either square generally praise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
