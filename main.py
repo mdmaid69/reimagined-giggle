3a839055d69d9cc8d3e7955a56dcfaeb430483a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:04 +0000

    mine however fault call live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
