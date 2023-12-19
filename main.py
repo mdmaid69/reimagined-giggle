3e07fd5dcc136cb15abeb579021f5d8ac0208456
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:25 +0000

    least burst slowly clear poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
