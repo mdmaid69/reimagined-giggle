66ff1cf250b3453512b49346b86aabe35890cfcf
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:56 +0000

    peaceful Qq roll compare arrival

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
