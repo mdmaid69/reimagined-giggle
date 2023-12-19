987dc0344eda00ac9ab87c6b94b170dd3efff352
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:37:58 +0000

    determined turn control display Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
