3ca43b566609268919b5e8c394b336bed220d238
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:27:15 +0000

    but grand pause brave road

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
