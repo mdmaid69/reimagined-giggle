3a9f6fcc4d616a68fec930fa81d9f60bf9335b96
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:38 +0000

    build find last comfortable work

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
