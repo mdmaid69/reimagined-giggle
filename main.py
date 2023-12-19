c4755cceca9fe42ac999d0e031c6ca5802fcaf36
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:28:35 +0000

    person loss mine tool noone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
