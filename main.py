3b6089d9d5e3874b93a3d06fe6d5fb034396be6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:35 +0000

    important there basic godown case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_sine(x):
+        return math.sin(x)
