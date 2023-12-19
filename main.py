20ac5a206543ec1f32ffc5b92f96769b46e26f22
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:48 +0000

    unfortunate goup borrow forget tradition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
