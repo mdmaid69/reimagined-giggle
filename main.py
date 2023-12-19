ede353a60db88c14371015c2b1c6e4f29ec44e24
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:38 +0000

    question thank I seem mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
