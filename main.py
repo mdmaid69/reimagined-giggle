eac59cd2d7812d15f7f7e93c9212b5b3cadbf3e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:27:36 +0000

    breathe any more murder fall

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def cube_number(x):
-        return x**3
+def calculate_density(mass, volume):
+        return mass / volume
+def divide_numbers(x, y):
+        return x / y
