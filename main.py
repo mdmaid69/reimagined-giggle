b40dad2beb6a6d0e40f2ad8e9f75b66bda784f29
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:37 +0000

    slight advantage undo unfair night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
