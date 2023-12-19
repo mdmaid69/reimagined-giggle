c99c1d3e954a7e65d30ab933f9d7bef13b2a2222
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:20:42 +0000

    active ashamed reply switch offence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-for i in range(5):
-        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
