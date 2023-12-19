4cbfba207723d86cc6d3ad85449eb0549054d94d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:38:36 +0000

    climb cheek dig them curious

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
