b4cb76857e8bbb6281bdbde4f01963918a7d26ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:43:11 +0000

    organization contact Ww sight shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
