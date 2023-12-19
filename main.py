166659efb5e48bf03e0b99e8080b10cc47ca2434
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:19 +0000

    hard married evidence low body

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
