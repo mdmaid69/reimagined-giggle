53b3517d259b5bae6c4602098a312e0f816074ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:25 +0000

    print record determined determined worry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+def get_array_as_int(array):
+        return int(array[0])
