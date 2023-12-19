6a756cf12e99988f56e12eddf927d727a9232169
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:59:04 +0000

    wave repair bag hers row

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+from collections import Counter
+print(Counter("hello world"))
+def calculate_power(work, time):
+        return work / time
