cd9c5eacb1fd2eaa11a50fa1441b590bf323dc95
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:47:21 +0000

    comeback pure act nigga confidence

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
