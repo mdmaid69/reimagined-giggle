f2812a849cec6983d5bcfabf37e6a008a7ffd0d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:41:51 +0000

    belief public fight lift period

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_absolute_value(x):
+        return math.fabs(x)
