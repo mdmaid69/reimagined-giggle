02a0ad49ec14db3cdbd6655ab93b67a5aa3ef56e
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:06:19 +0000

    block cheap owe happen record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def reverse_list(lst):
+        return lst[::-1]
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
