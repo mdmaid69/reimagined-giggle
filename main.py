6d5f136c29be26593f3c712a2e0a9a0ccd3d8b84
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:25:12 +0000

    call avoid distant crash who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
