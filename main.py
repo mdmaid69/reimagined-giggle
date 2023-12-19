3d221bdd5d90a227beb8d949538b49e694400b0f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:38 +0000

    sound flow over geton herself

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
