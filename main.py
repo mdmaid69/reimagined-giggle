bf3eca2166b8b164ff37f841644d98350d9ac738
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:01:56 +0000

    Rr spray label difficult wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
