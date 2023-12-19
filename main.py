2fbd4009ecf51e024c139b250b51d36490244665
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:24:50 +0000

    cap lonely behind admit beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def count_characters(sentence):
+        return len(sentence)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
