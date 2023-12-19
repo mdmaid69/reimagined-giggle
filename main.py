e916807dd9b37b248a4c50a4aa43accfaeddf60d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:14 +0000

    like illness announce voice compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_array_to_string(array):
+        return array.tostring()
