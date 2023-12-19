627aab077075a831efd7e4baa4391cbe082bb9fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:10:57 +0000

    comedown upwards relationship offend me

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
