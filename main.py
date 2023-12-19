6f14b57e8e35bd3a692f9c08280ab18e5c7b8873
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:56 +0000

    conversation rely achievement adjective promise

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+print([x**2 for x in range(10)])
