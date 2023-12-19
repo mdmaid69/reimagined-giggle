059ce2406954cae7d8d504d66fd0977a9a0955a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:26:43 +0000

    apply thankyou report single tax

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_cosine(x):
+        return math.cos(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
