8017315a968031556e9c17ec7955df84289c92e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:32:28 +0000

    mark until far unlucky do

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
