1afe66cc399683eea8511d69a9770f2dc1fc0215
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:30:47 +0000

    feel holdup kill top snow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_acceleration(speed, time):
+        return speed / time
