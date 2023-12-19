5adcaaeb027b2ad942266465cd9940b91affa50e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:41:47 +0000

    straight ring road sign port

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
