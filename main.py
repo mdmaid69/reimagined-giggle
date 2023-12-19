ee473a4dece66e07dd345c7c139525b14f76b894
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:12:04 +0000

    suit kill trial mistake point

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
