1e3dd301b4a59aecbf9f605c610c32b555ca9d40
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:02:27 +0000

    uponon surprising disgusting alive cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
