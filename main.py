a8ecad5aafdd25efb776886381cfa29c1bea48b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:20:32 +0000

    afternoon kill mess better stay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
