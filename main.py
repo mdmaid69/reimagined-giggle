a3c3cff5fdb0cbfc05241f444a47265deece7251
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:56 +0000

    scale wherever mention strike stay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
