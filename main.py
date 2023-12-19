a476704ed2a780c2e2faf28a186d6c5831aaa79f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:18:20 +0000

    ill some once least fight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def is_even(n):
+        return n % 2 == 0
+  def calculate_area_rectangle(l, w):
+        return l * w
