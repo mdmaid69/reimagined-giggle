809ff8b3f2a1583a5d8fd8063792a2bae543b206
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:49 +0000

    yet fast everyone rate colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
