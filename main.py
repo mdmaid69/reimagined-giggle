00c59bc3555888933de54d18f22e17cb34a2e598
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:38:31 +0000

    tool delight ordinary song angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
