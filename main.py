10744622ff152c3686b93734cab123a8b06c04ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:39:41 +0000

    married satisfactory thinkthrough design sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import logging
-def log_message(message):
-        logging.info(message)
+import os
+print(os.getcwd())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
