74aa77a371131a58d406ea4ddcda951e546f4f8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:42:35 +0000

    neat burst impossible want through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import math
-def calculate_pythagorean_theorem(a, b):
+def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
