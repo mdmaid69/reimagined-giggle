be8ce6f6430648a2dd86703387a52793c6a8fb35
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:43:50 +0000

    so if slowly turnaway desperate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
