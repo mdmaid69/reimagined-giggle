b4c17a71dcb0c02aa1c23297595c1f93dce56e10
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:19 +0000

    promise level experiment chief lot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
