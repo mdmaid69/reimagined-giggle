62e8d5989fc9d71ce1f100afe49a8f5998697dc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:26:59 +0000

    far Hh charge string complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_length(array):
+        return len(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
