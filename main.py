cb544dd762f7b0efef672128539b344ce119dac2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:06 +0000

    easy tight strike shut concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
