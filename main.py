d0bf8cc1267e137b9ec14463d837f4fee690920c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:32 +0000

    admit rough law uponon off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
