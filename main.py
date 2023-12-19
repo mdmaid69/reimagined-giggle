12d66b2fc640eededdbde0fb11426f42b22e1473
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:51 +0000

    explanation standard now genuine fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
