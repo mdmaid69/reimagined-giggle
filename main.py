9581bd70b820ca2bfd552f14184ef26d60060da4
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:25:13 +0000

    might of desperate boring majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import tensorflow as tf
+print(tf.__version__)
+import sys
+def print_python_version():
+        print(sys.version)
