e3445b248d6e1b317331b5ec021203ae670719ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:32 +0000

    breakin smell rely process spend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
