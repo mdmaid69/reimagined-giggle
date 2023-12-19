8f98a297eb2dd10886d7a6af75ed4381a0a017e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:59:27 +0000

    dick similar along ashamed square

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
