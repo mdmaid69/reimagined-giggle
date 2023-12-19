c408cefd12e960fe399187f42d9145e280d88430
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:33:12 +0000

    unlucky spot press fold arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_string_from_array(array):
+        return array.tobytes()
