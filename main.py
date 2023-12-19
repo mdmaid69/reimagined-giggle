31cf65a59a33a571261c283f852dbddceb98ff64
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:39:05 +0000

    plenty unfair realize takeaway no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
