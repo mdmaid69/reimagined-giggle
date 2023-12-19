1c532b5d82ca23c5f8c1d92a7792af725d9d32fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:12:19 +0000

    calmly treat through bet cost

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def multiply_numbers(x, y):
+        return x * y
+def is_odd(n):
+        return n % 2 != 0
