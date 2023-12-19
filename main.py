f5b9de7141df7117ab03206a1747218b516ef6dd
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:55:04 +0000

    myself arm dull fortunately clear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def is_odd(n):
+        return n % 2 != 0
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
