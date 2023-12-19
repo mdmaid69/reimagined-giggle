0701f2a210cd5679fb20dc8edb6c02dd883c72f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:16 +0000

    another central near treat turnon

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-print([x**2 for x in range(10)])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
