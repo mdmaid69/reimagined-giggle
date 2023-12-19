930dac511e776e452ad080266b042e5acbea85b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:49 +0000

    book paper laugh ignore grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
