233baeea39d873441c4e631f63f1e523ecbba3b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:44 +0000

    criticism pleasure block instrument taste

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
