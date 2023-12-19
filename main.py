a4d791127b8662094ac3176658996ad561bdc194
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:54:46 +0000

    understand tread yet one set

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
