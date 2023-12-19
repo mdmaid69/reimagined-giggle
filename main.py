3f04aec21dbb4eb3bd935e238482f6733fe10964
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:28:24 +0000

    Itself storm just manner trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
