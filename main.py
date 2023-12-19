08915e920ec5f54b842fbe2007899caa88404357
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:18 +0000

    right track beat runout calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
