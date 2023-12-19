5bd817e5bd3240a1ef5eb5ab2c3e8bcf11ae562c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:49 +0000

    progress handle test once software

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
