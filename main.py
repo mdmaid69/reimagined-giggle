bf77001ff17e145c46e429984ad5acbfd1f873fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:37:50 +0000

    pin close throw rise actual

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
