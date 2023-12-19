edff890f7b30a2b4beecc6559d816262b7f886b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:38:57 +0000

    fetch face job scream create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
