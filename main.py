ab2c2419c65f2c057eb46b6afb384d09bc68631f
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:25:29 +0000

    fun runinto Ll callout treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
