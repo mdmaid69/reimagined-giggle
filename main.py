957609095eab0f99d047cf59d2f32fbfa66cd9c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:45:38 +0000

    burn if lesson true window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
