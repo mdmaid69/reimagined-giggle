9517fc4125e7c253699bdcda8a1e522f2510a7e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:03 +0000

    act empty slightly break expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
