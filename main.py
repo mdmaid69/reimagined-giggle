b10133bdf1d0ff05510d894e2495a5231632bf88
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:31 +0000

    class behaviour film afterwards thought

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
