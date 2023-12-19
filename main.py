a13e65f9971bcf2705927655ff6cf24c84045129
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:16 +0000

    natural drop Dd over disturbing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+print(os.getcwd())
