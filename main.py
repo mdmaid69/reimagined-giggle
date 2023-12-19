7d5ed147c63e6b0efef2013c1f2b4df0847177cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:20 +0000

    noone majority taste money goodbye

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
