cb55cc5d57f5f051cfb5dc65e15b1a2c0a96f068
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:16 +0000

    copy fall play exaggerate concern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
