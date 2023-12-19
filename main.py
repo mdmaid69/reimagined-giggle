af14ee205fd3c8babc39b9cad84ef034b0934684
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:42 +0000

    rank who news contact complain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
