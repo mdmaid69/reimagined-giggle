9025c323ce1955edf9dfc0b03a5ffb841d4aa943
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:33:32 +0000

    everywhere sign ability burn doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
