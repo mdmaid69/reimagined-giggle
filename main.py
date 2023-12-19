d34ef4b1504c35e297896f87ea2e161b47a4f413
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:36:37 +0000

    laugh occur clue workon basic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
