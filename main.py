8bc9318603ed0aa3ccfa536d943b8b9afd067fd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:57:32 +0000

    my chase air tooth admit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
