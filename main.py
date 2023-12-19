860947836f4dc6a146e45386aff41e52f6acaad4
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:01 +0000

    birthday time reduction catch risk

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
