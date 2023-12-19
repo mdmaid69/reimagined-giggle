4725266e52dff24bf365c71306ef4e4dca444b03
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:49 +0000

    remark date offend wipe cold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+print(time.time())
