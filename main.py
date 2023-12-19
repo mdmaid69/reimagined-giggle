46ff6579a4e04cd692c17cca30425659dba3cdc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:00 +0000

    anyone handle bury bottom mouth

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
