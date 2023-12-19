17fe39cb887b2057cd8f165e5e391d71812183f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:19:58 +0000

    challenge ball advice centre determined

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_area_rectangle(l, w):
+        return l * w
