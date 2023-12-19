1fdae3f7eca5e2abd4d668da7a02d9a97a5e8870
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:08:06 +0000

    also behave least cold special

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
