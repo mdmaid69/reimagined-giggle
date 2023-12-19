5c36fa40fd74994e671f76e1f12e35816a57663f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:23:46 +0000

    test increase occasion building damage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
