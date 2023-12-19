ae095c22e5b1c2851693f59f43f5eef3654e62b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:25:40 +0000

    shape spray we ask machine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
