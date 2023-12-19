e641bd69f794cc02c6a7669d8bea774c0ec60b8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:33 +0000

    same hate strange better human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+print(os.getcwd())
