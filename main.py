1839877273dbd303e4a3540d5466a065329463bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:11 +0000

    warm simple content peaceful pen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
