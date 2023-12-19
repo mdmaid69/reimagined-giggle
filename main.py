5144de6e2aff1d8ce4d8f72ae88dbbfb03ced4c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:09 +0000

    delight reasonable offer above stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import getpass
+def get_username():
+        return getpass.getuser()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
