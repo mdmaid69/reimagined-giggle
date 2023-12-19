cb74827c461e39cc92a53d893b6cee43e9675f2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:32:05 +0000

    tune post against high call

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
