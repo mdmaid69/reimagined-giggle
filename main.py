7e6a20a668d7a4890b0fbbd740ac09c348e3cae3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:26:28 +0000

    giveout maybe blame idea guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def get_array_as_tuple(array):
+        return tuple(array)
+import sys
+def print_python_version():
+        print(sys.version)
