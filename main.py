36784a833276123d1c36fc6346b86580238937cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:27:44 +0000

    so reply enthusiastic on thirsty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
