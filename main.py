f477f78f8b1d660787ddf14eac5476344c4f0e6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:37:21 +0000

    sweep just stretch thing sticky

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
