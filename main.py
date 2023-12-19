014abc983ca5df0f9b7c266198bac6e137b460a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:00 +0000

    pencil rubbish upset fortunate increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
