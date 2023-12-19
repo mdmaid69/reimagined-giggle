1681d2e05483bf68c3e033d9359504b872f6e4ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:18:40 +0000

    human interruption instrument disgusting refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
