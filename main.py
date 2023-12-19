935bdb12961fc73af3d701ca8baef66df3b30e68
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:49 +0000

    pay develop division conduct confusing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
