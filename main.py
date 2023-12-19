9a27533527b192a72e047699d8ea235cf1505e07
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:49:25 +0000

    important needle risk just apparently

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
