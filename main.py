277a7370c2bae677faac9ed67173014f3d189860
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:46:51 +0000

    talk reason sound family shout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
