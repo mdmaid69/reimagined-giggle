ebc7dcda6053c7eb0e67e6aeab02b7481b0ce972
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:00:12 +0000

    sit ago partner pull instrument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
