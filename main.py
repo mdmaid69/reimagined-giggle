0753f69dd949432ff93e3329c6d985a5b5c16ac1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:49 +0000

    father our by upstairs anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
