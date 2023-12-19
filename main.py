512f0a4bff64e3f27a344b619a0c60a479aadf02
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:02:36 +0000

    handle evidence much so likely

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
