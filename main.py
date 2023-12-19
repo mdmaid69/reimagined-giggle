64c0c50a0a3ebc658cc61eed67ce06e4524d38a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:24:29 +0000

    somewhere act single brain pinch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_string():
+        return collections.UserString()
