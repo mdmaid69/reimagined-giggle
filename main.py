f396e8f7ac408dd4dc680d123dc3a2934a4627e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:23 +0000

    bring schedule organization pile hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_size(file_name):
-        return os.path.getsize(file_name)
+        return os.stat(file_name).st_size
