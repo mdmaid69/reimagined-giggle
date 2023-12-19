322766715c5d4454f600e4f354c1e4710af2bbe7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:06 +0000

    disgusting education just dangerous yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
