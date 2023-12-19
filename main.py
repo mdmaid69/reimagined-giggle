9de172b1c0ee06f00b28dafb00b4d2f91ba8c39a
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:38:13 +0000

    collect thumb ability pick neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
