4166fcabe15604c15c91d6ff464302eee5c5c6d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:09 +0000

    mind sting organization meaning surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
