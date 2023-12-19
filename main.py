754dc85276430faf80b8b9704be1fcd1d1d0ab6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:40:06 +0000

    arrival belief shout concentrate promise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
