7339eddf984abd52fc36bafc3dc6e17db7a5b111
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:03:31 +0000

    among talkinto Rr innocent word

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
