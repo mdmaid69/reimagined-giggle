ee9976bb87b5abde3382ab21f142216f5aba71f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:43:57 +0000

    contain cheque turnaway centre brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
