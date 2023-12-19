d85eb014930e7599343981c8d96832fcad34cde4
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:07 +0000

    equal will time ticket man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
