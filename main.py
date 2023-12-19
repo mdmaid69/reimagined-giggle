c267514d96fd6c62b2a781b5c71720602b25b4c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:32 +0000

    friendly bear grade this tell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
