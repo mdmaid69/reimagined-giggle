2977a0a753f6e65589f99ae92b02bee4845ffba4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:07 +0000

    hot tomorrow listen mark lay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import random
-print(random.randint(0, 100))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
