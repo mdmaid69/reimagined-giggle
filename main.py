325a69cdebd686139b7b425fb1cf5157c941b188
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:35 +0000

    unfair food summer consider nonsense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
