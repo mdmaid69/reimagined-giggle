c00543c05583b4585f7aa3ff0d3edb03cfd425f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:37:53 +0000

    suppose open memory cruelty port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_gamma_function(x):
+        return math.gamma(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
