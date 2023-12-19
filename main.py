e1bbeb09234796b2c3a7513e2bf0d31515ba930f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:01:54 +0000

    kind answer path knowledge come

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def count_characters(sentence):
-        return len(sentence)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
