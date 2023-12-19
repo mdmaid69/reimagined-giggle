234c15097fc779c7ead75a3b8156ba9e34cbf974
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:59 +0000

    lock cough blue club artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
