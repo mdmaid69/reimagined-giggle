0d7ea624123ee646b2516cc5bd0639de949beea3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:24 +0000

    tone successful quiet mouth honest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
