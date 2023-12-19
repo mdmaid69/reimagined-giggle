81e9687713647afe65f91a18f840c60c76b5fed2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:14:11 +0000

    discussion typical goodbye hope noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_list(array):
+        return list(array)
+import getpass
+def get_username():
+        return getpass.getuser()
