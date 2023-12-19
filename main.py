48d785e72480f808247a72f25882767ed4c1a70a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:11:34 +0000

    pity meeting job fond university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
