a4e32b104698596e14a9f030eda8b2faef073e7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:21:02 +0000

    problem shoulder flat fight breakfast

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
