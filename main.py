b3d797ef08a26cacd7b9ddec06bc38340fa90a2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:19:31 +0000

    argument slide cause grateful bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_average(lst):
+        return sum(lst) / len(lst)
