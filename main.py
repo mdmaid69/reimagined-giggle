2b1df052d4ebd5078b77d46e7db68b2806baa15d
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:19:02 +0000

    comeover everywhere through choice complain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
