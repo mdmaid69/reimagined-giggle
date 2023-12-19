34794514210b6a88bb598dcc5e8c7d8e0bf1229d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:35 +0000

    risk produce crisis day depend

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def count_words(sentence):
-        return len(sentence.split())
-def convert_to_hex(n):
-        return hex(n)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
