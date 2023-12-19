083a33a6433b27702c21d207e63e60f491e1fd6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:16 +0000

    run blame something authority argument

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
