c4afd4c595e1f9107cdc44a6cb6cd7d098d3e691
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:01:39 +0000

    criticism loss capital wrong neither

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import logging
+def log_message(message):
+        logging.info(message)
