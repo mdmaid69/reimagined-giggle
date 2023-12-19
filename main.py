a06e54074e2a21118c3e2d23d1c6bb04ba1da0f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:05 +0000

    love left brave entry force

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def convert_to_binary(n):
+        return bin(n)
+import math
+print(math.pi)
