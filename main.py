20b2b445a49b20584e9c753904331d351ad8c6cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:53:41 +0000

    drawing before sick pleased sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
