84e4b21ca8e66aac2b591cc00ce2848a1f56d854
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:14:06 +0000

    himself behaviour mix little threaten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_length(array):
-        return len(array)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
