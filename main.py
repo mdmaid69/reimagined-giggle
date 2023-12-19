77a93bad14d0d0cf18fc08dffff0534a0168ccbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:06 +0000

    sing part know especially substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+x = 10
+y = 20
+print("Sum:", x + y)
