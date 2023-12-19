edd661719d3663de03a205c3d17716e0a64c43a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:25:56 +0000

    sink workon invitation lay light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
