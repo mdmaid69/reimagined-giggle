7c7cb0f47966a5169160df6d226e1c0f85603d37
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:03 +0000

    miss unite fight authority tone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def cube_number(x):
-        return x**3
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def subtract_numbers(x, y):
+        return x - y
