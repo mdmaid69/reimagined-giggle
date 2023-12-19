e33693c735efe712bf8bf15439522a1deb1aa87e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:15 +0000

    suddenly responsibility majority free do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def reverse_array(array):
+        array.reverse()
