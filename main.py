66b593d1b48228d460837da6d786e26acc0f792d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:26 +0000

    famous preparation illness point production

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(10): print(i)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
