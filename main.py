9570371b2d9330da9d923e3688567cbcab030d62
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:35:03 +0000

    we earn Tt show lock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
