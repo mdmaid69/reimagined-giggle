6dd83a76b1164040683e84f81e05c1ae1940f7a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:29 +0000

    bite behaviour deep grand high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_item(array, i):
-        return array[i]
+def is_even(n):
+        return n % 2 == 0
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
