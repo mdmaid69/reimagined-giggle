b52c61a3cc6090b63405dd02f7b227c0c3df4ab9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:59:36 +0000

    proud fly neither right flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
