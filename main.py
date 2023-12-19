ff2d8886879946f5dbec10130894624fe292815e
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:12 +0000

    little dull quickly complete pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
