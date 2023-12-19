0497cf60d3b29897b31a3ae1d39c734638103eef
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:12 +0000

    thinkover character fight separate twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+print(math.pi)
