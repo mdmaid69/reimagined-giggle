87fb3754e8faab989bd644e3c05d83b829e90fa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:28 +0000

    guard certainly quick wind concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
