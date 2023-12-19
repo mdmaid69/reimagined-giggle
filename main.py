9066c480fe7a37f67b841a0561336b755c7c5de5
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:00:46 +0000

    coat wide dish tip important

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
