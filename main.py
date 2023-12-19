1352641cc6293ffd29b2b87d4bbf23cc5aa2efe1
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:56 +0000

    party power identify speech healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
