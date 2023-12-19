1803fcd5f86addb6ab7ac44f03808d23e17a601a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:15:11 +0000

    runout experiment both window brain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def count_words(sentence):
+        return len(sentence.split())
