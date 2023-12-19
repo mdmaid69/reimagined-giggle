2d15d16d8343a21273a944a20a80f1874c93a230
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:32:12 +0000

    blue for couple human workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_set(array):
+        return set(array)
