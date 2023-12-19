c2d79bcf47d3d34b508c7539bf837d4b9edde43a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:56:03 +0000

    repeat us punishment across welcome

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
