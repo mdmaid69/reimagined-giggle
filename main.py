feb9786ae89f4267741d9709035dbfab15bea882
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:10:13 +0000

    deep major waste petrol move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_user_list():
+        return collections.UserList()
