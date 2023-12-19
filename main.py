155b2672212a7b03e436e47a983c5ccd9fcfc65f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:17:01 +0000

    flat ring wild unable smell

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
