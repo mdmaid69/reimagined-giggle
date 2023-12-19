66f4730025c9f6804fe6625229a076d74aa909e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:28:14 +0000

    either glad juice related catch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def reverse_list(lst):
+        return lst[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
