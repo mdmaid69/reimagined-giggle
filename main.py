53111acf29a46cd696d19ec39bbc159bcd9bb670
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:30 +0000

    exam bath common system forward

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_typecode(array):
+        return array.typecode
