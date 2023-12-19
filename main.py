22ed1640930c0c596b32b182b3e3a4db011a49d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:36:05 +0000

    finger influence trouble inquire off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_elements(lst):
+        return len(lst)
