b787932a9154a3bc184d0e1a3b51e61fe2f5553c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:52 +0000

    about apart right machine my

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
