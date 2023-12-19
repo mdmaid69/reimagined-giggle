ae21b8c255957bbe6289c51455ef7fbea73851ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:48 +0000

    basic tonight connection butter press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
