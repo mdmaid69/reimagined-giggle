277a3a8c0b63204985beda4f53f5f6a1382f91a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:45:49 +0000

    close his dance sticky cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
