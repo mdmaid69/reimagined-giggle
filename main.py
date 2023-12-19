95147c12348e1cc1ec6bafc96823baa1cf96fb23
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:26:14 +0000

    risk include power shock switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
