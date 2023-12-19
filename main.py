788b61a7ba5dd6601df13ff05d66045aa4eda453
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:56:32 +0000

    obvious general for crowded throughout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
