97cd06d2bf0e91ad28c9c81c37347ec9ebfa7925
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:27 +0000

    them steady shake traffic guess

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
