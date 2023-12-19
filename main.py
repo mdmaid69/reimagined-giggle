956e12dd5e7ca3bdc5316c05133a53cb49d56f4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:09:28 +0000

    everywhere plastic best generous shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_unicode(array):
+        return array.tounicode()
