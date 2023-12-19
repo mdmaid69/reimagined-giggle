8e6e58e5d1c15a53b312f320b4066e365c972d4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:39 +0000

    case comeover operation Ss future

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_array_as_int(array):
+        return int(array[0])
