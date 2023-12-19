44a9fe4810c124cf5a9d74e3db00812f3a58a4cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:35:29 +0000

    run exactly possibly practical Dd

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def reverse_list(lst):
+        return lst[::-1]
