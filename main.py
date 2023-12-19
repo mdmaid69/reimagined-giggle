6021482d9b1a4ccb6219529c7b5b445a409a32fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:19:13 +0000

    land embarrassed convincing whisper imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
