082d4e61ee8ff9a3ec515d67d03c98b723517863
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:20:36 +0000

    only freeze rate talk flat

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
