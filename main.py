4e06da6d98160ecdc32f9a141bd632be4c706d4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:26 +0000

    gate plan square honest lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
