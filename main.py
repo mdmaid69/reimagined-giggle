b0c2a0a98dd5d4812eaca1c6f9c1a75835305cb3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:07 +0000

    runover Pp coat cover calmly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_list(array):
+        return array.tolist()
