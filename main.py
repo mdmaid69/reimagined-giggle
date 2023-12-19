1143cd9967b2997f98fea01808c608482072e9bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:34:18 +0000

    help classroom popular flight comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_set(array):
+        return set(array)
