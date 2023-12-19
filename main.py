4a1b4c9a5b782a34db416fff7cd646d0ccd1a325
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:18 +0000

    mark true emotion raw suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-print("Hello, world!")
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_float(array):
+        return float(array[0])
