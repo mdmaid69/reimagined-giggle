10d1dce1fddc3a4349d9532c2235eef63737bbf3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:52:33 +0000

    read appeal considering sport 565end

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def count_characters(sentence):
+        return len(sentence)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
