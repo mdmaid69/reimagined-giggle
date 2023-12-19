6f6f9a1af01f357e91425e838f732f42eacef843
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:55:51 +0000

    guess punish blow Hh personal

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def sort_numbers(numbers):
-        return sorted(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
