2581bcd4471e1cc97c1cb0714f9fd781b855f51a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:05 +0000

    emotion cruel peaceful battery ignore

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def count_elements(lst):
+        return len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
