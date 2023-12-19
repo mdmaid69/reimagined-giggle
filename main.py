7252dc725bb328b029b39038ce6d7bb11845ae9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:22 +0000

    pin telephone belong damage damp

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
