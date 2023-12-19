211409e1906c430a974d4d090625a55aad46d522
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:48:03 +0000

    side handle tie into world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_int(array):
+        return int(array[0])
