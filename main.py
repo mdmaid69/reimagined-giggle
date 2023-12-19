a7bd124ac6ac70614b8a14bfcaacab22e0f5352e
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:32:47 +0000

    believe too occasional know herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
