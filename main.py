e374d2c3197be44134b5d2b0cde27fe06fd6452d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:55:51 +0000

    back bored hear understand often

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+from collections import Counter
+print(Counter("hello world"))
