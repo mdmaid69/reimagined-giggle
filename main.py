9d74ae92df4a8322fa16eb6816cde12a3769b797
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:20:13 +0000

    each lookafter his to environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(lst):
+        return max(lst)
