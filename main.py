366048e91abea5ed8c50c5d57a1e29099727041e
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:39:24 +0000

    carry callup come lunch another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
