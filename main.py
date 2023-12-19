e1505ceaec6cc209b3bca78aad6528f8cc110cb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:21 +0000

    hate anywayanyhow refer switch borrow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
 def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
