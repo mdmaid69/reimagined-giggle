f8f0ace9589ee987388fa79cba25b526bfca636e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:10:10 +0000

    familiar fresh their statement inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
+  import sys
+  def get_python_version():
+        return sys.version
