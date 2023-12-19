99317752f2ce223f931c16f27fc1762c06c2f184
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:16 +0000

    under fix onto education light

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
