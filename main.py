a57a3bfa057c3a451d8b4236dd162d0cc64d0416
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:51:00 +0000

    gather present add finally yours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
