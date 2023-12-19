35ac1a7ac2a76c1d4e31520172df722203097e0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:45 +0000

    replace part ring delicate one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
