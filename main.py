03dd247fd3ec0a026c326efe41b17ff6f05e0dcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:42 +0000

    score maintain cross slip film

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
