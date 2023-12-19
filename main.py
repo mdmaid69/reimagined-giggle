c1778757762b768bb459f3ca4b8cd7653ce215cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:41:05 +0000

    serious rescue bend force present

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
