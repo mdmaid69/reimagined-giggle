76cfc74a366375907b17e3206aaac9eb2923ab50
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:38:33 +0000

    introduce cook innocent under form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
