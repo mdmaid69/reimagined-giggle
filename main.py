a8f724b1721c32fa7ed5ee6deacc190f87299cf5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:04:05 +0000

    rest pencil glad getup Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_item(array, i):
+        return array[i]
