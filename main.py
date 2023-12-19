9580e6df1520815d7ea0f335b2aee74a57c3fcd1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:23:04 +0000

    vision speak gather clear split

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
