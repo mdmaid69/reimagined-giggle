abd7de6187cbf79b6237e94cc10d9fcce634a602
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:53:27 +0000

    lunch rich attach come cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
