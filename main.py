beb4470a2dbbef21ae7313235e1204cd3008008a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:07:17 +0000

    suffer who shine fierce moral

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
