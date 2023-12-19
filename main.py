0a080384fa0cc915ee1f8bad8d59f533201731fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:38 +0000

    manage board typical content new

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def sort_list(lst):
+        return sorted(lst)
