1f9ca8d6dd1fbab2671c9539d9cd553a88c1b076
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:45 +0000

    speech really university tradition create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def multiply_numbers(x, y):
+        return x * y
+def add_numbers(x, y):
+        return x + y
