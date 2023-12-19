ba022b6924510232936124067e2e93bf84c9ab79
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:42 +0000

    free trick how second alone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
