a7ffe454437750928bfd2d3d2b53f78664860158
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:07:37 +0000

    common train use possible replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def find_max(numbers):
-        return max(numbers)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
