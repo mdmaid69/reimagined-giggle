d0f339b4da718a3b02dd89b30b285deb88d33f14
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:46:16 +0000

    fly error down pot tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
