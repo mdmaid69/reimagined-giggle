6433a48f6caa3ede8e2f56812968eb61ab57f98a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:16:40 +0000

    sit tape most bad town

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
