9379b742ca353736066dcb4189808c2bf241cbf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:15 +0000

    search become section guess small

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def append_to_array(array, item):
+        array.append(item)
+def count_words(sentence):
+        return len(sentence.split())
