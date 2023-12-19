c80561e6e9db700810565f9c53ceed4479708215
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:00:16 +0000

    ilovedicks callout approval goup vote

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+print(datetime.datetime.now())
