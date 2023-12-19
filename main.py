5d66ce57e0548d7b8d7de62acfb4547ed97d79cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:48:39 +0000

    fortunately motion somehow out criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
