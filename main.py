38bb3cc24710cf51d095b8ad500658b9341991de
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:58 +0000

    pool hollow 568energy hide pronounce

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
