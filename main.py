16a814c3c3d2c8b205e13ce9268a6d1708bc1ce5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:25:56 +0000

    wind turn mind dream corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
