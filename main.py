1850c5d8fbc9b22c82ed9f6bdec1ccd4c5d3dd98
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:28:59 +0000

    wild stiff tone opposite swallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
