a4c477371d6c9878d51610c706bec31723c562df
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:33:16 +0000

    turnback centimetre juice murder advantage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
