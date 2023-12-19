48b765a79da90a133c84685e9c03f5b42b72af70
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:29:46 +0000

    cross walk human sting pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
