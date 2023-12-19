7e5e8239f41b13b7b096053e4079611a2dc58781
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:27:15 +0000

    impression deliberately exactly risk bury

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_density(mass, volume):
+        return mass / volume
