bf17594cb21bebacf5267a26192164db807f4429
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:48:15 +0000

    stone complain without regard listenfor

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
