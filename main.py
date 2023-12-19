19d857e3d8f519a609d7979bb8677da8ba96a840
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:32:17 +0000

    have last fast reject rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
