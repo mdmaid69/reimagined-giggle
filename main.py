5dcd603416e3524a322ef320f03711da71c6d5ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:28:36 +0000

    team turnoff notice some clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_circle(r):
+        return 3.14 * r**2
