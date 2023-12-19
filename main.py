c920d7cc64dd9f8156711e044c0ac1941ad302c9
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:44:29 +0000

    minimum perform frightened soil approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def split_path(path):
+        return os.path.split(path)
