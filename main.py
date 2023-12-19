b544e1b173cd193adf237d0d98e4b18093fb6d87
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:10:14 +0000

    accident blow breakup suffer fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import re
-print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
