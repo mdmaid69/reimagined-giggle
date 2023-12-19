7324406423265c5fb5b3b96a82d87a0025bd9cb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:15:39 +0000

    perhaps write spill standard pot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import platform
+def get_os_info():
+        return platform.uname()
