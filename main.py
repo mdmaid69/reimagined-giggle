b614b659ab4a3717be7e7d6b617602dea9ec5004
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:56 +0000

    steady message count square 572enter

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def remove_duplicates(lst):
+        return list(set(lst))
