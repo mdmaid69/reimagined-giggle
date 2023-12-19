8567d66e04d3320bf9cca25ffafa33c264f486e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:31:59 +0000

    seat thinkof side foot mistake

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-print(sum(range(10)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_speed(distance, time):
+        return distance / time
