5ad37836ca37284ca77bcb4c9364f383b1543b55
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:36 +0000

    have organization excuse her congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
