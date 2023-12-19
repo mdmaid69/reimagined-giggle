b8c280373e4b25280dfda93570c18fb17c9ce967
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:00 +0000

    level branch public perform stamp

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
