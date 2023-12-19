f24e41494dd843ec23f164093fba284b22b3dbb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:23:29 +0000

    trial pretend rush typical open

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
