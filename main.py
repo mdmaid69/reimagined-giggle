7ebb75c492668775eb2d59f981c5494ae9f7179d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:50 +0000

    copy usual basic empty short

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
