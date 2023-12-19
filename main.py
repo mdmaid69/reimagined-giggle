99afd2176c137ba5cd02c23ea2e3dc2d7e9477b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:52:33 +0000

    invite approval explanation slide fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def sort_list(lst):
-        return sorted(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
