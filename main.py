6192a587c194407e4af5d7b0356241f82c2cce15
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:08:52 +0000

    house spill shut pressure freedom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def greet(name):
+        print(f"Hello, {name}!")
