bee42187044c6c506b882f5098c4b5b8ffdab000
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:23 +0000

    freeze accidental stand stranger image

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
