359b4d29f82b80cde28a017be281758bc42d6588
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:26:36 +0000

    open schedule survive available hole

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
