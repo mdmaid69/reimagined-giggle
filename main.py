5915fbb2f08d90b78454d052a30ab9e293f9079c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:43 +0000

    he rough union square around

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
