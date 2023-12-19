ba976bd581f7a08ea85586641bd633821d7f4e8d
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:24:34 +0000

    beat slow fire foot activity

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
