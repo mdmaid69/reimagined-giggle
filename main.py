884b5db567cd7368e5a8446eb5006f01d8a44955
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:01:18 +0000

    makeup outside thankyou doubt much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+for i in range(5):
+        print(i)
