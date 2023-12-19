17928abed044869589df019becf3bd3458010a18
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:20:27 +0000

    perhaps hope hello by distant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
