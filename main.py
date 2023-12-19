eea695181d680c015a5947d27e460b339753ec9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:30:56 +0000

    right price provide develop convince

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
