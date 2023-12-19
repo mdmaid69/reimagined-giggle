f124425fcab4af77cce3d2463e36a9323a7b66d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:52:47 +0000

    spring dear book bridge agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
