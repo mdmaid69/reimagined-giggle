48d71f120a8abf56506f171d30846316b6afc060
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:23 +0000

    argue well after imagination vote

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
