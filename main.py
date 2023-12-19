ce5f1b88a919ee51019e3a5cf4f910d340bb4d10
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:51:21 +0000

    gate permission state never bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def exit_program():
+        sys.exit()
