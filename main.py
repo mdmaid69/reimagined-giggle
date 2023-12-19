e711f74e5550302e0d63d4e286206b6f098b9327
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:31:01 +0000

    seriously responsibility love ticket change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_power(work, time):
+        return work / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
