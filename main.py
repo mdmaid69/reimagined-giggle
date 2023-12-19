5ff8b5c03b5111e1e38d8be301f485346f4e7d97
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:25:37 +0000

    noisy disapprove shrink listen avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
