cd3f3695cc4a081978d80b89a4619484e4fae5a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:48:39 +0000

    conduct sell congratulate hardly damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
