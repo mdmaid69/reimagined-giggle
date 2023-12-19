8fed1c50964d5958077c40102e0b205e008c0b61
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:18:43 +0000

    ill insist rise rule answer

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
