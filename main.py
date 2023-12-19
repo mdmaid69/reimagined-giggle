ecde1f8991c980e6aa132fea9deaafef343838a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:01:52 +0000

    us vote above laugh close

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def sort_list(lst):
+        return sorted(lst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
