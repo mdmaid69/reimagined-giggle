5efb1e9bb5241ea59c3a9024f1786ddc7d087c29
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:20:20 +0000

    they lookafter case season lift

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_speed(distance, time):
-        return distance / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
