3a970fcf8d57ccf393ee97bcb84096b1d7450cb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:38:45 +0000

    importance walk soon careless record

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
