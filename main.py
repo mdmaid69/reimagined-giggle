4588bf53530bd2fd281f1596a208849c4c4e23a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:09 +0000

    cut furniture shopping variety excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
