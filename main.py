f9397d33683bb65dbcb0b4966a101d18686cfc6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:10:39 +0000

    cause profit odd cost hungry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(numbers):
+        return max(numbers)
