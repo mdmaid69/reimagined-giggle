a5cb18801ea970c1062e1cc257a7e6ff55e5c169
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:50:33 +0000

    negative convenient low trial separate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
