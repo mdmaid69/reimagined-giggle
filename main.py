eb9de5adbec71377a6a108831baf3775b04ed6f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:54 +0000

    wind explanation sound choice man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
