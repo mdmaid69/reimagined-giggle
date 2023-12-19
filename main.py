a1965a3eb87040f278cc7b1bf917f8eb8671ce56
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:48:48 +0000

    race push hers improve mend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Powers of 2:", [2**x for x in range(n)])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
