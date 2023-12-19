f917c95fd3f68c29c307550aa93b972d3f47fb9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:07 +0000

    hand guilty important instant thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
