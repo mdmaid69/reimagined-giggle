fe733a7fb71fd38e964a773754765975eeae7439
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:15:10 +0000

    might weekend below apply cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_gid(file_name):
+  def get_file_group(file_name):
         return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
