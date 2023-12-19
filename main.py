d79cc38b3c654cf0ed65cc960637d43eb708000c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:59:19 +0000

    gooff beginning block complete confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
