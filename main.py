f69f15190566d5c4043855aa5685f158b54b4d5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:27 +0000

    trap chance hole desperate store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
