f992efd227808d192f704afb3559ae2c1aca7274
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:41 +0000

    concerning start genuine notice argument

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import json
+def convert_to_json(data):
+        return json.dumps(data)
