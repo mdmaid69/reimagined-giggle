62d9973d030cd5470c068eba55001844253e14c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:49:29 +0000

    rarely city border scheme door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
