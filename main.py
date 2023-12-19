6c64692ce5b0256c3ffe33889eb88515d3f4c347
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:32:31 +0000

    mix passage one there instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
