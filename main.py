f60d5d6f52c089afc9bc21c2cc5d36ea0a7f587d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:03 +0000

    furniture turndown pronounce prefer before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def is_even(n):
+        return n % 2 == 0
