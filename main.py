7954e6b3e5dfb448c75fa0dc7b8a5cd948176fe0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:23:23 +0000

    exciting getout key buy independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_as_repr(array):
+        return repr(array)
