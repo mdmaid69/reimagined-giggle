6859b4b65ea1144dfd3605d55aaa928ea94fc012
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:10:45 +0000

    Ee supply heart strict insist

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
