24bc8f46088c97fa9821e793174dfe27e358cc63
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:02:23 +0000

    low fold comment convincing quickly

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
