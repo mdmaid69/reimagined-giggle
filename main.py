3246105adcc7fcfa71f95081fd1420e701f7cdf7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:06:19 +0000

    own mouth coffee cope goback(to)

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import logging
+def log_message(message):
+        logging.info(message)
