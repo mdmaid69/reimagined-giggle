ba8a019edf456c4c39612355b32812fdc0e15c56
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:19:02 +0000

    tradition shock thinkof train lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
