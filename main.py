9aa1cce65a898358371eb6bf95fba87441ebded7
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:33:08 +0000

    on world partner thirsty art

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
