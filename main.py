162772a0967db8e8b3d7be40abe26ba762f5ad1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:45:29 +0000

    model vision material stay petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
