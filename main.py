66e37237d0283018f78da8b73976f0c5fd3b1925
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:46:06 +0000

    Ww confuse positive form test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
