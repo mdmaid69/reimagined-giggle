6959a4ef126ed63451f2791456e6f4258e66516f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:51:01 +0000

    send delicate picture listen same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
