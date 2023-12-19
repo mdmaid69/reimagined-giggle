76558c306a80968ff28b0c517bf29f14b45c77f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:18 +0000

    watch stick family 564encourage creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
