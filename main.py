b2dd8937c85b2357c154f81293ebbd8cc917d0de
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:59 +0000

    shoulder the mix responsibility clean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def subtract_numbers(x, y):
+def subtract_numbers(x, y):
         return x - y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
