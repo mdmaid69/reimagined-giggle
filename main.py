2d904a19b3864dc41344b7e5ce6550764d74d6b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:08 +0000

    deep contrast warn issue offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def add_numbers(a, b):
+        return a + b
