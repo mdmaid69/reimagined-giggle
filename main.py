1b65c655e8e298f344917f1e6ca3e0f21c860691
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:54 +0000

    sign unfair problem guess thin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-from collections import Counter
-print(Counter("hello world"))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
