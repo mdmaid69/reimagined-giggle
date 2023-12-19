86b79c9a12c07d89dcff05a180af46cede205c9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:23 +0000

    truth sleep copy touch waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
