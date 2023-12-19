b7fbc19f88c64e8943d25b885807feee1fb35971
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:18 +0000

    recovery ago wait switch whatever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+x = 10
+y = 20
+print("Sum:", x + y)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
