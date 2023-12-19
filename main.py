209b0fb4b18f40ecc705c7e04b88d2b752922bac
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:26:29 +0000

    best in happen shoulder lay

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
