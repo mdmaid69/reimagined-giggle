b020b85b445683041052f7695284e0538e7f601f
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:33 +0000

    attractive slip band suit would

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
