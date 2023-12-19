12c34b38ebfe983dd354f9be931b66f170c213a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:07:53 +0000

    praise statement boat declare poor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+text = "Hello, world!"
+print("Words:", len(text.split()))
