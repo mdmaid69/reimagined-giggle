829d8724dfe5350eb464e88663d4a384a547e0ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:18:41 +0000

    small stay this behaviour convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
 print("Difference:", set(list1) - set(list2))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
