d418279ec85a2be4b71f551d8952dfddbaf948a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:51:01 +0000

    still after alone such burn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
