2601ca04401a827afd6dcd85ba5dafd617e87c68
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:30 +0000

    at size round telephone murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
