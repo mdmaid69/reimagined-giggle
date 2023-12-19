7724b0b2c28d68abfc14cfa9b3004b0c2326f8d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:00 +0000

    connected go possibly suit there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def greet(name):
-        print(f"Hello, {name}!")
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
