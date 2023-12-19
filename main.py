8fc36ae81efeeea64cf50c9348d8ee10943818b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:00:21 +0000

    thin free prevent head convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
