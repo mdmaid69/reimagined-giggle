24154e6c9e77a4b7da03139859480e6ae0e673c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:21:56 +0000

    and autumn only meal seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
