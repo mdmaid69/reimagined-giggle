8194d6c635d7400e198ac52aeaea3c201036f039
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:24 +0000

    petrol see callback discussion angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+def exit_program():
+        sys.exit()
