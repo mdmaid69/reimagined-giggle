5b1bc98db1dee329d0cfd05639fa27f6a162644a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:31:25 +0000

    arm murder hide guess expression

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
