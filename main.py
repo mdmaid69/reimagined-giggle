e553714c437970653a672cd0984aeef99de1a272
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:45:24 +0000

    rest battery upstairs limit minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
