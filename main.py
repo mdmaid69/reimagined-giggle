d1e4c6190539cf6ebaefd167a5d2b3fec9bae359
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:23:43 +0000

    comefrom person comeback pretty laugh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_volume(length, width, height):
-        return length * width * height
+  def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
