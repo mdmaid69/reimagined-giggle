36fdd9667fd196d9573d8c3d7a60d73912bc3457
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:57 +0000

    ourselves deliberately narrow impressive beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
