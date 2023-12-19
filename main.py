a0584eda6a51386f97da1e286ebc36fee301c25c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:35:10 +0000

    upstairs matter coffee under hit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
