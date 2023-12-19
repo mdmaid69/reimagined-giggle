a9754e93b40c01acb4590b9511122d5f201eb59b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:11:55 +0000

    indeed noone nowhere do thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
