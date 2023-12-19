b7b8c7752972fa04a64c1e1d489d9bad0dd5550b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:15 +0000

    occasional obvious exciting discussion row

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
