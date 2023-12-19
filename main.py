9692a9105d3f01d37c39a9ac011b094f0c4cc3cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:45 +0000

    consult cover happen news far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
