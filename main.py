f637bba453fe37115a0bc817ae556744e96cb15a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:37 +0000

    process seat eat beyond complain

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_area_rectangle(l, w):
+        return l * w
