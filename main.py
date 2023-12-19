64d55b0451b17b6ec917cd12fb5ed48cf0267091
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:41 +0000

    benefit join godown work learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_exponential(x):
+        return math.exp(x)
