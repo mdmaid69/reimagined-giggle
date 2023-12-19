c2213b3d455a6f39459b0dbcdaf6d579118cb2bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:44:59 +0000

    worth morning tape guide help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
