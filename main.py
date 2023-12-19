fef50e75d1b14ef9cc71dc7f61c22b9c2e67cb0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:30 +0000

    clean Oo confident harm accordingto

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
