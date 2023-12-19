a8234f5e0a698adf66638cfe5e14134d81d23116
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:45:35 +0000

    play keen off city soil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
