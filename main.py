327ef26fde4c29f78663b05d94f1ba2b0e709a9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:25 +0000

    tap smooth ahead area study

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def sort_numbers(numbers):
+        return sorted(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
