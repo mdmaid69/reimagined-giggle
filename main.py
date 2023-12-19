465453df16aa4c694e03feb4e328c2e26e10c3d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:17 +0000

    world turndown recognize goout goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
