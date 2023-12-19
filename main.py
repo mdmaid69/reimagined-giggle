460c9aa721b5840a205d60b5e416383a2337aa36
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:02 +0000

    tonight fast instrument tap near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
