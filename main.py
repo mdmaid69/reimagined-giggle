3c005a7bbe75d6e4f2d17b5c319c0a0e2b6b2f10
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:46 +0000

    count medium praise whenever bye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def cube_number(x):
-        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
