db8ce2ce2cbbcc7d947afd92c5b24e85fc818691
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:53 +0000

    constantly actually fear game keepoff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
