cfe8c9700779b2836170c0eb622b2647e05834c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:35:14 +0000

    side middle callup answer seem

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
