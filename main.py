f5d32065dabc59a557ccc1cdce5de9fd151bbcf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:22:08 +0000

    goon cost clothes splash shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
