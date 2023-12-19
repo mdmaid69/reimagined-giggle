9d0a7d79792c94f0572a3bb4a7ff3ad52385a274
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:00 +0000

    dream mystery put rush run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
