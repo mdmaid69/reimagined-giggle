b27b2b9a2b615010a53cd3ca70daff71b8fe59ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:59 +0000

    control force rain it study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def add_numbers(a, b):
-        return a + b
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def change_working_directory(path):
+        os.chdir(path)
