4047e1bfa3a25661d9371cdc036d490504d461e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:27:52 +0000

    steer goup congratulate fun disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def multiply_numbers(x, y):
-        return x * y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
