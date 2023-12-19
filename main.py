92d7c3c9f8b0b67ab2981551f3a7eccceaf2f55b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:30 +0000

    control hole proof later increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-print(os.getcwd())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
