f1a6eff84eeef2172196b61d7b08d64e6becefc0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:11:41 +0000

    plenty shadow roll shut rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
