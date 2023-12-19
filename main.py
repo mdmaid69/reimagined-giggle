715a2d0153fc3c0d52279237abc595d9059933a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:06 +0000

    change decide sale share ignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
