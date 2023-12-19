05564a7956a729884af5b8b0ad1da8cdbe2772ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:39:37 +0000

    half track operation include together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
