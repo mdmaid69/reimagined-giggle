9b2ead7255da6cb3b592e4d68571fcc1fe7432de
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:26:25 +0000

    issue path strength beyond judge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-print(json.dumps({"name": "John", "age": 30}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_pressure(force, area):
+        return force / area
