6aff54ea1e4fe2f9dc012054db9573478afb3ba2
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:09 +0000

    eager rest murder fierce sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
