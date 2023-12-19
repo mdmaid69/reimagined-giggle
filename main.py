3f5c4fcf6e99c79cf2731cc03aadf8a7fdb8d3a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:15 +0000

    share hello responsible badly copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
