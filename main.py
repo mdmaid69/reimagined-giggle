62191901bb97ad2b3ffb132611918f239e30e930
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:41 +0000

    again interest tidy cum sky

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
