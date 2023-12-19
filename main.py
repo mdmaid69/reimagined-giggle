220e0b68966a010d94f364bf3b6573faeca1459c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:14:03 +0000

    getaway favour picture account Uu

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def multiply_numbers(x, y):
+        return x * y
