a34611dcc76d8146832d6dc60f385d255a21e491
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:50:38 +0000

    distance uncertain stamp connected dark

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
