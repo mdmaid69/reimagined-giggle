e129e78b460527a2f1543a18119e6952aa57ed2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:19:56 +0000

    shut advice affect noise gradually

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
