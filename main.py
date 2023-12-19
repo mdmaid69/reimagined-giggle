2adf353a6627207c9a53ef8b97391b0eec914696
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:08:30 +0000

    neck slip wide how passage

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
