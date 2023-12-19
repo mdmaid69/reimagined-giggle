53a9298b2cc244c6ae7c4197bf9e414f3f1d5872
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:23 +0000

    book secret obvious turn straight

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_average(lst):
+        return sum(lst) / len(lst)
