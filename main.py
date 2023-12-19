a8177b689eef0a8ca7f5a95fd7b936de6f99057d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:42:33 +0000

    connected completely thumb step closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
