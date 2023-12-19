a07d6eb5016bb6df740954047f67cf6f554c94b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:34 +0000

    duty fork few Nn relationship

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def remove_duplicates(lst):
+        return list(set(lst))
