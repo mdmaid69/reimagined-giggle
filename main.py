57187e3e239f34fa50c1c591b199ad3d5fd62696
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:49:56 +0000

    come train future address no

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
