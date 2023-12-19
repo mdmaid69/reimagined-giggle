55632441f2b5ec25c74af80dd9cafd37316fdf95
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:49 +0000

    spot guilty wear complicated slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
