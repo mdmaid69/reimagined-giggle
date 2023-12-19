874b8c41a9b892bfb24006f714f69a196cc0929a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:00 +0000

    offer protest wing duty open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
