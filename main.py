0c15e6584e12578bd27013813219d6e287d16f08
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:17 +0000

    pass bet cut far most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import sys
+print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
