506709310cb66f5b5c615fbb998552a18fe7243f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:44 +0000

    thought thanks mistake blue deliberate

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
