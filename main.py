f62a28e0226bd07156e7af6b5fb4d508e4eff98c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:50:51 +0000

    admire weather neat flat of

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def is_even(n):
+        return n % 2 == 0
