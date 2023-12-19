6a1778ef6a7f2ea65398f33edee6109bc1f0ff21
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:35:06 +0000

    stand pick match believe grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
