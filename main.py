64af967a6f9399386b1d54556af14aed273daacb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:06:01 +0000

    dick too deliberate go battery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
