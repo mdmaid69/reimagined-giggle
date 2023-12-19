c3a1b199aabb2e33f9f1d41536e1adc1a50ffe3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:32 +0000

    interesting post clear neat separate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
