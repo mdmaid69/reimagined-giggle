66fa3f2aae34a38478fc5f468525485539bc0be3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:25:49 +0000

    company hello organization return run

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
