58cb6a83be47755d2b6837f9335d69f762caee44
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:58:48 +0000

    window central stranger capital see

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-for i in range(10): print(i)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_density(mass, volume):
+        return mass / volume
