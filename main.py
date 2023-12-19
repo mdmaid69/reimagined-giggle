0d8270f5efb2b3a928aa608bb27a0d75e1aea1c0
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:41:32 +0000

    instant government pour mouth ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import datetime
+def get_today_date():
+        return datetime.date.today()
