8b4ba757007f5edee998f5e3b2429d6ac52032a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:22:14 +0000

    persuade open range question minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_item(array, i):
-        return array[i]
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
