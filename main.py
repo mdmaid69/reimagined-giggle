e297b7b690c9f2ada1606440dc81a6328cf8da0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:52:09 +0000

    memory today gather crime spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
