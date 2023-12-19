ea850357d3ccae098bddff2e9d235353adab6d7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:19:06 +0000

    comeround quit introduce expect replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
