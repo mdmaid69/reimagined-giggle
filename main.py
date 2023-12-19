9c292116e7bfa6bfecaea3e037869d43e776c485
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:05:22 +0000

    yourself concerned freedom congratulations bring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
