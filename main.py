de8f4812251ec1e26724764af34e4d7d744c832d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:47 +0000

    Dd borrow tune model splash

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
