034f63818d6cbfc20806fa37ea214583d925d73f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:34 +0000

    road base get contents interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
