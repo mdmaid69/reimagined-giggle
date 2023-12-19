987471355e76d5037a67deef1a2c388c3ef3dbae
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:14:41 +0000

    invitation good pride funny must

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
