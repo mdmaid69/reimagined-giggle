b9a6dbd00302bbe8514196c094a9ded0014c1e33
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:46:00 +0000

    morning would dull tie message

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def add_numbers(a, b):
-        return a + b
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
