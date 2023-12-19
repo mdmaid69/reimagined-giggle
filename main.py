b5e5ce9274809f06f8d537680a40a9cd46c2beca
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:47:31 +0000

    realize reduction period safe married

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
