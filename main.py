4ed1b58f1599fbe9d6e6e93cb025b33ce381d7b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:07:22 +0000

    jump several fast share freeze

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_set(array):
+        return set(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
