7cabcb2d8dc97f57a7aff5a3b42f4866324e02f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:07 +0000

    happen trade report down play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_int(array):
+        return int(array[0])
