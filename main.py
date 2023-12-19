8ee856f375c5c009d5859d1735bdc3fc39f1a4f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:40:48 +0000

    justice war genuine strict national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
