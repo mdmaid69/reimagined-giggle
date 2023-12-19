3447e024dda19cf1f22ef643f257fa6d1ecfa261
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:15:12 +0000

    behaviour comeback proper mainly sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
