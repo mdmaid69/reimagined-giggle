dc18e91cfc7e5ce1cc9309953ca0478b83bf41aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:48:25 +0000

    tread software over petrol water

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
