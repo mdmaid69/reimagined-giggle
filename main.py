febf4f03d5230a3cf3a031d7132a71fd29314fdd
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:49:47 +0000

    mass proper pipe man unfair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
