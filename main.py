c1921413ec17e21f07169418579a0fa7e5838a16
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:58:48 +0000

    centre in comment nurse method

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import sys
+def exit_program():
+        sys.exit()
