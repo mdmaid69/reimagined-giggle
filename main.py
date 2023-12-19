63836941a546d3a57640beb7417172e70fb9bdc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:44:45 +0000

    divide touch fence bank improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
