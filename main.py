440b31efeaa3c12dc6558336dda3fc1065941230
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:44 +0000

    upstairs close wish importance about

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
