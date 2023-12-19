550472a36733f838dcf8dbddf17d4c4024efc407
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:29 +0000

    steal than work treat safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
