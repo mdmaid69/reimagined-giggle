2eed19cb656cdd4aec145b678dc6e1e81fc12fe4
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:31:50 +0000

    view decide move spread gather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
