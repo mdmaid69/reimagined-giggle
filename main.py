f52f05aaa61fee9ffffe0a78cf49684918150977
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:47:00 +0000

    weak report occasion expect clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
