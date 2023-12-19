eef96f46017db83d3578cb942ccf1a624cda9d89
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:15 +0000

    fight travel tape issue plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
