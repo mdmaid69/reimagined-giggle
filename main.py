5148090500727d49d7071a9a5fb32b675e73dd76
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:32:52 +0000

    calmly bad form easy bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
