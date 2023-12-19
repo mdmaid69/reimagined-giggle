5074e4d02fffa3a294ea84c27725f068c76b9bfc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:42 +0000

    fit brother terrible float anything

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
