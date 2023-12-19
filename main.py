473bd7aabc63af365453c9ab692c9f440cfe86e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:54 +0000

    all responsibility forgive study opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
