7b0f8fa01e44f85b1f1baa4a516f812d875e4fec
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:29 +0000

    comeback loan education high touch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
