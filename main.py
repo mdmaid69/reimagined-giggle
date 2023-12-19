c353f2eebe6826a54517292479b57c67fa10b742
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:44 +0000

    dish at yours like small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def get_array_as_bool(array):
+        return bool(array)
