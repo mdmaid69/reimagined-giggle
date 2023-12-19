af9d8ee19606fb1508cc1b1b1d033ef7f6aa9b5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:09:46 +0000

    over past anxious so Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import platform
+def get_os_info():
+        return platform.uname()
