4e62e47230ae937a5019276bd8db80205539c8eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:44:15 +0000

    to before fear nothing keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
