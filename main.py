c25e64ef4a6297738ef7ea577618f1b1d2d35ab9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:46:56 +0000

    left smoke tie age challenge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
