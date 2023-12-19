79e94f1dacdbc2cdab402b02b182db86a7687b38
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:52:35 +0000

    moral hold bank image influence

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def count_characters(sentence):
+        return len(sentence)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
