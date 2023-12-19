dffaa292f98779d716158478657931407bff218b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:18 +0000

    criticism word smooth suggest crazy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-for i in range(5):
-        print(i)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
