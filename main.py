b71b5df79ce0b04d482bfaac21f51081969f109a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:11:40 +0000

    long decision before whistle explanation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_min(lst):
+        return min(lst)
+def multiply_numbers(x, y):
+        return x * y
