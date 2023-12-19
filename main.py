7e2ab8ca793982e338f715e2ecca1e57c580ab76
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:25:36 +0000

    watch total church confusing petrol

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
