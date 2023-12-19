9d9ae24219b8c996fbce20fc1429c19b695557cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:29:38 +0000

    frightened half seriously yours process

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import time
+def get_time_since_epoch():
+        return time.time()
