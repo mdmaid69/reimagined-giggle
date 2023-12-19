55813f19b113416f5b33599aaa54fe1241a48207
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:14:42 +0000

    callout never similar difference almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
