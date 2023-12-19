3b57db1225535734e13ef88a776f1cbf35522c6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:54:12 +0000

    upset command 566end enthusiastic freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
