b013d44cb5536aaf46e6d350e1f158470a9f8298
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:38:50 +0000

    dare vision could left pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
