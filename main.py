d7b6eb101edbf85c931e38a6a3990911c3080f71
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:15:43 +0000

    continue side among today empty

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+print([x**2 for x in range(10)])
+import json
+def convert_to_json(data):
+        return json.dumps(data)
