fac09166753b038c6df41534dbc5d6c235b4328d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:47 +0000

    warm ability usually apologizeapologise steer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Hello, world!")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
