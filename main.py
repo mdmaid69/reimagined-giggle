13180cd44ff163a4155b9b3cee90154e926370a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:50:29 +0000

    that exercise pattern listen pressure

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
