26b4b6524255596cd96bc6ab0f39eb8bbedc3b0a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:02 +0000

    acceptable vote tip confident wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
