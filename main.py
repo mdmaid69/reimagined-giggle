f0b4dc42050f249fd0335f52f8c1370549e95a9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:39:53 +0000

    chin phone favourite help moment

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
