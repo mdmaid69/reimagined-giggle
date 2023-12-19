d917b750bc111add9227b38e47c39162bb881106
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:47 +0000

    acceptable passenger apparently couple trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_error_function(x):
+        return math.erf(x)
