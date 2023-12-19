8baad5ee367ca58fc4c9b233fe5aec0467b57882
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:34:20 +0000

    danger order fault rescue habit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
