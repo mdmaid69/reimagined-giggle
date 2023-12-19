2a9a2ec4409a45a61637de275058dd8193c499c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:42:58 +0000

    female 571enough watch city company

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+for i in range(10): print(i)
