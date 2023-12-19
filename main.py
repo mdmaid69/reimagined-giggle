cf3941394f8003995e560105e395ea393aaf4971
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:51 +0000

    march habit theirs Mm protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_os_info():
-        return platform.uname()
+  def split_path(path):
+        return os.path.split(path)
