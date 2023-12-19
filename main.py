0066c4312e21c634bbce405343be281f48746223
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:00:43 +0000

    expect back anxious in spill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_min(lst):
+        return min(lst)
