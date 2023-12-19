a4ed6e2fc68f16591f03c5bf4fa04c021f721e97
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:53 +0000

    plenty related search harm disturbing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
