7b6753de1b96cc9fca223affa72ac9d8e44cda3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:26:37 +0000

    instruction seriously somehow weigh nice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
