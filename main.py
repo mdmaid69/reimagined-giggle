ee1e2d91e1eb955e76ceca9c2e5aececd5718fa5
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:18 +0000

    measure help spread suck defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import re
+print(re.match("h.*o", "hello world"))
