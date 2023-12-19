7e95e57f42e7663e6ae138889bda32708e115707
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:36:33 +0000

    dare generally patience estimate anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def clear_array(array):
+        array *= 0
