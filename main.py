873a4572428423453f2aec663d7fcd24788b45ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:18:42 +0000

    unkind over war no border

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
