f0b7c610212c84d047791bd9520b6e0a9da7eada
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:45:53 +0000

    convinced second why point sister

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Words:", len(text.split()))
