59f52ae0c1e82e943e4fd5e8d776641f92d112ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:48:58 +0000

    fast clear cook lookback(on) smell

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def sort_list(lst):
-        return sorted(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
