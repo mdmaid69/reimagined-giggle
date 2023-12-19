db9f2e8ff67d6ed656b07f0d60a458332f8ea3cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:31:04 +0000

    minority post key this stop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
