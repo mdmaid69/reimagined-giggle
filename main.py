4cb5e1bd3895ad38f476664e76cb08e7ee81708a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:38:57 +0000

    organization equal block dance bringdown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_circle(r):
+        return 3.14 * r**2
