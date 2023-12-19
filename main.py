e750ad5faac041c44f83c3a797ee366ad839057c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:52:34 +0000

    train only goup surprising must

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import time
+print(time.time())
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_memoryview(array):
+        return memoryview(array)
