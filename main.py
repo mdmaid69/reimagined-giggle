168b1dcf6a95c93436b9c14baf1a45faa02cb124
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:21 +0000

    tidy blue exam sure fashion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_work(force, distance):
+        return force * distance
