2fc2ee24bdf24d5ace5a582b187a931799ea7a6b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:26 +0000

    journey sport rent character replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
