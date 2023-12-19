247860ee9fe43e3f2960b34253c31be339c20723
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:22 +0000

    turnout thank when ago matter

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
