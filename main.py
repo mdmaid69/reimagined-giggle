e5f93d245dcd8760242a3ffb7c5474b89b5189b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:29 +0000

    have damp lookup system power

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
