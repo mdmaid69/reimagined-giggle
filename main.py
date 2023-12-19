6be5547f0c9230d9c5f7257d9666f51ab12d9135
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:09:40 +0000

    politics bring rush walk perhaps

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def sort_list(lst):
-        return sorted(lst)
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
