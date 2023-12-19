2f688704cd72d6ffe45684dd308d3029d487b3ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:57 +0000

    wave myself town path force

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def split_path(path):
+        return os.path.split(path)
