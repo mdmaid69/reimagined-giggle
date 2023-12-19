c52c9e0dd22235818055db980bda776ecb517a63
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:12:45 +0000

    rest handle television design street

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+print(math.pi)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
