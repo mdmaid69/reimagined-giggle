23a19baff1d8297cb467073c4e8f7a4ae7957ee7
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:47 +0000

    opponent trust set much give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def convert_to_binary(n):
+        return bin(n)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
