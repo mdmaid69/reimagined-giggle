07f1dcded99f340605e7e946795bddf6a23077b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:24 +0000

    respect dry perfect in watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
