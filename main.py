cdcdf0572ffaf5d9fded35dad7cc8a487732dcb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:48 +0000

    regret share far knock breathe

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
