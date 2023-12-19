8a09047e46c98748924c4a1b5a81c8319119ff68
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:09:58 +0000

    remain club perform section complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def convert_to_binary(n):
+        return bin(n)
