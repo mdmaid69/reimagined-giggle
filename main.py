56baa9750865c7f9f0d2bcd1b1d1b8da0b8e78e7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:20 +0000

    read confused herself hungry possible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_typecode(array):
+        return array.typecode
