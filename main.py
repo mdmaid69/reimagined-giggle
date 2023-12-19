9d8ebd0a8d43ed6e6037c85d2c347d25f03179d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:34 +0000

    tidy offence petrol fast room

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
