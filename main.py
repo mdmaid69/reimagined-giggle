fa7fa22ebb4cb52642f3c23c3293a0bc46297bba
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:47:52 +0000

    luck breakinto mean crowded trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
