1b889aad209e06faba6a863484fd2d9de0dbdf54
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:04 +0000

    phone impressive might should apply

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
