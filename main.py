2025470fdb1a19fcf6d09532f296fc4708d7ee1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:10:29 +0000

    fight our brush she crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_density(mass, volume):
+        return mass / volume
