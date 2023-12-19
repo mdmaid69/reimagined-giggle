925d5dd8dc9106aa19bb11ffd04f79d60d7079ea
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:03:23 +0000

    satisfied she sort office nor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
