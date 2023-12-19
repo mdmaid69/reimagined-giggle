23e0420ec3ad8d64701c0f6a3ca149576ca56cd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:16:28 +0000

    under comeout behind confusing repair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def split_path(path):
+        return os.path.split(path)
+def remove_duplicates(lst):
+        return list(set(lst))
