7fd7ec043056dee7e27844e8c9b37697f5ba281b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:16:20 +0000

    bet goout sure 570enjoyable emphasis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def count_elements(lst):
+        return len(lst)
+def is_palindrome(s):
+        return s == s[::-1]
