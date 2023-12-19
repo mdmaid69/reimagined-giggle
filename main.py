2ce6da822037d2087e62de5b16212c8b427fee2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:25:57 +0000

    scare flight ocean cool purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
 import array
 def check_if_array_contains_item(array, item):
         return item in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
