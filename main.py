5fbe515b689c413cb46b683d83269bb5fb0e3b6a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:49 +0000

    refer far comment turnback every

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
