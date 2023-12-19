0091b91bc51873d19a5268da517bc14c7ec120e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:46 +0000

    except ignore kick manage earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
