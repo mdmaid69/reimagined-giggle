29b9fd5eb9cc80d5b95ab37feb738faf8d55240c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:37:43 +0000

    tense pride impatient free culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
