8191e8a064bc4b281836a3a066ef8c36ec654798
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:58 +0000

    opposite gooff turnoff bend repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+print(math.pi)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
