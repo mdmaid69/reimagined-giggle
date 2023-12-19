23f2bf914a345321e07d0332cb588a4803601b6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:19 +0000

    time copy compete weather break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
