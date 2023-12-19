a658983f176f1c2b50688788c7bb57474bbe596f
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:05:35 +0000

    zero lead label explanation measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
