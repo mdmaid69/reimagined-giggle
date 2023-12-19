7d4b985f1d7592e137990b1dee31974e1dafe7a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:23:11 +0000

    pause accommodation safe prize protection

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
