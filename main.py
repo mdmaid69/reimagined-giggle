cdae81507e3520f3163f4862a430de1d596e3bce
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:15:44 +0000

    accidentally ashamed accidental temper bottom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
