92994c0e7ce7fa6e27f051e69f3535715ccac92e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:07:53 +0000

    will disappear mark thinkof find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
