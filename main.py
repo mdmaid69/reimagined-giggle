9890ab813a79d312097d6a287f98ef18a1201832
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:31:30 +0000

    until behaviour dress each company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
