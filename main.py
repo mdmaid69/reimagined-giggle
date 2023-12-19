1252895bd347de5b089f16cae621f7058fbd7ee3
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:58:35 +0000

    appearance question keepout call concerned

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
