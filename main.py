53ec40c2a54bbe05615c2271bab5013c91a5d108
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:31:16 +0000

    fierce spite cry quality demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_force(mass, acceleration):
+        return mass * acceleration
