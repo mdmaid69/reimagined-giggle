9488edeaa011cfb1b7bfc4fd618c97ccf048a0d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:22 +0000

    issue oil nigga land pause

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
