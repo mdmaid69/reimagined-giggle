932f20ef06400570d1f562ba4869bb681c17ffba
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:04 +0000

    only instant choice agree listenfor

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
