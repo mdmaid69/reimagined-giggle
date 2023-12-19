5564fcc93c45747eaaf063ac2fc6a4e3ca9887c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:34:51 +0000

    soon bad doctor defend next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_binary(n):
-        return bin(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def is_odd(n):
+        return n % 2 != 0
