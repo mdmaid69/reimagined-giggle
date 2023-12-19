d72adbb7177cc80188990e98ea1a85b94a1596a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:28 +0000

    content tell hurry unable tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
