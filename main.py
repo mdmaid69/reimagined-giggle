4ede6568fcda07524d3e9d9790f40a3dcfbba412
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:25:17 +0000

    breathe 567enemy like confuse down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
