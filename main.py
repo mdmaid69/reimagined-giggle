d1865f475911572e021d6a3414f47683ca43bb60
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:11:26 +0000

    estimate world spring separate surprising

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
