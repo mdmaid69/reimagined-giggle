ff7b4027749477154cbb55c989709724b80637aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:05:02 +0000

    lot fear during underneath most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def convert_to_octal(n):
+        return oct(n)
+import platform
+def get_os_info():
+        return platform.uname()
