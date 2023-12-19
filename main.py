58538ded1b95bd598530f9a491b7e9415f1b9d5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:59 +0000

    basis swimming clue greeting instead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
