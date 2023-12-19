27bd4b92efd7decc27d9075558a7c811df6f9388
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:00:02 +0000

    should worst thanks choice foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def cube_number(x):
+        return x**3
+import os
+def change_working_directory(path):
+        os.chdir(path)
