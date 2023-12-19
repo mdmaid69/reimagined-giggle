e5d474e0747ffcad56d5d9ffd5d4420334f28a5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:05:59 +0000

    smile respect lead damage round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
