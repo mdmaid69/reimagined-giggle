0c4dbc5ab7f3dd29b1f740e52c55d3a6f166f2c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:03 +0000

    late band rule 573entertainment permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
 def calculate_euclidean_norm(v):
         return math.hypot(*v)
-def count_elements(lst):
-        return len(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
