f5c2a1ec9b88dd6f103b31708e41a791c2ee1a3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:16:51 +0000

    main wave system speech tap

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
