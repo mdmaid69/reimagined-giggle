87dac6a5fab2d7acedae5ffa946af525b4f3f074
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:47 +0000

    across our conduct lean dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
