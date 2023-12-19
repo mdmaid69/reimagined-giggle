06a726b49bd022e64e346526fdc262159e70c837
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:42 +0000

    benefit wherever average aware fire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
