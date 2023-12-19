fd3fb11ce120e5ec6963f878b87b55cd61b57d7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:29 +0000

    keepup spring scale perhaps reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
