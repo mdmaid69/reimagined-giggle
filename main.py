ea5f1a253c0f37030583dd0df24515fa1f4b69b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:13:58 +0000

    speed mess fault visit matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def convert_to_hex(n):
+        return hex(n)
