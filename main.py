ee7590dd411e4678fb0296402a028acf7f61a7eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:54 +0000

    land order tread winter above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
