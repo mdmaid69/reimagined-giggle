a87f5a2fd6dfbf9db7122eb91c299c48ba55969e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:38:42 +0000

    school drink free read anywayanyhow

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
