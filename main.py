740f6cdc29bea31402dd63f78fd4f932ca8ce761
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:24 +0000

    introduce impossible rest nature like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
