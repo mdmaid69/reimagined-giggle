5326108f35db2868a725231098757414cfc87b3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:42 +0000

    cruelty juice ought appeal medium

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-text = "Hello, world!"
-print("Characters:", len(text))
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
