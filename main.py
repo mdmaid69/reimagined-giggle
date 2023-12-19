adb93ff89db274997e0b6342f3d4f7c18c0c2c9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:28:28 +0000

    building claim operation stop criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
