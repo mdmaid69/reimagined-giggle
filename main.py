af966c1b292a2ff58d08671b007927a813bcacfd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:15:45 +0000

    failure study fetch he way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
