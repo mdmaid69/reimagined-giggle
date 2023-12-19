ea62c40bcc3c996e24785cb47a832f053200ca0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:39 +0000

    press whether hit night blind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_hex(n):
+        return hex(n)
