cbe3f913b80c3f30d89569c2bfbf2e7816f4fa9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:09:54 +0000

    heart plenty range crime breakout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
