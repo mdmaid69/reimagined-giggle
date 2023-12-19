d70d8a6e33dfa9e58632f8fc729308d3d76e122e
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:25:06 +0000

    worth involve no turnup lend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
