1ad829626bfc236ee9f3fd094d5daf3a9f95977a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:11:51 +0000

    translate convince doctor violence can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
