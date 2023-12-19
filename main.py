680f7ddddfd61c923d9c41266107b0d659903e71
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:33 +0000

    advice pin unit trick reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
