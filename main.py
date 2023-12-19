087600f2f947d7cabcc9f0b2f90068a1b44ce260
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:15:52 +0000

    tie thought influence cross attract

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
