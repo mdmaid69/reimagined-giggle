300547d238c2b4e3ec6b0bbc320061ec92d2507e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:11 +0000

    serve stop pocket practice community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
