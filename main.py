b5346e436a3a7a0313b0d3afa8a3b77c7dd63994
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:06:29 +0000

    past certain border children sight

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
