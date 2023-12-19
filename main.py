e5bbf65aefbc402cab28c3f3c46a6cb139ee176c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:49 +0000

    busy strong command direction odd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
