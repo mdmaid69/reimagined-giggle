1382a93e5e1528f66b8154a7c93f4152d051d569
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:16:30 +0000

    ocean passenger pronunciation under false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_unique_words(sentence):
+        return set(sentence.split())
