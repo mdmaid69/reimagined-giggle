4d65b95195a1c82a747b196836b7e93bee324542
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:04 +0000

    wash backwards contact lookup duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
