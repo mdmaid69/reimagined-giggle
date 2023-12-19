88fe147d23fa52a421718a125e5518058d58a8c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:14:51 +0000

    frightened broad season rich shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
