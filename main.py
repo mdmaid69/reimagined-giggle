26dccba4df2cfc1f1543186ade8fb4280fa60fd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:35 +0000

    yours keepfrom switch rescue better

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
