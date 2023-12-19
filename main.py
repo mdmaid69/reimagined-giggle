2a069ed8de0ab63258c71050bbf635f85026d706
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:25 +0000

    worst free goup favourite relief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
