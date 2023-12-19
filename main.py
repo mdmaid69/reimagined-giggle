386ebdf3ea312c467203d5707b5f7815f3b16d16
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:09:43 +0000

    escape sense freedom beat respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def reverse_string(s):
+        return s[::-1]
