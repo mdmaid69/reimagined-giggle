9cca7c347f866138155eb60c778eac3268c35941
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:01:25 +0000

    walk would everybody visit generous

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+x = 10
+y = 20
+print("Sum:", x + y)
