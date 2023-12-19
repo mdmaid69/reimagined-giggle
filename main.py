6c73255d0562fc902b9caeafb3844bb0474980bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:45:41 +0000

    holiday brush pin survive warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
