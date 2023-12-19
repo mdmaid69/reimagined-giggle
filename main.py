d889f44020e9fad068980f653f58b31639209748
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:52:06 +0000

    warning relief letter private after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
