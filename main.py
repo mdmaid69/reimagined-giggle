965ec3db6a39219bbb8a671c905973ada3bb2e1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:00 +0000

    finish hurry some almost switch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
