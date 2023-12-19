90caf03bfddedf345b39d12de7b666f491fef453
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:02 +0000

    acceptable back difference dull anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def clear_array(array):
-        array *= 0
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
