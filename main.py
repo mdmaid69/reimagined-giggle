1529463f138cb49004d95139c16709f3a72c0d7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:24 +0000

    curious thank goout every swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+name = "Python"
+print("Hello,", name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
