d6ae8143c3475e8c327169d7466bf3a286e376c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:48:09 +0000

    government noisy scheme smile tape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
