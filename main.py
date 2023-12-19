aa6a03abe41f95964be5f0da1c2120e756ba7fc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:51:33 +0000

    be world design 569enjoy constantly

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_hex(n):
+        return hex(n)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
