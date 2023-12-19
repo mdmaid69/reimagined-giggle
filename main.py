d9edaa86849a17419f5957981c4c0879da86d74b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:19:11 +0000

    delight shallow otherwise talkinto couple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 def find_union(list1, list2):
         return set(list1) | set(list2)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
