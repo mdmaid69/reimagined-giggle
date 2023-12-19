a53fcfdfcf5edf7223ccdc61efb69b384f5fe638
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:20:10 +0000

    succeed excuse tax country draw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_counter():
-        return collections.Counter()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+print(re.match("h.*o", "hello world"))
