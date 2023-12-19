61546a0bb1b07db14249d5b2b72fa3a0ed86056a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:16:47 +0000

    decision through loud ticket less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import random
+print(random.randint(0, 100))
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
