32ec5f7d384b690f2f216c57eaf3d57d9ff6fce9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:30:04 +0000

    anyone identify manner card notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
