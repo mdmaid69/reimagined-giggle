29db53c93dcfd254c9a5b89ef89c868658ef8c40
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:26:07 +0000

    delicate peace rough pencil abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_to_binary(n):
+        return bin(n)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
