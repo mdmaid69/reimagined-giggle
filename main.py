fe01b6808ccdf3bf9c4b78c8b60c8be1e92fc5b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:56:11 +0000

    trip swimming separate house OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
