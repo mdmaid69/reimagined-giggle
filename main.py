2bcb0db9b05688d48b003ec9f8226dd7eac21c42
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:57 +0000

    slight influence few surprising them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_list():
+        return collections.UserList()
+  import sys
+  def get_python_version():
+        return sys.version
