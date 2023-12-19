77729403dbfbabd631565d3ed88f9588fad9677f
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:27:28 +0000

    march fashion area mark turnaway

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
