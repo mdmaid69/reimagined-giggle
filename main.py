c656223e36f998eaee45841f55a8655b4ce1a24a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:29:35 +0000

    phone bang in mistake goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def divide_numbers(x, y):
+        return x / y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
