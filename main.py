9b1636d45a6d59109b59049363bedf091631cff0
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:16 +0000

    copy risk team reduce some

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
