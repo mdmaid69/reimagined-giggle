28f284e30c2fadefe9535b12b36e64413d40e9ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:32:51 +0000

    agree throughout treat one comeup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
