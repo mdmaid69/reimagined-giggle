71afbc418b028ac880121c2284606fef80085c52
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:20:28 +0000

    formal left deep stand entry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_as_bool(array):
+        return bool(array)
