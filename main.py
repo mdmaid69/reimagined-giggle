1f89bb756f225a354b0cc71ab56da23fd2e1bc1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:07:50 +0000

    eye phone bang help discover

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
