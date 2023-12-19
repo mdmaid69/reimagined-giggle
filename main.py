3399dfa078abcd229b19c946c767c2d12ff903a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:03:16 +0000

    stand scale produce hospital wash

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_pressure(force, area):
-        return force / area
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
