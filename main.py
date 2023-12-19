d5737ebf71519756ce7d6f88ed65d082f59e6834
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:02:23 +0000

    worth undo mix sometimes serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
