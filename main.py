97f0059b26823e84a340e08883c5f43a463e2f51
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:57 +0000

    press curious tie hard patient

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
