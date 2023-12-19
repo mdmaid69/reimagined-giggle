d5e391f430eda23de2b0337dd90e613e2f886645
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:04 +0000

    block notice cost workout soon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
