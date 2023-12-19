68223d6654f76bfe33f3c1c83ec229b5462ec9ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:29:47 +0000

    when deliberately Tt without smile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-def add_numbers(x, y):
-        return x + y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
