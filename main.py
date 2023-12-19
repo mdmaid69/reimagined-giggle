1f4f09f02749427df1e6d0736248371aabe49a20
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:13 +0000

    pupil steady strike pot back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
