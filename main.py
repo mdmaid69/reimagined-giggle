67603a9d5ceb7ad95029838c821cdbb20457bba5
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:09 +0000

    sensitive strong shock drop various

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+print(sum(range(10)))
