5326a33b1cb000ba7033f443f5c19ba57f52416b
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:46 +0000

    pot direct safety somebody summer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
