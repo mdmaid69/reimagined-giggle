17637770316db40b51ab1a418c2d89eb080f964c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:50:28 +0000

    interrupt medium exhibition turn foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
