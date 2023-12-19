aaa8a70d2bbee627ad09069590cd7c0c5f5571f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:48 +0000

    bite medium pull dig burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import array
-def get_array_as_set(array):
-        return set(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
