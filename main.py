59dda0415c784f05b8bbb9ae05dc59e60d1ab7dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:33 +0000

    remember comein run turnaway embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
