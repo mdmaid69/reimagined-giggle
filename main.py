e9c21823f4c8c0ece2d85c7acebb7e40f44cf0a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:16:38 +0000

    why nice test considering neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_speed(distance, time):
+        return distance / time
