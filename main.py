0ef0955cfc604fb8ec4583b1649db6fa4ba50f5b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:40:58 +0000

    government along too exist statement

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def sort_list(lst):
+        return sorted(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
