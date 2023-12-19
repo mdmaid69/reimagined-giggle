83d9e3c1750588b21c276365c3ecb3005a001611
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:15:17 +0000

    convinced surprise precise create otherwise

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
