49c9a4633ff0fc27bffc9c94eaf3ab384661c5c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:28:12 +0000

    environment number makeinto mouth reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import random
+print(random.randint(0, 100))
