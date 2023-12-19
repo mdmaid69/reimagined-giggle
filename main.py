d4be7533de526156053ac51c0d01c6cf08e71ffb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:11:55 +0000

    single will arrange neither surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+name = "Python"
+print("Hello,", name)
