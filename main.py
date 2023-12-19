f4d53a51864244a399a5dbd5e000b87396ba6069
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:23:04 +0000

    obtain clue question situation kick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
