93ba75846a7eb2eb96f1c73f4c7f5b785ba367bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:23:39 +0000

    reliable concerned to pencil goout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
