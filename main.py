49f263acec472a4f91ac78315d4b0cd8b551e02a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:38 +0000

    new ago ride square strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
