b82a8ca4a3f7e7bb7e8c912b16e97cd35f507f41
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:59:50 +0000

    amount interesting uniform ignore so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
