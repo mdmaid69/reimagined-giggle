215734ef6851919083540d37a950cd88d2a08f10
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:04:39 +0000

    wave nothing disappointed swallow correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
