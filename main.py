cd96c89f9e1f4b50ab226a2f9bdfca0c0b716d32
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:55 +0000

    stage poison break edge admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
