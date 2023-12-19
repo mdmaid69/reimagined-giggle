65c99e9078e96f1e81c9820f57cff3c39e87f913
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:53:09 +0000

    have breath some near plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
