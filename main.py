bd2551990e758322a86d9e3fa47a90ca183fa084
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:08:22 +0000

    entrance sleep high bottom heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
