ea48fe4d74d947df30700b14d1c7a852cd9a4687
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:41:29 +0000

    display body frame promise mean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import logging
+def log_message(message):
+        logging.info(message)
+import getpass
+def get_username():
+        return getpass.getuser()
