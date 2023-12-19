e2f64e9e23acb77cd5db36188fbc5d9dbf7affd8
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:51:05 +0000

    avoid city relation noise trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
