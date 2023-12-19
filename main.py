830ee8f55b593e5b0321afa7cea3c132db8e46ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:10:49 +0000

    rubbish lead fresh slow runinto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_min(numbers):
+        return min(numbers)
