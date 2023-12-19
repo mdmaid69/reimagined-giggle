321653a8d67e08b00955695d5ba35c8707227df3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:29 +0000

    past front injury turnaway loan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_density(mass, volume):
+        return mass / volume
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
