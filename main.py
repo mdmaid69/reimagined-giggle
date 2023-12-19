5b871e6f3312c516dc38716109b907cb12c56b3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:14:57 +0000

    cruelty old week arrangement bridge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def add_numbers(x, y):
+        return x + y
+def find_max(lst):
+        return max(lst)
