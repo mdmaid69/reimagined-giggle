28f2f678b6139ad40be84ff3375a4269311d6a86
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:26 +0000

    either thanks shop disturbing correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
