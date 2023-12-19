2c2bff81e697b1f2670dace9c22bb3088a471d76
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:33:06 +0000

    long quality face entrance effort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
