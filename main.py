9e2c73f76710de467e2fd25ab0dd82d6ae287ac3
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:45 +0000

    question slow programme bringup common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
