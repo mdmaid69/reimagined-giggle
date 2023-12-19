eb9afdf120ed839970960552272b3e7cefb28482
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:39:01 +0000

    how lucky shoulder fire regret

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
