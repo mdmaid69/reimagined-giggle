521da5deaa718cf7e78ff5c3add96927992e1a2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:44:25 +0000

    frighten beautiful fear problem prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
