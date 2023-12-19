43aa0e023be378eb12bf24ee0739387b199ef736
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:17 +0000

    building connection wind fine on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
