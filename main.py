93545fea0fbebdbbe15862a0aa18dcca2f36080b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:39:14 +0000

    complete buy closely land gas

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
