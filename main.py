4407a0665c974622857895698f9275bb67758b26
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:41 +0000

    threaten dark promise lookafter gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def add_numbers(a, b):
-        return a + b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
