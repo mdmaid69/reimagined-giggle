77b132a32f0536a7ad6c0972668f4d55c14b34f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:24:42 +0000

    people although even unless very

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
