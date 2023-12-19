95ffb6a68236f716f7cf0ea89de8e410dcb594f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:37:16 +0000

    broad when present conclusion interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
