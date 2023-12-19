16693334ad323ef8e175f70ce62f930301a20c75
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:51:41 +0000

    compare impatient patient yourself hear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def multiply_numbers(x, y):
+        return x * y
