67d911168a3b24e1ff0f610e109c5f0f8e48801e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:45:44 +0000

    talk criticize valuable return recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
