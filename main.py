b62060995f6bd600d440034cdc0d57184bbfdb46
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:59:27 +0000

    ours afraid fast actual convinced

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
