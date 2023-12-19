57a573867e663fd09d4575cfde49f185cd8907d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:50:25 +0000

    round wave regularly joke file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+print(math.pi)
