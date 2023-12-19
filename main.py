9d9c3381f7ed7af606124295f4a659ef0a926a5f
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:11 +0000

    luck success they communication that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_set(array):
+        return set(array)
