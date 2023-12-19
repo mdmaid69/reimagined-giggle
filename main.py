6c87d0cc15eea6f8cc113d2ab18f83c8746b7ea3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:20 +0000

    value callback wing pay empty

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def subtract_numbers(x, y):
+        return x - y
