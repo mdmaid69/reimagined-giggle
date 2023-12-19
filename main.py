58ecbbc964afc97eaf515a73cf067b7b7f72123f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:28:49 +0000

    turn front future father suddenly

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
