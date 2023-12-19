b2a83552849009fb32895ea5584678e906125817
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:55 +0000

    with walk necessary able face

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
