3e8ff864688a44f942e8e81db88d9744334d0165
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:37:57 +0000

    today disagree dress again ill

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
