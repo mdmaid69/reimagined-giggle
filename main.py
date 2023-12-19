ee831fefe051a7fddb3824d78ef14cdafe5117a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:58 +0000

    sing mail just new turninto

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
