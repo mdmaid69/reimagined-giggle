e723e7219313969a04cdf6108bf328ad493acc50
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:51 +0000

    union emphasize please trouble splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
