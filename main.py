5f641efefb8ab1e08503ebef981d198715c0b41e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:25:55 +0000

    light connect afternoon since workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_combinations(n, k):
+        return math.comb(n, k)
