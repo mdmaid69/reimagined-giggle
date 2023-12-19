cdd20b1fa769f52efffd786b9c69f9b793c38763
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:51:42 +0000

    quantity year address field pretend

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
