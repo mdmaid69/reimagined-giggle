64fb5933f3f8a1c232f52f5fd1a4b0e5e953ae65
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:47:00 +0000

    story stranger earn die answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
