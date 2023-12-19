07f6a5a8191cc1ac155e049b4f4884d5bba544d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:37:28 +0000

    discovery nose exaggerate bang supply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
