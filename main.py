cb9e2578ee745b7f268fceeda52d502b56184ef2
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:55:18 +0000

    national difficult disturb read ready

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def sort_numbers(numbers):
+        return sorted(numbers)
+print(sum(range(10)))
