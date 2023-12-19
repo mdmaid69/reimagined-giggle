41dcf708d791bd03bd22f22fc0c743020d8bd595
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:54 +0000

    choose getout account system land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_min(lst):
+        return min(lst)
