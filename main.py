f110205966d77ceedd5c3b11a87142c8c1feb7a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:07 +0000

    set smooth tool charge freedom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
