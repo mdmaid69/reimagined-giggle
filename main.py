e93c2b3cc6d10e5e1eb338dcca60ef31995c750e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:07:17 +0000

    switch refuse bringback boring present

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
