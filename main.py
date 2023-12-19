d195d45a9f7b3f4df4e15d96358732e0b2420a60
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:45:21 +0000

    approval stupid indeed back rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
