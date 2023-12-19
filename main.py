11ff05f1d7e09c340e2287bc813c8eff4d0a49c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:43 +0000

    onto cruel worth stand appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
