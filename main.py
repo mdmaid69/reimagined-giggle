044625fdb65776496525177bdbb6da4fd28df4cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:18:11 +0000

    trick brown used sting violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
