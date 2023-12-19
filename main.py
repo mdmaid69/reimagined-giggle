4b93ba4254ad8c05e2f28b1ea65dbf5477a0a879
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:05:02 +0000

    lunch affect classroom agree afraid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
