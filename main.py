68582defe3eb8ab0a1c54648377b9cf0e1632f51
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:56:10 +0000

    past attitude scare pen giveaway

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
