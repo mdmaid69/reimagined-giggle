a7f14add60255ccf3cbe57a1c628a19595439d8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:56:18 +0000

    manage justice balance talk finish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
