0428c5d6ca953055e279d731d8765cd22fad1b16
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:51:11 +0000

    shall last meanwhile hope traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
