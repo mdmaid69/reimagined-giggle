114d16c84401eb44d0f8c9579590729470e41278
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:54:21 +0000

    church live convince thin knowledge

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
