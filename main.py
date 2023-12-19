37f4e25dd2a430c7f142ed06a56f45656c96d7a2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:04:57 +0000

    wide spill government Hh over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
