f96755655e4dbe2f4d553e3afa40dd6a52442e46
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:06:28 +0000

    wave minor pretty Cc teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-def get_current_time():
-        return time.time()
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
