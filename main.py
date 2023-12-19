becc4bb33fbbc06d0494d91e03b05b55a170cec8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:19 +0000

    help result short value scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
