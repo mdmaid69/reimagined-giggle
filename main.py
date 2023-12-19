9a2e4d5cda6c74332b6256c3c061ee480078f8b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:24 +0000

    push experiment authority first situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
