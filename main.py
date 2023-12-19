653e13ca3c0607a5a15505008df17ad025215f1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:24 +0000

    drink comeback why by fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Characters:", len(text))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
