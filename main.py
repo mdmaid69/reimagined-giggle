36f5a087581dc1671e260b25ab8d0f818029d3fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:44:52 +0000

    pride centre sky complete blame

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
