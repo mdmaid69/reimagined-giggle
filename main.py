2ba95bb082186d312e6c2481e3b31cd8d3f3b012
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:57 +0000

    its favour off turndown new

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def sort_list(lst):
+        return sorted(lst)
