1894e453d3b67fdf1c34d8a07dbc67f11fbca76a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:37 +0000

    become impressed comeout dear there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
