9a3bb5cf9e3c2f01c399d2158fd8e6ecbcc722ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:50 +0000

    book taste part child exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
