28be58976cf7cb89b3acf41b260626911fb91822
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:01:15 +0000

    equal bus machine tread actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
