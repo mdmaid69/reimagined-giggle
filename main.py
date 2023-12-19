d0f562a15f8a4bebfbbfc56b1d9737b7dbe9bd7e
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:30 +0000

    shall impression sensible confident front

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
