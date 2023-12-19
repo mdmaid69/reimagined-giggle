d79c0a169c8e6c50eb44ee488ef644174134adc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:32:25 +0000

    discovery along blame anywayanyhow impatient

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import time
+print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
