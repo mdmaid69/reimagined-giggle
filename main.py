2901eb5407d42b4d1671efce1ed336871f3b0949
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:10 +0000

    identify raw commit accordingto favour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
