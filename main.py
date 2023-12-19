5f6a76043b54488a31b3feb914f4d4fef2919815
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:49 +0000

    shopping old nor black introduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import sys
+def print_python_version():
+        return sys.version
