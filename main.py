da218f2e12b5dfc560c6731705602e461d42a652
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:50:38 +0000

    emphasis belong apparently protest nice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
