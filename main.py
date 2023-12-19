c6e82f07ac63e1efab5287f986f4c88ef2a9c3aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:11 +0000

    partner bottom skill afternoon female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
