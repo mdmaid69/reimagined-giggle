d5109ea13890fc24e29453643d634e2cb2b8e0ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:41:59 +0000

    left score fit bill flood

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
