a84aee5b8030db2c332496379b2c5cf0c488f451
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:42 +0000

    cold shut combination loan destroy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
