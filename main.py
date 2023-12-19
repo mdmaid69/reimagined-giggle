8b467e5d4d99227d96e3889e5c590e9a6d386c06
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:32 +0000

    guess frame reduce relative fork

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
