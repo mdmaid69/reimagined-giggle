a81f929c368b5d9ff45038486390decca8599193
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:40:14 +0000

    introduce centre and vision behaviour

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def divide_numbers(x, y):
+        return x / y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
