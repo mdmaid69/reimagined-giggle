33a0dba3ecf9b391511e7025c875cf22c54951c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:41 +0000

    children difficulty yourself over cut

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  def reverse_list(lst):
+        return lst[::-1]
