95ca3f687085eb5204986a47ddf9037b7797e27a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:45 +0000

    drive radio balance free order

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
