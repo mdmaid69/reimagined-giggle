b640324bb6ecb0c6463d6b340b7f7e548a791530
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:52:45 +0000

    warn slight people challenge close

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
