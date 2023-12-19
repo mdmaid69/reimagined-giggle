182704025668c9b22a9395d2d01a7f9844607f84
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:07 +0000

    bin nigga careless male mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
