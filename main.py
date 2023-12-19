8256010b8e6bba629248955e87f95103e42e8d51
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:10 +0000

    Yy look school achieve wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
