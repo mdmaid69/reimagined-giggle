74314c6e6b0f814921a6bda4b5cff3a51699e5cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:54:25 +0000

    true division better roof secret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
