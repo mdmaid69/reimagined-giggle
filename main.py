6fbdf26656c52f3a4d637b93921060cf76f6c26c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:36 +0000

    grateful uniform however measure past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
