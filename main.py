41392308521a4fcac7beb154fb3dc5883f003e15
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:05 +0000

    need goodbye talk climb pair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def multiply_numbers(x, y):
+        return x * y
