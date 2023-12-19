84e25f742b0d5eca8f4cce8a700f9acc091cf56e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:35 +0000

    trip give urgent peace answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import random
+print(random.randint(0, 100))
