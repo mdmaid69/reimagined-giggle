68a5ecc9f0c574a1314f9bb1244b73399afd5515
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:10 +0000

    meal funny continue out especially

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_min(lst):
-        return min(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
