d969b0cc08d06b087f0a2c9756f57a702c054c41
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:23:07 +0000

    influence through introduction switch invite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_str(array):
+        return str(array)
