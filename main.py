91da6eb6c814f5aff6c9c9c2d7b9bbc637fe4867
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:38 +0000

    influence what thought convince offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
