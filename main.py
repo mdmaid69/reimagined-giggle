a48b9c5580d0413846023711cbe3fde716d7dc2d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:49:06 +0000

    shadow middle certainly she name

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
