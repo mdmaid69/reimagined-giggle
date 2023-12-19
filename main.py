cc05f7cef267d9310d8ba075cbf12733d6cf6b72
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:32:27 +0000

    report label command private couple

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
