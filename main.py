70a2a6d23e678b58b8e738ba572670566107ff1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:07:29 +0000

    here bringup stir Uu smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
