b502b8b771cd4646fa849cbbb5b72e83f55a77de
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:57:05 +0000

    concerning drive black upwards advice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def delete_file(file_name):
+        os.remove(file_name)
