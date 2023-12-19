bc1cc0151da2094bd60c92d7758df6f21f697c09
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:06:54 +0000

    creature switch lookup name mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def cube_number(x):
+        return x**3
+import array
+def get_array_as_repr(array):
+        return repr(array)
