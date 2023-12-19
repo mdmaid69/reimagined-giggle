f4f2ea9a9baca602791e3a307edd170c9f48a2d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:00 +0000

    and account satisfied pick regular

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
