8ccd1abb14f2b4334865bf4c3a527511e82d3552
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:36:32 +0000

    opportunity tradition absolutely limit cap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-for i in range(10): print(i)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_index(array, item):
+        return array.index(item)
