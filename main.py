03ef0fd5e4e29fa60de4ccc02a59fdfa73ea4e2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:09 +0000

    judge beginning emotion 568energy practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_number_of_links(file_name):
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
