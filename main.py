09b3c6a0ab7aea30dcbbb3adf719524526228a8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:17:26 +0000

    over be total increase project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
