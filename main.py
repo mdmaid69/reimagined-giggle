d4a7d5759307eb1df4db969d3247b646bc26bbf8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:56 +0000

    lookup platform calm fast present

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
