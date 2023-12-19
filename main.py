43224c633e91cc25e0cec4baa8b03e682e9600a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:46 +0000

    excuse still spot escape turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_max(lst):
+        return max(lst)
+  def square_number(x):
+        return x**2
