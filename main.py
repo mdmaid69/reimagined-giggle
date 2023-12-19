a99c95bd58a6acf713bcfdb101de238a9cf23031
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:00 +0000

    fail number report dick shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
