537d8453c07d5f13126511f1ed7692a69aa478fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:36 +0000

    sleep workout mean answer community

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
