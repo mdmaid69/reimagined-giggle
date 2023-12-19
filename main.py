1bebaf1450148cccfc1a41ccbd2562f8fa115590
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:13:31 +0000

    none fire ticket rest time

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
