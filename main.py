1e47e9960e23ffec5da116c3b079dedb0488e002
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:41:59 +0000

    determined open strong cope do

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+print("Hello, world!")
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
