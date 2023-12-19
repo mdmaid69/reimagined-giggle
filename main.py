a2dfa73310007d6d51f9ad2335d29168b7497383
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:13:11 +0000

    closely spite search produce anywhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
