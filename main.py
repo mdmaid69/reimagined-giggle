8951e0b70900129e5c8d36b1a14c005f880d6a92
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:16:40 +0000

    light make basically gooff meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
