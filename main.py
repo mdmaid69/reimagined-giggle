bcf45cb3dd1787ad281a922638f45bbf3ed1f9ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:15:47 +0000

    correct pretend between war up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+print(math.pi)
+def calculate_work(force, distance):
+        return force * distance
