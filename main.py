10d1a3b11ab59d5b2d6b5be199d8786484f47765
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:39:12 +0000

    check school in agree wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
