532a9d00f7f1410f9b50a6cda5d4d8f881013c96
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:05:45 +0000

    middle adjective basically pipe draw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
