7a71b5b93fc65420f6ad7f6bd79e964affaac5ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:54 +0000

    general police control contents detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
