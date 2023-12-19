dbaa3f428cb44fd9a99e77f684707882fd7c440e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:25:54 +0000

    treat bread giveback quick voice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
