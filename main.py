95a627936aed121540958ebdb24230481deaf84c
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:21:11 +0000

    minor runover definite scrap than

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
