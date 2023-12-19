eff296feb4bb16281ca059e9881da5758436248e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:04:25 +0000

    bend aim wind broken polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
