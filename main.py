1c8a1a546bb36a847ddaef481dfc13f3fa433876
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:56:49 +0000

    however succeed check professional opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
