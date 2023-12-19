8ea05a861dd6060a5eb84e66d190f86351e3dc95
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:32:05 +0000

    address action respect my lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
