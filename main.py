6debbe906885f0b5c3d8524317d13bd6eaac69b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:12 +0000

    rest produce really because mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
