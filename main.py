4f049751c20827ccd808da8e6ba380bdaa31a3b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:37:30 +0000

    cause Tt likely extraordinary lesson

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_min(numbers):
+        return min(numbers)
