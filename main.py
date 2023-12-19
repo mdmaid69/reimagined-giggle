65966f2e9ae80dbd5e9087f36c7b51ff47cc6eb4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:47:47 +0000

    influence expression disappear hospital shallow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
