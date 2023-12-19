f48ea31fdeb96bfa65a4e4640626f32028edfbe1
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:00:44 +0000

    legal include adjective team transparent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
