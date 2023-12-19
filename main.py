0f97efdd966bafd94ea009f8575af578f45bd53c
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:57:02 +0000

    skill deaf often 571enough broad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
