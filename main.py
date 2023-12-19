788a33010d50b26f15de966e1aa914b435930c52
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:33:19 +0000

    practical estimate hard nor follow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-print(math.pi)
-name = "Python"
-print("Hello,", name)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
