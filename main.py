c50300e3b62637f80aceb15cd736a9f7b63c1aa7
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:06:49 +0000

    hurry sound long equal across

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import datetime
+def get_today_date():
+        return datetime.date.today()
