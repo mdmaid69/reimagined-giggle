986c2febc48dd9f023b70f9d38f33df23c27930a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:59:54 +0000

    born break small represent dream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
