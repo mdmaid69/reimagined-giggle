f4c3afd679e051ea8171c8541584d77047e4704a
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:50:53 +0000

    closely typical display level satisfied

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
