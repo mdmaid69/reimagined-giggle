f89754f6bf15c6490b287ec755736a3364fb4d81
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:15:30 +0000

    description settle along old below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
