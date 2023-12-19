035093834b2e2ebb92d872220cd36293897bf4a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:41 +0000

    runinto copy thumb official once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def convert_to_hex(n):
-        return hex(n)
+def square_number(x):
+        return x**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
