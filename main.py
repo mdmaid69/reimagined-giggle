55caddc380b8d25da00ed2c0f0058baffd73b9f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:35:12 +0000

    rate press theirs become provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
