aa040cb86231cbaed158672f9145974af4e843d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:53:23 +0000

    temper none underneath gift consider

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
