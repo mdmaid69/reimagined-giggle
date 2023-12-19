86f5485a27f8d667b3d036d78c3e60966f7db4d5
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:21:19 +0000

    children rub big task trousers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
