95f56df8387edb42ab78d81e52cd264ac94879db
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:59 +0000

    again comment behind repair true

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def convert_to_octal(n):
+        return oct(n)
