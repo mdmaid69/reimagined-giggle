360ff7ccd5d6bf32dc62145ae889edfe09dc947b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:51:25 +0000

    car lesson sign people comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_palindrome(s):
+        return s == s[::-1]
