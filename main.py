018b3a45e4437d5b769a7ace7d8933cbf0abe470
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:04:32 +0000

    impressive tap stranger stamp Bb

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(lst):
-        return min(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
