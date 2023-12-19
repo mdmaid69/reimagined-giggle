0fdc2817c890ac04c94e6833dd26c3cc0b166643
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:04 +0000

    yesterday danger worry prevent mixture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
