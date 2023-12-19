f44f65f4c4f2ac2507122fcc07f4c1ac59259f07
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:37:07 +0000

    direct until task pupil its

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
