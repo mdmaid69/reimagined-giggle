57c927488133c774e0d0314e1a9a6587e44e2b97
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:38:08 +0000

    soak mouth fire concerning soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def find_min(lst):
+        return min(lst)
