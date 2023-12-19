a2a9736372104b5d0e2e40a9c7fc46f62ac37ff2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:25 +0000

    minor bottom taste bread apply

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
