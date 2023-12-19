a1fb35ec64a584ff0f4321e48d0b2e12eac4d1d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:37 +0000

    share keepoff completely impatient advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
