5911e62b5bf839af2782b4b40e60fbe688d1ba7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:08 +0000

    interview ring sting company our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_string_from_array(array):
+        return array.tobytes()
