455a843b36374c26e3af2ae19d276c2d9d6f3f7c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:19 +0000

    arrangement form spread beginning judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
