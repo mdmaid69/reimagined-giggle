8843325c4b21b6fa74ff692d1d7197d84601bd77
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:06:24 +0000

    offend protect ago fun practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
