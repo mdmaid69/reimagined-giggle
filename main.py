e70f4d8e5d6c84d59bd21ccf32195f8053c43e05
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:42:04 +0000

    effort die beside obvious serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
