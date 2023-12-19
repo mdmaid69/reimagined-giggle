88a43abce586a3b55fa599a0164c33e3a7094d50
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:10:16 +0000

    matter gooff fit this image

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
