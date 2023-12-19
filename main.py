dc5fc39f8645d814b57933d247b9efe00990526f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:59:25 +0000

    good hurry sign loan performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_power(work, time):
-        return work / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
