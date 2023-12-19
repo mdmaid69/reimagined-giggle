2fb990092d6f48bf3084e3c6dd36ce981ec07207
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:55 +0000

    impressed because takeoff spring move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
