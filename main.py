0179289a118baf0e44a3054829252b94cbf1f8f7
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:56:12 +0000

    anywhere grow success load hold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
