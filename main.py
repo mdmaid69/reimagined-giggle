9233da4c8bf1ddbdfb6d631cdd3b943424b713fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:05:20 +0000

    teach limit one meanwhile home

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
