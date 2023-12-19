d549a546b1abc49c0ad876152a6141596d344639
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:20:13 +0000

    mess mail right apologizeapologise drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import re
+print(re.match("h.*o", "hello world"))
