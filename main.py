3031a264729275730d6c1911d75a28938595c716
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:57 +0000

    continue odd 565end priority touch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_str(array):
+        return str(array)
