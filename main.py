6df2b651b5314b165506d884622e2cbcd935c6b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:54:23 +0000

    listen first join otherwise wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
