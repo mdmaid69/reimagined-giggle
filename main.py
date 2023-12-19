19e2870284b213a789d710930e550d02cec926d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:37:50 +0000

    tip shape pretend best since

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print([x**2 for x in range(10)])
