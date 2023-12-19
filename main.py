8853d3d57e343689ad3762851959361ce67660a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:16 +0000

    variety idea test ready past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
