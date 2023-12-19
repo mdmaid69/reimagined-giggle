b782f995078df79df378bcddf3e4328a23d34bae
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:30:40 +0000

    watch border disgusting happen separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
