5b3b1824d8dbbf190c4ef9f716ea0ed08b609c5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:26:18 +0000

    switch divide tip meet marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def square_number(x):
-        return x**2
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
