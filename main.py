82e6443406ac330f6fb6a11dacce67653360a89d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:25:35 +0000

    earth mean pencil off priority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
