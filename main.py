fb460d824ee09e98f295dabe6c0a2cbab5b42e34
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:21 +0000

    wall activity calloff recognize fast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
