9c3563a27fc7816d1e644f78d28c445872e7dbb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:59:17 +0000

    original fork university after wing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def convert_to_binary(n):
+        return bin(n)
