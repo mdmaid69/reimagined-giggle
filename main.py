4a79944e7c846238bb08e457d6a4e94f4e2a4cf0
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:31:44 +0000

    safety every beneath improve march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
