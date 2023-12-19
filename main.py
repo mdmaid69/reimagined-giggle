315cc67d14f3d2f7401af38e545f2f920c59b9bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:40:04 +0000

    finally small occasional last breakthrough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_max(lst):
+        return max(lst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
