f946ff7b35c1603707d5ebf31ca2b33c8c4de427
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:32 +0000

    keepoff accommodation fair best talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def convert_to_octal(n):
+        return oct(n)
