8bf803270c5fa83db922fa6c97d8abe5a5032e60
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:17:28 +0000

    joke smile us uponon 568energy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_str(array):
+        return str(array)
