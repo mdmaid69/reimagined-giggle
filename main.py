20b250dc4f4197176aae2410fac1c9f27ef999ef
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:53:55 +0000

    confidence exchange smile ask day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
