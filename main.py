c05af0d80aeafc04cc7cca80be970d11ff9d95b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:11:23 +0000

    gradually identify ball sing height

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def convert_to_binary(n):
+        return bin(n)
