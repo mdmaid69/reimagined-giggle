38c0f2c8d39839ac7c571bcd2c6a4c0b69a43718
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:16:28 +0000

    uncertain turn warn attempt reasonable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
