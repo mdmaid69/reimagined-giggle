963ca932e7a255fb9184faf8470881af500b84cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:35:12 +0000

    desperate honest mainly soon breakin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def convert_to_hex(n):
-        return hex(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
