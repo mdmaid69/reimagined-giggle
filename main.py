bd25bd0d962a015d26a13dc54d5e563b28c71ce9
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:22 +0000

    something love interest use hang

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def sort_list(lst):
+        return sorted(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
