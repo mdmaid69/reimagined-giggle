cf7b73c57ce528abfc5aebbe8fd03237358c4f8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:05 +0000

    service may reaction deal check

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_area_rectangle(l, w):
+        return l * w
