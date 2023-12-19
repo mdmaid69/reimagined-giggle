681d0268a3087ae33fd60f9535e44d28399c91b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:58:54 +0000

    support lock noisy lay preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
