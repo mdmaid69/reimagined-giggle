0afa41b201a0b4432217b7609a17b976dc1db2a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:26:40 +0000

    commit children convincing board impressed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
