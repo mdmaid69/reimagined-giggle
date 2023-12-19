c8fc5f658f2056e0e94e547d424a98e56bdfe867
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:57 +0000

    generally make comefrom tonight disgusting

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
