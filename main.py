713b2d512c94ee66fc72aae298c8f5169a011c5d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:13 +0000

    interview rich artificial call shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
