b693d4b2cc930fe615cf7b5b3af668ca25bf247f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:51 +0000

    kick exit geton scare instrument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
