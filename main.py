d6b051c91c2ca7992a8342946160b4bfed8dd532
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:04:44 +0000

    rely that substance spend shop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
