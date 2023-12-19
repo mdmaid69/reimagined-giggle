20dddee1a882c6878d719e8798ba42cc87bfba00
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:58:43 +0000

    case join sweet ought mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
