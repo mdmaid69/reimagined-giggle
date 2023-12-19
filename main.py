4c8d9a3117ec09be7a440d5a1b520bdb2f3f5d09
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:49:51 +0000

    her build room second breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
