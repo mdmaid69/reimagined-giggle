694a7beb0cce2010e3e01e4f2c9a27231572c9f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:17:05 +0000

    tooth press warn plant honest

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
