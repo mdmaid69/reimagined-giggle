b88f61bb22a30f6462b877430018a4a496d1524d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:32 +0000

    important mark open public back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def square_number(x):
-        return x**2
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
