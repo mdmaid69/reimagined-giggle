a2a207a510d6777d498926d2e31b069d91646d4d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:53:57 +0000

    protest skill alone water arm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_pressure(force, area):
+        return force / area
