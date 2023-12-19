3c05edd46f8f3ccea0cdfad3d05d69f7c771eb30
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:40:06 +0000

    mean price brother half worry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
