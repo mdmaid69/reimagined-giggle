4d131fb288e3ea9217e5a82bfdb0fb835287311e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:23:19 +0000

    object receive programme please into

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_power(work, time):
+        return work / time
