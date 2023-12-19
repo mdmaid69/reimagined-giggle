e1a8ca5ed57205a3c7d83fb8181049514bb11a61
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:52:23 +0000

    beginning sharp keepout near concentrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
