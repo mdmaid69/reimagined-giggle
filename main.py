72eaaef1d82eae02cb6bedb09e61773a2f531b75
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:56:58 +0000

    week song bother bush each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import time
+def get_current_time():
+        return time.ctime()
