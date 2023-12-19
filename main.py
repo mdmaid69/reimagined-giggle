f31803326e4c4a77cec73ef860e3513272c5bce3
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:13 +0000

    forward but cover measure connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
