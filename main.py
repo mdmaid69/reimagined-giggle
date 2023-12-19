32c5cc515c570b5caec571682ddb27f1e6489511
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:14:13 +0000

    collection runaway alive pen extraordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
