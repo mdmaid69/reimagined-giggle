16624305793122eae08e6093abfd88924b0dfc56
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:08:32 +0000

    means recommend shade always possibly

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
