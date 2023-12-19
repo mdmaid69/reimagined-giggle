8cbcb062c65aef63e33513398bc22f03ed035e49
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:46 +0000

    its long behind quality permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
