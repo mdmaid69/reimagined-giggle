404ef6995b56222a042ecf0105cc358b74e61284
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:31:26 +0000

    few estimate emphasize you dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
