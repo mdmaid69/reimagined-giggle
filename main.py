d399a2dbc3f8bfca5dd2250f27e28b9912f1b40d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:03:53 +0000

    scene state nose please ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
