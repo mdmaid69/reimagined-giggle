269a7ffca0bf40051180785376cfbdf7664a5ca4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:11:49 +0000

    turnback company guess tough race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+print(math.pi)
