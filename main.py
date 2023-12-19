b144f785981820321095753c726eb289dbdffef2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:01:37 +0000

    ought determined spring truth patient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
