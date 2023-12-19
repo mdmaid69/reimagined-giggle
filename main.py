208fb38956cc8c97ac743056da0b3cc9bf9b642f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:22:01 +0000

    thirsty war later loss whoever

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
