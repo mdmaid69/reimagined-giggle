cc98884b008489868992b4899cee79526bafb5aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:34 +0000

    unpleasant hair over fire awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
