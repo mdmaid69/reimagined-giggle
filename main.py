45e2a625a35fe7129c2953ec277a4faa02372519
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:12:48 +0000

    term against stop interview theirs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
