5803665d20273bfae96102297ab50bb44057f599
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:04:32 +0000

    start meeting crash interest friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-print(random.randint(0, 100))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
