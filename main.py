38e9b0db020e6edee172dbd3f6a5c888be0cd34f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:42:11 +0000

    receive basic return dish float

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
