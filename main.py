cb0711d10eeb72e33cf7251c00dffda606795804
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:24 +0000

    matter someone date man breath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
