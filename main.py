c0186db7c7441eb26b176ca2acb737efafb17e49
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:16:28 +0000

    regular act experiment paper trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
