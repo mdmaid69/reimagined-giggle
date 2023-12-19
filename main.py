38241b3ff901e7b2e3de3a0148d984e1c5825f33
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:45:29 +0000

    silent getback used base pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
