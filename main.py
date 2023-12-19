8384185ef37d39a5f466e81bb2a2a6b45e88b5a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:20 +0000

    mood judge rain weather noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
