963d2875994704c8def1ad50162364f882881cb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:42:24 +0000

    enthusiastic quit supply reliable now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
