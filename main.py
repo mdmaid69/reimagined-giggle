c984e6eaae7e724ed41363434349b0cd90ecae7f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:10 +0000

    than however split free transparent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def subtract_numbers(x, y):
+        return x - y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
