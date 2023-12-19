6c9d15866716b6e2f136bbacf228662795e82701
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:15:09 +0000

    finish clue nothing turndown keepout

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
