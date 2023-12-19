f532b94763aa358147486a55516d39244442ac65
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:01:02 +0000

    far during schedule slide open

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def count_elements(lst):
-        return len(lst)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import collections
+def create_user_list():
+        return collections.UserList()
