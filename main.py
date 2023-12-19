ed161e6e056a33981e68581fe1b5c821a58ce5c3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:21 +0000

    perfect happen reply involve description

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
