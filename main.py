9894a50ef6af7de2c57bcc0be6a515247f3e5210
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:50:23 +0000

    join short sentence lesson before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
