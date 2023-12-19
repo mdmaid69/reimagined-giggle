8edf1addbd47ea877da2eaaef6f4e25c3ec328ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:30:16 +0000

    sense firm murder home however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_str(array):
+        return str(array)
