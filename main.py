919274acc76c6f1433de1b0a40e8c1dca9fea4be
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:24:54 +0000

    wind study prepare treat pair

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
