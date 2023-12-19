612f4d98b00f25abc394369da5459360226c70b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:48:06 +0000

    murder fill crazy cook tread

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
