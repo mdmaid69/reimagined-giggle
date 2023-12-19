ad5da84ad2802211fba8c8b43e38f32f02d2b2a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:48:22 +0000

    related material discuss throughout person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def add_numbers(a, b):
+        return a + b
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
