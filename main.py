80e4832308da2b37719b8aa380ef79a6d5db2aff
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:19:02 +0000

    recover programme anywayanyhow goaway new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import datetime
+def get_today_date():
+        return datetime.date.today()
