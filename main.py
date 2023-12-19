714869fa3bb2abf080dc5a405fee852dae3e55d4
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:23 +0000

    coat plenty trap brush usually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
