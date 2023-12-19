7d8456807537f1df73b1aee4faabbe4053823b1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:50:17 +0000

    mess clean extremely rain comeoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
