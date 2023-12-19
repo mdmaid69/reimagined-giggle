12a05b4a734aebb3562a1604851ce547fa8d813b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:26:30 +0000

    accordingto past big judge pure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
