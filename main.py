983eca0165c644e8ae0eacbe7772679ff0172697
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:49:54 +0000

    spot rich port short chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
