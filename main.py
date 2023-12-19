64a22d7f3e4ec03ef3aa925a5060d16a1f9d89e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:22:10 +0000

    fail beat doctor change dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
