f806d3051fb86b2e93e027cf25eb7f29faca443f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:43:10 +0000

    watch stranger rest involve sleep

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
