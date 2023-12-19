53a7cc3a15f75a6251c8753f40eb00732da71bf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:59:11 +0000

    pupil trial match circumstance herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
