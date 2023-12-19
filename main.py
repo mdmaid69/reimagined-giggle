67177941ecff1c5e34fa0d9e2dab473f08981478
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:55 +0000

    satisfied catch motion manner accept

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+print([x**2 for x in range(10)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
