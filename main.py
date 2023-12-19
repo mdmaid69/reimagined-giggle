aead96108371881e5f4c34400f286f0ee1c5123b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:11:52 +0000

    mind minor war pretty whatever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-name = "Python"
-print("Hello,", name)
+import sys
+print(sys.version)
+import os
+def change_working_directory(path):
+        os.chdir(path)
