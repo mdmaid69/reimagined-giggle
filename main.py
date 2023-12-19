b5c912c68afea3bd6981bd20a30c255fba857264
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:36 +0000

    object company Ee position sky

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
