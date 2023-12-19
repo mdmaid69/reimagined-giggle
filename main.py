6f70035fdb73e1bae71f52368be6ff1790100c9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:22:45 +0000

    left walk reply workon careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
