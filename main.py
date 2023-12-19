1218e712326fddfe6fd2a34f6ca84fad7bbe244b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:36:26 +0000

    another whenever sight ear rule

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
