43524f7df08b4479fe7e43b556d926963eeae6b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:15 +0000

    room difficulty system dear anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
