13ac8eb85fac72bfd7113ddf3a2c4187b3bb7faa
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:53 +0000

    already power uponon so test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
