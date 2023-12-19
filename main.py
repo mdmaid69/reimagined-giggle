d9ccdaaafeffe8c0d67a46118bddc76d94dccc01
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:15:17 +0000

    however proud through authority obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def reverse_string(s):
+        return s[::-1]
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
