4c6e483167bb8b3de2a269d566fb34d8b1c801ff
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:20:17 +0000

    copy warning slowly patient lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
