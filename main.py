b5d6ebad8a2eff98da096cc84b3613431e8996a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:20 +0000

    freeze everyone second grateful left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def is_odd(n):
-        return n % 2 != 0
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
