fd17dcdc7ef4fe4e218543d78adea41de3192881
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:27:04 +0000

    habit illness best now fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
