0eeb43dd5e5503631221752162a75ef066c57302
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:16 +0000

    hollow by call modern abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def is_odd(n):
+        return n % 2 != 0
