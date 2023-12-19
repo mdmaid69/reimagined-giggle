3c774ee1397447540d06b80781691cffe1d93fa0
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:30:10 +0000

    high temper Zz discussion direction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def find_max(lst):
+        return max(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
