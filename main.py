106efe9545b5a0cd12b5e9d73fea92715277519a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:13:01 +0000

    equal memory hear health wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
