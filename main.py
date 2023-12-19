66ff091eac6d4a6609e63eb445bd97e2af61cb49
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:53:40 +0000

    chin sometimes same value comfort

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_str(array):
+        return str(array)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
