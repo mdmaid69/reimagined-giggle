f371d442da4f3943aecff332ba6810f0c4bf4f99
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:38:48 +0000

    hollow ahead calm pick port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
