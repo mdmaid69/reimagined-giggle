bea388e6ed214343f906705cc9229088f647b219
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:54:50 +0000

    ashamed wander consist boil yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import sys
+print(sys.version)
