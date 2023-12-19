f90f62f91eb5276830ac7c8e35131e485c30fc83
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:13:04 +0000

    factory rough apologizeapologise hard could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
