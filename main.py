5a2f4e62b422e9496d1181edb3af4999961cd2f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:23:24 +0000

    bath score mix tie wild

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
