63856a6180848d53b7acd3c72c526e4c2e59b1de
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:09 +0000

    thirsty giveback judge type slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
