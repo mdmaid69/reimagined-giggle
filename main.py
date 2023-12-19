97bca1436887d97dfc39b01cc0af47b5904b2b81
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:52:23 +0000

    air delight myself discussion know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
