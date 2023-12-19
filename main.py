04fc576872fdd4dfd0349eddc41edd734504654b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:49:44 +0000

    reach yours moon female hungry

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
