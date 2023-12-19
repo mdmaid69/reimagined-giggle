38f4851b3a2ad85e830834ea46475aa0687c5a2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:11:33 +0000

    race pupil passage suggestion walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import tensorflow as tf
+print(tf.__version__)
