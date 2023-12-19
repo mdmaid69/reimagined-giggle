825fe50291d1cbe6463581524be704e3d22d8901
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:06:33 +0000

    ago straight whole cause dig

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
