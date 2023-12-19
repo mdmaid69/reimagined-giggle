8742a1fb90e5642cdf1c7132c2caa7f9f4cd241b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:13 +0000

    pair light exhibition finish constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
