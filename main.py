dedc62ff1bd49d3aef2bd17564f5f671fb05f1eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:09:50 +0000

    peace late dry zero government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
