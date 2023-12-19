cd60fe5e61a13496fdc64021371bc6f358200016
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:28 +0000

    burn respect that bet disappointed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-print([x**2 for x in range(10)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
