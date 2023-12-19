209bfe434c395487d7861ecb90d9eaa7e61086eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:28:21 +0000

    front statement impressive black hear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_odd(n):
+        return n % 2 != 0
