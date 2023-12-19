16f7d44a5b54f36acb08b61f996d52bc7436db43
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:58:46 +0000

    trust colour can quality part

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
