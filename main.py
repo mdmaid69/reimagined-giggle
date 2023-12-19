ffb04c96d2e343013353f0fe8ab59170f11230b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:38:10 +0000

    snow winter pollution hole agree

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
