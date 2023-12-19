4fd1e6f08fcbde015bfcde9cb531b6925909c1f4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:20 +0000

    apparently there 569enjoy brush curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
