72d2ec9d6feb5d76cf4bde7609fb5b56cbd7864f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:36 +0000

    squash vote treat stretch smart

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def count_elements(lst):
+        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
