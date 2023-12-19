5b55719f0d89bb1601e833d7ad2c7468fd12fb83
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:50:58 +0000

    train outside rest acceptable tool

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
