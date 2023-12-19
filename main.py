28d46672cc09ca57c1b565cb68ce74a0d9678b9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:58:00 +0000

    interview weather but share creature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print(sum(range(10)))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
