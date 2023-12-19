cce5dab428d69d318874b8dc8d01ca330873d7e0
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:42:15 +0000

    everything argument change perform practice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
