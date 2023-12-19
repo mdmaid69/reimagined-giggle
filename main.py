c8f616ac85bb682bcd2dedf2d34d053d483d6ad6
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:59:17 +0000

    breath on sure else appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
