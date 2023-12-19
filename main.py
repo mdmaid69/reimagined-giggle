d1c9f8c5b3c9ec9fa14fbacde140b05c3b73be83
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:02 +0000

    protest image news forget comment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
