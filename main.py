a0eee295a6f9251cf35eff9701901fe579eefcba
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:34 +0000

    dig bye cry such payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
