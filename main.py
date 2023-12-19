da653f5a30349f242b95b3dfc99938988357613a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:15:26 +0000

    least innocent flat joke goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
