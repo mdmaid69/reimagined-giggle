c7aa8aa1b5de97b1f765a1ac7b7e04393d81bdd5
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:02:29 +0000

    shopping during combination sore character

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
