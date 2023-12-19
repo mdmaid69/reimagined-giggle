c5cbc85ac2d2310b2fb49b30f1be974c7fad7af5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:46:11 +0000

    will really any Nn turn

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import re
+print(re.match("h.*o", "hello world"))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
