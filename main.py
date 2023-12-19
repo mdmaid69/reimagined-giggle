7e9d60d8ba9314088f3ca3ddd5e7fe5e76c5b8c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:48 +0000

    brush Zz Ss turndown any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
