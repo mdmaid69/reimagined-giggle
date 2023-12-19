5b6da22faad5a8f15811bbd454466f706543c569
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:03:54 +0000

    afford turnaway him concerned study

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
