4b0e2670770cd5f6ad21b48636dcd2d5e9dd93f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:44 +0000

    promise herself produce summer promise

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
