d4855d77ed88301641fa7f350038ffa094438670
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:39:43 +0000

    boil discovery pocket land ability

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
