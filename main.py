cc330f1cfa920cd494af2efe23020b969893af45
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:52:36 +0000

    hollow dick minimum sleep regional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
