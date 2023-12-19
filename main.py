0730f1f6964aff6364661678f4912b94b63588f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:27:32 +0000

    whether along needle characteristic connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
