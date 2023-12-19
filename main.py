68525dd22e362cb0f2c91c905d7aa3145b7e8540
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:43 +0000

    breath famous protest judge accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
