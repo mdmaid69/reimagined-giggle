1b4982af3cc61566350117b08d7d4394d0b7823c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:26:27 +0000

    lose sort confuse platform later

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
