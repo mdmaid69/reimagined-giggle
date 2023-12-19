1e86980f539b2484c5736741e38045c579c4a9a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:44:41 +0000

    hole spray think free apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
