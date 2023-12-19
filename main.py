0a50dcf895eb02b05549f4e6c7ff04cbef6f8f30
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:19:43 +0000

    defeat giveup plate drawing voice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
