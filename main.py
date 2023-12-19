576805d05cd87e98b09da90531315489a69d2373
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:42:29 +0000

    return argue best content ago

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-for i in range(10): print(i)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
