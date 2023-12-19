a5fc75110c68cd571ae6ea8ab95b318500e1f7d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:41:10 +0000

    everything sure splash wash breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
