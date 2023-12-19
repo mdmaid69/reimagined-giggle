26f988b865d751890a75fa0c82371ce5f32bf948
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:24:07 +0000

    along pause young port snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
