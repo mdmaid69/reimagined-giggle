107802db83c782fa78a322b69bb9dbabe8a2c470
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:47 +0000

    drawing just fly conclusion understand

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
