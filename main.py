2c2e1fb1919f479c47f1d04a3b082068f21a9f1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:16:45 +0000

    check idea swim behaviour section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def find_min(lst):
+        return min(lst)
