3711e0803ce93a5fa1bea10d1146d8187c2697a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:20:33 +0000

    anyone file now label active

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
