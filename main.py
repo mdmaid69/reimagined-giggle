b2f05f897a748f5abbd690a64a3f4090703112f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:29:03 +0000

    brain thought service wing book

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sys
+def print_python_version():
+        return sys.version
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
