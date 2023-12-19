8bc9b8e4649096fd49e14acbeab6fa837b59be16
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:23 +0000

    country tip exit sister where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
