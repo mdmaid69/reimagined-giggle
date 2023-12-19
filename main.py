b831148b7ba668ceabb3872cd7eaebb971126422
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:06:31 +0000

    year first unusual think spin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
