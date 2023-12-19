0483d16f280fac66f99236e8a5c2d452c6d29f9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:42:46 +0000

    regular trust fear ordinary correct

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
