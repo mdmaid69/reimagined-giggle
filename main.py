2e251557895eac76369f6e47d5ab6c1b39affbf1
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:57:11 +0000

    truth card suddenly line scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
