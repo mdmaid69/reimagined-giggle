e1eea4367fda19fbb3d5bf5d47811f9bbf8cf34d
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:27:34 +0000

    place butter bread too hardly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def convert_to_hex(n):
-        return hex(n)
+def find_max(numbers):
+        return max(numbers)
+def find_min(lst):
+        return min(lst)
