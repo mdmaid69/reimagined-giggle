7c2f4d880ff337a7d3d72fd5e9cd9c2ad7aca383
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:15:19 +0000

    valuable weekend man morning takeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
