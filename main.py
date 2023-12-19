5d76caee231f9810f6d147ceb8a54d99def08c2e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:38 +0000

    goout vote takeoff flow well

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
