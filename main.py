f80ea7a66bf57f72335b8d7ce055347224a6a107
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:36 +0000

    consider defend slow impossible free

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import sys
+def exit_program():
+        sys.exit()
