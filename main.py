4ebf1cc5b332ff6b5deb6d04260f016df4bad26a
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:41:36 +0000

    lean plenty value really impression

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_current_directory():
+        return os.getcwd()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
