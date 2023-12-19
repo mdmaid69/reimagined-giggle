612bbef3aede7490e1e7654803cb172ac8dd2227
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:47 +0000

    welcome rub wheel hold set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+def sort_list(lst):
+        return sorted(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
