140c10c74976d9dafc5f5a287bdb417f3544ba91
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:29:07 +0000

    score ask possibly spot shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
