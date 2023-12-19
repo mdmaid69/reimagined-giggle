3d6a8ad4303d8c02582de47227af02e70c8837e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:29:21 +0000

    leave rare on national sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import array
+def get_array_typecode(array):
+        return array.typecode
