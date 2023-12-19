3d659a0c68b52f482bd0406a8adb92704f034e84
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:50 +0000

    sick belief play cruel value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
