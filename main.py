1d3391db96bbd6906fe7e5de60d66fcf36ace445
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:23:40 +0000

    shine pick eye clever mine

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
