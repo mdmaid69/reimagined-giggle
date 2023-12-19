b2f3cd13f66b4641d7d24abf9b66955d5359425a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:12:58 +0000

    centimetre also own home crash

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
