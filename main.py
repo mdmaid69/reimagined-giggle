a1d7008dabc2c7e12d3f98308fc1dfbac816b267
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:23 +0000

    oil public risk crash stretch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def clear_array(array):
+        array *= 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
