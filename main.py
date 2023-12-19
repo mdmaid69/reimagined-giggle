c4be5df0608cb9604ea6e50b250f5c93296ff715
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:07:32 +0000

    car fun fear reach hold

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
