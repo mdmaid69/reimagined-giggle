2bd7b7a9927f2502549f73df067226d85c2c79fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:00 +0000

    565end exit safety comment study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
