e45aed744f93c1bf516ce4059a01189301c7e782
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:31:03 +0000

    present satisfied twist trade transparent

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
