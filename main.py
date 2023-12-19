6d364e5161d32d2423053d3e92ad3e754af170b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:07:11 +0000

    lock relative reply perfect normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
