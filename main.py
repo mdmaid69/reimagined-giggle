2f086cf285bd643979adbd7120853bc3555a1988
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:48:58 +0000

    fill snow manner meet dead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
 import collections
 def create_user_string():
         return collections.UserString()
