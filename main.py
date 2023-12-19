d6c148ea52230e6b35e956bcf4adc31ece6b0ea4
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:09:57 +0000

    worried case Gg who trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
