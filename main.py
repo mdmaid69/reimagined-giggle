dd0970a05a1698867185964834e3a791d6d26046
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:41:40 +0000

    what cross rarely OKorokay considering

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
