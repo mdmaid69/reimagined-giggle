2464c194c405f36968e5884711680f26b8c08f05
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:32:16 +0000

    dress insist post usually sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_volume(length, width, height):
+        return length * width * height
