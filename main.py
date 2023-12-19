7b09bbc19a087a629a6d42641ea7b4c32108be41
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:36:58 +0000

    quarter refer shop rid positive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
