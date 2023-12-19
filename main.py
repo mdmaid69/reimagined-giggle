468fa53a06c12df6aab8f35c7e567b2f4ba86cab
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:47 +0000

    reason complaint may pin glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
