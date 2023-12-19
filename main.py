fee0bef7b64244f0ce0c60ead35a05ed821de3db
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:14:09 +0000

    bend until slight dark several

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
