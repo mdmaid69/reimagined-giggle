cec61e63878efc77795a033d48b40af1456d9159
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:53 +0000

    exit fall walk scene easy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import sys
+def exit_program():
+        sys.exit()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
