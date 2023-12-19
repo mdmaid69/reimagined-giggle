3a8d9a3ee2bb0554903b42af45611776fd1eb0d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:17:56 +0000

    factory across in occur wealth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
