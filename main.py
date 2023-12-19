581a1af7edbf0c845c939c6ffb0cd5e31129a01c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:20:01 +0000

    party health strength handle Vv

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
