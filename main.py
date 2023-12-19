a05e09b8e485791462a85fd13df00e40deeb91b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:09:20 +0000

    else content whatever match below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
