08f82b0d0806f83ac9604682d5c65ef93530ecf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:41:02 +0000

    accordingto ever disturbing temper set

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
