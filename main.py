215fd128b80ae7b1ee0ab7df7d23c607f4ec518c
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:20 +0000

    Dd prove square together also

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import sys
 def exit_program():
         sys.exit()
+import datetime
+print(datetime.datetime.now())
