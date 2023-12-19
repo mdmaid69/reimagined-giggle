80d2a146b7d3ff8efd8e36ed7ff27b51e9ab985d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:56:00 +0000

    manage necessary create pride somewhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
