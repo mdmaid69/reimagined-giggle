f0475f4985a4838c801eba5c6f4071eded4106a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:12:42 +0000

    whisper their familiar band leg

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
