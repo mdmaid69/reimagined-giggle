7c088df850bdd6f6e3c9377dcce457789986ad55
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:35:44 +0000

    street about side blue whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def cube_number(x):
+        return x**3
