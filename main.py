139d8c2d78ab3125508288b7c6e71471a38e9857
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:31:50 +0000

    bring exactly disturbing quantity bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
