06f35d98682ebf03d05e56b78d183b7910b4cc83
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:33:11 +0000

    mystery square good trust holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
