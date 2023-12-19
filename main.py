73ebedc8655eb802835b030b687a26c1f89f131f
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:36:33 +0000

    quality tired bringdown quit coffee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def square_number(x):
-        return x**2
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
