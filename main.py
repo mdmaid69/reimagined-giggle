a6be6e00cb8aa3f59b0c8528023324b7047dc759
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:05:18 +0000

    slight lonely hide float handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
