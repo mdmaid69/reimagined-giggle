02843b886b7e7ff4be19a58b93aceb7f28377ff0
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:49 +0000

    weigh forgive waste consider soon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
