b0b1e1116b21d8379ebce72e817b9d6b937a6bcc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:42:42 +0000

    path die dull gather lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
