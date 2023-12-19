b8c118269d4d3330a019ac22bb361d19bb3b7664
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:05 +0000

    children dead weak practical just

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
