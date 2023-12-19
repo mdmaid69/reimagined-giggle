43807177c6413953b40c0273a15967c74520e2d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:48:07 +0000

    if call exchange under drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
