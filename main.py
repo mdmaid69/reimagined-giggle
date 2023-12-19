1198cfaa6e3fb773ed9e36351aa9ec16364ef93d
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:35:50 +0000

    by bye distance takeback above

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
