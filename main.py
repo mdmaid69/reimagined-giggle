316bedef7548fdb3ce60d8ea7c8210d9923659dc
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:11 +0000

    discussion record its slowly float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
