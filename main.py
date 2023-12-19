fc8bc5230d57e1ea81b559ad44f829e386244796
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:22:25 +0000

    chin born matter breakinto normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
