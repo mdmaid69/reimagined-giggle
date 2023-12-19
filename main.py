bd5eed7497ba424345313f9d1a0c85886681189a
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:00 +0000

    read explanation shoot whole tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
