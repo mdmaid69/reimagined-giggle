eaca187089647047bf57c30a92d7f3259c12be8a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:09:25 +0000

    basis smile floor birthday public

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
