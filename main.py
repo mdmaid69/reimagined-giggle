dcbaf9f11659de809fbb679095155add84fa69da
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:00:01 +0000

    union guard almost spray reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def create_stack():
+        return collections.deque()
