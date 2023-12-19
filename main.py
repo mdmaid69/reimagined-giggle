2544c4701ea65050a4b478f2d7d16ce0ff6a0a10
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:15:03 +0000

    bringforward interrupt cook its expect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
