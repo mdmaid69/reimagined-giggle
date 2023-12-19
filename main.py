35c74c8efa28352e38da5e6065dfae77f21c5f54
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:40:52 +0000

    foreign cope choice mistake mass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perpetuity(payment, rate):
+        return payment / rate
