01430c7a35da3d6a11e1050c93fff862724834ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:24 +0000

    emphasize curious alone battery ago

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
