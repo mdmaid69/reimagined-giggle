74452bd6f02f0783182b088b61820d1086d3df6e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:59:51 +0000

    every pen price set matter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_index(array, item):
+        return array.index(item)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
