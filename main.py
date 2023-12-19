1bac8162e87d377d83aab2dd35098db146497b31
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:45:28 +0000

    storm scream off base easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
