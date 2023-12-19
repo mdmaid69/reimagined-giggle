47a6975e996aa129b258f0c26d7fa6c1ac61b1a0
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:34:32 +0000

    butter learn will unlikely job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def get_array_as_bool(array):
+        return bool(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
