bc99b8cf3caf21203e40cf229f78191762458d5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:01 +0000

    ride fun stir weak mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
