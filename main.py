86396058956c8ec6669e0a693b6058ded34eca76
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:20:45 +0000

    innocent unpleasant strange argument easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def get_array_length(array):
-        return len(array)
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
