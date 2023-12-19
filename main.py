bac4a2c10c271cab8d990d0971dca17aad6e1cbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:22:09 +0000

    yourself callout breakin surprise develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
