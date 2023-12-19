335cc8d575189babdabf6f704bd0c69e53b1ca97
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:54:27 +0000

    Ii against forgive foreign buy

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_work(force, distance):
-        return force * distance
+def count_characters(sentence):
+        return len(sentence)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
