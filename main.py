117fd04f13ef77d3789287191105cab0d9a50ac8
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:47:00 +0000

    top once oil positive spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
