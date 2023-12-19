8465c0b855bf685a1ccee492d085cea1e5db7e90
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:15:00 +0000

    powerful children comment organization thank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
