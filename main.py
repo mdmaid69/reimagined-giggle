faa838e4a160e3032cf7fccd9b1c9a122a816190
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:58:13 +0000

    with style but performance serious

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def convert_array_to_string(array):
+        return array.tostring()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
