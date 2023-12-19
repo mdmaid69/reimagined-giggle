353069c4f046fe8603af10a9732536ac74e0169c
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:56 +0000

    discussion fit great gradually might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
