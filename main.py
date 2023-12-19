cd979604f2f0fe23c75ea5298c7380eefef898b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:23:08 +0000

    act like unkind smart full

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
