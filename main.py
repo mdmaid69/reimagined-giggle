2a846cb016392078387612aaa6230980ccfa05e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:39:57 +0000

    sound group move actual trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
