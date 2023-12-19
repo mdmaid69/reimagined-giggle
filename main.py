eaa64e68774cb9aa2fb8094b4144377b4ec66191
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:19:35 +0000

    average temper thank tour pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
