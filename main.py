3c82de6857187bf27fd45a1079fe5f8911a1a738
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:01 +0000

    feed evidence damage miss post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
