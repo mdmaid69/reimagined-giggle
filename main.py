ea3ce85e5c04ea70d4dfcbb721824d8d154463ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:28:13 +0000

    bunch send against almost down

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
 def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def convert_to_octal(n):
-        return oct(n)
