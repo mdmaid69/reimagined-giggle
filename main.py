285899e720d6b21761a182d27535144706ac6423
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:25:38 +0000

    cold experiment price quarter memory

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def sort_list(lst):
+        return sorted(lst)
