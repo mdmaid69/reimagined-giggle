9020d3f1875f1e2b6045ec0c5b3a7f6ab29851c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:07:50 +0000

    release benefit concentration peace round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
