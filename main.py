47389eb8b8d8836482362ef2f44b70d02e5cd8d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:03:14 +0000

    late week sweep although beautiful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
