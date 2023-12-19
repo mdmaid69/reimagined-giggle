201af03fd2849c682cc56fb0cd365adad166a8e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:32:10 +0000

    justice night head share when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
