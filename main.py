62b2eae4fef54e4fa2533c10a011582257d5f6bd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:41:58 +0000

    go delicate work consult boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def multiply_numbers(x, y):
         return x * y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
