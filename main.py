a1f62e99e30bd353636ec00bf0db49b8a29e2e80
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:13:29 +0000

    holdout correct pile swallow language

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
