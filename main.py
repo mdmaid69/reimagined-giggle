8839a34e1b8bc4949b03acbcc0480c7c9dd7932f
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:50:52 +0000

    already average respect load disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
