a64fea6d753b80c4946ab0d8fee4ec1bf258b8a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:40:54 +0000

    brief lead practise odd hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
