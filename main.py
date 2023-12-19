8ae9234be2211fd45e36ca08f275134d4f9d6af2
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:04:36 +0000

    completely lucky work story steep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-def convert_to_hex(n):
-        return hex(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
