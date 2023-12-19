5399ae3407c84234523b3433c5e3491c929c2ea0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:07 +0000

    indeed me view mystery tip

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
