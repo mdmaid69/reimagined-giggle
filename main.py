4582f6a52f8f84d144667b23708fe5e5b81a96da
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:16:03 +0000

    again about Ee priority creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
