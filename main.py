4edf01692deba50f51e77ee262b209eccd1516e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:41:27 +0000

    mine flat money friend disapprove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
