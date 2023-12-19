f46df788e18561bd771597164936bfe7d57451d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:19:19 +0000

    buy reserve short visit shall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
