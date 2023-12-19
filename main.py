bf6c32048c9b4c5f0939e1d71d0e66c0576e64ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:09:50 +0000

    excuse reply direct brush rent

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
