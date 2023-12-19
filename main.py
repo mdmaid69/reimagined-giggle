208ba8a2a34b7907afb0fb10668e943d4110e5cf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:50 +0000

    section bang frightened drink tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
