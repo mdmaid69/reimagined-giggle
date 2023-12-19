c80a96cdcd9df35d47de863edd24001434ec61ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:54:19 +0000

    tired constantly imagination stamp couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
