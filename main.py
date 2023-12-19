3020ead449505e2fca437226f4f2c779ac089b58
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:40:20 +0000

    stiff middle against wherever persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
