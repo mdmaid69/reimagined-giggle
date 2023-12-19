64fb54ec8b6eeb741db3bffc1f3ad7b8a64515b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:20 +0000

    brain knock central music himself

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
