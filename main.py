a56c339cf189bbbb32407a7c77d64aba17930bf6
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:13:07 +0000

    fear hate tap breakinto now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def sort_list(lst):
-        return sorted(lst)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
