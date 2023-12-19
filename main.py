cb764f2b259f4944f89fe708375d53c8c4afb3f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:53:27 +0000

    slide situation object no operation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
