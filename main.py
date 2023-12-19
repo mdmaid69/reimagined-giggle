1e89910435ffbe5095625761901b166edf1f4362
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:24 +0000

    post performance target patient satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
