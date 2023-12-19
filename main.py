3775a9918d519d85dde09b8e5f2b61797a336c29
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:33:01 +0000

    tour Mm poison mend smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def split_path(path):
+        return os.path.split(path)
