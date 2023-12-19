f893e75f8ade8472a8d91a34fa0eb6bca4ad282a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:04:40 +0000

    safe talkback regular 571enough Qq

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
