8069845d4aa96b40255cde5422e0b7ae6cb8d226
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:07:34 +0000

    badly performance uncertain over discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
