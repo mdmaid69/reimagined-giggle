b65b164710f8e54571710749ccdcdc0adf70fcb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:21:20 +0000

    previous everyone crash unfair thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_length(array):
-        return len(array)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
