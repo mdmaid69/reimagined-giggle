1533b264b18f4a9ed3698235272cbec8a625c393
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:56:52 +0000

    slow neat surprising perform mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
