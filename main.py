e5b9ebe91595f56e824468a5cd5ae1705a9f96df
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:09:37 +0000

    middle lead more downstairs container

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+print(sys.version)
+def find_max(lst):
+        return max(lst)
