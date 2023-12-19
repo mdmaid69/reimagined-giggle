13accca53b8b2a6c6889119f9f71d51a3e12eda4
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:29 +0000

    admire damage radio through path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
