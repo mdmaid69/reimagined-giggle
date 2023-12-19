40dd0fb8dafec13bcf188d5fade47024c0226e1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:17:56 +0000

    attempt mind school sure talkback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def is_even(n):
+        return n % 2 == 0
