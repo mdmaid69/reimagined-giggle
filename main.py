cdc7d056f42f51a0890dbbb6ac4cc68e4ee9863d
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:55:26 +0000

    try position shout 570enjoyable frightened

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+print([x**2 for x in range(10)])
