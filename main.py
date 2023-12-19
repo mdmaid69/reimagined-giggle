608d7d6ffd7ee8f74143565cf35c016918d73963
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:58 +0000

    material store force after turnoff

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
