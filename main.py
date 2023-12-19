656dddc9e8bd61ad46e43a9b933a78207a8173ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:18:36 +0000

    hope basic exist protest cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
