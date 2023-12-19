2920e8c7c7af44bf92013b24b7179d02545bbfd4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:35:18 +0000

    around giveout simple leave exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
