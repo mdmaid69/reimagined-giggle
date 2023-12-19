a3dc7a2ae98de135c95512ff874af4bd75ca25e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:52 +0000

    majority crowded chance children turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
