1946854ab1bd8496b4f5c07e6002acac64be9ecc
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:11 +0000

    dress ourselves inquire invite threaten

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
