db10ee596068b3ade389ea58a144bb158930724b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:49:15 +0000

    sight win clever print otherwise

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
