3556817efe531dc4eea6a6a2fb51192c36168271
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:38:04 +0000

    piece main precise opinion damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def exit_program():
+        sys.exit()
