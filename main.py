5417e2f143bd0088a46794c3e4193129ad8e48c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:19:02 +0000

    regard will guilty play minimum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def find_min(lst):
+        return min(lst)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
