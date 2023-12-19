0e9ae6fa16ca0936b63b137da3c51ab7f7c151f0
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:53:51 +0000

    miss police destroy must tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+print(random.randint(0, 100))
