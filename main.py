6a68364b16a3cea0ba80725b47e1f283f06369b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:44:22 +0000

    accordingto into trousers trick sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
