1c77e2941705f78971d6dc81de40c39cf5ef2d78
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:19:13 +0000

    smart bet sorry separate everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
