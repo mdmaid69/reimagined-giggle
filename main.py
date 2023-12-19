5f985950f4e5dc97e78dfd6b29d17ee35803b9f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:00:56 +0000

    visit book entry cover mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
