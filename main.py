41f6310f54adc602249770d5d1f325c9de786acc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:16:46 +0000

    murder fault gentle cause intend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
