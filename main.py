a57e9d8845bce051c2fbf34b574c42586df4d0c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:28 +0000

    especially manner pleasant danger basis

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+print(os.getcwd())
