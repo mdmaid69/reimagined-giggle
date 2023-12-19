a68969430bfa8c4f9caf3f187cc61a05765d8ed2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:52:07 +0000

    innocent work play always stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
