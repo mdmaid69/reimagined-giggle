74ca92dc15763c6670733a66b9fe42b7ac63cd91
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:37:18 +0000

    increase grade odd benefit actual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
