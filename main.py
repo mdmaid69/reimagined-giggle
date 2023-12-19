6fbd0d84373d81ad9d3fd1d652ba882f26d6358d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:52:17 +0000

    matter fashion pass acceptable doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
