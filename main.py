e5a6c054d66f7e81eafd9f5c24221822d568db19
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:36:40 +0000

    address object basic reply shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
