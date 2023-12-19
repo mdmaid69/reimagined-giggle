988fcd64108c6ce993af63c1e178c85083bc89fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:27:36 +0000

    preparation talkback surface convenient class

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import datetime
+def get_today_date():
+        return datetime.date.today()
