6e13366612f8cd3d2dfc005b25943937ef262f4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:52:17 +0000

    backwards hello oil somebody target

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
