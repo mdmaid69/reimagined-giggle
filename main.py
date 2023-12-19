3d08fef5bac560f391684dbbf99023ef890e4a21
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:28:07 +0000

    terrible measure react large subject

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
