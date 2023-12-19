9050fe764d2973a1bf194d6c6e0142fa15a510d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:59 +0000

    passenger responsibility scream sight turnaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
