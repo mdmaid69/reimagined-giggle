b03da7c7f59a32462e306409e628066f394c9595
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:11 +0000

    risk male flow reason lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def cube_number(x):
-        return x**3
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
