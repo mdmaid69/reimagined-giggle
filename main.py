ec5c6533427132c28fab16201ba105ab16b11e0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:22:09 +0000

    family positive maintain nose soak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
