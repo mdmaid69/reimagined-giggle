34ea087fa23483182420cfd40e74341d4b3721e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:25 +0000

    hold use nice closed while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
