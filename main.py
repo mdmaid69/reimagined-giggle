3b287ffbd908143149bd49ea0b91f6dda35cd961
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:42:36 +0000

    balance consist excuse skin comeup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
