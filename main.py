46dcd950200ef72bab04a4dab82e14c3d994eaf8
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:52:30 +0000

    add attention famous improvement sense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-for i in range(5):
-        print(i)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
