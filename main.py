3360e9004cf134eec0380a66901925be01663285
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:29:35 +0000

    black lock what makeinto calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
