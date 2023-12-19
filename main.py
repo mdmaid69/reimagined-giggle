659fa6869733286fa5ae21e23fbe250479c2d409
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:24:30 +0000

    small order slight idea steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  def multiply_numbers(x, y):
+        return x * y
