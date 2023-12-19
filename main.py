f7ecf35b6cd0bf0672852bcd9fe7a6f34fb5f45f
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:00:58 +0000

    strict memory round comeoff bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
