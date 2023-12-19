b8e8af2019721d44f837cc0454e00602be55b625
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:05:06 +0000

    goon worst quick major distant

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
