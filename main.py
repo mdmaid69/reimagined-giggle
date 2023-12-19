0fa243954011a6bf3ed8b7236b16a38e5b9a1c4c
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:34:01 +0000

    bunch detail cruel cheek eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import os
+print(os.getcwd())
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
