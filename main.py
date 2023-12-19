4a29f7623aab2acb4d2d1546b8ca295d217695c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:58:08 +0000

    deaf shine evidence thing still

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def count_elements(lst):
+        return len(lst)
