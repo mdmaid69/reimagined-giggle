99f62e940fa24efcb5e0b899709072503620bab1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:02:28 +0000

    goback(to) level brief hope loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
