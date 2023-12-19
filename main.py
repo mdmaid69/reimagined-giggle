13384693a6156069e137c71d5e564b061907dc75
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:25:34 +0000

    exactly hardly edge song clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
