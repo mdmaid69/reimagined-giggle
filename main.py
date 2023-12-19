5d7080a3088bd359a5dd2cee6edade00e21ea322
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:34:06 +0000

    criticism soft congratulations minority murder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-x = 10
-y = 20
-print("Sum:", x + y)
+import datetime
+print(datetime.datetime.now())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
