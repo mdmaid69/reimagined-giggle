b8105b1abbb9e1132808848c50be5e51d6e41210
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:10:24 +0000

    exaggerate exaggerate extremely impressed improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
