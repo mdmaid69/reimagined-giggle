1444827ff2affe4eea788c5cfa678509c77ffd62
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:51:46 +0000

    gradually live list constantly cope

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_set(array):
+        return set(array)
