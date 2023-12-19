13ff1b5a29d3ccb57cc981cc3250e50bb109bea4
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:59:55 +0000

    related bite bin language sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
