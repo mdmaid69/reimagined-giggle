9ddef9d463a9e30e193ad3035d0c1104191d3841
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:21:15 +0000

    thinkof myself sight learn walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
