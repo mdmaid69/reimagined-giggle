5e61e7ae60de2f58f31d83dec0f7f44e73c4c61f
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:59 +0000

    score term authority trip platform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
