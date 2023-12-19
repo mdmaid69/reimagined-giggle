c195cd3fa8520d5fbd0689ada90ae8e51d41db34
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:11 +0000

    rain Ll rush ours danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
