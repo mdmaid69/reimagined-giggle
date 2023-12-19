648da60b39554f33635eec7b0b8c47c9564375eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:00:18 +0000

    share ill repeat computer organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
