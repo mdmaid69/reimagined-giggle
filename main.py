de5762259c405cd676cb954159ebc4345bad2427
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:48:00 +0000

    we unlucky block afraid mend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
