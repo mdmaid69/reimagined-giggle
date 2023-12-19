94df83f5fef93d9e8f0a3a99acb89e9472d99b17
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:20:52 +0000

    murder success earth OKorokay figure

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
