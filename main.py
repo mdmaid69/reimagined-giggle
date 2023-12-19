7c268a8ea9bc696d403c333f0bfd7695a61ab44b
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:54:48 +0000

    further ear favourite he back

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
