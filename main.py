d272dd0e5d612dd2d92894225d8ea2e1753c779b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:03:49 +0000

    left uponon stupid workupto stone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def is_palindrome(s):
+        return s == s[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
