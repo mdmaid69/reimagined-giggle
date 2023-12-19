3a152abbdb8d35a6683e6d5c4d7948e81f14359b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:26 +0000

    along anywhere nerve fit plenty

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
