69c0a322164feb9cee26871c64c4cb06b5eea1c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:10:10 +0000

    quick mouth press black excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.time()
