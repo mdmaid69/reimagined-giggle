f16e01295418a70929e979f1c9cd264820096bc9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:57:35 +0000

    related already majority least complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
