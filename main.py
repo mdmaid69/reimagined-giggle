cc147443f5c60bd65acf715e11d3c0a78205c46c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:57 +0000

    delicate serious if detail country

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def convert_to_hex(n):
-        return hex(n)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
