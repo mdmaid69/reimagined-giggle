37b68f1b33dc37e3d82084df74f141938c3eff52
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:05 +0000

    special guide themselves mention suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
