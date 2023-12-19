958f8862c83e817d6601ee03b1ab42244e72f01c
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:26:48 +0000

    phone answer difficult earth children

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+for i in range(5):
+        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
