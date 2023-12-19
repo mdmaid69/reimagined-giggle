6284bcb66fbe265a382dcccb982c8819a34b3b86
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:02:10 +0000

    temper another complete instead object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
