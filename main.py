47a15024d4842287925ff6ea5857be850d65c667
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:49:03 +0000

    tend fold that business thinkthrough

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
