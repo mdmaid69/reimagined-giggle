24262446bc743e3cca039021a16a1bcaa9142def
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:14:04 +0000

    charge whistle takeout private concentration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
