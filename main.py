eef2f6c841815085b194f7d0861aa297311ce124
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:27:36 +0000

    at benefit doubt rank squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
