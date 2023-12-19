dbbfd36e227c37272e156d147111e600fdc447b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:44:59 +0000

    yourself Cc giveaway average new

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_characters(sentence):
+        return len(sentence)
+print("Hello, world!")
