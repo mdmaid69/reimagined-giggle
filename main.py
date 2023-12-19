f89560fe71014d58cfb70418238a333a57b6d1df
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:06:11 +0000

    swim sticky impression exchange snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_str(array):
-        return str(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def sort_list(lst):
+        return sorted(lst)
