3e4acf8cacb247013d6ba988070c9ba56d77f595
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:54 +0000

    port instant Qq shock until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
