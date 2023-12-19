1258ffcb56a679655c2eebce5cb56b93fe0e16a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:22:32 +0000

    advise holdout room figure score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
