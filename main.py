6336a160d9f053fd549d54bfe94d356101da61f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:43:16 +0000

    release favour convinced demonstrate lookup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import tensorflow as tf
+print(tf.__version__)
+def convert_to_octal(n):
+        return oct(n)
