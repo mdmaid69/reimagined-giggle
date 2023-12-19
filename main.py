87ed089ed1fc79268ec4aebf056c1966487db8b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:51:16 +0000

    mouth joke lot display opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
