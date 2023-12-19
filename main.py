41a27bf22627939eb7ce52d5010ebaaeff7ef512
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:20:36 +0000

    culture comment way properly blow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
