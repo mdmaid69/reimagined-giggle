0fbef0eee550f26789c241365809f06641d9ba69
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:06:58 +0000

    unfair curious arrive Bb disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def clear_array(array):
-        array *= 0
+def convert_array_to_list(array):
+        return array.tolist()
