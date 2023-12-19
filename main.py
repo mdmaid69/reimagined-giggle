222b0d93605676028a294e4277ebada5e9f23711
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:01:56 +0000

    boring slightly congratulate bill cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_base_name(path):
+        return os.path.basename(path)
