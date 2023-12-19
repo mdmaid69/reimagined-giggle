b059b94c6712d3dcd7d992cffb23a0ac9481848e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:04:16 +0000

    turnon fear crime chance away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
