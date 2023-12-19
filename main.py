9d31a57532c06c3bb3d4ee4ac886d3eab87aa658
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:08:07 +0000

    turnup reason stand success add

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import sys
+  def get_python_version():
+        return sys.version
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
