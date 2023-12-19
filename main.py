96480b5e89d3c768a05375616ee722aaf09922af
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:10:35 +0000

    thumb we attractive wear nerve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
