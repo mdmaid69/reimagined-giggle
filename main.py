8505ea518d94f6c9cc8b6829f5aec129c837a187
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:30:13 +0000

    lead want turninto direct win

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
