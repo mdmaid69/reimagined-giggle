0f503f9387021e2ce1417d8284dbe7a7ee050224
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:56:25 +0000

    squash hot occur yesterday line

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
