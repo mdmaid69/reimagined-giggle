4594ed651d7956975118227bc44c8305698769ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:15:52 +0000

    rest set Jj whistle close

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_rectangle(l, w):
+        return l * w
+def find_unique_words(sentence):
+        return set(sentence.split())
