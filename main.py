429f1c83c501dfefe6365746f515ef8bb3b9ed97
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:44 +0000

    bridge unable scrap wire instead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
