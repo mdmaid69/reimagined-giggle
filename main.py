c637241f80fbf3ca7d71e30eb571e84b40f129a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:29:14 +0000

    defend herself practice progress injury

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
