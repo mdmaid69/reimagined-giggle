2ac8b0e89ca006fab3d6821e3180d5cb83f1ae9c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:23:26 +0000

    Jj operate tie quarter snow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
