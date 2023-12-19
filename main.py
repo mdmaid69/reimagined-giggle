a05b98a8b061faaba4face9585918f3d558de1ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:46:45 +0000

    mark trial chat admit concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
