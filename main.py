74d323e59cfdd7a03582a39abd2d487629d01094
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:04 +0000

    organize sentence charge shame moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def get_array_slice(array, i, j):
+        return array[i:j]
