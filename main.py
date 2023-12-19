f59bb71b927e0d2bcc04d819bb9e460a0f288878
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:57:53 +0000

    belong he become some Tt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+def find_min(numbers):
+        return min(numbers)
