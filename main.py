4c5d1013b30b8580215b3b5d05e0b4e4f13d2e81
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:07:34 +0000

    choice well noisy arrangement door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
