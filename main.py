2d3146ac4f3ba1e3759e3563f5900c65bf40fe1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:39:20 +0000

    kill move history play stamp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
