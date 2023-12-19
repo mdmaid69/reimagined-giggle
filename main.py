26dac306274f92ed7348942ff13317c1816e166b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:45 +0000

    upwards regularly ignore secret tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
