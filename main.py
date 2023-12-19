ac94c569dd77f7e07007ce7f9b88b49f25e16994
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:57:30 +0000

    fight beneath plain trial wait

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+for i in range(5):
+        print(i)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
