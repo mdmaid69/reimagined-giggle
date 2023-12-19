52554b8713e24fff24411dab288799fa267ea04a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:19:18 +0000

    convinced eat distant scream everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
