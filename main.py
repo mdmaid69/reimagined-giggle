df2dbfff72d163dcf1ee2986884f95c08b7bd133
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:00:06 +0000

    memory doubt interview low man

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
