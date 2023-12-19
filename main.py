de5db94c9a551ba04b4736888e3e05bf08909e9a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:35:18 +0000

    creature somehow rubbish private seem

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
