c63a8271b3b323550cf93273408f4b736463f137
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:18:27 +0000

    report murder over refuse expression

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
