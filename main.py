ae3602e376c438aa95354e5725e26aa8266523b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:17 +0000

    factory share definite adjective big

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+for i in range(5):
+        print(i)
