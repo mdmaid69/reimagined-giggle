68ee1d78d25e6a276c497e4f370efbe583e7ebe2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:06:55 +0000

    take recent sport breakthrough nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
