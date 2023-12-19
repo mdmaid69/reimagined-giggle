ce475e035eef48da3d1de2b85ad253ff0d2daeb1
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:40 +0000

    frighten exhibition better chat once

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import math
+def calculate_error_function(x):
+        return math.erf(x)
