c87b0dd916e102232dc0baff322bc5f42d1963a3
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:51:43 +0000

    responsibility emphasis climb professional although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def is_palindrome(s):
+        return s == s[::-1]
