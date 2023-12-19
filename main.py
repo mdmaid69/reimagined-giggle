76ea4ce819dbd51769c3c914c18d5d3682b2f59f
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:46:01 +0000

    shape variety petrol again pleased

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
