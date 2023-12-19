05cd978647adda193578c01973b8585a9ca7d285
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:57:01 +0000

    disappear scare perfect my car

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
