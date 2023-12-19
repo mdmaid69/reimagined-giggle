b9f74ad991dadac64248fece5a237bbfdb3f2cc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:14:03 +0000

    favourite shoot back trousers large

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
