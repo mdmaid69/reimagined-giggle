40c292ebb2cefc1d60c403573dfeadb3f37d15b6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:07 +0000

    free steer immediately change 571enough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def reverse_string(s):
-        return s[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+print(re.match("h.*o", "hello world"))
