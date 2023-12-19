2cc7f007ccc353a6085b61df8cffdb9a62adaa7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:10:09 +0000

    nurse quality settle lead scrape

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def sort_numbers(numbers):
+        return sorted(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
