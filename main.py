5fef08d7066d2bc835c40730a137d6fa79bea1aa
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:42 +0000

    if possible command custom collection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
