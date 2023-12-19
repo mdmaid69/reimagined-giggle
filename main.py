49cb8873d5cf0713c22fe9e131bac3a6181333d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:30:13 +0000

    rob close clever neither image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
