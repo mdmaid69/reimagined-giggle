e507807a9b9e83840813b0ea7b0cad4899fb7ffb
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:40:11 +0000

    naturally right independent soft wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
