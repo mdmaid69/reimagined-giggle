24506c6183dcb89c21ae6e6c31edca4a84c73492
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:30:20 +0000

    wet every poor dream view

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import tensorflow as tf
+print(tf.__version__)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def append_to_array(array, item):
+        array.append(item)
