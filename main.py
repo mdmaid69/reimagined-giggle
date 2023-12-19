6fa21083e9f6d5a8852fe54b94c5244e2a38c90d
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:08:50 +0000

    that ours callat peaceful offer

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
