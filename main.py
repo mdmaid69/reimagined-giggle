1e0c44838117067b2ac981c2fdabf2bc38a6b77e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:38:23 +0000

    either return further boring meeting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
