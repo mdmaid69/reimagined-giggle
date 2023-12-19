bac4627e1151699adddb456be43c4e7dfb83294f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:03:57 +0000

    crash above moment standard average

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def sort_list(lst):
-        return sorted(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
