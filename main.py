67aa6c35079bb33a4a06b1da771cfd105b43bde5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:33 +0000

    process plan set move judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
