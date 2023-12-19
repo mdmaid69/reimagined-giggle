538615dfd93b8a9b93d6f0574d0a865a30e3f67a
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:01:55 +0000

    accordingto like kind worth rub

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
