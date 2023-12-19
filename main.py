0fe12c7cc5fa67fabfbeeb3c59459e92412cc8b8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:08:31 +0000

    weak luck perfect frightened wall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
