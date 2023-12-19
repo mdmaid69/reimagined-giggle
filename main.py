71b03c6f4abaa3024cdac2bf8d7d426e11465427
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:29:53 +0000

    tax accordingto present promise achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
