fd30097c05adc013d17f1d8ee0a0cbfa2a99f92d
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:55:08 +0000

    keepaway disapprove grateful point whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
