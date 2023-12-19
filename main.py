e1d3e1e7247a9b136dd478678c324ae0629591da
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:30:07 +0000

    character impression square offence division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
