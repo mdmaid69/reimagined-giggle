6969d2bd3ae52a52dbd34342786b5446b3d551cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:40:28 +0000

    straight independent I squash rule

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
