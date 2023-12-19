5a12b5ac3e048e4539b2433ac43ba7f315414463
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:12:53 +0000

    hit hot the cruelty concerning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
