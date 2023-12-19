d2b7dfc062a56811025fa390ed9ef5f8cb0d2448
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:12:43 +0000

    steep such strange poison bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
