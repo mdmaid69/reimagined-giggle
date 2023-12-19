5dc247f730cf525cbdc871fb279fc1b30cbd7d3f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:41:03 +0000

    calm really contribute mention miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_sine(x):
+        return math.sin(x)
