56bfd45df17eedc3c76dd21b89dd6e0f7a188cdc
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:25 +0000

    than smile high clothes mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def is_odd(n):
-        return n % 2 != 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
