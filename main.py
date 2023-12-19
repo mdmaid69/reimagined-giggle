319812849dfb497201e38c7d30571cfd0741bd2b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:45 +0000

    wipe famous goon bring story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
