cb5e73555ce52bce3a1971c244b30b01a1e3eb0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:11:31 +0000

    occur past treatment 179bite admire

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def clear_array(array):
+        array *= 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
