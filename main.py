c070ec92eb358edf5056dd3f2d908b5c088f9270
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:18:09 +0000

    brush wait trap bet call

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
