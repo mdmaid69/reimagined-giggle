327944d8f669edee8541616f8a8f29846774a3e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:51:11 +0000

    appear mass tread last supply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
