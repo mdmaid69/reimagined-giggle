3436bc129dd375eadacc467d345a31d6baf3649f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:15:11 +0000

    one along stupid drink period

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
