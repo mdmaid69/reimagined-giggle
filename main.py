5b3adcdf27f0eeb3e782235a9e5bccfa17ca85b0
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:32:00 +0000

    loss pot win past into

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
