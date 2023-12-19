20372630d2dbd50de9ab9aa5d7bdd28c3c57174f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:47:48 +0000

    Ss lot land unusual matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
