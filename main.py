4df22f47693c7a3420e7f115fa6ab7c9529d4ba2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:25:50 +0000

    summer talkinto expression match might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_max(numbers):
+        return max(numbers)
