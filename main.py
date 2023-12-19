fd32d6d373919169ccaab318e5a1431849d0ddbd
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:19:49 +0000

    bath work provide separate bad

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-print(math.pi)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
