979c772b3918059e7557a00e4f08fe02326671e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:48:27 +0000

    middle dead rubbish deal objection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import os
+def remove_directory(path):
+        os.rmdir(path)
