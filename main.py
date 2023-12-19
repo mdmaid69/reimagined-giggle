33d36c015ed4958bfdb89e00ab7d7ce70302a37b
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:25:37 +0000

    remove illness strength taste let

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_work(force, distance):
+        return force * distance
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
