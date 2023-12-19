3566af568a8778bf9416a68daa786d9b0b56dd17
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:38:39 +0000

    no think wonder near wide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
+import getpass
+def get_username():
+        return getpass.getuser()
