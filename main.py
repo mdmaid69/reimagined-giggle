500ac74a7c372346130c08639ff436e2bbf2f140
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:15:44 +0000

    contrast deaf cough offer standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
