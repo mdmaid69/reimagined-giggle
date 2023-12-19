cf582dba5c519b6a2430e095592343ecc8b81e22
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:09:34 +0000

    key surprise finger free hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
