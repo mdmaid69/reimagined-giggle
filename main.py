8b13560ae2752dfa8bf3e140145d8e93f3cadbc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:13:53 +0000

    top regional threaten track commit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
