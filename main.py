b8b6256b50880514a485496d1b303d905bea8988
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:26:32 +0000

    clothes busy final female communication

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
