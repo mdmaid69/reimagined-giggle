9f9073b5b49288062f0978ec74c2e7c0ec9f44f5
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:02:22 +0000

    file choose favourite deliberate bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
