129ce78878c6a9845de1008a8668ed4a0aaa860a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:13:05 +0000

    minor fly stay dress idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
