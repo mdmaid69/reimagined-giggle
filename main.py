bac965aecfc98c25a4c76045300aa6cddd237317
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:05:04 +0000

    fresh secret persuade involve unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
