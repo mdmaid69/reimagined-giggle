5f1a3dc7396c10b06e773714b1322c2d71e6b847
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:36 +0000

    display active side guess feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
