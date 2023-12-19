a78164351bdbd962bc945c3601bc7ee795332543
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:14:36 +0000

    scare attractive shop hard noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
