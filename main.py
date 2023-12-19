5f1ea07173a1c3ee78534aa407b26a14a57f1e02
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:27:26 +0000

    transparent remark only warm separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
 def create_stack():
         return collections.deque()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
