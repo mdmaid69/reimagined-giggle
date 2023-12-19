ad42113282822f47285edecbf7758d41ce2476fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:53 +0000

    finger above work odd deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
