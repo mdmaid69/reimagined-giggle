75db80f8fda304705a0fa9ea3169a46c9ef24f9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:02 +0000

    clear identify embarrassing need tone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
