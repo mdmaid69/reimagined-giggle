38554145274e2214a83b2575f0dc32129a1a14ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:38:54 +0000

    aim instant comedown clearly content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
