072833207d998a225eb657cdff85000a5276b906
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:46:58 +0000

    night about either replace read

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def append_to_array(array, item):
+        array.append(item)
