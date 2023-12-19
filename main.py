02f1da375c685c741499beb8355098de1920be1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:13 +0000

    disturb autumn split term wide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_factorial(n):
+        return math.factorial(n)
