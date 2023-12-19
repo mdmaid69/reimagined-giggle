e033d27be841ef0eca773c349161004e5afa3dad
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:10:02 +0000

    movement rent equipment determined hot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
