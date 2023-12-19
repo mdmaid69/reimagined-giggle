e1898f41911f5c954aa9fcf19b126eca10cb09ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:22 +0000

    case 567enemy conclusion direction force

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def reverse_list(lst):
+        return lst[::-1]
