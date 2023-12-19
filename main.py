4909d15e49bd7a509b6bd79460443f050388b92b
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:42:45 +0000

    success punishment considering remember least

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def convert_to_octal(n):
-        return oct(n)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
