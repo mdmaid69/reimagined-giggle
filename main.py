88cbfedc35441627f8421ba304098070c85ea6e8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:24:36 +0000

    anybody moment Oo purpose business

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def subtract_numbers(x, y):
-        return x - y
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
