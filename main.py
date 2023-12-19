d959374415f15bc8e1bec8fd50f141cdbe7135a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:08 +0000

    total tap people voice wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+for i in range(5):
+        print(i)
