4f4dc4011bda4606cf616ba8bf54b53f9b141cb8
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:26 +0000

    period wonder ring cover try

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
