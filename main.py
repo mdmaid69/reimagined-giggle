d8d1ab56be6037e78c955b66edad8c139285dc8c
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:25:56 +0000

    firm neck he long brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
 def calculate_factorial(n):
-        return math.factorial(n)
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
