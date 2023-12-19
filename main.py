0092dff0f4b403c050b62a1db4bfde5044a1cd30
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:06:04 +0000

    Yy issue lead male wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
