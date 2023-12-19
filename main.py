64492b29779fbe9f538a034646416992dff41509
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:18:23 +0000

    describe store tool find face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def find_min(numbers):
+        return min(numbers)
