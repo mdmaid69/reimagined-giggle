2a6e43df865c8fec98f335c57a4b1e487e97ca2f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:12:31 +0000

    turnup operate finish tall improve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
