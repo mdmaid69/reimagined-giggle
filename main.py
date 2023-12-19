aeda204e8534bcc708d955be7d63f2fa26cb018c
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:07 +0000

    although world among disappointed house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
