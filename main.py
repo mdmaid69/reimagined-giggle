f9740afef03f24718793ae48b05adeafa24b0a55
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:07:38 +0000

    chance still hard behave extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def append_to_array(array, item):
+        array.append(item)
