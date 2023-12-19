f1c8e8b8364d2d5888d2c8a4faf5c84852c930da
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:38:32 +0000

    Vv little unfortunate front down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
