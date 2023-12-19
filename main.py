7d64c91ca0fdce800a92ae81c9c660a573441388
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:36 +0000

    late gas public slowly true

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
