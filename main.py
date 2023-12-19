79a698cb5ac1488a5214bc8e9e7dd5229288ed7a
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:34:15 +0000

    bother regard work first between

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
