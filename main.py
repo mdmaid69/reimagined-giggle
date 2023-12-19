42166756ece8d04bd99a73b90e01ae09b283ccc6
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:48:33 +0000

    572enter single trap sharp calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_density(mass, volume):
+        return mass / volume
