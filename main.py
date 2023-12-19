ac2c5d4280275ebd9eca8963005ff910f6c2669c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:22:10 +0000

    hear touch unable mess undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+for i in range(10): print(i)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
