447c9a3ebc09c5604105241065165e96d3747739
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:07 +0000

    earth past worth order order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import sys
-def exit_program():
-        sys.exit()
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
