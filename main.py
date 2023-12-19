dbe7be728ac3cf2ba94c9071259f5537004f2cee
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:27:52 +0000

    responsible safe however innocent second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
