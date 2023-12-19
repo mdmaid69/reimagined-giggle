25551fc72cb2504442bb64d5a17592db8d7693ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:46:11 +0000

    healthy thankyou order shelter bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
