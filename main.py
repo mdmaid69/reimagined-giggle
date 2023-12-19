5511cd55956572f38ed81a25069eaf5f9f849ce7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:16:58 +0000

    wind ever recent onto pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
