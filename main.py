533f9a89197ea9b2a57f684289183e79403c92da
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:55:44 +0000

    extra decision polite such above

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
