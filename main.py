70454bc2f06d3de621aa58483ee154ad6a7ecb1e
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:26:44 +0000

    mood beside chin solve I

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def get_array_as_set(array):
+        return set(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
