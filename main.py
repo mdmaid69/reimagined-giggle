62d2f6127a60dd985202371f2cdedb07e8eaf89e
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:27:39 +0000

    escape custom window nonsense ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def multiply_numbers(x, y):
+        return x * y
