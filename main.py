b42ab24000b5e17f91ab674a5686633b61195577
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:32 +0000

    just flow style promise sport

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
