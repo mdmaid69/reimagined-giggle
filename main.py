ca3152f2f1da958b945da640473fd5bec83805c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:55 +0000

    innocent period strange fault stamp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
