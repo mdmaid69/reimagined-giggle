f064a73ae2f2892a1589d7ef8e318f945327fbde
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:29:50 +0000

    hard central fire mad takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
