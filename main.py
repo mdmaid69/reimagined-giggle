c20a36bb2a779da05467831087f2430ab27dc9c6
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:22:09 +0000

    turnout push method smoke lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
