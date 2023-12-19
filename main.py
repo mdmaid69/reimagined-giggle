41cffbb95902c1b3eeac399fef2dbcfab95ce1cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:39:42 +0000

    sight break smell process promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
