dc92a94602d8aaf3f6d57158312d601e550ff795
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:03:41 +0000

    worry store step moment interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_list(lst):
+        return lst[::-1]
