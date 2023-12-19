d65fb46157096b468627fbf4b7fd019908332c97
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:43:51 +0000

    argument part about uncertain expression

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
