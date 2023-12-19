7878b331432f071095f9a661d3d04b9de12953a7
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:13:43 +0000

    threaten better confused round behind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+print(os.getcwd())
+def add_numbers(a, b):
+        return a + b
