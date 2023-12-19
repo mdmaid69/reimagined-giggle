6380db627d86eaa851ebcc4802e3a59b59722eed
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:11 +0000

    fill thick tongue to next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
