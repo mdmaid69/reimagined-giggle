3b9b9cff4034e96bad67c499c494d4982032f186
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:58:29 +0000

    light well science claim embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
