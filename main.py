1626475f6a43811b638bdb65fbeebd1d9e11a40f
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:35 +0000

    guilty grow limit urgent now

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import platform
+def get_os_info():
+        return platform.uname()
+  def sort_list(lst):
+        return sorted(lst)
