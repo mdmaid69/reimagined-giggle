0e9c23a3cfef4e2b7d9cc8f6e9e5536beea6b7a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:24:30 +0000

    popular seat workout especially pocket

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
