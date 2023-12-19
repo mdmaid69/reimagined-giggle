b710e3229c8c5890ed5504ca96525f1895ddbcbf
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:12:39 +0000

    sing exist doubt beginning think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def clear_array(array):
+        array *= 0
