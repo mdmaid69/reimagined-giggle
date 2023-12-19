749d601b509548202176efa6ce897fee67e80d61
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:45 +0000

    fall read past piece firm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
