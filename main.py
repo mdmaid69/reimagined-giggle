aecf4f6598d063ad2aff41e3110de61f5137e33b
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:55 +0000

    wash steer straight greeting characteristic

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_max(lst):
-        return max(lst)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
