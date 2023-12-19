29f1c7c821dcd734cce36440bc222e38a906a21c
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:57 +0000

    prefer memory any friendly 570enjoyable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
