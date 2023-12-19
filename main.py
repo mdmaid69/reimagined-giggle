d0929aace5d014296151f831bc79299ac8e80cf5
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:25:00 +0000

    party giveaway minute art brain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
