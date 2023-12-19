b7d2c4bae7a3168c419c2e3ba968400772c77d1b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:28 +0000

    Pp there gap study pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_list(array):
+        return list(array)
