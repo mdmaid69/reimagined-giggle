9a2440c8dade8100d756697bc13a623f7542ae06
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:43:54 +0000

    dark forever minimum after language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
