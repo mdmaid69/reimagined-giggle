31b698b922d07d2468b95b28ffe334128e80a4e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:35:23 +0000

    protest present rich university bill

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def subtract_numbers(x, y):
-        return x - y
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
