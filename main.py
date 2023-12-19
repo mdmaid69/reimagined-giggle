e7213677aabf7b5c10a557a9d16399dd964d7ff0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:02:43 +0000

    notice open point both rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def get_array_as_repr(array):
+        return repr(array)
