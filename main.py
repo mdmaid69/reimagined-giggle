9c9b8171dd2e76fa0423eebc56ca0a71f11321ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:36:51 +0000

    charge twist lead ball nature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
