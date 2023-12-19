1d4b551638269519d15ec2050b3acf72f5123ac4
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:48:21 +0000

    loss Nn sentence hard spot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
