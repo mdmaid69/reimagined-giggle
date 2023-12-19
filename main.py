4f99e88d6c05d0ac8853def7e5525cb3facd2742
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:22:33 +0000

    least target circle behave wrong

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
