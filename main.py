576ad7d72ad743fa89a04a22e2d91f966eb4ace0
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:23:17 +0000

    motion happy up guess study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
