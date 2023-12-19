f8541671fe6d4edcda9e85c87f54fc65e85aa169
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:27:42 +0000

    later case steer exam official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_length(array):
+        return len(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
