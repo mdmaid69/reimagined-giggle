6b88aa29be0c68b4229c3fa32beb53b215f29ee8
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:23 +0000

    off whether afternoon brush strange

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
