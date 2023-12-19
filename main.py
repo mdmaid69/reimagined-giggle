2f782c3b8444778d7251d5a04a5809b66b1c3195
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:33:37 +0000

    murder ride protect lucky love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
