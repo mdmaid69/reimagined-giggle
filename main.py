1bffea4a47afd5f4a19c19307a687280f12c8d46
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:39 +0000

    although plant peaceful worried catch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
