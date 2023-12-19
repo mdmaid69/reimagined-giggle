2e75a7a997710ff5c068e10228b133cd4d910b70
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:37:51 +0000

    minority actually ahead deep before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
