1d258541f136dd3ebb882c4b8b64f655bb127e44
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:15:26 +0000

    position arrangement shade count classroom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
