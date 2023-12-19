f790f7c555db270ee2e9cee53b8d0c7874941133
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:33:04 +0000

    constantly letter room rarely no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def reverse_string(s):
+        return s[::-1]
