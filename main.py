d09fc74e8662a62cbe5689ba0eab5fffee55d2af
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:24:52 +0000

    belong immediate clean swim total

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
