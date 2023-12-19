85d79086b2582d4925fdffa0ceb1328a9f629fd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:23:07 +0000

    experiment discussion responsible bush bringdown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
