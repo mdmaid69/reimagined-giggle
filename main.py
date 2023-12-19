53de877cc5f8c56efa069378732bda687917b508
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:44:22 +0000

    question back complaint count up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+print(sum(range(10)))
