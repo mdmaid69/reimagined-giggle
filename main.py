19ecb19468a3c45da013d34b8662b1e9842df211
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:02:02 +0000

    flat shadow why uncomfortable nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def add_numbers(x, y):
-        return x + y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
