81632cbe59f71403ec5d9d24f201930b8cb9d042
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:21:02 +0000

    step concentration eager satisfactory wide

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
