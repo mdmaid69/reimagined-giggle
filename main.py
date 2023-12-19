ed056e5b06bfd861fafbf755681ba5e68275bc6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:25:40 +0000

    office invite ordinary habit godown

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import tensorflow as tf
+print(tf.__version__)
+def calculate_area(radius):
+        return 3.14 * radius * radius
