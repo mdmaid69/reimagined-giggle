9947b8213d182e617c130200bc3f416d719cdb3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:39:13 +0000

    steam afford possession sensible pain

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
