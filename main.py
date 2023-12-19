ba79de9767f7c3f147940e84377389f02b358d9e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:36:00 +0000

    idea careful pick tap shine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
