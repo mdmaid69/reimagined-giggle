c72cb657ecdb3cd3d9644b0852daaca405fc803b
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:35:51 +0000

    dangerous relationship yourself process balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
