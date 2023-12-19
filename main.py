113f76b6ebbeeb89c35c9e85c6de3fc4270cac45
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:40:41 +0000

    remember hold form turn true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
