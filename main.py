dbc410afcf55336cc256390c8b0b1d8a446b5228
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:57:16 +0000

    pollution music best right minimum

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def load_json(filename):
+import csv
+def read_csv_file(filename):
         with open(filename, "r") as f:
-        return json.load(f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
