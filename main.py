73af7170497a03281ac3ffa3e039902e9999e2cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:19:15 +0000

    murder reply 573entertainment reply glass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
