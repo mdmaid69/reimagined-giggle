29dd4f7f882deddc5468ba7a116247d172a996fb
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:13:01 +0000

    patience emergency upwards nor bush

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-for i in range(5):
-        print(i)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
