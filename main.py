2cbceede3146d83bbe39c77962888a1229fbc0b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:36:10 +0000

    strange invitation trip contain exploration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_acceleration(speed, time):
-        return speed / time
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
