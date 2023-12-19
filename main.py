42529c7314e64694c41e9163c60d2eb80887ef76
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:00:59 +0000

    record spread by bush runaway

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def is_palindrome(s):
-        return s == s[::-1]
-for i in range(10): print(i)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
