31c0bcf84cee3adffbb17127298064b9dd545127
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:30:14 +0000

    low relationship keepup bringup spin

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import time
+print(time.time())
