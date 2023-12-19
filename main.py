2a597a9de5cfd5043ce794e36b23bde802a67b79
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:57 +0000

    now sister reply bound force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
