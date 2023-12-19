864fda5c474131295a35d99416fc2c488c81bab4
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:41:45 +0000

    formal professional post favour stupid

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
