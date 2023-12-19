5bc2e90e696206422dd37ec551c8570c1cafd285
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:07:33 +0000

    patient sing wealth soil stage

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
