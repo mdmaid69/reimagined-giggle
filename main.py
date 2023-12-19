5cb2f252c230fdab3d6f3a187dfdf768ad5ec022
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:19:04 +0000

    disappointed reduce steep hand vanish

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import datetime
+def get_today_date():
+        return datetime.date.today()
