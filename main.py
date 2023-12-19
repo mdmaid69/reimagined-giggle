65dba75872a5168f79d1380a1854f7d01ab8f6c4
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:54:00 +0000

    anxious sail improve shut ability

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
