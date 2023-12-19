140f63380ea17de28bda9f4ced2e1a89e660aa6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:28:17 +0000

    final above some experience around

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
