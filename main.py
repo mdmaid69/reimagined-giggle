234f449a5fe53044e0d8ea5fa3ee390418650b5a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:59:14 +0000

    gas entrance because march instead

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
