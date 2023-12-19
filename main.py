f40550497eda7c1b9e41572215110643e8593e43
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:20:44 +0000

    instruction protest 569enjoy rarely raise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
