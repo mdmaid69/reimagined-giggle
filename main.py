270e910ae1072094203645a0d8af1c40b641fd17
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:36 +0000

    turninto welcome visit gift emergency

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def delete_file(file_name):
+        os.remove(file_name)
