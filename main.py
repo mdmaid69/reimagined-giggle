d1b10e72834ef9b2c47b099304f6600e5f72fb14
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:35:20 +0000

    watch maintain sail closely thought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
