1c825e3d532f60b43b46599c1410a74690cbf196
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:38:35 +0000

    bringback hurry instant where below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def sort_list(lst):
+        return sorted(lst)
