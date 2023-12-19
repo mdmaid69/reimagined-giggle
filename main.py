40308b356ebf618d61ca444b77a142d60fe9d8ec
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:05:31 +0000

    hers snow condition reject appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
