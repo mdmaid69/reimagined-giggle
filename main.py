4ab964e9214cf271617a4eadaeb93e04cbfe0610
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:57:18 +0000

    dinner estimate container concentration fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
