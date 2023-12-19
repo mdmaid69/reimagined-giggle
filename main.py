a29aa1854642a74fca6799d371084cbfa55d83e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:01:13 +0000

    window measure amount complete hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
