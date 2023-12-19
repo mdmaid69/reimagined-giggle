157241b23ef3ac843731365dca508aa9bbac1734
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:30:49 +0000

    give middle awake stamp contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
