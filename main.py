5d0714e4d4cadfd77daa14665bbd10a1a8e60277
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:19:15 +0000

    stretch rain last emphasis think

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
