582e41d91babfa9362e3c48855856adc16439e93
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:55:15 +0000

    comeoff frequent exactly right music

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def remove_duplicates(lst):
+        return list(set(lst))
+  import sys
+  def get_python_version():
+        return sys.version
