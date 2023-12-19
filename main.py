4328dbb0ac1c1cd8a401a49ccbc8881a390f6734
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:17:29 +0000

    communicate delight born disturbing joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
