d0f5c03d6bb5f9113507d2e7d6bd3da10dc17bd2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:38:35 +0000

    exercise comefrom question loss hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
