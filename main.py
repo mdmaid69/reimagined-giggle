e3c8a51f003a06e62fbce30d9e11f48d3ad4c104
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:44:07 +0000

    score later care block talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
