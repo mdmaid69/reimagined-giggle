e6f517db5665e48969ff01903cb72b7e08e26dcd
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:20 +0000

    correct port sweep estimate something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def calculate_speed(distance, time):
+        return distance / time
