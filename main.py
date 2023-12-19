6f502222ba0e0de28affc5527e8d5bd563b48391
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:40:44 +0000

    path lead relation present impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
