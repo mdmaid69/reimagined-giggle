6f0af5181dc5dad8665376b4781a4a1e8e7c1f14
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:31:37 +0000

    together anyone power far attractive

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
