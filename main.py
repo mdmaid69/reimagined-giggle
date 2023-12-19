24a508978c9d85cc754c0903ea585e7042ec84a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:01:03 +0000

    adjective careful guilty peace innocent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
