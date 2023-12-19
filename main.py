ca1a9884cf191d51eacc1c9846c6240ce1006220
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:00:20 +0000

    load quarter instrument many dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
