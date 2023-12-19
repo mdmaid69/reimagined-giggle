99b1d3851f18f16265471ab3a0bb5e576bf0e2c1
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:44:47 +0000

    sore unlucky skill smooth imagine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def square_number(x):
+        return x**2
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
