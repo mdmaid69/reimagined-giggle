ec691aa9c1d0259096ec8e710138387f4d195c43
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:08:47 +0000

    scared half many please both

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
