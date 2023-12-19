29c7f082ba6f88b18a2ee6656d61f31ba117e89d
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:57:33 +0000

    trust laugh further cross decide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
