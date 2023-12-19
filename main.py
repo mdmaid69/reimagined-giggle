ff7a5ff001af836c7c5942d81b867533de63e3fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:28:19 +0000

    turnaway medium nigga really label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
