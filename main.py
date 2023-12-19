c41e8b4daeb7f0e2ddd5c24a5eac321f7ce613d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:59:59 +0000

    takeout accidentally thought spring design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def roll_die():
+        return random.randint(1, 6)
