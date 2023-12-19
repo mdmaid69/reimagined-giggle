fc4615b4dddf4a17f65be4366d496b5a3f615398
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:44:55 +0000

    apparently keepup wind correct everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
