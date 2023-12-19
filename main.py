e49c816280955c20d1833f09b0022c1e944307fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:21:15 +0000

    bringdown force mess disappear breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
