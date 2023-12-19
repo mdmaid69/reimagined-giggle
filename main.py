a47d51f4492c4a7b0d874cde592317cfc82cc3f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:26:18 +0000

    grow pity care deserve tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
