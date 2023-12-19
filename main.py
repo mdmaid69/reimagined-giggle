399f47c9a6dc988e3b7f0147d3bd0dbffef60ca2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:17 +0000

    custom backwards office disappointed polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
