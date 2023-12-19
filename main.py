61c1444b12769d9d646c579c4ee7ee00b03ca83d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:31:39 +0000

    gas absolutely recovery worth tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
