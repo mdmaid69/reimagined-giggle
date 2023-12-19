8c3cecd839152a7d4c957f6f6052a812006c99b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:00:18 +0000

    glass disagree family interested in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
