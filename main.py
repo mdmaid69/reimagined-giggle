cf53fda2c5488847e31dffd945ccaf57a3f4c0de
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:11:35 +0000

    relief arrangement mark pretty ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
