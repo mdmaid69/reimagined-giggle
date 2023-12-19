e0e0e2cbe961814e7b26d0f9bf49340ff3db6f18
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:59:49 +0000

    repeat nervous object side pile

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(lst):
+        return min(lst)
