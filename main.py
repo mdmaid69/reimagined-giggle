f8028ca920e2312a7378b7c5d1059e838b6c7abb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:36:01 +0000

    drive corner argue crash wrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def sort_list(lst):
-        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
