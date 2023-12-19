d697f2b8cb0e81095c44270ea768b54e03a5f27b
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:43:25 +0000

    tomorrow direct top shrink shame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def cube_number(x):
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def cube_number(x):
         return x**3
