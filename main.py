a616a8a0eb41082e0e1aa9d8f360e8938ed1893f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:46:20 +0000

    think even slip prove peaceful

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
