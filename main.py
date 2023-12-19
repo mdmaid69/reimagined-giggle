2b228bfde08a8140b21a723db61cb74952abbcc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:50:08 +0000

    chat meaning pay design another

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-print("Hello, world!")
-n = 10
-a, b = 0, 1
-while a < n:
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
         print(a, end=" ")
         a, b = b, a+b
