0a28ee1c6900b5cfabc6c1e955052dab9fd48ae6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:26:16 +0000

    concentration path goon polite post

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
