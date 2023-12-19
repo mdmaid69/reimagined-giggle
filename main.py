605239009eeeef55a9202165eeccfa7db97b8793
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:38:31 +0000

    luck future wherever wear pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
