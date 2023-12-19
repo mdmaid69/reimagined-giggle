ffa686be65cba445c1aa4680d9645157ce39ffb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:45:03 +0000

    onto appeal share will hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
-def exit_program():
-        sys.exit()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
