fd0d3d4e7643b72ad330ca01770e9f22b12dba29
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:40:23 +0000

    area few downstairs back bound

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
