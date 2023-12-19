126ea09791dd710fc05c213248a2cb49ea1408e3
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:39:44 +0000

    future point callat fortunate but

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
