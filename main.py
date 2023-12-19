07d60a5a7b88a53147aa4b9c208555eca58edfaf
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:56:08 +0000

    persuade wander exactly tight bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
