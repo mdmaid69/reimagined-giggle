1325a30753692134a0cd8628957761178c51a6d3
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:01:40 +0000

    pretty such understand purpose how

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
