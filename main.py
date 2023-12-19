a07e0c7ae8b51acf5f21adff143d36dd9300f16f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:58:31 +0000

    thank story mistake act sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  def convert_to_binary(n):
+        return bin(n)
