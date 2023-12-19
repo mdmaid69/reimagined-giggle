2d88417f5e0579daaba41bd7d6592bded53a3851
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:17:58 +0000

    edge needle head crazy service

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_palindrome(s):
+        return s == s[::-1]
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
