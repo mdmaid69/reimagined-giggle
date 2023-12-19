198378be87f9600af22d061e54274cdf14a2890e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:48 +0000

    score truth Tt fault top

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
