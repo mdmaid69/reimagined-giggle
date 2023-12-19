a898fe3913557914e2565a1e8c0c58311ae8fceb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:05:34 +0000

    thinkof actual fault basic straight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
