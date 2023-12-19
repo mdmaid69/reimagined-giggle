35e8b54b8e051b5d2ed9bfff2aabe0996e95ea70
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:11:51 +0000

    content noone consist window freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
