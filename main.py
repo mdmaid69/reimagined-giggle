eaa2bb61f61922291102321bb7955ac786399c0b
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:18:33 +0000

    compete guide dress able result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
