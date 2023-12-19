692102b5d53adf162f130abcff0d95f33b926ac2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:08:55 +0000

    comment partner care introduction drink

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def convert_to_octal(n):
+        return oct(n)
+import platform
+def get_python_version():
+        return platform.python_version()
