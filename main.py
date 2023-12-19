3ac729c847f2b1497a2ade23b97b5b47622dc7a1
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:33:25 +0000

    knowledge condition so clean apply

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def convert_to_octal(n):
+        return oct(n)
+  def is_odd(n):
+        return n % 2 != 0
