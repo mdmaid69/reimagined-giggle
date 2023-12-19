1b23549770d9155220762312d1d8a74ae61418fd
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:46 +0000

    pretty situation lost too mad

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
