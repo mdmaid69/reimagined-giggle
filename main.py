9fc318de191208b9fdba027701a15bca440e2db4
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:37:53 +0000

    ability message price thick better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
