dec9cde35561d0bd49bb189687417b1bacd1051c
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:51:09 +0000

    treat lost during understand complicated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_repr(array):
+        return repr(array)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
