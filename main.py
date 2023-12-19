e486e7e0468d8116092334718c5edabdaf6f90d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:19:54 +0000

    prize dead mistake normal luck

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import sys
-print(sys.version)
-print("Hello, world!")
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
