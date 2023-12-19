1e420a88e8f930223ab70ebf3bbc3dcdff81aa6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:51:58 +0000

    supply meeting gooff group speech

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
