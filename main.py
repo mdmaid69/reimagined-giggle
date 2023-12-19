028ad3a16c5749b0f51fe04f7b1804956cfa872a
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:29:21 +0000

    murder and upstairs number couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
