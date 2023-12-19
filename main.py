17d3a406ec8af084eccfe8747e7de208716001a6
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:56:18 +0000

    visit dick hurry guilty relief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+print([x**2 for x in range(10)])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
