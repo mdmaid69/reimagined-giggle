e125e4353593a381d6db67a3fd1bf09e37c22caa
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:55:13 +0000

    excuse that hang belief pinch

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
