fe79e0f6c83dc8bf8f26a14fa8c7d186812d3a34
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:45:04 +0000

    peace dangerous word age hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-print(datetime.datetime.now())
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
