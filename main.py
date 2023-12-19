c9bbacb999f2a5002500fa1465830e7712b43d53
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:15:24 +0000

    attitude large dream estimate steer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_as_float(array):
+        return float(array[0])
