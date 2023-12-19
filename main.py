d70241d34c04d8d1baee26656c3089cc6afc2b94
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:40:13 +0000

    find capital manner tomorrow complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_tangent(x):
+        return math.tan(x)
