fbd156508e1c5d5f193980a0b17f2c04fbbaa760
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:55:56 +0000

    translate get fall tradition bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
