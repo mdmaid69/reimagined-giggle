448739a076db6e94cbb59c158404c377ea5b13b3
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:25:12 +0000

    many afterwards what protect sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
