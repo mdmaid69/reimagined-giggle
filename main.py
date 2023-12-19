c5c2d0e966e9d46622dd0e7e1df1e6341d162f14
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:22:45 +0000

    slice summer dress guess change

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def is_odd(n):
+        return n % 2 != 0
