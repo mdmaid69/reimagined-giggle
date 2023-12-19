e5ef1522dc1456f2337102b2ce535b3e013cab1d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:14 +0000

    meal fall fail deaf rent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def get_array_as_int(array):
+        return int(array[0])
