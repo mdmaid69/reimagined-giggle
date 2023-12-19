a2e5557eb6ca46c7779e11fea9302e194c411150
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:14:21 +0000

    level character scrape statement borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
