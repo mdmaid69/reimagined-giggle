0c0c38ea8a1b21a348a8658f0d4a7393f963693c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:49:15 +0000

    butter face recover effort fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def count_elements(lst):
+        return len(lst)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
