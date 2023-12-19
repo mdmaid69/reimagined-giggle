93941391ec7718b0b5ae98fe0f9d343e9e502bf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:53:33 +0000

    please various formal holdon criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
