4daece649e9da1eb4c43de9a0a527d5c98b0d7a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:21:25 +0000

    to step concerning clearly skirt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def subtract_numbers(x, y):
+        return x - y
