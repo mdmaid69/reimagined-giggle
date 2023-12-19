bc05433630091c894e43bf0fdd85f394d17abf59
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:57:19 +0000

    increase profit judge goodbye imagine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
