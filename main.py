da3c2af723c0a361952ffbe0ab636aff0f21b36b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:59:48 +0000

    task bring contents sea difference

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def append_to_array(array, item):
+        array.append(item)
+def convert_to_octal(n):
+        return oct(n)
