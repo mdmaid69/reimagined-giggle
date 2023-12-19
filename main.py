ab82175b4b1535bc53cd3623a0bc6dbd43130232
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:12:27 +0000

    without spill set careful easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
