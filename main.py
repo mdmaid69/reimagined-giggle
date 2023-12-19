c70f04967a3f030be5bc03f7c21f98e67be8b11e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:36:48 +0000

    just count question stop except

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def square_number(x):
-        return x**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
