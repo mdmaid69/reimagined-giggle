14a379590573fd1c3b8eb3df0f540d247da50d6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:00:42 +0000

    practise question callat tidy cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
