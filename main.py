40ae79f65b4857485fa93fd7ddd5ef52c7395bf9
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:38:32 +0000

    prize cook turnback insist treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_array_as_int(array):
+        return int(array[0])
