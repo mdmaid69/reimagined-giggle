aa12ac82a577fb89659604d88e174f67d0b01ff8
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:48:20 +0000

    unusual he his destroy translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+name = "Python"
+print("Hello,", name)
