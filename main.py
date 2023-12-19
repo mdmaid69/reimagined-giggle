2f4511007aa7946e8c5f88308bcd18c95e85a2f9
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:57:23 +0000

    slide act computer hollow favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
