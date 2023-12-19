29d71c1c4761edc35cf5b50a1192197f6eaa0ded
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:58 +0000

    top innocent culture tip fashion

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
