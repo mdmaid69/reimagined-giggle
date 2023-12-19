fabdccfa85c4f4ed56916d06c00318b8c1c61e93
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:22 +0000

    mistake mad journey dare turnoff

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
