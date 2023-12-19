270f84be2ab104431203a27b594f3e11bb727645
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:12:17 +0000

    unusual divide need something luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
