59f4697fd4052a157f1879cc24e800bf51a93964
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:30 +0000

    public takeback trick takeaway date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_power(work, time):
+        return work / time
