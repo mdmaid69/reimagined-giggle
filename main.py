3b550062cb95d59fb1d554af42a3989f91642e22
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:14:14 +0000

    pot tape after sort expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import sys
-  def get_python_version():
-        return sys.version
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
