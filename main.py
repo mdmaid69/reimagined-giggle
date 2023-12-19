e02ab05a53287c9f7109594fb998cff070e71c51
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:07 +0000

    fit exciting agree mine bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
