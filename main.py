855b0307ea29c6d6c9efc2742c87772c83a44496
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:54 +0000

    under classroom emphasis crowd traffic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_priority_queue():
+        return collections.deque()
+def calculate_power(work, time):
+        return work / time
