91d224548d8ec0aee6b619fcb18cf09e65fcc5f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:29:18 +0000

    praise maybe rescue away amount

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
