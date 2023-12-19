ff815891cc6ad85b7321727ee7cd370fe3976f61
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:08 +0000

    thorough paper somehow getup suffer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def add_numbers(x, y):
+        return x + y
+def calculate_speed(distance, time):
+        return distance / time
