fb1da3c2719acaa9b3826d6ff8f398e2ade4c63e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:40:01 +0000

    get distance nowhere object quantity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+print([x**2 for x in range(10)])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
