ad43a4f8e00b7e5fb39f97814038d0e057d14b89
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:01 +0000

    inform off nor paper set

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
