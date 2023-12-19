d07014b642ab814f4f7eba83cd4699ff80a31ba8
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:01:12 +0000

    meet feed proof disagree float

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
