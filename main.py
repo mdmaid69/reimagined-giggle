32d2ef763ca6680ff81e686488596ade95abdd8f
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:42:22 +0000

    criticism sting trust invite explanation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_directory():
+        return os.getcwd()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
