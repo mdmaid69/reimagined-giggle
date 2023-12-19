c779b5bd195e84b18ac8840c6230e9004da4b7a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:54:19 +0000

    attach memory bill mad over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
