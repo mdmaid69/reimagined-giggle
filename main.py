d7797fc8ce96ba7408f7d3c0abb8c1c3e8e173b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:35:12 +0000

    birth emotion children correction make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_typecode(array):
+        return array.typecode
