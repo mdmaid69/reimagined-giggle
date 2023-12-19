9bc6598e4d1ffbe5515fc3fc6d5d1af4061adae3
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:44 +0000

    total change play there should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def divide_numbers(x, y):
-        return x / y
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
