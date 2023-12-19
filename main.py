3ad8e78e5ba2b219e2cd8a7851c7bce00fd1e51f
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:03:24 +0000

    effect display match complete event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
