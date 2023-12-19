565a446aab717dde62d2ddcc0136502a5988263d
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:28:25 +0000

    please talk turnback hot when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import re
 def find_pattern(pattern, string):
         return re.findall(pattern, string)
