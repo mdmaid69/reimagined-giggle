a51b35d2c689268a5974b668e644ab73c23e85a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:30:05 +0000

    lean workupto listen turninto entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
