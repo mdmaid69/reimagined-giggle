43025c0d6d88569a5413cf5729f5b4f2ecf0b5ca
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:55:51 +0000

    Cc tour circumstance people public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_binary(n):
-        return bin(n)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
