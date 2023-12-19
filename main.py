ab91143b73ca5907b0bc5269f83aee724dc70fb0
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:30:07 +0000

    talkoutof plan people tooth priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def count_words(sentence):
+        return len(sentence.split())
