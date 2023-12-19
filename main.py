a545ff5dc19c22c4c7342b730aa40cd1525c332a
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:43:22 +0000

    whistle connected few expect very

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
