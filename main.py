3ed312b18ea79e50f491873e54de01decfb9f2cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:01:44 +0000

    likely load song cinema front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def square_number(x):
-        return x**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
