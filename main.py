4a04b6f74c522eaee588bfe0a01780b85cab1580
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:03:55 +0000

    home sudden dress opponent place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
