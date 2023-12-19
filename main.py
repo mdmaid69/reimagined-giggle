241ed75b3fa520a21845c9cc32f4dacdffcecc94
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:05 +0000

    advantage principle tip advice listen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
