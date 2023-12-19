0035d9eee4cca7a66e625f599c1259cd0b3e25f2
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:54:07 +0000

    until crash paper contents favourite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
