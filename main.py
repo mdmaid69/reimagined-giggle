242744ae3502ce50259b9b5a475564ceea3b682b
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:25:41 +0000

    division idea again wire note

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(10): print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
