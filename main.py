bf5feb5f7759f1d12be54683abe5d6cd2ab9aee7
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:58:37 +0000

    rid secret tear whatever grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
