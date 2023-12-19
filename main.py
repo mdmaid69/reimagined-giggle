0fb9ce03f85973e31091ee43303cd8aa82ce687e
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:47:33 +0000

    sting grand face convinced score

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
