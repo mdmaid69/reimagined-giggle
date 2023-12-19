c8315f86d762e701f1e6994d1ade097f81ddcc42
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:56 +0000

    fashion squash style shout ago

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def add_numbers(x, y):
+        return x + y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
