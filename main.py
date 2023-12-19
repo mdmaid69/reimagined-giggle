8a37fc31f147b2d4b169ec1a373768f540580842
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:52:47 +0000

    evidence plate frightened flavour summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_length(array):
+        return len(array)
