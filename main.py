6a6a5c9d6220a41d6d148411123a30cd62df362f
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:31:27 +0000

    hurry settle benefit scrap lookaround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def convert_to_binary(n):
+        return bin(n)
