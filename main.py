b95a16d733c29c3f61afa8e6e11175c980bd78bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:50:53 +0000

    tie equal bringback law sport

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
