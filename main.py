73c943867a6c0dfaa218bb271eb315920c375f60
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:17:16 +0000

    form unable answer immediate help

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
