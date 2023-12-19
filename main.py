612a7f61e00baf50083e65b9f0c8e66452aa08d2
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:52:03 +0000

    stick size as traffic visit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
   def calculate_perimeter_rectangle(l, w):
         return 2 * (l + w)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def convert_to_hex(n):
+        return hex(n)
