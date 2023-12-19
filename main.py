a089eeabd7e0d6036cfe7296e5614bebcbd30233
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:16:33 +0000

    aim letter secret godown light

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
