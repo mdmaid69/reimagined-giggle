97112b866c0de229439d32fa0ae0f4c464d34ed2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:33:00 +0000

    so land priority game centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def append_to_array(array, item):
+        array.append(item)
