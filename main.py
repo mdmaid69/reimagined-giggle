1d7d10b85d398cac50787fc0fce37ed6d07559ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:17:45 +0000

    combine government language giveback service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
