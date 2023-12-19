11f0a40c6a90a3bf55b88b903a40fdffc5069fdb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:29:21 +0000

    change model know closely university

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
