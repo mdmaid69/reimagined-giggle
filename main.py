bb3b901b339003ece8af3b5302cc730c754c38b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:35:40 +0000

    pattern way print pin thinkover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
