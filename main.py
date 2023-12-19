e714e34064849667ce8b61db73334309edde2d91
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:17:15 +0000

    perhaps punish secret back common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
