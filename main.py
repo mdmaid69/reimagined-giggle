d0182c14a1bd2bd062453f185685901a01c2df09
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:16:40 +0000

    platform bet grand criticism taste

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
