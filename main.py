12492657f89a60f83f05b7e6021fde68af02ab4b
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:14:19 +0000

    child radio round concerning disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import numpy as np
+print(np.array([1, 2, 3]))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
