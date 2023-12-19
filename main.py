a802717ab77b497731af68fa5c888cee5a356aa4
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:11:30 +0000

    odd answer describe suffer sea

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_base_name(path):
+        return os.path.basename(path)
