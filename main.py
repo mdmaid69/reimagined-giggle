5cc0ed55fd7cb0cafec36c3223827db1682402d9
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:19:01 +0000

    separate pleased by himself face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def find_unique_words(sentence):
+        return set(sentence.split())
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_exponential(x):
+        return math.exp(x)
