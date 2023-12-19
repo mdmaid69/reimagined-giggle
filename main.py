7b0d5cae084f25bf38e3754e3eacb6634b177287
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:34:34 +0000

    become force dark classroom certain

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
