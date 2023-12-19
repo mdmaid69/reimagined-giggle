abceee6d9e7e4963f2ab55f84cde2fc23b9e7f17
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:09:09 +0000

    themselves car bury nice definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def remove_duplicates(lst):
+        return list(set(lst))
