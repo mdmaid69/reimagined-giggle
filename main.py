c5d2552e148d255bbefafb75e59eb246f80b9fff
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:47:15 +0000

    aim goback(to) fasten join rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
