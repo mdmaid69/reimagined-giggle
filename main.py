70550dfb1052e602b4f5d6df0a5b7f1e9fcd7853
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:32:41 +0000

    swim profit straight old harm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
