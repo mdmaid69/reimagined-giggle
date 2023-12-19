c6c8dbfb780f90d9927b5e9d5fa1261821456fba
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:06:57 +0000

    lookup music contact test gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
