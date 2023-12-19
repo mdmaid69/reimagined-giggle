fbd6dc403a255138e829d1d2a0b00485b19b0bff
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:45:47 +0000

    public sign their bridge collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
