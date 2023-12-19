cdc3483fc8f69d0ed0bcd6c4c39e081dc7d3f5bf
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:50:58 +0000

    little hope apparently lookafter control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def square_number(x):
-        return x**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import os
+print(os.getcwd())
