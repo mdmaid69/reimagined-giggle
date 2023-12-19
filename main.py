2177419967570c95a4e28a98c0cb008d6bd7a4ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:37:21 +0000

    grade back plan to picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
