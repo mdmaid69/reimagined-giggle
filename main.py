753acb926f04af0011bb606f30440084e6e45940
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:15:39 +0000

    doctor imagination match worst pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
