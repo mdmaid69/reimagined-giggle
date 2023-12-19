8b815d029304113d3f15d125b52bd001690f002c
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:06:10 +0000

    exact kind please occasion hurt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_max(numbers):
+        return max(numbers)
