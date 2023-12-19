bcc30dccf41d73df193e4c5f5a1422c4c3f98fd9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:39:38 +0000

    combine meet story turninto smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
