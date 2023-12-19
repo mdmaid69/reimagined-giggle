48495bf705d7e7e48fc32e9a589b940a663301a8
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:54:14 +0000

    off match worried somebody kick

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
