6774785803de3034996ee5c029893588bde94b7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:04:51 +0000

    several pen actual closely cut

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_cosine(x):
+        return math.cos(x)
