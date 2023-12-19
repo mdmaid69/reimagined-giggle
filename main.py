d1032b80647e5dc8b69130a95006d7383984033a
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:27 +0000

    Jj bread least inquire feeling

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
