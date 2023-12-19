bf17e0712cab90f4471c24abeb55062dddfca61c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:52:15 +0000

    effort when tooth story far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def count_elements(lst):
+        return len(lst)
