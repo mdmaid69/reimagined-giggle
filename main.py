6aba7acfd7ab38b936d8e02c2f9e0ebb47b3b81c
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:16:37 +0000

    takeoff used cool main least

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def cube_number(x):
-        return x**3
+for i in range(5):
+        print(i)
+import re
+print(re.match("h.*o", "hello world"))
