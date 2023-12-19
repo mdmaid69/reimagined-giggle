331ef32a9c59f39843d10f0848e906bfffcd29a4
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:43 +0000

    dream Hh keep their brown

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
