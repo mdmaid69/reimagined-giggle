5790349a73313a8fe4d2383daa84bd65c1271a8b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:40:25 +0000

    half present wide deliver hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
