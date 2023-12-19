df381f81252e801daf64b2128fe3b2b6b57ffe61
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:03:23 +0000

    first giveup card load polite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
