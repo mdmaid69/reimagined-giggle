da485ffa2395a18c39e9ed8e037d32e02cebe054
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:02:06 +0000

    pin apparently character favour blow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def convert_to_binary(n):
+        return bin(n)
