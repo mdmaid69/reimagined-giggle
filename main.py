97b7669e234d54dc3abb479dbf73fcb058362bca
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:44:21 +0000

    entrance comedown when steep die

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
