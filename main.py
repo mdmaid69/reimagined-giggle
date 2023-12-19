e75b7da1db18e648fc7d10678ed0d16e06362cac
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:02:27 +0000

    solve system action middle she

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_item(array, i):
+        return array[i]
