126ed168522d2f7d68fd6d19d677c59bde942aee
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:26:29 +0000

    hers food join juice wipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+for i in range(10): print(i)
