bdc68dccbc1d674cb7d409c1f190e30b44b15c97
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:31 +0000

    permission mysterious miss fashion basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
