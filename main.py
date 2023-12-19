6c90d353b79c058b81aca6a380a02ce3d5c4ddca
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:19:49 +0000

    arrange rush upstairs rush consist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
