9e7a0823b19ff4b8815136b1a0a44917f4f9bb38
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:04:08 +0000

    exist opinion through pick interfere

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
