c90e478291a491d743f0720d8b5a4930c7a88388
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:57:12 +0000

    big off greeting opportunity experiment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cosine(x):
+        return math.cos(x)
