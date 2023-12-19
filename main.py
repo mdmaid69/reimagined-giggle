dcf367465f8c8098e4d94185de7c8cf5d3f5ab6f
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:22:58 +0000

    whistle open confusing fierce gather

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
