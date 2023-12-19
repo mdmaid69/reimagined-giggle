9e368cfda552410e24371e1116ea48f76089fba5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:18:02 +0000

    steal culture nearly after heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
