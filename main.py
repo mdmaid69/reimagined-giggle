d6ec2102855a3f6af0a96729fa51d2ae91f499c2
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:22 +0000

    shock rescue card nose cheap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
