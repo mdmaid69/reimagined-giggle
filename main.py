72365e150fb9da385dba68a348485e8e90579067
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:27:59 +0000

    but special talkback imagine there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
