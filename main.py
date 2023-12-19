8c92fc96dc44a7449a153b5392d588aad2571e35
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:34:09 +0000

    interesting direct boring slide Pp

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
