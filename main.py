ac1a2ca138240a63488a0c7944cd762b840bb132
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:44:49 +0000

    thing convenient base second after

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
