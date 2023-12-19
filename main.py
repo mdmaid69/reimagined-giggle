c36de2460e0c26d6370ef421325e57205ead7c94
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:12:52 +0000

    proud tune getaway violence bear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_octal(n):
-        return oct(n)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
