0f5f07347d973c2f55ec1c4f591b60742af7ceaa
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:44 +0000

    soil let man complaint top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
