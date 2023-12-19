d50621fa075b5b2c15434d1c13e3b3bdbd5b2896
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:36:47 +0000

    occur use talkinto fasten act

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
