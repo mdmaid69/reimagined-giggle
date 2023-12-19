08f5f680491bbe6256a24280618a7561782392f3
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:43:16 +0000

    equal keepup declare limit so

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
