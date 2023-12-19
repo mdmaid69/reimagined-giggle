3b265498313797a51e42655543e0ff053db0ce52
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:21:36 +0000

    slight respect cough tight precise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-def is_even(n):
-        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_speed(distance, time):
+        return distance / time
