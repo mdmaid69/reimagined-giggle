8d3e95314c84175392a3885e52446b72b6fdc10c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:59:39 +0000

    shade necessary snow weigh fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
