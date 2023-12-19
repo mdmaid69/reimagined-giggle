5b90a605fb8333265bf70e394867474c423f963b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:44 +0000

    rain talkback broad block silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_string():
-        return collections.UserString()
+  def convert_to_hex(n):
+        return hex(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
