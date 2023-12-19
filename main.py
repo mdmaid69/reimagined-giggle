c98658615ccba196b1a6106af30ecfd93d8307af
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:18:52 +0000

    fall freedom contrast also one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
