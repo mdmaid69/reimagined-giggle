b5c13d842ddd31b42b09a79601cec7a93130bdfb
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:17:53 +0000

    factory public contribute display coat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
