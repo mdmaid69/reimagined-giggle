768c1f455e11a6e65e68abedf979dd19195562a5
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:08 +0000

    hit comment sound rest turninto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def get_array_as_float(array):
+        return float(array[0])
