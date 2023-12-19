c4037dfe5522fb7f6c0539761a245c8e68f9024e
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:10:45 +0000

    snow Rr area knowledge dress

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
