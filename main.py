0703b9ac278b53a7d59ca6c4b3e2a968d8bd976b
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:48:30 +0000

    each contact whole the question

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
