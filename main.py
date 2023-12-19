b2e93e727cfa80104649c279fea2fbfffa9b9b4a
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:36:30 +0000

    otherwise further authority Ww mistake

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
