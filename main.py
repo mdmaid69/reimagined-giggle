516c3a5e223ddcc9386987fbfb317ce9d366d4cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:01:37 +0000

    purpose wish off phone last

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_even(n):
-        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
