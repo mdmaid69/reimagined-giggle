f5d7db3170a0907646eca93a2638e3bea2cf9101
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:21:07 +0000

    much accident wealth reaction stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def append_to_array(array, item):
+        array.append(item)
