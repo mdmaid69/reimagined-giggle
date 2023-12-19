09fba6597da33651d7b2baff0e3394dc2bd35a0d
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:04:46 +0000

    field rich measure separate copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
