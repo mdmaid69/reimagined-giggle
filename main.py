b99f2074e5acb1bda50d999c238393f13cc8d3de
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:34:21 +0000

    book group lucky hit frighten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+print(re.match("h.*o", "hello world"))
