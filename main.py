93a9560b95caca368d4c524d5ff8a9672a67124f
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:22 +0000

    likely godown disappointed something there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
