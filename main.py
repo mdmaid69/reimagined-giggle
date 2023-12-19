699232bbcecec88f8c2b43f344dcd303ba49f38e
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:45:15 +0000

    single shape fast opponent nowhere

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_list_from_array(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
