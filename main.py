ce5ab6b538295b665d35812c6a7b76ec71ef1979
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:00 +0000

    autumn however praise temper shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
