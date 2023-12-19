9fd5100e27cfc38a4a9f50f931bb7133cbe4d328
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:02:13 +0000

    free or time peaceful anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
