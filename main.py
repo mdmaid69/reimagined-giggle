f6c8d968ac0ba99201d33d87cf4db94ab6386fca
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:53:22 +0000

    stare past their of extraordinary

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
