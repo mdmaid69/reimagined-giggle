0ec6325f50f30eb986a0dbf6e15586fe331597fc
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:02:09 +0000

    bad give her serve earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
