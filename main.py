1e0684d8bbd82ef567dfe12a4b708427921a56ab
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:20:24 +0000

    friend speech no keepup during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
