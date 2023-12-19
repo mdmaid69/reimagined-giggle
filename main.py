34f56c68597c5116c51b3975fb375ad8b2b24ecc
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:42:42 +0000

    exercise love arrangement temperature test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
