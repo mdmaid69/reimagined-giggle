cffc76775019265c354ab24305ea90a01bde150d
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:08:09 +0000

    win different active shut stamp

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
