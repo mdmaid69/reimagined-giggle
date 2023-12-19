f7f697f1f0584a50382bc6710a5dc79e2f385daf
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:57:08 +0000

    last promise bear imagine advantage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_floor(x):
+        return math.floor(x)
