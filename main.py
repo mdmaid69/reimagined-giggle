12a6481c20d216eae42c0939e9b69447b4b44a51
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:00:32 +0000

    just several during thought trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
