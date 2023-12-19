1c03ccf29b1ac0e2db76d4a91efa53b544d2be11
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:57:06 +0000

    thumb great agreement godown rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
