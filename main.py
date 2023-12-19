2b76c120b6dc2047a3b3ffb6040d3120f1b15ea3
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:34:47 +0000

    behaviour score crash design chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
