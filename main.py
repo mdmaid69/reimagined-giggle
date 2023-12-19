84538574c48c6eaf701446a7ef3db7aadd0d962c
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:20:43 +0000

    wrong whistle though emergency fly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
