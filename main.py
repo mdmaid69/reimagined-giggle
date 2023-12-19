fe298f0b3f2fe4c86246d8d995885be3ec5d7a73
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:21:59 +0000

    season discussion better scream entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
