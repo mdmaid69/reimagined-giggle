48afd855808f66ad776b13e3287ed21c5994e516
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:00:07 +0000

    suit take phone aware matter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def append_to_array(array, item):
+        array.append(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
