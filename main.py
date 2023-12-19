31818c63ea69562bdcacf470d63ed429102a0805
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:42:41 +0000

    deliver protect bank possible view

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
 import array
 def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
