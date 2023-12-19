09fbd49a6d666205976069d3929fde14545ffe24
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:49:35 +0000

    hate tune country well breath

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
