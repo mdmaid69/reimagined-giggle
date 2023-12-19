a8b5e1bdbd231f341683294b2171103c895df55b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:37:04 +0000

    myself error waste effort picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
