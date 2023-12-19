7c268e8c7d5a4b66346fe3dca97bffafe2919760
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:48:29 +0000

    ticket knowledge work comeover activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
