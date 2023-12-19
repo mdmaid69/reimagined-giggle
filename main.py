4edd9b2f5f30c241f18108105ec6a874e8b5b4e4
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:24:05 +0000

    gift mood walk relax 572enter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
