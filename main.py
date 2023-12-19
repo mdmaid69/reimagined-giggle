30c6bcd34793eba09bb3e2156c210dc33d63d03e
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:42:38 +0000

    bad succeed total weak deliver

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
