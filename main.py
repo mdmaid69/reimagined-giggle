33e3a6df44f49cfbb91aad7f23b9d10c92d7b550
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:17:38 +0000

    limit somewhere sick clean comment

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
