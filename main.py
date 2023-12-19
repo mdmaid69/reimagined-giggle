e26eb6e4e4cb260d6c2817149fb38fe5c8c16be2
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:21:39 +0000

    that substance confident pronounce edge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def count_elements(lst):
+        return len(lst)
