450f5dd0918243e7083020ef1106488c4eb08c43
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:41:53 +0000

    week argue health centre still

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
