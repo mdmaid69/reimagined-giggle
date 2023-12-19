347298f415fae827b9ad88c667abddf2b8925cbc
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:55 +0000

    side burst temper boring key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
