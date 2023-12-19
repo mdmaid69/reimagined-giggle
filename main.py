3cede1141b537195f8ddb574646ca230d0eac89c
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:50:35 +0000

    workout thorough weak properly pair

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def find_min(lst):
+        return min(lst)
+def calculate_power(work, time):
+        return work / time
