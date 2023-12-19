4d9153e508e1f8c2a9f1c6426543fb41d06ef071
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:14:25 +0000

    information formal unfair close herself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
