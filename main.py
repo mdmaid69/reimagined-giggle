1587ff2d8ea065e82f7800fca24ec0a4b6706c28
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:00:58 +0000

    deliberately object joke fair forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+print(math.pi)
