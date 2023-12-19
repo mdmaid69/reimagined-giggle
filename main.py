2e2af0adf888b49c64e4870c4a68925eae4b63ee
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:38 +0000

    woman comment until trouble male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(numbers):
-        return max(numbers)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
