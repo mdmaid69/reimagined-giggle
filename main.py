bedf543a362f124cae530ae57e6c096da5a1fdb9
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:31:14 +0000

    complete take freeze ear involve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def clear_array(array):
+        array *= 0
