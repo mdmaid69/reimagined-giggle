81a19aef4b792ef13990a048fb149ada1f2a20ce
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:21:09 +0000

    contents children corner fly round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
