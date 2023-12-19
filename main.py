9afd31bf6d246e5b84079cad86b9b20c4f01de3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:30:23 +0000

    agreement next lock sure justice

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_min(lst):
+        return min(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
