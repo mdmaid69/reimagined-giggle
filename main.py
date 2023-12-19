b721c9c8acad022bdef334f93ceea450abe118bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:43:11 +0000

    unhappy sudden keepout invite rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import os
-def remove_directory(path):
-        os.rmdir(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
