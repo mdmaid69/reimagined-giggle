a7328789b4ad0c7202248264f34ebbe996521257
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:26:29 +0000

    standard anxious communication waste female

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
