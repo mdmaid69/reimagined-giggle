f50d61dedc029c2f94a996f4085fda62cfdd6fce
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:35:45 +0000

    wheel collect professional joke bringdown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
