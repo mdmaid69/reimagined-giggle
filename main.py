c509eec5181b9cb4f9751878ea1499496d58a7cb
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:05:10 +0000

    steep left copy create deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
+import platform
+def get_python_version():
+        return platform.python_version()
