5313d8a898096c26fbf002b010ad59a01579e43e
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:31:44 +0000

    fight send ocean decision description

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
