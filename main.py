deba5c7c68dc54d93e0113a99199844680fce010
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:09 +0000

    track poor trick correct everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
