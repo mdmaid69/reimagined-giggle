4b1509bf8d9e81c58a587ae6b86239fe0e7e74af
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:21:48 +0000

    by list organization meal wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
