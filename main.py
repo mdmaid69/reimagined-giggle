f1f072ee8c4bb71c1b257e6df9baf4860d48a361
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:52 +0000

    miss loose recently unfair also

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
