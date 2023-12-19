272e1926f9c540ad87a62cd3b1a53151431f0b58
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:07 +0000

    Ii love lonely famous Zz

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
