55ff39556a5de13cb1a642c6db319b6d6cca3816
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:44:03 +0000

    lay scheme unfortunate eager ilovedicks

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def reverse_string(s):
+        return s[::-1]
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
