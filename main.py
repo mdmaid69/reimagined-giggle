35d10045a2fc5ec7f5391898c5128a92a963db0e
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:57:43 +0000

    exchange period sit person much

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
