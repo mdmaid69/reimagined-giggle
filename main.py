47c9c201de5f07cd7af24c30282e15e33e549978
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:08:08 +0000

    quantity future noone hard belong

--- main.py
+++ main.py
@@ -1,9 +1,3 @@
-  def is_even(n):
-        return n % 2 == 0
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+for i in range(10): print(i)
+def convert_to_hex(n):
+        return hex(n)
