24d47be69d62d6a6b388e1b7298dd6282820f4ac
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:18:52 +0000

    prove high workupto reduction culture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def count_characters(sentence):
-        return len(sentence)
+  def convert_to_octal(n):
+        return oct(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
