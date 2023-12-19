d3aed36ede6269405cd75cee230abb5fbadccd47
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:43:23 +0000

    character manage smooth exam proud

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def sort_list(lst):
+        return sorted(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
