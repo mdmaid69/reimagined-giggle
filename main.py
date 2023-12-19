d60e2870c63f5ff8ec3d396ee5f991eaf41916f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:38 +0000

    report accidental sleep accident term

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
