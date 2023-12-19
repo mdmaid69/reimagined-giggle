701c19cf12cb4a3d296c870587252f210e1202d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:12:29 +0000

    move why difficulty trouble cause

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def find_min(numbers):
+        return min(numbers)
