bceea69d4ad764886c109712de41a9029e41526b
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:55:46 +0000

    payment money represent last light

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
