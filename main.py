42909c476c344457cf04b43157affc42c13e6946
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:44:23 +0000

    state coffee quantity message shop

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
