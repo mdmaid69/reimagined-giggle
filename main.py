9c50191ce6f0a314faf52576cc2f612b640d8172
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:16:08 +0000

    work squeeze lay error matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
