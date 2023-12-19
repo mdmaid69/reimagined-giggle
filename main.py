511e3535a376c96a837d22c0c179225d1b195ad2
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:30:53 +0000

    blind none listenfor miss fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
