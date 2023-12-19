682b87db65aa6ae7811942c0e74399859ee28a3b
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:56:33 +0000

    powerful himself pause love artificial

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
