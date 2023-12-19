1335fc8fe9bfc24bd510ae6d04f11a949ee1d120
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:42:48 +0000

    colour spoil value work pupil

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
