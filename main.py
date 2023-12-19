7961d066611980fe1981c7aba14a1573efa2da8e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:29 +0000

    usually often ring agree silent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
