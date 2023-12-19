ab01e352cf12d37b6625a4818f6dc0e831c0c277
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:33:37 +0000

    minute system they performance range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_list(array):
+        return list(array)
