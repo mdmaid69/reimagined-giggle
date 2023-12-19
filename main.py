8fb8896a73c75356e9377f5e998e8cf242046c80
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:06:16 +0000

    try top whole profit condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
