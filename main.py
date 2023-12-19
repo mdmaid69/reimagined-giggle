d460bdbfd787807c56e2c94a23160b70dee4cae5
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:32:04 +0000

    smart before disturb outside holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import collections
 def create_queue():
         return collections.deque()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
