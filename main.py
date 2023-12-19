9302a64ad1e60721acec886aa4619392d08b41eb
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:40 +0000

    tip realize nervous oil gate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_power(work, time):
+        return work / time
