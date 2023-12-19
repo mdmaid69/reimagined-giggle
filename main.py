6e0126a00d75bc0e458f7cbabc234c12a64e4ed5
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:13:41 +0000

    operation content uniform branch excited

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
