1d047da7548bb9dc7e03fefc052ef72f78e31a07
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:02:51 +0000

    powerful opinion person backwards cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
