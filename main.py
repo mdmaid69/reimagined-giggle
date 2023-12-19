521e344814d574a095788f97ad886eab0f958ae6
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:50 +0000

    visit together unhappy lock happen

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
