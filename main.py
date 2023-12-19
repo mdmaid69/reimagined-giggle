8ef918ebe448e3b49b7b0acda275204ac8df6d3e
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:15:53 +0000

    else Uu some jump feeling

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
