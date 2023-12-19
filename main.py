fd0b74bd714477834582fd9cc2bf9df3592c9449
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:16:26 +0000

    butter rest Dd fat hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_directory_name(path):
+        return os.path.dirname(path)
