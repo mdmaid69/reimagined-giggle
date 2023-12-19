3a052e11c142ac6294c9fd4af730d14d8d499a7b
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:54:05 +0000

    shopping introduction winter passenger opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import itertools
 print(list(itertools.permutations([1, 2, 3])))
