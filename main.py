f5bc43fbc9e4503d4a0d40ae497834d3e0362c46
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:32:17 +0000

    joke store anything load pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
