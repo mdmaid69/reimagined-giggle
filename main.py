97ee6b3f56dec75adee2ed30ded5ee16cd058e9b
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:49:40 +0000

    order mistake especially change teach

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-print("Hello, world!")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
