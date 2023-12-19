f7764c5d4a7e24fde8e0f57430c15cd9bfb46e07
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:11:26 +0000

    nervous before once cum mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
 def convert_array_to_list(array):
         return array.tolist()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
