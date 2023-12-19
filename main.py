b99192719ebe7fa0775f3f1b748c834f427a51fe
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:01:30 +0000

    nigga welcome record match argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print("Hello, world!")
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
