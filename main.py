73c24c1478417366a18032605340abbdd71a7bc5
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:00:35 +0000

    sweep dig surprising offence indeed

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
