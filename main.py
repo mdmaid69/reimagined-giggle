31567cd39cc279f49301a259d9ed8cd3644acf81
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:49:32 +0000

    wake takeover splash let talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+print(sum(range(10)))
