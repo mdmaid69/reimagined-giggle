4dac70019309e3cc1367adbbd4e194df99749cc3
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:08:52 +0000

    positive principle respect not best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
