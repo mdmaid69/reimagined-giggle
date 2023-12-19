3d96557eba3b8d5c65524b89a046cca91b11d371
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:27 +0000

    Ii interfere miss change bath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
