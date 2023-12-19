6ccabbc5b1a11f09f4a975cbf5c1642ea843656e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:52:59 +0000

    and waste immediately part owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import time
+def get_time_since_epoch():
+        return time.time()
