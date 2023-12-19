675571af7fb22cc60874902fb33840355e905a90
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:12 +0000

    cross after behind talkback excuse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_as_set(array):
+        return set(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
