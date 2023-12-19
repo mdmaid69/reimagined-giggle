b7545067946b8dcf35c3cb2bd6f59ed8b5ab59a9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:18:09 +0000

    wet wild nice on school

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
