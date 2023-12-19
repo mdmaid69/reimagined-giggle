6b4831b844bb9615ecca0145fa3cce7ceef64029
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:26:45 +0000

    free takeoff exaggerate shut path

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
