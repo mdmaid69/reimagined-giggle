96c2e3b2eaab4400ac2b5f2da051d0f81bf6c7ed
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:09:40 +0000

    talk pencil name short odd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
