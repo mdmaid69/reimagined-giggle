41e7d03def0443d90744b679aedce8e9a32735ba
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:23:44 +0000

    affect good Ff failure wish

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
