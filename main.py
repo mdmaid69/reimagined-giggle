5765527c17944d33a50be4134801a776ead57a2a
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:28 +0000

    clear warning holdon emphasis scale

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def count_elements(lst):
-        return len(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
