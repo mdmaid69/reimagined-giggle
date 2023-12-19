5f72dd940ed90962afd4644084c5caaa313e115b
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:08:14 +0000

    him someone carry reach car

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
