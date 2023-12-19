30487032fa013b580cfd81c57383865db9660400
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:26:57 +0000

    row plan threaten anyone murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_tangent(x):
+        return math.tan(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
