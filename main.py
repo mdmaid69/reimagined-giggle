837345211f531aee709b982177d1a41409ffbdb6
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:59:15 +0000

    loss scrap centimetre honest language

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def set_array_item(array, i, item):
+        array[i] = item
