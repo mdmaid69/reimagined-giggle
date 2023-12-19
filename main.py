e91d20faf437b6bd5e79b2c27244cef3719b70b1
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:35:39 +0000

    careful connection high play stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
