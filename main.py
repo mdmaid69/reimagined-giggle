fbcb6facad7dd32abc996ad2ce30e904a898027e
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:39:52 +0000

    awake future afford impressed nice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def count_words(sentence):
+        return len(sentence.split())
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
