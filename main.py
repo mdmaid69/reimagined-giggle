e8135a4e6a0b20281827b151daea0a7c55d0e01b
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:14:04 +0000

    behind separate common real wherever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
