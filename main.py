6d6631f030265178c8607921bd305a5b36323108
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:45 +0000

    heart polite life bill habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
