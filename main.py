817200c3f0e6601e21a899efb8474bca60e28b1a
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:34:19 +0000

    succeed clear broken draw him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_base_name(path):
+        return os.path.basename(path)
+def reverse_list(lst):
+        return lst[::-1]
