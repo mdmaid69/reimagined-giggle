a3ec6c5978ef5a7eb876bca2cd309543a69f87f6
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:33:28 +0000

    wish butter now official between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import numpy as np
+print(np.array([1, 2, 3]))
