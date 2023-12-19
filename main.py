ae64cda83103245ccc24703aff9f356a2f9835de
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:18:51 +0000

    active Uu life lookup my

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
