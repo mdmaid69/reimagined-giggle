fc0c92eee8f6556d6f03993f6c3a132b8470d970
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:14:31 +0000

    hers appeal handle check tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
