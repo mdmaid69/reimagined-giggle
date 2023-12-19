6ecc3e353885bae5f6c7269a5e025d854060fc5e
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:11:43 +0000

    discuss level flat please over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
