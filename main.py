0a0c6a2b221a237feff84d9e21efd58028e39121
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:01:15 +0000

    poor dry part display hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
