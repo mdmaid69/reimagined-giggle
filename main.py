f38cad399db338c0056273ddd737c7ca2a3b5a45
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:49:15 +0000

    sign sense freedom claim sick

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_work(force, distance):
+        return force * distance
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
