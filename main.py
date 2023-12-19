25ccdc47cbc8546c0d8e1555e6de1764dd565362
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:17:59 +0000

    terrible genuine sad decide fair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
