958e14049ebc63449420ee30760d15f768b066ad
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:13:39 +0000

    mother bringdown whom kill walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
