5fa9408aafb2a90a1f6d1e237f70fb89e0fd076f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:34:30 +0000

    story protest debt rub difficult

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
