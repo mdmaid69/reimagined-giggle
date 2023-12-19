509ca2990cb6292715d479cd5322688294ebb051
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:47:22 +0000

    over concentrate trousers ride even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+  def reverse_list(lst):
+        return lst[::-1]
