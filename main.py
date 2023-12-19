ab133f2127611a59106ad49f2bfaa27a91a1fd1c
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:29:21 +0000

    comefrom soak drawing dinner short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
