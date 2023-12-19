0145dd066d9702802e0ab3191f4591d77d974975
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:41:09 +0000

    rely trial smooth future skill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
