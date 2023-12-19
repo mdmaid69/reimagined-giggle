e15fd475444c8faff19262420ff4800510213c29
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:55:51 +0000

    deserve upstairs speak ear situation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
