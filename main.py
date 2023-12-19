6fd28d73e8c6701adb085c9699f628e8bfc71a9f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:39:31 +0000

    language carry funny kind string

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def multiply_numbers(x, y):
+        return x * y
