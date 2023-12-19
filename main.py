1180a72bd59ea7615f7ff58a163f2bf89e63809a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:24:53 +0000

    land turnoff bit charge mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
