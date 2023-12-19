269ea641df2614c55472be359a4204e360ca8f69
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:39:42 +0000

    description treat the brush small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
