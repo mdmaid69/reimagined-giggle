9ff0f299bbe1e86c2de609e2f15d00e1c8409929
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:52:29 +0000

    past move light relation thank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_unique_words(sentence):
-        return set(sentence.split())
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
