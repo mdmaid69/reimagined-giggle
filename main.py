fff04437df1d199d6a67a0a9d3cdf7d31cc25e6d
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:51:36 +0000

    tell fond each touch question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
