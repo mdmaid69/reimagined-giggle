649638ac2e98db9259f35c2388d7d3592702e4c8
mdmaid69 <robot@alexsyw.me>
2023-12-19 02:05:58 +0000

    slide bet either in keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
