f141283a409371136ddc732d709bf11826ef9e52
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:07:03 +0000

    brush better voice centimetre fortunate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
