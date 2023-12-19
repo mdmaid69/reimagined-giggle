371c77c882fb681d41ea36be1c8ab11e067fa3b9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:34:05 +0000

    commit exhibition while mad hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
