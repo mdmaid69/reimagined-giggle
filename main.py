f89b57205f2d9fe96b69a91447ba99f4a5415a9d
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:11:37 +0000

    generally exploration appearance no part

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
