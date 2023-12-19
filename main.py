8fa3fadafa2ed007612d5bb52610957b9fe80539
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:42:38 +0000

    goodbye recover dress doubt nose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_pressure(force, area):
-        return force / area
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
