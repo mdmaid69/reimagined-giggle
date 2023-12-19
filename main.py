6ba19435264b3913e3ac5affed987746ff20430a
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:01:17 +0000

    Zz report talk skin practise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
