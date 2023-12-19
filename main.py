4e4a58b3ece112ac549d7392f1e92d664a38b3f8
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:08:32 +0000

    affect feed quickly effect project

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print("Hello, world!")
