43c3b4eca3f9aee5c90130da6aed6a527e1c91bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:38:32 +0000

    road information fast shock relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
