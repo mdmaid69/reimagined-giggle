ad227bfa66b63e9346de4daa42f732960a2c7eb9
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:25:49 +0000

    report scream present flat contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
