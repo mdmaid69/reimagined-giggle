a62143305164022a582781608222b3d358b3d0f1
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:46:09 +0000

    own giveback priority image crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
