903e1b506017e0cb5575d2ef4b79d38c7369a629
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:10:03 +0000

    save wide mention tool worry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_work(force, distance):
+        return force * distance
