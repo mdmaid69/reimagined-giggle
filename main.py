42c4e63a076d3a86d0fc78cfee0ea2b28192c84d
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:08:52 +0000

    probably stamp ear understand crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
