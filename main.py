f086ac2e2a7d40c25585d880530968ea3eb2f6e9
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:47:40 +0000

    bright punish happen order mix

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def find_max(numbers):
+        return max(numbers)
