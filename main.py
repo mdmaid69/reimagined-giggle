20e564f6af088a557a6c7ff3b1ec202d22a6ae18
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:06 +0000

    shop meanwhile life Oo advantage

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def count_words(sentence):
-        return len(sentence.split())
-import sys
-print(sys.version)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
