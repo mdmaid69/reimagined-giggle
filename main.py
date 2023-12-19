8c894dbdc41ec856fc62a26c060cbbfa0155aa65
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:22:00 +0000

    past display accidental talk face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import numpy as np
+print(np.array([1, 2, 3]))
