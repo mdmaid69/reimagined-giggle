aa0e0fa90a4677318c5bb79718402c80aa0f0e22
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:56:34 +0000

    wing takeaway pretty subject rough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
