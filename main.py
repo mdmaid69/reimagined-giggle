a98899b1e307f5ec2f2f49b1cd13a0ffaaf84160
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:47:01 +0000

    getup himself getoff cost closed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
