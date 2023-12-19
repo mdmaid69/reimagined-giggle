eaa237358eb51a2feef03373309ae50867899c13
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:59:02 +0000

    middle common secret copy clue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
