825341771460cbf291d2d6918ae99a85cb248a11
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:44 +0000

    tradition depend tip quickly squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
