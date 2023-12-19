86025f41c340330564234f6d7321cc3428ca9700
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:19:15 +0000

    few their already cook everything

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
