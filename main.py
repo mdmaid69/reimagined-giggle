4b4c29144299148e8e42c3d58d73944757c1e718
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:37:30 +0000

    matter himself bread Uu suggest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
