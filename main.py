817c2222996c41e14e84b895da14a5cf357184cc
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:50:14 +0000

    lean fat interest final furniture

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
