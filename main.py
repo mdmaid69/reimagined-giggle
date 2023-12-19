92c8c84cb7ee0fb92ac059ead04b0c30c98dd055
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:54:22 +0000

    yours kill frightened brown plan

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
+import array
+def get_array_item(array, i):
+        return array[i]
