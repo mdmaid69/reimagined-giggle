83416178a5d7e74d2bf712a156396c9526485d1f
mdmaid69 <robot@alexsyw.me>
2023-12-19 00:32:00 +0000

    claim react connect apologizeapologise back

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
