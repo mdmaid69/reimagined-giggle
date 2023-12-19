c30c5f2abd36d247926ef1e24bad91abb1aa8d7d
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:06:36 +0000

    consult benefit slowly everywhere scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
