a9e6c47cc2bdd9600a0a1b3062aa265cd027f610
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:51:03 +0000

    off shopping charge Ss innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
