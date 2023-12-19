0df7783f90ea17ab22921453330b5387182337ae
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:41:07 +0000

    petrol react aware print takeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
