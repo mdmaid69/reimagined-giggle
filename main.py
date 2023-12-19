098db5f70ebf21262178c6880262305b75b5b150
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:28:11 +0000

    sea false accidental undo worse

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
