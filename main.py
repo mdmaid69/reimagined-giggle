f25a9b43df2ebbe07ea3aac4c77ebbdeb3c0b16c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:39:53 +0000

    ear far especially destroy increase

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import tensorflow as tf
+print(tf.__version__)
