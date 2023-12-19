d5833f6975be4b2166fbde2bd6d6e5482b5554c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:11:40 +0000

    interesting warm succeed bringback keepup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def reverse_list(lst):
+        return lst[::-1]
+import platform
+def get_os_info():
+        return platform.uname()
