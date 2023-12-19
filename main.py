0448fdb39a287057aa55f2f493bd68b78962f761
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:13:36 +0000

    distant chin remain miss keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
