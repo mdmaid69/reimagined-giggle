a4b659f05ca0286d13a79f0c43b919a3f95c2f56
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:27:27 +0000

    book sport shopping smile top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
