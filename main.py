4552eb783fb4d4689d9f43fb1e4f01d2717ed871
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:55:41 +0000

    top connected large innocent relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
