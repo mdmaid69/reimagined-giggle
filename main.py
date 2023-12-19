f4db7e54bd0be6a031ca7ce5ed65f6c5c32db431
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:09:23 +0000

    guess intelligent play freedom connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
