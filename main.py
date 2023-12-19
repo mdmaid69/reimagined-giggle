1e8812271cb080d4e75dd21ddeae7c8f401d794e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:00:57 +0000

    chief pot centre control break

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_absolute_value(x):
+        return math.fabs(x)
