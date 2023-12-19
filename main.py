09bd09b4ac93fe4f02b4ae10a8c7e1e03bcc7b30
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:55:08 +0000

    aim forgive fight avoid pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import platform
+def get_os_info():
+        return platform.uname()
