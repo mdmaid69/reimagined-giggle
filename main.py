6ca66eed8b12a9d7aa5aff120d6624f4fb065854
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:05:17 +0000

    challenge use slowly tongue deliver

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
