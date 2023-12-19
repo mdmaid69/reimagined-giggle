11ca0738f22d9b6ca3125208e15ccf03aa3e50be
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:44:24 +0000

    coat takeback impossible check man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def convert_to_binary(n):
+        return bin(n)
