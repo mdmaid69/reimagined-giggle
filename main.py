00e3901ef2f5dafd1148e97803770082dbd99033
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:04:50 +0000

    tool hospital month clear together

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_min(lst):
+        return min(lst)
