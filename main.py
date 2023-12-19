78618d86ece5e40585ee6df7205184aa9f3df4cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:15:07 +0000

    learn beyond bus bottom famous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
