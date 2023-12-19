a3ec88df76db8f76251ad1d8debcb6911ea36664
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:10:19 +0000

    ring colour sing famous she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_exponential(x):
+        return math.exp(x)
