f4e5a424cb0550bd2551a019e18f4bd766c4e3af
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:26:35 +0000

    school inside runinto phone especially

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
