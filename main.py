1a8bc8f5dce1aa26278967893d1c584e2f622d4e
mdmaid69 <robot@alexsyw.me>
2023-12-19 08:20:58 +0000

    stick polite friend such throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
