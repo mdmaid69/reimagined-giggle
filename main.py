930da0ac7d6afa0ccdd6491f7e62de3fcfd11410
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:02:33 +0000

    the genuine term fierce worse

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print([x**2 for x in range(10)])
