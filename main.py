0bd927559fc185a59779903477ad3bd222fd865a
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:47:08 +0000

    swimming food but by argument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
