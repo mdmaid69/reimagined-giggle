08fc0c53d6c34e796bf0c6f6bbd9a55467f184d7
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:04:13 +0000

    runover pass cross hollow force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+print(math.pi)
+import os
+def change_working_directory(path):
+        os.chdir(path)
