50e1eb18502f27f6d5ef418645f2bb08ea98671f
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:45:47 +0000

    age especially phone callback control

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import platform
+def get_python_version():
+        return platform.python_version()
