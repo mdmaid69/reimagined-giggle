225c6fe5ac8c51a3a900bf398c59041cd0bdeadf
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:23:37 +0000

    bound entrance challenge work why

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
