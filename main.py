df6392d8ef1934571ab8efc285e5a01544be488d
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:12:24 +0000

    lunch result understand private general

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
