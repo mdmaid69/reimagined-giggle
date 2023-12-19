a876cb518ea697fd4687dc0d2a4e3b26cfad3eb2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:24:24 +0000

    angry hollow it tongue cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
