bdb0570f80938514c6b1311c58aa8b0dcc321ec9
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:42 +0000

    rank closely complaint disapprove entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
