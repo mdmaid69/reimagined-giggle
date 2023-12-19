d14b1d7fc7942183d631d420696669d505d474e2
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:39:36 +0000

    maintain flow sign job entrance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
