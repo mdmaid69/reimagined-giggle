31a85d119d077ecdc29f7a6e014fffccdb7a2370
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:23:39 +0000

    strict position emotion provide dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
