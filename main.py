5edaaed23b20cca0f4244c3aae362c178bea9496
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:06:23 +0000

    anybody everybody pure direct worried

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
