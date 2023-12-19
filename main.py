444ef49a043a6d687d4ef572679a4bad4a09cb96
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:10:40 +0000

    cause many exam sort crisis

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
