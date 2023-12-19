b193d514ceaf90776e3089227e454f2109284b92
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:28:45 +0000

    trip voice rise pin path

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
