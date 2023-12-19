4b7649e66bc24aa6419101010ebe0c1edaf9cd25
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:55:39 +0000

    confident move across sense statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import datetime
-print(datetime.datetime.now())
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
