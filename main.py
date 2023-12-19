0d81639d99d661e679adf0f6a921425c0aa99555
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:30:56 +0000

    thin which interview invite best

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
