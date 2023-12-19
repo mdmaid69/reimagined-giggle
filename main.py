3a9f1f34b2383383e2ea4b34deb2d5877309cf11
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:29:24 +0000

    destroy constantly offend gap justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
