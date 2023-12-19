648b739ebbf01bde33c7021622897bedf73c161b
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:03:44 +0000

    goout yours arrive compare exactly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
