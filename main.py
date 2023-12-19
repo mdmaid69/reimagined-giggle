a69501991d94ad282773174c7726301fa2f7ca05
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:08:03 +0000

    geton into rather unit black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_acceleration(speed, time):
+        return speed / time
