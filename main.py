fe493490753ee421756a7cfb8632c57252089fa2
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:52:54 +0000

    believe record point nerve reasonable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_sign(x):
+        return math.copysign(1, x)
