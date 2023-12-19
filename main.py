fc48f603966956a51e0bf3826a79ed651d56ee17
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:23:33 +0000

    song abroad each regularly above

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_as_int(array):
+        return int(array[0])
