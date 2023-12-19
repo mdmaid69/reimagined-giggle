b0a093a6cc445e0cc595d13ce417ace867a1e8b2
mdmaid69 <robot@alexsyw.me>
2023-12-19 21:00:05 +0000

    cry 573entertainment expect bye store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
