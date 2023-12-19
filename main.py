f4418d04bee2f718b8c3d0e4502457bac8f73991
mdmaid69 <robot@alexsyw.me>
2023-12-19 13:47:46 +0000

    convincing grateful club ear swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
