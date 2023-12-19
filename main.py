5d8f776bc81ed30bd8e17d9487e946d955a48376
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:23:31 +0000

    sense hello interview lesson over

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
