5aa787ea10a8a58d9bf6dd2016f85a1c87abda81
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:18:42 +0000

    dead eye impressive length late

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
