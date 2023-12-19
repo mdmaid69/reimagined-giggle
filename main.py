3a613138b030f3ce63b4417f1a4dc8f34a23b11a
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:59:24 +0000

    gather whether marry month neat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
