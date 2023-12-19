4aa219f0c5a09286d6c370fd8d576ef4b915060c
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:41:22 +0000

    tell address target willing company

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
