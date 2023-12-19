e59f99a65c4bd2dce001c537e4f18999aeaeadf2
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:51:51 +0000

    anywayanyhow terrible occasional remove comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
