b12eb30c3442bb93da0201df345ff21f4cae2967
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:33:07 +0000

    arrive cross shout compete purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_length(array):
-        return len(array)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_min(lst):
+        return min(lst)
