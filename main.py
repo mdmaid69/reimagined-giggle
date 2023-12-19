e83c7f209c4012040268772c7259576c0033aacb
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:05:11 +0000

    calloff score day juice plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
