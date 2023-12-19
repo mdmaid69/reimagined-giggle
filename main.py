5875be3978d80d47588b94346698da9799c21756
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:27:25 +0000

    border contrast constantly pin skin

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
