43ab33495d06c593dbc3d7729ecfb05db649e4e1
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:27:14 +0000

    government ours short share advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_density(mass, volume):
+        return mass / volume
