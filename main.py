962375fc6d0d529ed4a230a4527c2695cc53314e
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:25:01 +0000

    treat awake progress risk centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
