04a8e0def4b652bb5d79ef1e90c0f1fe582d682c
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:13:39 +0000

    difference money surprising relative manage

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
