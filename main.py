969bfaf26823b47efe49e10f82b9337381e9f02f
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:50:05 +0000

    at harm but floor never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
