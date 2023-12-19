0fe749e456e34837f64d1d4e37d1d33c2b10e74b
mdmaid69 <robot@alexsyw.me>
2023-12-19 22:40:44 +0000

    platform eager race air upwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import numpy as np
+print(np.array([1, 2, 3]))
