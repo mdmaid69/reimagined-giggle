fbc3ceec842af9038f3102def4b6a303d71aa78d
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:42:30 +0000

    steady hurry smile team touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
