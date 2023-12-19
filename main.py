d6f1232d260ae7d9e1ac99675056a415a119d7b5
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:27:43 +0000

    hide promise still call read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
