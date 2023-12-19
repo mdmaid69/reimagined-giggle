217aa89585b1ca46ba8f93851b2b5e500866b477
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:21:10 +0000

    artificial relax whom short wander

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
