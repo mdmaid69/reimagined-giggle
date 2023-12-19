cb22852108e8a91ae337440e39eb77ed526327bb
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:43:25 +0000

    interruption pile cool count report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
