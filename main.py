37649b4ccf0fd3f8313fd1fe178415b19f4c66d0
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:43:38 +0000

    mind pressure history bread fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
