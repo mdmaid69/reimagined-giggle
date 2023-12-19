fdeff59b5027cce9f6f42cbcae6fc3ba21dbd90e
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:50:28 +0000

    record variety practise strength interested

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import numpy as np
+print(np.array([1, 2, 3]))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
