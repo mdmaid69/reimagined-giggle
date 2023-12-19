113f4c4ed18206516a31fff41c3c56c5e0298205
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:56:23 +0000

    welcome something plant correct criticize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
