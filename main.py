6e98866bf63a4963ad45827a6c7eae99b0962480
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:44:29 +0000

    calm gentle ear physical warm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def is_odd(n):
+        return n % 2 != 0
