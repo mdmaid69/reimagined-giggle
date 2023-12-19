ef51d6287fcc71e06b5c9d753ea28c07a5136004
mdmaid69 <robot@alexsyw.me>
2023-12-19 04:05:30 +0000

    lose negative slightly basic strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
