d656a8d971145a5077d072f22d33aaeb291024b4
mdmaid69 <robot@alexsyw.me>
2023-12-19 06:58:45 +0000

    importance weekend goup strict squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
