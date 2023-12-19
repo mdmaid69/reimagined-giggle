e1839c7fd49cabb2b89ec1ba9716331de4b03e6c
mdmaid69 <robot@alexsyw.me>
2023-12-19 10:31:50 +0000

    suggestion second cry expect swim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def convert_to_binary(n):
+        return bin(n)
