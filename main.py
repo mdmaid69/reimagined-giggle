69e8997f693c081db9cde42bf75d8ea2046049c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:10:27 +0000

    tie charge blind strike stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
