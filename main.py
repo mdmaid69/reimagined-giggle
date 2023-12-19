305828588f672ae5c2fd2f029d9c56ef83edf9e5
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:03:47 +0000

    swing quarter mess wherever criticism

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def reverse_array(array):
-        array.reverse()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
