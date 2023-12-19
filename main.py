ec085521f023530c0cef8851fbcfcbbb68f084e6
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:06:59 +0000

    pronounce challenge fear everywhere arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
