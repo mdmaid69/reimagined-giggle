d574a42d9ad07445ad2bd4d33b2fcefd69f358c5
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:41:26 +0000

    uncomfortable convincing sometimes mood into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_str(array):
+        return str(array)
