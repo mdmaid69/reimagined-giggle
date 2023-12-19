0800d5ae6b7819d79a742385a19cf7a42811ff89
mdmaid69 <robot@alexsyw.me>
2023-12-19 07:52:38 +0000

    skill city fun emotion borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import time
+def get_current_time():
+        return time.time()
