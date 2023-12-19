560b47d0ef8b2cb739bf88387340ff5b9e2a26bc
mdmaid69 <robot@alexsyw.me>
2023-12-19 19:09:42 +0000

    call cook first pleasant confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
