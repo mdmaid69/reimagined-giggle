bc65df001bea05e9c372a04e96edf9cb2a19658a
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:55:32 +0000

    dream stick star comein order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
