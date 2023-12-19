bd9b2612ad1c81a2a7a57544e9db54de0779b7c7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:06:56 +0000

    harm entrance tidy soak throughout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_queue():
+        return collections.deque()
