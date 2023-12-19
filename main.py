248b8459cb6f403f4079d6cf8110c13574bb4a98
mdmaid69 <robot@alexsyw.me>
2023-12-19 03:15:12 +0000

    breakdown finger watch chase accidentally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+for i in range(10): print(i)
