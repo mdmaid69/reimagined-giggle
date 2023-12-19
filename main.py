a17f55d1a4aba3f5db33a1e7c787e04d48d141cd
mdmaid69 <robot@alexsyw.me>
2023-12-19 23:04:43 +0000

    scare fly hope reasonable must

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
