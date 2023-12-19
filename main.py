2880eed4bb966121ff454f463772b8f81d38a280
mdmaid69 <robot@alexsyw.me>
2023-12-19 05:23:33 +0000

    receive comedown nonsense mysterious hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
