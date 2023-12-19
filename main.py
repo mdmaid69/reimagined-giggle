47c11519ffc17ecc954360b16e4fb7996fc38277
mdmaid69 <robot@alexsyw.me>
2023-12-19 15:59:21 +0000

    window mainly blame fix means

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import sys
-print(sys.version)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
