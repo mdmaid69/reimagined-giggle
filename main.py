acd2b5c9639c539fdf4beb1a216a80cb225f49b7
mdmaid69 <robot@alexsyw.me>
2023-12-19 18:38:11 +0000

    turn me stranger true nice

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_acceleration(speed, time):
-        return speed / time
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
