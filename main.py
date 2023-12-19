e05a2027de5d66c7cbe72731835d70532a49a708
mdmaid69 <robot@alexsyw.me>
2023-12-19 20:41:57 +0000

    yourself people mark country little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
