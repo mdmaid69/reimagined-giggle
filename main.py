d58bc228a78aedd788a7a9718a9921c90af29d46
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:41:32 +0000

    storm size accidental cook innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
