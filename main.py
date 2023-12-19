68e1cf8b713a69428be506bb91e547c3c03fe6d6
mdmaid69 <robot@alexsyw.me>
2023-12-19 14:31:30 +0000

    tragedy beside treat calm rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
