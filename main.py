4cc9b028efdc0b5a719cb813e398f23d2fc54735
mdmaid69 <robot@alexsyw.me>
2023-12-19 16:50:10 +0000

    win foot task break late

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
