093f6476deff432939ebd060d23640cc59177ecb
mdmaid69 <robot@alexsyw.me>
2023-12-19 17:54:23 +0000

    permission emergency spite forgive accident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
