0ccade5d9724781bc8fd53f0a97289fe3fe74a79
mdmaid69 <robot@alexsyw.me>
2023-12-19 09:54:11 +0000

    reply during beside enthusiastic stop

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
