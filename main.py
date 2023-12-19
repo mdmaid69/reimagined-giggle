0cc9b4bf383b3d4b9bd513e21d67bb13dc0ad826
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:19:52 +0000

    seriously ill tradition notice street

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
