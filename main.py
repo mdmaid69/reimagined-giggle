4a28034232451245a4b513f59c5c4d45524ac0d8
mdmaid69 <robot@alexsyw.me>
2023-12-19 12:00:49 +0000

    back travel means escape stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
