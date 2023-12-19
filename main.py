fb4f927bb04fc7e31b4283b57b50138039cae407
mdmaid69 <robot@alexsyw.me>
2023-12-19 01:46:21 +0000

    on relative unfortunate pleased honest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def remove_directory(path):
+        os.rmdir(path)
