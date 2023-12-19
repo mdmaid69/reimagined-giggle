aff18ad507747413577a5ff4c2cde42fae72c206
mdmaid69 <robot@alexsyw.me>
2023-12-19 11:39:41 +0000

    pinch respect through out level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
