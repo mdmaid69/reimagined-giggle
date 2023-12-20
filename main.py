70fc9fad6a7ea6c723c76f550e9c3572b69e6ab0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:17 +0000

    freedom thing contact circumstance help

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
