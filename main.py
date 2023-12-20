591dcba6488a9be0be5666318d99f53a02a9d6e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:29 +0000

    dare long create table on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
