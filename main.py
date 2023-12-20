2997241dab2ef0092100a04fa631a5b650926156
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:53 +0000

    train middle interview soil whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
