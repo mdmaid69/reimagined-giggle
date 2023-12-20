b23d92da3acabe4f778b2b8eea8fa694f71e7d65
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:09:06 +0000

    Oo uncertain worried Gg necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
