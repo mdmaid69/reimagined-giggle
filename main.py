e2537d068dc31bdd8396eb386c80ef503550c194
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:08 +0000

    minute opposite sleep angry approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
