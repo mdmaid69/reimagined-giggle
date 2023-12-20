94442e80dc2cedddc9d8bcad9c22b4770b590aaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:06 +0000

    star modern about reserve temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_density(mass, volume):
+        return mass / volume
