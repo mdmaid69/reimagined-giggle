ac0e24045b49c6df4831b876a32a52a06519da4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:47 +0000

    crowd snow workon exam empty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_slice(array, i, j):
+        return array[i:j]
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
