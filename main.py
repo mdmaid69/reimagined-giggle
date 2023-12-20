e9c17069af1546b0ad5b8d8f6d9b6d1295ec8d1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:54 +0000

    unfortunate hello properly deep spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
