05c1d3d75b0308480fe01d3237c2d112d038e286
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:42:26 +0000

    benefit this upset should onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
