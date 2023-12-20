a0e265f64cdefe914146b1302df26832b2ecd757
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:03 +0000

    blue turnup war wise myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.time()
