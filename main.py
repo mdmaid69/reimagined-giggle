ebdb4fa89c0e7e9ba95c0d85cfdbe3d1c1e83fe0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:51 +0000

    already comment comein miss correct

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
