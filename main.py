0eb023da0987e193e7c0a301d6aa31b7b102f4ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:50 +0000

    song neither trip science unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
