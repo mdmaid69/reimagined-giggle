a2ee29245cc3be7a8fc01d26768f576f148c40c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:10 +0000

    respect light want cheat breakdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
