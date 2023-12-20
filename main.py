a1c3a7076c1edb102e9b43bf7951ca2c7c7974a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:16 +0000

    stamp disturb even side personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def reverse_list(lst):
-        return lst[::-1]
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
