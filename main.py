462faeb23b2c64d9cc01dc64ad91d2467f7639cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:49 +0000

    welcome ignore narrow goout phone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
