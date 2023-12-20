6f79b8ed0c16ba6e10b7135b2aca190f4b54d50f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:32 +0000

    read big scale fit but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
