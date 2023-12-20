e16f932cee9e7293bdd477c9e61c39a5c29f1bc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:25 +0000

    language new shape healthy plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+print(os.getcwd())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
