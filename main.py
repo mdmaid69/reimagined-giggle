1171f4051706c99ecdbd251e4f4379a33c3a58b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:39:25 +0000

    equal correct movement arrangement other

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-def greet(name):
-        print(f"Hello, {name}!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
