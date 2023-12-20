eedc2ccfeaf09d89728f10e8d4d558518c356b7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:25:28 +0000

    dry goup extremely lie bring

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
