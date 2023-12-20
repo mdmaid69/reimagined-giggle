44a1cbdf80c7895b7a72f2610eb959d5f30148d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:36:13 +0000

    goodbye team underneath wish tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
 import sys
-def exit_program():
-        sys.exit()
+print(sys.version)
