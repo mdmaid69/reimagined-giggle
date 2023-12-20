8c473ec4eaaf24b0f471d9bc91e822718d3878ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:49 +0000

    order course recommend however possession

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
