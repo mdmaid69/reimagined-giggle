baa8fb0ff13e9975765516c9d7bf9fd366364618
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:23:04 +0000

    something popular progress grade advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
