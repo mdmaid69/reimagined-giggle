c3ab7d641918bddb446b0f8043076b58912b6931
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:11 +0000

    hour deep pour 565end none

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def sort_list(lst):
+        return sorted(lst)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
