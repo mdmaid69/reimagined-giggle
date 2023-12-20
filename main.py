00e2d4d6faad8f0ddfc6403e7402a962d86eb2bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:02:05 +0000

    joke although before tragedy tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
