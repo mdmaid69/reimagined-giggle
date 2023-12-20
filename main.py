65cf0fd2a57d16eae098356365eb9b2d21e2783c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:33 +0000

    roll would first hurry kind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_directory_name(path):
+        return os.path.dirname(path)
