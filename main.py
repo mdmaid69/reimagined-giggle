a26cb2944c7cb628b3d6769b246f9cebf3fae2a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:21 +0000

    turnaway chin force care surface

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
