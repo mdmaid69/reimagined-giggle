8f9d4fb73573057ff9b945ed1467298ab4a80ae8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:30:42 +0000

    negative himself run next curious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import platform
-def get_python_version():
-        return platform.python_version()
+for i in range(5):
+        print(i)
+def calculate_work(force, distance):
+        return force * distance
