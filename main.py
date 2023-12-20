95251837465882cbd89e4040b1316d727c08dfcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:44:19 +0000

    risk rent impression modern neither

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
