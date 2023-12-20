0736dab0c32bff93ede5edd8ae52a92ab6829de7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:38:57 +0000

    whether think across eat game

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
