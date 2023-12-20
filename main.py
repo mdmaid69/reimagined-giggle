c7dcfdcb0245c1a5646798ab569ceed58735063c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:55 +0000

    model worry whoever port speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
