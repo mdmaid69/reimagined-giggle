d12f22d053f773930ea417782e1551d1a48276dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:54 +0000

    turndown your punishment would conduct

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
