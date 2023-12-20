dded2c4757f1a6cbb75657f72aaf4e7f79fac190
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:59 +0000

    separate hello already ring wind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
