9b0f17df5d405e38052383be02cf44ee672e0095
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:00 +0000

    voice forever joke identify niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
