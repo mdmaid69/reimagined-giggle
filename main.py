cae2408217f63af6b6bc290911bdbf72fae3c69b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:24 +0000

    practical tune stick dance compete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
