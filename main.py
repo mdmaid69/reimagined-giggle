187b06452d0f41470051c9809edcf6c5cd237530
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:27 +0000

    doubt wonder extremely do preparation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
