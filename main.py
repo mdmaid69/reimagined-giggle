594ff68b0068fa437c6170835c256566f031fb15
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:23 +0000

    free friendly pick such juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_max(lst):
-        return max(lst)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
