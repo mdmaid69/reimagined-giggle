7898ffd4d47f09259db4baaa3ccf675ceac4340a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:42 +0000

    fun base through reason cost

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
