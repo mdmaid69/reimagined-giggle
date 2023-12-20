21e2fc092d820fcfdf9398b1b280327320114b3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:19 +0000

    storm battery receive wave into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
