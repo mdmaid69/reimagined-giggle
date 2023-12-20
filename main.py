4e39481af475eb4540d360f817186d1f990145dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:44 +0000

    hospital scream race position object

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_work(force, distance):
-        return force * distance
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
