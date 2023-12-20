e8f1bfe5a6a62355adb7c8cefd0acec4ebfe2fce
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:59:08 +0000

    ordinary bringdown listen strength expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
