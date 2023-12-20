a02ff01ed3a84018f861cfb2d6b40439465eaa91
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:25 +0000

    rank now earn cum spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
