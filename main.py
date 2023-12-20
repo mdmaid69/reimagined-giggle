84b53bacf7ca858c0290f18ea9d836fd6d61b5e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:08 +0000

    cry well in return emphasize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
