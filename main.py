409a829307d3fbac1ce54e3851b8cb86c7dcdb8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:17 +0000

    roll nurse knock regional evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
