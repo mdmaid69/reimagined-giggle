ec4d3c6961e254a050de6f675c78d0e41bb7a842
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:55:26 +0000

    dream indeed slight swallow pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
