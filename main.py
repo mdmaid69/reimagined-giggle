06d95497fd0528c8f80fc3525cc739cc0a96139d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:40:17 +0000

    squash choose repeat effort inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import sys
+def exit_program():
+        sys.exit()
