0f1a21849aa0d7bc3c2e4748182f3898d0adf747
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:58 +0000

    fact satisfied quarter among system

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
