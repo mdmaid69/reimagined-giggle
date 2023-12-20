80ad37f158b4c5fb4c49c932b222c48efbd4bcf5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:55 +0000

    guard tall duty connected by

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
