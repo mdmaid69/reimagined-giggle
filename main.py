85d34916dfc35fda11d74aae18b18b61e9f81164
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:49 +0000

    their dance wall Hh make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
