044c6bc43053171fbd84dae344407022f1640d5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:21:48 +0000

    purpose future somewhere narrow necessary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
