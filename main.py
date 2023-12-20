58e5cfe033e3c597ba26780c83ce19e1afbdefb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:30 +0000

    notice rule comeround money through

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
