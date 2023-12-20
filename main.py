3aff39195ec1efe1013878d9c912e0f851a81118
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:23 +0000

    exactly clothes scared exaggerate occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
