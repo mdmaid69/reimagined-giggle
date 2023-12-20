49b1322f3e2b5305443c7776206851925d621d8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:06 +0000

    though fork but bang question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_item(array, i):
+        return array[i]
