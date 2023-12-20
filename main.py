9c6208da44b4376da4558021053948e5f5041566
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:51 +0000

    experiment conduct however scream willing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
