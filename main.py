6360d494d408fe009d4fe039bbaf218b5fd13e64
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:57:54 +0000

    half us ball in wherever

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
