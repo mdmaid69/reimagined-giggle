f2afe883fde9589ea7bc2fb5f9210663924f0b86
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:12 +0000

    fortunately fear whether pair boil

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
