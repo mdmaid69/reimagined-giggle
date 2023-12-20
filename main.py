8d7d2510536e3a8279fe510c2ad29a225838f1ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:05 +0000

    reduction valuable gooff range surprising

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+print([x**2 for x in range(10)])
