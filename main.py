c3b6ae1be3028978dd9dbcc86d49bd27c3ceb767
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:44:11 +0000

    available concern excuse ago 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
