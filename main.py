713a76d93044765ddf13695395669c50f6fcfdfd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:43:15 +0000

    Jj post us suggestion damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
