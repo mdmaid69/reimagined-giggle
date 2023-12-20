6d368988a941b50d2373f5c190ef788905818b44
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:51 +0000

    combine this nothing relationship platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
