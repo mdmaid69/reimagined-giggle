8a00ecaadf259457a7d32128b11c023c90af7436
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:01:56 +0000

    serve size surprised break emphasis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
