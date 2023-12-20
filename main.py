282fbb86b71fea4ab4ede62710e1eab32176d25e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:35 +0000

    area clean worst show loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def count_characters(sentence):
+        return len(sentence)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
