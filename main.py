36f895edf67888a9758d3ca1ac6808a875abb00e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:09:01 +0000

    doubt science and 573entertainment then

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def find_min(lst):
-        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
