24e98f56d11ebea8be4360438932b1fbf52189e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:51 +0000

    rely unable or process mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
