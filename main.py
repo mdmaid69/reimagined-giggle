1969a8a7449398e783a758d939dafc7bb6b7ef3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:04:11 +0000

    cause wild forget hold pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
