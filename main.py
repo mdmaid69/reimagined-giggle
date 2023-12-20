a88027483be54e1eddeca9fc9a91bcbacffec6e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:37:29 +0000

    downstairs connected workupto at depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
