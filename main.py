c6decdf831f1a165112d6583e6e18146ec66725a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:35 +0000

    go lonely previous before notice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+x = 10
+y = 20
+print("Sum:", x + y)
