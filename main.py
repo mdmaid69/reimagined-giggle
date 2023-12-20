ce11ffb2d8228691c36c097d5e809618d459bbd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:16 +0000

    search least cry equal wipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
