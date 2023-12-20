5c6466e99ce3651e32f2550f1f310918397f1fdf
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:59 +0000

    firm as whichever admire exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
