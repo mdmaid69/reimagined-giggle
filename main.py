f667e91f9588af73215389d5dca91b8e6640463e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:17:26 +0000

    plan law shut patient mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
