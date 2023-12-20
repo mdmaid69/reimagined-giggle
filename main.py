cc63ba9d7270733a1a4e2e08f5037f5f629da655
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:48 +0000

    care last occur exist wire

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
