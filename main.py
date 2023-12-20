0a8c51c22366c1c3b873311631048e5bd6b2fff6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:55 +0000

    weigh unable film search much

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def subtract_numbers(x, y):
+        return x - y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
