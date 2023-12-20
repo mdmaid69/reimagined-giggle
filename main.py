51be2f460f831942d357055bd41b6169ebc54fcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:30:54 +0000

    bin available little breakup ring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import os
+print(os.getcwd())
