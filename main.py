ea39c15505028b11d461accef6ebd4e8550fc5d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:53:26 +0000

    want fall flow guilty on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 text = "Hello, world!"
-print("Words:", len(text.split()))
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+print("Is palindrome:", text == text[::-1])
