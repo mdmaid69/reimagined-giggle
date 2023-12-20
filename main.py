4f46b5de8e5710eb496d69b7af5aa7538ed7d49e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:53 +0000

    final original suck smile switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
