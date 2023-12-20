9767ddb1f6e2322fb6cb49e2d4aace3487db2816
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:32 +0000

    card destroy mine building collect

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
