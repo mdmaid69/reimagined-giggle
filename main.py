671cd6764f163a889489a541a98710c1550c5bcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:22 +0000

    unusual crash smile choose giveback

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def add_numbers(x, y):
+        return x + y
