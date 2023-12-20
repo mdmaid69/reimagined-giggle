f7581f4e00be834a1b550e1bcbc554626a66d637
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:31 +0000

    hit zero clothes run put

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
