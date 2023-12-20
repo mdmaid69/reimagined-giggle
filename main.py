ffa9ba06782bc6f8b5c1b38834b6ce7dc330b579
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:31 +0000

    trap thick steal nervous complain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
