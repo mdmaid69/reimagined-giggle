5ba0555792912e250c397686543edc4d106c9d7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:36 +0000

    awake stretch spread pile 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
