1dcca46585c6a0df2cbd2a457b1232188b4a7be1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:29:59 +0000

    opinion straight stiff their face

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def remove_duplicates(lst):
+        return list(set(lst))
