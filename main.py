ae885f1c4a6a5b511a572b4ffee72f0db6c539eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:18 +0000

    hold Gg conclusion comefrom town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def convert_to_hex(n):
-        return hex(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
