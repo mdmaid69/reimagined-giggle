e96250810accd40d07cc4840e1c4a18dccb3cb53
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:29 +0000

    under thumb gift certainly accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
