ff3a3c9c09ffa6c2734de9f386ac60c7f899ddaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:56:39 +0000

    between partner before unkind keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
