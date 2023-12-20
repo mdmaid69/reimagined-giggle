550eb02975c1f15fe7a4b2bcf442b98f2256598b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:46 +0000

    book factory black watch might

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
