7d0b09a455a849dc6896073101232f10a1ab24bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:29 +0000

    petrol crash dangerous bend concentrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_length(array):
+        return len(array)
