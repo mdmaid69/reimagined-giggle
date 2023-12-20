986dae8e2fd2d14bde384d951f64536749cc31d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:07:21 +0000

    criticism recent platform 569enjoy stir

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-print(sys.version)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def reverse_list(lst):
+        return lst[::-1]
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
