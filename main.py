d8b593bb2b049262c008e8e0b80aa25a7febae77
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:34:45 +0000

    line compare not string wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import datetime
+def get_today_date():
+        return datetime.date.today()
 import array
-def get_array_length(array):
-        return len(array)
+def set_array_item(array, i, item):
+        array[i] = item
