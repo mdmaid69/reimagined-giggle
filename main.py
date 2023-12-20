34a55c940f201bc938719501e093f7d7f666d014
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:00:25 +0000

    force door bang pretty near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
