8943540e738263205af45e3d256222d7d3957fea
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:21 +0000

    available chat university tired hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import getpass
-def get_username():
-        return getpass.getuser()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
