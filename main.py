116a2ab260cd92799b2ffb7b487936f263ae39be
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:34 +0000

    turndown practise reliable protest smile

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def convert_to_hex(n):
-        return hex(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
