2af562b0637fc2766e33a41fa674dbed1f5c21bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:34 +0000

    getback extremely rank total surprising

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
