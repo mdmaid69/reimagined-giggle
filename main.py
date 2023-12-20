6f2ad32194079e355edac1e9bcbe9202a379850c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:50 +0000

    compete point illness hello whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
