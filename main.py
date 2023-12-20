85e50c6e84127460d1b052031722241588be3930
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:44:21 +0000

    catch care cruel flight consult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
