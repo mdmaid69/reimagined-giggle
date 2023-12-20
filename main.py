540dba71e4ba169d8f7b167cd7038a2bada67124
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:20:43 +0000

    few chin especially obvious and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_density(mass, volume):
+        return mass / volume
