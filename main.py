a2feb5f5f0aba141188d4a69a1edf04e96cccf60
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:24 +0000

    brain move excuse sure rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+print(os.getcwd())
