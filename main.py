dfc5f94cfb6353efbdca28b9383ef3d0ea661bb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:49 +0000

    sure sign connection border admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
