2a6fd198cb8e026ca339a00321f7b12e989986d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:09 +0000

    freeze cheque tired gate side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-print(sum(range(10)))
+def convert_array_to_bytes(array):
+        return array.tobytes()
