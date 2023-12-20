c37b732fc8031205d95705fe947201fed8ba0d25
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:37:44 +0000

    emphasis great second relationship be

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
