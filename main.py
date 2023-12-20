2c80063e8794f5e60177fdd68348980bb10028ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:39:43 +0000

    shopping night temper official birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
