32ee45fbdfe7a7df6bc6e996deb5ce761d1fa4f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:19 +0000

    worse sight excited search speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
