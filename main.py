0d1019b30542d881f382d9ba59deb5a025ecfbc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:23 +0000

    off fortunate deal tear greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
