ce9e02f8d2282d9fe295fc92a1fb75f96dd7fd57
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:59 +0000

    convince Qq offend development defeat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
