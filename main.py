4a7b2931963f8cd7df762f1a4c7b497fa0c0aa13
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:20 +0000

    wish friend paper later throw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
