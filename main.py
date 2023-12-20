e2d9bf9588c489ff50f9fd576f27ef494d26b75b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:56:48 +0000

    burn manage deliberate part hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
