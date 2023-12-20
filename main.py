4fb2f4e79dd87ca5126016ad85fc03e2e198b9fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:49 +0000

    hospital mother consist dark city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
