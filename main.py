e56a56d0341b92ab544d59368c65c2b06298c005
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:34 +0000

    around remark yourself estimate record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
