9bbf55fd752f0fc0abfab1af79929613f6be3662
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:31 +0000

    rest sore present loose his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
