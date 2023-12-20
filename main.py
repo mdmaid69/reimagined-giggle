aa05a468853338e9383455cc53067c42eab423de
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:26:38 +0000

    everywhere decision go division related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import tensorflow as tf
+print(tf.__version__)
