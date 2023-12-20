f426da97e2973d09214569b8bb27fcbe4d5bf796
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:35 +0000

    duty subject rule come wise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def subtract_numbers(x, y):
+        return x - y
