fbc9fdc23ea299aea3dde8dbe951980cfecaabf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:40 +0000

    thankyou finish cough yes majority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
