30bae9cbe361ce12ea331c642eaa6cb86439f98f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:47 +0000

    complicated power sleep fire oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import os
+def remove_directory(path):
+        os.rmdir(path)
