4d591e9175ea0584ff0df9864b913771e2551d46
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:05:08 +0000

    before include mainly position watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
