e1ccd34ce05ac79a5e64f3ee5b96050b3c13b105
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:49:06 +0000

    claim deliberate involve tend until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
