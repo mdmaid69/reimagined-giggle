794eeb82c657aeabad0008cde7a3f8dd9f481132
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:44 +0000

    right rush means shock common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
