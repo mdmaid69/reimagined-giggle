960ca62ec6d0ea18b8babca42ff2a36cbe6da857
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:44 +0000

    fork close train thinkof hello

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
