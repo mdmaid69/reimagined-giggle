ee97db9304aa8341305fd71bfc19600a99c21e4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:19 +0000

    surface provide cry wide breakin

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+print("Hello, world!")
