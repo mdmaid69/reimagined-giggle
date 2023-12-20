b3a3a7b4754a09c78bad1be9d3795599e7948239
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:05 +0000

    load quiet attention fall bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
