5e9960702c09d7445bf053c54acb5f709756d598
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:15 +0000

    afterwards often only heat goout

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-print([x**2 for x in range(10)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
