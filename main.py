0ca843991d06f731e105874cd1ec74bd3547a687
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:24 +0000

    culture gas borrow tape behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
