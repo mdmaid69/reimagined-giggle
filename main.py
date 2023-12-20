0988b5493e40e9e8300810893c994b38f7fe77f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:28:11 +0000

    completely large long part calmly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
