95daa47031660fc8d32e056b97854f32d9535df2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:52:14 +0000

    politics much already raw no

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
