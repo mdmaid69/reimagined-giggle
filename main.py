5cab0d4a3e3e3e1bd6b42c57faccea00fa7f57ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:17 +0000

    exact accidental lie wet surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
