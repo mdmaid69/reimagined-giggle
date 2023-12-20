6c1586aa00e30b09beed47789e470df567c00db7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:49 +0000

    wheel welcome acceptable slightly activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
