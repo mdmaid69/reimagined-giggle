fdc039f517e57c2ab549e3adb07c7f141f15768a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:35 +0000

    future this rain during hospital

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
