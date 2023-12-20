a88bc70e7775cc796611698dd658aa05a0b7dff1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:03 +0000

    first wear consist gate circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
