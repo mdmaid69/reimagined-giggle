c507ad1f72f509772fa21e7b1c37c50796c014ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:46 +0000

    muscle especially contrast memory like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
