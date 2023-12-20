5a6acad14a67d3fdad3dd63b97cac864a6c9f855
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:01 +0000

    plenty acceptable rest Nn snow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
 import array
-def clear_array(array):
-        array *= 0
+def get_bytes_from_array(array):
+        return array.tobytes()
