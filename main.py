1d746b44de4e3e3dbb57fe04a9a286c55c0fec28
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:33 +0000

    object tip quarter pin bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def multiply_numbers(x, y):
+        return x * y
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
