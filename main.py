c7e971f06f9aa3402d9f836c19a7df52be6c6409
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:21:59 +0000

    convinced realize than lose pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
