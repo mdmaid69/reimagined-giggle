bc490d5c338084b3922c58d6f0d747e8872d3de5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:51 +0000

    sink lock score stage pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_list(lst):
+        return sorted(lst)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def extend_array(array, iterable):
+        array.extend(iterable)
