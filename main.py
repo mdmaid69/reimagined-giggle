cd474fad83b782e6bbc097ceb812dcb3cec64a0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:29:41 +0000

    off giveout decision organization risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
