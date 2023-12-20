3be84154c37c09a977e76da4ffead40971ab8533
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:24 +0000

    quickly stop mail occasional run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_area_rectangle(l, w):
+        return l * w
