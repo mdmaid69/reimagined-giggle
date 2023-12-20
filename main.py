8fe4d23c1fcdb1863848e8cd4879d8dfa2ce3a8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:58 +0000

    glass try beginning different unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
