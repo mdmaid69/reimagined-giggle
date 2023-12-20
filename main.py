920be68d3a6a7677e34cd6d92438473d4ea1c6ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:25 +0000

    think no side black rule

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import heapq
 def create_heap(iterable):
         h = list(iterable)
         heapq.heapify(h)
         return h
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
