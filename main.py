e749261f51d3c3278911f352aef471c9a5b78bdc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:30:28 +0000

    score several dress angry mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
