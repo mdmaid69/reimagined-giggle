7598a89a406f3484d41406669d6de04ebf8f44f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:58 +0000

    survive sale between high habit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def is_even(n):
+        return n % 2 == 0
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
