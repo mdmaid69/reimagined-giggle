a76fcda2631740ed45bae0b9f47ef510b7686ede
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:44:36 +0000

    between clear exercise storm replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
