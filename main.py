e4ae0e64eaf6b233f3e7ad7f548a15ed99e97af4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:57 +0000

    fly ticket learn knock pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
