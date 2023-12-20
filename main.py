b5f9ba5a098cd521459d7206fe90134354f48b9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:58 +0000

    mainly fail regret try once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def count_elements(lst):
+        return len(lst)
