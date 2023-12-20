da289bf12f1f50b8b3a7ef601fc0e8afaa56d4e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:04 +0000

    lucky later bag face friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def square_number(x):
+        return x**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
