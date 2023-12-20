25edd8386c9eb2360f625af0870e0d67dd40779a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:20 +0000

    mend break increase mine there

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+def count_words(sentence):
+        return len(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
