dfda3c87833911fd15c8bf0470c44a5cef1681d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:56 +0000

    rise old love appeal rob

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
