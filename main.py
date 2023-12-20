95b3f081b6cd6fde31fc44da6bd2fc7539f3dccc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:01 +0000

    home open neither period shut

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-for i in range(5):
-        print(i)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
