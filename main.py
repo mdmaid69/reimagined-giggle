d59f98c89f20f8fff3ad833eed2c4bcf05cdb01f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:30 +0000

    decide everyone depend pinch cope

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
