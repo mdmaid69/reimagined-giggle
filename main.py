6b130841889461c77cd95cc07352c87357512cb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:57 +0000

    change left free yes fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
