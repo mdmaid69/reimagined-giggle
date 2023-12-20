97ab0f398f359abefa1da8388b0fd4e931ddda69
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:31 +0000

    already powerful possible but since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
