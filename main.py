cc4db5cbff2f9fa9ee47d22de5e9de4e6ae00254
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:04 +0000

    bridge slowly formal answer talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
