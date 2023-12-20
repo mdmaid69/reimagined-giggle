ca1000689cd4a5c7ac3de42cfad128cf151eb57e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:37 +0000

    I quickly best keepup shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
