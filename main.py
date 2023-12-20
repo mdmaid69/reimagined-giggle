b3b9f7c570ba1d0c43c3f6aceb0530a23770879b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:49 +0000

    suddenly university black meal circumstance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
