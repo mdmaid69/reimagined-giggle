c3ff0209e2120e73c4fcf113bec5fe28f12a0bf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:21 +0000

    worst innocent since by frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
