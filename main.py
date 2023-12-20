b789f71d89260cc03d6844f659bd79d625f735e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:04:43 +0000

    please promise comeround keepaway pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
