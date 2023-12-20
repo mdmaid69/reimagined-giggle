8724d88cca4ac3c6464f9b1c6b1a61928e5bb686
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:03 +0000

    thing out talkback sell software

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def find_min(numbers):
+        return min(numbers)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
