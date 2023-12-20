eb738c64bd992fabaa56baea67b678ad6d126916
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:38 +0000

    movement breakout pin still future

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
