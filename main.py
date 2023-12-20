0119340744a011e8b32f1f11f8334c37750bb0b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:08:05 +0000

    basic police dry cheek account

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def clear_array(array):
+        array *= 0
+import sys
+def exit_program():
+        sys.exit()
