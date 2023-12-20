fcf143c3cdfb1883996f78be0534c9331d277b5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:23:53 +0000

    account begin offend be second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+x = 10
+y = 20
+print("Sum:", x + y)
