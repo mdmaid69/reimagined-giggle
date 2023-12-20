5fbea4a54d8ae2ad1c0c041d48b8276215c5521d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:05:36 +0000

    juice identify shout eager draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
