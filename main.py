3bc2b1e35fdf3db83d6465b2b8445d85d84c4b9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:58 +0000

    ground medium environment brief shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
