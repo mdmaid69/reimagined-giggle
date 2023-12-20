602689a0976bd097da3236b7d7b824d299c9ddb6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:23 +0000

    space last nice speed person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
