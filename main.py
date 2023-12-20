38a6ecf14546c54ae8fa26eeafadd4c27f2a184a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:27:49 +0000

    run beginning live talk information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
