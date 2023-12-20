1aaa29e206443e22cc23b2e60d3abc2d7719417b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:16 +0000

    practice ordinary switch runaway note

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
