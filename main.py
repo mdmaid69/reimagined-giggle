6ccef1a47095dd43de63a882037155244c2f979e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:25 +0000

    pay feeling instrument shoot all

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
