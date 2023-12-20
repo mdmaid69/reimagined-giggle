91a44fb0debf33eb39da81df73b394e201989e42
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:34 +0000

    back give race off star

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
