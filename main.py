a22b7da4931dacadb19a930e9d99fd6327273068
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:49:25 +0000

    winter form this offend fill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import collections
+def create_counter():
+        return collections.Counter()
