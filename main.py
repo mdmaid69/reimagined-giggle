55d66e17e152c20012968a5c2072aa89f3db6f8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:22:11 +0000

    my nor bringdown average no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
