6f2fd3c97c259f7b17c0f4631f623e81a8c5922b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:03 +0000

    stretch advise save argument bottom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
