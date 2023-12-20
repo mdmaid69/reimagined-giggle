ad3a742488275ca08801a23f2145d6424cd60ee4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:24:07 +0000

    attract chat model love Rr

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def is_even(n):
-        return n % 2 == 0
+def count_characters(sentence):
+        return len(sentence)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
