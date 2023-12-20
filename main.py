18dcb4641c74db873213a6a8ead0e48812aa43f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:34 +0000

    talk hate close prevent alive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
