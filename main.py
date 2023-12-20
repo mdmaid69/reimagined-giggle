ffe30cd50ddab6a8bc07111d1c1a8f65b25c5059
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:09:45 +0000

    crowded Vv count new since

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
