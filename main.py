3f39d0c69ab4332cf87e8829f3cd3869a2b65120
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:17 +0000

    secret minimum help path spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
