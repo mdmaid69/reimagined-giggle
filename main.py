563951a3d204a62503029fc05641307cf5020a86
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:55:04 +0000

    best process disturbing doubt floor

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
