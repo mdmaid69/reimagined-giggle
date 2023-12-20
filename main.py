81fab67374a7342bc86ec80aab00412b93033a69
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:21:09 +0000

    anywayanyhow scream zero interruption it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
