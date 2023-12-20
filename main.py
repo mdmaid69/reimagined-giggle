bb80688868955da92719f8af9917a938cb16d999
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:56 +0000

    clear sail stage possible intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
