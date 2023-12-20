876858306cbc3f6849031343f3397b97fd0598f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:28 +0000

    push promise react address information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
