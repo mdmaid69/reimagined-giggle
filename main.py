993d4247b4c806d6f06c35e443b6e3db4ee93976
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:16 +0000

    valuable upwards sale ago keepout

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
