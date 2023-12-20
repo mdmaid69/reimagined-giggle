74e9567429d61421018889cb5e58c74b41157a33
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:49 +0000

    pronounce walk respect 564encourage release

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
