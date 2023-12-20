c34036f7ef8c3cfe37fcdb9bac6fe9856304dbe5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:25 +0000

    physical catch comefrom short way

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
