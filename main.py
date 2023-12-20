5bffeba4e7e1ca2a5df4ef864479f9e60a780b42
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:00 +0000

    somewhere noisy age treatment similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
