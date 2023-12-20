b98b64e46ca4fb4e0fa7a5423fcb5a33f1172cb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:41 +0000

    pain shade arrangement boring influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def calculate_area_rectangle(l, w):
+        return l * w
