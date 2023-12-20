e9f7b19952c9b43a272926a1c8c41a88c711c030
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:55 +0000

    attach raw runover level exit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
