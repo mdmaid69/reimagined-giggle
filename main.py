33320e9062d464d6bab42a7e095ff492c7467175
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:35 +0000

    only stick quality number plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
