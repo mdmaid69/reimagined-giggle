fb0526181c24e23566b68e1a7ddd6f1b6f1611e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:09 +0000

    record rob tomorrow politics air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print(sum(range(10)))
