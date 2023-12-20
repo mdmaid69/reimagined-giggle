d96294d3f8cdb01c52f028cdea82f1f5ddbe96c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:14 +0000

    turndown community cinema compare exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
