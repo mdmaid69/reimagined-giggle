66974c47fff74ced69e832b799a92ac9b2401d7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:05 +0000

    noone precise breakout keepup 573entertainment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
