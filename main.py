b68c4d7b96a3d70d85b3ff2b3b4e734a34b02bb4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:03 +0000

    separate smell dress play defend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
