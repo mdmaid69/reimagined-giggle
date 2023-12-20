f52093a40f5310d28f7ccd18fd0381be7802496b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:16 +0000

    bread OKorokay mark shake advantage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
