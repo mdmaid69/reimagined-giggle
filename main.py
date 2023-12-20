dafbf4236dc4fdd9be3a08f99fa1537c33083227
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:42 +0000

    luck great importance poor mood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
