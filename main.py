7536496ef7f72f366be8237cc6a1e1b48e84b7ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:41:04 +0000

    date commit peace practise shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
