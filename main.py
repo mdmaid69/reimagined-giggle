18ca5cdfa04d004c14666ed76a11f10a10eb1b8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:30 +0000

    word instruction involve try store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
