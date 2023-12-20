ac71515f6ea6439eb303eb7ab9cc23212469222c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:26:21 +0000

    once substance slide lie ticket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
