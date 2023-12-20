65bde66897d710a99f4841342adc454ee30388ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:15:22 +0000

    threaten reply alone pump sleep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
