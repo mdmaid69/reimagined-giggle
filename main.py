d6328f507366e8ce656279c5a7ec5011f9d152bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:32:42 +0000

    spare smile shock home private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
