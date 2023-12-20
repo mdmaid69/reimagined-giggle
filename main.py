5a044087b23afb97daed2ff7abc95bbf8d570cae
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:12 +0000

    tap anybody divide embarrassed sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
