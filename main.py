4ad86eb7b706c1fec4a5c71ca4a0dd5839b03bf5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:25 +0000

    somebody roll measure pass subject

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
