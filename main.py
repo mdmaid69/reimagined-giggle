9cd9175b3439ecafa9c38d535aec5c2a04b39382
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:19 +0000

    door whenever themselves flat sad

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_power(work, time):
-        return work / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
