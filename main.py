4dbd3aaf0013987b599137f022e83d67538867ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:19 +0000

    sleep home bin clear spill

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
