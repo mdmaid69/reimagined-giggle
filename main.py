2f0e61dfec5ce6aa05d94e49c5b329c751859dc4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:25 +0000

    OKorokay far borrow report holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
