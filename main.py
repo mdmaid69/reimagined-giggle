fd5b11f215e47a3f5d256bfe7617c13eb820ec9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:17 +0000

    important when permission dry turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
