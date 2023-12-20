e0cc49fcf7125de7e0b92a580043f0c53a3ee0d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:22 +0000

    soil finger natural laugh honest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
