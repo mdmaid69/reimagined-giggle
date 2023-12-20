ca0a417bc4b6fa254611f096a55edb1208f777b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:01 +0000

    chat dance strike hardly uponon

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
