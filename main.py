7e3457ca1deba0f828ff709d297c7f537b316b20
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:16:15 +0000

    pity separate polite share the

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def square_number(x):
-        return x**2
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
