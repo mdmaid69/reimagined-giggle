6643e32bf3715813cab0365aa68e4018324a86b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:48 +0000

    building need dark standard release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
