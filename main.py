d2c37a2d745a7648571a75080472327b1e6c7b26
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:16:43 +0000

    hole dear confidence admit push

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
