1f4ca4f2f56be984823fc51320bf280ffd67d0a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:25:19 +0000

    method account wish autumn last

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
