2f47a39707db93cce994aff3530af09e64770c37
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:58:32 +0000

    none whistle blind treat workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import getpass
-def get_username():
-        return getpass.getuser()
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
