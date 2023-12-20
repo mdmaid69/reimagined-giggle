3944f6a10bebbc25e65c473dbba2338be1f81943
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:42:13 +0000

    trap accommodation crisis Ee border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def print_python_version():
+        print(sys.version)
