5e87ed8ae526c3e03d3c3c04d8f8c08147f38581
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:29 +0000

    home description soil none mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
