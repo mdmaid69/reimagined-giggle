6dca33333c8e63cff85e785e67b5d1a52c69b45a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:49 +0000

    load empty guess temper children

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
