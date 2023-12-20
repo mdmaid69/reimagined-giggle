df9ba9c353e77211c7454c2bb50ef84cd169ca26
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:17 +0000

    operation then herself description ring

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   def square_number(x):
         return x**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
