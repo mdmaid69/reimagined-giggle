3c89db7edf9940f5fcd860569383dcdf1564465c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:29:06 +0000

    comeout copy evidence clothes sting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
