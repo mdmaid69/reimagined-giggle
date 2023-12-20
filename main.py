01f3f4aa71153f1c50bcb06feb2adc54981cfdc3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:11 +0000

    better total protection getout marry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import time
+def get_time_since_epoch():
+        return time.time()
