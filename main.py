4be8890f964efbba9d5e754e6607340b5725af02
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:06 +0000

    become now somehow anywayanyhow name

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
