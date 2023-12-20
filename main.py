607fd7fb323c68c1430aa8b3498b5550cd334a89
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:40:42 +0000

    fire about concerned blow inform

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
