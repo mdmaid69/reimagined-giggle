dc74f342050210955bc07bd0493d67ec9f76ac2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:53 +0000

    brother glad speak exhibition need

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
