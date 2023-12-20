aeee61a73eeebb1bd85dbc7ce7fca965e3cf4afb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:46:43 +0000

    fall not public comeoff complete

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
