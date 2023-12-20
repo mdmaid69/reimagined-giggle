840bbfbbcc1e860c8df74b96737dfd4fecf507bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:23 +0000

    obvious relief vote last interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
