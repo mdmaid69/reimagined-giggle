74ba72282558b0b5010b89f5e19894fc087ff40d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:32:32 +0000

    circle right believe round read

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_typecode(array):
+        return array.typecode
