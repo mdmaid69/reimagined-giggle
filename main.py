bff4f53e139143009ec7b43b5b669b0a3092ba8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:20 +0000

    position crime sink separate expression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def divide_numbers(x, y):
+        return x / y
