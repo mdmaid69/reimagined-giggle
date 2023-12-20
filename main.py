c62c89d9510b399127c73dbbd011bae78bbad842
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:47 +0000

    top forever down actual month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
