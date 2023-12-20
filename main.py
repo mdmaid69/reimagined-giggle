6c94ba6ba9da88ad95f01263a9d3e92ad15d5d86
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:16 +0000

    cause close heart fire because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
