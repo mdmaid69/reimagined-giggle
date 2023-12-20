6745af1e2ec92b9e31c937a0d0bbbf2aa65a330a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:21 +0000

    quarter explain perform recover promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def convert_array_to_string(array):
+        return array.tostring()
