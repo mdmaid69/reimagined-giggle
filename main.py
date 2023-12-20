3b3a247c4bba7470de4714906c35a72d501b41bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:29 +0000

    snow spread argue instruction welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import random
+def roll_die():
+        return random.randint(1, 6)
