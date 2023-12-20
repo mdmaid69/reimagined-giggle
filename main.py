3ae1052c2c031ce9fd2ae18113afa42dcadfd459
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:42 +0000

    last discovery hear push impatient

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+print(os.getcwd())
+for i in range(5):
+        print(i)
