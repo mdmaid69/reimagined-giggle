47633006ba39af02073aa76abf0d8ebef8de6eff
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:15:36 +0000

    afternoon top gentle along correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
