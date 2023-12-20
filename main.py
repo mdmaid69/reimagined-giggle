f7376017cb5f17c7e3f115f804de1e54bd85f313
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:18 +0000

    wake hungry yes sweet related

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
