54290eb47bd1e6b495f318d8c5f17eccfd793449
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:55:30 +0000

    stamp pressure late find fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
