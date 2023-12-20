a145961e7af888afdf4b9af7a701fe80d99cde23
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:29 +0000

    around shopping compare direction weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def append_to_array(array, item):
-        array.append(item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_user_string():
+        return collections.UserString()
