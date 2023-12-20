dc7aab4421cd7e07638699fba392c61c28b048a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:32 +0000

    spray used treat breakup offer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def cube_number(x):
-        return x**3
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Words:", len(text.split()))
