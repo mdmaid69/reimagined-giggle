d56d9e6cb0bfb67b7711407460808716396d3261
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:44:20 +0000

    broken regular right period takeaway

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
