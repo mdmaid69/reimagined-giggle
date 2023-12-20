10930deb40726c29f649542571f128eb56973dfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:29 +0000

    test hair high late impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
