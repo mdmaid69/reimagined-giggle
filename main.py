f58fb200cc8c91d84064cfa4701a43f38e8a31e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:20:36 +0000

    suck impressive breakfast success confusing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
