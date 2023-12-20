cdbd95ca6a3cb98a9283ca4eda736616bcec27eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:41:31 +0000

    limit before either bringdown open

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  def is_odd(n):
-        return n % 2 != 0
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
