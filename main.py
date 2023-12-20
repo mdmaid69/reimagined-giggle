ff3d527e610de34d73f826faf588294953c00d47
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:40:04 +0000

    warning luck stone greeting improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
