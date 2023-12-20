94df9dc7a3b0b01dd3958b7e04e336c818943ad9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:08 +0000

    freedom left invite guilty change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
