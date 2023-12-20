210e1b336d5422edabc9a81804c7d56ab187534f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:53:47 +0000

    her bury practical fortunate disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import platform
+def get_os_info():
+        return platform.uname()
