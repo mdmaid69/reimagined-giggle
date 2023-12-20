5897fcb00cc0703c393970ecd1fedde2b935cc05
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:29 +0000

    order waste service language copy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
