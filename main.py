a9df40a928683a7a98a4f10ece27baa8478da23a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:42 +0000

    unfair slice simple shock cut

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_counter():
+        return collections.Counter()
