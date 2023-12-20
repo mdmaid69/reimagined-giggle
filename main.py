1be9af80191b6f4c17363ab945d3d84e03e849a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:43 +0000

    style like either laugh flight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
