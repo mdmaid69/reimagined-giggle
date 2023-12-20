989ba44b58b7bac6ae55bf2e238298a7eaa24e17
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:40 +0000

    sing race combination nor after

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import tensorflow as tf
+print(tf.__version__)
+def sort_numbers(numbers):
+        return sorted(numbers)
