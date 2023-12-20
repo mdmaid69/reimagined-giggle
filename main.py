e27ce05ee62809dab1b7904bfa91371abe12d1cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:40:28 +0000

    strike afford stone steam cut

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def add_numbers(x, y):
+        return x + y
