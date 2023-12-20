158424d441a0e073f0e8d59758f16c32f6a00ca7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:49 +0000

    wake possibility splash increase noise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
