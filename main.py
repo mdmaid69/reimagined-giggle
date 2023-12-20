e8651e46761deda588a2d61de1924fa94006fbf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:04:14 +0000

    worry poison left this later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_acceleration(speed, time):
-        return speed / time
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
