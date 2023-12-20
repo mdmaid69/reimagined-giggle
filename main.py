718999b0774a5807bbc065047047c58607f35788
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:11 +0000

    giveup store friend boring hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
