88c74f61551120a0cd3459df5a8b3c7190b94822
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:37 +0000

    near smile limit shop characteristic

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import time
+print(time.time())
