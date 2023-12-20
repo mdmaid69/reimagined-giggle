801b0aa93ae5c9d261bb34bc9652fb72d2bb9ae6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:25:01 +0000

    collection joke basic although cruelty

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
