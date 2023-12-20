07fbcc15cb8ae4c32e28aebb28319885734d6d0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:17:33 +0000

    pity friendly impressed basis tape

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
