3639316fad294d6fc613d861b18dc1cae726c433
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:02 +0000

    basis bet offence phone fire

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+print(time.time())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
