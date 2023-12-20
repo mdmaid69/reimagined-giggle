cf0d734bd6f27801671200137f460e3a8548666f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:44 +0000

    interesting contain godown means board

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def subtract_numbers(x, y):
-        return x - y
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
