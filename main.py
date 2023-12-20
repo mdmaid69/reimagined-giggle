67ff6f8d441803d1be544cd55b1cfab197687c02
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:38 +0000

    recover speed money press separate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def convert_to_hex(n):
-        return hex(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
