d978f0f297eeefa06b4a1468f60a9c1d16abf838
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:33:27 +0000

    dream arrange introduce search knowledge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
