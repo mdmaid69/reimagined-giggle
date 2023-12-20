c1d927c20498e351f5c890e29aafd76972e75ce7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:27 +0000

    word pile object number usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+print(os.getcwd())
