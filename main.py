7a73379e30ff17c56a459d07c91e1765d7069536
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:38 +0000

    sail enthusiastic replace duty rubbish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
