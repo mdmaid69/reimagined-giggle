42a39a02534e38c3919a7d91d16b8309326d136b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:06 +0000

    nervous than forget nerve him

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
