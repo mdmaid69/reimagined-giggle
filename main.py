385e1f5f8e02ec0a0e7f1db278c2cd434d80204c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:59:37 +0000

    display uncertain crash good appeal

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def cube_number(x):
-        return x**3
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+for i in range(10): print(i)
