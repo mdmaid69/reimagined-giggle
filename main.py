71627c44981c81d22ebde7f77ef1b04e5e32ac29
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:53 +0000

    smile arrive task people possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
