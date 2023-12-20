07e7f824154091847e975aa3b4a265117b56b134
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:57 +0000

    persuade opinion impatient through fact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_pressure(force, area):
+        return force / area
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
