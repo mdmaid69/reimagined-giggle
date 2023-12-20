3f9b7cc03fe44b3a466f667fb1384f7c62f826b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:59 +0000

    pollution finally heavy natural used

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def count_words(sentence):
-        return len(sentence.split())
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
