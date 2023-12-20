09701ed120d9d26e4d2c84dd9e0b69a9ffe952ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:14:58 +0000

    alive tomorrow regularly and comment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
