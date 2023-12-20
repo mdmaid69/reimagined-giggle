0f08c79a4b657b536da4c76b9b3be6b1395956e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:01 +0000

    it slice this buy comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
