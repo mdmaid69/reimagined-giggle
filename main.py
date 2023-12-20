29dce67a52d5c0a66a3744268060f5d0f6b9b340
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:16:42 +0000

    correct wave corner concentration contrast

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
