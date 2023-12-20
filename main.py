0dceb46b0a3e9c14bb780aecf0153305d5af3ffa
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:46:59 +0000

    downstairs choice frame service shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
