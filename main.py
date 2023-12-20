9590070d6417ce25c669562b35770b700fc5e639
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:08:50 +0000

    book flat everywhere now way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
