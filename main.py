bb481425a4cb7a84d484209340c8a79c130aeea4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:07 +0000

    throw movement yet unable fact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
