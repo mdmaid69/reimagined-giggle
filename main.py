d90707fb7a1bb0fd1865ccd86439819416044074
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:40:57 +0000

    Mm possibly rush charge available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import numpy as np
+print(np.array([1, 2, 3]))
