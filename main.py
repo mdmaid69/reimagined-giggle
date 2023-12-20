840377abb87bee60e104ad2ff50238e0e75a88d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:40:08 +0000

    mysterious be bringback fashion risk

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
