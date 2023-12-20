ec786d4b66684d311d1905a7247137f0d352aa68
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:19:08 +0000

    handle ride careless communicate strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def cube_number(x):
+        return x**3
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
