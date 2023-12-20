7283b0a9d7455b2689a9bf7844e4a5c34ae60e63
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:04 +0000

    couple close advice rarely almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
