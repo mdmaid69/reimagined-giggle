51aa70ef24d997e4939866dc2f04ba1bac17a458
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:26:22 +0000

    godown unfortunate bet concerning down

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
