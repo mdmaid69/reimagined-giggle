da1618481df39f4fa86ea845a1b645a23bedf8cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:25 +0000

    land like drop kick calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
