1f60e8ae210e3c8b739d0c64be6ffe759aa14254
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:15 +0000

    light narrow embarrassed look meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_work(force, distance):
+        return force * distance
