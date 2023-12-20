70e5d81f6d618c96918bd25ba51ab5721e61a40e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:17 +0000

    hello Ii against change know

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
