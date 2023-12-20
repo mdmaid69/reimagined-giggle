dcd53d4eb808c0d6b5d01f19334e8d2b098904db
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:24 +0000

    bet example plant display back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import logging
+def log_message(message):
+        logging.info(message)
