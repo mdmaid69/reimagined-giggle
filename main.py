ca3a8567fbdf3aaa47dd18c9b1c691e9745a3fbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:42:41 +0000

    town interested waste wind watch

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
