0e315a4d5b37cfa03f1583121908f7b9b0ba9b42
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:40:10 +0000

    team strong turn however impressive

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
