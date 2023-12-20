b12455ebb5d88da85df240a9f1e6c904a60a1907
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:15:14 +0000

    contact limit Rr on excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
