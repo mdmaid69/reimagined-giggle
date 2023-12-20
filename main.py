054c1f2053e971dae3c27c04ccf32e5b7575cac7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:19 +0000

    instrument bath below operation improve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
