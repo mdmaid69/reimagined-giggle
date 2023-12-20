6399bcd35ae78d3aabbdf2374be708dcbf98097f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:58 +0000

    that knowledge responsibility than die

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
