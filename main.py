136a26638962f0971b4886954d971f5e9c26c465
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:03 +0000

    explain wonder tax reach process

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print([x**2 for x in range(10)])
