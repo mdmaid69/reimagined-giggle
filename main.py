9aacc978d55bd4e3152d88c07d0987aef9aea780
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:27 +0000

    row taste couple month appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def remove_directory(path):
+        os.rmdir(path)
