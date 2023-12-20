a7cd0cd00030520b51d251984ab7376c08af12e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:04:40 +0000

    cut weak like send plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def find_min(lst):
-        return min(lst)
+  def sort_list(lst):
+        return sorted(lst)
+import datetime
+def get_current_date():
+        return datetime.date.today()
