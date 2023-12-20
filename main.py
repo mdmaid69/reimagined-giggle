462255e1866db1697603ea1b17d03f970dbe2320
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:13:03 +0000

    art mean expensive polite crazy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
