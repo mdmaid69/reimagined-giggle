18e60c2a3a5d6c2bbbd072e8c4a421e33a78089b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:03:19 +0000

    draw light maybe convincing bringforward

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
