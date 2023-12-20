f8c333d0510461b96c4977344c837ca698f2dd83
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:31 +0000

    wash health eager call that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
