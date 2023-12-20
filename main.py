9ed09fb90569a7eb71da6a7ad2e6484d0d167c6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:33:34 +0000

    pleasant contact permission steady minority

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Sum:", sum(numbers))
