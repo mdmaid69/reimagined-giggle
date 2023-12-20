df6429e2ddeb918b5f0699ef89d065cde7dd277b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:13 +0000

    Rr brain thing broken sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
