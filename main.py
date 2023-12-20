7639ac570945535b8b5762fba8e4ba11ded34f61
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:40:28 +0000

    fire yours sure stamp attract

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def add_numbers(a, b):
+        return a + b
