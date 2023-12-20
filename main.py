21d46e70c30dd2d0c5a9b177962a85fcc38c33c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:26:09 +0000

    sound ticket fast release perfect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_queue():
-        return collections.deque()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
