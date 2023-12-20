0c117790c289add85154e359284250dc94adde71
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:24 +0000

    frame unlikely factory round value

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
