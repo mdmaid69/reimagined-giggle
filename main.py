136c631cf2f3b430888528662d3d6590994bbf0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:05 +0000

    near image coat pencil search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
