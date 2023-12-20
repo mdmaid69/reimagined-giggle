da16e157eb11360d1a9f42d711723cf632d3c9a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:50:20 +0000

    Nn side earn unit save

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
+def reverse_string(s):
+        return s[::-1]
