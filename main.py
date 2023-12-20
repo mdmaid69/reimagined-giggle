8bc916aaaf207dc0f7c492ec700ad8f85acf87da
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:22:49 +0000

    several group drink back behaviour

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
