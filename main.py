5e720834882a97ecd722dd773dcafc823e126ed2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:53:58 +0000

    juice pause Ll 568energy considering

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
