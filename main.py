2fca63c9e42217c279a9b99c6463600ee1d4c099
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:19 +0000

    together pocket bunch fairly Zz

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
