14b557b857b37b512cf23bea08a48c36d2adafc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:57 +0000

    smile slice suit maximum account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_ceiling(x):
+        return math.ceil(x)
