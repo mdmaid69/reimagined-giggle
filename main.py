fb7409c02de71604a42c27ac2c8751375c02c03e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:23 +0000

    public spin many collection protect

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
