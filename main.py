8e5256effa1c800e3f94766c05ccf3fad3cbe078
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:39 +0000

    sign underneath holdout children earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_today_date():
+        return datetime.date.today()
+for i in range(5):
+        print(i)
