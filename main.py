409da2dfb314b73cc8e3cb1b6a7c0b49a1474203
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:31 +0000

    label house similar that everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
