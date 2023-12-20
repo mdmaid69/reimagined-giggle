a45ad36c1dee01411753bf82f9236540a2c78c82
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:55 +0000

    hold reply fail control divide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
