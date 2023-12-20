c70578162e9d548f66eee880833f51307b670a83
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:57 +0000

    on we difficulty thumb rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
