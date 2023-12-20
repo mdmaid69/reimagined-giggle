c0a1bf63348a517dc608af6d1c9ba4285d75bace
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:01 +0000

    vanish disturb justice danger compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
