ee9982d3f424e62a087a01aef1af6d2b195b22b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:47:58 +0000

    free leg soon give increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_min(numbers):
+        return min(numbers)
