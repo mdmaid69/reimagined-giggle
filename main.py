f2fd2d395e60de503a7c6e2f3929282da70d7701
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:00 +0000

    complain cost stir mean bear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
