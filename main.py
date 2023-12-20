1e6b0f5a7bcbced1c4ae5cae4ce36b44c2c6f8cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:13 +0000

    remember goon lend reach sad

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def sort_list(lst):
+        return sorted(lst)
