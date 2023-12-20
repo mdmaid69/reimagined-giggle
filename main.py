5250566715888024dd50e9d0ba8811d37aac3b66
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:54 +0000

    bath study turninto choose sail

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+for i in range(10): print(i)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
