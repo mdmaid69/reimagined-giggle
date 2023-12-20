3ebd40586a47a2a577a58a035fc9ca87bab27e5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:30:48 +0000

    pause used order borrow attach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
