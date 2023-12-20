f0408e575207b4c1a28f3c86941fb7f979dd5bc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:13:24 +0000

    appeal rate yourself greeting bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
