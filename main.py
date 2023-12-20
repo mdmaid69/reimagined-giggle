0543857e425b184d957b66725de9fa4e6af6c5c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:48:52 +0000

    bill funny only pattern concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_int(array):
+        return int(array[0])
