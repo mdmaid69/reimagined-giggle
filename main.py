9656bf16a5306b640d29495c955e0cac1aa7724d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:19 +0000

    trust refer vote Bb fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
