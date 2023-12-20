71550a80f834d2c234ac43d4de9270c85c6c270b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:19 +0000

    cause everyone official listen male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
