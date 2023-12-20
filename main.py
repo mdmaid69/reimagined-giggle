c64ecf6237817a80eff1d610be998e525821a8ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:26:22 +0000

    takeout dance suffer control block

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
