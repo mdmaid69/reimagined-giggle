52f84c843a7122a6270188e39276c096e12677d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:05:59 +0000

    gooff before ball first hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_pressure(force, area):
+        return force / area
