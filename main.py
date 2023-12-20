383705a40241b6bdb867dbc3491b83f2b9116a14
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:56:24 +0000

    fact waste necessary pick national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import collections
+def create_user_list():
+        return collections.UserList()
