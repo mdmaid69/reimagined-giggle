bcbe6627a59fb0a6dd3b61acff8a7fe0e8916ca6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:44:54 +0000

    free shut trade reserve free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+print(os.getcwd())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
