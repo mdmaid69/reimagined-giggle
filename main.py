b89e0d862806bd58adc5e279e874c8fec771ddd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:20:24 +0000

    but whisper brother base tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
