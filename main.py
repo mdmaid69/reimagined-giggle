13072cf4ba811d26f48f5cc12282a12697e4f64e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:11 +0000

    swallow formal transport behind process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
