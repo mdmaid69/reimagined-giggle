68a9d89ad0e616ecd4ed274deb379a4c23150e2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:48 +0000

    ago simple defeat during song

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
