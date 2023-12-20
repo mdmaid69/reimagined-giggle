8226fcf8b10f3c78441bfa51900d7f38ceeafdc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:04 +0000

    after break bill unkind arm

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
