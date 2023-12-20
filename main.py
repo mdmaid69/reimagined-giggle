2d8ec1dc04ec87b386a9b1bfaa3e5390b50df61c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:47:44 +0000

    question grade fact eye level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
