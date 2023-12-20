88e2e42fc3476d50c1358f18adbd86bd44f5c4ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:32:01 +0000

    plenty equal copy listenfor joke

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
