a39a653268c2d0e853fb1912d9064738788965f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:29:48 +0000

    zero fetch exchange Pp set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
