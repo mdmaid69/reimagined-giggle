aa5e0d452713fe14fe8325057aa593a80c5dc567
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:45 +0000

    wire statement comein Rr rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
