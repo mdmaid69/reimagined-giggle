85fc61d8657f6b05af198bcacd32372bd6c66be8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:09:49 +0000

    off manage foot unfair travel

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def get_array_item(array, i):
+        return array[i]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
