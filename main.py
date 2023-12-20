9b8eb6664b57100eb472eb2ad1bc0e987efb99f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:33 +0000

    punish major violent opposite terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
