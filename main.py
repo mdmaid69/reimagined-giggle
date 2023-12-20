d1a5988d8c13aaab3d8a6450b1bb078709ccfa7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:09 +0000

    opposite price scene question crowded

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
