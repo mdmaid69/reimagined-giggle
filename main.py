dd447826b406cd0fbddbb1734670c3861396502d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:55 +0000

    car sight introduce please summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
