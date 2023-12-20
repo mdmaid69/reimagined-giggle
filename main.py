2d4f9b20ba8a8e43913072c96b7a68c51fbefa2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:14:03 +0000

    detail chin Pp create rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
