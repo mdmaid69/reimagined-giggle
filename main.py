5e7b5c145a58efe7a3233c383eb62c6d989f4686
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:42:04 +0000

    fat cap until like possibility

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
