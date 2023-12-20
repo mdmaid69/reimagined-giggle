6fadd8ebb3fbbc4110dd1bc1913011103582d779
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:33:41 +0000

    whether reduce fortunately determined meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
