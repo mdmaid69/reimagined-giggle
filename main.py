8c6874033253e549b04080f758cba8f33a3f02f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:49 +0000

    scream side target emphasis home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
