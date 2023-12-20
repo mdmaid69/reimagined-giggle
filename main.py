e66f34f8fcac802d67dc94c7f6b7292d3a87b89b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:29:44 +0000

    gradually drink risk finish yet

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
