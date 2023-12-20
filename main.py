34eaa1268252005c95ce855262f9ceec5f888e25
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:05 +0000

    smooth neat voice empty bottom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
