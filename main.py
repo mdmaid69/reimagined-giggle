118f6053a698cde93a0de5847c150c9f0afe8af1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:44 +0000

    front exactly tie tend wind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
