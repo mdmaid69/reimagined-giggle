7b337168f20aeb92d999dff77df9cb68acdab071
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:10 +0000

    deal pin centre mysterious trade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
