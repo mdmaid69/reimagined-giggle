76295cd6f378a64cba2a1f0737c05a9782505be1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:45 +0000

    them hope point badly advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
