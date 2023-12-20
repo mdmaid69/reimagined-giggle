8cd9f1f9cba90e34c7a6b915a49f35967d3092b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:05:07 +0000

    embarrassed later desperate hope attempt

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
