c0858f5fbe2ddbfaa17f372d287d3273dc204d44
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:15:31 +0000

    material tomorrow uncertain meeting side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
