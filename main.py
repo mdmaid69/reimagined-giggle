6fcd5129fe0fbb5946cf01de61c62ffab333d6af
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:18 +0000

    immediately day paper laugh politics

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
