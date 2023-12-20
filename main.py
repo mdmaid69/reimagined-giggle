9f38af33bf0463fb52ac6313b938dbea5cf97b73
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:35 +0000

    hurt traffic constantly meanwhile stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
