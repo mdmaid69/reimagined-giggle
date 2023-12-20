bf14c4522486e62cd8bd14689ed0be78699db3a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:43 +0000

    exact except rent fat comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
