c6aa94e39e117878cdd5657d3f27fc1a8ae48346
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:59 +0000

    communicate word write keep beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def cube_number(x):
+        return x**3
