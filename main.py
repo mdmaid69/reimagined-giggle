b050c6554b39fbabccd36743b4bbde88ed02af2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:21:45 +0000

    spray shock Bb married outside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
