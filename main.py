0da081e6e7f5497e14a117fac948394799d4aa93
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:12 +0000

    fast punish book butter community

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_speed(distance, time):
-        return distance / time
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
