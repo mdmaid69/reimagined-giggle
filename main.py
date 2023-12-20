b9a4cba71f9f327db4bdbe09e6c0b3ee8887a76e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:04:10 +0000

    preparation book limit conclusion yes

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
