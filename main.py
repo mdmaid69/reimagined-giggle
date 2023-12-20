b1a62f417b67c1be98f6dcce640a93296475ae1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:01 +0000

    entry business pronounce polite remark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import sys
+print(sys.version)
