dd97b0ef9d5ba17ee2a12908c7c6d768fdce96d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:04:57 +0000

    study real autumn wander plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
