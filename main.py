16f5bdad9e33e952f8218afe39e0966021b4ac27
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:09:26 +0000

    order goaway though secret fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
