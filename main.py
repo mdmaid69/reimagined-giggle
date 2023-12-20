bcea91114c281831d4b85a1f05b9d7bd977ab6bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:33:14 +0000

    serious owe much getout tune

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
