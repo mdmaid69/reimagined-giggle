b54018a5828f6ea916cbfcc8a1103003a698f92a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:34 +0000

    temper defend lesson minimum party

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 def fibonacci(n):
         a, b = 0, 1
         while a < n:
         print(a, end=" ")
         a, b = b, a+b
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
