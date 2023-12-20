40decae84263b54edd0d2da69fb68eb4a8340a07
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:39 +0000

    573entertainment relax stick makeup give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
