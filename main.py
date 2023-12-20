6e640820b211f1368f7b40ae038c51c3d72d2933
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:25 +0000

    deliver furniture refuse draw smoke

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
