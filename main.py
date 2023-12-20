fab620ac2e2e40ca12761058d48e594b4a9350bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:30 +0000

    borrow bringdown object another question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
