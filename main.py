08e756ef98fd7769628e397148fa6c1febb6b687
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:32 +0000

    closed scale outside process stand

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
