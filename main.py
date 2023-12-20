76b541d09934e33366b5802cdd84bd5b12b87fe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:20 +0000

    callback actually mother trouble meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def subtract_numbers(x, y):
-        return x - y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
