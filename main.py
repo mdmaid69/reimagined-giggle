62b626b61dcd0c781740567b21d97ad46359ac2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:48 +0000

    back step dish definite successful

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
