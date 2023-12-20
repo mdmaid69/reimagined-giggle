aff5487c86b94e85fd79e7c7331311780620702c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:45 +0000

    sometimes refer possibly silent big

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import datetime
+print(datetime.datetime.now())
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
