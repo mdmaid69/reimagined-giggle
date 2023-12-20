2c4a513efd2775c085ff0aab27ef3db22e8b9b0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:12:52 +0000

    typical programme evening holdup destroy

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
