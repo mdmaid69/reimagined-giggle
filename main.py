d3eefed2df8b452ae0693af87498c41a2de068be
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:12 +0000

    today bite thinkthrough discovery drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
