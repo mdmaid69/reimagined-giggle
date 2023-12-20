f6fa48018538fa98fdb61839dda51a5f23941689
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:44 +0000

    holdout list future cross emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
