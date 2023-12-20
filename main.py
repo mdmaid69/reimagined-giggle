fcc41107424c10538e9f426e28ee229514065e45
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:20 +0000

    ilovedicks comeout delight front deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
