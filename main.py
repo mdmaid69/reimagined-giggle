0fc0e28cc8a68968bc11a85e28894b0cce6ba330
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:48 +0000

    react related whistle explanation demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def add_numbers(x, y):
+        return x + y
+def find_unique_words(sentence):
+        return set(sentence.split())
