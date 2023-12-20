a99d4d0e3fde8cf7fec26622e919bdf63e09e36f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:19:05 +0000

    day different card finger roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
