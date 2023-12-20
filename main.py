30b2d1c3e697e05b21a0a722895bb39e64f488c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:06 +0000

    thorough exam spring many introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
