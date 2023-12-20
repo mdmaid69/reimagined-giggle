d136d3b3c430a5d55cd85cbcc27c406802696f91
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:46 +0000

    firm quality noisy exciting slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
