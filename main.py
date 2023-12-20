d062eeaae1d125081e96a7e4945a024b6efc8682
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:16 +0000

    sensible fly friendly tradition best

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
