3bf3f78375c631d9fdd1cd5c783ed608d3029ade
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:41 +0000

    punish reserve fork bye sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def remove_duplicates(lst):
+        return list(set(lst))
