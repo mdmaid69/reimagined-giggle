baacf4125eb8d76254c85cfc54a46e31120bdccd
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:50 +0000

    drop prefer study hurt doubt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
