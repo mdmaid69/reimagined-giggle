6b11677487492ffa81bf2850776e30a0ef55079c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:18 +0000

    relax before welcome impressed convenient

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
