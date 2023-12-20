cdb9e432eb2e02e33bf0441d5a9b56cd3cad1b39
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:42:41 +0000

    suckme support zero smell ordinary

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
