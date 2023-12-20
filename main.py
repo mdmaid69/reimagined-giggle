0f7357fae82c432bbabe84781cf442a5301c62da
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:20:59 +0000

    file plate strange giveaway many

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
