18cb1e21fe47bb0c7c697953b0e583cfaa6a0087
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:25:26 +0000

    in but brush earth often

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import sys
+def print_python_version():
+        return sys.version
