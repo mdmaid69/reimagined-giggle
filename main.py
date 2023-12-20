53421e00728c4ab0a5b15693db143d7dde4d757d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:32 +0000

    advise bear cough lookback(on) cruelty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
