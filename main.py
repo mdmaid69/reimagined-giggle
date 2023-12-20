e6f0aeadf684acbb1fdf0cf90b5befae28c5fee9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:40 +0000

    sight step hurry similar gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
