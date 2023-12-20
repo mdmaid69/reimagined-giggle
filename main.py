d400a4a70402ec55babe91d271d155f4b011a533
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:44:40 +0000

    back use perhaps tall mine

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_density(mass, volume):
+        return mass / volume
