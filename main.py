460395099e3c831d78540e238b9a1bfe49dd0180
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:06:48 +0000

    dance apply mistake communication hole

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
