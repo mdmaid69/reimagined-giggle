3581268ffd00c0940173e7af5f822aadb171b1fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:39:22 +0000

    letter bin nature father stranger

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import sys
-def print_python_version():
-        return sys.version
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
