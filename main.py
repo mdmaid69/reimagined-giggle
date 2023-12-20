b509172d096472d815d397f1f2e00eb68c03ee95
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:45:48 +0000

    ring question call union drink

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def create_user_string():
+        return collections.UserString()
