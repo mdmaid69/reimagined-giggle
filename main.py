252af4b5e29052d834b40aee172b668c4f610688
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:16 +0000

    light great opinion company lunch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
