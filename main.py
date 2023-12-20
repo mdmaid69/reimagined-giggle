eacadfb99f65c227f7e7be2fee404bd1bf908488
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:18 +0000

    holdup turnout congratulations snow event

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def get_array_as_bool(array):
+        return bool(array)
