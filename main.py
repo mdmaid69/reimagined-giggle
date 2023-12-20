670ff3a6c0327b16e17cab63104828006db18b9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:48 +0000

    previous spare repair hurt age

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
