13e4b18141f06f3ac58cc787e6fe05081eef5c00
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:14 +0000

    translate break more part concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
