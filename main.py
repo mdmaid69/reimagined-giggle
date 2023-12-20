6af6d5ff0883234af9d361c971bd73add041a303
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:45:41 +0000

    stir narrow impossible cost involve

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+def sort_list(lst):
+        return sorted(lst)
+def reverse_list(lst):
+        return lst[::-1]
