65d70bb67c31b59612679807fe87fcba38ba3889
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:29 +0000

    keepup leave sort high explain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
