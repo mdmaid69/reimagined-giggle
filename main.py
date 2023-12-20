b8bacc42159d475fe44bde61cf8881b20363ea48
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:10 +0000

    report avoid stamp leave kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
