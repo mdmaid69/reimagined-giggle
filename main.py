547252e3e9e007320d6b93a5edf103d9df80a709
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:26:43 +0000

    present pure dear push Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
