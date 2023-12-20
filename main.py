2cdc1736dbea2804965333cddf43eb72709508db
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:47 +0000

    beyond top cheap takeaway unusual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_int(array):
+        return int(array[0])
