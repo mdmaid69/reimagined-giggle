437d5c480459d55126a4b791f32f205f7d949c2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:24 +0000

    welcome smart cruelty plenty disappear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_error_function(x):
+        return math.erf(x)
