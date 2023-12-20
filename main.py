cb8f0f3728288978640ba8fbe51d67dde440a285
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:54 +0000

    cheek crowd straight score pick

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
+for i in range(10): print(i)
