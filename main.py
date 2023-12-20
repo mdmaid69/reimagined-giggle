7a0f22eeec4052ceeda9a39125c5c770a29d83c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:41 +0000

    century calloff count beneath demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
