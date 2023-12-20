a30e849ed90f2c825e0ec093f6aaafcba9ed57a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:10 +0000

    morning calloff anywayanyhow cook measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
