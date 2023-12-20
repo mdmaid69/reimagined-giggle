130382c7db7fd49a66f1fa0e3b50c210f6e1327c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:48 +0000

    tool shall shrink accordingto nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
