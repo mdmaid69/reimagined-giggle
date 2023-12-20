cb701f43a8de83bf9177fdb57213bdb675dc1c0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:23:29 +0000

    connect board transparent position male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
