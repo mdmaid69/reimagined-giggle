9986b700c2f0a83baf314554feaa712e3564d884
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:37 +0000

    life goup work mass risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
