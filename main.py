0bf984ff55e00b1068115139fc764e0a5ebdaca7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:12:35 +0000

    may tall better fence true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_item(array, i):
+        return array[i]
