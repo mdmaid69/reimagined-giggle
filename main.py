f2bbfdd30ee6c8744a6945d69b5fb11fef6f6243
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:25 +0000

    maybe fear colour some load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
