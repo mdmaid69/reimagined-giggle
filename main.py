c3fd387579f9f67dff1ee648f8257fd531444e51
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:32 +0000

    measure refuse payment breathe generous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
