e05796ce861130e59f48ed284d3592205dede128
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:47 +0000

    bag destroy spray nowhere holdup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+print([x**2 for x in range(10)])
