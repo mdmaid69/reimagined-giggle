f14a72d51b38b4beca14e8eab43ed62598c95f27
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:53 +0000

    wise exploration live benefit neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
