604b0f9a5fefc05ab411a7f7b1d8c83420a7ae25
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:22:22 +0000

    wealth fasten door constantly sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
