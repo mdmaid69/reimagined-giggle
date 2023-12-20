59652f02591a9ddd6e3df7f50a016636399e6e98
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:27 +0000

    war score news advanced purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import datetime
+def get_current_date():
+        return datetime.date.today()
