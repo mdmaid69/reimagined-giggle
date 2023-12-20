a57a627c5f380314ab2499eca61c5ac0fde9bd15
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:45 +0000

    scale act matter suit relationship

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
