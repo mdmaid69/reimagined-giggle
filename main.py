189a87c6aa4a5bb5ea6fffa7df48ebcaff5b2a92
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:25:17 +0000

    within smart right song Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
