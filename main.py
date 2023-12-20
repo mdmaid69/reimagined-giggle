3fe0db14ffaaa1928949c7139adea21e9140daa0
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:36:00 +0000

    sweet schedule position apologizeapologise greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
