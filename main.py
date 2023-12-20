082b9c2fa3b2936490b8931c95d8dc0ab26359c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:14:54 +0000

    motion favour move measure spin

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
