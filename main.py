8160b9bce889962ae4669efa9b9cea25c313dbb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:33:31 +0000

    slowly comeout bend fit any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
