f6bdad56eb2ec9e7692aeea4cd600b72a0b57028
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:05 +0000

    flavour similar behind black run

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
