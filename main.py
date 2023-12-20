188905efde310468084f947541c59c8a4a344e8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:40:06 +0000

    behaviour season remember genuine calmly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
+def divide_numbers(x, y):
+        return x / y
