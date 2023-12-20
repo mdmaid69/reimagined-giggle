79c3a85460571a343ebab453b5d65895d9cf04a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:37 +0000

    intend 570enjoyable hair show somehow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
