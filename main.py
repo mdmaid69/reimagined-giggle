013c064e7b597b5f7c60a10867323d96ddba71ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:09 +0000

    search visit wear plastic leg

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
