56ac37df66a2123f5afeba988e0850edba9fc908
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:38 +0000

    complaint runinto thinkthrough entry danger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
