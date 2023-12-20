4ccfcc6858e14fc743c747b242848b791d7b63e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:12 +0000

    Dd sink underneath Gg treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
