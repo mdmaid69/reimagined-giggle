68e7c7fad4cf3b31d7ae1efb104b5267e6428104
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:08:58 +0000

    storm attitude snow strict measure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_item(array, i):
+        return array[i]
