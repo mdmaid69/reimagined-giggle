3c9ded9ef8a078af4cd9c18fe91b7af927460db6
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:01 +0000

    arrange send advice this appear

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
