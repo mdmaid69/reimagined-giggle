5abc8f5eadc8125e5a7d372158bc0511c403564f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:37 +0000

    back union theirs pair hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
