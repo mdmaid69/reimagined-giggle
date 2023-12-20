1713db8770f25d357d98100a0557435c81888486
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:44 +0000

    scale offence accidental crash there

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
