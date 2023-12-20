d8d4ec07ddb75510be40a336cd2a5bdcc2ead67d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:58 +0000

    move at cheek come justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
