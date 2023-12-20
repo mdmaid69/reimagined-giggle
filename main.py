b8eb42d95355feb316ba00654e4e0c4693fefca3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:09 +0000

    scrap behind treat payment refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def is_odd(n):
+        return n % 2 != 0
