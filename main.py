7750b8101969511adbae5c72e4d6bbe36cbee095
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:07:30 +0000

    exam note size but worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
