b076d536e2a2df1477d199f05bc44f1e1adc3169
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:11:19 +0000

    doubt run keepup first set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
