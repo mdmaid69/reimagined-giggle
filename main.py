ba2caa25c844676ae18781e8f4d19264bc68120c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:25:54 +0000

    underneath shrink hot drawing responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
