444bb739baa1b297b9bf42cf2a00d32f0abf175f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:44 +0000

    answer quit everywhere fond turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sys
+def exit_program():
+        sys.exit()
