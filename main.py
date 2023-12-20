2fc37d3cfd3a5c010b9c9d2e84a4cdfbabb88b70
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:00 +0000

    front top relax admit party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
