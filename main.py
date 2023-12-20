1a0c061fdd2c84f80a3f149be661e24802aa75db
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:35 +0000

    wrong prove wrong cinema rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_list_from_array(array):
+        return array.tolist()
