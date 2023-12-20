3e23b1e5317c3bad77efc67c46b32869561509e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:14:42 +0000

    smell downstairs set hold hang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
