a2fa4a5a2c34f20accdd090199c4ef731d2dedf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:31 +0000

    much formal typical variety explanation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_density(mass, volume):
+        return mass / volume
