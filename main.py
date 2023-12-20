78373292d71c1526abf771800092b8a2c6789f4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:26 +0000

    bunch season change mess after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_file(file_name):
+        os.remove(file_name)
