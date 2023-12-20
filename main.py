2f405c62bc43931d829a723d81583b2c35fb4396
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:59 +0000

    welcome bag raw necessary study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_density(mass, volume):
+        return mass / volume
