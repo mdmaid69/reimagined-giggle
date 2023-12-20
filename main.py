11f35b48887209de4a052e5c6e796be25e36bcc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:24 +0000

    ill score always bound pleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def multiply_numbers(x, y):
+        return x * y
