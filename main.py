10fb5cf97864ad59313cf71c2743ca1a1c73a2d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:21 +0000

    majority friend increase lift much

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
