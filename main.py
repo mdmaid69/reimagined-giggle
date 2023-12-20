d6059d7ed4a50f0d1a97c139c0bc875380269170
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:23 +0000

    behaviour tap takeout Cc above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
