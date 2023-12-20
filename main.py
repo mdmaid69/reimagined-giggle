f8d905221c24a5b7ae182d9df3ea9343754403ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:23 +0000

    unite paper peaceful male pay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
