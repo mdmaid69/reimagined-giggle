b3954d8cdecb789dad08e74b47a4d33e6aba279e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:04 +0000

    fight experience day phone tidy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
