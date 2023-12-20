07d53203ce9d296ea5fba888dd7927658f1d4f26
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:51:15 +0000

    process suddenly pin aim point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
