bdfcb1892dbfe629650ab24b69e762971162d439
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:05:06 +0000

    least noone shall all extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
