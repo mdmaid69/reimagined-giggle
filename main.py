e7ec78ae547de1e3bd6c96f3b804beb6dee656ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:22:19 +0000

    event ilovedicks risk sudden loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
