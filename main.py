3d20027c2cf0d00371d9938ebc05bb1d19a46f74
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:02 +0000

    turnback spoil hope party steam

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
