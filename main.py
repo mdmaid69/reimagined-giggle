db4ef56463c06ed9d0eb54ba161ba8b75566278e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:14 +0000

    fire joke public about save

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
