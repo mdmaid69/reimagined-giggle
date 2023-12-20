f6524982bb6f8ecfd0beae82af3f40a4a0dc053e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:27 +0000

    reply remember difference area either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
