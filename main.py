b87f96118dae68e11f52c110f14ba5ab7eed8e4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:16:16 +0000

    shop slow cope demand characteristic

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
