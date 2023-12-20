72ebc44b56bb44e1a9d480c2f4c4ab45457b904c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:24 +0000

    callout respect suggest new fall

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def add_numbers(x, y):
-        return x + y
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
