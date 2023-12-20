0edf92f5e93e7754ebcbbde6b125c547169b8dbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:58:57 +0000

    wash entry heat suppose count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
