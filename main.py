b7678530c9c35ccb8397ea351b115c5662a456ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:51 +0000

    compete birth dish know cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
