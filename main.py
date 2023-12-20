34d32aa4751afa0b316bd44f64a428fb68994baa
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:50 +0000

    war shake thank wing hold

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_force(mass, acceleration):
+        return mass * acceleration
