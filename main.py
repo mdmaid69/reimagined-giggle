89f80dad74dc0be740438f4a651e3764070e0131
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:56 +0000

    declaration head more mean soft

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
