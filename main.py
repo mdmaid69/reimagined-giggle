ff2ad075dba6666ea87f98bf46b2381063b17ba1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:07:11 +0000

    priority sport 573entertainment instead gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
