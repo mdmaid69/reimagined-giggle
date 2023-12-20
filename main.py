ee5c4f787148610006ee10cdd32d8d3dcecb1e9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:01 +0000

    temper measure corner else school

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_floor(x):
+        return math.floor(x)
