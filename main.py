0014b9f38d3bf4fce5de6395941996d777a13ac4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:02 +0000

    race motion communication holiday emotion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
