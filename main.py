d5b9e5aa7fc1e62973d23058a272f2322885df62
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:52:51 +0000

    direct book crime except react

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
