2a4c91a224bb8b5fa77aec34f4140fe4f26d5eee
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:43 +0000

    scene office too transport loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
