7c616415b36a85df76b68e148ce4cb9fe8c7ac1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:13 +0000

    cause male dinner similar brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
