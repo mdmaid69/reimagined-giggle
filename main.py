f99a189394c070076431a7628ff495ce7abb262e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:35 +0000

    fortunately introduce copy recommend fast

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_words(sentence):
+        return len(sentence.split())
+import collections
+def create_user_string():
+        return collections.UserString()
