5d54910e4cb863274efe547cfd5642d6340c1a01
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:55 +0000

    sure throw father legal value

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
