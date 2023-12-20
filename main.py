53790a76b4e88d69af3b3a7c389ca6117591cc27
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:07 +0000

    suit trouble general fight ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
