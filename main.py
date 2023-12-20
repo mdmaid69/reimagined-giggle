8f3cbd14a798dbb78e5a477213ae9c0b2e14c48e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:10 +0000

    plan luck drop event mind

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
