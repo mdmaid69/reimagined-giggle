fb62519cfa919a4e4011bdd9e70f6bcc752435c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:34:59 +0000

    height possibly deliberately bored someone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_user_dict():
+        return collections.UserDict()
