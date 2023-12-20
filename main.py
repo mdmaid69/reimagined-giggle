52ddcbb8921bf94fdf410973fec3758910eba869
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:47 +0000

    nice poor cost add show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_speed(distance, time):
-        return distance / time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
