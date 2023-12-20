72cc3d7e48648ae5a49e7ec1b77fc51fddf27ce4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:33:29 +0000

    degree hair independent taste lookaround

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import platform
-def get_os_info():
-        return platform.uname()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
