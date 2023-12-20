63489a64dfd1c053b0f1af57e2221818d34ea401
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:57 +0000

    lead protest tread evidence fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
