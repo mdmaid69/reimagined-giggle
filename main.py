4de69414472b79ffa4bf2a302733bad354c6d039
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:22 +0000

    beneath breakdown result needle but

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
