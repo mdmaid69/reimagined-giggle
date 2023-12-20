ded151d2cc3be3639502f8cfdcba26b0c72927b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:36 +0000

    software car boring life arrival

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
