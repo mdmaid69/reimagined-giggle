d27ba406450b04b3a7ef0677a22d1cc2ba486998
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:55 +0000

    urgent tend position idea perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def sort_list(lst):
+        return sorted(lst)
