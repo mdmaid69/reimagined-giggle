5e00bcc152e54f899a805edccaf890bc12d8de13
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:04 +0000

    quarter authority Ww ball level

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
