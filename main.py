50631e5a6625f4c92a8268dd60f592b7ff1b40c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:30 +0000

    another classroom stretch anywayanyhow move

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_speed(distance, time):
+        return distance / time
