2b2608245cdc98dc420a129529f91a1e378ee5fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:36:28 +0000

    plant guess nurse suit that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
