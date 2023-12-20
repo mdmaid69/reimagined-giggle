797603e53e05f33799f7604dae2d5f8f687f840a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:21:43 +0000

    teach bye waste none plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
