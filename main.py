d8333a61a32b6a66d3caf62a2840e35de3275d1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:32:55 +0000

    breakinto lower so taste chat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def sort_list(lst):
+        return sorted(lst)
