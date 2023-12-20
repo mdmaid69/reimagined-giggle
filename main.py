6f236a82836ecb79731842a7138a60c877a9d685
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:11:18 +0000

    tomorrow difficulty cap imagine gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
