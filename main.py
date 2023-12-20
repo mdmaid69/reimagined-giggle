b204fa3a7a12cd1350d8622cb0fa14cb53f77561
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:50 +0000

    there brush deal but square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
