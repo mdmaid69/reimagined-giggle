c1a3bf2cf0d8091fe35010430140959d27ce8605
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:20:59 +0000

    fortunate large number estimate closed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import getpass
-def get_username():
-        return getpass.getuser()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
