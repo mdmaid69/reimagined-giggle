913160b801dbfe1d295d94ec2a29b2f97c285b1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:37:48 +0000

    top belong shoot treat active

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-print(math.pi)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
