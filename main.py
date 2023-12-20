94821514461280ffdad62e8f7e7fbea7c4c21dc9
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:37:22 +0000

    measure mother secret on offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
