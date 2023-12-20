516f296fc8f6594b3c92bcab6aa5bbabdae25b95
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:39 +0000

    proud attention rubbish declare price

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
