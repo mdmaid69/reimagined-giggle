4bc06f43382e14a980fa683c407739d6e87e5e52
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:20 +0000

    stop habit please dear calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
