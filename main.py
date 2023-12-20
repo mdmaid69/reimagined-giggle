ec1a5710e58113b3fdc44b8a3a3edd6d763d850c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:41 +0000

    ilovedicks convenient marry ignore window

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
