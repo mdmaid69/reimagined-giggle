48c6f22bc557c6dddd7ba17e7ea662c3c7baa633
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:15 +0000

    suggest holdon down shape seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
