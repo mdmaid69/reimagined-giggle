8d6d5c84a16252ec77101658a7e719df5d20617e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:46 +0000

    doctor left train cost scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
