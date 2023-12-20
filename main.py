0c178f590ee4f2cb65bbe8d0b70f0b2695d31ecc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:49:00 +0000

    nor number bill risk bother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
