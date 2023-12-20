7787f0e0f9167f629920d7058cb9375cc5ffd9f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:41 +0000

    our permission discovery 566end cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
