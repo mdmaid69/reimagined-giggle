40768ca3c754e1ba89a3946d91585eeae991371a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:25 +0000

    basic main limit waste period

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print("Hello, world!")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
