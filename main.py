2a2c7ad0ab93d82fc3461d9559e6b45f743cff9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:30 +0000

    science suggestion forever because never

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
