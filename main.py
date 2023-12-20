e7f9936a768b5bc5b7e276f4d6ef433b66c42308
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:58:17 +0000

    custom thanks shine bottom provide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import tensorflow as tf
+print(tf.__version__)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
