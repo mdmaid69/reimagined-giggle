8a521e12bb82ecf943036f6abb9462e70ac1c9f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:45 +0000

    of delight effort mixture drink

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import array
-def get_string_from_array(array):
+def convert_array_to_bytes(array):
         return array.tobytes()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
