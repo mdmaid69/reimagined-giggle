199c1efc147ec0f75cdbeb4b9ea57afd019dd500
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:01 +0000

    ticket plant claim comment sail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
