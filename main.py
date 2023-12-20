98a24c6faad7344c97e20f0cc04fa0e4afae2c2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:22 +0000

    land care face moment tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
