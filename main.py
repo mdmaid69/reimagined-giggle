3e8acdc807926c7ec1fe1fdc3f3275a5af473787
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:49 +0000

    treatment error Uu cope pass

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
