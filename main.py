c990235565701d82795e4c2a78c919b9390a8035
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:28 +0000

    window aware contents well run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_directory_name(path):
+        return os.path.dirname(path)
