832ed3e569936662c2208b46baa08a19c4024485
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:39 +0000

    cost by sick correct if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+print("Common elements:", set(list1) & set(list2))
