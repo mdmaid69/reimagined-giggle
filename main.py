2ab31436f42a7bf76b5f82277c7135a8ab0e379e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:24:00 +0000

    develop worry stupid sense pile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def count_words(sentence):
+        return len(sentence.split())
