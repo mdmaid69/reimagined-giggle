7313e5aafcc7f822d4af4a54681987f113c07302
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:26:54 +0000

    simple Itself make could show

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
