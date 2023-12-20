8ae640622eddc9cf4f13c293b58631b12c00b5c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:16 +0000

    pay priority early Oo tonight

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
