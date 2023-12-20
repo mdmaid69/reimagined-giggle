1c99a9e8257b83402ae76806f46db1a1711fae69
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:56 +0000

    before memory woman afford discovery

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def square_number(x):
+        return x**2
