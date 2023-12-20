f13af4f98544c46d32855288d1f18d5b1190c558
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:56 +0000

    then rely bar foreign insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
