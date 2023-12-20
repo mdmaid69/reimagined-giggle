7d4b81b6fd4ad4604d679eadcdbf544facdbda48
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:44 +0000

    protect dream 567enemy search wall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
