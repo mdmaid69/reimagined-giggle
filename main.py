4c53f500309368cb686f0a2a22a91a4a03f8a0eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:12:08 +0000

    complicated shoulder hear behave bar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
