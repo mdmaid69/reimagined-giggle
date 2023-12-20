aaf6fd5d943352abc8a49a5394d5ba093e9a518a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:55:29 +0000

    marry turnback would tie custom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
