62564191cb3d0a151139559cd06b0017b10c6891
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:00 +0000

    if chat ourselves lucky slice

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
