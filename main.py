5f0a7e8805c14190be61ca0920a02d18fb210251
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:25 +0000

    mine hang software steal Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
