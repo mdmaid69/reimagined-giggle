c8481f20313d478604c7331318404c90be09b2a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:04:51 +0000

    major live remember small history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
