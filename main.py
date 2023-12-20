56bda2fd334dccf9a6e42012ed775eeb29e2358f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:47 +0000

    cry announce national port practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+print(math.pi)
