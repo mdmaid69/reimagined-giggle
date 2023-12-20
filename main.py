5c3ba14222852a844a831f0c71a2785e7d0b87ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:35 +0000

    top always attach they fetch

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+text = "Hello, world!"
+print("Characters:", len(text))
