565c860d0a50ccfcd1ad5903c1ade9dce59e4f69
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:27:56 +0000

    male wave and offence heavy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
