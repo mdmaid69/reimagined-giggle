3d94c16ed53db3f58ca28626ab1e44353b613941
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:01 +0000

    scene by shock avoid evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
