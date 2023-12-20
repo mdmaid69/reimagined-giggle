97191b18ef33c89aa8039ca1efe3ea6834d612db
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:38 +0000

    cheat again if mark possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
