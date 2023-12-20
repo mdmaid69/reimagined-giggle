3584d19daf5610a69f542e2e82f1d2322127e2ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:02 +0000

    brief approval solve advice manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
