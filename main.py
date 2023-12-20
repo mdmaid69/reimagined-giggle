f061d8999623c0994c8fcbf4eab2294aafd085d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:16 +0000

    Ii war see change exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
