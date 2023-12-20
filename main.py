fd81c701d157cc95be4a32933f0a167bf94b176e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:10 +0000

    circle sell respect might quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
