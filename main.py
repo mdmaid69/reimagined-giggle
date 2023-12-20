7efef8343f01a53d3c30e2566d4e1c90813dee6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:39:31 +0000

    check certain why rank later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def cube_number(x):
+        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
