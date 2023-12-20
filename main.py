3d05664cbbd5ba926025cc6235786a0704d3a5e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:56 +0000

    make carry change scrape bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
