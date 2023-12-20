efce94d6c72f495c60833c5d156aa6d531da6466
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:03 +0000

    takeoff change tip 565end class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def get_array_as_int(array):
+        return int(array[0])
