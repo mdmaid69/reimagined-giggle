49337b4459689ac3d538acb14ca5941915f6450e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:42 +0000

    nature loan ticket dare responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def cube_number(x):
+        return x**3
