235435fdaaaef47be608fc7c8789e6fa678af26c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:56 +0000

    one flood almost please it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
