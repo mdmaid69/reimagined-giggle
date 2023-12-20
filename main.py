7dcde415dfcd91facb80bbcbb82e9392a0bca6ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:36:51 +0000

    later experience let characteristic disturbing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
