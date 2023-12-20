be9c64a95c020fabab6725a0341ae29c5ff21028
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:06:15 +0000

    record from runover kind follow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
