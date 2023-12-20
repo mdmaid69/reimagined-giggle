79097821bc8a979d0a70ba221b21732056b71c86
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:58:24 +0000

    flavour journey music exist consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
