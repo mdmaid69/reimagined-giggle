3d1659ee881afd62c309b67c17e560a778c6f5bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:43 +0000

    Ss least nurse blame quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
