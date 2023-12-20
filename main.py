e4eeb5561b64b07c2071f75c34de47dab71afc98
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:47 +0000

    advantage nonsense Itself crazy common

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
