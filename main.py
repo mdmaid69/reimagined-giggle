a057a27dd3603f53a8946f4d84b1e6995aba0682
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:54:46 +0000

    please lock loose possibly succeed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+print(os.getcwd())
