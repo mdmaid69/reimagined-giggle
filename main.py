db8b6dd1740ef10b0cd813f27c12e4a89e7fc738
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:44:49 +0000

    beautiful add wire coffee violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def square_number(x):
-        return x**2
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def get_current_time():
+        return time.ctime()
