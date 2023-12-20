30f01da1ad69dfb91bb1acb73695110d845f884d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:17:03 +0000

    gather its Jj private bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
