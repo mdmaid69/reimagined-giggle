1be761109b12abae8ba967d6760598e02e0c8dc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:54 +0000

    want holdup book hardly behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
