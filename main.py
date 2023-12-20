05a8d60ba3954d88f2f0c9b9bbcf0a091671cdb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:05:06 +0000

    middle permission moment have trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def multiply_numbers(x, y):
+        return x * y
