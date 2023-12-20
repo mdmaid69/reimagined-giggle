1c57c6d83f8e18c66678a91540c7e6c73fad6f83
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:40:21 +0000

    cinema dangerous belief interrupt tidy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
