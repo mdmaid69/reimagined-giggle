906071b42cbcf7004507e786591f806fd6d8dadf
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:54 +0000

    pronounce Jj fun middle reduce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
