f33a266b8e8644d9697e2d8eb6d6f9d92c165368
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:51 +0000

    physical listen relative advantage dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
