9258d689d93181b1dc53634deb268bc6dfdbcb0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:51 +0000

    well sentence head mistake feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_inverse_hyperbolic_sine(x):
+def calculate_hyperbolic_arc_sine(x):
         return math.asinh(x)
