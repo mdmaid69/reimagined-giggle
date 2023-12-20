5fd75980aaf4b9979b22a58377bff2d8cbd26cfa
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:48 +0000

    seat trade wing separate future

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import platform
+def get_os_info():
+        return platform.uname()
+import collections
+def create_user_list():
+        return collections.UserList()
