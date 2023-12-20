c971e4143838a1b0cf51adbef6e4e40c129bd464
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:45 +0000

    willing comeout accept exchange twist

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
         plt.show()
