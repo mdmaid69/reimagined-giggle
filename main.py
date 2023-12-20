f88fa5cee380537ef200eaf67d6b5a14525f13dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:44:40 +0000

    satisfied object arrival ago above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
