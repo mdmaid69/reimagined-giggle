d7c9d9f3ce94a2d75eb0a84211167390dd3ad241
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:46 +0000

    loss roll point independent meeting

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def convert_to_hex(n):
+        return hex(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
