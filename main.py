2754c03c75f672298c552e3e448c557aa4a8ea4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:36 +0000

    state cause cross expect blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
