1c0ea354b79e94bb12d67c05eb25048af728e77d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:11 +0000

    manner deal repeat heat view

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
-import platform
-def get_os_info():
-        return platform.uname()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
