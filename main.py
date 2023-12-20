17ffb9ab122db211ce4a07c99827e8a65ef67bff
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:51:03 +0000

    teach arrive present must best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def is_palindrome(s):
-        return s == s[::-1]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import array
+def get_array_as_bool(array):
+        return bool(array)
