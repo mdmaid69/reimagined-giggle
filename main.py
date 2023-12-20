2f1f2cacf065901f9e0c74f8f6cfff50b05fc9d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:02:01 +0000

    turn warning wave drink confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import logging
-def log_message(message):
-        logging.info(message)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
