f8519e37e1942d400dcc4e94e3ed4be1aea26b45
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:16 +0000

    arrange go hers reserve success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
