f08ff85c2dc3bbdacc33d06d6dc414dace1c8b14
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:06 +0000

    if beneath search Yy natural

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
