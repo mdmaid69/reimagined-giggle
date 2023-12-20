7df32d71e54ccd66cfc6065f2b8ab50560ea5bb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:04:13 +0000

    school shout sound movement but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
