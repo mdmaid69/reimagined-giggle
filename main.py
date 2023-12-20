ee9a2d52c6c4a3e961f0f7d432b5f6bba6a52444
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:11 +0000

    lift embarrassed shake proud familiar

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
