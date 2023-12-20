f4e66dd4039b4565772d5bb743d337913aba0e98
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:51:20 +0000

    after involve inquire sing concentrate

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
