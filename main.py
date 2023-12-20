2d32cec6067abba3219b9943e2a28acd8bcdb992
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:14 +0000

    onto basic speak broken help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
