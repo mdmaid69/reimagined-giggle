14a0603b7698889f879b34159fe5e67020d7b970
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:29 +0000

    possible repair work occasion better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
