74761b785de6a55b0eb0910959c83a28234aa1bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:26 +0000

    experience he confident know suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
