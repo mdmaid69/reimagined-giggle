464866baaeefe0116791b3c7f6e37378dcdea407
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:57 +0000

    city cough carry activity Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
