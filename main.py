bb77bff56f82f6d9c1a4459db5199833dfb6e41b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:15:59 +0000

    hand teach march tune appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
