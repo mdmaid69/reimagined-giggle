5faae03fc52827ce4097c2611ff596a6e52370ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:13:52 +0000

    shade vision close achievement fit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
