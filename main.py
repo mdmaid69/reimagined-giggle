86b5273664fa4c7ce60a31a1b158504243b57002
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:27:30 +0000

    treat practise lock whistle owe

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
