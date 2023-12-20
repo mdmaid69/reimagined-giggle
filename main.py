7e74e4a6dc0a6e85e491fc724e4c8e63da2339e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:57:15 +0000

    contact expensive exist pull same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
