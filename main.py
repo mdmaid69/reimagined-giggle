a18edc1e71cca3efd2cc90e38310841afd00be81
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:50 +0000

    wake nonsense bang since Hh

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
