e3747c675db0009fcf26dd130dad58c61a2ec407
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:02 +0000

    blind sharp interview away contribute

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
