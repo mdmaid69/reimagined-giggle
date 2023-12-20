00aeaad8d0e03f8fb37476bcbcfe3ed461628a2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:38:18 +0000

    me getup pair where beneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def split_path(path):
+        return os.path.split(path)
