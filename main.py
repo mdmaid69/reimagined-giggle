ffed1d75ba5cf5ebfe2465e5bb7fcb1a99e3ef4a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:20 +0000

    closely test breakfast class weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
