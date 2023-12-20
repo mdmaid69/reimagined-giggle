4e6c81be4a7cabc7b0f7abd401cfa62dfe84c90b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:51 +0000

    raise family interest clue keepout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
