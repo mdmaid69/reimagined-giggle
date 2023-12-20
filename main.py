72dec8ef542af388d984806d88d1c80a518dbf3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:15:49 +0000

    takeback if start harm about

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
