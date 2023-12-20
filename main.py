a5614aced073900ec81dbe25f60d1ba5b92b571c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:57 +0000

    offer position estimate protest top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
