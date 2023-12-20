13aec323ebe4471c167779928f226591694c693e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:31:21 +0000

    split disturb explanation money job

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
