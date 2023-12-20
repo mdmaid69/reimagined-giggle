4ae19a4e5ff8140912ee014d194286a84d0379fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:47:16 +0000

    mind judge thinkover comeround hardly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
