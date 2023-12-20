c6b13b3759a744fd102d0e66e4410246e678771f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:57 +0000

    nice them impressed disturbing alive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
