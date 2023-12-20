d060d9c5183cbebbecce1f5768fc5439e934fc16
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:34:58 +0000

    bet door up effect position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(lst):
+        return min(lst)
