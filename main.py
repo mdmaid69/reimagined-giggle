0d177587665f48744f1b7251f44c60ec17d67e73
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:55 +0000

    soft why plain side juice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+for i in range(5):
+        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
