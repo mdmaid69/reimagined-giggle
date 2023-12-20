61497935af09feac0db5942d92c4983c00f0b649
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:48:49 +0000

    idea deep recover cause tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def log_message(message):
-        logging.info(message)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def remove_duplicates(lst):
+        return list(set(lst))
