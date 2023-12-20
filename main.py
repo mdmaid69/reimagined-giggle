d6485aa35c76a3814957f5f44f5621cb87d26e7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:10:52 +0000

    exam many disappointed nerve tooth

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
