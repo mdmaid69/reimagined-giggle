d9e4362157f8453275431cb2dbf1808b0ce61640
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:00 +0000

    mark vision business after approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
