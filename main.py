c2876835d581b28d25e7056e55f3db04c696860d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:38 +0000

    breathe complaint open tell exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
