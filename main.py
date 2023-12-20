fa57b8a6d1f51666aea0e25c0826294f8ee77ac2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:47 +0000

    568energy the public secret exactly

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
