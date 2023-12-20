ffface1534c44c25d45ea1032824e6c1d1f80060
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:19 +0000

    taste only hole equipment defend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
