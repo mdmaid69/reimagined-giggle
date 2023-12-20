be4e724a145a4b1dee91df7953536e85c3cd7691
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:52 +0000

    safe travel rent bring attach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
