2dc7ae6a4f7a0fb121aac35dde489165b4c416d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:14 +0000

    provide method consult flavour sound

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
