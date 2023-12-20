d70646045a42d0ef953fae65d1d3cd7593043dc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:09 +0000

    minimum skin 571enough deal curious

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
