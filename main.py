b1b00e576a4bbf4f665671a01ab5903a75ec7c95
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:05:48 +0000

    quantity thin responsible refuse careful

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
