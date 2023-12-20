0c0f5e1a62acc126b8f61f6faeaaff84444ce1b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:24:42 +0000

    shade increase fly flavour drawing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
