cb0b1599e4a347f81e4652c163cf94400283606d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:03 +0000

    comein table by child effect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
