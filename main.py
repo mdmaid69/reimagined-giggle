7d2d7d5d21223633519f239876b4bb8498e412fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:21 +0000

    year head lead name comeround

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_max(numbers):
-        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
