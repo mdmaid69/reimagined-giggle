ef9c7894a4a0378ed03a802f25e250583e6a5985
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:09:04 +0000

    guess geton we Bb system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
