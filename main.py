2cb7e57fe63d9b11b81277d4988a7f4f78fc4405
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:05:31 +0000

    rule slice copy Cc broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
