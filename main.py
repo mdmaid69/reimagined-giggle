6691c918be447c3ab5554b87f839ba3014e77ecf
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:46 +0000

    breathe turnup Ee beside rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
