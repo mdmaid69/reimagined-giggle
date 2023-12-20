5318c7256c27851abfcfad7de474c81327ff5c87
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:50 +0000

    fast complete breakinto peaceful before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
