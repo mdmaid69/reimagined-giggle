32dfa0760e9ffe5938e3519863782588f304b19a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:15 +0000

    whistle whoever my blind indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
