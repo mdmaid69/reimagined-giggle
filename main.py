fcd18ef038c2b2f534a57e5c7bbc654078b7d61b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:52 +0000

    lend down brown male Zz

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def convert_to_hex(n):
+        return hex(n)
