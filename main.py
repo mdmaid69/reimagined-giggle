dfec8ce48c5fa741854e43a51621a23ffd7e641b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:28:16 +0000

    giveback means deliberate section tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+x = 10
+y = 20
+print("Sum:", x + y)
