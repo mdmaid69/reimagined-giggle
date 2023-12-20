63cac67f71d851b6a66f2019648de18046375a24
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:29 +0000

    warn hollow short plastic business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
