6106ec6178065ce3603e67ca36d7e64db831fef2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:59 +0000

    term around violence unit punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_power(work, time):
+        return work / time
+import platform
+def get_python_version():
+        return platform.python_version()
