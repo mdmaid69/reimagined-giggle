9c0d25f183ebc8d7751b07a949cb7e8a7dd48d67
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:09 +0000

    contribute arrangement file crowded sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
