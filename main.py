bb72238bd395ad7e20b9726c777cde1bbc2672cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:42 +0000

    artificial appeal weak go print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
