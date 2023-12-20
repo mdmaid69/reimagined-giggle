3c0a49af282c7a00e21715988ce8e905803b2a56
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:04 +0000

    vision pleasant tidy goup crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
