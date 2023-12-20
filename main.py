94691e74d545a40bffde58dc0559786a846455be
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:14 +0000

    thankyou tip even rid traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
