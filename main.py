b582598d62957369a75697458ac560f1b75b15dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:58:09 +0000

    open refer classroom previous could

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
