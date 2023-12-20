acad8cd30ae6531e0123bc3dfdd11482b811e976
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:39 +0000

    tip by journey spill stir

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def find_min(lst):
-        return min(lst)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
