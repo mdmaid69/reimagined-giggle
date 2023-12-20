4788e8319389311cc8527ba3d4c9194adda0daec
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:46 +0000

    protection crowd lunch permission stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import collections
+def create_stack():
+        return collections.deque()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
