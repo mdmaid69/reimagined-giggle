d5da82069ae6acab17860c74ea60bee0659be219
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:51 +0000

    beyond would hit once deliberately

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
