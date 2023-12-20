4426f5a89fad3c4d51e7710e8e70fee2318ba4c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:20 +0000

    wave test contrast intelligent naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
