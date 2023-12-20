c1703b9eff4f665274ed16fdcb1634a44ff90864
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:41 +0000

    narrow suit comfort unhappy street

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
