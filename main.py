db987ae5b43b1ef361f4e2ef0f0ca4a307d82ff9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:19:26 +0000

    speed workon rank conclusion authority

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
