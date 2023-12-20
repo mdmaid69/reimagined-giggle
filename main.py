7e069a5a164d6bee7fed6bea136baf8553823116
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:11:30 +0000

    can dear talkoutof teach piece

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import time
+def get_current_time():
+        return time.ctime()
