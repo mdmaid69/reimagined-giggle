ae8b87b2e5a1dc5b124043d52ca8c3d7d2f9c474
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:20 +0000

    concentrate Dd sea guilty dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
