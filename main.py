d1afd8e9083f0e9ad4e51816127b3171fe8f2cb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:24:49 +0000

    hang grateful slide lean clothes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
