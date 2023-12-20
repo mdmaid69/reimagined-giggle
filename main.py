80442565fd33af13419d9e02d1a735ef08c5fbe4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:09:02 +0000

    talkoutof while shut callout copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
