1c7090750de44b9ffd1117a334f5f405ec01cf9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:55 +0000

    vision pause nervous sleep 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
