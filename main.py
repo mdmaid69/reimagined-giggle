6b34c5b5445c6c65d5a493bee993da04bf7e8fbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:28 +0000

    forgive free march after in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+import sys
+print(sys.version)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
