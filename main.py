9564f6b0a28e7c1efc3cb5a1adf49a47a55c5987
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:28 +0000

    quarter Vv wherever fine wash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
