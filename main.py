4cf20b938955d081ae3ff7bf6c95793b2ffab715
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:33 +0000

    meanwhile 567enemy success damage imagine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
