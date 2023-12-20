593a47d0daa29d2ef94916109f75a4587dadd47c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:57 +0000

    design responsibility immediately fresh pay

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
