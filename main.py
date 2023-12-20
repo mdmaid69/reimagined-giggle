efd8791a55216357c2d22d65def5aa93f7c496d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:26:25 +0000

    arrival solve see touch freeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
