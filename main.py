6be0bcba9cca68f4ce7bd5d11aad034fc1c12245
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:39:21 +0000

    Vv loss pollution pinch warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import collections
-def create_queue():
-        return collections.deque()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
