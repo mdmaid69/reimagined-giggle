6975d4e9d5c4ab5fdb6b5526166000a7aff85456
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:49 +0000

    trip forever I cost head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import os
-print(os.getcwd())
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
