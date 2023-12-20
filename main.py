9fe2ec0a37c05868305102131f7c2bf98dd72f2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:38 +0000

    soak sign extraordinary spray who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_list(array):
+        return list(array)
