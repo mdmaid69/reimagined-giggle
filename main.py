f57e0d16905d9c27e83188b3add2072d963c2683
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:11:51 +0000

    advantage difficulty say floor while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
