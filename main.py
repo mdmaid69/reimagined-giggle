170391aef6eaf118b7452ae8a91fd163cd34397a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:41:50 +0000

    used charge mention tone ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def extend_array(array, iterable):
+        array.extend(iterable)
+import numpy as np
+print(np.array([1, 2, 3]))
