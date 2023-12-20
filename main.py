c09740cbef3be9b608500dc6aaf0e04a7b51df48
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:34 +0000

    object sell relief question interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import sys
+print(sys.version)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
