1f74333221f1faaefec5d8249abeb8649f22d73f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:55:56 +0000

    children high variety quick memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import os
+def change_working_directory(path):
+        os.chdir(path)
