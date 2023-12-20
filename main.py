1bd2919d2512b6434391f73933b1505c41bb9cef
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:16:05 +0000

    group fire by shut education

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import collections
+def create_user_dict():
+        return collections.UserDict()
