c25b9c46df87d86dd18b2861ffdb764dc3f5faae
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:26:08 +0000

    character sting giveout trip die

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def multiply_numbers(x, y):
-        return x * y
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
