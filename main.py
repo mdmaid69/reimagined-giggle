282e2dc8775c1ec6083a101515f5ce71311aa10a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:00 +0000

    border apart spin walk cap

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
