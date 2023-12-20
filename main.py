be5834dbcc066098306d470a9c2b11b870390898
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:48 +0000

    reduction swing up put boil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
