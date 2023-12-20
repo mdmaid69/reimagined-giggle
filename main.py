af4643c646671e56467da261f92b8c130f861643
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:12 +0000

    how all responsible price rare

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import collections
+def create_user_list():
+        return collections.UserList()
