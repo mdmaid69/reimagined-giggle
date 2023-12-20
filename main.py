f0c8c8289ab656aa3e1735f3b3156e9d4b8122ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:10 +0000

    succeed over strength cross never

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
