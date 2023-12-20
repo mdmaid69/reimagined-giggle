e1dc428630c38b2e8684cfd701a73b90e7f46fe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:57:02 +0000

    before consult depend patience turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
