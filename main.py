ccb46395d2ddd7ec7eabad40403c73700e319f3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:40 +0000

    direction surface negative concentration underneath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
