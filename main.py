24978c71b23b541b54ce582c2a2fc074ca818d32
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:54 +0000

    play brief regret hit warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
