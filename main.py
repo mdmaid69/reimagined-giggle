09e4214a535b41d931eb0d34eacc370cb73b8879
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:43 +0000

    568energy introduce pretty measure home

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
