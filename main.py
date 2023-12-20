65ee77d89753bea3ae29cd7dc1ad366966c6d4c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:12:48 +0000

    feed treatment somewhere succeed too

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
