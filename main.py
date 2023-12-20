ac916cc90f4c5c9a8bf1406eedeff4899047b3d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:43 +0000

    bend pick hardly project whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-print(os.getcwd())
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
