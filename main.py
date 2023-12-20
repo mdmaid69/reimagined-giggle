c75f86a588b498691ed82d85f6a9bcad3d668a17
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:26 +0000

    value arrangement Yy Uu powerful

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
