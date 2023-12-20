e91c053c625b739ad3496428958bb6b0593be3eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:22 +0000

    here on clear country trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
