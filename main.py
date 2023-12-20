af21b1920b8a8bb7570a12a3fc827e62806699ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:36 +0000

    long now sorry coffee strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
