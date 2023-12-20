6c309dd17394c88558c99288ecab4c707accbc62
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:25 +0000

    wrong admire lock wake fasten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
