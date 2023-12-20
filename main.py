cf62558179caa39f1c3a23d9f30e34b097466a80
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:53 +0000

    importance turnon behave fault less

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
