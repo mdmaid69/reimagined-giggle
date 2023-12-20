fcc34010c4e8cff9309621e696c2be424e41518a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:00:36 +0000

    shock light fast crash lower

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
