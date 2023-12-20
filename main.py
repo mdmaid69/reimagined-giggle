4b682dc70733f6915017fc2e073f43ff4b5d549e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:42 +0000

    impressed exploration excited related recent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
