4fbf97d35c741e0b7d59736593f941fdeb08b23d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:00 +0000

    division over score post control

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
