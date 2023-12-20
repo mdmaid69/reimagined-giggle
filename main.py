bdf29d9691423e5de7c529db181e984b296643c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:32:39 +0000

    recent far moon fill remark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
