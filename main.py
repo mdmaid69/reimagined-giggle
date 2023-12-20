cafb69558738c4f093e849dcb967a4ab0a516605
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:41 +0000

    fix if flight breakfast indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+text = "Hello, world!"
+print("Characters:", len(text))
