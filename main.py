543faeb60c47969229fb82b5c8147b38c476d093
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:10 +0000

    fight buy loan this letter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
