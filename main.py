8f4642c9340405f61dde2e3da6006782046f1924
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:59 +0000

    pen probably imagine place preparation

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
