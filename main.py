938bb46bf0fe279305a1f8bb493ea98dc34be9e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:38:55 +0000

    sister expect sure motion anywhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
