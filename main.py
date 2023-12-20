e8e526d350da3c57e9a832bf5376795a00b037cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:16:27 +0000

    minor disappear major hold bored

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
