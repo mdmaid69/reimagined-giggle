bc56256661222219d4d7281a7c9a2369493d6a70
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:44:23 +0000

    nerve public start account allow

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_octal(n):
+        return oct(n)
