effdaaa8b30a869a5a974f4e45b23546dda1bf11
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:31 +0000

    shoulder ocean battery wall recover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def is_palindrome(s):
+        return s == s[::-1]
+for i in range(5):
+        print(i)
