42602b7682d4821e6003b40f3f73e7815f1e32b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:00:53 +0000

    compete far come material straight

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
