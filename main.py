1ef2e92bc70b61eb94bbd21f80a0132205f8d803
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:15 +0000

    phone operate recently flow history

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
