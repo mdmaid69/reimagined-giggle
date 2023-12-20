388bf37a6328231f1a77f501aedbabcad3895e24
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:37:08 +0000

    relationship bear traffic minimum estimate

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
