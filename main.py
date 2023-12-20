45eca788451010825835596102e32c5811d2b1ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:11 +0000

    unkind until new choose trick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
