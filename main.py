91c4a811c4a72f670745f6a3c83d2788e644051c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:29 +0000

    probably pleasant mix unkind often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
