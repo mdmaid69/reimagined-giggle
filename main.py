16f747cce778fa4e946822f895a3886777655357
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:24:55 +0000

    shout extra callat bite within

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
