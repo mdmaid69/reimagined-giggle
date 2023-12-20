cb554b7f9d52fbc9c40c1945f369464245b06adb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:52 +0000

    drop hurt poor 566end tense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
