b2ebea313629349052e7184d139fedf3e6ef2a85
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:41 +0000

    enthusiastic artificial under shadow strange

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
