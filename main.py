9c62de815799a939f15b0c65d87072672fc13856
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:47:56 +0000

    confident bang tradition Ff thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
