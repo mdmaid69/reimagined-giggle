368f0e6c375d8d03ff86e26e6f634d44fa24240b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:32 +0000

    improvement hole guard tense test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
