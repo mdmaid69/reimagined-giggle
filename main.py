89e40a497ea1e004d0a598d9ae8e5cec81c2eb44
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:06:09 +0000

    right action environment disappointed check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def square_number(x):
+        return x**2
