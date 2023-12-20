5e7562e5c30ae1cb08f36cd4a6ff1ed3c912c04e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:32:39 +0000

    escape centimetre pay smoke impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
