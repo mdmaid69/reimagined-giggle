21544a63cbdc28e1793a9559df5f2f4b33d92976
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:14 +0000

    method difficult pool 571enough scream

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
