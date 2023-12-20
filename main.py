f3da8f536b0cc6d00ed257f7558dae6b21e471f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:06 +0000

    then thick cheek kind this

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
