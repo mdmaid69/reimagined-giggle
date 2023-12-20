a04cd57d8b0abfb06b155e8c0458d2b8c70d1a8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:01 +0000

    right let regard raise herself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
