592814c3005325e45047218679437207ee948b7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:28 +0000

    accidental behave produce except shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
