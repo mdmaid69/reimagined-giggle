626c118d7e6d97a0da2148b1ae37bcd880e216df
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:43:16 +0000

    talkback pipe minority account why

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
