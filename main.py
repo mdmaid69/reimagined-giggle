d4e74099824ee5cb042f2e4536fbce881ec5d11c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:56:53 +0000

    ball seriously fairly phone follow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
