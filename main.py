5f34ba8caa9335e8017390a953d68d5ef7953857
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:09 +0000

    interesting collect goon whole several

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+for i in range(5):
+        print(i)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
