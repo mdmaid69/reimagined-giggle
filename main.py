e926ad5c20cb76cc231cae5c198bc667190bc070
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:56 +0000

    we comedown average have borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
