973cecfbcc2d3b7dd6b352495e02828726754ca5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:20 +0000

    already charge equipment matter contribute

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
