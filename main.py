9a1c6d3c9dc90434e369a235888151e186bc1c35
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:10 +0000

    mind century call identify on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
