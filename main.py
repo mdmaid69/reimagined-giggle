59f085e2a4a7c1636447a7b7ac8e29dc88cc96f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:04 +0000

    high shadow fasten but contain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
