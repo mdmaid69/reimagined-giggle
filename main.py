08be2c67ebd780e02b71163da3bf539ae115a0ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:15 +0000

    race Kk cost dry joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tensorflow as tf
+print(tf.__version__)
