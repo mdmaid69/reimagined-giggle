83a76f8eddd3feadca61dcd1f3c01d3a8f47e98d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:00:25 +0000

    friend raw twist bury push

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
