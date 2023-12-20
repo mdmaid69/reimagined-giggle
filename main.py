db6ea3608632f7e506f75d54eccfc1b052e3dd81
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:05 +0000

    comeback spite criticism habit full

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
