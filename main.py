6f66a7059ee2c8a7cb21bb72c31e64cb4845ed7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:21:02 +0000

    let pay thinkover face fresh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
