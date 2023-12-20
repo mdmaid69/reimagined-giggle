ae5c8d4121a245574d5391ef14f6dbc0a20ba223
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:05:41 +0000

    shopping trap target doubt fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
