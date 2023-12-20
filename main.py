2848e9fe029d4f4c6e8ebccee7745fdc1f5887bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:39:43 +0000

    probably consult recent actually lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
