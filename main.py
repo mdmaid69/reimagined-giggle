72dfbdacafb3555db60b162d20747fe3d71116dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:47 +0000

    claim disturbing split clever around

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def reverse_array(array):
+        array.reverse()
