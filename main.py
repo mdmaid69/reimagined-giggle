2879506bc02a7b6ac621ea0d1c170a83d76f8fcf
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:59 +0000

    safety general closed trade ago

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
