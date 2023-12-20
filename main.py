b01d249c03f6ae607e3015f24a3f595b8e098599
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:18 +0000

    Dd rescue pump importance weak

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def multiply_numbers(x, y):
+        return x * y
