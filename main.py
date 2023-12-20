bddb676187dd9faac8d32a6c55acab1ec95c7d55
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:09 +0000

    correct natural mine mood afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
