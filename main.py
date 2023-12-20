5ccae1c86a6accd58e1a25cf15d18ab0f76d29f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:03 +0000

    example forever entry fault blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
