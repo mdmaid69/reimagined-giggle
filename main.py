aac3b0e2c606c8ba6af41e841af00335e3556ef8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:40:13 +0000

    film kind press gradually flow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
