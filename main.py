8ca63535cc0ea9fd2e4b2cd3fd95318b2511e776
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:15 +0000

    excuse accident 573entertainment disturb excuse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_elements(lst):
-        return len(lst)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
