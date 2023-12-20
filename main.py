02708ab3eebdc0625360f5747a2bf0cc46709f57
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:34 +0000

    cold typical runinto worse noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import sys
-  def get_python_version():
-        return sys.version
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
