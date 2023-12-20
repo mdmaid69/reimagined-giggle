0aa8197bdccc5916f7c982a02ebd284dcdd086cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:00 +0000

    should deliberately half father push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def add_numbers(x, y):
+        return x + y
