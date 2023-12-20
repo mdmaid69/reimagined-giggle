f7841c4e6b3ba0fab9a50124772d39dfe5befbda
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:08:37 +0000

    sick far clear search lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
