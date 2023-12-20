9c4f21bf2cab20bf25a89b6225d295ea8ae04b1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:20 +0000

    shoulder near oil convincing experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
