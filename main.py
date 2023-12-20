fb7dc456243305f3cc97da89aac8d6d8ad764002
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:16:54 +0000

    pipe use do argue corner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
