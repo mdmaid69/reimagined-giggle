fd0f4e722b0edc6f389dbeda52aaba074d249b08
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:23:51 +0000

    meet protest duty total smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
