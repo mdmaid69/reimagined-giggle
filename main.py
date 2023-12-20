66853ca4406ef3ceae7b6c5d966197986382a85d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:00 +0000

    favourite cook show act certainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
