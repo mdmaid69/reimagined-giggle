49e3d10063633f73e4a34cc613d9dc54d79a4868
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:44 +0000

    sensitive relative direction ordinary deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
