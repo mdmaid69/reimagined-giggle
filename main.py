fadb57d3354416e4e14b1b4257588599c1fc49c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:38 +0000

    sky regret early work shine

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
