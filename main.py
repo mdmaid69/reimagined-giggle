d8160e1b9330ad29926f5aa34760e89c5d3487d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:40:23 +0000

    swimming exchange badly ability thirsty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_string(array):
+        return array.tostring()
