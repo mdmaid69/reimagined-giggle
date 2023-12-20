78882d8254c2527e2a1180e547bcba14c984995f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:46:56 +0000

    turnoff quality typical themselves visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
