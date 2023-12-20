6a8a8f99ee2b4439423e3741ff848546056171bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:06 +0000

    way wear naturally welcome each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
