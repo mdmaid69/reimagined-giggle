7dfcf8d5dfcf1e50f5bb9fa979d0b5ec2cf21c2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:26 +0000

    so weak partner cry pile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
