8418b1ae3ab5729f8960df68501bcf5496a03518
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:53 +0000

    prove insist afternoon getup silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
