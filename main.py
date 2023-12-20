0755f355ff5e9f1b964d2865a81c2f1fa4ce9e24
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:33:03 +0000

    stop complete car before issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
