4d6c6b797ec1c2d9196d50584da0a2704e32a79b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:56:02 +0000

    rule her drawing thumb sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
