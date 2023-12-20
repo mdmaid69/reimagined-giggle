26103dc4030d3862d07105fa3f6a16c1a8fff913
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:56:47 +0000

    word stare home deep much

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
