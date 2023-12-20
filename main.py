23fd6a1aff0bb1d7a8313db5b3c0e8c211d43751
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:22 +0000

    criticize closely watch ready turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
