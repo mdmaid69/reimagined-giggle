c59deef73b769a2120a44b459addfeb39cd3aa1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:45:32 +0000

    condition whoever butter keepfrom confident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_logarithm(base, x):
+        return math.log(x, base)
