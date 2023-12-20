baae9e36f53425f907872a6f9fbc5d1db0a0f159
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:26 +0000

    price usual imagination test aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
