66eb99beebd1abf063b7ba671b1bc72b1d7d5cc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:07:52 +0000

    ask plenty embarrassed start lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
