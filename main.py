7b261d17cbd27515a6e06f49a80dc3c45ef40a12
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:22 +0000

    environment design vanish opposite case

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def multiply_numbers(x, y):
-        return x * y
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
