64f0eb46874c46381121363026ff441a1aeb70ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:49 +0000

    exchange tone build plenty spring

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
