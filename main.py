05bc91cb892d4f616c5f0440bf40e8839ca22a43
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:41:56 +0000

    confusing hold disappointed value shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
