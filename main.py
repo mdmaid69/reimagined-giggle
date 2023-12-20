f1ba34e8df41db014d3a83cd39b0134a42efe950
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:26 +0000

    brother how grand foreign building

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_bytes(array):
+        return bytes(array)
