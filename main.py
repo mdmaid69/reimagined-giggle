c76b7a8aa566c80948816c6f9a9a6f0c6418c3ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:00:47 +0000

    tight lookaround closely catch rich

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-def multiply_numbers(x, y):
-        return x * y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
