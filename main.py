88863fedc3690189ad052da8f48dfc1572d668ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:27:17 +0000

    use judge strength ocean extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
