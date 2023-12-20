dc521cc75c2e342ff306962621ee8d09add5df70
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:56 +0000

    bring can keepup dress friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
