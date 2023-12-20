6f292323ea0e772dd8af9a70ff7467028b556529
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:41 +0000

    workupto system present convincing sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
