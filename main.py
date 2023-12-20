7fb3224cd5998efaf5557d9e234c7b23f986557b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:01 +0000

    man solve calloff meanwhile copy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
