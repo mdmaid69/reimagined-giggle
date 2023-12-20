1874c18e9e285d7cea894c2e9d9b266597026e35
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:16:27 +0000

    beside quiet sing mind watch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def count_words(sentence):
+        return len(sentence.split())
