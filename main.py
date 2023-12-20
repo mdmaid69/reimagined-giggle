f0c117127670eb63cb3434807d64b8b92df3c943
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:59 +0000

    stir invitation imagine tooth tap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
