a4dceb40351b9f5c289c7d60816087aa09dde96b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:29:32 +0000

    copy surface effect forget majority

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
