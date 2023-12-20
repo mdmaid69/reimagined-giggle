b6bab817fdc5e611cb8ce1d30d01b65c12252696
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:32 +0000

    tell Vv lookback(on) authority want

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def sort_list(lst):
-        return sorted(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
