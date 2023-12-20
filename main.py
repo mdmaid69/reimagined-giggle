0b6407fa00bcc871c750710f95fb87f318033d70
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:29 +0000

    force area mix left involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def reverse_array(array):
+        array.reverse()
