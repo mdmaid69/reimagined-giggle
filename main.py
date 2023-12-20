15375b0b3d5ae34331cdf0abc8c1476fe79454df
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:20 +0000

    region he tear thank sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
