4dcfb75b7380349dc3c77abc91ffaf9223cc51a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:37:54 +0000

    father character independent callout ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_volume(length, width, height):
+        return length * width * height
