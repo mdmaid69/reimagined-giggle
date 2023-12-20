8dde5c0f266bbc9f30116948595463d71a635107
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:00 +0000

    spot holdup worry party priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
