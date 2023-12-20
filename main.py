5f860b12b3fd1ff1f48662a388640c10714763dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:16 +0000

    forever gift them low debt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
