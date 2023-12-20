0e940a0072938df97aa2df9ba2973620ea8c6a55
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:16:38 +0000

    giveback criticize hot mistake police

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
