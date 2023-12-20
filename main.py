9eb3e630440b908dd3b5d2af071d8b266e4800a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:18:37 +0000

    somewhere vanish broken hurt command

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def split_path(path):
-        return os.path.split(path)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
