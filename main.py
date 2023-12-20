a7e131920309b5ebb0afc61501bf998ef7bad2f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:00:36 +0000

    touch remark special drawing wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
