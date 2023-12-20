976d525ab0725a356b38e68537e4d00f0365081a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:41:23 +0000

    fill impression reject injury risk

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
