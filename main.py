138c3aaccd0b199542ba36ab9af09f8b83356e15
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:11:53 +0000

    offer price warning sound whoever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
