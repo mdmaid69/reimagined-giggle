8ddaf836feeae372a2b70cc797bb239bf5e66d2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:57 +0000

    whoever lookaround tend inside better

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
