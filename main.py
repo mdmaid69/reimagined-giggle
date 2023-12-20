4a5503edd1e6a006f8e779f5e7292f7e191470cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:27:04 +0000

    pronunciation also abroad strict fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_item(array, i):
-        return array[i]
+def get_array_typecode(array):
+        return array.typecode
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
