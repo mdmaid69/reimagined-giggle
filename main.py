3efef412a234609135b820875ca31f2b8ff5ef92
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:32 +0000

    Ff stranger music build about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
