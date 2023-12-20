d80cb86ced7176eeb2b73339955542c2ee74e373
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:41 +0000

    design welcome organization talkoutof left

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
