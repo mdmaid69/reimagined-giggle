3ec854aeccc6c0f2c3b630922ef5c75e43320e7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:14 +0000

    body trust Rr steal ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_cosine(x):
+        return math.cos(x)
