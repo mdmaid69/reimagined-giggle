0ec6ea3f2439f1c692e9821c5e4ea2c4f1fb0c9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:08 +0000

    sudden sleep organization shall punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import time
+print(time.time())
