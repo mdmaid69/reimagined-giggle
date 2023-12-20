8b4b44120c28186446df0d400032609a59936e5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:10 +0000

    be disapprove sleep back central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
