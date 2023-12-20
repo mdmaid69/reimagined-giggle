350ec70b7e99c154eedf122db45cfa2c8e4e0f91
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:30 +0000

    suddenly old gentle anywayanyhow wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
