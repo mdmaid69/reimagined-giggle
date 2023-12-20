747f8b4f86620841c6abef4300ffc26ac4d18227
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:32 +0000

    suggestion near dinner find pinch

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
