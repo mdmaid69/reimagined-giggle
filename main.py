0bfb422fd088cedff4d4d327a39e39e5c2d1b065
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:47 +0000

    furniture bunch opposite suggest field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+import array
+def get_array_item(array, i):
+        return array[i]
