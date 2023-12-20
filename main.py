0d092a813b73329ddf882f999dd98d6397c2fb8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:16 +0000

    that Ee entrance slightly convince

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_octal(n):
+        return oct(n)
