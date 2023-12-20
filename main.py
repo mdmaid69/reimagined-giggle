8d5beae3c4924d4ef8b908b6c5eb9e9f912caaf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:27:52 +0000

    figure hurt emotion hurt usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import os
+def get_current_working_directory():
+        return os.getcwd()
