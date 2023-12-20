3bfcd39627b0ab51fee763dd997a9824319e1982
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:34 +0000

    legal activity some noisy control

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
