8b7a52c165654ff9b521be453372e3f93833c611
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:35 +0000

    face thankyou government distance plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def square_number(x):
+        return x**2
