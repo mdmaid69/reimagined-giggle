7b9e8efba669ff6422f446d676ecae4a1a7edd7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:01 +0000

    pull pleasure greeting start notice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
