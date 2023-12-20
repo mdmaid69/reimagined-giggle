afaa07d4fac91f76afa2b6e5f6afc21de7257682
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:53 +0000

    act trade uncertain spread turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
