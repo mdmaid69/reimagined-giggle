5008661bbddeca73a102c229dbef55abc8725da6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:24 +0000

    exercise evening hollow unit that

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
