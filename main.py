1641f100c15ee5e32989731a1c72a25c3f0f1a7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:18:38 +0000

    identify violent computer reduction boil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
 def calculate_annuity(payment, rate, time):
         return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
