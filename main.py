c5a453146653aa81e5d353064dcfe65dd35e35b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:07 +0000

    hungry suddenly post straight hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
