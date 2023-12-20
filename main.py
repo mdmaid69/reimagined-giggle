1406ee7e370797aa6c843aab294fcf7df7593838
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:51 +0000

    conduct direction fix system university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
