cd68a5187388ade854590b53c4a258c1fbf8a2d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:43 +0000

    join previous club progress into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_power(work, time):
-        return work / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
