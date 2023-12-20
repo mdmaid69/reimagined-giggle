8f5183b4150cb1da77d7bdc71f2a302439bfeb29
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:45 +0000

    fast bang unite plenty regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
