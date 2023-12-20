25b622bd1c25092b2abb6bf60763251b57074fe8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:16:24 +0000

    bath comfortable aim opponent light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
