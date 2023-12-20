e0cfe9e2ea80446d8dcb32ebba6fab97561d1ac2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:49:28 +0000

    base fit free away tax

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
