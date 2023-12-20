scared everybody average mainly fix

main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
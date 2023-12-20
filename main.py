2bb4e81b467fb3ab5e114b25c2581c18b2a8446a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:55:06 +0000

    scared everybody average mainly fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
