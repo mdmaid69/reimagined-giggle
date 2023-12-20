b11eb3d8385c69bb4f0240dff74559642bef4bcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:33 +0000

    trap prepare period tend affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
