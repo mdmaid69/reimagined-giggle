b226752ac4986d5d4f7a4ca303f8a4547436f79a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:22:14 +0000

    cough left post poor laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
