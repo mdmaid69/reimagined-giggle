aa76d8d26f0be99aeebd4f24facda9cd686c95fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:06 +0000

    season even time sign strike

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import sys
-  def get_python_version():
-        return sys.version
+import os
+print(os.getcwd())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
