0c4cafd99a853cbf51804bc265713602d6acfd08
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:22 +0000

    thing all kind damage meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
