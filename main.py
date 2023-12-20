f255711f37f5e496f1e656043665fed75eecaf84
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:09 +0000

    glad lock naturally flood holdup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
