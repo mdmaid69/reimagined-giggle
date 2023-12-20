c24a1f4e0f9660943944a2c540740a30c28b6dc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:47 +0000

    laugh evening bunch broad comeround

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def is_palindrome(s):
+        return s == s[::-1]
+  def convert_to_hex(n):
+        return hex(n)
