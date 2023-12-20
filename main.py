d49a78ed69dd87798327a9af7dd3dd244b8469ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:20 +0000

    school when accidental similar getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_logarithm(base, x):
+        return math.log(x, base)
