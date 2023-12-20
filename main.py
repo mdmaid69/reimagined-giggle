95447697bf5ca8e3311b2b7ca9afc880a60b695d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:30:30 +0000

    everyone interview base offend light

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
