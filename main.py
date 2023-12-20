971e5555ddb8ecfa41342d5312498d82426e1ae5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:29 +0000

    anywayanyhow guess quit interrupt afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
