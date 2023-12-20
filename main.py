5965f651ff02a14ed6999b984a04d57f8ff3765a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:03:47 +0000

    worried process week pot breakup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
