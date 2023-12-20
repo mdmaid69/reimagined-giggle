0504c2ee589517238f17c03da730ceabdc90dd8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:16 +0000

    desperate near whom early holdout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def get_array_as_list(array):
+        return list(array)
