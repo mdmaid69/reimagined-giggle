41886555a250757f8f90cc7ff34e0e42e1d2a68e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:54:08 +0000

    thin trick common skin hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-print(sum(range(10)))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
