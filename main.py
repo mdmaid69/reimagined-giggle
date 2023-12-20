cb5c72ff8a86f220ff291b14af40b3e019917d80
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:02 +0000

    contain attempt ability world back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def square_number(x):
-        return x**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
