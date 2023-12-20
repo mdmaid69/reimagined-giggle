36fe433288b88477f232a8f9849358da5aa524dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:41:15 +0000

    nor freeze job furniture right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
