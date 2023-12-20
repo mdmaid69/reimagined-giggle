014a9c75fb0e6ca28ece29fef655f0653e55d209
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:10 +0000

    effort dress scheme brave get

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def subtract_numbers(x, y):
-        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
