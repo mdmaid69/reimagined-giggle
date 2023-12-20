bc32dc7f4ff15551e47f58bcd970b7ebd6dfc955
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:58 +0000

    behaviour flow bang well return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
