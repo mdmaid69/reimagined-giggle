0393b18c39ed5584efcb732fcc922b2c1444cf0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:50 +0000

    thanks once definite worry workupto

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_area_rectangle(l, w):
+        return l * w
