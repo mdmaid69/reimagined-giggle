01c6df40134571a140e98a04fd41077c985d0028
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:45:45 +0000

    great this giveout basis far

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
