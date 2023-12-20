4d6e5768804d434a287044d747138d70335ad580
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:22 +0000

    runinto idea basis buy test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
