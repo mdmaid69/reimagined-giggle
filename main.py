54a3d6aee16f0e90814eba55b29dd510be0bcd13
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:57 +0000

    study healthy reply Ww organize

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
