28a4c1357f55fb60297c960a8075e8ce869091f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:11 +0000

    564encourage present cruelty split water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
