ba8f5b6607d8cdea00bb504d95649c2e421b32fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:34:41 +0000

    through path fence exchange related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
