5680682fa6b74b218507c8a8a6b9aa9dd7e0aa0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:11 +0000

    movement scrap fear smoke suggestion

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+print([x**2 for x in range(10)])
