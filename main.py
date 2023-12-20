4e8aa25b302f380d8ed7b4625e2a8f5343b26c16
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:39:11 +0000

    success common hurry mood coat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def is_even(n):
+        return n % 2 == 0
