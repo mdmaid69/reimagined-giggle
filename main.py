6b177c2d17ece3ef001ec4e60fbf4759fc7f422c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:54:15 +0000

    frequent glass plan occasional fly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
