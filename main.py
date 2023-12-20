3eb427424babc57397d0794a4c1fab1845b0465b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:50 +0000

    rid possibility border traffic way

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
