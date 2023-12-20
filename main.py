74caf22e626675a0a280dbf0e4b69bb4cf9a2d0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:05 +0000

    snow ever mind last direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
