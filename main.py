3c28e1ae40f54bb45f2aab1256db75fb42dbd124
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:09 +0000

    constantly post bag cheat hair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 def setup_logging(level):
         logging.basicConfig(level=level)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
