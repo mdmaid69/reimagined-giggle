bf35ef55f295a2ab9482367f932db908f7795c1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:04 +0000

    mend wake gentle comeback abroad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
