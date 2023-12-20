0b9cc6fc183ab538b01b45ab5f981c10d42c3fb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:18 +0000

    base police partner complete there

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
