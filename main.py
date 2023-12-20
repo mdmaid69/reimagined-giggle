57a6c651b61b666a2dd8b8576bdc1ff962cfa7c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:30 +0000

    university blind best close plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
