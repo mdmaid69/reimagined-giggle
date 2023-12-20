33605522f3ca734879bc532a6faeef91b69cc4bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:06 +0000

    confuse makeinto miss female pencil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_speed(distance, time):
-        return distance / time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def convert_to_binary(n):
+        return bin(n)
