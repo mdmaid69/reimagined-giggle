6d0173167a0498654033065f64a02b035ca7948e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:53:02 +0000

    stick will first clothes survive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
