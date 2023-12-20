405e36b42f2bb5d176c99a605c98c68684b4c4ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:06:37 +0000

    pass cost flight shout ignore

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def count_words(sentence):
+        return len(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
