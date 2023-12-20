9943e9d5c4958100e5b98fd7d5b34867f011b446
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:48 +0000

    watch destroy understand development touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def add_numbers(x, y):
-        return x + y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
