b30c057de1982bc9ba79340155cee8e802230724
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:18:45 +0000

    trip fine hole get interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
