e5ca2a08c6014a416ef16002364977e70efb1e56
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:14 +0000

    curious copy keepaway hurt imagine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
