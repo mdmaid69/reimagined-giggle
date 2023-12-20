98c06aef36d3442a1cebeed80f8fbb7ccd296b85
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:42:52 +0000

    shade worth side walk water

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def reverse_list(lst):
+        return lst[::-1]
