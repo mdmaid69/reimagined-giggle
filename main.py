4997ef0cad32f3b6b692d4281ab10efbad7162eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:18 +0000

    gooff comeup intend frequent pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_density(mass, volume):
+        return mass / volume
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
