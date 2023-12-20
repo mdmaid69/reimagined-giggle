60af3dea8994a8d4d65cce4690d1baad8a5c298f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:38:45 +0000

    constantly speed pause pinch control

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
