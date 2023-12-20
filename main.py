212c7e17e4538ee7c6e566590dd69d63b258c3fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:26 +0000

    push reserve fire surface load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
