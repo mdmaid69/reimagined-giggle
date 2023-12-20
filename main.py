921a611b64de55a2a9a930bfcb871e77d6fc3555
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:01:59 +0000

    although round colour call own

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
