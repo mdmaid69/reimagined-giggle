b9076a649566fd968ff5f703b306cae11e2dfc88
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:13 +0000

    children extraordinary fast train spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
