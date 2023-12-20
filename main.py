dfd4b4416e7f968d14952752d64717773a53e769
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:57 +0000

    cool thing hungry occasional later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
