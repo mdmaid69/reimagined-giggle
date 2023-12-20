2dd74df35861608e794b7c7c0512f4045f7fa95c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:47 +0000

    polite list also exist instrument

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
