ba92cd071b49405d7791e4b65f44d45082f4d49f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:47 +0000

    disappear importance ago mark position

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def find_unique_words(sentence):
+        return set(sentence.split())
