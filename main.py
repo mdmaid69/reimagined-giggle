e6049561cea3fc14c34b5d0fcaf93729fe772698
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:43 +0000

    Nn short mess naturally injury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
