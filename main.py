3f4ed6bdf5265c56237ca7335b82491f8c1c1dba
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:38:21 +0000

    gap space game everywhere week

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
