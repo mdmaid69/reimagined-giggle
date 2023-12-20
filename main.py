c7f740f3ec0413274065e0f80d018d36ab84d337
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:41:44 +0000

    I sometimes plenty smooth back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
