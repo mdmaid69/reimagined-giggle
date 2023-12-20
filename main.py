8c424cfa2220d2083b884c6fd221a4db8febf7b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:26:25 +0000

    Yy juice increase description ours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
