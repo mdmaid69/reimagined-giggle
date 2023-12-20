63ee7af85fe99dc0a10d97d68e5d442f67634911
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:13 +0000

    getback translate upstairs clearly edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
