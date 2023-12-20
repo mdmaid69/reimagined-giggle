77bd9a10f899e75b014e7c6d1a549ea509bb6bbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:05 +0000

    brother art properly sure comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
