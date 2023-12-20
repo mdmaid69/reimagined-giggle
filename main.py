e59783b9ff65cb16696583bfe6df29c5812ac48b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:52 +0000

    straight heat maintain traffic awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
+def is_even(n):
+        return n % 2 == 0
