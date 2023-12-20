6534a43ce969d79827bafb671c254fbbbad47c8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:26 +0000

    rise ask hit thank over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
