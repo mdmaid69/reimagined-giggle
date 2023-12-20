e1984dfabb799430d3005c7b53147781e46f7138
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:44:25 +0000

    speech relationship how near programme

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
