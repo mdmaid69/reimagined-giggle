dbe7469961580fad15381487f1ab7db495e62e0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:28 +0000

    rain service respect getup own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
