c979ecce059ba65f708d87be5eb885e576de10d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:11:36 +0000

    mother hold store doubt occur

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
