d793570d04e5b35e73e93f5b7bdb24c4f4e94624
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:17 +0000

    formal film hour whisper aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
