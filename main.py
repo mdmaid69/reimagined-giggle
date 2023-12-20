c35bcbbe73766a7908b14a980f50aae2b8ddcfee
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:25 +0000

    suck on surprise instead cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_list(lst):
+        return sorted(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
