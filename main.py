20351d377cb415a8b27430ad53cf0befbe01dfe2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:18:45 +0000

    chase wipe around Oo improve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def cube_number(x):
+        return x**3
