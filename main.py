920a54b76c65bb28e5490ecf4171b9f749de9ef8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:16 +0000

    announce responsible child great blame

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
