485cd973db65ec0b209a9c034eb4061ee52899f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:58:39 +0000

    office too back patience all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
