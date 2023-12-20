d27e040843cc7805c68b583ae80939d8aaeb7bb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:18:31 +0000

    Cc belong doubt born actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def is_even(n):
-        return n % 2 == 0
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
