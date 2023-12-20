43547140053aab9d1c613ba11f8a7501d2a741e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:17 +0000

    interesting punishment famous would authority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
