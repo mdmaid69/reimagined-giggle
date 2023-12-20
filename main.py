af2c23a92e0c4f8f5b6072e98cd066bbcb3bda63
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:11 +0000

    giveup move exam tradition quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
