6a53501f1e0fd2bbf2d9ef1381af5b1476e6d014
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:41 +0000

    balance error water welcome before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def print_python_version():
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
         return sys.version
