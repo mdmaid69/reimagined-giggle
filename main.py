3255ef30c2206e95358a5f8ce6d447d6850fc38e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:25:38 +0000

    achieve unkind reason receive treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
