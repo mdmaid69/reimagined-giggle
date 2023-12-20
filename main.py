2531012c623efc15f918f7a7423336e41a0a9cad
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:09 +0000

    unlucky which way nothing reduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
