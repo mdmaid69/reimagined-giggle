a215128e21f8be7aa9d80c5e7025248fd044b6f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:33 +0000

    dinner I aware apparently 571enough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
