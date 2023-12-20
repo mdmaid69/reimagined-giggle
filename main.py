2c812bcff7ad73badbd6ff52b1a90267c8372189
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:23:13 +0000

    Bb thank head contrast cinema

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
