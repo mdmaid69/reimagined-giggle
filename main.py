fda63370d37f8fade1770e3360bb7376484ed165
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:46:13 +0000

    cry pencil reserve high noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
