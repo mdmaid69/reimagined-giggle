9e7c9411916860987923373d3b5c7d387a5ac8eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:04 +0000

    hope card regard cruel meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
