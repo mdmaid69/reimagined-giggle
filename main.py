4a608773083eedea280cc416bc9a3f43b76e33e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:44:56 +0000

    round only mend behind far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import sys
+def exit_program():
+        sys.exit()
