16c8070fedbbcad2b1c5a48520e41c04d23ca92c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:46 +0000

    major school round waste breakout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+print([x**2 for x in range(10)])
+x = 10
+y = 20
+print("Sum:", x + y)
