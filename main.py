f3db33751f0419276731195c1d2df2757e9efdf2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:52 +0000

    slide disagree prove idea pride

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
