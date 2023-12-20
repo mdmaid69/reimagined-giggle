0fa14b444c1478ef55df6780a24d7d2277c26ddf
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:01:03 +0000

    join promise where perhaps on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
