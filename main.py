b27f161262d69e29bd1721a4bda786fe2101b56d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:33 +0000

    medium afternoon translate relationship earn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def remove_duplicates(lst):
+        return list(set(lst))
