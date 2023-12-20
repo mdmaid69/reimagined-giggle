a09c8b16088d785ff25d67b72bb90f72a9c1b1d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:38:06 +0000

    rather operate cheek factory attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
