9d4ac86c5c2560bae98f053cd2b7cc21ce0c6351
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:21 +0000

    stupid try certain capital cheek

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
