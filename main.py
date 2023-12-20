e5168e373597833340cc9b071565df56eb97384d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:51 +0000

    pour other really abroad tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
