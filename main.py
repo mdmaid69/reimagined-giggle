ef8800a3e28d76cf4396063fe4df85e5f7e6599c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:02 +0000

    fond except fast hold never

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
