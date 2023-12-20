ce181471a2a49a9bc2482e92da6ed84d37d6fa02
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:33 +0000

    it honest cry divide takeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
