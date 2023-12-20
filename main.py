d241e8be852d6f1d7edff3d05f64457780d10535
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:26:40 +0000

    needle post theirs regular trap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
