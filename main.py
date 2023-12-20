0396ac009fea073c1dd5755490231a6c2355d48c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:22 +0000

    payment mess stop tired safe

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
