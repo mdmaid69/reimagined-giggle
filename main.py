6173766d7062eaa09ac923e9f83580fb20c60d37
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:53:05 +0000

    ordinary wrong notice freeze keen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
