1718352aba67f771a79cf387bb942aa3b50349e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:21 +0000

    mixture throughout equal cross sting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-print("Hello, world!")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
