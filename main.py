575d6007ae2c0be53f3261aaf2117e6a527ec4ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:31:53 +0000

    trust party praise tongue contact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
