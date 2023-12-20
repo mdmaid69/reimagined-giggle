b50023fe18846a31d748fa3dc95fc89027f95abf
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:18:12 +0000

    bunch under closely regular suck

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
