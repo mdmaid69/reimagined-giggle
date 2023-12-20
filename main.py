934809b7535a4373b7836c4c06ee8dd3cc074d46
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:37 +0000

    Nn description he declare female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def sort_list(lst):
+        return sorted(lst)
