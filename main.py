d1494ac3cdb08733e7907325ee6e9851bd9fafab
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:02 +0000

    trip depend embarrassing vision story

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_work(force, distance):
+        return force * distance
