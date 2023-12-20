a255677c894ecd2191c1beb3d62c1abc178e2c67
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:07 +0000

    unfair takeover personal cause trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
