f9d0b61e94b0ab143de3d17e34a3a4119030c4c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:35 +0000

    confusing it except fold range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_string_from_array(array):
+        return array.tobytes()
