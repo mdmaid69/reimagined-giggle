eea654d0a481e3b6bc9d049f3ad2d59db414b2d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:06 +0000

    hospital unable lose terrible any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
