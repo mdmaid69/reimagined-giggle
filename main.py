7fdf49468aac731ec265efd6cf3e72b4cb64cade
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:17:27 +0000

    breakthrough outside 564encourage offer from

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import re
-print(re.match("h.*o", "hello world"))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import array
+def get_list_from_array(array):
+        return array.tolist()
