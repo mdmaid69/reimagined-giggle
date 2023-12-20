dbad3130a877568490273cd3cb47a78130ca5e03
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:07 +0000

    573entertainment normal maybe speech hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
