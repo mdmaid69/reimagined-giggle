059aefd75ef5db2ad54a20d0c1999790a39853d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:03:43 +0000

    bringforward walk risk pull contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
