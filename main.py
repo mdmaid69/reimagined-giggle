043ce23b9bf1d4e2f527cad4ef4dfb155e151187
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:01 +0000

    mess view declaration produce doubt

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
