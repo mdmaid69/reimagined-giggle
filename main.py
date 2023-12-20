7d4f184a9cf67bf594e8d9c248a21b9df1d8327c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:56:00 +0000

    takeout visit benefit print flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
