aa78c57990583d40511d543467557046d6cf8fe8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:56 +0000

    emotion even warning limit skin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_sine(x):
+        return math.sin(x)
