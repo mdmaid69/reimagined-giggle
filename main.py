887e69c7788b6246a7820d7b430fe577a4f18cf4
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:14 +0000

    imagine only recognize apply importance

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
