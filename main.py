727434382bd146ce6cd917c1512a052e18e73655
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:53 +0000

    original bread present free protest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
