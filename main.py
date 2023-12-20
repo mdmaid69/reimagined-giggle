8476b3f4fa694a87d2f8627c5fa8482c6c55b518
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:40 +0000

    doubt trick difficulty formal catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
