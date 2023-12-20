57522625cb750929d32595aafd4110b8aa0164e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:26:24 +0000

    petrol die mine twist run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
