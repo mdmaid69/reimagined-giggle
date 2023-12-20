f636c73d78536b3179496ea55cddbef5c1d56d9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:57:21 +0000

    sink trap another stiff cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
