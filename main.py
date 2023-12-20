bc8b4ce2eb17a4b7bb1f31101727c30828c43808
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:12 +0000

    account wake occasion birth bringdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def cube_number(x):
+        return x**3
