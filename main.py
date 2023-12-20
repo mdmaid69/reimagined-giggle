142d5e724168e4c610e77da192d043436c13c089
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:22 +0000

    trial block behave pleasure circle

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def is_odd(n):
-        return n % 2 != 0
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
