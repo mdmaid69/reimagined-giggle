5e4b809ed018a5cc8138d2c056e63ab06824ed63
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:23:00 +0000

    tongue used collection complaint should

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
