ea195aa45da4fa62f6bf8f8a93279b8efbbd97e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:16 +0000

    difference someone absolutely subject proof

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
