64232e06b211f0f94646d18780ab73b510c9ab56
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:55 +0000

    best backwards cry fat condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
