043ee7ae67ac9189e6a17a5e320d6935d390d9ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:35 +0000

    turnover ride measure team careful

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_length(array):
+        return len(array)
