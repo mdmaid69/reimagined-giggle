b07711cef3e74054c8e65d442dd88ed3cb2ab4b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:59 +0000

    delight whistle date argue priority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
