b96fef44f94532d0390d3b5d5a5199591588332d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:36 +0000

    present friendly characteristic substance fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
