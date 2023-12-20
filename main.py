45aea40f0418bfb356671047f2f1ef755fe069bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:08 +0000

    perfect rough push exaggerate lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
