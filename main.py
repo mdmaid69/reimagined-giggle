d98758ed4a183f1db8a22c9c1a4f2f8ffcfe9958
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:48:41 +0000

    safe sing measure exaggerate frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
