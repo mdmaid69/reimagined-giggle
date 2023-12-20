e5f124eb37841cdc7cf88823c40c0f3038d54d44
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:39:33 +0000

    speech worth thinkof Ww lost

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
