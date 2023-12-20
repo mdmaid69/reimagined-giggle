6df07e423307a25838726ccd0053b2bf16b52641
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:45 +0000

    crime illness sister once uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
