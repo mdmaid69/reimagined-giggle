73196f27e2a29dff3d071bed3a634fe8f328feb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:12 +0000

    cost equal seem crash entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
