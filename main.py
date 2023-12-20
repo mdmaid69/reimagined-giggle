0aed39a3b4c143f5ea653b6c41b1b8cc82cfa473
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:49 +0000

    waste holiday belong whistle blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
