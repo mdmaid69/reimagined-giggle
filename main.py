00a4e555ebf004f83d27bd114ba0cd7c52a0336f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:10 +0000

    introduce space trip obtain lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
