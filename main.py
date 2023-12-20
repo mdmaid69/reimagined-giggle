f51803ec8ea322cf4be2e133e58c984130ecc637
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:56 +0000

    find interruption top task ability

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_octal(n):
-        return oct(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
