b39600165045765718170ae3f9e10c319b8782f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:52 +0000

    consult takeback record nose extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_work(force, distance):
+        return force * distance
