606ff6fdefc7222b37528c144728c220f35bd091
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:20:55 +0000

    sail pay something drop crowd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
