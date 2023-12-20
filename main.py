3c6612522aee7aefec6cd2b370a623508f34daf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:36:30 +0000

    exercise indeed reason present trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
