ba15cf68a7fc388b79a891a79d03526080dd3ca3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:35:03 +0000

    public song type accident eye

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print(sum(range(10)))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
