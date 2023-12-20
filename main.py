d4ce6abe4b69224fb0e221ac651d4c7e457406f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:18 +0000

    weekend sharp any cruel switch

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-print("Hello, world!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
