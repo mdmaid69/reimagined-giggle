a4e98791118798f7e4eea26106878322522c287f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:10:36 +0000

    secret always film angry surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
