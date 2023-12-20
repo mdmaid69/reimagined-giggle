9a2f9929ff7359147c60ec6adead7c899c73d931
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:32 +0000

    hospital track division luck or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
