c8ede01d51b905e9b447af268dfaa59188898b71
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:24 +0000

    guilty confidence clean comfortable arrangement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(5):
-        print(i)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
