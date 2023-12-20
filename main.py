d7da6ba6d0195984a21bb5790cde43bc39952dca
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:16 +0000

    speech afford beneath hold through

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
