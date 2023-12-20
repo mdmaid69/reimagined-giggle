d00cb8bcc6fb1c8aa5ff2e687aa29d19df0cc584
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:40:00 +0000

    new bill bear clear understand

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_rectangle(l, w):
+        return l * w
+def sort_list(lst):
+        return sorted(lst)
