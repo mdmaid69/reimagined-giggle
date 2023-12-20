c146861d272a230773f957a65d7a729a61a417a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:23:45 +0000

    depend place mean tall by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
