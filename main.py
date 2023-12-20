c7d91218a74296a1a9d40a7f2d8e2d06c3eab6f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:13 +0000

    recognize value object your giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
 import logging
-def log_message(message):
-        logging.info(message)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def setup_logging(level):
+        logging.basicConfig(level=level)
