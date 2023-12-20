f6042e99933ab26af99074160f02751d586734d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:27 +0000

    opponent scrap touch way start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
