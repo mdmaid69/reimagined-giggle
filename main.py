35f7214435fbb71a29ac5340092993aa38bfe922
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:47 +0000

    this community closely giveback keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import getpass
-def get_username():
-        return getpass.getuser()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
