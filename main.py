7bd50882556e9c3f511379773ee3e3bc71de359d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:54 +0000

    coffee angry cheap pay on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
