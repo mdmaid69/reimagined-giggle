21c238ee8ac485b83057a55bb5667be20c804bdb
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:32 +0000

    mark collection far rush 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+print(os.getcwd())
