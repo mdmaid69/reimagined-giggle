6b689b1a27dcbc76836800e0b69bea224a9d019a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:21 +0000

    creature admit effort violent join

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
