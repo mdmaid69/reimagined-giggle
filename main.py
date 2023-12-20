77c7fa7c5172c2efcebe56e986324185cd24208f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:27 +0000

    considering police relief slip sail

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
