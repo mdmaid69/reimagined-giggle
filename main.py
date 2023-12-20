7e81ea7e61ee21ed09a0e2e5a08e7b87a4850ac8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:21 +0000

    trap tidy rubbish upwards hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
