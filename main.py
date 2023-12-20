9a92a9f34a2c9b309338c73789df4ff26b247093
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:38 +0000

    moon lonely offer basic pretend

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
