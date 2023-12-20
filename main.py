fbfc499c627bb8b96e8e7d1c09714046a9178497
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:40 +0000

    worried Vv act place activity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def calculate_speed(distance, time):
+        return distance / time
