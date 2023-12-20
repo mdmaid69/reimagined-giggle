6c609cd4ac945f7c1ecf4ec7fb66504e11ff8219
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:56 +0000

    priority well contrast separate match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
