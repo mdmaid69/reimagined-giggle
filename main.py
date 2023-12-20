eaa4eedfca3ea4446aaab9a87d4cebcccd2ed415
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:14:00 +0000

    face inquire bread cold concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
