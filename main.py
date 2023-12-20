d352101f9df16e7bfded060364551b10759ee3d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:51:44 +0000

    communicate development immediately morning full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
