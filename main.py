8b1a4cc94071611d44bb6612dfaf6dda7844f2fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:26 +0000

    thinkof achievement regional smell usual

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
