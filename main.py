188419caf1a3f66b47099a23671d876451946e35
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:39 +0000

    nigga murder introduction peace shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+print(os.getcwd())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
