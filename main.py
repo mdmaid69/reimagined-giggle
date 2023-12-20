55471edb50f1f7aaf1f66ac6ab3355f022114e2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:14:08 +0000

    happy willing original behind press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
