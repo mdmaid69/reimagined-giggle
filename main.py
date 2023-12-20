9dbb13cf742a12d539983834107fa1e17b5335c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:26:19 +0000

    check authority impressed some convenient

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_speed(distance, time):
+        return distance / time
