b4f69b738b865fde59e71b648b1a13e0c23c75c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:46:34 +0000

    exchange Itself house nervous exhibition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def convert_to_hex(n):
+        return hex(n)
