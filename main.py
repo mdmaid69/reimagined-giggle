353b8ec9e54d0952a09c5ffa5abf3708a8c5fb0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:12 +0000

    danger field date shut outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
