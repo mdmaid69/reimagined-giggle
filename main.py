db02c8293646fa3996909f9455be34c6b0bd2f3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:09:26 +0000

    mistake fight buy pollution set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
