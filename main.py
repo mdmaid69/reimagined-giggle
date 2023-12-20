2abb2696af3f2b8e34171a21b4c2acd2b9858ae7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:17:33 +0000

    brain second influence 566end behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
