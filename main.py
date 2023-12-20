e08580d64180135f352a8e0d9c2710ab981b6ef0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:20 +0000

    immediately movement post agree acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
