1d6a0eb07982b5956cda9424f508cd4500d23bd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:28 +0000

    risk swimming keepfrom round sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import random
+print(random.randint(0, 100))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
