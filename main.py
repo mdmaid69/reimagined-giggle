159265b56ade1e60cc78fa34a4276c247c4a5657
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:04 +0000

    sweep motion disagree else yesterday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def cube_number(x):
+        return x**3
