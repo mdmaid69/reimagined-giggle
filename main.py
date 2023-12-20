63285408d865d188557c77212e0a78e346489df2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:27 +0000

    call light cross train black

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
