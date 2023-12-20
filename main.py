7d85632fdd7c37d275fcd9f8e7f65cf727cb0fee
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:15 +0000

    not lose square solve free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
