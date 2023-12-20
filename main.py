0975cff504efa5cbc449f57c0669cc0627aa2026
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:54:18 +0000

    over call probably trade test

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def square_number(x):
+        return x**2
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
