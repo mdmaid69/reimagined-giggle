1e970cc26882ecf516fc8dd3dbad6b14d73193ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:53 +0000

    closely cry one ear pretend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
