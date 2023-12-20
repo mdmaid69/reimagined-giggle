08b627a7c3ff799b257583fe8375bbdd93b4fcab
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:09 +0000

    beat temper extremely address position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
