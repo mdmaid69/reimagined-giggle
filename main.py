74f90d917211f7016fec88941dfa4f8b66632765
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:50 +0000

    basically pick development breath disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_item(array, i):
+        return array[i]
