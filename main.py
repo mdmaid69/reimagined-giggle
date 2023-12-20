3bde9f6bb78802bd4e2b36c2a987cbd6dd9cc7d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:47:24 +0000

    big happy buy Tt finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
