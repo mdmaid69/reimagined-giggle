a7a8fd7ca1c001e96ef355f391e20839fa4d9ad6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:07:01 +0000

    noise slowly smile accident experiment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_priority_queue():
-        return collections.deque()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
