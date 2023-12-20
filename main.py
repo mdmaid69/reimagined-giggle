9d0d7c5efa004baf4c652c4d0b417ee4a5b4286d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:20:04 +0000

    popular concerning die strange practical

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
