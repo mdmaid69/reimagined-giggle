0954158d5e93a64a8bc174f4a22f1e03699b9291
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:51:15 +0000

    pin together wide content trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
