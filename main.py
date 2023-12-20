8e39bb21fdc9b77729f8daa80e1d6921c8e4686d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:31 +0000

    become comefrom reaction dry pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
