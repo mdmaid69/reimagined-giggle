d7f1597cd17d60bbdb48690b5cc6164e45cdd32f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:56:14 +0000

    lead foot smile real apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
