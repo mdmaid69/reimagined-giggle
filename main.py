d4b0d7cd7c4006556df084aa421946c4e47a2ce4
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:31 +0000

    thankyou allow murder appeal unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def is_even(n):
-        return n % 2 == 0
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
