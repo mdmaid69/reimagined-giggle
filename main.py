7357b64c06bd745e64987ecfc5b5b9bb8b6cd120
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:55 +0000

    hair letter pattern persuade for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def reverse_string(s):
-        return s[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
