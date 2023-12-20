d4fe82d82aa055dbc4561d0293e8195cc54f9e79
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:39:14 +0000

    fetch car damage stupid file

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
