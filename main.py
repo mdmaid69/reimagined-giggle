3e972e8e9096827196e959ef242b42184c5dc471
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:20:52 +0000

    offence guess cough on smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
