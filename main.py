bfe2baa8b3dc70c5c129de2609baa5fb103e7e1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:05 +0000

    can dangerous 568energy next justice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
