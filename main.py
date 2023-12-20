fbc20a8ae52ddf53cb8612f36517be9209fe0d2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:00:46 +0000

    direct guess later thing police

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
