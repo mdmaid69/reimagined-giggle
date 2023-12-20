9d0dbc9223baa6f58611db507c0f3651c42476d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:47 +0000

    aim raw bring strange wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
