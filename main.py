5ef542f3f128ea6875de100b10df82ca475f498a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:23 +0000

    unusual matter please advise sign

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
