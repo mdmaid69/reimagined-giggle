6fd51a1e5689c1e5ba9a34de3f977bf0b88e24de
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:04:51 +0000

    wander deal congratulations equal take

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
