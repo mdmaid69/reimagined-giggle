a44e49b842d27af3b64eb9e0be35d1d57492620c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:34 +0000

    react rest stamp thick straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
