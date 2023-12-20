6a2be5ba236b85b5a8596844bdbb21c23fecc350
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:17 +0000

    area disturb vote tidy relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
