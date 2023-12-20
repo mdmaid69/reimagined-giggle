16714185c0bac0139ebb6a6d38054ee1edf6f2e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:52 +0000

    himself sort skill doubt prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
