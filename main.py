3ffb7e971ebe901bb43aa309f183f9846b96e774
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:42 +0000

    turn exactly extraordinary opportunity sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
