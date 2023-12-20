747d61ab27b94298f6367602773eb308b4f769a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:18 +0000

    violence shop answer order division

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
