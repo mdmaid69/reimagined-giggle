0166b819d68efea567a5265c55ebef3406b2df5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:16 +0000

    design extra activity simple opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
