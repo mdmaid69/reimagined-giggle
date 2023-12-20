56c01b19495bb0199d70dd74b09765f8c0d44b5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:10 +0000

    check straight criticize like neat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import logging
+def log_message(message):
+        logging.info(message)
