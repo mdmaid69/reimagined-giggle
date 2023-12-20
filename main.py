617b71758be0422bb6a7076d51b44b7e490105cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:42 +0000

    trouble talkinto interesting nervous square

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+name = "Python"
+print("Hello,", name)
