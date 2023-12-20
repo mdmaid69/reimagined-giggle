6c5f2513d0e67dd740887c5ebdd732983f1e6544
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:37:28 +0000

    provide right fork average easy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
