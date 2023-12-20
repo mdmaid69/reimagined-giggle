c2be6cdd6b3bfb4deb185572dc3d40593b9ce30c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:27 +0000

    custom among make star on

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
