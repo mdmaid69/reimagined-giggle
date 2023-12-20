cf256acf7de9ca496304c4ab0c414ba94afee2b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:19:01 +0000

    telephone someone organization much exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
