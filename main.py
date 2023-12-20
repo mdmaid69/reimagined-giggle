5bc0435e12ad5fa3502de46269883ffc6e02fe75
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:32 +0000

    responsibility neck firm pleasure pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
