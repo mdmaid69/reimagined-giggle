c906fe4ab82207eb33e7b3d91a8d971f47b82fc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:08:38 +0000

    still public fix nurse long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
