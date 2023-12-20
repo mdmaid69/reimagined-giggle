dbdd95b174a0c27d85f9996c393896a85c20ac92
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:18 +0000

    other joke pleasant violent fashion

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_itemsize(array):
+        return array.itemsize
