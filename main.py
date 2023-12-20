e801bdf9fd048de312b2975df670f34a5276fc9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:22 +0000

    sharp visit occur goon memory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
