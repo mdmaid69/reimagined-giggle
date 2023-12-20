129fb44dbf607454d0a92c9569d62d53c9ff95df
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:50 +0000

    decide right onto late grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
