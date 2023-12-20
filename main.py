77d405694b326d6d09e5d1a373b39271dbc0b80f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:10 +0000

    promise name cry worst plain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def convert_to_hex(n):
-        return hex(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_current_directory():
+        return os.getcwd()
