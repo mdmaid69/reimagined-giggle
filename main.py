b2a6ebe82b90c9e28eb34c27349cfb5ea1b9acd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:56 +0000

    to central rule hers stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def count_characters(sentence):
-        return len(sentence)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
