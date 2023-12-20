0e2f975ca0ddd446a420ea795e173b416c345c86
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:14:17 +0000

    beginning sweet post here first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
