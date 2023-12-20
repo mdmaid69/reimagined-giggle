b33fd4ee1579dbad2f621664e510bf2ab633d87d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:59:33 +0000

    shut strict flat injury squash

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
