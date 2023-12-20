c246a09df4c6ac03bee60f27d2503b036fb0ca16
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:41 +0000

    achievement every rule waste whether

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
