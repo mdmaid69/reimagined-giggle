4ff01580704f3ddae4da9c4b811accbed7a39beb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:18:45 +0000

    possibly health empty bother beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
