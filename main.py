1a33cf9e8289766472ccc4e71cbf90ead8e1f027
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:59 +0000

    lost idea command achieve especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
