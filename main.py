dce16298005c1e8476c30ec95474a8b4acdb8d61
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:01 +0000

    advise exam speak trousers promise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def count_elements(lst):
+        return len(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
