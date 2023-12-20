3f0d4566b40d865b31aed2fd3294f83a63e67177
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:19 +0000

    repair rid necessary coat organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def is_palindrome(s):
+        return s == s[::-1]
