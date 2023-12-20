7fcea17d731c7b135536032f6f963acf0f6efd0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:51:25 +0000

    weigh television police listenfor mail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_density(mass, volume):
+        return mass / volume
