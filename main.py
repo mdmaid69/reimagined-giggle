3b9645b090aae57cdb1991c7844a30729a4b3c4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:01 +0000

    sleep holdup appeal picture criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
