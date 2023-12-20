89f6bad87f2e5da1ea5669bd10200416016b2cb6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:58 +0000

    slightly Ii correction field it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
