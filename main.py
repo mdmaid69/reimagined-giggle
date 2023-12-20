3ee19a1c658062224b29d9e95b63bab2c865673a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:58:15 +0000

    tall dance prepare crowd drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
