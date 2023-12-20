1ac29857e673f482d99d845f1066e1cb94adee10
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:03:41 +0000

    space family expensive mark neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
