233b559f8402eec302a1e64282a5b42c87a5744a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:47:35 +0000

    become apparently Jj tidy tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
