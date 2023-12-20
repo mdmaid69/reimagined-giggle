b4e531aa49530a5cdc0614772a40f8013b8bd2de
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:51 +0000

    glass breakinto pour treat pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_typecode(array):
+        return array.typecode
