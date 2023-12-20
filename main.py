f5ec12b2d17739d10e6213cbfd2d453e471d790b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:30:20 +0000

    post obvious grade pot drive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import os
+print(os.getcwd())
