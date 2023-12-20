15efd03b573b4f7cbea281a7e5c190e27ab0638e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:00 +0000

    copy report welcome emergency considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
