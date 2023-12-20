f7435a9119a9a465440148d4f16f14b0f719e97d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:23:22 +0000

    combine wind statement strange boil

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+for i in range(5):
+        print(i)
