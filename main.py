593116a9027cd59d1b92095f3c9c0643395fb659
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:39:33 +0000

    rule anxious give home curious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
