a1ef0d69d2985bf5cb7eb22eb8c78022b0cf8f5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:47 +0000

    force either afternoon thorough breakout

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def count_elements(lst):
+        return len(lst)
