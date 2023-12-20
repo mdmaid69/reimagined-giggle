c96f4cf5405ef1c7433ab8ad325ca0d9fd2f2aff
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:35 +0000

    succeed fair completely store goout

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
