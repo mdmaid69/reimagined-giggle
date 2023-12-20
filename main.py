efde9b83d369795c33a81b6210c712b42a51a95a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:31:45 +0000

    after vision influence black comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def reverse_list(lst):
+        return lst[::-1]
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
