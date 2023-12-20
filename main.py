09190a5ee47fd58db0c9c37d7c5915a58e5747b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:27 +0000

    flight than total smell spill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
