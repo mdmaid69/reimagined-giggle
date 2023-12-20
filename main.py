1109c0942c85d3000b6f592848a9e6d323a43296
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:52 +0000

    accident characteristic wish neat other

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print("Hello, world!")
