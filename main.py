ceedf358b3bfc40bafc2c623903f14ae0e6a661c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:35:10 +0000

    greeting prize equal mother swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def remove_directory(path):
+        os.rmdir(path)
