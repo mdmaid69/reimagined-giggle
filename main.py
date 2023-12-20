fe9dd815a762a761f38fa52e880fec496ebf87ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:53 +0000

    fly different right but belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
