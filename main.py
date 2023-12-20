2f768dc21f30d530c22bfc3769263a4b43a87223
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:57 +0000

    decision neither past position family

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+x = 10
+y = 20
+print("Sum:", x + y)
