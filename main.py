981520d4885a25397c7dbc058688e0e227cca47a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:16:14 +0000

    listenfor shopping dark gate measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
