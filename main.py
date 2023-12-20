b0aaedd9a96099b207bb28bc38fc3853fafa72f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:43:54 +0000

    pretend plenty custom ago wheel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
