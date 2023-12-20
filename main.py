9fe7a1eb78e110e1d5ac148ef9887211d8afd370
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:30:40 +0000

    talkoutof into figure hurry fit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
