b189ec4cea79ca305f8dbf79646627bd581e8776
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:46 +0000

    sweep seriously fortunately too holdup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def cube_number(x):
+        return x**3
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
