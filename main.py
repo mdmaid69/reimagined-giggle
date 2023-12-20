7167d52f636cc38c271b282d8532ae9f8ad00ab9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:42 +0000

    principle blue pure themselves present

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
