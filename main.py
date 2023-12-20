e84dfb807875d7f1443c9231443cee9ada27859f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:51 +0000

    war one no slow design

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
