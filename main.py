3bc112cbacbf8df1a05938c35c78d3cc27c84ec9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:06 +0000

    car regard explain lend release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
