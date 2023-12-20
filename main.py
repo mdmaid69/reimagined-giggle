e6262b7ade464b9ddb808b401ee9daf36f75e86d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:20:48 +0000

    how glad common autumn another

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+print(math.pi)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
