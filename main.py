2a0fa866d04a46f41cd78b88790ddb04a642405e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:37 +0000

    common fact tie quickly central

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print([x**2 for x in range(10)])
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
