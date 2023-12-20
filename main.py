4752ccde6d93af5a523d0e434988bfc19b755f91
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:39:45 +0000

    lie false improvement formal across

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+def exit_program():
+        sys.exit()
