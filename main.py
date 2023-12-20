6c2cc271c6f260207d45a751d9478ea815591a2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:07 +0000

    mistake past hole trust grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
