4388a1896ce3f96a462fa29aa5a35e19f422de3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:41:59 +0000

    congratulations gooff thin ring appear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
