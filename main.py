f15a61e818a00c89a0b325f4d81195224125c981
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:38:33 +0000

    holdup train score demand body

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
