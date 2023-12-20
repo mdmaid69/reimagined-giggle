18898ee3a4bd95bae10dfdb9d747f3b1596076a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:41 +0000

    action game please far tear

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
