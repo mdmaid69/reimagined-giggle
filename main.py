c2398a1fa96dd7e50ee8d7976e8e9b98e2486bd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:34 +0000

    record wrong pair appear during

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_pressure(force, area):
-        return force / area
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import os
+def change_working_directory(path):
+        os.chdir(path)
