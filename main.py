c384f689ebb24a2a28559c9ffb62340b29e9f315
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:32:36 +0000

    her welcome then first port

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
