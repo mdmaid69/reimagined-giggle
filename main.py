6ab70f97baecea60f6e0fb25807d3a64d8610224
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:46:51 +0000

    getout lesson convincing city comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
