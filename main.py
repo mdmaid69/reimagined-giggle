e92a9eeafa62a52b7809b6d69094e07a53a1cd92
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:02 +0000

    himself blue uncertain excuse why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
