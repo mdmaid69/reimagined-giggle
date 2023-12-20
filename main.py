87d53dac5c78b9ebc9937d9e76582524fa03193a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:20 +0000

    market friend pen idea nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
