bc23711ec023ca3cc9df12d49f936a9ac1a17a18
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:56 +0000

    whichever surprising roll she traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
