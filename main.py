c601ca235bd8dffea2d48b19acafdc6a0ee67441
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:28 +0000

    private always weigh offence cause

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tensorflow as tf
+print(tf.__version__)
