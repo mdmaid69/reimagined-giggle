5599c67731d6926b7597ee5eca0ff7516dc6f9db
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:02:53 +0000

    right continue when apparently machine

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def change_working_directory(path):
+        os.chdir(path)
