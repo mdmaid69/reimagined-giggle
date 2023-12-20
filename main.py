96d829fe5dd3cbbaee4eda935241e869d23feaf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:13 +0000

    prize nothing maybe comein thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import tensorflow as tf
+print(tf.__version__)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
