dd62097ee6d471a92d6f6fa5ae1ebeda38ed463d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:12 +0000

    environment thinkof apparently clever trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
