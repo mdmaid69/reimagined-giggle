05562489f1a9c93675e304f6dcb4865fc971e170
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:49 +0000

    move move either anywayanyhow pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Reversed:", text[::-1])
