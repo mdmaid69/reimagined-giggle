fa6036314c87b86ad886749d6df886e5748b5823
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:05 +0000

    meaning grade probably bill church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
