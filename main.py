203880136a67a4bcecbe4adab18dbb873c697eab
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:50 +0000

    only passenger list immediate afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import os
-print(os.getcwd())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
