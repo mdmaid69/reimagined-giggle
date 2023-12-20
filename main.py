1c3858d619b41e19cc9c1c9f9ccc5554a7f84e8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:19 +0000

    guess marry search major however

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
