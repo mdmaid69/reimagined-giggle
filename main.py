00b48d1bd14b07355e74c23edbdf5af8865d94b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:40 +0000

    study responsible level identify person

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
