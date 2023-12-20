645349cbbb5b8b04acf23663856f708a1e55e49d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:34:11 +0000

    no angry doctor clever splash

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import sys
-print(sys.version)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
