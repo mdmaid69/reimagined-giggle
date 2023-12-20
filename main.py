7b7cd716ef099174c74d9a84889b8e0079b7ec88
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:26 +0000

    Hh justice real rush whether

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
