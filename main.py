fbeec063df71dd995e2693fbd8cdc072cf88e6f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:56 +0000

    ourselves mood sing cool collection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_time_since_epoch():
+        return time.time()
