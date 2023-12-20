128523ba422a8b9c6dddf478ec9f38ad533f3de3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:21 +0000

    comeback add trouble treat unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
