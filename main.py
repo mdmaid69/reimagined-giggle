b8e09f71bb0dbb9e1430f6204bc10d0d6c2e194d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:36:22 +0000

    bang section visit tired thing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
