87929e54c8bdb19d8e2f8f7d8048998dcbf7e003
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:50 +0000

    narrow keepoff split capital check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
