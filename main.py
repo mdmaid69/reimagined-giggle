bce5c40fdd2d5fb46f16af855aeefe0d0cc0d7b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:24 +0000

    neither before might think permission

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def is_even(n):
+        return n % 2 == 0
