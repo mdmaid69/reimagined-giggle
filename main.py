baab6ce21b1e449319105055cc5c4c17f69176a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:32 +0000

    mend pocket receive deliberate body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def reverse_array(array):
+        array.reverse()
