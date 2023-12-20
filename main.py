3a63b99389f48a1176e5f8446bcbe1984df4fedf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:03:30 +0000

    speed belong average sudden fond

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
