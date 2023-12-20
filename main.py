16ff3c46d150dfa1b81607bbfb769758f3ac64f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:49 +0000

    nose protest morning suckme meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
