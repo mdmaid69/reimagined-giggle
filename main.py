ea02a2e89b190b0e82c1008370c2d61a00ce7f9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:55 +0000

    hard comeup blue turnback breakthrough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
