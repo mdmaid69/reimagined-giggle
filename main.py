f406f5eab8662a64b555ec700bd14bf6c78e6e63
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:54 +0000

    shine excuse coffee everywhere until

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_density(mass, volume):
+        return mass / volume
