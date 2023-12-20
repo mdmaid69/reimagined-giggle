5df18310a3ce14198bcd1265939bf7faa28a21f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:34:11 +0000

    miss rough listen separate flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
