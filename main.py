7c4ad19a72fa098d4c6f79dfd256b527f9858708
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:59 +0000

    sure marry snow genuine case

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
-import re
-print(re.match("h.*o", "hello world"))
+text = "Hello, world!"
+print("Words:", len(text.split()))
