b08b8e641a5a41c21086bbdc261276b4ec45f521
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:17 +0000

    left reliable odd somewhere cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
