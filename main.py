837e5bfe49574decd6cebb81edbd311f6e652563
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:11 +0000

    ever wind float 573entertainment ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
