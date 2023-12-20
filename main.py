18a8cd902a00ce10859ba0418873a6a0c215cd1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:42:45 +0000

    birth miss meal worry ought

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_string(s):
-        return s[::-1]
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
