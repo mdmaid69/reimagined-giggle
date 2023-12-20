8ea27fcd4ab1d6678630997a3da1403c0cc1e36a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:36 +0000

    along search vision fly lend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
