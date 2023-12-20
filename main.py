f8a4dde01caa6af30c58ec66069af7a85180ff8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:24 +0000

    study supply combine slow take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
