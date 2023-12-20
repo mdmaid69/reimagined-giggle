320b7beacafc568d9b1ba4d60210efea23d5c61d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:20 +0000

    help empty quarter only show

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
