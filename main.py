1949ce2173431b87aee06981f04647f496197210
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:25 +0000

    attractive regard original cope habit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def sort_list(lst):
+        return sorted(lst)
+  def is_odd(n):
+        return n % 2 != 0
