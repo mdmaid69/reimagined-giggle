02cfdaff513d860c782deae6ddae6e66877c90ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:44 +0000

    few memory invitation base phone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_power(work, time):
+        return work / time
