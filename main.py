b81bfb2cfc18d219fcc53acc9965d73bc3ce8018
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:41 +0000

    noisy damage emotion watch child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
