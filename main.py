a3124d57c48fd31ae3ca5e229eb7f358d33e1e5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:51:15 +0000

    sell underneath godown process wake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
