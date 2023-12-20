60cab642db2e68746d4c80cfcb28491a18625e97
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:14 +0000

    theirs tidy challenge post her

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
