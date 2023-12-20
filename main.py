c99bfa942d20c0068b89f7468ce5dc4ce6e2a54b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:11 +0000

    wait watch on ourselves makeup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
