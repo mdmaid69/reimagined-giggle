fad157ed54885240f90e6836468d67e287e39d2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:32 +0000

    taste fortunately breath deserve guard

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
