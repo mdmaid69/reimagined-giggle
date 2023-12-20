944db21474972b6902f03d92ef83431dbcad9793
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:31:15 +0000

    quality preparation inform fix please

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+from collections import Counter
+print(Counter("hello world"))
