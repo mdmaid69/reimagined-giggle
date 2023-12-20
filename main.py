1c531c846d0f7bdf91ffc8cad41758a467c876af
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:56:10 +0000

    bag keen trust fear feeling

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
