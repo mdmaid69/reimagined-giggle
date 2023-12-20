fe3bc9c3874380d6684ae037682a33e511d461ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:51 +0000

    raise copy upwards wall identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
