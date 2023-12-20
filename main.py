afe6334f42e55f8d55b6fbc71150e3c51a0814a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:13 +0000

    no crime fault cold move

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
