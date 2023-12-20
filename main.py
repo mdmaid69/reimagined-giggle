182c8e4a142ef7c9e5dd4ff5db12afef541a8065
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:35 +0000

    please pipe seat whom splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
