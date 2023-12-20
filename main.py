d52603f651a03531a7821cb7ddb5a53b949bd8fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:30:05 +0000

    nerve see bill like ahead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   def add_numbers(x, y):
         return x + y
