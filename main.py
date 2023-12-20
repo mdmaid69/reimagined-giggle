6f319b8227777353dc561285f8e3988157280ca0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:01:28 +0000

    threaten gift sudden reject forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
