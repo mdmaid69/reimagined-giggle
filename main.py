405ca06c25f8764cdf3f35d27e37ae24fb7e6a18
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:07:46 +0000

    there edge factory seat expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def sort_numbers(numbers):
+        return sorted(numbers)
