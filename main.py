b1e48f5f7f0c0484b53632cfe93c7971a07cfe9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:15:41 +0000

    flow base anywhere murder sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_floor(x):
+        return math.floor(x)
