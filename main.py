3f7914568398277f4a36c4c261d90630154aa744
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:30 +0000

    charge doubt suggestion afford public

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
