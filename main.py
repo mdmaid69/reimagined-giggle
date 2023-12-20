2b176a2e995a484d8b3bd1fc1d9c184cdcfb41f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:55:05 +0000

    production return anywhere shoulder Oo

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def greet(name):
+        print(f"Hello, {name}!")
