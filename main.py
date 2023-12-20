1b784f311cc4fe4f429c29e6d1e584e643711908
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:12:25 +0000

    repeat fire pause something holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def cube_number(x):
+        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
