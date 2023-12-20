d747de0ba246bf61684befe71dcbf6e0d968b29d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:27 +0000

    skirt someone right afterwards style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
