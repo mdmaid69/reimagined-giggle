e3b27e989c9ceb0048a632557d10c642bd3664f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:02 +0000

    similar nowhere argument market too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_hex(n):
+        return hex(n)
