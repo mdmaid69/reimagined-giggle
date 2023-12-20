ac14ca26ef014a2f9041a3164e48dc2f0ca8f739
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:08 +0000

    bus keepaway use over lucky

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
