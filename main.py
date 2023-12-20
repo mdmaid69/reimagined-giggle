be7d3294f1103db7b3e975d5ccf86959016d9b2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:07 +0000

    there lend ring sky greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
