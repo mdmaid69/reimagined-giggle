61a1035be258abb60abef7e0424251f732cacf84
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:28 +0000

    ourselves pin for back other

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
