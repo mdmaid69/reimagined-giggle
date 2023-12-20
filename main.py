76bc3c141b64991baed9a3deb1dae77c7aac8fa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:23:30 +0000

    develop sudden spin lean keep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
