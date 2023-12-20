a761c7779b6d41f5a86dc3c8032a91c03719e493
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:12:46 +0000

    church suck motion thorough flow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
