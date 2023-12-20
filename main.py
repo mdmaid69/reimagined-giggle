2a907b6694c44c030e36e0ecc5ee1c90b8638588
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:25:33 +0000

    even object pause challenge properly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def sort_list(lst):
+        return sorted(lst)
