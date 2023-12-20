edd8549ad8b9f398f717472d991fe3035de4abd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:23:02 +0000

    object object switch consist activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import platform
-def get_os_info():
-        return platform.uname()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
