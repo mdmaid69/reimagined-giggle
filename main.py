1c6c02a47aebae9fd0c81c5c142d157a1c8207bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:20 +0000

    average tidy bored minor spray

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area_circle(r):
+        return 3.14 * r**2
