e51590a7905e5d2c5f8dbde1fe89a9a9633e253e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:15 +0000

    letter arrangement involve keepout set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
