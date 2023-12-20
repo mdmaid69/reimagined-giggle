a6e8d2fd917bcd08c322de9b44c16ce26a298a35
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:33 +0000

    congratulations tear touch beginning display

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
