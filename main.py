b1418020ddca508fe7d62a659cefae4242e0e5d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:39 +0000

    mention accordingto whole country fresh

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
