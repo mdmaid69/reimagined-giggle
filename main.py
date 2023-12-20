897412b67bb0444ca8c64b73f1128e6d29f06116
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:40 +0000

    emergency free land best otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
