8b6f6fbe18ba5e90c7ce27efc93956dfc314721c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:40 +0000

    rather slow brief hungry business

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
