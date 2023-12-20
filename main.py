f4f9a8feb71e4ff74f885f5977ef2fa6a1c5124f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:19 +0000

    take hope introduce calm brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
