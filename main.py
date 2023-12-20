b7ffa1a9596f9d2ad5bdc1b732944887e6533d2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:18 +0000

    few nerve hot where less

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
