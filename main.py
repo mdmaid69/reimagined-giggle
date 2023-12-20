049cf509f795b7016107e5834d37bb9ca736d5bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:17:35 +0000

    earn impossible contents thankyou definite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
