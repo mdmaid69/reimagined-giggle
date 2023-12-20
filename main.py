efeafa8cc12fd55114a2f56025745241554071f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:54 +0000

    apart modern harm path violent

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
