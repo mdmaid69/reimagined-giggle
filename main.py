92feffa98cffea4b22733d8985dda1b995697b63
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:29:11 +0000

    keepaway smooth thinkthrough unfortunate art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
