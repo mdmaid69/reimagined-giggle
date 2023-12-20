1f465d3a399fa9730eea38130583061edef00d69
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:52:25 +0000

    rush choose otherwise surprise confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_even(n):
+        return n % 2 == 0
