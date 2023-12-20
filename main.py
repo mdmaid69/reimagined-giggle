1eec58aa797fa1ec512eab6f4d2149d572412e82
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:39 +0000

    original her welcome any dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
