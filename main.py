3be029c9b640f22786352eb1ed4f0699123a406e
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:37 +0000

    another moment Mm neither noisy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
