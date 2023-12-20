83b94bdd0c0825c68baf61bc0e7e8aead0c8167f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:57 +0000

    support charge schedule instant escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_length(array):
-        return len(array)
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_repr(array):
+        return repr(array)
