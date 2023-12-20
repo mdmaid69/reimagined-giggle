15f2b04ebabba45a27e17ad6b49aaab2c0bfd963
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:02 +0000

    opportunity machine yet unpleasant great

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_tangent(x):
+        return math.tan(x)
+import re
+print(re.match("h.*o", "hello world"))
