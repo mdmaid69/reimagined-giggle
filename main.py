e46288fcf85d67b6ef23f012b6e25f5400c9a721
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:17 +0000

    ready Vv guide floor unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_typecode(array):
+        return array.typecode
