4b5522a7759cc4eecd61302b24ef9e3926eddede
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:59 +0000

    range compare Yy meaning fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
