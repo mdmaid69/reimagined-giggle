dca4a2cefc07b692ee073fce9b9b2c7fb4c470bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:32 +0000

    answer concerning wealth cinema degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import sys
-def print_python_version():
-        return sys.version
+def exit_program():
+        sys.exit()
+import os
+def change_working_directory(path):
+        os.chdir(path)
