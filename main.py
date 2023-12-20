783f8c9d9d02d638c443f66530ad850a60d8b982
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:24 +0000

    year add yours sell nothing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def find_max(numbers):
-        return max(numbers)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
