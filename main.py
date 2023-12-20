58bb9c1c551c3d19d3708c4f8633da176f05af16
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:30 +0000

    or tomorrow too move defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_queue():
-        return collections.deque()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
