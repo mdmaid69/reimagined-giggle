40da5793785fff88923a2699c9274941fe7ddf2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:19:57 +0000

    soak car will right accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
