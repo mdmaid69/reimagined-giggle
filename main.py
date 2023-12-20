13845371fc3a17f206a53f77bf0964f0e6dfbf9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:10 +0000

    567enemy rather tense help rid

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def remove_duplicates(lst):
+        return list(set(lst))
