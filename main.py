f74072d6ca5de8ef7c998774f7c827a2cddbd789
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:46 +0000

    right decide public branch shut

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
