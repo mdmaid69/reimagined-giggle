0c44ca1808a01c1d4ad83183acc84b39b11db910
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:03 +0000

    water separate rather concerned minute

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
