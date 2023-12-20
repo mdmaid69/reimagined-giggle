2652d30828b96305506f5cd8084a20f61336d862
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:38:59 +0000

    warning none lift except wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
