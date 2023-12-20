e139104c1925e3b173cf03deeb89562563320298
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:36:43 +0000

    nothing you concerning challenge accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
