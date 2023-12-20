c5deeb30f039f423ffcb7c9ea1e61829ecf28f24
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:04 +0000

    simple grade alone nor connect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def convert_to_hex(n):
+        return hex(n)
