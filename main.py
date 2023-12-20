a7209275b8d7cad5ae52fb292be3536a6ed8420a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:03 +0000

    interruption introduction little without worried

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def multiply_numbers(x, y):
+        return x * y
