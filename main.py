e94ee41268007f77abc343931e15910679947442
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:41:49 +0000

    describe service basis sign piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
