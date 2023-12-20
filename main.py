5c457a869808e028b7169cd230646ee44607043c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:23 +0000

    difficulty day care slice commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
