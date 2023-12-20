db42ec76198686dc7c1c0ebafaec13df50bf53e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:22:05 +0000

    present save number Gg eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
