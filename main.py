7070d25e93942fba6402606193a67bd93f73ddca
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:28 +0000

    free stamp uniform put whichever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
