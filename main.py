519d7820639bf96019e74b5413175e32b92dca5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:05 +0000

    allow deaf takeout explanation improvement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
