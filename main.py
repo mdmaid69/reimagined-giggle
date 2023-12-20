d6851ddf12158e3cbcdb847b6b12f06dd33564c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:48:58 +0000

    fine difficult reach confident until

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
