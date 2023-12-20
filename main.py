b2f62e23718e0d2dc3e3928cb5b648139a723803
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:50 +0000

    absolutely pick together surprised bit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def change_working_directory(path):
+        os.chdir(path)
