f34dfd5e3b66d60504452a29bd6e03be06cfb783
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:58 +0000

    track what rarely quit tragedy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  def calculate_area_rectangle(l, w):
-        return l * w
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
