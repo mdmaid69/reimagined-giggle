8f6b01a27d9b8706b27aa57302a17aaf3fe3776c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:19 +0000

    juice broken meet vote regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def remove_duplicates(lst):
+        return list(set(lst))
