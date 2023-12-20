875dcc963debd6f83c914836280d60f369e414bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:17 +0000

    except promise original lead lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_float(array):
+        return float(array[0])
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_square_root(x):
+        return math.sqrt(x)
