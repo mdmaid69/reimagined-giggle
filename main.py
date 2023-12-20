cf543a84d4d6a24e4e110137682c8e1490e7b5bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:21 +0000

    criticism worse strict until prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
