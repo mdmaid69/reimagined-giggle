1ab573ee4f5d3f161aef8c5b0ca90b4e016bff0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:17:18 +0000

    learn steal confused same seem

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import platform
+def get_os_info():
+        return platform.uname()
