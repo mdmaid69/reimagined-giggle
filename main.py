248e8324cf310642783d42b602c4035d436fc3d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:07 +0000

    wave could sink shake match

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
