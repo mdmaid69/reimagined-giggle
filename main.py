71f961bbc57b12ee369a3dfe79ffef5e846cb971
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:20:44 +0000

    attempt wonder fortunately separate away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def convert_to_binary(n):
+        return bin(n)
