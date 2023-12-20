034e45bd9109617411f87dfbfc8d17553afe95a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:54 +0000

    fair cry escape book land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
