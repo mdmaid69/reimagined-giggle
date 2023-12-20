6299b4476fcd8a80a899b8d6654c0d807d4f95dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:18:54 +0000

    everyone address confidence mind except

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def remove_duplicates(lst):
-        return list(set(lst))
+import datetime
+print(datetime.datetime.now())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
