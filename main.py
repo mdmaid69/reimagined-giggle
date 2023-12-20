63307eb1d3310f0367afa91abb983fd5fb16d89c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:04:10 +0000

    allow us pipe leg down

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+name = "Python"
+print("Hello,", name)
