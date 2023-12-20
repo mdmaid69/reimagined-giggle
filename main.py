3b79964ac1510c964319cd7aa1a90468fe5fade7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:27:26 +0000

    Tt show hit wide plenty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
