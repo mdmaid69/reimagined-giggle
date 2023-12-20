3a4f2437c0abe935bc5b945ad7ea50375dcf98ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:41:43 +0000

    concentration joke will correct within

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
