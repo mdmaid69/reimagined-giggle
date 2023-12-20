31ec27c0bbf0b7031ad29ecf9f4f47895b7facd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:40 +0000

    occur police stamp lookaround dinner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_str(array):
+        return str(array)
