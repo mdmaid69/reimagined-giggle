b193591364c0969696a24cfc5ca6ae5f82b3451e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:47:52 +0000

    how until very will nurse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
