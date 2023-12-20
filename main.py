949059c6068a9982a60014d66720574e4851b94d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:48 +0000

    rid Ii score part influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-print(math.pi)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
