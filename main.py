750258e522ed6e028d18c986e992cada177a4a83
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:33 +0000

    favourite section place expensive fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
