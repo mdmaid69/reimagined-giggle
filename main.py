40c8f7555e1b59ead71390e2f23bc3a3af9d3084
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:45 +0000

    angry impossible exchange exam collect

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
