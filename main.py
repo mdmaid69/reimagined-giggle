6e459c4c001430c3176b6810b2f43ac20294910a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:51 +0000

    taste account always year unpleasant

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
