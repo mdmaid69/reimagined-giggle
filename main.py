f37055505fe98f949b24b9ffa35f84d9f812ce48
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:45:44 +0000

    ring such goon letter conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
