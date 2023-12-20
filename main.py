baf889cc072647bf96ead421c92d4ce7c0d875d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:51:46 +0000

    independent coat funny soft force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print("Hello, world!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
