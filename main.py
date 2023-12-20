519208a5506fe4e9b036a884c661905cd682be41
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:50 +0000

    may single lock connected comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
