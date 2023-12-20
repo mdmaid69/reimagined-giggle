e9ab71e07e1e1cec746a2532c746abcc6800b69c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:53 +0000

    remain teach complaint this regional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
