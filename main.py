f5b852f5662a7da15af28c47edeb90409be944db
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:59:50 +0000

    before port pocket ignore proper

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
