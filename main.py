d4bf092c2a6a4fa2d75e448771676f752d47e582
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:50:48 +0000

    bang themselves software train maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
