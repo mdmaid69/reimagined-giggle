61a243ef6ebcff15190ac07c1813e8e7f84b166a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:38 +0000

    square will wise acceptable juice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_power(work, time):
+        return work / time
