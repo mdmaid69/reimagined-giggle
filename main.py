75d628a9ccf3bcf732dae50e4b4df5896ec63e40
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:59 +0000

    or jump Dd shop design

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
