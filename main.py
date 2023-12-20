d8cba0ffda32f45fd07b2a83c4938a1492037454
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:30 +0000

    among good tread impatient active

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def is_even(n):
-        return n % 2 == 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
