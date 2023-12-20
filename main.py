a575c2e88a524ba6dd636bad2cfcc228923f285a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:27 +0000

    sad begin Cc board method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import time
+print(time.time())
