31fd99c71a0225576968fe76a155b893d6186bc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:00 +0000

    bus feeling fine suggest flood

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
