75c2d4d5bb08436da7e5777b77a85dc0ecb1f3ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:44 +0000

    lookback(on) sorry pretty argument almost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
