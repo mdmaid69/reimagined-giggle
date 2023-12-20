e9ff252a46d11de2dd17741178553ec2aca7fddc
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:58 +0000

    thinkover snow consult godown workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
