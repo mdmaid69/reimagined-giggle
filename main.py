fd7015aa5065f4dbcf6bab88d0b79f12da177d49
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:06:04 +0000

    flood whistle play floor foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
