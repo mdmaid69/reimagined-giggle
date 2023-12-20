48635098406cc201d22121e0eff7630c2b44d7a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:23 +0000

    callout proud Ee few cruelty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
