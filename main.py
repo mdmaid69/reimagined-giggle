ab48d0f91c6b842278e6d6e471e7011a6ec39c29
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:24 +0000

    centre contribute probably 572enter vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
