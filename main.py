8b2a8a2e8b486dba5f9de990e90d09fdb13d47db
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:08:28 +0000

    everything only unlucky match match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def greet(name):
+        print(f"Hello, {name}!")
