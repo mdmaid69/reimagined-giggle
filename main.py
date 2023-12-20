49f52fb4b5a53cb4c86668187711adf4a5b71fe2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:09:30 +0000

    nature path upwards already cover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
