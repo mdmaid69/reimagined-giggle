54b78b4972d8fafdb76780c281cf3c6c7b1fa639
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:25 +0000

    cruelty model guard congratulations again

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
