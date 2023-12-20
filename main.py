2a8b2247ed22e62053b28115b604fec69e1228e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:25 +0000

    run promise goback(to) length human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
