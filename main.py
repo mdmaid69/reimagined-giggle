1583a68622791d32a6f491071858a36c10b02d04
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:59 +0000

    total leave plate tight smell

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def cube_number(x):
-        return x**3
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
