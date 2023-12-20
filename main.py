561e2e78facbdd5f83b45cef77b0619e6370ae0e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:58 +0000

    yours secret angry offend main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_max(numbers):
+        return max(numbers)
