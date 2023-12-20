99511a5c5c4feafd286a96948527a8fbe5d23b0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:16 +0000

    comefrom laugh amount card respect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
