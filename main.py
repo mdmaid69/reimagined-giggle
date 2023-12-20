0338ec872ac6705c019252af34a51d011cd6a42c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:41 +0000

    goout whom niggaacademy about comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
