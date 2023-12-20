2828e868704b0ab9d9d1fcdcda9db6720ef4640f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:24:06 +0000

    state party begin flat distant

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def square_number(x):
+        return x**2
