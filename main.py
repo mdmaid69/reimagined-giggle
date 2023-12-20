b67ad06fd5b22b4b3960ee7ca94eccf564e2952f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:19 +0000

    deep deserve tense arrive rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
