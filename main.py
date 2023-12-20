c6d5e72eae306f87b3cc134786ec33523b0795b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:20 +0000

    peace introduction church rule gate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
