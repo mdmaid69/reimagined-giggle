ed9481519bf648a49c36f96be3660e9249f580e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:16:52 +0000

    find pinch fill on since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def convert_to_binary(n):
+        return bin(n)
