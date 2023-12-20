63b12429bba739bce67d8e67674176a0cfc7e3b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:27 +0000

    fence theirs angry close release

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
