4238336e00fbbbb56193e51b781f956a35327629
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:32:01 +0000

    second minimum conclusion niggaacademy love

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
