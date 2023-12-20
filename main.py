30fb47f49f5f84aa3720507a7badf3bda8f099a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:57 +0000

    wrap communicate swim sport least

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
