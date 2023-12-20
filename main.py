3e85ecbc9aa50b390162dab039829eaa5f196eeb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:26:02 +0000

    connected mind swallow mean system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_speed(distance, time):
+        return distance / time
