a982e0f3f5006b58e90bd78e85e8c0b992a991d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:41:13 +0000

    damp the naturally but patience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+def get_current_time():
+        return time.time()
