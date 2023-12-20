99bd7eaf8a652bfc474446b334c8bd3c1d44f519
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:22 +0000

    regular pass physical wing young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
