a3a2c516282cd8e298209e286e526042f8773317
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:11:39 +0000

    capital jump car shop tight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
