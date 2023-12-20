c1803d2e69500da386456db2cf12c8ada65e500a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:18 +0000

    fix keepup suffer train slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
