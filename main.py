29ca18a91903599d50a8f65cd648d300c5b8390d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:18:53 +0000

    disagree everybody realize honest pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def get_array_buffer_info(array):
+        return array.buffer_info()
