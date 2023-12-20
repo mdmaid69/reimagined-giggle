b52b3c55eaabf18cb3b3d84f452201b36b8944e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:36 +0000

    empty stay collect takeout match

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
