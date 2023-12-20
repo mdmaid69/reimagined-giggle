004f53d1ffae98ff5d5be272895d4b823e80d2ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:13 +0000

    instrument several figure slightly accident

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+print(os.getcwd())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
