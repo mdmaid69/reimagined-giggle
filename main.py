02d4f1c661591140857dd996f2bca2db2e89f8fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:40:23 +0000

    means ahead city face ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
