6368351cdceaeef32dbdb3d53ae7e6acd8454539
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:10 +0000

    century pile join eager bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print(sum(range(10)))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
