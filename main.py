f51aa3dd9dca07d2e4691cf0e75f14129d356006
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:56 +0000

    back likely contribute cost inside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def is_odd(n):
+        return n % 2 != 0
+def calculate_area(radius):
+        return 3.14 * radius * radius
