48507441f77f52eb4762a9904a447956c9aef26b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:18:26 +0000

    ill last all respect apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def convert_to_binary(n):
+        return bin(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
