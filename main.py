36a58cde1b60dc1ea755a8f77a9c7ad1c5bb6c55
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:25:28 +0000

    note shine whichever idea thinkover

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def sort_numbers(numbers):
+        return sorted(numbers)
