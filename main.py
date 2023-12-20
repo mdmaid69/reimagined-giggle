5bedbb8ec1f171120a91ea20fdd0b028e7937a8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:11:54 +0000

    advice neither please group model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def cube_number(x):
+        return x**3
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
