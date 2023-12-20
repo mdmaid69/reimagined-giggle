76634af38acbd20b1708b15e0a7105aa4a21b1aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:12 +0000

    unpleasant bite warn poison characteristic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
