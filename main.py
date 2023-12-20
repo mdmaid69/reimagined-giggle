7f14646e356e9c81ec9051b40b449908bbe4523e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:56 +0000

    surprising notice through collect finger

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_item(array, i):
+        return array[i]
