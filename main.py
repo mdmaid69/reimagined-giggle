e3effaf611150326ffe9d54505800227b0f977a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:05:31 +0000

    damage attention breakdown mouth 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
