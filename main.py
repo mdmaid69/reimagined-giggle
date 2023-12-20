18a4f9e1962d7b0f61410a1870827520319ec08f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:10:07 +0000

    find favourite return tidy company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
