ba1031d2f3461c1b26bd6f69bdb7bc953f79b297
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:55 +0000

    comment news now bother wheel

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
 import math
 def calculate_logarithm_base_2(x):
         return math.log2(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
