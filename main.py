d18d90b89e47bb42e2e7d6c234f265189b2827a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:03 +0000

    able keepfrom Kk interruption quarter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
