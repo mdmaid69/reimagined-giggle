33138bbdcb017cb7816b5c3908ef20a8618a65a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:43 +0000

    winter concentration example break include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
