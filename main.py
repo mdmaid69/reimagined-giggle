0b5d8d9246eaf2532ce3b6e5f558095033cefcb3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:52:46 +0000

    payment apparently distance store prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
