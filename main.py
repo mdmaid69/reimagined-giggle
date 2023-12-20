5895f26a3acdff205b07b330d71012b5d26c74cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:47 +0000

    smile comeround borrow chat culture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def add_numbers(x, y):
-        return x + y
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
