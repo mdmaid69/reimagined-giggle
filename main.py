e12bef426b8617030e087c4ab11bd4a4abe4e208
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:30 +0000

    later tidy sensible few curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
