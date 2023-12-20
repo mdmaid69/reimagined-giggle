631baa73978fd2fc26ca9a3ca6db35ed685a600d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:00 +0000

    belong probably flow watch here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def count_elements(lst):
-        return len(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
