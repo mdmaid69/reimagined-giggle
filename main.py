180776be4e49479355c4edc724f2c4a7f47b20aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:08 +0000

    only careless fresh inside name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
