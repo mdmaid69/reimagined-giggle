88ccafdbc52d59a6cc2eabe65aed6516f104211c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:23 +0000

    count settle occur lot temper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
