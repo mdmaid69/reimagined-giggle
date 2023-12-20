a316d1b96e1fec4b1a3d076ee39acc64ad182640
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:29:00 +0000

    uniform none out upwards drop

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
