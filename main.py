5ec3250a482827879f8ad505a825748f92ace00c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:55 +0000

    wipe contribute passenger guess especially

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
