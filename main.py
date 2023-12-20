2952a5c82e3de7c3a27ce5ca9ac6674415b01a66
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:02 +0000

    match marry uniform across front

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
