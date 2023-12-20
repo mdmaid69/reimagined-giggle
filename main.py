af25ee917d65317a8e67409fe843d15405a138a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:57 +0000

    thinkof talk drop any cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
