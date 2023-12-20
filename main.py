2d8b4494737fda84d88e425da6b6653a18209996
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:33 +0000

    interested wish shut close weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
