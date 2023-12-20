d0e7addcf5e016a92b1e992ff872fa9d29b4c55f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:54 +0000

    curious stop wipe only image

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
