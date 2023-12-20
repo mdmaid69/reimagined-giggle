16e0f68fc2c3944e117ea01bbd11767a5cc88910
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:06:24 +0000

    raw deliberate character failure shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def reverse_array(array):
+        array.reverse()
