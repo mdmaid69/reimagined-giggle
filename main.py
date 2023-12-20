a83a1bcfc444a45eb751c8f03c4b2deed82830e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:00:20 +0000

    post leg escape evidence beautiful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
