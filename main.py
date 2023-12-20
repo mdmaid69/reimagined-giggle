2b109d495fea416f590c4862efdf3fae811f108e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:26:26 +0000

    coat scared thinkover hurt confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_index(array, item):
+        return array.index(item)
