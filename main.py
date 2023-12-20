432c7cde969ccb6dca5d1903c031fd545fc0ba30
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:55 +0000

    exam rate weather forward crisis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_work(force, distance):
+        return force * distance
