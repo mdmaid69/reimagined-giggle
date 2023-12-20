5b97e29ecc6803fe37517456f1666e877ef29edb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:22 +0000

    slice gas they my unit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_power(work, time):
+        return work / time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
