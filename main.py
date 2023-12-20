386a3f68bbfcbf95e8bfbb63e6297202d3bfde30
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:40 +0000

    pleasant right squash hour most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
