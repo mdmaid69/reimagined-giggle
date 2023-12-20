a41a2ee449fd6c61d8e8dde7120542ee89727a35
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:58 +0000

    card underneath generous constantly scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
