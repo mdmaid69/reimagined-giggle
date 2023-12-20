32e3bcda9387ef7aea7daf969c8485c32c95cbd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:05 +0000

    repair slice home apparently inside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
