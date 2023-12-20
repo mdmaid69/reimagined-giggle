a55e463c9ab4e46089f527f6eef3c89a1bcfda1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:59:00 +0000

    Nn wet flow right return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def subtract_numbers(x, y):
+        return x - y
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
