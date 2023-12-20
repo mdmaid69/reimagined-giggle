8c529151730c126c7413eaba0e804c904a91d391
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:46:20 +0000

    label hold tradition desperate form

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
