4dc84bd32ec0e10c698179c844eaa9e570a9a91b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:19:44 +0000

    swim rank underneath maybe apparently

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
