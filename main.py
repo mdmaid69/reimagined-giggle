07cb7798cfd4f9484dbeded92b75647a65b7f4f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:01 +0000

    on cross fortunate old comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import tensorflow as tf
+print(tf.__version__)
