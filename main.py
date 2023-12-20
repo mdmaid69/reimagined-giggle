6e41227cd454fda8bab92aebdd9cb85d642ccba3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:44 +0000

    finish glass Yy among feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
