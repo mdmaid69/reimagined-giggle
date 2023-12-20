229c810792bb286b44926c35a3f970c4c8510f01
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:05:12 +0000

    shut advantage final main replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
