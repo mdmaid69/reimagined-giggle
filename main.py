d7b7d075f307bb6520893ae607907e8a1eeb0742
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:23 +0000

    pass angry hurry pattern kind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
