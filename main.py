3393f8d2f6e5ecc2e929f7bb38e2707fea1f6e20
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:42:47 +0000

    variety extraordinary early please sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_item(array, i):
-        return array[i]
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
