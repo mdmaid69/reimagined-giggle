daf8293710d075a4368804d1d8dea8c6373d6d49
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:35 +0000

    door marry another prepare spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
