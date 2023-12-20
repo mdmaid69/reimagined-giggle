99ff9eacc24f0a695db93e92d8c5704795b35ee3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:00:17 +0000

    brain pleasant unless science turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
