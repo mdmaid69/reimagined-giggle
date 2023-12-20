aa9a203648f44afeaba52638ef9a1a2117641784
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:40:29 +0000

    plan surface fear still circumstance

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_rectangle(l, w):
+        return l * w
