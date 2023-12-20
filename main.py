efc9029202c75409569ee9906e74384a5a99e4f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:30 +0000

    among whisper delight either cry

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_density(mass, volume):
+        return mass / volume
+  def subtract_numbers(x, y):
+        return x - y
