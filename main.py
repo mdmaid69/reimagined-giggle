30ca759832ec975d7cab8274b6ea602b19ed00e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:54 +0000

    recovery crash inform grow term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  def subtract_numbers(x, y):
+        return x - y
