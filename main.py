c82be8086cdb83ef1fdb0b58c9bc06ff75cfd1b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:16:48 +0000

    happen interfere shoulder properly scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
