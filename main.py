7e4ca398be455c10de192182aedbc0ad1ce63d46
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:23 +0000

    breakin cheap past second comedown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
