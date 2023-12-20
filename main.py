0e9c0b6d30cf880309a2b4d9fd8aecf7e8775ab5
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:50 +0000

    exercise height workupto disapprove leg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_to_octal(n):
+        return oct(n)
+  def is_even(n):
+        return n % 2 == 0
