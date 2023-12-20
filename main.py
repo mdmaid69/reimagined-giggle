5bb41f986474ad507628d2d12807a905e473ab9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:22 +0000

    interruption store pot brother survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
