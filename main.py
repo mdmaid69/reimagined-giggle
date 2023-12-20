900c7525184d014a6133197b95ebc3825e1c1019
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:57 +0000

    far practise estimate image blue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area(radius):
+        return 3.14 * radius * radius
