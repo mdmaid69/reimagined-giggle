af1c31cdd7e4fae2c598b286ae1b36034020d86d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:07:47 +0000

    commit formal official slide unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
