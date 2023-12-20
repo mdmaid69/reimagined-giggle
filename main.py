aa90005759b9318b8b4ccad74d5d0d1432ca481e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:05 +0000

    firm smoke eat twist early

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
 def calculate_permutations(n, k):
         return math.perm(n, k)
