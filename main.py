c9bb1db070ba49dd24a41890a8fc5a6843368625
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:50 +0000

    steady innocent normal switch off

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
