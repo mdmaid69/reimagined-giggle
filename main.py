ccad2af198887ed68bfe060219f92ccbf2f3dfb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:04:00 +0000

    left translate brown tax dish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
