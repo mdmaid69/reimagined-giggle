9ec9ab1ab0d191670078b30724727850529dfd1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:49:20 +0000

    language Jj passage bad mark

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_pressure(force, area):
-        return force / area
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
