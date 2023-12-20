4c0f9c08d114e3f152f41655e96109273b88c9f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:53 +0000

    skill plate desperate embarrassed than

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def convert_to_binary(n):
+        return bin(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
