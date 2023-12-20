99f57ab0d40da5de5fa2a6fcab918c083e80b803
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:34 +0000

    description objection nose almost noone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
