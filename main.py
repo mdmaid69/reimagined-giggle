67a5b1fcca566e2746971348d76a7b1469d58d0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:01:48 +0000

    money regard little plan release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
