9451995d7356edc5102d62e84ba901717477f481
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:14 +0000

    squeeze dig course frequent turnoff

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
