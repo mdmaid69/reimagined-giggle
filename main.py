9d49095bcd013204fbce6277d0079eae616c5a24
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:18:08 +0000

    when Ww song excuse who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
