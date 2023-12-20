a71ef8917dfe2f42fba2770bc807974ef1e45678
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:19 +0000

    suffer region dance afternoon original

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
