754052bd461520c05b6d6092d1658bbc7a79818c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:40:03 +0000

    pull situation add beautiful statement

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
