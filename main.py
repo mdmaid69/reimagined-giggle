8c849859e726e8328dbf68d78332f11ccab63e15
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:09:04 +0000

    bit themselves tape his tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def count_words(sentence):
+        return len(sentence.split())
