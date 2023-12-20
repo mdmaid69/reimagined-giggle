ce61feae095f3140cc8909ed9e22e541b1dccf30
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:47 +0000

    supply belong experiment vote closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
