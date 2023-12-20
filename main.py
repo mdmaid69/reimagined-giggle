a5656acbd5658fa8436e8cf70ccf8551d1b17e46
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:58 +0000

    afford must no later danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_float(array):
+        return float(array[0])
