8cc2e81fcaa8eed628ad974865cf2120206719f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:35 +0000

    about cinema Ss admire ring

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
