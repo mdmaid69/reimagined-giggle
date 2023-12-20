efb349da5f4441719f89f3ac2a3a8ae8b0ae8cf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:19 +0000

    myself reach dream buy passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
