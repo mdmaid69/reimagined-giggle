00ca5a141bb143e8363e0e098fffe1b07b43ce5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:24:22 +0000

    past obvious fashion tap condition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_array_to_string(array):
+        return array.tostring()
