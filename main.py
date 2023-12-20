d3fe74118b4cece5e926bfa08ade31f5f7ee9f1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:03:04 +0000

    gap sting anyone consider black

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
