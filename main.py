996ca113cbde6dfd30e167530f0ac2c35d446bd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:12:03 +0000

    570enjoyable change custom luck stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
