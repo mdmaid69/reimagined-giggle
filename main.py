ed00cc3d59a04994835d60ea8ab5c49a216b4be4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:18 +0000

    comein range ashamed meaning uncomfortable

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
