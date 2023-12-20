9e82680bc055a111c99b202141bb7a5c0812a09f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:14 +0000

    vote laugh explanation 572enter flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
