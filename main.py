d3e3b19fe911c611d8a3ef8f3d4f64c277fd02c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:42 +0000

    there wrong scare safe quick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
