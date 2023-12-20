6d619c147e90d3e8af4afe4f004bff4ce370260d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:04 +0000

    beat defeat excuse corner avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_sine(x):
+        return math.sin(x)
