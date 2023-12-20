999bf98f0ff5adceba581c12ffe247debd6be472
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:11 +0000

    case exit record piece Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def count_elements(lst):
+        return len(lst)
