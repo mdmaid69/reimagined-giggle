cda3e02b207fdeaab076fc0659632a038217e33d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:05:32 +0000

    suffer clothes bus interesting it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
