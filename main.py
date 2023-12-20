fbd4e14657d22e6ae23336d9de9b7e22af78a16c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:05 +0000

    protection pocket thinkover clothes turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
