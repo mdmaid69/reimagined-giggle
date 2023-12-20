de35b5c29c2ec07abc40493f1560591f094f0392
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:31:42 +0000

    view half natural die confuse

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
