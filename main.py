fdc5db66361077ad4e392461bf9cd1ab26eda329
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:22 +0000

    law recovery smell funny foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
