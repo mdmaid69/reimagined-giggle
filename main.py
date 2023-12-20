b09590a812ae036faec09628d6340535ce34eebc
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:06:22 +0000

    swimming pronunciation store cry task

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
