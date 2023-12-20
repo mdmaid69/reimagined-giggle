578f1d8dae77de224cb58b90840cac75569da1bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:58 +0000

    dress prize programme 566end right

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
