a601fb449d49d09867ee8a566dcb759c0cec7b2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:44:49 +0000

    send rather attractive best corner

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-text = "Hello, world!"
-print("Characters:", len(text))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
