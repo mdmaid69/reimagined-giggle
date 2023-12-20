dae5f9c12d6a3264ef42c2e6d5cfbe2013971ffa
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:06 +0000

    city arrive bad command used

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
