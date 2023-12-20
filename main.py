03b9538be722d7ac104b1502926e4f4500e65387
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:54:57 +0000

    realize wide cut again ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
