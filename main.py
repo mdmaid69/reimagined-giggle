85fa7a2f3a4f3164544886413edf1532d4cf79a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:23 +0000

    aware standard partner exit tooth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
