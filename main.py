f6027a967d43d68bd25df9d96d40204aa01ff4ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:55 +0000

    Qq attempt ilovedicks mend cheek

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
