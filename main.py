48e4a35058b04b1d23fdda98529cc6f6e6f535c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:40 +0000

    book opposite still negative smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def convert_to_hex(n):
+        return hex(n)
