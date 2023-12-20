6664913943172871a1bcb1ebbe3a48996991a5aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:01:51 +0000

    respect stretch mainly confused persuade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
