6905ba39e8ead6a6ac48e460434cf070c22b9b36
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:48 +0000

    valuable organization brush usually lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_item(array, i):
+        return array[i]
