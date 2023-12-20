6d9feb6dd1239a7ab53fa556b2c85c1b3245eb33
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:27:11 +0000

    past later break present table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
