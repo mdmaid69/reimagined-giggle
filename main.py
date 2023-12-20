bc427feea32e02d0e20e118d019cda39ceb0a499
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:28 +0000

    excited neither instead live too

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
