cdfe2f5e92101232e312bc0dc5732cadadafd780
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:18:11 +0000

    threaten evening range exam mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import array
+def get_array_as_int(array):
+        return int(array[0])
