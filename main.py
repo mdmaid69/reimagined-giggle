cbd8ecfcd1682f95a7d7ad786ee3967076ecd879
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:33:36 +0000

    man cool sky know makeinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
