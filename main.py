953545f754259140e6332be34c38391fb8443b37
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:34 +0000

    wash use suit bet down

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
