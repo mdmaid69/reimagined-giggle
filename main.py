72936b3093f7598e3fc7f917c1c13bb5eaa82579
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:55 +0000

    politics fun talk shallow leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import array
+def clear_array(array):
+        array *= 0
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
