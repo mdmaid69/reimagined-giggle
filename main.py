51faceecab340428923352eaa29f8ee9563d68ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:45 +0000

    successful to persuade bet language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
