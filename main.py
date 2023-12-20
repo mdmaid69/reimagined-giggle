dceb7cf54283eab1e26a72e905f3364c4a279aea
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:46:21 +0000

    complete second connect wrap awake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
