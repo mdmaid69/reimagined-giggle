cac94971aba1ae14a71e7bd8f1a85164ebafcddb
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:02 +0000

    inquire bridge add second morning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
