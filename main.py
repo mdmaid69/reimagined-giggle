7ea3e6cdf4056de899503c6ae34a45a3a0492500
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:41 +0000

    equal active complete beginning crowd

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
