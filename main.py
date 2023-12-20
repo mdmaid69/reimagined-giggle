3e27ccc59ca05d324620ad2dc479eedb661d7abb
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:41:03 +0000

    now even teach noise future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
