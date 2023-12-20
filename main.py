d844f8db81433c1f75a5c88fbe2db63be846f406
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:13 +0000

    wander meet left 573entertainment urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
