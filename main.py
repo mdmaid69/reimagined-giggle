a33b65cdd8896ff8055420908774b51a446fde51
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:50 +0000

    sorry ground run fasten generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def sort_list(lst):
+        return sorted(lst)
 import collections
-def create_user_string():
-        return collections.UserString()
+def create_user_dict():
+        return collections.UserDict()
