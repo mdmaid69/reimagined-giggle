6e0aaf80e7900c4e66a434a63cafc8f71cda450b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:24 +0000

    best doubt tight complete beneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
