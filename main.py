dd04727b32fa0ef3b680179a17540b123a7255aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:14:16 +0000

    always area squeeze otherwise everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import datetime
-print(datetime.datetime.now())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
