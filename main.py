c9df95471e0f55ba8b4c32c387f8faae08e65da6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:51 +0000

    565end destroy couple odd brush

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-for i in range(10): print(i)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
