88e5b4558b4ba078d805b5d5a2249b1bf94eb29e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:05 +0000

    fit point consist file along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
