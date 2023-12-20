e271941ec7bc96dff2c3f59d0664de116ac8f458
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:45:50 +0000

    extra finger identify excellent subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
