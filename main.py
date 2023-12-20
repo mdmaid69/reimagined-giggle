7957b2b5b63204904b86eb746c9384454b4edf9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:59 +0000

    muscle any shelter answer drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
