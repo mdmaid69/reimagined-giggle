8685d74c6ebc15cd68651a713b2770f231235128
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:39 +0000

    cruelty person flood takeout list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
