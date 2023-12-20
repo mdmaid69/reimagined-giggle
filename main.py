3fff4c8e8b7ebb79cd84ab0dfec9dad7507cec44
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:48:15 +0000

    past clean deserve attention sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
