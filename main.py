af079aa0e355c39e1be34ae6eda09288b450292d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:55 +0000

    handle memory describe exist workon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
