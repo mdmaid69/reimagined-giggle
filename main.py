98acc8818ed5c2ba08a32e9a47e72c8dadc60cc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:18 +0000

    soon wild fasten complicated matter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
