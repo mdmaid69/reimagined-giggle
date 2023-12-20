d77282f636cc555de91bb45fabc763cfeef70a88
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:12 +0000

    game rob accident young long

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
