32d9962535895dc9c384fa645efa9e22092820eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:03:31 +0000

    carry along alive identify threaten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
