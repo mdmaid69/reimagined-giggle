25aa39e623807b6652dd49cb88cbbe5e6786943c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:31 +0000

    benefit sink system cover die

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
