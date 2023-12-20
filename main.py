07d87913e453b3d25e40f25b081a6c12a4234a1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:19:05 +0000

    sister contact custom seriously responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
