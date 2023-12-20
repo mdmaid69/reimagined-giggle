26377de04a6c784bf3d0c06cf81eaff69c85359f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:28 +0000

    expensive attractive variety closely face

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
