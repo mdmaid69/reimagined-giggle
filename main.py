f3a8b2208980c1b19bc31dda73e23eee6a4212f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:27 +0000

    vote fresh gather discussion embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
