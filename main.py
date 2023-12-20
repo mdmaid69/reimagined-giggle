516d818a4524fd353e91e4957df7468c55f77bc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:28:13 +0000

    stick forever getoff runaway soil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
