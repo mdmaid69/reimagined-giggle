bda8ccc63f3648e17b4d19982a267ac1d80408d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:56 +0000

    authority related quality scrap 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
