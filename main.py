cc6276e2bb2893ec90b3c337fb5180e4b683978d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:56:33 +0000

    important noisy zero excuse dance

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
