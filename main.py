971e023b36591f4b66f9a02407997a2c2ffac214
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:47 +0000

    pull abroad word without tough

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import random
+def roll_die():
+        return random.randint(1, 6)
