e36b2d76423018df64914fa7e0e7ba2db0af8172
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:12:21 +0000

    Kk risk surprising tidy battery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
