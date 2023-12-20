f729ff1452148d920a87a3bf25dd0bd1b6fac999
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:45 +0000

    excellent hot sign 565end transparent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
