006260588b071e91cd242608ce61a5d0b6e414ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:45 +0000

    immediate admit ocean pile name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
