5467521d91a2335fd24f42cfc6da72ae21df9f42
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:01 +0000

    mistake harm welcome centre wire

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
