6673de01460f6a826709a086fa89407cd430027e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:58:31 +0000

    present line scared confidence lead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_pressure(force, area):
-        return force / area
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
