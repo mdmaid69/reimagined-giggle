583494125c6273ddd5b0306bc52eff8913bfec38
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:33 +0000

    turndown poor geton task world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_set(array):
-        return set(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
