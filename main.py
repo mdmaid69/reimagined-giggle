d0f3d85d9f9f460cd1399299c69f714dd0fcfbbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:20 +0000

    official whistle ability have breakdown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+for i in range(5):
+        print(i)
