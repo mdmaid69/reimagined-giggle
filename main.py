ceaace8663c7ae3fb640de534f77040ae1bdb546
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:02 +0000

    success badly course tough become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
