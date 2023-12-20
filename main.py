c4e05042536a6aa917ad23ce78c89b2baee45456
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:49:45 +0000

    finish else deep blue deliberately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import datetime
-print(datetime.datetime.now())
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
