ee6b13c18862ae587173db248af59237fac15d39
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:43 +0000

    judge genuine education please weigh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_queue():
+        return collections.deque()
