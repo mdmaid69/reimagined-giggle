d63db29c249277a6e56b4a772633954649b43965
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:12:27 +0000

    public pollution drop curious excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
