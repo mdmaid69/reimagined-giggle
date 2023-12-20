395aca24915ab1f98497c8e6967bbf6f973d0a38
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:28 +0000

    form nervous across row building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
