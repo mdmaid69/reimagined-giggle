a53318204aff6a3381fa963175df0ad9fe3c7275
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:32 +0000

    mark how increase careless feature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
