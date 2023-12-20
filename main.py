caa69b2e8a24a4d591983d4f6867346d08d94bc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:08 +0000

    run everywhere terrible matter fit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def add_numbers(x, y):
+        return x + y
