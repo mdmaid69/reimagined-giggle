6bbecbae7452d962940af69b5b14559d78bdb1bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:51 +0000

    strange outside taste prevent pure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
