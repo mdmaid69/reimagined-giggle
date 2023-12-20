02a755b165f6c8a77cd47ecd08cca26ed4d908c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:57 +0000

    tidy though hot wonder calm

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def count_characters(sentence):
+        return len(sentence)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
