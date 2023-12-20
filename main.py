17575365796bfbad240b44a305d03d41644e01d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:40 +0000

    come relief treat prevent snow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import re
-print(re.match("h.*o", "hello world"))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
