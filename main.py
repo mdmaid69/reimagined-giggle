c26a78537da8e6570228ea7731254aaa0917cca7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:37:26 +0000

    564encourage react space system extremely

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
