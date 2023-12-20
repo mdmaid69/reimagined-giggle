510f4635dac7cf854a4801eb77a769827156e185
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:59:33 +0000

    turninto nigga unit hole hand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def find_min(lst):
+        return min(lst)
