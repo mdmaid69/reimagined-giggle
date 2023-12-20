dd7abb89b39248616d0925525b4fd839623f8a2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:53 +0000

    soon let anywayanyhow anywayanyhow cold

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
