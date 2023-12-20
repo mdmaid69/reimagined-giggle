69b5ffae670e833029c7a6309a6b3eb16e2484fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:31:34 +0000

    past represent stop slide break

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
