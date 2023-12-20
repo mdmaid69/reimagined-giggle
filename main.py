91f2a34bebdbed2fd04cad3fc68863a72ff5e466
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:01:16 +0000

    visit position rise carry and

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
