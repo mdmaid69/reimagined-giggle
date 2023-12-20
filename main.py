9cb2285b0019f7ec1557bb39652bc9d6efe1dd3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:40:59 +0000

    fairly forgive poor stiff Hh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def is_palindrome(s):
+        return s == s[::-1]
