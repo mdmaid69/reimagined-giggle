357bab9e6dac74a975c2817446485c1687812efe
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:30 +0000

    concerning discovery whole raise act

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def count_words(sentence):
+        return len(sentence.split())
