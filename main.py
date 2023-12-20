2278197a50ce7cf793a1115882cc7871c2c2c16e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:08:18 +0000

    city building factory through trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
