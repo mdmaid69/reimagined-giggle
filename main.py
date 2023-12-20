8da06f03294a910eb1622196e138339000d9c587
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:40 +0000

    tape goout peaceful turn callback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
