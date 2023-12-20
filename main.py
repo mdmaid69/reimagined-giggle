2d7f4d57807822c7c299a41b3ea3360954db1b08
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:28 +0000

    man spot behaviour must callup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
