440f00216872688980e0c8aea8388a86eca2b9fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:20 +0000

    gooff maximum fortunately spot learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def is_even(n):
+        return n % 2 == 0
