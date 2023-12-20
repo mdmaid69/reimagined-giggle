a0f3749f3c2a1907a2a5a7cd02d6c6902a4f2e57
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:00:12 +0000

    defeat gate may sticky off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
