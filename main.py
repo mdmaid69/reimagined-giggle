5bc75de8d941452a6648b94bbfeca36ea4c31eb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:44 +0000

    method it reply turnback grade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
