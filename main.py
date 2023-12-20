5471e65bb64537472405a329434e0a5e0b5b7173
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:57 +0000

    Yy vanish left know great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def is_odd(n):
+        return n % 2 != 0
