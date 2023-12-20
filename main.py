fdb2ce05931e528b3a00590776bb43775fd74bc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:32:13 +0000

    section between getout home yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
