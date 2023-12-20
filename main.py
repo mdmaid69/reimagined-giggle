d0994de89306e7ce037de453e2dfc98a54dec5cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:12:16 +0000

    sure excuse avoid tape shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
