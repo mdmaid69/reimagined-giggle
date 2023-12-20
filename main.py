4f950875e53b0e45707dde32c986516a98254a1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:49 +0000

    wrong practical time key free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
