a4b97d6a8e8ee09d06ae3c55522f682e81ff698c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:58 +0000

    work deliver goback(to) born doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
