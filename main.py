895eeb559c6c06a4fe1b96736dff299a93ad82e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:52 +0000

    exist right announce nerve cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def cube_number(x):
+        return x**3
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
