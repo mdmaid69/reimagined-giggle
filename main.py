0f64d97f430168cf29873e83480fdfbe9e8f568f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:21 +0000

    hope callout everybody attempt touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
