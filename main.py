ddf21e3c52702f0460c411e32be68e077bb2b977
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:51 +0000

    one interested schedule calmly great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
