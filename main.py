eec7c512001ede7497fe419851426562f837f501
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:19:27 +0000

    star quickly cause convince steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_speed(distance, time):
+        return distance / time
