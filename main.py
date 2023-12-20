ca8bcfce11735fb9c19e721ec33c5f6e2624a1a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:25 +0000

    brown rescue enthusiastic point container

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
