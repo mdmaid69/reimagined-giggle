f8cf2dfe16e14f8117e5034cb9e76b47e9554841
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:50 +0000

    worth himself upset disapprove obtain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
