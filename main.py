7e25969221e3c7e850a2ae67ccde629207f15a67
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:17:29 +0000

    maximum strange consider gooff emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import numpy as np
+print(np.array([1, 2, 3]))
