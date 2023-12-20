4f02a835b1c5c1b72b08511149025f040ac745b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:17:39 +0000

    problem even study respect telephone

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
