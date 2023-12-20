618e45f596ab226b4389279cd9c09c618b3ad40c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:32 +0000

    taste position action change price

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
