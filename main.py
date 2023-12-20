43a5b164aaa543b75eb08d66375d858dc2b4580e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:48 +0000

    better ready satisfactory injury sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
