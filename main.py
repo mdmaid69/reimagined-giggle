3630ceeec51f8e1cba20e8c984fde0fa93734155
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:32 +0000

    while keepup mystery suggestion comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
