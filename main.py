5fed70413089b597acf9a4f9125d0ad67916eed3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:26 +0000

    water lot touch demand child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_list(array):
+        return list(array)
