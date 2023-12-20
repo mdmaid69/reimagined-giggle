cbff0a72df0f7127e7d42c890a748a637085a7ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:06 +0000

    steal brave morning shut certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_even(n):
+        return n % 2 == 0
