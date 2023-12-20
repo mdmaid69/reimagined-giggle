5a1def38067f60728cdd86cc5128d1d7b16ad08b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:55:39 +0000

    achievement getback single own squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_arc_tangent(x):
+        return math.atan(x)
