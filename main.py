a7036a774d4b0bba453d5df576d68ad0687553da
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:16 +0000

    scene silent goon everybody possibly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
