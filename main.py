31103a996dd0f3aa82f0134d69156bc5d36acc0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:31:53 +0000

    share lead only backwards sleep

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
