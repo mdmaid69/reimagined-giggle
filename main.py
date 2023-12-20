cfee8b40c2b7ab48b9722588d362aaf1d9d9978d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:33 +0000

    manage kick fetch them habit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
