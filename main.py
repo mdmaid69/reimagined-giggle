fdb5c9a3587144936f6e8de607bbbada21e87dbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:48 +0000

    thinkthrough show human declare responsible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
