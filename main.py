26acf9c457acf497b1f15f756c67548e679d7586
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:29 +0000

    bye test pencil air fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def divide_numbers(x, y):
-        return x / y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
