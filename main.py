8ea85b742b29d313cb337e503b9ae3350fea3947
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:02 +0000

    home pour influence difficulty lean

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
