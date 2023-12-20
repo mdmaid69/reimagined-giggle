2eb91708f47f7ca82b717a6ee5ba8b40538d60ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:22 +0000

    position note conversation from excuse

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
