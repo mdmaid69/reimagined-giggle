d0bc9bd13d4bb5b27b62ef201cf2f853ba29f675
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:06:11 +0000

    to disturbing power lose impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
