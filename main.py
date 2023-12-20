42d5091ad4051f24135bb65beab94512ab3c2040
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:06 +0000

    meanwhile advise now stone permission

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
