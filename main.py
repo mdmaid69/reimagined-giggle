6592d86881c17c1c9f9b665c14b61bc63f755109
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:44 +0000

    emergency character uncertain tour effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
