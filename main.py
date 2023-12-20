936e1f2a89c927ba785df684f301ba18366360cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:05:56 +0000

    its central stupid permission thinkover

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+from collections import Counter
+print(Counter("hello world"))
