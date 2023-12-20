79580fdd29b97cd7b5d35782d6301e706b87b42e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:43:17 +0000

    school until opposite trust final

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def convert_to_octal(n):
+        return oct(n)
