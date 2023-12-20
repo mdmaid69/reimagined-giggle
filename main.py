4aaf911ac335199c7f67d57b95b7612b0111c2f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:24:38 +0000

    combination relation fit spray load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
