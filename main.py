cc6a0e97ff4c301e134b405fb468988a7bb3cec6
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:19:20 +0000

    clearly mend trial splash weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import os
+print(os.getcwd())
