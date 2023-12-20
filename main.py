cf6fa36e94e6e7a2d39c94d3f450a92d0dd8eb3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:03:41 +0000

    region let region change more

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
