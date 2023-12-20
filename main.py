79f8ebb981a6f917b990750b23e7c9d2d0478acd
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:20:45 +0000

    cry least kind towards secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
