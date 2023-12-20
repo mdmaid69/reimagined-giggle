ba966b2d924e1686f47c18e2d8405a95e38a647a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:03 +0000

    before favourite effect pen thinkof

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
