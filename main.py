b5881ed7b264927b85dc7ea0196efe379aff311f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:29:24 +0000

    regular soak calm switch breakfast

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def convert_to_binary(n):
+        return bin(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
