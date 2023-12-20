16a87be68e026400f23dffd12473ecf0202155f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:14 +0000

    along keen experiment pay limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
