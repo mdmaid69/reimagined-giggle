9cf90e8df0d540ce8bd5857b9897737d70cbc14e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:42:05 +0000

    occur rain convincing beyond complete

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
