85d7196c0fa2f1d813da3ffe6978a63a3fb64e22
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:54:22 +0000

    between Yy suffer around decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
