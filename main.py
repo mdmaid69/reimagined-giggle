6b6cd418b7260df927e951627de3e07f8bbc66da
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:19 +0000

    load second something myself pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
