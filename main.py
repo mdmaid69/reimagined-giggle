9f463bf743c72f3203093b22b587f2576a517f30
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:19 +0000

    protection deliberate funny temper Rr

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
