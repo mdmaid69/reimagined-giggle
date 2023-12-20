78994e54fb7c88a6629fdf5d97106032d0d17b8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:32 +0000

    their forever case tone accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+print(os.getcwd())
