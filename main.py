e86aba48b29c78c20a25a557296369a2633056a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:31 +0000

    coat live speed comeout increase

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
