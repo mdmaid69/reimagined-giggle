f71104270c158e7b1b78b949ad58ead1a30cd90e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:55:02 +0000

    fierce comefrom down upstairs experiment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
