761090eeb07971950f67beb9e186e59081985bfb
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:58 +0000

    within make over fond keepaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
