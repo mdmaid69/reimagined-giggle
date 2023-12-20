bf43e99d5a9afd677fd6f0e0571bfe37df913304
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:06:18 +0000

    lesson fit error from dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
