ecae1cbca1fa50ee43b345bd21652250a032399f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:13:19 +0000

    furniture feel of Jj trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
