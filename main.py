6f77912f661b8ef477df99e213edf429d69a2dd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:28 +0000

    solve breakup fasten bored offer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
