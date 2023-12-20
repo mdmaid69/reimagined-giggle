c2085485e74a45f9ee4b5155ccdcfe978deccc30
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:57 +0000

    question tune factory Mm hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+x = 10
+y = 20
+print("Sum:", x + y)
