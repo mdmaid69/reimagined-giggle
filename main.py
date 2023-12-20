9d5697e55cabf750b10fc7ba7837b7bdfade9193
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:05 +0000

    object demonstrate shock concentrate objection

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
