ea04c89c49c0273c7940a23896905883112c4275
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:02 +0000

    artificial concern for in wonder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
