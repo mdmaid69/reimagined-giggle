638cca444d9b2d39f4228f0c0c41593207b80f3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:50 +0000

    but direct opposite base start

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_sine(x):
+        return math.sin(x)
