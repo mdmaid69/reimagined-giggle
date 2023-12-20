3d42c761694692cbb66664ccb7838a55b9b0fc44
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:40:15 +0000

    chief funny get Ii reasonable

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+print(sum(range(10)))
