2cb38222405c6edf8767a5a477b1b415ebfc197a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:49 +0000

    goaway necessary left find announce

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
