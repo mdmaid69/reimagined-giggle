f16cbbb1a34b07c3695666258f16b81e68330e60
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:22:49 +0000

    trousers patience instruction night cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
