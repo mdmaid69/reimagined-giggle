8bb70cade7f2e4b40c0b6094132dceac998eafff
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:59:19 +0000

    let deaf noone principle embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-print("Hello, world!")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
