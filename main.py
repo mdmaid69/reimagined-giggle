0aa030fc262a554ab6f2059bf2ad525475b3f1df
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:10 +0000

    failure central straight everything union

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import getpass
+def get_username():
+        return getpass.getuser()
