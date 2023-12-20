b29bb3e4042cd1e81112ce2895f97959c9bb1e08
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:34 +0000

    another copy about desperate for

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
