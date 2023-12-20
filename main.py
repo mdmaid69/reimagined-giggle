e435e5a6d9d2ca3886e8eb97c486417d899cdd41
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:08:24 +0000

    fly better separate base stand

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
