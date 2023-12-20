dc456bfe7f22fb2726bcc47998a1f14082969779
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:05:14 +0000

    land promise furniture trust prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
