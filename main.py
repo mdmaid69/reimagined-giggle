1d2bf7774a03438d6085240c33468adc86df6320
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:58 +0000

    offer amount level brush father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
