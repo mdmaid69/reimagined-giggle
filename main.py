d1fa2aafb0c7c9a834cf985dd5d1282b9024611c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:21 +0000

    deliver tight payment explanation talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def reverse_list(lst):
+        return lst[::-1]
