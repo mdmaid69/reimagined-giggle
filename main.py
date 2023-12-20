1de98fc8491b9d2448817f5e1064480d82d9138c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:23 +0000

    eye bring prize post gentle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def convert_to_hex(n):
-        return hex(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
