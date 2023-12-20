421839a95ddd42773a81e0d75de6ce3e3bb3becf
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:15 +0000

    invitation attractive runaway picture soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
