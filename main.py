bec87f26ab9b53035ffeb7ca2a5c235c9c71db75
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:56 +0000

    oil ashamed welcome blame only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
