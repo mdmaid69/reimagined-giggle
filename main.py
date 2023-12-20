5437624a3aa0dedd7c365e64e7e0bd00bcb73521
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:26:31 +0000

    serious means improve speech mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
