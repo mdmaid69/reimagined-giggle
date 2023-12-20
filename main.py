6fdbb4e2de906c7fd052853153de9a71510a612f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:49:06 +0000

    police mood model goaway talk

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
