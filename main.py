e61a25d5a25652e8f5a25204e792960a8d322fda
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:51 +0000

    responsibility insist concentrate state sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import datetime
+print(datetime.datetime.now())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
