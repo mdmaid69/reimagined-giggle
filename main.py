a112608382c9974d5bef6eae824b84e31c887325
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:00 +0000

    embarrassing nice character breakin class

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
