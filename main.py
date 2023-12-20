25e7c7bfc75adf92c5743bde59d0b05d92519913
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:31 +0000

    under crowded cold anywhere exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
