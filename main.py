18060dfe6c573416619f8f2cc9cb4022ac01bee9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:33 +0000

    black come write deliver otherwise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def add_numbers(a, b):
+        return a + b
