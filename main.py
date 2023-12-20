0a6e4c39a223e477f4b8801d2ead4182d62edae0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:34 +0000

    Ss exact clear unkind middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def cube_number(x):
-        return x**3
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def get_array_as_set(array):
+        return set(array)
