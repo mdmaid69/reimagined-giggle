4d442e3533eef9c4bf91b4f61d39e61f87cbe6de
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:51:14 +0000

    aware hot scream left brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
