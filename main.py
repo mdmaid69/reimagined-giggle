461c9cd9fe66dd54e8085f725c19a9e637554664
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:17:40 +0000

    meet tradition 565end ask but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_floor(x):
+        return math.floor(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
