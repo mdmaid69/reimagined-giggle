9cf4d379a25ffde1a765f4104b731e5814a317b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:55 +0000

    gap my fit increase attempt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
