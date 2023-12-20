54637931ad45ddbaf5529da0cb4c25d176ef3233
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:08 +0000

    busy stand serious back connection

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+print(math.pi)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
