805e802855431c280877df6194abe88276c100b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:24:22 +0000

    protest reach announce test perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
