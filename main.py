6999d499aa045f8131d02597062004b1027412e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:27 +0000

    man chat takeaway soft spill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
