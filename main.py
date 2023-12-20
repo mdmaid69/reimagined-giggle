b817bf924fdea8def4e15365cbb97af69ddfab3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:01:28 +0000

    accidentally train rich sail free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
