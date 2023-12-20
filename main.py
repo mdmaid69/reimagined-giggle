7afb7d9745183f07f9ab79e805e4c4baf4272d6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:16:23 +0000

    drink spring waste exact laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
