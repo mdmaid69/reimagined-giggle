dec4944e697216fc590891a3a068613c5befd185
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:05 +0000

    slow breathe but tense kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
+import os
+print(os.getcwd())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
