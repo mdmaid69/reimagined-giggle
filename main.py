0b315036bb4b52b8d177fd956b29745c61e06fbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:14:46 +0000

    wake large sore 179bite careful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
