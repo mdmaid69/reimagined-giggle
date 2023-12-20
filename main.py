9e724ef6cfba48a344397f60f3fd412fcf1b3b2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:03:53 +0000

    mail alive complain front practice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
