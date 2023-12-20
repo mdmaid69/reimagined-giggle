1209b07aabf05312b088004f34d1287f612b64da
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:43:47 +0000

    switch turnoff ring opinion grade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
