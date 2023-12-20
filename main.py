e9cdf2e7d3e9bd8d9f8a74aace5fa499707a7f61
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:00 +0000

    reduction improve get delicate run

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_acceleration(speed, time):
+        return speed / time
