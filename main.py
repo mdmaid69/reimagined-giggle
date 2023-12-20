d21ae2b4233b04030aa4e226f797f497ca5eb759
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:48 +0000

    show building matter common waste

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import math
-print(math.pi)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
