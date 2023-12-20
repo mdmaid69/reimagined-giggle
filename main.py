e415c5216d1dfc097c8185c4a58900d3774efaa5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:27 +0000

    both butter terrible genuine hurt

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
