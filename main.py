ed7c4c44cc544c70815f48dc6fcbf0d62d5d3d1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:15 +0000

    point Jj autumn ring hard

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def square_number(x):
+        return x**2
