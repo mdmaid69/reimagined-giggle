d8077034116505e0d5bfb23d65507fb7eb519a43
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:43 +0000

    pretty bringup care will try

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  def is_even(n):
+        return n % 2 == 0
