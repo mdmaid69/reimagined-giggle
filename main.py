f5cfcd798bf9aa59a92871a4e6822fd31b44214b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:44:57 +0000

    address ask nurse close there

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
