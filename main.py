502311a83e224bd39c715bc6dc073d58571137dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:55 +0000

    fold day entry really tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
