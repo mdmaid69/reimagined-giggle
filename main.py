369af1fd556ff7e4e154376cfb5fc2d8088d49c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:51 +0000

    job power hurry ring precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import datetime
+print(datetime.datetime.now())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
