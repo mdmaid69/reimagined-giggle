54f04dfd0d96d25b70d0f2fa5abd9b06c5cf7053
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:37:39 +0000

    build show length mess there

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
