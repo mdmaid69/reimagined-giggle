c8af266eebb535cb2ed2d0c4c91bed8919789a39
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:33 +0000

    correction may practice load butter

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_acceleration(speed, time):
-        return speed / time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
