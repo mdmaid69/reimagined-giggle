bcd96fc5dafaedc717b8d10ebde443a5f62f82b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:40 +0000

    conclusion naturally outside every wander

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
