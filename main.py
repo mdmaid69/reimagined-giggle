d523acc1b228e0fc13f1af5be181b2e86fdcbd7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:57 +0000

    improvement scheme take together accept

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
