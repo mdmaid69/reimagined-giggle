da44165821cf250ed4c5de215f09d6b5bd937623
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:37 +0000

    such innocent pile mood shut

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
