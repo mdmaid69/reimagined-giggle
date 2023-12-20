b5fc2d251caba5415548ff7f553c725f1f749903
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:42 +0000

    cap cruelty look ability hurry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print("Hello, world!")
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
