b53983b0440b64aeb10fde2185a12cf2d5a195ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:21 +0000

    central onto important 569enjoy somehow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def greet(name):
+        print(f"Hello, {name}!")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
