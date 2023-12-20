f36687f0dcd4dd812a22588a17fd21c893ee25cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:59 +0000

    name speak concerned mouth tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
