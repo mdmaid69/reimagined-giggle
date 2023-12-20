e8dbe4f55ab8aab8c197e4d6192c5948a56fef80
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:01 +0000

    development best term instrument ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
