95f17573700b035382185a0958828931bdffa023
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:50 +0000

    uponon object bag pile attention

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
