ddd5416ac371b216529120a6e03320e5606d4f80
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:22:46 +0000

    that touch print exercise disappear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
