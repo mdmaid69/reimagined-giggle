2ead7e1f30a503b8f06fe8c324e2e0279244a151
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:29 +0000

    platform table defend apart be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
