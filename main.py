9221e14f2b93ad098f277f6ca6a734df4f4281ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:18:14 +0000

    rub somehow collection list perform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def cube_number(x):
-        return x**3
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
