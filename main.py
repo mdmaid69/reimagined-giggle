1efaf8e0e71cb7ce840c470425b7a3f4a671ccf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:42:42 +0000

    stretch tradition matter mix release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
