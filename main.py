717fe950b102a8172fdbcce8f081375400828821
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:09 +0000

    trade experience must opportunity similar

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_min(numbers):
-        return min(numbers)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
