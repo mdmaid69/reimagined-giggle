19bc65bbc1ace5a46e782f9803dc255ab8be4215
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:55:03 +0000

    break slightly community branch main

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
