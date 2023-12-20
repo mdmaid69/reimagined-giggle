14cd6ff3edea4fb8258b48a20a6cd332c6532966
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:22 +0000

    coat owe hers blind injury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-def sort_list(lst):
-        return sorted(lst)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
