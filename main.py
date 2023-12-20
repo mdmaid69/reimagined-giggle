cb3b9c538aca79cedbcc8aff6d5531dbf226b6b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:09:10 +0000

    tell afford occasional smooth bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def set_array_item(array, i, item):
+        array[i] = item
+def reverse_list(lst):
+        return lst[::-1]
