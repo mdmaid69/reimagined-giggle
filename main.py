cbda8c64d68352dbc05746e0e4c3e656fb9fc58d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:47 +0000

    regional method immediate content after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
