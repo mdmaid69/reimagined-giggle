85b8041b8ec2b63226392e41cd09708cc17b0be8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:20 +0000

    number brown bear represent break

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import time
+print(time.time())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
