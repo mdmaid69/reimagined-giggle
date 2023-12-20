2b41c26fe78839e31520c02a07a6759af259615b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:59:41 +0000

    glass autumn noone eat information

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
