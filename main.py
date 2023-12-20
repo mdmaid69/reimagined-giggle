de160a4a103ff3122fae990e1335efd4b1e5a1d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:21 +0000

    range steal 179bite original kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
