6187e093f00b0d9328939f1e4d7352931fa9522d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:32 +0000

    occasion shoulder pronunciation Itself convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
