4c1cac686f6b4854fed5473044d28d47f9bba70c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:37 +0000

    raw sweep answer trap tonight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
