d044d0b287d4c9b14e2cbb543b0b8692c3f771bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:49:39 +0000

    doctor practice crime trust board

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
