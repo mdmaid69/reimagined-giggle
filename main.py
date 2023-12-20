936a2aa697574b41d79d3670f41a79e26f54cce5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:07 +0000

    least key responsible weak example

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
