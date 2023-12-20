21b755ff117cc00b96c0c7a278ca19944d9d265a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:07:45 +0000

    home fill persuade quit schedule

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_pressure(force, area):
+        return force / area
