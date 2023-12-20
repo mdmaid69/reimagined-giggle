675e6de8b1b3d0eabfe7e1783ce3e4c526186416
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:37 +0000

    lay sudden dig scream properly

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+for i in range(5):
+        print(i)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
