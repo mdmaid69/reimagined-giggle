4f479fd09f8265dc06e6f20ef6d84e91752d46f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:15:35 +0000

    door damp convenient show theirs

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
