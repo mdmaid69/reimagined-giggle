b25da5c4b6ce4b58cd45d1e62ba448a552ba6c01
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:43 +0000

    card flood Bb discovery listenfor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
