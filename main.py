604fb3848496e7dd2bdcf2ae02b45470da13711a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:41:22 +0000

    speech better under takeover badly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
