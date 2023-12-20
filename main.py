1dec5c8f62a2ac7a10c2261651b411a1246e5874
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:22 +0000

    situation square neck plenty 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
