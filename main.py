e8f9473ea981042270a59f2a6c3a855431b59cea
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:21:27 +0000

    game sell keepaway several platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_speed(distance, time):
+        return distance / time
