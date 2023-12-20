a1bf766e1f0ad8f781f918d502ec96fd9a46d07b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:21 +0000

    price thinkthrough forever base skin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
