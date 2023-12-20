41fd53fac744944f43a91442a64887fed1ce46dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:19 +0000

    warning cheek description instruction vote

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
