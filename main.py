6147c7ad89888c8619ee45759942d55221c097f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:47 +0000

    practise half describe wherever Ii

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-name = "Python"
-print("Hello,", name)
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
