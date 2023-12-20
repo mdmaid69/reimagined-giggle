199d36d3eb289a60dcb30c7231b57df05f89dd70
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:17 +0000

    interview purpose feed completely borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 def remove_duplicates(lst):
         return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
