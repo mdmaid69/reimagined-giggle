8f7c07176a1bccccb91d0c9483de93f180ad0af3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:41:59 +0000

    string connection their opponent divide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
