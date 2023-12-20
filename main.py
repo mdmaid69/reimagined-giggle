59a04292b57d0c465fffb0e1037fcecbff03f1b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:40 +0000

    deserve whatever guide party crowd

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def calculate_power(work, time):
-        return work / time
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
