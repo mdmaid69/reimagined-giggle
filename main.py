4361eb0d70ae10afce958c4f6f26fd5b7ac73a88
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:44:31 +0000

    success influence justice expensive minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def list_files_in_directory(dir_name):
         return os.listdir(dir_name)
