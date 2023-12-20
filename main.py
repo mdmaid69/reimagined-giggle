2c100a0cfd1fcdfc711163134f0b49e0da9ee32b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:15:19 +0000

    style experiment worst just hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
