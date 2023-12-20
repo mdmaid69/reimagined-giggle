72a3756c8573ae04b9de4966f36d2483e764eca5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:58 +0000

    by miss breakin disapprove why

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
