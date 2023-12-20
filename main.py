b91060524fd103cf74a803ca8d31ebbf7fc4cc5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:18 +0000

    mistake exchange bet ashamed land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
