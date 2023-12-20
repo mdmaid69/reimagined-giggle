0eb1e8c2b96ee9437aa5fceb7ef98ac17de3c309
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:20:05 +0000

    suggestion nose convenient load scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
