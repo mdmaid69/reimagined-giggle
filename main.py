5beafc2a8696aaa07e5811247bfa744153adbedb
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:34:20 +0000

    onto fear away middle speed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
