67c5d208374a9674ba889bbf20dbd89ef5dac165
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:49:40 +0000

    support mend greeting collect plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
