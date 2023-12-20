84cb81faba940cfe593009e17b21dc0f28bcdb9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:23 +0000

    ever give trap calloff trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_to_octal(n):
+        return oct(n)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
