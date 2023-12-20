5184b8b41a35837a670663916bf551610d32a922
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:33:36 +0000

    collection tip give force have

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
