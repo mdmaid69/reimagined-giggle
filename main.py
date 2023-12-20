65cf631a8b6871fb43f1ff09a31d0b6227819c3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:56:56 +0000

    scared of even damage fat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def cube_number(x):
+        return x**3
