e6a8e1b15030245aa3cda62b04e2fa4845347143
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:05:29 +0000

    them somewhere divide tie wrap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_as_bool(array):
+        return bool(array)
