20a4c0d2a1aa0fd76ffa426d4f62e8d1472f1c1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:40 +0000

    normal concentration towards valuable side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
