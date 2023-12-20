17bfffda93524dd374119f4717df6dc8b41c59b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:36 +0000

    special turnaway style shape level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
