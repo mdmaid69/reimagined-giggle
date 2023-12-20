1b95daf241455fa334e3b72b965fda7f4b3ac6e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:46:44 +0000

    cum force concern adjective hole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
