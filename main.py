03ccfbca9bca3e5a9f8356aa001f97da81249092
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:29 +0000

    speech married look compete sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def find_min(numbers):
-        return min(numbers)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
