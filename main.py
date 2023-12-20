2b4a936555c3c9614bd1a4b7a2bfffd86e52bcc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:59 +0000

    down aim large relationship bread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
