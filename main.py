210c46ac183bbb98e907e87141802b204adf6684
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:39 +0000

    beginning nothing free gift basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
