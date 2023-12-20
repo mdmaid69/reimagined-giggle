86223404a0fe22dce548ce32233ed3bbd9392e53
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:11 +0000

    complicated small lookaround its turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
