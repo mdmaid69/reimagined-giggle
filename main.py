4ab06bb1ef4e42df52122e649d99f7903633eb1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:27 +0000

    repair soil part sound care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
