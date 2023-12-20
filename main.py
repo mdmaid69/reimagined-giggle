f215f9a78f88f423329c90fa1a5e8c10a6d39a87
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:39:34 +0000

    report belief science temper count

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
