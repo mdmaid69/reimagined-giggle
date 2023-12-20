efb98957d2cd3d3709cb0ecd2894bad727b0ea86
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:24:34 +0000

    classroom tear recently woman calm

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
