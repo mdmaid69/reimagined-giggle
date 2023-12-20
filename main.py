f24dc7b3062d2f5e3fb831eedb9d7ee77829a6c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:44:51 +0000

    anyone picture stop blame formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
