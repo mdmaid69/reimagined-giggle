7501e2947ef9b42eae139f3997b82ecf0749782a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:56:21 +0000

    hollow travel future confident greeting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
