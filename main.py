be3450ef6e85c1aa3c087f057e5b6dd2b47356da
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:49 +0000

    war sick succeed chance university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
 import math
 def calculate_greatest_common_divisor(a, b):
         return math.gcd(a, b)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
