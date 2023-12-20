31f300cb62100c873c5e009fd8431dfdde5a3812
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:27:18 +0000

    crowded comeout upset exact themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def multiply_numbers(x, y):
-        return x * y
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
