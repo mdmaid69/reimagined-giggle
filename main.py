a039144b24a99dfac541ccd6d60bc237f430b686
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:48 +0000

    surprised birthday experiment region knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
