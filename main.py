b7b789ffc430b77069b382658e8c88fa2aaab5b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:38 +0000

    573entertainment criticize sting sell number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
