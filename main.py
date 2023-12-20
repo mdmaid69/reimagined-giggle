680ea811bf4ff52751d42946830614e7b5c2cd78
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:19:05 +0000

    later mine else material top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_power(work, time):
+        return work / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
