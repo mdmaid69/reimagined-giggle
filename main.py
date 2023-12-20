1fdebfdebaacd541b86f20f5d3fe7f22cfcd7c6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:22:27 +0000

    second accordingto sorry father day

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
