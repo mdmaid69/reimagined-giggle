e21bb474bf63b6341eec9002eb1011d74c78a395
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:01 +0000

    callback lost down attempt aim

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
