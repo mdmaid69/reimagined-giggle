cdfe741fe7bbceba71d507f35701745db381c4a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:33:46 +0000

    corner embarrassing active strict uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
