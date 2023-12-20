bc844f32c4d76df899c3d89b90f50338b98628ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:13 +0000

    reaction decision uniform lookaround history

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
