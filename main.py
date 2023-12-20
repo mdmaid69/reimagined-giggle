005c0d3408943f634ca9086a681d989dbaf4446c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:22:11 +0000

    almost talk shout left forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
