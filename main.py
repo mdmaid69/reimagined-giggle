dc219363f377aae22d582fa4a78b1069d7ac971e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:33 +0000

    remark narrow correction sail collect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def reverse_array(array):
+        array.reverse()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
