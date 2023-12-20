983f8d3dab65d4cb04331364572d1eec14707874
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:41 +0000

    nervous brush content female wild

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
