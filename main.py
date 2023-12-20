1ab94644a78e51b6a1ff0e373705d1627ae5afda
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:56 +0000

    mysterious comfort world that moon

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import os
-print(os.getcwd())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
