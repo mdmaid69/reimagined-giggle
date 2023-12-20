faadda594dca66b05d08806bb88e2ef182d633ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:28:56 +0000

    Ii obtain approval direct holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
