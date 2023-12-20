8d7c48702ff160660fb38a2aa499efa71651d731
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:37:35 +0000

    normal persuade square watch frightened

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def subtract_numbers(x, y):
+        return x - y
