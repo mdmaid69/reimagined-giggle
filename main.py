c29b39dcd44b03a7b0310925e0def50f79caf9b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:35 +0000

    wipe bad relief cope yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
