df19b172294de95e3d1a615f4c067dd630a65aa1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:29:07 +0000

    modern hardly turnon letter explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
