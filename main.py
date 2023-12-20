8795f3e8595b9505a4aa4a86e615e3e05f2d9b3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:32 +0000

    new row news level hit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_current_time():
+        return time.ctime()
