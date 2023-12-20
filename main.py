5a75ab274d8dc4fabeef12e98bb66b59a0aebda1
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:29 +0000

    fast pressure matter police rare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
