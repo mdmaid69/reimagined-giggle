a2e3f8512be1483a0811fced7e419a0073b459ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:34 +0000

    hot mend loss friend frame

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def reverse_list(lst):
-        return lst[::-1]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
