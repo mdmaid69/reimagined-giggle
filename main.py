ac6ac7ef38c7d6bd2a03d28de267680dc6dcc8ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:13 +0000

    bright match confident control each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
