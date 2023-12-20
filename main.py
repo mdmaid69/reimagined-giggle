782da93825145b91ad7046789a58114a592d9491
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:11 +0000

    grateful whom choose immediate gate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import sys
+def exit_program():
+        sys.exit()
