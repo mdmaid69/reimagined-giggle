0cf87bd4caf38a18c090f82be7637e295b248ef2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:24 +0000

    along push appeal wing shelter

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
