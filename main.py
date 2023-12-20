fa79367d48c43200df570bebc74e82de0dd8f77b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:54 +0000

    pay yet bad preparation hand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
