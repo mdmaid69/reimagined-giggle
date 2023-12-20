392fb5e923ab50c9c4df83f38effe3c96db0226b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:20 +0000

    estimate main last some politics

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
