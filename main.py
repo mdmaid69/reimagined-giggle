cb006b79f2df4b98277d818b9fb6738ea16eee2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:56 +0000

    would comefrom then last luck

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_even(n):
+        return n % 2 == 0
