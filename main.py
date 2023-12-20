e4074683eeaca43650fb0fa975525753d491537b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:08 +0000

    meeting difficult nature even fairly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
