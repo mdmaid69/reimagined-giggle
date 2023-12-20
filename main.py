f943f8bc645f4441d2cefa6ca253072b31dbeea8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:38:05 +0000

    before along help face instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import platform
-def get_python_version():
-        return platform.python_version()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
