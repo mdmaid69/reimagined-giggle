2fbdc42af319a535654b3f40e0b0b15fcb370dce
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:58 +0000

    company land his tour pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
