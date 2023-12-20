6f42d1eaadfbfecc392015a67b2e1f9ab328f6a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:17 +0000

    know worth 569enjoy shape shall

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def get_string_from_array(array):
+        return array.tobytes()
