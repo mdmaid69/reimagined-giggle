8e04c90a8b6897bb740a2ce0240ea7d4b36e2eaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:42:41 +0000

    vanish read interest prevent badly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_characters(sentence):
+        return len(sentence)
