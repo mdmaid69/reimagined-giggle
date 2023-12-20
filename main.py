5e215789060a06ff4a45a629bfe56e9822191c2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:58 +0000

    free cook dream possibility mend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
