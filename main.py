ffbc778844e2f99d3cbcd7da712d50526ab6fc48
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:00 +0000

    cheap go old about exam

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
