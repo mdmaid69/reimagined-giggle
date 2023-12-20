a8a0c063b02960259a5b62cf1f1b4e0fc68e329c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:36 +0000

    trick 570enjoyable lost Yy guide

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
