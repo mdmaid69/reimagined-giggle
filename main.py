baba409fb16c9736f6414ca54bc270231ab965ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:29:34 +0000

    excuse birthday ashamed guess meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
