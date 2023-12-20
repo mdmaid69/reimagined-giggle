cac872104763176d2546365855cc840004e7cfac
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:22 +0000

    school keen secret depend tap

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
