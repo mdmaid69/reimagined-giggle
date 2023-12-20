3fd3f7d2b9761b40596c0ca7589dc981952d29af
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:57 +0000

    limit its somebody press swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
