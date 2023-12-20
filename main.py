56860682af3b370952dd26ab8a0c3e6ff86e89c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:36 +0000

    belief drop themselves detail bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
