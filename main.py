ae64dfda3289010fdf540fde3ad365c417a9b8df
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:50:55 +0000

    culture sight water pleased square

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
