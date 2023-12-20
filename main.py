a1117fb68919df4c3fee73f13020013d9448466b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:22 +0000

    fit out summer phone breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
