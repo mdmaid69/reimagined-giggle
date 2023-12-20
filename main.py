56c6ad7173b53d7d756329fea84d4b3a50f87da1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:25:44 +0000

    heavy avoid reply delight otherwise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
