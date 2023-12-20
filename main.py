9456d0bd4772b7b45abf5f131a3fe5356fb621e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:32 +0000

    regularly fire somehow carry holdup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
