0506f665046ee2186c7b44ff6ff825b4c4c7bc7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:10 +0000

    moment to blue speech city

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import sys
+def exit_program():
+        sys.exit()
