572c8a96e543d765d573c4f83bf44957dcfa45dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:18 +0000

    call mean ought statement negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
