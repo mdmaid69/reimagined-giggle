556c888dcfaa6e567dcf5374f31a45f5913a4bca
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:30:14 +0000

    large impressed different trade rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
