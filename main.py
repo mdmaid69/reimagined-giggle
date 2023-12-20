2b3da0a5dc06791d1b99b98ff783c8be4467fa1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:53:02 +0000

    knock description space protest she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
