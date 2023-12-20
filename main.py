e0857a9d6367bca8b7d1ba4443269a1538c864de
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:23 +0000

    vanish at goon vanish sound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def sort_numbers(numbers):
-        return sorted(numbers)
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
