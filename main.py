cbbc39db1273b41850585e68727e2a518eb3b1b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:42:25 +0000

    callup intend mistake confident instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
