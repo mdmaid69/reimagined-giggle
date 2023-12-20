66d7744d42a748ef0627099320c56b28c8d14596
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:17 +0000

    in Dd early bit land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
