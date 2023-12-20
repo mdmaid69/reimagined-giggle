b95ea512a73de7132897c4a06852768c850fd28c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:03 +0000

    date word professional glad load

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def convert_to_hex(n):
-        return hex(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
