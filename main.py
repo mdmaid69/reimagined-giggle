4f0158670ada34437bbcd56768e7127d95c15d18
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:50:26 +0000

    on soft into pile involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
