81104092524654b1e564d75e3a91816ded07c036
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:28 +0000

    convincing wander whichever turnon invitation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
