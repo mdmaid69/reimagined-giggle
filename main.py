7020553ff9592ec0b1ef63600bb71ffaf3583d98
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:51 +0000

    body used bang half able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_force(mass, acceleration):
+        return mass * acceleration
