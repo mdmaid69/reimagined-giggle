0291df1f76cc3bdc262da9a6b8a67e0eca39c0e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:26 +0000

    describe rely underneath land giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
