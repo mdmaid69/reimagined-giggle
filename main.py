bdf5f4518a537f3272ff4e98ee30f54b0f7e85e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:14:26 +0000

    who ignore strength unkind spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
