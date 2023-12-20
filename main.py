fef33261a4e64ffc5641512c48f607900a4b8bce
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:01 +0000

    nothing include lead call yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
