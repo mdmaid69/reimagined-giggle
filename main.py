88ba9c23effc205815d09c24076db262c318e1b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:29 +0000

    house keen special bag before

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
