f1e3218dd709577b54d79599ad08a4f443da4433
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:51:05 +0000

    dare start history when occur

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
