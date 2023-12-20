3779667abd3c77703f0e654d36294552e429da9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:19:40 +0000

    bound takeback realize lesson word

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
