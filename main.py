28c8c06a7abdae6d3355dbe9fe20892b4b018532
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:46 +0000

    bet smart stone appeal repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_rectangle(l, w):
+        return l * w
