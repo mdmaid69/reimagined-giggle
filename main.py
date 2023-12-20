a925fe4b97bb1d0d7c521670be38fcbc95f933ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:31:22 +0000

    pronunciation yesterday mainly develop interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
