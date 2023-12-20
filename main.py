dc552e13f77aff1e2daeca6a1a44f5b25f5118a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:15 +0000

    suppose write carry in sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
