045ea93fbc683d543feabe9bddf1e5f92f0f8f2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:28 +0000

    record follow giveout Gg on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_queue():
+        return collections.deque()
