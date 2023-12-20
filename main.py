e4bdc2eb6689d68bcb89494b5eb3f4cb8c035b7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:00 +0000

    equipment yet label once wave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
