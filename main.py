240d4e3a644fa015a43b0e4ac607f2d642549327
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:44 +0000

    history step often mysterious close

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
