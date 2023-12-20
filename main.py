9fbf3164438efc76e1396978ebf7ac345b0b729e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:46 +0000

    about apart direction away doubt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
