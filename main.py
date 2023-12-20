6b70d6af12e7bbec25d78fe6db3c6709e3d53ee1
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:41:05 +0000

    mail age concentrate division change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
