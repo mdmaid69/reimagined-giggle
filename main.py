37eb6cda274b51bd7e52407ab34a2aa6811c8c16
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:05 +0000

    570enjoyable music doubt further order

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_set(array):
+        return set(array)
