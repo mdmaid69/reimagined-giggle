d7d1f0a79ac6436d69e365231c8f3c2271ea75cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:29 +0000

    mouth black future big choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
