6cac349220d483362fe8abd43a691d88150da266
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:51 +0000

    getback wrong note woman count

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-name = "Python"
-print("Hello,", name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
