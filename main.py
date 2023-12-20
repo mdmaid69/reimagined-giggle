0025a09c50ce80a210177033f681607d4ce8bc49
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:59 +0000

    yours ill family pride deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
