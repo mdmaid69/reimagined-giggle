91a86cb0de04e0458135b8c9f59eef06436589cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:33:45 +0000

    go look copy finger many

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
