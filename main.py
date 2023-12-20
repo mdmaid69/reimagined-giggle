9e011c986d5917f0cf34e2940fb060c8d63c2c12
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:40:13 +0000

    road hello limit exciting example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
