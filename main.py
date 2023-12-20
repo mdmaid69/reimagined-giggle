f7a6666a360e007ba03e8d5ae35c89ae427e5469
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:11:26 +0000

    difficulty part aim service on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
