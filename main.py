aa3d0604e986f54cf7378c240d833376306f920d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:44 +0000

    since game unite file lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def get_array_as_float(array):
+        return float(array[0])
