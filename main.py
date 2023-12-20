bc5856e86c0c701242322ecf8102f426b19d7728
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:45 +0000

    uponon excited stamp television mainly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
