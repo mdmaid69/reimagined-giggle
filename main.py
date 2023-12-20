f838ecca5c5f182930f6b751ce49dab3a5f60a52
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:30 +0000

    break food determined offence me

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def reverse_list(lst):
+        return lst[::-1]
