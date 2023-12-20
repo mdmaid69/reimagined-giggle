16d4853017657b1cc5d31a9538287fb76bed05ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:49 +0000

    genuine add workupto tune appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_average(lst):
+        return sum(lst) / len(lst)
