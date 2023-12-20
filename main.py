fa11b19accfd1f68e47a847792c0a1685335daf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:04 +0000

    fierce apply threaten cool bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
