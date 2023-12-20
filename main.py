7e49bb61b4901116268aed8cdb5b02e7b37bf86f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:44:35 +0000

    bath finish voice purpose wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
