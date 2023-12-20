bfaf8f4266784595814d92a182b2a7a880db9f2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:02:02 +0000

    anyone upset help heavy guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
