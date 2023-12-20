8c06dc541db557d52950e3347bbee0d373e1c6af
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:54 +0000

    where valuable report water leg

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
