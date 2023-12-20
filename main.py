7e52f27a1b383ed5034892ccd382c871e7494b45
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:27:12 +0000

    sensible swing birthday bag jump

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
