5f0abb8d7a9ac3b2e5f55fdb9e26e01c7d2db2da
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:18 +0000

    game seat flat undo pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+def get_today_date():
+        return datetime.date.today()
