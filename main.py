0f74617fc7b7c688b87d731426a32dcaee69f4f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:49:33 +0000

    gap flat naturally top theirs

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import time
-print(time.time())
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
