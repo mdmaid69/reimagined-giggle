87e741d5b3794e3005a7fa8b8177fa19de63a565
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:40 +0000

    card comment store future talkback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
