830c6e6f3ac91a1a15d567f98b9dbdf28462281e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:09 +0000

    constantly fit strong health woman

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
