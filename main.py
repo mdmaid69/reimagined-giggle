47d2a38f61917484bd1312d8bf7d9a8434a21e5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:07 +0000

    back invite port settle fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
