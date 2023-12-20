6cf0124ed5c18e23e36bf959d2c9fb6e7e0767f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:41 +0000

    male mix next fortunately crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
