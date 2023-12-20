d8936ff831018b8cf269d30b21abcb3af2853d3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:07 +0000

    believe today care later equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def convert_to_binary(n):
-        return bin(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
