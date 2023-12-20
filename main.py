e46775f74f9d9facf0090f45f604fe34d07f894b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:58:49 +0000

    hang scream rubbish remove unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
