22eed3bf61c4f1a7a8caceeb95e6ffaa94915340
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:22 +0000

    community live excuse interfere unless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
