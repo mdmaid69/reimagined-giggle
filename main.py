5a1a59a4309aba55c07f9605ea88e00eaae28e8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:37 +0000

    bit bright building front shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
