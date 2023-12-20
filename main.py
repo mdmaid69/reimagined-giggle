b322f991f6aeffc7e8c0785e8e14995a8c32836f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:06:53 +0000

    few improve success ask flood

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_current_datetime():
+        return datetime.datetime.now()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
