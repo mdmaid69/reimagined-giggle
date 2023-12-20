2abecc4c708f12cf2906bb88bfab38e9412eef0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:28 +0000

    float takeback trouble whole star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
