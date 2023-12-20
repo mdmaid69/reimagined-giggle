be6a31b634438d03dedfcabd171abb64b40c2d1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:45 +0000

    spare inform consider 570enjoyable describe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_force(mass, acceleration):
+        return mass * acceleration
