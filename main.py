38b349d59bc81cb772cbaf6fa4ca212b8c1d2642
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:49 +0000

    573entertainment side switch case matter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def roll_die():
-        return random.randint(1, 6)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import getpass
+def get_username():
+        return getpass.getuser()
