8a29ba8eb87a9d9064932079032de5f90c63e455
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:55 +0000

    fear bill step challenge eat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def multiply_numbers(x, y):
+        return x * y
