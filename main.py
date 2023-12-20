f92537c946c4d97dbaf137e813316711abb25094
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:18:24 +0000

    convince cause such unhappy closed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
