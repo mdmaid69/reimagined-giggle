23319003b11c7793f4f04f0d0a1cf56510647fe3
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:46:34 +0000

    used offer centre attention centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(numbers):
-        return min(numbers)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
