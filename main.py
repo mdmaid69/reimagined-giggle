0575d2ee1e67b85e0f55146210447bf314a68f6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:22:57 +0000

    contact transport closed hard judge

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
