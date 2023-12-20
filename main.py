6d793a54ec2f0c6917744ed1660a323a38ebb1d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:49 +0000

    afterwards clearly expect promise cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
