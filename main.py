11caca926c283aed19e4776624df5024af4d4f52
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:53 +0000

    special foot pretend central past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
 def calculate_hyperbolic_sine(x):
         return math.sinh(x)
+  def convert_to_binary(n):
+        return bin(n)
