d9c621408390844be4b3d553f916f6642eeea556
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:24 +0000

    dress follow test cap naturally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
