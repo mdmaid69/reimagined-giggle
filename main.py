93dabb6d84346cdc8ad7360c3e9957f1ae93b0c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:17:56 +0000

    but bread today left tell

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_pressure(force, area):
+        return force / area
