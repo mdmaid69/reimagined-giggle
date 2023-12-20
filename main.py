685971a04b41271138e14cf638bc636b2c65021b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:59 +0000

    top put thick consider level

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def convert_to_binary(n):
+        return bin(n)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
