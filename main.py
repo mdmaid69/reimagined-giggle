0608845cb3057c4bd4e7a1e626b138a9e361e4a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:07 +0000

    equipment father present ground constantly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_current_working_directory():
+        return os.getcwd()
+import collections
+def create_stack():
+        return collections.deque()
