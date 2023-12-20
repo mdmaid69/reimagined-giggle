81fd67b7b6aeb2b074ff859c5c10028377e4c1d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:20 +0000

    separate respect proof keepfrom chat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
