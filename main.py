fe41c8b01db6e1824581d4939d7b12c17ddea0a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:04 +0000

    future approval surprising target warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
