dc21975784615461218f0a0f0e85c8124b100bb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:17 +0000

    paper bridge colour somewhere middle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
