53d2e63faabb1ba709012bcad8125837bf478b76
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:09 +0000

    luck excuse square front seem

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
