f1a034e1a1ec1d166ea70fbb406caa57a894ad27
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:19 +0000

    steam attract best about scrap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
