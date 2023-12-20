95b41419ebb5da3ad2964fba45b24da4c099a604
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:57:37 +0000

    plain centre wash pay doctor

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
