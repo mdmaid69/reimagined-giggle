217c4751c52986cd7f2c8e3e6998022db0e8d128
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:40 +0000

    brain angry both pen circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
