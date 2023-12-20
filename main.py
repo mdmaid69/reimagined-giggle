d6b76dfdeb5c8399c1fe3e4330ef56004d3fcbe9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:00:04 +0000

    bus ticket still juice proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
