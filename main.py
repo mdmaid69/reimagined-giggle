bcfc28c673bb3fceeaa23bc4870ff484d6fa7f3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:14:35 +0000

    lower play squeeze embarrassing wind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+print(os.getcwd())
