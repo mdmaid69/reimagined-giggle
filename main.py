8fb0e13f2e54e511a17cc16cd2cd7adf37a3742b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:23:35 +0000

    shake chat guide sound spot

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def add_numbers(a, b):
-        return a + b
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
