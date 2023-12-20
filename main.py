0d0df8d0db30344024457d5e6fc983159d034f41
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:23 +0000

    regular manner seriously attempt glad

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
