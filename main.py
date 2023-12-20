e931b87cdf2a78d8b18bda52133ed6d50f5839a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:14:06 +0000

    easy dull address thick admit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  def square_number(x):
-        return x**2
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
