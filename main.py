b71521c0cf9fda6d699c563b0fddc1a596d37250
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:56 +0000

    without zero borrow smart wherever

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
