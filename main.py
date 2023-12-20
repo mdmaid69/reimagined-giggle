69883e7047a84ec5ecf3a073ef42ce5c1dfc6436
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:04:56 +0000

    ball spot machine slice quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
