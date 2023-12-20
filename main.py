5b479947f4cf3765b8738c825a6c4339fd3fbdb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:41 +0000

    official how handle university inform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
