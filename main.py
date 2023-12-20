0a740b3a6fc2567b4bc4650e9f3e4811bba56559
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:48:51 +0000

    far only during runover corner

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
