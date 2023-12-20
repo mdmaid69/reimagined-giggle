cfaac0f8e83856c9542636c6f8b53e12da81f2d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:14:44 +0000

    interview shadow street disappear sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
