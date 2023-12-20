ae24cda698c6d2605e65869aa2a5449d57288c96
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:04:25 +0000

    hang cross late available formal

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+from collections import Counter
+print(Counter("hello world"))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
