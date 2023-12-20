ef2c14c28d82b99b37ae44eba1bf4ef6b36a4d8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:47 +0000

    example strict mysterious pool official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import time
+print(time.time())
