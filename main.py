ea61e2c605921548dee403d6f44bfd07397f2c6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:59 +0000

    than so bag fit wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
