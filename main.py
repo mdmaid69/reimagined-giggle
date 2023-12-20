d82fe422d25bd4ee3aa0f8e0027cf06bd1cf2144
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:36:35 +0000

    sink back top mine belong

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
