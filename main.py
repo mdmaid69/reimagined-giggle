ec09c838ddd9e5b7a269b60daabc30d2a983c37e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:04:09 +0000

    stiff trouble attempt Gg relative

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
