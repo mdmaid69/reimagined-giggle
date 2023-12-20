a35def1896d71e958fe1e48d27bdf7411d37319b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:50 +0000

    point put aim section expensive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
