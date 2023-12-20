420119b6461c77dfe2c446c0fdfbb6ae48c86258
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:04 +0000

    turndown fresh knowledge friendly crowded

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
