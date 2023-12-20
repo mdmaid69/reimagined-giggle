376f044127651e57cfa39c4f65f99d2ed16f1070
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:38:59 +0000

    trip come by spin drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
