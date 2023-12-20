3d291ddc999fd6f939e0185aa72d9d5e0f3db28f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:02 +0000

    sale cause last either destroy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print("Hello, world!")
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
