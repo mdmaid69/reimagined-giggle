7bcc1ac885754bb47e9a7421ba317dcfd94cb075
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:05 +0000

    able sense nonsense whenever concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+import math
+print(math.pi)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
