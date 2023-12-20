15da6735e7523e707900e6f69718c15b8e8c2e25
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:16 +0000

    mood sensible her keepaway determined

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+print(datetime.datetime.now())
