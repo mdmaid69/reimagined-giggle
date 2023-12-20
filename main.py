0801870ec63ea1db3e5ec469599965d3eab2f244
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:44 +0000

    stamp after after long precise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
