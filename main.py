2c2d6a651499c9f5eac021867919f34f6162bad6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:10 +0000

    cinema afterwards within meet opinion

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_power(work, time):
+        return work / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
