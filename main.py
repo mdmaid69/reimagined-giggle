fccbb13442f7f8b461dee6cc591520dc07ebf2d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:24 +0000

    regard excited proof not immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
