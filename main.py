7dcc191b0981fb018b1bceeb7705e0d414c3d857
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:06 +0000

    need remain giveout extraordinary laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-print([x**2 for x in range(10)])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
