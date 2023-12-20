35b96f926c85d9bd8e416db404a6178f8b81068b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:47:30 +0000

    health Ww influence fear some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_min(numbers):
-        return min(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
