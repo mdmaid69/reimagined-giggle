1e12c184ed585dfe782890e064952859d860b719
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:01 +0000

    neither exploration game support kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
