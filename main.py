c5ee6ca979441d6718a6840848a37e164ac83160
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:42:27 +0000

    development embarrassing challenge Ff furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import sys
-def exit_program():
-        sys.exit()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
