59597e7e50326c6252b18de7c93efae678571ce5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:30 +0000

    upset declare cheek sea lesson

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
