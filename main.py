11d3adcc0c280691f62e27dd614f4a4c58d5bd61
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:10 +0000

    thought pay wash interesting floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
