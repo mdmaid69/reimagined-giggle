a073f6659ea6d6638290868d83f75ad106cde714
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:35 +0000

    breakdown record only general natural

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def count_characters(sentence):
-        return len(sentence)
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
