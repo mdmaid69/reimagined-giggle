c8542f12897a51d45b271803f9898de3491b761a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:30 +0000

    sensible contrast like workupto congratulate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
