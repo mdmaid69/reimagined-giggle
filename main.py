1cdcf8bed6bc93e6401cc8cf817251ac85333b13
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:04:09 +0000

    may them claim concerning world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_exponential(x):
+        return math.exp(x)
