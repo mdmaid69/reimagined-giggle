605ce89ec69f179ae7e6828ebe1ffea038457ca0
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:53 +0000

    wish finger purpose name trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
