31fc679c4a60eecbca1a5bb548954ce8286041c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:51 +0000

    pin uponon centimetre well abroad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
