4d30ed9149bc3165c4a1d7f8250543a198e602ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:15:49 +0000

    she business fortunately will understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import os
+def get_current_working_directory():
+        return os.getcwd()
