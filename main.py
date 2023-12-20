9ab7946ae0c678ddf78582343653432db02e6974
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:10:51 +0000

    street near congratulate street tonight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_hex(n):
+        return hex(n)
