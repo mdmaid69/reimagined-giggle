6fc3d1919a153c86dbe3c73cb36d1ce89220af60
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:32:27 +0000

    sink Tt science disapprove gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
