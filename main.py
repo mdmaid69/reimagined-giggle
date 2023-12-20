265c9cc99e082d59f71a5ad26adf3704c2d02f76
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:00 +0000

    keepout like rule into breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
