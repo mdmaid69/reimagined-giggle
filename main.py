e29bb5b90daafc2c207e79753fcb9a21ed236741
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:55:34 +0000

    offer suppose safe distant means

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
