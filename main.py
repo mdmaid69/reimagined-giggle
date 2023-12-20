9e76cb8601fc7a35a2c8b95c56e686f276b31673
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:38:21 +0000

    female majority wherever cope remark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
