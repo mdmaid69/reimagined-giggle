eb3a80a4e5944c1103b25e98e1c83074d291aace
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:42 +0000

    close system slip frighten holiday

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
