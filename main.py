cbd0d333e901a3749984fb4e8b74e9ccd491ad6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:59 +0000

    impossible film smooth yes especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def convert_to_binary(n):
-        return bin(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
