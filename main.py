118e30d1e69eb1f346975bcda2dced37e3b37ec8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:52 +0000

    extraordinary unlikely pressure nature coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
