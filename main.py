e0e1eaf14e673a396d45cdfb56c10775a9372406
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:26 +0000

    hardly regret estimate afraid ought

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
