d5fa9bc6608b3cba94fb34a24d17fcc2276588cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:29 +0000

    attach club Hh university delight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_arc_sine(x):
+        return math.asin(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
