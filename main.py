bc76ee53143b04ab00c21704da522161c2592b28
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:34 +0000

    discussion operation I blame test

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
