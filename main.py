d9d8a49e352152a9f364ca840abeacd8f49d8b10
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:24 +0000

    nervous fly steady performance advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
