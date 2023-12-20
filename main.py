ff9462cadf57b36b086bedc1d6c15dbd77b26a18
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:11 +0000

    spill mind city voice table

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+print(math.pi)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
