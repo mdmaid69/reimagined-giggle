408316b6e6980ca05eec64c26800f03fe447ec7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:29 +0000

    embarrassed Pp carry transparent near

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
