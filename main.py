e4f1538690c6e7db84b585302a79f39d12b695e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:07 +0000

    respect then careless sign soft

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
