54f97e94f735ab7a79804300b08703273061084d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:43 +0000

    young cold serious blind path

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def remove_duplicates(lst):
-        return list(set(lst))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
