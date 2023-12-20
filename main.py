6e06fb5c0034f5a1215c0a8a9e4cac7f3c08cdc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:55:43 +0000

    minority cope wire underneath dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
