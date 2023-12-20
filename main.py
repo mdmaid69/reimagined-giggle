b6da46a0c06570595b1145c6f72aa789cd370a28
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:16:52 +0000

    let tape explanation sign ourselves

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
