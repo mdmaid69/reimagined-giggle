68f2b8c22dc38a4287166e75b12bbe8290f27f49
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:27 +0000

    most succeed everyone top wet

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
