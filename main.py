537a64017bc74044ea734d15ec1881ac22843a1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:55:32 +0000

    exact entry division rate speech

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_str(array):
-        return str(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def is_odd(n):
+        return n % 2 != 0
