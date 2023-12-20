f625e19085918d57b8f3a08e459f6a89772636b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:31 +0000

    control difficult such interview dear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
