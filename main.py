2e0b8a13271fda46f64491be9952b0d614523365
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:23 +0000

    concern Ee find sentence hurt

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
