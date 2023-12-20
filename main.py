ee9ced4891e92c546de4357bb130fe80f5fb454a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:09 +0000

    rate please all tie turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
