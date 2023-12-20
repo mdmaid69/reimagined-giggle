cb0c515487c06a03678e3ebe807beb2daaff2324
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:20:25 +0000

    possibly can crazy around strong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
