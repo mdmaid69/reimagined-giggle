d783dd3dccd8bcf456554bfb4b69435dab1aa4f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:20:42 +0000

    why over future short dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
