01e58da55503131be7d3c8274cb0189e7cc5d1bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:48:47 +0000

    damp lunch breakdown accidental wind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-print(os.getcwd())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
