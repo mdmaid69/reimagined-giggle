c6fa84915b7a6ed59a5b5cce715349e7cfe36f31
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:45:04 +0000

    obvious talkback dry official rely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
