d86c7210c81cc0a36b1d1ee0ea05665ffd697be8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:08 +0000

    drink man suck culture turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_as_float(array):
+        return float(array[0])
