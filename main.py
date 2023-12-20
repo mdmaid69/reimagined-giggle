314bd21eea6c9f28cc33c98496b4448cb27acabd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:11 +0000

    surprising Nn lookback(on) shop only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
