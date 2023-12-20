1297872331c42158e05e2d339d06babf2c9649b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:13:20 +0000

    exaggerate mine weather strong workon

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
