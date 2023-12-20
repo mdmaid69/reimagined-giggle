dbb9efe8037e0cd45bbea670a51d4c5c10bf20d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:21:57 +0000

    system journey government way where

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
