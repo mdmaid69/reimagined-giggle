1d71036fa09052a9ca8f52f705ac18fbc2a51a88
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:36 +0000

    intend tend holiday involve note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
