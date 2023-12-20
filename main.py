2013423dcb585857d400fb61edcdab1d5ee1ad37
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:40:09 +0000

    section noise pleasure deserve stay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
