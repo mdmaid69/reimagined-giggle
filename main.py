b3c07d20d88c9c9616c8c382d8a68c3ef7ddbe56
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:37:37 +0000

    fond possible fail turn flow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
