8135694787c0c2c5408f49ca78d89906fb7993de
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:54 +0000

    keepout later reach big young

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import sys
-  def get_python_version():
-        return sys.version
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
