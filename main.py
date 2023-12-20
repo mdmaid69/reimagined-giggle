b52ff2b62c4163c0857e0728bee02e4aa74b8643
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:23 +0000

    hit little sudden some move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
