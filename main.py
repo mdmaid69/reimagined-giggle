c60b4ed3725368814180cf89c7808b5657552695
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:25:02 +0000

    become its between mess up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_string_from_array(array):
+        return array.tobytes()
