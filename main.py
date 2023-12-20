b037354c1c521ce32f84cc64838a7c1ba1de4398
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:58:19 +0000

    strict getout persuade tense left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-for i in range(10): print(i)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_volume(length, width, height):
+        return length * width * height
