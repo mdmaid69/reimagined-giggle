41e6ced7b2ade7a719531b642244613476010395
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:46:34 +0000

    force minor quarter Pp past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
