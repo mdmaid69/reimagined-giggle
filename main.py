8baf61a967b2c964614dc6f4b99bc6815b35af2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:08:54 +0000

    better exam geton dull opposite

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
