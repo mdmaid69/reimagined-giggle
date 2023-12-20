c61d32ef5442aa19b2b7a088b43bc4dbc3fea255
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:07 +0000

    concerned test shade genuine mine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def is_even(n):
+        return n % 2 == 0
