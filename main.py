8f73bf1be78bba43c108def393ddb91d5b283491
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:44 +0000

    lock needle father unit reserve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
