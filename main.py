c089002e4e72b70c6046869673cc832b8757fb21
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:13 +0000

    possibly bored type bringforward condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def sort_list(lst):
-        return sorted(lst)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
