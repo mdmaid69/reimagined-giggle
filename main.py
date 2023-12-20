b61aa25a57dfaa8c594bc7779d1da4f70e50bc68
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:02:00 +0000

    turninto live patience shut respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
