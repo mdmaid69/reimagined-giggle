bf75582784c0fd74d2477fd076c438dec00f90b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:10:01 +0000

    tend second and escape properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
