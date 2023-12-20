8893fdc19a910ffec44d3c17ea2319dd21829228
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:11 +0000

    forget impatient press surface among

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_int(array):
+        return int(array[0])
