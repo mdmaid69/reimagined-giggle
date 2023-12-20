406dc7be8609e68bfbba6ec7b9cf93cb5d80ba23
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:16 +0000

    shoulder century pressure measure strong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
