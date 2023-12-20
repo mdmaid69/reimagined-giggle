b15249fe7ce9142de690711785ef8353f6fbb208
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:53 +0000

    unlikely shut love wind turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
