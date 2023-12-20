42b4766fa907c04cc2b653624de02c27e65393a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:59:45 +0000

    quality exam profit goaway bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
