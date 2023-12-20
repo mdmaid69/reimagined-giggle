c17663a184b2d79aa07a38ba7b9e570d15fa5e32
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:57 +0000

    start artificial her today quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
