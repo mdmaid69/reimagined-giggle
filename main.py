bde7bab3fb62bea53445b9d8a0302497b08498f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:12 +0000

    opposite improvement add sort space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_typecode(array):
+        return array.typecode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
