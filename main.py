cb9a6349ef822e2349128928ce3014f1e9bd4a7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:35 +0000

    thanks freeze pain our sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
