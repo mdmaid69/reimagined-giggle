160b44bfa67c38c1a573d3a7175e223053051f65
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:39:33 +0000

    save fire tape polite display

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
