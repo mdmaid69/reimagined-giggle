6573157d5c33bf36cdae544136f6ff0b93b10027
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:31 +0000

    protect reply schedule off getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def reverse_list(lst):
+        return lst[::-1]
