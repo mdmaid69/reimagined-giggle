fc9970fb2f420cdff39cbc880d8b90486d30a542
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:06 +0000

    cut base under there think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
