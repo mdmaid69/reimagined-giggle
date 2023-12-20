7e4b54611bdbe7502df7059d19226988fa497f98
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:27 +0000

    scream intend shallow aim difference

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
