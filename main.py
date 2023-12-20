3b6267c847db9c88998735e0acc728ff4e9277bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:37 +0000

    dig into shadow clean learn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
