6ed847c73f319977fceca6c38c76a1168ee89eff
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:04 +0000

    order remove proud cry politics

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
