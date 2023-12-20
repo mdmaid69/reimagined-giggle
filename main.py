0ec15d206567cc5ecc3c2d09160f2ed317901573
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:10:58 +0000

    rush act bus replace weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
