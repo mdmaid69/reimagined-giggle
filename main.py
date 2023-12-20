55ddbdfb234d6a387ad9041833861965a5742932
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:18 +0000

    meaning vanish exam legal city

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
