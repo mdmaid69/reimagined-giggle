1c7b538886bd0b056b35e47bf1a522068d3484e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:02:44 +0000

    past zero breakthrough day drop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_power(work, time):
+        return work / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
