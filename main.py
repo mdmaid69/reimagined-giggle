6545298091ff063cf647136d31f0ee2e6d70c6ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:49 +0000

    habit description disapprove underneath last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
