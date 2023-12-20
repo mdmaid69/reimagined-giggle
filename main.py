3cd6381855218a805abaa882ae986dfb05d7ec56
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:31 +0000

    bath slowly sorry day scrap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
