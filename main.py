25abb809d798c52107665d6a64c378b5da9de630
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:28 +0000

    shoot clear dark everyone reject

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  def is_even(n):
-        return n % 2 == 0
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
