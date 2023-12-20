de0cedef239f190a77d31aac438360e45295cf1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:49 +0000

    proper thorough somewhere hurt chase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
