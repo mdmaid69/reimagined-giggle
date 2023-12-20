8ab681a9226a421336af32a7ad279eb06d3c81f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:16 +0000

    bringup calloff degree thought Tt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
