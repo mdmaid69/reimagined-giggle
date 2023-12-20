e27a2cda7aedb56a7b3eba5ab9f804e25fa56248
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:11 +0000

    mess post giveout dick below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
