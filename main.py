122a541abc148e0ee206787d392ba18ad7d4137c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:55 +0000

    exit hurry street rain set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
