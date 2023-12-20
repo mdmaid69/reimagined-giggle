1e649442036bb0e1996afb56febf9de896de72bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:52:02 +0000

    pupil steep sign possibly interview

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import tensorflow as tf
+print(tf.__version__)
+  def convert_to_octal(n):
+        return oct(n)
