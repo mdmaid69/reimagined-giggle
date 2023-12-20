96c63c02b04758392676ec4e08eb54d5d85db423
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:59 +0000

    original send scale task doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def is_odd(n):
+        return n % 2 != 0
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
