16bda3dcecf7ffcf09fa709775d7976c32cc2e3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:56:10 +0000

    soft well telephone last takeoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
