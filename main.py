8c72fd9821617233555d7598a94f0dd1ec0b7039
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:27:19 +0000

    shop thin unpleasant deliberately thirsty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
