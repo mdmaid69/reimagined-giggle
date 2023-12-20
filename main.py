66fe0403a455732e521a75515a11e485a7c622e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:50 +0000

    legal damp joke anxious split

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-print([x**2 for x in range(10)])
+def find_max(numbers):
+        return max(numbers)
+def calculate_density(mass, volume):
+        return mass / volume
