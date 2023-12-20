c2b4c80bca5efd558ee2de1fcad47b9ed9657bea
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:33:42 +0000

    size relax nigga Jj pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def greet(name):
+        print(f"Hello, {name}!")
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
