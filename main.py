c7648fa24363990b2c5d8f269a2eb766f2857fbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:29 +0000

    cruelty pipe slight care actually

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_min(lst):
+        return min(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
