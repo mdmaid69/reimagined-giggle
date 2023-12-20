cc3e0af44734734a71b463dde27d791962c0a743
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:42:31 +0000

    appeal push sore here silent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_current_working_directory():
+        return os.getcwd()
