9454d9ec3ad944dc577ae65cb1f0d5e13d78f9ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:15 +0000

    beyond trouble turndown sleep floor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
