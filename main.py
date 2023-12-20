e174a2f53ccdb85f38e0eb99ee3305e4463f95ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:37 +0000

    threaten left interest unpleasant disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
