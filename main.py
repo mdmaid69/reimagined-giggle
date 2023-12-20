334a97e31f6d81576e6433a90e2d650cca9efb0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:30 +0000

    news light talkinto draw office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
