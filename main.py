2367b1942d5dac3eb1a53b2b50cc4bd8e67c0bcd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:25 +0000

    build compare phone plenty declare

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_work(force, distance):
-        return force * distance
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
