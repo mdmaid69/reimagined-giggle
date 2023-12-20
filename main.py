bd25532781c014cc9288ed9d33a56fc6f44374fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:02 +0000

    couple news pool yours we

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def square_number(x):
+        return x**2
