1fd6410569ca14aa2107c59280fb5e3c202ffe89
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:43 +0000

    involve still modern walk convinced

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
