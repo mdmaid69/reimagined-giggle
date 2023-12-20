c63e158d887b2711105e954497c7c416cf6cd2a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:40:52 +0000

    goout cut nurse decision smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_acceleration(speed, time):
+        return speed / time
