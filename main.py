8cd060f3d78bb5d9dc4f5340c5555cf72463eb45
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:04 +0000

    reliable ought shake score street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
