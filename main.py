92af128de72402af1945d0f5483654f0957f33d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:11 +0000

    not medium black throughout several

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def is_even(n):
+        return n % 2 == 0
