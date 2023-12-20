883d7a1bf60a050991e1225ccea364906a7ccfbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:34 +0000

    sudden wherever suffer powerful lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_array_index(array, item):
+        return array.index(item)
