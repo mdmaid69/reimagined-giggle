6e1f765a00d849805dc284f51a9b8db8abe7afb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:41 +0000

    apart sure now ourselves any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
