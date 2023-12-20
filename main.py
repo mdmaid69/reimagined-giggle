e9fac68ed014730c31c5572a6ae34fb622c3fdf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:08:26 +0000

    between rid worth mix trust

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
