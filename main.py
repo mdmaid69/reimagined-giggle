db3edabe3a52204526dbe50c9ab83007e145968a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:38:16 +0000

    if sometimes invite stay roll

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
+def sort_numbers(numbers):
+        return sorted(numbers)
