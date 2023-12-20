5f0eb5e3dd7f19d8f058697e326bbd81f8164c5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:10 +0000

    breakin male crazy some usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
