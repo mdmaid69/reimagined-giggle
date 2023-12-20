f1bed1dd0930f4f6633647115b69de662489df37
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:10 +0000

    cheek hurt great track friend

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
