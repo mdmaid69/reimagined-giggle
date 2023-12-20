62cc5d3ad0ecf2eab05d839871f165843b16cd4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:09:20 +0000

    separate I throughout full long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_current_working_directory():
+        return os.getcwd()
