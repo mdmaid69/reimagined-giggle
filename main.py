b6ad05029d62705cdb57dcc7fabc817b7fb9b4fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:15 +0000

    definite rid lend bear act

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
