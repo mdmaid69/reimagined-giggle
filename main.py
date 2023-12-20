1b166f42843a210b30761120067bb58de794e9cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:31 +0000

    who occasional impressed not Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
