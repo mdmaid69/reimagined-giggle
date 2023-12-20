6379c3083fd329e8b96ff203af0d691a83c8cc95
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:28 +0000

    Oo shake subject square contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
