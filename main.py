bcd37cc22a81c4cdb60ad3a47a2fae75c35e8329
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:09:40 +0000

    drop plenty should thirsty makeup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def greet(name):
-        print(f"Hello, {name}!")
+name = "Python"
+print("Hello,", name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
