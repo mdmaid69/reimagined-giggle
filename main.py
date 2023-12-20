69dedd70e15433fd10a87b0c6fdb5065df5be246
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:59 +0000

    pool attach everybody wait fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
