c5b4d2c535602b11aec549685e344bd268ea160c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:03:21 +0000

    unfair complain improvement deliberate hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
