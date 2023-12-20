a9b36adc7be6fbd0aa9650fffd85bab6157d2d53
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:00:29 +0000

    pass about length work produce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+print(math.pi)
