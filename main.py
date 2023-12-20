c7a1581bfbbbfddf0be5ede191a09e53e22cffeb
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:24 +0000

    switch embarrassed arm her taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def divide_numbers(x, y):
-        return x / y
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
