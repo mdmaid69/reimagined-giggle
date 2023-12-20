3fcb8fd1573c2e34f1e07365e40520c09be7fd8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:43:25 +0000

    once private nervous comeup thank

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_work(force, distance):
-        return force * distance
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
