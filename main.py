d5dc268250c05dfb84bab5460bdd5092c34ca37c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:32 +0000

    have every represent rest record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def change_working_directory(path):
+        os.chdir(path)
+def find_min(numbers):
+        return min(numbers)
