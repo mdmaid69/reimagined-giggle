1b7d4c29081d18e4df76353687aa9a8580e070ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:09:59 +0000

    train through acceptable powerful live

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_circle(r):
+def calculate_area_circle(r):
         return 3.14 * r**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
