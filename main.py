b8770ce165af9a9c93e13dd43bb3d2987f1a329d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:38:52 +0000

    tooth speak mind 565end mend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
