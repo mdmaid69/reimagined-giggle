9460f87e4b0096f83be7f99622a17dc497fc1867
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:24 +0000

    evidence petrol argument theirs direct

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
