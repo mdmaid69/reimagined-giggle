8dfc56fcde8dd62b36e5f1619d9f5aa5430dae76
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:55:44 +0000

    tie bad belong noise eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
