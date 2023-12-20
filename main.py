1da3ebb70c06ec0d8c432e5153eeaf9f7d8574f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:37:06 +0000

    passage shout stop small phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def square_number(x):
+        return x**2
