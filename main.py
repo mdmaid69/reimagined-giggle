36ed8ff045d400a693481fb0fcd92185b55475a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:49 +0000

    comeround tired guide typical every

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def greet(name):
+        print(f"Hello, {name}!")
