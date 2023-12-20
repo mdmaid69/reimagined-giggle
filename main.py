09a83b7d6e4fcd4a7a89d4941c928315723e07a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:55 +0000

    straight ear probably fortunately innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
