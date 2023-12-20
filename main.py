7ffef61f20a335042a9018dc8ef3cd22b38813ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:19:25 +0000

    degree convenient drink family birthday

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
