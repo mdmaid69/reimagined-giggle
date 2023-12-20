93afda106f28c52605afb8bf6de7dad167884c54
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:13 +0000

    far flood mind innocent priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
