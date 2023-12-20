49f19eb496804f5a1f592a40dd1c6e5b19e5280b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:04 +0000

    terrible downstairs unfair drink Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
