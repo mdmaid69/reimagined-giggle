02f1943033dfccf025d9c0a2096be69de2c9b9f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:57 +0000

    suffer market mother Qq party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
