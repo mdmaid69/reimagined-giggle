5149dafc8013526586df8ddd07a5487c939c3de1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:38 +0000

    base remember Dd move Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
