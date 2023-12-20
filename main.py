bc3403b8f66fcc66c90704ca6b60ce806ab1fb59
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:51:01 +0000

    nervous turndown ordinary smooth brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
