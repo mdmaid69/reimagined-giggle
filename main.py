94e398143b37443829445cd25a57678f05d79145
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:37 +0000

    problem spring shine comeround after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
