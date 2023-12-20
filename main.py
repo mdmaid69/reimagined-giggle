58c4924eec677a563147e0dcf2b0f9190b90b8e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:18:26 +0000

    comeover pass mind gradually right

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
