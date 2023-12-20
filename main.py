a6a390b6b14aaa04f90256330dda3729c656a239
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:00:44 +0000

    cheque often giveout treatment sight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
