ac70fb58b5ff41f0da9747c5f7ce569b7285c44c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:21 +0000

    criticize soon flat means list

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_index(array, item):
+        return array.index(item)
