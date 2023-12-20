784671298b4237d5146f15a43035538a3f60006b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:17:26 +0000

    could progress over sweep sense

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def cube_number(x):
+        return x**3
+  def subtract_numbers(x, y):
+        return x - y
