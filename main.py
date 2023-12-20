3f3ddfbbb6e60e171630f998e15b52ccdebd004b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:22 +0000

    dry comeover though emergency exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def clear_array(array):
+        array *= 0
