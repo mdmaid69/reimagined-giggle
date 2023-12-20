844030ddaa4a11eb96f20ed648d6c5c6c13b1999
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:33:32 +0000

    blue exhibition range terrible angry

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
