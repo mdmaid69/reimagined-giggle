cc63c98d1d5c25797609b17aa48da53f557d1cff
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:07 +0000

    nurse awake light speak yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
