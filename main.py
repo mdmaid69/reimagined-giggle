2ac8ad5e0f16e128b925acda1783011d9566cf2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:04:56 +0000

    thinkthrough anywhere hurt young release

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
