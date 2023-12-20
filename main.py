a86e393e220bffda72ad88e618c442e3990db3ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:03:11 +0000

    compete start notice bringback measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
