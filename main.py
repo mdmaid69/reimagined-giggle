26505143b8f4f0959c473ae7f99b1d307f3aee69
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:34:39 +0000

    house off tired giveup refer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_acceleration(speed, time):
+        return speed / time
