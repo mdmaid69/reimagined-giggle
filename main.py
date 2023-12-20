9811ec37d92b19455cdbc5b1df9d0b5b6db0eb07
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:42 +0000

    still demonstrate young reliable general

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
