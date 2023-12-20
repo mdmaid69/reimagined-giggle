31de470392edae445b40184d201314a4fa8eeb98
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:18 +0000

    definite deaf laugh Gg call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
