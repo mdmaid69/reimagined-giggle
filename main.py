83034882f9c7828f794a52c9415f07a1f7463064
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:10 +0000

    pleasant attach matter scene again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
