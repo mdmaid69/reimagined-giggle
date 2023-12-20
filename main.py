0c95c6134773c1db4575c7ab897f82c1816975b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:02:56 +0000

    wear strong equal still runout

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
