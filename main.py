337175d23b40d8cd657444d75c9956de971993bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:22 +0000

    surprised sweep attitude open do

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
