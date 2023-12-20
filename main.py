f98db57f4b9855ab93a62f2a04abd5de45fb373a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:16 +0000

    our wash remark actually able

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_binary(n):
+        return bin(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
