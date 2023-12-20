8b876a2d45c2046728a151f6ff19a6faf1f32454
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:37 +0000

    snow tool offence time raw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def cube_number(x):
+        return x**3
