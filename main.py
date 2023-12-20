1b95955607021be031c3cd4af4fd507b047d8a92
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:12:26 +0000

    deliberately uniform sit among edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
