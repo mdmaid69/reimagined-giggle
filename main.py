65a0cdab6c076ae9da2fb3dcbc19ccb9adfb8312
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:27:20 +0000

    Qq branch before delight emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
