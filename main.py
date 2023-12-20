00b4952dafb108d1a84a97e8c633d6104fa5abce
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:54:16 +0000

    chance giveback study during brain

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
