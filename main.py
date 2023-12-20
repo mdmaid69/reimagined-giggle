7bb6b3d5a3022c03c0013aed261fe5802d57f33a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:04:22 +0000

    public payment wall identify rid

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_area(radius):
+        return 3.14 * radius * radius
