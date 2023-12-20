866a237a8342d9e968529fb989f4f549918ef148
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:02:24 +0000

    solve blow ought mention neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
