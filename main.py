9baca422db3f154d22317bc1ae1dc157bf86352f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:37:37 +0000

    power worth better better word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
