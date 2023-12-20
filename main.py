b320d8efa861c6f9677cf623cb2473bda4d58350
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:11 +0000

    rush well deliver plenty business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
