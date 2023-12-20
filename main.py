87a24229b9179d7bfa199c555322e1c3f77a1df7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:44 +0000

    takeover spot trust smile behind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
