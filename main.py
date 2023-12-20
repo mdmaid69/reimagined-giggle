bd8c0fb93b6a74f1c7f1ece881a4d10bee38be66
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:00 +0000

    cause 573entertainment less several tradition

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
