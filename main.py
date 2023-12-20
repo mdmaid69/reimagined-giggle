146e4a884b637aadc50f2484151a3709e0625cd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:33:48 +0000

    zero bar crime correct behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
