78256673650741fd6ff531064491bcd24fcfacff
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:18 +0000

    memory discussion round heart exchange

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
