760ffc837db8209e7759a8cbb61539823e74e20a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:36 +0000

    rest woman correct easy praise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
