eaf029350226fe919916059f8c1d3229ad47617b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:36:27 +0000

    attract challenge unlikely comein without

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
