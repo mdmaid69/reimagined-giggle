e6682f0a58249b1e944b240504917616f303d86f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:06:15 +0000

    shout offer otherwise while sit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
