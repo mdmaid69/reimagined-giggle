88224784089fc4e9b4b8746e049226cbb8d947f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:06 +0000

    neat doubt behind pair reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
