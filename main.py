4d80c6bc45dd46f13fc4aef4ba4cd3aaf40da2ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:03 +0000

    view happen emphasis culture reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def set_array_item(array, i, item):
+        array[i] = item
