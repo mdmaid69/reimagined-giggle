da9807858f67b3440c81551d3bf91a86fa0fbde8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:14 +0000

    generous shoulder pump Tt creature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
