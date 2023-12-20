31d1f9ef5bf55eaf485258d83ae7def57c5bed17
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:47 +0000

    visit acceptable hour family into

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
