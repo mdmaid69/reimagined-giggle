52e0b7419aca721d41aa8233357a91ebe7e2d303
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:03:04 +0000

    large reserve flow mean sensitive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-def calculate_density(mass, volume):
-        return mass / volume
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
