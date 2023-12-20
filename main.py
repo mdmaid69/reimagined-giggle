5a93309c59bb51782094efce06beaef4dc43a8e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:02 +0000

    dry hardly about embarrassed mind

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-for i in range(10): print(i)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
