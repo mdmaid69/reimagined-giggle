914f78dbefd57987ec5e96399c81d4b59134fa16
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:28 +0000

    even every feature thinkover calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
