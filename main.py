a8f76d1a195033150e01ebe5fb8267b62f56b702
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:06 +0000

    medium forever search discover spare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_hex(n):
+        return hex(n)
