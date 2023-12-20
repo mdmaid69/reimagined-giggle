e3cd8b2d5ae077f1b5eb52e88599cce48ae96f3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:04:21 +0000

    exaggerate make famous miss lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
