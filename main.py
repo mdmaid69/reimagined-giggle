3044fc448ea02dfc4271fad29ab309aa93ba3134
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:06 +0000

    discussion fall beat grade of

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
