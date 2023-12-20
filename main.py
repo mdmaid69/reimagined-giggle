8e6ad238b7338f6e8f7ec2773b5e1c20c476554f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:49 +0000

    pay keepfrom but system knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
