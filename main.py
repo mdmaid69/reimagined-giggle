5a7d7762c2327465d601e6b9f29161bc429c73f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:09 +0000

    OKorokay heavy failure explain plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+print(math.pi)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
