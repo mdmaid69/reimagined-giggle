eb500b09a9eb46ca5876697763ad48252c64f7f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:26 +0000

    picture good second development long

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def square_number(x):
+        return x**2
