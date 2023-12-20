2ea420134a598d27b60f9454b275cc5eb05d54ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:26 +0000

    complain acceptable angry use beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
