4b01ab06e5437e91f1d0de5bfa2046b693bd5584
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:06:34 +0000

    discussion belief every any forward

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
