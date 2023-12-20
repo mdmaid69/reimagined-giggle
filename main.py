83a6982482acd8ff9c418d4402d8080fb23eeaf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:39:38 +0000

    mark bin pen wet hello

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
