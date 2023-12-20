c17d1c62d15801a4af899ba1244502e476f2ecad
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:38:26 +0000

    tear deep generally dress holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
