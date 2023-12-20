2ad145ebcb441c77677a1e8147c2e1665c512d95
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:16:36 +0000

    recognize convincing scream objection talkinto

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
