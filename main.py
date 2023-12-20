91e09d9aa3df3573651d285abe5f242a302be56a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:09 +0000

    past point intend carry please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
