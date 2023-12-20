8d3b946b527a8894aa928a5b8e8d77ab5f576470
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:50:26 +0000

    ticket slight break space giveup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import random
+print(random.randint(0, 100))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
