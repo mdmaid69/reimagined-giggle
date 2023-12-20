6dc35a09380187bd23814e97dbace23cb8a31bc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:04 +0000

    bound move hurt right fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
