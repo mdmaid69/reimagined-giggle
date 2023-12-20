ccfdb0757b12b13340c11cfadbe606c344c6e99e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:54 +0000

    raw flat tape middle walk

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
