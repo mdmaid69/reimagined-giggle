223152dc70659e4637b5cbc15fed624ea70c51f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:10 +0000

    noone similar satisfied hurry cool

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
