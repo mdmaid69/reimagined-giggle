e137ab3be046fec2e45edb515a284dd3074141a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:18 +0000

    oil waste towards thought lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import os
+print(os.getcwd())
