bbff98975002c04009b9177c817e1994b333746c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:49 +0000

    spring holdup number pay please

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def find_union(list1, list2):
+        return set(list1) | set(list2)
