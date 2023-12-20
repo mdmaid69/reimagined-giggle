bf60790e933bbc2844f8e6012f0519872a8ce2f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:04:28 +0000

    breakup deliver total Dd computer

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_characters(sentence):
+        return len(sentence)
