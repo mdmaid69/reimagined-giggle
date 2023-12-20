5ae0311855a2db3da7628ae7488979d47543c03b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:12 +0000

    state play cover ashamed protection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
 import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
