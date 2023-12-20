2e63a829b9a61cf1d698d4e735bc5de2ec5767c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:16:27 +0000

    touch pressure advice question write

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_even(n):
-        return n % 2 == 0
+  def add_numbers(x, y):
+        return x + y
+def cube_number(x):
+        return x**3
