efbf7e767b8330658d37304e3f07bfcfb9b11a4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:51 +0000

    duty argument habit backwards crisis

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def convert_to_octal(n):
-        return oct(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_acceleration(speed, time):
+        return speed / time
