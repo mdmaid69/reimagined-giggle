7f9df1d4dcd2c60eaef2a837bf7fd81d6119882b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:06 +0000

    vanish anywayanyhow mine move somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
