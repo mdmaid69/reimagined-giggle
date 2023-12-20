b5d6f845e9bd48a2af1dadd48fbb1a821172bc0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:21 +0000

    new cool life history constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
