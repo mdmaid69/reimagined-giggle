72653520bbd05b6031efb54494dc186779b0ed67
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:54:13 +0000

    score side bath favourite crowded

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
