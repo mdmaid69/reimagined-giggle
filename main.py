3249f1afa7270c9f1044512fd0b5e6fd98de7ae2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:26:34 +0000

    load this wake lucky until

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+  def count_elements(lst):
+        return len(lst)
+  def convert_to_binary(n):
+        return bin(n)
