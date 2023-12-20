5b310192cbcbdfe3999b4b71a82a071f8c2ed9c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:03:22 +0000

    yours terrible sore table bank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
