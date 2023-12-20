cb8d639c0bd1fec0628269f9e38f3541f958acd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:03 +0000

    possible identify heart task record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
