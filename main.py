330d07a9cbe8d8cf9ec3872bd51f9e1caabfa265
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:45:04 +0000

    though hollow introduction ordinary stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
