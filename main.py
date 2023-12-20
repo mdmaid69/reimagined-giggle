7ebe4fc74eae76371bd1c99fd1249e1a4751777f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:32 +0000

    whistle straight wealth disapprove unusual

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def reverse_list(lst):
+        return lst[::-1]
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
