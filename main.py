8b191108b3c61598217614fdf5a1508ce13934ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:09 +0000

    needle empty before under building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+text = "Hello, world!"
+print("Characters:", len(text))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
