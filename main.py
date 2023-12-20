aa0a40b2f40184662d4514cd7721c7fe993dec7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:58 +0000

    juice flow discover provide handle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
