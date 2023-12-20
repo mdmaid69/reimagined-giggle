d928f388151fb1211349e0eea0ceaaa9b2047cbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:27 +0000

    behaviour problem order ignore frightened

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
