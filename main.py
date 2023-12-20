8b1e43282223eeabc1c8b61b472a97568e60c28f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:37 +0000

    different high flavour deserve any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
