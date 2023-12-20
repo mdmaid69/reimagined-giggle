64eb98b9cc1a3bd0764f16a116633daebefbbfda
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:04 +0000

    appearance complaint rush account challenge

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_item(array, i):
+        return array[i]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
