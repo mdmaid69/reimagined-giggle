a3a049e4900df24cbaea9ccd4010c1b5f5b49b16
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:46 +0000

    various hit amount month finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
