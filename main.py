636044dbc5d7f660811756eaaef58b51f79d5d94
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:01:44 +0000

    wave opposite half order door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_repr(array):
+        return repr(array)
