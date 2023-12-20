1e915bef712b17764d34293494787cab42441482
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:11 +0000

    would illness seriously wide breakin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-for i in range(5):
-        print(i)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
