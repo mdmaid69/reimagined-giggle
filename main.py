4fa31083fc9ccd5c7d2822c994801f49af046532
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:36 +0000

    narrow platform dull health fortunately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+x = 10
+y = 20
+print("Sum:", x + y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
