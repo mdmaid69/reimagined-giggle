beedeff4d938c6959087e256e0501a3462ab1874
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:39 +0000

    pronounce short surface keepoff wherever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
