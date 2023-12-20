e6c884aecf257603565d855d80eccb7788930e0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:54 +0000

    storm with interested 565end smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def divide_numbers(x, y):
-        return x / y
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
