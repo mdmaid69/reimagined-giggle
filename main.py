df90eba966f83eedeb21cb89b86ee3a4075923d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:18:24 +0000

    tonight range female scream which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def count_words(sentence):
-        return len(sentence.split())
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def reverse_array(array):
+        array.reverse()
