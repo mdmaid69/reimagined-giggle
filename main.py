bfe6b01176afa9e3997c51cf590dae1717e84592
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:18 +0000

    lot young fence both height

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+name = "Python"
+print("Hello,", name)
