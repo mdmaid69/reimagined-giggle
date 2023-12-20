2db449853ef3ebe5551acaa6614477a0c526aa96
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:01 +0000

    talkback swing ticket replace manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def remove_from_array(array, item):
+        array.remove(item)
