ec650086487429acca32924311d60fbf4fde1539
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:26 +0000

    afford description refuse interview destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
