ab277294057e6aa5beae15276a797907716fadb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:10 +0000

    objection will miss off luck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
