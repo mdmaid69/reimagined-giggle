4934fe841ac062dc8f621a2c060a80d69eae0771
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:30:58 +0000

    smile paper attitude begin jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
