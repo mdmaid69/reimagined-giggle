68ae785cd1c30f37667a73adb6c44c8836c828e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:06:47 +0000

    limit round region comeout bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-print(os.getcwd())
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
