6d9a1c4a121d28942a57886bf5c22bba856066bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:50 +0000

    tradition accident figure criticize invite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
