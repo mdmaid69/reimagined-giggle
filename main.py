067f3b3a9f0131955f4b1fcf7ec3897aaab09918
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:53 +0000

    now would politics pass teach

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
