132e3176b8ff23a4cff806d93af4cf3534998a35
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:04:29 +0000

    wheel gate health concern emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-print(math.pi)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
