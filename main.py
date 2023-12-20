b3850b9079467f8098f9fe238713bd65743ddd81
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:33:08 +0000

    never occur their together congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
