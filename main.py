7138fcae0d5d798cf8af8530653f1cb47082a04d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:09 +0000

    blame commit demonstrate long time

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
