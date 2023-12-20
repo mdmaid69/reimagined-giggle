f52f3cc0baf79e525baa6f2e93356c9b8cf4ae99
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:30:35 +0000

    sting band hit development target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import time
-print(time.time())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
