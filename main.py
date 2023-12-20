f9d7e0d416dbf9af3c072b8b571cd944887d3994
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:58 +0000

    keepfrom accept concerned legal fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
 import array
 def get_array_as_complex(array):
         return complex(array[0])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
