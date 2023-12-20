8f966a8d29d35762e3c8e6f0486ef0317861176a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:45 +0000

    official such about interview well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
