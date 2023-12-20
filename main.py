76c8a8d9f3720fd7e0b4e884f0e0175c80008119
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:33 +0000

    forever any holiday seat sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
