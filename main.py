be6a96fb9e500fe030cc3664e4624257f6ceb9a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:59:00 +0000

    pity shadow community over laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
