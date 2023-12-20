5daba63e31d09e2799abed09360ffbb667ce4cd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:02 +0000

    work lock bye touch either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
