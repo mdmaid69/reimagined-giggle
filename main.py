95b33573cdeecad91aec9cd3326ba16f541a0aff
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:44 +0000

    bridge stamp night view underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
