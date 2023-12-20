fbc1a3fa74e62d7df8f086e645876c21cb6e44c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:34:58 +0000

    fault lookafter justice upset whatever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_string_from_array(array):
+        return array.tobytes()
