53d1a64e66f415140d1b06a931da1d199436bb2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:40 +0000

    represent definite design duty experiment

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def delete_file(file_name):
+        os.remove(file_name)
