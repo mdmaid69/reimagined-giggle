1abcf58015f9cf0e6e84a491157e7daecf9dfdf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:33 +0000

    mean modern soil place all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
