3d240dab5f0471ee7f456ea2afab38411d2c1283
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:53 +0000

    unless minority provide back make

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import tensorflow as tf
+print(tf.__version__)
+import tensorflow as tf
+print(tf.__version__)
