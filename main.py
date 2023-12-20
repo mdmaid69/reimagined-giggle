00c34b1edfad9710d5d32b0d3bbf29b890d01169
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:30 +0000

    whichever bottom among shop tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+def get_today_date():
+        return datetime.date.today()
