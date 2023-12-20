05f0ec87e298be465b7df932ee466104a134b252
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:55:50 +0000

    lean pump bush possibly well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
