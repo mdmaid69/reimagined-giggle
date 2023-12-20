28dc94db721338b9a7f42b535bd2170c688dfc27
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:09:58 +0000

    will grow takeover murder cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
