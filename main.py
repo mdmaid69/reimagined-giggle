2bca54b2f7b95d9921eb05e746a196d104808d2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:04 +0000

    callback arrive pronounce scheme cover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
