b81d9014641ec54e47b7b45924156e7024fbea02
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:43 +0000

    impatient now close sure suggest

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-print(datetime.datetime.now())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
