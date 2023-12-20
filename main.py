6eb9df12edd4d4ce3c24e823d1d70d428dec6815
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:45 +0000

    announce sing spoil passage patient

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
