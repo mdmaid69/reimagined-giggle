2c9da2db6eee79ec68e37c55f93ef08b26e885bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:29:09 +0000

    rise tool guilty remark inform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
 import collections
 def create_priority_queue():
         return collections.deque()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
