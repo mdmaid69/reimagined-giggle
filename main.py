22546ebff8972a7db3a50d59365a0b1615862585
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:38 +0000

    spread look feel snow please

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
