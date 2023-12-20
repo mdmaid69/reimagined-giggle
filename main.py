3d740dcbb422638185ac9dc142de9aac201864e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:21 +0000

    appearance case heat depend who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
