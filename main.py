6ca95248675ec31557d19ed279663b7aa6721422
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:02:08 +0000

    top blind prize drive spoil

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_list_from_array(array):
+        return array.tolist()
