c5ad7f6dc7ec16ed3b616ed2e956dcdb5a35e21b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:56:29 +0000

    present Uu world nowhere feeling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
