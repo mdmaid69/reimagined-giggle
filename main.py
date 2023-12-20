a1f9e370b67f5a338a63425a85d5557fe76af2c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:25 +0000

    community plan gap length study

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
