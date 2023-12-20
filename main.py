7449ec1293cec02b787701b6a47e374ebc08c4d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:36 +0000

    wave listen earth himself Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_rectangle(l, w):
-        return l * w
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
