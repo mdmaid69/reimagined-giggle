68b3ec6440c0f1360a55f04a1a6b84506047ec1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:07 +0000

    excited like little part minimum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_error_function(x):
+        return math.erf(x)
