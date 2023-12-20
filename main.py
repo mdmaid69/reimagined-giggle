50dd0238dfe9d314db7063ee8519b0ffde35f7b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:49:31 +0000

    total treat company accident contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def greet(name):
+        print(f"Hello, {name}!")
