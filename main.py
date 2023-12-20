b72d9e85024155bff50fee584213fd42fe5c7ea5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:54:58 +0000

    comeout spare colour long turnup

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def is_prime(n):
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+                if n % i == 0:
+                return False
         return True
-def calculate_force(mass, acceleration):
-        return mass * acceleration
