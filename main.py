d66a0f45a13382d8080daa0d3fdbe0627a628419
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:25:41 +0000

    nature regional Gg fix event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
