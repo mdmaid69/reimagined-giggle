5e92794e60459a4eb50255f3ebd0d11733e48314
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:26:06 +0000

    level increase discover mistake interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def add_numbers(x, y):
+        return x + y
