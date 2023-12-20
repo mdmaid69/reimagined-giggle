1bf2e73ff4a6a5a55ded5a5c375740fb0e8e96c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:33 +0000

    upset 570enjoyable giveup Itself slice

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(5):
-        print(i)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
