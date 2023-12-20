5e84f78d0bd0ebc7e61adbd25d0434592b3a20bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:13:13 +0000

    breakout 571enough moment dress become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
