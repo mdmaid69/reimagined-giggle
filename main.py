61d5cf8ed059f153e812b14f810e5e107adba72e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:41:32 +0000

    thinkover mysterious traffic degree long

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import sys
+print(sys.version)
