faab12e57f9eee469bdea80b78455eb5ccd825ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:41:56 +0000

    question second separate cruel church

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
