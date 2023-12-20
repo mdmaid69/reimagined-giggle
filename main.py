39a04b9a15bef4e2c38a7cc835396fb79ca18163
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:25:49 +0000

    in suit pretty injury tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
