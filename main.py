213117207a2acba5b912364b9caa1fbd01bd920c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:49 +0000

    discuss plan goaway store may

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
