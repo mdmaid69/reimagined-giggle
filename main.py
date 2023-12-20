cc8e84882fb0d8797eea084f0e8f146e2ae1a2fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:46 +0000

    control name niggaacademy film address

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_acceleration(speed, time):
+        return speed / time
