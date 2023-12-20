eb5aff5bf2793b670995ee46bbdaa6e00a6f2099
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:12 +0000

    answer relative simple quarter means

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
+def sort_list(lst):
+        return sorted(lst)
