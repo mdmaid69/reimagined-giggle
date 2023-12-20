b0cc8890873a6e9a08f514860c4627155607eb04
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:17 +0000

    hit board label side character

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
