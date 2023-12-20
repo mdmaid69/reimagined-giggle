c4ba37835359a64637a9d0765207508961c359d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:51 +0000

    anything black surprised recommend group

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sys
+print(sys.version)
