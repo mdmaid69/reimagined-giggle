6624bad15ab3d627e92a10d0af513ed1d2be6b35
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:49 +0000

    replace so word quarter whenever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def convert_to_hex(n):
+        return hex(n)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
