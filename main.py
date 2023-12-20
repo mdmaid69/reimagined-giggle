dbd9ced102924bdcd2e30730c828d10a022f0012
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:53:26 +0000

    purpose Yy noisy order history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
