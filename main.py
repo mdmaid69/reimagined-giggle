a6ac62c16d168d56f46528e9032df089c58d1214
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:18 +0000

    exciting female before yourself hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
