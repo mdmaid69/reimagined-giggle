90f63a0f5c4896daf323ac382598495f114e604d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:12:48 +0000

    shrink black welcome term since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_volume(length, width, height):
+        return length * width * height
