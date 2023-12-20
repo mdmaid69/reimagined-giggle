b7f917677d742d2eba8a3ed5b6b29ad510012d06
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:06:45 +0000

    continue breakfast over cap danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_os_info():
+        return platform.uname()
