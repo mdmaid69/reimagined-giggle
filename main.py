eaccd9cd5b5b794f5779eb97b4482d0e35aa440c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:35:37 +0000

    hospital charge face rest compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
