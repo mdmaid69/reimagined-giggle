a45ff6e49304d652c74116aea0679256b641f40f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:02:31 +0000

    travel trap between near temper

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def remove_duplicates(lst):
-        return list(set(lst))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+print("Hello, world!")
