aa92b414736ea8efefcd56c6a29ad65bdbfe735b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:31 +0000

    fortunately bad steady experience risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
