538366e0da22090eca4e6867ecce6cc868abdcf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:03 +0000

    trick common print zero whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
