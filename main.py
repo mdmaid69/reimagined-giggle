a42366ce81cfda80259a895e088b163365c99a42
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:40 +0000

    insist female keepout provide goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import collections
-def create_counter():
-        return collections.Counter()
+def convert_to_hex(n):
+        return hex(n)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
