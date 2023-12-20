055bdd97e86f7d00a7120a93715e0cd88486586d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:10:08 +0000

    hang news scrape young tall

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
