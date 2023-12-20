57a7454ab431a297f53ce7155d435c88cfde530b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:42:07 +0000

    carry settle comeoff responsible common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
