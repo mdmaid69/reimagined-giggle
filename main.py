2435bbf6961535c6bae387fc5e0b1cc85361780e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:48:15 +0000

    like clear chase crazy bin

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
