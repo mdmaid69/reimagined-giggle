e6b5768e53c5c2e390cab071a2eee5b3c0e4182e
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:18 +0000

    spring mind sad sound window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item(array, i):
-        return array[i]
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
