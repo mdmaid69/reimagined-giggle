94f4e1e8dbdc455a66b819d31c9e7a2492557ad7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:45 +0000

    point breath mark programme worth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import itertools
 print(list(itertools.permutations([1, 2, 3])))
