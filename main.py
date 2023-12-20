95a617cdf6da836fc40acb181cbfb3df194014b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:23 +0000

    work trap pattern memory if

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-print(os.getcwd())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
