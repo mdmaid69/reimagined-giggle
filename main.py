705cf3c5af485b6582adfe6698954fc89f811f6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:02:34 +0000

    develop beside interview accordingto afraid

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def is_even(n):
+        return n % 2 == 0
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
