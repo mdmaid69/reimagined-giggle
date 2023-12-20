16b523b1eafa70b2e9731af95ba1ea6df69a9c3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:05 +0000

    head ability otherwise offend instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
