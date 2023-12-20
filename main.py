c8f2ffccd15201243285f173afecbd6090e3d1b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:50 +0000

    deep provide unhappy eye stick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
