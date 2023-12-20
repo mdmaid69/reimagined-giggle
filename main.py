f2ce98960df1d7d087f9f0df7f60b58b35a88b1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:37 +0000

    condition office simple glass become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
