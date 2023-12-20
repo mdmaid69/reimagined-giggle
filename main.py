4989c5fa31fc6a03849887808f08024a30cc7aa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:45 +0000

    worried himself wake result fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
