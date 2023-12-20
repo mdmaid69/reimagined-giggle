f8d36ec2f082438c6172fc2e337dfe3c11fc6897
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:30:34 +0000

    form correct importance once turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
