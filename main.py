919770d2026d6996583b857a8151e94d29bcdb5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:06:42 +0000

    address thirsty imagination world side

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-for i in range(10): print(i)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
