ac5b9d955e0cd20c0326d9d900e5ea5724340d60
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:49 +0000

    pin declaration describe suit fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def convert_to_binary(n):
+        return bin(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
