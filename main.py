d46e1cad5e0af20f3fd607d471367eef12a91e22
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:45 +0000

    mouth shadow certain destroy condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
