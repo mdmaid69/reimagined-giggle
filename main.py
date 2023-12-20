196704b804469a0941c9a9781ce886f4627403ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:04:47 +0000

    food peace manner dig reach

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+name = "Python"
+print("Hello,", name)
