0e33546522f53a1cf684a87f8eb0a872d305e286
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:35:39 +0000

    confusing forever group ordinary careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
