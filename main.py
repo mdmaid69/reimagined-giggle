a58fbe68165265338453e6b9b59eef608dd5601d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:57:24 +0000

    brain avoid whisper high mention

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_sine(x):
+        return math.sin(x)
