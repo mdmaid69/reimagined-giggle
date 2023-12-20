9657c2351138c362bbc181a262c531664513df04
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:11:20 +0000

    keep doubt suit 571enough it

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
