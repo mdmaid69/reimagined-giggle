99d7a48b1125277742d9ec8a570300fa4fd5672f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:04 +0000

    unusual to freedom test treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
