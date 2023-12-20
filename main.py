9422088cb82d799c9c4acf1a452c960e75a7fb52
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:53:25 +0000

    switch relief goback(to) crime try

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_current_directory():
+        return os.getcwd()
