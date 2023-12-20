fee7f72586a6c5feba12c4b7f9dfb9a3fe1a4c2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:07:37 +0000

    cost responsible receive spin could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
