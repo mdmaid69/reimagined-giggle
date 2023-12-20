5f48928a8dc1d779896debccd30cdee81da2ff24
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:29:02 +0000

    strike personal treat help Ee

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def is_palindrome(s):
-        return s == s[::-1]
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
