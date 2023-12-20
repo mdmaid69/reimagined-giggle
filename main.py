cd5c195d9f85b7e0c6a26c4c00a17018f8352568
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:08 +0000

    quick interview offence window occasional

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+def reverse_string(s):
+        return s[::-1]
