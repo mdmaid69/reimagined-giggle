128e8c7960ab44672f0a99a77b6a66b0211fb53c
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:21:51 +0000

    rely spoil sting front different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
