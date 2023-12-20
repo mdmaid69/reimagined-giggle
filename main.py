ccc3bd1e0140d8f00d819ce6c08d17df5d2cc832
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:23 +0000

    goup above old order shop

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
