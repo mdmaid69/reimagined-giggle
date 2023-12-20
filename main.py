f39b0b1ca21e5012f11480a46d7300f7addf99c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:03 +0000

    boring whatever onto nice hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
