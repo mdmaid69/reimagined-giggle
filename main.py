338bc5f833db8e931d55cd3b5626077539edc3be
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:06 +0000

    copy join length actual building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
