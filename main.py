93eedca3ded17b87711c89d93513bd14bf30915c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:11 +0000

    support hit runinto bringup line

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+print([x**2 for x in range(10)])
+text = "Hello, world!"
+print("Words:", len(text.split()))
