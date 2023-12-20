b83b309e7d5a2a9d06e8f7949040e770bc16f64c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:27 +0000

    bring win anyone and clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
