97a866be417436c60ed758effa1fc07e3e9302ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:07 +0000

    expression approval part mark several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_string_from_array(array):
+        return array.tobytes()
