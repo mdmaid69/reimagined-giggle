61d7bad62a8c6d15efbf1b8f19a9f4a1fe286ecd
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:01 +0000

    boat tap cruel display shoulder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
