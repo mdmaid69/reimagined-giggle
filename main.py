0336a54437b116c70e1a936c7b4422d71e59a780
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:19:16 +0000

    bury me future pleasure front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
