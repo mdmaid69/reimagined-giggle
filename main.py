1544f02eab121fb07ea1c9b70aeea44651c56913
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:31 +0000

    wide main evening takeaway wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
