948e063b777612f3516e92ee3aa646e7d8c9eb7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:40 +0000

    ignore unlikely pleasant thinkof reaction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_stack():
+        return collections.deque()
