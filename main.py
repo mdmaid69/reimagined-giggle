b3ee77a398ed5dbfbaf9a40071862e1f219c0170
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:31 +0000

    appearance receive close goodbye plain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
