b77aaa658e905ed588165b3ecc4540b33b2c7d68
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:06 +0000

    any laugh opposite far deep

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def remove_duplicates(lst):
+        return list(set(lst))
