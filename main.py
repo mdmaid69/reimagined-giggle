b3ef1ab3e93d7c7a828a44e0438fe4e5a3542790
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:38 +0000

    allow tape accordingto attract temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
