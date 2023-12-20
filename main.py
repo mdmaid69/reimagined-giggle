9a81f07964e8becb4d08773e8f54e38efcd7d808
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:38 +0000

    contain tragedy stay contact breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
