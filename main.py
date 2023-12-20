5aa900e643b05912e770cabad9fd49f4a228f1de
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:08:56 +0000

    convincing language theirs mainly fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
