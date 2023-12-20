3cd2760d3cefc23799da886cd5ca4ae8d987f028
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:21 +0000

    fast power trust weigh each

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
