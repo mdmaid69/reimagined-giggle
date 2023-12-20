446f9f9c6cd06fab91db6a2a6030148b1af8d9ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:22 +0000

    pleased opportunity some last share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_current_working_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
