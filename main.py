fda737cf622a6056f5699ab66eb901ce4d1e9398
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:45 +0000

    return television delicate pipe exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
