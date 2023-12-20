33cbf860df3e3537b5cdd3f0df96e2a96606b019
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:04 +0000

    flavour for now sport during

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
