34ec5bfb6107c34d95963f8f50af92e0c323cd97
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:57:21 +0000

    rule computer treat force region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
