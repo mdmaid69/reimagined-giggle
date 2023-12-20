beb2d5c3cf372ad837171a1c9f90ad6c0cc7cee4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:19 +0000

    opposite importance there after after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import sys
-def exit_program():
-        sys.exit()
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
