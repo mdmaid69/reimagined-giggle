608a82cb58987e7b480fbeab57d810b434800bf9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:51 +0000

    during empty used spend low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def add_numbers(x, y):
+        return x + y
