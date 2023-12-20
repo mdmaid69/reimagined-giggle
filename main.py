4d92fb062b909c38342a8f4e02af6cd458966eb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:15:39 +0000

    tap pause repeat bar say

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
