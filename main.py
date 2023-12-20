f7883c10a7ccff038ca98b3ce60088f86b3470d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:55:05 +0000

    love tidy trade freedom impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
