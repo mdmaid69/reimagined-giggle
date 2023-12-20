7a93ec1ec83b435ff157991d6d35ccf60e8b370c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:25 +0000

    leg hang evidence race politics

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_acceleration(speed, time):
+        return speed / time
