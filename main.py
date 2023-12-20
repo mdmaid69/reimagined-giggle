daabd0f679bed2c1dac0ce1f4b3d21e287446315
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:16:59 +0000

    furniture wing future sad cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_speed(distance, time):
+        return distance / time
