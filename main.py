fb87e32bcc782bbb4da2bd92eb6156713203d1af
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:52 +0000

    declare praise opposite hurry excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
