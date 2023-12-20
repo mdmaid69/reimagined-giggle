ee01ea880b9a6ddec0074ba7ba6b9600b031c99e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:45 +0000

    wealth talkinto early either brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
