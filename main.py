2f27caac68a322963f1cab53678f3262fbcc60ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:06:15 +0000

    over one team really moment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
