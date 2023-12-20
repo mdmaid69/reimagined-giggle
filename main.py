1c180067f86901deb42ec0561bdde343025bd17c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:30 +0000

    dear hold means love live

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
