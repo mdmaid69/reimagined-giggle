48f64079a6f5e5e5a14ce610ec8a52518910ae66
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:32 +0000

    result tidy crash obtain sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
