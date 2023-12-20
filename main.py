04e33874b9f1d90a5bfdd2a0ecfa19524224aec9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:42 +0000

    someone recognize public wrap clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
