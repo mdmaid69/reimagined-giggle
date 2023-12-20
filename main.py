2eb32b8ee634c2dfd8f2f407e1a8e3c96d05c507
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:32 +0000

    lost keepout keen at Tt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
