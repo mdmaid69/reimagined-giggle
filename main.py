291c11aa8f27ea747ed33d7f66bfc8891c3e86b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:36:47 +0000

    book charge dead behave lookaround

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
