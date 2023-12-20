369fd412cb6e55e68a7e5eacaf042e58422c45bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:56:46 +0000

    miss mark frame face giveback

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import sys
-  def get_python_version():
-        return sys.version
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
