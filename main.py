b4dc9e1f02ebf1896c690c2087e3a0819cb3af6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:01:23 +0000

    might message say custom crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def find_max(numbers):
+        return max(numbers)
