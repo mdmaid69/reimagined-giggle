6b8d3f739f9aac9e460c09dca7574b7975d71cd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:04 +0000

    custom bury trap skill quiet

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
