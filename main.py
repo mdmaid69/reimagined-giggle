f7a10e008e7f7efab4bae19838b65885c633421c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:55:51 +0000

    hide shut dear memory attach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import collections
+def create_priority_queue():
+        return collections.deque()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
