c97f36e0f2970235d387b85be903d32785422f98
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:49 +0000

    quick popular juice otherwise stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
