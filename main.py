31a0912725701b5e9ddb90677d3015aaf9eb0803
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:19:22 +0000

    fortunate instead match preparation fetch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import json
+print(json.dumps({"name": "John", "age": 30}))
