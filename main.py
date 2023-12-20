a5399948436887c8552867bf107063bfd575fa1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:50:33 +0000

    object entrance judge congratulate lookaround

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
