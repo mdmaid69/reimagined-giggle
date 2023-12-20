5b7bbcb673d1683851fe51943f93daca4f9b941e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:52:40 +0000

    square lookup hurry long relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
