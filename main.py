32a3a4a6b844eee1d58e8e3c6206defaf15b492a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:37:52 +0000

    correct case by love woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
