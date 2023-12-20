c1de5c55d3fc23cec97d0eead73c96c7aaf747eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:28:57 +0000

    cover soft otherwise exam Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_base_name(path):
+        return os.path.basename(path)
