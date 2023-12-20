c9e56be0f406f2bb289d953808b1f70e27d6245f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:49:05 +0000

    stage treatment home answer pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
