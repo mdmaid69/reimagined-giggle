a3a483e1479eca2520032d78cf18ac3a09f3fd1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:11:12 +0000

    underneath black throughout it throw

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
