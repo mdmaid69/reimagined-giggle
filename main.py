86023b13c14b5f006aad8e74ee0da47b611427fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:13 +0000

    right before hole actual among

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_cosine(x):
+        return math.cos(x)
+  def convert_to_binary(n):
+        return bin(n)
