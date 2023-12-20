4a42f6caacf92ee1b388ac4255195aae2acb5df7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:27 +0000

    main run type comeup effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
