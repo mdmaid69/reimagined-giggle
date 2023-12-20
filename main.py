1920a14e1abff974d77d4bdc71724913a0a28b08
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:05 +0000

    broken congratulations great estimate forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
