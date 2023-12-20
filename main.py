092b5c7c2e75f89a5f32a7ac590a466f7c8a5cc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:15:18 +0000

    past band popular conclusion form

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def is_odd(n):
-        return n % 2 != 0
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
