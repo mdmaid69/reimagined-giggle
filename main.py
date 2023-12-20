27e2d879c2fc3db527030c3c47a21eb0bc92012a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:40:50 +0000

    set loss fat Ll day

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
