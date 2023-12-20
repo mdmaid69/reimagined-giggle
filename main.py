ea7fb0ec5fb98f7f9eeda0e133ec7fd4f2d5c57c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:19 +0000

    succeed strike immediately table now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def square_number(x):
-        return x**2
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_max(numbers):
+        return max(numbers)
