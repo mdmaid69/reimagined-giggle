6545ea2f69465901f72398a8d6ae518546eca4e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:28:22 +0000

    takeout fear neither world Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_itemsize(array):
+        return array.itemsize
