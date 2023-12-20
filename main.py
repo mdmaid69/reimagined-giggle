4e2b08b8aa9616e8b7e8010581684d628443dfc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:17:15 +0000

    learn union turn high yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
