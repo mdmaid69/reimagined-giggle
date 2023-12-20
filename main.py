6b9fce1bf7cfc07836822ed2624fb70bd893a7d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:38 +0000

    destroy beside perfect wind aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
