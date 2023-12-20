984b3b189b7d5abdc021c189f7a9f7793576cb21
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:49:01 +0000

    mean content family could clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
