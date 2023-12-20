388136c2463ead6ac82b5e17f5595167bb408b7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:22 +0000

    broad talk free model culture

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
