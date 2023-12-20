81310a8591820f15a5b6b2d964d3c7089290209d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:46 +0000

    else tragedy holiday question discuss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
