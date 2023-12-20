63749e4e864d07b4e1ec55544514cb32993fab07
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:20 +0000

    forward taste cook condition famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
