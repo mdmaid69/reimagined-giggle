64b983a611f0cb37d9cfe04b2273d593014472fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:30 +0000

    we mess Oo still morning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
