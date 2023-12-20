d71b0248823c20e42155f8de9f1cb094da1e5eee
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:59:59 +0000

    getout steer shall occur personal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
