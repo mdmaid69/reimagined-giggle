021d583db4b1e2100cd7e991b0e9f5ed9bf0ca64
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:10:57 +0000

    pot study activity imagination seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
