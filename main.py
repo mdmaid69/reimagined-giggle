cfff08171f5b06aee705d434cc9a9535388025f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:05 +0000

    neat plenty characteristic on along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import random
+print(random.randint(0, 100))
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
