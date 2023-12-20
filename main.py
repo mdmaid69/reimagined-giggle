3848fbd7dfea66a08e623f1e2599508d8e00ed03
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:40 +0000

    combine plenty flight measure again

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
