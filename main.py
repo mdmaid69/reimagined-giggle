84fb09d10498297d49601b8cce76c4f809c28344
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:26 +0000

    practice try bank blind go

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import logging
+def log_message(message):
+        logging.info(message)
+for i in range(5):
+        print(i)
