ce228811b773548ffd95d37f641e0227b42a4d28
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:19:56 +0000

    cheek yours bad calmly love

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def count_elements(lst):
+        return len(lst)
