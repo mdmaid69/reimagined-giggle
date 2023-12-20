2366ec30101277f2079dc320cce5d814f0dd36b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:58 +0000

    confident me goback(to) increase wish

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
