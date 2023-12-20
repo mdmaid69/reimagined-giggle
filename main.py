1b8252343809117ee6ed2480bbab0ba5b4d8873c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:32 +0000

    smell favourite alone news maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
