0d4207b3f65a435681c4bc209582ca92bdecb754
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:48 +0000

    even goup exaggerate service reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
