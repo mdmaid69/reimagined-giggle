189b9f4d6efdb5f0e02ebc9304deb682e7e4377f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:11 +0000

    search turnover stop beside petrol

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
