29b8c8b4b0fc3bcc612395939c490a5866db363a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:32:17 +0000

    concentration big school ill tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
