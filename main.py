dc7509a328d2e940772931355ae7fb2fe0609017
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:47 +0000

    travel waste bite takeover such

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
+  def convert_to_binary(n):
+        return bin(n)
