8fa76f2765c68cc75443cabd97c3da3ff2fe1db9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:18 +0000

    no interview Qq home now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
