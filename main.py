5fe1d2aebb52e62f9350e3ff3fd0d542c6d51102
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:00:29 +0000

    wrong avoid inquire divide surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def find_min(numbers):
+        return min(numbers)
