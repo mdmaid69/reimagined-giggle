1814c49f36cf29e11a92ff8f9a3de6680048c7ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:15 +0000

    smart complete before equipment subject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
