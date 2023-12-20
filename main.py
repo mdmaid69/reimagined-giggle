fb70129c54d9baa1b011c5251f33a591210707f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:51 +0000

    square interruption will they rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
