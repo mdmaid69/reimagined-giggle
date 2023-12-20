e90e69408adb0da48da746638beb45316b598e30
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:35 +0000

    action set would roof sense

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
