d8a34d10950cb640b3a050dcce7ab086b083a769
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:42 +0000

    comment guess of completely replace

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
