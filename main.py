9e07b07dadb8e622d0499c81ec131f9474ff58b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:49:14 +0000

    crazy behave term left vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
