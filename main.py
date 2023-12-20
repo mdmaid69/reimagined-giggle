25384f80d3e3740590a4063ae482200ec0f4839b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:00:03 +0000

    inquire cheek influence demonstrate hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import os
 def get_file_size(filename):
         return os.path.getsize(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
