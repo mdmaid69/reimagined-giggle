31ad4769dec77ed1297f19c66728027ebb38ee82
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:54 +0000

    control sense splash its front

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
