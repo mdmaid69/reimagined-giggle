7f2998d1af20d13bdf7afa4dece04961c8215cdc
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:14:15 +0000

    own trap fire through maximum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
