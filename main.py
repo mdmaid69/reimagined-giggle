dfa3ba91aa6ed18762400bfe41a77ad5406ee96c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:45:46 +0000

    blame city takeback guide length

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_acceleration(speed, time):
+        return speed / time
