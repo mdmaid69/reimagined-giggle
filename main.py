ed559b46915b2e5a1f1e9d73c828ffe108702156
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:06 +0000

    sea from fresh tonight cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
