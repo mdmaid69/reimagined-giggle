478131eace1dc37241dc40aa6c044daaed1a4c1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:00 +0000

    hear behind demonstrate minimum hurt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
