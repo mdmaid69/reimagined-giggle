28f7adc49c0756b8bbc81ade89913c3863e3f2b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:33 +0000

    late subject copy drink steady

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def sort_numbers(numbers):
+        return sorted(numbers)
