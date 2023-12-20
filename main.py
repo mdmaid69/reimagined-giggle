5f6abc52e07417f653ebd34ce08aa7ef64dd08ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:00:09 +0000

    only win match complete bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
