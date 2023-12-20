df7209a53b873288827e137410f6f2fe2393e3ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:27:23 +0000

    still tap part estimate left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
