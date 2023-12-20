0ea5620c8e8f7472ec9bcfeb755f42df42b3deb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:42 +0000

    money 572enter pleasure afraid ground

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
