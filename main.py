831a88254582f6d52307a421a43c9f98413f5a3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:01 +0000

    greeting back personal runout ticket

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+def remove_directory(path):
+        os.rmdir(path)
