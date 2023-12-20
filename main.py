ea9a1f66bd921676c8567cbb7f7d25a2ab34467a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:35 +0000

    word tense noone fire block

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def add_numbers(x, y):
+        return x + y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
