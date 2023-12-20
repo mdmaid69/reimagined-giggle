eeb196066c1a0cedcdc4efbba09840aebcff63ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:17:33 +0000

    565end fortunately hair spill convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
