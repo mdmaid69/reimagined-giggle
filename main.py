e6d0f282c861cf46894a8952206e43d24d0c16d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:08 +0000

    chin complaint plenty estimate off

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
