151fd3037c57c9911713b04810e6f8c97033cd16
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:27:40 +0000

    future pile disturb appear try

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_area_circle(r):
+        return 3.14 * r**2
