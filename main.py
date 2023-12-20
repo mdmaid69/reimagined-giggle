9de1151e13fed4d0b36f570f10d9d9d77e196d9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:18 +0000

    runout Ee interfere basic strike

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
