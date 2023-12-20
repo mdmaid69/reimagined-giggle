ec96aaf77ae7c5cb7bc1c5b8d800cd0cc6291f2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:11 +0000

    phone century tax offer deep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def reverse_array(array):
+        array.reverse()
