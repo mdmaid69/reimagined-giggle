d6e35e7ef943d77e8e7b159a2c607634041becb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:15 +0000

    company charge design list mind

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
