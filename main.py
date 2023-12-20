46d54d57874f05aa9f2faee2a67af34fd4634097
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:26 +0000

    thing chance tight makeinto list

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_as_int(array):
+        return int(array[0])
