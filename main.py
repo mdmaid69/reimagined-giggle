2768fc9efdd89f91b6ce97c749da52e0a7641f89
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:14:06 +0000

    path pupil choose several sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
