73aa2ab82f08dabc8b985e197841762fc8663662
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:35 +0000

    thinkof far slide evidence tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
