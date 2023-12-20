558354f69002e26a6c1399fd7a9993e37f07352f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:57:06 +0000

    only perfect birth big powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def split_path(path):
+        return os.path.split(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
