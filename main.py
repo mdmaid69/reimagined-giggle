abb618cd1dc0acee040736cc02f2f90fbf7d4507
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:10:28 +0000

    related agreement callout bound official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
