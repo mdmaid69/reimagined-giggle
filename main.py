5458de4daf0c9c4a62636285c016fe46f07c0514
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:08:07 +0000

    shopping bridge pride better different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def sort_list(lst):
+        return sorted(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
