15d5ab5126266570bf68992eaa6305990f10813d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:15 +0000

    fair record world gradually grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def remove_duplicates(lst):
+        return list(set(lst))
