2e365774616adf8f02c705da72fbc753d5c517a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:25:48 +0000

    hot share mark fix visit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_min(numbers):
+        return min(numbers)
