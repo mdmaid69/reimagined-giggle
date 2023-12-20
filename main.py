3c5889e8670db585d5fa726cfaccb6bb7b377876
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:15 +0000

    concern man claim keepfrom meet

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
