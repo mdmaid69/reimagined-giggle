85a7ded61be6433b83f584916b4d2a852ea5fe29
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:28 +0000

    century profit reply weather memory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_odd(n):
+        return n % 2 != 0
