4d89ca9689fd5b37ab92de63daf82627c0f8c943
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:55 +0000

    unable children next conclusion inside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
