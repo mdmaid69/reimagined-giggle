6044e65c6cd5f8aa9657c42d4a6d0b8de95fd445
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:46 +0000

    uncomfortable hide clothes perform wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
