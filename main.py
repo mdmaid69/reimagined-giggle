5cabbc8f93937ed6960f161683397414ade2829b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:51 +0000

    school process confident almost perhaps

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def square_number(x):
-        return x**2
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
