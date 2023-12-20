6d682dd0079cb252e59f984763a9034bc11cf512
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:40 +0000

    scene average from kind understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
