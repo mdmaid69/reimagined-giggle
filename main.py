e06293dce20436888c35f62b3be4479c350890b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:54:12 +0000

    send admire spread combination weather

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
