81fda84114947d5febe9706330c3fe25c4dd4089
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:35 +0000

    expect yes return opinion crash

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
