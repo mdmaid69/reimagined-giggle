b74eb0977cd45e12d9fbd368482a7215cdc120ac
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:50 +0000

    trial apparently obtain order thick

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
