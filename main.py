a3fee9c5c3eae7a60d7e7f8e73a8f44d078a5676
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:20 +0000

    turnon never mark base top

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+print(re.match("h.*o", "hello world"))
