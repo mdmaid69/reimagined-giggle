0d0f93da91e023a5ed26a42a7f3102b043fc532b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:52 +0000

    behind bad shop pause turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
