f2adfdd795379f61f3eb132c1fc51868ab805742
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:46 +0000

    hospital belong proof steal kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_priority_queue():
+        return collections.deque()
