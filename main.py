bfb8c0ad8ac07b79e9a02bb981451e5197e45f66
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:32 +0000

    uncertain battery scheme chase equal

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
+for i in range(10): print(i)
 def calculate_annuity(payment, rate, time):
         return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
