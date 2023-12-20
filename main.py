b0be1e7a3a8ccb3aec7b8e76db925cb595b36e32
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:21 +0000

    praise contact themselves happen praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
