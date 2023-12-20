3386ceae6191a282413df5fee6f273b053fe850e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:45 +0000

    Yy kill person instant love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_speed(distance, time):
+        return distance / time
