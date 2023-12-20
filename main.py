72f5574928383e498d14803123de60d25d49e848
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:29 +0000

    bottom shame let part degree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
