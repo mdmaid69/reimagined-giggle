dd9a4fdc3bd71d719aa8154cf1ad4a70cc9320b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:49 +0000

    group benefit cheat push battery

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_queue():
-        return collections.deque()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
