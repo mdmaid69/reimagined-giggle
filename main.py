b4d6a2930417c514c7a4ed5c16986c624c6a6a11
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:11:38 +0000

    continue turn classroom slightly pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_pressure(force, area):
+        return force / area
