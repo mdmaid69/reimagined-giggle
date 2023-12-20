b110975b8a7131ddeb2d4935f2ae4f7f779f3eee
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:41 +0000

    window contribute pleasant strong more

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def is_even(n):
+        return n % 2 == 0
+import os
+print(os.getcwd())
