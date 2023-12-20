f468990cda67a71852d4d48b745ebaaf9f18ba1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:57:42 +0000

    could quit second collect character

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
