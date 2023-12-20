5884d569e61a815f9f34d385b7a551ee4cd874aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:28:20 +0000

    sad transparent combination high popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
