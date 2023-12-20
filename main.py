87f3d7fa0427293cadb08e989a9a4dc1e50b1186
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:04:47 +0000

    any fasten my believe nor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
