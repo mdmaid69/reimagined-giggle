e1bb76445860f28cf84022a4778d29b2f4aeb27b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:36:25 +0000

    opposite dance best excellent be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+print(os.getcwd())
