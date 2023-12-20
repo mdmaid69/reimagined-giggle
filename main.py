27d08efea79f6e5d565f08f260a5a8d5fae54faa
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:10 +0000

    fashion party branch colour punish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
