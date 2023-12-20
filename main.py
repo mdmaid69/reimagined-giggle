163c9f399c64b548fab2aa4f42764c271f28afe4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:23:54 +0000

    bored ask ordinary note street

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import sys
-def exit_program():
-        sys.exit()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
