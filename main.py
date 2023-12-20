845a5f017bfc43c7fb9100d16f18feff48d46884
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:54:55 +0000

    handle belief city nice exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
