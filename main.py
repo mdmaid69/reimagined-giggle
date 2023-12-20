9e6e9f2055a544ed01e3934582396ca2b64becce
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:13:14 +0000

    period identify feature interested either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
