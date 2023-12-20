79168f1f5e19abc51df927013e2fbb97b56c9909
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:38 +0000

    terrible wonder equal skin today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
