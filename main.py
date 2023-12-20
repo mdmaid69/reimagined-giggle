453f16feff6030f5d6ce70947f82f5708a24ee04
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:29:20 +0000

    giveout connection space in circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
 import array
-def get_array_as_set(array):
-        return set(array)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
