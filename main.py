da9a71af2c9819ba5826fe5e9cbc843677ed6604
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:23:53 +0000

    project hole bringup face learn

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
