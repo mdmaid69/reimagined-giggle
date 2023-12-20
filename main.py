d232cc9382d7756e205d794a8bd4feedcb133cdb
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:37 +0000

    base improve Rr care pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_user_string():
-        return collections.UserString()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
