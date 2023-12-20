2d87e1ef721398e8cb29f02ad29cf273419b6145
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:32 +0000

    spare face us free burst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def cube_number(x):
+        return x**3
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
