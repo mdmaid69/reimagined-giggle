5c53fdd539368c4454e23ba0973afc3372a4f6e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:19:56 +0000

    same fear judge on bet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
