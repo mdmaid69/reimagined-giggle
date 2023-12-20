dc54de4750c886ced3b40a9556e2f21e4d13deaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:25:20 +0000

    shoot keen complete modern artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
