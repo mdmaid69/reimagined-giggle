28cbc1b6d9c5207aa873feb8f539577c89ba2664
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:36 +0000

    present also exchange lift fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def find_min(lst):
+        return min(lst)
