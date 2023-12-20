dfaa37cdf9262fd4375e6dae8cb95dc962aa77ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:27:12 +0000

    emphasis theirs official any match

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
