4c254c7e611dda99ab5ec62a8c5da1d029f817cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:28:58 +0000

    special science suckme commit brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
