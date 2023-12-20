4b48f88b670a4a877fbd591358981f3aefb28998
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:50 +0000

    butter party fat ordinary name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+print("Hello, world!")
+import array
+def get_array_as_float(array):
+        return float(array[0])
