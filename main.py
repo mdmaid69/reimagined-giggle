416b4115f465d7779c6460e415bc8c80f42afb2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:17 +0000

    possibly translate shame minor couple

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
