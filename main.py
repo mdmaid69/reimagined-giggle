9dae387fa46016129b4a75c1635bf62064894d4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:22 +0000

    again play both people prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
