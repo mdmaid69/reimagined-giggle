17ea2266fd146231fc38a813e8fd5d320d9f012c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:55:40 +0000

    pleased dream pick contribute gift

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_density(mass, volume):
+        return mass / volume
