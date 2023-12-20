d9650d9452515200328ba0c3dde4a84c8d293686
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:34 +0000

    label wise finally child share

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
