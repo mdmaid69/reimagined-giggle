7fe9b7b3cf2255601b41cea3d8186041a6b098bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:01:52 +0000

    patience 570enjoyable characteristic hard honest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
