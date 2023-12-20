4acec29ebe56ed7bbaf42078176b4688cccb13ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:45 +0000

    every suit angry indeed that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
