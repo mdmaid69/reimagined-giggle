9d6b3480b98d91ef4547ade0a8bea571b6a0b3f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:46:06 +0000

    preparation everything wish disappointed after

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-print(sys.version)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import collections
+def create_user_list():
+        return collections.UserList()
