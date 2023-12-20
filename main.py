55b99addec7bfbf6b1c756c6a1f6e576f8d251f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:00 +0000

    probably habit different reduce occasion

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
