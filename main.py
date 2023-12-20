cb4a3843230fbabc424fc7a4deb56f1b706d3900
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:10:38 +0000

    top otherwise family information sure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_odd(n):
-        return n % 2 != 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
