d6dd795d50d8c5d40919db79b2b7dc357c0fd1c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:31 +0000

    possibility among smile land change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
