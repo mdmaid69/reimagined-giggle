5cc7d77dda43a94921b16c4fad64609e7a94eec3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:57 +0000

    ride upstairs judge rough crash

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-def is_odd(n):
-        return n % 2 != 0
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def set_array_item(array, i, item):
+        array[i] = item
