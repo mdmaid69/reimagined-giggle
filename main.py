a60a3ae9353b6ffe9ab61288fbcc4842db60e260
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:35 +0000

    but organize generally guilty key

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
