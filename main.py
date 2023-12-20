0b528a1a2a3f0a1d5771b581471d7cb0407f9925
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:59 +0000

    healthy generous guard field now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sys
+def print_python_version():
+        return sys.version
