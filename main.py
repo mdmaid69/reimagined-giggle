3f4923daeced7b4fcc2774466b1ee1d710dea8ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:54:11 +0000

    somehow exam hour choice married

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
