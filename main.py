90d897967e507f765e73315a83af921844fbbb64
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:23 +0000

    another software will actually deep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
