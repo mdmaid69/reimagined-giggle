8b8be8492f775661b75dbd887e4a33bca5488d2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:02:58 +0000

    below equipment tragedy spare split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
