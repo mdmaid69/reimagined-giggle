ef430f83f4ae6327a1bb74c88ad9ac7f5f347093
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:10 +0000

    butter pleased within pure cry

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_length(array):
+        return len(array)
