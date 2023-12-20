4ae74d8e1ea826882276910297e55b98ef3b9b44
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:08 +0000

    give under black lead promise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_int(array):
+        return int(array[0])
