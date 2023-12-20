07a17bae58eb6fb947467717181fc6d5ff3518ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:19 +0000

    lesson turninto warning market empty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+print(json.dumps({"name": "John", "age": 30}))
