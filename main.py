f823d3e87a71c2cd3052ae0269281e53ed023f9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:04 +0000

    pencil until pinch accommodation stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
