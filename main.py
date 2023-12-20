deb588c2dd570fdf180ed9e2f97860241bbac798
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:28 +0000

    explanation always 565end broken guess

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_circle(r):
+        return 3.14 * r**2
