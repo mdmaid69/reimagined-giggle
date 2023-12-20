d49b2d1199b9957a7da33f02e255c3e26a7028ec
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:33 +0000

    regret careful dry clearly agreement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
