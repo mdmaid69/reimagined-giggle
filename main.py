d670e4a4b5c814b65b55aa5f2561ffb1593145f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:00 +0000

    comfortable to announce healthy necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def add_numbers(a, b):
+        return a + b
