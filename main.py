a0af4d543e19b9b8ef2fb39124b5b63a2bc1a18a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:34:08 +0000

    limit border home permission machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
