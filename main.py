93d261df7c57bb1dd1fcf160df45ad259d42ae2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:11 +0000

    their suggest declare independent slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
