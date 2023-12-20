c7823158ef1215e6f62eb95eba45f317a7853cca
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:38:59 +0000

    least reply juice cool repair

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
