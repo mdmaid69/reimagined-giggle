07a1c3b084fd7086f8108333511fc05b275a7403
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:56:31 +0000

    contents fall waste uponon think

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
