0445fe5ec457bc2590b38937f014c132e1d2db2d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:11 +0000

    famous downstairs Nn firm sentence

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def reverse_list(lst):
+        return lst[::-1]
+  def remove_duplicates(lst):
+        return list(set(lst))
