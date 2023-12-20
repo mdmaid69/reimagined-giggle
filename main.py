bfbae7ff16dde1a0755a5f9b8038b31bd6870fe8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:53 +0000

    area Hh plate same inside

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
