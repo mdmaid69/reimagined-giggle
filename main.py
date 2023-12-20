48117d345121531e5dac5daf23a8298bd0d4ccc0
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:38:08 +0000

    disturb one hollow guilty statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
