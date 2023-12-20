6ae585a03f781f8d971a824f47487d0b91e6450e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:30:29 +0000

    answer ever task undo clever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
