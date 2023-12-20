b8c0c48393f0532bd608589aff76663e183fe360
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:08 +0000

    ability violent fast same proper

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+print(sum(range(10)))
+def convert_to_binary(n):
+        return bin(n)
