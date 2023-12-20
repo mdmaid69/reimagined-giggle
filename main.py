bedc11c49de0f54b27cd6eafce44aeaf1f2e4860
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:00:31 +0000

    present recovery clever rely luck

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
