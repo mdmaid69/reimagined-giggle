38fde9b23c5b4526e80de13b02fc756ca05eb0b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:00 +0000

    Ff her community distant patience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
