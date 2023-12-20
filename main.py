390122089c437d63c4ab1c328daa20a35d842a42
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:45 +0000

    great lesson book cross brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
