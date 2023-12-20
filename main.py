eeb76aa5711ea2f2c176aed08e3a686fb95f937a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:14 +0000

    lonely major protest damp zero

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
