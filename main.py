c74fcef2f1c5f2bea0e8fbc104ce7b34cea2891d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:24 +0000

    blame question bye suggestion shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def convert_to_octal(n):
+        return oct(n)
