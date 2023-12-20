3fbe67440820df06c73ff7adc7e8232807c7aa65
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:56:04 +0000

    continue hurry turninto reach create

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+print([x**2 for x in range(10)])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
