9fe13aa2efaaca56cc731ec0da1ea56b2e5c166a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:29 +0000

    sensitive embarrassing science surprised sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
