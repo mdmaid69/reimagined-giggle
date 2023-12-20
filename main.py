5f047e1d256d1d4993872c66407b4a49f10d08be
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:38 +0000

    mind bus spread treat before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
