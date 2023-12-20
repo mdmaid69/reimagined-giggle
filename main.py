af9a042a98d167333cac0d6b47d7ff9a66d4d795
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:57:29 +0000

    who save band themselves entrance

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
