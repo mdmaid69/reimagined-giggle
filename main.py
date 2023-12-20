878ba6dfb16c79216210ef8a8abaea0a94b487a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:08 +0000

    trouble bottom quality connection quarter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
