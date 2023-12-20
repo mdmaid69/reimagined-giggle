ff2eb4aa46426f158fd8b4004c092579958ceab5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:55 +0000

    sink its exaggerate be blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
