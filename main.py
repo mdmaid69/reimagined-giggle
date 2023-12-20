6ce7e593ae5a86b84a6c988ed26374d930acfb59
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:18 +0000

    could black store stamp onto

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
