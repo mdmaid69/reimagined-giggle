b8b5380928c8f3e47754ff14279f65e424fcf270
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:40 +0000

    history vote interest call fly

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+print("Hello, world!")
