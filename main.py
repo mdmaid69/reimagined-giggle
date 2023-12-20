41ef5b8f1ee0da7443d1f8dc152051a84d558447
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:24 +0000

    health food quantity turninto hide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
