cc9882d61959dd2cd9bd07a67be5daf49038f185
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:05:26 +0000

    famous quickly set treatment begin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_sine(x):
+        return math.sin(x)
