18da889c5a36dd9d2e07fd033373aea54a72f250
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:37 +0000

    sense dish step pretty convince

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(lst):
-        return min(lst)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
