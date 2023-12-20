ba726ffc08d387a9141d410deffffd8e34abf352
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:28 +0000

    able turnover tune date cough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import tensorflow as tf
+print(tf.__version__)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
