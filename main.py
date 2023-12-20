e45cba8385df6c6a610fd2ce8f7bebc49b7295ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:49 +0000

    language gentle recovery motion hit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
