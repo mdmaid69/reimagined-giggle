f9eaba7b095f09469c40553f6607193da49aac81
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:19 +0000

    mark breakthrough board slowly compete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
