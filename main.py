5506575f35aff5f07873ae07481cd6455fd851f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:59 +0000

    dance getback rich contrast takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
