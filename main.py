815ffa56fd103de8462d749811c68f294ab64299
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:11 +0000

    lead whistle brief breakup fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
