46f671d5928a95490a7c346f284572fdb08d152d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:25 +0000

    respect star judge accept lunch

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
