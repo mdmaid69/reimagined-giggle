541e277c45d6951de38f0e2d1a4ae4f841b346b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:41 +0000

    introduction expect church job yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
