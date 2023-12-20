55f5b3d1c4d13beb70a92424eabe153e66169285
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:20:55 +0000

    programme name worst patient loan

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
