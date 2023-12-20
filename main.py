2d32d85fb847551795f60c31310b14acbaeb0550
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:31 +0000

    comedown what agree Mm Uu

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
