45325f6bf2d62b901024538f23685eeb137c4003
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:40:56 +0000

    image another mouth bringback correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
