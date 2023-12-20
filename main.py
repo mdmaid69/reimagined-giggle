d0a7b441bfebdac18979aa6b4fd91fcc81ce7d6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:16 +0000

    home without waste police thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
