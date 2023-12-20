1a16526e4bf79f60291bbdb55bae7fd7f9cd9138
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:09 +0000

    unlikely miss direction disturbing comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+for i in range(10): print(i)
