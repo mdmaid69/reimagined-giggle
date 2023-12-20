b98b89258fb608006d7dd5fa888bf7ead2722838
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:13:02 +0000

    anything willing earth trust process

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def divide_numbers(x, y):
-        return x / y
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
