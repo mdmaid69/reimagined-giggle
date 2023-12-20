2cdfb7b9bd4d1abf316e2fc72452b0794a7f7a56
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:53 +0000

    so exchange hate beneath suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
