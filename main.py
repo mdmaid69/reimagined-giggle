a26aa13d90ed356a330e78d0fadb56c5af05633c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:12:11 +0000

    thinkover put love smooth serve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
