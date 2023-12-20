ceba1e5e5316b07057cca38e2e09844601e82e82
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:47:26 +0000

    rule stand payment Ff him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
