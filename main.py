edc606f7a992c4d4d0c14e028e1afe6c4429de6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:38 +0000

    slide undo regret think until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def reverse_array(array):
+        array.reverse()
