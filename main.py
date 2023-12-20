e8f8f0871e39863a06285669ffde62f6f82da489
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:04 +0000

    another favour stranger government soak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def sort_numbers(numbers):
+        return sorted(numbers)
