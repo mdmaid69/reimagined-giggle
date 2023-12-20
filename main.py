fd04f8b4eaffe7290c013d576e6c52aaa76fde6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:45 +0000

    full when exit extra imagine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_speed(distance, time):
+        return distance / time
