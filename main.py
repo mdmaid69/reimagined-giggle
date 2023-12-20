25e130f18a7ce8424dc299d91db90173044bafad
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:52 +0000

    region shut surprise runaway strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
