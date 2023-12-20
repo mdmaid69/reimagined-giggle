fb0b24c05226403e4636c4358904cf19d1fd9186
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:46:16 +0000

    between bin let water Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
