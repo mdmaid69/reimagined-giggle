e98817edabf5b7144f82f01340a479f65a495feb
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:47 +0000

    advanced make rule belong invite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
