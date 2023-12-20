064b52c593d37efc8426eaa410290d1404ca1e30
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:47 +0000

    his division please 567enemy whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def convert_to_hex(n):
-        return hex(n)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
