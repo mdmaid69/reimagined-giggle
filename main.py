ef10255116920974af4ff69e2695b159b393fea5
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:25 +0000

    suck play hole skirt persuade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
