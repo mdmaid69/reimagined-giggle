cf125e45ec8118de5f5590be05d5c8dc77e103c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:23:08 +0000

    although mystery dress food send

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
