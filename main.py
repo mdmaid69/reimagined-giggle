8ffea21ade6f670ea9ca4842cd549cf439b83dd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:14 +0000

    true children play production chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import os
+print(os.getcwd())
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
