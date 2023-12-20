9879958f6f43f82bb97c96fa90379b691b3d4af6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:50 +0000

    length save service wake scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
