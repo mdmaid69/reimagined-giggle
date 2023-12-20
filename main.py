209fb0139b3eb3662df4025571cdf9e26e7c910f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:03 +0000

    swim under yet regard control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
