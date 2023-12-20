57ff824ade99fafec7836f2a44fe4209f5172ca4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:53 +0000

    narrow deserve heat heat arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
