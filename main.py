72cf794541f5e71d0312e0d1a2912b3518d14665
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:15:23 +0000

    heavy tall guess empty weekend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
