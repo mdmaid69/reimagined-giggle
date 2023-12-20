990b85d2fcd1aec49aa3d462714b00527692b25b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:18 +0000

    win point foreign meal praise

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_circle(r):
+        return 3.14 * r**2
