17d2e3abc044f1c696c57eec8ecd945680af9fd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:44 +0000

    different mysterious surface raise obtain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
