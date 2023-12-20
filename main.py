fdba95cd0811a78fc70540b0f22eba7472d2ef03
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:34 +0000

    anywayanyhow attempt mad neither Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
