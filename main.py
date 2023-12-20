88d25029eb9cc84376c8c4f3d1d89d0f8644238e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:12 +0000

    prepare ball situation doubt truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
