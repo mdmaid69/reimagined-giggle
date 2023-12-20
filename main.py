0eec8ad54bc6282f247d668aff584f8ab60edaa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:45 +0000

    Ee false idea use pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
