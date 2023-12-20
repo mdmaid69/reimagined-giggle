5691a36208d66504abefb8c702f17089f0e5176c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:59 +0000

    exaggerate skin geton mine attention

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
