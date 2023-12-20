ac010366a71feb627a3c845f435294895660ea74
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:52:53 +0000

    plenty mark realize guess fierce

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
