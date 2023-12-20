1e0641115bcd31d0d34e312bfb339af24b2256e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:44 +0000

    force rough pleasant can bringback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
