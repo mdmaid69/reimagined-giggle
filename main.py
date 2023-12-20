6e085648564ad76861e7fec345ad5cd6bdfc48d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:21:26 +0000

    share dick guess whatever to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
