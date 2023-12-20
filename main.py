dd77c61c340d86587edb232818cb5713f7d650e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:26:19 +0000

    single neither sea lookafter memory

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
 import array
-def get_string_from_array(array):
+def get_bytes_from_array(array):
         return array.tobytes()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
