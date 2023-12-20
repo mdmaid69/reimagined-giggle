602d0e41216e05278f82e71e8f82a9b76a732023
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:16 +0000

    dull recovery behaviour interview load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_power(work, time):
+        return work / time
