65876be62c44f46f3d1af9d730fa27299bd51fc4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:11 +0000

    life breathe form collect phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_directory_name(path):
+        return os.path.dirname(path)
