26410e4fc6482f82492b193271f9823a0aacd7f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:45:13 +0000

    to drive way hurry right

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
