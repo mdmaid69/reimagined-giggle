560d953b5bcf8cdb372c8b3570d2c793ee3c73c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:21 +0000

    hard task reaction block dig

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
