31196e430b5386597e8c2ef7ed53628d2af762f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:48:33 +0000

    free us workout file criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
