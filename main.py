0e031fe930108a80ab64b407bfe327c383ab42b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:59:33 +0000

    giveout deliberate word poison fall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
