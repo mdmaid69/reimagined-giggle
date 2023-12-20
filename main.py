d6555c2a38a3784784542ab6a16c3e02de9eab4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:49 +0000

    basis afraid threaten business ought

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+for i in range(10): print(i)
