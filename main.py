bb78b9e2d83c326987742f410ec18d324ca6f01a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:29 +0000

    might brother slice whistle down

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
