7a74ab79904c8fabb4ae825e979a524b63a483c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:22 +0000

    design lift show send play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
