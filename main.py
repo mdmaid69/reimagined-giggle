a75c0d2e4de2db898709ffcce65436d945108229
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:34:09 +0000

    contain hospital beat thirsty university

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
