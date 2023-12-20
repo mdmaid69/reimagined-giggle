72d2ee237939fd1ef52cbc9b0ab1595fb6fc83c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:43 +0000

    noone stone warn date respect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-print(time.time())
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
