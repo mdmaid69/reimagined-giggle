ca924e63cabbd439c93c231bbe206c78d4238032
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:32 +0000

    wire song keepaway put mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
