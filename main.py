51936a3dfa347f3f40df35cf8ac9580df34fd847
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:07 +0000

    respect base measure success reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import datetime
+print(datetime.datetime.now())
+import collections
+def create_stack():
+        return collections.deque()
