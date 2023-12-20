da5d6d8ed1c9841315aa5c3d64d71f1a10baed09
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:44 +0000

    improvement breakin begin slice port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
