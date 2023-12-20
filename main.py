3d750f7be6ed4dd23ca1903f0b738a8e8e1bf5dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:54 +0000

    undo us his soil getup

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
