6b24676cba60ceb5ac2011f99c0024d26577e796
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:51 +0000

    ground perform phone upset sense

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import json
+print(json.dumps({"name": "John", "age": 30}))
