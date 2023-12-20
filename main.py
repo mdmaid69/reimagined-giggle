fd4ab0c552e90c40c0e37db9995259559bbcd631
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:11:34 +0000

    admit lock interruption position ground

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
