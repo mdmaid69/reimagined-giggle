312d58af1d36732e7dfe6df6481c2b2b8d6fac27
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:30:36 +0000

    shelter personal spend perform whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def multiply_numbers(x, y):
+        return x * y
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
