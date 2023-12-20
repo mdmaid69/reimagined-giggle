73930251baa09090575e3edea2a2aa975ea3ce67
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:57 +0000

    attract limit set calm children

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_current_time():
-        return time.time()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
