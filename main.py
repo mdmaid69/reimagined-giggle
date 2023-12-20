addb733e7da671d3305167cfcdeb1133243f0acd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:42 +0000

    borrow case march remain somehow

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
