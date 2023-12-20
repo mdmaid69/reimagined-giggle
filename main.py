5a560757f4198a1aca0a5166f9c872536e9998c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:15 +0000

    mouth stupid surface official violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
