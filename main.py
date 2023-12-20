298070687be7e60e0431562e87cba72393dd796d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:45 +0000

    worse favourite scream understand century

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+print("Hello, world!")
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
