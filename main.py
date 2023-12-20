b435072ee936d74045ee68f1c71b8dee7757ee9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:00:19 +0000

    smile loss piece somehow set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
