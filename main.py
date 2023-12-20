3cea5ba2b36fa96a814ddf7b010f14d86705ff27
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:10:23 +0000

    Rr suck organize couple quick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def clear_array(array):
+        array *= 0
