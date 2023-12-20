28665ce927fb378498fac78194585e1fcab17a94
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:34:49 +0000

    pleasure properly report pull near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
