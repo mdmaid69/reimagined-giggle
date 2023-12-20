3463044a8c8f218a6aa7f1995a7f08d69084ad3c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:09:53 +0000

    bored adjective considering ticket knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
