eb378cb5e03cc440f94668097e0d71bacc81ea79
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:58:44 +0000

    blow cost mystery area light

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
