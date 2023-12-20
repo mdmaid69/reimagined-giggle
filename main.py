2b9695ded72aaa85c33464f02ebceb0778ce1764
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:41 +0000

    breakthrough list polite scream replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
