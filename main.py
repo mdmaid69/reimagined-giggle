b69df0bad37a54b5d6b0d7ae2dd8abe16527c968
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:26:13 +0000

    also class fly direct mess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
