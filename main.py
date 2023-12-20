659811d1808985717b8b744fb8014066188840af
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:41 +0000

    reserve afternoon sticky report thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
