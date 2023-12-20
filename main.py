85a2a0dd03d55f729971368e8d5a2b86344e2266
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:46 +0000

    deliver whichever fasten against speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def square_number(x):
+        return x**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
