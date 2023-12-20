956d960c76fba80bba2055322650b520be90fb77
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:19 +0000

    trial matter tradition offer disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
