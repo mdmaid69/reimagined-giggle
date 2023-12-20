adde243c859e40c1a59954f3c58e50244f5c471c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:35 +0000

    start will compare region scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
