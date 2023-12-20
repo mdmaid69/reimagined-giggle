7b78e511f7610228726f32d8f8813a902abbe148
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:47:44 +0000

    drive bringup shallow from urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_max(lst):
-        return max(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
