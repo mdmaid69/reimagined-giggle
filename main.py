d9453a830424b1ec80c757ce66152cb0590274ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:09:05 +0000

    ordinary type best slowly space

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def subtract_numbers(x, y):
+        return x - y
