13e7a39cded7ee80cec335492fe4f17731c6d9ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:48:01 +0000

    ahead door well drawing concern

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def sort_list(lst):
-        return sorted(lst)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+print([x**2 for x in range(10)])
