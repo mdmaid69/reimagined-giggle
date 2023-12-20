abb5ed60667fe2507e08d6d717e1f5de959ab939
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:51 +0000

    substance turnoff bus find subject

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_power(work, time):
+        return work / time
+def find_min(numbers):
+        return min(numbers)
