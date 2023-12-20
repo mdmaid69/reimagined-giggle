cc2665bcee6428f5cea9dc8872126da4b47e082a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:23 +0000

    market thought alone they thing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
