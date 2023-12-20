ff1e74bf352a7da9f8302645d6ee63b54b6b3c6f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:29:20 +0000

    complicated direction unlucky even complaint

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
