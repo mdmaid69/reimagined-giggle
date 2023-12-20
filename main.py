41c7ff12ebeb78742a64965c0da9ca02bc62a3d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:37 +0000

    relax cough culture small black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
