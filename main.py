d594840a0a075e5a785deb09f710149862535f0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:47:20 +0000

    may bus old exciting he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import datetime
-print(datetime.datetime.now())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
