5835b1365e7c7c728dc60c2487a7943913fd6413
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:27 +0000

    kind deep too himself nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
