fda5a818c78b4efa65651330c240dc5468747776
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:37:34 +0000

    light finally bet wear terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
