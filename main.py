670b04f1101a7d518c1d33acde5cf2480f3af751
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:07 +0000

    profit nor model wear benefit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
