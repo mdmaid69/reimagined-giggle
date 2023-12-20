8a60ed1677020c18e6dd8d4733aae3ebf081bea2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:31 +0000

    design judge well soak hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
