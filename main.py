2856a9fb1a327cbfb339c3000a87b3545e9a4d9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:23 +0000

    disapprove eager very sure bridge

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def convert_to_hex(n):
+        return hex(n)
+print(sum(range(10)))
