0a9d6fabf687caa681b68bb6facbd450719f06f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:37:22 +0000

    advise error mainly smell command

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import collections
+def create_user_string():
+        return collections.UserString()
