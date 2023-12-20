240ad9b0fba99b61e09498dcce00da8899ec67ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:07:59 +0000

    refuse bread visit even free

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
