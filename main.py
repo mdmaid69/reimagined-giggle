cb8c006b1941ab5647e79514f46789c4d9b44ccb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:59 +0000

    extra patient cheap getout what

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
