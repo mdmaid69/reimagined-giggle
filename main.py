f5c366c3bf017ac1d6fcd5fbdb2362ab617143b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:43:16 +0000

    yours design weak respect fasten

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
