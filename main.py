79508cae4cdc960f5b74dae0410109d30e6958a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:08 +0000

    process mood error suckme slice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
