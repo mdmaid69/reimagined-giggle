284ebd2944c4303cc04ae16b3670f96fca82e16a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:48:35 +0000

    reduce poor present Pp range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
