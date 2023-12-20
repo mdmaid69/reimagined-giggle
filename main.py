7e9f2c17d5493feacbee74dad6e8259139a3e7b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:18:15 +0000

    under history high light used

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
