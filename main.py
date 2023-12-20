35e8c4e1c681ec750ad8d58ac92e5cbd913871a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:39:50 +0000

    equal list rely bill reason

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_current_directory():
+        return os.getcwd()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
