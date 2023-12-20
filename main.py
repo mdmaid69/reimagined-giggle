3e076048363f98456c99a3e30eafdeb7fb692a89
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:09 +0000

    often embarrassed store feeling combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
