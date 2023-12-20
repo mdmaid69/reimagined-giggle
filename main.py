2bb3507e7c596afa3499d03a149c85e2b78f9aa4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:45:01 +0000

    criticism choice exciting difficulty not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
