9e73c9ccfdacaa72e8017ff05677dc0bb1710320
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:54 +0000

    now recommend classroom once damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
