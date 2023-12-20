7b94073f31539324e4bfe83ad0db2078290661af
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:20 +0000

    turnoff attempt closed but clever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
+import sys
+def exit_program():
+        sys.exit()
