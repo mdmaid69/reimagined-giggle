29c3cbb572c2082c12f23c82ff8664540b43208e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:46 +0000

    event mix mend soon tired

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
