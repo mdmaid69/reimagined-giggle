a8514ae02ec4f2ae58cb9fdda8baa9b05064e18a
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:13 +0000

    feeling law gentle deliberate understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_unique_words(sentence):
-        return set(sentence.split())
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+def count_words(sentence):
+        return len(sentence.split())
