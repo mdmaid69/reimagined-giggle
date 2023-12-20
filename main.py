76b2b20fd9d6e3b9f4013a325b073471e94666c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:56 +0000

    destroy correct getup bringdown form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
