e7151678ac97967562b1c03dc491128aa28f5329
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:00 +0000

    care development clue coffee attempt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
