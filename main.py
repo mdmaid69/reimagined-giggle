25ade99e837528eaf61efa3c565626d4dfa06f2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:06:23 +0000

    roof seriously many sensible high

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
