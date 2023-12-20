0bf2b2cd6fd1a626a9595804e8dbae109a819bd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:41:57 +0000

    mean air disagree use lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
