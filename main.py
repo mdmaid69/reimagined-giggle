79bb680a700aeed45e7759f3dac3d3e7e51b53dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:56 +0000

    how borrow move model waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def multiply_numbers(x, y):
-        return x * y
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
