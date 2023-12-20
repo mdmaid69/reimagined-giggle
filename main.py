b998672e549ff145c7b3cfd5ea108e980c5b5792
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:56 +0000

    temper fear willing share apply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def delete_file(file_name):
+        os.remove(file_name)
