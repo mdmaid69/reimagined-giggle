fa19ed23ee884cc5caa9434b959c7816b3e52b24
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:41:25 +0000

    record agree action quality wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
