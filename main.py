b0c72f471ba1482d6f1d615969bc0587439455a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:51:39 +0000

    hollow difficulty conversation wave serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
