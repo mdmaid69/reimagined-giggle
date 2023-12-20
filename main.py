48822760a4d0b7c2c660bc8b00e55b33cde71bbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:04 +0000

    where company report tax meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
