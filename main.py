5dbaa5147bd49f6d39701cf2608f0d93555327c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:05:36 +0000

    stretch outside hole except 179bite

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def count_characters(sentence):
+        return len(sentence)
