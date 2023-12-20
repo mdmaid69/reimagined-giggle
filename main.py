2457682582f7878fcfbe739224c47236235fc20e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:27 +0000

    ready previous soft cover gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
