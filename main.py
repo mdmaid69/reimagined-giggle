4ff019b45adde7bb64abd876a1c13172184085b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:32:16 +0000

    insist poor pity protection eager

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
