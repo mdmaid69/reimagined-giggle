d47bdc8c5b14fcd42bae589d39d53e4cbb0ea921
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:56:39 +0000

    sleep clue eye block pollution

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
