67d44d7df833f209d2c705965566b2a0a35a4a40
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:57:23 +0000

    unless exploration successful more like

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
