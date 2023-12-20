0d843c42f3bdab403ffa743b6e86781e0d188d21
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:19 +0000

    correction relax decision plan design

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
