85331b3b3a938e771434f587f683af2d9a3d9710
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:12:08 +0000

    sound city peaceful active burn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
