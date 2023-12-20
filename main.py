63eb5fe0993fa5004b657d17eea35c694ea5ad04
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:12 +0000

    drawing well my pass worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
