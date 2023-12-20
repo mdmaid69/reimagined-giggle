892ae91bb4a35d4fefaae5fd02338ff845134ff3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:30 +0000

    break receive reduce load container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
