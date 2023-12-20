9ff23d4aae78479c64ad27f48f647501233b52a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:08:00 +0000

    grow straight concentration 564encourage happen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_speed(distance, time):
+        return distance / time
