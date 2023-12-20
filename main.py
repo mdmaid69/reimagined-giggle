9fcaca1ea22deb4f69c3269c166ad1204e363573
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:02 +0000

    responsible lunch same direct confident

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
