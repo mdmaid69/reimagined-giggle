0ba6193be1ee1afc33575ef1df6e81138929cbf9
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:31 +0000

    regret main sit hide age

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
