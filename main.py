82b0e4c6a89ca5292e203b4cca5bcc7516ec7e9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:42 +0000

    road rest Itself best wave

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
