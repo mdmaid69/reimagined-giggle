951c8a3ed9d570950583e08c2ee390405317fb4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:34:38 +0000

    relax thirsty recover pronunciation chance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
