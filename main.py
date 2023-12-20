08b672eee74f517480c94e2ae34485329e12d047
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:58 +0000

    cross vote before train community

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import sys
+def exit_program():
+        sys.exit()
