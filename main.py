a9979459b94cb9f73728d8cf6d785939aa55102d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:08 +0000

    his bright shop gas glass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def multiply_numbers(x, y):
-        return x * y
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
