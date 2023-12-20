701b66dd94359b27a122b5cc3cdc9b094b596225
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:40:27 +0000

    personal health do also distant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
