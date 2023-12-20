270976ca676c4eed6ead3ddddc0e8b7265596658
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:33:22 +0000

    emotion book raw label just

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
