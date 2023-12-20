024160be67905324c959bf7ed48a6a9facacccaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:03:31 +0000

    argue down that straight hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_set(array):
-        return set(array)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
