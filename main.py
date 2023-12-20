7af7a24b79cd5454c0d09330945bc157290ed3d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:14 +0000

    coffee difficult regularly port season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
