260dd1ce174b876080028ea567f3d2f434d0026f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:45 +0000

    wherever measure different slow hate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def count_characters(sentence):
+        return len(sentence)
