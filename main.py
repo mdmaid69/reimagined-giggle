6338b24d9896b64484b171b2e37d57dc7e73fb97
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:43:00 +0000

    confuse match die letter autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import os
-print(os.getcwd())
+def get_array_as_bytes(array):
+        return bytes(array)
