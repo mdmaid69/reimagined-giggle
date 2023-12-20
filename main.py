f0c7e42338c50c62bc4628db634704639aaffae5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:46 +0000

    bar runout confident probably joke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
