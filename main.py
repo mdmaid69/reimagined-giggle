250661ed762e81b051233fc8f0b8b5aa7bbe3cc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:44 +0000

    even giveout feed stick band

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
