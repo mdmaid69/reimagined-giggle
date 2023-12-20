2a36f67b41abeaf4f40e3bdf1e1de75e4d6048fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:30 +0000

    once uncertain music strength next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
