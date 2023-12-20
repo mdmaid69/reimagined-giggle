eced77ed0b260d9044008681ccca934bac072391
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:40:21 +0000

    regular term central future familiar

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
