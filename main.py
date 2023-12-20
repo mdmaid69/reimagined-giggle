c0d69f505d5712e6c51cc7c14e5efce16a608611
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:12 +0000

    fire regional depend precise purpose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
