5f0485707196a531ac622764001c36e6ba65c581
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:31 +0000

    sink ball replace break cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
