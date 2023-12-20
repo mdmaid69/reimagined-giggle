8f0595f64f5b7c85d82016aaa84895626e34d41a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:31 +0000

    people press meanwhile regret news

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
