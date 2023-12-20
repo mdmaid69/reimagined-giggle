49f125afd97a3f0f8d8967e835bdc43f61e16a87
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:30 +0000

    voice sink expensive thorough attempt

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
