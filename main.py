34771b2757a3e5bcd19675b77f783aa9a2feed1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:40:30 +0000

    work memory apparently high fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
