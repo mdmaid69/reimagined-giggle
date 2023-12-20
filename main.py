2e3da617fdb8e088277f8abaa14a57118f870b6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:46 +0000

    thorough plastic turnoff manage trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
