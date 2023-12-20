3f76530eb1d8d5b4ab1fea23fd0cfad2a5031a50
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:36:26 +0000

    pretty comeover swim suggest return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
