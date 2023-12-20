bedb98f225e812b284d41b2f5296c0e3f2439818
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:00:44 +0000

    regional none another hope tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def calculate_area_rectangle(l, w):
+        return l * w
