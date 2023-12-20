ca75e61b9d3f9fcf0ffcff958f041207625b0128
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:07 +0000

    shock load treatment obtain add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_list(array):
+        return list(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
