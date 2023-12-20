87cb15e7b082b16cb2544cf421f4fcb29a88925b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:01:53 +0000

    contribute fear imagine abroad plenty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
