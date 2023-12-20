e14cee0c23bb679c550254f9c7652bc9acb0647e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:51:27 +0000

    rob best lookafter extremely fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
