df1aae2dd47150ba8f67dd779974df8e0e23cf0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:55:19 +0000

    play onto difficulty content pupil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
