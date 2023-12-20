e91b98a43540da2d1b6686a4dca6cb04eed07634
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:44 +0000

    defeat tradition perform cause against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
