fdb02348456b175d52c2ff367fa01462d761de6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:50 +0000

    war distant can illness spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import os
+def remove_directory(path):
+        os.rmdir(path)
