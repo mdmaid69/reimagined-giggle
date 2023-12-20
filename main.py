62e9b84d67bf7bac339641f1c749590041a53030
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:52:09 +0000

    concerning 564encourage sentence part female

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print("Hello, world!")
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
