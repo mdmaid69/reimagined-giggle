d0e95ef4399169e6a421e586dd45efdfd2f7efeb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:52:34 +0000

    free surprising runover 568energy advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
