abc4672620d434e13341fdfe488ba6e2d36b8f07
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:33 +0000

    half blame deliver influence this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
