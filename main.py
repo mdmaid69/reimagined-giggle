637909c2fbcf1a6a42fcd22795eea7eb8bc2b407
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:18:39 +0000

    dangerous turndown behave another pull

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
