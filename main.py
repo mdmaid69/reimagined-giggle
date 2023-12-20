8dba6d4daab6d91234a58e59182d20cc5fe7e552
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:55:13 +0000

    gather hand thankyou when tune

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
