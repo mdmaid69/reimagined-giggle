e6561a0251111ca0623b9142f8574bad26b9d98e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:07 +0000

    thin tour language art only

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_item(array, i):
+        return array[i]
