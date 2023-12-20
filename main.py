a93eada12acf7c9ba66bbdce0c9e9c55e9647ad8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:01:17 +0000

    pressure ahead pressure health some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
