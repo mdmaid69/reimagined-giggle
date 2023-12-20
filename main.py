c974becfce06c853739cbf2ea4f4a533a1901144
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:33:42 +0000

    doubt rub prove interview imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
