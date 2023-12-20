6d98bd1a90590799d210ea868fb4097355940233
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:24 +0000

    best height short emphasis write

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
