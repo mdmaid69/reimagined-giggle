956cac72606df4ac110f95b088acc697e707d95d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:13:39 +0000

    false collection left until touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
