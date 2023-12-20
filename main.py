fa7aff43cc9d5f8962e5f4869e774e2723a6faa0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:58 +0000

    turndown famous arm previous church

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import sys
+def exit_program():
+        sys.exit()
