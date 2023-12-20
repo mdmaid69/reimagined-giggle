d74ba3ced17a11c51d9df66a241bda2d34ab3ec7
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:29 +0000

    bringforward giveout direction warn tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
