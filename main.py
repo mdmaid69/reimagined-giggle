9ad930ac8c4cb5a79661dcfadc199471ec5aff3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:42:23 +0000

    organize regret exploration case curious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
