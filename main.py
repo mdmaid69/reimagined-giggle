8295ff2055180cb002716d1b6343f6c18a8244e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:07:29 +0000

    considering mad act whoever admire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
