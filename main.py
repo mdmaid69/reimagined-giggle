f3adaf8b478295c9b275c71b794afccc190583c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:20:14 +0000

    weekend issue track through comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
