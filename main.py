e0928cc0a2f866378427988cd7f1580fb4cb28c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:42:10 +0000

    trust limit proud repeat wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
