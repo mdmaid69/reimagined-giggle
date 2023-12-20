5367a88c7fe3ad1c742a008c4f677fe641a1b5f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:03:51 +0000

    future muscle getback gas tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
