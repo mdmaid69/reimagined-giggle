c834d3de40831d99bd6428c52f310ec83455cf0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:53 +0000

    sink wash their suddenly callout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
