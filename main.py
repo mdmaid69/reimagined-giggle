334497a2dfdcb712c54e34f638f18f450be4a826
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:34 +0000

    do debt choice shrink already

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
