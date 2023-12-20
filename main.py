84e6998d429f26452ad7c50878af2e9d22d4263f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:21 +0000

    below basically inform within priority

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
