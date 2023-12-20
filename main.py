a5fd8831939167fc242bf46cda207c36c39b7368
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:52:15 +0000

    bus improvement occasion refer turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
