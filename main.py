c4906ca7a8c0a9cb6e8173b7037691dcf572de88
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:52 +0000

    chase morning climb rare regard

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
