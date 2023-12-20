f6a574dfa998ceb0e6c0eb7114518db4c235d6b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:14 +0000

    any under avoid friendly rarely

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print(sum(range(10)))
+x = 10
+y = 20
+print("Sum:", x + y)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
