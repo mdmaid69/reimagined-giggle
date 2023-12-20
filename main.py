ac39c6f9e3dc0a272bdb59f80baf258788479d47
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:32:39 +0000

    nearly attract pipe runinto reject

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
