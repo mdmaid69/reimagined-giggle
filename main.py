22ea8a9575fbeb1167f975cceef7db9109b926d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:11:20 +0000

    shine freedom believe ride disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import time
+def get_time_since_epoch():
+        return time.time()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
