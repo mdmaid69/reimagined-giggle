a1d706f1c7355ffd18911a93570531aaf8097425
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:12:17 +0000

    back equal excuse impression nothing

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
