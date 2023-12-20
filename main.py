d83e0ff4e463c151bc0c9bb7168b0ef3cf1bb4b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:11:41 +0000

    do likely influence survive bang

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
