9c9415af2a650d7faa63f8c662dbbd53c1f65de9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:46 +0000

    quick needle defend church shelter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
