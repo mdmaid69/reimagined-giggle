53f41968bf333c49a3f85dfe976a9ecd80a8ec1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:55 +0000

    Jj boring crowd harm sensible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
