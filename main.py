6fc797a2faa476a595fbf59a8f6c442f4e7bfa46
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:28:04 +0000

    admire capital stamp comfort discuss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import sys
-  def get_python_version():
-        return sys.version
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def square_number(x):
+        return x**2
