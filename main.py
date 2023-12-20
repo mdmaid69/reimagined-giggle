3cb3fdfea7127f85e39b51beaf3a2c5fa69e7894
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:10:16 +0000

    drop store defend needle yet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_hex(n):
+        return hex(n)
+import datetime
+print(datetime.datetime.now())
