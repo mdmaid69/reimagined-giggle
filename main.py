d58da0728fdff9e3e50d873012709eaefd04fd20
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:47:14 +0000

    fetch colour himself person maybe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
