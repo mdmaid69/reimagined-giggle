568959e1b0a14fd05a3adb9b361c15076d981e7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:20:18 +0000

    flow deliberate spring forever giveback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
