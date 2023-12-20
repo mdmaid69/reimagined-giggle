46e6ffbeff027f8df53094f704296ddd23903394
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:46 +0000

    approve disgusting poor priority shade

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
