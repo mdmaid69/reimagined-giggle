412b545bd30d476bd50d925da82106118d30cc5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:19 +0000

    since best shop excellent unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def is_palindrome(s):
+        return s == s[::-1]
