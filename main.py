a7dec715ccd75ea5faa9114b589c842d562e5c39
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:45 +0000

    trousers check challenge powerful mystery

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def square_number(x):
-        return x**2
+for i in range(10): print(i)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
