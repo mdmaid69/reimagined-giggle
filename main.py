38dcd001e3544fe4d037fe7941d559840303ed2b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:17:24 +0000

    Mm complete later holdon splash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
