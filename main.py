75431a9dd5faae13e388e610d82b551d1316839b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:01 +0000

    contrast else now properly equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+from collections import Counter
+print(Counter("hello world"))
