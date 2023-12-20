0bf13fb85c75387d1a19fac3e81b252285531a04
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:30 +0000

    few buy know even mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
