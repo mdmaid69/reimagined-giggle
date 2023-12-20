2bfe7809b5da3b08f5183479859df4ee89143b39
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:03:54 +0000

    secret recognize separate history nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
