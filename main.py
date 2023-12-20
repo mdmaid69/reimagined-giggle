f3ed5a5b26bb4ec3ec5541680322b90ebdbf1e95
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:45:09 +0000

    congratulations sight unfortunate good burst

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def is_even(n):
+        return n % 2 == 0
