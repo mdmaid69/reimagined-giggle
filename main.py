22fd4b124c782de3b5eea23c84fad2ceb81c854c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:14 +0000

    close adjective confused rent miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def roll_die():
+        return random.randint(1, 6)
