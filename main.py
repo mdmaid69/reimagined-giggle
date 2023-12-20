963aca5ca3d2b3167afb86c88e59a1dc74acfcd1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:55:10 +0000

    appearance tongue raw part interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
