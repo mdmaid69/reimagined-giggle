c96923f740d3d32d92f9bac6d5bf3ab77397246d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:29 +0000

    pressure outside runover breakfast until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def count_words(sentence):
+        return len(sentence.split())
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
