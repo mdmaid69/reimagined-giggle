8bf6d1d35e2959120fedb20d9a0f1a6925d631fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:13:28 +0000

    rub before near phone sail

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
