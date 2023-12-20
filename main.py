d465e51305e40ccf3f1a0f4a5f2078cbc60c2091
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:16:59 +0000

    steal offence other ilovedicks hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
