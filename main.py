a96d4a4e05a6b0362b7866b5102323b396a2a8a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:55:24 +0000

    tomorrow suddenly hold amount formal

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def subtract_numbers(x, y):
-        return x - y
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
