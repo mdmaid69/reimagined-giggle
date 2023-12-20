3fe0b02800366b6073573c25d1468f940b943839
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:39:17 +0000

    load car bet poor burn

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
