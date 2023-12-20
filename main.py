a823f4b8608c1e8e897a56ccc07866de1a6a57f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:39 +0000

    butter apart apparently design excited

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
 import time
-print(time.time())
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
