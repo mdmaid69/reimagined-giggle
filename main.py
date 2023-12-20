85a327050cd6fee8f0c7fd68b20b89ea4f208c0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:59 +0000

    rent remain almost report loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
