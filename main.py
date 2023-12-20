7c2e9e31a5a085e5e7ecf16dcd6d0af3c2596ca3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:39:41 +0000

    cover switch disgusting shout jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
