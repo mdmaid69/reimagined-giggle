9ecdf4487999795b9e3bd6af35be0043b405dc1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:56 +0000

    press birth crowded pretend plate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+def calculate_power(work, time):
+        return work / time
