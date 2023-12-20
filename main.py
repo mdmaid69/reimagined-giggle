4a0d98152e5f52928f7abf999d707182dcc76c00
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:57:43 +0000

    government mess house listenfor flavour

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
