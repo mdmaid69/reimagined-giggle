991b3ef1464728100d26a9ccace77bfca0f28d3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:36:00 +0000

    slip simple ought city kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
