0f1829218f6f489a66d4e5b0c982dab987cc39fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:05 +0000

    wish crash wear thorough passage

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
