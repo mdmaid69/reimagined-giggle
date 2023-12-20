78ece553ee11267e4943cfe99e97009d636fdc80
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:49 +0000

    situation eager without keepfrom birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
