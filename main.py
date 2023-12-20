d2d0a14ecb8d55b10acfebf9643cde74ea806229
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:15:20 +0000

    band thing there part fast

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+print(os.getcwd())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
