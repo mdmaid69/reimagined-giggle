bf428ce0ae91ae65d53fb0b05346931b9d559459
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:35:47 +0000

    shame legal afford your pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
