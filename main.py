404638a376b5e1a6dd0be18cb5bfa32d824a38fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:05:41 +0000

    turninto false now tip beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
