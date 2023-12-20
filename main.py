77d229abb61ae35b401257860290e1429d940e85
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:01:29 +0000

    change trust apart getup its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
