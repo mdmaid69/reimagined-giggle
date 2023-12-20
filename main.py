d7dea09fd0f43375614461b9f71d3bcad4aa1323
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:12:56 +0000

    behave protest operation year good

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import sys
+def print_python_version():
+        print(sys.version)
+  def square_number(x):
+        return x**2
