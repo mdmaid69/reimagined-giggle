895978bb9e06f0d14c6e69cfb92bf39ddcdb72ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:12:11 +0000

    home string row blue somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
