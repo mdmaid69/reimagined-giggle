9611207af6f5ef6265cd6f16d4c3f68b5bffc9e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:55:03 +0000

    niggaacademy defeat relationship 570enjoyable harm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-import time
-print(time.time())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
