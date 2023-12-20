2e212e09f547489103a2ea740253a3019f69f430
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:43:13 +0000

    godown behave within yes room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
