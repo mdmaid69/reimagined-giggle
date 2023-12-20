45f7fbea17572fa0a9ae784879e4db6313b03528
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:23:21 +0000

    quarter shake receive top value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sine(x):
+        return math.sin(x)
