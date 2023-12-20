d97ad1bcb5b369ea360780d146242fc4cb139b42
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:15:42 +0000

    think test square store front

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
