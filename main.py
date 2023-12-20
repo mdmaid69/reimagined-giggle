c75e3b1149d42ce6aa101ba95b594083ac1b3433
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:36:36 +0000

    relative circumstance hollow you tour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def subtract_numbers(x, y):
-        return x - y
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
