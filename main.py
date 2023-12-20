664bcf7f57b4bb2ec2bd33f16008753e97eb033e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:29 +0000

    programme once use runover like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
