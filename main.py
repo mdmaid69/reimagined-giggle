1215037f37eab9197b782d7c0cad0797c3211f08
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:42:12 +0000

    foot comment 569enjoy come confidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def split_path(path):
-        return os.path.split(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
