08138039f79ad2e678e45659a7e24c1d46672aaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:29:54 +0000

    over way project female attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
