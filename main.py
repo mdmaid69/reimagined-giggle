b5a6b15fe489ebc1ac3920398e3f7d5bc2eea5e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:08 +0000

    improvement knock payment year knowledge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_octal(n):
-        return oct(n)
+  def delete_file(file_name):
+        os.remove(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
