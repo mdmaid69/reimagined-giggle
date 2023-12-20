cd076355a5a020f144eb499b4cef631e0cfafa49
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:54:01 +0000

    tip although mark play workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def get_current_time():
+        return time.time()
