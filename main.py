2b746c6a14023761df26c4caae36ac67ce4d93cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:49:30 +0000

    early ashamed deep steep trap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
