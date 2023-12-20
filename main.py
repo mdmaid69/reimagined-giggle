13741d118db353ed0d40dbd7817ed272319faaee
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:44:20 +0000

    write dangerous season arrangement after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
