5b6dd4abce9222b570eb66357f1be66e7903225c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:54:41 +0000

    calm week promise telephone idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
