dcdfb3972af6ab55abb634fecc9f6aecf21fe8d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:35 +0000

    genuine achievement fine bag under

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
