c1cb01707343a405e6f1a412bb4091fbc40a8af6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:25 +0000

    gradually show stare ring defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
