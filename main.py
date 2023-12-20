2cf53e37a7da4eddf5b5591836914ca199542726
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:20 +0000

    skirt only fear pride hold

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
