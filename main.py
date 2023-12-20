93eafca8302a20e8ed7a57b5efdff075b6dfae77
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:29 +0000

    extra forget myself takeout collection

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
