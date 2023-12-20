5b563d1ff42aa2400b2d4e7111324ea3a35f18af
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:06:04 +0000

    disturbing pump circle operate treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
