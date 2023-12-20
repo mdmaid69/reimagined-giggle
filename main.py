d822d2a6e535f13eb2481a5b2f135bcfa72501f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:55 +0000

    reach suggest worried home achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
