f74028015c2dfa45c08ed27c7e6c98a14215fbbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:40:12 +0000

    raw understand health little you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
