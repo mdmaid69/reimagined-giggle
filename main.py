5d496f34b055f4551986238126b220815b1c825f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:50:35 +0000

    dress process mind swallow everybody

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+print(os.getcwd())
