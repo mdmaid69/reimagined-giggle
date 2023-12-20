27047f62deb6b21da9fd91a8b4af7486f0e69096
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:51 +0000

    judge fight load food bath

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
