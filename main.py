c0f62831f040d30b855b10f8475691aef1756525
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:48:23 +0000

    until contrast ill comeround after

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def add_numbers(x, y):
-        return x + y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
