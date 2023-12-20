3b1f96bc36abb5bad3d60e85805bad643ec798d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:45 +0000

    bet 572enter steam back married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
