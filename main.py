29992995ce2c4ab040f513aa36966c42e0004349
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:00:07 +0000

    ball geton hit heavy emphasis

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
