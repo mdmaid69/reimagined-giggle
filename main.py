44c401f8aba225ce9667f649446d09c98632bfbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:47 +0000

    fault help sticky switch damage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_pressure(force, area):
+        return force / area
+def convert_to_octal(n):
+        return oct(n)
