893b1d358773683493685c914f5c743e59e34535
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:57 +0000

    side work afterwards concerning tall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
