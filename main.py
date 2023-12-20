ce604487d3543be3a00528feb8c9209af9333a32
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:11:20 +0000

    group brave expect ask take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
