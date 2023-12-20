0d079a20c6e6b7a2b7b11ae1a8b7016fbf4d1f36
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:50:28 +0000

    child 567enemy scale every whistle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
