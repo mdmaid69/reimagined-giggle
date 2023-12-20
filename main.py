a11eb485adf48efaa3c67dc0bd35207fbfe98cf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:06 +0000

    private fear keen block problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
