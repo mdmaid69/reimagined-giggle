31b6c09300763e5ef5d9fe150a649e16d394dd48
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:30:20 +0000

    make exchange dress charge challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
