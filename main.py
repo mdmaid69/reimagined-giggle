3e74ecf8c5151310dfe8c2915eac0c6228bc0993
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:36:25 +0000

    where independent deep flat Bb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
