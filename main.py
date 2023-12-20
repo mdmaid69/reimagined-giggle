63a289bddbc2134da6ddaafc9832b51b82e8400d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:05 +0000

    computer rain hungry tomorrow careless

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
