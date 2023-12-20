93245e6dc1484a91f7bf3f851eefc2328a44f100
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:41 +0000

    sing gentle before television cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
