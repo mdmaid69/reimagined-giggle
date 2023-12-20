81884dd6192eae39d72b903201fdca3ed04ba484
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:23:35 +0000

    usual bringup crowd figure pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
