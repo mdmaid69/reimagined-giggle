609e4a9d1308efcf5bde9e50b76ca3bb05373547
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:52:56 +0000

    greeting demonstrate exist improve see

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
