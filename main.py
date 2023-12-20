456606af64f8cecbfbb3c0c051d5753477f7213b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:54 +0000

    eager course knowledge pure demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import os
+print(os.getcwd())
