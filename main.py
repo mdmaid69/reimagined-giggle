46f2bbe6522def720648d63f7f599dac241203cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:50:37 +0000

    over brain interruption brush old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import sys
+  def get_python_version():
+        return sys.version
