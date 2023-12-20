5d6cf1249c12c64efadc37e92b173b312e392905
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:35 +0000

    Zz balance drawing bringdown night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
