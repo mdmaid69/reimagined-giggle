f742f70429128a2a254483e9a153390c9bb886e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:01:27 +0000

    morning take bit far nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def divide_numbers(x, y):
+        return x / y
