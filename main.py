fd4ddbb3d6797628cca40a8144b9c45363b18da5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:08:50 +0000

    makeup world urgent split completely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def convert_to_binary(n):
+        return bin(n)
