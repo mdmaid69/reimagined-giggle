1df1f7796fb3df6417780f1ac50eeeda2f9972bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:02:59 +0000

    whoever number comeover measure thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
