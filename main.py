c86e1db7426ee84fc31d4be4a3714b683bddd1c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:26:10 +0000

    danger train hollow minute pretty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
