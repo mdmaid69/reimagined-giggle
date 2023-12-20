df397219c14bf1c1287427131fd639ecb5f1db7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:01 +0000

    office confuse question cry cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_pressure(force, area):
-        return force / area
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
