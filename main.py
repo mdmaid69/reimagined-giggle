83842301f0d5926a158592c214efb4cc7bc053b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:55 +0000

    number behave should frequent keep

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import time
-def get_time_since_epoch():
-        return time.time()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
