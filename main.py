8d928aa44b4a866a5c9b97faccc0a0f9e174f238
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:24:25 +0000

    complete software control measure gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
