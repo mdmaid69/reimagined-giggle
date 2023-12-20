9242d20b79dac0572e64bc373ca3a3990520184c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:08 +0000

    game sensible target transparent track

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def find_min(numbers):
+        return min(numbers)
