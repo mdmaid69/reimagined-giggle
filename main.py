f4a4b81028e92d6dfe3021584f908f3222f47b69
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:29 +0000

    second earn edge warm makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
