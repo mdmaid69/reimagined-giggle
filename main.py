d04a23ca62bb6beae03ea08e7da97e27620db7f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:27 +0000

    broad touch dig excited relative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
