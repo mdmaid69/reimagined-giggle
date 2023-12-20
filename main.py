87abe71d2fb5f0a286b14677fcfca7c51bff47fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:01:10 +0000

    admire drawing pattern market original

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
