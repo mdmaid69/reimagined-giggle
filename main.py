c2b1526214203e288dad737c1cb2ab85a6ea1dfa
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:37 +0000

    inquire necessary for callout building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import os
+print(os.getcwd())
