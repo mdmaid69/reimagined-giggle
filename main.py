8c4a096ddcc5550f58ca1029adea01aa922f5cdf
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:55:02 +0000

    process their strong alone let

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
