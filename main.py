1384465439370d177dde30e49b1c04e12bd43465
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:49 +0000

    demand compare pronounce badly shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
