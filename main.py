fc0f2eaece09b491db57a21f62711accb6936bd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:48 +0000

    busy impressive rubbish choice birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
