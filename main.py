2315a9bd81cb7d337d904c0dfbfaa169192367df
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:37:42 +0000

    constantly surprised answer mine punishment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
