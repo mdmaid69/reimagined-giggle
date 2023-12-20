f293e408eb7072c03717ef9645e81905fdf254b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:22 +0000

    crazy sticky full giveback in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
