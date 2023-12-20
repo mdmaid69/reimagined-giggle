89ca406ccb2968c590a61bcd978dbff60d97e14f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:40 +0000

    black rise proper when company

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
