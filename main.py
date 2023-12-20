61a8fe27d05323021ae3bbf6406bbae900703783
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:31 +0000

    basic favour market fill since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_floor(x):
+        return math.floor(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
