e9a6ae4084a409bb1026bb30ea02be17d54907a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:57:46 +0000

    left traffic them either consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
