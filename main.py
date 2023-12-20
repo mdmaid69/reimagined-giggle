c7ea0980f95166d2e08141bc5087be806efbb621
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:44:14 +0000

    turn train position embarrassed tend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
