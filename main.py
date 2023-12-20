c39202244feeb9a8d9f32d6951939ca69b1c7f99
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:21 +0000

    aim rest community scream confident

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def count_elements(lst):
+        return len(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
