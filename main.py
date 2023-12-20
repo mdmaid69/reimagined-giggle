4b0c70d6b742000e85dc4cc1503f5e77eba7f354
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:08 +0000

    advantage pair event it angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
