1d4f054b8f51405543f7f60aa9cc99ff3308cf00
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:22 +0000

    it praise stamp direct song

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def convert_array_to_list(array):
+        return array.tolist()
+import random
+print(random.randint(0, 100))
