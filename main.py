5ff09451c60cecd1479a9c20b4ae8fbebf2f8873
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:45 +0000

    tap sticky best bye like

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
