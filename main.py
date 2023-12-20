b20018b6996807f77bcc5f5e9f2ae9d29fec20ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:41:20 +0000

    again steam study maintain cool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+x = 10
+y = 20
+print("Sum:", x + y)
