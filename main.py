1a2079b0a9a8cdf6945f36fd09d0ac861f869ff9
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:12 +0000

    late how detail splash extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
