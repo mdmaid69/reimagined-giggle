2a16c80df045a2ef3f13c5c6b559e6db26b772b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:19 +0000

    catch up base style impressive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
