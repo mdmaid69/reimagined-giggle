d8fe238397093c580f20af7254494926eca972ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:30:55 +0000

    frighten error shock constantly storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def is_palindrome(s):
+        return s == s[::-1]
