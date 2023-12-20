0fbc33b5380d0c0d701e1eca77affd482762688d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:03:58 +0000

    godown ask grateful bar knowledge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
