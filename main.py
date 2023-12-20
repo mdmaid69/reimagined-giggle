792c1f79f6f77d056acd0452030317a827cf1b15
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:08:10 +0000

    runaway breakthrough process door tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
