ac9eeca52c3a73c7d73fe1de0596d62862993833
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:48:11 +0000

    arrange declare fire public check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
