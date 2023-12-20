07a322f9095d45c893145bbc3ef6896859123893
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:31:02 +0000

    treatment tie future rarely knock

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
