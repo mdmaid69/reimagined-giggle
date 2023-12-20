64c9e1ed0133626f868d767f7f477b58a751bda1
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:46 +0000

    thing woman behind press street

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
