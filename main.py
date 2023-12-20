ea88963ceabb403adc248512c155077b4f1d619d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:43 +0000

    area actually snow second complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
