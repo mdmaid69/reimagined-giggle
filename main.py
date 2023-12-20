7ae33b84b88cb59036db9681bc6d3ebbfb8c719e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:21 +0000

    narrow vote major switch defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
