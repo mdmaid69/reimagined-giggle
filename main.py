3bd8cffc3c095bac2b176d5931ab81b47db4aa80
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:29:14 +0000

    force claim keepup Cc trick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
