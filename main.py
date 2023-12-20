34872fae8a376222fd265d3b3f823ee20277caba
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:40 +0000

    or shop succeed last them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
