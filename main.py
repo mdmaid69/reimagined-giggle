c04c6b1d6edf91099f132e067bac0a76eda177ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:35 +0000

    every broad test ocean gift

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
