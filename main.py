d658c16c4d4c0e3aef45809c03af84d1b892c2d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:09 +0000

    back drawing wish neither snow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def multiply_numbers(x, y):
-        return x * y
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
