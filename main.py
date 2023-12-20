ddc2bb227194e42af53a39f9037d934c5a675b99
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:15:10 +0000

    through possible better fat workon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
