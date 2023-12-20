d3e2d4a6c0f4125a055b8a0b4473da7b089c348f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:54:38 +0000

    giveout right reaction space shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
