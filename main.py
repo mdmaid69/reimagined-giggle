f8cd16628360abd68cd85206654cded79162fa96
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:32:48 +0000

    bill discussion genuine pay tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
