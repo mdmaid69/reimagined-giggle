5168bd5b14f4459f0b3514cf9c44c853f809a39b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:08:11 +0000

    ring left hope must sound

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
