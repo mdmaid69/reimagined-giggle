9b1fe16ef9706212ab8f0df08741219c1b01628b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:44 +0000

    report somebody environment explain way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_power(work, time):
+        return work / time
