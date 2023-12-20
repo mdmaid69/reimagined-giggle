a64252bde0fa05e39e0ddff1eb5f2d121822051b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:12:14 +0000

    murder all tune favourite regularly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
