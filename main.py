2ac23b8bc3b828a21d22b078f887aee9ea42c40e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:50:48 +0000

    post anywhere cause often angry

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
