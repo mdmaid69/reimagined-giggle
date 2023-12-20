b0be666325d59d50df4ad936c105d307cf45b85b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:07 +0000

    unlikely hope divide length their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import sys
-  def get_python_version():
-        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
