67c37e781b01084835443f11ce800c65b05ea9bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:50 +0000

    closed traffic tour my project

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import platform
-def get_python_version():
-        return platform.python_version()
+  def split_path(path):
+        return os.path.split(path)
