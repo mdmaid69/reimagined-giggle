0493449361be8884d34d433502030f5ddc2f5022
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:18 +0000

    discuss wall left paper breath

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+print([x**2 for x in range(10)])
