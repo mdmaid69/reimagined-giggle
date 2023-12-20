f4884f3c43922754153aa57b704d342b1d33ad88
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:32:09 +0000

    sticky approve holdon spin suddenly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
