32f52f7cecf13d1ad9d7671a8c7038339717fb9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:42:40 +0000

    properly relief change break stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
