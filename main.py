c17791277bc6fd2331d201c33b15f8a7ba4ff2e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:03:55 +0000

    wait night direction near away

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-print(time.time())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
