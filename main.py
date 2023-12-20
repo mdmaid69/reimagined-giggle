d4e86a47461957e8c704ba0db76770f8039a54e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:26:51 +0000

    splash direct punishment method statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
