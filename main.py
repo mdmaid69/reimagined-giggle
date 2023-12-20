4b49043197f5b1957bd301cc2b9c7d4fe5ff32ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:04 +0000

    experience replace doubt young among

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def add_numbers(a, b):
+        return a + b
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
