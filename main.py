c35d9681b806ded2d835aeb12345ee460928de84
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:53:51 +0000

    spread add die OKorokay responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
