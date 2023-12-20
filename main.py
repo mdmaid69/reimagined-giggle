57f8ca82094f225ce0493bca27f22d12ac287ed8
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:45 +0000

    holiday introduce mad car run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
