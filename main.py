bdee2fdc0859f32373cec065f8a234d1cc921511
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:51 +0000

    certain mix back foreign long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
