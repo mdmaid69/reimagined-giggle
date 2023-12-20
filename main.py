5f997847a61f7eb7a6aef501926d3ba0a44934eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:17 +0000

    success instead stamp form show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
