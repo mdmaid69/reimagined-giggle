e9b2ab06a68466a0da56439fccbede95310be7d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:25 +0000

    pour method definite sense light

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.ctime()
