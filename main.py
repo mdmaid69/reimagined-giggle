c2495a6a7b26e8a29db047d85b98e35dca41d162
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:21:00 +0000

    more neither relative second limit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
