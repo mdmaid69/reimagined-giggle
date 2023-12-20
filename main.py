e7af5c576da9eb0bb6826259c51b5c0b86cc91eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:28:02 +0000

    similar manage bread door always

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
