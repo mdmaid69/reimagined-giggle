710d787fce5bee1f9044fa96889cb04dbc4c3346
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:41:42 +0000

    blame guard speech next tragedy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
