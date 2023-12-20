4f8ee95e18f33e95dc6094049f2fa2796a123a20
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:48:51 +0000

    ignore art welcome slightly complete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Characters:", len(text))
