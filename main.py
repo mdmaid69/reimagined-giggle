2c1e547f7cabd71e0940370f1af850a0789409b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:24:44 +0000

    turnup borrow sure behave record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
