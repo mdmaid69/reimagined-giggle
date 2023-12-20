97964d91ca3cf1575231b41cf0444a532461aa02
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:06 +0000

    make possible chief wash taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
