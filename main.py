f5d72b825e150c6b415ec41037b4187504678d7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:24 +0000

    arrangement light about name inform

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
