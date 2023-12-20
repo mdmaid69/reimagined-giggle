6e5240ca385af6d0aa7f8e1f8bbc6ff4bdeb2531
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:02 +0000

    water disapprove rule rank process

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
