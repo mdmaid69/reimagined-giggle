59ba252d4593c8794d5fa20b5f5c91efae94a0e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:15 +0000

    visit reasonable crash powerful usually

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
