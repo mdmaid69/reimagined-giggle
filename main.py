4fe3ed89fd30db55e1b8e592ce2241aca24c8435
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:57 +0000

    mouth black protection then content

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
