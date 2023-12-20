7865ab47d0e7803a66145e42052af999c038a84a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:28:52 +0000

    that tear connected anywhere else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
