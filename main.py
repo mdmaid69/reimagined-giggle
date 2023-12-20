542880e4c02ddb39d36b5c29488102bf66ad6a0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:46 +0000

    objection regard behaviour body trust

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
