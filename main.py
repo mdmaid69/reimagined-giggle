5a4eb47c23176fbd32123d9a42c9cb5f9cf9c5e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:03:16 +0000

    treat pronounce birthday search surface

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
