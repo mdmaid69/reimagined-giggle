cb81670744b5752e782e5827cf53a6add957ef7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:18 +0000

    must brother conclusion up turn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_min(lst):
+        return min(lst)
