d914a287d23546bad422b6aa817bddcf9f5155eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:07 +0000

    late public task far necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import getpass
+def get_username():
+        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
