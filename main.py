d0fb239352a2af79c4bb4235e022ee5a9e11d760
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:32 +0000

    kick side error worry late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
