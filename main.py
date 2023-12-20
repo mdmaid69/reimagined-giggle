0faee0fc36ca2ba70d75c9d92612220742da67e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:20 +0000

    else young mark accept whole

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_list(lst):
+        return lst[::-1]
