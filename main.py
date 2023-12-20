b41ced39354a87bc2d3e5c3acdba50881b5035a0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:41 +0000

    afraid shall hair job smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_int(array):
+        return int(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
