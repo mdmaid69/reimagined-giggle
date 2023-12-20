c9a04085c5e2a88408be27edfa81753b2ec6c3b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:45 +0000

    guess before goup head ill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
