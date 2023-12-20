88c71db01460a1e4544426211627154324238552
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:43 +0000

    circle deserve after formal debt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def get_current_working_directory():
+        return os.getcwd()
