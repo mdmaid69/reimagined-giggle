519849511e1c85da6070a96a358391cb7ebdc2f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:28 +0000

    store face along makeup trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def sort_list(lst):
+        return sorted(lst)
