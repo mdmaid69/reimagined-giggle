6312faf7e7315861ff2bb0fa202fcf4ec3e24958
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:13:10 +0000

    earth back promise pick argument

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-for i in range(10): print(i)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
