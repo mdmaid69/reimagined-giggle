be533556d74c6dfbe7417472bfd73b5fe84b93a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:37:30 +0000

    show cheap pool sing takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
