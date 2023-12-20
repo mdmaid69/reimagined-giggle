8884f36d818b878be0aff13e9721cc2e60c4d392
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:21 +0000

    birth result slip expression behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
