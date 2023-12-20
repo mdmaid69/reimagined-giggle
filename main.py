ddffd6fb4028916f3046c96b1c06bf76ac545e35
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:20:08 +0000

    deal moral disapprove behind doctor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
