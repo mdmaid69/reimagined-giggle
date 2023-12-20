088ca6604725cb1a797019d549c044b0d0115135
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:38:00 +0000

    breakin treat church glad hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
