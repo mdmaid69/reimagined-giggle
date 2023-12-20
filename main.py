42ef988f3192ed67326ecba91dd764bbb5840c82
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:12 +0000

    house left cruelty dry recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
