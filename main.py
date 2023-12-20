2a801d32489cfa4217d69f072c00974576d8f9a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:59:00 +0000

    violence bite mouth possibly usually

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import os
+def change_working_directory(path):
+        os.chdir(path)
