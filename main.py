200f1a1680cccd664da360b53bd0ba952119bf83
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:40:40 +0000

    stir fix interruption gather find

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
