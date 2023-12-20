494c4ff6ee2b460d26611239519dd9379fd1cbf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:58 +0000

    worried protest mainly send past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
