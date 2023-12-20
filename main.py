362e6d8842e37a0cbcd768bfe05bc3fee8785d91
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:07:24 +0000

    pot may none behave centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
