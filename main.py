09986054d1b3e892269416e5c04f2489ce4afbdc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:01:48 +0000

    night paper look store trouble

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
