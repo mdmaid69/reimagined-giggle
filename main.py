5820ff04c9a52c4e2509159933bb48395017f89c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:37:50 +0000

    pollution why own takeover goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_set(array):
+        return set(array)
