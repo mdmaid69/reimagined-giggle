71504f9d7523a42339c348b73706ab0b0e380052
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:36:02 +0000

    correct bring calloff near height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
