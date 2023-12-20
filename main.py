a32de563352e66aaaccc32936392495b31a03fc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:15 +0000

    arrange equipment bus business shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
