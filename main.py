82323fe9e8dd404795578f3ad7a1b4737a9bc2c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:51 +0000

    problem tall fold still boring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
