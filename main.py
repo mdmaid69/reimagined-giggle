5e1fad8decde28de44ba17051ca0953f82891dd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:08 +0000

    again wander basic description top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-print([x**2 for x in range(10)])
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
