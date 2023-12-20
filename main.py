49e5a781fa16e4e29075c952676a9a8ab7676507
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:28:18 +0000

    main wash arrival translate whole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
