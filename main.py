e3582f9dc3d255bdd4ff14d12f18cb067173fab3
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:03 +0000

    reserve ours risk score mass

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def find_min(lst):
-        return min(lst)
-def square_number(x):
-        return x**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
