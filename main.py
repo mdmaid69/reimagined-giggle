b1484109d3dabf23d1595596863826def01fbb52
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:06:28 +0000

    winter book into regard plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
