fffd7dabbb3c968cabf6396f03c6298b27ce9ce3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:14 +0000

    future well achieve unfortunate ring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
