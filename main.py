08db9a15366c357d8541451da6035dd8c790ca97
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:54:04 +0000

    whistle birth talkback uncertain wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
