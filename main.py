c7b0d4c83fc5071496a7aae95f28aabeedaadacb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:53 +0000

    consist sport contrast disgusting moment

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def greet(name):
+        print(f"Hello, {name}!")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
