7f91018c56df7ce8e0061ce64916475f26f7ef36
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:26:09 +0000

    least set position have until

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
