38943d4323b863c7803f0d8c4525836974c74128
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:12 +0000

    get pipe roof into makeup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
