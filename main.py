0a03dabc76e0a8fda21be58326c3eababd045bbf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:42:33 +0000

    leg wall high squash exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def cube_number(x):
-        return x**3
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
