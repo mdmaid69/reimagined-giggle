ad757ad6176d6ae0443b19fdd38319f66cd5d3cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:20 +0000

    Ss long weak throw public

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import time
+def get_time_since_epoch():
+        return time.time()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
