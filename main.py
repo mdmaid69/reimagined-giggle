4e9295b3e99c94e4aea449dc89b63338836e84fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:08 +0000

    leg deal talk range blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def find_min(numbers):
+        return min(numbers)
