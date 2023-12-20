ff204041931e557b997d361d69e100e2b20143c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:53 +0000

    save trust their contact giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_interest(principal, rate, time):
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
