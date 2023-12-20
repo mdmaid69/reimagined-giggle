02d6779b6ac32d6f5651b3130fc5447c8f7657a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:28 +0000

    wrong brush lesson obtain goon

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
