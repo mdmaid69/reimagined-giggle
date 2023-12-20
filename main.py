b4e62fa6bc02cfc057598279657ab48175b6ee70
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:20 +0000

    frame interrupt frighten comeround smell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
