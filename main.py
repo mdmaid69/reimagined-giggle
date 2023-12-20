e687d5863e21665557dd9ade0b96f3ae4ddb92dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:32 +0000

    keepfrom excuse extraordinary aim wish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
