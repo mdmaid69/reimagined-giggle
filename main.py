cbaef1f929d6c746ffc7fa7cc3198719d9b01007
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:26:19 +0000

    feature whatever control pride remain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
