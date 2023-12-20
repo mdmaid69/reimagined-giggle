2a6cd003151cd46ef763dd3da4ad91ec206989fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:29 +0000

    somewhere prize word unfortunate run

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
