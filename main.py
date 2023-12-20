3b5a2789bdb89b38cc6d47830f544233c6140a87
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:34 +0000

    perhaps comeout difficult switch feeling

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
