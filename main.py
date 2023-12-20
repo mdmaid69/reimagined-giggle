7b76222065ce99276d04637ba55e6421722684da
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:29:23 +0000

    split please sometimes knock aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
