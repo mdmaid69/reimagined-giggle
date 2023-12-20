18bd9cfb4261de6bda76a32d44abf65f2f4df22a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:50 +0000

    gradually form clue land accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
