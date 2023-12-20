612318ea4ba7cf106f3d6792f92704aa3fcc4500
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:53 +0000

    lookafter else direct direct inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
