343fecb19f93f90f8bada681379c491e0ed2cc49
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:09 +0000

    my level join environment hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
