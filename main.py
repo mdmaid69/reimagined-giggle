e889335fdf595eb4fc9ccc161bc3098ce9826c20
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:03:55 +0000

    geton you ready everybody bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
