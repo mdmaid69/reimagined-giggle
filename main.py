f4ebe9ba017fd778af8fc801824dd6d520ccf165
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:53 +0000

    common quiet sore foot climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
