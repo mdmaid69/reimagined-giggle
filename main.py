3b5852b0b57a464612d4eb6fa5568fef6b8aeba4
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:03:01 +0000

    noisy wrong study pretty tight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import os
+def change_working_directory(path):
+        os.chdir(path)
