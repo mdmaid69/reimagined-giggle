25243b450a3beec5a6f004a28d5afa0280881b18
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:45 +0000

    turnon middle colour confidence worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
