f0a0522a04b3e66ecd2984739251c0ecdd243bcf
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:17:32 +0000

    dark pick after kind shoulder

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
