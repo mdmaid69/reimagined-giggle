49f3a9900d3446e87e2bc177891a4b31e7def49b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:16 +0000

    reduction rank bringback clean begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
