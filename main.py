7ac7bbc0134f76d660f8d172d662ae895298760f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:49:47 +0000

    walk chance long rule certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
