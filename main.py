1052030f47593b3f126fef32ded2215ecbc73710
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:39 +0000

    cut unhappy past mean equal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
