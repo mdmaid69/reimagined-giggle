09092485b3b369ee7be159dff005c0854da27eb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:28:34 +0000

    terrible bright close forget acceptable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_username():
+        return getpass.getuser()
