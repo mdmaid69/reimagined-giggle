57e9cb05e57292d52fd62891e39ae37488a4eb3a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:13 +0000

    water control hard earn spread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
