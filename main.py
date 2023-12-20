ead2f5ae7511f35cd3b2c50441de44d13e3b2e1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:08:38 +0000

    crash idea weekend hers so

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
