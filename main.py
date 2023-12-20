06ba50e338b4633d2007b1b3abf584e7d438868d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:28:36 +0000

    or knowledge tired smoke basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
