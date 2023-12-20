7790e3afa61ba36f3368b24209f7869c0cb0ba8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:55 +0000

    correct name simple ever pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
