44e54b535194c0aab58ea97916e3987880ded57d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:25:58 +0000

    bye welcome turndown rent development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
