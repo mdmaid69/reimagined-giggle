917bb04790f8d2ec8c7e5151ac5bf86815724c82
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:13 +0000

    niggaacademy affect tread embarrassing road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
