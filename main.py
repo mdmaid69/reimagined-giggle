c4829bcc871fff70e14e906dd1f243d6965c3905
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:55 +0000

    development we together maximum mass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def split_path(path):
+        return os.path.split(path)
