0f79daa81b1c40d6eec2038c7bf6a503912861cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:29 +0000

    decide characteristic justice fair past

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-  def multiply_numbers(x, y):
-        return x * y
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def is_even(n):
+        return n % 2 == 0
