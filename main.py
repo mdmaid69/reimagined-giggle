25f657ba47e61293a435150eeb9d5f1bc4b89cde
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:07 +0000

    straight relax escape load soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
