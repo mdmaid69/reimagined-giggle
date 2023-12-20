06a8bb46a7a29db984ebc00cdaa3f293d4b16fda
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:19 +0000

    embarrassing like gather sudden impossible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
