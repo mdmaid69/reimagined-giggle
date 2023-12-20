50a1b5445fb03cff2cd972a16620e698247d966b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:40:05 +0000

    cum complaint broken scene deliberately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
 def clear_array(array):
         array *= 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def is_palindrome(s):
+        return s == s[::-1]
