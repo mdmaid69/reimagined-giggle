b0187016a75d62b71a8d04b1c69d7e2d7389e2fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:17:49 +0000

    uponon argument suggestion vision better

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
