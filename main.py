902f55168ceb3e5399b3effbf2399b8b62472532
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:39 +0000

    plan train many beat concentrate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
