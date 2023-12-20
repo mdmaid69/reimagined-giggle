cb2ffd50b4a4895e6afcba8832ca0e1233c53462
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:57:50 +0000

    design suggest believe frequent temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
