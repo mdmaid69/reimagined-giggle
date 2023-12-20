32689c63fac59fc82d9893405628eab383801fe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:06 +0000

    trick chase that word OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import collections
+def create_stack():
+        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
