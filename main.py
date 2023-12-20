153bc6ced3c6600fb47b1846a1f08f4c920f408c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:39 +0000

    maximum gap team blame tear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
