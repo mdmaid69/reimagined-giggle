de7069ee9c84589d52ff3e054aaf89e19739c216
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:09 +0000

    ought confused branch date splash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
