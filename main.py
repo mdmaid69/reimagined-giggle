3c628d222500a152d745a540bd9342f945ea84cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:13 +0000

    kill priority no cheque spoil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import math
-print(math.pi)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
