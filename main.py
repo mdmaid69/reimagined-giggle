d988e4e518693fcf107d580c76d0a2c57025a2e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:20 +0000

    runaway marry gap friend Ii

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
