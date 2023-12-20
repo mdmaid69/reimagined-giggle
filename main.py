6137e0f5b9620b93c6331370dd47164e5a00c691
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:15:36 +0000

    official workupto talkback oil exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
