13ea708a710b38737069c1a5ea5ca53f11a71ff4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:29:40 +0000

    pretty stand age opponent low

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def is_palindrome(s):
-        return s == s[::-1]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
