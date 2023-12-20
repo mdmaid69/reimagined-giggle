62aa062d3d7b3dc1d23c56af522b51caef89ca66
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:34:13 +0000

    whether brown up think exhibition

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
