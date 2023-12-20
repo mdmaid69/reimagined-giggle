11dd8beebebd569852804b155c098448d0040d34
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:02 +0000

    ask strike later disturb necessary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
