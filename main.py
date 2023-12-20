7ab3fc4db27be0d945091070e0a74eaab9042873
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:13:11 +0000

    position pay exhibition confident shopping

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def convert_to_hex(n):
-        return hex(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
