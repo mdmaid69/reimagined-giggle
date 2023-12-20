b28fd1ed27db4115684f7299151b00585fa8384e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:26 +0000

    depend turninto book task nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+from collections import Counter
+print(Counter("hello world"))
