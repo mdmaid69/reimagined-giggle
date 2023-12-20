55096858b29824ff7fc509d1c9dc350ed21043ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:36:18 +0000

    instead tie scheme government ring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
