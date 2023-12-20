a0ef408f469453111eee94dd791ff2e82d6ec4da
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:48:11 +0000

    bottom have considering left run

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-print(time.time())
+def multiply_numbers(x, y):
+        return x * y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
