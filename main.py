4aec8b7be40d58140389ab94da3779b0d3af3256
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:22:47 +0000

    stick obvious might somewhere whistle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_typecode(array):
-        return array.typecode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def divide_numbers(x, y):
+        return x / y
