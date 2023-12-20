1b6371ed8b0bfc00d76302b344b504a2ecb3d91d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:22:30 +0000

    polite bar in dear very

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
