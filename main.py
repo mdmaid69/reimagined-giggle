df42ed57fe2f2a923d214249714b8740fb1872d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:04 +0000

    organize zero organize left somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_as_list(array):
-        return list(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
