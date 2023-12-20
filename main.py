0a21052441612d014c6b2ed1be9159c0965164d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:58 +0000

    petrol rare describe guess evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def add_numbers(x, y):
-        return x + y
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
