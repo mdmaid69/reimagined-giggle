41447f901f7123a3c99e360833d5a3ba80e4de04
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:36:47 +0000

    effort distance pocket sensitive comeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print([x**2 for x in range(10)])
