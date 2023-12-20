012f94134bf2ff1954ebe8d257aceef0484bf0c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:28 +0000

    light look along teach goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
