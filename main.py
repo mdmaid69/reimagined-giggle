6b43679d8c61677c37f3d9d929c174ba539440d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:20 +0000

    ticket introduce breakdown nearly suck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
