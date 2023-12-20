1fcf4de17372870689abd64285d06611cbf14f66
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:19:08 +0000

    sound break stop standard it

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import platform
-def get_os_info():
-        return platform.uname()
+def find_max(numbers):
+        return max(numbers)
+def convert_to_hex(n):
+        return hex(n)
