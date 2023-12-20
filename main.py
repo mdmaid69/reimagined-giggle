5d2c8c34fec18af7c994ee0d35e836f1c4871142
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:50:36 +0000

    in ticket operate steady pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
