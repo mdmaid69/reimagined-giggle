fd94fe348a06d39412772a9249075eab78a6f0f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:14:51 +0000

    book left anything last taste

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_density(mass, volume):
+        return mass / volume
+def is_palindrome(s):
+        return s == s[::-1]
