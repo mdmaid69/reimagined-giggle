235ab789f195da4077ec55011cf1b5340a563bfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:53:53 +0000

    crowded tall closely insist comment

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
