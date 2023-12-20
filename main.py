64f5bf03a39713f6b58c975e5c1f285a8e35616b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:31:57 +0000

    official serious swallow order kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_item(array, i):
+        return array[i]
