200e53e0588b1f84f733837615a76b6490e79114
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:58 +0000

    collect bath alive runover care

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_octal(n):
+        return oct(n)
