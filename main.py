7798539fa2c542952fa4bc283c4b91d0d663d6fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:23 +0000

    may healthy promise refuse deaf

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-name = "Python"
-print("Hello,", name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
