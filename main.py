b540740a4f4a232aba711f242de63eec2b5ab272
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:11 +0000

    drink arrival laugh agreement can

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
