c095c1e299039036e423dbf75d8bd0a0cab10579
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:18 +0000

    crash increase behind shock case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
