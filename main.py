bf4c834d80679144fb2b746750b986dfe869bdd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:48:29 +0000

    workupto mysterious intelligent together previous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-print(math.pi)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
