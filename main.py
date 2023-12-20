dea34dcc4c7005039ac00a548f224a512f76cd8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:14 +0000

    silent law pretend tool manage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def cube_number(x):
+        return x**3
+import array
+def get_string_from_array(array):
+        return array.tobytes()
