2487852fe3019631da36bad6ae09672430771b22
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:09 +0000

    holdup evidence ear example formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import tensorflow as tf
+print(tf.__version__)
