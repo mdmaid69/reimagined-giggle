25dee89f35771ed8db26b5182d595cc5c68cc431
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:04 +0000

    down half push short mouth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
