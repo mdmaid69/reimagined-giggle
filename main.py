8dc6c614fbcfeec32721f6a8fa01f674c4c44d54
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:28:02 +0000

    national equipment nigga usual steep

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
