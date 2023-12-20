0c921db3b8f0f23ad020fec2a60163e68295584a
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:55 +0000

    free 179bite bored regret trade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
