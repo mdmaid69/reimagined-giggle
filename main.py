c231e81fad38dcfc392db2e0be972571b2ee6776
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:44:44 +0000

    attempt regular negative talkback miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
