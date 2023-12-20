4efb88cde0f2ba235f125f1c1f49f3f2006bee8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:50 +0000

    nurse march sink bottom pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
