5f8d223c29b810d0d6bded22f2d59e6ff8ad1592
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:45 +0000

    mainly several run floor sit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_pressure(force, area):
+        return force / area
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
