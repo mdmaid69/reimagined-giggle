bd8c752fee3ac65f958a258d927a3b68b0d4b81f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:46:57 +0000

    remain name question central rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_logarithm(base, x):
+        return math.log(x, base)
