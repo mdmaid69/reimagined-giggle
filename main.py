2eef404bb17e1cfb6def09e067cc8cc316eb1937
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:39:20 +0000

    step responsible beyond second in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
