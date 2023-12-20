370ce2297f5d96304c3cea1f1db11e8478621732
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:37:52 +0000

    custom cum method discuss how

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
