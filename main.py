7ca3dcb53ba34f1006cc46dbfae862119cef692b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:04:35 +0000

    disappear protest free week top

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
