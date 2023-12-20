4833fe9c55eebc2822c78491935ad169dc85e99f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:38:40 +0000

    police formal workon climb distance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
