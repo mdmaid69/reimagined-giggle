87268e7effcd05c2386ca87957db1a836e55ec75
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:20 +0000

    division method road good declare

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
