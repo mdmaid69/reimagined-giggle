3f2a1ce05104154b44f90434b74ab6c11d3c5748
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:17 +0000

    reject tip search right copy

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
