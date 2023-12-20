17245958813ec06079f7a01c184156478db8bf70
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:22 +0000

    weigh against emergency it delicate

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
