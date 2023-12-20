fe4f5ed6cc0f1903accfac78e56a908ed3a8edb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:00:52 +0000

    upstairs trip influence illness talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
