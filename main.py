bd954fad93a3dc3986b38dfb65c2c1edf8d3e347
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:49 +0000

    phone damp twist getback prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
