06cc74cdbebd7f20fb5166dacfe6a28b382df3d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:14:56 +0000

    private fine telephone nigga old

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
