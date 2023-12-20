ce89fcc6497d3b19a86f9d4a6a68d06290a1832b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:39 +0000

    collect beside computer arrange before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
