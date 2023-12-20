363bc5d32260258db3ffa6aa554f6bc366d95c79
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:05 +0000

    contain personal rest wise exchange

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
