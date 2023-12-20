9e464fcfc3114c48dad0ca867947a84af25223ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:04:34 +0000

    little hold under dress pause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def square_number(x):
-        return x**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
