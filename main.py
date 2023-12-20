6d57ebc7a40e3510416bff81b47f18b8926c60c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:57 +0000

    anyone the fetch strike divide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
