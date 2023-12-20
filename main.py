1c40582424a05cf37f4193d17cf25b3b1f069552
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:04:06 +0000

    tidy direct fair tense seem

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import sys
-print(sys.version)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
