f19050fdbfb8f78231996fc408e2fec11a21548d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:11:27 +0000

    straight plenty unite clearly comefrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_max(numbers):
+        return max(numbers)
