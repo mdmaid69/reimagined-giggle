afac5c43cbc246f4734256eec2c704300fb8de3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:37 +0000

    finger bet introduce disapprove ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def delete_file(file_name):
+        os.remove(file_name)
+def cube_number(x):
+        return x**3
