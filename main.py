64649c7960ea237d510ff64936f43a7d640b0396
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:01:38 +0000

    bar average callout show favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
