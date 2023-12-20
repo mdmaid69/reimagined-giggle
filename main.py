9e279737011098511dde2e67e18aeb8f1d3c6dbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:12:12 +0000

    affect opinion thanks sensitive develop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
