d4cace1419fc01004b961a46e35a324e5e00f829
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:59:36 +0000

    Mm quantity judge neat hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
