fc48b580a120466eff54e596847cbe29f93b49d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:34 +0000

    hers exciting afford closely share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
