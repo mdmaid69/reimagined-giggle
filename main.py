bbd68f38d80b9d5aefd168ea016cf122521ae188
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:22 +0000

    wave tradition decision secret loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
