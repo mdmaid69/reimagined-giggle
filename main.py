3949d5bdaa0703bb4f1311d77b487875d5cc39d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:52 +0000

    glass Rr worth chin much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def reverse_list(lst):
-        return lst[::-1]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
