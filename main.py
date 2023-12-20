4fc9a270d66adcdc9a4a9751e23eb881aeeb9b67
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:15 +0000

    connect proper purpose from partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
