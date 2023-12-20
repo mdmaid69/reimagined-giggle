35a80f5a6c95d60ce8bf3fcaec7783104894138c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:29:39 +0000

    straight rescue run clean feeling

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
