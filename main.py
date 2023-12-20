40224b64092d8e88874905d797bc694c660e6131
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:40:47 +0000

    forward wonder friendly call brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
