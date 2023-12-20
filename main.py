63f143080b4d2e255991f5075ba58e89970f592b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:25:58 +0000

    regularly yet nerve occasion Yy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
