509ee14e9c7faa749fee1ba3e66446bd9d4e4527
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:38 +0000

    among choice laugh official roll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import numpy as np
+print(np.array([1, 2, 3]))
