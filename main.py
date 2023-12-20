d9a93f410efbd6d2198eedb696dc4573e8da7b06
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:31:54 +0000

    trap he town wet friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
