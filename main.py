573ac6502875e26530a7f8b6b47e6c6938df57f1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:02 +0000

    benefit suit deal target nigga

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def is_odd(n):
+        return n % 2 != 0
+def subtract_numbers(x, y):
+        return x - y
