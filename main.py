a6b90629070a98c2087d954c04a8af80ac8e6028
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:03 +0000

    discuss remain bit brush fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
