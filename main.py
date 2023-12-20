35411625e2d1b1d9e39bfdf9d1cab999d25deacc
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:08:43 +0000

    birthday rise than region once

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
