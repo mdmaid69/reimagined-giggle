3dbddcb031af3d004d15447cf1c67ee91c3e9445
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:01 +0000

    journey admit freeze afterwards cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
