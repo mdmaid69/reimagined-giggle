ac84a43ba8f493565e68ac4a29ef13f955998a8e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:56 +0000

    comein live love style congratulations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
