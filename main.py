487ceafb87ba6b7ebc8e42cdf950afd30371daab
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:10 +0000

    pause steal by float whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def reverse_string(s):
+        return s[::-1]
