bd5f63b98bdd564ad876b261015b97fe441e6e53
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:03 +0000

    react concentration nice art body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def is_odd(n):
-        return n % 2 != 0
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
