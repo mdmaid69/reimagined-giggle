3a948e7d36fc784b7df104dca74422c796390e0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:37 +0000

    crowd approve lookafter ability search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
