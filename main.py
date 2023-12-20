11fd1a04da00034a6bbe074525705be8bb0c54c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:00 +0000

    say kill entry shopping thinkof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_union(list1, list2):
+        return set(list1) | set(list2)
