ca1c78d616c7ac25e77840786a538ab400c39885
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:48 +0000

    fork swing software ought sensitive

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
