0031b48e7608566d4fdf4aa20f70fba2f63182d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:00:03 +0000

    visit opponent certainly contrast determined

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
