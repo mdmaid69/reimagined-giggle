da569d4bca8377774f4c7904727a2b9b98a38fca
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:25 +0000

    usually but choice roll keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-name = "Python"
-print("Hello,", name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
