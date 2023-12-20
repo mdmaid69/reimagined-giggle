1f2b09f84739b23e26c2b85c474349c1d1f14e14
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:14 +0000

    method shock prepare by now

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
