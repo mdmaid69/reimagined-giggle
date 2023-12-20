171a63fe70738c9daba63d84ae16021bd8b47852
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:15:08 +0000

    otherwise gas entry dish effect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
