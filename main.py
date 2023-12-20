9e7f3390d453453f769935f787e670f5f60a5e89
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:10 +0000

    popular leave opposite brain inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
