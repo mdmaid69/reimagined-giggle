d3a4fbe7b50cf579523f3e5b725b4b460a15a28d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:46:21 +0000

    convince have fall drop remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_pressure(force, area):
-        return force / area
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_str(array):
+        return str(array)
