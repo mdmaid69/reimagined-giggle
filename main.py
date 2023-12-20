c1a557aeaaf30659e5545fff1ca9ee9eccc80e66
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:43 +0000

    goup ordinary rest interfere thankyou

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
