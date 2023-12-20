3d7100ed000b9da6e3f8a229d4af62556b1630d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:32:05 +0000

    usually mainly by pupil fill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
