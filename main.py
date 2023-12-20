fdf5a66a3a4ec3c9a9997c7d04a33f2143f545c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:05:38 +0000

    draw string split reject Qq

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
