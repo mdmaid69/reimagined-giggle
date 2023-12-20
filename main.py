7f9e27598f93f1a2c35728be9a8824c7822030bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:18:27 +0000

    rule like image destroy quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
