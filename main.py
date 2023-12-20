b4ae18aa3d2a12c90de12e203a2c84952ccc06a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:59 +0000

    smooth out 570enjoyable burn push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
