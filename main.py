854b81df56e2efa230cb0e62139f1a8e097ff96b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:13:31 +0000

    mean free on advise afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def reverse_array(array):
+        array.reverse()
+def find_unique_words(sentence):
+        return set(sentence.split())
