c2e1a15da56c43d9e00869c933fe264b91a00cbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:27:11 +0000

    use choose trick hour together

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
