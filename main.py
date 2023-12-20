33aa54237024a9fe2494dc417300555fab6c8763
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:13:37 +0000

    underneath if bury discover challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
