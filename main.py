c53cb70942ac432485707d91b22e8272623dff1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:52 +0000

    identify tap class cost choice

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def greet(name):
-        print(f"Hello, {name}!")
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
