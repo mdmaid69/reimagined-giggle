3bce06766148832cdbb44370ebb3183cdee1faa6
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:20:14 +0000

    judge match wind by post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
