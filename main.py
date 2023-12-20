2a1e6997209fbe2df76b129bc190d53378f2472b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:59:10 +0000

    minor tool taste time finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
