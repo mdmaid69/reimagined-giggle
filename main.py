2fbba793ffbb8c30d5bdf2b94b9b6b9afa0f91d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:02 +0000

    cut unless safety possibility trust

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def calculate_area_circle(r):
+        return 3.14 * r**2
