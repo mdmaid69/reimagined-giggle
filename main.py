8b273325a6cfa500ed0c1cebe1f0210557265b65
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:09:02 +0000

    treatment Nn shout protest light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_typecode(array):
+        return array.typecode
