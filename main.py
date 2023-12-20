950f27341ff19c1dd4443bfec98f5695de5158bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:36:03 +0000

    excuse no mad body course

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def reverse_string(s):
+        return s[::-1]
