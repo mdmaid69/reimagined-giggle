3868cecb33cfabe84e2ef01b3edf7e2be5ad9f4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:50 +0000

    reaction bag tour reason square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def add_numbers(a, b):
+        return a + b
