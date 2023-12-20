2c07a2be139b95eeaa0d3db59a48eecd659cc191
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:07 +0000

    dare introduction mark drop no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
