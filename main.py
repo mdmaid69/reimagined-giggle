3a0f981fe02e9bbdbd9d249a87d719fecbd38698
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:06 +0000

    meet she pressure this slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
