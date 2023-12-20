d23489600b07570488653d6947ba5b19a740a0bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:35:08 +0000

    storm quick sort side truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
