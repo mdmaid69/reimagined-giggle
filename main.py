74ab4c8d70e8a4671103c4bcd97aa824c156c486
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:33:51 +0000

    pile sweep lookafter about after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
