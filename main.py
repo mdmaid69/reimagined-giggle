e3c72e7d3a04ae2732c091d3b8523b7992dba2c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:39:27 +0000

    equal clean very creature pleasure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
