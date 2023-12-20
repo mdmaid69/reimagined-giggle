a6475c1af2ed9505eb7a2c1bdb0c1168c55d8324
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:34:16 +0000

    relationship lonely between keepaway happen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
