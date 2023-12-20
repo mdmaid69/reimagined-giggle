2f8a38772e5e1075620f34125cf5939d88e48e66
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:44:21 +0000

    wheel foreign search wake punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
