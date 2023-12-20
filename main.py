476eff373e152273ac8b59870ecc3a94e1f33e4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:06 +0000

    adjective attempt debt if own

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
