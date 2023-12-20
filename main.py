9888c12463a12e2f8135a9a683dcfd0094f40701
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:45:40 +0000

    trap building through hollow power

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
