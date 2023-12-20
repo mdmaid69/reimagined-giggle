132f747ba87b315537efaedd9008153d8bb20c86
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:41 +0000

    only write mystery finish face

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
