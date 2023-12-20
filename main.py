7303aaf97be25db6827e86d5d2c763843d31ce9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:14:07 +0000

    future never callback high progress

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_max(lst):
-        return max(lst)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
