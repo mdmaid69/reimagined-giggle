1b6a92855387e45e689c00e36d66d3859b516a65
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:29 +0000

    during pleasant occur slide unit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
