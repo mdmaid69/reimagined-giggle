dfb3132c8f3ba009e1d5d6d3369b33d68ea55d09
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:54:04 +0000

    sense talkback impatient permission pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
