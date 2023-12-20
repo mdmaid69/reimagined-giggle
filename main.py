cf72fa9cee3448a1b75230a3754484982c4bcf37
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:52:56 +0000

    your nowhere blame escape run

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
