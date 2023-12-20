32a05b1f812d0db267c5dac160d0fddef425a899
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:06 +0000

    defend general share blow tie

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
