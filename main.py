ea5e16e649c77fc627f09e90242706933bb92f3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:20 +0000

    squeeze funny eye loud shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
