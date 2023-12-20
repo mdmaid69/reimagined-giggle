5a83761c814a54515b72108e0ddf718bf04a7121
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:23:49 +0000

    work nervous interest cold seat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+print("Hello, world!")
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
