09bb7dfbef7ebddea450da7478900862b3629ccd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:09 +0000

    him part persuade bread fold

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
