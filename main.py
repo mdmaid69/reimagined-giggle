d860cbcd7782f7c7061ce5e08b68c37146881b8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:44:01 +0000

    ground improvement Cc disgusting equal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
