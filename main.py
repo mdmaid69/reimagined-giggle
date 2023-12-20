3fe3e88dcf6e4effdcadb052f09614f804d320d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:49 +0000

    strength makeup fit spare rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
