ccfd963f0d0c3534069dd72eec1f944c440fed77
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:39 +0000

    union beginning complain cheek look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
