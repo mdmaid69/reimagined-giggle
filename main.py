f522f4ed959288a378389d4210e9ee8cabfcca46
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:14:58 +0000

    year hear possibly lookup month

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+def convert_to_hex(n):
+        return hex(n)
