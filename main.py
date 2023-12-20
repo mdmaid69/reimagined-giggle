f9b462745528669f33e241cca7f02b110e88da29
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:12:05 +0000

    join achieve term suit up

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
