25df95cb9d4e4c696d987b2d3c02f3f21b0c50dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:46:13 +0000

    slight your plastic lookback(on) mouth

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
