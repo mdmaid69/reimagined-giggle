f4e2cd9d4e8f1e261dd652e0f49cd674a72af655
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:10:43 +0000

    heat talkinto card compare spite

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print(sum(range(10)))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
