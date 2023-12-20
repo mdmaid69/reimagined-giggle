a7edcf2d7be70bdc6f97def9b2a072e7684a3f3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:54:52 +0000

    father correct without thirsty lunch

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
