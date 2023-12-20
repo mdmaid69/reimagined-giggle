a39beea7a15e251c8417a50af42877cf31a5bd40
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:19 +0000

    skill pronunciation war fine suddenly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
