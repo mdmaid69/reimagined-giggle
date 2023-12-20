ee81338607ac5ade3e47eb82e8f8e891d28022e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:57:03 +0000

    class throughout again recommend up

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
