aa1c993401fb53868c052c288ce6dfab7099ae42
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:31:21 +0000

    neck live might his concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
