246f81146755a47ca520841ffaf88b91e5b48ac0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:47:18 +0000

    surprised organization cheque city embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
