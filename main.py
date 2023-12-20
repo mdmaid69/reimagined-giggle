9b7346f9bb0631c5d6b08af11355a895a9c5be0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:24 +0000

    holdup ear Pp often weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
