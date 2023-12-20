dcdbb74d6c830bfe81dac1a9957ebf8d780c07c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:16:19 +0000

    bear loan holdup hair frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
