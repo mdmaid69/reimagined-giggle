27ac3d2bafef43e705f72f51b7a444cf9c992b5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:31:44 +0000

    worry love water set apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
