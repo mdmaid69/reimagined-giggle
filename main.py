55b622fd72753fb7f371edb7ecab4feb9972d2fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:23:48 +0000

    suggest indeed culture food study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import os
+print(os.getcwd())
