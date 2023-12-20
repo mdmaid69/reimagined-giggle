9c5ee869bc0083faaecc919043245a3e77dd00f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:09 +0000

    reasonable defend relationship pay dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
