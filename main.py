3582cb92554fd0a8a806583b75982b90274b470e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:17 +0000

    break except justice for information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
