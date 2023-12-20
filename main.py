fd0d568accd1fe6480241cb1d564309e1e4a644b
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:45 +0000

    vote pinch crash only adjective

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_time_since_epoch():
+        return time.time()
+import os
+def remove_directory(path):
+        os.rmdir(path)
