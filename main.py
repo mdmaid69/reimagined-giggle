80fd742e6eeffd1c049b3aab016dcec82540398b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:30:29 +0000

    rid hurt begin pupil whisper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-name = "Python"
-print("Hello,", name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
