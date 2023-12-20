417b49ec6fd8384f4ad94426e698b939b066c2b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:08:18 +0000

    label advice cover past accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
