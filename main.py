2ef268745c3c8623afac74a30cb36dcc62481828
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:35:54 +0000

    wish risk squeeze woman him

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
