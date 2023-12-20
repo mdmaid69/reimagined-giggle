6d5cbabab32f32c22da0c03c73834a6b08946453
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:41:45 +0000

    bad switch wealth pass argue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
