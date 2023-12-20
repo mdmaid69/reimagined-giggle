772244fe8bcc7a6fe3fa9ac70fb48dae00a87a78
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:25:11 +0000

    clear fact fashion bringdown rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
