73257c056e6a0f84cc4a7c0db3b19a8af0f48126
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:02:05 +0000

    concentration simple ability imagine subject

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def count_words(sentence):
+        return len(sentence.split())
+  def count_elements(lst):
+        return len(lst)
