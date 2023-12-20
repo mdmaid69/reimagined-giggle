0be39157b91ad27be2d9c001d5a65e199b44e5ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:11 +0000

    break fill today various tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
