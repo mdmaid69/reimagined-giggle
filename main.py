b12bad004cea526daf1bce14a6cff7abdf65960c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:24:17 +0000

    process safe discussion dangerous pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
