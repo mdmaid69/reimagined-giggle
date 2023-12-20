d77475358e5d293752e642b15bfa25854bc5bc3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:59:50 +0000

    name cruel Yy team somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
