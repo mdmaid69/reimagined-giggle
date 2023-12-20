7fd16b1c171d01bd1a00614f2f06e60613b1ebce
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:22 +0000

    drive issue into plate order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
