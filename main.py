4a1fbdbee3514531117fe9b457f993050de3ec14
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:30:24 +0000

    surprise summer sign happen steep

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
