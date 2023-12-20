87268a490af6bd05a221729898ae783b9e9904a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:54 +0000

    attempt meaning area thick way

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
