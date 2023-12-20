c33f64f116b2e5aea38f69f19fa6aeeef32236e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:23 +0000

    flavour cough closely shrink relationship

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-print([x**2 for x in range(10)])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
