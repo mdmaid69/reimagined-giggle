a79a4a71183ee0b99e0f32bab308ab82e2607ccd
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:24:59 +0000

    criticism beneath hurt very shake

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import random
+print(random.randint(0, 100))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
