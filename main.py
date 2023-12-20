ea37c2a20635feab177d6239399177aefc0f9877
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:56 +0000

    right reserve pay problem search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
