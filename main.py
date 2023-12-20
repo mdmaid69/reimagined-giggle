086397411c5b0a0a253bacbe71b567cc47ef7511
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:03:47 +0000

    odd the close Ss constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_bool(array):
+        return bool(array)
