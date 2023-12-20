1b82d5082cd71f11a9ce1f16fc30515aff6004fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:07 +0000

    giveaway fire machine appearance treatment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
