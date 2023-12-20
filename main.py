03be32b2686ae559274774ab13cd6aa98bab8060
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:04:08 +0000

    responsible everyone trust burst pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
