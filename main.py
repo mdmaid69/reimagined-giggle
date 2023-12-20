92a638d95d4376b9ead1b7c72dda8a0d24e0122d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:24 +0000

    colour distance rescue slight that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
