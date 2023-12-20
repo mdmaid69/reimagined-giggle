a99be7ec45d56aa951a813f54e0d3849e36c6c63
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:28 +0000

    drawing contents need succeed balance

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
