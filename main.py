a0e8899b6c0a8c7900de1590d368134965918ba3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:52:00 +0000

    comfort relative disgusting friend straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+for i in range(5):
+        print(i)
