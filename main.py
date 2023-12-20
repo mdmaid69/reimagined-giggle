00aed1e1792273dcc5f87c170b0407f5698c9508
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:33 +0000

    that confusing yet turnup announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def print_python_version():
+        return sys.version
