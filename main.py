129f066c627735791358d7c5cecd68d8a87708e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:44:46 +0000

    better country closely sky pocket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
