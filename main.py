1a3120e83692b4d855e3a4e23641244df17cd0c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:15 +0000

    send shall clothes hardly attention

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_as_list(array):
+        return list(array)
