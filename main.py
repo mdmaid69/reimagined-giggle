c5c54425657dbc1eee7df73c2f1a659c7b688abc
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:23:55 +0000

    shine brain start complain combination

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
