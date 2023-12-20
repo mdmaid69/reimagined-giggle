84f42de0b29a654010dae54e3b986a9e76c3ec9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:23 +0000

    cheque sore opposite tread anybody

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
