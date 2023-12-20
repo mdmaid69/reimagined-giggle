f36c8b136475cdd56a13004154e85f5478577a34
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:22:20 +0000

    wide splash important interview relative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
