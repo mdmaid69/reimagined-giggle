176bb5ce267ad97433a556c835ab66589c7800ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:41:13 +0000

    big store true through rare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_array_as_str(array):
+        return str(array)
+def square_number(x):
+        return x**2
