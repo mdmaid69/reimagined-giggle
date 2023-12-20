83bf16b4ab0c62229739c403ee879e11dd3fb15a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:28:41 +0000

    nowhere strike prepare please easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
