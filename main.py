068f1b73b363c8d2e395a6718c7b56ab32afdb28
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:40:01 +0000

    beautiful raw worst dream cover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
