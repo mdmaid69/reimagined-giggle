9ae212a4bf90cae52c40a236aa37396ac449e604
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:01 +0000

    window wave turninto dull phone

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
