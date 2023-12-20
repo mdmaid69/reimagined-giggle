94207b9223ca861e5c0354153d5723aa7efcc426
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:15 +0000

    fall able goodbye most round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
