61fe1e2f606187bff3073bde6b09e9c7e298ce16
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:51 +0000

    body justice swimming exercise meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def split_path(path):
+        return os.path.split(path)
