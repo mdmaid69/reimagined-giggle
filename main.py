8aec4f5e3539ac1cc73a6d77c1280bad2b13c045
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:58:40 +0000

    ever unpleasant roof indeed lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
