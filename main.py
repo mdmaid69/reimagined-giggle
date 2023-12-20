769e57eb613382cf76bdf1df4b83e505610eddfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:12:41 +0000

    instrument 566end practice mind national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
