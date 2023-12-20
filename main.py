0f9c7aeee5a924a11271b247e494ef445af11ff0
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:02 +0000

    position fit undo share talkback

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
