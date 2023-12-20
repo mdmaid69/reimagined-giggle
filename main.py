cd7425d58bb327a580e92e67023ec9867bfb5665
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:00:56 +0000

    recent within empty unable busy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def count_words(sentence):
+        return len(sentence.split())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
