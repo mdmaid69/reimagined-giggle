b20c53716b709cf481e37124b4ebb8121a39c927
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:26 +0000

    event centimetre convince mean afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
