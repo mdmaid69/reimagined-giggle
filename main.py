84781029c84c71ba2ed98f0a44f12349a883d107
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:25:36 +0000

    stay generally school between swing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def reverse_array(array):
+        array.reverse()
