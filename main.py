450ba7e7ea1daa057f34f063711125741fb3766e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:18:57 +0000

    ignore talkinto money speed keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def reverse_array(array):
+        array.reverse()
