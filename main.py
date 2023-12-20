c1329ac98759e4db28937aab628bafa13e65716a
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:22 +0000

    man put offer rid memory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
