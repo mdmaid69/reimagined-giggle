a26dd878b8f9822e4388d14a3f8262118559ea9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:37:25 +0000

    range willing patient shock aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
