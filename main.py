b48053765aad699dea8b27b4def31e4d54cf4adb
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:54:32 +0000

    deliberately formal think consist speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
