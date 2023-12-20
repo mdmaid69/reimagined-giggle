7e27b915cd13eb469f037bd484b22f08c44f67b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:36:55 +0000

    promise thorough form repeat suggestion

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
