395877f3950db629aed1127638935335977e0e5e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:35:37 +0000

    appear Ll translate mass across

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
