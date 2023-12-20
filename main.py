28ae31cc65c0ef08080db56cc84ded2ebcb9d522
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:08:35 +0000

    set clever add task safety

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
