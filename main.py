8bed71a19081efde5e29011167d240c1e0407f72
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:39 +0000

    strict popular Ll load prevent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import sys
+def exit_program():
+        sys.exit()
+def find_max(numbers):
+        return max(numbers)
