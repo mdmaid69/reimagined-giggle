7d98291df387d097effae28e6c7d3d8926f747f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:34 +0000

    previous dare same handle blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
