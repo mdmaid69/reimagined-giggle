2e4d403839572040dce96bcbe7f586f713f48c61
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:04:01 +0000

    belief area distant improve sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def subtract_numbers(x, y):
+        return x - y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
