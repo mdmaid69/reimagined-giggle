dc6392d32debe9a093e88d41aece771d2379825f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:01 +0000

    address exchange wind refer reserve

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def reverse_list(lst):
+        return lst[::-1]
