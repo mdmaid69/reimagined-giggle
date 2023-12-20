462f3faf55f60812f703979a273569681d1d7f73
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:14:09 +0000

    pride week where Vv standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
