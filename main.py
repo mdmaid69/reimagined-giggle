ae2dbefdd04f479f21a6f9895ef1a328040426aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:44 +0000

    fond frequent offer practical hear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
