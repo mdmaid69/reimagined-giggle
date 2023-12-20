6df71aee4785390816684a06bc3554e823d2c9ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:59:02 +0000

    rid loose geton anywayanyhow majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
