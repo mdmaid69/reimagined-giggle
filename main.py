52d284e56c7e882227931f11ca47cf8aba6de534
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:06 +0000

    succeed goout thought pencil uniform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-def convert_to_octal(n):
-        return oct(n)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def is_even(n):
+        return n % 2 == 0
