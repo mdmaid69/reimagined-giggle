48211d845b12103eba0edf248b8f5ca91983fbf4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:38 +0000

    violent undo Vv possession danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
