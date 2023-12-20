d0b508f50ed6f2ddf3c9fb13626d19d361c6900f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:41:32 +0000

    getback noone cause often square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def find_max(lst):
-        return max(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
