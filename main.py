1fd9b01be3d8e3539181b7cf72d00e6d5ea210ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:31 +0000

    wish luck winter quick wander

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_hex(n):
+        return hex(n)
