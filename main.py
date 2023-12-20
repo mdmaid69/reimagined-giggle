298babdc91bc8d54842065988baf17fbbafa431f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:15 +0000

    claim slight accidentally call anywayanyhow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_work(force, distance):
+        return force * distance
