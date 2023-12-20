32953de83655b25bcb28e4a22c5366bd821659ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:55:41 +0000

    argument swing door wish consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_list_from_array(array):
+        return array.tolist()
