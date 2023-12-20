f7aaa1786606ebe66b908fec57ccc368e2b25058
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:30 +0000

    not warn giveout makeup pollution

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
