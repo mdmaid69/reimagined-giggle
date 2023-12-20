e252fafd5aa16b160ab2ee9655fa2eed75577551
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:34 +0000

    thing card tomorrow take accept

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
