076392173512d852b3ed54882096ca6da3ff36c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:46 +0000

    letter last after if central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
