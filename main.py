3fbfbfa1a047ba46e7ff35bd87a9a7e52f949fe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:45 +0000

    Uu spill thumb deaf description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
