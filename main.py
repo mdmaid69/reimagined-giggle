98def5f739f421f63a5a1a1762cb5b3f90468363
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:57 +0000

    ashamed trade turnoff actually just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def get_array_item_count(array, item):
+        return array.count(item)
+def multiply_numbers(x, y):
+        return x * y
