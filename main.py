966f3d747f3d04c5a84d3ea1426660e6707e1826
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:58 +0000

    at anxious owe error this

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
