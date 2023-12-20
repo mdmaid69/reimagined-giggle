3bcc572ca6c5b4c7d404a19fa3a1b1c350a0a85a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:20 +0000

    thin entrance please hit cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
