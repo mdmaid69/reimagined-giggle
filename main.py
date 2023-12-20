ae496fc033495895da8db52cae184b9e6b58287c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:55 +0000

    gooff goout shop when if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
