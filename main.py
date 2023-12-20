bf30e67b78146d7340c4fd2325f9964aac0f88d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:11:45 +0000

    foreign rain education age tooth

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-for i in range(10): print(i)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
