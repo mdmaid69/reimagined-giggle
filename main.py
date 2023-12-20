ab9171eee1046a7e795f27a1737235375654eeb5
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:30:31 +0000

    apply activity either season argument

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
