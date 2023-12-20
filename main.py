fac3b5a95c9b03cc4cfba5ac8bdd5d30e32c609f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:25:47 +0000

    worst also government add content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+print(math.pi)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
