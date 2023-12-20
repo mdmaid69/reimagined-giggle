b1877f0ec87d5b2cd65f2018f0603541b2806f10
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:12:06 +0000

    179bite special vanish standard debt

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
