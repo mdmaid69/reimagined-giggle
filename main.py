13e723d61eb7ea9ddba6d921a32fe4f0fa7fc7e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:51 +0000

    although wake waste step this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
