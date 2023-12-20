d11028a14d5d0ca4e0b1a2d77dadf5c4c475c21f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:57:37 +0000

    quit tone crazy comeback child

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sys
-def exit_program():
-        sys.exit()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
