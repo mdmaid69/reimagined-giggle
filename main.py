45b20c9dabcac71d8980c6237d1bfc83a4751283
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:35:37 +0000

    drink leg risk talkback find

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_min(lst):
+        return min(lst)
