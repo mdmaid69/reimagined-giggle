1eebfc7e1b2166b8c4cd0e4bfca9a8e006894a23
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:43 +0000

    muscle sure meeting quiet union

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
