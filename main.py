e12574c8604f5188dbeb4df57a955fa570a1b543
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:24:13 +0000

    content if stiff interview fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
