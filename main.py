10a22750cca52e513de7036c9a21ac8dceb3c506
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:47 +0000

    content clear comeoff in treat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
