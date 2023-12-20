73e2b54febe1e6f1123707f7b79ac36f50eb1bed
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:28:40 +0000

    quarter throw child cut real

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def cube_number(x):
+  def cube_number(x):
         return x**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
