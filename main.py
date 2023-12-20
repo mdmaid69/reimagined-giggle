78df50a636f69b3a4ff7c262e99cdd5df69d534c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:50:10 +0000

    point runout rate afford apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
