5d4920e3556f69cd173c4e4bedbe73c125ba7ec0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:26 +0000

    convincing injury gooff if towards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
