6885f6578a8650968ee1e8065a299b026b76bd5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:17:15 +0000

    war makeup interested quit disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
