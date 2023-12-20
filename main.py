cdfc97a9dc90c8e5c0bf68f252676bff8427284a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:42:48 +0000

    ilovedicks female stay that save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
