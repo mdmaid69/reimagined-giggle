747506ad3715b6d7ffa54db5b9b4c737fc1e74c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:05:20 +0000

    single judge pencil the answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
