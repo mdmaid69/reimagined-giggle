56d9b3b9c62169979cebe31a5c2c0777448b8bf1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:27 +0000

    petrol declare result cut please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  def multiply_numbers(x, y):
-        return x * y
