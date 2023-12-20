02c5c04cd6fbc6e64e61585c742954a2296118bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:00 +0000

    his any finish Bb free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
