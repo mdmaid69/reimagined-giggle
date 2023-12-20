4f826cf604012f36ce77ec1dec5919b62cb48e0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:19 +0000

    frightened regularly mean regional land

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
