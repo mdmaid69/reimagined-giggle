2c6de9ce067dd57237fed6fa8bdf2fd39e19113f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:00:38 +0000

    spread often vote anybody brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
