869d20eded94ca40ccd4b405eaef5b3320ed1123
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:17 +0000

    hear nurse turnup wish getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
