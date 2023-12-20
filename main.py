8851baf2a3ed23f4bb903d6b9cd35daecd482628
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:37 +0000

    anyone sure post detail confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
