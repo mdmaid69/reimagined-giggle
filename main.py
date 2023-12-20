f733198522bc0f9cb063d5c4a7430e36dcc6ee83
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:06 +0000

    takeout brush cover still steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
