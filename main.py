48f9b16b65639d40b290ff846cd536efc2f3a17c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:48 +0000

    develop clue power away drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def square_number(x):
+        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
