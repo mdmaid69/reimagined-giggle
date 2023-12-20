b02e697c1a3416b506dd179025b0e20d6f78b4d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:39 +0000

    centimetre tread art mass neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
