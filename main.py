b9a4038678fb7c24382edd5ee1c9916118f881ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:45 +0000

    wish damp to blow word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
