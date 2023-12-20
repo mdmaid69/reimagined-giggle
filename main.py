7a9a9fb96a5b2bf9af2b00c8b1e41d20b0332b6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:11 +0000

    even glad later why goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
