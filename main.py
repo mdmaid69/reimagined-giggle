7aacbeff06baea090746fc2c864ef8eeac78b261
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:34:49 +0000

    tour nonsense 569enjoy follow skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
