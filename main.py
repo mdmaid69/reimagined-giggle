a1e9cdd46be6094067303fa8a3f822806af097f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:06 +0000

    remark do normal spray over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sys
-print(sys.version)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
