c2c024a6938e61e84a1660bcbe42696c4c2513f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:14:15 +0000

    turn bringback birth react slow

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def square_number(x):
-        return x**2
+import math
+print(math.pi)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
