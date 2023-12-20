6910bc41a474b96b8e62aebeb2adc4bf5ad8accd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:08 +0000

    operation workupto muscle several moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+for i in range(10): print(i)
+import platform
+def get_os_info():
+        return platform.uname()
