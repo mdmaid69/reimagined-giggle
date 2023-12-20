b5a1382d4f7f9d0993a865a78458f221b9c08030
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:51:27 +0000

    completely respect change belong cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
