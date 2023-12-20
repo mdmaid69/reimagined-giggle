02cbd505b1bc0314bb14342083ba74e7626b6d88
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:23:22 +0000

    prefer sound himself him spread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
