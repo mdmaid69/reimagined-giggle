12bc8f99660e038c6b1a2b89f24573efe64c0e4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:30 +0000

    butter row hair smell private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_min(numbers):
+        return min(numbers)
