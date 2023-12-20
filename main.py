d3a9cd96da76b944e5f9a3586cd3366bd2108c19
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:41:04 +0000

    once slip fair always post

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
