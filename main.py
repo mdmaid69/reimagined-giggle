58201b31d243a1a647708af31d6fb1bcc5758d62
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:45:12 +0000

    switch discover tough badly collect

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_volume(length, width, height):
+        return length * width * height
+def remove_duplicates(lst):
+        return list(set(lst))
