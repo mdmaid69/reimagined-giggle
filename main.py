04e38c3f6e8971a08659c9236ba1e504d4844bed
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:21 +0000

    my borrow Hh keepaway lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
