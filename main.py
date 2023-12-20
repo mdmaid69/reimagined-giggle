167966197910c54e5ce18bd383248974cd61c976
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:40:24 +0000

    let increase shop quarter workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
