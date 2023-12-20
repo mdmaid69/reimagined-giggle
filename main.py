c11530135a620b15669552dfc3561511ce6550bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:43 +0000

    face worst move giveup suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
