d681ff563222fb63d40628a0c0a695f23739857b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:42 +0000

    healthy might set low everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
