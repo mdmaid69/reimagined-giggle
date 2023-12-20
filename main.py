888c86b3204d51944b260d94589601d039249dee
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:33 +0000

    flow front cook tall approval

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
