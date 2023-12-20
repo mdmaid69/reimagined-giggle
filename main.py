9a571994c6a8afc988f0e257fd02f90b9c179b6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:23:43 +0000

    everything disturbing visit half pleasure

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
