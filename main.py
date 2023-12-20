d9164ff8e8cad02568cd013d1eaa00b049d42f84
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:11 +0000

    holiday getoff star it support

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
