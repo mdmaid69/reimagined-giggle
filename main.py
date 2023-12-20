77a56eb7d1cb58f9ad5054be9e966b5a5deff3cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:44 +0000

    lock bottom culture class question

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
