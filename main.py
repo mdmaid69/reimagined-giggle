fdf73bab98265986d97ec1c368ba4cc40edc02de
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:18:09 +0000

    however equal fork display move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
