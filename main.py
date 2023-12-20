9fa8de1462dcba88e934cc8a95d32235027504cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:24:42 +0000

    colour apparently prepare how interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def delete_file(file_name):
+        os.remove(file_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
