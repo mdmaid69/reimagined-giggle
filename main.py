b05552bd1ab87c0384245e62ba751b7a21835194
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:45:58 +0000

    hard experience brush involve head

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
