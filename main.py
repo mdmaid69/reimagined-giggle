515a1a01ff7dd281eb4f6cb63cdeb7d570480621
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:42 +0000

    heat punish party reject apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
