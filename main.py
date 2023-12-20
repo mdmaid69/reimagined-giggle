bffa54b8043129ce207bd5098db1f622ee122cf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:27:51 +0000

    Uu Rr odd precise male

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def append_to_array(array, item):
+        array.append(item)
