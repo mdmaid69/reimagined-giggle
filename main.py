f1d33dbc32f4ac80da8aaa0048cd8d86d7512771
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:09 +0000

    acceptable better recent especially first

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
