3c6ed119b98c02df23a9b349b6a84770bac03145
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:09 +0000

    float present neck direction without

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
