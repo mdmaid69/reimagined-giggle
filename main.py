2f6bff6c719a65ba8caebbb7cbfda1b451c13357
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:42:12 +0000

    increase choose true present rent

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
