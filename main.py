669cda53e13b0081dd1388c7e387249f64e85dbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:50:12 +0000

    call bill base hot imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
