cc90c1368b05d51fa0994a7a2f02e04fc50dcbba
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:11:35 +0000

    usual skin fence official note

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def sort_list(lst):
+        return sorted(lst)
