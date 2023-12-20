df82f29e156bc04bd5fd50c64cf8e0d632bfee5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:55:41 +0000

    furniture example mess into step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
