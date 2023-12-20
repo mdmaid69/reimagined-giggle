39846263c15109bb4d11d3dfef2be1f209ffef34
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:24:44 +0000

    whenever crime thinkof tooth share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
