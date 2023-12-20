5e26baa5c8de2e19dff7a1c3eb592b9f9e2699d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:25:11 +0000

    friend moral contents doubt delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
