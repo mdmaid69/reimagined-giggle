bb179f021c1e37b39df13d6c973515d58d5ec972
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:43 +0000

    frightened kill mean anyone shop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
