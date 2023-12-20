b28ff5b80af7de7e2f7df5fbd746f335325eea4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:08:41 +0000

    false steady grow fetch square

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+for i in range(5):
+        print(i)
+def calculate_acceleration(speed, time):
+        return speed / time
