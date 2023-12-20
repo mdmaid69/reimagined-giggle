bbf993f61657ed10dee451e88f60ef73f5e27afe
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:47 +0000

    careful quit start evening fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def count_elements(lst):
-        return len(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
