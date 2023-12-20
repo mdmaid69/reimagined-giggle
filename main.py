b6dc44c32e94ee1101b1b2ed8bccdaee72f1f55d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:40 +0000

    excited story treat round slight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import numpy as np
+print(np.array([1, 2, 3]))
