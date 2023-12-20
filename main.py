a79900bab3a6467843401b29267b1139fdb37fcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:16:12 +0000

    drop whatever better suck justice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
