0854b24738ed7562af9d1b9983aea2a666f36fc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:13:03 +0000

    connect line eat knowledge entry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_acceleration(speed, time):
-        return speed / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
