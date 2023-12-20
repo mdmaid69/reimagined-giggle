4699789793ec7d5b02904eea58511511a7c27de2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:55 +0000

    interest nurse makeup share breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def find_max(numbers):
+        return max(numbers)
