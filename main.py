51229062777b36ad2f12b4d82ee7a95e0e86d288
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:48:45 +0000

    office read slight before criticize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
