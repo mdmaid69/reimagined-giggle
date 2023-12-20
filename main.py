b8a1bcc155675edbf5f0b04f05146266002df8a4
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:36:01 +0000

    finally I compete takeout than

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
