0ddaf02b84cc7d5ff965dc58cef011d3e1031974
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:57 +0000

    separate action fierce protect rough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
+  def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+print(math.pi)
