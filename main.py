d1798d585ad82458796cdf55793e6678b609efd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:03:35 +0000

    trade silent callback sight fairly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def clear_array(array):
+        array *= 0
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
