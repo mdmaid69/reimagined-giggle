69665b2a56e0261c897e6e86e91df6eb65850feb
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:40 +0000

    history account separate shoulder takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
