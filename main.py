a6efc809755a262a468a4beb9f539eda70210730
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:31 +0000

    save patience play roll season

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
