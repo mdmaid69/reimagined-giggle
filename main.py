d71134fe8183291342d886b74a0294de3b314dea
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:01:32 +0000

    round luck object confuse OKorokay

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
