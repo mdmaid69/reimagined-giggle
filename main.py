15df3c4baac1012c1efc4fd854051f4f54685e96
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:24:06 +0000

    section twist attract consider full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
