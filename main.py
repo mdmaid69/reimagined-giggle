8c3a0db0b2683ae467d6e27ab6dada2ad4b308db
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:22:25 +0000

    physical practical correction gap general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
