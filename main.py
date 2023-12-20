e20f407b32467ed07c15ac233dd852475ce38b28
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:47:08 +0000

    personal car slowly imagination danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
