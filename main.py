81d816ad1e02ab48fcf3e21f0dbba2a8985802fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:24:51 +0000

    beside quit use square rush

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+from collections import Counter
+print(Counter("hello world"))
