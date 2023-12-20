88f06269b84e300ba342144694077a19148af515
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:03:53 +0000

    Tt fast round trap border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.ctime()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
