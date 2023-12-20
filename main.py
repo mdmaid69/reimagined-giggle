e12dee7753d432419b2383c63b14a8c63fa309de
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:39:05 +0000

    bringforward close flat shut call

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
