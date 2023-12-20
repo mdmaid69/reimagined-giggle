aaa520f528b853e0588b5bc99299e868ea988e87
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:05:08 +0000

    entry laugh smooth protect reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
