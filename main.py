5cdb534879a0e83f47190d6d49a5329146d4f25d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:07 +0000

    whisper straight raw backwards fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
