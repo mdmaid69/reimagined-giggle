6b91678e9b5bcff96ecfe072ef397e40db529803
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:10:54 +0000

    Itself mother season thinkthrough weigh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
