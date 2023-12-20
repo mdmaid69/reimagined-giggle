3226b79e16bbac58e91064d2c29af1b5d95f804b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:55:35 +0000

    needle mean minority weigh like

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
