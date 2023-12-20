66afea6d79338aff4de8db20f290198ed429f660
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:23:31 +0000

    something advise balance certainly steam

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
