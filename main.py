ccd01e70e9479160f77608fec822c5697eaad939
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:36:39 +0000

    scrape nonsense act violent appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
