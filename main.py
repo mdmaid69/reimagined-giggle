139d1047fb5dbe9edc4c573a500b1e45d39f36d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:01:20 +0000

    trousers interruption thinkover moon pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
