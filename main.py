81a6bbef3365453cda04179c52b1b12c3d5d3785
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:18:42 +0000

    length adjective schedule top recent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import time
+def get_current_time():
+        return time.time()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
