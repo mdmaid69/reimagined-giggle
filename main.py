39016d1c7d03127a134ad6c88d8dfd7b768a8e73
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:59:28 +0000

    workupto summer range wise likely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
