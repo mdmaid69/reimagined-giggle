f59421a307ffd2b69e8f47b9099f66f64b3d2e16
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:54:48 +0000

    suit breathe out always damage

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def convert_to_octal(n):
+        return oct(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
