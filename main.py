e593bd93903b3c2555d29ba03db787efb902e6dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:05 +0000

    hear attach job winter rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
