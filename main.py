4231f935c68af0355fa72fd4203f350cb16f0876
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:56:58 +0000

    fortunately train within sell trick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
