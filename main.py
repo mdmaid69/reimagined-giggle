59692d1ead7976ccacb4cac9a434191c235e5f4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:21 +0000

    cinema neither first prize war

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  def subtract_numbers(x, y):
+        return x - y
