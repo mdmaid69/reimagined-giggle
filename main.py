8a859e8aa369c563d8987e87ceb50847bcf90751
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:51:18 +0000

    advantage than extremely successful note

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import sys
-def exit_program():
-        sys.exit()
+  def convert_to_binary(n):
+        return bin(n)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
