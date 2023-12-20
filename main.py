27fca25b6e37ebe774b6af32e272a7cfb690a213
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:19:23 +0000

    hot convinced load wash Ww

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def count_words(sentence):
+        return len(sentence.split())
