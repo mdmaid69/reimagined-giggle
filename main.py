dabafe51095751f478b6221581c3ced487077b8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:47:53 +0000

    guess free body claim design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
