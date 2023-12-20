f9f0446f4d6a7f464a2e0f71d8819902645096e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:44 +0000

    dry cope comefrom excuse make

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
