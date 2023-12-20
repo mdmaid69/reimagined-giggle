ce3fa75ae446841a90fb875be532a1b905c3e79b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:44:23 +0000

    much capital far complaint fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
