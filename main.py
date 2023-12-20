eb4332a442d6aa5bf18bf60f1ea5c4eb0a31456b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:22:29 +0000

    correction unable once freeze miss

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_min(lst):
-        return min(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
