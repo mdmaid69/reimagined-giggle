0074b31d597a502e2d6edcf2a087c67155141d6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:46 +0000

    Oo track half little involve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
