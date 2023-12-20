e9be953cf9ad27f96536f3060c3e4c6ab5262308
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:43 +0000

    rare confident by strike contrast

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
