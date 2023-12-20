dcc09eb8be575303221ef24da35cd0ecab3053aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:11 +0000

    emotion acceptable become perhaps music

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
