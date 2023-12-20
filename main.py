e345ecfc38a6d0f48eb1ebf392e1d36b80de5e5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:43 +0000

    consider turnaway bury talk protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_counter():
+        return collections.Counter()
