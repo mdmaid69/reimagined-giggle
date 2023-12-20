b387b682b8bb320eff118774dd64569328e5924d
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:34 +0000

    present run like rather no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
