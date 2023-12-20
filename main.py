7c59553195ccade5c77a53cd5ece18d31d3368e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:42:46 +0000

    seem whom rubbish hope later

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import collections
+def create_counter():
+        return collections.Counter()
