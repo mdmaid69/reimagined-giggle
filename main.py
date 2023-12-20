e499f8e58f7409515eb409faadc1125293b495e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:19:45 +0000

    purpose flow down there occasional

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def remove_from_array(array, item):
+        array.remove(item)
