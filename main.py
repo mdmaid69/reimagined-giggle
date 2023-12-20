5f44e5ee316f781e07694d34754fefb0361bf63d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:49:11 +0000

    mix issue quit slowly able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
