bd9a822822ce8098105ec932e880570d1e107fd6
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:25:10 +0000

    smell base busy frequent rest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
