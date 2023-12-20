fb90fe67d1db9127500db1049bae1749a233fc98
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:38:35 +0000

    spend prepare study ticket doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+for i in range(10): print(i)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
