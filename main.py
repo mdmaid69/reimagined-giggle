63e04dd3d44758ee0bbf0a0321d6ac0e2328a76c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:48 +0000

    coffee dear recovery compare foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
