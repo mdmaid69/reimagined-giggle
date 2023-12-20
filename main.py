1c80b0a245203980ebbf809f76c979f00c49580d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:24:53 +0000

    sense taste bar separate whether

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
