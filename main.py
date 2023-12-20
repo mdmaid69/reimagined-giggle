830ce1055635ca70e4a9a35cf325cfefbacb9951
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:41 +0000

    travel disturbing in constantly collect

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
