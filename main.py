5efe2592f6d8571443b9eb830879e048585feb6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:50 +0000

    finger race weigh next giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
