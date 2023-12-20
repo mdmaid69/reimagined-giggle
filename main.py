088c1822a70ea4378f39f5615f4430583c8fd9a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:34 +0000

    size loan defend worst calloff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
