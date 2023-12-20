0eec3ddc0dc0f104fcad44e5a653c93fc88ee171
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:07 +0000

    hang without yesterday state Tt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
