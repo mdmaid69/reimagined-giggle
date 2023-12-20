1dfdfcae65ddcc10051c2ec30ba4b9d1afecdd2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:11 +0000

    only contribute greeting 571enough contents

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
