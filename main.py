0ad72887e54d6b97006f523fe28d97fc4af6d134
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:34:51 +0000

    lose common couple allow condition

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
