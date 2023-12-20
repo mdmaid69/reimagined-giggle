543210c3aef6f2a0b8cb0854198e67fd6598136e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:13 +0000

    left on unable pleasure impressive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def convert_to_binary(n):
-        return bin(n)
+import datetime
+print(datetime.datetime.now())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
