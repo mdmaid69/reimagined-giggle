99c6fbbd96d15c629ab1401a544a8300d553bc15
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:06:32 +0000

    exhibition polite breakfast game you

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
