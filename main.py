c9ea49d20ded30b6d7ad42b27753816b7f114bb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:08:06 +0000

    fine university unpleasant answer figure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_current_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
