6a374d82e4947607b9f2bc9868ffbd5b3fc77725
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:55:58 +0000

    dish disappointed basically prevent pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_length(array):
+        return len(array)
+import sys
+def exit_program():
+        sys.exit()
