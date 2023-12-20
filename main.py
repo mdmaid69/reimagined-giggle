a08eb090324d51bac8cf07bbca83f3f7ee99ee1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:19 +0000

    congratulate pot aim priority bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
