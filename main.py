8b1927e1dac085383c750a378b6b740c1ff370fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:29 +0000

    most perfect pleased one base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
