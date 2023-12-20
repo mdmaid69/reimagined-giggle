dc15721d2bee4def46eedfd70bce082d0a50d335
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:46:47 +0000

    evening try comeup include smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
