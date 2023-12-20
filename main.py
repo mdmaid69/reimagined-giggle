bd628fd14e03775cdfdc446ca59e5b46dab88485
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:38:04 +0000

    keepaway touch act opportunity report

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
