11cb65c5830daa8db00b4abe386e602522ca610f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:11:42 +0000

    cry best 572enter Ff part

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_exponential(x):
+        return math.exp(x)
