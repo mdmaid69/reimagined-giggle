6e36b4b0196d0ff9efe51ead15cc09b333b7bbd3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:16 +0000

    wide illness size shock shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
