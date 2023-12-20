e877269fc1ebc34e225bcf970918ec50556c2ef5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:45:27 +0000

    born gradually cross land punish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
