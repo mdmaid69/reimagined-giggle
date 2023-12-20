5ccf4d8768b75e3fc14f46988b3c8740b6b2dc4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:25:15 +0000

    convenient forward address search satisfied

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_current_directory():
+        return os.getcwd()
