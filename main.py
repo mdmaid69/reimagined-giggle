8017b3351b474984bf0bc2bc9ae2944f3d61deb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:17:16 +0000

    complaint remove pen whom even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
