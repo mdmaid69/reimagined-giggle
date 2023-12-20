81df621f58b611b1d6c6fd4c224967c8fb444a4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:24:47 +0000

    glad practice because can shop

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def find_max(lst):
-        return max(lst)
+def count_elements(lst):
+        return len(lst)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
