0e2ac320070f7f57ba2d3347602178069f09fb7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:49 +0000

    prevent circle flat change joke

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def reverse_list(lst):
-        return lst[::-1]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
