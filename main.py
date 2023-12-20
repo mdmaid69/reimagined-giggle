44a89affd6a309221e6355c04558b1c8b6e54b8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:03:59 +0000

    be I discuss something now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
