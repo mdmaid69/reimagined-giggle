c338d43686feb29f730fc56e11de908f27439250
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:03 +0000

    himself even but please best

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
