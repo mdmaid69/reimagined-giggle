f3f20a180f851911b781624f0a8c30065dc94334
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:52:08 +0000

    hospital spend load anywayanyhow length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
