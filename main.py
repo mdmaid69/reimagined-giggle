d4d5f0f5c797f88f012bafbc52b5715b7992beb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:34:23 +0000

    getaway tight table cause concerning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
