4015a4d306de4a25cb61de7b198980311f5fffbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:23:11 +0000

    expensive power trial afterwards memory

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
