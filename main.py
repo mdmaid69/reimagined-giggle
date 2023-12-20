5141ed5ceed0d71bb83d54d1d7d311cc23785266
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:17 +0000

    fork follow against finish point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import collections
-def create_counter():
-        return collections.Counter()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
