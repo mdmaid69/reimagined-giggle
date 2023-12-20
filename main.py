c8c9c5608991bba96159c6991d005afb65884487
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:21:42 +0000

    dry letter comeover declare meaning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def add_numbers(x, y):
-        return x + y
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_palindrome(s):
+        return s == s[::-1]
