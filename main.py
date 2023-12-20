4dc07ae4108ea38bff06becc3977db0fc7490db5
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:03 +0000

    566end mail objection loan juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_set(array):
+        return set(array)
