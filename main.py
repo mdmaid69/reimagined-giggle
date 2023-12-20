fa26bc101d0e1968a70aeda5e6a8162dcba80050
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:54:32 +0000

    row turnover shoot trousers rise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
