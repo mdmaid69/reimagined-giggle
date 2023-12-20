87ff7339c1ceccd0272bcdd2a44d90bc227d0043
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:59 +0000

    content centimetre confident hurry far

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-def convert_to_octal(n):
-        return oct(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
