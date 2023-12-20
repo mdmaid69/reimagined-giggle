63a7c64f6fb6a446722255aced8ce3002e01642a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:31:02 +0000

    illness proof idea dare side

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
