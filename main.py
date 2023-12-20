4fec3e4f8b59c22376fcac3e6d9a618565535c62
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:09:51 +0000

    press unable whichever record neither

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def convert_to_hex(n):
+        return hex(n)
