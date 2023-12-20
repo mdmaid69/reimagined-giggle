6efb24860d57da28cf828cdc6ca9a33a9378c97c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:39 +0000

    uniform control variety sister thirsty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
