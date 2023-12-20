7b28cdfad6f9063e2466facaf340406773502eaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:54:46 +0000

    speed comedown snow telephone holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
