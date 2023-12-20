df2cf7a4115f6e93424f09b801aa3a1a576eb406
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:19 +0000

    prefer scene rule Bb far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
