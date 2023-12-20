45605f5094410e0fc9717d78b88a944aa5b06ea7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:11:02 +0000

    type invitation sensible several weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_speed(distance, time):
-        return distance / time
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
