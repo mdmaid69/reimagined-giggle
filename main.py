d65a22ce707edd36b8654f6c8f2c1b2dd6c66c91
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:44:11 +0000

    typical hide as nice identify

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
