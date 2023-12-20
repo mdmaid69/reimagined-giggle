b2ea9d11497a1889705327a57950066582772a93
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:23 +0000

    Cc Ww powerful necessary unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
