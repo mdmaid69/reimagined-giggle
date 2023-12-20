a4225bec0b895f2f760240e795a9d416b903d9b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:00 +0000

    usually man exciting evidence form

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
