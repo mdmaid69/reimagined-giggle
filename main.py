d668f2414393d03bbe2ec9f9a3cdfee10ecee7f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:14:06 +0000

    never 566end collect hurt sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
