3031914260bd238a96df3ba633c879ee4110c56d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:43 +0000

    weigh laugh Pp bath design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
