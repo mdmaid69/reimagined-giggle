9775f7d193150ee35a6ad3d09764b1dd3ff06723
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:16:27 +0000

    off ball reduction kind comein

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def get_today_date():
+        return datetime.date.today()
