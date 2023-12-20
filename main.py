18a48cefecefa627c42254541a9fd02b6ea711b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:10 +0000

    Ww comeover trick drink term

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
