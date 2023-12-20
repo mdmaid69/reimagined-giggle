1e98da092188cbacaadb7b9abf6a51aa24d6e48a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:10:44 +0000

    judge communicate scale runout stiff

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
