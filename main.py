a6691bdd2002c5d5cd86c756ae49d67f3b9c6e76
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:02 +0000

    design guard shock look act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
