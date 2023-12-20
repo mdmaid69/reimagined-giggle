fa0d0e4a5701826c9bc6999cc5e838b27bc3a288
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:28:52 +0000

    line business collection afford old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
