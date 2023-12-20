2208028f9b8152bee9997c025881eed4d4e1d6b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:39:16 +0000

    brother talk broken collection pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def convert_to_hex(n):
+        return hex(n)
