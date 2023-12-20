8be44c879b71c04a6d44d63e268b33b66bfa3694
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:12 +0000

    wonder goaway upstairs callup sail

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_palindrome(s):
+        return s == s[::-1]
