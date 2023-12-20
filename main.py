90de1064be315af07b53a0eeade7f3a869e13b33
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:25 +0000

    sister married raise naturally whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
