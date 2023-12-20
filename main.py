18ea8d94d2f844cc8d289619aad9d38ce524172e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:15:05 +0000

    them imagination right catch closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
