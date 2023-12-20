cfbe3966537942c83428934fff9754b5ed57e5c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:33 +0000

    road culture maximum everywhere centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
