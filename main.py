5835b4d640686a6bc508ccbac46ba9117b67dbf6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:51 +0000

    hurry could anyone properly OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
