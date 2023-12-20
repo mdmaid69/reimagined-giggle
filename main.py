aeb3fe62b97f3eb290f2c8ebc64c559ddf1d9037
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:30:49 +0000

    prove moon hurt expensive failure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
