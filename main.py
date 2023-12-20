a4b7bb94cd3f3612920608193c2f1d3784f9a9f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:17 +0000

    long hardly marry relative whichever

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
