0e0752e2bbfae4047f09e1daf7e5318b2ac722bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:33:23 +0000

    pile skirt workout pollution university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
