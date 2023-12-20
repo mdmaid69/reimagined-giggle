96dee3c112ef34ff7c5d99043c395bd05e1932a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:32 +0000

    public mix tip up independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
