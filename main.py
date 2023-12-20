211873a6e8ee374ec9231b7e3f9e49667f200d16
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:50 +0000

    abroad hit 573entertainment ocean possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
