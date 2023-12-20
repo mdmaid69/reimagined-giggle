1b4671ffded4f13b8801f26d2ee550ee3471030d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:21 +0000

    occur black mind makeinto similar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
