9b2ab09a71f4052307423ebc85c17fdf3fce6db7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:24 +0000

    question lonely firm natural risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 def calculate_mortgage(principal, rate, time):
         return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
