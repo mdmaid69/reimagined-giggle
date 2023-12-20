ea97e0ecf450db29c6c50e10402a2bc1633fe177
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:58 +0000

    proud sink support play business

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
