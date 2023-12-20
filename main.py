b29d2a0a0844873fb3c76bd10483c91b8c825df4
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:57 +0000

    breakinto attract into schedule slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
