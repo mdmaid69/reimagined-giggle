8b6fea589eb03aa620b5b47caf5eb5a4c7825e2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:19 +0000

    regional welcome shout interrupt maybe

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
