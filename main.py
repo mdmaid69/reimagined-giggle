17b3435f4ddb66dd993f9398d1f445b65f80662e
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:11:47 +0000

    male possibility talk bag cut

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def divide_numbers(x, y):
+        return x / y
