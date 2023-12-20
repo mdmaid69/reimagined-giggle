67ce12a9b27fc9e1b81ddf192fe4400b4588db29
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:18 +0000

    date somewhere Gg minority progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
