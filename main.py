0f4b10d2d63efd9d76b0b7c3373a8995194bb242
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:24 +0000

    concerned rain note mess exam

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
