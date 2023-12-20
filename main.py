fd706fe57aa6fe433203ba813a930fe6031d93ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:53:15 +0000

    length fast protection attract door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
