c494bb9a7f8261885642a7109c61e191a1c339c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:54 +0000

    emotion performance rain still Yy

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+print(sum(range(10)))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
