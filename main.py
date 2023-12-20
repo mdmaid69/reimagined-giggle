c04e8c051c182ed27f5fc95a1f6a7121aa5d1cec
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:08 +0000

    spite necessary taste kind activity

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
