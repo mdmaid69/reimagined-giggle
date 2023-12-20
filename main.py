6505135629655090b58477c1bb285ddb05011f2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:41:09 +0000

    possibility distant closed place actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
