105e4e3e33f8b6b324f72357b320493e64916a27
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:33 +0000

    guard anything head rule answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
