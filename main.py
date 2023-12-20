ee4eef92294e9bab6cf26221a6f421ef62ae5ad7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:12 +0000

    cheap but game total suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
