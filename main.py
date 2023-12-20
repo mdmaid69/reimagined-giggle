93a0c61edf2117e9a2480f65b9896bd47be429f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:36:14 +0000

    cool present couple tidy bottom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def convert_to_octal(n):
+        return oct(n)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
