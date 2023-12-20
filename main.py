3a7284c533b804f37d9cca1d084dd4ada01195ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:30:06 +0000

    awake minority square sense over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
