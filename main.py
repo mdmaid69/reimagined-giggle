e5c7edead34efa62d57c895ca1866115cfda1884
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:05:31 +0000

    steam trade introduction who previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import datetime
+print(datetime.datetime.now())
