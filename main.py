8598e1d447fc4218acc540974609d8546d2a0668
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:38:21 +0000

    scrap notice equal several practise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def convert_to_octal(n):
+        return oct(n)
