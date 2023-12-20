f88b2a32fbad76165ad7fb7d532ece34431055db
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:37 +0000

    interruption include after protection principle

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def count_characters(sentence):
-        return len(sentence)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
