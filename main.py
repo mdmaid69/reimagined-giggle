f851c1aae8cacd2f9025025aa676c6f2c5fbf4d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:12 +0000

    geton goback(to) sleep know now

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_octal(n):
+        return oct(n)
