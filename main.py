1e59a6bf4d7c5f18771519e02e08f6d5aa8d5c33
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:26:54 +0000

    illness part complete failure clearly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
