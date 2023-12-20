79e45fd44ac7c3ce540f628b8a679a25773b54cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:01:42 +0000

    late emphasis plate else towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
