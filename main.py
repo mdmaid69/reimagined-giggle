570b3ac7feb121d53fc6649aa18390a31f529c61
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:07:43 +0000

    correct it downstairs nice forgive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
