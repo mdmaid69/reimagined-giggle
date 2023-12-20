da88c1ceb404fad35693513a83859b205b7d5050
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:46 +0000

    accommodation really sky understand above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
