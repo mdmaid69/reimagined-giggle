33eb676aa83230aa19c2014fb3a599f9d194d76a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:01:34 +0000

    plenty nor new trap evening

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def create_priority_queue():
+        return collections.deque()
+  def count_elements(lst):
+        return len(lst)
