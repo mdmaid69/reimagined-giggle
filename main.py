7f60b99b2fc20c746d4bac03cff1223d60cb08d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:03 +0000

    trousers comeover money apparently say

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
