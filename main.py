14c959e3ad314c32f900b1402e6c1849ca775a82
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:09 +0000

    ability board score refuse needle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
