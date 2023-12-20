26d84464f5fab7ae2ec75104caea42cc05e84011
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:24 +0000

    form key up decision surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import collections
+def create_stack():
+        return collections.deque()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
