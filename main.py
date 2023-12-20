d03979416ce8da4ebcf341998d4a5127a6fbc847
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:20 +0000

    run head deep between force

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
