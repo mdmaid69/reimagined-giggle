b68b741846302b3e6816e0ec044bab8a2eb13db2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:41 +0000

    turnaway breakinto material breakdown dry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
