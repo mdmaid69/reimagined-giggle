402458ea073999d5e31047f61c25c7f79cd52307
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:46:17 +0000

    breakin accidentally exploration dig environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import itertools
 def get_cartesian_product(*iterables):
         return list(itertools.product(*iterables))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
