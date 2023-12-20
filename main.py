3a372c2d9c6c00d01d5309ca9db9dbb489ea0028
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:34:55 +0000

    copy stone breakinto Ll least

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
