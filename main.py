710e10fd536b2c6501bbbbe72f40ffb7be678c2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:18 +0000

    stay warm allow wave less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
