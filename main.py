b7d5246973fd10c9f389462a241aece7ddeaec7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:47 +0000

    rid fair high speed put

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
