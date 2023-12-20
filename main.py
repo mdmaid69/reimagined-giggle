6c954204f5f0f6c541f8e7904cdce460225fe9ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:52:14 +0000

    anyone sign edge slice foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
