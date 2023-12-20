00454f8a6c9e3f00066b4728e13a39118a3f738a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:06:22 +0000

    band substance determined popular production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
 def divide_numbers(x, y):
         return x / y
