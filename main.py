44ebe9637f3baa42f4386c120f9879418a0d7fc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:50:56 +0000

    effect whichever welcome dare touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_power(work, time):
+        return work / time
