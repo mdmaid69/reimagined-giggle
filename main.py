6ce991dbb29ca8ac26358d016f251193fd8a9a2f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:51:33 +0000

    height persuade similar my worth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_work(force, distance):
-        return force * distance
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
