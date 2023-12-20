c5b7f753e7f415ee228ec6417b1cb905d7e0fedc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:18:26 +0000

    sort rest introduction relief against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
