19db05d22a3bcb3f5472a466352cf778087dbc93
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:43:57 +0000

    as tip whichever anybody emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
