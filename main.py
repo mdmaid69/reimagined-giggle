565c304648eec83fa6d48976298d1f999a817cfb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:11 +0000

    method spoil throughout back mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
