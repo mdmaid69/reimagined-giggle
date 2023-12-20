3459196abada1d8f05f84b963094423fe4db4733
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:04 +0000

    punish possible ticket involve strict

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
