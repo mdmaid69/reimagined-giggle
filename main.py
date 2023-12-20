38985a05f4d670e7be4b5af186889e76eb575c20
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:58 +0000

    of appeal long concern education

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_index(array, item):
+        return array.index(item)
