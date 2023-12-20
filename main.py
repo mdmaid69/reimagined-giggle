9615863fd85ba6aff3a64ef68e93833e347643f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:22:34 +0000

    skill luck height sink occasional

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
