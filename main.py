10023b674e9ecfee7ce3cfde4cc3f39a4d8dbc3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:34:11 +0000

    objection protest near hope bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
