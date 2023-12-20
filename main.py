600ae37c1d52edd383fa2865d1031b54e45dd3bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:10:24 +0000

    healthy takeoff funny evening star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
