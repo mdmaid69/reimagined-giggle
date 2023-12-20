186c27b1aedefb7188b179968e132228098cc6dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:46 +0000

    invite complain truth before mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
