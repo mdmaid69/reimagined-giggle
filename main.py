d1ff86797ec85463663e23619fd5d481f818740a
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:09 +0000

    sell container question contact wonder

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
