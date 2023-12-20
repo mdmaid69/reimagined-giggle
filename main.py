32484165e095cb28d01ed00ba377380838afd463
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:42 +0000

    official coat lock meal art

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
