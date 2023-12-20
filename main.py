62e0b66109039584b1761e7ee4f1f517a4178889
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:44:15 +0000

    character praise type Ff remark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
