aee0f57014e8c4cd39b7f67e3e9a75a4aad2db64
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:10:39 +0000

    force patient protest notice say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
