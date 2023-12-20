632a90ba886b54aa69e1b153d85c754d9b595b56
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:50:04 +0000

    sink long unkind afraid purpose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
