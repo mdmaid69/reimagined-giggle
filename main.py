fddc3b2aaa40a0c02cdca8e03275b63139f76f14
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:54:55 +0000

    common comeback process course learn

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def print_python_version():
+        print(sys.version)
