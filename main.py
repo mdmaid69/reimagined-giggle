6ab08c9bc460df34963a46cc5887281436163b78
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:35:19 +0000

    price close strength second reaction

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import os
-print(os.getcwd())
-for i in range(10): print(i)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
