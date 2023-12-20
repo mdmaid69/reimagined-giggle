6132e2ddfdb722aaea4d5913ff84a70dd2dc54bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:06:47 +0000

    air borrow ground store new

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
