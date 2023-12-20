8f95a2f783d484c97de949b3d6c221f961433734
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:29 +0000

    laugh attitude precise comeover then

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
