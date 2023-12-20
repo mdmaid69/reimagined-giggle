cb7ab4df65163cf8d48ad2c2ee8bfc865bb2fa7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:32:03 +0000

    below feel idea him sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
