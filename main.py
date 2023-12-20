17fd3ef55dedc805195541753e1a387ebed3f02e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:17 +0000

    might blind makeinto ear yourself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import sys
+def exit_program():
+        sys.exit()
