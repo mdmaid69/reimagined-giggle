989b21e0bf03b4eb149ff1c0f1188c6a92e040f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:23 +0000

    thank goback(to) mean smile estimate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
