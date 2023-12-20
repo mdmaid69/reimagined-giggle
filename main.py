7bba5dd4afb5a3e010b5720a2bb7434568391efe
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:43:27 +0000

    in let wing off various

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
