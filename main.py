b375128a5d1beb52bc737c041f451bce8172d20a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:04:00 +0000

    open trouble tense treatment accept

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
