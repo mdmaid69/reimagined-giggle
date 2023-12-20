91d61d63e4d2175f49eeffc8aaa9ce48365a8d26
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:58 +0000

    tooth below inquire chat be

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_pressure(force, area):
+        return force / area
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
