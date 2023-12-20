ccfbf3e2ee5801917634bc28c0ab63bd5e3deff5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:16 +0000

    turnup floor behaviour best concentration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
