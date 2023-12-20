256d3a6a521f81b03e1d9d05ea4f7f95d8f5a0da
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:35:12 +0000

    match spin risk matter safe

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
