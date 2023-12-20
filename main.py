029be69feb72879c1fb411d7ae9f16022f407e83
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:57 +0000

    twist remark include like talkinto

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
