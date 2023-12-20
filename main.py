9b270d5ea78be4dcda6563920b67d474c1126a49
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:09 +0000

    top thinkof birth plate match

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def cube_number(x):
-        return x**3
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
