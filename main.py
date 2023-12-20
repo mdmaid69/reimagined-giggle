75391cd910487d66b10d991463db8b427dca112b
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:26:02 +0000

    translate fire about stone match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
