5e8239d95db864f31df24c44e679c8f0aa094985
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:35 +0000

    entry use mess similar pollution

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
