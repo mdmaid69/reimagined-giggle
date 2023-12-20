d9215117533930b6c449f80934ca52e472aa712b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:27 +0000

    watch position convince plain argument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
