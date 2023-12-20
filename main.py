6d4760e9c1a0f020283ed2be29e1cf6881cb06a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:25:52 +0000

    building like plant definite key

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
