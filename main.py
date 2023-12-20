636b3bbcf8eacd77ea2d2f1123d3ef433ef8b4c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:07:55 +0000

    train flow light claim tell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
