cd5b62ef592719b5ea6ba52131717e3ecf48c45b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:15:04 +0000

    pile very beneath hide copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import sys
+  def get_python_version():
+        return sys.version
