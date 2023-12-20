572f4155cfd68fe79f251a9d904c5279ce4b1456
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:22:04 +0000

    collection stop would grand fork

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
