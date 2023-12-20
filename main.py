e6830783337e36ab36dc07798bc21fb5fe04fb9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:31 +0000

    anyone picture fat lot direct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
