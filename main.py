11c9a63cc95dcf5367f03ff0a4be06d7bcfaa293
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:52 +0000

    tax valuable black less hate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
