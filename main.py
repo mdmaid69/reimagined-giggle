aceadb60beea8f4d30a9bb34931ffeb878023b80
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:04 +0000

    great preparation afford trap better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
