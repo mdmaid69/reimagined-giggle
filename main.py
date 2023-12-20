e2d45ae08007b3ede39fa1346b3b310209c2bf71
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:32 +0000

    table crash university mail lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
