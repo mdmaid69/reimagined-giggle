b3e0dd117b3eeb7b589770c37af224a361f22902
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:38 +0000

    unable arrival final press peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
