ace5d4f0f697f7263e083b05afb6dc6964396d9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:46:50 +0000

    somebody die thinkover born step

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_min(lst):
+        return min(lst)
