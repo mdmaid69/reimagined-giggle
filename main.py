79194e052930762ef7264dbc6bd10cd4d2392ea1
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:38:41 +0000

    nowhere record trick shake crazy

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-print([x**2 for x in range(10)])
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
