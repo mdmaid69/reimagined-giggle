34df115de9c45a1aa3edeed60e50cf3873101f02
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:03:08 +0000

    otherwise gooff extra love half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
