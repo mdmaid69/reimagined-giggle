11ed9140e97ad0e5175f49cb172fd6af97f9822b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:27:44 +0000

    notice description nothing unit close

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
