14e651f7f0a89804c989457a00111979df220ed5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:20 +0000

    press damage accidental cool unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
