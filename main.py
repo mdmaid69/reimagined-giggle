9115c95eeb0764bb5a605a53c6f2e71b955b23b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:09 +0000

    thinkof test bar watch unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
