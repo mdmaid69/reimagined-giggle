c1a5e5d19e6ce8ec175847ec87c02016dc2d683d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:55 +0000

    best bath male small rid

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
