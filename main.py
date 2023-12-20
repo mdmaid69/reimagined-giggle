8e1c9796685a2a3c964709c408cad6d546870ffb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:26:00 +0000

    label after standard copy rule

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  def convert_to_hex(n):
-        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def reverse_list(lst):
+        return lst[::-1]
