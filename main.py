3126d6895ba4045a3ff287a81152dfb6b416d654
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:10 +0000

    quickly I use admit beside

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import collections
-def create_stack():
-        return collections.deque()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_set(array):
+        return set(array)
