b8e9c36649f3ca19271b62bdc28ce422c257b819
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:12:25 +0000

    appeal niggaacademy gather business dish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_int(array):
+        return int(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
