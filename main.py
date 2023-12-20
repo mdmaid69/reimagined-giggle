f89b64829e56a573e63c39f3f4143e2ed86ccd2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:23 +0000

    spring fit less gate shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
