8557c0428371a8c02feb75a902d22784463d8949
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:57:16 +0000

    phone lesson instead condition nothing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
