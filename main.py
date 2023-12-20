7574428222673d2aeb92d95db84b9b70bee53650
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:46 +0000

    reply empty runinto quality geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
