f66ce2b6c62230511728d8c5dccae7c9244f214e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:43 +0000

    suggest want change add low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def get_array_as_set(array):
+        return set(array)
+def calculate_area_rectangle(l, w):
+        return l * w
