082ac50a0d9271f575de8015a189800208503ce9
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:31:53 +0000

    exploration friend proper bend track

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
