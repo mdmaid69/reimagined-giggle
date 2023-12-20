c97beea6d9cdc2e5632eefc0feedad6b97657ca0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:02 +0000

    whichever fight invitation flood picture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
