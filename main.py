a2343366300db0f43d40ceed0580a9d6275971d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:41 +0000

    affect furniture bad plate blind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
