7d084c7e1fa9223e4bf38906e21907d991c87d25
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:22:41 +0000

    scare closed general bring callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
