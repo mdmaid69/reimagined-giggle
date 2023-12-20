a2e7db56fad9b32af40be895f11ff9394214ff9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:38 +0000

    back complete her pen influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import platform
+def get_os_info():
+        return platform.uname()
