5732d7b1fdc008124261bc1d5209bf49c15366ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:13 +0000

    strong comment object refuse towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
