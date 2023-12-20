ec41903fccb3a2caeddb8e5e33b59b9080a11672
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:59 +0000

    shock contrast allow place open

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
