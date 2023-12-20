e779f4fbb123c725a17fb2318257a5719dac57c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:39:37 +0000

    shut weather poor keepup admire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
