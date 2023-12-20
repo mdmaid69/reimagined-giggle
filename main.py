4c25bc76429e66fc9af73ef0041500a634184fea
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:34 +0000

    with connected guard favour hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
