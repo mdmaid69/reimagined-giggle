b5054295e016615ba6db24b311debc037ad7c8dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:57 +0000

    importance will combine expect smile

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
