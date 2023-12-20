277f5e9d5b90f8312c11096fb5ba6708e7f175a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:30 +0000

    deal detail post decide thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
