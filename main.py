dd9ec32b672ffa09a27751c5c8ab98024b280fab
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:42:10 +0000

    afraid perhaps comeup speed clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import time
-print(time.time())
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
