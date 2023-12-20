e62fcaee7ba85cf7e3b231eee6b6620885d18e09
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:53:49 +0000

    personal card compare wide fill

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_area_rectangle(l, w):
+        return l * w
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
