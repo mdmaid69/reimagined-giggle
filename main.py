9966ae20e23c7b02e4ea7cd9df5259023337dec2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:30:39 +0000

    guard bread boil law direct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
