2ae1eea5bc24e5e32ddbc31b4979af5840f7fc18
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:56:57 +0000

    apply mystery fairly taste forever

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
