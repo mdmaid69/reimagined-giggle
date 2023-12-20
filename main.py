b2a25ecb070202a8052c1a887cd152a48148d3b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:06 +0000

    Tt mistake like address lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
