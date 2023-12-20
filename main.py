1eb52fec643505cd48492a62d8fb2234448ed29e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:22 +0000

    always poor heavy interested regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
