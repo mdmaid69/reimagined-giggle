0603d364c969963d39d8625211894d3c949175c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:27 +0000

    occasion takeout dig live crime

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
