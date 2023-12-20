571e353ed4899d44b3beee67497329716aad701a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:03:50 +0000

    ask rarely consist meanwhile out

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print(sum(range(10)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
