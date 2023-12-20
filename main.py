74ac25ea77f47b20fec9e4b758cfc3acde645ebd
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:27:57 +0000

    rubbish miss fast dream where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
