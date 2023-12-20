9bf38a6c6f60d594cb50937ab99cabf2237c8e42
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:36:47 +0000

    prize sea fault everything cruelty

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
