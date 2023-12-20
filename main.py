61775d4222e81ee7c276981504b97c282ef7e1b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:42:49 +0000

    yourself open somehow wind intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
