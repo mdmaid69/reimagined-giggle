6f8a4ed79a6c549b5a046952378e7ac9f20fc5eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:40 +0000

    and upwards piece strike angry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
