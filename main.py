14173d299ee2e44d002ff75c8d360f2bd32146be
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:19:33 +0000

    eager survive recent male sight

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+print(sum(range(10)))
