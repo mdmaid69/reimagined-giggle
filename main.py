6bd701628a292eeddbf912e90bb467f6192c4c6c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:44:31 +0000

    record vision patience lie so

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
