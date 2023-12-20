37d2f807ee6a7213fec7958532440e6653056cf8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:41 +0000

    official thanks since state increase

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def extend_array(array, iterable):
+        array.extend(iterable)
