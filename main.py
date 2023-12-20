963d52520883ca0258f7c03fe58e084198576034
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:54 +0000

    nigga temper uncertain sudden comein

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
