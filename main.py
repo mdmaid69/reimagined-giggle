28711baa53a2f94667e3b66bd6f9cab3b09084d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:33 +0000

    design trick comeover opinion set

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
