552949f2d5b866b1130f31aa70e82f91fe1a53f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:45:53 +0000

    drawing tonight share loose ever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
