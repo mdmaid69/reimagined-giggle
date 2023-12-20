b5bc02a2e4bf4b06dae36abe9f182aba1eb7afb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:32 +0000

    supply sale home save already

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
