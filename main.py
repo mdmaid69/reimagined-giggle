ce121519944459803031bc9bddf9d1652958fb99
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:41:13 +0000

    spray turnover age difficulty least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_typecode(array):
+        return array.typecode
