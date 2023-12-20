b0c97eb9c5647b6802691a4a5572e0a43d54560a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:54 +0000

    pupil trial still violence century

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def count_characters(sentence):
-        return len(sentence)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
