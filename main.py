fcbb35f036dac3322dd4fc5b1ab4854c9404f961
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:08:58 +0000

    expensive sick arm pull lower

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
