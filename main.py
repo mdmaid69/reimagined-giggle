1d93aa09f1fc90910e469e252e416999d1f9dc01
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:46:42 +0000

    breakthrough help swimming there shout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_work(force, distance):
-        return force * distance
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_sine(x):
+        return math.sin(x)
