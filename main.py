1e15a46c944b5eedc0132297e437c14b81ff650c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:16:39 +0000

    paper completely maintain question trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
