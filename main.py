d3f9e9aa1212f3588d9fb2383fe5f79a64021720
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:48 +0000

    suffer appeal extraordinary cheat copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
