5ab40032854ddd8f276b28b1384ef34ca826696a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:30:21 +0000

    point apologizeapologise cover expensive size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
