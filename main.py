813e71ad4987d46090757e57b50611c35540d74f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:10 +0000

    after stop sort excellent cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+for i in range(5):
+        print(i)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
