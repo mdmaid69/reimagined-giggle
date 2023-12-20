4165eab9322803794a552f4ec2359f4f0d29055b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:36:05 +0000

    whistle achieve we other store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_item(array, i):
+        return array[i]
