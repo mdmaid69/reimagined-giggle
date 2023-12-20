22c997674952821958c29e7e99d4434faf10438d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:54:09 +0000

    scene clue Yy project game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
