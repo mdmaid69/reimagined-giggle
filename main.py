ecbae67bc6e8e70dda5f408d152673c659fc3c7f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:35 +0000

    sea public hide method feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
