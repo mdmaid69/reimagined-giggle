854c52bb61f7c9a59594de14543f8606ae4c8be4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:09:05 +0000

    right whole space curious remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def create_stack():
+        return collections.deque()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
