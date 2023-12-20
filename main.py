01271d3ae71db642be64091225e3390b8e388c2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:21:15 +0000

    comeback rent arrange pretty invitation

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
