cbe7e63e44482540047dd1ad53f21cf87c1d07fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:04 +0000

    famous honest hurry advice over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
