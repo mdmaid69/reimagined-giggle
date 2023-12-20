8cfdb34e25497bc4b921cb8fb9ed8c398b238393
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:26:55 +0000

    call now evening meanwhile do

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
