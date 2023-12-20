2459cca95bc4116be3ff5192478eed62ff01b42c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:56 +0000

    smell while hit tend book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
