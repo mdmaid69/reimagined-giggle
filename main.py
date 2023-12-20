9c5125759b90084fa1a1b91feffa132838434d58
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:46 +0000

    after move before tooth most

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def multiply_numbers(x, y):
-        return x * y
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
