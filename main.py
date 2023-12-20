aa24cc6cce55bee338517d19d965cd4b89f309da
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:36:28 +0000

    Qq even feed Nn welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
