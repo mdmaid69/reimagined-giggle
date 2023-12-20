787fa8537c430ea98c07604fa400f3252c611c38
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:40:27 +0000

    bill deserve dead there protect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+for i in range(10): print(i)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
