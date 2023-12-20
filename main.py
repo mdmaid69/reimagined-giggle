887977b01dbb2d6a1d1792b51043f4fd20e5dbf5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:18 +0000

    protect fond work risk share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+print(sum(range(10)))
