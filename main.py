a76f069319f3b1ed23eee85de8d7891480ef2141
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:09:09 +0000

    pipe Pp search 568energy favour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
