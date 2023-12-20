fae19835197fce077db9eb679564eef2850c5e7c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:13:05 +0000

    state blame soon ours process

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
