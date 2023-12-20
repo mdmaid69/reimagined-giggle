1d8c96272d1448371fc35162fa2ca5a7c9f534ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:03:51 +0000

    week shut case foreign task

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
