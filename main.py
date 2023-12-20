62f8513a996ee8865d036bfcb4456768f0a126ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:50 +0000

    close Pp closely trust embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
