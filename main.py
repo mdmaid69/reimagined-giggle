375340f7d0778d960a3cf22c0a3e4915ae4bc136
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:25:10 +0000

    back back better along interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_stack():
-        return collections.deque()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
