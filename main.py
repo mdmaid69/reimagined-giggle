c87f978029db9dc591332f19eba89a8f38303cd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:15:57 +0000

    variety meet correction similar mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
