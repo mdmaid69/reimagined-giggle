da77fdd473c57a02311af6b4c15b67051f2e869c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:45:13 +0000

    long sound wish translate in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
