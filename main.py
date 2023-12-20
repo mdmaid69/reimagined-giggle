1bde6dc3cb5ee2d7c375f5021417bab715630128
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:29:23 +0000

    afford pause lay Jj regard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
