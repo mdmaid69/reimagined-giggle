98b26871f04dda90f4b59754aa7ab5dcf9300347
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:30:04 +0000

    564encourage certain fly stranger remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
