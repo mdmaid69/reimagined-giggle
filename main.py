553803bba14a2a0cbca41594a7d1c46f98d39ac2
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:50:18 +0000

    old listen getoff dream week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
