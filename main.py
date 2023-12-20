ac08f179ea4abef1530bdcd6b41b395bbe278a1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:51:20 +0000

    fight attempt learn sale mend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
 def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
