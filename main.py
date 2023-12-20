dda8e2ddc7b884865d3d64983f00479789dfc2a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:10 +0000

    ill law summer guilty tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
