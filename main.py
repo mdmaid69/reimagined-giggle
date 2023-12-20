05ef25cef478d3b59518a83d86d31849c028a162
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:03 +0000

    shelter otherwise happy influence earn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
