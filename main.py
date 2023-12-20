0b012b02f866117d21a8be9263141e1803e7cd44
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:04 +0000

    deliberate means sting round likely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
