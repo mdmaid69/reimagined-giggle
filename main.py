e64b307d8ec06ac034766e21384097d8a081da36
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:12:50 +0000

    sometimes any point coat ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
