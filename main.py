2f5a541a2c4fc58a8b279799f42137cfa75f20ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:38:39 +0000

    clearly touch separate rather past

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
