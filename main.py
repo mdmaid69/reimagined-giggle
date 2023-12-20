436d9aece747f7b32d8e73254d74c5f6fd66cd74
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:29 +0000

    exist his near suggestion Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
