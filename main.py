7484eb0e532517dee5658e2fe676021380bc3a62
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:11 +0000

    live care breakin by disagree

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
