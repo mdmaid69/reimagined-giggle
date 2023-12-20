1e7ff69901f25a3129f15754e3074a0e11daabf3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:38 +0000

    difficulty house Oo say relative

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
