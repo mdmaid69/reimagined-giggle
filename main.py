1b03487cc9231fbe0c9df3d6ab4e90e008028103
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:23:01 +0000

    subject comeoff decide steady defend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def clear_array(array):
+        array *= 0
