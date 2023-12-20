2f659330a332a4b24b6d783d8fe1d96499cef347
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:12:26 +0000

    silent interfere blame anybody protest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_work(force, distance):
+        return force * distance
