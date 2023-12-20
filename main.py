d71e6223697f04d37f0e2c55786d2ab1fdf8e0fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:11:38 +0000

    high entrance split behaviour shop

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
