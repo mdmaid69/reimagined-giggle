6df10d59023702bea1c7f95c502574d33cc705bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:30:18 +0000

    give talkback knock town imagination

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def count_words(sentence):
+        return len(sentence.split())
