461ab44f0e55ce82cf8f295abf7994398da83052
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:03:43 +0000

    advise form long absolutely dinner

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(numbers):
-        return min(numbers)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
