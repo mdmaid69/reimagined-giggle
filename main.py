c92bfa8d47982b6423f4d0342b028fdd4ed3be34
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:24:02 +0000

    young spring create increase aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
