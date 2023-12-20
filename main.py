42b2b7102e316cb35176a4d0758a9da65c438fe4
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:52 +0000

    tonight display hate real telephone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import re
+print(re.match("h.*o", "hello world"))
