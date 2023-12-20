ab95181fe050d464d685c7b593e1ef7ebe6ec9a1
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:04 +0000

    birth involve priority memory dig

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
