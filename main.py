a640a23b4eed94695f5f63a65f88f8aa3ac5e44a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:47:41 +0000

    politics hold hear tooth attach

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def reverse_array(array):
-        array.reverse()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def get_array_as_float(array):
+        return float(array[0])
