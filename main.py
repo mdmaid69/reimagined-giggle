96128a453ebe7bd2203c6e757aa9c218302b349e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:04 +0000

    Mm by Vv win vision

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
