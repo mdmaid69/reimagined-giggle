44f74e75fe6627220f12e684a0c8c7ab6b358dbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:49:32 +0000

    comeoff tend she divide able

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_list(lst):
+        return sorted(lst)
