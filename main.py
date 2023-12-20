490c951fd34649e3daca17af9bb9c13b55e59aec
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:28 +0000

    instrument loud surprised winter recommend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
