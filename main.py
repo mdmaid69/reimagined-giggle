7ab6a8aa0707568289b4e1d81da2b78e3fad6c39
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:57 +0000

    partner their run fail regularly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
