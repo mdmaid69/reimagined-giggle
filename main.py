ffea56477bb2fa3e70df303a2319abfe87773888
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:41:17 +0000

    broad Rr Ff period under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def calculate_factorial(n):
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
