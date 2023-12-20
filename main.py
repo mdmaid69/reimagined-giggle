eda9bf3dcff66976e4ad5c6cccd570e034b1f6c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:23 +0000

    giveup there again sleep lead

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
