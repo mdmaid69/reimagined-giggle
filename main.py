807748cd2c241a13daff564fdfa3116bf84221bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:50:10 +0000

    equal innocent leg swim copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
