e9806481da71968806c18578f18ff1ff99510213
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:25 +0000

    plain steam final quit season

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
