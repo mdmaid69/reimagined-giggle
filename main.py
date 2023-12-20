ac97683503e8063ff4223720d39a9685a60afd05
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:16 +0000

    adjective dead somehow hurry dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
