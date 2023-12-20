e194fe2febeec1c26e34b57abb71ff2a61608462
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:55 +0000

    frighten judge free impressive basis

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def print_python_version():
+        print(sys.version)
