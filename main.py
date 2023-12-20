76e96c1ac45904e635470b2c1428230d37f45ef4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:35 +0000

    heart explanation no considering replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
