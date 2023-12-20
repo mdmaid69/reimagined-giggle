9d055496e5e42794ccde31bd395a9fb5dbe38faf
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:43:22 +0000

    tip weigh blue situation force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
