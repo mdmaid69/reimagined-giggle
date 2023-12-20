3734ca93cb6c163ccdc00df1d2b35eefb4e36f46
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:12:21 +0000

    underneath brush themselves smooth still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
