c623b05df7c9d7caae30a69adc45f11e456fc558
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:03 +0000

    your software squeeze performance tip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
