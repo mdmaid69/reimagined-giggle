0a7f2f51093f74a6e3157388c8e0972d1305d714
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:31 +0000

    light rush Zz zero judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
