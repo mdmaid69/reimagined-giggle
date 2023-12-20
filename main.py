92e234febba28bcef167bd63d3e08b3ba75e5909
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:37:22 +0000

    Yy balance pure roof by

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+for i in range(10): print(i)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
