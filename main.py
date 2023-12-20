36a3cb7e04e6f5570d17ff41f80f39516e9cf02e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:25 +0000

    meaning determined everybody pair mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
