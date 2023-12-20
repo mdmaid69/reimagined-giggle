a5398834fbd425ce800b4913964d4dced5db15c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:41 +0000

    now foot clothes except almost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
