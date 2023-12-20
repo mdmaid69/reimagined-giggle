3e780f18e939d5b7efbb5ceec776772ef6ef4602
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:48:59 +0000

    shape when top raw different

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_user_list():
+        return collections.UserList()
