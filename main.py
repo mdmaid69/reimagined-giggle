03e278b89db6f85ede4325110e2fbb669f21b33a
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:06:02 +0000

    brave tonight niggaacademy round popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
