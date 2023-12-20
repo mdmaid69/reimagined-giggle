77668aab1b17746d954cf0663a3ad1e99f15694b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:58:14 +0000

    path memory damage deep nature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
