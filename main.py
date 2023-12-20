225498ff56090a27c226686ec7ef380f4809a9f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:18:05 +0000

    pretty pull scream peaceful finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
