7a4abd7a46c184471ca06bcaeac4a17e2dbb6e30
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:24 +0000

    upset hollow obtain them us

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
