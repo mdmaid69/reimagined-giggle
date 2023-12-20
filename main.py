fac7e485a9b1ffb385d2fcc5b2ab139d9b6d8d62
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:05 +0000

    murder otherwise opposite appeal excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
