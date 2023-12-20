fc03462b5aae1e58e72c91389fe7682c8c56cb4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:26:04 +0000

    listen solve who equipment wind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
