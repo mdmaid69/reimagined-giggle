ae5b68c87e8cb4eccc800db53242eca0a16140d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:52:45 +0000

    opportunity unlucky glad powerful effect

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def convert_to_binary(n):
-        return bin(n)
+def extend_array(array, iterable):
+        array.extend(iterable)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
