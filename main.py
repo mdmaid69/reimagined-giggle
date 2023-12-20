939dff25abfb2d4341dc633f9b3601bb93348171
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:47 +0000

    practice nearly complicated breakfast image

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_odd(n):
-        return n % 2 != 0
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
