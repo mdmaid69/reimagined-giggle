2c4f234e4b27e99d3e192abc28109d92eadc46b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:51:50 +0000

    fit storm fierce now town

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
