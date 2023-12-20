918914071bc8c18cf062d053698803f3ba250bdf
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:26 +0000

    ilovedicks usual ill scared experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
