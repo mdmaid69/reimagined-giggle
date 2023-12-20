e17944111162dd5fb12146ee4c983b67373f84d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:46 +0000

    especially recognize flood basic public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import collections
+def create_stack():
+        return collections.deque()
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
