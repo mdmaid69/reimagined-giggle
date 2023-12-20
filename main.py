f6b3d7fd16c830ec024960fdf000212269c37cb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:29 +0000

    boring identify regular people average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def create_priority_queue():
+        return collections.deque()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
