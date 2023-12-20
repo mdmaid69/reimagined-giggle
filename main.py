ed2afa0db4cadd2e3904459b651a39b31ca9665a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:46 +0000

    when impressive confused talk hope

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
