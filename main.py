67017bca4b705d75f1cad12251c80cef2b5481bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:19:35 +0000

    preparation sure murder sudden hate

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+print([x**2 for x in range(10)])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
