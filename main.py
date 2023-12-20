b6b8d4a285fb2de7cdaa85db09ca8ae0b88684cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:11:49 +0000

    play behind invitation compare fork

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
