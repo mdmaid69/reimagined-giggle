5bd6487fa1137afd71452a30459a32e0042d6972
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:57:45 +0000

    purpose deliberate rise me regional

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
