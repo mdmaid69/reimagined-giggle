5363f4bf4955b7c788ea99f94c7ddc6283650d7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:43 +0000

    exaggerate alone since approve offer

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def count_characters(sentence):
+        return len(sentence)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
