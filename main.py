54046111713a2bff19088cf8190cbec22d9c2853
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:02 +0000

    your whistle question run cap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sys
+  def get_python_version():
+        return sys.version
