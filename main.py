10c7b545a93dae41e8e2fe559fa57795442c301b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:35:43 +0000

    eye Qq exchange couple beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
