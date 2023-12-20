2792ce3ed3a34d2e243386f5e2b42ff711b362b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:55:32 +0000

    building comein pin find draw

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
