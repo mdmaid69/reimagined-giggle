85cbfdedfc03813cd3690ce65f9d1e489a2ad205
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:09 +0000

    behaviour hard rest sad off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
