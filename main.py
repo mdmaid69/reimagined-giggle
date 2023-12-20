23b3ebcb28b2714901c12d56029dc2be469b8e73
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:34 +0000

    punishment seriously side into in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
