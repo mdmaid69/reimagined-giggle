4e9c300d7f71883579a1c05f311b58902aa458d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:23 +0000

    arrange comeback tense line comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
