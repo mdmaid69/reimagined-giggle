40aa09ab604f834c46029de497b3787270de1ce5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:31:46 +0000

    square shopping same content butter

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
