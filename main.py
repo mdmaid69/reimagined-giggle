8ce2307f1eca40d92c6ab5fe6db83f50eef7df14
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:21 +0000

    port force able side object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
