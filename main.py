bb9e47c35e4a5079a481aa84030a2867982eb553
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:26 +0000

    conclusion flat temperature maintain border

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
