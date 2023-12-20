c2a4ed2917aa76db064b55d2f267325fec27f3e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:11:31 +0000

    steal mean sorry custom serious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
