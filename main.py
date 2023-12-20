6a68dbc61014ad41f2276d8248a53595de436028
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:40:58 +0000

    public breakin kill Kk top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(lst):
+        return max(lst)
