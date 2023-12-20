8c1332b3869ed46b22156c72d27e4f7088503513
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:47:31 +0000

    fine tend touch all bother

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
