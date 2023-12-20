1a3abe6be730f079d1f8edf7bd38789b3414169b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:25 +0000

    opinion holdon bin yes judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
