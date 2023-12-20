6db137ce5bca72358d51104753905d80b245da3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:55:07 +0000

    study breakin excited long copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
