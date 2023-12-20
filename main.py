edbb1b3e0daa6037711bd6d3c9a9b19b031c9cbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:35:07 +0000

    Ss number tool your write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import sys
+print(sys.version)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
