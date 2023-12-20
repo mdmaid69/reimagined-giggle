b6f291d2b30f5c03196a72d5902e52d0bf11b842
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:26:43 +0000

    during fault remove window ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
