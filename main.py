54347ad2ab86364c1c02eff3272bce0efe1a31dd
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:18:03 +0000

    amount how storm as declare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import datetime
+def get_current_date():
+        return datetime.date.today()
