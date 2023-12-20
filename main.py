f2569e79226ab2c7b3ba42aae470674212e20846
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:52:06 +0000

    black brave trick formal tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+text = "Hello, world!"
+print("Uppercase:", text.upper())
