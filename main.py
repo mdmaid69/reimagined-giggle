d13c8cf869c3d8d4a7e2dc6f718fbeef7e6e504e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:59:12 +0000

    along everybody callup heavy friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
