0fb875688bacb1e72c00bca86a1569c37f1266d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:11 +0000

    while similar rain problem suffer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
