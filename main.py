43c12282ce3167addeaecfb4953e03ce35ad18ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:18 +0000

    replace lookafter less recommend answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def multiply_numbers(x, y):
+        return x * y
