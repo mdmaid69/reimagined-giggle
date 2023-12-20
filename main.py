baf438a8cd3599cc22f61e91b8c645d3c5f81ac5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:49 +0000

    name bear disappointed love nothing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
