112a04f40943519768af1abc3ec7af6294388926
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:46:27 +0000

    dish judge watch bother unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
