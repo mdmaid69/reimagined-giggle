2df58fb0e168833c9bf08d1ed90cd439319bca51
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:07 +0000

    passage I walk consult risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_pressure(force, area):
+        return force / area
