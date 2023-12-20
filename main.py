08598208b13b1adf48941f1e0b7509ab1f98057c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:23 +0000

    arrive probably and alive appear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def convert_to_binary(n):
-        return bin(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
