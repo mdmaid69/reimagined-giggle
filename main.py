cea4e08eee1f7d1b12b7da206431ab4ed1257b8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:28 +0000

    worried maintain travel handle wear

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def convert_to_octal(n):
+        return oct(n)
