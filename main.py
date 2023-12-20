6ec0f10b53b5695c3804788fb84d4fde33b1e051
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:16 +0000

    beginning tell breakin after person

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
