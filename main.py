a514cbf623543e73f8243c943eea22528ba3cce9
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:35:51 +0000

    smile cinema pause pour suppose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import time
+print(time.time())
