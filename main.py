6958e30b483018e4dda9b0982eeb47fdc4df7930
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:17:05 +0000

    forward concentrate argue view perfect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def greet(name):
+        print(f"Hello, {name}!")
