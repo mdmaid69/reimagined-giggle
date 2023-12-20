fd05c01711442d7ee53b710a503a64498dd83322
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:40:36 +0000

    Zz shop lay community trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Reversed:", text[::-1])
