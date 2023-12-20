87d3e14aa0489b2298a763de76d59e1c4ab99768
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:36 +0000

    doubt little here arrive weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def is_palindrome(s):
+        return s == s[::-1]
