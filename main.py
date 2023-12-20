874c076ee0680154b366f57ad420593907d8bed9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:33 +0000

    hurry clever statement like tour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_int(array):
+        return int(array[0])
