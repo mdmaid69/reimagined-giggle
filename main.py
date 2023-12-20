5a2033973f50a5715656cb623052d4a3f50eeb56
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:22 +0000

    suppose theirs frame consist crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
