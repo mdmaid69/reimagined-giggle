c1232a7dbcf7e72cb74b52b58c3403cd759986ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:15:04 +0000

    comfortable hand problem outside question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
