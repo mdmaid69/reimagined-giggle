0f42d57fa802d630c03ebb4a4c6e1584cb82ca73
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:37 +0000

    blind clearly blame walk naturally

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
