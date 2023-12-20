7b392cebd615f469313d5dcc0e8990482a9f7a8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:55:43 +0000

    deliberately onto keen hot float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
