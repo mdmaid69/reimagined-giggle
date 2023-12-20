1114d431f8b6f380e98d59c88cc5da22e5f70297
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:40 +0000

    record suggest it strike dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
