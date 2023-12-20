9ac939f3a2f0b007eb174610647ed39697fa620e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:08 +0000

    record communicate me ought central

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
