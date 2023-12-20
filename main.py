774febce923d079c529099ba10af2aedb521efc5
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:58 +0000

    179bite store convenient influence in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
