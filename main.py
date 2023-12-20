5f35f3a6cf0fd9df4869682dd22285d84017ee26
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:38:13 +0000

    turn strict middle fill sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
