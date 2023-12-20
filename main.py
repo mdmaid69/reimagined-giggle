832497c9afbb1a1fd70586ce27df07b3d999d178
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:32 +0000

    slip distant murder in love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+print([x**2 for x in range(10)])
