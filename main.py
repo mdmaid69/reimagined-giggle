b8b9224e0c217c8b39196138cc356adda313c342
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:51:48 +0000

    back direct needle beside brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_string(s):
+        return s[::-1]
