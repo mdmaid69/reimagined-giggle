48a71c0a37571542889496f26715a9f86bfdccce
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:15:09 +0000

    many bread everywhere gap discuss

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
