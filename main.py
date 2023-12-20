dc88c7d613543469f831b911cdb1d1b4dcf80e86
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:03 +0000

    affect stage to number because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_repr(array):
+        return repr(array)
