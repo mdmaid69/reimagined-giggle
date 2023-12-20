24348701c61b8ab2b671770b7d81429cba458381
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:29 +0000

    definite defend style face consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
