08d32d5db2cd4b6ddb75c1c03fb01b00c57c7b8b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:25:38 +0000

    imagine performance cheat future dish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
