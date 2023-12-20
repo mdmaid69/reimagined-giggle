6c9b11336524ed7b92acda62411de0aea8c4c6b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:00 +0000

    goodbye demonstrate statement range money

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def get_array_index(array, item):
+        return array.index(item)
