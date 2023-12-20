01aaad3d0e4786722c2d0220b36ac386513e6fe1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:54 +0000

    impressive calm dream pool defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
