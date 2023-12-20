fb679bcfd70d8511a82464712bc06fcce3ea6c8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:48:16 +0000

    my adjective dream its here

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
