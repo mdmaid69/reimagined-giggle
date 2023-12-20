e71a3940144b845a027dacf7ead4191c762c6ae9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:12 +0000

    hear receive draw sure road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
