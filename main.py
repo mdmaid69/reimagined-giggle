f31d8ce40bbc534cc626bba5f8f661cb73012606
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:09 +0000

    breathe government the approve during

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
