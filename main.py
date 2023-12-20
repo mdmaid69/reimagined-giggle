e9321cdb8bc55846b1052b6b62552e4239c5237a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:14 +0000

    intend complicated season thumb wear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+print(datetime.datetime.now())
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
