bb75049f734301dc113b653e17d764649890009d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:56:04 +0000

    arm total immediate rid always

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
