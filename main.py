1e888bda6e6328bd9101f7a8aece7f0d159030d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:32 +0000

    connection arrive sleep there along

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_work(force, distance):
+        return force * distance
