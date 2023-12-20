c70c1ed1c7128c045b4768662b486b5839105563
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:13 +0000

    appearance country basically call mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
