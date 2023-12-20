3e4b16d1f00f5a255da8a6b9afcd786c990969b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:47 +0000

    recommend sleep calloff dinner delicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print("Hello, world!")
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
