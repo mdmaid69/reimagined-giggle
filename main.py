3d821afff6ceb1a5c135676add3bf70639b088f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:04 +0000

    morning only soon basic pick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_max(lst):
+        return max(lst)
