f0bac5e04a534da0e439bffd228b4848d011e309
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:41:23 +0000

    jump furniture arrangement turndown dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
