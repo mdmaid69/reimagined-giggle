308e6094cce81fbd10b82fffbafa6f3ba07e68a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:07:31 +0000

    next murder disagree hold heat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
