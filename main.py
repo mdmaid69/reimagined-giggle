2f454dda3bdbe8909e5824a9010ab2c9fc08317f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:15 +0000

    true bringforward understand vanish either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
