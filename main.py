d290c4a15d87b8fdb944c15510a299d9725b9b03
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:05 +0000

    him use punish this badly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
