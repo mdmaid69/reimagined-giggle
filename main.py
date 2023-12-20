bcea39abb08b1e20416cb5dc3236989f8898a5ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:04 +0000

    now conclusion survive content demonstrate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
