75d80c7dd27755df37c9f6ea61bf731155e1f1bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:35:14 +0000

    occur except choose used dead

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
