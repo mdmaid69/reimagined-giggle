0d84505107f70ca48aa3f1ac1099f0f169881ad1
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:56:49 +0000

    drive such arrangement through fold

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def subtract_numbers(x, y):
+        return x - y
