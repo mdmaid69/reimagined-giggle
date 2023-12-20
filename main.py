2ff9a9977425b5216574480e1bf772c3f8db4b1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:05:21 +0000

    best stone breakthrough score seriously

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def remove_duplicates(lst):
+        return list(set(lst))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
