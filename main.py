5337a812be1ea9fc7d316eb03c420517f7730a23
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:35:42 +0000

    out event arrangement thick part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
