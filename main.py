48e621feaee88da70ae6cf0be724dc4de1dcbf49
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:33:52 +0000

    judge hurt visit fine university

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
