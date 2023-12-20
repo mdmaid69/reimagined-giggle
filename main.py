c4d830e2bd590db311988a6e500b24da3f158eb9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:13:51 +0000

    disturb touch there trust difficulty

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def convert_to_octal(n):
-        return oct(n)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
