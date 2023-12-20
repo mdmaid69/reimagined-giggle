4e351e025852d2577e8a96d27ae5b4b3389b3ede
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:30 +0000

    deliver choice interested backwards cough

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_set(array):
+        return set(array)
