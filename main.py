bc9a62d7cd0450382a096616f1eca2360b07f54d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:19:09 +0000

    pay shout perhaps block pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
