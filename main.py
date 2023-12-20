b960b96177c68f1bcc50ec9d4e2bd6da9dcaa9aa
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:59 +0000

    fit scream smart identify insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+def is_palindrome(s):
+        return s == s[::-1]
+import array
+def get_string_from_array(array):
+        return array.tobytes()
