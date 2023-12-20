19a33999586d0bcfb76fdf09f5869a425ce89e4a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:06:17 +0000

    concentration lost fond forget hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
