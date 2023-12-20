a09394a2392474fe00e66f1e7bc8e0540e469360
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:52:59 +0000

    comfortable hard me attract feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import array
+def reverse_array(array):
+        array.reverse()
