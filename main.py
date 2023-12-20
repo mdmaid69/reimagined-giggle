91b3057a31e4b830df0390ee8d85b0b498499d4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:44 +0000

    custom treat part judge plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
