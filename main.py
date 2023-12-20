2f2ebe7507c53f77cd858952a6a37363f6f5b09f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:09:52 +0000

    greeting slice gentle without challenge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_volume(length, width, height):
+        return length * width * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
