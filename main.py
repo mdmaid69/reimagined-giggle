1da1c46b51f834062b85342a0e7f0d952051ec31
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:51 +0000

    rest least action half tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_priority_queue():
+        return collections.deque()
