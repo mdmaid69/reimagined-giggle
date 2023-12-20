ec42b1edb27b6d742effa9ed1f6c2296e94ba0b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:23 +0000

    lead regular past brush back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
