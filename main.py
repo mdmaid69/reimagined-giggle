e3b44b4bbdae583150dd87f64dd07ac3578fe88e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:43:46 +0000

    skirt mark whenever true basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_speed(distance, time):
-        return distance / time
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
