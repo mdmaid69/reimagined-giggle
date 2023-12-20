2ecf6839951ac4230fd81ea116cc65ff76c8dd1f
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:48:28 +0000

    loose arrive steam win only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_arc_sine(x):
+        return math.asinh(x)
