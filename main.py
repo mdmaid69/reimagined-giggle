5b1c13156bceeee3078dcaead266afadb66e98c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:08 +0000

    talk hardly yourself blame prize

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def sort_numbers(numbers):
+        return sorted(numbers)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
