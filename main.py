bb0f574b0f8fb3a27b5fbd536f54cf344fa09c57
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:55:07 +0000

    leave store know appear broad

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def find_max(numbers):
+        return max(numbers)
