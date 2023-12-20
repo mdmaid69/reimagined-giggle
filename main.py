e71b7099e8e633d5d42ec2acf688ac7488228ba5
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:24:22 +0000

    circumstance mad someone either difference

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def subtract_numbers(x, y):
+        return x - y
