bcdfb098d343fdb4f8aae2a39389367edf192421
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:01:22 +0000

    truth unhappy it since pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
