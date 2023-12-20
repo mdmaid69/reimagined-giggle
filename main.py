6ce6b5a88cc8a4149f791a0eea8b5b13b969c70b
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:15:13 +0000

    trip sort invitation wall cope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
