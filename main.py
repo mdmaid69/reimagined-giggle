1fd7ec93d76121970ec8178261572cc824c5e88c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:52:23 +0000

    morning to feeling probably case

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
