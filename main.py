e56df6e590381b6983b5cdbb8a7d1a42a001cb31
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:54:40 +0000

    close unusual gentle thirsty cut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
