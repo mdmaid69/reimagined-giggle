eb3b2375a1038f20ebb9cc004c9e08f699ba9b3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:46 +0000

    mind history plant welcome whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import sys
-def print_python_version():
-        return sys.version
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
