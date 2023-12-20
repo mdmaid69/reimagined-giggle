f8af6018f09d8ce7b222f95a05bba8a11afad54c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:26:02 +0000

    kind type pollution discuss Bb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
