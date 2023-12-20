f9277b639da4487e2c965162c6de5cc7ba0df383
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:37:57 +0000

    near bad possession taste left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def reverse_array(array):
+        array.reverse()
