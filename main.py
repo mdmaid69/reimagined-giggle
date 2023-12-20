fa07992b4bb10300daef543d8ea3f2ac02f21e8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:33:02 +0000

    story telephone luck think production

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
