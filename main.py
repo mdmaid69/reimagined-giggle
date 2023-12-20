46ed156fc12c6fe4988302d388c741ba4d10c969
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:32 +0000

    awake town bring watch university

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
