ba168ee1990ed0f678d0e2dab74365367ff24c14
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:17:03 +0000

    pronunciation office runout refer pressure

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
