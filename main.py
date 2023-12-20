1436c3eedece15f03940383c731f9fccb439ab9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:43 +0000

    surprising OKorokay lucky middle bother

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-print(math.pi)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
