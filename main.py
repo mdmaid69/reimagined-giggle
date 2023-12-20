fd375db3d9f86de8083f293eeaa5c807de82c611
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:31:47 +0000

    lead Ss less instruction because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
