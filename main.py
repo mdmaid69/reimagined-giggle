d3d0f296c70e5d04a7d7e726f5ee05dbc6f7026b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:19:30 +0000

    apply tip own stone fear

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
