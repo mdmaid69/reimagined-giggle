85ecb98a0646e4fd788c0af3264e936d2d46ad93
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:26 +0000

    promise side little difficult opposite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def create_numpy_array(lst):
+        return np.array(lst)
