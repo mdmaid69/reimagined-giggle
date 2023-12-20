3475200c0ceb235525e609c4ef15aed4d038a50b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:09:37 +0000

    communicate trust new available unlikely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
