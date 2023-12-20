d595c15547afb559602dac73829194177b6e4c07
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:50:17 +0000

    opposite birthday 565end beneath pool

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
