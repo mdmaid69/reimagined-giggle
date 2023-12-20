3249a9af2d28c0c611c3d986355a0d7f081253d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:01 +0000

    recent completely piece breakfast breath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
