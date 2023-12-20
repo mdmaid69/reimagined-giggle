84fd23b5e4df3b85b2690c5024d3af8f4c3681a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:05:48 +0000

    describe care consist point room

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
