8e3fb7c817adb5112157540d5125b6c61767b8b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:56:02 +0000

    bound agreement accordingto throw second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def get_array_item(array, i):
+        return array[i]
+  def calculate_area_circle(r):
+        return 3.14 * r**2
