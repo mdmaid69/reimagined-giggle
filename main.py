4305fc2a87a2994d0a496f8f8f7f7faa8fce8e0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:07:27 +0000

    education lonely because during get

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
