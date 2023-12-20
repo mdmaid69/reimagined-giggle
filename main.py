ed1e660708b18a0a0de19732121ea787e3180693
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:33:23 +0000

    limit prize lend file dress

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print("Hello, world!")
-def calculate_area_rectangle(l, w):
-        return l * w
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
