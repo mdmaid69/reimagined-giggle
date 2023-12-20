01692280db09fc20475aa6aaa743e4578ee129fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:21:34 +0000

    possibility turn circle various interest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
