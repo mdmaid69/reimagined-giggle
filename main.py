1069b942aeeb09fc538f9c84a3cee662391d91da
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:02:18 +0000

    everything criticism factory insist heavy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
