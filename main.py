94dbf492ba2b8cafc295e0f83caacb6edf7f2162
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:13 +0000

    stiff wear standard might force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
