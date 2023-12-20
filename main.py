1f13fd38ec361bdce1948784cfe5a39d3039aeba
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:52:56 +0000

    weak pour chief straight become

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def sort_list(lst):
-        return sorted(lst)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
