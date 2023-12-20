14c1c8146adc41d73f591b1e32665cf90512d3a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:38 +0000

    tend unlucky sale only street

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
