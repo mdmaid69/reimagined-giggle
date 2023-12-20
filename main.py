b40e8e0eda63df2aae5d5942d35b4ec64af91df9
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:13:25 +0000

    comment eat 566end way need

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_directory_name(path):
+        return os.path.dirname(path)
