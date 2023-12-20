137939bf9f64d2801009ec3bd433faec51d801fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:49:43 +0000

    important mistake rush reserve angry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
