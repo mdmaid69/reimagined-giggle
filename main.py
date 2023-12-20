5e7d3e4c64a41377c612122ad10216389e2f2992
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:09:12 +0000

    name comeoff proud bridge comment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def add_numbers(x, y):
-        return x + y
+import random
+def roll_die():
+        return random.randint(1, 6)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
