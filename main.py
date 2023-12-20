4669a51941e86ac43616805db9508cde0c4994e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:20:21 +0000

    maintain bridge easy hard wish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
