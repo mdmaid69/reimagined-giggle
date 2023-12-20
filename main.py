617811ad4c4d30c89e7d048bacb8a4777fe38e60
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:01 +0000

    gradually successful above flight juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
