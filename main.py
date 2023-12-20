ef3c9cf362c8060022562e090442474d81f6f6cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:16:39 +0000

    strike object by add search

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_power(work, time):
-        return work / time
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
