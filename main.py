c9e18567e5959427cd48406ed1cb6a73572a5149
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:58:53 +0000

    wrap very convinced keepoff where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import sys
-def print_python_version():
-        print(sys.version)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
