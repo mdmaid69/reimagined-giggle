232cab7c6d8dff939c9d49797a7e3fb31288a88e
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:53:39 +0000

    machine design row address limit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
