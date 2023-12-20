d945449ee53e01f5325b7a93d9621d8f549c74c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:38 +0000

    turn lookafter breath he forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
