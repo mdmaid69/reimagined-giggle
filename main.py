021ff247094e8b96c8fa4875cdfd2991e0841a0d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:06 +0000

    minimum strike war fence part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def square_number(x):
-        return x**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
