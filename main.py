0e21921ea5ad3c31b579ebb61110d65676ca3309
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:54 +0000

    male same sink dark responsible

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
