39178acf5cf822e3e1621180d98ae3ee79e61dd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:15:21 +0000

    no pronounce clothes apologizeapologise light

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
