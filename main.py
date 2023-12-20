705cf1dc2e06cf3391083abf472e94bc9272a8a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:06:40 +0000

    schedule patient picture fast proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
