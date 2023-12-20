218dc541eb2a96dd574280031844e82c102cc93a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:56 +0000

    position outside go finger brain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
