7e1371d90154ddc6c4d25c777e69590539b0ef9e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:36:09 +0000

    apologizeapologise them always sort safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
