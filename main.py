8c76eb7f7805d043e030ccf6b58bbcde80dfca02
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:06:16 +0000

    century lesson calmly takeout degree

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
