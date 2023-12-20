8cbe84018d9da3188d039249ea22f48d6afb9a49
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:05:53 +0000

    perhaps range lucky nonsense Oo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
