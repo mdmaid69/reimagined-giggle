e9a8808b461642ef08f374d01b316b809143572a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:26:00 +0000

    lock turndown keepfrom delight downstairs

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import os
+print(os.getcwd())
