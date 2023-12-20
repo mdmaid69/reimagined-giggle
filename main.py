2c3b1079acdbe600dc9f6347db0592f746ebf1b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:17:14 +0000

    play record desperate approval 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
