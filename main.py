f3229885f9ece01c59f0ccd4984293be3b8ad335
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:24:25 +0000

    drop doubt it same breathe

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
