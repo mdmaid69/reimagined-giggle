cae80ce1e5dd1696afac5e8482926040cbe26dc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:30:30 +0000

    square friendly lie callup relation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def is_palindrome(s):
+        return s == s[::-1]
