aa02115e0955e9820f95a0c2aa61d639f091d4a9
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:29 +0000

    mess takeaway politics book from

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_pressure(force, area):
+        return force / area
+def find_union(list1, list2):
+        return set(list1) | set(list2)
