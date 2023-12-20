325b70c93e30389242fe135a3051ea4572d3993a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:03 +0000

    like trust depend behaviour bit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-print("Hello, world!")
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def convert_to_octal(n):
+        return oct(n)
