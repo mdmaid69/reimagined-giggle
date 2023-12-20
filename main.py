6a2da67ed7ecfc54c9ae5f274314f35d36c1ec20
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:24 +0000

    runinto quantity sudden Ss work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_density(mass, volume):
+        return mass / volume
