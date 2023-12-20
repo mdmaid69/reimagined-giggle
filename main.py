1b847c26a8b14a18942569e5dd9cb4f9eba2eb37
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:18:03 +0000

    shout its provide fit label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
