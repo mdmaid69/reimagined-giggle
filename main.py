17254cca53db46c7909f5940d89fe01189ff1d18
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:13:01 +0000

    smell trade air afraid bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
