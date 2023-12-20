05e49da6baf6930f84c1ad5878e13fccd95bf2c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:24:53 +0000

    regional news score concerned tread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
