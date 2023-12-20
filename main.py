4f5b1c0bd4f2732aa3880e3369c3a7063b1b5ffd
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:27:49 +0000

    bush order man crash measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
