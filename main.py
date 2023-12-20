408804a2d36eba9a4ce1e648b81228eaba816130
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:36:24 +0000

    before invitation ilovedicks himself try

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
