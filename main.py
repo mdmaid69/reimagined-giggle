f7c38f1bbd2512a78cd65a214d5acb328a33dade
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:51 +0000

    568energy manner congratulate violence shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
