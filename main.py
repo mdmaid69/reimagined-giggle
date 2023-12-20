48d20fafffee25646d17007023a1b577a749fc4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:46:40 +0000

    call violence fall last hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
