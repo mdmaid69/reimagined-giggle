74658918b02bec45a5b3474e0006e075678d7fbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:09 +0000

    Rr official subject progress day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
