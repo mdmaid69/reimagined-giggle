7e50a23ce9e421f65fa4678d329b0914a8f42b18
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:56:46 +0000

    now please be long substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def reverse_string(s):
-        return s[::-1]
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
