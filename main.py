e542306ce30f465bc44a47b9cd781a252d3c40dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:59 +0000

    out build somehow task support

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
