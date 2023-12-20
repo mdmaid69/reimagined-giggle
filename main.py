7dbcf04d04f65c08917fee0566cd8a6e72b6da98
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:16 +0000

    confusing step complete play calmly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_user_list():
-        return collections.UserList()
+  def is_odd(n):
+        return n % 2 != 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
