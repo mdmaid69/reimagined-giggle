d37b0bfe0cb465bb473ad367ca081c7582ef5e57
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:37:38 +0000

    Ww allow on method badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
