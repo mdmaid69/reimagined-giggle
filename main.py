705e03b2800b639023259cf9ab3d7d4aefb18675
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:10 +0000

    piece discover news hole mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
