2bbd3987a092ebbab78c0ce49cca927606d23a12
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:32 +0000

    shop be stage science home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_unique_words(sentence):
+        return set(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
