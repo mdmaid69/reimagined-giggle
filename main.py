821bb618311dacc922d4c580cbfd5f4ae6489fa2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:17:16 +0000

    translate once earth game steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
