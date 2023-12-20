4260b2c73642195349472f529a22390b823ddca1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:22:43 +0000

    guess exactly lunch beginning activity

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
