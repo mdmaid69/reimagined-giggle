bcc3a2417be5ee2fc0ad1088fa36cd5fee6af619
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:54:23 +0000

    most choice dark pollution agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_absolute_value(x):
+        return math.fabs(x)
