c85c8516a8e5f458270963ba6ab7c4a1e16bfa5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:53 +0000

    clean callup 570enjoyable team affect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
