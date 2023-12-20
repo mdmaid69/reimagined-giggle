2596c43f78c7668f0e58a0a897c2b97fcbc8abec
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:07 +0000

    steady stop tired relax 570enjoyable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
