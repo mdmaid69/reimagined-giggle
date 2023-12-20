87c766d44dd80233abe04305f526f607d749a9ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:53:03 +0000

    to office mess tidy minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
