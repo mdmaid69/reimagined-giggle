b4735617c29a1d7ea9c63786925636ec8d6147db
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:56:54 +0000

    prize instant continue habit create

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
