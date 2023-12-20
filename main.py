5892ab20332bdcd5294cab6f1cedbe500be9bc60
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:02 +0000

    anywhere hold give hungry movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
