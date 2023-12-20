107dd61cc31611e78c03c67474f5dca75590f939
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:11:45 +0000

    worried explain period turndown write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
