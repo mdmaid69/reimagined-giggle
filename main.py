59ab0aff51093bcf82fcdd23a4a40c834134b73b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:36:15 +0000

    objection respect occasion soft style

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
