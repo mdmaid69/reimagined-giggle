23bc6bcd8620d100095fa3dda426de5f83292260
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:45:58 +0000

    mean intend tap ever remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
