e6cecb3bc251c04117d54b879adfd505897bd63c
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:07:05 +0000

    wish its laugh dig hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
