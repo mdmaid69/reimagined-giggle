84a62c15d259f7c338fc5707af6846534533540c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:39 +0000

    miss chance treat work brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
