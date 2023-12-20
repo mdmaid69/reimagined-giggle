8c79e7e990b49800805eb171ff4aa37f04389092
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:47 +0000

    concerning present worry include whole

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
