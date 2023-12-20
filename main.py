2c1c1154acc1ab1fb2daed2c4641d7266c0bbe7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:39:06 +0000

    careful should throughout Vv recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
