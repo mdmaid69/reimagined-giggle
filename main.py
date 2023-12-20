c1b7bd502475a4cac8bed7d7b0d30ef7cf97e2d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:44 +0000

    shallow hate ring task bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
