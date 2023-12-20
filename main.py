e5c1c3a334cdc58219dba02cf468db18b05a4815
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:56:11 +0000

    break position prefer typical loss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area(radius):
+        return 3.14 * radius * radius
