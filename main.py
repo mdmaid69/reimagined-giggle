84035e670af9bbdd40e01fef7c6ae509d90f0f21
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:25:45 +0000

    disappointed still weigh strike coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
