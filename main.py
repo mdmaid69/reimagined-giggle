8d0715e3d0cc98d387f09e35b9f5e718cd8b7062
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:28 +0000

    care scheme respect thorough from

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
