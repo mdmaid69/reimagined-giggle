385443892c711f15336905fa8911dbe4391c22ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:02:32 +0000

    book fast soon term afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def is_odd(n):
+        return n % 2 != 0
