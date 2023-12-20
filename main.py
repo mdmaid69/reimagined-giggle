19ed3f81f98baa9886bf380d9a014a1486874df2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:59:28 +0000

    bag hot ashamed correct message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_base_name(path):
+        return os.path.basename(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
