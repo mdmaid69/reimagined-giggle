208a501d371172ec3078f0c351a2ebd26b690965
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:18 +0000

    face at why meeting copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
