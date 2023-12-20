f906e439fa3f56ffb2d5e6d9cd4b93ab6490b8a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:03 +0000

    surface material dinner release sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
