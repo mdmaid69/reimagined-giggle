bdf107604013d4d5a45f5896cf456c676d7a618c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:45 +0000

    manner pollution control meeting giveup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def cube_number(x):
+        return x**3
