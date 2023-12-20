47f258b100af28670bd10d7467a92679c9002ff1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:31 +0000

    have sting fierce voice season

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
