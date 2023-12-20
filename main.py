bb36dccc0993e122ca5bcb5f1cc6f9809ae3ec54
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:28:21 +0000

    hungry cope appear shade proper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def sort_list(lst):
+        return sorted(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
