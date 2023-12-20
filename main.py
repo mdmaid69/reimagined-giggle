2a5020e4ff6f8e7191bb6cc716a4faa358ce873d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:47:59 +0000

    rescue 571enough good base represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
