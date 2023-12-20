83fde816a6a1ba2cbf1c8f077385a6e3956f626d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:38:28 +0000

    absolutely underneath thinkover consult suffer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
