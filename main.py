3ce75da3b1da3888a38a13050585fb546518d99e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:34:18 +0000

    smell doctor pressure present satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
