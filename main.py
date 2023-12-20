8f73577330b9466d269aaa22eb2c023d4eadca1b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:47 +0000

    force equal touch market press

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
