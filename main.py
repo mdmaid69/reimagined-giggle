8665b37bc3ce73bcdcd3b5e7807b51645f06f5f2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:22:02 +0000

    all letter anyone himself temperature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
