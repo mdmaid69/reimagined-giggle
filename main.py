1a1d404189bb70b8b1e617b52ce7191754995d60
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:08:33 +0000

    pool excuse relationship personal throw

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
