cfbe296bdc97f400ef44ad94b5cc34bf0db8ac72
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:38 +0000

    as strength open consider conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
