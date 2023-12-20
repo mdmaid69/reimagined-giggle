971f52c70e21cdf888362a7883ef36dfbce08b65
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:55:58 +0000

    demonstrate attach habit unable lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def count_elements(lst):
+        return len(lst)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
