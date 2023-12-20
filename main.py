cc48fb8d6f8c7bba9d01574ce670734edb3d875b
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:12:05 +0000

    offend believe crisis must ourselves

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
