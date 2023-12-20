4fe3cd3e2d7004a636ed214de523f15bd62aa261
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:28 +0000

    lift carry achievement short mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
