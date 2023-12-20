54531c38904842f9dfe673ae15abe16f58fe046a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:31 +0000

    introduce doubt fortunate importance wander

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
