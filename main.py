658b8a4a717b9947a304b93d14d3c5b02f2d3278
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:34 +0000

    advise sell object programme wire

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
