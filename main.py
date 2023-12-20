157238a52a627ec8840ac508b5fb57b38decfea5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:49:58 +0000

    affect yes all die OKorokay

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_words(sentence):
-        return len(sentence.split())
+import platform
+def get_os_info():
+        return platform.uname()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
