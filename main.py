0d03cd9a9af6b1f14c885bd73bacce575bc38ca4
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:32:17 +0000

    ought short mean mess tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
