2a849e7161b724936cd291c83be155e1d347c82c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:23:31 +0000

    workupto room there certain naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_str(array):
+        return str(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
