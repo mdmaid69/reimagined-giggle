e78e3bd6afa97141c887a52f3117d416187b9b43
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:02 +0000

    poison safety goout happy lost

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import random
-print(random.randint(0, 100))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
