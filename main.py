d0f01c01f9066e35888fddbc66adc5205e344c98
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:36 +0000

    calloff sense nervous deaf patience

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_octal(n):
+        return oct(n)
+  def sort_list(lst):
+        return sorted(lst)
