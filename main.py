41b042ed61a073edab96860ecb8c19342ff5da72
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:24:14 +0000

    problem have friendly free decide

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
