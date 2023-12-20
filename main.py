3dbd85dd018e61a7304943301eabf5b1c7360c7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:18:34 +0000

    to frame share breakout cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_speed(distance, time):
+        return distance / time
