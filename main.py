2d9874aef02476b69d78891fcb666c71204b3b11
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:09:15 +0000

    beat fun interrupt reduce people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
