93d51892d3911f0f1f851873ddab160f8ee94784
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:50:42 +0000

    Ss window emphasize past light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
