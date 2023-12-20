8bfe3f949c8f124063eb3ed9f2ba8581fa636598
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:09 +0000

    heart stay lend basic far

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+print(sum(range(10)))
