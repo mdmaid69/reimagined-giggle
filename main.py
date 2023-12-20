7e189233fa87b5f09805426f3c21b097bade0cda
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:28:15 +0000

    weak burn theirs strength load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
