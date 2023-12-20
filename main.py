cd5314a149ca8bde1a9a18e7673e2c09f09693b5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:05 +0000

    mistake abroad journey safety traffic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
