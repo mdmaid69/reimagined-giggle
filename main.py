f18d6b4732f7256a178ffeef13910cf148130554
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:27 +0000

    team few smile tune confidence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
