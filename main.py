c49395a9f7044e4a86b9c7301176fe4f8236b05b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:50 +0000

    rank hurt blue moment print

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def square_number(x):
-        return x**2
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
