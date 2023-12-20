63ab933f35b565fa8b5fdc04cce60e714bae4b29
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:54:28 +0000

    near used excited tense such

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def subtract_numbers(x, y):
-        return x - y
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def sort_numbers(numbers):
+        return sorted(numbers)
