bd5239e70d166163740ce2f574b492122f35cc4a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:43:58 +0000

    Uu him march mystery communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def count_elements(lst):
+        return len(lst)
