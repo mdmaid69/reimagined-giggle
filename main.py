5828287f47db4e742f54a2f580d0305f750952a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:10:51 +0000

    their peaceful bath paper rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_unique_words(sentence):
+        return set(sentence.split())
