02572c46cb541321ef84b1329d5ade8691e41427
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:17:45 +0000

    swallow line boat perfect receive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
