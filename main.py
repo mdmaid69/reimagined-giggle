537be248cb9894ec4d77b8c9d73eb233a7aaeb6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:33 +0000

    drive office too fear someone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
