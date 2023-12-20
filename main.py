d898e8cfd5e40c5ea8494e62bb25b815144dd9bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:46:49 +0000

    book opinion force weigh serve

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print([x**2 for x in range(10)])
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
