33dd35dd4069ae91b73a16d76f6958ca9416972e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:31:35 +0000

    dry average ring react gooff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_item(array, i):
+        return array[i]
