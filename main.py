578b2bc3814b29c34930bca125d6ea96f8a069ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:28:31 +0000

    ring tense ordinary circle lucky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
