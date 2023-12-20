db804f6ccb96f4e4548de377c506e04acf6947f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:39 +0000

    appearance then interview Ll follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def sort_numbers(numbers):
+        return sorted(numbers)
