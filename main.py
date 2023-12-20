7ceedb2620c6a617ada929f7c7e765866a2de27e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:33 +0000

    shall instrument fire worry beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
