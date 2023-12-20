4c5c863676906eb2a24c4f2c765fd7505fb6de41
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:02:56 +0000

    think fetch weak centre feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
