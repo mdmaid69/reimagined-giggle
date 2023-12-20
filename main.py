4d1cb95d55ceadddd7655962b00e8d2ec1da17a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:30:23 +0000

    control colour book pinch information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
