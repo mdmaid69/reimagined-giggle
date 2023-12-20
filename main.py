ed128d04690a59be83ceca2e145e69901bd6c256
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:14 +0000

    nowhere ought count hit empty

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-def count_elements(lst):
-        return len(lst)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
