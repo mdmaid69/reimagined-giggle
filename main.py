4be760a60a67f9fe6ae283e11270ab2b73d64f08
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:05:10 +0000

    vanish escape ever hardly body

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
