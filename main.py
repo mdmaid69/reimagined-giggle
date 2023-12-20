cb80f760e2299712d0f0432ee6ed0002010d0a49
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:23:53 +0000

    along long frighten development perfect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
