2642308042cf145cf20e23e68dd0c312df2c3208
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:52:58 +0000

    train form software attempt invite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
