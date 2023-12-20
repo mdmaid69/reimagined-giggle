eb02b0210eb4b9bd6d60837aeb7f9721386ce5dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:59:47 +0000

    clever building soak discover simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
