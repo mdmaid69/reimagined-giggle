34165198e38eccfa7da6dad7aa582c823929f6c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:14 +0000

    hole side advanced rise smell

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
 def fibonacci(n):
         a, b = 0, 1
         while a < n:
         print(a, end=" ")
         a, b = b, a+b
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
