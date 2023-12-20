bf43f53cccef57c73391245a19d286a933f71631
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:55 +0000

    positive closed influence finally protect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
