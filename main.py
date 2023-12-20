dbfd962f2414b56d688feadb1de7dac44ea361a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:08 +0000

    let case pronounce personal hope

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
