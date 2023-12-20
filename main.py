783a6e8f1da107fbdc7425a69fda8e65cd438b75
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:19:18 +0000

    march wealth equal come file

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
