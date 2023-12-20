c66c306f0d260f1dcd6a7dcdbb6dd79814c34803
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:04:59 +0000

    benefit extraordinary lost drawing crowd

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+print(os.getcwd())
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
