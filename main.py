4a0e9275ce2e12097da69c3feb637404807ae502
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:39:41 +0000

    spray popular frighten spring risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
