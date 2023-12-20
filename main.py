8984f63d188d62ccb6fec689b7db3bb16e8337cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:12:55 +0000

    post dance direct job speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
