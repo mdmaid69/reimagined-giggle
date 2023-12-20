fe99e8630291a2cf99e17e5f533c8040cb7a3a3e
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:02 +0000

    classroom proof holdup row mixture

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-for i in range(5):
-        print(i)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
