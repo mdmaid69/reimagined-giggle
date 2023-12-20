0f7052a1a75288ae1e354674fdc9673660ee8c00
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:08:02 +0000

    second escape print thinkthrough popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
