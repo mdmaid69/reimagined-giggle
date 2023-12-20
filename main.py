f77cf0462e1d768e5ebc9a7a4ac39d06ecf763d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:23:06 +0000

    eye train holdup score commit

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
