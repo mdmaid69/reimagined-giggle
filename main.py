23bb0bbdf1aa94439165152e2b17dcb5ce2e3ac8
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:24:02 +0000

    power dress on pause you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
