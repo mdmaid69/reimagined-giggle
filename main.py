b54d8c70b33db42ce00b3edbd7cb538c1aa1cc4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:24 +0000

    waste material fear stamp control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
