8d7da2699b043a4aaedceeb5b53181a98edd2f80
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:39 +0000

    pride seem makeup glad lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_string(s):
+        return s[::-1]
