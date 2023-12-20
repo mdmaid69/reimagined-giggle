f37c7f84ca738e2e2d77a8b9e9ac5bb6238b605c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:45:30 +0000

    transparent stamp bad university of

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
