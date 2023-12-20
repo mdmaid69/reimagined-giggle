b020da938f58dfd3d303b5aa043c75a68ee8936b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:16 +0000

    sink pronounce coffee no nothing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
