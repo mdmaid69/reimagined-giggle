6e8d6824e1e9963670dbc8f62d723efe31b0a95d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:50:34 +0000

    past part scrap pipe suit

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def sort_list(lst):
+        return sorted(lst)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
