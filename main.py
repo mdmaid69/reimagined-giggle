2abc33ff513fa8166702ba3c0233ba04b68a20e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:29 +0000

    flat future taste fold load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_tangent(x):
+        return math.tan(x)
