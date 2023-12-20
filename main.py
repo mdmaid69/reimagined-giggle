2de66ece12993d5460e34b0d6074322e0bbbb1ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:27 +0000

    use private far lot be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import time
+print(time.time())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
