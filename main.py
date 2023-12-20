16e8ed0452b704fe6ecfbb38e44016334f9f7788
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:01 +0000

    sweet chase already bury present

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
