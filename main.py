50ea6db913e1680d4b601c9dbcf122f26b656d3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:30:42 +0000

    lay basis confusing string without

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
