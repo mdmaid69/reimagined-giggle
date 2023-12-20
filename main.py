c41f4e33e22e24c2fb5f677759bf17fadd330398
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:43:34 +0000

    announce welcome colour at become

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def square_number(x):
+        return x**2
+  def reverse_list(lst):
+        return lst[::-1]
