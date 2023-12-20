a7651a484a8e20c74206fcf1235ee8fe708a430f
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:13:49 +0000

    crowd declare doubt desperate love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
