dc4211996ddbcdadb9c39fb941e39a6a9e6a82bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:08:14 +0000

    behind proud noise cheek brush

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_int(array):
+        return int(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
