be57ea7d8f991b926e59a065357e22f215c398d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:30:28 +0000

    wish burn force empty desperate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
