7d489f35ff096983fc52bac803d3269c5ec53139
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:01:19 +0000

    post tape manage sit ear

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
