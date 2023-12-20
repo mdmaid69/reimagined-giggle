9124459856e014a6187fdc20a3cae362d488e0c4
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:46 +0000

    opposite welcome fail delicate care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
