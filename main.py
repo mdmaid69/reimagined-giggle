8e844dc088adae856c3c055a9405b208604103f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:30 +0000

    getout search wish pure down

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
