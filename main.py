b34061bbf763dc99497d02ddd605c9604d79a1b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:02:51 +0000

    lookafter square back now copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
