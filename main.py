0fa97c3b01c0c2dbf80d91d7d921eafd31c2c734
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:02:55 +0000

    children handle dance arrange listenfor

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
