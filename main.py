84ceb209d6223e3ca8f58e7e85b929702cd722d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:18:15 +0000

    single defeat meet politics science

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
