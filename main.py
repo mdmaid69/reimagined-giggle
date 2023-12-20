63c28529f20ca7c9016a75dd06e1fec75254eb94
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:27:45 +0000

    software pause search fail wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import array
+def clear_array(array):
+        array *= 0
