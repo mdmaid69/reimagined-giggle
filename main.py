98d6c5ae26fa1f059909c057a481d15846e36f7a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:46 +0000

    surprising cut instant message number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
