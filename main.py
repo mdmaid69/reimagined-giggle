2c2e6ccb9ba3d82055677ff38f8a81139617dd8a
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:14:35 +0000

    danger chief narrow extra joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
