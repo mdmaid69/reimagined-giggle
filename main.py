f0d41bff47bf5e788d0fcc2f7b57e3ec7925c526
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:41:57 +0000

    off by meeting reduction tone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
