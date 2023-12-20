925ef3eb3477d3425813513b6bd3077f0346c04f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:09 +0000

    middle sore though break world

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
