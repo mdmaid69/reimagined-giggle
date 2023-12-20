4c9f89a45b422cdd900812432750937f7dc776ff
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:10:13 +0000

    feel exercise justice yours month

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+print("Hello, world!")
