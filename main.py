03c02a81b4c16c6383aa975d487cd470ef851aaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:51 +0000

    emergency question practice subject turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
