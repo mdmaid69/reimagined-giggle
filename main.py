7440927fe28f0dde02e27a8f221b459953719999
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:06 +0000

    show lift acceptable giveback old

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
