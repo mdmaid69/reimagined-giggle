c1cecb2ef9eadb245d4552a03c2fea6a45f6fe72
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:17:16 +0000

    left centre gate list outside

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def count_elements(lst):
-        return len(lst)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def calculate_area_rectangle(l, w):
+        return l * w
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
