612b92c82b143677e3ba335d96aa8f6a74013b24
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:03 +0000

    comein once press hollow conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
