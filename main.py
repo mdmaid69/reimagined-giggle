fd00ccdde6e07159bc6af76355300e6e43c12bbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:37:04 +0000

    fit male 568energy disappear extremely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_counter():
-        return collections.Counter()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def convert_to_octal(n):
+        return oct(n)
