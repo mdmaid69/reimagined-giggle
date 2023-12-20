2076b34f2b2f850bf867c866c3af789a17d763c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:41:46 +0000

    which drive search pipe they

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
