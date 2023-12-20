edbaca947740fa0eabca6173eced2bfc867a2ba4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:19:18 +0000

    ashamed tour either runinto wrap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def subtract_numbers(x, y):
+        return x - y
