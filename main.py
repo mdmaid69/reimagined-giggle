005d818190e915772426b7cf8c259e64b0bd742e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:13:43 +0000

    motion burn repeat public considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
