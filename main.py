864301423e8708bcc1c85adaf7b36e4699895ff6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:01:50 +0000

    justice tomorrow ashamed marry tense

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
