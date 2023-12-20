5b34b2f12d47675bdd4b5bb2d0c39e95b652f9a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:02:41 +0000

    anybody spin careful success corner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+def find_max(numbers):
+        return max(numbers)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
