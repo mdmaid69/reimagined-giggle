a8b4334316212bc6ddaaa214c3d30472b798d624
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:24:57 +0000

    private mean valuable sort square

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
