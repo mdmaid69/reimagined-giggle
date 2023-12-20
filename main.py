2bb3ccb4d9d570a8e7433e846b403de3367a0468
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:00:14 +0000

    in congratulate pass by injury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
