77e23f61b48f46981c95c480ec49681ddb6f25ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:33 +0000

    system exist suppose by meanwhile

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def convert_to_octal(n):
+        return oct(n)
