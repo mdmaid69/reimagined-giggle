edde9e3524591a81f940d56c91d2c380479c7718
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:50:03 +0000

    certainly wing comeround push large

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
