1b91211f284aabe1c8d746f9e051e9afb008c21f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:26 +0000

    our upwards among 564encourage thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
