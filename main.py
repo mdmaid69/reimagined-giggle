9fc76c5d9fc48250c1040ff728b435c29f924a5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:56:32 +0000

    brief but stiff sore must

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
