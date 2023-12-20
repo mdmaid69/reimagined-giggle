f300f27c5ee276044630e42552749c31342b8012
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:46:53 +0000

    length issue succeed plenty contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
