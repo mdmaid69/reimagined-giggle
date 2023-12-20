e25183737752477ce207a6493b6bae4f8ff3be05
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:41:56 +0000

    worst relative thank takeaway fetch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
