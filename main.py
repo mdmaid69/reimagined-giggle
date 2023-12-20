3c2f5c1ce69493a5c7edf7bd3392f96185e8703f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:03 +0000

    organize exist run use protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import time
-print(time.time())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
