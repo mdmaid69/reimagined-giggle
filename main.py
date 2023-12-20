38f0c01c8c234c4707f2d272ec660597d5c6a51a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:51 +0000

    whether refuse lead swallow instead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def convert_to_hex(n):
-        return hex(n)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
