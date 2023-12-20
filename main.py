aaa22af713a79128af57745d49443f761413b6a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:39 +0000

    school geton contact herself command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
