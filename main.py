3a8e215eed61c3a077c8893be8dfd56f316ff9c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:11 +0000

    birthday secret admit choose replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
