9a740c0bfc570b888310b811be8cfb6d661d165d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:39 +0000

    effort conduct height difficult anywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
