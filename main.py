0ea2afbf9bf8c686c5fb1c6ef1d87577c58d6e40
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:12 +0000

    Uu violence yesterday especially throw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
