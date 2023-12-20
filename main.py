ce76706cfe8b4c2028e711e2afa21c3cddb65bb0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:50 +0000

    finish way shake cold precise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+print(math.pi)
