7e90fad1279083112840730e249f1a29d7ada960
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:14 +0000

    turnover previous considering content set

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
