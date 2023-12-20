c6da40f86a14fd315c846e398ce46ea97519caf2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:25:09 +0000

    friendly suit deliberately lost hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
