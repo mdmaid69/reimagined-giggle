d439f9f53ca21d10e06e70610562d642359406fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:26 +0000

    invite bound smoke room goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def add_numbers(x, y):
+        return x + y
