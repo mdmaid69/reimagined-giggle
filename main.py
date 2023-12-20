f5853492b38e1395c39a252e63c3cd3f8071d8e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:51 +0000

    age turnup exchange powerful term

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
