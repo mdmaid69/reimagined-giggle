3429a2c9c6d2d2f71dfe65a9b96ad729da54f7e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:07:33 +0000

    brief supply worth hide back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
