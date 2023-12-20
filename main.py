4abc608852bfa52cffea37288c2ecae78334c34f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:37:59 +0000

    summer approve waste date appear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
