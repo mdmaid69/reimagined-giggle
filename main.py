685d0ed2074a5abe42d79277145b72e002e2816f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:46:45 +0000

    pot deep mistake train type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
