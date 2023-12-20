eb9a9e5e17b7c42f7b252b00ca9c5b24ed1e3299
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:04:24 +0000

    bear peaceful bank street convincing

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import array
+def convert_array_to_string(array):
+        return array.tostring()
