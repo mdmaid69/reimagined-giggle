a3456cc519510e4a09f7899136e27d29e2d953b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:19:33 +0000

    brush rescue string serve inquire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_directory():
+        return os.getcwd()
