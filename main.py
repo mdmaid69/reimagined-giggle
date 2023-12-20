8a6f53c33a877576910561a8a2e55e9bf775b67d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:48:06 +0000

    value considering risk corner exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_typecode(array):
+        return array.typecode
