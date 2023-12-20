08d3db965990c12fb76355ad6095a50ae66d6234
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:18:47 +0000

    peaceful past dear home raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_hex(n):
+        return hex(n)
 import sys
 def print_python_version():
-        return sys.version
+        print(sys.version)
