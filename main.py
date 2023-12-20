5e5b8ff76d677ed1b3f351ddec43a056c7e7e48e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:41:26 +0000

    fight workon limit turn tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
