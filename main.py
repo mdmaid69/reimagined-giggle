4581e865f4ebd5a61e0488e280afbd373f2ca60b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:54:15 +0000

    across thumb natural tap distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
