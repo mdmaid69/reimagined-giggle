3907f7f086f2458312c7b9746e28fa7f35731192
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:38 +0000

    first or even result quickly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
