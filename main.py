111a0ece426a29a02e1b12ab8d7418e82e156ade
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:48 +0000

    record colour exciting exam relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
