3a9854331b864d447abf5a022896234fdb69beb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:21:11 +0000

    thinkthrough until polite live far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_memoryview(array):
+        return memoryview(array)
