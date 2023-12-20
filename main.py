69017c1bc01ccfa676ac4edc79e349774011bf83
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:32:51 +0000

    Cc join each wealth safe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
