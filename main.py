bd31fc264646277cda2cab94cfb6bec735ff590b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:34:45 +0000

    middle perhaps comfortable visit closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
