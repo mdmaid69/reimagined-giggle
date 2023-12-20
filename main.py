0f88ec09145e25d7bf7fe508757638ad73acdbef
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:53:39 +0000

    else sale popular and appearance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def clear_array(array):
+        array *= 0
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
