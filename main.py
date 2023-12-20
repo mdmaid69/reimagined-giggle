ae7b7e867fd782a8aaca5e1edda8dec834625330
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:00:10 +0000

    final notice danger no sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-import collections
-def create_queue():
-        return collections.deque()
+import os
+print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
