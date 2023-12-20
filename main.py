96f21610e18121ff97163f3d94a9c9e56923121c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:45:31 +0000

    past bite Ss trade bang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
