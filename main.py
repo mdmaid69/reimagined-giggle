3fcf6db1a3d7a87a59ac75be380bf9ca6adee21a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:15:04 +0000

    realize distance over low consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
