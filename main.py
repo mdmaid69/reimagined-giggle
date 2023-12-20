cb23227ca965a57c7c7356f1f396f77c1e71d6d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:54 +0000

    store thinkthrough conversation runout people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
