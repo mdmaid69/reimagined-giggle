400dde8b955eb9d775e68384e2c1f7e7d63e9bf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:46 +0000

    Ss tidy description bag down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
