cb63e509f9544a152cec0ac50e74bed528bd3a1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:27 +0000

    soft love heavy church sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
