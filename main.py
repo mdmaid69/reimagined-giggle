6b079a721016dd6006f4cb5c01a123e1160f4c9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:16 +0000

    whole now instead dry shock

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
