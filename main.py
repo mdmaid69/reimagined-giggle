ca5d6692d6d5cb5b1327179c269f321329bc9e49
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:44:29 +0000

    remark snow arrival food exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def is_even(n):
-        return n % 2 == 0
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import time
+print(time.time())
