af3876910405fb3fe0d1ebd95f09368d247a75d8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:34:23 +0000

    mind 565end of danger storm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def find_max(lst):
-        return max(lst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import os
+def get_current_working_directory():
+        return os.getcwd()
