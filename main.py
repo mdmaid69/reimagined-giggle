44227cef1bef2db2c1842defccb63e7d6b543b24
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:31:38 +0000

    politics most fault unhappy laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
