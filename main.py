881ac291a2dd2649d5de21384f92c4fce408cbba
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:26 +0000

    muscle discussion objection tradition uncertain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
