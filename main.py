2001330e71c10e6f01d444f5fffb280304940cc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:43:00 +0000

    guilty ordinary sell influence takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
