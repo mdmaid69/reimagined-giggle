43c65b315596335306d9620f210b3ca70902311a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:33:42 +0000

    message holiday lookaround issue quiet

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
