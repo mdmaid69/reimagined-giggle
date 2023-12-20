732fb81bc43799df7aa2da6ab5ee6ea4b8ca4139
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:27:50 +0000

    fact result careful male vanish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+print(math.pi)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
