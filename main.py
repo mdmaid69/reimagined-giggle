22597955bca95ee800fb51b780f11a7a5a303ac6
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:40:09 +0000

    especially plan influence Ll achieve

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
