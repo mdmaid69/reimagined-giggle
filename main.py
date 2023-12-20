94dc883e5b4a56072b6a17e72350cd7a135630d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:20 +0000

    bringup fond occur tradition course

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
