520625c8ea86fc7101607b75208219889de20b05
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:52:43 +0000

    about claim damage imagine wheel

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_hex(n):
+        return hex(n)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
