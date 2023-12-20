3bb5900e8d6892378424ebac69184411e93daedc
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:39:51 +0000

    tune declaration charge busy but

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+print("Hello, world!")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
