125cd09364cb107bc3af447e7e664872fecac3e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:24:15 +0000

    considering lonely television blue always

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
