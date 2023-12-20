080c877d979b8646f40b80200edaece9b9c10077
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:05:24 +0000

    urgent either check confidence display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import platform
+def get_os_info():
+        return platform.uname()
