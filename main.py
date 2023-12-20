f988f6e2c1625aa9cfb67bbc69deb3134011ea5a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:28 +0000

    blame move attach preparation us

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
