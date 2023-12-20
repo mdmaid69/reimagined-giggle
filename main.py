3053f5493d963e3b6ba9b0a3575a6508b88d3406
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:57:25 +0000

    anything breakthrough future pipe conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
