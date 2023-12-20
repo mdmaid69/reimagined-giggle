11b82ccfaab93a35b6cb27d8dc8b03639df17a70
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:04 +0000

    curious crash sad call arm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+name = "Python"
+print("Hello,", name)
