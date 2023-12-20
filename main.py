6d2ab4f9166842493ab320d0380711ac50922bb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:30:45 +0000

    fire needle content problem create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
