cdb311f16276427a0fb119b12a2db53f02ceebcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:35:33 +0000

    performance innocent actual dish work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_area(radius):
+        return 3.14 * radius * radius
