c4ce522fee072e45e7bf61e93bac70adf60d6969
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:41 +0000

    sort sure inform occasional rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
