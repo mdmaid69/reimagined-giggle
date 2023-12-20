789d5f4f54a85d4e0767162ff80198a3239fe39b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:02:27 +0000

    beginning woman tired term ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
