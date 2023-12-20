ec7c1f6f27fcdd488f061ded74e52f5740a6680e
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:20:01 +0000

    anywhere great crowd hold world

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-x = 10
-y = 20
-print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
