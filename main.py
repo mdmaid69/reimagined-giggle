8fe91791f4074fafd97d16fab618170f1d2159b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:11:17 +0000

    smart solve or preparation keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
