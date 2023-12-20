d936f007ea06f352941fe931fb65b58a75a01313
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:16 +0000

    I attract describe other nature

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
