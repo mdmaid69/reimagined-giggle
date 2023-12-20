1473f551dc9f83cd6afefbd326293ca5db973ddb
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:55:37 +0000

    spread passage everywhere lookback(on) my

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
