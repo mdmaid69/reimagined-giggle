8056aeaa3144e1167c937339a3fd7672dd5fc6e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:56:57 +0000

    572enter ignore geton duty drink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def count_words(sentence):
-        return len(sentence.split())
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
