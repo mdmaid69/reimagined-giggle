54b90da25bbc8792fd617a591e3b27cfeff21b0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:29:39 +0000

    team ever cry goup thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
