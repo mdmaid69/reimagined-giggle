066df97b318fe2231c65475d5c9b500f261f6430
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:31:43 +0000

    protect nature present easy succeed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
