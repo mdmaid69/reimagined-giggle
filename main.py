2dc65ec7c17af621cda11465c83161db05aca57f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:21 +0000

    port head talk guess more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
