b7a24cb33f6f51482cf5b4326118c387760cac84
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:47:03 +0000

    movement only suit record people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+for i in range(5):
+        print(i)
+  import sys
+  def get_python_version():
+        return sys.version
