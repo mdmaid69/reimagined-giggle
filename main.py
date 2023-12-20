95373350371fd0c0cc85c682f60d1a382f9c45bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:11 +0000

    acceptable course describe Dd mainly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-print(sum(range(10)))
+def calculate_speed(distance, time):
+        return distance / time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
