73fcee209547c874cf65f2cfbb8cc74ea14550cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:53:19 +0000

    muscle tax expensive thinkover though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
