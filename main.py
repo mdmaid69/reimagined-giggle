2031cf4436a19562e037716b3acc76d43d77fe1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:05:45 +0000

    spoil build business mysterious smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
