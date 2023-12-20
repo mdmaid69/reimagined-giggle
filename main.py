e24eb50453ca67b20cdbbe64f93dcf9e2b8d11da
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:22 +0000

    herself supply attitude factory scream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def is_even(n):
+        return n % 2 == 0
