6540857dd1876adc01cb086b1f79077c39c45172
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:57:08 +0000

    fit keepfrom clean damp feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_array_as_set(array):
+        return set(array)
+import time
+print(time.time())
