78ab05a78d2a3414d0b136aae67d405959236ad9
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:25 +0000

    begin will workout before dream

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def square_number(x):
+        return x**2
