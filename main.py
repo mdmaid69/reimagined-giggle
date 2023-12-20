da8bec4dd09ae36413ae16cb0ff3fba310b48fbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:24:40 +0000

    born computer speech private study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
