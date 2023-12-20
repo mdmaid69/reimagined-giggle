ecd496d38fc241734ab638db50a5ce6d6d05ef32
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:00 +0000

    Ff balance beginning either warn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-print(sys.version)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
