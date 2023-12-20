7b54a7636edc9f14247c257689eba33682a752ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:03 +0000

    school brush achieve contain scream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
