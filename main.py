e313ccdff8b539f6f239866a9be492d8cb1ac539
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:43:00 +0000

    top early pot business emphasize

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+print(sum(range(10)))
