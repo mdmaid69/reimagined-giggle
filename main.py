c17e3c0da5fdac0297f0badc5c06c38ca30ca915
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:38:18 +0000

    distant Hh gradually neat similar

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
