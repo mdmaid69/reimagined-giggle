011cbb94cb8d6c22af0587181d5e45905c67b5be
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:10 +0000

    piece pattern collect noise sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
