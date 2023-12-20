bb8cded4693836aaf4d6d9ade55f8a6f1d386fb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:30:32 +0000

    bus forgive bridge very mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def set_array_item(array, i, item):
+        array[i] = item
