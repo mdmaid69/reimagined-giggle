ed7606b471f468fbdf6ecd9ff72ddeaad67a0f99
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:01 +0000

    improve bang stay subject class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_density(mass, volume):
+        return mass / volume
