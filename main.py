87c237469a8425e9bd068ef23a9faddaff19e6ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:53 +0000

    yet corner hungry deliberately hurt

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
