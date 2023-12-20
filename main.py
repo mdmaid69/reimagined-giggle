999181652aa755bc2f1a9e58bccc8c4a8e7fddfc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:01:14 +0000

    first Dd snow among by

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-name = "Python"
-print("Hello,", name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
