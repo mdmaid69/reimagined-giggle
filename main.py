1039c8f94777bd873e6a57d4a3e692a6f226d287
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:29:19 +0000

    comment communication sweet cheek quick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
