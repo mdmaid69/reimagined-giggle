a5b4f9ca38212f7ce2cb24a22f7732dff9b2aeab
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:12:09 +0000

    turnout answer marry breakin dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-for i in range(5):
-        print(i)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def append_to_array(array, item):
+        array.append(item)
