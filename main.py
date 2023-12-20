73eafd2d26f184a36533aa5251d6e0726e8950c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:28:30 +0000

    unhappy promise relation organization forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
