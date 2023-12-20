5dec3c0be853af21d026d8ddf89f1b25ba6141df
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:53:04 +0000

    shine piece hand exit common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
