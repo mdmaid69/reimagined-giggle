647ca5af3f8ae9edae60f0c8fdaeda16636c6551
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:47:02 +0000

    progress crowd wheel insist bet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def count_words(sentence):
+        return len(sentence.split())
