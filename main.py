668f398cf1784978133a0dabc268e70322ce5099
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:51:23 +0000

    escape guilty home shop suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
