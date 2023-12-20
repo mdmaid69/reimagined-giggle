557e7666b86fa70f99e2d94755c73ad0125bcda3
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:22:58 +0000

    question opposite pump punish something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def reverse_array(array):
+        array.reverse()
