284433402c8cc29d6cf2c3b30c250794c5ed6049
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:58:55 +0000

    expression punishment fasten flood public

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
