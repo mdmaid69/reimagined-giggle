7d6f8709509c93ae7833769d5063e5e02054149f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:39:07 +0000

    drive shout waste equal there

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
