a5d9916aed29fc66645a26ea1ec16d3e5ce9995c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:52:37 +0000

    everywhere quiet complaint problem afternoon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def print_python_version():
+        print(sys.version)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
