d78b1a421f277710bf3c8e0b93341b2133c609a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:39 +0000

    attitude destroy female hair actual

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-name = "Python"
-print("Hello,", name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
