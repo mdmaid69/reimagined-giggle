c09f9d037c0a93711661a83e0031cef1400108f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:39:32 +0000

    example holdon correct float down

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
