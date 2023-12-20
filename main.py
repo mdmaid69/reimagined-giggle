e8ab5b3060767f4db8c39d5d4ba61be6f292ba8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:48:22 +0000

    community grow slice consider concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
