4fd4634d3b298f50a26fe07e18382b22a206fda3
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:28:41 +0000

    cost water best wherever middle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
