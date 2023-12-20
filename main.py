68828bc757f80a5b509b34e1e8d350da94bd558d
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:35:26 +0000

    lend afterwards though apart then

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+print(os.getcwd())
