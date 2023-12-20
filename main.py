6602f2f10e4de7df042878972b4fd0378d5ab17f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:00:15 +0000

    order worth some pride mouth

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
