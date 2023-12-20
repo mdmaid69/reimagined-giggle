6588b71adb8de1a25d73d51fb306b18f14df0254
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:18:26 +0000

    suppose Dd reply embarrassing let

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def square_number(x):
-        return x**2
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
