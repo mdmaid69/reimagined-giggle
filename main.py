2b5e75ec727e97f2ee572fdd6307e5a2ea0a2bc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:24:00 +0000

    embarrassed spoil gradually nerve trade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
