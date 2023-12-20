3f668f529b8429389b5d8305dc2576bd1acfd87c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:25:07 +0000

    tradition shape brief short float

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
