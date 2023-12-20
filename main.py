a8d3080b84410c7ab78038c09dda47bf40846752
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:55:08 +0000

    emphasis failure feed slightly fear

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
