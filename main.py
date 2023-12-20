2929747aa6f0d455f90dee3020faab5b5204efbc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:16:00 +0000

    dare agree combine better strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
