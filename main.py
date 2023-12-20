9261e1584affb784dd350c0f1be5c978065f5be9
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:21:03 +0000

    unfortunate order almost exciting himself

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
