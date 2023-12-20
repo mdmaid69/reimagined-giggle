1413c1760111371b396599d0f25140746f6f1efc
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:11:17 +0000

    be yourself square pencil trust

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def count_characters(sentence):
-        return len(sentence)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
