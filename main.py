98fbbfb7bf96b232d40830d3ba07c9dbba427574
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:56:35 +0000

    drop confident post contrast will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import os
+print(os.getcwd())
