7f76ecd4bb96ce9cea150e02eafa55fcad31b094
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:52 +0000

    history stupid exciting stay exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
