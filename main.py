e95428fa02d1b92c1a92a128a6d8f252ee9769cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:30 +0000

    opinion comeover below only glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def cube_number(x):
+        return x**3
