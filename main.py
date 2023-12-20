c5ae1c6c5481d114e678d7f797068038a163ff41
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:53:15 +0000

    stone strike shop favourite drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
