d220629e07030ce51e129136f5ee2be2fefe204c
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:28 +0000

    room unpleasant name purpose achieve

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
