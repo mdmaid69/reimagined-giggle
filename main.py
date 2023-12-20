5d14d1e57dfca0ac552da7ed5599e9fb429fb4e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:38:46 +0000

    season crash whatever certain declare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
