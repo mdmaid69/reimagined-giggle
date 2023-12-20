fcd46909de84e815f7d99cb4925aa5759b278581
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:42:11 +0000

    hello tear please different importance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+def print_python_version():
+        return sys.version
