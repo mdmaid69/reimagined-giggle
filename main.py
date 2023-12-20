ad75caac320ad1be61ac4a366bab2ab9fd83b32e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:29:04 +0000

    recognize hurt powerful plan ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
