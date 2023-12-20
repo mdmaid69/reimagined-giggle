48a3bba45c41bfa6d55b71df067d7d630eae5a80
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:53:40 +0000

    birthday emphasis down immediately negative

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
