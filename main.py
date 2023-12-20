a09339ebf750d6d314fe716eeba98c71d7a98737
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:50:06 +0000

    acceptable course Dd close official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
