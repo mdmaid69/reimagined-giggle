88201563cad83244d27b8d1d9f6d998c6bf6f199
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:28:20 +0000

    pure thinkof on fetch wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
