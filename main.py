85abbb6437e1fed3d2f3723a6755f03f37930470
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:48:04 +0000

    view rescue accident black brief

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+print(math.pi)
