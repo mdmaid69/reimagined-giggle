0e16653354b542808275fbbd8dcce3a444602cd7
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:18 +0000

    nearly crime shut circle neck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
