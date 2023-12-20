3a33306736429aa22ba7843785735802cc9a0dcf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:44:36 +0000

    sort waste sting hold relationship

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_unique_words(sentence):
+        return set(sentence.split())
