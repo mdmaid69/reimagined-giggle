cc26d5ac1b704ed46544dc287b13193a11c7233d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:11 +0000

    runover phone delicate slice aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
