4d88d176e597d65213412c4d2d3b6abaf8b4cd79
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:43 +0000

    scream most together minor correct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
