24d55be82fbd90b2410ab7e98857e0218157bd1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:36:19 +0000

    sure grateful dream Dd measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
