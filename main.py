68d8d1833f25df3e332a42b89688d782071a6563
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:15:34 +0000

    better thankyou introduction especially pencil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
