2ac22660b1a6ef724481a3058f21348d082d91b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:21 +0000

    ticket claim angry breathe gooff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_current_directory():
+        return os.getcwd()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
