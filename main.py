e61f7f68659a94c730ee5841c22115ff41f1fd59
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:48:45 +0000

    speech welcome wash lot front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
