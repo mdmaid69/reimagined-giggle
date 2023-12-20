bae07e931c9d849256be97c9b981fe4028e1c851
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:31:02 +0000

    floor already fair occur basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import collections
+def create_counter():
+        return collections.Counter()
