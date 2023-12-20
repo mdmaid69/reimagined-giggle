8cf865007df96a45f3bc35daf8a965ee2d01db3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:11:37 +0000

    nature protest separate frequent birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
