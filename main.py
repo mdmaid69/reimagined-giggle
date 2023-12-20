67dac33d20e6c4a1f14007d97458e00d0ec309d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:53:11 +0000

    lost count bye aim negative

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
