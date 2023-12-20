5e8ac9e65b2abc777c54c66bab6dc5484e1a2719
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:07:32 +0000

    dare apologizeapologise 179bite belong knowledge

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+print("Hello, world!")
