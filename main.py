35506489b3a6020be8616ba166f039c7daea38ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:21:48 +0000

    interruption space round willing learn

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def add_numbers(a, b):
-        return a + b
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
