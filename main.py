3c679298dcadb6bbeea6295e107c2071ba0af3fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:57:56 +0000

    freedom its name bottom uponon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
