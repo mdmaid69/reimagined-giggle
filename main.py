75c497510e772ba90690b5e566ad77a679aa5d42
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:36 +0000

    count complain nerve nurse step

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
