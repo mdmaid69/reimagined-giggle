0bddb454644a56d8100ec9ff14664f18fb072f87
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:48 +0000

    takeout area turnon quit gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_power(work, time):
-        return work / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
