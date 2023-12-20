f7cd97718d303e77b92dc8e23c763fee778c16d6
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:14:23 +0000

    luck relax purpose top head

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import time
+def get_time_since_epoch():
+        return time.time()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
