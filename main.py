45c64618b20e6b44dff3cb87e2543f010b8dd554
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:11 +0000

    yours separate confused sleep female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_cosine(x):
+        return math.cos(x)
