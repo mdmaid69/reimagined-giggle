4f00f5cfd9290979b3d6efb718532c4b124cfe21
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:59:23 +0000

    come influence towards press slow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def count_characters(sentence):
+        return len(sentence)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_error_function(x):
+        return math.erf(x)
