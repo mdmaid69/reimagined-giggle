89bb7fcec79d8de3160011061577574aeee76a8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:17 +0000

    work debt chance pay rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
