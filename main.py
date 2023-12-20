a599c4e2ccd7215a3f054ffdb9a13e013782f6f9
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:12:01 +0000

    convincing warning mess scene minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def convert_to_octal(n):
+        return oct(n)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
