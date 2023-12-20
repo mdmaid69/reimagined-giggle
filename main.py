82c9db053c6c8d59a1668993514d625f936d4d06
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:05:35 +0000

    buy sweep tired cause just

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_environment_variable(var):
+        return os.getenv(var)
