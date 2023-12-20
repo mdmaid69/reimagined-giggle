c0fd00cff320e104afd856bf320179eba1b6d8e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:24 +0000

    repeat concern joke land weekend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
