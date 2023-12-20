b26ee2236b4bfd5e74eeb74bc1623f2096ac308d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:00 +0000

    raise aim than operation whether

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
