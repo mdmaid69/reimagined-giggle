3b0b18f5400655fb4d7cb427718c39ea5ca134b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:01 +0000

    set once worth mess see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
