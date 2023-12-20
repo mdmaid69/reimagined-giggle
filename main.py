f93580f5a319c312e3b15c820595d93e9acc5199
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:01:09 +0000

    contribute giveback narrow anybody bath

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-for i in range(10): print(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
