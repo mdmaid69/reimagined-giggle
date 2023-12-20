95929e9f6a8fcd7c8d235abd8497222ac8161f56
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:10 +0000

    yes getup wing see boil

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import random
-def roll_die():
-        return random.randint(1, 6)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
