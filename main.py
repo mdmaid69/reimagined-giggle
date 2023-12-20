38fab350af8875456a624c5e78c8bfcc86e6cc94
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:19:40 +0000

    approve design port round payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
