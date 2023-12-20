277262918a459cadfbf3f4e9ba4fa590b7c22d62
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:01 +0000

    plenty always stiff dead legal

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
