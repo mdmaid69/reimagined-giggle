f5e7edd150b15df0ae217fef9b0d99499e347a6a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:25:34 +0000

    trade idea activity trap calloff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
