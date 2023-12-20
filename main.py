4073c9594545d6b65287e858609f2ef2110a5c8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:27 +0000

    trap generally talk left see

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
