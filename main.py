dca3ab574b0427fc1a811c47c175b4526a109417
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:22:50 +0000

    warn under smell unfortunate sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
