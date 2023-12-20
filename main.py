2c157f16f1fcc8c7c084aeb06a60c0d380be5aee
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:04:58 +0000

    none valuable drive thorough inquire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
