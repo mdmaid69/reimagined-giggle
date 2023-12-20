85d0850b336f20493eeb1b29f928b673354ddd68
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:37 +0000

    sweep touch exit fence read

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
