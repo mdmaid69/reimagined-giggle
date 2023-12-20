0df0ba5bde1b32856829782bb2ef307647cb4d69
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:14:18 +0000

    empty joke protect authority present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
