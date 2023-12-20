12d2d095d9ba7f94fb7b9ef432de424b44efe5e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:02:25 +0000

    hot ashamed fortunate taste know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
