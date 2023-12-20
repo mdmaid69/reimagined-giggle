a89fe568b0f6fb07460aa43f519a30eb785135f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:20 +0000

    its task reserve sorry roof

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
