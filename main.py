da00bae5c11ffee13b133797410f7b668e777779
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:20:34 +0000

    justice pure safe contrast if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
