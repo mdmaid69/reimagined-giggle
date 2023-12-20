f83445296cd8871aa4e91ae8642a8d2fcc484672
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:09 +0000

    muscle mistake see team clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
