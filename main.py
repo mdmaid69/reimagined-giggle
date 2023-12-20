1e9c976debb86be8365a6e32349b2d4f17900607
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:40:16 +0000

    slide hard payment he impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def convert_to_octal(n):
+        return oct(n)
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
