3ecb959dd993c161bb35a48e30ced5ea34df1349
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:08:16 +0000

    more develop trip imagination off

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
