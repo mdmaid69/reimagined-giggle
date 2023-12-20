8f77a54e05f4c3b10bb841b3429081898cae48e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:46:38 +0000

    recognize dish luck land marry

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
