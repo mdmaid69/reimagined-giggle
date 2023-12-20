1e11c8c5afa9214639a80eb4e7e6169254fb7d43
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:57:51 +0000

    trade hide measure should takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
