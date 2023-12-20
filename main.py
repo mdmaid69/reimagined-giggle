4daa6543f2ddf16f9899b2c972a9d949d3e2da42
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:18 +0000

    bright glass spring goodbye aim

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def get_array_as_set(array):
-        return set(array)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
