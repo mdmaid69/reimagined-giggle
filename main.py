483f73aada00eb2508c3215e1a0edb0a94f42342
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:39:26 +0000

    principle talkinto whole born damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_item(array, i):
+        return array[i]
