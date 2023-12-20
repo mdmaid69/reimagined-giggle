405874d1fe9d3e92b6a95c7153ca6d5befdf02e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:22:59 +0000

    guess wait send doubt jump

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def subtract_numbers(x, y):
+        return x - y
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
