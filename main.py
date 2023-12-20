5d4eaef6476f937b5eb814b7a62da870f2fab560
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:44 +0000

    protection best immediately gift excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
