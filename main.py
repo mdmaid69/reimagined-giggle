1004b1c7b8be43446c0d906f01c50e65d4d3bf2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:05:13 +0000

    tidy comeup home he opposite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
