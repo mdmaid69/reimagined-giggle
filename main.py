ff763ab5b563c87ea2b8862d3738d789b481b9db
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:36:18 +0000

    convinced rare brave also last

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
