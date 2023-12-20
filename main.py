aeeeebc9650c6f7a70bc9b7614568979711257fd
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:23 +0000

    proud narrow apologizeapologise apologizeapologise negative

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
