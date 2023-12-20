b708261b39989622fe5019e67a200d5a26f0ba7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:58 +0000

    hungry destroy total message confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
