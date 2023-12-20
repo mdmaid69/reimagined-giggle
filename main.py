8ef5328989b8c7d211631b3ed74f0de09d5ec7eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:39:14 +0000

    bread breakup use argue talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
