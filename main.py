f1d2b28f1b136660d268c69b9f0cc8283e8258cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:26 +0000

    level board doctor fire meet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def reverse_string(s):
-        return s[::-1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
