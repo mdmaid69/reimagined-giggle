3aa278f69842c336abdb5451c0589a0a08484a11
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:52 +0000

    force sit really occur string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
