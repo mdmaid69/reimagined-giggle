5788dc124bebc78ca4576de8ef6d64bc12ad7096
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:43:11 +0000

    splash weekend floor adjective nothing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_work(force, distance):
+        return force * distance
