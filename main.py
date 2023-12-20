f0e85dff869ba382ce2e0871f172f46f989be19d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:08 +0000

    not bottom play test too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
