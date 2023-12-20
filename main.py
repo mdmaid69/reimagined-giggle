95e029eea210ca5048645c2915ef2ceed32aa990
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:36:39 +0000

    song explain band pump criticize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
