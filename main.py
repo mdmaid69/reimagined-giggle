ec225c31101d78c991448b0ac8160693e7e9e50c
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:53 +0000

    lookup thorough load combine situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
