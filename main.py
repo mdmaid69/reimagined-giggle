68185b72f118cf4fd2d581ddb8b9ada3448bfa72
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:40:00 +0000

    little makeinto excited slide peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def add_numbers(x, y):
+        return x + y
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
