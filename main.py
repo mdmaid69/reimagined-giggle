903a3a0effc370784e8e07feb0b9a126a99e52cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:15 +0000

    ask spread Vv share past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
