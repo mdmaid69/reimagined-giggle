e23ab95e4fc0d1e281dfcb81548d823d94413797
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:59:03 +0000

    narrow swallow step cap load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def reverse_string(s):
+        return s[::-1]
