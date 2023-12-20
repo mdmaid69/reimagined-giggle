3a43f453e2f21a674418be9b74405fa41a6a5273
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:30 +0000

    ticket exact display rid call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def convert_to_octal(n):
+        return oct(n)
+def remove_duplicates(lst):
+        return list(set(lst))
