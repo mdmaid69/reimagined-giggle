15ca2411e86e424135c2cc1b1533725c37f5a26b
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:24:37 +0000

    main major tonight stop suppose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_acceleration(speed, time):
-        return speed / time
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_string_from_array(array):
+        return array.tobytes()
