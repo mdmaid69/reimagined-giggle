076b3b3abbfd988303b11e2f756bd39a8076f0fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:55:07 +0000

    giveout upstairs university yes reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_average(lst):
+        return sum(lst) / len(lst)
