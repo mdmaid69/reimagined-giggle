46f5448b1535a26763ea4a8606e7709d327b78bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:57:19 +0000

    shout public meal touch involve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
