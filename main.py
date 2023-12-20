03757e0609a62911f9d2a095cbdb378cb6d6fb00
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:05 +0000

    skill regret them cold lost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
