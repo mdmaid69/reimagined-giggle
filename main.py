3b03a41d9666ef87c1e643f4f68b7b1b7e38a816
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:41:58 +0000

    study mad wind turnover black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
