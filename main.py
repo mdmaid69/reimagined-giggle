9df8fd9ff2fa6b166a324a96818889acbf520bb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:04 +0000

    total waste gate become shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
