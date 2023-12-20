febec8d40e3dc7ac960780400f124428c5104a44
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:09:28 +0000

    ground oil adjective comeround pause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
