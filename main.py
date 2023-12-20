2fc8be4f1f17c496fc6c17f480d61877bc70a177
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:52 +0000

    though urgent disturb away cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
