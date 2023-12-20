95fe671224462d8e5fc64c2969090219c0979bf0
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:31:59 +0000

    roll underneath blind wet evening

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def divide_numbers(x, y):
+        return x / y
