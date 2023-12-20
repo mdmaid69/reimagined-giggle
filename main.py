62e7212d10c4d6b56eceb262ded23afd4f69e589
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:36:08 +0000

    round tread roof winter thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
