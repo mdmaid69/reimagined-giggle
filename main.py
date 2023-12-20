93c5b76926218e284e4d78109ba6c63254c4ff9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:05:18 +0000

    goon doubt show letter address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
