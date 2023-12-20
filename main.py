035fb6ea6489c3e510c141631989665dfd8d34d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:04:20 +0000

    sign succeed clear reply turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
