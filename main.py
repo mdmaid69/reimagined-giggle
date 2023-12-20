adb3e18b40818ab5e78ab9dfc6f1786660533290
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:53 +0000

    remove government compete goback(to) murder

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
