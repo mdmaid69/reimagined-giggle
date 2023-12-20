2e352e3b614ffb72a5f5874cbb6f9a55b05348c1
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:24:10 +0000

    pretend crash 567enemy design our

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
