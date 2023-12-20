b279b01a8fd83d52ac31f157b34e30d4874d415a
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:04 +0000

    left world 568energy fasten herself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
