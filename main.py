6bcc61cbc32c64bbec9a5cdb35fef0116bd63847
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:09:04 +0000

    wait base closely fall pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
