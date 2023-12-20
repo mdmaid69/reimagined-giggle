bf30fd32708e21ef4b6fcebc39280a9bd516ba8d
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:47:20 +0000

    pleased step niggaacademy uponon workon

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+text = "Hello, world!"
+print("Uppercase:", text.upper())
