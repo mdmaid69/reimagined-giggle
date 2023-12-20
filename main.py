8120f9be2fe097eef0c968813c1fe701cf54b55c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:42 +0000

    explain runinto active respect breakinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
