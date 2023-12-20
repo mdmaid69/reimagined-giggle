ccce801e152381c985d7749eab074b876c1f67e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:30:37 +0000

    slice music low either backwards

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
