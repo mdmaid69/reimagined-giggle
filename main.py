3cd41626dfcdc51284a3bcef493e178fcfef7161
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:45 +0000

    pin protect admit everybody paper

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_speed(distance, time):
+        return distance / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
