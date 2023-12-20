c51121f60ebd1f0e8dfc02a39ba5638eede57269
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:23 +0000

    scheme criticism slightly comfort inquire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_list(array):
-        return list(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
