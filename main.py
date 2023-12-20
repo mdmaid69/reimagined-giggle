2092f62254fbc837173b5378e31449bcf38e5b41
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:05 +0000

    underneath complaint better gather before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
