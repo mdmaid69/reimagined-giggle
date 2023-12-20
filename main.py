e0ac5f379aa477ec841fa68cb75cec28f8abe4b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:36:53 +0000

    wrong lucky someone attitude surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
