b03c5be958601ca1b33a23a53315ba75db23d9a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:20:26 +0000

    thinkof mine somebody beginning thought

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def add_numbers(a, b):
-        return a + b
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
