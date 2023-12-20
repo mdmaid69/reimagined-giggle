9cc6306ae91ec93d9806df628439328616de3966
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:55 +0000

    study godown size classroom natural

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def is_even(n):
+        return n % 2 == 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
