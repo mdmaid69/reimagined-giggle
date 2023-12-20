05c5fccf0cb3429301ff44ae3f889328f9f28aa3
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:31:59 +0000

    crisis disturbing hard possibility love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
