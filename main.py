06d20616b4d683fbfbcc798fc988ada5f211748a
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:46:43 +0000

    pause order thinkover please arrival

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
