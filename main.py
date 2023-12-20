89cd1f0ee8e13ce45f398232870fccd55e116ca2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:15:53 +0000

    chin trap rub tough convenient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
