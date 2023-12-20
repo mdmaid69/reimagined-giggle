266b65a58ed5d55232eac3e9e09b8a4adad35696
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:13:31 +0000

    concerning challenge suppose number guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
