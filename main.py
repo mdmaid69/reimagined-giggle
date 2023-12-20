a94a5c0b93ecf49fce1eec75f30b1f62e4696965
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:10:06 +0000

    pull herself dead accident must

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
