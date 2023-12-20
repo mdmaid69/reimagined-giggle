b973ba40dacaf5b8bd1e111e0f068aa393bd79ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:45:20 +0000

    high otherwise result also under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
