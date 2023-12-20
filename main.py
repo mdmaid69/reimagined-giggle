897ad963cd5d3117a543388eb664aa5988f048c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:39:29 +0000

    professional owe fill write unite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
