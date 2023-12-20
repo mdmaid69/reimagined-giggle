74097a62aebeac3a2e14291b01497d844e1469a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:06:30 +0000

    come day apart someone dinner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def sort_list(lst):
+        return sorted(lst)
