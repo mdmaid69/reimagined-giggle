d04728e21859e9c6a9ecf59f14c195aca6225aa8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:09:06 +0000

    in flow kind use cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
