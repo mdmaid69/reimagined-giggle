bbd645b1587ec7b3e7dcd3078e86da7ed3b1e43e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:33 +0000

    quit breakup top fit workon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def multiply_numbers(x, y):
+        return x * y
