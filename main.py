c8ec8f4d601a328503c9a1b774dd4d31f89cc8bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:36:17 +0000

    involve benefit yet reasonable week

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+print(time.time())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
