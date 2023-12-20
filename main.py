8e05d46db93b655a5811a11412d435217c27f65e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:14:21 +0000

    great scream lean police club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
