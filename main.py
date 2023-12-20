3cfd5ec371b3e368f3d773d887c546682cbcf082
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:53:56 +0000

    stay stamp fire purpose safe

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
