b4eafe78b7b842c655723e014521028ca2fd279f
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:14:45 +0000

    car principle ever possession opposite

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-def calculate_density(mass, volume):
-        return mass / volume
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
