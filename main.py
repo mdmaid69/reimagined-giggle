25049bcb9146e5035dbfe309b137cf0ee2bfd06f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:59:26 +0000

    direct could field alone spare

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
