49a9a09bbb405a334cfecf39caf91de9dc7311bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:32:44 +0000

    transparent yesterday flow slide not

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
