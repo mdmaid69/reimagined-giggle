c5894829fbb0760312d65ec856e2cdd47ad4de84
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:13:31 +0000

    show confuse term bringup gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
 def calculate_logarithm(base, x):
         return math.log(x, base)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
