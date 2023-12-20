bef6427afffe60eb4385c7755536ee6190a4e9bf
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:51 +0000

    persuade rank correct reply butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def split_path(path):
+        return os.path.split(path)
+def find_min(numbers):
+        return min(numbers)
