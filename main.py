038ec72f46a20190a09a1fdeff675b6370d5e732
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:47 +0000

    preparation tragedy makeinto sink insist

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def clear_array(array):
+        array *= 0
+import os
+print(os.getcwd())
