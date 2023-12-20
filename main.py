d5463ad64970fcc0a0c9cdbee58501295ebcfe4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:02:54 +0000

    real grateful callback basically experiment

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_density(mass, volume):
+        return mass / volume
