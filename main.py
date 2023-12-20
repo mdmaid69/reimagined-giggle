052f50b44452a8328eeef7d84b7e8545a2f04d9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:03 +0000

    angry help account organize afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
