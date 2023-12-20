78aaa1418cbe4aaa76b988e1077eaafe91abd7d0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:47 +0000

    object mine both the actual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
