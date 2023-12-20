50f2491cdb15f090a36a8896ed55413156a460f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:31 +0000

    drink afraid case man flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
