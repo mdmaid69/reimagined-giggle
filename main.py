0ef4438fb732e515b97ffdfcf421f15dc5ec91cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:11:16 +0000

    sudden children place government advantage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print([x**2 for x in range(10)])
