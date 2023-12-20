8cdb1a287be5479f1e60e22955274eae0545fa45
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:33:58 +0000

    complicated law suppose judge forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
