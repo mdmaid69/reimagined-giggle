a091356030e4bcc947ec3a34b8e8521e9cffbc65
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:54:00 +0000

    crazy objection job reach measure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
