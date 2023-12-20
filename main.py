390c39fba9667262e21080a7d8dbb0fca24076d2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:36:25 +0000

    ear goback(to) them spin luck

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
