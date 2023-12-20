3aae2ead574a8cfdac0e6c196cc61d53b7836819
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:01 +0000

    we such them thank circumstance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
