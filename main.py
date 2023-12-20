a71604b11c509d4197a3c88b9a58fa0f649803fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:06:25 +0000

    matter nervous matter busy trust

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
