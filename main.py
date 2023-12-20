3fdafc46427d7366f7e8c83e39efec2f047fe662
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:08:09 +0000

    impatient feed discussion evening tight

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import datetime
-print(datetime.datetime.now())
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
