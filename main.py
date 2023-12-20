6805f621556a830869f7e6425cbffc47040b46ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:05 +0000

    kind prepare this patience impossible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def count_characters(sentence):
+        return len(sentence)
