c9535885cf7557011a9604e047742140abfeb70d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:48:41 +0000

    hide inform immediately until put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
