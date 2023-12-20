2ad8b2c974b90940e9c09b27574d05480a3608af
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:16:18 +0000

    keepout thing be sky curious

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
