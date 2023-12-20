1e50f44bd2ece043e7dec74e20dd2e09e3396575
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:41:29 +0000

    letter bored trust entry easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
