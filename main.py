4c08abcff9425fe6e607ff1a52150392bcd437ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:47 +0000

    nose full human wind stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def square_number(x):
+        return x**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
