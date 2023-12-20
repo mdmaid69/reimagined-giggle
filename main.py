9420d66bf6cfd8ec16dd61e679e989b27c711cd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:51:27 +0000

    copy true say beneath belief

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+def subtract_numbers(x, y):
+        return x - y
