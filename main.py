ee8b4ce486f65ce41a326910beee96877c1b6f25
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:50:58 +0000

    temper go fall receive forever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
