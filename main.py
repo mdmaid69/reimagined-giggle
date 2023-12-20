5b219021c521ed2f51a1fbe040f9db07c36491ad
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:13:33 +0000

    joke soil accordingto criticize appeal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
