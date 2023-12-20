279a315b7177f5daeed33b6503ba7097d4047817
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:15:50 +0000

    bet favourite day refer certainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
