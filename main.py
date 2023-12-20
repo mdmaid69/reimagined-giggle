b6e17f9761109988619da23f4b7bc83298955b5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:43:41 +0000

    murder appeal replace Itself cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
