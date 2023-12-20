17a1b294969c41e866090e244403e5b8933ef9d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:13:35 +0000

    566end suppose congratulations scrap new

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_sine(x):
+        return math.sin(x)
