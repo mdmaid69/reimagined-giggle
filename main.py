5098d9dc4d327bc0d015d258a8a738f0b244366d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:42 +0000

    invitation loan likely to petrol

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
