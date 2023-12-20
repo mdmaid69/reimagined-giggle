5934cd5f78c9c333159bc3c06b40ceea5772ad6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:45 +0000

    contact direct wind relationship imagination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(lst):
-        return min(lst)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import re
+print(re.match("h.*o", "hello world"))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
