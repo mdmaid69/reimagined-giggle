b11873f4de2cd5ef479fd9e44621ad1a7c041c5c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:12:09 +0000

    relative pleasant gather someone suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def delete_file(file_name):
+        os.remove(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
