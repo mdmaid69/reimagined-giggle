1552697e7c541777b5124ce28e9918b3e3c60d86
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:12:47 +0000

    in justice properly wall boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
