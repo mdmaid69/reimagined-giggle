edddd9310cb85d0bc23ce84fc322db26be1e14ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:16:05 +0000

    choose proof satisfied black formal

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
