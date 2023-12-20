5b5b0e58899a969d01fdf3c518db7cdae091ba56
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:59:20 +0000

    sound little door mass swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
