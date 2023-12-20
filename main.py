8ceecaddb8e81d8d01260058d628beb2af68593a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:39 +0000

    excuse town mess across later

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def cube_number(x):
+        return x**3
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
