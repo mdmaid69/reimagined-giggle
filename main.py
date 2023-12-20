4ebc1396c06d87b6ffb0ca8fab0d216a397fcc57
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:48:57 +0000

    use drink bill move progress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
