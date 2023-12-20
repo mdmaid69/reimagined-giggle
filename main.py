f775ee0d769857d9319c1d379e32cce0f74ee972
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:16:43 +0000

    involve upset mean worse tooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
