9406a90e731062289c904e76287e62c90db2f3c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:36:03 +0000

    environment because embarrassing noone everywhere

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
