1c20ac933d66bcc8a52a24428284bd8973ef23f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:37:57 +0000

    jump rescue rubbish traffic common

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-for i in range(10): print(i)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
