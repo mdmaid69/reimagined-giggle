64a589d56a7ca684ecd499a2e8ff253674f8602d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:20:01 +0000

    stand pick cost steer makeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
