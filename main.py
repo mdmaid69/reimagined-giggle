49f1e189e3fae41a18fb1616e305634e6d2e5f2c
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:06:34 +0000

    able transparent popular takeback thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import json
-print(json.dumps({"name": "John", "age": 30}))
+for i in range(5):
+        print(i)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
