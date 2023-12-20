37003eff1f523951554768cdd8e0414e5c3d5de2
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:10:19 +0000

    love clearly either closely argue

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
