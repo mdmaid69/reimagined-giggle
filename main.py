82cf6a27bc6370223dba0bb4b4d3994d4b239ae8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:59 +0000

    meanwhile like while roll recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
