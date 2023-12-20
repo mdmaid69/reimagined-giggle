af351240bc824ff00ff7e3bbe6f322e72677d923
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:42:20 +0000

    still shadow row original anywayanyhow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import sys
+def print_python_version():
+        return sys.version
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
