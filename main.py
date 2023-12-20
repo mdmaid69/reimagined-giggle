0f160ef2e3dbd778764986bd34df22df3ebbdee2
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:40 +0000

    load still sense occasional 568energy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def sort_list(lst):
+        return sorted(lst)
