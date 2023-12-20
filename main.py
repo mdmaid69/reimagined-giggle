e953fd4c2d24774e30815d4961b63fa436e79093
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:31:31 +0000

    shadow period market lock prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
