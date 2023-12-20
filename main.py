58a577d58dd0915ee57df5c51628376c7bba0f0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:29:02 +0000

    hard suit calm study nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
