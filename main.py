a361b8bb131b96fc7d5155bef2f08dcb5e727a79
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:45:06 +0000

    immediate remember listenfor pass hardly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
