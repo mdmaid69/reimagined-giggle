ed1ddd961ae42bfdc41a6544fe7fd17df5f1f516
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:53:01 +0000

    near getup run branch bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
