23675e3b609b4b1dd60fcebc298a60b7718a66f0
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:50 +0000

    bringup day 568energy grow strict

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
