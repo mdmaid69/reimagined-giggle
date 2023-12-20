e8d5d65022c7621623ecc3d6b58177cf39ddd61c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:54 +0000

    private song whether partner know

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
