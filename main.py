2e8b26065375e1d74e7cad9aa0b4a9ac2e905d7b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:06 +0000

    open connected shine project weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_octal(n):
+        return oct(n)
