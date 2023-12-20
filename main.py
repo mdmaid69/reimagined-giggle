a437cb1a233685907ca45311b3650c04527c2fe3
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:04:55 +0000

    comfortable side ring long whenever

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
