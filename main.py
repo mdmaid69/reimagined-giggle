131a4dd22ba72c9504c3b106a82bf3315d4f6f2e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:55:28 +0000

    fill transparent spot during weekend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
