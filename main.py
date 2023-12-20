3487a0306d7a6cd70e7a94f47d4364847737ec07
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:32:43 +0000

    actually damp trip so content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
