26c03c95c2734e3f351da4f9f71b6a0523b17df7
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:18 +0000

    question downstairs arrange anything callout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import logging
+def log_message(message):
+        logging.info(message)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
