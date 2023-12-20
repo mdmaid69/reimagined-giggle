7b86b5a5fb84280a7963a1d8aa185de4b71cc339
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:23:39 +0000

    divide keepfrom where hurry accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
