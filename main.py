31d7108974844af398082e41b50749b15d1776e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:16:57 +0000

    worry glass sight period scream

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
