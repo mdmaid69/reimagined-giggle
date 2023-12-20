1325742675206c7bd8d486dd41f467be46747f4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:36:18 +0000

    wind contact yourself method wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
