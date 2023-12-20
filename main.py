8a854ca20884ee892605688677ddbbef1a21d5fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:40:27 +0000

    awake speed pencil thorough smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_density(mass, volume):
+        return mass / volume
