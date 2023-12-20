fb9ff92aa253db09efbde99c46023018cb70d7ce
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:22:44 +0000

    connect principle treat tomorrow hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
