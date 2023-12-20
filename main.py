5b4c9cb448638299c908a42519a1f1e03835e489
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:46:25 +0000

    treat coat artificial cruel they

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
