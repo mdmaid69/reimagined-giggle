5771aafd36bc60263dd793ab78e288d48cca8360
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:37:02 +0000

    music journey rule innocent traffic

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
