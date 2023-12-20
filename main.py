e4b379ecc261618bb01a58922def6b142c18fdae
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:40:25 +0000

    mark lesson scene base tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
