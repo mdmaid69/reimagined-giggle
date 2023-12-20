76d9b7a3c2cf29e35676ee2318640731a288a61e
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:53 +0000

    between approve though no confuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_speed(distance, time):
-        return distance / time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
