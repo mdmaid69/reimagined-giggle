e43d445a03e1f627b058eb736994845edd87f500
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:27:54 +0000

    pretend market giveup entrance however

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import time
+print(time.time())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
