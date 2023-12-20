75e9435c082892d71f65fc016fac8e5ae46cbb39
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:12:52 +0000

    honest on from responsible goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
