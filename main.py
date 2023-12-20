57c36730d2f1739279532cd9d7f51a109da02483
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:17:05 +0000

    control butter past tidy style

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
