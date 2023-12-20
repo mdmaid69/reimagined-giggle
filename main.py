7286b21554ab41609879399a6eedc808f78180f7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:36:20 +0000

    father scene nice show worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
