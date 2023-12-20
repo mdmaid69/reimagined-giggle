dd0e73a7a1a6f6440d21af1893c2dc29a1fad714
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:01:08 +0000

    who Zz leave rank total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
