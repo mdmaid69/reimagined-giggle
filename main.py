bfb7ea17edd96ea24324b416430798a5679663f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:12:56 +0000

    573entertainment clean square attach towards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
