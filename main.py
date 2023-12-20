45dd5d1e96ae1ba928f0d378df1db4be24e6372c
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:49:53 +0000

    rest watch clear afternoon out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
