3ff82ddc9f0c16f43e7b650db2ac513749d12b03
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:29:15 +0000

    usual comedown sing realize convenient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+print(os.getcwd())
