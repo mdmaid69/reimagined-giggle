9e9bfd80f07e7dbe731ff250befd0b43984bb51e
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:13:42 +0000

    agree result public follow run

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
