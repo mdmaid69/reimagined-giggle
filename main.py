3a638dc8f5785dab3b5f4a52564bef25f36bb54b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:19:57 +0000

    finish somewhere operation ilovedicks fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
