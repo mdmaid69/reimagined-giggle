c532e8576307ae410822c4867080983a4c785f7e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:42:44 +0000

    side sentence high laugh moon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
