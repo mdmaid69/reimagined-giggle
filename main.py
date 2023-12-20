86810118e56e36dc0811e9ec3f73243deb8d67fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:08:39 +0000

    that peace thin fix clever

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
