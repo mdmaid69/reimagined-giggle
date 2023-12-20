0a12bd8ab451111a72bee864183a1a755cd37356
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:21:40 +0000

    suck thick fond cinema rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
