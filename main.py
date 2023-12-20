d62ae81fd3733b16411a7d33f9b017d20770fa41
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:46:32 +0000

    hard fast much talkoutof experience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
