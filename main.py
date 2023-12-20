cebd9c18d854725d5fe6d9587f9ac52b4fd1e217
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:34:18 +0000

    into neither up personal convinced

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.ctime()
-import sys
-def print_python_version():
-        print(sys.version)
+name = "Python"
+print("Hello,", name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
