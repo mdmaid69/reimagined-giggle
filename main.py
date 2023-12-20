d88de9a75b7e0e7e94c485f4bc62f786d0254de0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:25:57 +0000

    warn giveup murder each wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
 import math
 def calculate_sign(x):
         return math.copysign(1, x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
