d8f2718271946ce84a27db6c6e5c9bc10362f5df
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:05:24 +0000

    friendly connection point ahead apart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
