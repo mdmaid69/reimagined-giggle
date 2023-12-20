294b762063dbdbd8fde8c42177d201e8895964c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:45:49 +0000

    bar reduce then know broad

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
