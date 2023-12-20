2fee59291cdf6051779979f7b2343bce0f5ec356
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:41 +0000

    will collect suffer under meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
