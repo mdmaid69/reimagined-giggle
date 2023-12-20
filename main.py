333f41eb75c2cc74eabf39b388b1d081047da2bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:41:44 +0000

    neat average memory contribute display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import math
 def calculate_radians_to_degrees(radians):
         return math.degrees(radians)
