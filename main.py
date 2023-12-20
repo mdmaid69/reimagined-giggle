d3e787af12ac6df0d2b8b7a7b6959d57110b65b7
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:15:54 +0000

    drive curious offer clever precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import re
-def find_pattern(pattern, string):
+def find_all_occurrences(pattern, string):
         return re.findall(pattern, string)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
