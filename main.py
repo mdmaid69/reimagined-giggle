67dcf6b5472e28dfa17eefb6c5350f5474d0fdf5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:33 +0000

    laugh popular guess mistake top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
