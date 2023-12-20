26f941dfe52640e3eeb01f0d88a4600446862d73
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:28 +0000

    free grand string transport load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
