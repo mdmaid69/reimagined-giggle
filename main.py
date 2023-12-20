520243b795a8b389e237cca7a7d9e2d2a2e88579
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:59:27 +0000

    perform begin 179bite last clean

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
