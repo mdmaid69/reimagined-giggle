e0c24a67768ad4ec03948ad0f8b27743290597d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:51:37 +0000

    evening shopping receive whom flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
