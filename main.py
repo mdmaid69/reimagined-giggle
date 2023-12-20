c76bd32c6258a138464a5c9b1e3d083afc0c24fc
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:10:02 +0000

    straight keepaway failure broken hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
