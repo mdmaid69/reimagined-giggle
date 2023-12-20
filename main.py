ff81ab768eb592747d21379a0cc80fa4624c4252
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:06:09 +0000

    away trick write search bag

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
