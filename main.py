4950475f7b03345ee7f3d02761ae1a248f1b4472
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:32 +0000

    crash all until behave influence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import re
+print(re.match("h.*o", "hello world"))
