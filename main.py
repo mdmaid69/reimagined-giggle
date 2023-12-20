7d48c7fde59a859d47631ac4edc40371b9c0622c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:33:58 +0000

    bush protest closed your fortunate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
