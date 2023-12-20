28cba7bab6695372d32a24130824e695d9184fe6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:57:27 +0000

    kick pleasure will his also

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
   import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_acceleration(speed, time):
+        return speed / time
