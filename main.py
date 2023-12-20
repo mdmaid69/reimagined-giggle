c949fd10b73c51886d95fc5f43d373ece8111f1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:02:45 +0000

    room callup powerful spread before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
