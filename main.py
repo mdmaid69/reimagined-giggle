00562ffb9827be41fb3506e7c004a5fa25f2beb1
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:54:52 +0000

    steer again respect mind choose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
