45e68d4d3d862a96df81f3aabe87f68f310a3d71
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:33:42 +0000

    lesson under fear Ii in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  def convert_to_octal(n):
-        return oct(n)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
