61b1a97846775e0fb4fb38001637c328b921c27b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:42 +0000

    guide study curious acceptable fast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def add_numbers(x, y):
+        return x + y
