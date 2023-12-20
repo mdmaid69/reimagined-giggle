94a8e279ede729fbc54fd3fa6fd9e1dea201131d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:04:08 +0000

    split base process certainly figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import collections
-def create_queue():
-        return collections.deque()
-def calculate_pressure(force, area):
-        return force / area
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
