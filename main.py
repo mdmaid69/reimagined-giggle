151107f7ba558fc914f97c7b9cc59be1de3ab141
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:36:45 +0000

    repair one attention plastic level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
