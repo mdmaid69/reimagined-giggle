e82694889915dba60a3f6374fb2b9566ef057857
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:19:37 +0000

    mood draw town summer level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
