1d5bc997cf8f8f4138195501ede996d5d9c4bc1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:10:08 +0000

    damage exercise very recover breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def convert_to_binary(n):
+        return bin(n)
