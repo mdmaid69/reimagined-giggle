1ca77dd0f7f7e2643be3d593c858653271130e08
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:56 +0000

    exploration loose expression along OKorokay

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_index(array, item):
+        return array.index(item)
