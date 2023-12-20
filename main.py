1473c005b6f92a33997696c184fc74d6b4903d75
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:35:24 +0000

    thinkthrough suggest distance properly thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
