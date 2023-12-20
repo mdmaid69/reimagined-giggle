a704caad04a29c9ff326927da2004cad9b089f65
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:34 +0000

    group 569enjoy beautiful he laugh

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
