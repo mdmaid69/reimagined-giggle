39e6fec5df9794e91319c146b74c5ae65aa44f70
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:09:19 +0000

    permission area supply habit tape

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
