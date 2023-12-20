8d2a31393ad9cee606fc0c3fd455879987d940e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:43:30 +0000

    themselves prize company thick cope

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
