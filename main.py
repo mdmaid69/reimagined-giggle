2563a9599957895fbf92b402658866ea3e59e6c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:03:36 +0000

    meeting class region mystery shock

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import collections
-def create_user_list():
-        return collections.UserList()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
