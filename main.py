d609d3aeab01e3fc433e1aa68c12a8e28e5d0d17
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:08 +0000

    it middle comeround precise excited

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
