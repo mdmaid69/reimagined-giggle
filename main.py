546f6a9143433315df3477e99c3029976198d935
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:30 +0000

    everybody fasten strike delicate likely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_string(s):
+        return s[::-1]
+  def sort_list(lst):
+        return sorted(lst)
