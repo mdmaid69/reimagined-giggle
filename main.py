980a1616547b263813dd25fb4559bd215247e36b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:36 +0000

    opinion scream 567enemy scream talkinto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
