d9f8bf5f3dc9b45848dd3449fff46e04f3282d18
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:06 +0000

    unfortunate pocket night bath thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+from collections import Counter
+print(Counter("hello world"))
