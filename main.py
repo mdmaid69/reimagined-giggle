1324f2bdf3d524525b85f4b396d866e99187718a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:32:51 +0000

    strength climb since pipe wash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def clear_array(array):
+        array *= 0
