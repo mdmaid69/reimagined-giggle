d19ea8627ccb8343e20dd77e7086f63b22a03dec
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:53:37 +0000

    pleasant value passage pure rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
