40822c551b7ffca99e1980ba8d9557178614d49f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:40:24 +0000

    force rough sometimes process escape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
