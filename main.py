692314baea1033fd5d4cd7a79bc2f7f9fb849d3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:42 +0000

    wish forget choice deliver side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_work(force, distance):
+        return force * distance
