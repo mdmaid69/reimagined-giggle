77bec8aca9c1b0a90068a2ceafcd27cbe79aecc8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:33:44 +0000

    miss small over introduction since

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
