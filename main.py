4a69040007b1ea54822c27a33d307923259bafba
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:16 +0000

    service group raise form control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def square_number(x):
-        return x**2
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
