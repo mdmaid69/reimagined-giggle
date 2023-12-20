5957e91327ef60e5a472198eba9d40e44318dcfa
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:37:43 +0000

    warn violent sink straight statement

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
