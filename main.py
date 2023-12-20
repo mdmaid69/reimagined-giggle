acd155da35ecec98fd85fc7465821408ca65e8fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:38:10 +0000

    climb arrive pretty fortunately choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-for i in range(10): print(i)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
