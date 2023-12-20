8902f06ea8144d90dac4c711115e38b5246c185c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:25:03 +0000

    hungry target telephone next suckme

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
