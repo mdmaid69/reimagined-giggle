1602980fb3ef839fd6c8c156ac8b986dcd49c192
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:39:26 +0000

    offer variety age but sudden

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
