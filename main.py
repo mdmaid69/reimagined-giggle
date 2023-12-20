35af93b3a66ba7464a177dde22fa9451f4c841d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:48:45 +0000

    attempt him unit however reason

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
