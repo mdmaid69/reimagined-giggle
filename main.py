51f868b43f0e14ab987dff05eaa94f568d86c87e
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:49:29 +0000

    trust swim project scream uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
