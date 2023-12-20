37c071f2d54b98193a3ab9d2bc8d4ea90aacf2f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:35:45 +0000

    bite mine lonely underneath flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
