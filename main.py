1b8558b9a407eae138b4bc7456e77fd715b29f62
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:52 +0000

    tune test strength pleased basis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
