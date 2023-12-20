f2ba97f4ff27c27bcd3962afec5b343dba7a8562
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:56:16 +0000

    regret middle slightly ours swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def convert_to_hex(n):
+        return hex(n)
