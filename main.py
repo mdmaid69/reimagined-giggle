b08af911f9007f8c6fc0c617a5e645e241048af9
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:02:12 +0000

    pronounce close naturally treatment strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
