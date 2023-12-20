7cb15938185d8164bfa91652e330aeffc1256feb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:36:11 +0000

    argue sticky coat machine vision

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_bool(array):
+        return bool(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
