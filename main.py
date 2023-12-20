8ab7d56429375c587d91739306cd083e525d512e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:56:15 +0000

    crisis closely sink not set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
