56ae75460d9924fe8240492150b92efa80c06226
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:05 +0000

    wave cut recovery reach criticize

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
