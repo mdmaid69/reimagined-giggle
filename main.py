1efbed4c10fe0c88dc4855fcad344bd46e969471
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:50:27 +0000

    I warning look turnoff immediately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
