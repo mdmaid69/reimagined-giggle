af9ca97ac9efbfaaa9d31fccdfb49bb2480725bd
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:00:31 +0000

    difference clear good sleep really

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
-import datetime
-def get_current_date():
-        return datetime.date.today()
+for i in range(5):
+        print(i)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
