1af5ef6a6e86bde5dafc59e71c644b7acfd47059
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:14:12 +0000

    port original address tradition healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def is_palindrome(s):
+        return s == s[::-1]
+import platform
+def get_os_info():
+        return platform.uname()
