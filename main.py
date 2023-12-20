b1036296086d17622002be7afcbf01439b463221
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:47 +0000

    search rate score thick murder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
