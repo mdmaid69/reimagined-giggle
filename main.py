5ade3aa7f6629a2fe878bffd9a65885a9496e305
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:50:28 +0000

    nurse brain load Mm upset

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
