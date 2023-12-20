2620983800da52e86f2e4e826c057c4a7331a341
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:52:00 +0000

    shop partner replace prefer wall

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
