eb375411e0952c6b8316b8e150eafd93997d8c5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:41 +0000

    work bread light dear friend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
