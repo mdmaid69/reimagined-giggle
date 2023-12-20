b36687b4f3aed7a93425e467d076032ef0ddffd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:15:21 +0000

    now basically on downstairs repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
