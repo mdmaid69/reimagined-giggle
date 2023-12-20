353e5f30edbc9b7aed75a14c1e9cae4bed1d1d69
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:15:16 +0000

    door squash pity star history

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
