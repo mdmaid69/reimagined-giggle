4338ccefa12f0b6bcd14b8e9fa1bc747ea860f83
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:38:45 +0000

    nice equal talk label answer

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_prime(n):
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
