52e44d03d15761f9e791682fd79beb53ecfa58ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:49:29 +0000

    me far keepfrom please maintain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def add_numbers(a, b):
-        return a + b
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import platform
+def get_os_info():
+        return platform.uname()
