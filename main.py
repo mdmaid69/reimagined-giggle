581b2d67b55008affd7ebed59122861a65a870b3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:58:17 +0000

    many excited finally whether onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_base_name(path):
+        return os.path.basename(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
