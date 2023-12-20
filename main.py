9493521ef3937962fefb635d8443bc1e26283949
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:14:10 +0000

    deep combine bit consider fasten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
