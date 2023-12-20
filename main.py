6a5c69c4c0acee64cb346c1df85ee47a9ce852ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:38:24 +0000

    suckme very normal dry drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
