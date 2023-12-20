04cbffda97cf533abc0a4b54f86250d62dd91dee
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:25:58 +0000

    just accidentally left tape new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_area_rectangle(l, w):
+        return l * w
