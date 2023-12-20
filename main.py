2c98d710a818af5814b02a5fd67f7ce86a93701a
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:42:06 +0000

    consist combine effort crazy harm

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 name = "Python"
 print("Hello,", name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
