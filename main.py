24b5bd479a3e96d9ba469563744119e73b09870c
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:17:37 +0000

    moon union into contact plain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_speed(distance, time):
+        return distance / time
