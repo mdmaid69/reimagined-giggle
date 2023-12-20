6bc9ae25b64a95e6be1352fdef81db5706d953bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:11:28 +0000

    generally already week day release

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
