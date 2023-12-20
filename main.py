86899237e0acbc41c004bdcc3c3d783109afe15e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:31:28 +0000

    after shout confused direction second

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
