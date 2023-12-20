17e45064dd99af11e4c5961a04e3156509034ced
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:43:32 +0000

    usual mouth steep often holdout

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
