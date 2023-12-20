a4ff07e68eaf35c27c4f370ab7a32d13f6b57882
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:18:32 +0000

    build getoff after children guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import datetime
+def get_today_date():
+        return datetime.date.today()
