63a58b2082e0ccaa6f09cbe4b3da82e964cac56a
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:22 +0000

    content vanish nurse whole workupto

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import datetime
+print(datetime.datetime.now())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
