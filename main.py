1c941de14b9326ee3710c48ce3dfbd0529372678
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:46:46 +0000

    pour eat surprise keepfrom argue

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+print("Hello, world!")
