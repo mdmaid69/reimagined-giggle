ebc46464773afeee0af81bb63a72cdb816979c76
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:23:03 +0000

    glass fire book communicate whisper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
