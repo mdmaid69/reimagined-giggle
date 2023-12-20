b4f87d9467bad0a7826a1d88b1d1c99abd1b2b02
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:22 +0000

    often custom unpleasant imagine tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
