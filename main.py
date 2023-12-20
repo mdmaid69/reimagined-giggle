c530cc23a94ff0fa0b17e37ba4537f5797520b94
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:48:43 +0000

    time colour drive means force

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
