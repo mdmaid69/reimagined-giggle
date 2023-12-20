5dcf14f18fbfd4ebaeb3bdf8c8afbf848d00d517
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:29:30 +0000

    Ss increase allow risk classroom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
