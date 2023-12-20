f149cdb8cc9f4682183c76d17a49f66b3b4c75f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:45 +0000

    stay genuine though negative concerned

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
