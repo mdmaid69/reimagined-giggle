d61f8a857e249109dd62903dd68c4696f4a5d172
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:16:22 +0000

    step rub argument country blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
