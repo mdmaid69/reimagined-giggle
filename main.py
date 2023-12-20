e6e4b173adf8628d5f800cb191bac6db1d2808d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:53:55 +0000

    giveout whisper sure line coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def convert_to_hex(n):
+        return hex(n)
