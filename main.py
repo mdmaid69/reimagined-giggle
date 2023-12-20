962a8ef63e0f0a61223a4b62bec1d80233b6f0cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:50 +0000

    fierce quiet case disturb judge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
