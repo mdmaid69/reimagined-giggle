227cbc09a8ddf38bbb05a6e78f1ea2845fbcd057
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:22 +0000

    stare goodbye market getup stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
