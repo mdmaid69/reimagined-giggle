2f2f7b2b2ba3c718ad3feab8c6cc07eb39d4cc6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:07:04 +0000

    rule punish maximum until strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
