5c3ed46c288bb72d8a8d09eecf773d4dd7e8895d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:07:58 +0000

    makeinto beautiful beginning light tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
