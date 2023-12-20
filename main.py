94e8b3d79bda7c87a4b178335667e26c1d88b7b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:59:09 +0000

    goon card uniform behind consider

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
