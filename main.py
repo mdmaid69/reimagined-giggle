f2ad212e37929d5b824178708fb6f058e54b498b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:15:32 +0000

    late deaf mad about label

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-print(math.pi)
-import array
-def reverse_array(array):
-        array.reverse()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
