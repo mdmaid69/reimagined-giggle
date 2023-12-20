da2aeab696663246cb269414a48d59496ab95973
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:33:56 +0000

    angry may turnout ear factory

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
