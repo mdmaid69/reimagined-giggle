1444d46dc3eb51a728b7d591de1b6d0ac490c1d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:50:04 +0000

    myself long declaration rub within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
