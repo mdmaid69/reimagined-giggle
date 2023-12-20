03efbf81e8654c589b3230dc44ebc5b9ee6ac89f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:45:31 +0000

    corner rub send onto occasional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
