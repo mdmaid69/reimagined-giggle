d96d6632a2fef540e286fe4cfc8b920961184385
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:54 +0000

    callup free advanced fun major

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def convert_to_hex(n):
+        return hex(n)
