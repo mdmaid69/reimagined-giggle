6f8695394089da8c7f4448bacb4154fa9489a7be
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:55 +0000

    round unpleasant separate butter moment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
