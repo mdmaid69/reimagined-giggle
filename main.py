c6692792a920280f7ed53ce51892138207137384
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:29:39 +0000

    cry responsible splash sure set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
