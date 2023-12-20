868aadecb58663fa6e48179b1e0d2771f429bd73
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:26:51 +0000

    clothes blow trouble knowledge flat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
