daf2286e1dafa602eb09ed13bf8f9b0ea9c18563
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:06:53 +0000

    answer basic well shrink store

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
