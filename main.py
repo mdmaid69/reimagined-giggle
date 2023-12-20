a110bc0d481a9180ecfb063aaabc7101b858fca6
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:30:21 +0000

    further wait room used tear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
