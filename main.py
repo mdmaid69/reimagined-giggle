98ab7f846b6b93e36274d487d88d508ca057656e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:08 +0000

    surprised ourselves fight make new

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
+def reverse_list(lst):
+        return lst[::-1]
