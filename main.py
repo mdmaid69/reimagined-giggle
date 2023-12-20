385acce1092e6a1d46371c13b246561641cf8995
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:06:25 +0000

    prize wide choice frequent opposite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_speed(distance, time):
+        return distance / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
