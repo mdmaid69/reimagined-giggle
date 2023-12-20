55b32264e309a63b47e57dfecc428b35021768cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:29:40 +0000

    fly environment seat welcome sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
