0c61149eb6d3cbd12704eee8933d622574142564
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:11:47 +0000

    alive swim education taste fold

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
