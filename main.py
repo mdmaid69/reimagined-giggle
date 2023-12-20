13d918af55d00228fe70de94ce6831006bb59084
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:53:30 +0000

    water scheme name share face

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
