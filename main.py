5d0d6157a03d41978fefa106f24dd9c2916a0c82
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:21:40 +0000

    pair rush prepare stay save

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
 import os
 def get_file_creation_time(filename):
         return os.path.getctime(filename)
