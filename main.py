e3275083a150936eaa02fb31da47028b58efba11
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:06:15 +0000

    clear point question road test

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
