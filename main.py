6fba028135c3084762656e2f83ceee1c726c2fb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:15 +0000

    history corner second before Ff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import time
+def get_time_since_epoch():
+        return time.time()
