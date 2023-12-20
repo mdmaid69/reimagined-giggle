32765a12de4b174afd3da75f35ccadcc20f2bbaf
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:14 +0000

    reach police join ask much

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
