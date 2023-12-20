ac32a836a453d3e03d69a54e42f8d2e2bc46fc4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:51:59 +0000

    powerful favourite winter reduce expensive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
