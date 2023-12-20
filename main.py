cf4b957a7b9ab75fd8a29700b9c6954ea5c1e809
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:41:53 +0000

    exam thirsty task limit 565end

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print([x**2 for x in range(10)])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
