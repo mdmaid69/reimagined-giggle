33859e1c1ab9a44277126526cd652c29e2581c0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:05:52 +0000

    steam help work brain tap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def sort_list(lst):
+        return sorted(lst)
