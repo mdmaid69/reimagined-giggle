9ed5610f4f5c0b92d72984892d132589f9413f6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:13:10 +0000

    congratulations single partner cut cope

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_as_set(array):
+        return set(array)
