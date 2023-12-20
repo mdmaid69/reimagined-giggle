889c0032ef95c69288c006fb5d4708c91a6fdedc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:04:12 +0000

    below comeround form anxious fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def convert_to_octal(n):
-        return oct(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import datetime
+def get_current_date():
+        return datetime.date.today()
