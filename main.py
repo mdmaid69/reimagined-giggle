b639793ce1dd3bd758a250de16bfb101bc011faa
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:28:20 +0000

    country sure easy phone regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
