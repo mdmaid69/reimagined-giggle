adfe31b613d36efa8e3a3f0eee278daf77c90665
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:58 +0000

    steady lookafter bank bin visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
