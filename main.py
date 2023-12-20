4d00e6f9d2d96651c2088a2043552cc42defec70
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:47:26 +0000

    open exchange star care public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
