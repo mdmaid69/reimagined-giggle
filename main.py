34fb71b07e2cf2f258bab58158b3b349e5a976f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:20:59 +0000

    order turndown fail play as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_counter():
+        return collections.Counter()
