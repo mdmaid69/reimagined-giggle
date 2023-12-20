b7289a2b4166798ad9547e95b3015cfdbeb60597
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:42:15 +0000

    attempt crash now less takeaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
