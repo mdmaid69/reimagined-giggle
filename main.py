19a948755d31b6a41df3da52b0277b1f4eb297e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:58:44 +0000

    issue think conduct machine again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
