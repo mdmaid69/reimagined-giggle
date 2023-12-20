ef6de3c75f62a88175f163dd7f84c0ec117a797b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:28:42 +0000

    ought front getaway warn opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
