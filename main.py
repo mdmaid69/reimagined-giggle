5a6d49b62813c7c43c30e4d247113f9b999c70b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:05 +0000

    time speak bottom peaceful for

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_area_rectangle(l, w):
+        return l * w
