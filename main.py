5075e10afa963ce17f39b0962788b13afe1bb6e6
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:18:09 +0000

    factory book remove get share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def generate_random_choice(choices):
+        return random.choice(choices)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
