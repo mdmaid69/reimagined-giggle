3fb55d25ea474ae21ff9a10f71bed04a0af5543d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:24:23 +0000

    somehow closed container state feeling

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
