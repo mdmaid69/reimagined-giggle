77ff5f83cf0a65c7c887fa34f033fc82e9400125
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:25 +0000

    advantage theirs question swallow workupto

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
