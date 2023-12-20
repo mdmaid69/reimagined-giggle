a6b0146ccd1fd4baf71ef885ce3f14528211ca0f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:15:25 +0000

    discuss workupto describe turnoff born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import collections
-def create_counter():
-        return collections.Counter()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
