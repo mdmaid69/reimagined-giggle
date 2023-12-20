9f68c4a1c2ff5f930f659684a1320bcc4e31a2b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:59:29 +0000

    benefit disappointed treat flow imagine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
