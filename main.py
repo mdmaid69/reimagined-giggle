9483f56d9dc84c2bd6fec47f94c90abee11dd0d5
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:51 +0000

    objection house trade arrange right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
