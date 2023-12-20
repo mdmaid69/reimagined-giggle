fa7d9d6f624bba243a8bfdfb95e11a1ce9f5d09d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:54:13 +0000

    measure scene agree clue male

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
