8bfefdbd2fb4e52f877bd5f52a7bbea79f6d963f
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:02:55 +0000

    fast below cruel here pile

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
