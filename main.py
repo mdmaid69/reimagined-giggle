5ce3379b146eca6806a24def3203980ba9b12d4b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:40:42 +0000

    face generous hole secret about

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
