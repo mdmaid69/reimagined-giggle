47a83d39f1c0e21a55d47821a63400f2cbe4187b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:41:05 +0000

    door behind burn holiday push

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
