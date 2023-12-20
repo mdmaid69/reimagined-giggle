650398b31fe735156a89fed74db93a1fbe56f24e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:41:39 +0000

    turn bad first swim translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
