a83f87658efe5d1d8b510cdf8225c046400b672b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:57 +0000

    treat succeed boring serve please

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
