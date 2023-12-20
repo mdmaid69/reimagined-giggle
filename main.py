19a69b72ce40a0df4797ae78ff225507acf82ef6
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:02:27 +0000

    seriously score community borrow pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
