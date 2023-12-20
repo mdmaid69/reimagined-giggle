1f5024973e78f2a7163edc50180042b2f27d9dbe
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:52:33 +0000

    smoke offer skin trade to

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def cube_number(x):
+        return x**3
