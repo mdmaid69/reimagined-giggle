a16b15f76c7c556cab0d44ff4886af487ff1f680
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:38 +0000

    then so maintain suddenly remember

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
