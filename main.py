0bd7c667224188ef09e9b3923b121113047f5771
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:35 +0000

    opinion fierce left violence constantly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
