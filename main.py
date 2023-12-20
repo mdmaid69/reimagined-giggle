6617bbb13924084240d81825685f6910238f3aec
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:24 +0000

    top rob our account cook

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
