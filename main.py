cbe3b837dcc23b34d2e8b7e62052ab8ec92c155e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:45:44 +0000

    convincing unlikely far warn anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
