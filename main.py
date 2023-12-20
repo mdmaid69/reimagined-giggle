187a36216b66786b35767e6338acdb7ded482d77
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:37:32 +0000

    language crash care dance nigga

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  def sort_list(lst):
-        return sorted(lst)
+def convert_to_hex(n):
+        return hex(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
