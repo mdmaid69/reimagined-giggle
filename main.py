158a22f50eeffa3d06b2f10e366dc77037208aad
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:32:09 +0000

    advanced reply long question both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
