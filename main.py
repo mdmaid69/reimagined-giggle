4289ad519673764db1e2492e3df4b0f53b96462e
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:51:15 +0000

    favour towards only ring boring

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
