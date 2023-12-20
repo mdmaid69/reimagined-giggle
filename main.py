991ba9b48ea7757c62ff06158f89a38759ef3c4a
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:01:32 +0000

    rub intelligent dangerous receive refer

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
