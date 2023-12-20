8074f472bcf0aa34af0b89c51c89893337a51ed7
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:07:34 +0000

    bath shine into stick bit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
