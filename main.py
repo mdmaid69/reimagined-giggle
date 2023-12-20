0405a2806024292f78fbc755b62bfc11ad2d8972
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:27:48 +0000

    home twist fall wander deliver

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
