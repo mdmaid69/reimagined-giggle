4fb05e5a4fb0341f17f8c54be1ba07fd5693552f
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:10:12 +0000

    protect base since look ignore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
