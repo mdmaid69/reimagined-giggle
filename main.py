88e3fb15a7843370ff1f78380923d2ed45cf8bc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:06:04 +0000

    exciting disturb healthy pure environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
