47f2632b90b4ec999c4b833cfefe7fd5e3cd4d1a
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:08:37 +0000

    yes please silent replace return

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
