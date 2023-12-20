f26852cd178c5c5e83e697644273056cf8c81ee2
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:08 +0000

    situation survive section mark repeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-from collections import Counter
-print(Counter("hello world"))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
