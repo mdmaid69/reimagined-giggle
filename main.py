c5bb48f5bc291506f522297485cf1f34d2b54bde
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:57:30 +0000

    worst opportunity reply tune tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
