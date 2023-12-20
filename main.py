466df44cb83b30539d40660b9fc84e4f4b5eb8f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:57:19 +0000

    reply clothes opinion blow loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
