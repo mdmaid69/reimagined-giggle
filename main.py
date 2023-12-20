2615489aa12878aa1e549752c579111db188e7ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:08:28 +0000

    safe turnout upstairs watch takeout

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
