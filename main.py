d54992cb4f738e81a576bda91771b6e7c8bcfdb8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:16:54 +0000

    like avoid emotion bush soak

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from collections import Counter
-print(Counter("hello world"))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
