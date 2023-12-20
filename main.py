a3a08e303407a4e26720a1252455a991ae181f3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:29:18 +0000

    worth operate somebody exaggerate peace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
