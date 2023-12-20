997ff18f4335b35ab6b1c9d7e305299593a001e8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:04:55 +0000

    keepout ear confidence scream dark

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
