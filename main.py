07b918e2e7447b7f1886ec9ca333b759926fcce1
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:33:10 +0000

    feed from breakthrough think foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
