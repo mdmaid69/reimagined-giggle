4cf173bcf56734c2dc19f976a5ff412e4a11ff04
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:13 +0000

    eager world apart secret apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+print(math.pi)
