8c382c4632989c7cca3375196a49602eedbdb074
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:20:02 +0000

    blue joke quarter nothing rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def find_max(numbers):
+        return max(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
