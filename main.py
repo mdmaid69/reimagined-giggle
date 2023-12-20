c7d8a28070dca34b88bff2090117d1e2c9ef5220
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:25:30 +0000

    theirs until yet watch translate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
