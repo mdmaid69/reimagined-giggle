c8c7c77963dd503d01b30a43d0c416c0fc6f4c84
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:40:29 +0000

    born wait no future the

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(10): print(i)
