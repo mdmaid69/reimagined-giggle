06aa184e0936378ee41eca98b02a6226e07d4d1c
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:44:01 +0000

    reaction hurt secret wide translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
