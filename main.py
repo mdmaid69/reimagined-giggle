8e027ff8b1b2e3504b9f76b8ecd98ce5a68ae2c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:49:35 +0000

    left exercise figure brush offend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
