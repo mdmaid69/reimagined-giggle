4f50119ac37b4f2eb7fdfc66e7fea2663ff8cb42
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:46:13 +0000

    area correct normal share burst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
