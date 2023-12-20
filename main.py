a9001d6e2e87db873e77f3634192c242bc43e4a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:51:18 +0000

    Pp summer content possibly rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import collections
-def create_user_string():
-        return collections.UserString()
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
