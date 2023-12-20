ea2b6a91a26524ffcf3988136f59859cc199b3ed
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:07:12 +0000

    care fashion patient murder travel

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import time
+def get_current_time():
+        return time.ctime()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
