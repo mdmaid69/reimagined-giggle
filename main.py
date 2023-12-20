6c7beb8d6d0457e60c68a39b8ec1aaa410908139
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:32 +0000

    forgive separate different mystery length

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
