5ddcce7c7da23bee7ffde8bc5abc8a3d1d02fece
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:31:49 +0000

    improve runinto change suck comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
