55aa501cab4d2f902ce45b4aa88c4cb15c118541
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:22:51 +0000

    change ordinary shut single tax

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
