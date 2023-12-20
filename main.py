896349075419281e0fa3bf09bd85bd6cd1f85156
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:48:33 +0000

    turndown find he press sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
