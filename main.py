0f4d3b66b086ca9c053d26ebf9efd4e44b934676
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:04:15 +0000

    tragedy willing ashamed 572enter form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
