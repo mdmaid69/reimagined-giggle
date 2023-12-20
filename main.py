5f66d4815fe07dfb72600e8b1b9aa1c2746a3cd0
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:40:06 +0000

    taste save best letter deaf

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
