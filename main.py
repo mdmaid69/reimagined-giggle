df89b7b05079b2316169c2c73bd46e9d0138436f
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:54:33 +0000

    he winter description slice wheel

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_work(force, distance):
-        return force * distance
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
