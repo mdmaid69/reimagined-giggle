2ced1b132e876fb4bd051b4490186b12501c15db
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:23:31 +0000

    mark giveaway pair bush consist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
+  def convert_to_octal(n):
+        return oct(n)
