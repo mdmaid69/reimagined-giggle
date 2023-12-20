784ea96b4a7355793d1a51361edbb97d863cab8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:48:40 +0000

    admire juice score opponent main

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
