b80d7c51a7d16afffdaa4591608b3277978a8f1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:20:21 +0000

    notice dinner stay burst performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
