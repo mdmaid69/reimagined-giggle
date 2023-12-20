9f06f54cb84e9d496793d5fcee45e6b4595760ef
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:33:31 +0000

    extra protect away along talkback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_item(array, i):
+        return array[i]
