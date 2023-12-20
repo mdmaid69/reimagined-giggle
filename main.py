3b0513400157271551b6251cf09100971778f943
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:24:36 +0000

    dull why ask minority rid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import time
+print(time.time())
+def sort_list(lst):
+        return sorted(lst)
