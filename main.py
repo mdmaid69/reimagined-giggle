822198bbe03578f1d37307cddecb04f78a5411dc
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:37:06 +0000

    lunch answer ashamed admit feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
