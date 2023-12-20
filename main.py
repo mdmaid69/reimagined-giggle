c8781d59157b7623185cf9c09a346b578469f582
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:51:01 +0000

    keepoff foot together rather operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
