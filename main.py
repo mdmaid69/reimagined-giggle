bf6f7f52c361326503bc79224939c3bcb055d2e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:31:29 +0000

    bottom label from catch careless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
