0caa386162d287a424c83a46551968887771fbd1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:20:53 +0000

    live pen habit hurry Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
