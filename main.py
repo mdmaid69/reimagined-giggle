54dd34243a12ac41868a755cc3bd67652861c50b
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:45:15 +0000

    ordinary basis slice follow anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def square_number(x):
-        return x**2
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
