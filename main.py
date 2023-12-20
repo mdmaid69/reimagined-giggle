b7537c81d7613ada2735f409159e89576eac0850
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:08:33 +0000

    market big ability thanks lookafter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
