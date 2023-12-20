ef92ca1ad8cbb2de77932a290dc305664655a932
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:19:00 +0000

    stop reach pleasure guard under

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
