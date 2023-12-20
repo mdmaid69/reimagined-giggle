34bd9fabc68ed1cb04caecac31545babb02c515e
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:15 +0000

    direction spray damage climb recognize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
