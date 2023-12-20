e645acba5bd2d64047331cc02673ad5affb9d743
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:08:28 +0000

    Qq another wake failure contact

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
