86dc0ee5d7e2a711e88fd14be7f0e5c7ddd59fbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:04 +0000

    possession confused store achieve sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
