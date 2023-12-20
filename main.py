2b361f0dd46faa066d381352cfdaf7a2a28ac36f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:25:36 +0000

    kind related patience word second

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
