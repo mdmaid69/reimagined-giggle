337ba40069f2376ed80d17bb7d3fe93266342650
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:23:42 +0000

    intend touch exploration congratulations position

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def reverse_string(s):
+        return s[::-1]
