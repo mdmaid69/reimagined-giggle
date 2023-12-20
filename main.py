8c5b81265020d20fd84679e7f1248878f29b481c
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:31 +0000

    forever even arrangement on yours

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
