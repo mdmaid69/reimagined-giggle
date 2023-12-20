42cbd755da25a6a451b2110ed7e97715b7e81a85
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:15:44 +0000

    search thorough dinner trust cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
