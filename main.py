a037a23b6c7efe451a2bc8cd316b69dea6de5dc2
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:51:32 +0000

    too ring cool over healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
