fb3f67c9a2c4f9f60bf196397e6718644721e92d
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:28:27 +0000

    standard please legal whatever have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
