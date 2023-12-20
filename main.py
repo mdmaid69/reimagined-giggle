38f98d2c5d4b0cda5507874b5be9773905d1cb60
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:44 +0000

    arm bit weekend noisy aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
