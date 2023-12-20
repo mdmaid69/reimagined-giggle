99d7b233c96917161e8cdd69b64611d1b4629f95
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:40:46 +0000

    comeout still congratulate favourite practise

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
