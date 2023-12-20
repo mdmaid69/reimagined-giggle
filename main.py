c799e53e08d0bf8e7982356dc32180e4ec61bc31
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:59:02 +0000

    object talkback concentration admit confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
