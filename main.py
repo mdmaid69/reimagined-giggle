86138efec1eaf09d8f1ffba963dc0090484a47c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:18:26 +0000

    under thank mark key cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
