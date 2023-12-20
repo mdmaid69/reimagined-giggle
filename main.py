c18deda900487650f258fca128531ff834bf1d14
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:33:38 +0000

    bury afternoon maintain label obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
