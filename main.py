bd61dc5d196018f61202f9e15c6a8e214f972b3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:22:04 +0000

    level impossible waste material extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
