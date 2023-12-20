c0bfc2e0b349b35977e2fbd30b5685021004f491
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:16:40 +0000

    say in people well influence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
