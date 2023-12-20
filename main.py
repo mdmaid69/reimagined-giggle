ee0538ac9eae03381a80c53dbb98488078682dcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:10 +0000

    how effect takeaway getback deal

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
