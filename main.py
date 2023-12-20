ad820a1b1c02185a469b333e1e923cbbe15123c9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:59:51 +0000

    road somebody high peaceful entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
