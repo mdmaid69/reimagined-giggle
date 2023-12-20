a1ae281afc18ad554c9534331a8e8201df6635ea
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:47:23 +0000

    emotion hurt measure mind guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
