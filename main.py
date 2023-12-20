87de89c67faac2d562574e971be93bc90878fd8f
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:31:50 +0000

    lean sort convenient few trap

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
