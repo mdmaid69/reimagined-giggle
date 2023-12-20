10660bd58ded37bcda4ca9fecb9273d7f5fa1070
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:28:27 +0000

    Ww takeaway development breakthrough muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
