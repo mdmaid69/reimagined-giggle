700174d75a737bded99f8efbe29c3de5f1e31d3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:44:05 +0000

    period present between pause while

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import csv
 def save_csv(data, filename):
         with open(filename, "w", newline="") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-def calculate_area_circle(r):
-        return 3.14 * r**2
