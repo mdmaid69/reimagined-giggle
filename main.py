5a5d1efb2c4273bae827c8a5663dc1245d77c98f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:43:24 +0000

    estimate exercise remove model temperature

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
