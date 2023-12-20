a8f3bdc8f3624303f0a1f80302d41ac0b96d29ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:45:02 +0000

    argument deep ready sticky keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
