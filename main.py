db2e9cc986034b16c99fd57b7c8ad815301a24a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:15:33 +0000

    remain table possession match split

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
