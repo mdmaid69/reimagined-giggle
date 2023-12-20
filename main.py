ceab903038c5fad00eb9c5801ebedeb73902bcbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:00:04 +0000

    callup public another prove accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
