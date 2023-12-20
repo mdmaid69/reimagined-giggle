96001517e94f87fc6a725fee43466d4d09aa17cb
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:55:45 +0000

    intend patience contribute him defeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def count_elements(lst):
+        return len(lst)
