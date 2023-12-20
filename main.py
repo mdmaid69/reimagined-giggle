6b1cae23cdd9856e9d9ca426c58aa732a601b1cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:35:56 +0000

    us pure use belief transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
