fe482322d775d250231db9a18c14696b31b2455c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:22:53 +0000

    noone pause Ff market commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import getpass
+def get_username():
+        return getpass.getuser()
