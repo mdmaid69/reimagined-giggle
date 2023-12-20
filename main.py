b963409f6477fa7db1f32453ee6fddc2836684ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:52:31 +0000

    band because majority warning action

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
