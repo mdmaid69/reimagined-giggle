287b8f35a8c717ad6e2d32fdb58efd631016de6d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:47:09 +0000

    often communication great knock measure

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_pressure(force, area):
-        return force / area
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+print(json.dumps({"name": "John", "age": 30}))
