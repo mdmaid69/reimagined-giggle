6aee0ebb24e38f5fa9e7d852eb2e15f945d2ff37
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:25:57 +0000

    in head shut duty instrument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
 import glob
 def find_files(pattern):
         return glob.glob(pattern)
+import collections
+def create_user_list():
+        return collections.UserList()
