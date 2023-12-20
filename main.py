13edc3c4d0d07eae30daa125ad543cc01ed23afd
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:26:15 +0000

    soon note so passenger gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
