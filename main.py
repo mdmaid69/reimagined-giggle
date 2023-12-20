5ca3845662b5d14387449740d15a8ed99ca3b5d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:18:59 +0000

    hot nowhere appeal basically narrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_arc_cosine(x):
+        return math.acos(x)
