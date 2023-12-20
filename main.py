feb1a880907627329f2df0b954aa407e0f7260f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:05:20 +0000

    payment corner true family sweep

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
