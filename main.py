2b12bb881079726228957f6f840054440ea916ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:36:12 +0000

    unfortunate fine keepfrom twist children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
