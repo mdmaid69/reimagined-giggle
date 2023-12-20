cc9717ade56996d25d57c0471539159b79e4a083
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:28:59 +0000

    exchange payment believe funny average

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def count_words(sentence):
+        return len(sentence.split())
