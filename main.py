f4654547794603396177005f98832386ff5fb863
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:40:54 +0000

    plenty owe murder bath government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
