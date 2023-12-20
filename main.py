90346e4b198d41debd167a7191a5c47550ac8427
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:12:54 +0000

    study character not lost inform

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
