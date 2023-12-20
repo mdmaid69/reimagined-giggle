a9db864b0973edad739af6cacce0957232a4a60b
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:22:12 +0000

    ordinary book hate violent related

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def count_characters(sentence):
-        return len(sentence)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
