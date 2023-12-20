e889d89b9b4eccc4f4c408170a1487bb99551730
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:51 +0000

    save handle room of calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
