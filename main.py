70c46987b468d0a2711a535c81e63b2707367a9f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:10:24 +0000

    promise happy middle attention week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
