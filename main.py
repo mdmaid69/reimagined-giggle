294112194065d82c68a713b92c133215540b5b7d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:18:14 +0000

    suit wheel strange gas translate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
