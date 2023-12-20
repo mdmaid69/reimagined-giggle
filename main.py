5829f9f8e7030a41ad9f24f99ad6e9da5edbc0da
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:24 +0000

    calm independent fortunately achievement concentrate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
