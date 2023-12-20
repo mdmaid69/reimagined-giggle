f38d65a73a9e26b1e300a8e5aa4fba4420b91929
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:29 +0000

    service love tune human travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
