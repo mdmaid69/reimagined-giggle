eb19d94ffc570bca2488eec0084fd88b43a6b743
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:10 +0000

    neither hurt wise OKorokay awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
 import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
