1dfe4e94ae8b2247415fda5ce64e49fd76f11a0a
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:59:05 +0000

    rid blame would head trust

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
