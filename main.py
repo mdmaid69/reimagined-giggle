4b8d4f30aaf2a6fcf62a201bf668ac9afab200fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:15:09 +0000

    tongue sensitive pool climb union

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def create_stack():
+        return collections.deque()
+print(sum(range(10)))
