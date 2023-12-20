a32bf9d71fc34cbaf490348f3053f9c174b4479e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:33:34 +0000

    address hope book meet neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
