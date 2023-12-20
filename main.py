145af133b96ac5c93f76960538fc690a0ae6e9c6
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:47:31 +0000

    breathe through track fall peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_power(work, time):
+        return work / time
