607cfa0e43058f8a3680801eb1445e54553cd73f
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:47:48 +0000

    city about port over remember

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def convert_to_json(data):
+        return json.dumps(data)
