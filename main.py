f6ecd79bf95b45bcc97711bd321ede77e9975d30
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:05:30 +0000

    plan fence sport sale terrible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_bool(array):
+        return bool(array)
