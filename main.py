dae0741338dfd4b0348f94fbd5af25fa64afc384
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:37:56 +0000

    worth crash sport act neat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
