d0e6ca0305767ce4d0d2e352638bee74cb429bcb
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:31:00 +0000

    harm importance how release protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
