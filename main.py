5ad08dcdc0fa7acd88811bae53f9f8fb5d3d8834
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:53:04 +0000

    order right dress progress dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
