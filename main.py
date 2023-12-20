00947a7120be5e79abbc5df01b7ce7559c0ee67b
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:50:00 +0000

    neither move ashamed connection number

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
