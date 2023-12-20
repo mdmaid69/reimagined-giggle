9cddc76a9ec6b4b508058beed986477015966676
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:21:27 +0000

    lost substance desperate combine love

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
