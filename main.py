818ee005b1acf0808eb1445cb61982d44cb1c30b
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:56:52 +0000

    influence road week callup broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
