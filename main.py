d9173746b3c356fe8a1927e652b89b03e87379c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:08:01 +0000

    top deliberately callback holdout fact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
