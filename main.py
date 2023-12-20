012d27ad507f62924666d063f8ac200e2c64944d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:55:15 +0000

    school share apart fierce rain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+print(sum(range(10)))
