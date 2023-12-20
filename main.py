8aec5769dea69a2a4daadd7b4718a4fbdb7b92eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:29 +0000

    design test at repeat hair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
