7e18226290c499d84ef6133935cd654b0f235fcf
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:22:45 +0000

    wall attractive close do knock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-print([x**2 for x in range(10)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
