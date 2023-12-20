63918c76c3859bfcdc6e129677495b111127ed8c
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:03:03 +0000

    emotion active single nor feed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
