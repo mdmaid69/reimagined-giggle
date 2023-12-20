0b679a4c47b36385f77b01181142fb6e4d17f875
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:21 +0000

    turnover often fast recently both

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
