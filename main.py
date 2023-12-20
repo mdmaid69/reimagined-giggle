301775cfae86674c2bcdf8e6983700f6fc67caa8
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:26 +0000

    polite convincing market importance who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
