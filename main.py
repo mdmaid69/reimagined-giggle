73e5baf7d66568939b398bbbb3ffdf40ef5f7c76
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:04:22 +0000

    pupil successful cope turnaway sing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
