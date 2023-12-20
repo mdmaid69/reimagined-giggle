e1740b2ea63ab233a48d6a8cdd6b44d87484ddb2
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:59:30 +0000

    work lookup someone Rr from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
