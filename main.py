53107846b75c506d4284002178fcbd001157af87
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:11:51 +0000

    upset admit statement complain cum

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
