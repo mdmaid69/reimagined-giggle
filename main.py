327afcbc05ded103b343490de38c1176380a82c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:22:33 +0000

    suit school payment undo sometimes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
