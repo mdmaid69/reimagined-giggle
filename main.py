95a550a90442d2dfdcffb442bd82ce60a7467d22
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:07:38 +0000

    comeround spite borrow kick study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
