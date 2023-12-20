efbde627c85876f725c80f576d3e8f680262fd17
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:11:50 +0000

    breakin schedule mark union wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+print(os.getcwd())
