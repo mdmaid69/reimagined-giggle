56200932b9de534b33e95354ea632d08c6cd052a
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:36 +0000

    instrument advice tell pick achievement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
