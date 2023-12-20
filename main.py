843b727fd887fe8de6b63e67d53893f299f5cd32
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:23:56 +0000

    draw glass swallow hope easy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import sys
+  def get_python_version():
+        return sys.version
+def is_odd(n):
+        return n % 2 != 0
