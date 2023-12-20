337bb802a43f365a536dcd3fd3c10f2436e356e3
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:02:08 +0000

    thorough goout middle divide Pp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import datetime
+def get_today_date():
+        return datetime.date.today()
