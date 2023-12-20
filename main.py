e36e6efe862c75d3e33b387bdf7af0f990a6ef1d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:45:42 +0000

    agree condition lead swing disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
