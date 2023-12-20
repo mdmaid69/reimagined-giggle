0b7877c8b9d81f24039818d63ca598a34fad4af1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:56:54 +0000

    terrible seat range scrape flat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_current_working_directory():
+        return os.getcwd()
