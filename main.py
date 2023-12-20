d46f6a7137fe03335f554b807bde2ba8e1ceea41
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:44:57 +0000

    opportunity wash indeed design place

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
