0da5ef8f8f980d572d6c2eb5c56f5c7b59911b42
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:41:40 +0000

    journey avoid glass sentence could

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
