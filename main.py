261d69c195fb056ac09fbb8510a46185deddda6b
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:15:02 +0000

    twist every my strong anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
