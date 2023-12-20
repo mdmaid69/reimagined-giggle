3fff394a136d22afb08c29cdf89d97bb965c5c44
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:48:01 +0000

    wind complete silent funny name

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
