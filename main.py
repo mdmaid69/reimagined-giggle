9e640636fb349e64af9ca3338992d8ab24969b5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:18:33 +0000

    history discuss dance exciting frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
