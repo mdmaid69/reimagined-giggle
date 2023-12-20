36dabf12cc953aadfba00a1b6162a3ec9de85177
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:27 +0000

    get succeed murder demand false

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
