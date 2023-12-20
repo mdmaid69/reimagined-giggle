d62b74695dea036cdacf1797704dbdc0c85a0147
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:59:24 +0000

    number ability him keepoff follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
