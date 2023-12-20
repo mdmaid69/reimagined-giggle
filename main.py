daee48860a683b75577dc4e7c1a28f7e60d9aeba
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:47:11 +0000

    entrance comeout watch theirs collection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
