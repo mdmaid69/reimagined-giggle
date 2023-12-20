4641e4b147d46098899af2a6c99f5d4675d3c2fe
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:27:03 +0000

    carry false lead precise least

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
