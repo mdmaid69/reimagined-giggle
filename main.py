0ad12e8653b3cc853e194ff3fc6e19c0f667b666
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:35 +0000

    vanish around while practise all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
