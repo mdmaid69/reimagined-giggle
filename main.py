9e8c057248643375db8a6fd18f0ec4cda02200fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:12:16 +0000

    old solve determined clue later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_list(lst):
+        return lst[::-1]
