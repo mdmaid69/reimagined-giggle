25a0a0bc65c245f88d4dc26585450e7db2857239
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:09 +0000

    level report fault soak image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
