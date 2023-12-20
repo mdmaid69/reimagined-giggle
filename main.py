0d166d55d9be08f01722e6067b1873611a293591
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:45:04 +0000

    cause early honest better substance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def add_numbers(x, y):
+        return x + y
