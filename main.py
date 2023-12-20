ded89ccb0ab4d44c87cfe3fe458b5b7c435f2d14
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:44:59 +0000

    short order extremely prepare tend

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
