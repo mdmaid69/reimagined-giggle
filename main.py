7edde46aa93e615fe29b61142c71ccef6f6dc7c3
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:32:46 +0000

    exactly scared pool desperate content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def square_number(x):
+        return x**2
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
