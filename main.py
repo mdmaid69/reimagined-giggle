7e2acaeff6904727e152f7389d45c3f91be86497
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:38 +0000

    shock rate unhappy phone burst

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
