c56d6a584576065b30d8f7cbfc3b40fdde733825
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:27:48 +0000

    fasten rough later march runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
