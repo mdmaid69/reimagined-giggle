a7a1eae6562867447c941ad00296ba4dc320c2a6
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:44:43 +0000

    blame contact damp step trust

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_set(array):
+        return set(array)
