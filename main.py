7afcdd3442d529ddfa42f4cb05cdb85110835128
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:35:34 +0000

    lay sore way body soft

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def sort_numbers(numbers):
+        return sorted(numbers)
