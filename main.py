1761414c6a7297801862294d2ad430bb92a79a18
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:11:01 +0000

    design save fork obvious improve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
