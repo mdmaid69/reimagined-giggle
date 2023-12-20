3e93ec7807ca1bcde11a0876d1e71dabbcd2b967
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:54:37 +0000

    mainly disappear whether cross takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import time
+print(time.time())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
