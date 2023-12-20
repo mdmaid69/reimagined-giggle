4d498c2f7063ecbd540061af2abab2a56004bb97
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:33:35 +0000

    environment dish might question clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
