a7128c018864bfd8d00ecb2a13c103a9111d727a
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:46:45 +0000

    fasten motion hide battery sting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import collections
+def create_stack():
+        return collections.deque()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
