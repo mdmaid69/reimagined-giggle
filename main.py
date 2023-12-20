b13fa83ea9bb843155d682b041b264c003521f9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:18:27 +0000

    failure general repair invite smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
