377849dccca31377200d4e227321bc9869c69fc1
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:00:10 +0000

    cheque herself Pp command quit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
