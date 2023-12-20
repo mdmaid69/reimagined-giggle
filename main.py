bfbe572fbbfca34f6d5f2c5efb34d8f65babab81
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:44 +0000

    comeup evening invitation scrape sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+def print_python_version():
+        return sys.version
