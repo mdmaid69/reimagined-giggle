b33849a41254bf34ca1951715816cf706f7d1ba7
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:30:23 +0000

    band nigga listenfor equipment study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def cube_number(x):
+        return x**3
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
