1f0244193a0da6df4ac66b87e61ecc4353424a11
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:21:50 +0000

    characteristic avoid warm almost foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
