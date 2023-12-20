bb79b95aa598a11cdb901e5774b904be327ea22b
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:45:52 +0000

    round development actually thirsty damp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
