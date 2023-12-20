850c2c648e58b46ead1bc7dd7b67367c0cb0e0e9
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:53:14 +0000

    busy difference track talkoutof sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
