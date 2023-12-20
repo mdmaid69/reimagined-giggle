89bb52bdaf1a9764182f98ee0dcad2ba770fb217
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:54:07 +0000

    post week honest support influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
