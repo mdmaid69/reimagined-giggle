7e8a805cf8a3d06ec7ca0f90d13eeb5cad3f6339
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:49:59 +0000

    protect turnover mess herself lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
