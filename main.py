8bcca07e7011b74d7c23eef8a65982ed398f3bff
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:16:50 +0000

    hope comeoff recommend frequent ignore

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def clear_array(array):
-        array *= 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
