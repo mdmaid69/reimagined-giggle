c0426ef7a142c3ab64d6d89998997750af42bb4e
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:12:09 +0000

    style reliable operate underneath community

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
