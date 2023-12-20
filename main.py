ba677362dca5032a0d1524a82a33e845fa4c5848
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:25:43 +0000

    big tragedy 570enjoyable leg set

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
