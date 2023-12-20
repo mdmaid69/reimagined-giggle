992c08c53c66282661f413f463f8b031cce6e9ca
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:28:05 +0000

    noisy raw live negative forget

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def clear_array(array):
-        array *= 0
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
