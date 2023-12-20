bbdb0771ff7d48b4237953f15a3dd933b7b61fdd
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:22:40 +0000

    exactly me hole contain round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
