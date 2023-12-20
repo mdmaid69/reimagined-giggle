3eee21cabef53d3dede9d5ead10b26cd139344c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:08:19 +0000

    somehow blame turnaway wear rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
