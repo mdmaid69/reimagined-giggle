be766f23d10be91605769ab04732fe10be725939
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:17:52 +0000

    tidy trap pronunciation Ii fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
