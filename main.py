e8cca7c56de2e930d0275ca7c06d7005fb422c05
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:05:38 +0000

    level stay closely expensive light

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
