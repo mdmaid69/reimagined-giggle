10b94271dcf6302a0867daced40ca2c233ac4fbd
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:27:37 +0000

    unlikely operate far rubbish kind

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def cube_number(x):
-        return x**3
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
