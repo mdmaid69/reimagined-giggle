4735e34d920316dc29afa2cc6222d99486b3504f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:55:31 +0000

    ring paper reliable comment so

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
