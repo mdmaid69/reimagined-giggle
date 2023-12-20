4ac64ded7f53c05385354798ef720beba5a85094
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:54:12 +0000

    talkoutof temper operation who grateful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
