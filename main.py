83eb5818ea9c31b7668fb2233b76db01171a32d3
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:38:45 +0000

    possible declare demonstrate direct tie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
