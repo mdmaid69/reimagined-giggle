ee07e8306ef7653bcb8bf5e1b046a24667ac5016
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:41:35 +0000

    sea welcome recommend confuse choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
