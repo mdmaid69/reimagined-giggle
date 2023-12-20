8c12eb1894a3093a8ad67c239cf7d42a57f65ca2
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:29:19 +0000

    post describe further ability chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
