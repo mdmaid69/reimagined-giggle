44c438ac8372561a00d3584eec3440781abff036
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:24:55 +0000

    impossible strange issue fun improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
