68d26c7514db6164e695ffa905516396bf1e8891
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:40:14 +0000

    this reduce top search hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_array_to_list(array):
+        return array.tolist()
