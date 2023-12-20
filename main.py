11311b09ade56668c55250dfd55399a1489815bc
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:43:41 +0000

    previous official comeover perform fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
