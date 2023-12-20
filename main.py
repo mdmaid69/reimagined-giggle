daf3d10fef9ef50a773ea38419cf3f3cc39ca137
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:03:18 +0000

    pipe suggest shock clever important

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import time
+def get_current_time():
+        return time.time()
