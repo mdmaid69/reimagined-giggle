a2da29e755a47e64ff80230743172b864384614f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:29:54 +0000

    bar public time reduce achievement

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
