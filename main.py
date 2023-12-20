c5e05cfdbea8cd4a5c6c27fbd12c5b027452647d
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:12:52 +0000

    several face only so preparation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
