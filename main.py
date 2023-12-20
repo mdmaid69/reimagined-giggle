93a09d8d6dfcf679d42950cf255c9e0aec03bb17
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:21 +0000

    steal bend over drive complaint

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
