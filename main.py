ef03aedf7e30232c6fb7ee15a88aafb61bff1656
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:10:40 +0000

    apply probably thumb future freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
