3e774ea9937b0c0bc8a04fce222139683c06f7f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:39:30 +0000

    telephone consult market lower software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
