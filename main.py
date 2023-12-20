c1d34fb4f3fae89895c8f70d33b2254bbe603eda
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:20:30 +0000

    past yesterday congratulate turnback shoot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
