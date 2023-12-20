9057943fb9180e0c35d4a8d8a05432264bb752bb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:34:52 +0000

    pupil satisfied university dull respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def add_numbers(x, y):
+        return x + y
