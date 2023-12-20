5dd394aa3a919e1fe583827abe7beaa1b565be40
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:09:33 +0000

    pipe ago free experience warn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
