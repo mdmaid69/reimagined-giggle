79c693ce78f66fe83b86c7d6bf27993a5afd4ab8
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:58:44 +0000

    occasional man shut pass aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_set(array):
-        return set(array)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
