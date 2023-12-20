51b2f23fe47f70363c20ea76a8d3553f83166e95
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:11:52 +0000

    notice science clear consider method

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
+def sort_list(lst):
+        return sorted(lst)
