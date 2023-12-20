aeb801c59eeb826dd9419b5ca3ae366a7f6ac7a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:33:56 +0000

    classroom meaning immediate freedom until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+from collections import Counter
+print(Counter("hello world"))
