2c8a37783effb69d0e02985aa5a403f2b83f05b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:08:35 +0000

    learn win summer through geton

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_power(work, time):
+        return work / time
