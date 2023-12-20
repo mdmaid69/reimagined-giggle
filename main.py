adc113db882a056c96a4553050010104602d7e55
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:02:44 +0000

    visit thank frighten problem properly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-def find_min(lst):
-        return min(lst)
+import collections
+def create_user_list():
+        return collections.UserList()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
