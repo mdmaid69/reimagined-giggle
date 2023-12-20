b03965582a79e35f95bb6c2596e5c0b20e8333cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:19:34 +0000

    although love watch mind Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_area_rectangle(l, w):
+        return l * w
