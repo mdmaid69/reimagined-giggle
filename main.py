2b4436e27fc786a35cdd18ed5dffe89c5b215392
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:20:07 +0000

    getup rule answer pure land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
