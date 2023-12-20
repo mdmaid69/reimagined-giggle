84f5a84ff20f687c2c519d2fa767546ffcdd1537
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:47:44 +0000

    age container quit usually emergency

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
