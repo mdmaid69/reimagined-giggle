2ed6fb3c5cf9683ad88c521ce74d1e3d967b772d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:37:25 +0000

    importance hurry town proper shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        return sys.version
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def count_elements(lst):
+        return len(lst)
