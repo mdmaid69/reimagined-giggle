325c56cf2e400de48e1d1c6dfbd35af325a9c049
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:47 +0000

    defend so bottom mail pupil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
