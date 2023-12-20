82b9bcd9f8e811a783642a1a188a359b7b280902
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:34:16 +0000

    limit pretty lend conclusion trouble

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
