57cac6ee99af88b237d0c1080972cc12d872152a
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:28 +0000

    hard so character goup bang

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
