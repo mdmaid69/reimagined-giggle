b6dbf63f1a5b1e793f9b33c0dd62831695536bd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:09:31 +0000

    impressive wing fear rush software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
