adbeb9347be7b4c46eef0cc52296e9fe66c82f42
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:07:18 +0000

    level behaviour hardly expression quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
