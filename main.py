31ce09302dba07096d99ff391965c8da260e8c4f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:07 +0000

    separate avoid create translate considering

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def convert_to_json(data):
+        return json.dumps(data)
