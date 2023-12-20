1ebfdd78097913491da5625370e46a1ff1e4899e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:57 +0000

    command impossible lookback(on) central goon

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
