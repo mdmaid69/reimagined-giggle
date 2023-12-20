d8d979d32d001bddf9dcf493266784c8cb8a0d99
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:35:54 +0000

    kind also whether without new

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import time
-def get_current_time():
-        return time.ctime()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
