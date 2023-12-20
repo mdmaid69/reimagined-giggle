7f2aa264b4fbc3b4216180f7b335c64dc327b6f4
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:10:32 +0000

    equipment she fond frame runover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
