20bc9db3c1f6dfddeab785c0e3098e9350f56063
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:45:37 +0000

    minority national environment worse glass

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
