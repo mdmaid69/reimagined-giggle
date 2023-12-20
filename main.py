07f5d21eaf54eb331c631fa7083b26c5efcd932f
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:10:02 +0000

    welcome anybody angry strong excuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import time
+def get_current_time():
+        return time.ctime()
