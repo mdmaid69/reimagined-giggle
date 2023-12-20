6de0a3f0c9631d8265d3e57792c7673a514a19a2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:40:15 +0000

    somehow arrival passage hate giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
