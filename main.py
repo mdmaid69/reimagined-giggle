7988521b39e679da8e0074a130c8a6c82184a0ab
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:50:43 +0000

    smile mood leave suck meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def is_odd(n):
+        return n % 2 != 0
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
