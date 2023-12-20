a9a536b967279a242c39746d3b3f909a597b8724
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:26:04 +0000

    long flat cruel visit extra

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
