6000b315165845e27347a3096ce2714a3b068cdf
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:25:11 +0000

    body giveaway wake stupid tear

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
