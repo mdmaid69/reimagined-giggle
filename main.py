2518fa9a08325711ee262ca31365ff16d7a63753
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:11:51 +0000

    attitude remove comeback tidy such

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
