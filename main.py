ec5d1b219213625edc56223c35a830296088f8a7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:34:19 +0000

    key near insist list upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
