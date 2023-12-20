4277cbb342581599a6d9cb6024306a51f9669b0c
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:14:02 +0000

    window possibly accommodation turndown sink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+print([x**2 for x in range(10)])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
