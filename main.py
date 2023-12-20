c0f77cf6194a46c0fe2bf146f2e448b01b29e464
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:42:22 +0000

    together secret steer immediate view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-for i in range(5):
-        print(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
