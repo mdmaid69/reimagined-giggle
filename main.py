16df7b800474a3155250d28bb753269c1b0371be
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:05:57 +0000

    respect right mention zero scream

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
