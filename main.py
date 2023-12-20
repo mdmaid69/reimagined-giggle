8c58f6c571bbca713bc8c490e727295894bf2071
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:23:34 +0000

    concerned someone spin opponent definite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
