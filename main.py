334cd8733ce0ec8f41dbbabaae9dee7cf027c3b6
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:43:10 +0000

    child 566end Zz make dead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_speed(distance, time):
+        return distance / time
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
