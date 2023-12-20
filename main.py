9fd64050b98e4eb6f86ddd7a12cec86f9c5c93e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:00:52 +0000

    colour hope scream surprising play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
