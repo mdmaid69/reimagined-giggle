010e1d533e6bfb87065a8e759c35b5f924681aee
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:54:21 +0000

    strike flight modern definite late

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
