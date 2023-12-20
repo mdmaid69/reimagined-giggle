fb43843531069938baf05722abc0ebd695d87621
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:51:01 +0000

    plan show trap sick empty

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  def convert_to_binary(n):
-        return bin(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
