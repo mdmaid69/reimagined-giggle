a27c49bf46c0e4575b795abe1a929591f0d16131
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:28:30 +0000

    responsible fasten house certain fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
