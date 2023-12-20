c104a814398c8a56f8bda58ecc3a8debda122b4c
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:35:58 +0000

    argument runover statement pick press

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
