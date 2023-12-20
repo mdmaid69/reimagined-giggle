a1a2380dba0c67c9ddc89de02d516e5a408b55fb
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:10:14 +0000

    surprising waste anything cap payment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-print(os.getcwd())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def reverse_string(s):
+        return s[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
