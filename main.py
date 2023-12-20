1b3e8fadcde1411175e54ce8b2dc028e3d635c17
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:18:13 +0000

    window difficult trip if past

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
