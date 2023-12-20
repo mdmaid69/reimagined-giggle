b9f5c612ce435fabe91cb9aada73008480f77e34
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:33:52 +0000

    criticize rid pride Rr prepare

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
