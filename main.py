73d6e195b4a12a2380e7a116219e2b592c611590
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:47 +0000

    shout only owe tidy shut

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
