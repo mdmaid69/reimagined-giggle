afb84d4c8c3f18c945e337c8c96a607601a1089c
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:17:35 +0000

    recover next load clear exchange

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
