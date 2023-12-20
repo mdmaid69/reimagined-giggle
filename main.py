dbbec5708ba0cd4c40e277f7d6f0d3ae6d85247d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:43:54 +0000

    occasion original report fire meaning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
