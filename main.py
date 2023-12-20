1e93c9294607ea9cd31ace7eee0e02dc09cfa046
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:56:49 +0000

    keen again opposite eat expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
