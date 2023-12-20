468651a9815cdd1539f6459004f4020d13ea4350
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:39:15 +0000

    holdon climb pick mean tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
