fd4eb6319498b7c9996097de54624ef4f478616d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:14:35 +0000

    cool almost attractive repair feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
