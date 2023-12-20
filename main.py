07817b79bc83bd3e3a7e0066e16e7046374942b8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:09:25 +0000

    foot brother sleep upstairs sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
