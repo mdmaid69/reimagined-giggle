441253eacc5c696e51883a5f50454d396d50553f
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:35 +0000

    about partner dance tidy street

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
