a617596cab7b7826b7ad5a1534005d4dd3a89291
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:31:45 +0000

    condition shop yours unfortunate begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
