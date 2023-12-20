22454b4c7e9588142859e6fd0e2837418e3f6129
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:45:56 +0000

    attach wander mainly boil small

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
