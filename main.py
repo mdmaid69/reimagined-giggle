e9909ded46cd7bafb39e52b0d7098d29d042e202
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:51:27 +0000

    situation road protest skin realize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def set_array_item(array, i, item):
+        array[i] = item
