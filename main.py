d90c27448d1761263c703f1e9ca496e03d5df594
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:41:12 +0000

    defend eye bus everything bored

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
