5873b1fcb725c6dd15b489e299cb605b6bbdfe52
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:20 +0000

    border feeling average pleased talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
