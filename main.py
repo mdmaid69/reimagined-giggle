d8c65dd7c37fd64bb99693428505d9d149562940
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:49:55 +0000

    how moral confidence minor bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
