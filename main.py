a90fe2cf134aff9733122dda53a39a41df799e12
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:20:51 +0000

    concentration unpleasant natural slow flight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
