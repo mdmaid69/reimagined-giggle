f07f6d31b95ec2c22a0bac7343049583f24fb8b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:15:12 +0000

    situation neither divide drive battery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
