30b08409652b9f7d6120b2897fe6e1f940f3f0e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:04:33 +0000

    reply operate card wish unfair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
