a2169faae4956f28a12f5f57f5be850dbf83ebd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:21:20 +0000

    Oo fall each consult nigga

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
