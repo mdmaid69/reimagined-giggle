10ab470a116518fd5a9f5aab8bc2bc4e218e559e
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:31:12 +0000

    thing matter careless Ff summer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sys
-print(sys.version)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
