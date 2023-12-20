96e3d585f6538605ca988bec1db1459bbfa4061d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:23:10 +0000

    Uu professional in laugh level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
