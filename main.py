1b91e1780ee851f3d3bfc5bf358c21c849f86f91
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:53:05 +0000

    contain congratulate limit afterwards interfere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def convert_to_octal(n):
-        return oct(n)
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
