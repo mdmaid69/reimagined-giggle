fc3d88f5996fad589034595cd2948b64bb9c2bd2
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:34 +0000

    pencil cheek remain shut slice

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
