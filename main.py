11628470435c5fb8e2470e57c4ef9802e7cc4dce
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:17:43 +0000

    now often complicated over lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
