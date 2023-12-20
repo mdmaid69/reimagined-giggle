24cc9a61d8d852051ca37aa9754ca77c6ec341ae
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:38 +0000

    star settle connected turnoff splash

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_array_to_string(array):
+        return array.tostring()
