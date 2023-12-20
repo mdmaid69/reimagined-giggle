287ec44bcc8fed580567a025d4700405c03d26a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:10:23 +0000

    Dd bus squeeze sad decision

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import time
-print(time.time())
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+import re
+print(re.match("h.*o", "hello world"))
