e1bf3d84c8128fd96f812d5efec1932e8d2aca23
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:07:15 +0000

    difference downstairs steep father bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
