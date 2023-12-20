edb2a1c581b63fbbf9aa35d89993b0f6e43fdf58
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:05:21 +0000

    wish unlucky shake pride round

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
