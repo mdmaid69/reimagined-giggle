2672b206c056cea26b6d180b5a4b3b7a6c34e02e
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:44:27 +0000

    convenient sure available less minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
