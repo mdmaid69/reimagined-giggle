b5a82f4b00dcdb7f02c4d680ce640f8966add008
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:16:40 +0000

    grow extremely chase this remember

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_average(lst):
+        return sum(lst) / len(lst)
