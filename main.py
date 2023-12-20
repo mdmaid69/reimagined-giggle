8cd1271a318acc658922a42adb695bdb924028f8
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:22:54 +0000

    approval discovery hello stretch valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
