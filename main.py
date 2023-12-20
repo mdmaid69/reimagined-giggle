05a1fcdb0ec81f65d73b55c6c809ee4d4c08664c
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:38:35 +0000

    middle wave thirsty claim tip

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def multiply_numbers(x, y):
+        return x * y
