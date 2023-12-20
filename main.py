68805696c19becd2d45904a7e6b62e3f5bb9ab9b
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:49:31 +0000

    swim carry about pocket pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
