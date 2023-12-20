841453f2e5f81807b1c5737b420a4c749ebc1013
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:21:37 +0000

    destroy sweet science spite wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import sys
+def exit_program():
+        sys.exit()
