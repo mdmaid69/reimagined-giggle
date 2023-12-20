b932852fddfbb7479f88abfe17214993b0824f22
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:49:09 +0000

    place take skirt scared takeaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
