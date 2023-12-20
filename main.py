3423d9f8ba959bd8bf26987062418ccd5fe8fd9c
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:06:49 +0000

    hurry miss flow range rescue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
