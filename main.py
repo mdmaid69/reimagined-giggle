60c8a9535cc6b1c5dc23aa824ad97844b6dbe9d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:16:01 +0000

    unless later file mean goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
