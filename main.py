d714aeb24073ecb5fa140e0a47ffec993a167933
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:46:04 +0000

    flow judge back comeup command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
