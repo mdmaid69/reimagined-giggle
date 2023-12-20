1fccf96a033503b2e64331ba7c64ea96d3ed795e
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:00:11 +0000

    sale warn mark advanced temperature

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
