3365216fb6082b9d08a6a72ededb1851027e8ecb
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:00:22 +0000

    cheek minority equal before considering

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
