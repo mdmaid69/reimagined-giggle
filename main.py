e9c0e59b19c1a180c388777ebfdcacad8d7daf24
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:38:30 +0000

    maybe getup approve image contact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
