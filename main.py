bf5ce73f427d666add4dbf38738d62b06fd72c4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:31:04 +0000

    length thirsty stay tape motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
