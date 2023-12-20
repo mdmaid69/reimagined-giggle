be65e05994fc20ad78a8d9b62c343fe3e6e1032d
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:26:33 +0000

    anything pride sad chance uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
