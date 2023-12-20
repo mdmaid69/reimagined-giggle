2ad848e61ea9acd040771ea7bf332a45ab766dc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:57:31 +0000

    spill mess strength runaway present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import time
+def get_current_time():
+        return time.ctime()
