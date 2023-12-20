a752f86cd656f4b25497c5211e161efa1bb38a83
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:17:03 +0000

    would band learn freedom OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
