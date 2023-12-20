ea8e868388e96bdaa35f6ed7fb8b0749ed5816b0
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:30:26 +0000

    Zz pin embarrassed aware return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
