8e275a04e258291a581f65d8e140e8976397862d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:21:15 +0000

    otherwise contrast neither exist breakup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
