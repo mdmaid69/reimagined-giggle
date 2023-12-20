f14c3fa7e76c71567f7c41b004d6d159e58a1c30
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:49:48 +0000

    official deserve might their apart

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
