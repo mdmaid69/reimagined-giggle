d053e2b5bdd244796dd5d90927cdebb53a997720
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:57:28 +0000

    sure control contribute will though

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_list_from_array(array):
+        return array.tolist()
