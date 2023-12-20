774a3d6bc715c255b62f6bb510806f507f14d28c
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:59:37 +0000

    firm wake what allow thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
