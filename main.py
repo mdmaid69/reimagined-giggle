aa4e6dc59206303349f22cc73efb3123b7cc569d
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:14:15 +0000

    drive none getback left turn

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
