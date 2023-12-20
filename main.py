e1b0e32ea9c985f394c867a2842b08d8cb0bd33d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:35:48 +0000

    after tidy scared press only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
+import array
+def get_array_typecode(array):
+        return array.typecode
