47ada1cdb8d0de0ee064c27cc6dd334052c03a9a
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:08 +0000

    cinema convinced shrink tread forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def log_message(message):
-        logging.info(message)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
