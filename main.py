f3de854a1f45a93af4e4a38d5dda115d585d1c15
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:48:23 +0000

    form trouble fashion watch noone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
