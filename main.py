7111ce25df586e25a85428ba3b27dc4939ed9073
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:08:20 +0000

    but refuse score prefer reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_power(work, time):
-        return work / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
