3d7bd61f79894856d2c53063e4d0c9647a9f57c0
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:08:22 +0000

    hair fond runout outside game

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
