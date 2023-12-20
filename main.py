f7e6f76b8790784ffc908c6816d36894cef4886e
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:59:55 +0000

    speak treat afterwards start floor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
