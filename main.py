9384660689e9f8666dfe72145b8f456b02ac27c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:46:57 +0000

    impossible better choice ready sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
