393bb08589e78be2166264a6d18ce89fad93b43b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:03:18 +0000

    history mood show proper likely

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
