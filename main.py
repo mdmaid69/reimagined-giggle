d57f0022cae62eb4e42dc160c7c7fb8c9b13cf34
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:18:11 +0000

    edge Ww run freeze great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
