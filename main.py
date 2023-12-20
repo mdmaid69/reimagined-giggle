3a1a2be8e6fba5431e242aa43f3f3e57b741b23d
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:00:39 +0000

    make normal callout card correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
