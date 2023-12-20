024635990a9859d464dfb2dd3359d98780f8969a
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:43:02 +0000

    cheap gap argue supply wherever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
