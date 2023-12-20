acc752ca40b764db48eade8d6aafe4e113098107
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:04:35 +0000

    telephone treat petrol trial beautiful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
