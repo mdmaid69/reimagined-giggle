412b9e904f5b2a0bed2d03e4aa981e922d53f2fa
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:22:53 +0000

    relation giveout consist tap material

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_union(list1, list2):
+        return set(list1) | set(list2)
