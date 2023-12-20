f7b5741bcc4dff1ae36730bea607ad792b48cff8
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:46:12 +0000

    bang country involve show waste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import getpass
+def get_username():
+        return getpass.getuser()
