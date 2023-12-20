566156a74fa927860a3d41b0403bfdf74c313542
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:38:08 +0000

    family lift store after please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area(radius):
+        return 3.14 * radius * radius
