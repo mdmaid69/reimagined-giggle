a301648cbc8ba41364364b9934707b1d1f08c113
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:00:16 +0000

    anyone someone condition flat payment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
