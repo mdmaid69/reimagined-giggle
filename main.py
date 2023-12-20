4495390f212220e2b9b2c9a8d05faeeae4bd01b2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:57:03 +0000

    brief small able estimate 566end

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
