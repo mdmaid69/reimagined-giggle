5462572bbb1aed2b25e144da0beba26dffceb038
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:42:20 +0000

    label mix line come less

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_current_directory():
+        return os.getcwd()
