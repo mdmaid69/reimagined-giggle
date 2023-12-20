0b62f48aa07164eb6f94622dda21c336d1c2f7f6
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:45:26 +0000

    success final neither improve description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
