56a9856c80f54921f6ac2455ff4c74c972db511f
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:51:45 +0000

    control tend receive decision 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
