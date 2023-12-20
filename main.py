3147208048c8f97a58b11c07a07301bad2ad9627
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:12:58 +0000

    holdout finger apart low frighten

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def divide_numbers(x, y):
+        return x / y
