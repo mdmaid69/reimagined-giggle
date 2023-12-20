b44e5fa72ea5a1cfbe6113af5c74ade4b949b789
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:33:11 +0000

    practise reason dick equal getup

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
