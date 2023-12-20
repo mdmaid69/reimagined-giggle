32db9ef33240df5934b08ca53377043775736d0b
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:08:07 +0000

    them please permission watch behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
