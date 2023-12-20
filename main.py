72b6b3e7815d0d2ce76b4aaac31431afa014a2ba
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:10:48 +0000

    hold instead bright book minor

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def find_min(lst):
-        return min(lst)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
