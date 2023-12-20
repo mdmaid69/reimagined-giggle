2d8b4a38d2e6a488df58958096495949bb29a6cf
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:41:11 +0000

    group brain soak comeround set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_work(force, distance):
-        return force * distance
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
