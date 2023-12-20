eb86d9cfd4757269f89ba54c2e0a75e6a6c11393
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:32:30 +0000

    us vision wish pity long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
