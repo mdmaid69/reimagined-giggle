c520c0a4150cd97c8db273f90fc99ffbfc986bd9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:53 +0000

    freedom loss perfect better leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
