2adbd7b57f0bae0004bcdbe6f6ee9ea92e0914e5
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:13:27 +0000

    if this chance realize noone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
