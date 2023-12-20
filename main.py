f170c679a8acad16d3af8a51ed465bf94d50366d
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:51:56 +0000

    trousers task throughout total blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_power(work, time):
+        return work / time
