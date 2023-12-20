7a0f0561160af912978c45265b724171627e8a16
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:32:30 +0000

    example as get violent chin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
