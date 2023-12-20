f5354bba91abce7ca761b8c421ed8fa111a73438
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:22:16 +0000

    matter possession country age sail

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
