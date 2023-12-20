712af4a4187d627286f8a6aa23c4c86a2a2d8258
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:13:41 +0000

    onto war frighten into pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_stack():
+        return collections.deque()
