093b539f411119dd7566a472b1ef5127c79513b1
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:16:24 +0000

    war even idea shock means

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
