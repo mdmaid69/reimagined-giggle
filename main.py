41608b0ae21ea79c2db7d79db2521e9e3263e5c2
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:33:00 +0000

    shut holdout ours drop both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
