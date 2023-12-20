4a91bb0f02d79e877bc5d9b8b04a0f635227f277
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:02 +0000

    call takeback coat funny her

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
