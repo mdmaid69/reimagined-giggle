371d543c812642721265eb287f9df0b8577f3bd4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:01:27 +0000

    exhibition bag bath rank birthday

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_power(work, time):
+        return work / time
