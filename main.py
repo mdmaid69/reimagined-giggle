63e7d646bf77f0b70f84472af71f250acdc9af93
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:38:57 +0000

    shout plan pair entry comedown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  def convert_to_hex(n):
+        return hex(n)
