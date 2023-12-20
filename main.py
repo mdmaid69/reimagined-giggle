4933dfc6f64c2a5186ee9eee605e1b2394ebbbe2
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:33:05 +0000

    unlucky godown kind for Nn

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
