d076b434c49059d0a296e11047e150ba64fc1e42
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:43:40 +0000

    year sweet unfair previous advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
