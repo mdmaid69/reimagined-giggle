b67db8a528706836dc76a5a68754c252c9905af2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:43:33 +0000

    vote familiar unusual talkoutof nervous

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
