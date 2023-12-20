a73ead55afd97031f5247a1c81353606fe62e883
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:00:05 +0000

    stamp woman store means mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def add_numbers(x, y):
+        return x + y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
