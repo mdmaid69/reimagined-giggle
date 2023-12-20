2f7c2c0d1bce2613a64eb98713ef25b84617040e
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:19:41 +0000

    watch at enthusiastic wealth in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
