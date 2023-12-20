6f8c2136dd18a0b1b3054e8b8919f6c51f1cff79
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:52:16 +0000

    put involve eat workon ours

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
