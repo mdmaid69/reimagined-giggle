5b61b53c78cc7c7b33439ecd88f7692e0f76644b
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:13:33 +0000

    yes challenge trip favourite cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
