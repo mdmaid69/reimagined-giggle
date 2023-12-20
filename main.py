355181bc62135bbaf5fa47965e0307a5be41c05c
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:44:07 +0000

    win comfortable state fashion level

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
