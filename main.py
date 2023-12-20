576e1252c97629a820db7d4c7ad035c4a77c87e4
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:08:59 +0000

    failure minimum cap basic high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
