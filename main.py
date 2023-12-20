2c692af192724cc1f9ea5b6ea21dc68250642edd
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:58:24 +0000

    gas thankyou burst hit necessary

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def count_elements(lst):
+        return len(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
