d3bccdb1d77ff9adc1f5aeaee17c58cd2506c4a8
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:03:17 +0000

    open float healthy judge cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
