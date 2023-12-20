0d03666d2bba3fa952a58eb981def7cedc04f690
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:28:50 +0000

    thirsty degree success vision balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
