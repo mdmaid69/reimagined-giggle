1b2e9c4864574e08a5450185080081d2ece79e06
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:36:12 +0000

    order there shoot professional creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
