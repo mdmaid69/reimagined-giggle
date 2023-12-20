19383a81ef94fe09d9bb885c98d9db9bd40b7c52
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:54:59 +0000

    high flood couple programme trust

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print("Hello, world!")
