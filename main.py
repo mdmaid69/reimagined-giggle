706df133fbc6fe1f597376ddf1f069b5c09b861d
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:27:43 +0000

    might top include getaway container

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  def count_elements(lst):
+        return len(lst)
