02b3163a9b7e13a15853dcf58002b7e6103b19e7
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:55:02 +0000

    female watch excited desperate neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
