ab7c5809765f78aca09962f317873ae59353f675
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:15:59 +0000

    repair grow character far interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
