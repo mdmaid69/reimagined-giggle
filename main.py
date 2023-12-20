eda517a6d4d16eb0f3b11c26fe9098ac7be9db6e
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:27 +0000

    worry suddenly shop play confuse

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
