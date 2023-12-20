a020be28ad6bf7804d7bc7e225a0fb3965118995
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:24:01 +0000

    sure copy wind mysterious damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
