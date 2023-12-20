e133ae7c5a56b2abbe6dcc653dc107a276e9ca22
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:24 +0000

    nice upstairs also when pour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import platform
+def get_python_version():
+        return platform.python_version()
