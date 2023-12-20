d60395b9c122254f4f63424b5c4e3b68bd946ed8
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:47:02 +0000

    lookafter money knowledge major instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
