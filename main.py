5004be6b7409ebc9311908cdffa5b0d42ac5e962
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:52:57 +0000

    neither first shoulder disappear forever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
