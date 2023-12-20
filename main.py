f0b401c403e030a783bf1de83157ec626c96b44f
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:50:01 +0000

    survive slice position depend hot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
