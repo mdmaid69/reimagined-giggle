372811ff34cd51653e52edf6a52d9c3f732cf796
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:47:30 +0000

    Ll fetch open set bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def count_characters(sentence):
+        return len(sentence)
