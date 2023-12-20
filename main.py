74cf73caed0878b4bdd886e721e8dbeb2b33f921
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:35:58 +0000

    day evening strong upset steam

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
