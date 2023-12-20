1a6fd47dc267ad84a60aa2aae1278769d73f979b
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:11:25 +0000

    favourite convince there land children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
