ce1f9504ec957e4d612b35985959787dda086997
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:02:49 +0000

    backwards better polite ask list

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
 import array
-def append_to_array(array, item):
-        array.append(item)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
