7149cd35c34bf411bef69e6f95a5fb8f456c61cc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:00:30 +0000

    or interview appeal payment spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
