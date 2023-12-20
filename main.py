7b5e8d7465bd6ef936bbfdf151c2a2f165ff53c7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:23:54 +0000

    satisfactory set mad offence address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
