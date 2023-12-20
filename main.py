2ee4c8818a283b6c8f8decf54f30c4e1e935ef2a
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:35:34 +0000

    reply settle keepaway entrance turninto

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_pressure(force, area):
-        return force / area
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import sys
+def exit_program():
+        sys.exit()
