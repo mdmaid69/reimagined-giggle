0c3fadc3f23a02474ce2db8bf99d2724f38f6ac3
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:13:42 +0000

    kill calloff concern scared record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def count_words(sentence):
+        return len(sentence.split())
