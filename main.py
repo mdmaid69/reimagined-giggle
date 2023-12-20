f1d3c0f0422d03c1bd75e55919c13064264e099f
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:26:27 +0000

    level label early increase game

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
