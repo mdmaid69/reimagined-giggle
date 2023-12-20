0d0304548f34109cab00d954bbee1899bf145da9
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:35:19 +0000

    finally already hot pen reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
