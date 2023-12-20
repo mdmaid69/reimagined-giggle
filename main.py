71c925b812cb6977b049ff3ddbfa92c30d89c9c5
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:59:38 +0000

    satisfactory strange limit me child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
+def reverse_string(s):
+        return s[::-1]
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
