56e80bba9bca81e467075af945bde19a7ab89209
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:53:24 +0000

    prepare drink sale them crash

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_exponential(x):
+        return math.exp(x)
