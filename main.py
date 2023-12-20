c7307c412bfc3888e22c0b90e22b8a14b3785bec
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:37:54 +0000

    forward mess large blind age

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
