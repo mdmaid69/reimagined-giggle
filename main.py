82a626c460d31d520ab36b611dc41e8d283da0a3
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:27:00 +0000

    very goback(to) instruction only sense

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def sort_list(lst):
-        return sorted(lst)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import sys
+def exit_program():
+        sys.exit()
