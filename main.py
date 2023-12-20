8412deb6be71dc0a29e0c83b4e042bb8732568f3
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:34:32 +0000

    general tradition makeinto run neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
