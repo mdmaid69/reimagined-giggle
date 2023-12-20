947768685b45e320ba97e4aecf9a396b90cdebcc
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:59:29 +0000

    cut season neat 179bite believe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import getpass
+def get_username():
+        return getpass.getuser()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
