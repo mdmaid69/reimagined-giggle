2492830efe076f911312e380fd6a27e2fbe46a5d
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:03:18 +0000

    worse know deep nervous sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
