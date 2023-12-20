8c84f40d194b327486eef26b7ebcbdf74ed74e52
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:01:58 +0000

    debt street expensive breakthrough together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
