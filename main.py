5f38b7ce3e248fe7fa303f3d7ada879ac0ce2a74
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:08:01 +0000

    guess hour performance somewhere as

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import array
+def clear_array(array):
+        array *= 0
