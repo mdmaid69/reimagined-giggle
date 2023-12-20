0dde631a7c69f2ffa5f73e84fb8e2efaaf17ad9d
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:41:23 +0000

    trip skirt light collection noisy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
