3f789e181d2eecd77746bc8e4a7485f32efdbe73
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:31:12 +0000

    whom trust lot strike loose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_circle(r):
+        return 3.14 * r**2
