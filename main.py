764c783ffa61fa9ee10eacfaf168a70db829c262
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:51:32 +0000

    mind tape sight 567enemy cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
