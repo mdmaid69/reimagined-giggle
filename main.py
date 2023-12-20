c5012ca2036336700d7a50e3040a82bef132ab83
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:17:47 +0000

    rescue bang tear happen pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
