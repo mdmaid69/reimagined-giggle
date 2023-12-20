634b21e1a484806a84d6651d2f45e7d786611f36
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:57:08 +0000

    machine listen order weak which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
