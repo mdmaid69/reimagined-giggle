bec7e7806fde5f80bee1a5732b7a4a8df616f2ee
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:32:37 +0000

    unable opponent nurse consult depend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
