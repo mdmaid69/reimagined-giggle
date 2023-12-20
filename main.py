d1f80c30b197dc5a842adef2148b2d6803f719c8
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:57:08 +0000

    through usual environment thirsty break

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
