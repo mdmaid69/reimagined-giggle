6581f0adf3626d46f58c9b802715f304d0d04df2
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:06:46 +0000

    please decision programme be physical

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
