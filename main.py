ba5eebf73ece4cf8fd922e0ed43923e6dfb3a600
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:00:48 +0000

    mystery equal pause breathe suckme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
