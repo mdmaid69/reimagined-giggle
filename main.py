12775a3351bd2601931b33379245eb19a5f90e1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:13:08 +0000

    central moment from brave taste

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
