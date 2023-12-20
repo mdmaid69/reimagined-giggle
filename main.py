c399a261389dbe961e3883203940af3f3ec0cc91
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:41:08 +0000

    consider practice bend taste apply

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
