7876ac78f8c3293c4735b57c76c9e3be9023b5b9
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:31:26 +0000

    should afraid public often fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_area_rectangle(l, w):
-        return l * w
+def clear_array(array):
+        array *= 0
