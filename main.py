7888c0c70bee25ac8a78b9277a12b04763acf619
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:24:29 +0000

    pretend mixture when promise swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
