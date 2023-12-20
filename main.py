598187130a87e85c12a3250b5d1573a8edd23c11
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:17:00 +0000

    happy comeback holdout waste clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+import sys
+def print_python_version():
+        return sys.version
+import getpass
+def get_username():
+        return getpass.getuser()
