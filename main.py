57bd27d2d641074ff0fcc869b6dea75fbbc34a69
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:24:38 +0000

    suddenly responsible agree unless suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
