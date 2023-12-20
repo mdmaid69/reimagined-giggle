55c6725411ceb25833bdad74eb59c14529914972
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:50:23 +0000

    criticism flood even memory might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
