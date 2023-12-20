6c7c6737521bfdbbfc8ff426ae152238d4f4f870
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:32:41 +0000

    relax bad return send port

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
