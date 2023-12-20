d958ac149a64cc97b760613adb478827f98fe6eb
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:58:19 +0000

    refer check sleep dry high

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+print(os.getcwd())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
