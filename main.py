52873fb8c810de8c60ff4aeebee698b7296241e1
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:28:41 +0000

    no smell contribute better blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
