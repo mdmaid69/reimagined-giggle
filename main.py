7db30768baabb3250dc70f1dfbf9ffafc95259cd
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:47:44 +0000

    difficulty gate if car turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_density(mass, volume):
+        return mass / volume
