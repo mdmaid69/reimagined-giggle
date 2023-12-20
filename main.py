74ba206c7cc95aa7bc48b76fa5e076118abd1bed
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:13:54 +0000

    underneath calloff spring hope speed

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def add_numbers(x, y):
+        return x + y
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
