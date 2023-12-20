aa862688a7e670d80b3ceadd2e72b2e20cdb554d
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:42:31 +0000

    next excuse thirsty disturb fault

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
