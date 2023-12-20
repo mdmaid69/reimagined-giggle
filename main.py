8662a2f46135ebd1b857a21e3692af7fecb147d4
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:31:16 +0000

    like special translate concerning idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
