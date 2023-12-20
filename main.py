f76a7edcda5dde6c6f0871ac3441e7f83521e879
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:29:30 +0000

    serve answer sentence responsible smooth

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
