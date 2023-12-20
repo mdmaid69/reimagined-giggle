2a60d8d7a5f00fb34d1217556d0978a8a3a850d9
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:35:00 +0000

    account tidy tense smoke part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
