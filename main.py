2dc760faf6145c948630c8f1f5a2a1cde9b60c1e
mdmaid69 <robot@alexsyw.me>
2023-12-20 07:40:32 +0000

    squeeze whoever die wise light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def cube_number(x):
+        return x**3
