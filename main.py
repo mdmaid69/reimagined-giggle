3741b74fdde0f0bcd705fe3111780cd3d4b38280
mdmaid69 <robot@alexsyw.me>
2023-12-20 16:23:41 +0000

    scrape wing married foot convenient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-for i in range(5):
-        print(i)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
