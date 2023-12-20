fcef160b4715da7ff2ae106bccab4e00f7f0d99c
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:42:09 +0000

    wash life use behaviour pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+print(math.pi)
