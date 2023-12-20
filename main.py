61a65b33d4ef40635c029040ffa954fb3af548a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:07:43 +0000

    rubbish anxious mend comeover woman

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
