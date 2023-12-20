f7100c8b05e625beee2d7ab43919e62fd7ce1713
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:46:57 +0000

    congratulate talk medium blind bin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
