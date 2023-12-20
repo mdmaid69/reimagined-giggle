ac4532cd9452d0798f6b3a0032e12e9766c7144f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:23:11 +0000

    tongue speak challenge anywhere house

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
