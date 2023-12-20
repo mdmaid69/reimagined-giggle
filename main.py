ab216dc4f6977abab4d1ee9c15eb36380da7dcc7
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:06:07 +0000

    square basic culture me cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
