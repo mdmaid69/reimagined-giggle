0cfd7ee27b76299428b901c0500f7c996cab3cc6
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:20:38 +0000

    collection illness patient actual frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
