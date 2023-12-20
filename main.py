8a6dbec09dfb8ee9424a77d39eeaa5a83f1cd2e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:29:02 +0000

    ahead happen clean point sore

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
