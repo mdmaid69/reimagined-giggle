311756b3a1961a83ac156756cdce377144658841
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:20:47 +0000

    sad task demonstrate proof wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
