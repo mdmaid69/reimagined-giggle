de5d2dfeacc4495a25fca6f247992dedeca5b4a5
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:32:27 +0000

    help 179bite main cheek rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
