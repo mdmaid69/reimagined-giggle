be414ea8cb986813d49cf89aafc9d6f8da8e4aba
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:24:40 +0000

    study all public this bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def is_even(n):
+        return n % 2 == 0
