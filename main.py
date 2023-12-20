e151f1d4962aa94367241476e5dc64872e789027
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:15:51 +0000

    grow dream naturally ticket bus

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
