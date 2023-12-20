d3ff91589602911113b30c5278e1fd1a31a2e5f5
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:17:12 +0000

    invite hollow thumb weekend programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
