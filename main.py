5109d7e27b3968157bdac7721d7ae99d8a9cf060
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:30:38 +0000

    question pump stay justice blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
