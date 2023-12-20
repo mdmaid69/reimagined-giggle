d8fe49342613cf7f814b034e53698d9395c3ae3b
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:51:21 +0000

    black only listenfor accommodation approve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
