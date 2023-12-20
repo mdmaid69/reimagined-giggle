0806a5bb3f645f62aece275a20505b2df0b67d73
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:47:50 +0000

    freedom field team reason bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
