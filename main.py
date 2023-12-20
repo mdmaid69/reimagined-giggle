91096c2d9ce82e0a1d91873a4e4eed0a6ca1ac3d
mdmaid69 <robot@alexsyw.me>
2023-12-20 12:19:13 +0000

    unkind right 569enjoy entry disagree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
