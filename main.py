93f32b9e491468dd37756f736259858201ace548
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:29:38 +0000

    stare angry whenever pronunciation connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
