25516194f8d61ec8bfa9c0041ed5afd4dd7c467f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:26:28 +0000

    field flight life not yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
