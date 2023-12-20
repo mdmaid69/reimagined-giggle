5230a9305a605f8067c0ab76ec8372f31c622a32
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:30:03 +0000

    wind rush bush pass exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
