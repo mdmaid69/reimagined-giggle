a4bfadb81685d9919276500fd34e06caaaac5960
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:49:21 +0000

    rest formal turnback speak neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
