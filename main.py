4ad44c29e000a34ff295d516aed292c5c2a8c27c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:02:05 +0000

    shoulder freeze result petrol art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import sys
-print(sys.version)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
