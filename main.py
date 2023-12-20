619d52fa3538fded909038f03f783d49fed2f630
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:09:55 +0000

    trousers normal storm moment material

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_octal(n):
+        return oct(n)
