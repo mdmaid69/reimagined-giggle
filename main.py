703e4d73d924a1fcfea5ecdb56891cb9fe31006c
mdmaid69 <robot@alexsyw.me>
2023-12-20 08:11:02 +0000

    getup worth store address loose

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import array
 def get_array_buffer_info(array):
         return array.buffer_info()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
