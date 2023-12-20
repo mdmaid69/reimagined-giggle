7b70a19fdfab48f5ee76fa1d4269ed32c0f5b11f
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:39:56 +0000

    dig change centre or indeed

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def add_numbers(x, y):
+        return x + y
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
