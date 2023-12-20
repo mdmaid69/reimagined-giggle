3a98e5d5685a33ce692b076b298818e676cfe6b4
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:03:35 +0000

    break hurry goaway pipe snow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
