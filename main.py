21998811d7b99cfe80979760bfa376340ee7bdbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 20:39:41 +0000

    send previous thing personal middle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
