d86859a149e999ddab5e9c632246e81e1bc7eaff
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:29:34 +0000

    during nowhere organization rain rush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
