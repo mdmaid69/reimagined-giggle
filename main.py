a63bdc72c0db21541e615661334e45e8afe9ddaa
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:20:19 +0000

    comedown keepfrom scheme case unlikely

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
