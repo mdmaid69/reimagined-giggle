b1e0097506bfec3204b511ec28f48276384d2f3f
mdmaid69 <robot@alexsyw.me>
2023-12-20 03:29:44 +0000

    public beat shopping group wire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_username():
+        return getpass.getuser()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
