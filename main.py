0a804bb54c04abd85fbb5ae5cb6eecd703eca40e
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:01:10 +0000

    convince female tear snow tradition

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import tensorflow as tf
+print(tf.__version__)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
