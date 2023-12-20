a6a9bb47828de815afe82ececfd07f27167d09e0
mdmaid69 <robot@alexsyw.me>
2023-12-20 13:34:29 +0000

    damp cause software pen urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
