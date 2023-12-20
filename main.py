abaa55863d6e3535536af5ad9b71fa248527838b
mdmaid69 <robot@alexsyw.me>
2023-12-20 06:06:03 +0000

    strong rate squash avoid coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
