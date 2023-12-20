2f0ed6696e2739f2a0c3200a0929fe122f6e988f
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:37:48 +0000

    rid likely bush left dark

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_max(numbers):
-        return max(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
