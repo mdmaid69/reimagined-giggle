b99e5f97affd8f6da7642338ca3daa37e783dbb7
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:26:06 +0000

    write use disturb voice ring

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_logarithm(base, x):
+        return math.log(x, base)
