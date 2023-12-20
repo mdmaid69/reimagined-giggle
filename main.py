1a4162e15d27524026e84db12e52880f829bfb4d
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:03:47 +0000

    separate chin sad Cc comeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
