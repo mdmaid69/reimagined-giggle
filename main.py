d7208158c606c34a183e1804dc35c83044f04ab4
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:22:08 +0000

    connection table sort control everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def reverse_array(array):
+        array.reverse()
