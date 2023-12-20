7ea152d9d7ff967012924e4c065a226e6227b007
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:57:42 +0000

    worth Gg yesterday ashamed phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
