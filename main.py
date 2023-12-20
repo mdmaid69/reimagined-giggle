cdc6bcaa8f1deae11c4852667f100e3749d8b77b
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:24:33 +0000

    well begin copy bad behaviour

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
