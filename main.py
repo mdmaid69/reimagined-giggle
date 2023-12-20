a852a89df7bcbcebf4010adc5b7097da35f30a5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 18:07:15 +0000

    question achieve wander division bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
