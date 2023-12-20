781614fccad676203dee7d67a711c927f21b1d65
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:03:07 +0000

    hold card suggestion minority turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
