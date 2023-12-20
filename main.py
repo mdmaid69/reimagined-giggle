7b9e36edd9e11110df87610f4a66f0451ed38be9
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:14:54 +0000

    still once light involve profit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def is_odd(n):
+        return n % 2 != 0
+def find_union(list1, list2):
+        return set(list1) | set(list2)
