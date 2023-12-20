c7176332536edcd317d34fcf77c17d607b430a74
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:36:49 +0000

    song imagination 571enough vote act

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def get_current_time():
-        return time.time()
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
