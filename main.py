9efc32e89d2bed204139862a644234b71af23a69
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:55:17 +0000

    characteristic hurt steady during collect

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
