cf541ce3c3056b5bebc8c3c701ad4f9b2f5d7ccc
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:05:22 +0000

    proper impatient offence calmly clever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import getpass
-def get_username():
-        return getpass.getuser()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
