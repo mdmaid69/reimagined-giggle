f14700f5727a49f687300591bd9142f06beddc10
mdmaid69 <robot@alexsyw.me>
2023-12-20 23:17:29 +0000

    turnoff dig foot right talkback

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def is_odd(n):
-        return n % 2 != 0
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
