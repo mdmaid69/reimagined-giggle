38608923b4218aedd3411eb02cd66f5440520787
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:02:02 +0000

    sort office in wave chat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
