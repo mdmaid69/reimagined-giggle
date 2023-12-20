aad868a4ecf8fde9d1c671216795597e527fc417
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:34:19 +0000

    present try pity love pure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
