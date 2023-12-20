a83cd72a72df7004b81761bf74352b327b6fa2d1
mdmaid69 <robot@alexsyw.me>
2023-12-20 10:38:04 +0000

    amount that now reserve collect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_exponential(x):
+        return math.exp(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
