1be6838d5e70b240c81605ef0eb004992a8fff70
mdmaid69 <robot@alexsyw.me>
2023-12-20 19:52:17 +0000

    mass over understand question artificial

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_octal(n):
-        return oct(n)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
