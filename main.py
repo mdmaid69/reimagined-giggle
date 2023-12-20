768bf50ac453888367d905e016df1770032d1ce2
mdmaid69 <robot@alexsyw.me>
2023-12-20 04:04:48 +0000

    display press second brain forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
