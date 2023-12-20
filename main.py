f8a15177111b6455e08fa6d941d70cc4cb6b8b26
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:34:08 +0000

    exact settle nice frighten official

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
