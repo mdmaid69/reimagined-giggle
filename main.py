547e13922824cb9b7c9e24e72a7c1bc25d0a7bbb
mdmaid69 <robot@alexsyw.me>
2023-12-20 15:50:46 +0000

    bridge talk poor fold bunch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def print_python_version():
+        print(sys.version)
