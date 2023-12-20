4d08452529de8ccf9cc5c5749147c8a398df14e2
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:21:23 +0000

    wise beyond open gradually nonsense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_min(lst):
+        return min(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
