fc2bd4edf028a5688e7bce6779c4acaefe533acf
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:55:14 +0000

    study male union who 568energy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
