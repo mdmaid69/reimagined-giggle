16ed6c0c23c675209a6098dbe00468ff1135f67f
mdmaid69 <robot@alexsyw.me>
2023-12-20 09:20:17 +0000

    organization trip finish able around

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def print_python_version():
+        return sys.version
+import platform
+def get_python_version():
+        return platform.python_version()
