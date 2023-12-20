e97825a299e28867501141f3947e036dfb474f5f
mdmaid69 <robot@alexsyw.me>
2023-12-20 17:09:32 +0000

    ourselves dance later priority film

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
