fc7d09e354bc7a89f29eb7ae590b1b5a284f1b77
mdmaid69 <robot@alexsyw.me>
2023-12-20 01:36:10 +0000

    phone sight change press down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
