2b0b5d6e34f4e90be0143260111ba2908a1821be
mdmaid69 <robot@alexsyw.me>
2023-12-20 11:13:03 +0000

    unlucky getback fix lookback(on) takeover

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
