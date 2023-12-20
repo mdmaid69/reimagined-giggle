68d1866385744668bcda7dfaae85a7af37675131
mdmaid69 <robot@alexsyw.me>
2023-12-20 00:31:42 +0000

    mad within used strange reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def sort_list(lst):
-        return sorted(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
