7fdcb9fe7c629d7ece8e6e0e21f37be811a18110
mdmaid69 <robot@alexsyw.me>
2023-12-20 22:53:51 +0000

    play heat wherever watch left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
