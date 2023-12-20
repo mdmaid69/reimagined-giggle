01f2505cae25cad1a0aa5dc7c6ffd6879aac540a
mdmaid69 <robot@alexsyw.me>
2023-12-20 05:35:37 +0000

    first how couple age back

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
