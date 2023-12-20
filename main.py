e4346ebd3636cb2dc5031225d200d3a7bd7dc2d7
mdmaid69 <robot@alexsyw.me>
2023-12-20 14:37:42 +0000

    at spot pure compete story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
