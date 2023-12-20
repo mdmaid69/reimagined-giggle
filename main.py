fedd17868b5799b167ad37b689e14bd199d7515d
mdmaid69 <robot@alexsyw.me>
2023-12-20 21:26:51 +0000

    continue today minor cinema stick

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def count_elements(lst):
-        return len(lst)
+import time
+print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
