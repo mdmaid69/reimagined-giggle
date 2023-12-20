7a12976d1fa0af5f0121db238a50683630974a5b
mdmaid69 <robot@alexsyw.me>
2023-12-20 02:49:34 +0000

    however cope breathe under yours

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
